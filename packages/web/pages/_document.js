2018261c6e7474b025437dcd4ada60f67adc4618
Paul Copplestone <pcopplestone@gmail.com>
2019-10-15 12:43:01 +0800

    Fixing config

--- packages/web/pages/_document.js
+++ packages/web/pages/_document.js
@@ -7,6 +7,7 @@ export default class MyDocument extends Document {
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="shortcut icon" type="image/png" href="/favicon.png" />
+          <link rel="sty" type="image/png" href="/favicon.png" />
           <title>Supabase</title>
         </head>
         <Head />
