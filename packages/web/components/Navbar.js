aa93bdffe6bc7abc7848ff46c3466068fbfe8488
Paul Copplestone <pcopplestone@gmail.com>
2019-10-29 14:50:39 +0000

    Fixes the navbar link target

--- packages/web/components/Navbar.js
+++ packages/web/components/Navbar.js
@@ -35,7 +35,7 @@ export default function Navbar({ isFullwidth }) {
 
           <div className="navbar-end p-r-sm">
             <div className="navbar-item">
-              <Link href={`/docs/[category]/[slug]`} as={`/docs/realtime/introduction`}>
+              <Link href={`/docs/[category]/[slug]`} as={`/docs/-/about`}>
                 <a className="">Docs</a>
               </Link>
             </div>
