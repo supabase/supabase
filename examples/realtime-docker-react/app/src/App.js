e046cfd827a947fda88baec637f6c1898147012f
Paul Copplestone <pcopplestone@gmail.com>
2019-10-30 15:54:32 +0000

    Adding postgrest

--- examples/realtime-docker-react/app/src/App.js
+++ examples/realtime-docker-react/app/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react'
+import axios from 'axios'
 import './App.css';
 
 function App() {
+  const [firstName, setFirstName] = useState('')
+  const [userList, setUserList] = useState([{ first_name: 'Paul' }])
+  
+  const onAddUserClicked = async () => {
+    try {
+      addUser(firstName)
+      setFirstName('')
+    } catch (error) {
+      console.log('error', error)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div><input value={firstName} type="text" onChange={e => setFirstName(e.target.value)}/></div>
+      <div><button onClick={onAddUserClicked}>Add User</button></div>
+      <div>{userList.map(user => <p key={user.first_name}>{user.first_name}</p>)}</div>
     </div>
-  );
+  )
 }
 
 export default App;
+
+const addUser = async (first_name) => {
+  let url = `${window.location.host}:3000/users`
+  console.log('url', url)
+  return axios.post(url, { first_name })
+}
