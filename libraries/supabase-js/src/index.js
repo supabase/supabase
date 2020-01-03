58d64fdc4b5bf359398cdaa3d335f71d9e0155ae
Paul Copplestone <pcopplestone@gmail.com>
2020-01-03 07:05:53 +0000

    Starting the boilerplate NPM function

---
+++ libraries/supabase-js/src/index.js
@@ -0,0 +1,10 @@
+const defaultAwesomeFunction = (name) => {
+  const returnStr = `I am the Default Awesome Function, fellow comrade! - ${name}`;
+  return returnStr;
+};
+
+const awesomeFunction = () => 'I am just an Awesome Function';
+
+export default defaultAwesomeFunction;
+
+export { awesomeFunction };
