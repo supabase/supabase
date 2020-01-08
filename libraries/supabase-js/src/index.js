684524b43b2347300c8675707e9207504e239cff
Copple <pcopplestone@gmail.com>
2020-01-08 09:11:27 +0800

    Mocking up a basic supabase-js instance for further iteration

--- libraries/supabase-js/src/index.js
+++ libraries/supabase-js/src/index.js
@@ -1,3 +1,46 @@
+
+
+class SupabaseClient {
+  constructor(supbaseUrl, supabaseKey, options) {
+    this.listeners = []
+    this.supbaseUrl = supbaseUrl
+    this.supabaseKey = supabaseKey
+  }
+
+  /**
+   * @todo
+   */
+  subscribe () {
+    this.listeners.push()
+  }
+}
+
+/**
+ * Instantiate a new Supabase Client
+ * @param supbaseUrl
+ * @param supabaseKey
+ * @param options
+ */
+const createClient = (supbaseUrl, supabaseKey, options = {}) => {
+  return new SupabaseClient(supbaseUrl, supabaseKey, options)
+} 
+
+export { createClient }
+
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * REMOVE BELOW 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
 const defaultAwesomeFunction = (name) => {
   const returnStr = `I am the Default Awesome Function, fellow comrade! - ${name}`;
   return returnStr;
