2b6692ff6a8cc0483ccfd142a15a25464518e1d9
awalias <antwilson@hotmail.co.uk>
2020-01-17 19:10:12 +0800

    react-live magic on world example

--- examples/world/pages/index.js
+++ examples/world/pages/index.js
@@ -1,38 +1,45 @@
 import { createClient } from '@supabase/supabase-js'
+import { useState, useEffect } from 'react';
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
 
 export default class Index extends React.Component {
-  constructor() {
-    super()
-    this.supabase = createClient('https://world.supabase.co', 'FkGhDLmT3V')
-
-    this.state = {
-      countries : []
-    }
-  }
-
-  async getCountries() {
-    let countries = await this.supabase
-      .from('countries')
-      .select("*")
-  
-    this.setState({ countries: countries.body })
-  }
-
-  componentDidMount() {
-    this.getCountries();
-  }
-
   render() {
-    const countryNames = []
+    const code = `
+    function World() {
+      const [countries, setCountries] = useState(<div>Hello wordl</div>);
 
-    for (const country of this.state.countries) {
-      countryNames.push(<li key={country.name}>{country.name}</li>)
+      getCountries = async function() {
+        this.supabase = createClient('https://world.supabase.co', 'FkGhDLmT3V')
+
+        let countries = await this.supabase
+          .from('countries')
+          .select("*")
+      
+        const countryNames = countries.body.map(country => <li key={country.name}>{country.name}</li>)
+
+        setCountries(countryNames);
+      }
+
+      useEffect(() => {
+        getCountries();
+      }, []);
+
+      return (
+        <div>
+          Hello
+          {countries}
+        </div>
+      )
+
     }
+    `
 
     return (
-      <div style={styles.main}>
-        {countryNames}
-      </div>
+      <LiveProvider code={code} scope={{createClient, useState, useEffect}}>
+        <LiveEditor />
+        <LiveError />
+        <LivePreview />
+      </LiveProvider>
     )
   }
 }
