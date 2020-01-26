3f2c2beec642aa72fce51fdf76f938574dc80e49
Angelico <angelico.delosreyes@gmail.com>
2020-01-26 23:36:51 +0800

    #12 fix .like() & .ilike()

--- libraries/supabase-js/src/utils/Filters.js
+++ libraries/supabase-js/src/utils/Filters.js
@@ -1,39 +1,43 @@
-export function Eq(columnName, filterValue){
-    return `${columnName}=eq.${filterValue}`
+export function Eq(columnName, filterValue) {
+  return `${columnName}=eq.${filterValue}`
 }
 
-export function Gt(columnName, filterValue){
-    return `${columnName}=gt.${filterValue}`
+export function Gt(columnName, filterValue) {
+  return `${columnName}=gt.${filterValue}`
 }
 
-export function Lt(columnName, filterValue){
-    return `${columnName}=lt.${filterValue}`
+export function Lt(columnName, filterValue) {
+  return `${columnName}=lt.${filterValue}`
 }
 
-export function Gte(columnName, filterValue){
-    return `${columnName}=gte.${filterValue}`
+export function Gte(columnName, filterValue) {
+  return `${columnName}=gte.${filterValue}`
 }
 
-export function Lte(columnName, filterValue){
-    return `${columnName}=lte.${filterValue}`
+export function Lte(columnName, filterValue) {
+  return `${columnName}=lte.${filterValue}`
 }
 
-export function Like(columnName, stringPattern){
-    return `${columnName}=like.${stringPattern}`
+export function Like(columnName, stringPattern) {
+  let stringPatternEnriched = stringPattern.replace(/%/g, '*')
+
+  return `${columnName}=like.${stringPatternEnriched}`
 }
 
-export function Ilike(columnName, stringPattern){
-    return `${columnName}=ilike.${stringPattern}`
+export function Ilike(columnName, stringPattern) {
+  let stringPatternEnriched = stringPattern.replace(/%/g, '*')
+
+  return `${columnName}=ilike.${stringPatternEnriched}`
 }
 
-export function Is(columnName, filterValue){
-    return `${columnName}=is.${filterValue}`
+export function Is(columnName, filterValue) {
+  return `${columnName}=is.${filterValue}`
 }
 
-export function In(columnName, filterArray){
-    return `${columnName}=in.${filterArray.join(',')}`
+export function In(columnName, filterArray) {
+  return `${columnName}=in.${filterArray.join(',')}`
 }
 
-export function Not(columnName, filterValue){
-    return `${columnName}=not.${filterValue}`
+export function Not(columnName, filterValue) {
+  return `${columnName}=not.${filterValue}`
 }
