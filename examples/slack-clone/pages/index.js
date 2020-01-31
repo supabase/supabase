d92bdb954005423901ec1ec0b3233a1452f99c08
Paul Copplestone <pcopplestone@gmail.com>
2020-01-31 20:07:38 +0800

    FIxing class -> className

--- examples/slack-clone/pages/index.js
+++ examples/slack-clone/pages/index.js
@@ -10,23 +10,23 @@ const Home = props => {
   }
 
   return (
-    <div class="container mx-auto h-full flex justify-center items-center mt-8">
-      <div class="w-1/3 mt-8">
-        <div class="border-teal p-8 border-t-12 bg-white mb-6 rounded-lg shadow-lg">
-          <div class="mb-4">
-            <label class="font-bold text-grey-darker block mb-2">Username</label>
+    <div className="container mx-auto h-full flex justify-center items-center mt-8">
+      <div className="w-1/3 mt-8">
+        <div className="border-teal p-8 border-t-12 bg-white mb-6 rounded-lg shadow-lg">
+          <div className="mb-4">
+            <label className="font-bold text-grey-darker block mb-2">Username</label>
             <input
               type="text"
-              class="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow"
+              className="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow"
               placeholder="Your Username"
             />
           </div>
 
-          <div class="flex items-center justify-between">
+          <div className="flex items-center justify-between">
             <a
               onClick={handleLogin}
               href={'/channels'}
-              class="bg-gray-900 hover:bg-teal text-white font-bold py-2 px-4 rounded"
+              className="bg-gray-900 hover:bg-teal text-white font-bold py-2 px-4 rounded"
             >
               Login
             </a>
