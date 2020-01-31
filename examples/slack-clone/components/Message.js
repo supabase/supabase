ebef5a56949d6aaea0a6e8213812ba197a7bb77b
awalias <antwilson@hotmail.co.uk>
2020-01-31 15:28:51 +0800

    added intermediate state to enable live messages

--- examples/slack-clone/components/Message.js
+++ examples/slack-clone/components/Message.js
@@ -2,7 +2,7 @@ const Message = ({ message }) => (
   <>
     <div className="flex items-center">
       <div className="">
-        <p className="text-white font-bold">{message.author.username}</p>
+        <p className="text-white font-bold">{message.user_id}</p>
         <p className="text-white">{message.message}</p>
       </div>
     </div>
