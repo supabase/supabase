ff5d58d73d251c9004088b91329e6a81262d2f43
Paul Copplestone <pcopplestone@gmail.com>
2020-02-04 12:48:20 +0800

    Creating full example with scrolling, fake auth, and store

--- examples/slack-clone/components/MessageInput.js
+++ examples/slack-clone/components/MessageInput.js
@@ -13,7 +13,6 @@ const MessageInput = ({onSubmit}) => {
   
   return (
     <>
-      <div className="mb-4">
         <input
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           type="text"
@@ -22,7 +21,6 @@ const MessageInput = ({onSubmit}) => {
           onChange={e => setMessageText(e.target.value)}
           onKeyDown={e => submitOnEnter(e)}
         />
-      </div>
     </>
   )}
 
