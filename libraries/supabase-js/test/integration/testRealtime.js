24afbf36ccf340d822643c3ac7fe1b2656012129
awalias <antwilson@hotmail.co.uk>
2020-02-20 12:22:43 +0800

    array tests now passing

--- libraries/supabase-js/test/integration/testRealtime.js
+++ libraries/supabase-js/test/integration/testRealtime.js
@@ -9,24 +9,17 @@ import { createClient } from '../../src'
 describe('test subscribing to an insert', () => {
   const supabase = createClient('http://localhost:8000', 'examplekey')
 
-  afterEach(async() => {
-    console.log(supabase.getSubscriptions())
-    for (let subscription of supabase.getSubscriptions()) {
-      supabase.removeSubscription(subscription)
-    }
-  })
-
-  it('on() and subscribe()', done => {
+  it('on() and subscribe()', async () => {
     function callbackAction(record) {
       assert(record.new.message === 'hello', 'inserted message is incorrect')
-      done()
+      console.log("ASSERT MADE")
     }
-    supabase
+    await supabase
       .from('messages')
       .on('*', callbackAction)
       .subscribe()
 
-    supabase.from('messages').insert([{ message: 'hello', user_id: 1, channel_id: 1 }])
+    await supabase.from('messages').insert([{ message: 'hello', user_id: 1, channel_id: 1 }])
   }).timeout(10000)
 })
 
