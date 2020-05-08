f81760cab2cc0f7a171fb4ec31dcad46b38df973
Paul Copplestone <pcopplestone@gmail.com>
2020-05-08 14:32:20 +0800

    Adds an OG image

--- web/docusaurus.config.js
+++ web/docusaurus.config.js
@@ -21,6 +21,7 @@ module.exports = {
     forceDarkMode: true,
     darkMode: true,
     image: 'favicon.png', // used for meta tag, in particular og:image and twitter:image
+    metaImage: '/img/supabase-og-image.png',
     googleAnalytics: {
       trackingID: 'UA-155232740-1',
     },
