a518a6fea5bdcfb7d17a8d5dca715adfe1ecc267
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-06-01 17:33:44 +0100

    Remove unused import `UseEffect`.

--- examples/slack-clone-basic/components/Layout.js
+++ examples/slack-clone-basic/components/Layout.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { useContext, useEffect } from 'react'
+import { useContext } from 'react'
 import UserContext from '~/lib/UserContext'
 import { addChannel } from '~/lib/Store'
 
