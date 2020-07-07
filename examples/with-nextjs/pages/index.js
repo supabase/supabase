407bbecf7e924f1daf93d88472727b0a88d8aeba
awalias <antwilson@hotmail.co.uk>
2020-07-07 12:56:17 +0800

    file moved

--- examples/with-nextjs/pages/index.js
+++ examples/with-nextjs/pages/index.js
@@ -1,6 +1,6 @@
 import useSWR from 'swr'
 import Link from 'next/link'
-import { useUser } from '../utils/auth/useUser'
+import { useUser } from '../lib/useUser'
 
 const fetcher = (url, token) =>
   fetch(url, {
