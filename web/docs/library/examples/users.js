494938c3f64c4acdaa55d39c91f43206a43ef6d6
Thorsten Schaeff <thorsten@stripe.com>
2020-08-08 17:59:14 +0800

    docs: Fix user auth code snipptes.

--- web/docs/library/examples/users.js
+++ web/docs/library/examples/users.js
@@ -1,29 +1,25 @@
 export const signup = `
-let { body: user } = await supabase
-  .auth
-  .signup( 
-    'someone@email.com', 
-    'fOdaPdyTpkpxJgDVIORt' 
-  )
+const {
+  body: { user },
+} = await supabase.auth.signup(
+  'someone@email.com',
+  'fOdaPdyTpkpxJgDVIORt'
+)
 `.trim()
 
 export const login = `
-let { body: user } = await supabase
-  .auth
-  .login( 
-    'someone@email.com', 
-    'fOdaPdyTpkpxJgDVIORt' 
-  )
+const {
+  body: { user },
+} = await supabase.auth.login(
+  'someone@email.com',
+  'fOdaPdyTpkpxJgDVIORt'
+)
 `.trim()
 
 export const userData = `
-let { body: user } = await supabase
-  .auth
-  .user()
+const user = await supabase.auth.user()
 `.trim()
 
 export const logout = `
-let { body: loggedOut } = await supabase
-  .auth
-  .logout()
+await supabase.auth.logout()
 `.trim()
