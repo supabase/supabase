0367b9cffdfdf9139f843002e13b838abb59844f
awalias <antwilson@hotmail.co.uk>
2020-08-28 20:06:45 +0800

    feature: adds docs for password reset and email confirmation

--- web/docs/library/examples/users.js
+++ web/docs/library/examples/users.js
@@ -23,3 +23,55 @@ const user = await supabase.auth.user()
 export const logout = `
 await supabase.auth.logout()
 `.trim()
+
+export const requestRecoveryTokenEmail = `
+axios.post(
+  'https://<your-ref>.supabase.co/auth/v1/recover',
+  {
+    email : '<the-users-email>'
+  },
+  {
+    headers : [
+      'Content-Type': 'application/json',
+      'apikey': '<your-supabase-api-key>' 
+    ]
+  })
+  .then(console.log)
+  .catch(console.error)
+`.trim()
+
+export const verifyRecoveryToken = `
+axios.post(
+  'https://<your-ref>.supabase.co/auth/v1/verify',
+  {
+    type : 'recovery',
+    token : '<the-recovery-token>'
+  },
+  {
+    headers : [
+      'Content-Type': 'application/json',
+      'apikey': '<your-supabase-api-key>' 
+    ]
+  })
+  .then(res => {
+    console.log(res.data)
+  })
+  .catch(console.error)
+`.trim()
+
+export const updateUser = `
+axios.put(
+  'https://<your-ref>.supabase.co/auth/v1/user',
+  {
+    password : '<some-new-password>'
+  },
+  {
+    headers : [
+      'Content-Type': 'application/json',
+      'apikey': '<your-supabase-api-key>',
+      'Authorization': 'Bearer <users-access-token>' 
+    ]
+  })
+  .then(console.log)
+  .catch(console.error)
+`.trim()
