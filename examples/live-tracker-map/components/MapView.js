166f6487eaa32c8b9bb66d9df87cbed5103b16dc
Pham Hieu <phamhieu1998@gmail.com>
2020-09-09 08:53:20 +0800

    chore: update live-tracker-map example with supabase-auth

--- examples/live-tracker-map/components/MapView.js
+++ examples/live-tracker-map/components/MapView.js
@@ -1,46 +1,68 @@
 import { useState, useEffect, useRef } from 'react'
 import { MapContainer, TileLayer, Polyline } from 'react-leaflet'
-import { RoundToFixDecimals } from "lib/utils"
+import { RoundToFixDecimals } from "lib/Utils"
 import TextLog from "components/TextLog"
 
-function MapView({ supabase, clientRef, center, zoom }) {
+function randomColor() {
+  var array = ["black", "magenta", "blue", "indigo", "green", "blueviolet"];
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function MapView({ supabase, center, zoom }) {
   const [log, setLog] = useState(undefined)
   const [positions, setPositions] = useState([])
+  const [userPositionsDict, setUserPositionsDict] = useState([])
   const mySubscription = useRef(false)
 
   useEffect(() => {
-    let newLog = `Ref: ${clientRef}\nStart listerning...`
-    newLog += positions.map(item => { return `\nid=${item.id} lat=${RoundToFixDecimals(item.lat)} long=${RoundToFixDecimals(item.lng)}` })
+    let newLog = `Start listerning...`
+    newLog += positions.map(item => { return `\nuser_id=${item.user_id} lat=${RoundToFixDecimals(item.lat)} long=${RoundToFixDecimals(item.lng)}` })
     setLog(newLog)
-  }, [positions, clientRef])
+  }, [positions])
 
   useEffect(() => {
     // Listen to INSERT event on locations table
     mySubscription.current = supabase
       .from('locations')
       .on('INSERT', payload => {
+        console.log('Change received!', payload)
         const { new: newItem } = payload
-        const { id, ref, latitude, longitude } = newItem
-        if (ref === clientRef) {
-          console.log('Change received!', payload)
-          setPositions([...positions, { id, ref, lat: latitude, lng: longitude }])
-        }
+        const { id, user_id, latitude, longitude } = newItem
+
+        let userPositions = userPositionsDict[user_id]
+        if (userPositions) userPositions.data = [...userPositions.data, { id, user_id, lat: latitude, lng: longitude }]
+        else userPositions = { color: randomColor(), data: [{ id, user_id, lat: latitude, lng: longitude }] }
+
+        setPositions([...positions, { id, user_id, lat: latitude, lng: longitude }])
+        setUserPositionsDict({ ...userPositionsDict, [user_id]: userPositions })
       })
       .subscribe()
 
     return () => {
       if (mySubscription.current) supabase.removeSubscription(mySubscription.current)
     }
-  }, [supabase, clientRef, positions, setPositions])
+  }, [supabase, positions, setPositions, userPositionsDict, setUserPositionsDict])
 
-  function drawPolyline() {
-    const temp = positions.map(item => {
+  function drawPolylines() {
+    // console.log(userPositionsDict)
+    function drawPolyline(color, data) {
+      const polyline = data.map(item => {
+        if (!item) return
+        const { lat, lng } = item
+        return [lat, lng]
+      })
+      return <Polyline pathOptions={{ color: color }} positions={polyline} />
+    }
+
+    let result = Object.keys(userPositionsDict).map(key => {
+      const item = userPositionsDict[key]
       if (!item) return
-      const { lat, lng } = item
-      return [lat, lng]
+
+      const { color, data } = item
+      return drawPolyline(color, data)
     })
-    const polyline = [center, ...temp]
-    return <Polyline pathOptions={{ color: 'black' }} positions={polyline} />
+
+    return result
   }
 
   return (
@@ -50,14 +72,9 @@ function MapView({ supabase, clientRef, center, zoom }) {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {drawPolyline()}
+        {drawPolylines()}
       </MapContainer>
       <TextLog log={log} />
-
-      <style jsx>{`
-        .map-view {
-        }
-      `}</style>
     </div>
   )
 }
--- examples/live-tracker-map/pages/index.js
+++ examples/live-tracker-map/pages/index.js
@@ -1,75 +1,45 @@
 import { useState, useEffect } from 'react'
+import Link from 'next/link'
 import Head from 'next/head'
-import dynamic from 'next/dynamic'
-import { createClient } from '@supabase/supabase-js'
-import { nanoid } from 'nanoid'
+import styles from 'styles/Home.module.css'
 
-const MapInput = dynamic(
-  () => import('components/MapInput'),
-  { ssr: false }
-)
-
-const MapView = dynamic(
-  () => import('components/MapView'),
-  { ssr: false }
-)
-
-export default function Home() {
-  const [clientRef, setClientRef] = useState(null)
-  const apiEndpoint = process.env.NEXT_PUBLIC_SUPABASE_ENDPOINT || "https://BeQiprVORewGZImzsKAS.supabase.co"
-  const apiKey = process.env.NEXT_PUBLIC_SUPABASE_APIKEY || "IHDL7hnmTSlqQ1fm7kYw5SBQPY11Rp"
-  // Create a single supabase client for interacting with your database 
-  const supabase = createClient(apiEndpoint, apiKey);
-  const center = {
-    lat: 1.3489728457596013,
-    lng: 103.77043978311998
-  }
-  const zoomLevel = 14
-
-  useEffect(() => {
-    let ref = localStorage.getItem('_client-ref')
-    if (!ref) {
-      ref = nanoid()
-      localStorage.setItem('_client-ref', ref)
-    }
-    setClientRef(ref)
-  }, [])
-
+export default function Page() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <Head>
         <title>Live Tracker</title>
         <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
-          integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
-          crossOrigin=""
-        />
-        <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
-          integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
-          crossOrigin=""></script>
       </Head>
-      <main>
-        <h1 className="title">
+      <main className={styles.main}>
+        <h1 className={styles.title}>
           Live Tracker
         </h1>
 
-        <p className="description">
-          Get started by drag-drop the marker around to simulate location update
+        <p className={styles.description}>
+          Get started by choosing your role as Driver or Manager.
+        </p>
+        <p className={styles.sub_description}>
+          You can only login 1 role at a time. Please use incognito window to test multi users.
         </p>
 
-        <div className="grid">
-          <div className="card">
-            <MapInput supabase={supabase} clientRef={clientRef} center={center} zoom={zoomLevel} />
-          </div>
-          <div className="card">
-            <MapView supabase={supabase} clientRef={clientRef} center={center} zoom={zoomLevel - 1} />
-          </div>
+        <div className={styles.grid}>
+          <Link href="/driver">
+            <a className={styles.card}>
+              <h3>Driver View &rarr;</h3>
+              <p>Find in-depth information about Next.js features and API.</p>
+            </a>
+          </Link>
+          <Link href="/manager">
+            <a className={styles.card}>
+              <h3>Manager View &rarr;</h3>
+              <p>Find in-depth information about Next.js features and API.</p>
+            </a>
+          </Link>
         </div>
+
       </main>
 
-      <footer>
+      <footer className="footer">
         <a
           href="https://supabase.io"
           target="_blank"
@@ -79,117 +49,6 @@ export default function Home() {
           <img src="/supabase.svg" alt="Supabase Logo" className="logo" />
         </a>
       </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-
-          width: 100%;
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-grow: 1;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-          .card {
-            width: 100%;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-
-        .leaflet-container {
-          min-height: 20rem;
-        }
-      `}</style>
     </div>
   )
 }
