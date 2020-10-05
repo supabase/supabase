ef027b11919d1fa64e71b26e56f4364b972d3f9d
Duncan Healy <duncan.healy@gmail.com>
2020-10-05 18:50:47 +0100

    formatting, remove ts-jest #215

--- examples/sveltejs-todo/svelte.config.js
+++ examples/sveltejs-todo/svelte.config.js
@@ -1,11 +1,11 @@
-const sveltePreprocess = require("svelte-preprocess");
+const sveltePreprocess = require('svelte-preprocess')
 
 const preprocess = sveltePreprocess({
   postcss: {
-    plugins: [require("tailwindcss"), require("autoprefixer")],
+    plugins: [require('tailwindcss'), require('autoprefixer')],
   },
-});
+})
 
 module.exports = {
   preprocess,
-};
+}
