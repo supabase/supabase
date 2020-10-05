f37598ee75d2580fb61234be31a782f0dd557624
Duncan Healy <duncan.healy@gmail.com>
2020-10-05 19:33:23 +0100

    feat add user to todo svelte changes for #215

--- examples/sveltejs-todo/src/LoggedIn.svelte
+++ examples/sveltejs-todo/src/LoggedIn.svelte
@@ -1,73 +1,15 @@
 <script>
   import TailwindStyles from './TailwindStyles.svelte'
-  import Home from './Home'
-  import Items from './Items'
-  import { getLists, getItems, items } from './items'
-  import { addTask, updateTask, createList, fetchList } from './store'
+  import Lists from './Lists.svelte'
+  import { items } from './items'
   export let user // = localStorage.getItem("user")
   let lists
-  let todos
-  let LIST_ID
-
+  // let todos
+  let list_id
   $: lists = items()
-  LIST_ID = lists && lists.length > 0 ? lists[0].id : 1
+  list_id = lists && lists.length > 0 ? lists[0].id : 2
 
-  $: todos = lists && lists.length > 0 ? lists[0].tasks : []
-  if(!lists) {
-    createList(uuid())
-  }
-  // || createList(LIST_ID)
-  const ENTER_KEY = 13
-  const ESCAPE_KEY = 27
-  let currentFilter = 'all'
-  let editing = null
+  // $: todos = lists && lists.length > 0 ? lists[0].tasks : []
 
-  const updateView = () => {
-    console.log({ lists, todos })
-    currentFilter = 'all'
-    if (window.location.hash === '#/active') {
-      currentFilter = 'active'
-    } else if (window.location.hash === '#/completed') {
-      currentFilter = 'completed'
-    }
-  }
-  window.addEventListener('hashchange', updateView)
-  updateView()
-
-  function createNew(event) {
-    if (event.which === ENTER_KEY) {
-      addTask(event.target.value, LIST_ID)
-      event.target.value = ''
-    }
-  }
-
-  function uuid() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = (Math.random() * 16) | 0,
-        v = c == 'x' ? r : (r & 0x3) | 0x8
-      return v.toString(16)
-    })
-  }
-
-
-</script>
-<pre>
-  {JSON.stringify(user)}
-</pre>
-{#await lists then lists}
-
-  <header class="todos-center flex">
-    <h1 class="rounded border border-teal-500 border-solid px-20 ">todos</h1>
-
-    <!-- svelte-ignore a11y-autofocus -->
-    <input
-      class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
-      on:keydown={createNew}
-      placeholder="What needs to be done?"
-      autofocus />
-  </header>
-
-
-  <Items items={lists[0].tasks} bind:currentFilter bind:editing />
-
-{/await}
+  </script>
+  <Lists user_id={user.id} lists={lists} list_id={list_id} />
