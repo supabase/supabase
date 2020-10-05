f5cf0e39aca4c32f2331083923581f6f04f3078c
Duncan Healy <duncan.healy@gmail.com>
2020-10-05 23:30:21 +0100

    feat: add items per todo list #215

--- examples/sveltejs-todo/src/LoggedIn.svelte
+++ examples/sveltejs-todo/src/LoggedIn.svelte
@@ -3,13 +3,13 @@
   import Lists from './Lists.svelte'
   import { items } from './items'
   export let user // = localStorage.getItem("user")
-  let lists
+  // let lists
   // let todos
-  let list_id
+
   $: lists = items()
-  list_id = lists && lists.length > 0 ? lists[0].id : 2
+
 
   // $: todos = lists && lists.length > 0 ? lists[0].tasks : []
 
   </script>
-  <Lists user_id={user.id} lists={lists} list_id={list_id} />
+  <Lists user_id={user.id} lists={lists}  />
