548d33ad942b1fdc5da03e1775c7a8b388dacd02
Muhammad Rivki <muhammad.rivki@live.com>
2020-10-08 01:36:38 +0700

    style: fix linter issues

--- supabase-ui/src/components/Button/Button.stories.js
+++ supabase-ui/src/components/Button/Button.stories.js
@@ -1,14 +1,10 @@
-import React from 'react';
+import React from 'react'
 
 import { Button } from '.'
 
 export default {
   title: 'Basic/Button',
   component: Button,
-};
+}
 
-const Template = (args) => <Button {...args} />;
-
-export const Primary = (args) => <Button>
-  Some Children
-</Button>
+export const Primary = (args) => <Button>Some Children</Button>
