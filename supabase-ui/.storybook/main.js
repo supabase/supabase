3619e8ab8eba3b7dfcdf3e4eb7cf32874c9a9a3f
Muhammad Rivki <muhammad.rivki@live.com>
2020-10-08 01:40:46 +0700

    style: remove comment

--- supabase-ui/.storybook/main.js
+++ supabase-ui/.storybook/main.js
@@ -9,25 +9,11 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ],
-  // babel: (config) => {
-  //   const babelConfig = babel.loadPartialConfig({
-  //     configFile: require.resolve('../babel.config'),
-  //   });
-  //   if (!babelConfig) {
-  //     throw new Error('Failed to load Babel config');
-  //   }
-  //   return {
-  //     ...config,
-  //     ...babelConfig.options,
-  //   };
-  // },
   webpackFinal: async config => {
-
     config.resolve.alias = {
       '@emotion/styled/base': '@emotion/styled-base'
     }
 
     return config;
-
   }
 }
