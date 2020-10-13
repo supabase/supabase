190ae775096b2b789f74b35f28036809cdc25b17
Duncan Healy <duncan.healy@gmail.com>
2020-10-13 19:57:19 +0100

    format: svelte code #215

--- examples/sveltejs-todo/src/Login.svelte
+++ examples/sveltejs-todo/src/Login.svelte
@@ -7,13 +7,12 @@
   import Supabase from '@supabase/supabase-js'
   let importEnv = true
   try {
-    if(process.env.NODE_ENV==="test")
-      importEnv =false
-  } catch (error) {
-    
-  }
+    if (process.env.NODE_ENV === 'test') importEnv = false
+  } catch (error) {}
 
-  const { SNOWPACK_PUBLIC_SUPABASE_URL, SNOWPACK_PUBLIC_SUPABASE_KEY } = !importEnv ? process.env : import.meta.env
+  const { SNOWPACK_PUBLIC_SUPABASE_URL, SNOWPACK_PUBLIC_SUPABASE_KEY } = !importEnv
+    ? process.env
+    : import.meta.env
 
   const supabase = Supabase.createClient(SNOWPACK_PUBLIC_SUPABASE_URL, SNOWPACK_PUBLIC_SUPABASE_KEY)
   let values = {}
