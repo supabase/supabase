defd35a4b539ef56b3dcb3d683682d7859d3f718
Duncan Healy <duncan.healy@gmail.com>
2020-10-23 16:33:55 +0100

    add future feature to tailwind

--- examples/sveltejs-todo/tailwind.config.js
+++ examples/sveltejs-todo/tailwind.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+    standardFontWeights: true,
+    defaultLineHeights: true,
+  },
   purge: ['./src/**/*.svelte', './src/**/*.html'],
   theme: {
     extend: {
