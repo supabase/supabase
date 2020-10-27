b43a9720bb43a680f37729b1d8cae03751f7c19f
Paul Copplestone <pcopplestone@gmail.com>
2020-10-27 19:56:13 +0800

    Moves most of the stored procedure filters

--- web/docs/library/examples/stored.js
+++ web/docs/library/examples/stored.js
@@ -42,77 +42,19 @@ const { data, error } = await supabase
   .match({name: 'Beijing', country_id: 156})
 `.trim()
 
-export const storedEqJs = `
-const { data, error } = await supabase
-  .rpc('echo_all_cities')
-  .eq('name', 'San Francisco')
-`.trim()
 
-export const storedNeqJs = `
-const { data, error } = await supabase
-  .rpc('echo_all_cities')
-  .neq('name', 'Lagos')
-`.trim()
 
-export const storedGtJs = `
-const { data, error } = await supabase
-  .rpc('echo_all_cities')
-  .gt('country_id', 250)
-`.trim()
 
-export const storedLtJs = `
-const { data, error } = await supabase
-  .rpc('echo_all_cities')
-  .lt('country_id', 250)
-`.trim()
 
-export const storedGteJs = `
-const { data, error } = await supabase
-  .rpc('echo_all_cities')
-  .gte('country_id', 250)
-`.trim()
 
-export const storedLteJs = `
-const { data, error } = await supabase
-  .rpc('echo_all_cities')
-  .lte('country_id', 250)
-`.trim()
 
-export const storedLikeJs = `
-const { data, error } = await supabase
-  .rpc('echo_all_cities')
-  .like('name', '%la%')
-`.trim()
 
-export const storedIlikeJs = `
-const { data, error } = await supabase
-  .rpc('echo_all_cities')
-  .ilike('name', '%la%')
-`.trim()
 
-export const storedIsJs = `
-const { data, error } = await supabase
-  .rpc('echo_all_cities')
-  .is('name', null)
-`.trim()
 
-export const storedInJs = `
-const { data, error } = await supabase
-  .rpc('echo_all_cities')
-  .in('name', ['Rio de Janeiro', 'San Francisco'])
-`.trim()
 
-export const storedCsJs = `
-const { data, error } = await supabase
-  .rpc('echo_all_countries')
-  .cs('main_exports', ['oil'])
-`.trim()
 
-export const storedCdJs = `
-const { data, error } = await supabase
-  .rpc('echo_all_countries')
-  .cd('main_exports', ['cars', 'food', 'machine'])
-`.trim()
+
+
 
 export const storedOvaJs = `
 const { data, error } = await supabase
@@ -126,32 +68,6 @@ const { data, error } = await supabase
   .ovr('population_range_millions', [150, 250])
 `.trim()
 
-export const storedSlJs = `
-const { data, error } = await supabase
-  .rpc('echo_all_countries')
-  .sl('population_range_millions', [150, 250])
-`.trim()
 
-export const storedSrJs = `
-const { data, error } = await supabase
-  .rpc('echo_all_countries')
-  .sr('population_range_millions', [150, 250])
-`.trim()
 
-export const storedNxlJs = `
-const { data, error } = await supabase
-  .rpc('echo_all_countries')
-  .nxl('population_range_millions', [150, 250])
-`.trim()
 
-export const storedNxrJs = `
-const { data, error } = await supabase
-  .rpc('echo_all_countries')
-  .nxr('population_range_millions', [150, 250])
-`.trim()
-
-export const storedAdjJs = `
-const { data, error } = await supabase
-  .rpc('echo_all_countries')
-  .adj('population_range_millions', [70, 185])
-`.trim()
