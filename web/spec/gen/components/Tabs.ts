eecf153874bece5e055c7d2d276386ad56e50f30
Paul Copplestone <pcopplestone@gmail.com>
2020-11-02 11:03:26 +0800

    Adds a default value to all of the Tabs

--- web/spec/gen/components/Tabs.ts
+++ web/spec/gen/components/Tabs.ts
@@ -1,6 +1,7 @@
 const Tabs = (libraries = [], examples = []) =>
   `
 <Tabs
+  defaultValue="js"
   groupId="libraries"
   values={[${libraries.map((x) => `{ label: '${x.name}', value: '${x.id}' }`).toString()}]}>
 
