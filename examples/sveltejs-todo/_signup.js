8ad7a52b4229fe59e44746203821175873334ebc
Duncan Healy <duncan.healy@gmail.com>
2020-11-03 11:51:53 +0000

    change to login supabase 1.0.2

--- examples/sveltejs-todo/_signup.js
+++ examples/sveltejs-todo/_signup.js
@@ -9,6 +9,6 @@ const {
 // const [SNOWPACK_PUBLIC_SUPABASE_URL,SNOWPACK_PUBLIC_SUPABASE_KEY] =
 const supabase = Supabase.createClient(SNOWPACK_PUBLIC_SUPABASE_URL, SNOWPACK_PUBLIC_SUPABASE_KEY)
 ;(async () => {
-  let { body: user } = await supabase.auth.signup(SNOWPACK_PUBLIC_USER, SNOWPACK_PUBLIC_PASSWORD)
-  console.log({ user })
+  let { data, user, error } = await supabase.auth.signUp({email: SNOWPACK_PUBLIC_USER, password:SNOWPACK_PUBLIC_PASSWORD})
+  console.log({ data, user, error })
 })()
