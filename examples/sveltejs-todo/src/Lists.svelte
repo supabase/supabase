3bf678f67a659ad3df5ab656d1d23c50fca16e06
Duncan Healy <duncan.healy@gmail.com>
2020-11-11 15:45:25 +0000

    style list add new #215

--- examples/sveltejs-todo/src/Lists.svelte
+++ examples/sveltejs-todo/src/Lists.svelte
@@ -51,7 +51,10 @@
 
 <header class="todos-center flex">
   <h1 class="rounded border border-teal-500 border-solid px-20 ">todos</h1>
-  <button on:click="{createNewList}">add new list</button>
+  <button 
+  class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded text-green-700 border-green-500 hover:text-green-500 hover:bg-green-50 focus:outline-none focus:shadow-outline focus:border-green-300 transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10 float-left"
+
+  on:click="{createNewList}">add new list</button>
 </header>
 
 {#await lists}
