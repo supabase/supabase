271e1460f2b934e56ccb5b763763e4e19e1c0826
Alex Chiu <alexchiu11@gmail.com>
2020-11-14 16:40:37 +0000

    Show Auth component when no userSession is available, if there is, user is logged in so show Todo List

--- examples/todo-vue3-ts/src/App.vue
+++ examples/todo-vue3-ts/src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="app" class="w-full h-full bg-gray-300">
-    <div v-if="false" class="w-full h-full flex justify-center items-center p-4">
+    <div v-if="userSession === null" class="w-full h-full flex justify-center items-center p-4">
       <Auth />
     </div>
     <div v-else class="w-full h-full flex flex-col justify-center items-center p-4 max-w-sm m-auto">
       <TodoList />
+      <button class="btn-black w-full mt-12" @click="logout">
+        Logout
+      </button>
     </div>
   </div>
 </template>
@@ -12,13 +15,15 @@
 <script>
 import Auth from '@/components/Auth.vue'
 import TodoList from '@/components/TodoList.vue'
+import { userSession, logout } from '@/vuetils/useAuth'
+
 export default {
   components: {
     Auth,
     TodoList,
   },
   setup() {
-    return {}
+    return { userSession, logout }
   },
 }
 </script>
