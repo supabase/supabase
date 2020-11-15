15acb2a268f95be3e871e6f99ffb3c5bf9c4c46b
Alex Chiu <alexchiu11@gmail.com>
2020-11-15 19:52:35 +0000

    Iterate over allTodos and render Todo components

--- examples/todo-vue3-ts/src/components/TodoList.vue
+++ examples/todo-vue3-ts/src/components/TodoList.vue
@@ -2,21 +2,15 @@
   <div class="w-full">
     <h1 class="mb-12 font-bold text-6xl">Todo List.</h1>
     <div class="flex gap-2 my-2">
-      <input
-        class="rounded w-full p-2"
-        type="text"
-        placeholder="make coffee"
-        value="{newTaskText}"
-      />
+      <input class="rounded w-full p-2" type="text" placeholder="What do you need to?" />
       <button class="btn-black">
         Add
       </button>
     </div>
 
     <div class="bg-white shadow overflow-hidden rounded-md">
-      <ul>
-        <!-- for loop around a todo component -->
-        <Todo :todo="{task: 'test todo'}"/>
+      <ul v-for="(todo, index) in allTodos" :key="index">
+        <Todo :todo="todo" />
       </ul>
     </div>
   </div>
@@ -25,11 +19,18 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Todo from '@/components/Todo.vue'
+import { allTodos } from '@/vuetils/useTodo'
 
 export default defineComponent({
   name: 'TodoList',
   components: {
     Todo,
+  },
+
+  setup() {
+    return {
+      allTodos,
+    }
   },
 })
 </script>
