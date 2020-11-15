c5f95731f902ae0f72b62f02aa2d0e2552449c19
Duncan Healy <duncan.healy@gmail.com>
2020-11-15 16:21:23 +0000

    fix login for first time #215

--- examples/sveltejs-todo/src/LoggedIn.svelte
+++ examples/sveltejs-todo/src/LoggedIn.svelte
@@ -1,16 +1,13 @@
 <script>
-  import Lists from './Lists.svelte'
-  import { items, subscription } from './items'
+
+  import LoginLists from './LoginLists.svelte'
+  import { items } from './items'
   export let user
   let lists
-  $: {
-    lists = items()
-  }
-  // $: lists2 = subscription()
-  // lists2.socket.isConnected() && lists2.socket.connectionState()
+  $: lists = items()
 </script>
 
-<Lists user_id="{user.id}" lists="{lists}" />
+<LoginLists user_id="{user.id}" lists="{lists}" />
 <button
   class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded text-green-700 border-green-500 hover:text-green-500 hover:bg-green-50 focus:outline-none focus:shadow-outline focus:border-green-300 transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10 float-left"
   on:click="{() => {
