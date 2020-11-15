5819cd31acd1e4e95d3aecf4b68d5dfa5b35c11a
Alex Chiu <alexchiu11@gmail.com>
2020-11-15 09:59:57 +0000

    lint

--- examples/todo-vue3-ts/src/main.ts
+++ examples/todo-vue3-ts/src/main.ts
@@ -4,11 +4,10 @@ import { supabase } from './lib/supabase'
 import { userSession } from '@/vuetils/useAuth'
 import './assets/tailwind.css'
 
-createApp(App)
-  .mount('#app')
+createApp(App).mount('#app')
 
 supabase.auth.onAuthStateChange((event, session) => {
-  console.log("Auth Event", event)
-  console.log("supabase session", session)
-  userSession.value = session;
+  console.log('Auth Event', event)
+  console.log('supabase session', session)
+  userSession.value = session
 })
