12caa5477e1ff1dfbdb947f16b7ff8de4e669a74
Alex Chiu <alexchiu11@gmail.com>
2020-11-16 17:45:41 +0000

    Code tidyup added in comments

--- examples/todo-vue3-ts/src/vuetils/useAuth.ts
+++ examples/todo-vue3-ts/src/vuetils/useAuth.ts
@@ -4,6 +4,10 @@ import { Session, Provider } from '@supabase/gotrue-js/dist/main/lib/types'
 
 const userSession = ref<Session | null>(null)
 
+/*
+* Handles user login via email + password into a supabase session.
+* If not password is empty, it will send a magic link to the users email address.
+*/
 async function handleLogin(credentials: Credentials) {
   try {
     const { error, user } = await supabase.auth.signIn({
@@ -24,9 +28,13 @@ async function handleLogin(credentials: Credentials) {
   }
 }
 
+/*
+* Handles signup provided a valid credentials object.
+*/
 async function handleSignup(credentials: Credentials) {
   try {
     const { email, password } = credentials
+    // prompt user if they have not filled populated thier credentials
     if (email === undefined || password === undefined) {
       alert('Form cant be empty')
       return
@@ -44,11 +52,18 @@ async function handleSignup(credentials: Credentials) {
   }
 }
 
+/** 
+* Handles signup via Third Pary Login.
+* https://supabase.io/docs/guides/auth#third-party-logins
+*/
 async function handleOAuthLogin(provider: Provider) {
   const { error } = await supabase.auth.signIn({ provider })
   if (error) console.error('Error: ', error.message)
 }
 
+/**
+* Handles password reset. Will send an email to the given email address .
+*/
 async function handlePasswordReset() {
   const email = prompt('Please enter your email:')
   if (email === null || email === '') {
@@ -63,22 +78,24 @@ async function handlePasswordReset() {
   }
 }
 
+/**
+* Handles logging a user out of a superbase session  
+*/
 async function handleLogout() {
   console.log('logging out')
   try {
     const { error } = await supabase.auth.signOut()
 
     if (error) {
-      alert(error.message)
-      alert('error signing out')
-      console.error('Error!', error)
+      alert('Error signing out')
+      console.error('Error', error)
       return
     }
 
     alert('You have signed out!')
   } catch (err) {
-    alert('Fatal error signing out')
-    console.error('Error!', err)
+    alert('Unknown error signing out')
+    console.error('Error', err)
   }
 }
 
