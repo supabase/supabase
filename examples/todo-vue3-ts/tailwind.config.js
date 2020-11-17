4cce54cbc0f62076062d65ca38fbbad3d2f747c5
Alex Chiu <alexchiu11@gmail.com>
2020-11-17 10:25:18 +0000

    Example: Todo app using Vue + TS (#290)
    * init new vue 3 project
    
    * Installed Tailwind
    
    * Removed vue router and rationalised boilerplate components
    
    * Scaffolded main components + styling and positioning
    
    * styling tweaks on todo component
    
    * Installed supabase
    
    * Track changes to the supabase user session
    
    * helper to initalise supabase
    
    * setup global ts types file
    
    * Show Auth component when no userSession is available, if there is, user is logged in so show Todo List
    
    * lint
    
    * Composition function for auth. HandleLogin, hadle forgot password & logout
    
    * Consume useAuth composition function in Auth component
    
    * function renaming
    
    * consume handle signup function and new function names
    
    * consume new function name
    
    * Added Footer component
    
    * Implemented oAuth signin
    
    * Spacing tweak
    
    * Setup Todo component
    
    * lint
    
    * lint
    
    * allowed for todo prop in component
    
    * retreive todos and store in state
    
    * Testing todo component with fake todo
    
    * handle nulls in supabase call
    
    * Iterate over allTodos and render Todo components
    
    * spinner component
    
    * Error handling on fetching todos
    
    * consume fetch todos function
    
    * Implementing Suspense and loading screen
    
    * implemented update task completion
    
    * added interface for Todos based on db schema
    
    * Implemented insert task
    
    * Implemented composition functions for adding and updating records
    
    * tweaks
    
    * Implemented deletetion of todo
    
    * Updated readme
    
    * Included example env.local file
    
    * Update README.md
    
    * Code tidyup added in comments
    
    * Added comment
    
    * Code cleanup
    
    * Code cleanup and added in comments
    
    * Added comments
    
    * Code cleanup
    
    * lint

---
+++ examples/todo-vue3-ts/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
