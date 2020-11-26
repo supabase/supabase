aad5a9f8c7ccb766dfa68234251e32e6e4787e3c
mildtomato <jon.summers.muir@googlemail.com>
2020-11-26 11:06:27 +0800

    add copy code functionality into code block example component
    - added new package `react-copy-to-clipboard`
    - just note that @ts-ignore is being used and needs fixing

--- www/components/index_sections/CodeExamples.tsx
+++ www/components/index_sections/CodeExamples.tsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
+
 import { Light as SyntaxHighlighter } from 'react-syntax-highlighter'
 import js from 'react-syntax-highlighter/dist/cjs/languages/hljs/javascript'
 import rainbow from 'react-syntax-highlighter/dist/cjs/styles/hljs/rainbow'
+
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 
 import { examples } from './../../data/CodeExamples'
 
@@ -9,6 +12,7 @@ SyntaxHighlighter.registerLanguage('javascript', js)
 
 const CodeExamples = () => {
   const [example, setExample] = useState('subscribeExample')
+  const [copied, setCopied] = useState(false)
 
   const exampleList = ['subscribeExample', 'readExample', 'createExample', 'updateExample']
 
@@ -16,6 +20,7 @@ const CodeExamples = () => {
 
   function handleClick(key: string) {
     setExample(key)
+    setCopied(false)
   }
 
   const buttons = exampleList.map((id) => {
@@ -23,7 +28,10 @@ const CodeExamples = () => {
       <button
         type="button"
         onClick={() => handleClick(id)}
-        className={"m-1 mb-4 rounded-md border border-gray-200 px-4 py-2 text-base font-medium text-gray focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-transparent sm:w-auto sm:text-sm" + (example === id ? " border-brand-100 bg-brand-100 text-brand" : " hover:bg-gray-200")}
+        className={
+          'm-1 mb-4 rounded-md border border-gray-200 px-4 py-2 text-base font-medium text-gray focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-transparent sm:w-auto sm:text-sm' +
+          (example === id ? ' border-brand-100 bg-brand-100 text-brand' : ' hover:bg-gray-200')
+        }
       >
         {
           // @ts-ignore
@@ -37,31 +45,31 @@ const CodeExamples = () => {
     <div className="grid grid-cols-12 gap-2">
       <div className="col-span-12 text-center">
         {buttons}
-        {/* <button
-          type="button"
-          className="mt-12 inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-brand-600 hover:bg-brand-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-500"
-        >
-          <svg
-            className="-ml-0.5 mr-2 h-4 w-4"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-            <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-          </svg>
-          Explore documentation
-        </button> */}
       </div>
       <div className="col-span-8 col-start-3">
         <div className="rounded" style={{ background: '#474949' }}>
-          <p className="text-base text-gray-400 text-white p-2 pl-5">
+          <div className="p-2 pl-5 flex justify-between items-center">
+          <p className="text-base text-gray-400 text-white">
             {
               // @ts-ignore
               examples[example].description
             }
           </p>
+          <CopyToClipboard
+            text={
+              // @ts-ignore
+              examples[example].code[lang]
+            }
+            onCopy={() => setCopied(true)}
+          >
+            <button
+              type="button"
+              className="inline-flex items-center px-2.5 py-1.5 border border-gray-500 shadow-sm text-xs font-medium rounded text-gray-300 hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              {copied ? 'Copied!' : 'Copy code'}
+            </button>
+          </CopyToClipboard>
+          </div>
           <SyntaxHighlighter
             // startingLineNumber={3}
             language="javascript"
