cbc47c949e3a7fd5557548733eeb55d50dd0a2ca
Joshen Lim <joshenlimek@gmail.com>
2020-11-28 04:14:11 +0800

    Reduce logo size in header

--- www/components/nav.tsx
+++ www/components/nav.tsx
@@ -127,12 +127,12 @@ const Nav = () => {
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex-shrink-0 flex items-center">
               <img
-                className="block lg:hidden h-8 w-auto"
+                className="block lg:hidden h-6 w-auto"
                 src="images/logo-light.png"
                 alt="Workflow"
               />
               <img
-                className="hidden lg:block h-8 w-auto"
+                className="hidden lg:block h-6 w-auto"
                 src="images/logo-light.png"
                 alt="Workflow"
               />
