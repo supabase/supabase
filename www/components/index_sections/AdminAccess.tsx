dc155befcb4c0b1bf1412d792f69cce12b990d71
Joshen Lim <joshenlimek@gmail.com>
2020-11-27 18:38:11 +0800

    Refactored AdminAccess and tabs and added screen grab videos

--- www/components/index_sections/AdminAccess.tsx
+++ www/components/index_sections/AdminAccess.tsx
@@ -1,17 +1,33 @@
+import React, { useState, ReactElement } from 'react'
 import Tabs from '../tabs'
 
 const AdminAccess = () => {
+  const [tabId, setTabId] = useState('tabTableEditor')
+
   return (
     <div className="py-16 bg-gray-50 overflow-hidden lg:py-24">
       <div className="relative max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
-        <div className="relative mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
+        <div className="relative mt-12 items-center mt-24">
           <div className="relative">
             <small>MADE FOR DEVELOPERS</small>
-            <h2 className="text-3xl leading-8 font-bold tracking-tight text-gray-900 sm:text-4xl">
+            <h2 className="mt-4 text-3xl leading-8 font-medium tracking-tight text-gray-900 sm:text-4xl">
               Organise your app, without a developer
             </h2>
-            <div className="mt-6">
-            <Tabs />
+            <div className="mt-6 grid grid-cols-2 gap-52 items-center">
+              <Tabs
+                tabId={tabId}
+                setTabId={setTabId}
+              />
+              <div>
+                <div className="w-full rounded-t-md h-5 bg-gray-700 flex items-center justify-start px-2">
+                  <div className="h-2 w-2 mr-2 rounded-full bg-gray-500"/>
+                  <div className="h-2 w-2 mr-2 rounded-full bg-gray-500"/>
+                  <div className="h-2 w-2 mr-2 rounded-full bg-gray-500"/>
+                </div>
+                <video className="rounded-b-md shadow-md" src={`videos/${tabId}.mp4`} autoPlay loop muted>
+                  Your browser does not support the video tag
+                </video>
+              </div>
             </div>
           </div>
         </div>
--- www/components/tabs.tsx
+++ www/components/tabs.tsx
@@ -1,17 +1,12 @@
-import React, { useState, ReactElement } from 'react'
-
-type Props = {
-  children: ReactElement[]
-  event: any
-  target: HTMLInputElement
-  tabMarkup: any
-  tabId: string
+type Props = { 
+  tabId: string,
+  setTabId: Function,
 }
 
-function Tabs() {
-  const [tabId, setTabId] = useState('tabTableEditor')
+function Tabs(props: Props) {
+  const { tabId, setTabId } = props
 
-  function handleTabChange(event: Props) {
+  function handleTabChange(event: any) {
     //@ts-ignore
     event.preventDefault()
     const tabId: string = event.target.id
@@ -19,97 +14,77 @@ function Tabs() {
   }
 
   const tabMarkup: any = {
-    tabTableEditor: (
-      <div>
-        <div className="mt-5">
-          <dd className="mt-2 text-base text-gray-800">
-            Manage your data with the familiarity of a spreadsheet
-          </dd>
-          <dd className="mt-2 text-base text-gray-500">
-            You don’t have to be a database expert to start using Supabase — Our table editor makes
-            Postgres easy to use even for non-techies.
-          </dd>
-        </div>
-        <button
-          type="button"
-          className="mt-6 inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-brand-600 hover:bg-brand-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-500"
-        >
-          Explore table view
-        </button>
-      </div>
-    ),
-    tabSqlEditor: (
-      <div>
-        <div className="mt-5">
-          <dd className="mt-2 text-base text-gray-800">
-            In-built SQL editor for when you need greater control
-          </dd>
-          <dd className="mt-2 text-base text-gray-500">
-            Write, save, and execute SQL queries directly on our dashboard, with templates to help
-            you save time from common queries and even build applications.
-          </dd>
-        </div>
-        <button
-          type="button"
-          className="mt-6 inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-brand-600 hover:bg-brand-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-500"
-        >
-          Explore SQL editor
-        </button>
-      </div>
-    ),
-    tabAuthRules: (
-      <div>
-        <div className="mt-5">
-          <dd className="mt-2 text-base text-gray-800">
-            User management as straight-forward as it can be
-          </dd>
-          <dd className="mt-2 text-base text-gray-500">
-            Easily manage your users via our authentication, or integrate third party logins. Create
-            policies with complex SQL rules which fit your unique business needs.
-          </dd>
-        </div>
-        <button
-          type="button"
-          className="mt-6 inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-brand-600 hover:bg-brand-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-500"
-        >
-          Explore auth policies
-        </button>
-      </div>
-    ),
+    tabTableEditor: {
+      header: 'Manage your data with the familiarity of a spreadsheet',
+      description: 'You don’t have to be a database expert to start using Supabase — Our table editor makes Postgres easy to use even for non-techies, and you can do everything right in our dashboard.',
+      cta: 'Explore Table View'
+    },
+    tabSqlEditor: {
+      header: 'In-built SQL editor for when you need greater control',
+      description: 'Write, save, and execute SQL queries directly on our dashboard, with templates to help you save time from common queries and even build applications.',
+      cta: 'Explore SQL Editor'
+    },
+    tabAuthRules: {
+      header: 'User management as straight-forward as it can be',
+      description: 'Easily manage your users via our authentication, or integrate third party logins. Create policies with complex SQL rules which fit your unique business needs.',
+      cta: 'Explore Auth Policies'
+    }
   }
 
   return (
-    <div>
-      <div className="border-b border-gray-200">
-        <nav className="-mb-px flex" aria-label="Tabs">
-          <a
-            href="#"
-            id="tabTableEditor"
-            className={"w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm" + (tabId === "tabTableEditor" ? " border-brand-500 text-brand-600" : " border-b border-gray-200 text-gray-500 hover:text-gray-700 hover:border-gray-300")}
-            onClick={(event: any) => handleTabChange(event)}
-          >
-            Table editor
-          </a>
-          <a
-            href="#"
-            id="tabSqlEditor"
-            className={"w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm" + (tabId === "tabSqlEditor" ? " border-brand-500 text-brand-600" : " border-b border-gray-200 text-gray-500 hover:text-gray-700 hover:border-gray-300")}
-            onClick={(event: any) => handleTabChange(event)}
-          >
-            SQL editor
-          </a>
-          <a
-            href="#"
-            id="tabAuthRules"
-            className={"w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm" + (tabId === "tabAuthRules" ? " border-brand-500 text-brand-600" : " border-b border-gray-200 text-gray-500 hover:text-gray-700 hover:border-gray-300")}
-            onClick={(event: any) => handleTabChange(event)}
-            aria-current="page"
-          >
-            Auth rules
-          </a>
-        </nav>
+    <div className="flex justify-between items-center w-full">
+      <div>
+        <div className="border-b border-gray-200">
+          <nav className="-mb-px flex" aria-label="Tabs">
+            <a
+              href="#"
+              id="tabTableEditor"
+              className={"w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm" + (tabId === "tabTableEditor" ? " border-brand-500 text-brand-600" : " border-b border-gray-200 text-gray-500 hover:text-gray-700 hover:border-gray-300")}
+              onClick={(event: any) => handleTabChange(event)}
+            >
+              Table Editor
+            </a>
+            <a
+              href="#"
+              id="tabSqlEditor"
+              className={"w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm" + (tabId === "tabSqlEditor" ? " border-brand-500 text-brand-600" : " border-b border-gray-200 text-gray-500 hover:text-gray-700 hover:border-gray-300")}
+              onClick={(event: any) => handleTabChange(event)}
+            >
+              SQL Editor
+            </a>
+            <a
+              href="#"
+              id="tabAuthRules"
+              className={"w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm" + (tabId === "tabAuthRules" ? " border-brand-500 text-brand-600" : " border-b border-gray-200 text-gray-500 hover:text-gray-700 hover:border-gray-300")}
+              onClick={(event: any) => handleTabChange(event)}
+              aria-current="page"
+            >
+              Auth Rules
+            </a>
+          </nav>
+        </div>
+
+        {/* Might want to extract this out into a component within this file too */}
+        <div className="mt-6">
+          <div>
+            <div className="mt-5">
+              <dd className="mt-8 text-2xl text-gray-800">
+                {tabMarkup[tabId].header}
+              </dd>
+              <dd className="mt-8 text-base text-gray-500">
+                {tabMarkup[tabId].description}
+              </dd>
+            </div>
+            <button
+              type="button"
+              className="mt-8 inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 rounded-md text-white bg-brand-600 hover:bg-brand-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-500"
+            >
+              {tabMarkup[tabId].cta}
+            </button>
+          </div>
+        </div>
+
       </div>
-      <div className="mt-6">{tabMarkup[tabId]}</div>
     </div>
   )
 }
