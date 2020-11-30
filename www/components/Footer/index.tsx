d64cd98c2a3f1ce0d1003bdb683dea2a29b6ff87
Joshen Lim <joshenlimek@gmail.com>
2020-11-30 23:02:38 +0800

    Fix a couple of dark mode stuff

--- www/components/Footer/index.tsx
+++ www/components/Footer/index.tsx
@@ -61,7 +61,7 @@ const Footer = (props: Props) => {
   )
 
   return (
-    <footer className="bg-white dark:bg-dark-400" aria-labelledby="footerHeading">
+    <footer className="bg-white dark:bg-dark-500" aria-labelledby="footerHeading">
       <h2 id="footerHeading" className="sr-only">Footer</h2>
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
         <div className="xl:grid xl:grid-cols-3 xl:gap-8">
@@ -129,7 +129,7 @@ const Footer = (props: Props) => {
               className={`
                 relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer 
                 transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 
-                focus:ring-brand-500 ${darkMode ? 'bg-dark-200' : 'bg-gray-200'} mx-5
+                focus:ring-brand-500 ${darkMode ? 'bg-dark-300' : 'bg-gray-200'} mx-5
               `}
               onClick={() => toggleDarkMode()}
             >
--- www/components/Nav/FlyOut.tsx
+++ www/components/Nav/FlyOut.tsx
@@ -59,7 +59,7 @@ const FlyOut = () => {
       <a
         key={name}
         href="#"
-        className="-m-3 p-3 flex flex-col justify-between rounded-lg hover:bg-gray-50 dark:hover:bg-dark-600 transition ease-in-out duration-150"
+        className="-m-3 p-3 flex flex-col justify-between rounded-lg hover:bg-gray-50 dark:hover:bg-dark-300 transition ease-in-out duration-150"
       >
         {content}
       </a>
@@ -104,7 +104,7 @@ const FlyOut = () => {
       >
         <>
           <div
-            className="absolute inset-x-0 transform shadow-lg border-t-2 border-gray-50 dark:border-dark-600"
+            className="absolute inset-x-0 transform shadow-lg border-t-2 border-gray-50 dark:border-dark-200"
             style={{
               zIndex: 999,
               position: 'absolute',
@@ -116,8 +116,8 @@ const FlyOut = () => {
             }}
           >
             <div className="absolute inset-0 flex" aria-hidden="true">
-              <div className="bg-white dark:bg-dark-300 w-1/2"></div>
-              <div className="bg-gray-50 dark:bg-dark-200 w-1/2"></div>
+              <div className="bg-white dark:bg-dark-400 w-1/2"></div>
+              <div className="bg-gray-50 dark:bg-dark-300 w-1/2"></div>
             </div>
             <div className="relative max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2">
               <nav
@@ -135,7 +135,7 @@ const FlyOut = () => {
                     <li className="flow-root">
                       <a
                         href="#"
-                        className="-m-3 p-3 flex rounded-lg hover:bg-gray-100 dark:hover:bg-dark-600 transition ease-in-out duration-150"
+                        className="-m-3 p-3 flex rounded-lg hover:bg-gray-100 dark:hover:bg-dark-200 transition ease-in-out duration-150"
                       >
                         <div className="hidden sm:block flex-shrink-0">
                           <img
@@ -158,7 +158,7 @@ const FlyOut = () => {
                     <li className="flow-root">
                       <a
                         href="#"
-                        className="-m-3 p-3 flex rounded-lg hover:bg-gray-100 dark:hover:bg-dark-600 transition ease-in-out duration-150"
+                        className="-m-3 p-3 flex rounded-lg hover:bg-gray-100 dark:hover:bg-dark-200 transition ease-in-out duration-150"
                       >
                         <div className="hidden sm:block flex-shrink-0">
                           <img
