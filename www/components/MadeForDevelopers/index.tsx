c3bb6aca313e93d072213ccb87fd0fe4611d14c2
mildtomato <jon.summers.muir@googlemail.com>
2020-12-01 01:26:16 +0800

    fixed responsiveness on mobile for code examples

--- www/components/MadeForDevelopers/index.tsx
+++ www/components/MadeForDevelopers/index.tsx
@@ -5,19 +5,20 @@ import CodeExamples from 'components/MadeForDevelopers/CodeExamples'
 const MadeForDevelopers = () => {
   return (
     <div className="py-16 bg-gray-50 dark:bg-dark-400 overflow-hidden lg:py-16">
-      <div className="container mx-auto sm:px-16  xl:px-20">
+      <div className="container mx-auto sm:px-16 xl:px-20">
 
-      <SectionHeader className="text-center sm:w-3/4 xl:w-2/3 mx-auto" title={'Supafast easy-to-use APIs'} title_alt={' that do the hard work for you'} subtitle={'MADE FOR DEVELOPERS'} paragraph={'We inspect your database and provide APIs instantly so you can stop building repetitive CRUD endpoints and focus on your product.'} />
+      <SectionHeader className="text-center mx-auto" title={'Supafast easy-to-use APIs'} title_alt={' that do the hard work for you'} subtitle={'MADE FOR DEVELOPERS'} paragraph={'We inspect your database and provide APIs instantly so you can stop building repetitive CRUD endpoints and focus on your product.'} />
 
-        <div className="relative lg:grid lg:grid-cols-12 lg:gap-8 lg:items-center">
+        <div className="relative grid grid-cols-12 gap-8 lg:items-center">
 
           <div className="mt-2 col-span-10 col-start-2">
+
             <CodeExamples />
 
             <div className="grid grid-cols-12 gap-2">
-              <dl className="mt-12 space-y-10 md:grid md:grid-cols-2 md:col-span-12 lg:space-y-0 lg:col-start-2 lg:col-span-10 lg:gap-12 xl:col-start-3 xl:col-span-8">
-                <div className="md:border-r-2 md:border-gray-200 pr-6 dark:border-dark-200">
-                  <div className="mt-5">
+              <dl className="mt-12 grid-cols-12 grid lg:space-y-0 gap-12 col-start-1 col-span-12 md:col-span-12 md:col-start-1 lg:col-start-3 lg:col-span-8">
+                <div className="col-span-12 lg:col-span-6 lg:border-r-2 lg:border-gray-200 pr-6 dark:border-dark-200">
+                  <div className="lg:mt-5">
                     <dt className="text-lg leading-6 font-medium text-gray-900 dark:text-white">
                       TypeScript support
                     </dt>
@@ -32,8 +33,8 @@ const MadeForDevelopers = () => {
                     </button>
                   </div>
                 </div>
-                <div>
-                  <div className="mt-5">
+                <div className="col-span-12 lg:col-span-6">
+                  <div className="lg:mt-5">
                     <dt className="text-lg leading-6 font-medium text-gray-900 dark:text-white flex">
                       Local emulator
                       <Badge>Coming Q1 2021</Badge>
