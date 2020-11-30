878e7f028c125f56cd3972f90e7531f4c1fcd25f
Joshen Lim <joshenlimek@gmail.com>
2020-12-01 01:33:00 +0800

    Add favicon and update some meta

--- www/components/meta.tsx
+++ www/components/meta.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
+import { DESCRIPTION, HOME_OG_IMAGE_URL } from '../lib/constants'
 
 const Meta = () => {
   return (
@@ -28,13 +28,13 @@ const Meta = () => {
         color="#000000"
       />
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="msapplication-TileColor" content="#1E1E1E" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-      <meta name="theme-color" content="#000" />
+      <meta name="theme-color" content="#1E1E1E" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta
         name="description"
-        content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
+        content={DESCRIPTION}
       />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
--- www/pages/index.tsx
+++ www/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { CMS_NAME } from 'lib/constants'
+import { APP_NAME } from 'lib/constants'
 
 import Container from 'components/container'
 import Layout from 'components/layout'
@@ -20,7 +20,7 @@ const Index = ({  }: Props) => {
     <>
       <Layout>
         <Head>
-          <title>{CMS_NAME}</title>
+          <title>{APP_NAME}</title>
         </Head>
         <Container>
           <Hero />
