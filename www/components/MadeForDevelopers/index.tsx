70ccc975cb3da74d4b24ac64c1e638b56043f3b2
Joshen Lim <joshenlimek@gmail.com>
2020-12-01 20:41:33 +0800

    update dev features in made for dev section

--- www/components/MadeForDevelopers/index.tsx
+++ www/components/MadeForDevelopers/index.tsx
@@ -3,6 +3,8 @@ import Badge from 'components/Badge'
 import SectionHeader from 'components/UI/SectionHeader'
 import CodeExamples from 'components/MadeForDevelopers/CodeExamples'
 
+import DeveloperFeatures from 'data/DeveloperFeatures.json'
+
 const MadeForDevelopers = () => {
   return (
     <div className="py-16 bg-gray-50 dark:bg-dark-400 overflow-hidden lg:py-16">
@@ -26,30 +28,28 @@ const MadeForDevelopers = () => {
             <CodeExamples />
 
             <div className="grid grid-cols-12 gap-2">
-              <dl className="mt-12 grid-cols-12 grid col-span-12 sm:gap-4 md:col-span-12 lg:gap-8 lg:space-y-0 lg:col-span-10 xl:col-span-8">
-                <div className="col-span-12 mb-10 sm:mb-0 sm:col-span-6 sm:border-r-2 sm:border-gray-200 sm:pr-4 md:pr-6 dark:border-dark-200">
-                  <div className="lg:mt-5">
-                    <dt className="text-lg leading-6 font-medium text-gray-900 dark:text-white">
-                      TypeScript support
-                    </dt>
-                    <dd className="mt-3 text-base text-gray-500 mb-6 dark:text-dark-100">
-                      Type definitions for both server side and client side
-                    </dd>
-                    <Button type="secondary" text="Explore more" url="#" />
-                  </div>
-                </div>
-                <div className="col-span-12 sm:col-span-6">
-                  <div className="lg:mt-5">
-                    <dt className="text-lg leading-6 font-medium text-gray-900 dark:text-white flex">
-                      Local emulator
-                      <Badge className="ml-3">Q1 2021</Badge>
-                    </dt>
-                    <dd className="mt-3 text-base text-gray-500 mb-6 dark:text-dark-100">
-                      Develop locally and push to production when you're ready
-                    </dd>
-                    <Button type="secondary" text="Get notified" url="#" />
-                  </div>
-                </div>
+              <dl className="mt-12 grid-cols-12 grid col-span-12 sm:gap-4 lg:gap-8 lg:space-y-0">
+
+                {DeveloperFeatures.map((feature: any, idx: number) => {
+                  const blockClass = idx !== DeveloperFeatures.length - 1
+                    ? 'col-span-12 mb-10 sm:mb-0 sm:col-span-3 sm:border-r-2 sm:border-gray-200 sm:pr-4 md:pr-6 dark:border-dark-200'
+                    : 'col-span-12 sm:col-span-3'
+
+                  return (
+                    <div className={blockClass} key={`dev_feature_${idx}`}>
+                      <div className="lg:mt-5">
+                        <dt className="text-lg leading-6 font-medium text-gray-900 dark:text-white flex md:flex-col xl:flex-row">
+                          {feature.name}
+                          {feature.badge && <Badge className="ml-3 md:ml-0 md:w-20 md:mt-2 xl:ml-3 xl:mt-0 xl:w-auto">{feature.badge}</Badge>}
+                        </dt>
+                        <dd className="mt-3 text-base text-gray-500 mb-6 dark:text-dark-100">
+                          {feature.description}
+                        </dd>
+                        <Button type="secondary" text={feature.badge ? "Get notified" : "Explore more"} url="#" />
+                      </div>
+                    </div>
+                  )
+                })}
               </dl>
             </div>
 
