4ed5b0cfb6c0f1797f5cae4998d482eaa6491c52
Paul Copplestone <pcopplestone@gmail.com>
2020-12-02 12:45:12 +0800

    prettier

--- www/components/Footer/index.tsx
+++ www/components/Footer/index.tsx
@@ -62,15 +62,23 @@ const Footer = (props: Props) => {
 
   return (
     <footer className="bg-white dark:bg-dark-500" aria-labelledby="footerHeading">
-      <h2 id="footerHeading" className="sr-only">Footer</h2>
+      <h2 id="footerHeading" className="sr-only">
+        Footer
+      </h2>
       <div className="max-w-7xl mx-auto py-12 px-8 lg:py-16 lg:px-8">
         <div className="xl:grid xl:grid-cols-3 xl:gap-8">
           <div className="space-y-8 xl:col-span-1">
-            <img className="h-10 w-auto" src={darkMode ? "images/logo-dark.png" : "images/logo-light.png"} alt="Supabase" />
+            <img
+              className="h-10 w-auto"
+              src={darkMode ? 'images/logo-dark.png' : 'images/logo-light.png'}
+              alt="Supabase"
+            />
             <p className="text-gray-500 text-base sr-only">Any description</p>
             <div className="flex space-x-6">
-
-              <a href="https://twitter.com/supabase_io" className="text-gray-400 hover:text-gray-500">
+              <a
+                href="https://twitter.com/supabase_io"
+                className="text-gray-300 hover:text-gray-400"
+              >
                 <span className="sr-only">Twitter</span>
                 <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                   <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
@@ -87,7 +95,6 @@ const Footer = (props: Props) => {
                   />
                 </svg>
               </a>
-
             </div>
           </div>
           <div className="mt-12 grid grid-cols-1 gap-8 xl:mt-0 xl:col-span-2">
@@ -103,10 +110,18 @@ const Footer = (props: Props) => {
                         <li key={`${segment.title}_link_${idx}`}>
                           <a
                             href={link.url}
-                            className={`text-base ${link.url ? 'text-gray-500 dark:text-dark-100' : 'text-gray-400 dark:text-dark-200'} hover:text-gray-900 dark:hover:text-gray-300`}
+                            className={`text-base ${
+                              link.url
+                                ? 'text-gray-500 dark:text-dark-100'
+                                : 'text-gray-400 dark:text-dark-200'
+                            } hover:text-gray-900 dark:hover:text-gray-300`}
                           >
                             {link.text}
-                            {!link.url && <span className="block text-sm text-gray-300 dark:text-dark-300">Coming soon</span>}
+                            {!link.url && (
+                              <span className="block text-sm text-gray-300 dark:text-dark-300">
+                                Coming soon
+                              </span>
+                            )}
                           </a>
                         </li>
                       ))}
@@ -118,9 +133,7 @@ const Footer = (props: Props) => {
           </div>
         </div>
         <div className="mt-12 border-t border-gray-200 dark:border-dark-300 pt-8 flex justify-between">
-          <p className="text-base text-gray-400 dark:text-dark-200">
-            &copy; Supabase Inc
-          </p>
+          <p className="text-base text-gray-400 dark:text-dark-200">&copy; Supabase Inc</p>
           <div className="flex items-center">
             <SunEmoji />
             <button
