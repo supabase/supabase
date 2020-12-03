b0c4133c2993f2783a04f753bd8ff2906639fa18
mildtomato <jon.summers.muir@googlemail.com>
2020-12-03 17:28:39 +0800

    fix icons

--- www/components/Nav/Developers.tsx
+++ www/components/Nav/Developers.tsx
@@ -2,9 +2,17 @@ import React from 'react'
 import DevelopersData from 'data/Developers.json'
 import AnnouncementsData from 'data/Announcements.json'
 
+type Props = {
+  text: string
+  description? : string
+  url? : string
+  icon? : string
+}
+
+
 const Developers = () => {
-  const iconSections = Object.values(DevelopersData).map((company) => {
-    const { text, description, url } = company
+  const iconSections = Object.values(DevelopersData).map((company : Props) => {
+    const { text, description, url, icon } = company
 
     const content = (
       <a
@@ -24,7 +32,7 @@ const Developers = () => {
             stroke-linecap="round"
             stroke-linejoin="round"
             stroke-width="2"
-            d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z"
+            d={icon}
           />
         </svg>
         <div className="ml-4">
