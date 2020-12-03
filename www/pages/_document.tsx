5ef8e584ef5f0509e524a7a4a9a6124a2df8d0e6
Inian Parameshwaran <inian1234@gmail.com>
2020-12-03 23:16:36 +0800

    componentdidmount doesn't run

--- www/pages/_document.tsx
+++ www/pages/_document.tsx
@@ -1,16 +1,29 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import ReactGA from 'react-ga'
+// import ReactGA from 'react-ga'
 
 export default class MyDocument extends Document {
-  componentDidMount() {
-    ReactGA.initialize('UA-155232740-1')
-    ReactGA.pageview(window.location.pathname + window.location.search)
-  }
+  // componentDidMount() {
+  //   console.log('inain')
+  //   ReactGA.initialize('UA-155232740-1')
+  //   ReactGA.pageview(window.location.pathname + window.location.search)
+  // }
 
   render() {
     return (
       <Html lang="en" className="dark">
-        <Head />
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+                
+                ga('create', 'UA-155232740-1', 'auto');
+                ga('send', 'pageview');`,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
