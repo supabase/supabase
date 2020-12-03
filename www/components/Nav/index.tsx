718e0d0bd0c3091147f4bc96c33477ecf283082f
Inian Parameshwaran <inian1234@gmail.com>
2020-12-03 18:05:28 +0800

    fixing pricing link in local

--- www/components/Nav/index.tsx
+++ www/components/Nav/index.tsx
@@ -243,7 +243,7 @@ const Nav = (props: Props) => {
                 <Link href="/beta">Beta</Link>
               </div>
               <a
-                href="/docs/pricing"
+                href="https://supabase.io/docs/pricing"
                 className={`
                   inline-flex items-center px-1 border-b-2 border-transparent text-sm font-medium
                   text-gray-500 hover:text-gray-700 hover:border-gray-500 p-5
