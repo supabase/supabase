5ffdaea564868edea396a4f180bd8a6a69546b3f
Inian Parameshwaran <inian1234@gmail.com>
2020-12-03 19:41:10 +0800

    redirect force

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -602,14 +602,14 @@ function Home() {
   )
 }
 
-export default Home
-// export default () => {
-//   if (typeof window !== 'undefined') {
-//     if (window.location.href.indexOf('localhost') === -1) {
-//       window.location.replace('https://supabase.io')
-//     } else {
-//       window.location.replace('/docs')
-//     }
-//   }
-//   return null
-// }
+// export default Home
+export default () => {
+  if (typeof window !== 'undefined') {
+    if (window.location.href.indexOf('localhost') === -1) {
+      window.location.replace('https://supabase.io')
+    } else {
+      window.location.replace('/docs')
+    }
+  }
+  return null
+}
