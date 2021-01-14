33432165a616f314d52d1107879ddfb97b411ed0
Thorsten Schaeff <thorsten.schaeff@gmail.com>
2021-01-14 16:42:13 +0800

    style the SSR example

--- examples/nextjs-with-supabase-auth/pages/profile.js
+++ examples/nextjs-with-supabase-auth/pages/profile.js
@@ -1,14 +1,30 @@
 import Link from 'next/link'
+import { Auth, Card, Typography, Space, Button, Icon } from '@supabase/ui'
 import { supabase } from '../utils/initSupabase'
 
 export default function Profile({ user }) {
   return (
-    <>
-      <Link href="/">
-        <a>Index</a>
-      </Link>
-      <pre>{JSON.stringify(user, null, 2)}</pre>
-    </>
+    <div style={{ maxWidth: '420px', margin: '96px auto' }}>
+      <Card>
+        <Space direction="vertical" size={6}>
+          <Typography.Text>You're signed in</Typography.Text>
+          <Typography.Text strong>Email: {user.email}</Typography.Text>
+          <Typography.Text type="success">
+            User data retrieved server-side (from Cookie in getServerSideProps):
+          </Typography.Text>
+
+          <Typography.Text>
+            <pre>{JSON.stringify(user, null, 2)}</pre>
+          </Typography.Text>
+
+          <Typography.Text>
+            <Link href="/">
+              <a>Static example with useSWR</a>
+            </Link>
+          </Typography.Text>
+        </Space>
+      </Card>
+    </div>
   )
 }
 
