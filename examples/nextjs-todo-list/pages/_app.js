a6206e6f96123fa49e3baaa7e13d460435f48cc9
ykdojo <yksu4p@gmail.com>
2021-01-15 14:35:49 -0800

    Next.js todo list - fix the refresh issue
    Fixing the issue where the login page was flashing for a split second when refreshing the main page (after logging in)

--- examples/nextjs-todo-list/pages/_app.js
+++ examples/nextjs-todo-list/pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/index.css'
 
+// Reference: https://dev.to/apkoponen/how-to-disable-server-side-rendering-ssr-in-next-js-1563
+// Without this, you'll get an error like: Expected server HTML to contain a matching <h1> in <div>.
+// It's probably because server side rendering doesn't seem to work well
+// with Supabase. I'll look into this issue next.
+function SafeHydrate({ children }) {
+  return (
+    <div suppressHydrationWarning>
+      {typeof window === 'undefined' ? null : children}
+    </div>
+  )
+}
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <SafeHydrate><Component {...pageProps}/></SafeHydrate>
 }
 
 export default MyApp
