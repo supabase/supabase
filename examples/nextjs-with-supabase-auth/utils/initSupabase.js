af99c593bc2a86368d395173beaf51afe4600b05
Thor 雷神 Schaeff <5748289+thorwebdev@users.noreply.github.com>
2021-01-15 14:54:46 +0800

    feat: Add Supabase Auth components, update password, and SSR Cookie example. (#512)
    * feat(examnples/nextjs-with-supabase-auth): Add Supabase  UI Auth components.
    
    * feat(examnples/nextjs-with-supabase-auth): Add SSR Cookie example.
    
    * Add the Supabase Vercel deploy integration.
    
    * chore: add basic styling and layout of Auth component.
    
    • used @supabase/ui to help layout text and style around the Auth component
    
    * fix nextjs link
    
    * style the SSR example
    
    * remove unused imports
    
    * chore: update dependencies
    
    Co-authored-by: mildtomato <jon.summers.muir@googlemail.com>

--- examples/nextjs-with-supabase-auth/utils/initSupabase.js
+++ examples/nextjs-with-supabase-auth/utils/initSupabase.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_KEY
-);
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+)
