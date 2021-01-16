363da5464540cc4f3d843ffd5d36081bdd455e09
ykdojo <yksu4p@gmail.com>
2021-01-15 16:32:18 -0800

    Updated the packages on nextjs-todo-list

--- examples/nextjs-todo-list/pages/_app.js
+++ examples/nextjs-todo-list/pages/_app.js
@@ -2,8 +2,9 @@ import '../styles/index.css'
 
 // Reference: https://dev.to/apkoponen/how-to-disable-server-side-rendering-ssr-in-next-js-1563
 // Without this, you'll get an error like: Expected server HTML to contain a matching <h1> in <div>.
-// It's probably because server side rendering doesn't seem to work well
-// with Supabase. I'll look into this issue next.
+// This is because when rendering on the server side, Supabase is not able to retrieve
+// the current user. To properly fix this, you'd need to get and set auth cookies
+// like this: https://github.com/supabase/gotrue-js/blob/f09d4cbf3a7446325e9578736fd9d9b3bff3796e/example/next/utils/useAuth.js
 function SafeHydrate({ children }) {
   return (
     <div suppressHydrationWarning>
