6b5a4756cdcf5daefd1a600aca259a2bf26ac7c4
Inian Parameshwaran <inian1234@gmail.com>
2021-01-27 20:11:52 +0530

    revert earlier pr

--- www/pages/_document.tsx
+++ www/pages/_document.tsx
@@ -11,7 +11,19 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" className="dark">
-        <Head></Head>
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+                
+                ga('create', 'UA-155232740-1', 'auto');
+                ga('send', 'pageview');`,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
