0bc99bce7d3d254b93ab95cbe425d1a0f84b0923
Paul Copplestone <pcopplestone@gmail.com>
2021-02-16 17:07:10 +0800

    example: with-cloudflare-workers

---
+++ examples/with-cloudflare-workers/index.js
@@ -0,0 +1,25 @@
+import { createClient } from "@supabase/supabase-js";
+
+// Fill you client credential here.
+const db = createClient("YOUR_SUPABASE_URL", "YOUR_SUPABASE_KEY");
+
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
+  try {
+    // Uncomment this part to start querying your database using Supabase client.
+    //
+    // const { data, error } = await db
+    //   .from("TABLE")
+    //   .select("name");
+    // console.log(JSON.stringify(data, null, 2));
+  } catch (error) {
+    console.log(error);
+  }
+
+  return new Response("Hello worker!", {
+    headers: { "content-type": "text/plain" },
+  });
+}
