99d5e5325e060afd9b0bb83227ad47c85128e908
Jonathan Summers-Muir <jon.summers.muir@googlemail.com>
2021-02-22 19:09:16 +0800

    chore: add anchor IDs to headers

--- www/pages/blog/[slug].tsx
+++ www/pages/blog/[slug].tsx
@@ -17,8 +17,9 @@ import Badge from '~/components/Badge'
 
 const components = { CodeBlock }
 
-const toc = require('markdown-toc')
 const gfm = require('remark-gfm')
+const toc = require('markdown-toc')
+const slug = require('rehype-slug')
 
 function BlogPostPage(props: any) {
   // @ts-ignore
@@ -117,32 +118,17 @@ export async function getStaticPaths() {
     fallback: false,
   }
 }
-// export async function getStaticProps({ params }: any) {
-//   const postContent = await getPostdata(params.slug)
-//   const { data, content } = matter(postContent)
-
-//   return {
-//     props: {
-//       blog: {
-//         content: content,
-//         ...data,
-//         toc: toc(content),
-//       },
-//     },
-//   }
-// }
 
 export async function getStaticProps({ params }: any) {
   const postContent = await getPostdata(params.slug)
   const { data, content } = matter(postContent)
-
-  console.log(content)
 
   const mdxSource: any = await renderToString(content, {
     components,
     scope: data,
     mdxOptions: {
       remarkPlugins: [gfm],
+      rehypePlugins: [slug],
     },
   })
 
@@ -158,100 +144,6 @@ export async function getStaticProps({ params }: any) {
       },
     },
   }
-}
-
-// // pass props to BlogPostPage component
-// export async function getStaticProps(context: any) {
-//   const fs = require('fs')
-//   // const html = require('remark-html')
-//   // const highlight = require('remark-highlight.js')
-//   // const unified = require('unified')
-//   // const markdown = require('remark-parse')
-//   const matter = require('gray-matter')
-
-//   const slug = context.params.slug // get slug from params
-//   const path = `${process.cwd()}/_blog/${slug}.mdx`
-
-//   // read file content and store into rawContent variable
-//   const rawContent = fs.readFileSync(path, {
-//     encoding: 'utf-8',
-//   })
-
-//   const { data, content } = matter(rawContent) // pass rawContent to gray-matter to get data and content
-
-//   // const result = await unified()
-//   //   .use(markdown)
-//   //   .use(highlight) // highlight code block
-//   //   .use(html)
-//   //   .process(content) // pass content to process)
-
-//   return {
-//     props: {
-//       blog: {
-//         toc: toc(content),
-//         ...data,
-//         // content: result.toString(),
-//         content: content,
-//       },
-//     },
-//   }
-// }
-
-// // generate HTML paths at build time
-// export async function getStaticPaths(context: any) {
-//   const fs = require('fs')
-
-//   const path = `${process.cwd()}/_blog`
-//   const files = fs.readdirSync(path, 'utf-8')
-
-//   const markdownFileNames = files
-//     .filter((fn: any) => fn.endsWith('.mdx'))
-//     .map((fn: any) => fn.replace('.mdx', ''))
-
-//   return {
-//     paths: markdownFileNames.map((fileName: any) => {
-//       return {
-//         params: {
-//           slug: fileName,
-//         },
-//       }
-//     }),
-//     fallback: false,
-//   }
-// }
-
-// Heading renderer solution
-// https://github.com/remarkjs/react-markdown/issues/404
-
-const flatten = (text: string, child: any) => {
-  const result: any =
-    typeof child === 'string'
-      ? text + child
-      : React.Children.toArray(child.props.children).reduce(flatten, text)
-
-  return result
-}
-
-/**
- * HeadingRenderer is a custom renderer
- * It parses the heading and attaches an id to it to be used as an anchor
- */
-const HeadingRenderer = (props: any) => {
-  const children = React.Children.toArray(props.children)
-  const text = children.reduce(flatten, '')
-  const slug = text.toLowerCase().replace(/\W/g, '-')
-  return (
-    <a href={'#' + slug} style={{ textDecoration: 'none' }}>
-      <Space>
-        <span className={blogStyles['header']}>
-          {React.createElement('h' + props.level, { id: slug }, [
-            props.children,
-            <IconLink size="small" className="ml-2 hidden" />,
-          ])}
-        </span>
-      </Space>
-    </a>
-  )
 }
 
 export default BlogPostPage
