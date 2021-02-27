d8a7efe2e687f55b042901621b78b7c70db8b3cd
Ademílson F. Tonato <ademilsonft@outlook.com>
2021-02-27 12:46:19 +0000

    chore: add stencil-supabase to example/stencil-supabase

---
+++ examples/with-stencil/src/global/app.ts
@@ -0,0 +1,7 @@
+export default async () => {
+  /**
+   * The code to be executed should be placed within a default function that is
+   * exported by the global script. Ensure all of the code in the global script
+   * is wrapped in the function() that is exported.
+   */
+};
