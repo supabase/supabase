4a2ae8b2b47df866c4f1dc5cbb120f2056d4c568
Jonathan Summers-Muir <jon.summers.muir@googlemail.com>
2021-03-11 16:38:56 +0800

    chore: all updated docs files

--- web/sidebar_spec_cli.js
+++ web/sidebar_spec_cli.js
@@ -4,13 +4,13 @@ module.exports = {
       type: 'category',
       label: 'About',
       items: ['reference/cli/index', 'reference/cli/supa-init'],
-      collapsed: false,
+      collapsed: true,
     },
     {
       type: 'category',
       label: 'Local Development',
       items: ['reference/cli/supa-dev', 'reference/cli/supa-eject'],
-      collapsed: false,
+      collapsed: true,
     }
   ],
 }
