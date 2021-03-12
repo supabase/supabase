224b72b3abd31d3c56bf9cc8623c278b91d680c3
Paul Copplestone <pcopplestone@gmail.com>
2021-03-12 23:34:51 +0800

    Adds 3 simple pages for Postgres

--- web/sidebar_spec_postgres.js
+++ web/sidebar_spec_postgres.js
@@ -9,25 +9,13 @@ module.exports = {
     {
       type: 'category',
       label: 'Database',
-      items: ['reference/postgres/database-users', 'reference/postgres/database-passwords'],
-      collapsed: true,
-    },
-    {
-      type: 'category',
-      label: 'Schemas',
-      items: ['reference/postgres/creating-schemas'],
+      items: ['reference/postgres/database-passwords'],
       collapsed: true,
     },
     {
       type: 'category',
       label: 'Tables',
       items: ['reference/postgres/creating-tables'],
-      collapsed: true,
-    },
-    {
-      type: 'category',
-      label: 'Columns',
-      items: ['reference/postgres/creating-columns', 'reference/postgres/column-types'],
       collapsed: true,
     }
   ],
