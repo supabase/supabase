85425b78b12b004adb63f17ca81618a282a05412
Jonathan Summers-Muir <jon.summers.muir@googlemail.com>
2021-03-17 16:03:25 +0800

    chore: remove old ReactGA comments

--- www/pages/_document.tsx
+++ www/pages/_document.tsx
@@ -1,13 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-// import ReactGA from 'react-ga'
 
 export default class MyDocument extends Document {
-  // componentDidMount() {
-  //   console.log('inain')
-  //   ReactGA.initialize('UA-155232740-1')
-  //   ReactGA.pageview(window.location.pathname + window.location.search)
-  // }
-
   render() {
     return (
       <Html lang="en" className="dark">
