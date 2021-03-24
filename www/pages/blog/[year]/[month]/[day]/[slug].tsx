b0bfb872927129b89ebe82360dd80d178cf6afc1
Paul Copplestone <pcopplestone@gmail.com>
2021-03-24 19:22:05 +0800

    Makes the link an a tag

--- www/pages/blog/[year]/[month]/[day]/[slug].tsx
+++ www/pages/blog/[year]/[month]/[day]/[slug].tsx
@@ -177,13 +177,13 @@ function BlogPostPage(props: any) {
               <div className="lg:py-12 grid grid-cols-12 lg:gap-16">
                 <div className="col-span-12 lg:col-span-10">
                   <Link href={`/blog`} as={`/blog`}>
-                    <div>
+                    <a>
                       <Typography.Text type="secondary">
                         <span className="hover:text-gray-900 dark:hover:text-white cursor-pointer">
                           View more posts
                         </span>
                       </Typography.Text>
-                    </div>
+                    </a>
                   </Link>
                   <Space className="my-4">
                     <Typography.Text>{props.blog.date}</Typography.Text>
