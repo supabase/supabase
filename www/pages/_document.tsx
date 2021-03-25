01b23df9934045db9bc1e34794f3145b863f8230
Florian LEFEBVRE <florian.lefebvre3184@orange.fr>
2021-03-25 11:51:34 +0100

    make mobile menu full width and remove body class

--- www/pages/_document.tsx
+++ www/pages/_document.tsx
@@ -5,7 +5,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en" className="dark overflow-x-hidden">
         <Head></Head>
-        <body className="lg:overflow-auto">
+        <body>
           <Main />
           <NextScript />
         </body>
