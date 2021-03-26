7911928f6ba2bafd483ee32c477006c4cb72fd53
erkeen <erik.eenbox@gmail.com>
2021-03-26 19:51:36 +0000

    Added nuxtjs-user-management

---
+++ examples/nuxtjs-user-management/components/Account.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="account-container">
+    <div>
+      <label for="email">Email</label>
+      <input
+        id="email"
+        type="text"
+        v-model="state.propSession.user.email"
+        disabled
+      />
+    </div>
+    <div>
+      <label for="username">Username</label>
+      <input id="username" type="text" v-model="state.username" />
+    </div>
+    <div>
+      <label for="website">Website</label>
+      <input id="website" type="website" v-model="state.website" />
+    </div>
+
+    <div>
+      <button
+        class="button block primary"
+        @click="updateProfile"
+        :disabled="state.loading"
+      >
+        {{ state.loading ? "Loading ..." : "Update" }}
+      </button>
+    </div>
+
+    <div>
+      <button className="button block">Sign Out</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { supabase } from "../lib/supabaseClient";
+export default {
+  props: ["session"],
+  data() {
+    return {
+      state: {
+        loading: true,
+        username: "",
+        website: "",
+        propSession: this.session,
+      },
+    };
+  },
+  mounted() {
+    this.getProfile();
+  },
+  methods: {
+    async signOut() {
+      const { error } = await supabase.auth.signOut();
+      if (error) console.log("Error logging out:", error.message);
+    },
+
+    setProfile(profile) {
+      this.state.username = profile.username;
+      this.state.website = profile.website;
+    },
+
+    async getProfile() {
+      try {
+        this.state.loading = true;
+
+        let { data, error } = await supabase
+          .from("profiles")
+          .select(`username, website, avatar_url`)
+          .eq("id", this.stete.propSession.user.id)
+          .single();
+
+        if (error || !data) {
+          throw error || new Error("Profile not found");
+        }
+
+        setProfile(data);
+      } catch (error) {
+        console.log("error", error.message);
+      } finally {
+        this.state.loading = false;
+      }
+    },
+
+    async updateProfile() {
+      try {
+        this.state.loading = true;
+
+        const updates = {
+          id: this.state.propSession.user.id,
+          username: this.state.username,
+          website: this.state.website,
+          updated_at: new Date(),
+        };
+
+        let { error } = await supabase.from("profiles").upsert(updates, {
+          returning: "minimal", // Don't return the value after inserting
+        });
+
+        if (error) {
+          throw error;
+        }
+      } catch (error) {
+        alert(error.message);
+      } finally {
+        this.state.loading = false;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.account-container {
+  min-width: 250;
+  max-width: 600;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+}
+
+.account-container > div {
+  margin: 10px;
+}
+</style>
