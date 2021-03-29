ac81a56f15b487ee6a3194503a3f7642de16b93d
Paul Copplestone <pcopplestone@gmail.com>
2021-03-29 09:20:46 +0800

    Rapid updates for the copy text

--- www/data/products/storage/permissions-examples.js
+++ www/data/products/storage/permissions-examples.js
@@ -2,9 +2,9 @@ export default [
   {
     lang: 'sql',
     title: 'Public access to a bucket',
-    code: `CREATE POLICY open_all_update 
-ON storage.objects 
-for all WITH CHECK (bucket_id='bucket1');
+    code: `create policy "Public Access" 
+on storage.objects for all 
+with check ( bucket_id = 'avatars' );
     `,
     detail_title: 'Allow public CRUD access to a bucket',
     detail_text: "This will allow any user access to the bucket named 'bucket1'",
@@ -15,9 +15,12 @@ for all WITH CHECK (bucket_id='bucket1');
   {
     lang: 'sql',
     title: 'Public access to a folder',
-    code: `CREATE POLICY crud_public_folder 
-ON storage.objects 
-for all USING (bucket_id='bucket1' and (storage.foldername(name))[1] = 'public');
+    code: `create policy "Public access to a folder" 
+on storage.objects for all 
+using (
+  bucket_id = 'bucket1' 
+  and (storage.foldername(name))[1] = 'public' 
+);
     `,
     detail_title: 'Allow public CRUD access to a folder in a bucket',
     detail_text:
@@ -28,10 +31,13 @@ for all USING (bucket_id='bucket1' and (storage.foldername(name))[1] = 'public')
   },
   {
     lang: 'sql',
-    title: 'Authenticated access to a folder',
-    code: `CREATE POLICY authenticated_folder 
-ON storage.objects 
-for all USING (bucket_id='bucket1' and (storage.foldername(name))[1] = 'authenticated' and auth.role() = 'authenticated');`,
+    title: 'Authenticated access to a bucket',
+    code: `create policy "Logged in access" 
+on storage.objects 
+for all using (
+  bucket_id = 'bucket1' 
+  and auth.role() = 'authenticated'
+);`,
     detail_title: 'Allow any authenticated user access to a folder',
     detail_text:
       "This will allow any authenticated user access to the folder named 'authenticated' in the bucket 'bucket1'",
@@ -41,10 +47,14 @@ for all USING (bucket_id='bucket1' and (storage.foldername(name))[1] = 'authenti
   },
   {
     lang: 'sql',
-    title: 'User access to a file',
-    code: `CREATE POLICY crud_uid_file 
-ON storage.objects 
-for all USING (bucket_id='bucket2' and name = 'folder/only_uid.jpg' and auth.uid() = 'd8c7bce9-cfeb-497b-bd61-e66ce2cbdaa2');`,
+    title: 'Individual access to a file',
+    code: `create policy "Individual access" 
+on storage.objects for all 
+using (
+  bucket_id = 'bucket1' 
+  and name = 'folder/only_uid.jpg' 
+  and auth.uid() = 'd8c7bce9-cfeb-497b-bd61-e66ce2cbdaa2'
+);`,
     detail_title: 'Allow a specific user access to a file',
     detail_text:
       "This will allow a specific user based on the user's UID access to a file named 'only_uid.jpg'",
