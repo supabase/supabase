6973d152140a90ac85737ba042323ec03d6c5f44
Paul Copplestone <pcopplestone@gmail.com>
2021-03-29 15:07:52 +0800

    Updates key names to "ANON_KEY"

--- examples/nuxtjs-user-management/lib/constants.js
+++ examples/nuxtjs-user-management/lib/constants.js
@@ -1,5 +1,5 @@
 export const NUXT_PUBLIC_SUPABASE_URL = process.env.NUXT_PUBLIC_SUPABASE_URL
-export const NUXT_PUBLIC_SUPABASE_KEY = process.env.NUXT_PUBLIC_SUPABASE_KEY
+export const NUXT_PUBLIC_SUPABASE_ANON_KEY = process.env.NUXT_PUBLIC_SUPABASE_ANON_KEY
 
 // export type Profile = {
 //   id: string
