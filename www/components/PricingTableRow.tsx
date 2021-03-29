00c1fb1171872a22c5518e853bbc251c77034eee
Jonathan Summers-Muir <jon.summers.muir@googlemail.com>
2021-03-29 21:19:28 +0800

    using :any just to force a build

--- www/components/PricingTableRow.tsx
+++ www/components/PricingTableRow.tsx
@@ -81,7 +81,7 @@ export const PricingTableRowDesktop = (props: any) => {
               )}
             </th>
 
-            {Object.values(feat.tiers).map((tier) => {
+            {Object.values(feat.tiers).map((tier: any) => {
               return (
                 <td className="py-5 px-6">
                   {typeof tier === 'boolean' && tier === true ? (
@@ -128,7 +128,7 @@ export const PricingTableRowMobile = (props: any) => {
           </tr>
         </thead>
         <tbody className="divide-y divide-gray-200 dark:divide-gray-600 ">
-          {category.features.map((feat) => {
+          {category.features.map((feat: any) => {
             return (
               <tr className="border-t border-gray-200 dark:border-gray-600 ">
                 <th className="py-5 px-4 text-sm font-normal text-gray-500 text-left" scope="row">
