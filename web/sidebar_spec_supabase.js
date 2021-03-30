c86650d2c570bad844348071651a83b4ecd83bd5
Paul Copplestone <pcopplestone@gmail.com>
2021-03-30 17:31:19 +0800

    Updates ref docs

--- web/sidebar_spec_supabase.js
+++ web/sidebar_spec_supabase.js
@@ -27,7 +27,7 @@ module.exports = {
     {
       type: 'category',
       label: 'Storage',
-      items: ['reference/javascript/storage-uploadfile', 'reference/javascript/storage-createsignedurl', 'reference/javascript/storage-downloadfile', 'reference/javascript/storage-listfiles', 'reference/javascript/storage-updatefile', 'reference/javascript/storage-movefile', 'reference/javascript/storage-deletefile', 'reference/javascript/storage-deletefiles', 'reference/javascript/storage-createbucket', 'reference/javascript/storage-getbucket', 'reference/javascript/storage-deletebucket', 'reference/javascript/storage-emptybucket', 'reference/javascript/storage-listbuckets'],
+      items: ['reference/javascript/storage-createbucket', 'reference/javascript/storage-getbucket', 'reference/javascript/storage-listbuckets', 'reference/javascript/storage-deletebucket', 'reference/javascript/storage-emptybucket', 'reference/javascript/storage-from-upload', 'reference/javascript/storage-from-download', 'reference/javascript/storage-from-list', 'reference/javascript/storage-from-update', 'reference/javascript/storage-from-move', 'reference/javascript/storage-from-delete', 'reference/javascript/storage-from-createsignedurl', 'reference/javascript/storage-from-deletefiles'],
       collapsed: true,
     },
     {
