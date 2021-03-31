059133b57feae336d393e87e5eb4966be40144cd
Paul Copplestone <pcopplestone@gmail.com>
2021-03-31 08:51:12 +0800

    fix: #990. the blog link in the docs footer

--- web/docusaurus.config.js
+++ web/docusaurus.config.js
@@ -146,7 +146,7 @@ module.exports = {
           items: [
             {
               label: 'Blog',
-              to: '/blog',
+              to: 'https://supabase.io/blog',
             },
             {
               label: 'Open source',
