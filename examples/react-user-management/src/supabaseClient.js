3c55554dd08599a4db0595f2d691e915067f7e38
Paul Copplestone <pcopplestone@gmail.com>
2021-05-18 15:18:58 +0800

    Adds a React guide - first version as basic as I can possibly make it

---
+++ examples/react-user-management/src/supabaseClient.js
@@ -0,0 +1,11 @@
+/**
+ * lib/supabaseClient.js
+ * Helper to initialize the Supabase client.
+ */
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
