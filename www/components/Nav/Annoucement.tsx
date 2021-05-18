a048265d73e9d46def8e418af2c984b2abcbef13
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2021-05-18 20:59:59 +0200

    improve annoucement nav coherence between light and dark modes

--- www/components/Nav/Annoucement.tsx
+++ www/components/Nav/Annoucement.tsx
@@ -17,12 +17,12 @@ const a: A = data;
 const Annoucement = () => {
   if (!a.show) return (null)
   return (
-    <div className="bg-brand-700 dark:bg-gray-700">
+    <div className="bg-brand-700 dark:bg-brand-800">
       <div className="flex flex-col items-center justify-center py-2 mx-auto space-x-2 text-white sm:flex-row lg:container lg:px-16 xl:px-20">
         <span>{a.text}</span>
         <a
           href={a.link.url}
-          className="inline-flex items-center px-2 py-1 space-x-1 text-xs transition-colors bg-white dark:bg-brand-800 rounded-full text-brand-800 hover:text-brand-800 dark:text-white hover:bg-opacity-90 dark:hover:bg-brand-700"
+          className="inline-flex items-center px-2 py-1 space-x-1 text-xs transition-colors bg-white rounded-full text-brand-800 dark:hover:text-brand-800 hover:text-brand-800 hover:bg-opacity-90"
         >
           {a.link.text} <IconArrowRight size="tiny" />
         </a>
