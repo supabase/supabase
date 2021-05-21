5cb5f12f3a25bd65f72f65cd81aa43d9a40ecf90
Carlos Stenzel <carlosstenzel@hotmail.com>
2021-05-21 08:07:23 -0300

    teste for format for action

--- www/pages/index.tsx
+++ www/pages/index.tsx
@@ -20,7 +20,7 @@ const Index = ({}: Props) => {
   return (
     <>
       <Layout>
-        <Container>
+                              <Container>
           <Hero />
           <Features />
           <div className="relative">
