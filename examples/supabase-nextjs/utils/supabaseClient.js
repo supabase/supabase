78ac47f2a028729411fb823fe7ba91558e0978ac
Paul Copplestone <pcopplestone@gmail.com>
2021-05-27 20:25:50 +0800

    remove redundant example

--- examples/supabase-nextjs/utils/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
