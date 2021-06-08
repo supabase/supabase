59138921afe3f6392bf52fe1087224abd2a4d170
Mark Burggraf <markb@mantisbible.com>
2021-06-08 08:18:49 -0700

    added Github login

--- web/sidebars.js
+++ web/sidebars.js
@@ -122,7 +122,7 @@ module.exports = {
       type: 'category',
       label: 'Auth',
       collapsed: false,
-      items: ['guides/auth/auth-facebook', 'guides/auth/auth-google'],
+      items: ['guides/auth/auth-facebook', 'guides/auth/auth-google', 'guides/auth/auth-github'],
     },
     {
       type: 'category',
