cc894034716d365df036660be56a5753e0adc159
Paul Copplestone <pcopplestone@gmail.com>
2021-07-01 19:55:01 +0800

    adds Side menu logic

--- studio/components/layouts/Dashboard.tsx
+++ studio/components/layouts/Dashboard.tsx
@@ -1,16 +1,61 @@
-import { ReactElement } from "react"
-import IconBar from "../nav/IconBar"
+import { ReactElement } from 'react'
+import { Typography } from '@supabase/ui'
+import IconBar from '../nav/IconBar'
+import Head from 'next/head'
 
-export default function DashboardLayout ({ children }: { children: ReactElement}) {
+type MenuBar = {
+  title: string
+  categories?: MenuBarItems[]
+}
+type MenuBarItems = {
+  label: string
+  links: MenuBarLinks[]
+}
+type MenuBarLinks = {
+  label: string
+  href: string
+}
+
+export default function DashboardLayout({
+  children,
+  title,
+  menu,
+}: {
+  children: ReactElement
+  title?: string
+  menu?: MenuBar
+}) {
+  // Render the side menu
+  const renderMenu = (menu: MenuBar) => {
+    const { Title } = Typography
+
+    return (
+      <div
+        className={[
+          'w-64 h-screen overflow-autohide-scrollbar border-r ', // layout
+          'bg-sidebar-linkbar-light', // Light mode
+          'dark:bg-sidebar-linkbar-dark dark:border-dark ', // Dark mode
+        ].join(' ')}
+      >
+        <Title level={3 } className="border-b p-4">{menu.title}</Title>
+      </div>
+    )
+  }
+
   return (
-    <div className="flex">
-      <div className="w-14 h-screen bg-sidebar-light dark:bg-sidebar-dark border-r dark:border-dark">
-        <IconBar />
+    <>
+      <Head>
+        <title>Supabase Studio {title && `| ${title}`}</title>
+        <meta name="description" content="Supabase Studio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex">
+        <div className="w-14 h-screen bg-sidebar-light dark:bg-sidebar-dark border-r dark:border-dark">
+          <IconBar />
+        </div>
+        {menu && renderMenu(menu)}
+        <main className="flex-1">{children}</main>
       </div>
-      <div className="w-64 h-screen overflow-auto bg-sidebar-linkbar-light dark:bg-sidebar-linkbar-dark hide-scrollbar border-r dark:border-dark ">
-        Menu List
-      </div>
-      <div className="flex-1">{children}</div>
-    </div>
+    </>
   )
 }
--- studio/pages/docs.tsx
+++ studio/pages/docs.tsx
@@ -1,21 +1,9 @@
-import Head from 'next/head'
 import DashboardLayout from '../components/layouts/Dashboard'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <DashboardLayout>
-      <div className={styles.container}>
-        <Head>
-          <title>Supabase | API</title>
-          <meta name="description" content="Supabase API Documentation" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-
-        <main className={styles.main}>
-          <h1 className={styles.title}>API Docs</h1>
-        </main>
-      </div>
+    <DashboardLayout title="API Docs" menu={{ title: 'API' }}>
+      <h1>API Docs</h1>
     </DashboardLayout>
   )
 }
