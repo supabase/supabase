5579e4fb76b4a6dcd8b56a4a5e21dcbcc0b5b60a
Paul Copplestone <pcopplestone@gmail.com>
2021-07-01 18:28:37 +0800

    Cleans up the icon bar

--- studio/components/nav/IconBar.tsx
+++ studio/components/nav/IconBar.tsx
@@ -1,41 +1,48 @@
-import { IconColumns, IconGrid } from '@supabase/ui'
+import { IconColumns, IconZap } from '@supabase/ui'
 import Link from 'next/link'
 import { ReactElement } from 'react'
 
+/**
+ * Route: Type definition for Sidebar Items
+ */
 type Route = {
-    key: string
-    label: string
-    icon: ReactElement
-    link: string
+  key: string
+  label: string
+  icon: ReactElement
+  link: string
 }
 
+/**
+ * Full list of Sidebar Routes
+ */
 const ROUTES: Route[] = [
-  { key: 'ICON', label: 'Supabase', icon: <IconGrid />, link: '/' },
-  { key: 'API', label: 'API', icon: <IconColumns />, link: '/api' },
-  //   { key: 'DATABASE', label: 'Database', icon: <IconMail />, link: '/storage' },
+  { key: 'ICON', label: 'Supabase', icon: <IconZap size={20} strokeWidth={3} />, link: '/' },
+  { key: 'API', label: 'API', icon: <IconColumns size={20} strokeWidth={3} />, link: '/docs' },
 ]
 
 /**
  * Icon nav bar
  */
 export default function IconBar({}) {
-
-    /**
-     * Renders all the icons
-     */
+  // Renders all the icons
   const renderNav = (routes: Route[]) => {
+
     return routes.map((x) => (
       <li key={x.key}>
         <Link href={x.link}>
-          <a className="block h-10 w-10 bg-black mx-2 mt-2 rounded text-gray-300 dark:text-gray-400 hover:bg-gray-50 dark:hover:bg-bg-alt-dark dark:hover:text-white">{x.icon}</a>
+          <a
+            className={[
+              'flex items-center justify-center mx-2 mt-2 h-10 w-10 rounded', // Layout
+              'bg-gray-200 text-gray-600 hover:bg-gray-200', // Light mode
+              'dark:text-gray-400 dark:hover:bg-bg-alt-dark dark:hover:text-white', // Dark mode
+            ].join(' ')}
+          >
+            {x.icon}
+          </a>
         </Link>
       </li>
     ))
   }
 
-  return (
-    <ul className="">
-      {renderNav(ROUTES)}
-    </ul>
-  )
+  return <ul className="">{renderNav(ROUTES)}</ul>
 }
