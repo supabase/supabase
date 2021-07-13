bccfda590c84ca9411ce1d322b3c1e4dfd677118
alon.barad <alon.barad@intsights.com>
2021-07-13 19:48:33 +0300

    Removed constractor.

--- studio/components/layouts/AuthLayout.tsx
+++ studio/components/layouts/AuthLayout.tsx
@@ -4,38 +4,31 @@ import Error from '../../components/utils/Error'
 import { fetchOpenApiSpec } from '../../lib/api'
 import { ReactElement } from 'react'
 
-export default function AuthLayout({
-  title,
-  children,
-}: {
-  title: string
-  children: ReactElement
-}) {
+export default function AuthLayout({ title, children }: { title: string; children: ReactElement }) {
   const { isLoading, error } = fetchOpenApiSpec()
 
   if (isLoading) return <Loading />
   if (error) return <Error />
 
-  const buildSidebar = () => {
-    return [
-      {
-        label: 'General',
-        links: [
-          { label: 'Users', href: '/auth/users' },
-          { label: 'Policies', href: '/auth/policies' },
-        ],
-      },
-      {
-        label: 'Settings',
-        links: [{ label: 'Settings', href: '/auth/settings' }],
-      },
-    ]
-  }
-
   return (
     <DashboardLayout
       title={title || 'Authentication'}
-      sidebar={{ title: 'Authentication', categories: buildSidebar() }}
+      sidebar={{
+        title: 'Authentication',
+        categories: [
+          {
+            label: 'General',
+            links: [
+              { label: 'Users', href: '/auth/users' },
+              { label: 'Policies', href: '/auth/policies' },
+            ],
+          },
+          {
+            label: 'Settings',
+            links: [{ label: 'Settings', href: '/auth/settings' }],
+          },
+        ],
+      }}
     >
       {children}
     </DashboardLayout>
