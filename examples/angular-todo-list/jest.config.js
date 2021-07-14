13533229a96c18ec426ba19acd97df10699b3aec
geromegrignon <gerome.grignon.lp2@gmail.com>
2021-07-14 23:20:57 +0200

    feat: add Angular todo list example
    add Angular todo list source code
    add the example to the existing list on supabase.io
    add codeowners file for automated assignment fo reviews

---
+++ examples/angular-todo-list/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  moduleNameMapper: {
+    '@core/(.*)': '<rootDir>/src/app/core/$1',
+  },
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+}
