8edbfa37c98e115a1bf49567e80eaf02f316f969
Paul Copplestone <pcopplestone@gmail.com>
2021-07-20 12:08:42 +0800

    Starts the dart reference docs

---
+++ web/sidebar_spec_dart.js
@@ -0,0 +1,10 @@
+module.exports = {
+  docs: [
+    {
+      type: 'category',
+      label: 'About',
+      items: ['reference/dart/index', 'reference/dart/installing', 'reference/dart/initializing'],
+      collapsed: true,
+    }
+  ],
+}
