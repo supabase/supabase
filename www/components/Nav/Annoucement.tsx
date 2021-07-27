3642eff46c8f031bb7f12ba65743e75ed6cddbbd
Paul Copplestone <pcopplestone@gmail.com>
2021-07-27 19:13:14 +0800

    Adds Discord announcement

--- www/components/Nav/Annoucement.tsx
+++ www/components/Nav/Annoucement.tsx
@@ -17,12 +17,12 @@ const a: A = data
 const Annoucement = () => {
   if (!a.show) return null
   return (
-    <div className="bg-brand-700 dark:bg-brand-800">
+    <div className="bg-indigo-400 dark:bg-indigo-400">
       <div className="flex items-center justify-center py-2 mx-auto space-x-2 text-white lg:container lg:px-16 xl:px-20 text-sm md:text-base">
         <span>{a.text}</span>
         <a
           href={a.link.url}
-          className="inline-flex items-center px-2 py-1 space-x-1 text-xs transition-colors bg-white rounded-full text-brand-800 dark:hover:text-brand-800 hover:text-brand-800 hover:bg-opacity-90"
+          className="inline-flex items-center px-2 py-1 space-x-1 text-xs transition-colors bg-indigo-900 rounded text-white dark:hover:text-indigo-100 hover:text-indigo-800 hover:bg-opacity-90"
         >
           {a.link.text} <IconArrowRight size="tiny" />
         </a>
