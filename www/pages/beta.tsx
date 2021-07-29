7c95df3622340429d76f71d2dd4136f5f00bc60a
Flyken <39961800+Flyken271@users.noreply.github.com>
2021-07-28 22:51:14 -0400

    added `a` element to the next/link

--- www/pages/beta.tsx
+++ www/pages/beta.tsx
@@ -996,7 +996,9 @@ const Beta = () => {
         <div className="sticky inset-0 z-50">
           <div className="shadow-lg py-5 px-5 xl:px-20 bg-dark-800 flex items-center justify-between">
             <Link href="/">
+              <a>
               <img className="h-5" src={`${basePath}/images/logo-dark.png`} />
+              </a>
             </Link>
             <HamburgerMenu openMenu={() => setMenuOpen(!menuOpen)} />
           </div>
