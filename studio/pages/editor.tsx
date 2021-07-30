2dae830a865e10a1200aad6df06349a647c191f7
pip <jrphiri@gmail.com>
2021-07-30 22:19:18 +0200

    fix: adds padding to table editor page

--- studio/pages/editor.tsx
+++ studio/pages/editor.tsx
@@ -3,9 +3,9 @@ import EditorLayout from 'components/layouts/EditorLayout'
 export default function Editor() {
   return (
     <EditorLayout title="Table Editor">
-      <>
+      <div className="p-4">
         <h1>Table Editor</h1>
-      </>
+      </div>
     </EditorLayout>
   )
 }
