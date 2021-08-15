9f71134cb86ebf08cf05014343a1bfc81a7e9449
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-08-15 09:45:36 +0900

    feat: spec generation will generate dart pages

--- web/sidebar_spec_dart.js
+++ web/sidebar_spec_dart.js
@@ -3,7 +3,13 @@ module.exports = {
     {
       type: 'category',
       label: 'About',
-      items: ['reference/dart/index', 'reference/dart/installing', 'reference/dart/initializing'],
+      items: ['reference/dart/installing', 'reference/dart/initializing'],
+      collapsed: true,
+    },
+    {
+      type: 'category',
+      label: 'Auth',
+      items: ['reference/dart/auth-signup', 'reference/dart/auth-signin', 'reference/dart/auth-signout', 'reference/dart/auth-session', 'reference/dart/auth-user', 'reference/dart/auth-update', 'reference/dart/auth-onauthstatechange', 'reference/dart/reset-password-email', 'reference/dart/delete-user'],
       collapsed: true,
     }
   ],
