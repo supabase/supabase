5a697b841e283827cbc3f2d8bc875a0437cf57c1
pip <jrphiri@gmail.com>
2021-08-21 14:13:58 +0200

    feat: Adds "New Table" SidePanel

--- studio/components/layouts/EditorLayout.tsx
+++ studio/components/layouts/EditorLayout.tsx
@@ -2,8 +2,8 @@ import DashboardLayout from '../../components/layouts/DashboardLayout'
 import Loading from '../../components/utils/Loading'
 import Error from '../../components/utils/Error'
 import { fetchOpenApiSpec } from '../../lib/api'
-import { MouseEventHandler, ReactElement } from 'react'
-import { Button, IconPlus } from '@supabase/ui'
+import { ReactElement, useState } from 'react'
+import { Select, Divider, Input, Button, IconPlus, SidePanel, Typography } from '@supabase/ui'
 
 export default function EditorLayout({
   title,
@@ -13,6 +13,7 @@ export default function EditorLayout({
   children: ReactElement
 }) {
   const { tables, isLoading, error } = fetchOpenApiSpec()
+  const [sidePanelVisible, setSidePanelVisible] = useState(false)
 
   if (isLoading) return <Loading />
   if (error) return <Error />
@@ -31,9 +32,17 @@ export default function EditorLayout({
   const buildSidebarButton = () => {
     return {
       label: 'New table',
-      action: (_e: any) => {},
+      action: (_e: any) => setSidePanelVisible(true),
     }
   }
+
+  const toggleSidePanel = () => {
+    setSidePanelVisible(!sidePanelVisible)
+  }
+
+  const handleTypeSelect = () => {}
+
+  const handleDefaultSelect = () => {}
 
   return (
     <DashboardLayout
@@ -45,7 +54,47 @@ export default function EditorLayout({
         button: buildSidebarButton(),
       }}
     >
-      {children}
+      <>
+        <SidePanel
+          wide
+          align="right"
+          visible={sidePanelVisible}
+          title="Add new table"
+          onCancel={toggleSidePanel}
+        >
+          <Input label="Name" />
+          <Input label="Description" labelOptional="Optional" />
+          <Divider className="my-8" />
+          <div className="flex flex-col">
+            <Typography.Title level={5}>Add existing content to new table</Typography.Title>
+            <Typography.Text type="secondary">
+              Upload a CSV or TSV file, or paste copied text from a spreadsheet.
+            </Typography.Text>
+            <Button className="my-4" icon={<IconPlus />} type="secondary">
+              Add existing content
+            </Button>
+          </div>
+          <Divider className="my-4" />
+          <div>
+            <Input label="Name" />
+            <Select label="Type" onChange={handleTypeSelect}>
+              <Select.Option value="">---</Select.Option>
+              <Select.Option value="int2">int2</Select.Option>
+              <Select.Option value="int4">int4</Select.Option>
+              <Select.Option value="int8">int8</Select.Option>
+              <Select.Option value="text">text</Select.Option>
+              <Select.Option value="uuid">uuid</Select.Option>
+            </Select>
+            <Select label="Default Value" onChange={handleDefaultSelect}>
+              <Select.Option value="">---</Select.Option>
+              <Select.Option value="Automatically generate as identity">
+                Automatically generate as identity
+              </Select.Option>
+            </Select>
+          </div>
+        </SidePanel>
+        {children}
+      </>
     </DashboardLayout>
   )
 }
