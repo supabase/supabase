3172b157d358d1464f600bdcd98f366c080b1a58
pip <jrphiri@gmail.com>
2021-08-21 11:47:10 +0200

    fix: adds SERVICE_KEY instead ANON key for working with @supabase/grid

--- studio/lib/constants.ts
+++ studio/lib/constants.ts
@@ -1,2 +1,3 @@
 export const SUPABASE_URL: string = process.env.NEXT_PUBLIC_SUPABASE_URL || 'http://localhost:3000'
 export const SUPABASE_ANON_KEY: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
+export const SUPABASE_SERVICE_KEY: string = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY || ''
--- studio/pages/editor/tables/[tableName].tsx
+++ studio/pages/editor/tables/[tableName].tsx
@@ -1,7 +1,7 @@
 import EditorLayout from '../../../components/layouts/EditorLayout'
 import { SupabaseGrid } from '@supabase/grid'
 import { useRouter } from 'next/router'
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from '../../../lib/constants'
+import { SUPABASE_URL, SUPABASE_SERVICE_KEY } from '../../../lib/constants'
 
 export default function TableEditor() {
   const router = useRouter()
@@ -14,7 +14,7 @@ export default function TableEditor() {
           table={String(tableName)}
           clientProps={{
             supabaseUrl: SUPABASE_URL,
-            supabaseKey: SUPABASE_ANON_KEY,
+            supabaseKey: SUPABASE_SERVICE_KEY,
           }}
         />
       </div>
