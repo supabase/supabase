69d65553315f959bdab7ade595da36b4798b511b
Paul Copplestone <pcopplestone@gmail.com>
2021-08-24 21:31:28 +0800

    Trying another technique using the "blog" functionality

--- web/docusaurus.config.js
+++ web/docusaurus.config.js
@@ -209,77 +209,95 @@ module.exports = {
     ],
   ],
   scripts: [{ src: '/scripts/telemetry.js' }],
-  // plugins: [
-  //   // [
-  //   //   '@docusaurus/plugin-content-docs',
-  //   //   {
-  //   //     id: 'supabase-client', // for first plugin-content-docs with "resources/" path
-  //   //     // homePageId: "doc2",
-  //   //     path: './ref/supabase', // Path to data on filesystem, relative to site dir.
-  //   //     routeBasePath: 'ref/supabase', // URL Route.
-  //   //     include: ['**/*.md', '**/*.mdx'],
-  //   //     sidebarPath: require.resolve('./sidebar_spec_supabase.js'),
-  //   //     // disableVersioning: true, // if not set with versions, throw: Identifier 'React' has already been declared
-  //   //   },
-  //   // ],
-  //   // [
-  //   //   '@docusaurus/plugin-content-docs',
-  //   //   {
-  //   //     id: 'postgrest-client', // for first plugin-content-docs with "resources/" path
-  //   //     // homePageId: "doc2",
-  //   //     path: './ref/postgrest', // Path to data on filesystem, relative to site dir.
-  //   //     routeBasePath: 'ref/postgrest', // URL Route.
-  //   //     include: ['**/*.md', '**/*.mdx'],
-  //   //     sidebarPath: require.resolve('./sidebar_spec_postgrest.js'),
-  //   //     // disableVersioning: true, // if not set with versions, throw: Identifier 'React' has already been declared
-  //   //   },
-  //   // ],
-  //   // [
-  //   //   '@docusaurus/plugin-content-docs',
-  //   //   {
-  //   //     id: 'gotrue-client', // for first plugin-content-docs with "resources/" path
-  //   //     // homePageId: "doc2",
-  //   //     path: './ref/gotrue', // Path to data on filesystem, relative to site dir.
-  //   //     routeBasePath: 'ref/gotrue', // URL Route.
-  //   //     include: ['**/*.md', '**/*.mdx'],
-  //   //     sidebarPath: require.resolve('./sidebar_spec_gotrue.js'),
-  //   //     // disableVersioning: true, // if not set with versions, throw: Identifier 'React' has already been declared
-  //   //   },
-  //   // ],
-  //   // [
-  //   //   '@docusaurus/plugin-content-docs',
-  //   //   {
-  //   //     id: 'realtime-client', // for first plugin-content-docs with "resources/" path
-  //   //     // homePageId: "doc2",
-  //   //     path: './ref/realtime', // Path to data on filesystem, relative to site dir.
-  //   //     routeBasePath: 'ref/realtime', // URL Route.
-  //   //     include: ['**/*.md', '**/*.mdx'],
-  //   //     sidebarPath: require.resolve('./sidebar_spec_realtime.js'),
-  //   //     // disableVersioning: true, // if not set with versions, throw: Identifier 'React' has already been declared
-  //   //   },
-  //   // ],
-  //   // [
-  //   //   '@docusaurus/plugin-content-docs',
-  //   //   {
-  //   //     id: 'realtime-server', // for first plugin-content-docs with "resources/" path
-  //   //     path: './tools/realtime', // Path to data on filesystem, relative to site dir.
-  //   //     routeBasePath: 'docs/realtime', // URL Route.
-  //   //     include: ['**/*.md', '**/*.mdx'],
-  //   //     sidebarPath: require.resolve('./sidebar_realtime_server.js'),
-  //   //     // disableVersioning: true, // if not set with versions, throw: Identifier 'React' has already been declared
-  //   //   },
-  //   // ],
-  //   // [
-  //   //   '@docusaurus/plugin-content-docs',
-  //   //   {
-  //   //     id: 'postgrest', // for first plugin-content-docs with "resources/" path
-  //   //     // homePageId: "doc2",
-  //   //     path: './ref/postgrest', // Path to data on filesystem, relative to site dir.
-  //   //     routeBasePath: 'ref/postgrest', // URL Route.
-  //   //     include: ['**/*.md', '**/*.mdx'],
-  //   //     sidebarPath: require.resolve('./sidebar_spec_postgrest.js'),
-  //   //     // disableVersioning: true, // if not set with versions, throw: Identifier 'React' has already been declared
-  //   //   },
-  //   // ],
-  // ],
+  plugins: [
+    [
+      '@docusaurus/plugin-content-blog',
+      {
+        /**
+         * Required for any multi-instance plugin
+         */
+        id: 'faq',
+        /**
+         * URL route for the blog section of your site.
+         * *DO NOT* include a trailing slash.
+         */
+        routeBasePath: 'faq',
+        /**
+         * Path to data on filesystem relative to site dir.
+         */
+        path: './faq',
+      },
+    ],
+    //   // [
+    //   //   '@docusaurus/plugin-content-docs',
+    //   //   {
+    //   //     id: 'supabase-client', // for first plugin-content-docs with "resources/" path
+    //   //     // homePageId: "doc2",
+    //   //     path: './ref/supabase', // Path to data on filesystem, relative to site dir.
+    //   //     routeBasePath: 'ref/supabase', // URL Route.
+    //   //     include: ['**/*.md', '**/*.mdx'],
+    //   //     sidebarPath: require.resolve('./sidebar_spec_supabase.js'),
+    //   //     // disableVersioning: true, // if not set with versions, throw: Identifier 'React' has already been declared
+    //   //   },
+    //   // ],
+    //   // [
+    //   //   '@docusaurus/plugin-content-docs',
+    //   //   {
+    //   //     id: 'postgrest-client', // for first plugin-content-docs with "resources/" path
+    //   //     // homePageId: "doc2",
+    //   //     path: './ref/postgrest', // Path to data on filesystem, relative to site dir.
+    //   //     routeBasePath: 'ref/postgrest', // URL Route.
+    //   //     include: ['**/*.md', '**/*.mdx'],
+    //   //     sidebarPath: require.resolve('./sidebar_spec_postgrest.js'),
+    //   //     // disableVersioning: true, // if not set with versions, throw: Identifier 'React' has already been declared
+    //   //   },
+    //   // ],
+    //   // [
+    //   //   '@docusaurus/plugin-content-docs',
+    //   //   {
+    //   //     id: 'gotrue-client', // for first plugin-content-docs with "resources/" path
+    //   //     // homePageId: "doc2",
+    //   //     path: './ref/gotrue', // Path to data on filesystem, relative to site dir.
+    //   //     routeBasePath: 'ref/gotrue', // URL Route.
+    //   //     include: ['**/*.md', '**/*.mdx'],
+    //   //     sidebarPath: require.resolve('./sidebar_spec_gotrue.js'),
+    //   //     // disableVersioning: true, // if not set with versions, throw: Identifier 'React' has already been declared
+    //   //   },
+    //   // ],
+    //   // [
+    //   //   '@docusaurus/plugin-content-docs',
+    //   //   {
+    //   //     id: 'realtime-client', // for first plugin-content-docs with "resources/" path
+    //   //     // homePageId: "doc2",
+    //   //     path: './ref/realtime', // Path to data on filesystem, relative to site dir.
+    //   //     routeBasePath: 'ref/realtime', // URL Route.
+    //   //     include: ['**/*.md', '**/*.mdx'],
+    //   //     sidebarPath: require.resolve('./sidebar_spec_realtime.js'),
+    //   //     // disableVersioning: true, // if not set with versions, throw: Identifier 'React' has already been declared
+    //   //   },
+    //   // ],
+    //   // [
+    //   //   '@docusaurus/plugin-content-docs',
+    //   //   {
+    //   //     id: 'realtime-server', // for first plugin-content-docs with "resources/" path
+    //   //     path: './tools/realtime', // Path to data on filesystem, relative to site dir.
+    //   //     routeBasePath: 'docs/realtime', // URL Route.
+    //   //     include: ['**/*.md', '**/*.mdx'],
+    //   //     sidebarPath: require.resolve('./sidebar_realtime_server.js'),
+    //   //     // disableVersioning: true, // if not set with versions, throw: Identifier 'React' has already been declared
+    //   //   },
+    //   // ],
+    //   // [
+    //   //   '@docusaurus/plugin-content-docs',
+    //   //   {
+    //   //     id: 'postgrest', // for first plugin-content-docs with "resources/" path
+    //   //     // homePageId: "doc2",
+    //   //     path: './ref/postgrest', // Path to data on filesystem, relative to site dir.
+    //   //     routeBasePath: 'ref/postgrest', // URL Route.
+    //   //     include: ['**/*.md', '**/*.mdx'],
+    //   //     sidebarPath: require.resolve('./sidebar_spec_postgrest.js'),
+    //   //     // disableVersioning: true, // if not set with versions, throw: Identifier 'React' has already been declared
+    //   //   },
+    //   // ],
+  ],
 }
---
+++ web/faq/2019-09-05-hello-docusaurus-v2.md
@@ -0,0 +1,18 @@
+---
+title: Welcome Docusaurus v2
+author: Joel Marcey
+author_title: Co-creator of Docusaurus 1
+author_url: https://github.com/JoelMarcey
+author_image_url: https://graph.facebook.com/611217057/picture/?height=200&width=200
+tags: [hello, docusaurus-v2]
+description: This is my first post on Docusaurus 2.
+image: https://i.imgur.com/mErPwqL.png
+hide_table_of_contents: false
+---
+
+Welcome to this blog. This blog is created with [**Docusaurus 2**](https://docusaurus.io/).
+
+
+This is my first post on Docusaurus 2.
+
+A whole bunch of exploration to follow.
