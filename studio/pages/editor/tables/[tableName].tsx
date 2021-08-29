da4cee7aa4b5dcfd83de8e2595f8d4fe5a33719c
pip <jrphiri@gmail.com>
2021-08-29 15:02:35 +0200

    fix(grid): adds missing props for full-height, new column & insert row

--- studio/pages/editor/tables/[tableName].tsx
+++ studio/pages/editor/tables/[tableName].tsx
@@ -1,21 +1,47 @@
 import EditorLayout from '../../../components/layouts/EditorLayout'
-import { SupabaseGrid } from '@supabase/grid'
+import { SupabaseGrid, SupabaseGridRef } from '@supabase/grid'
 import { useRouter } from 'next/router'
 import { SUPABASE_URL, SUPABASE_SERVICE_KEY } from '../../../lib/constants'
+import { useRef } from 'react'
 
 export default function TableEditor() {
   const router = useRouter()
   const { tableName } = router.query
+  const gridRef = useRef<SupabaseGridRef>(null)
 
   return (
     <EditorLayout title={`Editor: ${tableName}`}>
       <div className="h-screen">
         <SupabaseGrid
+          ref={gridRef}
           table={String(tableName)}
-          editable={true /* clientProps must have SERVICE_KEY not ANON_KEY */}
+          editable
+          schema="auth"
+          gridProps={{
+            height: '100%',
+          }}
           clientProps={{
             supabaseUrl: SUPABASE_URL,
             supabaseKey: SUPABASE_SERVICE_KEY,
+          }}
+          onError={(error) => {
+            console.log('ERROR: ', error)
+          }}
+          onAddColumn={() => {
+            console.log('add new column')
+          }}
+          onEditColumn={(columnName) => {
+            console.log('edit column: ', columnName)
+          }}
+          onDeleteColumn={(columnName) => {
+            console.log('delete column: ', columnName)
+          }}
+          onAddRow={() => {
+            console.log('add new row')
+            return {}
+          }}
+          onEditRow={(rowIdx) => {
+            console.log('edit row: ', rowIdx)
           }}
         />
       </div>
