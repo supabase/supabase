b360ce758b394f32f8b4190957d8285167c3679d
Paul Copplestone <pcopplestone@gmail.com>
2021-08-30 10:56:14 +0800

    formats the discussions

--- web/src/theme/BlogListPage/index.tsx
+++ web/src/theme/BlogListPage/index.tsx
@@ -52,15 +52,15 @@ function BlogListPage(props: Props): JSX.Element {
                 const month = MONTHS[parseInt(match[1], 10) - 1]
                 const day = parseInt(match[2], 10)
                 return (
-                  <div className="row is-multiline">
+                  <div className="row">
                     <Link
-                      className={clsx('card', styles.PostPreview)}
+                      className={clsx('', styles.PostPreview)}
                       to={BlogPostContent.metadata.permalink}
                       key={BlogPostContent.metadata.permalink}
                     >
                       <div className={clsx('card__body', styles.PostPreviewBody)}>
                         <div className={styles.PostPreviewHeading}>
-                          <h2>{BlogPostContent.frontMatter.title}</h2>
+                          <h3>{BlogPostContent.frontMatter.title}</h3>
                           <div className="avatar">
                             <img
                               className="avatar__photo avatar__photo--sm"
@@ -73,21 +73,7 @@ function BlogListPage(props: Props): JSX.Element {
                           <div className={styles.PreviewDate}>
                             <time dateTime={date} className={styles.blogPostDate}>
                               {month} {day}, {year}{' '}
-                              {readingTime && <> · {Math.ceil(readingTime)} min read</>}
                             </time>
-                          </div>
-                          <div className={styles.PreviewArrow}>
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              viewBox="0 0 20 20"
-                              fill="currentColor"
-                            >
-                              <path
-                                fillRule="evenodd"
-                                d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                                clipRule="evenodd"
-                              />
-                            </svg>
                           </div>
                         </div>
                       </div>
