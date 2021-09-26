fcb0e0c7eb5c88eeada872e9efeea09e4dd87602
pip <jrphiri@gmail.com>
2021-09-26 21:35:38 +0200

    fix: points default API_URL to :8080 - addresses CORS issue on :8000 (no CORS config provided for :8000/api in kong config

--- studio/lib/constants.ts
+++ studio/lib/constants.ts
@@ -2,5 +2,5 @@ export const SUPABASE_URL: string = process.env.NEXT_PUBLIC_SUPABASE_URL || 'htt
 export const SUPABASE_ANON_KEY: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
 export const SUPABASE_SERVICE_KEY: string = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY || ''
 
-export const API_URL: string = process.env.API_URL || 'http://localhost:8000/api'
+export const API_URL: string = process.env.API_URL || 'http://localhost:8080/api'
 export const PG_META_URL: string = process.env.PG_META_URL || 'http://localhost:1337'
