973ae4652751c9d0658c4548c6a7d1af78b8e554
Joshen Lim <joshenlimek@gmail.com>
2021-09-29 15:56:48 +0800

    Add link to blog page on header nav for desktop

--- www/components/Nav/index.tsx
+++ www/components/Nav/index.tsx
@@ -239,6 +239,16 @@ const Nav = (props: Props) => {
                   >
                     Pricing
                   </a>
+                  <a
+                    href="/blog"
+                    className={`
+                    inline-flex items-center px-1 border-b-2 border-transparent text-sm font-medium
+                    text-gray-500 hover:text-gray-700 hover:border-gray-500 p-5
+                    dark:text-dark-100 dark:hover:border-dark-100
+                  `}
+                  >
+                    Blog
+                  </a>
                 </div>
               </div>
               <div className="hidden lg:flex items-center sm:space-x-3">
