0c5a573e19137948c5f18c0b29365fd4ceee2a05
Copple <10214025+kiwicopple@users.noreply.github.com>
2021-10-01 11:41:30 +0100

    reverts colors on announcement

--- www/components/Nav/Annoucement.tsx
+++ www/components/Nav/Annoucement.tsx
@@ -6,7 +6,8 @@ const Annoucement = () => {
   if (!annoucement.show) return null
   return (
     <a href={annoucement.link} target="_blank">
-      <div className="bg-dark-700 text-white hover:bg-dark-600 dark:bg-white dark:text-dark-600 dark:hover:bg-dark-200 transition-colors">
+      {/* <div className="bg-dark-700 text-white hover:bg-dark-600 dark:bg-white dark:text-dark-600 dark:hover:bg-dark-200 transition-colors"> */}
+      <div className="bg-[#5865F2] text-white">
         <div className="flex items-center justify-center p-2 mx-auto space-x-2 text-sm lg:container lg:px-16 xl:px-20">
           {annoucement.text}
         </div>
