7f43e5b92ce57c42b30cb279a413160e67bbfd42
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2021-10-28 22:17:10 +0800

    fix warnings

--- www/components/ImageGrid.tsx
+++ www/components/ImageGrid.tsx
@@ -16,7 +16,7 @@ interface iImageGrid {
   lgCols?: colSizes
 }
 
-export default ({ images, smCols = 3, mdCols = 6, lgCols = 8 }: iImageGrid) => {
+const ImageGrid = ({ images, smCols = 3, mdCols = 6, lgCols = 8 }: iImageGrid) => {
   const smBreakpoint = {
     3: 'grid-cols-3',
     4: 'grid-cols-4',
@@ -47,9 +47,12 @@ export default ({ images, smCols = 3, mdCols = 6, lgCols = 8 }: iImageGrid) => {
       ${lgBreakpoint[lgCols]}
     `}
     >
-      {images.map((x) => {
+      {images.map((x, i) => {
         return (
-          <div className="col-span-1 flex items-center justify-center bg-gray-50 dark:bg-gray-700 p-8">
+          <div
+            key={`${x.name}-${i}`}
+            className="col-span-1 flex items-center justify-center bg-gray-50 dark:bg-gray-700 p-8"
+          >
             <div className="relative overflow-auto w-full h-8 p-8">
               <Image
                 layout="fill"
@@ -71,3 +74,5 @@ export default ({ images, smCols = 3, mdCols = 6, lgCols = 8 }: iImageGrid) => {
     </div>
   )
 }
+
+export default ImageGrid
