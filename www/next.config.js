071a186e982218a75abde55c287e38dec9645c18
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2021-10-28 14:55:39 +0800

    add zones setup. add baseurl to docs. remove redirects on /web

--- www/next.config.js
+++ www/next.config.js
@@ -3,4 +3,20 @@ const withMDX = require('@next/mdx')()
 module.exports = withMDX({
   basePath: '/new',
   pageExtensions: ['js', 'jsx', 'tsx', 'md', 'mdx'],
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `/:path*`,
+      },
+      {
+        source: '/docs',
+        destination: `${process.env.NEXT_PUBLIC_DOCS_URL}/docs`,
+      },
+      {
+        source: '/docs/:path*',
+        destination: `${process.env.NEXT_PUBLIC_DOCS_URL}/docs/:path*`,
+      },
+    ]
+  },
 })
