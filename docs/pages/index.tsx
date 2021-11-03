ac4df2e4f43b670309b67f1a33bc6a6aa5e7394f
pip <jrphiri@gmail.com>
2021-11-03 14:57:20 +0200

    docs: new docs website as a Nextjs project

---
+++ docs/pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Introduction | Supabase </title>
+        <meta name="description" content="Supabase docs" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}></main>
+
+      <footer className={styles.footer}></footer>
+    </div>
+  )
+}
+
+export default Home
