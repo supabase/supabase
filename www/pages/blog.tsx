2663a7fe7bb98e19bf1b3f9bb648bed8c0c2bfe7
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2021-11-22 18:24:02 +0800

    chore: switch dropdown to use tabs

--- www/pages/blog.tsx
+++ www/pages/blog.tsx
@@ -10,10 +10,21 @@ import { getSortedPosts, getAllCategories } from '~/lib/posts'
 import authors from 'lib/authors.json'
 
 import DefaultLayout from '~/components/Layouts/Default'
-import { Typography, Badge, Space, Dropdown, Button, IconChevronDown } from '@supabase/ui'
+import {
+  Typography,
+  Badge,
+  Space,
+  Dropdown,
+  Button,
+  IconChevronDown,
+  Tabs,
+  Input,
+  IconMail,
+} from '@supabase/ui'
 import PostTypes from '~/types/post'
 import BlogListItem from '~/components/Blog/BlogListItem'
 import BlogHeader from '~/components/Blog/BlogHeader'
+import { Panel } from '@supabase/ui/dist/cjs/components/Tabs/Tabs'
 
 export async function getStaticProps() {
   const allPostsData = getSortedPosts('_blog')
@@ -50,6 +61,13 @@ function Blog(props: any) {
     )
   }, [category])
 
+  useEffect(() => {
+    return props.categories.push('all')
+  }, [])
+
+  // append 'all' category
+  // const categories = props.categories.push('all')
+
   return (
     <>
       <Head>
@@ -62,26 +80,33 @@ function Blog(props: any) {
       </Head>
       <NextSeo title="Blog" description="Latest news from the Supabase team." />
       <DefaultLayout>
-        <BlogHeader title="Blog" />
-
+        {/* <BlogHeader title="Blog" className="pb-0" /> */}
         <div className="bg-white dark:bg-dark-800 overflow-hidden py-12">
           <div className="container mx-auto px-8 sm:px-16 xl:px-20 mt-16">
             <div className="mx-auto ">
-              <Typography.Title level={3}>Latests post</Typography.Title>
               {props.blogs.slice(0, 1).map((blog: any, idx: any) => {
                 return FeaturedThumb(blog)
               })}
             </div>
           </div>
         </div>
+
         <div className="border-t dark:border-dark">
           <div className="container mx-auto px-8 sm:px-16 xl:px-20 mt-16">
             <div className="mx-auto ">
               <div className="grid grid-cols-12">
-                <div className="col-span-12 lg:col-span-8">
-                  <Typography.Title level={2}>More posts from the team</Typography.Title>
+                <div className="col-span-12 lg:col-span-12">
+                  <Tabs scrollable size="medium" onChange={setCategory}>
+                    {props.categories.map((categoryId: string) => (
+                      <Tabs.Panel id={categoryId} label={categoryId}>
+                        {/* <p>{categoryId}</p> */}
+                        <></>
+                      </Tabs.Panel>
+                    ))}
+                  </Tabs>
+                  {/* <Typography.Title level={2}>More posts from the team</Typography.Title> */}
                 </div>
-                <div className="col-span-12 lg:col-span-4 mt-4 lg:mt-0">
+                {/* <div className="col-span-12 lg:col-span-4 mt-4 lg:mt-0">
                   <Space className="lg:justify-end" size={6}>
                     <Typography.Text>Select a category</Typography.Text>
                     <Dropdown
@@ -111,7 +136,7 @@ function Blog(props: any) {
                       </Button>
                     </Dropdown>
                   </Space>
-                </div>
+                </div> */}
               </div>
             </div>
 
@@ -119,7 +144,7 @@ function Blog(props: any) {
               <div className="grid grid-cols-12 mt-16 gap-16">
                 {/* <ul> */}
                 {blogs.map((blog: PostTypes, idx: number) => (
-                  <div className="col-span-3 mb-16">
+                  <div className="col-span-4 mb-16">
                     <BlogListItem blog={blog} key={idx} />
                   </div>
                 ))}
