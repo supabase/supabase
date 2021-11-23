90b1670ae9380302e419c04faeb3ada91e372d10
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2021-11-23 19:41:05 +0800

    chore: update announcement

--- www/components/Nav/Announcement.tsx
+++ www/components/Nav/Announcement.tsx
@@ -1,18 +1,45 @@
 import React from 'react'
+import Link from 'next/link'
 
 import announcement from '~/data/Announcement.json'
+import { IconChevronRight, Button, IconX } from '@supabase/ui'
 
 const Announcement = () => {
   if (!announcement.show) return null
   return (
-    <a href={announcement.link} target="_blank">
-      {/* <div className="bg-dark-700 text-white hover:bg-dark-600 dark:bg-white dark:text-dark-600 dark:hover:bg-dark-200 transition-colors"> */}
-      <div className="bg-[#5865F2] text-white">
-        <div className="flex items-center justify-center p-2 mx-auto space-x-2 text-sm lg:container lg:px-16 xl:px-20">
-          {announcement.text}
+    <Link href={announcement.link}>
+      <div
+        className="
+          relative
+          cursor-pointer
+          bg-gradient-to-r from-green-400 to-green-600
+          hover:from-green-500 hover:to-green-700        
+          text-white 
+          flex flex-row space-x-3
+        "
+      >
+        <div
+          className="
+            flex items-center justify-center p-3 mx-auto text-sm lg:container lg:px-16 xl:px-20 font-medium 
+            lg:divide-x divide-green-400
+          "
+        >
+          <span className="px-3 hidden lg:block">{announcement.text}</span>
+          <span className="px-3 flex items-center space-x-2">
+            <span>
+              {
+                // @ts-ignore
+                announcement.cta
+              }
+            </span>
+            <IconChevronRight size={14} />
+          </span>
+        </div>
+        <div className="transition-opacity absolute right-4 h-full flex items-center opacity-50 hover:opacity-100">
+          <IconX size={16} />
         </div>
       </div>
-    </a>
+    </Link>
   )
 }
 
