79701a57c3e947e78a229467f70578fe51d22ba8
Copple <10214025+kiwicopple@users.noreply.github.com>
2021-11-24 19:44:16 +0800

    add simple test for meta

--- tests/performance/meta.js
+++ tests/performance/meta.js
@@ -1,20 +1,12 @@
-// require('./../test-env')
-// const env = path.join(__dirname, '../.env.test')
-// const envConfig = dotenv.parse(fs.readFileSync(env))
-// for (const k in envConfig) {
-//   process.env[k] = envConfig[k]
-// }
-
-// console.log('envConfig', envConfig)
-
-// @ts-ignore
 import http from 'k6/http'
-// @ts-ignore
-import { sleep } from 'k6'
 
-// console.log('__ENV.', __ENV.SUPABASE_URL)
+export const options = {
+  vus: 5,
+  duration: '10s',
+}
+
+const META_URL = 'http://localhost:5555/tables'
+
 export default function () {
-  http.get(__ENV.SUPABASE_URL)
-
-  sleep(1)
+  http.get(META_URL)
 }
