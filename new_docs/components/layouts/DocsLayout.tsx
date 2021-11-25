754b3935e9e1d7e82e7bfa6e1697154822270fc7
pip <jrphiri@gmail.com>
2021-11-25 17:29:48 +0200

    refactor(new_docs): moves all layout elements to DocsLayout

--- new_docs/components/layouts/DocsLayout.tsx
+++ new_docs/components/layouts/DocsLayout.tsx
@@ -1,20 +1,29 @@
 import { ReactElement } from 'react'
+import Head from 'next/head'
 import styles from '../../styles/Home.module.css'
 import NavBar from '../nav/NavBar'
 import SideBar from '../nav/SideBar'
 import Footer from '../Footer'
 
-const DocsLayout = ({ children }: { children: ReactElement }) => {
+const DocsLayout = ({ title, children }: { title: String; children: ReactElement }) => {
   return (
-    <div className={`${styles.container} h-full`}>
-      <main className={`${styles.main}`}>
-        <NavBar />
-        <div className="flex flex-row ">
-          <SideBar />
-          <div className={`${styles.content} p-8`}>{children}</div>
-        </div>
-        <Footer />
-      </main>
+    <div className="h-screen">
+      <Head>
+        <title>${title} | Supabase</title>
+        <meta name="description" content="Supabase docs" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className={`${styles.container} h-full`}>
+        <main className={`${styles.main}`}>
+          <NavBar />
+          <div className="flex flex-row ">
+            <SideBar />
+            <div className={`${styles.content} p-8`}>{children}</div>
+          </div>
+          <Footer />
+        </main>
+      </div>
     </div>
   )
 }
--- new_docs/pages/index.tsx
+++ new_docs/pages/index.tsx
@@ -1,20 +1,12 @@
 import { Typography } from '@supabase/ui'
 import type { NextPage } from 'next'
-import Head from 'next/head'
 import DocsLayout from '../components/layouts/DocsLayout'
 
 const Home: NextPage = () => {
   return (
-    <div className="h-screen">
-      <Head>
-        <title>Introduction | Supabase</title>
-        <meta name="description" content="Supabase docs" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <DocsLayout>
-        <Typography.Title level={1}>Introduction</Typography.Title>
-      </DocsLayout>
-    </div>
+    <DocsLayout title="Introduction">
+      <Typography.Title level={1}>Introduction</Typography.Title>
+    </DocsLayout>
   )
 }
 
