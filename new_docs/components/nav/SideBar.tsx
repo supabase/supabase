361b2dd52741d1d7c2321f81dafa043480a4dc63
pip <jrphiri@gmail.com>
2021-11-25 23:25:47 +0200

    refactor(new_docs): moves menu items to external json file

--- new_docs/components/nav/SideBar.tsx
+++ new_docs/components/nav/SideBar.tsx
@@ -1,38 +1,37 @@
 import { Menu } from '@supabase/ui'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 import styles from '../../styles/Home.module.css'
 import ThemeToggle from '../ThemeToggle'
+import menuItems from './menu-items.json'
 
 const SideBar = () => {
+  const { pathname } = useRouter()
   return (
     <div className={`${styles.sidebar} fixed relative h-full`}>
       <Menu className={`border-r dark:border-gray-600`}>
         <div>
-          <Menu.Group title="Overview" />
-          <Menu.Item active showActiveBar>
-            Introduction
-          </Menu.Item>
-          <Menu.Item>Architecture</Menu.Item>
-          <Menu.Item>Database</Menu.Item>
-          <Menu.Item>Auth</Menu.Item>
-          <Menu.Item>Storage</Menu.Item>
-          <Menu.Item>APIs</Menu.Item>
-          <Menu.Item>Examples and Resources</Menu.Item>
-          <Menu.Group title="Tutorials" />
-          <Menu.Item>Quickstart: Angular</Menu.Item>
-          <Menu.Item>Quickstart: Flutter</Menu.Item>
-          <Menu.Item>Quickstart: Next.js</Menu.Item>
-          <Menu.Item>Quickstart: React</Menu.Item>
-          <Menu.Item>Quickstart: RedwoodJS</Menu.Item>
-          <Menu.Item>Quickstart: Svelte</Menu.Item>
-          <Menu.Item>Quickstart: Vue 3</Menu.Item>
-          <Menu.Group title="Tutorials" />
-          <Menu.Item>FAQs</Menu.Item>
-          <Menu.Item>Going into Prod Checklist</Menu.Item>
-          <Menu.Item>Contributing</Menu.Item>
-          <Menu.Item>SupaSquad</Menu.Item>
-          <Menu.Item>Terms of Service</Menu.Item>
-          <Menu.Item>Privacy Policy</Menu.Item>
-          <Menu.Item>Acceptable Use Policy</Menu.Item>
+          {Object.keys(menuItems).map((key) => {
+            return (
+              <span key={key}>
+                <Menu.Group title={key} />
+                {menuItems[key].map((item) => (
+                  <span key={item.link}>
+                    <Link href={item.link} passHref>
+                      <a>
+                        <Menu.Item
+                          active={pathname === item.link}
+                          showActiveBar={pathname === item.link}
+                        >
+                          {item.text}
+                        </Menu.Item>
+                      </a>
+                    </Link>
+                  </span>
+                ))}
+              </span>
+            )
+          })}
         </div>
         <div className="mt-16 w-full">
           <Menu.Item>
