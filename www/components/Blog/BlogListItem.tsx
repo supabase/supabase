969cdd0dab19952ad6d470df153ec73a0a7aae7c
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2021-11-26 19:37:55 +0800

    chore: spacing

--- www/components/Blog/BlogListItem.tsx
+++ www/components/Blog/BlogListItem.tsx
@@ -20,35 +20,36 @@ const BlogListItem = ({ blog }: Props) => {
     <div key={blog.slug}>
       <a href={`/blog/${blog.url}`}>
         <div className="inline-block min-w-full group">
-          <div className="flex flex-col space-y-4">
-            <div
-              className={`relative overflow-auto w-full h-60 border dark:border-dark shadow-sm rounded-lg mb-4`}
-            >
-              <Image
-                layout="fill"
-                src={
-                  !blog.thumb
-                    ? `https://images.unsplash.com/photo-1569982175971-d92b01cf8694?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1`
-                    : `/images/blog/${blog.thumb}`
-                }
-                objectFit="cover"
-                className="transform duration-100 ease-in scale-100 group-hover:scale-105"
-              />
+          <div className="flex flex-col space-y-6">
+            <div className="flex flex-col space-y-3">
+              <div
+                className={`relative overflow-auto w-full h-60 border dark:border-dark shadow-sm rounded-lg mb-4`}
+              >
+                <Image
+                  layout="fill"
+                  src={
+                    !blog.thumb
+                      ? `https://images.unsplash.com/photo-1569982175971-d92b01cf8694?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1`
+                      : `/images/blog/${blog.thumb}`
+                  }
+                  objectFit="cover"
+                  className="transform duration-100 ease-in scale-100 group-hover:scale-105"
+                />
+              </div>
+
+              <div>
+                <Typography.Title level={3} className="m-0">
+                  {blog.title}
+                </Typography.Title>
+              </div>
+              <Typography.Text type="secondary" small>
+                {blog.date}
+              </Typography.Text>
+
+              <Typography.Text className="m-0" type="secondary">
+                <p className="text-base mb-0">{blog.description}</p>
+              </Typography.Text>
             </div>
-
-            <div>
-              <Typography.Title level={3} className="m-0">
-                {blog.title}
-              </Typography.Title>
-            </div>
-            <Typography.Text type="secondary" small>
-              {blog.date}
-            </Typography.Text>
-
-            <Typography.Text className="m-0" type="secondary">
-              <p className="text-base mb-0">{blog.description}</p>
-            </Typography.Text>
-
             {author && (
               <div>
                 <Space size={4}>
