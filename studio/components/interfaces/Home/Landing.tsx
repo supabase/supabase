cd96e6d31f7ba795e44cffc08dab7d2d2754204e
Isaiah Hamilton <70828596+Isaiah-Hamilton@users.noreply.github.com>
2021-11-27 03:04:14 -0500

    Change supabase.io to supabase.com

--- studio/components/interfaces/Home/Landing.tsx
+++ studio/components/interfaces/Home/Landing.tsx
@@ -20,7 +20,7 @@ const Landing = () => {
         <nav className="relative flex items-center justify-between sm:h-10">
           <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
             <div className="flex items-center justify-between w-full md:w-auto">
-              <a href="https://supabase.io">
+              <a href="https://supabase.com">
                 <Image
                   src={theme == 'dark' ? '/img/supabase-dark.svg' : '/img/supabase-light.svg'}
                   alt=""
