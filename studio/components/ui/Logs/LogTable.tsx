23f39b7a661a566445f2c7e610723862aaddbcb3
TzeYiing <ty@tzeyiing.com>
2021-11-30 04:02:57 +0800

    added focused log line metadata display

--- studio/components/ui/Logs/LogTable.tsx
+++ studio/components/ui/Logs/LogTable.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Table from 'components/to-be-cleaned/Table'
-import { Typography } from '@supabase/ui'
+import { Typography, SidePanel } from '@supabase/ui'
 import dayjs from 'dayjs'
+import { Panel } from '@supabase/ui/dist/cjs/components/Tabs/Tabs'
+import LogSelection from './LogSelection'
 
 interface Props {
   data: LogData[] | null | undefined
@@ -17,12 +19,13 @@ export interface LogData {
   metadata: Metadata
 }
 
-
 /**
- * Logs table view
+ * Logs table view with focus side panel
  */
 const LogTable = ({ data }: Props) => {
   const columns = ["timestamp", "event_message"]
+
+  const [focusedLog, setFocusedLog] = useState<LogData | null>(null);
 
   if (!data) {
     return null
@@ -32,18 +35,24 @@ const LogTable = ({ data }: Props) => {
     return <Typography.Text className="text-center">No data returned from query</Typography.Text>
   }
 
-  return (
-    <Table containerClassName="mx-auto container"
+  return (<section className="flex flex-row gap-x-4">
+    <Table containerClassName="mx-auto container overflow-y h-full"
       head={columns.map((c) => <Table.th key={c}>{c}</Table.th>)}
       body={data.map(d =>
         <Table.tr key={d.id}>
-          {columns.map(c => <Table.td key={c} >
+          {columns.map(c => <Table.td key={c} className={`${d.id === focusedLog?.id ? "bg-green-800" : ""} `} onClick={() => setFocusedLog(d)}>
             {c === "timestamp" &&
-              <Typography.Text className="block bg-green-800" code>{dayjs(d[c] / 1000).toString()}</Typography.Text>}
-            {c === "event_message" && <Typography.Text className="block" code>{d[c]}</Typography.Text>}
+              <Typography.Text className="block bg-green-800" small code>{dayjs(d[c] / 1000).toString()}</Typography.Text>}
+            {c === "event_message" && <Typography.Text className="block" small code>{d[c]}</Typography.Text>}
           </Table.td>)}
         </Table.tr>)}
     />
+    {focusedLog && (
+      <div className="sticky top-0">
+        <LogSelection onClose={() => setFocusedLog(null)} log={focusedLog} />
+      </div>
+    )}
+  </section>
   )
 }
 export default LogTable
