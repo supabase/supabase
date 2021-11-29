a6666014958b924bdfdc7c2125b4549022729d7b
TzeYiing <ty@tzeyiing.com>
2021-11-30 03:12:18 +0800

    added simple client side text search

--- studio/pages/project/[ref]/settings/logs/[type].tsx
+++ studio/pages/project/[ref]/settings/logs/[type].tsx
@@ -6,10 +6,10 @@ import { LOG_TYPE_LABEL_MAPPING } from 'lib/constants'
 import { observer } from 'mobx-react-lite'
 import { withAuth } from 'hooks'
 import { get } from "lib/common/fetch"
-import LogTable, {LogData} from 'components/ui/Logs/LogTable'
+import LogTable, { LogData } from 'components/ui/Logs/LogTable'
 import useSWR from 'swr'
 import { API_URL } from 'lib/constants'
-import { Typography } from '@supabase/ui'
+import { Typography, Input } from '@supabase/ui'
 
 /**
  * Acts as a container component for the entire log display
@@ -18,22 +18,31 @@ const LogPage = () => {
   const router = useRouter()
   const { ref, type } = router.query
   const title = LOG_TYPE_LABEL_MAPPING[type as string]
+  const [searchQuery, setSearchQuery] = useState("")
 
   // handle log fetching
   const {
     data,
     isValidating: isLoading,
     mutate,
-  } = useSWR<{ data: LogData[],  count: number, error: any }>(`${API_URL}/projects/${ref}/logs?type=${type}`, get, {
+  } = useSWR<{ data: LogData[], count: number, error: any }>(`${API_URL}/projects/${ref}/logs?type=${type}`, get, {
     revalidateOnFocus: false,
   })
   console.log(data)
   const { data: logData, error } = data || {}
+
+  const filteredData = logData && searchQuery ? logData.filter(d => {
+    return d.event_message.toLowerCase().includes(searchQuery.toLowerCase())
+  }) : logData
   return (
     <SettingsLayout title={title} className="p-4">
       <LogPanel isLoading={isLoading} onRefresh={() => mutate()} />
+
+      <div className="flex flex-row justify-end p-4">
+        <Input className="max-w-32" placeholder="Search" onChange={e => setSearchQuery(e.target.value)} value={searchQuery} />
+      </div>
       {error && (<Typography.Text className="text-center w-full block">Could not fetch data</Typography.Text>)}
-      <LogTable data={logData} />
+      <LogTable data={filteredData} />
     </SettingsLayout>
   )
 }
