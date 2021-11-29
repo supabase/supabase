6505225ffb66d41af1138084710e16dfe53097b4
TzeYiing <ty@tzeyiing.com>
2021-11-29 19:30:51 +0800

    adjusted logs styling, added swr for data fetching

--- studio/components/ui/Logs/LogPanel.tsx
+++ studio/components/ui/Logs/LogPanel.tsx
@@ -1,28 +1,38 @@
 import React from 'react'
 import { Button, Icon, Typography } from '@supabase/ui'
+import Panel from 'components/to-be-cleaned/Panel'
+
 interface Props {
-  title: string
   onRefresh?: () => void
   isLoading: boolean
+  children?: JSX.Element
   heading?: JSX.Element
 }
 
 /**
  * Logs control panel header + wrapper
  */
-const LogPanel = ({ onRefresh, isLoading, heading }: Props) => (
-  <div className="flex items-center justify-between w-full">
-    <div>
-      {heading}
-    </div>
-    <Button
-      type="outline"
-      loading={isLoading}
-      disabled={isLoading ? true : false}
-      onClick={onRefresh}
-    >
-      Refresh
-    </Button>
-  </div>
+const LogPanel = ({ onRefresh, isLoading, heading, children }: Props) => (
+  <Panel
+    title={
+      <div
+        className="flex items-center justify-between w-full"
+      >
+        <Button type="secondary">Recent - All</Button>
+        <div>
+          <Button
+            type="outline"
+            loading={isLoading}
+            disabled={isLoading ? true : false}
+            onClick={onRefresh}
+          >
+            Refresh
+          </Button>
+        </div>
+      </div>
+    }
+    footer={children}
+  >
+  </Panel>
 )
 export default LogPanel
