209d2924e966980d192a4fe9fe4b4f8c1a1f5288
TzeYiing <ty@tzeyiing.com>
2021-12-02 18:06:47 +0800

    added ui logic for new log polling, regex log search.

--- studio/tests/pages/projects/logs.test.js
+++ studio/tests/pages/projects/logs.test.js
@@ -21,7 +21,11 @@ useRouter.mockReturnValue({ query: { ref: "123", type: "auth" } })
 import { LogPage } from 'pages/project/[ref]/settings/logs/[type]'
 import { render, fireEvent, waitFor, screen } from '@testing-library/react'
 import { SWRConfig } from 'swr'
+import userEvent from '@testing-library/user-event'
 
+beforeEach(() => {
+  get.mockReset()
+})
 test('can display log data and metadata', async () => {
   const data = [{
     id: "seome-uuid",
@@ -42,6 +46,7 @@ test('can display log data and metadata', async () => {
 })
 
 test('Refresh', async () => {
+
   const data = [{
     id: "some-uuid",
     timestamp: 1621323232312,
@@ -50,25 +55,71 @@ test('Refresh', async () => {
       my_key: "something_value"
     }
   }]
-  get.mockResolvedValue({ data })
-  render(
-    <SWRConfig value={{ dedupingInterval: null }}>
-      <LogPage />
-    </SWRConfig>
-  )
+  get.mockResolvedValueOnce({ data }).mockResolvedValueOnce({ data: [] })
+  render(<LogPage />)
 
   const row = screen.getByText(/happened/)
   fireEvent.click(row)
   await waitFor(() => screen.getByText(/my_key/))
-  expect(get).toBeCalledTimes(1)
-  get.mockResolvedValue({ data: [] })
 
-  // simulate full refresh, loop because swr dedupe behaves weirdly
-  await waitFor(() => {
-    fireEvent.click(screen.getByText(/Refresh/))
-    expect(get).toBeCalledTimes(2)
-  })
+  // simulate refresh
+  // wrap with a waitFor, and trigger twice, as button becomes disabled when Loading and may take time to reset
+  await waitFor(() => userEvent.click(screen.getByText(/Refresh/)))
+  // called by counter twice and refresh twice
+  await waitFor(() => expect(get).toBeCalledTimes(4), { timeout: 1000 })
   // when log line unmounts and it was focused, should close focus panel
-  await waitFor(() => expect(() => screen.getByText(/my_key/)).toThrow())
-  await waitFor(() => expect(() => screen.getByText(/happened/)).toThrow())
+  await waitFor(() => screen.queryByText(/my_key/) === null, { timeout: 1000 })
+  await waitFor(() => screen.queryByText(/happened/) === null, { timeout: 1000 })
+})
+
+
+test("Search will trigger a log refresh", async () => {
+  get.mockImplementation((url) => {
+
+    if (url.includes("search_query") && url.includes("something")) {
+      return {
+        data: [{
+          id: "some-uuid",
+          timestamp: 1621323232312,
+          event_message: "some event happened",
+          metadata: {}
+        }]
+      }
+    }
+    return { data: [] }
+  })
+  render(<LogPage />)
+
+  userEvent.type(screen.getByPlaceholderText(/Search/), "something")
+  await waitFor(() => {
+    expect(get).toHaveBeenCalledWith(expect.stringContaining("search_query"))
+    expect(get).toHaveBeenCalledWith(expect.stringContaining("something"))
+  })
+
+  await waitFor(() => screen.getByText(/happened/))
+})
+
+test("poll count for new messages", async () => {
+  get.mockImplementation((url) => {
+    if (url.includes("count")) {
+      return { data: [{ count: 3 }] }
+    }
+    return {
+      data: [{
+        id: "some-uuid",
+        timestamp: 1621323232312,
+        event_message: "some event happened",
+        metadata: {}
+      }]
+    }
+  })
+  render(
+    <LogPage />
+  )
+  await waitFor(() => screen.queryByText(/happened/) === null)
+  await waitFor(() => screen.getByText(/Load new logs/))
+
+  userEvent.click(screen.getByText(/Load new logs/))
+  await waitFor(() => screen.queryByText(/Load new logs/) === null)
+  await waitFor(() => screen.getByText(/happened/))
 })
