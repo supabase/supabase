4b3e3476b390d40897eb51fd38297016ebeb6622
darkgl0w <31093081+darkgl0w@users.noreply.github.com>
2021-12-05 12:01:48 +0100

    feat: handle graceful exit and improve logging

--- docker/apps/api/index.ts
+++ docker/apps/api/index.ts
@@ -3,10 +3,19 @@ import { config } from './config/config'
 import plugins from './config/plugins'
 
 // Global Fastify instance
-const server = fastify()
+const server = fastify({
+  // In production we want INFO level logs while in dev mode we want TRACE level
+  logger: process.env.NODE_ENV === 'production' ? true : { level: 'trace' }
+})
 
 // Server handler
 const main = async () => {
+  // Exit on unhandledRejection
+  process.on('unhandledRejection', (err) => {
+    console.error(err)
+    process.exitCode = 1
+  })
+
   try {
     // Load plugins
     await plugins(server, config)
@@ -15,16 +24,31 @@ const main = async () => {
     await server.ready()
 
     // Start server
-    const address = await server.listen(process.env.PORT!, process.env.HOST!)
+    await server.listen(process.env.PORT!, process.env.HOST!)
 
-    // Success
-    console.log(`Server listening at ${address}`)
+    for (const signal of ['SIGINT', 'SIGTERM']) {
+      // Double signals exits the app.
+      process.once(signal, () => {
+        server.log.info({ signal }, 'closing application')
+
+        server
+          .close()
+          .then(() => {
+            server.log.info({ signal }, 'application closed')
+            process.exitCode = 0
+          })
+          .catch((err) => {
+            server.log.error({ err }, 'error closing the application')
+            process.exitCode = 1
+          })
+      })
+    }
   } catch (err) {
     // Handle error
     console.error(err)
 
     // Exit
-    process.exit(1)
+    process.exitCode = 1
   }
 }
 
