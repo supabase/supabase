c2d0ae19b72ac963519ef58bf4d8798452309c43
pip <jrphiri@gmail.com>
2021-12-08 16:38:47 +0200

    removed old experimental studio

--- studio/lib/constants.ts
+++
@@ -1,4 +0,0 @@
-
-
-export const SUPABASE_URL: string = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
-export const SUPABASE_ANON_KEY: string = process.env.NEXT_PUBLIC_SUAPBASE_ANON_KEY || ''
--- studio/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import DashboardLayout from '../components/layouts/DashboardLayout'
-
-export default function Home() {
-  return (
-    <DashboardLayout>
-      <h1>Home page</h1>
-    </DashboardLayout>
-  )
-}
