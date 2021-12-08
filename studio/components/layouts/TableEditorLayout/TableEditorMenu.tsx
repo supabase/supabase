92faefb06eea2d59653d75a68592958cca1dc8f7
Nikhil Nayak <nikhil.nixel@gmail.com>
2021-12-08 16:50:29 +0530

    strong typing variables in TableEditorMenu

--- studio/components/layouts/TableEditorLayout/TableEditorMenu.tsx
+++ studio/components/layouts/TableEditorLayout/TableEditorMenu.tsx
@@ -20,7 +20,7 @@ import {
   IconLoader,
   IconRefreshCw,
 } from '@supabase/ui'
-import { PostgresTable } from '@supabase/postgres-meta'
+import { PostgresSchema, PostgresTable } from '@supabase/postgres-meta'
 
 import Base64 from 'lib/base64'
 import { useStore } from 'hooks'
@@ -74,8 +74,8 @@ const TableEditorMenu: FC<Props> = ({
     setIsRefreshing(false)
   }
 
-  const schemas: any[] = meta.schemas.list()
-  const tables: any[] = meta.tables.list((table: PostgresTable) => table.schema === selectedSchema)
+  const schemas: PostgresSchema[] = meta.schemas.list()
+  const tables: PostgresTable[] = meta.tables.list((table: PostgresTable) => table.schema === selectedSchema)
 
   const schemaTables =
     searchText.length === 0
