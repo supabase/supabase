f81cfe08c33bcf83a47ffc3f67726e8089df113c
Joshen Lim <joshenlimek@gmail.com>
2021-12-08 19:53:56 +0700

    Update logs page UI

---
+++ studio/components/interfaces/Settings/Logs/Logs.constants.ts
@@ -0,0 +1,11 @@
+import { LogTemplate } from '.'
+
+export const TEMPLATES: LogTemplate[] = [
+  { label: 'Recent Errors', mode: 'simple', searchString: '[Ee]rror|\\s[45][0-9][0-9]\\s' },
+  {
+    label: 'POST or PATCH',
+    mode: 'custom',
+    searchString:
+      "REGEXP_CONTAINS(event_message, 'POST') OR REGEXP_CONTAINS(event_message, 'PATCH') ",
+  },
+]
