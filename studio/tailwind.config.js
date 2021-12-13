817e325b68dabc18f9ae1e13abd06ea3e8b1dcc2
Harsh Karande <harshskarande@gmail.com>
2021-12-13 10:40:30 +0530

    fix: remove invalid prism token reference

--- studio/tailwind.config.js
+++ studio/tailwind.config.js
@@ -48,19 +48,6 @@ const coolGray = {
   900: '#111827',
 }
 
-const teal = {
-  50: '#f0fdfa',
-  100: '#ccfbf1',
-  200: '#99f6e4',
-  300: '#5eead4',
-  400: '#2dd4bf',
-  500: '#14b8a6',
-  600: '#0d9488',
-  700: '#0f766e',
-  800: '#115e59',
-  900: '#134e4a',
-}
-
 module.exports = {
   mode: 'jit',
   darkMode: 'class', // or 'media' or 'class'
@@ -90,7 +77,6 @@ module.exports = {
         green: { ...green },
         blueGray: { ...blueGray },
         coolGray: { ...coolGray },
-        teal: { ...teal },
 
         /*  typography */
         'typography-body': {
