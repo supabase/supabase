3dfc2e116630be918d5dd0c24dc6a0e563476890
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2021-12-16 21:07:40 -0500

    fix: console error

--- www/components/TextLink.tsx
+++ www/components/TextLink.tsx
@@ -4,12 +4,12 @@ import Link from 'next/link'
 function TextLink({ url, label }: any) {
   return (
     <a href={url} className="block text-sm text-gray-400 dark:text-gray-400 mt-3">
-     <div className="flex items-center">
-       <span>{label}</span>
-       <span className="ml-2">
-         <IconArrowRight size="tiny" />
-       </span>
-     </div>
+      <div className="flex items-center">
+        <span>{label}</span>
+        <span className="ml-2">
+          <IconArrowRight size="tiny" />
+        </span>
+      </div>
     </a>
   )
 }
