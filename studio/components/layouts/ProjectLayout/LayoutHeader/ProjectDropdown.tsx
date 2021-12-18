a344d9164ca48b67ca4ed5b6ffd569f281c39edb
Waldemar Penner <waldemar.penner91@gmail.com>
2021-12-18 13:46:24 +0100

    Bugfix[#4327]: stay on route when changing project

--- studio/components/layouts/ProjectLayout/LayoutHeader/ProjectDropdown.tsx
+++ studio/components/layouts/ProjectLayout/LayoutHeader/ProjectDropdown.tsx
@@ -4,16 +4,15 @@ import { Button, Dropdown, Divider, IconPlus } from '@supabase/ui'
 
 import { useStore } from 'hooks'
 import { IS_PLATFORM, PROJECT_STATUS } from 'lib/constants'
+import { useRouter } from 'next/router'
 
 const ProjectDropdown = () => {
   const { app, ui } = useStore()
   const selectedOrganizationProjects = app.projects.list()
   const selectedOrganizationSlug = ui.selectedOrganization?.slug
   const selectedProject: any = ui.selectedProject
-
-  // [Joshen] If let's say we want to support changing projects to retain sub-route
-  // const currentSubRoute = router.route.split('/[ref]/')[1] || ''
-  // But need to ensure that pages update correctly when the project ref changes
+  const router = useRouter()
+  const currentRoute = router?.route
 
   return IS_PLATFORM ? (
     <Dropdown
@@ -25,7 +24,7 @@ const ProjectDropdown = () => {
             .filter((x: any) => x.status !== PROJECT_STATUS.INACTIVE)
             .sort((a: any, b: any) => a.name.localeCompare(b.name))
             .map((x: any) => (
-              <Link key={x.ref} href={`/project/${x.ref}`}>
+              <Link key={x.ref} href={currentRoute?.replace('[ref]', x.ref) ?? `/project/${x.ref}`}>
                 <a className="block">
                   <Dropdown.Item>{x.name}</Dropdown.Item>
                 </a>
