21a510dc758d6b5e692b68dcc613701af3f63a9e
Joshen Lim <joshenlimek@gmail.com>
2021-12-21 16:22:55 +0700

    Fix build error in API docs page

--- studio/pages/project/[ref]/api/index.tsx
+++ studio/pages/project/[ref]/api/index.tsx
@@ -78,7 +78,6 @@ const DocView: FC<any> = observer(({}) => {
   const swaggerUrl = data?.autoApiService?.restUrl
   const headers: any = { apikey: API_KEY }
 
-  if (IS_PLATFORM) data?.autoApiService?.endpoint = `https://${data?.autoApiService?.endpoint}`
   if (API_KEY?.length > 40) headers['Authorization'] = `Bearer ${API_KEY}`
 
   const { data: jsonSchema, error: jsonSchemaError } = useSWR(
@@ -113,7 +112,13 @@ const DocView: FC<any> = observer(({}) => {
     )
 
   // Data Loaded
-  const { autoApiService } = data
+  const autoApiService = {
+    ...data.autoApiService,
+    endpoint: IS_PLATFORM
+      ? `https://${data?.autoApiService?.endpoint}`
+      : data.autoApiService.endpoint,
+  }
+
   const { query } = router
   const { page, resource, rpc } = query
   const { paths, definitions } = PageState.jsonSchema
