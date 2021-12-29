fd7ca03788160074981427eb15567f41251585e5
Saurabh Srivastava <vasudeveloper001@gmail.com>
2021-12-29 23:54:37 +0530

    added-react-native-todo-list-example-v1: fixing promise errors

--- examples/ReactNativeTodoList/native/lib/constants.js
+++ examples/ReactNativeTodoList/native/lib/constants.js
@@ -8,8 +8,6 @@ if (!process.env.REACT_NATIVE_SUPABASE_URL) {
 export const SUPABASE_URL = process.env.REACT_NATIVE_SUPABASE_URL;
 export const SUPABASE_ANON_KEY = process.env.REACT_NATIVE_SUPABASE_ANON_KEY;
 
-console.log('SUPABASE: ', SUPABASE_URL, SUPABASE_ANON_KEY);
-
 export const Styles = {
   fontNormal: 20,
   fontMedium: 28,
