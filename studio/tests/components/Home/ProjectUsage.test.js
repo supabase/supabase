4583dafed0f1ed60426278f70d129b14e6959f70
TzeYiing <ty@tzeyiing.com>
2021-12-30 02:20:08 +0800

    add in clickDropdown helper, radix setup file for dropdown workaroud, added interval dropdown

--- studio/tests/components/Home/ProjectUsage.test.js
+++ studio/tests/components/Home/ProjectUsage.test.js
@@ -1,4 +1,3 @@
-import ProjectUsage from 'components/interfaces/Home/ProjectUsage'
 import { render, waitFor, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 jest.mock('lib/common/fetch')
@@ -7,43 +6,84 @@ import { get } from 'lib/common/fetch'
 // mock the router
 jest.mock('next/router')
 import { useRouter } from 'next/router'
+import { clickDropdown } from 'tests/helpers'
 useRouter.mockReturnValue({ query: { ref: '123' } })
+
+// need to wrap component with SWRConfig in order to clear cache between tests
+// TODO: abstract out to global setup
+import { SWRConfig } from 'swr'
+const ProjectUsage = jest.fn() 
+ProjectUsage.mockImplementation((props) => {
+  const Original = jest.requireActual('components/interfaces/Home/ProjectUsage').default
+  // wrap with SWR to reset the cache each time
+  return (
+    <SWRConfig value={{ provider: () => new Map() }}>
+      <Original {...props} />
+    </SWRConfig>
+  )
+})
+
 
 beforeEach(() => {
   get.mockReset()
 })
 
+const MOCK_CHART_DATA = {
+  data: [
+    {
+      total_auth_requests: 123,
+      total_realtime_requests: 223,
+      total_storage_requests: 323,
+      total_rest_requests: 333,
+      timestamp: new Date().getTime() / 1000,
+    },
+  ],
+  total: 123 + 223 + 323,
+  totalAverage: 123 + 223 + 323,
+  totalGrouped: {
+    total_auth_requests: 123,
+    total_realtime_requests: 223,
+    total_storage_requests: 323,
+    total_rest_requests: 333,
+  },
+}
+
 test('mounts correctly', async () => {
   get.mockImplementation((url) => {
     if (url.includes('usage')) return {}
-    return {
-      data: {
-        data: [
-          {
-            total_auth_requests: 123,
-            total_realtime_requests: 223,
-            total_storage_requests: 323,
-            total_rest_requests: 333,
-            timestamp: new Date().getTime() / 1000,
-          },
-        ],
-        total: 123 + 223 + 323,
-        totalAverage: 123 + 223 + 323,
-        totalGrouped: {
-          total_auth_requests: 123,
-          total_realtime_requests: 223,
-          total_storage_requests: 323,
-          total_rest_requests: 333,
-        },
-      },
-    }
+    return { data: MOCK_CHART_DATA }
   })
   render(<ProjectUsage project="12345" />)
-  await waitFor(() => screen.getByText('Statistics for past 24 hours'))
+  await waitFor(() => screen.getByText(/Statistics for past 60 minutes/))
   await waitFor(() => screen.getByText(/123/))
   await waitFor(() => screen.getByText(/223/))
   await waitFor(() => screen.getByText(/323/))
   await waitFor(() => screen.getByText(/333/))
 })
 
-test.todo('dropdown options changes chart query')
+test('dropdown options changes chart query', async () => {
+  get.mockImplementation((url) => {
+    console.log(url)
+    if (url.includes('usage')) return {}
+    return { data: MOCK_CHART_DATA }
+  })
+  render(<ProjectUsage project="12345" />)
+  await waitFor(() => screen.getByText(/Statistics for past 60 minutes/))
+  await waitFor(() => screen.getAllByRole('button', { name: '60 minutes' }))
+  console.log(get.mock.calls)
+  await waitFor(()=>{
+    expect(get).toHaveBeenCalledWith(expect.stringContaining('interval=minutely'))
+
+  })
+  // find button that has radix id
+  const [btn] = screen.getAllByRole('button', { name: '60 minutes' }).filter((e) => e.id)
+  clickDropdown(btn)
+  await waitFor(() => screen.getByText(/7 days/))
+  await waitFor(() => screen.getByText(/24 hours/))
+
+  // simulate changing of dropdown
+  userEvent.click(screen.getByText(/24 hours/))
+  await waitFor(() => screen.getByText(/Statistics for past 24 hours/))
+  expect(get).toHaveBeenCalledWith(expect.stringContaining('interval=hourly'))
+
+})
