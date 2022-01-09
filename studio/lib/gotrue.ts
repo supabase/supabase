84d1fab8a9a7c253e65ff80883c8fa409e951048
DE OLIVEIRA Tristan <tristan@koji.fr>
2022-01-09 03:23:02 +0100

    feat: add support for custom url on api pages

--- studio/lib/gotrue.ts
+++ studio/lib/gotrue.ts
@@ -1,6 +1,6 @@
 import { GoTrueClient, User } from '@supabase/gotrue-js'
 
-export const GOTRUE_URL = process.env.NEXT_PUBLIC_GOTRUE_URL || 'http://localhost:8000/auth/v1'
+export const GOTRUE_URL = process.env.NEXT_PUBLIC_GOTRUE_URL || `${process.env.SUPABASE_URL}/auth/v1`
 
 export const auth = new GoTrueClient({
   url: GOTRUE_URL,
