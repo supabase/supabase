f0e038744a7e8e1c804d945f3e1a5d572b5439b4
TzeYiing <ty@tzeyiing.com>
2022-01-12 03:19:06 +0800

    fix ux bug where search/editor values were getting submitted despite being hidden

--- studio/tests/pages/projects/logs.test.js
+++ studio/tests/pages/projects/logs.test.js
@@ -187,6 +187,12 @@ test('where clause will trigger a log refresh', async () => {
     return { data: [] }
   })
   const { container } = render(<LogPage />)
+  // fill search bar with some value, should be ignored when in custom mode
+  userEvent.type(screen.getByPlaceholderText(/Search/), 'search_value')
+  userEvent.click(screen.getByText('Go'))
+  // clear mock calls, for clean assertions
+  get.mockClear()
+
   let editor = container.querySelector('.monaco-editor')
   expect(editor).toBeFalsy()
   // TODO: abstract this out into a toggle selection helper
@@ -204,6 +210,8 @@ test('where clause will trigger a log refresh', async () => {
     () => {
       expect(get).toHaveBeenCalledWith(expect.stringContaining('where'))
       expect(get).toHaveBeenCalledWith(expect.stringContaining('metadata.field'))
+      // should ignore search bar value
+      expect(get).not.toHaveBeenCalledWith(expect.stringContaining('search_value'))
     },
     { timeout: 1000 }
   )
