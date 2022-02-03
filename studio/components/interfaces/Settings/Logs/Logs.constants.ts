cecfac5caf80cb3e6874a7faea7c19e1a93aaeb0
TzeYiing <ty@tzeyiing.com>
2022-02-03 20:55:22 +0800

    add metadata ip template example

--- studio/components/interfaces/Settings/Logs/Logs.constants.ts
+++ studio/components/interfaces/Settings/Logs/Logs.constants.ts
@@ -8,6 +8,18 @@ export const TEMPLATES: LogTemplate[] = [
     searchString:
       "REGEXP_CONTAINS(event_message, 'POST') OR REGEXP_CONTAINS(event_message, 'PATCH')",
   },
+  {
+    label: 'Metadata IP',
+    mode: 'custom',
+    searchString:
+      `SELECT timestamp, h.x_real_ip
+FROM edge_logs
+  LEFT JOIN UNNEST(metadata) as m ON TRUE
+  LEFT JOIN UNNEST(m.request) AS r ON TRUE
+  LEFT JOIN UNNEST(r.headers) AS h ON TRUE
+WHERE h.x_real_ip IS NOT NULL
+`,
+  },
 ]
 
 export const DEFAULT_QUERY = 'SELECT timestamp, event_message FROM postgres_logs;'
