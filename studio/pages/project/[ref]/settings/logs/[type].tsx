8e7e8d4cce193d05804cfa652a91aa5cedb01dcc
TzeYiing <ty@tzeyiing.com>
2022-02-03 20:35:11 +0800

    added editor value cleaning

--- studio/pages/project/[ref]/settings/logs/[type].tsx
+++ studio/pages/project/[ref]/settings/logs/[type].tsx
@@ -180,8 +180,8 @@ export const LogPage: NextPage = () => {
       setEditorValue(template.searchString)
       setParams((prev) => ({
         ...prev,
-        where: isSelectQuery ? '' : template.searchString,
-        sql: isSelectQuery ? template.searchString : '',
+        where: isSelectQuery ? '' : cleanEditorValue(template.searchString),
+        sql: isSelectQuery ? cleanEditorValue(template.searchString) : '',
         search_query: '',
         timestamp_end: '',
       }))
@@ -191,8 +191,8 @@ export const LogPage: NextPage = () => {
   const handleEditorSubmit = () => {
     setParams((prev) => ({
       ...prev,
-      where: isSelectQuery ? '' : editorValue,
-      sql: isSelectQuery ? editorValue : '',
+      where: isSelectQuery ? '' : cleanEditorValue(editorValue),
+      sql: isSelectQuery ? cleanEditorValue(editorValue) : '',
       search_query: '',
     }))
     if (!logsQueryParamsSyncing) return
@@ -200,7 +200,7 @@ export const LogPage: NextPage = () => {
       pathname: router.pathname,
       query: {
         ...router.query,
-        q: editorValue,
+        q: cleanEditorValue(editorValue),
         s: undefined,
         te: undefined,
       },
@@ -227,7 +227,10 @@ export const LogPage: NextPage = () => {
     })
     setEditorValue('')
   }
-
+  const cleanEditorValue = (value: string) => {
+    if (typeof value !== "string") return value
+    return value.replace(/\n/g, ' ')
+  }
   return (
     <SettingsLayout title={title}>
       <div className="h-full flex flex-col flex-grow">
