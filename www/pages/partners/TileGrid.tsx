73d66abbc1a13e72cf77082deda73ab67bd92824
thorwebdev <thor@supabase.io>
2022-02-14 16:54:46 +0800

    fix: incremental static regeneration

--- www/pages/partners/TileGrid.tsx
+++ www/pages/partners/TileGrid.tsx
@@ -17,7 +17,7 @@ export default function TileGrid({
       role="list"
       className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
     >
-      {partners.map((partner) => (
+      {partners?.map((partner) => (
         <li key={partner.source} className="relative">
           <div className="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
             <img
