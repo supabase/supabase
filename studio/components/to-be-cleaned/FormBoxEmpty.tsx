2748553003665ad7a25ebb3735a8b55d935e9465
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-02-16 17:54:25 -0500

    chore: Triggers, Functions and Hooks SidePanels updated

--- studio/components/to-be-cleaned/FormBoxEmpty.tsx
+++ studio/components/to-be-cleaned/FormBoxEmpty.tsx
@@ -4,7 +4,7 @@ import { Typography } from '@supabase/ui'
 const EmptyPanel: FC<any> = ({ icon, text }) => {
   return (
     <div className="flex items-center justify-center flex-row py-4 space-x-2">
-      <div className="relative bg-bg-secondary-light w-12 h-12 rounded-full flex items-center justify-center text-gray-300">
+      <div className="relative bg-scale-300 text-scale-900 w-6 h-6 rounded-full flex items-center justify-center">
         {icon}
       </div>
       <Typography.Text type="secondary">{text}</Typography.Text>
