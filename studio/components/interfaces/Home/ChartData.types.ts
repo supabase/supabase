20a21691accb612a7cc78edf18a70b48cf9e0dde
TzeYiing <ty@tzeyiing.com>
2022-02-17 00:55:03 +0800

    added top api paths implementation

--- studio/components/interfaces/Home/ChartData.types.ts
+++ studio/components/interfaces/Home/ChartData.types.ts
@@ -3,6 +3,15 @@ export interface StatusCodesDatum {
   status_code: number
   count: number
 }
+export interface PathsDatum {
+    timestamp: number
+    path: string
+    query_params: string
+    count: number
+    method: string
+    avg_origin_time: number
+  }
+  
 
 export interface EndpointResponse<T> {
   result: Array<T>
