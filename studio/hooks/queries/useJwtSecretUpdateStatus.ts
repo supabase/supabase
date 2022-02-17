4501e6ee9c08b12d381730d4413cc11d87697fcf
Beng Eu <beng@bengeu.com>
2022-02-17 17:06:54 +0800

    refactor: use JWT secret update error, progress and status enums

--- studio/hooks/queries/useJwtSecretUpdateStatus.ts
+++ studio/hooks/queries/useJwtSecretUpdateStatus.ts
@@ -1,24 +1,23 @@
 import useSWR from 'swr'
 import { get } from 'lib/common/fetch'
-import { ProjectEvents } from '@supabase/shared-types/out/events'
+import { JwtSecretUpdateStatus } from '@supabase/shared-types/out/events'
 import { API_URL } from 'lib/constants'
 
 export function useJwtSecretUpdateStatus(projectRef: string | string[] | undefined) {
   const url = `${API_URL}/props/project/${projectRef}/jwt-secret-update-status`
   const { data, error, mutate } = useSWR<any>(url, get, {
     refreshInterval: (data) =>
-      data?.jwtSecretUpdateStatus?.event_type === ProjectEvents.ProjectJwtSecretUpdateProgress
-        ? 1_000
-        : 0,
+      data?.jwtSecretUpdateStatus?.meta?.status === JwtSecretUpdateStatus.Updating ? 1_000 : 0,
   })
   const anyError = data?.error || error
-
+  const meta = data?.jwtSecretUpdateStatus?.meta
   return {
-    changeTrackingId: data?.jwtSecretUpdateStatus?.meta?.change_tracking_id,
+    changeTrackingId: meta?.change_tracking_id,
     isError: !!anyError,
     isLoading: !anyError && !data,
+    jwtSecretUpdateError: meta?.error,
+    jwtSecretUpdateProgress: meta?.progress,
+    jwtSecretUpdateStatus: meta?.status,
     mutateJwtSecretUpdateStatus: mutate,
-    jwtSecretUpdateMessage: data?.jwtSecretUpdateStatus?.meta?.message,
-    jwtSecretUpdateStatus: data?.jwtSecretUpdateStatus?.event_type,
   }
 }
