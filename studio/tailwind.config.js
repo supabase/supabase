0fb7644b2850737d4aff357109bce25dbd0128b8
Benjamin Tan <benjamin@dev.ofcr.se>
2022-02-21 15:43:37 +0800

    Bump `tailwindcss` to v3

--- studio/tailwind.config.js
+++ studio/tailwind.config.js
@@ -49,9 +49,8 @@ const coolGray = {
 }
 
 module.exports = {
-  mode: 'jit',
   darkMode: 'class', // or 'media' or 'class'
-  purge: [
+  content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './internals/**/*.{js,ts,jsx,tsx}',
