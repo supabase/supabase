592d80e75a3500569561c76111d8a9d184d928bd
Joshen Lim <joshenlimek@gmail.com>
2022-02-21 15:28:51 +0800

    Add numeric data type and refactor some repeated logic

--- studio/lib/constants/index.ts
+++ studio/lib/constants/index.ts
@@ -11,17 +11,6 @@ export const PG_META_URL = IS_PLATFORM
 
 export const DATE_FORMAT = 'YYYY-MM-DDTHH:mm:ssZ'
 
-// Data types primarily for mapping icon in editor
-
-export const NUMERICAL_TYPES = ['int2', 'int4', 'int8', 'float4', 'float8']
-export const JSON_TYPES = ['json', 'jsonb']
-export const TEXT_TYPES = ['text', 'varchar']
-export const TIMESTAMP_TYPES = ['date', 'time', 'timestamp', 'timetz', 'timestamptz']
-export const OTHER_DATA_TYPES = ['uuid', 'bool']
-export const POSTGRES_DATA_TYPES = sortBy(
-  concat(NUMERICAL_TYPES, JSON_TYPES, TEXT_TYPES, TIMESTAMP_TYPES, OTHER_DATA_TYPES)
-)
-
 // Keyboard Shortcuts Related
 export const SHORTCUT_KEYS = {
   VIEW_ALL_SHORTCUTS: 'VIEW_ALL_SHORTCUTS',
