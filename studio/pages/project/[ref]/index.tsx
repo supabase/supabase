296cb9c50e6eccf10cbb008510a25add900d42a7
TzeYiing <ty@tzeyiing.com>
2022-02-23 02:28:35 +0800

    remove duplicate PlatformUsage component that was for testing

--- studio/pages/project/[ref]/index.tsx
+++ studio/pages/project/[ref]/index.tsx
@@ -47,7 +47,6 @@ const Home: NextPage = () => {
             {projectName}
           </Typography.Title>
         </div>
-        <ProjectUsage project={project} />
         {IS_PLATFORM && project && (
           <>
             {isUndefined(usage) ? (
