e3d1d628c0f1a6b3e18c092d654c7bffb6f6e423
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2022-02-24 00:25:08 -0500

    init tailwind

---
+++ about/tailwind.config.js
@@ -0,0 +1,53 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'gray-light': '#7B7F86',
+        'gray-dark': '#7B7F86',
+        'accent-1': '#FAFAFA',
+        'accent-2': '#EAEAEA',
+        'accent-7': '#333',
+        success: '#0070f3',
+        cyan: '#79FFE1',
+        brand: {
+          100: '#82dab0',
+          200: '#69d3a0',
+          300: '#50cb90',
+          400: '#C5F1DD',
+          500: '#9FE7C7',
+          600: '#65D9A5',
+          700: '#3ECF8E',
+          800: '#38BC81',
+          900: '#10633E',
+        },
+        dark: {
+          100: '#eeeeee',
+          200: '#e0e0e0',
+          300: '#bbbbbb',
+          400: '#666666',
+          500: '#444444',
+          600: '#2a2a2a',
+          700: '#1f1f1f',
+          800: '#181818',
+          900: '#0f0f0f',
+        },
+        gray: {
+          100: '#eeeeee',
+          200: '#e0e0e0',
+          300: '#bbbbbb',
+          400: '#666666',
+          500: '#444444',
+          600: '#2a2a2a',
+          700: '#1f1f1f',
+          800: '#181818',
+          900: '#0f0f0f',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
