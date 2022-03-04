06f028490680a9368c425a44b5e2df6b564120fe
TzeYiing <ty@tzeyiing.com>
2022-03-04 17:50:29 +0800

    added storybook dependencies and minimal setup

---
+++ studio/.storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "stories": [
+    "./*.stories.mdx",
+    "../components/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react"
+}
