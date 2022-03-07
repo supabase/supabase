b139bc90bc63acc9e723acd02373fbbf1ae45cf7
“louis-27” <louis27isnow@gmail.com>
2022-03-07 22:24:33 +0700

    chore: add cta for no created tables in /auth/policies

---
+++ studio/components/to-be-cleaned/NoTableState.tsx
@@ -0,0 +1,26 @@
+import { FC } from 'react'
+import { useRouter } from 'next/router'
+import ProductEmptyState from './ProductEmptyState'
+
+interface Props {
+  message: string
+}
+
+const NoTableState: FC<Props> = ({ message }) => {
+  const router = useRouter()
+  const { ref } = router.query
+
+  return (
+    <ProductEmptyState
+      title="No public tables found"
+      ctaButtonLabel="Create a new table"
+      onClickCta={() => {
+        router.push(`/project/${ref}/database/tables`)
+      }}
+    >
+      <p className="text-sm text-scale-1100">{message}</p>
+    </ProductEmptyState>
+  )
+}
+
+export default NoTableState
