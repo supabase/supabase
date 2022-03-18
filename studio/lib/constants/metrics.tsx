0a7157c858d36a0fd5aa7977286bec0a6386809e
Div Arora <root@darora.com>
2022-03-18 17:17:37 +0100

    feat: chart for disk IO budget

--- studio/lib/constants/metrics.tsx
+++ studio/lib/constants/metrics.tsx
@@ -46,6 +46,12 @@ export const METRICS = [
     category: METRIC_CATEGORIES.INSTANCE,
   },
   {
+    key: 'disk_io_budget',
+    label: 'Disk IO Budget %',
+    provider: 'infra-monitoring',
+    category: METRIC_CATEGORIES.INSTANCE,
+  },
+  {
     key: 'ram_usage',
     label: 'Memory % usage',
     provider: 'infra-monitoring',
