f5e29d1b7e41e264a754e4d27511f4ada7b3f4fd
TzeYiing <ty@tzeyiing.com>
2022-03-18 15:18:32 +0100

    Separated logs querying logic into two separate hooks

--- studio/components/interfaces/Settings/Logs/Logs.constants.ts
+++ studio/components/interfaces/Settings/Logs/Logs.constants.ts
@@ -172,15 +172,35 @@ LIMIT 100
 ]
 
 export const LOG_TYPE_LABEL_MAPPING: { [k: string]: string } = {
+  explorer: "Explorer",
   api: 'API',
   database: 'Database',
 }
 
-export const genDefaultQuery = (table: string): string => `SELECT
-  id, timestamp, event_message, metadata
+export const genDefaultQuery = (table: string, where: string = ''): string => `SELECT 
+  id, timestamp, event_message, metadata 
 FROM
-  ${table}
+  ${table}${where ? ' WHERE\n  ' + where : ''} 
 LIMIT 100
 `
+export const cleanQuery = (str: string) => str.replaceAll(/\n/g, ' ')
+// .replace(/\n.*\-\-.*(\n)?$?/, "")
 
+
+export enum LogsTableName {
+  EDGE = 'edge_logs',
+  POSTGRES = 'postgres_logs',
+}
 export const genCountQuery = (table: string): string => `SELECT count(*) as count FROM ${table}`
+
+export const genQueryParams = (params: { [k: string ]: string }) => {
+  // remove keys which are empty strings, null, or undefined
+  for (const k in params) {
+    const v = params[k]
+    if (v === null || v === '' || v === undefined) {
+      delete params[k]
+    }
+  }
+  const qs = new URLSearchParams(params).toString()
+  return qs
+}
