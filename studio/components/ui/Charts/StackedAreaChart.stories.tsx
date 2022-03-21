c3cb57fc55b04c97a2920db919629e04fcde2efa
TzeYiing <ty@tzeyiing.com>
2022-03-21 13:54:59 +0100

    fixed tooltip label color

--- studio/components/ui/Charts/StackedAreaChart.stories.tsx
+++ studio/components/ui/Charts/StackedAreaChart.stories.tsx
@@ -6,16 +6,26 @@ import StackedAreaChart from './StackedAreaChart'
 export default {
   title: 'Charts/StackedAreaChart',
   component: StackedAreaChart,
-  parameters: {
-    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'fullscreen',
-  },
 } as ComponentMeta<typeof StackedAreaChart>
 
 const Template: ComponentStory<typeof StackedAreaChart> = (args) => <StackedAreaChart {...args} />
 
 export const Default = Template.bind({})
 Default.args = {
+  stackKey: 'status_code',
+  xAxisKey: 'timestamp',
+  yAxisKey: 'count',
+  isLoading: false,
+  xAxisFormatAsDate: true,
+  size: 'large',
+  dateFormat: 'MMM D, ha',
+
   data: [
-  ]
+    { count: 18, status_code: 200, timestamp: 1647864000000000 },
+    { count: 1, status_code: 303, timestamp: 1647864000000000 },
+    { count: 4, status_code: 201, timestamp: 1647864000000000 },
+    { count: 18, status_code: 200, timestamp: 1647860000000000 },
+    { count: 1, status_code: 303, timestamp: 1647860000000000 },
+    { count: 4, status_code: 201, timestamp: 1647864000000000 },
+  ],
 }
