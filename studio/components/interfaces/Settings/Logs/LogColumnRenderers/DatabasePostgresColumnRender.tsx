0e126692dd6115b3455e81221ca239943e589df9
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-03-22 16:57:17 +0100

    chore: filtering selected is now added into the SQL query

--- studio/components/interfaces/Settings/Logs/LogColumnRenderers/DatabasePostgresColumnRender.tsx
+++ studio/components/interfaces/Settings/Logs/LogColumnRenderers/DatabasePostgresColumnRender.tsx
@@ -6,11 +6,11 @@ export default [
     formatter: (data: any) => (
       <div className="flex w-full items-center gap-2">
         <div className="flex items-center gap-2">
-          <span className="w-20 flex items-center gap-1">
+          <span className="w-24 flex items-center gap-1">
             <span className="text-xs">{dayjs(data?.row?.timestamp / 1000).format('DD MMM')}</span>
             <span className="text-xs">{dayjs(data?.row?.timestamp / 1000).format('HH:mm:ss')}</span>
           </span>
-          <div className="w-16 flex justify-center">
+          <div className="w-16 flex">
             <SeverityFormatter value={data?.row?.metadata[0].parsed[0].error_severity} />
           </div>
         </div>
