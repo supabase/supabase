8643f8ad0a7616b9419395f5a9842d1a700f0068
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-03-23 20:00:28 +0100

    chore: some little border color changes

--- www/components/Nav/index.tsx
+++ www/components/Nav/index.tsx
@@ -184,7 +184,7 @@ const Nav = () => {
       <Announcement />
       <div className="sticky top-0 z-50">
         <div className="absolute w-full h-full bg-scale-200 top-0 opacity-80"></div>
-        <nav className="backdrop-blur-sm border-b border-scale-900">
+        <nav className="backdrop-blur-sm border-b border-scale-400">
           {/* <div className="lg:container mx-auto relative flex justify-between h-16 lg:px-10 xl:px-0"> */}
           <div className="lg:container mx-auto relative flex justify-between h-16 lg:px-16 xl:px-20">
             <HamburgerButton toggleFlyOut={() => setOpen(true)} />
