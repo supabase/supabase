9b2c98bc219a7306dee2fc9fe2e824896847c2ad
Ian McPhail <ian@clerk.dev>
2022-03-23 14:18:34 -0400

    docs: Add Clerk integration guide

--- web/sidebars.js
+++ web/sidebars.js
@@ -188,6 +188,7 @@ module.exports = {
       items: [
         'guides/integrations/appsmith',
         'guides/integrations/auth0',
+        'guides/integrations/clerk',
         'guides/integrations/draftbit',
         'guides/integrations/pgmustard',
         'guides/integrations/plasmic',
