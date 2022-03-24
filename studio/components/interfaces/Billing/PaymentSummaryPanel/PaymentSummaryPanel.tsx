07a321982ca188e2c57ce586fee6144c496b934e
Joshen Lim <joshenlimek@gmail.com>
2022-03-24 19:13:32 +0800

    Fix prop errors

--- studio/components/interfaces/Billing/PaymentSummaryPanel/PaymentSummaryPanel.tsx
+++ studio/components/interfaces/Billing/PaymentSummaryPanel/PaymentSummaryPanel.tsx
@@ -54,6 +54,7 @@ const PaymentSummaryPanel: FC<Props> = ({
     (currentPlan.prod_id !== STRIPE_PRODUCT_IDS.PAYG && currentPlan.prod_id !== selectedPlan?.id) ||
     (currentPlan.prod_id !== STRIPE_PRODUCT_IDS.PAYG && !isSpendCapEnabled) ||
     (currentPlan.prod_id === STRIPE_PRODUCT_IDS.PAYG && isSpendCapEnabled)
+  console.log('PaymentSummaryPanel', currentComputeSize, selectedComputeSize)
   const isChangingComputeSize = currentComputeSize.id !== selectedComputeSize.id
   const hasChangesToPlan = subscriptionPreview?.has_changes ?? false
 
