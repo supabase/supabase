71e4412bc8b8ee41f6a8ffa941785ac49ea905ef
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-03-27 23:18:28 +0200

    chore: condensed layout fixes

--- studio/pages/project/[ref]/functions/interfaces/CommandRender.tsx
+++ studio/pages/project/[ref]/functions/interfaces/CommandRender.tsx
@@ -11,32 +11,38 @@ const CommandRender = ({ commands }: any) => {
           <div>
             <span className="font-mono text-xs text-scale-900">{`> ${item.comment}`}</span>
             <div className="flex items-center gap-2">
-              <div className="flex items-center gap-2 text-scale-1200 font-mono text-sm font-normal">
+              <div className="flex gap-2 text-scale-1200 font-mono text-sm font-normal">
                 <span className="text-scale-900">$</span>
-                {item.jsx ? item.jsx() : null}
+                <span>
+                  <span className="">{item.jsx ? item.jsx() : null} </span>
+                  <button
+                    type="button"
+                    className="text-scale-900 hover:text-scale-1200"
+                    onClick={() => {
+                      function onCopy(value: any) {
+                        setIsCopied(true)
+                        navigator.clipboard.writeText(value).then()
+                        setTimeout(function () {
+                          setIsCopied(false)
+                        }, 3000)
+                      }
+                      onCopy(item.command)
+                    }}
+                  >
+                    {isCopied ? (
+                      <div className="text-brand-900">
+                        <IconCheck size={14} strokeWidth={3} />
+                      </div>
+                    ) : (
+                      <div className="relative">
+                        <div className="block">
+                          <IconClipboard size={14} />
+                        </div>
+                      </div>
+                    )}
+                  </button>
+                </span>
               </div>
-              <button
-                type="button"
-                className="text-scale-900 hover:text-scale-1200"
-                onClick={() => {
-                  function onCopy(value: any) {
-                    setIsCopied(true)
-                    navigator.clipboard.writeText(value).then()
-                    setTimeout(function () {
-                      setIsCopied(false)
-                    }, 3000)
-                  }
-                  onCopy(item.command)
-                }}
-              >
-                {isCopied ? (
-                  <div className="text-brand-900">
-                    <IconCheck size={14} strokeWidth={3} />
-                  </div>
-                ) : (
-                  <IconClipboard size={14} />
-                )}
-              </button>
             </div>
             <span className="text-xs text-scale-900">{item.description}</span>
           </div>
