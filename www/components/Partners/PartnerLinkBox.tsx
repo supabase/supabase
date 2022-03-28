755452650fd09a2f9720a0a4a45e5568a05b04bf
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-03-28 13:11:41 +0200

    chore: update integration and expert layout

---
+++ www/components/Partners/PartnerLinkBox.tsx
@@ -0,0 +1,54 @@
+import Link from 'next/link'
+import React from 'react'
+
+const PartnerLinkBox = ({
+  href,
+  title,
+  description,
+  icon,
+  color = 'amber',
+}: {
+  href: string
+  title: string
+  description: string
+  icon?: React.ReactNode
+  color?: 'amber' | 'brand' | 'blue'
+}) => {
+  const colors = {
+    amber: 'bg-amber-400 dark:bg-scale-400 text-amber-900',
+    blue: 'bg-blue-400 dark:bg-scale-400 text-blue-900',
+    brand: 'bg-brand-400 dark:bg-scale-400 text-brand-900',
+  }
+  return (
+    <Link href={href}>
+      <div
+        className="
+        cursor-pointer
+        hover:bg-scale-300
+        rounded
+        group px-5 py-4 bg-scale-200 
+        border border-scale-500 dark:border-scale-400 
+        "
+      >
+        <div className="flex flex-col gap-3">
+          <div
+            className={`${colors[color]}
+            rounded-md block h-8 w-8
+            flex items-center justify-center
+            transition-all
+            group-hover:scale-110
+            `}
+          >
+            {icon}
+          </div>
+          <div>
+            <h5 className="text-base mb-2 text-scale-1200">{title}</h5>
+            <p className="p text-sm">{description}</p>
+          </div>
+        </div>
+      </div>
+    </Link>
+  )
+}
+
+export default PartnerLinkBox
