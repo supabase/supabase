df1b340460b650ccdabe691014d87ac529ff1439
TzeYiing <ty@tzeyiing.com>
2022-03-28 17:40:53 +0200

    fixed datepicker and dropdowndown behaviour

--- studio/components/interfaces/Settings/Logs/Logs.DatePickers.tsx
+++ studio/components/interfaces/Settings/Logs/Logs.DatePickers.tsx
@@ -1,17 +1,35 @@
 import { Button, Dropdown, IconClock } from '@supabase/ui'
 import { DatePicker } from 'components/ui/DatePicker'
+import { useState, useEffect } from 'react'
 
-const DatePickers = ({
-  to,
-  from,
-  //   defaultValues,
-  onChange,
-}: {
-  to?: number
-  from?: number
-  defaultValues?: any
-  onChange: ({}) => void
-}) => {
+type ToFrom = { to: string; from: string }
+interface Props {
+  to?: string
+  from?: string
+  onChange: ({ to, from }: ToFrom) => void
+}
+
+const HELPERS = [
+  { text: 'Last hour', value: '1_hour' },
+  { text: 'Last 3 hours', value: '3_hour' },
+  { text: 'Last day', value: '1_day' },
+]
+const DEFAULT_HELPER_VALUE = HELPERS[0].value
+
+const DatePickers: React.FC<Props> = ({ to, from, onChange }) => {
+  const [helperValue, setHelperValue] = useState<string>('')
+
+  const handleHelperChange = (newValue: string) => {
+    setHelperValue(newValue)
+    const toDate = new Date()
+    const fromDate = {
+      '1_hour': new Date(toDate.getTime() - 60 * 60 * 1000),
+      '3_hour': new Date(toDate.getTime() - 3 * 60 * 60 * 1000),
+      '1_day': new Date(toDate.getTime() - 24 * 60 * 60 * 1000),
+    }[newValue]
+    if (onChange) onChange({ to: toDate.toISOString(), from: fromDate?.toISOString() as string })
+  }
+  const selectedHelper = HELPERS.find((h) => h.value === (helperValue || DEFAULT_HELPER_VALUE))
   return (
     <div className="flex items-center">
       <Dropdown
@@ -20,19 +38,38 @@ const DatePickers = ({
         align="start"
         overlay={
           <>
-            <Dropdown.RadioGroup onChange={(e) => console.log(e)} value={'1_hour'}>
-              <Dropdown.Radio value="1_hour">1 hour</Dropdown.Radio>
-              <Dropdown.Radio value="3_hour">Last 3 hour</Dropdown.Radio>
-              <Dropdown.Radio value="1_day">Last day</Dropdown.Radio>
+            <Dropdown.RadioGroup
+              onChange={handleHelperChange}
+              value={helperValue || DEFAULT_HELPER_VALUE}
+            >
+              {HELPERS.map((helper) => (
+                <Dropdown.Radio key={helper.value} value={helper.value}>
+                  {helper.text}
+                </Dropdown.Radio>
+              ))}
             </Dropdown.RadioGroup>
           </>
         }
       >
-        <Button as="span" type="default" icon={<IconClock size={12} />}>
-          Last hour
+        <Button
+          as="span"
+          type={helperValue ? 'alternative' : 'outline'}
+          icon={<IconClock size={12} />}
+          className="rounded-r-none"
+        >
+          {selectedHelper?.text}
         </Button>
       </Dropdown>
-      <DatePicker onChange={onChange} to={to} from={from} />
+      <DatePicker
+        triggerButtonClassName="rounded-l-none"
+        triggerButtonType={helperValue ? 'outline' : 'alternative'}
+        onChange={(value: ToFrom) => {
+          setHelperValue('')
+          if (onChange) onChange(value)
+        }}
+        to={!helperValue ? to : undefined}
+        from={!helperValue ? from : undefined}
+      />
     </div>
   )
 }
