f06f79e499ba52c74887f52ccb717b0b1b8fc8e0
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-03-28 16:27:20 +0200

    chore: responsive fixes

--- www/pages/partners/integrations/index.tsx
+++ www/pages/partners/integrations/index.tsx
@@ -51,8 +51,8 @@ function IntegrationPartnersPage(props: Props) {
   )
   const router = useRouter()
 
-  const meta_title = 'Works With Supabase'
-  const meta_description = `Find Integration Partners and Expert Services that work with Supabase.`
+  const meta_title = 'Find an Integration'
+  const meta_description = `Use your favorite tools with Supabase.`
 
   const [search, setSearch] = useState('')
   const [debouncedSearchTerm] = useDebounce(search, 300)
@@ -117,14 +117,12 @@ function IntegrationPartnersPage(props: Props) {
       <DefaultLayout>
         <SectionContainer className="space-y-16">
           <div>
-            <h1 className="h1">Find an Integration</h1>
-            <h2 className="text-xl text-scale-900">
-              Extend and automate your workflow by using integrations for your favorite tools.
-            </h2>
+            <h1 className="h1">{meta_title}</h1>
+            <h2 className="text-xl text-scale-900">{meta_description}</h2>
           </div>
           {/* Title */}
-          <div className="grid grid-cols-12 lg:gap-16 xl:gap-32">
-            <div className="col-span-3">
+          <div className="grid lg:grid-cols-12 space-y-12 lg:space-y-0 md:gap-8 lg:gap-16 xl:gap-16">
+            <div className="lg:col-span-4 xl:col-span-3">
               {/* Horizontal link menu */}
               <div className="space-y-6">
                 {/* Search Bar */}
@@ -145,7 +143,7 @@ function IntegrationPartnersPage(props: Props) {
                     )
                   }
                 />
-                <div>
+                <div className="hidden lg:block">
                   <div className="text-sm text-scale-900 mb-2">Categories</div>
                   <div className="space-y-1">
                     {allCategories.map((category) => (
@@ -161,51 +159,53 @@ function IntegrationPartnersPage(props: Props) {
                 </div>
                 <div className="space-y-4">
                   <div className="text-sm text-scale-900 mb-2">Explore more</div>
-                  <PartnerLinkBox
-                    title="Experts"
-                    color="blue"
-                    description="Explore our certified Supabase agency experts that build with Supabase"
-                    href={`/partners/experts`}
-                    icon={
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-6 w-6"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        stroke-width="1"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                        />
-                      </svg>
-                    }
-                  />
+                  <div className="grid grid-cols-2 gap-8 lg:grid-cols-1">
+                    <PartnerLinkBox
+                      title="Experts"
+                      color="blue"
+                      description="Explore our certified Supabase agency experts that build with Supabase"
+                      href={`/partners/experts`}
+                      icon={
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-6 w-6"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          stroke-width="1"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                          />
+                        </svg>
+                      }
+                    />
 
-                  <PartnerLinkBox
-                    href={`/partners/integrations#become-a-partner`}
-                    title="Become a partner"
-                    color="brand"
-                    description="Fill out a quick 30 second form to apply to become a partner"
-                    icon={
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        stroke-width="1"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-                        />
-                      </svg>
-                    }
-                  />
+                    <PartnerLinkBox
+                      href={`/partners/integrations#become-a-partner`}
+                      title="Become a partner"
+                      color="brand"
+                      description="Fill out a quick 30 second form to apply to become a partner"
+                      icon={
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-5 w-5"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          stroke-width="1"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+                          />
+                        </svg>
+                      }
+                    />
+                  </div>
                 </div>
                 {/* <div className="flex flex-col space-y-1 sm:flex-row sm:space-y-0 sm:space-x-6">
                   <Link href={`/partners/integrations`}>
@@ -222,7 +222,7 @@ function IntegrationPartnersPage(props: Props) {
                 </div> */}
               </div>
             </div>
-            <div className="col-span-9">
+            <div className="lg:col-span-8 xl:col-span-9">
               {/* Partner Tiles */}
               <div className="grid space-y-10">
                 {partners.length ? (
