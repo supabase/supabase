63b42dabe0a2346e5f83d81addd3b4474ba18448
TzeYiing <ty@tzeyiing.com>
2022-03-29 19:42:09 +0200

    fix typing and console logging errors

--- studio/components/interfaces/Settings/Logs/LogSelection.tsx
+++ studio/components/interfaces/Settings/Logs/LogSelection.tsx
@@ -13,7 +13,7 @@ import FunctionInvocationSelectionRender, {
 import FunctionLogsSelectionRender from './LogSelectionRenderers/FunctionLogsSelectionRender'
 
 interface Props {
-  log: LogData | undefined
+  log: LogData | null
   onClose: () => void
   queryType?: QueryType
 }
@@ -93,11 +93,11 @@ const LogSelection: FC<Props> = ({ log, onClose, queryType }) => {
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
-                stroke-width="1"
+                strokeWidth="1"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"
                 />
               </svg>
