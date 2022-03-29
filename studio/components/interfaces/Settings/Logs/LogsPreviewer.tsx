53c530b3c6d401ae06ca97b3f16d2a02a3f3a10d
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-03-29 19:57:56 +0200

    remove rendundant loading shimmer

--- studio/components/interfaces/Settings/Logs/LogsPreviewer.tsx
+++ studio/components/interfaces/Settings/Logs/LogsPreviewer.tsx
@@ -170,16 +170,17 @@ export const LogsPreviewer: React.FC<Props> = ({ projectRef, queryType, override
         }
       >
         <div className={condensedLayout ? 'px-4' : ''}>
-          {showChart && <LogEventChart
-            data={!isLoading ? logData : undefined}
-            onBarClick={(timestampMicro) => {
-              handleSearch({ query: filters.search_query, toMicro: timestampMicro })
-            }}
-          />}
+          {showChart && (
+            <LogEventChart
+              data={!isLoading ? logData : undefined}
+              onBarClick={(timestampMicro) => {
+                handleSearch({ query: filters.search_query, toMicro: timestampMicro })
+              }}
+            />
+          )}
         </div>
       </div>
       <div className="flex flex-col flex-grow relative pt-4">
-        {isLoading && <div className="logs-shimmering-loader w-full h-0.5"></div>}
         <ShimmerLine active={isLoading} />
         <LoadingOpacity active={isLoading}>
           <LogTable data={logData} queryType={queryType} />
