e756bbceab37076a144a14ed2e50badbca223d67
TzeYiing <ty@tzeyiing.com>
2022-03-30 00:59:28 +0200

    fix: log line centering, added more filters for postgres errors

--- studio/components/interfaces/Settings/Logs/LogColumnRenderers/DatabasePostgresColumnRender.tsx
+++ studio/components/interfaces/Settings/Logs/LogColumnRenderers/DatabasePostgresColumnRender.tsx
@@ -4,13 +4,13 @@ import { SeverityFormatter } from '../LogsFormatters'
 export default [
   {
     formatter: (data: any) => (
-      <div className="flex w-full items-center gap-2">
-        <div className="flex items-center gap-2">
+      <div className="flex w-full items-center gap-2 h-full">
+        <div className="flex items-center gap-2 h-full">
           <span className="w-24 flex items-center gap-1">
             <span className="text-xs">{dayjs(data?.row?.timestamp / 1000).format('DD MMM')}</span>
             <span className="text-xs">{dayjs(data?.row?.timestamp / 1000).format('HH:mm:ss')}</span>
           </span>
-          <div className="w-16 flex">
+          <div className="w-16 flex items-center">
             <SeverityFormatter value={data?.row?.metadata[0].parsed[0].error_severity} />
           </div>
         </div>
