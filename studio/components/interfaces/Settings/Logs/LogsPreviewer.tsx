ea4e4157fd8e79e320bdeef5963103042286fdb1
TzeYiing <ty@tzeyiing.com>
2022-03-29 19:57:15 +0200

    use Histogram button for toggling event chart

--- studio/components/interfaces/Settings/Logs/LogsPreviewer.tsx
+++ studio/components/interfaces/Settings/Logs/LogsPreviewer.tsx
@@ -134,8 +134,6 @@ export const LogsPreviewer: React.FC<Props> = ({ projectRef, queryType, override
   return (
     <div className="h-full flex flex-col flex-grow">
       <PreviewFilterPanel
-        isShowingEventChart={showChart}
-        onToggleEventChart={() => setShowChart(!showChart)}
         isLoading={isLoading}
         newCount={newCount}
         templates={TEMPLATES.filter(
@@ -183,7 +181,12 @@ export const LogsPreviewer: React.FC<Props> = ({ projectRef, queryType, override
       <div className="flex flex-col flex-grow relative pt-4">
         <ShimmerLine active={isLoading} />
         <LoadingOpacity active={isLoading}>
-          <LogTable data={logData} queryType={queryType} />
+          <LogTable
+            data={logData}
+            queryType={queryType}
+            isHistogramShowing={showChart}
+            onHistogramToggle={() => setShowChart(!showChart)}
+          />
         </LoadingOpacity>
         <div className="p-2">
           <Button onClick={() => loadOlder()} icon={<IconRewind />} type="default">
