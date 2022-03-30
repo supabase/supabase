ff017584ee1d6669aecc414323d9769d6c8a1e48
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-03-30 15:50:44 +0200

    fix: tests

--- studio/components/interfaces/Settings/Logs/LogTable.tsx
+++ studio/components/interfaces/Settings/Logs/LogTable.tsx
@@ -295,11 +295,11 @@ const LogTable = ({
                               fill="none"
                               viewBox="0 0 24 24"
                               stroke="currentColor"
-                              stroke-width="2"
+                              strokeWidth="2"
                             >
                               <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
                                 d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                               />
                             </svg>
