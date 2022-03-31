7e1c0e5d823a440e8f73c972619ed16efbf0ee43
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-03-31 16:26:10 +0200

    missing file

---
+++ www/data/products/functions/usecase-examples.js
@@ -0,0 +1,92 @@
+/*
+ * description
+ */
+export default [
+  {
+    lang: 'js',
+    title: 'Sending an email',
+    size: 'large',
+    code: `import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+import { SmtpClient } from "https://deno.land/x/smtp@v0.7.0/mod.ts";
+const client = new SmtpClient();
+const { hostname, port, username, password } = Deno.env.toObject();
+
+await client.connect({
+  hostname,
+  port: Number(port),
+  username,
+  password,
+});
+
+serve(async (_req) => {
+  await client.send({
+    from: "jonny@supabase.io",
+    to: "div@supabase.io",
+    subject: "Notifications Ui has launched",
+    content: "And this is a notification about that",
+  });
+
+  return new Response("Email sent", { status: 200 });
+});`,
+  },
+  {
+    lang: 'js',
+    title: 'Connect to database directly',
+    size: 'large',
+    code: `import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+import * as postgres from "https://deno.land/x/postgres@v0.14.2/mod.ts";
+
+const databaseUrl = Deno.env.get("DATABASE_URL") ?? "";
+const pool = new postgres.Pool(databaseUrl, 3, true);
+const connection = await pool.connect();
+
+serve(async (req: Request) => {
+  // sql inject your database
+  const { query } = await req.json();
+  const response = await connection.queryObject(query);
+  console.log(response);
+  return new Response("Query executed", { status: 200 });
+});`,
+  },
+  {
+    lang: 'js',
+    title: 'Read from storage',
+    size: 'large',
+    code: `import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@1.33.1";
+
+serve(async (req) => {
+  const SUPABASE_URL = Deno.env.get("SUPABASE_URL") ?? "";
+  const SERVICE_KEY = Deno.env.get("SERVICE_KEY") ?? "";
+  const supabase = createClient(SUPABASE_URL, SERVICE_KEY);
+  if (req.headers.get("Authorization") === "super-secret-key") {
+    const { data } = await supabase.storage
+      .from("newbucket")
+      .download("supameme.png");
+    return new Response(data, { headers: { "content-type": "image/png" } });
+  } else {
+    return new Response("Forbidden", { status: 403 });
+  }
+});`,
+  },
+  {
+    lang: 'js',
+    title: 'Read from database',
+    size: 'large',
+    code: `import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@1.33.1";
+
+serve(async () => {
+  const SUPABASE_URL = Deno.env.get("SUPABASE_URL") ?? "";
+  const SERVICE_KEY = Deno.env.get("SERVICE_KEY") ?? "";
+  const supabase = createClient(SUPABASE_URL, SERVICE_KEY);
+  const { data } = await supabase.from("todos").select();
+  return new Response(JSON.stringify(data), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+});`,
+  },
+]
