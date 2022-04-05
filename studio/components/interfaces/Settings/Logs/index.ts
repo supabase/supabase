931703ae4a788ff5e53550f9fd787deb5dbc9640
TzeYiing <ty@tzeyiing.com>
2022-04-05 16:38:48 +0200

    moved utiliity functions to separate file

--- studio/components/interfaces/Settings/Logs/index.ts
+++ studio/components/interfaces/Settings/Logs/index.ts
@@ -1,5 +1,6 @@
 export * from './Logs.types'
 export * from './Logs.constants'
+export * from './Logs.utils'
 export { default as LogsSavedQueriesItem } from './Logs.SavedQueriesItem'
 export { default as LogsQueryPanel } from './LogsQueryPanel'
 export { default as LogTable } from './LogTable'
