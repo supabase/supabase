cfbfeac6d82457b724c283ceeaf1129ac94e2f88
phamhieu <phamhieu1998@gmail.com>
2022-04-07 14:15:58 +0700

    fix: project sidebar menu urls

--- studio/components/layouts/ProjectLayout/NavigationBar/NavigationBar.tsx
+++ studio/components/layouts/ProjectLayout/NavigationBar/NavigationBar.tsx
@@ -3,16 +3,7 @@ import Link from 'next/link'
 import { observer } from 'mobx-react-lite'
 import { useRouter } from 'next/router'
 import { isUndefined } from 'lodash'
-import {
-  Button,
-  Divider,
-  IconHome,
-  Dropdown,
-  IconUser,
-  Select,
-  Typography,
-  IconSettings,
-} from '@supabase/ui'
+import { Button, IconHome, Dropdown, IconUser, IconSettings } from '@supabase/ui'
 
 import { IS_PLATFORM } from 'lib/constants'
 import { useStore } from 'hooks'
@@ -24,7 +15,7 @@ interface Props {}
 const NavigationBar: FC<Props> = ({}) => {
   const router = useRouter()
   const { ui } = useStore()
-  const projectRef = ui.selectedProject?.ref as string
+  const projectRef = ui.selectedProjectRef as string
 
   const activeRoute = router.pathname.split('/')[3]
   const productRoutes = generateProductRoutes(projectRef)
