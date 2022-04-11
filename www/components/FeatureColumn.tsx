c127182fb3fa432b3f68f83c995c89b3aaab1607
Terry Sutton <saltcod@gmail.com>
2022-04-11 16:41:17 -0230

    Remove autoformatting

--- www/components/FeatureColumn.tsx
+++ www/components/FeatureColumn.tsx
@@ -2,7 +2,7 @@ function FeatureColumn({ icon, title, text }: any) {
   return (
     <>
       {icon && <div className="mb-2 p">{icon}</div>}
-      <h4 className="mb-4 text-base text-scale-1200">{title}</h4>
+      <h4 className="text-base text-scale-1200 mb-4">{title}</h4>
       <p className="p">{text}</p>
     </>
   )
