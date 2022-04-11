3cc7d466386cf190a9a424f8e705304caab0b558
phamhieu <phamhieu1998@gmail.com>
2022-04-11 14:12:33 +0700

    chore: tidy up

--- studio/components/layouts/ProjectLayout/TestConnection.tsx
+++ studio/components/layouts/ProjectLayout/TestConnection.tsx
@@ -3,6 +3,7 @@ import { observer } from 'mobx-react-lite'
 import { FC, ReactNode } from 'react'
 import { Project } from 'types'
 import BuildingState from 'components/to-be-cleaned/BuildingState'
+import { PROJECT_STATUS } from 'lib/constants'
 
 interface Props {
   project: Project
@@ -17,8 +18,8 @@ interface Props {
  */
 const TestConnection: FC<Props> = ({ project, children }) => {
   const router = useRouter()
-  const isBuilding = ['COMING_UP', 'RESTORING'].includes(project?.status)
-  const isProjectActive = project.status === 'ACTIVE_HEALTHY'
+  const isBuilding = [PROJECT_STATUS.COMING_UP, PROJECT_STATUS.RESTORING].includes(project?.status)
+  const isProjectActive = project.status === PROJECT_STATUS.ACTIVE_HEALTHY
 
   if (isBuilding && router.asPath !== `/project/${project.ref}/building`) {
     router.push(`/project/${project.ref}/building`, undefined, { shallow: true })
