58864d512225a7d875507eda58265a1470c6aa32
TzeYiing <ty@tzeyiing.com>
2022-04-13 12:25:37 +0200

    add additioanl whitespace formatting

--- studio/components/interfaces/Settings/Logs/LogSelectionRenderers/DefaultSelectionRenderer.tsx
+++ studio/components/interfaces/Settings/Logs/LogSelectionRenderers/DefaultSelectionRenderer.tsx
@@ -13,8 +13,8 @@ const DefaultSelectionRenderer = ({ log }: any) => {
   }) => {
     return (
       <div className="grid grid-cols-12">
-        <span className="text-scale-900 text-sm col-span-4">{label}</span>
-        <span className={`text-scale-1200 text-sm col-span-8 ${code && 'text-xs font-mono'}`}>
+        <span className="text-scale-900 text-sm col-span-4 whitespace-prep-wrap">{label}</span>
+        <span className={`text-scale-1200 text-sm col-span-8 whitespace-prep-wrap ${code && 'text-xs font-mono'}`}>
           {value}
         </span>
       </div>
@@ -33,7 +33,11 @@ const DefaultSelectionRenderer = ({ log }: any) => {
     return (
       <div className="grid grid-cols-12">
         <span className="text-scale-900 text-sm col-span-4">{label}</span>
-        <span className={`text-scale-1200 text-sm col-span-8 ${code && 'text-xs font-mono'}`}>
+        <span
+          className={`text-scale-1200 text-sm col-span-8 overflow-x-auto ${
+            code && 'text-xs font-mono'
+          }`}
+        >
           <pre
             dangerouslySetInnerHTML={{
               __html: jsonSyntaxHighlight(value),
@@ -48,7 +52,10 @@ const DefaultSelectionRenderer = ({ log }: any) => {
     <div className="overflow-hidden overflow-x-auto space-y-6">
       {Object.entries(log).map(([key, value], index) => {
         return (
-          <div key={`${key}-${index}`} className={`${LOGS_TAILWIND_CLASSES.log_selection_x_padding}`}>
+          <div
+            key={`${key}-${index}`}
+            className={`${LOGS_TAILWIND_CLASSES.log_selection_x_padding}`}
+          >
             {value && typeof value === 'object' ? (
               <DetailedJsonRow label={key} value={value} />
             ) : (
