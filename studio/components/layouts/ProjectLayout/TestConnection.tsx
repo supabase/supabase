d4b5c58efd2e4e7559d69da8e9c74917f2a6f28b
Joshen Lim <joshenlimek@gmail.com>
2022-04-21 14:47:30 +0800

    Show a clear indication of when project fails to connect on the dashboard, in particular for restart server

--- studio/components/layouts/ProjectLayout/TestConnection.tsx
+++ studio/components/layouts/ProjectLayout/TestConnection.tsx
@@ -1,9 +1,12 @@
 import { useRouter } from 'next/router'
 import { observer } from 'mobx-react-lite'
-import { FC, ReactNode } from 'react'
+import { FC, ReactNode, useEffect, useState } from 'react'
+
 import { Project } from 'types'
-import { PROJECT_STATUS } from 'lib/constants'
+import { get, head } from 'lib/common/fetch'
+import { API_URL, PROJECT_STATUS } from 'lib/constants'
 import BuildingState from './BuildingState'
+import RestartingState from './RestartingState'
 
 interface Props {
   project: Project
@@ -15,9 +18,24 @@ interface Props {
  *
  * TODO: how can we test project connection properly?
  * ex: the status is ACTIVE_HEALTHY but the project instance is down.
+ *
+ * [Joshen] As of 210422: Current testing connection by pinging postgres
+ * Ideally we'd have a more specific monitoring of the project such as during restarts
+ * But that will come later: https://supabase.slack.com/archives/C01D6TWFFFW/p1650427619665549
+ *
+ * Just note that this logic does not differentiate between a "restarting" state and
+ * a "something is wrong and can't connect to project" state.
+ *
+ * [TODO] Next iteration should scrape long polling and just listen to the project's status
  */
 const TestConnection: FC<Props> = ({ project, children }) => {
   const router = useRouter()
+
+  // [Joshen] I'm gonna default it to true for now so that it doesn't block the UI
+  // Figured this could be more of a check in the background
+  const [projectProps, setProjectProps] = useState<any>()
+  const [isProjectOnline, setIsProjectOnline] = useState(true)
+
   const isBuilding = [PROJECT_STATUS.COMING_UP, PROJECT_STATUS.RESTORING].includes(project?.status)
   const isProjectActive = project.status === PROJECT_STATUS.ACTIVE_HEALTHY
 
@@ -25,9 +43,34 @@ const TestConnection: FC<Props> = ({ project, children }) => {
     router.push(`/project/${project.ref}/building`, undefined, { shallow: true })
   }
 
+  if (!isProjectOnline && router.asPath !== `/project/${project.ref}`) {
+    router.push(`/project/${project.ref}`, undefined, { shallow: true })
+  }
+
+  useEffect(() => {
+    testProjectConnection()
+  }, [project.status])
+
+  // Ping Postgrest to check if project's DB and KPS are both online
+  const testProjectConnection = async () => {
+    const projectProps = await get(`${API_URL}/props/project/${project.ref}/api`)
+    setProjectProps(projectProps)
+
+    const API_KEY = projectProps?.autoApiService?.internalApiKey
+    const swaggerUrl = projectProps?.autoApiService?.restUrl
+
+    const headers: any = { apikey: API_KEY }
+    if (API_KEY?.length > 40) headers['Authorization'] = `Bearer ${API_KEY}`
+
+    const { error } = await head(swaggerUrl, [], { headers, credentials: 'omit' })
+    setIsProjectOnline(error === undefined)
+  }
+
   return (
     <>
-      {isProjectActive ? (
+      {!isProjectOnline ? (
+        <RestartingState project={project} autoApiService={projectProps?.autoApiService} />
+      ) : isProjectActive ? (
         children
       ) : isBuilding ? (
         <BuildingState project={project} />
