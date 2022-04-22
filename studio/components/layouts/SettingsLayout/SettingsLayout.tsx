252a92b0d2cea33a2dc1876edc539e374116148a
phamhieu <phamhieu1998@gmail.com>
2022-04-22 10:15:34 +0700

    fix: settings menus when project is building

--- studio/components/layouts/SettingsLayout/SettingsLayout.tsx
+++ studio/components/layouts/SettingsLayout/SettingsLayout.tsx
@@ -1,12 +1,11 @@
 import { FC, ReactNode } from 'react'
-import { isUndefined } from 'lodash'
 import { observer } from 'mobx-react-lite'
-
+import { useRouter } from 'next/router'
 import { useStore } from 'hooks'
+import { generateSettingsMenu } from './SettingsMenu.utils'
+
 import BaseLayout from '../'
 import ProductMenu from 'components/ui/ProductMenu'
-import { generateSettingsMenu } from './SettingsMenu.utils'
-import { useRouter } from 'next/router'
 
 interface Props {
   title?: string
@@ -15,15 +14,18 @@ interface Props {
 
 const SettingsLayout: FC<Props> = ({ title, children }) => {
   const { ui } = useStore()
-  const projectRef = ui.selectedProject?.ref ?? 'default'
+  const projectRef = ui.selectedProjectRef as string
+  const projectBaseInfo = ui.selectedProjectBaseInfo
 
   const router = useRouter()
   const page = router.pathname.split('/')[4]
+  const menuRoutes = generateSettingsMenu(projectRef, projectBaseInfo)
+
   return (
     <BaseLayout
       title={title || 'Settings'}
       product="Settings"
-      productMenu={<ProductMenu page={page} menu={generateSettingsMenu(projectRef)} />}
+      productMenu={<ProductMenu page={page} menu={menuRoutes} />}
     >
       <main style={{ maxHeight: '100vh' }} className="flex-1 overflow-y-auto">
         {children}
