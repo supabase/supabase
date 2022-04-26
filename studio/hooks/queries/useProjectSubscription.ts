5bbe02c2123f393ff76d657fc829e0ba32104e30
TzeYiing <ty@tzeyiing.com>
2022-04-26 21:27:21 +0200

    Re-implemented upgrade log promps

--- studio/hooks/queries/useProjectSubscription.ts
+++ studio/hooks/queries/useProjectSubscription.ts
@@ -8,13 +8,13 @@ import { StripeSubscription } from 'components/interfaces/Billing'
  *
  * @param ref project reference
  */
-export function useProjectSubscription(ref?: string) {
+export function useProjectSubscription(ref: string) {
   const url = `${API_URL}/projects/${ref}/subscription`
-  const { data, error } = useSWR<any>(ref ? url : null, get)
+  const { data, error } = useSWR<StripeSubscription & { error?: string }>(url, get)
   const anyError = data?.error || error
 
   return {
-    subscription: anyError ? undefined : (data as StripeSubscription),
+    subscription: anyError ? null : (data as StripeSubscription),
     error: anyError,
     isLoading: !anyError && !data,
     isError: !!anyError,
