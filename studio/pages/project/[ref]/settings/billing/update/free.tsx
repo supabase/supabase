f074cc605f1d3cab5454b37b1d1d1f0f63750ffe
Joshen Lim <joshenlimek@gmail.com>
2022-04-26 10:52:25 +0700

    Show confirmation modal when downgrading to free if project is not on micro

--- studio/pages/project/[ref]/settings/billing/update/free.tsx
+++ studio/pages/project/[ref]/settings/billing/update/free.tsx
@@ -8,7 +8,7 @@ import { get } from 'lib/common/fetch'
 import { API_URL, STRIPE_PRODUCT_IDS } from 'lib/constants'
 
 import { BillingLayout } from 'components/layouts'
-import { ExitSurvey } from 'components/interfaces/Billing'
+import { ExitSurvey, StripeSubscription } from 'components/interfaces/Billing'
 
 const BillingUpdateFree: NextPage = () => {
   const { ui } = useStore()
@@ -16,11 +16,15 @@ const BillingUpdateFree: NextPage = () => {
   const projectRef = ui.selectedProject?.ref
 
   const [products, setProducts] = useState<{ tiers: any[]; addons: any[] }>()
+  const [subscription, setSubscription] = useState<StripeSubscription>()
   const [isLoadingProducts, setIsLoadingProducts] = useState(false)
 
   useEffect(() => {
-    getStripeProducts()
-  }, [])
+    if (projectRef) {
+      getStripeProducts()
+      getSubscription()
+    }
+  }, [projectRef])
 
   const getStripeProducts = async () => {
     try {
@@ -37,6 +41,24 @@ const BillingUpdateFree: NextPage = () => {
     }
   }
 
+  const getSubscription = async () => {
+    try {
+      if (!ui.selectedProject?.subscription_id) {
+        throw new Error('Unable to get subscription ID of project')
+      }
+
+      const subscription = await get(`${API_URL}/projects/${projectRef}/subscription`)
+      if (subscription.error) throw subscription.error
+      setSubscription(subscription)
+    } catch (error: any) {
+      ui.setNotification({
+        error,
+        category: 'error',
+        message: `Failed to get subscription: ${error.message}`,
+      })
+    }
+  }
+
   const freeTier = products?.tiers.find((tier: any) => tier.id === STRIPE_PRODUCT_IDS.FREE)
 
   return (
@@ -44,6 +66,7 @@ const BillingUpdateFree: NextPage = () => {
       <div className="mx-auto max-w-5xl my-10">
         <ExitSurvey
           freeTier={freeTier}
+          subscription={subscription}
           onSelectBack={() => router.push(`/project/${projectRef}/settings/billing/update`)}
         />
       </div>
