88e28376aea69ee05da03de4bcf2108e3aca9d48
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-05-11 12:13:45 +0800

    fix: move form submit button into a form. native browser validation catches issue with date input incomplete state
    • todo: move to @supabase/ui Form
    originally tried to do that but it was too troublesome and required more refactor than anticipated.
    this is a quicker fix for now.

--- studio/components/interfaces/TableGridEditor/SidePanelEditor/ActionBar.tsx
+++ studio/components/interfaces/TableGridEditor/SidePanelEditor/ActionBar.tsx
@@ -8,6 +8,8 @@ interface ActionBarProps {
   disableApply?: boolean
   applyFunction?: (resolve: any) => void
   closePanel: () => void
+  loading?: boolean
+  noApplyFunction?: boolean
 }
 const ActionBar: FC<ActionBarProps> = ({
   applyButtonLabel = 'Apply',
@@ -16,6 +18,8 @@ const ActionBar: FC<ActionBarProps> = ({
   disableApply = false,
   applyFunction = null,
   closePanel,
+  loading = undefined,
+  noApplyFunction = false,
 }) => {
   const [isRunning, setIsRunning] = useState(false)
 
@@ -29,18 +33,40 @@ const ActionBar: FC<ActionBarProps> = ({
   }
 
   return (
-    <div className="space-x-3 w-full flex justify-between px-3 py-4 border-t border-scale-500">
+    <div className="border-scale-500 flex w-full justify-between space-x-3 border-t px-3 py-4">
       <Button size="small" onClick={closePanel} type="default">
         {backButtonLabel}
       </Button>
+
       {children}
+
       {applyFunction && (
+        /**
+         * old solution
+         * this is necessary when loading is handled by this component
+         *
+         * author @mildtomato
+         */
         <Button
           size="small"
           onClick={onSelectApply}
           disabled={disableApply || isRunning}
           loading={isRunning}
         >
+          {applyButtonLabel}
+        </Button>
+      )}
+
+      {noApplyFunction && (
+        /**
+         * new solution to avoid using applyFunction
+         * this is necessary for handling loading by form itself
+         *
+         * this does not require he applyCallback() above
+         *
+         * author @mildtomato
+         */
+        <Button size="small" disabled={disableApply} loading={loading}>
           {applyButtonLabel}
         </Button>
       )}
