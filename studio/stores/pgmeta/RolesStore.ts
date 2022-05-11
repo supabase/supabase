7b3565aedd8e723092304b795cd3636dc6be57d4
Joshen Lim <joshenlimek@gmail.com>
2022-05-11 14:16:48 +0800

    Filter out system roles for selection in policy editor

--- studio/stores/pgmeta/RolesStore.ts
+++ studio/stores/pgmeta/RolesStore.ts
@@ -12,4 +12,24 @@ export default class RolesStore extends PostgresMetaInterface<any> {
   ) {
     super(rootStore, dataUrl, headers, options)
   }
+
+  list(filter: any) {
+    const systemRoles = [
+      'postgres',
+      'pgbouncer',
+      'supabase_admin',
+      'supabase_auth_admin',
+      'supabase_storage_admin',
+      'dashboard_user',
+      'authenticator',
+      'pg_database_owner',
+      'pg_read_all_data',
+      'pg_write_all_data',
+    ]
+
+    const rolesFilter = (role: any) =>
+      !systemRoles.includes(role.name) && (typeof filter === 'function' ? filter(role) : true)
+
+    return super.list(rolesFilter)
+  }
 }
