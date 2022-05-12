d7fa6997e4e2c0afd7983b1b4e7f99b3af56a762
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-05-12 10:35:41 +0800

    feat: new auth providers form
    • added new mobx store for config
    • using yup() for validation schema
    • tried to follow some conventions of JSONSchema, but can do better. can investigate some schema-to-yup solutions later so things can be more JSONSchema.
    • made up a few parts of JSONSchema to show/hide properties - not following any official convention.

--- studio/pages/project/[ref]/auth/settings-new.tsx
+++ studio/pages/project/[ref]/auth/settings-new.tsx
@@ -1,53 +1,97 @@
-import {
-  Button,
-  Collapsible,
-  Form,
-  IconArrowRight,
-  IconCheck,
-  IconChevronUp,
-  IconGlobe,
-  IconTrash,
-  IconX,
-  Input,
-  InputNumber,
-  Modal,
-  Tabs,
-  Toggle,
-} from '@supabase/ui'
+import { Loading } from '@supabase/ui'
+import { AuthProvidersForm } from 'components/interfaces'
 import { AuthLayout } from 'components/layouts'
-
-import { withAuth } from 'hooks'
-import { observer } from 'mobx-react-lite'
-import React, { useReducer, useState } from 'react'
-// import AuthFormSchema from './AuthFormSchema.json'
-
 import { AutoSchemaForm } from 'components/ui/Forms'
+import { useStore, withAuth } from 'hooks'
+import { get } from 'lib/common/fetch'
+import { API_URL } from 'lib/constants'
+import { observer } from 'mobx-react-lite'
+import { useRouter } from 'next/router'
+import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import useSWR from 'swr'
+import { NextPageWithLayout } from 'types'
 
-const Auth = () => {
-  return (
-    <AuthLayout title="Auth">
-      <div className="p-4">
-        <WholeForm />
+// const Auth = () => {
+//   return (
+//     <AuthLayout title="Auth">
+//       <div className="p-4">
+//         <WholeForm />
+//       </div>
+//     </AuthLayout>
+//   )
+// }
+
+const PageLayout: NextPageWithLayout = () => {
+  const router = useRouter()
+  const { ui, authConfig } = useStore()
+
+  const URL = `${API_URL}/auth/${router.query.ref}/config`
+  // const { data: config, error }: any = useSWR(URL, get)
+
+  // console.log(authConfig)
+
+  useEffect(() => {
+    // temporary store loader
+    authConfig.load()
+  }, [ui.selectedProjectRef])
+
+  const [model, setModel] = useState<any>({})
+  const [isCustomSMTPEnabled, setCustomSMTP] = useState<boolean>(false)
+  const [externalProvidersModel, setExternalProvidersModel] = useState<any>({})
+
+  if (!authConfig.isLoaded) {
+    return <Loading active={true}>{''}</Loading>
+  }
+
+  // if (error) {
+  //   return (
+  //     <span>
+  //       <p>Error connecting to API</p>
+  //       <p>{`${error}`}</p>
+  //     </span>
+  //   )
+  // }
+
+  // console.log(config)
+
+  // useEffect(() => {
+  //   if (config) {
+  //     const temp =
+  //       config.SMTP_ADMIN_EMAIL ||
+  //       config.SMTP_HOST ||
+  //       config.SMTP_PORT ||
+  //       config.SMTP_USER ||
+  //       config.SMTP_PASS
+  //     setCustomSMTP(temp)
+  //   }
+  //   setModel({ ...config })
+  //   setExternalProvidersModel({ ...config })
+  // }, [config])
+
+  if (authConfig)
+    return (
+      <div style={{ width: '820px' }} className="mx-auto">
+        <div>
+          <h3 className="text-scale-1200 mb-2 text-2xl">General settings</h3>
+          <p className="text-scale-900 text-sm">
+            Turn payment methods on and off in one click – no engineering time required.
+          </p>
+          <p className="text-scale-900 text-sm">
+            Use our guide to check which payment methods are compatible with your integration.
+          </p>
+          <AutoSchemaForm />
+        </div>
+
+        <AuthProvidersForm />
       </div>
-    </AuthLayout>
-  )
+    )
 }
 
-export function WholeForm() {
-  return (
-    <div style={{ width: '820px' }} className="mx-auto">
-      <div>
-        <h3 className="text-scale-1200 mb-2 text-2xl">General settings</h3>
-        <p className="text-scale-900 text-sm">
-          Turn payment methods on and off in one click – no engineering time required.
-        </p>
-        <p className="text-scale-900 text-sm">
-          Use our guide to check which payment methods are compatible with your integration.
-        </p>
-        <AutoSchemaForm />
-      </div>
-    </div>
-  )
+PageLayout.getLayout = (page) => {
+  // console.log()
+  return <AuthLayout>{page}</AuthLayout>
 }
 
-export default withAuth(observer(Auth))
+export default observer(PageLayout)
---
+++ studio/stores/authConfig/ProjectAuthConfigStore.ts
@@ -0,0 +1,124 @@
+import { makeAutoObservable, toJS } from 'mobx'
+import { keyBy } from 'lodash'
+
+import { get, post, patch, delete_ } from 'lib/common/fetch'
+import { UserContent, UserContentMap } from 'types'
+import { IRootStore } from '../RootStore'
+import { API_URL } from 'lib/constants'
+
+// [Joshen] This will be the new ProjectContentStore
+// but use the one under the stores folder projectContentStore first while we transition
+
+export interface IProjectAuthConfigStore {
+  isLoading: boolean
+  isInitialized: boolean
+  isLoaded: boolean
+  error: any
+  config: any
+
+  baseUrl: string
+  projectRef: string
+
+  load: () => void
+  update: ({}) => void
+  setProjectRef: (ref?: string) => void
+}
+
+export default class ProjectAuthConfigStore implements IProjectAuthConfigStore {
+  rootStore: IRootStore
+  STATES = {
+    INITIAL: 'initial',
+    LOADING: 'loading',
+    ERROR: 'error',
+    LOADED: 'loaded',
+  }
+  baseUrl: string
+  projectRef: string
+  // @ts-ignore
+  data: UserContentMap = []
+
+  state = this.STATES.INITIAL
+  error = null
+
+  constructor(rootStore: IRootStore, options: { projectRef: string }) {
+    const { projectRef } = options
+    this.projectRef = projectRef
+    this.rootStore = rootStore
+    this.baseUrl = ``
+    makeAutoObservable(this)
+  }
+
+  get isLoading() {
+    return this.state === this.STATES.INITIAL || this.state === this.STATES.LOADING
+  }
+
+  get isInitialized() {
+    return this.state !== this.STATES.INITIAL
+  }
+
+  get isLoaded() {
+    return this.state === this.STATES.LOADED
+  }
+
+  get config() {
+    return this.data
+  }
+
+  async fetchData() {
+    const headers = {
+      'Content-Type': 'application/json',
+    }
+    console.log('this.baseUrl', this.baseUrl)
+    const response = await get(this.baseUrl, { headers })
+    console.log('response in authCOnfig store', response)
+    if (response.error) {
+      throw response.error
+    }
+    this.data = response
+    return response
+  }
+
+  async load() {
+    let { LOADING, ERROR, LOADED } = this.STATES
+    try {
+      this.error = null
+      this.state = LOADING
+      await this.fetchData()
+      this.state = LOADED
+      return this.data
+    } catch (e: any) {
+      console.error('Failed to load auth config', e.message)
+      this.error = e
+      this.state = ERROR
+    }
+  }
+
+  byId(id: any) {
+    return toJS(this.data)[id]
+  }
+
+  async update(updates: any) {
+    try {
+      const headers = {
+        'Content-Type': 'application/json',
+      }
+      let payload = {
+        ...updates,
+      }
+      const url = `${this.baseUrl}`
+      const updated = await patch(url, payload, { headers })
+      if (updated.error) throw updated.error
+      this.data = updated
+      return { data: updated, error: null }
+    } catch (error) {
+      return { data: null, error }
+    }
+  }
+
+  setProjectRef(ref?: string) {
+    if (ref) {
+      this.projectRef = ref
+      this.baseUrl = `${API_URL}/auth/${ref}/config`
+    }
+  }
+}
