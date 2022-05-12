6b6ebe085997d22acc26129ef4359d79328bab2f
Joshen Lim <joshenlimek@gmail.com>
2022-05-12 18:09:53 +0800

    Refactor

--- studio/lib/helpers.ts
+++ studio/lib/helpers.ts
@@ -210,22 +210,3 @@ export const detectBrowser = () => {
     return 'Safari'
   }
 }
-
-/**
- * Formats a PG datetime string into a format that's
- * readable across browsers.
- * - Postgres: 2022-05-12 09:24:12.639463+00
- *
- * These are the acceptable formats that Joshen has experimented so far
- * - Chrome:   2022-05-12 09:24:12.639463+00 (No format required)
- * - Firefox:  2022-05-12 09:24:12.639463+00 (No format required)
- * - Safari:   2022-05-12T09:24:12.639463+00
- */
-export const formatPgDateString = (dateString: string) => {
-  const browser = detectBrowser()
-  if (browser === 'Safari') {
-    return dateString.replace(' ', 'T')
-  } else {
-    return dateString
-  }
-}
