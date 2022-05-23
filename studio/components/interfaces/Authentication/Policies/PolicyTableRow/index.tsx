8a9b46250a67bfa1742fb9076b5f8b442bd05c2f
phamhieu <phamhieu1998@gmail.com>
2022-05-23 17:03:38 +0700

    refactor: auth policies page
    - remove mobx local store
    - filter table policies from meta.policies
    - clean up

--- studio/components/interfaces/Authentication/Policies/PolicyTableRow/index.tsx
+++ studio/components/interfaces/Authentication/Policies/PolicyTableRow/index.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react'
 import { PostgresTable, PostgresPolicy } from '@supabase/postgres-meta'
+import { observer } from 'mobx-react-lite'
+import { useStore } from 'hooks'
 
 import PolicyTableRowHeader from './PolicyTableRowHeader'
 import PolicyRow from './PolicyRow'
@@ -13,40 +15,44 @@ interface Props {
   onSelectDeletePolicy: (policy: PostgresPolicy) => void
 }
 
-const PolicyTableRow: FC<Props> = ({
-  table,
-  onSelectToggleRLS = () => {},
-  onSelectCreatePolicy = () => {},
-  onSelectEditPolicy = () => {},
-  onSelectDeletePolicy = () => {},
-}) => {
-  return (
-    <Panel
-      title={
-        <PolicyTableRowHeader
-          table={table}
-          onSelectToggleRLS={onSelectToggleRLS}
-          onSelectCreatePolicy={onSelectCreatePolicy}
-        />
-      }
-    >
-      {table.policies.length === 0 && (
-        <div className="p-4 px-6">
-          <p className="text-scale-900 text-sm">No policies created yet</p>
-        </div>
-      )}
+const PolicyTableRow: FC<Props> = observer(
+  ({
+    table,
+    onSelectToggleRLS = () => {},
+    onSelectCreatePolicy = () => {},
+    onSelectEditPolicy = () => {},
+    onSelectDeletePolicy = () => {},
+  }) => {
+    const { meta } = useStore()
+    const policies = meta.policies.list((x: PostgresPolicy) => x.table === table.name)
+    return (
+      <Panel
+        title={
+          <PolicyTableRowHeader
+            table={table}
+            onSelectToggleRLS={onSelectToggleRLS}
+            onSelectCreatePolicy={onSelectCreatePolicy}
+          />
+        }
+      >
+        {policies.length === 0 && (
+          <div className="p-4 px-6">
+            <p className="text-scale-900 text-sm">No policies created yet</p>
+          </div>
+        )}
 
-      {/* @ts-ignore */}
-      {table.policies.map((policy) => (
-        <PolicyRow
-          key={policy.id}
-          policy={policy}
-          onSelectEditPolicy={onSelectEditPolicy}
-          onSelectDeletePolicy={onSelectDeletePolicy}
-        />
-      ))}
-    </Panel>
-  )
-}
+        {/* @ts-ignore */}
+        {policies.map((policy) => (
+          <PolicyRow
+            key={policy.id}
+            policy={policy}
+            onSelectEditPolicy={onSelectEditPolicy}
+            onSelectDeletePolicy={onSelectDeletePolicy}
+          />
+        ))}
+      </Panel>
+    )
+  }
+)
 
 export default PolicyTableRow
