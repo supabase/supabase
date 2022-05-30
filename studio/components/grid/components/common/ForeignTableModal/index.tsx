6e3f919cb78919951d442b1d82ffdc167fde65da
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-05-30 20:22:15 +0800

    chore: made the CSS work for grid

--- studio/components/grid/components/common/ForeignTableModal/index.tsx
+++ studio/components/grid/components/common/ForeignTableModal/index.tsx
@@ -1,47 +1,43 @@
-import * as React from 'react';
-import { Button, Modal, Menu, IconExternalLink, IconTrash } from '@supabase/ui';
-import { useTrackedState } from '../../../store';
-import { Dictionary, Filter } from '../../../types';
-import { RowItem } from './RowItem';
-import { FilterHeader } from './FilterHeader';
-import Query from '../../../query';
+import * as React from 'react'
+import { Button, Modal, Menu, IconExternalLink, IconTrash } from '@supabase/ui'
+import { useTrackedState } from '../../../store'
+import { Dictionary, Filter } from '../../../types'
+import { RowItem } from './RowItem'
+import { FilterHeader } from './FilterHeader'
+import Query from '../../../query'
 
 type ForeignTableModalProps = {
-  columnName?: string;
-  defaultValue?: string;
-  onChange: (value: any | null) => void;
-};
+  columnName?: string
+  defaultValue?: string
+  onChange: (value: any | null) => void
+}
 
 export const ForeignTableModal: React.FC<ForeignTableModalProps> = ({
   columnName,
   defaultValue,
   onChange,
 }) => {
-  const [visible, setVisible] = React.useState(false);
-  const [foreignColumnNames, setForeignColumnNames] = React.useState<string[]>(
-    []
-  );
-  const [rows, setRows] = React.useState<Dictionary<any>[] | null>(null);
-  const state = useTrackedState();
-  const columnDefinition = state.table?.columns.find(
-    (x) => x.name == columnName
-  );
+  const [visible, setVisible] = React.useState(false)
+  const [foreignColumnNames, setForeignColumnNames] = React.useState<string[]>([])
+  const [rows, setRows] = React.useState<Dictionary<any>[] | null>(null)
+  const state = useTrackedState()
+  const columnDefinition = state.table?.columns.find((x) => x.name == columnName)
 
   React.useEffect(() => {
-    if (!visible) return;
+    if (!visible) return
 
-    fetchColumns();
+    fetchColumns()
 
     if (defaultValue && columnDefinition) {
       fetchData({
         column: columnDefinition.targetColumnName!,
         operator: '=',
         value: defaultValue,
-      });
+      })
     } else {
-      fetchData();
+      fetchData()
     }
-  }, [visible]);
+  }, [visible])
 
   async function fetchColumns() {
     if (
@@ -49,80 +45,84 @@ export const ForeignTableModal: React.FC<ForeignTableModalProps> = ({
       !columnDefinition?.targetTableName ||
       !columnDefinition?.targetTableSchema
     )
-      return;
+      return
 
     const { data } = await state.metaService.fetchColumns(
       columnDefinition?.targetTableName,
       columnDefinition?.targetTableSchema
-    );
+    )
     if (data && data.length > 0) {
-      const columnNames = data.map((x) => x.name);
-      setForeignColumnNames(columnNames);
+      const columnNames = data.map((x) => x.name)
+      setForeignColumnNames(columnNames)
     }
   }
 
   async function fetchData(filter?: Filter) {
-    if (!state.onSqlQuery || !columnDefinition?.targetTableName) return;
+    if (!state.onSqlQuery || !columnDefinition?.targetTableName) return
 
-    const query = new Query();
+    const query = new Query()
     let queryChains = query
-      .from(
-        columnDefinition?.targetTableName,
-        columnDefinition?.targetTableSchema ?? undefined
-      )
-      .select();
+      .from(columnDefinition?.targetTableName, columnDefinition?.targetTableSchema ?? undefined)
+      .select()
 
     if (filter && filter.value && filter.value != '') {
-      const { column, operator, value } = filter;
-      queryChains = queryChains.filter(column, operator, value);
+      const { column, operator, value } = filter
+      queryChains = queryChains.filter(column, operator, value)
     }
 
     // TODO: How to let users know that filter result limit at 20 results
     // should we allow a higher value?
-    const sql = queryChains.range(0, 20).toSql();
-    const { data, error } = await state.onSqlQuery(sql);
+    const sql = queryChains.range(0, 20).toSql()
+    const { data, error } = await state.onSqlQuery(sql)
     if (error) {
-      setRows(null);
+      setRows(null)
     } else {
-      setRows(data);
+      setRows(data)
     }
   }
 
   function toggle(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-    if (event && event.stopPropagation) event.stopPropagation();
-    setVisible(!visible);
+    if (event && event.stopPropagation) event.stopPropagation()
+    setVisible(!visible)
   }
 
   function onItemSelect(item: Dictionary<any>) {
     if (item && columnDefinition && columnDefinition.targetColumnName) {
-      const value = item[columnDefinition.targetColumnName];
-      onChange(value);
+      const value = item[columnDefinition.targetColumnName]
+      onChange(value)
     }
 
-    setVisible(false);
+    setVisible(false)
   }
 
   function renderRows() {
-    if (!rows) return null;
+    if (!rows) return null
     const temp = rows.map((x, i) => {
-      return <RowItem key={`menu-${i}`} item={x} onSelect={onItemSelect} columnNames={foreignColumnNames} />;
-    });
-    return <Menu className="foreign-table-modal__menu">{temp}</Menu>;
+      return (
+        <RowItem
+          key={`menu-${i}`}
+          item={x}
+          onSelect={onItemSelect}
+          columnNames={foreignColumnNames}
+        />
+      )
+    })
+    return <Menu className="foreign-table-modal__menu">{temp}</Menu>
   }
 
   function onFilterChange(filter: Filter) {
-    if (filter && filter.value && filter.value != '') fetchData(filter);
+    if (filter && filter.value && filter.value != '') fetchData(filter)
   }
 
   function onClearValueClick() {
-    onChange(null);
-    setVisible(false);
+    onChange(null)
+    setVisible(false)
   }
 
   return (
     <>
       <Button
-        type="text"
+        type="default"
         onClick={toggle}
         icon={<IconExternalLink />}
         style={{ padding: '3px' }}
@@ -158,5 +158,5 @@ export const ForeignTableModal: React.FC<ForeignTableModalProps> = ({
         </Modal.Content>
       </Modal>
     </>
-  );
-};
+  )
+}
