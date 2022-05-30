c37ee44d4bdec0ad5d3fb7a03bbbc3cc3b303669
Terry Sutton <saltcod@gmail.com>
2022-05-30 12:07:33 -0230

    Use the latest value for the chart heading instead of the average number

--- studio/components/to-be-cleaned/Charts/ChartHandler.tsx
+++ studio/components/to-be-cleaned/Charts/ChartHandler.tsx
@@ -128,7 +128,7 @@ const ChartHandler: FC<Props> = ({
     ? chartData?.total
     : provider === 'log-stats'
     ? chartData?.totalGrouped?.[attribute]
-    : chartData?.totalAverage
+    : chartData?.data[chartData?.data.length -1]?.[attribute]
 
   if (loading) {
     return (
