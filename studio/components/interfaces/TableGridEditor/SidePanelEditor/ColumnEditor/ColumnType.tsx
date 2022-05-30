9fef2360de2ee6ff53b274d635fa9d3d58eec61d
Terry Sutton <saltcod@gmail.com>
2022-05-30 09:17:08 -0230

    Add link to data types

--- studio/components/interfaces/TableGridEditor/SidePanelEditor/ColumnEditor/ColumnType.tsx
+++ studio/components/interfaces/TableGridEditor/SidePanelEditor/ColumnEditor/ColumnType.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react'
-import { IconCalendar, IconType, IconHash, Listbox, IconToggleRight, Input } from '@supabase/ui'
+import { IconCalendar, IconType, IconHash, Listbox, IconToggleRight, Input, IconArrowUpRight } from '@supabase/ui'
 import { PostgresType } from '@supabase/postgres-meta'
 import { POSTGRES_DATA_TYPES, POSTGRES_DATA_TYPE_OPTIONS } from '../SidePanelEditor.constants'
 import { PostgresDataTypeOption } from '../SidePanelEditor.types'
@@ -68,73 +68,83 @@ const ColumnType: FC<Props> = ({
   }
 
   return (
-    <Listbox
-      label={showLabel ? 'Type' : ''}
-      layout={showLabel ? 'horizontal' : 'vertical'}
-      value={value}
-      size={size}
-      error={error}
-      disabled={disabled}
-      className={`${className} ${disabled ? 'column-type-disabled' : ''} rounded-md`}
-      onChange={(value: string) => onOptionSelect(value)}
-      optionsWidth={480}
-    >
-      <Listbox.Option key="empty" value="" label="---">
-        ---
-      </Listbox.Option>
-
-      {/*
-        Weird issue with Listbox here 
-        1. Can't do render conditionally (&&) within Listbox hence why using Fragment
-        2. Can't wrap these 2 components within a Fragment conditional (enumTypes.length)
-           as selecting the enumType option will not render it in the Listbox component
-      */}
-      {enumTypes.length > 0 ? (
-        <Listbox.Option disabled key="header-1" value="header-1" label="header-1">
-          User-defined Enumerated Types
+    <>
+      <Listbox
+        label={showLabel ? 'Type' : ''}
+        layout={showLabel ? 'horizontal' : 'vertical'}
+        value={value}
+        size={size}
+        error={error}
+        disabled={disabled}
+        className={`${className} ${disabled ? 'column-type-disabled' : ''} rounded-md`}
+        onChange={(value: string) => onOptionSelect(value)}
+        optionsWidth={480}
+      >
+        <Listbox.Option key="empty" value="" label="---">
+          ---
         </Listbox.Option>
-      ) : (
-        <></>
-      )}
 
-      {enumTypes.length > 0 ? (
-        // @ts-ignore
-        enumTypes.map((enumType: PostgresType) => (
+        {/*
+          Weird issue with Listbox here
+          1. Can't do render conditionally (&&) within Listbox hence why using Fragment
+          2. Can't wrap these 2 components within a Fragment conditional (enumTypes.length)
+            as selecting the enumType option will not render it in the Listbox component
+        */}
+        {enumTypes.length > 0 ? (
+          <Listbox.Option disabled key="header-1" value="header-1" label="header-1">
+            User-defined Enumerated Types
+          </Listbox.Option>
+        ) : (
+          <></>
+        )}
+
+        {enumTypes.length > 0 ? (
+          // @ts-ignore
+          enumTypes.map((enumType: PostgresType) => (
+            <Listbox.Option
+              key={enumType.name}
+              value={enumType.name}
+              label={enumType.name}
+              addOnBefore={() => {
+                return <div className="bg-scale-1200 mx-1 h-2 w-2 rounded-full" />
+              }}
+            >
+              <div className="flex items-center space-x-4">
+                <p>{enumType.name}</p>
+              </div>
+            </Listbox.Option>
+          ))
+        ) : (
+          <></>
+        )}
+
+        <Listbox.Option disabled value="header-2" label="header-2">
+          PostgreSQL Data Types
+        </Listbox.Option>
+
+        {POSTGRES_DATA_TYPE_OPTIONS.map((option: PostgresDataTypeOption) => (
           <Listbox.Option
-            key={enumType.name}
-            value={enumType.name}
-            label={enumType.name}
-            addOnBefore={() => {
-              return <div className="bg-scale-1200 mx-1 h-2 w-2 rounded-full" />
-            }}
+            key={option.name}
+            value={option.name}
+            label={option.name}
+            addOnBefore={() => inferIcon(option.type)}
           >
             <div className="flex items-center space-x-4">
-              <p>{enumType.name}</p>
+              <span className="text-scale-1200">{option.name}</span>
+              <span className="text-scale-900">{option.description}</span>
             </div>
           </Listbox.Option>
-        ))
-      ) : (
-        <></>
-      )}
+        ))}
+      </Listbox>
+      <div className="text-sm grid md:grid-cols-12 md:gap-x-4 ">
+        <p className='text-scale-1100 col-span-7 col-start-5 -mt-4'>
+          <a href="https://supabase.com/pricing" target="_blank" className="underline flex items-center gap-1">
+            Learn more about data types <IconArrowUpRight size={14} className="text-scale-1200" strokeWidth={1.5} />
+          </a>
 
-      <Listbox.Option disabled value="header-2" label="header-2">
-        PostgreSQL Data Types
-      </Listbox.Option>
-
-      {POSTGRES_DATA_TYPE_OPTIONS.map((option: PostgresDataTypeOption) => (
-        <Listbox.Option
-          key={option.name}
-          value={option.name}
-          label={option.name}
-          addOnBefore={() => inferIcon(option.type)}
-        >
-          <div className="flex items-center space-x-4">
-            <span className="text-scale-1200">{option.name}</span>
-            <span className="text-scale-900">{option.description}</span>
-          </div>
-        </Listbox.Option>
-      ))}
-    </Listbox>
+        </p>
+      </div>
+    </>
   )
 }
 
