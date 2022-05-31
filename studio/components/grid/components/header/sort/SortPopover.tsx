376096f09a1f6462f189c2912b83dcd57fe314a9
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-05-31 18:13:19 +0800

    fix: colors and width issues- fixes #7079
    fixes #7079

--- studio/components/grid/components/header/sort/SortPopover.tsx
+++ studio/components/grid/components/header/sort/SortPopover.tsx
@@ -1,70 +1,63 @@
-import React, { FC } from 'react';
-import { Button, IconList, IconChevronDown, Popover } from '@supabase/ui';
-import { DropdownControl } from '../../common';
-import { useDispatch, useTrackedState } from '../../../store';
-import SortRow from './SortRow';
+import React, { FC } from 'react'
+import { Button, IconList, IconChevronDown, Popover } from '@supabase/ui'
+import { DropdownControl } from '../../common'
+import { useDispatch, useTrackedState } from '../../../store'
+import SortRow from './SortRow'
 
 const SortPopover: FC = () => {
-  const state = useTrackedState();
+  const state = useTrackedState()
   const btnText =
     state.sorts.length > 0
-      ? `Sorted by ${state.sorts.length} rule${
-          state.sorts.length > 1 ? 's' : ''
-        }`
-      : 'Sort';
+      ? `Sorted by ${state.sorts.length} rule${state.sorts.length > 1 ? 's' : ''}`
+      : 'Sort'
 
   return (
-    <Popover
-      size="large"
-      align="start"
-      className="sb-grid-sort-popover"
-      overlay={<Sort />}
-    >
+    <Popover size="large" align="start" className="sb-grid-sort-popover" overlay={<Sort />}>
       <Button
         as={'span'}
         type="text"
-        icon={<IconList />}
-        style={{ padding: '4px 8px' }}
+        icon={
+          <div className="text-scale-900">
+            <IconList strokeWidth={1.5} />
+          </div>
+        }
+        // style={{ padding: '4px 8px' }}
       >
         {btnText}
       </Button>
     </Popover>
-  );
-};
-export default SortPopover;
+  )
+}
+export default SortPopover
 
 const Sort: FC = () => {
-  const state = useTrackedState();
-  const dispatch = useDispatch();
+  const state = useTrackedState()
+  const dispatch = useDispatch()
   const columns = state?.table?.columns!.filter((x) => {
-    const found = state.sorts.find((y) => y.column == x.name);
-    return !found;
-  });
+    const found = state.sorts.find((y) => y.column == x.name)
+    return !found
+  })
   const dropdownOptions =
     columns?.map((x) => {
-      return { value: x.name, label: x.name };
-    }) || [];
+      return { value: x.name, label: x.name }
+    }) || []
 
   function onAddSort(columnName: string | number) {
     dispatch({
       type: 'ADD_SORT',
       payload: { column: columnName, ascending: true },
-    });
+    })
   }
 
   return (
-    <div className="py-2 space-y-2">
+    <div className="space-y-2 py-2">
       {state.sorts.map((x, index) => (
         <SortRow key={x.column} columnName={x.column} index={index} />
       ))}
       {state.sorts.length == 0 && (
         <div className="space-y-1 px-3">
-          <h5 className="text-sm text-scale-1100">
-            No sorts applied to this view
-          </h5>
-          <p className="text-xs text-scale-900">
-            Add a column below to sort the view
-          </p>
+          <h5 className="text-scale-1100 text-sm">No sorts applied to this view</h5>
+          <p className="text-scale-900 text-xs">Add a column below to sort the view</p>
         </div>
       )}
 
@@ -83,15 +76,13 @@ const Sort: FC = () => {
               iconRight={<IconChevronDown />}
               className="sb-grid-dropdown__item-trigger"
             >
-              {`Pick ${
-                state.sorts.length > 1 ? 'another' : 'a'
-              } column to sort by`}
+              {`Pick ${state.sorts.length > 1 ? 'another' : 'a'} column to sort by`}
             </Button>
           </DropdownControl>
         ) : (
-          <p className="text-sm text-scale-1100">All columns have been added</p>
+          <p className="text-scale-1100 text-sm">All columns have been added</p>
         )}
       </div>
     </div>
-  );
-};
+  )
+}
