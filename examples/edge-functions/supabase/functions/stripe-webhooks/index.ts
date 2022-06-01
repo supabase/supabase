7371c5bf818d3200d51bd1b57815c0f23bce963d
thorwebdev <thor@supabase.io>
2022-06-01 14:38:36 +0800

    feat: add stripe webhook edge function example

---
+++ examples/edge-functions/supabase/functions/stripe-webhooks/index.ts
@@ -0,0 +1,67 @@
+// @ts-nocheck
+
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+import { serve } from 'https://deno.land/std@0.131.0/http/server.ts'
+
+// esm.sh is used to compile stripe-node to be compatible with ES modules.
+import Stripe from 'https://esm.sh/stripe?target=deno&no-check'
+
+const stripe = Stripe(Deno.env.get('STRIPE_API_KEY'), {
+  // This is needed to use the Fetch API rather than relying on the Node http
+  // package.
+  httpClient: Stripe.createFetchHttpClient(),
+})
+// This is needed in order to use the Web Crypto API in Deno.
+const cryptoProvider = Stripe.createSubtleCryptoProvider()
+
+console.log(`Function "stripe-webhooks" up and running!`)
+
+serve(async (request) => {
+  const signature = request.headers.get('Stripe-Signature')
+
+  // First step is to verify the event. The .text() method must be used as the
+  // verification relies on the raw request body rather than the parsed JSON.
+  const body = await request.text()
+  let receivedEvent
+  try {
+    receivedEvent = await stripe.webhooks.constructEventAsync(
+      body,
+      signature,
+      Deno.env.get('STRIPE_WEBHOOK_SIGNING_SECRET'),
+      undefined,
+      cryptoProvider
+    )
+  } catch (err) {
+    return new Response(err.message, { status: 400 })
+  }
+
+  // Secondly, we use this event to query the Stripe API in order to avoid
+  // handling any forged event. If available, we use the idempotency key.
+  const requestOptions =
+    receivedEvent.request && receivedEvent.request.idempotency_key
+      ? {
+          idempotencyKey: receivedEvent.request.idempotency_key,
+        }
+      : {}
+
+  let retrievedEvent
+  try {
+    retrievedEvent = await stripe.events.retrieve(receivedEvent.id, requestOptions)
+  } catch (err) {
+    return new Response(err.message, { status: 400 })
+  }
+
+  return new Response(JSON.stringify(retrievedEvent), {
+    status: 200,
+    headers: { 'Content-Type': 'application/json' },
+  })
+})
+
+// To invoke:
+// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
+//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.625_WdcF3KHqz5amU0x2X5WWHP-OEs_4qj0ssLNHzTs' \
+//   --header 'Content-Type: application/json' \
+//   --data '{"name":"Functions"}'
