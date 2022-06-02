508f2d0000928b15af08b8c55dc00f833a8a1742
Isaiah Hamilton <70828596+Isaiah-Hamilton@users.noreply.github.com>
2022-06-02 00:15:18 -0400

    chore: ui update

--- apps/temp-docs/components/TabPanel.tsx
+++ apps/temp-docs/components/TabPanel.tsx
@@ -1,6 +1,6 @@
 import { ReactElement } from 'react'
 import { Tabs } from '@supabase/ui'
-export default function TabPanel(props) {
+export default function TabPanel(props: { id: string | undefined; label: string | undefined; children: any }) {
   console.log({ props })
   return (
     <Tabs.Panel id={props.id} label={props.label}>
