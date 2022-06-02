905cd470c44c51206164860de0250d1780b9dad5
thorwebdev <thor@supabase.io>
2022-06-02 13:10:09 +0800

    feat: add stripe webhook edge function example

--- examples/edge-functions/supabase/functions/stripe-webhooks/index.ts
+++ examples/edge-functions/supabase/functions/stripe-webhooks/index.ts
@@ -1,5 +1,3 @@
-// @ts-nocheck
-
 // Follow this setup guide to integrate the Deno language server with your editor:
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
@@ -7,7 +5,7 @@
 import { serve } from 'https://deno.land/std@0.131.0/http/server.ts'
 
 // esm.sh is used to compile stripe-node to be compatible with ES modules.
-import Stripe from 'https://esm.sh/stripe?target=deno&no-check'
+import Stripe from 'https://esm.sh/stripe@9.6.0?target=deno&no-check'
 
 const stripe = Stripe(Deno.env.get('STRIPE_API_KEY'), {
   // This is needed to use the Fetch API rather than relying on the Node http
@@ -37,6 +35,7 @@ serve(async (request) => {
   } catch (err) {
     return new Response(err.message, { status: 400 })
   }
+  console.log(`🔔 Event received: ${receivedEvent.id}`)
 
   // Secondly, we use this event to query the Stripe API in order to avoid
   // handling any forged event. If available, we use the idempotency key.
@@ -54,14 +53,8 @@ serve(async (request) => {
     return new Response(err.message, { status: 400 })
   }
 
-  return new Response(JSON.stringify(retrievedEvent), {
+  return new Response(JSON.stringify({ id: retrievedEvent.id, status: 'ok' }), {
     status: 200,
     headers: { 'Content-Type': 'application/json' },
   })
 })
-
-// To invoke:
-// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
-//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.625_WdcF3KHqz5amU0x2X5WWHP-OEs_4qj0ssLNHzTs' \
-//   --header 'Content-Type: application/json' \
-//   --data '{"name":"Functions"}'
