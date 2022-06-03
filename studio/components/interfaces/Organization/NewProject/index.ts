20a9fd0f13b09a4e0793a281fc879fcbd44bfcfa
Joshen Lim <joshenlimek@gmail.com>
2022-06-03 17:17:54 +0800

    refactor: bring out components from slug/new into their own files

---
+++ studio/components/interfaces/Organization/NewProject/index.ts
@@ -0,0 +1,11 @@
+import NotOrganizationOwnerWarning from './NotOrganizationOwnerWarning'
+import FreeProjectLimitWarning from './FreeProjectLimitWarning'
+import ProjectCreationDisabledWarning from './ProjectCreationDisabledWarning'
+import EmptyPaymentMethodWarning from './EmptyPaymentMethodWarning'
+
+export {
+  NotOrganizationOwnerWarning,
+  FreeProjectLimitWarning,
+  ProjectCreationDisabledWarning,
+  EmptyPaymentMethodWarning,
+}
