e3456ebd6d8a73df7c9d2d4b47416c8489ec3aa0
Aniket Singh <31aniket.singh@gmail.com>
2022-06-03 16:02:23 +0530

    added listView instead of column and await instead of then

--- examples/todo-list/flutter-todo-list/lib/main.dart
+++ examples/todo-list/flutter-todo-list/lib/main.dart
@@ -2,15 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:todosupabase/constant.dart';
-import 'package:todosupabase/functions/auth.dart';
 import 'package:todosupabase/screens/signup.dart';
 import 'package:todosupabase/screens/signin.dart';
 import 'package:todosupabase/screens/todo.dart';
 
 void main() async {
   await Supabase.initialize(
-      url: 'YOUR_SUPABASE_URL',
-      anonKey: 'YOUR_SUPABASE_KEY',
+    url: 'YOUR_SUPABASE_URL',
+    anonKey: 'YOUR_SUPABASE_KEY',
   );
   runApp(const MyApp());
 }
