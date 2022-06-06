aa5b4269b2bacea16eb6ed5555cfa5cd40e62ed3
Joshen Lim <joshenlimek@gmail.com>
2022-06-06 17:35:09 +0800

    Add incident banner

--- studio/components/layouts/ProjectLayout/ProductMenuBar.tsx
+++ studio/components/layouts/ProjectLayout/ProductMenuBar.tsx
@@ -1,5 +1,6 @@
 import { FC, ReactNode } from 'react'
 import { Typography } from '@supabase/ui'
+import { useFlag } from 'hooks'
 
 interface Props {
   title: string
@@ -7,21 +8,25 @@ interface Props {
 }
 
 const ProductMenuBar: FC<Props> = ({ title, children }) => {
+  const ongoingIncident = useFlag('ongoingIncident')
+  const maxHeight = ongoingIncident ? 'calc(100vh - 44px)' : '100vh'
+
   return (
     <div
+      style={{ height: maxHeight, maxHeight }}
       className={[
-        'w-64 h-screen flex flex-col hide-scrollbar border-r', // Layout
+        'hide-scrollbar flex w-64 flex-col border-r', // Layout
         'bg-sidebar-linkbar-light', // Light mode
         'dark:bg-sidebar-linkbar-dark dark:border-dark ', // Dark mode
       ].join(' ')}
     >
       <div
-        className="px-6 max-h-12 border-b dark:border-dark flex items-center"
+        className="dark:border-dark flex max-h-12 items-center border-b px-6"
         style={{ minHeight: '3rem' }}
       >
         <Typography.Title level={4}>{title}</Typography.Title>
       </div>
-      <div className="overflow-y-auto flex-grow">{children}</div>
+      <div className="flex-grow overflow-y-auto">{children}</div>
     </div>
   )
 }
