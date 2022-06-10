77cb743fac4f4f4b17d52d87d4cf0a471ab971f0
Joshen Lim <joshenlimek@gmail.com>
2022-06-10 17:42:42 +0800

    Fix all build errors on temp-docs except error name too long error

--- apps/temp-docs/components/Sponsor.tsx
+++ apps/temp-docs/components/Sponsor.tsx
@@ -2,10 +2,12 @@ import Image from 'next/image'
 export default function Sponsor({ imageUrl, handle }: { imageUrl: string; handle: string }) {
   return (
     <div className="my-4 flex h-14 w-44 flex-row items-center">
-      <img
+      <Image
+        className="mr-2 rounded-full"
         src={`https://github.com/${handle}.png`}
-        className="mr-2 w-10 rounded-full"
         alt={handle}
+        width={32}
+        height={32}
       />
       <h5>{handle}</h5>
     </div>
