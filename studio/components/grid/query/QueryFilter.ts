89b1ba1bd92458eef2dbb1d5e81fc6cbbea763b1
Joshen Lim <joshenlimek@gmail.com>
2022-06-14 15:44:47 +0800

    FIx prop errors

--- studio/components/grid/query/QueryFilter.ts
+++ studio/components/grid/query/QueryFilter.ts
@@ -1,23 +1,16 @@
-import { Dictionary, Filter, FilterOperator, QueryTable, Sort } from '../types';
-import { IQueryModifier, QueryModifier } from './QueryModifier';
+import { uuidv4 } from 'lib/helpers'
+import { Dictionary, Filter, FilterOperator, QueryTable, Sort } from '../types'
+import { IQueryModifier, QueryModifier } from './QueryModifier'
 
 export interface IQueryFilter {
-  filter: (
-    column: string,
-    operator: FilterOperator,
-    value: string
-  ) => IQueryFilter;
-  match: (criteria: Dictionary<any>) => IQueryFilter;
-  order: (
-    column: string,
-    ascending?: boolean,
-    nullsFirst?: boolean
-  ) => IQueryFilter;
+  filter: (column: string, operator: FilterOperator, value: string) => IQueryFilter
+  match: (criteria: Dictionary<any>) => IQueryFilter
+  order: (column: string, ascending?: boolean, nullsFirst?: boolean) => IQueryFilter
 }
 
 export class QueryFilter implements IQueryFilter, IQueryModifier {
-  protected filters: Filter[] = [];
-  protected sorts: Sort[] = [];
+  protected filters: Filter[] = []
+  protected sorts: Sort[] = []
 
   constructor(
     protected table: QueryTable,
@@ -27,15 +20,15 @@ export class QueryFilter implements IQueryFilter, IQueryModifier {
   ) {}
 
   filter(column: string, operator: FilterOperator, value: any) {
-    this.filters.push({ column, operator, value });
-    return this;
+    this.filters.push({ id: uuidv4(), column, operator, value })
+    return this
   }
 
   match(criteria: Dictionary<any>) {
     Object.entries(criteria).map(([column, value]) => {
-      this.filters.push({ column, operator: '=', value });
-    });
-    return this;
+      this.filters.push({ id: uuidv4(), column, operator: '=', value })
+    })
+    return this
   }
 
   order(column: string, ascending = true, nullsFirst = false) {
@@ -43,16 +36,16 @@ export class QueryFilter implements IQueryFilter, IQueryModifier {
       column: column,
       ascending,
       nullsFirst,
-    });
-    return this;
+    })
+    return this
   }
 
   range(from: number, to: number) {
-    return this._getQueryModifier().range(from, to);
+    return this._getQueryModifier().range(from, to)
   }
 
   toSql() {
-    return this._getQueryModifier().toSql();
+    return this._getQueryModifier().toSql()
   }
 
   _getQueryModifier() {
@@ -61,6 +54,6 @@ export class QueryFilter implements IQueryFilter, IQueryModifier {
       actionOptions: this.actionOptions,
       filters: this.filters,
       sorts: this.sorts,
-    });
+    })
   }
 }
