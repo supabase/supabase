e103768f9b360bb03071c72d078b21a01ea1f53e
Terry Sutton <saltcod@gmail.com>
2022-06-14 09:35:16 -0230

    Merge invite data with existing member data

--- studio/hooks/queries/useOrganizationDetail.ts
+++ studio/hooks/queries/useOrganizationDetail.ts
@@ -4,35 +4,34 @@ import { Member } from 'types'
 import { API_URL } from 'lib/constants'
 
 export function useOrganizationDetail(slug: string) {
+  // Get org members
   const url = `${API_URL}/props/org/${slug}`
   const { data, error } = useSWR<any>(url, get)
   let { members, products } = data ?? []
-  if (data) {
 
-    members = [...members,
-      {
-        id: 200,
-        is_owner: false,
-        profile: {
-          id: 98, username: 'supaterry', primary_email: 'terry@supabase.io', expired: true
-        }
-      },
-    {
-      id: 201,
-      is_owner: false,
-      profile: {
-        id: 99, username: 'timapple', primary_email: 'tim@apple.com', expired: true
-      }
-    },
-  ]
-}
-  const anyError = data?.error || error
+  // Get pending invite users
+  const pendingInviteUrl = `${API_URL}/organizations/${slug}/members/invite`
+  const {data: inviteData, error: inviteError} = useSWR<any>(pendingInviteUrl, get)
+
+
+  if (data && inviteData) {
+    // remap invite data to look like existing members data
+    const reMappedInvitedata = inviteData.map((x: any) => (
+      {is_owner: false, invited_at: x.invited_at, invited_id: x.invited_id, profile: {username: '', primary_email: x.invited_email}}
+    ))
+
+    members = [...members, ...reMappedInvitedata]
+  }
+
+  const anyError = data?.error || error || inviteError
 
   function mutateOrgMembers(updatedMembers: Member[], revalidate?: boolean) {
     mutate(url, { members: updatedMembers }, revalidate ?? true)
   }
 
-  return {
+  console.log('all members', members)
+
+return {
     members: members as Member[],
     products,
     isLoading: !anyError && !data,
