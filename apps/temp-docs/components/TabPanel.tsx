93b1c1260cb2f4252ae1d29fc1485bd8e7f79a6d
Isaiah Hamilton <70828596+Isaiah-Hamilton@users.noreply.github.com>
2022-06-15 00:06:12 -0400

    fix: build error

--- apps/temp-docs/components/TabPanel.tsx
+++ apps/temp-docs/components/TabPanel.tsx
@@ -1,7 +1,7 @@
 import { Tabs } from '@supabase/ui'
 export default function TabPanel(props: { id: string; label: string | undefined; children: any }) {
   return (
-    <Tabs.Panel id={props.id} label={props.label}>
+    <Tabs.Panel id={props?.id} label={props.label}>
       {props.children}
     </Tabs.Panel>
   )
