b2acec971c1c25ed3827cac5829b47ce5678f4c8
Terry Sutton <saltcod@gmail.com>
2022-06-15 08:08:32 -0230

    Pull profile info into join screen

--- studio/pages/org/[slug]/join.tsx
+++ studio/pages/org/[slug]/join.tsx
@@ -6,57 +6,91 @@ import { NextPageWithLayout } from 'types'
 import { toJS } from 'mobx'
 import { Button, Typography } from '@supabase/ui';
 import { useState } from 'react';
-import router, {useRouter} from 'next/router';
+import router, { useRouter } from 'next/router';
 import Link from 'next/link';
-
+import { API_URL } from 'lib/constants'
+import { get } from 'lib/common/fetch'
+import { useEffect } from 'react'
 
 const User = () => {
     const router = useRouter()
-    const { ui } = useStore()
+    const {email, slug, token} = router.query;
+    const { app, ui } = useStore()
     const user = ui.profile
     const [isSubmitting, setIsSubmitting] = useState( false )
-    console.log('router', router.query)
-    console.log('join-a user:', toJS(user))
+    // console.log( 'router', router.query )
+    // console.log( 'join-a user:', toJS( user ) )
+    console.log( 'ui org slug:', toJS( ui.selectedOrganization?.slug ) )
 
-  return (
-    <div className="flex h-full min-h-screen w-full flex-col place-items-center items-center justify-center bg-secondary px-3">
-        <div className="mt-16 space-y-4 p-12 max-w-md mx-auto border-2 rounded-md ">
-            <div className="space-y-4">
-            <Link href="/">
-                <a className="flex items-center gap-4">
-                    <img
-                    src="/img/supabase-logo.svg"
-                    alt="Supabase"
-                    className="h-[30px] block cursor-pointer rounded"
-                    />
-                <Typography.Title level={3} className="mb-0">Supabase</Typography.Title>
+    //console.log('org name: ', toJS(app.organizations.data['91']))
+    //console.log('org name: ', toJS(app.organizations.list()))
 
-            </a>
-        </Link>
+    // const organizationsLinks = app.organizations
+    // .list()
+    // .map((x: any) => ({
+    //   isActive: router.pathname.startsWith('/org/') && ui.selectedOrganization?.slug == x.slug,
+    //   label: x.name,
+    //   href: `/org/${x.slug}/settings`,
+    // }))
 
-                <h2 className="text-xl">Join organization</h2>
-                <p className="text-md">We sent an invite code to <u>your@email.com</u> to join this organization.</p>
-            </div>
 
-            <div className='flex items-center gap-2 mt-8'>
-                <Button htmlType="submit" loading={isSubmitting} size="small">
-                    Join this organization
-                </Button>
+    async function handleSubmitForm() {
+        setIsSubmitting( true )
 
-                <Button htmlType="submit" loading={isSubmitting} size="small" type="text">
-                    Decline
-                </Button>
+        // curl -X GET 'http://localhost:8080/platform/organizations/slug/members/join?token=XXX'
+        const response = await get( `${API_URL}/organizations/${slug}/members/join?token=${token}`, {} )
+
+        if ( response.error ) {
+            ui.setNotification( {
+                category: 'error',
+                message: `Failed to join organization: ${response.error.message}`,
+            } )
+            setIsSubmitting( false )
+        } else {
+            setIsSubmitting( false )
+            router.push( '/' )
+        }
+    }
+
+    return (
+        <div className="flex flex-col items-center justify-center w-full h-full min-h-screen px-3 place-items-center bg-secondary">
+            <div className="max-w-md p-12 mx-auto mt-16 space-y-4 border-2 rounded-md ">
+                <div className="space-y-4">
+                    <Link href="/">
+                        <a className="flex items-center gap-4">
+                            <img
+                                src="/img/supabase-logo.svg"
+                                alt="Supabase"
+                                className="h-[30px] block cursor-pointer rounded"
+                            />
+                            <Typography.Title level={3} className="mb-0">Supabase</Typography.Title>
+
+                        </a>
+                    </Link>
+
+                    <h2 className="text-xl">Join ORG NAME</h2>
+                    <p className="text-md">We sent an invite code to <u>{email}</u> to join this organization.</p>
+                </div>
+
+                {/* Check that the user in the query param matches the current logged in users email */}
+                {user && user?.primary_email == email && (
+                <div className='flex items-center gap-2 mt-8'>
+                    <Button onClick={handleSubmitForm} htmlType="submit" loading={isSubmitting} size="small">
+                        Join this organization
+                    </Button>
+
+                    <Button htmlType="submit" loading={isSubmitting} size="small" type="text">
+                        Decline
+                    </Button>
+                </div>
+                )}
+
+                {user?.primary_email !== email && (
+                    <div className='pt-4 mt-4 border-t'>Sign to that account to accept this invitation.</div>
+                )}
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
-
-export default withAuth(observer(User))
-
-
-
-
-
-
+export default withAuth( observer( User ) )
