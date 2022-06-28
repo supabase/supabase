a546e85b419a2d5bb9102345342689fb250ed51a
Copple <10214025+kiwicopple@users.noreply.github.com>
2022-06-28 15:07:31 -0500

    Fix the link structure

--- apps/temp-docs/components/nav/SideBar.tsx
+++ apps/temp-docs/components/nav/SideBar.tsx
@@ -7,49 +7,37 @@ const SideBar = ({ menuItems }: { menuItems: any }) => {
   return (
     <div className="dark:bg-scale-200 dark:border-scale-400 thin-scrollbar sidebar-width sticky top-0 flex h-screen overflow-y-scroll border-r py-4">
       <Menu className="w-full">
-        <div>
-          {Object.keys(menuItems).map((key) => {
-            return (
-              <div key={key} className="mb-4">
-                <Menu.Group title={key} />
-                {menuItems[key].map(
-                  (item: { link: string; text: string; sections: Array<any> }) => (
-                    <span key={item.link}>
-                      {Array.isArray(item.sections) ? (
-                        <>
-                          <Menu.Group title={item.text} />
-                          {item.sections.map((section) => (
-                            <Link href={section.link} key={`${item.text}-${section.text}`}>
-                              <a>
-                                <Menu.Item active={asPath == section.link}>
-                                  {section.text}
-                                </Menu.Item>
-                              </a>
-                            </Link>
-                          ))}
-                        </>
-                      ) : (
-                        <Link href={item.link}>
-                          <a>
-                            <Menu.Item active={asPath === item.link}>
-                              <div
-                                className={
-                                  asPath === item.link ? 'text-brand-900' : 'text-scale-1000'
-                                }
-                              >
-                                {item.text}
-                              </div>
-                            </Menu.Item>
-                          </a>
+        {Object.keys(menuItems).map((key) => {
+          return (
+            <div key={key} className="mb-4">
+              <Menu.Group title={key} />
+              {menuItems[key].map((item: { link: string; text: string; sections: Array<any> }) => (
+                <span key={item.link}>
+                  {Array.isArray(item.sections) ? (
+                    <>
+                      <Menu.Group title={item.text} />
+                      {item.sections.map((section) => (
+                        <Menu.Item active={asPath == section.link}>
+                          <Link passHref href={section.link} key={`${item.text}-${section.text}`}>
+                            {section.text}
+                          </Link>
+                        </Menu.Item>
+                      ))}
+                    </>
+                  ) : (
+                    <Menu.Item active={asPath === item.link}>
+                      <div className={asPath === item.link ? 'text-brand-900' : 'text-scale-1000'}>
+                        <Link passHref href={item.link}>
+                          {item.text}
                         </Link>
-                      )}
-                    </span>
-                  )
-                )}
-              </div>
-            )
-          })}
-        </div>
+                      </div>
+                    </Menu.Item>
+                  )}
+                </span>
+              ))}
+            </div>
+          )
+        })}
       </Menu>
     </div>
   )
