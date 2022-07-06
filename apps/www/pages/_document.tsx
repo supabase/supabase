2244c56539ee4e92eda6713b062331bc1d42080b
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-07-06 21:17:16 +1000

    Update _document.tsx

--- apps/www/pages/_document.tsx
+++ apps/www/pages/_document.tsx
@@ -1,15 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-// import { useEffect } from 'react'
 
 export default class MyDocument extends Document {
   render() {
-    // useEffect(() => {
-    //   const s = document.createElement('script')
-    //   s.setAttribute('src', 'https://platform.twitter.com/widgets.js')
-    //   s.setAttribute('async', 'true')
-    //   document.head.appendChild(s)
-    // }, [])
-
     return (
       <Html lang="en" className="dark">
         <Head>
