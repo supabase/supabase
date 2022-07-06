a08357e6e188879dc0d4a45beb21466e00a3c540
Joshen Lim <joshenlimek@gmail.com>
2022-07-06 14:00:41 +0800

    Add unit test

--- studio/components/to-be-cleaned/Storage/StorageSettings/StorageSettings.utils.ts
+++ studio/components/to-be-cleaned/Storage/StorageSettings/StorageSettings.utils.ts
@@ -2,18 +2,20 @@ import { StorageSizeUnits } from './StorageSettings.constants'
 
 const k = 1024
 
-// [JOSHEN TODO] Write tests for this
 export const convertFromBytes = (bytes: number) => {
   // Up to GB since that's our storage upload limit
-  if (bytes === 0) return { value: 0, unit: StorageSizeUnits.BYTES }
+  if (bytes <= 0) return { value: 0, unit: StorageSizeUnits.BYTES }
 
   const i = Math.floor(Math.log(bytes) / Math.log(k))
-  const unit = Object.values(StorageSizeUnits)[i]
-  const value = bytes / Math.pow(k, i)
+  const formattedIdx = i > 3 ? 3 : i
+  const unit = Object.values(StorageSizeUnits)[formattedIdx]
+  const value = bytes / Math.pow(k, formattedIdx)
   return { value, unit }
 }
 
-export const convertToBytes = (size: number, unit: StorageSizeUnits) => {
+export const convertToBytes = (size: number, unit: StorageSizeUnits = StorageSizeUnits.BYTES) => {
   const i = Object.values(StorageSizeUnits).indexOf(unit)
+  if (size < 0 || i < 0) return 0
+
   return size * Math.pow(k, i)
 }
