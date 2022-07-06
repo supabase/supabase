aaa9f41255c51889f825a06298dbab07e92a9c5a
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-07-06 21:17:01 +1000

    fix: embed tweet

--- apps/www/pages/_document.tsx
+++ apps/www/pages/_document.tsx
@@ -1,10 +1,20 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+// import { useEffect } from 'react'
 
 export default class MyDocument extends Document {
   render() {
+    // useEffect(() => {
+    //   const s = document.createElement('script')
+    //   s.setAttribute('src', 'https://platform.twitter.com/widgets.js')
+    //   s.setAttribute('async', 'true')
+    //   document.head.appendChild(s)
+    // }, [])
+
     return (
       <Html lang="en" className="dark">
-        <Head></Head>
+        <Head>
+          <script async src="https://platform.twitter.com/widgets.js" />
+        </Head>
         <body>
           <Main />
           <NextScript />
