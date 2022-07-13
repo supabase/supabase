f03c855543c074f39456040706e586b0b0fa0d78
thorwebdev <thor@supabase.io>
2022-07-13 23:39:44 +0800

    feat: move to playwright-chromium.

--- apps/www/lib/launchweek/screenshot.ts
+++ apps/www/lib/launchweek/screenshot.ts
@@ -14,28 +14,13 @@
  * limitations under the License.
  */
 
-import chrome from 'chrome-aws-lambda';
-import puppeteer from 'puppeteer-core';
+import playwright from 'playwright-chromium'
 
 export default async function screenshot(url: string) {
-  const options = process.env.AWS_REGION
-    ? {
-        args: chrome.args,
-        executablePath: await chrome.executablePath,
-        headless: chrome.headless
-      }
-    : {
-        args: [],
-        executablePath:
-          process.platform === 'win32'
-            ? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
-            : process.platform === 'linux'
-            ? '/usr/bin/google-chrome'
-            : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-      };
-  const browser = await puppeteer.launch(options);
-  const page = await browser.newPage();
-  await page.setViewport({ width: 2000, height: 1000 });
-  await page.goto(url, { waitUntil: 'networkidle0' });
-  return await page.screenshot({ type: 'png' });
+  const browser = await playwright.chromium.launch()
+  const context = await browser.newContext()
+  const page = await context.newPage()
+  page.setViewportSize({ width: 2000, height: 1000 })
+  await page.goto(url, { waitUntil: 'networkidle' })
+  return await page.screenshot({ type: 'png' })
 }
