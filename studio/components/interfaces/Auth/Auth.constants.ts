1761907b4ed6ecfb5e76a068b99598bf8cff497e
Stojan Dimitrovski <sdimitrovski@gmail.com>
2022-07-14 14:35:08 +0200

    feat: Add support for iOS deep-links in redirect URLs

--- studio/components/interfaces/Auth/Auth.constants.ts
+++ studio/components/interfaces/Auth/Auth.constants.ts
@@ -8,7 +8,12 @@
 //  "uptime-monitor-fe.vercel.app"
 //  "https://uptime-monitor-fe.vercel.app/"
 export const domainRegexStrict =
-  /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/gm
+  /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/m
 
 // [Joshen] This regex allows for localhost as well, less strict
-export const domainRegex = /^(ftp|http|https):\/\/\w+(\.\w+)*(:[0-9]+)?\/?(\/[.\w]*)*$/g
+export const webRegex = /^(ftp|http|https):\/\/\w+(\.\w+)*(:[0-9]+)?\/?(\/[.\w]*)*$/i
+
+// iOS deep linking scheme https://benoitpasquier.com/deep-linking-url-scheme-ios/
+export const appRegex = /^[a-z0-9]+([.][a-z0-9]+)*:\/(\/[-a-z0-9._~!$&'()*+,;=:@%]+)+$/i
+
+export const domainRegex = new RegExp(`(${webRegex.source})|(${appRegex.source})`, 'i')
