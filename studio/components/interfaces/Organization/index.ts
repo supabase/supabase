0479c5232492bf66137f33dc17823b7322094de2
Joshen Lim <joshenlimek@gmail.com>
2022-07-14 22:25:15 +0800

    Refactor OwnerDropdown into its own component

--- studio/components/interfaces/Organization/index.ts
+++ studio/components/interfaces/Organization/index.ts
@@ -2,5 +2,12 @@ import BillingSettings from './BillingSettings/BillingSettings'
 import InvoicesSettings from './InvoicesSettings/InvoicesSettings'
 import InviteMemberModal from './InviteMemberModal'
 import DeleteOrganizationButton from './DeleteOrganizationButton'
+import OwnerDropdown from './OwnerDropdown'
 
-export { BillingSettings, InvoicesSettings, InviteMemberModal, DeleteOrganizationButton }
+export {
+  BillingSettings,
+  InvoicesSettings,
+  InviteMemberModal,
+  DeleteOrganizationButton,
+  OwnerDropdown,
+}
