13838117d8ae75fba0b003335d6080f3e01846d3
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-07-15 11:57:22 +1000

    Update Auth.constants.ts

--- studio/components/interfaces/Auth/Auth.constants.ts
+++ studio/components/interfaces/Auth/Auth.constants.ts
@@ -1,6 +1,8 @@
 // [Joshen] Currently using the latter regex as the former is too strict, doesn't support
 // localhost - if we can combine these 2 together that'll be great but my regex-foo isn't too strong
 
+// [MildTomato] Combined everything into domainRegex for now
+
 // Regex from: https://stackoverflow.com/a/68002755/4807782
 // modified to accept numbers in the body of domain though
 // examples of matches:
@@ -12,7 +14,8 @@ const baseDomainRegex =
   /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/gm
 
 // [Joshen] This regex allows for localhost as well, less strict
-// export const webRegex = /^(ftp|http|https):\/\/\w+(\.\w+)*(:[0-9]+)?\/?(\/[.\w]*)*$/i
+// [MildTomato] no longer used
+const webRegex = /^(ftp|http|https):\/\/\w+(\.\w+)*(:[0-9]+)?\/?(\/[.\w]*)*$/i
 
 // iOS deep linking scheme https://benoitpasquier.com/deep-linking-url-scheme-ios/
 const appRegex = /^[a-z0-9]+([.][a-z0-9]+)*:\/(\/[-a-z0-9._~!$&'()*+,;=:@%]+)+$/i
@@ -21,7 +24,6 @@ const appRegex = /^[a-z0-9]+([.][a-z0-9]+)*:\/(\/[-a-z0-9._~!$&'()*+,;=:@%]+)+$/
 const localhostRegex = /^(?:^|\s)((https?:\/\/)?(?:localhost|[\w-]+(?:\.[\w-]+)+)(:\d+)?(\/\S*)?)/i
 
 // combine the above regexes
-// export const domainRegex = new RegExp(`(${webRegex.source})|(${appRegex.source})`, 'i')
 export const domainRegex = new RegExp(
   `(${baseDomainRegex.source})|(${localhostRegex.source})|(${appRegex.source})`,
   'i'
