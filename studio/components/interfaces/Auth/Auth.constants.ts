9561a7f0ddca1e5ce77f0dfee15e897d47993897
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-07-15 11:39:42 +1000

    fix: strict regex check now also accepting numbers

--- studio/components/interfaces/Auth/Auth.constants.ts
+++ studio/components/interfaces/Auth/Auth.constants.ts
@@ -2,18 +2,21 @@
 // localhost - if we can combine these 2 together that'll be great but my regex-foo isn't too strong
 
 // Regex from: https://stackoverflow.com/a/68002755/4807782
+// modified to accept numbers in the body of domain though
 // examples of matches:
 //  "vercel.com"
 //  "www.vercel.com"
 //  "uptime-monitor-fe.vercel.app"
 //  "https://uptime-monitor-fe.vercel.app/"
 export const domainRegexStrict =
-  /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/m
+  /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/gm
 
 // [Joshen] This regex allows for localhost as well, less strict
 export const webRegex = /^(ftp|http|https):\/\/\w+(\.\w+)*(:[0-9]+)?\/?(\/[.\w]*)*$/i
 
 // iOS deep linking scheme https://benoitpasquier.com/deep-linking-url-scheme-ios/
 export const appRegex = /^[a-z0-9]+([.][a-z0-9]+)*:\/(\/[-a-z0-9._~!$&'()*+,;=:@%]+)+$/i
+
+// combine the above regexes
 
 export const domainRegex = new RegExp(`(${webRegex.source})|(${appRegex.source})`, 'i')
