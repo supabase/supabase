9c5e98d9759663f771215def8d721aa35115455a
Ollie Halliday <22655069+Hallidayo@users.noreply.github.com>
2022-07-16 10:28:54 +0100

    opacity and text size changes

--- apps/www/pages/404.tsx
+++ apps/www/pages/404.tsx
@@ -41,7 +41,7 @@ const Error404 = () => {
         </div>
         <div className="absolute">
           <h1
-            className={`text-scale-1200 select-none text-9xl opacity-[5%] filter transition duration-200 ${
+            className={`text-scale-1200 select-none text-[12rem] opacity-[15%] filter transition duration-200 sm:text-[16rem] ${
               show404 ? 'blur-sm' : 'blur-none'
             }`}
           >
