5b6eba2934f30cea58af6a47373f111e244f474c
Joshen Lim <joshenlimek@gmail.com>
2022-07-21 13:26:30 +0800

    Fix invited logic

--- studio/components/interfaces/Organization/Organization.utils.ts
+++ studio/components/interfaces/Organization/Organization.utils.ts
@@ -2,7 +2,7 @@
 export function isInviteExpired(timestamp: Date) {
   const inviteDate = new Date(timestamp)
   const now = new Date()
-  var timeBetween = now.valueOf() - inviteDate.valueOf()
+  const timeBetween = now.valueOf() - inviteDate.valueOf()
   if (timeBetween / 1000 / 60 / 60 < 24) {
     return false
   }
