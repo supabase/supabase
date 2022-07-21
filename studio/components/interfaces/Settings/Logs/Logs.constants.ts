6b83b953a404cbd91c881f254ec79927e6ef4841
TzeYiing <ty@tzeyiing.com>
2022-07-21 19:09:05 +1000

    fix: switches the default previewer datetime range to 1hr instead of 1 day.

--- studio/components/interfaces/Settings/Logs/Logs.constants.ts
+++ studio/components/interfaces/Settings/Logs/Logs.constants.ts
@@ -435,6 +435,7 @@ export const PREVIEWER_DATEPICKER_HELPERS: DatetimeHelper[] = [
     text: 'Last hour',
     calcFrom: () => dayjs().subtract(1, 'hour').startOf('hour').toISOString(),
     calcTo: () => '',
+    default: true,
   },
   {
     text: 'Last 3 hours',
@@ -445,7 +446,6 @@ export const PREVIEWER_DATEPICKER_HELPERS: DatetimeHelper[] = [
     text: 'Last day',
     calcFrom: () => dayjs().subtract(1, 'day').startOf('day').toISOString(),
     calcTo: () => '',
-    default: true,
   },
 ]
 export const EXPLORER_DATEPICKER_HELPERS: DatetimeHelper[] = [
