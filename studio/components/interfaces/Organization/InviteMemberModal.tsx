f58c5ee79d12be174d91f864bc04968e48c136d8
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-07-21 10:43:49 +0800

    chore: update invite modal

--- studio/components/interfaces/Organization/InviteMemberModal.tsx
+++ studio/components/interfaces/Organization/InviteMemberModal.tsx
@@ -1,5 +1,4 @@
-import { Button, Form, IconMail, Input, Listbox, Modal } from '@supabase/ui'
-import { createConsoleLogger } from 'configcat-js'
+import { Button, Form, IconMail, Input, Modal, Select } from '@supabase/ui'
 import { useOrganizationDetail, useStore } from 'hooks'
 import { get, post } from 'lib/common/fetch'
 import { API_URL } from 'lib/constants'
@@ -12,131 +11,122 @@ import { Member } from 'types'
 import { object, string } from 'yup'
 
 function InviteMemberModal({ organization, members = [], user }: any) {
-  const initialValues = { email: '' }
+  const [isOpen, setIsOpen] = useState(false)
+  const [loading, setLoading] = useState(false)
+
   const { ui } = useStore()
+
   const router = useRouter()
   const { slug } = router.query
+
   const { mutateOrgMembers } = useOrganizationDetail(ui.selectedOrganization?.slug || '')
-  const [isOpen, setIsOpen] = useState(false)
-  const [emailAddress, setEmailAddress] = useState('')
-  const [memberList, setMemberList] = useState<Member[]>([])
-  const [addMemberLoading, setAddMemberLoading] = useState(false)
 
-  const { slug: orgSlug } = organization
+  // to do: move to a store @joshenlim
+  const { data: roles, error: rolesError } = useSWR(`${API_URL}/organizations/${slug}/roles`, get)
 
-  // useEffect(() => {
-  //   setMemberList(members)
-  // }, [members])
-
-  function toggle() {
-    // reset data before showing modal again
-    if (!isOpen) {
-      setAddMemberLoading(false)
-      setEmailAddress('')
-    }
-    setIsOpen(!isOpen)
-  }
-
-  async function addMember() {
-    setAddMemberLoading(true)
-
-    const response = await post(`${API_URL}/organizations/${orgSlug}/members/invite`, {
-      invited_email: emailAddress.toLowerCase(),
-      owner_id: toJS(user.id),
-    })
-
-    if (isNil(response)) {
-      ui.setNotification({ category: 'error', message: 'Failed to add member' })
-    } else if (response?.error) {
-      ui.setNotification({
-        category: 'error',
-        message: `Failed to add member: ${response.error.message}`,
-      })
-      setAddMemberLoading(false)
-    } else {
-      const newMember: Member = {
-        // [Joshen] Setting a random id for now to fit the Member interface
-        id: 0,
-        invited_at: response.invited_at,
-        primary_email: response.invited_email,
-        username: response.invited_email[0],
-      }
-      console.log('members for addMember', members)
-      console.log('members being sent to mututa', [...toJS(members), newMember])
-      mutateOrgMembers([...members, newMember])
-
-      ui.setNotification({
-        category: 'success',
-        message: 'Successfully added new member.',
-      })
-      toggle()
-    }
-  }
-
-  function onEmailInputChange(e: any) {
-    setEmailAddress(e.target.value)
-  }
+  const initialValues = { email: '', role: '' }
 
   const schema = object({
     email: string().email('Must be a valid email address').required('Email is required'),
+    role: string().required('Role is required'),
   })
-
-  const { data: roles, error: rolesError } = useSWR(`${API_URL}/organizations/${slug}/roles`, get)
 
   return (
     <>
-      <Button onClick={toggle}>Invite</Button>
+      <Button onClick={() => setIsOpen(true)}>Invite</Button>
       <Modal
         size="small"
         className="!overflow-visible"
         visible={isOpen}
-        onCancel={toggle}
+        onCancel={() => setIsOpen(false)}
         header="Invite a member to this organization"
         description="Members you'd like to invite must already be registered on Supabase"
         layout="vertical"
         hideFooter
       >
         <Modal.Content>
-          <Form validationSchema={schema} initialValues={initialValues} onSubmit={addMember}>
-            {() => (
-              <div className="w-full py-4">
-                <div className="space-y-4">
-                  {roles && (
-                    <Listbox
-                      label="Member role"
-                      value={roles[0].id}
-                      // onChange={(roleId) => {
-                      //   setUserRoleChangeModalVisible(true)
-                      //   setSelectedUser({
-                      //     ...x,
-                      //     oldRoleId: activeRoleId,
-                      //     newRoleId: roleId,
-                      //   })
-                      // }}
-                    >
-                      {roles.map((role: any) => (
-                        <Listbox.Option key={role.id} value={role.id} label={role.name}>
-                          {role.name}
-                        </Listbox.Option>
-                      ))}
-                    </Listbox>
-                  )}
+          <Form
+            validationSchema={schema}
+            initialValues={initialValues}
+            onSubmit={async (values: any, resetForm: any) => {
+              setLoading(true)
 
-                  <Input
-                    id="email"
-                    icon={<IconMail />}
-                    autoFocus
-                    placeholder="Enter email address"
-                    onChange={onEmailInputChange}
-                    label="Email address"
-                  />
+              const response = await post(`${API_URL}/organizations/${slug}/members/invite`, {
+                invited_email: values.email.toLowerCase(),
+                owner_id: toJS(user.id),
+                role: values.role,
+              })
 
-                  <Button block size="medium" htmlType="submit" loading={addMemberLoading}>
-                    Invite new member
-                  </Button>
+              if (isNil(response)) {
+                ui.setNotification({ category: 'error', message: 'Failed to add member' })
+              } else if (response?.error) {
+                ui.setNotification({
+                  category: 'error',
+                  message: `Failed to add member: ${response.error.message}`,
+                })
+                setLoading(false)
+              } else {
+                const newMember: Member = {
+                  // [Joshen] Setting a random id for now to fit the Member interface
+                  id: 0,
+                  invited_at: response.invited_at,
+                  primary_email: response.invited_email,
+                  username: response.invited_email[0],
+                }
+                // console.log('members for addMember', members)
+                // console.log('members being sent to mututa', [...toJS(members), newMember])
+                mutateOrgMembers([...members, newMember])
+
+                ui.setNotification({
+                  category: 'success',
+                  message: 'Successfully added new member.',
+                })
+                setIsOpen(!isOpen)
+                resetForm({ initialValues: { ...initialValues, role: roles[0].id } })
+              }
+            }}
+          >
+            {({ resetForm }: any) => {
+              /**
+               * catches 'roles' when its available and then adds a default value for role select
+               */
+              useEffect(() => {
+                if (roles) {
+                  resetForm({
+                    values: { ...initialValues, role: roles[0].id },
+                    initialValues: { ...initialValues, role: roles[0].id },
+                  })
+                }
+              }, [roles])
+
+              return (
+                <div className="w-full py-4">
+                  <div className="space-y-4">
+                    {roles && (
+                      <Select name="role" label="Member role">
+                        {roles.map((role: any) => (
+                          <Select.Option key={role.id} value={role.id}>
+                            {role.name}
+                          </Select.Option>
+                        ))}
+                      </Select>
+                    )}
+
+                    <Input
+                      id="email"
+                      icon={<IconMail />}
+                      autoFocus
+                      placeholder="Enter email address"
+                      label="Email address"
+                    />
+
+                    <Button block size="medium" htmlType="submit" loading={loading}>
+                      Invite new member
+                    </Button>
+                  </div>
                 </div>
-              </div>
-            )}
+              )
+            }}
           </Form>
         </Modal.Content>
       </Modal>
