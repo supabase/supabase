11ad9c203d865df69ca0f97024bc628b74be5755
Joshen Lim <joshenlimek@gmail.com>
2022-07-25 11:41:45 +0800

    Implement ABAC for project deletion

---
+++ studio/components/interfaces/Settings/General/DeleteProjectPanel/DeleteProjectPanel.tsx
@@ -0,0 +1,39 @@
+import { FC } from 'react'
+import { Alert } from '@supabase/ui'
+
+import { useStore } from 'hooks'
+import Panel from 'components/ui/Panel'
+
+import DeleteProjectButton from './DeleteProjectButton'
+
+interface Props {}
+
+const DeleteProjectPanel: FC<Props> = ({}) => {
+  const { ui } = useStore()
+  const project = ui.selectedProject
+
+  if (project === undefined) return <></>
+
+  return (
+    <section>
+      <Panel title={<p className="uppercase">Danger Zone</p>}>
+        <Panel.Content>
+          <Alert
+            variant="danger"
+            withIcon
+            title="Deleting this project will also remove your database."
+          >
+            <div className="flex flex-col">
+              <p className="mb-4 block">
+                Make sure you have made a backup if you want to keep your data.
+              </p>
+              <DeleteProjectButton />
+            </div>
+          </Alert>
+        </Panel.Content>
+      </Panel>
+    </section>
+  )
+}
+
+export default DeleteProjectPanel
