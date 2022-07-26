3d4b9a492cbe3b3110a796f0417595609d4efb20
Joshen Lim <joshenlimek@gmail.com>
2022-07-26 16:12:32 +0800

    Fix check in SpreadsheetImport utils

--- studio/components/interfaces/TableGridEditor/SidePanelEditor/TableEditor/SpreadsheetImport/SpreadsheetImport.utils.ts
+++ studio/components/interfaces/TableGridEditor/SidePanelEditor/TableEditor/SpreadsheetImport/SpreadsheetImport.utils.ts
@@ -123,7 +123,7 @@ export const inferColumnType = (column: string, rows: object[]) => {
   // Infer boolean type
   if (includes(['true', 'false'], columnData.toString().toLowerCase())) {
     const isAllBoolean = columnDataAcrossRows.every((item: any) => {
-      if (item === null) return true
+      if (item === null || item === undefined) return true
       else return includes(['true', 'false'], item.toString().toLowerCase())
     })
     if (isAllBoolean) {
