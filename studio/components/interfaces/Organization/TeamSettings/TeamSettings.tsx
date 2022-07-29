fa5615c3e91625d06065928d099201e3edc108e5
Joshen Lim <joshenlimek@gmail.com>
2022-07-29 14:25:35 +0800

    Implement ABAC in greater granularity for roles management in org team members

--- studio/components/interfaces/Organization/TeamSettings/TeamSettings.tsx
+++ studio/components/interfaces/Organization/TeamSettings/TeamSettings.tsx
@@ -2,27 +2,27 @@ import { useState, useContext } from 'react'
 import { observer } from 'mobx-react-lite'
 import { Button, Input, IconSearch } from '@supabase/ui'
 
-import { checkPermissions, useFlag, useStore } from 'hooks'
+import { useFlag, useStore } from 'hooks'
 import { post } from 'lib/common/fetch'
 import { API_URL } from 'lib/constants'
-import { confirmAlert } from 'components/to-be-cleaned/ModalsDeprecated/ConfirmModal'
-import InviteMemberModal from './InviteMemberModal'
+import InviteMemberModal from './InviteMemberButton'
 import MembersView from './MembersView'
+import { confirmAlert } from 'components/to-be-cleaned/ModalsDeprecated/ConfirmModal'
 
 import { PageContext } from 'pages/org/[slug]/settings'
-import { PermissionAction } from '@supabase/shared-types/out/constants'
 
 const TeamSettings = observer(() => {
   const PageState: any = useContext(PageContext)
-  const { ui } = useStore()
-  const [isLeaving, setIsLeaving] = useState(false)
-  const enablePermissions = useFlag('enablePermissions')
+  const { user, members, roles, rolesAddable } = PageState
 
-  const slug = ui.selectedOrganization?.slug
+  const { ui } = useStore()
+  const slug = ui.selectedOrganization?.slug ?? ''
   const isOrgOwner = ui.selectedOrganization?.is_owner
-  const canAddMembers = enablePermissions
-    ? checkPermissions(PermissionAction.SQL_INSERT, 'postgres.auth.subject_roles')
-    : isOrgOwner
+
+  const enablePermissions = useFlag('enablePermissions')
+  const [isLeaving, setIsLeaving] = useState(false)
+
+  const canAddMembers = enablePermissions ? rolesAddable.length > 0 : isOrgOwner
 
   function onFilterMemberChange(e: any) {
     PageState.membersFilterString = e.target.value
@@ -69,7 +69,12 @@ const TeamSettings = observer(() => {
           <div className="flex items-center space-x-4">
             {canAddMembers && (
               <div>
-                <InviteMemberModal members={PageState.members} user={PageState.user} />
+                <InviteMemberModal
+                  user={user}
+                  members={members}
+                  roles={roles}
+                  rolesAddable={rolesAddable}
+                />
               </div>
             )}
             {!isOrgOwner && (
