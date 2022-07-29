e349c639870d9287837349c9472e77ada65f256a
Joshen Lim <joshenlimek@gmail.com>
2022-07-29 14:47:32 +0800

    Fix logic for getRolesManagementPermission

--- studio/components/interfaces/Organization/TeamSettings/TeamSettings.utils.ts
+++ studio/components/interfaces/Organization/TeamSettings/TeamSettings.utils.ts
@@ -11,39 +11,20 @@ export const getRolesManagementPermissions = (
 
   const availableRoles = ['Owner', 'Administrator', 'Developer']
 
-  // [Joshen TODO] Hardcoded logic until BE is ready
   availableRoles.forEach((role: string) => {
     const selectedRole = roles.find((r) => r.name === role)
     if (!selectedRole) return
 
-    // This is if the logged in user is an Owner
-    rolesAddable.push(selectedRole.id)
-    rolesRemovable.push(selectedRole.id)
+    const canAdd = checkPermissions(PermissionAction.SQL_INSERT, 'postgres.auth.subject_roles', {
+      resource: { role_id: selectedRole!.id },
+    })
+    if (canAdd) rolesAddable.push(selectedRole.id)
 
-    // This is if the logged in user is an Admin
-    // if (role !== 'Owner') {
-    //   rolesAddable.push(selectedRole.id)
-    //   rolesRemovable.push(selectedRole.id)
-    // }
-
-    // Just comment out everything if doing for developer
+    const canRemove = checkPermissions(PermissionAction.SQL_DELETE, 'postgres.auth.subject_roles', {
+      resource: { role_id: selectedRole!.id },
+    })
+    if (canRemove) rolesRemovable.push(selectedRole.id)
   })
-
-  // [Joshen TODO] This is the correct logic but BE is not ready
-  // availableRoles.forEach((role: string) => {
-  //   const selectedRole = roles.find((r) => r.name === role)
-  //   if (!selectedRole) return
-
-  //   const canAdd = checkPermissions(PermissionAction.SQL_INSERT, 'postgres.auth.subject_roles', {
-  //     resource: { role_id: selectedRole!.id },
-  //   })
-  //   if (canAdd) rolesAddable.push(selectedRole.id)
-
-  //   const canRemove = checkPermissions(PermissionAction.SQL_DELETE, 'postgres.auth.subject_roles', {
-  //     resource: { role_id: selectedRole!.id },
-  //   })
-  //   if (canRemove) rolesRemovable.push(selectedRole.id)
-  // })
 
   return { rolesAddable, rolesRemovable }
 }
