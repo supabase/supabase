a061b22b4f58576e3d24490df74b883879efe3ba
Joshen Lim <joshenlimek@gmail.com>
2022-08-05 12:33:36 +0800

    First iteration

--- studio/components/interfaces/Home/index.ts
+++ studio/components/interfaces/Home/index.ts
@@ -2,5 +2,6 @@ import ClientLibrary from './ClientLibrary'
 import ExampleProject from './ExampleProject'
 import ProjectUsage from './ProjectUsage'
 import NewProjectPanel from './NewProjectPanel/NewProjectPanel'
+import OveragesBanner from './OveragesBanner'
 
-export { ClientLibrary, ExampleProject, ProjectUsage, NewProjectPanel }
+export { ClientLibrary, ExampleProject, ProjectUsage, NewProjectPanel, OveragesBanner }
--- studio/hooks/queries/useProjectUsageStatus.ts
+++ studio/hooks/queries/useProjectUsageStatus.ts
@@ -2,13 +2,13 @@ import useSWR from 'swr'
 import { get } from 'lib/common/fetch'
 import { API_URL } from 'lib/constants'
 
-export function useProjectUsageStatus(ref: string) {
+export function useProjectUsageStatus(ref?: string) {
   const url = `${API_URL}/projects/${ref}/usage-status`
-  const { data, error } = useSWR<any>(url, get)
+  const { data, error } = useSWR<any>(ref ? url : null, get)
   const anyError = data?.error || error
 
   return {
-    config: anyError ? undefined : data,
+    usageStatus: anyError ? undefined : data,
     error: anyError,
     isLoading: !anyError && !data,
     isError: !!anyError,
