183107bd32010786d895900d211eccf5ff7843b8
Joshen Lim <joshenlimek@gmail.com>
2022-08-15 15:25:35 +0800

    Integrate html-to-image and set up UI logic for taking a screen capture

--- studio/components/layouts/ProjectLayout/LayoutHeader/FeedbackDropdown/FeedbackWidget.tsx
+++ studio/components/layouts/ProjectLayout/LayoutHeader/FeedbackDropdown/FeedbackWidget.tsx
@@ -1,20 +1,30 @@
-import { useState, useEffect, useRef } from 'react'
 import Link from 'next/link'
+import { useState, useEffect, useRef, FC } from 'react'
 import { useRouter } from 'next/router'
-import { Button, Typography, Divider, Input, Popover } from '@supabase/ui'
+import * as Tooltip from '@radix-ui/react-tooltip'
+import { toPng } from 'html-to-image'
+import { Button, Input, Popover, IconCamera, IconX } from '@supabase/ui'
 
 import { useStore } from 'hooks'
 import { post } from 'lib/common/fetch'
 import { API_URL } from 'lib/constants'
 
-const FeedbackWidget = ({ onClose, feedback, setFeedback, category, setCategory }: any) => {
+interface Props {
+  onClose: () => void
+}
+
+const FeedbackWidget: FC<Props> = ({ onClose }) => {
   const router = useRouter()
   const { ref } = router.query
 
   const { ui } = useStore()
   const inputRef = useRef<any>(null)
 
+  const [feedback, setFeedback] = useState('')
+  const [screenshot, setScreenshot] = useState<string>()
+
   const [isSending, setSending] = useState(false)
+  const [isSavingScreenshot, setIsSavingScreenshot] = useState(false)
 
   useEffect(() => {
     inputRef?.current?.focus()
@@ -24,8 +34,32 @@ const FeedbackWidget = ({ onClose, feedback, setFeedback, category, setCategory 
     setFeedback(e.target.value)
   }
 
-  function onCancel() {
-    onClose()
+  const captureScreenshot = () => {
+    setIsSavingScreenshot(true)
+
+    function filter(node: HTMLElement) {
+      if ((node?.children ?? []).length > 0) {
+        return node.children[0].id !== 'feedback-widget'
+      }
+      return true
+    }
+
+    toPng(document.body, { filter })
+      .then((dataUrl) => {
+        console.log('Capture screenshot', dataUrl)
+        setScreenshot(dataUrl)
+      })
+      .catch((error: any) => {
+        ui.setNotification({
+          error,
+          category: 'error',
+          message: 'Failed to capture screenshot',
+          duration: 4000,
+        })
+      })
+      .finally(() => {
+        setIsSavingScreenshot(false)
+      })
   }
 
   const sendFeedback = async () => {
@@ -47,7 +81,7 @@ const FeedbackWidget = ({ onClose, feedback, setFeedback, category, setCategory 
   }
 
   return (
-    <div className="">
+    <div id="feedback-widget">
       <Input.TextArea
         className="w-80 p-3"
         size="small"
@@ -59,12 +93,56 @@ const FeedbackWidget = ({ onClose, feedback, setFeedback, category, setCategory 
       <Popover.Seperator />
       <div className="w-80 space-y-3 px-3 py-2 pb-4">
         <div className="flex justify-between space-x-2">
-          <Button type="default" onClick={onCancel} className="hover:border-gray-500">
+          <Button type="default" onClick={onClose} className="hover:border-gray-500">
             Cancel
           </Button>
-          <Button disabled={isSending} loading={isSending} onClick={sendFeedback}>
-            Send feedback
-          </Button>
+
+          <div className="flex items-center space-x-2">
+            {screenshot !== undefined ? (
+              <div
+                style={{ backgroundImage: `url("${screenshot}")` }}
+                className="rounded h-[26px] w-[30px] border border-scale-600 relative bg-cover bg-center bg-no-repeat"
+              >
+                <div
+                  className={[
+                    'cursor-pointer rounded-full bg-red-900 h-3 w-3',
+                    'flex items-center justify-center absolute -top-1 -right-1',
+                  ].join(' ')}
+                  onClick={() => setScreenshot(undefined)}
+                >
+                  <IconX size={8} strokeWidth={3} />
+                </div>
+              </div>
+            ) : (
+              <Tooltip.Root delayDuration={0}>
+                <Tooltip.Trigger>
+                  <Button
+                    as="span"
+                    type="default"
+                    disabled={isSavingScreenshot}
+                    loading={isSavingScreenshot}
+                    className="px-2 py-1.5"
+                    icon={<IconCamera size={14} />}
+                    onClick={() => captureScreenshot()}
+                  />
+                </Tooltip.Trigger>
+                <Tooltip.Content side="bottom">
+                  <Tooltip.Arrow className="radix-tooltip-arrow" />
+                  <div
+                    className={[
+                      'bg-scale-100 rounded py-1 px-2 leading-none shadow', // background
+                      'border-scale-200 border', //border
+                    ].join(' ')}
+                  >
+                    <span className="text-scale-1200 text-xs text-center">Capture screenshot</span>
+                  </div>
+                </Tooltip.Content>
+              </Tooltip.Root>
+            )}
+            <Button disabled={isSending} loading={isSending} onClick={sendFeedback}>
+              Send feedback
+            </Button>
+          </div>
         </div>
         <p className="text-xs text-scale-1000">
           Have a technical issue? Contact{' '}
