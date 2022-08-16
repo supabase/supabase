1f55321a4dd87430ada3c2781f81616c5bbb47cf
TzeYiing <ty@tzeyiing.com>
2022-08-17 02:46:05 +0800

    chore: move typings around, remove unused imports, improve type safety

--- studio/components/interfaces/Reports/Reports.types.ts
+++ studio/components/interfaces/Reports/Reports.types.ts
@@ -1,3 +1,17 @@
 export enum Presets {
-    OVERVIEW = "overview"
+  OVERVIEW = 'overview',
+}
+export interface StatusCodesDatum {
+  timestamp: number
+  status_code: number
+  count: number
+}
+export interface PathsDatum {
+  timestamp: number
+  path: string
+  query_params: string
+  count: number
+  sum: number
+  method: string
+  avg_origin_time: number
 }
