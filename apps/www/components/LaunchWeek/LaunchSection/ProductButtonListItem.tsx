010183891ee3d34448ed5a1f20b22f45ad29feca
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-08-16 08:48:12 -0500

    text size updates

--- apps/www/components/LaunchWeek/LaunchSection/ProductButtonListItem.tsx
+++ apps/www/components/LaunchWeek/LaunchSection/ProductButtonListItem.tsx
@@ -14,11 +14,11 @@ export const ProductButtonListItem = (props: Article) => {
           transition
           dark:drop-shadow-sm"
         >
-          <div className="w-6">
+          <div className="w-5">
             <SparklesIcon strokeWidth={1} />
           </div>
           <div className="flex flex-col items-start gap-0">
-            <span className="group-hover:text-brand-900 text-base">{props.title}</span>
+            <span className="group-hover:text-brand-900 text-sm">{props.title}</span>
             <span className="text-scale-900 text-sm">{props.description}</span>
           </div>
         </button>
