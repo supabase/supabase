270e73837090144d40932da12a4da7095e33aea0
Joshen Lim <joshenlimek@gmail.com>
2022-08-17 13:43:47 +0800

    Hook up upload logic

--- studio/components/interfaces/Support/SupportForm.utils.ts
+++ studio/components/interfaces/Support/SupportForm.utils.ts
@@ -1,7 +1,25 @@
+import { compact } from 'lodash'
 import { createClient } from '@supabase/supabase-js'
-import { IS_PLATFORM } from 'lib/constants'
 import { uuidv4 } from 'lib/helpers'
-import { post } from 'lib/common/fetch'
+
+// [Joshen TODO] To be shifted into an env vars before doing anything
+// Both bitwarden, .env, and vercel
+const SUPPORT_API_URL = process.env.NEXT_PUBLIC_SUPPORT_API_URL || ''
+const SUPPORT_API_KEY = process.env.NEXT_PUBLIC_SUPPORT_ANON_KEY || ''
+const supportSupabaseClient = createClient(SUPPORT_API_URL, SUPPORT_API_KEY, {
+  auth: {
+    persistSession: false,
+    autoRefreshToken: false,
+    multiTab: false,
+    detectSessionInUrl: false,
+    localStorage: {
+      // @ts-ignore
+      getItem: (key) => {},
+      setItem: (key, value) => {},
+      removeItem: (key) => {},
+    },
+  },
+})
 
 export function formReducer(state: any, action: any) {
   return {
@@ -15,26 +33,41 @@ export function formReducer(state: any, action: any) {
 
 export const uploadAttachments = async (ref: string, files: File[]) => {
   const filesToUpload = Array.from(files)
-  return Promise.all(
+  const uploadedFiles = await Promise.all(
     filesToUpload.map(async (file) => {
-      // [Joshen] Left off here - trying to figure out how to
-      // pass the files into the next API, which will upload the files then through
-      // the Supabase client
-      const res = await post('/api/upload-attachments', files, {
-        headers: { 'Content-Type': 'multipart/form-data' },
-      })
+      const suffix = file.type.split('/')[1]
+      const prefix = `${ref}/${uuidv4()}.${suffix}`
+      const options = { cacheControl: '3600' }
 
-      // const prefix = `${ref}/${uuidv4()}`
-      // const options = { cacheControl: '3600' }
+      const { data, error } = await supportSupabaseClient.storage
+        .from('support-attachments')
+        .upload(prefix, file, options)
 
-      // const { data, error } = await supportSupabaseClient.storage
-      //   .from('support-attachments')
-      //   .upload(prefix, file, options)
-
-      // if (error) {
-      //   console.log('error', error)
-      // }
-      // return data
+      if (error) console.error('Failed to upload:', file.name, error)
+      return data
     })
   )
+  const keys = compact(uploadedFiles).map((file) => file.Key.split('/').slice(1).join('/'))
+
+  const { data, error } = await supportSupabaseClient.storage
+    .from('support-attachments')
+    .createSignedUrls(keys, 10 * 365 * 24 * 60 * 60)
+  if (error) {
+    console.error('Failed to retrieve URLs for attachments', error)
+  }
+  return data ? data.map((file) => file.signedURL) : []
+}
+
+export const formatMessage = (message: string, attachments: string[]) => {
+  const formattedMessage = message.replace(/\n/g, '<br/>')
+  if (attachments.length > 0) {
+    const attachmentsImg = attachments.map(
+      (url) => `<img src=${url} width="200" style="margin-right:10px" />`
+    )
+    return `${formattedMessage}<br/><hr/><b>Uploaded attachments:</b><br/><div style="display:flex;align-items:center">${attachmentsImg.join(
+      ''
+    )}</div>`
+  } else {
+    return formattedMessage
+  }
 }
