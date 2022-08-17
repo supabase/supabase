bce5719eda384136a4ffab8b8c036a7927ede333
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2022-08-17 17:58:55 -0400

    chore: use api for open positions

--- apps/careers/components/Positions.tsx
+++ apps/careers/components/Positions.tsx
@@ -1,23 +1,35 @@
+import { useEffect, useState } from 'react'
 import Link from 'next/link'
 
 const Positions = () => {
+  const [data, setData]: any = useState('')
+
+  useEffect(() => {
+    fetch('https://boards-api.greenhouse.io/v1/boards/supabase/jobs/')
+      .then((res) => res.json())
+      .then((data) => setData(data))
+  }, [])
+
   return (
     <section className="positions" id="positions">
       <div>
         <div className="space-y-6">
-          <h1>Open positions</h1>
-          <div>
-            <Link href="https://boards.greenhouse.io/supabase">
-              <a>View Open Positions</a>
-            </Link>
-          </div>
-        </div>
-        <div className="mt-6 space-y-6">
-          <h1>Internships</h1>
-          <div>
-            <Link href="https://boards.greenhouse.io/supabase">
-              <a>View Open Internships</a>
-            </Link>
+          <h1 className="text-4xl font-semibold">Open positions</h1>
+          <div className="space-y-6">
+           {data.jobs?.map((job: any, i: number) => {
+               return (
+                <div key={i}>
+                  <Link href={`career/job/${job.id}`}>
+                    <a>
+                      <div className="px-6 py-4 border-2 border-white hover:border-brand-900 rounded-lg shadow hover:shadow-lg">
+                        <div>{job.title}</div>
+                        <div>{job.location.name}</div>
+                      </div>
+                    </a>
+                  </Link>
+                </div>
+               )
+             })} 
           </div>
         </div>
       </div>
