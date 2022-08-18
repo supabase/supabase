5dae6c38822e3860ed5bd2fcd1adb50059d6e4c1
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-08-18 09:24:49 -0500

    update data file

--- apps/www/pages/realtime/Realtime.tsx
+++ apps/www/pages/realtime/Realtime.tsx
@@ -2,6 +2,7 @@ import { IconGrid, IconLayers, IconMenu } from '@supabase/ui'
 
 // [TODO] These need to be relooked - I'm using multiplayer.dev as a "docs"
 import ApiExamples from 'data/products/realtime/api-examples'
+import AppExamples from 'data/products/realtime/app-examples'
 import RealtimeStyles from './Realtime.module.css'
 
 import Solutions from 'data/Solutions.json'
@@ -36,69 +37,6 @@ const Cursor = ({ className = '', color = 'none' }) => {
     </svg>
   )
 }
-
-const exampleApps = [
-  {
-    img: 'toast-messages.svg',
-    title: 'Toast messages',
-    description: 'Listen to changes in the database and show users a toast message.',
-  },
-  {
-    img: 'live-avatars.svg',
-    title: 'Live avatars',
-    description: 'Share the status of users across multiple clients.',
-  },
-  {
-    img: 'live-cursors.svg',
-    title: 'Live cursors',
-    description: 'Share the position and status of multiple cursors across multiple clients.',
-  },
-  {
-    img: 'leaderboard.svg',
-    title: 'Leaderboards',
-    description:
-      'Listen to changes in your database regarding the scores of your users and display them live.',
-  },
-  {
-    img: 'stock-market.svg',
-    title: 'Live charts',
-    description:
-      'Keep charts updated in Realtime by listening to changes in the database rather than polling at intervals.',
-  },
-  {
-    img: 'in-app-chat.svg',
-    title: 'In app chat',
-    description: 'Listen to changes in the database to display up-to-date text messages.',
-  },
-  {
-    img: 'text-editor.svg',
-    title: 'Shared text editor',
-    description: 'Users can collaborate with text documents or even code editors.',
-  },
-
-  {
-    img: 'whiteboard.svg',
-    title: 'Shared whiteboard',
-    description:
-      'Allow users to collaorate on a shared whiteboard, sharing positions and states of any kind of object in Realtime.',
-  },
-  {
-    img: 'location.svg',
-    title: 'Location',
-    description: 'Listen to changes in the database regarding the position of a moving coordinate.',
-  },
-  {
-    img: 'multiplayer-game.svg',
-    title: 'Multiplayer games',
-    description:
-      "Keep track of how long a player has been playing, who's turn it is and even share to other clients what the pllayer is doing.",
-  },
-  {
-    img: 'form-presence.svg',
-    title: 'Form presence',
-    description: 'Display the active selection of different users throughout an interactive form.',
-  },
-]
 
 function RealtimePage() {
   // base path for images
@@ -261,7 +199,7 @@ function RealtimePage() {
             </p>
           </div>
           <div className="grid gap-12 md:grid-cols-2 lg:grid-cols-4">
-            {exampleApps.map((example) => {
+            {AppExamples.map((example) => {
               return (
                 <>
                   <div className="flex flex-col gap-4">
