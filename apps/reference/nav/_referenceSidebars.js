ba9e5d5800c6fee9eeefe6f6dd690fc823338cf3
Inian <inian1234@gmail.com>
2022-08-18 15:56:59 +0800

    add guide for broadcast

--- apps/reference/nav/_referenceSidebars.js
+++ apps/reference/nav/_referenceSidebars.js
@@ -82,6 +82,12 @@ const sidebars = {
     },
     {
       type: 'category',
+      label: 'Realtime',
+      collapsed: true,
+      items: ['guides/realtime/broadcast'],
+    },
+    {
+      type: 'category',
       label: 'APIs',
       collapsed: true,
       items: ['guides/api', 'guides/api/generating-types'],
