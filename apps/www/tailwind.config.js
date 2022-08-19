65967afcb9ae167277b3bed39e05b391541a5324
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-08-18 23:55:57 -0500

    fix config

--- apps/www/tailwind.config.js
+++ apps/www/tailwind.config.js
@@ -1,13 +1,1 @@
-const config = require('config/tailwind.config')
-
-const extend = {
-  colors: {
-    'auth-widget': {
-      brand: 'var(--colors-brand)',
-      label: 'var(--colors-brand)',
-      input: 'var(--colors-inputText)',
-    },
-  },
-}
-
-module.exports = config(extend)
+module.exports = require('config/tailwind.config')
