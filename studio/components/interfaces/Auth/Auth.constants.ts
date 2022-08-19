3c2cae9e3f2aec2df04308dfa30909888ac41fa8
aar2dee2 <85004512+aar2dee2@users.noreply.github.com>
2022-08-19 18:41:11 +0530

    Update domain regex to accept chrome extension urls
    Issue #8338

--- studio/components/interfaces/Auth/Auth.constants.ts
+++ studio/components/interfaces/Auth/Auth.constants.ts
@@ -5,10 +5,11 @@
 //  "www.vercel.com"
 //  "uptime-monitor-fe.vercel.app"
 //  "https://uptime-monitor-fe.vercel.app/"
+//  "chrome-extension://<extension-id>"
 
 // Supports wildcards, port numbers at the end, paths at the end and query params
 const baseDomainRegex =
-  /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_*-]+(\.[a-zA-Z0-9_*-]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)+(?:\.[a-z]+)*(?::\d+)?(?![^<]*(?:<\/\w+>|\/?>))(.*)?\/?(.)*?$/gm
+  /^((ftp|http|https|chrome-extension):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_*-]+(\.[a-zA-Z0-9_*-]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)+(?:\.[a-z]+)*(?::\d+)?(?![^<]*(?:<\/\w+>|\/?>))(.*)?\/?(.)*?$/gm
 
 // iOS deep linking scheme https://benoitpasquier.com/deep-linking-url-scheme-ios/
 const appRegex =
