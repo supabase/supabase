128d63a018fc01c49034330075b86b29e76c0c30
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-08-18 23:54:11 -0500

    init widget

--- apps/www/tailwind.config.js
+++ apps/www/tailwind.config.js
@@ -1,1 +1,13 @@
-module.exports = require('config/tailwind.config')
+const config = require('config/tailwind.config')
+
+const extend = {
+  colors: {
+    'auth-widget': {
+      brand: 'var(--colors-brand)',
+      label: 'var(--colors-brand)',
+      input: 'var(--colors-inputText)',
+    },
+  },
+}
+
+module.exports = config(extend)
