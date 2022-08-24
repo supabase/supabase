c01ece43621b2ee5e53ea3b2078b46e8fa292452
TzeYiing <ty@tzeyiing.com>
2022-08-24 11:40:45 +0800

    chore: add eslint and strict tsc config

---
+++ studio/.eslintrc.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+const tsConfig = JSON.parse(fs.readFileSync('tsconfig.strict.json', 'utf8'))
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/jsx-runtime',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {},
+  ignorePatterns: tsConfig.exclude,
+}
