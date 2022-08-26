48d3a5158c9cf1e55ef1a41d16c29bfba493da8e
Joshen Lim <joshenlimek@gmail.com>
2022-08-26 15:32:26 +0800

    Add abac for db policies

--- studio/components/interfaces/Authentication/Policies/PolicyTableRow/PolicyRow.tsx
+++ studio/components/interfaces/Authentication/Policies/PolicyTableRow/PolicyRow.tsx
@@ -1,7 +1,11 @@
 import { FC } from 'react'
+import * as Tooltip from '@radix-ui/react-tooltip'
 import { Button, Dropdown, IconEdit, IconTrash, IconMoreVertical } from '@supabase/ui'
-import Panel from 'components/ui/Panel'
 import { PostgresPolicy } from '@supabase/postgres-meta'
+import { PermissionAction } from '@supabase/shared-types/out/constants'
+
+import { checkPermissions } from 'hooks'
+import Panel from 'components/ui/Panel'
 
 interface Props {
   policy: PostgresPolicy
@@ -14,6 +18,8 @@ const PolicyRow: FC<Props> = ({
   onSelectEditPolicy = () => {},
   onSelectDeletePolicy = () => {},
 }) => {
+  const canUpdatePolicies = checkPermissions(PermissionAction.TENANT_SQL_ADMIN_WRITE, 'policies')
+
   return (
     <Panel.Content
       className={[
@@ -34,34 +40,62 @@ const PolicyRow: FC<Props> = ({
         </div>
       </div>
       <div>
-        <Dropdown
-          side="bottom"
-          align="end"
-          size="small"
-          overlay={
-            <>
-              <Dropdown.Item
-                icon={<IconEdit size={14} />}
-                onClick={() => onSelectEditPolicy(policy)}
-              >
-                Edit
-              </Dropdown.Item>
-              <Dropdown.Seperator />
-              <Dropdown.Item
-                icon={<IconTrash size={14} />}
-                onClick={() => onSelectDeletePolicy(policy)}
-              >
-                Delete
-              </Dropdown.Item>
-            </>
-          }
-        >
-          <Button
-            type="default"
-            style={{ paddingLeft: 4, paddingRight: 4 }}
-            icon={<IconMoreVertical />}
-          />
-        </Dropdown>
+        {canUpdatePolicies ? (
+          <Dropdown
+            side="bottom"
+            align="end"
+            size="small"
+            overlay={
+              <>
+                <Dropdown.Item
+                  icon={<IconEdit size={14} />}
+                  onClick={() => onSelectEditPolicy(policy)}
+                >
+                  Edit
+                </Dropdown.Item>
+                <Dropdown.Seperator />
+                <Dropdown.Item
+                  icon={<IconTrash size={14} />}
+                  onClick={() => onSelectDeletePolicy(policy)}
+                >
+                  Delete
+                </Dropdown.Item>
+              </>
+            }
+          >
+            <Button
+              type="default"
+              style={{ paddingLeft: 4, paddingRight: 4 }}
+              icon={<IconMoreVertical />}
+            />
+          </Dropdown>
+        ) : (
+          <Tooltip.Root delayDuration={0}>
+            <Tooltip.Trigger>
+              <Button
+                disabled
+                type="default"
+                style={{ paddingLeft: 4, paddingRight: 4 }}
+                icon={<IconMoreVertical />}
+              />
+            </Tooltip.Trigger>
+            {!canUpdatePolicies && (
+              <Tooltip.Content side="left">
+                <Tooltip.Arrow className="radix-tooltip-arrow" />
+                <div
+                  className={[
+                    'bg-scale-100 rounded py-1 px-2 leading-none shadow',
+                    'border-scale-200 border',
+                  ].join(' ')}
+                >
+                  <span className="text-scale-1200 text-xs">
+                    You need additional permissions to edit RLS policies
+                  </span>
+                </div>
+              </Tooltip.Content>
+            )}
+          </Tooltip.Root>
+        )}
       </div>
     </Panel.Content>
   )
--- studio/components/interfaces/Authentication/Policies/PolicyTableRow/PolicyTableRowHeader.tsx
+++ studio/components/interfaces/Authentication/Policies/PolicyTableRow/PolicyTableRowHeader.tsx
@@ -1,8 +1,11 @@
+import Link from 'next/link'
 import { FC } from 'react'
 import { useRouter } from 'next/router'
+import * as Tooltip from '@radix-ui/react-tooltip'
 import { Badge, Button } from '@supabase/ui'
 import { PostgresTable } from '@supabase/postgres-meta'
-import Link from 'next/link'
+import { PermissionAction } from '@supabase/shared-types/out/constants'
+import { checkPermissions } from 'hooks'
 
 interface Props {
   table: PostgresTable
@@ -17,6 +20,8 @@ const PolicyTableRowHeader: FC<Props> = ({
 }) => {
   const router = useRouter()
   const { ref } = router.query
+  const canToggleRLS = checkPermissions(PermissionAction.TENANT_SQL_ADMIN_WRITE, 'tables')
+  const canCreatePolicies = checkPermissions(PermissionAction.TENANT_SQL_ADMIN_WRITE, 'policies')
 
   return (
     <div id={table.id.toString()} className="flex w-full items-center justify-between">
@@ -32,12 +37,59 @@ const PolicyTableRowHeader: FC<Props> = ({
       </div>
       <div className="flex-1">
         <div className="flex flex-row-reverse">
-          <Button type="outline" className="ml-2" onClick={() => onSelectCreatePolicy(table)}>
-            New Policy
-          </Button>
-          <Button type="default" onClick={() => onSelectToggleRLS(table)}>
-            {table.rls_enabled ? 'Disable RLS' : 'Enable RLS'}
-          </Button>
+          <Tooltip.Root delayDuration={0}>
+            <Tooltip.Trigger>
+              <Button
+                type="outline"
+                disabled={!canCreatePolicies}
+                className="ml-2"
+                onClick={() => onSelectCreatePolicy(table)}
+              >
+                New Policy
+              </Button>
+            </Tooltip.Trigger>
+            {!canCreatePolicies && (
+              <Tooltip.Content side="bottom">
+                <Tooltip.Arrow className="radix-tooltip-arrow" />
+                <div
+                  className={[
+                    'bg-scale-100 rounded py-1 px-2 leading-none shadow',
+                    'border-scale-200 border',
+                  ].join(' ')}
+                >
+                  <span className="text-scale-1200 text-xs">
+                    You need additional permissions to create RLS policies
+                  </span>
+                </div>
+              </Tooltip.Content>
+            )}
+          </Tooltip.Root>
+          <Tooltip.Root delayDuration={0}>
+            <Tooltip.Trigger>
+              <Button
+                type="default"
+                disabled={!canToggleRLS}
+                onClick={() => onSelectToggleRLS(table)}
+              >
+                {table.rls_enabled ? 'Disable RLS' : 'Enable RLS'}
+              </Button>
+            </Tooltip.Trigger>
+            {!canToggleRLS && (
+              <Tooltip.Content side="bottom">
+                <Tooltip.Arrow className="radix-tooltip-arrow" />
+                <div
+                  className={[
+                    'bg-scale-100 rounded py-1 px-2 leading-none shadow',
+                    'border-scale-200 border',
+                  ].join(' ')}
+                >
+                  <span className="text-scale-1200 text-xs">
+                    You need additional permissions to toggle RLS
+                  </span>
+                </div>
+              </Tooltip.Content>
+            )}
+          </Tooltip.Root>
         </div>
       </div>
     </div>
