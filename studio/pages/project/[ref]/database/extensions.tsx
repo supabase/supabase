4be9c4e26773e3c2e09e4a97f4417007633b888e
Joshen Lim <joshenlimek@gmail.com>
2022-08-26 12:22:40 +0800

    Add abac for db extensions

--- studio/pages/project/[ref]/database/extensions.tsx
+++ studio/pages/project/[ref]/database/extensions.tsx
@@ -1,10 +1,18 @@
 import { observer } from 'mobx-react-lite'
+import { PermissionAction } from '@supabase/shared-types/out/constants'
 
+import { NextPageWithLayout } from 'types'
+import { checkPermissions } from 'hooks'
 import { DatabaseLayout } from 'components/layouts'
 import { Extensions } from 'components/interfaces/Database'
-import { NextPageWithLayout } from 'types'
+import NoPermission from 'components/ui/NoPermission'
 
 const DatabaseExtensions: NextPageWithLayout = () => {
+  const canReadExtensions = checkPermissions(PermissionAction.TENANT_SQL_ADMIN_READ, 'extensions')
+  if (!canReadExtensions) {
+    return <NoPermission isFullPage resourceText="view database extensions" />
+  }
+
   return (
     <div className="p-4">
       <Extensions />
