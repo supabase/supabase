93702445373c1c5a18878752e734a4ac6ba24356
dannykng <danny@supabase.io>
2022-09-16 10:13:18 -0700

    Revert "chore: remove examples"
    This reverts commit 05b96de14238115a9e87e58de705ab8ec08f97e8.

---
+++ examples/todo-list/vue3-ts-todo-list/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+}
