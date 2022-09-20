e20e54d2fb0d10163490bbd65520daaf5e8c9982
Div Arora <darora@users.noreply.github.com>
2022-09-20 15:01:29 +0800

    Update apps/reference/nav/_referenceSidebars.js
    Co-authored-by: dng <danny@supabase.io>

--- apps/reference/nav/_referenceSidebars.js
+++ apps/reference/nav/_referenceSidebars.js
@@ -189,12 +189,12 @@ const sidebars = {
       label: 'Platform',
       collapsed: true,
       items: [
+        'guides/platform/disk-usage',
         'guides/platform/logs',
         'guides/platform/metrics',
-        'going-into-prod',
-        'guides/platform/disk-usage',
         'guides/platform/performance',
         'guides/platform/permissions',
+        'going-into-prod',
       ],
     },
     {
