55989b4809df0c572cbcdba59c06772e62eed560
Joshen Lim <joshenlimek@gmail.com>
2022-09-22 20:43:04 +0800

    Update notifications styling

--- studio/components/layouts/ProjectLayout/LayoutHeader/NotificationsPopover/NotificationRows.utils.tsx
+++ studio/components/layouts/ProjectLayout/LayoutHeader/NotificationsPopover/NotificationRows.utils.tsx
@@ -9,6 +9,7 @@ import {
   ServiceUpgrade,
   ViolatedLimit,
 } from '@supabase/shared-types/out/notifications'
+import { IconArrowRight, IconExternalLink } from '@supabase/ui'
 
 export const formatNotificationText = (project: Project, notification: Notification) => {
   const projectName = project.name
@@ -19,16 +20,29 @@ export const formatNotificationText = (project: Project, notification: Notificat
       .map((violation: ViolatedLimit) => violation.dimension as string)
       .reduce((a: string, b: string) => `${a}, ${b}`)
 
-    return `Your project "${projectName}" has exceeded its limits in the following areas: ${violationsText}.`
+    return (
+      <p className="text-sm">
+        Your project "{projectName}" has exceeded its limits in the following areas:{' '}
+        {violationsText}.
+      </p>
+    )
   } else if (notification.data.name === NotificationName.PostgresqlUpgradeAvailable) {
     const { upgrade_type, additional } = notification.data
 
     if (upgrade_type === 'postgresql-server') {
       const { version_to } = additional as ServerUpgrade
-      return `New version of Postgres (${version_to}) is now available for project "${projectName}".`
+      return (
+        <p className="text-sm">
+          New version of Postgres ({version_to}) is now available for project "{projectName}".
+        </p>
+      )
     } else if (upgrade_type === 'extensions') {
       const { name, version_to } = additional as ExtensionsUpgrade
-      return `New version of "${name}" (${version_to}) is now available for project "${projectName}".`
+      return (
+        <p className="text-sm">
+          New version of "{name}" ({version_to}) is now available for project "{projectName}".
+        </p>
+      )
     }
     return ''
   } else if (notification.data.name === NotificationName.PostgresqlUpgradeCompleted) {
@@ -36,21 +50,63 @@ export const formatNotificationText = (project: Project, notification: Notificat
 
     if (upgrade_type === 'postgresql-server') {
       const { version_to } = additional as ServerUpgrade
-      return `Postgres (${version_to}) has been successfully updated to ${version_to} for project "${projectName}".`
+      return (
+        <p className="text-sm">
+          Postgres ({version_to}) has been successfully updated to {version_to} for project "
+          {projectName}".
+        </p>
+      )
     } else if (upgrade_type === 'extensions') {
       const { name, version_to } = additional as ExtensionsUpgrade
-      return `The extension "${name}" has been successfully updated to ${version_to} for project "${projectName}".`
+      return (
+        <p className="text-sm">
+          The extension "{name}" has been successfully updated to {version_to} for project "
+          {projectName}".
+        </p>
+      )
     }
   } else if (notification.data.name === NotificationName.ProjectUpdateCompleted) {
     const { upgrades } = notification.data
     const upgradesText = upgrades
-      .map((upgrade: ServiceUpgrade) => `${upgrade.name}: ${upgrade.version_to} ${upgrade.changelog_link}`)
+      .map(
+        (upgrade: ServiceUpgrade) =>
+          `${upgrade.name}: ${upgrade.version_to} ${upgrade.changelog_link}`
+      )
       .reduce((a: string, b: string) => `${a}\n${b}`)
-    return `The following services have been successfully updated for project "${projectName}": ${upgradesText}.`
+    return (
+      <div>
+        <p className="text-sm">
+          The following services have been successfully updated for project "{projectName}":
+        </p>
+        <ol className="list-disc pl-6">
+          {upgrades.map((upgrade: ServiceUpgrade) => (
+            <li key={upgrade.name}>
+              <div className="flex items-center space-x-1">
+                <p className="text-sm">{upgrade.name}</p>
+                <IconArrowRight size={12} strokeWidth={2} />
+                <p className="text-sm">{upgrade.version_to}</p>
+                <div className="!ml-4">
+                  <IconExternalLink
+                    className="cursor-pointer text-scale-1000 hover:text-scale-1200 transition"
+                    size={12}
+                    strokeWidth={2}
+                  />
+                </div>
+              </div>
+            </li>
+          ))}
+        </ol>
+      </div>
+    )
   } else if (notification.data.name === NotificationName.ProjectInformational) {
     return notification.data.message
   } else {
-    return `Unknown notification type: ${notification.notification_name} - Please reach out to support for more information.`
+    return (
+      <p className="text-sm">
+        Unknown notification type: {notification.notification_name} - Please reach out to support
+        for more information.
+      </p>
+    )
   }
 }
 
