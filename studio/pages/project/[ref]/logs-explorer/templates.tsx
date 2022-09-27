4bc9378b2766f232f5e736fcd3323f53fa86f3de
Ollie Halliday <22655069+Hallidayo@users.noreply.github.com>
2022-09-27 15:03:27 +0100

    logs template sort order changed

--- studio/pages/project/[ref]/logs-explorer/templates.tsx
+++ studio/pages/project/[ref]/logs-explorer/templates.tsx
@@ -14,59 +14,61 @@ export const LogsTemplatesPage: NextPageWithLayout = () => {
   return (
     <div>
       <div className="grid grid-cols-3 gap-6">
-        {TEMPLATES.filter((template) => template.mode === 'custom').map((template, i) => {
-          const [showPreview, setShowPreview] = useState(false)
-          return (
-            <CardButton
-              key={i}
-              title={template.label}
-              icon={
-                <div
-                  className="text-scale-100 bg-scale-1200 duration-400 group-hover:bg-brand-900 group-hover:text-brand-1200 flex h-6
+        {TEMPLATES.sort((a, b) => a.label!.localeCompare(b.label!))
+          .filter((template) => template.mode === 'custom')
+          .map((template, i) => {
+            const [showPreview, setShowPreview] = useState(false)
+            return (
+              <CardButton
+                key={i}
+                title={template.label}
+                icon={
+                  <div
+                    className="text-scale-100 bg-scale-1200 duration-400 group-hover:bg-brand-900 group-hover:text-brand-1200 flex h-6
                     w-6
                     items-center
                     justify-center
                     rounded
                     transition-colors
                   "
-                >
-                  <div className="scale-100 group-hover:scale-110">
-                    <IconCode size={12} strokeWidth={2} />
-                  </div>
-                </div>
-              }
-              containerHeightClassName="h-40"
-              linkHref={`/project/${ref}/logs-explorer?q=${encodeURI(template.searchString)}`}
-              description={template.description}
-              footer={
-                <div className="flex flex-row justify-end">
-                  <Popover
-                    onOpenChange={setShowPreview}
-                    open={showPreview}
-                    className="bg-scale-100 rounded-lg"
-                    size="content"
-                    overlay={
-                      <pre className="bg-scale-100 whitespace-pre-line break-words rounded-lg p-4 text-sm">
-                        {template.searchString}
-                      </pre>
-                    }
                   >
-                    <Button
-                      type="default"
-                      as="span"
-                      onClick={(e) => {
-                        e.preventDefault()
-                        setShowPreview(!showPreview)
-                      }}
+                    <div className="scale-100 group-hover:scale-110">
+                      <IconCode size={12} strokeWidth={2} />
+                    </div>
+                  </div>
+                }
+                containerHeightClassName="h-40"
+                linkHref={`/project/${ref}/logs-explorer?q=${encodeURI(template.searchString)}`}
+                description={template.description}
+                footer={
+                  <div className="flex flex-row justify-end">
+                    <Popover
+                      onOpenChange={setShowPreview}
+                      open={showPreview}
+                      className="bg-scale-100 rounded-lg"
+                      size="content"
+                      overlay={
+                        <pre className="bg-scale-100 whitespace-pre-line break-words rounded-lg p-4 text-sm">
+                          {template.searchString}
+                        </pre>
+                      }
                     >
-                      Preview
-                    </Button>
-                  </Popover>
-                </div>
-              }
-            />
-          )
-        })}
+                      <Button
+                        type="default"
+                        as="span"
+                        onClick={(e) => {
+                          e.preventDefault()
+                          setShowPreview(!showPreview)
+                        }}
+                      >
+                        Preview
+                      </Button>
+                    </Popover>
+                  </div>
+                }
+              />
+            )
+          })}
       </div>
     </div>
   )
