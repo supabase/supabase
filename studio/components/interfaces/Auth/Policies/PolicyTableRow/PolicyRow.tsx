360e1825087a76dc5d2a361a505d0bf3dae391a5
Ollie Halliday <22655069+Hallidayo@users.noreply.github.com>
2022-10-04 15:48:22 +0100

    removed truncation styling

--- studio/components/interfaces/Auth/Policies/PolicyTableRow/PolicyRow.tsx
+++ studio/components/interfaces/Auth/Policies/PolicyTableRow/PolicyRow.tsx
@@ -30,7 +30,7 @@ const PolicyRow: FC<Props> = ({
       <div className="flex grow flex-col truncate space-y-1">
         <div className="flex items-center space-x-4">
           <p className="text-scale-1000 font-mono text-xs">{policy.command}</p>
-          <p className="text-scale-1200 max-w-xs truncate text-sm">{policy.name}</p>
+          <p className="text-scale-1200 max-w-xs text-sm">{policy.name}</p>
         </div>
         <div className="flex items-center space-x-2">
           <p className="text-scale-1000 text-sm">Applied to:</p>
