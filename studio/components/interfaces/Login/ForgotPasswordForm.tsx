ecb75863d26079984a0ac1fbfbdc470cba331717
Alaister Young <a@alaisteryoung.com>
2022-10-10 20:48:44 +1100

    adds forgot password page

--- studio/components/interfaces/Login/ForgotPasswordForm.tsx
+++ studio/components/interfaces/Login/ForgotPasswordForm.tsx
@@ -1,5 +1,53 @@
+import { auth } from 'lib/gotrue'
+import { Button, Form, Input } from 'ui'
+import { object, string } from 'yup'
+
+const forgotPasswordSchema = object({
+  email: string().email('Must be a valid email').required('Email is required'),
+})
+
 const ForgotPasswordForm = () => {
-  return <div></div>
+  const onForgotPassword = async ({ email }: { email: string }) => {
+    const result = await auth.resetPasswordForEmail(email)
+    console.log('result:', result)
+  }
+
+  return (
+    <Form
+      validateOnBlur
+      id="forgot-password-form"
+      initialValues={{ email: '', password: '' }}
+      validationSchema={forgotPasswordSchema}
+      onSubmit={onForgotPassword}
+    >
+      {({ isSubmitting }: { isSubmitting: boolean }) => {
+        return (
+          <div className="mb-4 space-y-4 pt-4">
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              label="Email"
+              placeholder="you@example.com"
+            />
+
+            <div className="border-overlay-border border-t" />
+
+            <Button
+              block
+              form="forgot-password-form"
+              htmlType="submit"
+              size="medium"
+              disabled={isSubmitting}
+              loading={isSubmitting}
+            >
+              Send Reset Email
+            </Button>
+          </div>
+        )
+      }}
+    </Form>
+  )
 }
 
 export default ForgotPasswordForm
