a71d18e9a120fe97ef2992174e966f6f259dfd3e
thorwebdev <thor@supabase.io>
2022-10-11 10:11:47 +0800

    chore: update functions examples to supabase-js v2

--- examples/edge-functions/supabase/functions/select-from-table-with-auth-rls/index.ts
+++ examples/edge-functions/supabase/functions/select-from-table-with-auth-rls/index.ts
@@ -3,7 +3,7 @@
 // This enables autocomplete, go to definition, etc.
 
 import { serve } from 'https://deno.land/std@0.131.0/http/server.ts'
-import { supabaseClient } from '../_shared/supabaseClient.ts'
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.0.0-rc.12'
 import { corsHeaders } from '../_shared/cors.ts'
 
 console.log(`Function "select-from-table-with-auth-rls" up and running!`)
@@ -15,14 +15,26 @@ serve(async (req: Request) => {
   }
 
   try {
-    // Set the Auth context of the user that called the function.
-    // This way your row-level-security (RLS) policies are applied.
-    supabaseClient.auth.setAuth(req.headers.get('Authorization')!.replace('Bearer ', ''))
+    // Create a Supabase client with the Auth context of the logged in user.
+    const supabaseClient = createClient(
+      // Supabase API URL - env var exported by default.
+      Deno.env.get('SUPABASE_URL') ?? '',
+      // Supabase API ANON KEY - env var exported by default.
+      Deno.env.get('SUPABASE_ANON_KEY') ?? '',
+      // Create client with Auth context of the user that called the function.
+      // This way your row-level-security (RLS) policies are applied.
+      { global: { headers: { Authorization: req.headers.get('Authorization')! } } }
+    )
+    // Now we can get the session or user object
+    const {
+      data: { user },
+    } = await supabaseClient.auth.getUser()
 
+    // And we can run queries in the context of our authenticated user
     const { data, error } = await supabaseClient.from('users').select('*')
-    console.log({ data, error })
+    if (error) throw error
 
-    return new Response(JSON.stringify({ data, error }), {
+    return new Response(JSON.stringify({ user, data }), {
       headers: { ...corsHeaders, 'Content-Type': 'application/json' },
       status: 200,
     })
