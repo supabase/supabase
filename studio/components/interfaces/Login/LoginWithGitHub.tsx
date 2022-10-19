bd13da182804d99cd8db9e31f67b0b30528030ff
Alaister Young <a@alaisteryoung.com>
2022-10-19 15:17:25 +1100

    update github sign in redirect

--- studio/components/interfaces/Login/LoginWithGitHub.tsx
+++ studio/components/interfaces/Login/LoginWithGitHub.tsx
@@ -7,7 +7,11 @@ const LoginWithGitHub = () => {
       const { error } = await auth.signInWithOAuth({
         provider: 'github',
         options: {
-          redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/projects?auth=true`,
+          redirectTo: `${
+            process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview'
+              ? process.env.NEXT_PUBLIC_VERCEL_URL
+              : process.env.NEXT_PUBLIC_SITE_URL
+          }/projects?auth=true`,
         },
       })
       if (error) throw error
