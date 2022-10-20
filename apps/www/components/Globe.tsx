0c6b022fda71ad8bb79d690e085f3c6c1370ab9c
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2022-10-19 20:14:46 -0400

    fix: build error

--- apps/www/components/Globe.tsx
+++ apps/www/components/Globe.tsx
@@ -3,11 +3,11 @@ import createGlobe from 'cobe'
 import { useEffect, useRef } from 'react'
 
 const Globe = () => {
-  const canvasRef = useRef()
+  const canvasRef = useRef<any>()
 
   useEffect(() => {
     let rotation: number = 0
-    const width: number = 0
+    let width: number = 0
     const onResize = () => canvasRef.current && (width = canvasRef.current.offsetWidth)
     window.addEventListener('resize', onResize)
     onResize()
@@ -25,7 +25,22 @@ const Globe = () => {
       baseColor: [1, 1, 1],
       markerColor: [62 / 255, 207 / 255, 142 / 255],
       glowColor: [62 / 255, 207 / 255, 142 / 255],
-      markers: career.globe.markers,
+      markers: [
+        { location: [53.4084, 2.9916], size: 0.1 },
+        { location: [1.3521, 103.8198], size: 0.1 },
+        { location: [-40.9006, 174.886], size: 0.1 },
+        { location: [14.0583, 108.2772], size: 0.1 },
+        { location: [37.7749, -122.4194], size: 0.1 },
+        { location: [41.3874, 2.1686], size: 0.1 },
+        { location: [49.2827, -123.1207], size: 0.1 },
+        { location: [-36.9848, 143.3906], size: 0.1 },
+        { location: [42.3601, -71.0589], size: 0.1 },
+        { location: [52.52, 13.405], size: 0.1 },
+        { location: [33.749, -84.388], size: 0.1 },
+        { location: [35.6762, 139.6503], size: 0.1 },
+        { location: [9.19, -75.0152], size: 0.1 },
+        { location: [-25.2521, -52.0215], size: 0.1 },
+      ],
       onRender: (state) => {
         state.phi = rotation
         rotation += 0.005
