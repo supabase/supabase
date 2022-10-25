7f59460573dd36695545c357f2f13ccd33a06e44
Lakshan Perera <lakshan@laktek.com>
2022-10-25 15:11:55 +1100

    fix: function name

--- examples/edge-functions/supabase/functions/read-storage/index.ts
+++ examples/edge-functions/supabase/functions/read-storage/index.ts
@@ -58,7 +58,7 @@ serve(async (req) => {
 })
 
 // To invoke:
-// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
+// curl -i --location --request POST 'http://localhost:54321/functions/v1/read-storage' \
 //   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.625_WdcF3KHqz5amU0x2X5WWHP-OEs_4qj0ssLNHzTs' \
 //   --header 'Content-Type: application/json' \
 //   --data '{"name":"Functions"}'
