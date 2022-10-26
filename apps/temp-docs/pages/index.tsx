85260c22f0e19bbe6ce7d84620583cca458fbc17
Joshen Lim <joshenlimek@gmail.com>
2022-10-26 23:01:28 +0700

    Fill up side bar nav data for other pages and refactor

--- apps/temp-docs/pages/index.tsx
+++ apps/temp-docs/pages/index.tsx
@@ -2,7 +2,7 @@ import { MDXProvider } from '@mdx-js/react'
 import { MDXRemote } from 'next-mdx-remote'
 import { serialize } from 'next-mdx-remote/serialize'
 import components from '~/components'
-import { menuItems } from '~/components/nav/Nav.constants'
+import { menuItems } from '~/components/Navigation/Nav.constants'
 
 import Layout from '~/layouts/Default'
 import { getDocsBySlug } from '../lib/docs'
