1cc91a989ece299d1cd624ba9a22376d9890d788
Alaister Young <a@alaisteryoung.com>
2022-10-26 16:47:54 +1100

    fix infinite sign in redirect loop

--- studio/pages/500.tsx
+++ studio/pages/500.tsx
@@ -1,17 +1,27 @@
 import { NextPage } from 'next'
+import { useRouter } from 'next/router'
 import Link from 'next/link'
 import Image from 'next/image'
 import { Button } from 'ui'
 import { observer } from 'mobx-react-lite'
 
 import { useStore } from 'hooks'
+import { auth, STORAGE_KEY } from 'lib/gotrue'
 
-const Error500: NextPage = ({}) => {
+const Error500: NextPage = () => {
+  const router = useRouter()
   const { ui } = useStore()
   const { theme } = ui
 
+  const onClickLogout = async () => {
+    await auth.signOut()
+    localStorage.removeItem(STORAGE_KEY)
+    await router.push('/sign-in')
+    router.reload()
+  }
+
   return (
-    <div className="relative mx-auto flex h-full w-full flex-col items-center justify-center space-y-6">
+    <div className="relative mx-auto flex flex-1 w-full flex-col items-center justify-center space-y-6">
       <div className="absolute top-0 mx-auto w-full max-w-7xl px-8 pt-6 sm:px-6 lg:px-8">
         <nav className="relative flex items-center justify-between sm:h-10">
           <div className="flex flex-shrink-0 flex-grow items-center lg:flex-grow-0">
@@ -36,11 +46,15 @@ const Error500: NextPage = ({}) => {
         </p>
       </div>
       <div className="flex items-center space-x-4">
-        <Link href="/projects">
-          <a>
-            <Button>Head back</Button>
-          </a>
-        </Link>
+        {router.pathname !== '/projects' ? (
+          <Link href="/projects">
+            <a>
+              <Button>Head back</Button>
+            </a>
+          </Link>
+        ) : (
+          <Button onClick={onClickLogout}>Head back</Button>
+        )}
         <Link href="/support/new">
           <a>
             <Button type="secondary">Submit a support request</Button>
