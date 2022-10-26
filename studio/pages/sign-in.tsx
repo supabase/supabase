7bd4a9896ba080f22e9a103ca9833b32db4cc6ac
Alaister Young <a@alaisteryoung.com>
2022-10-26 22:47:13 +1100

    design feedback

--- studio/pages/sign-in.tsx
+++ studio/pages/sign-in.tsx
@@ -46,18 +46,20 @@ const SignInPage: NextPageWithLayout = () => {
             <div className="w-full border-t border-scale-700" />
           </div>
           <div className="relative flex justify-center text-sm">
-            <span className="bg-scale-200 px-2">Or</span>
+            <span className="bg-scale-200 px-2 text-sm text-scale-1200">or</span>
           </div>
         </div>
 
         <SignInForm />
       </div>
 
-      <div className="my-8 self-center">
+      <div className="my-8 self-center text-sm">
         <div>
           <span className="text-scale-1000">Don't have an account?</span>{' '}
           <Link href="/sign-up">
-            <a className="underline hover:text-scale-1100">Sign Up Now</a>
+            <a className="underline text-scale-1200 hover:text-scale-1100 transition">
+              Sign Up Now
+            </a>
           </Link>
         </div>
       </div>
