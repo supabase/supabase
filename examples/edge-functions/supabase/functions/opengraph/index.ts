d536a899a0071325b7d9c1ace5a0e870b7575b2b
thorwebdev <thor@supabase.io>
2022-10-27 15:14:43 +0800

    chore: cleanup

--- examples/edge-functions/supabase/functions/opengraph/index.ts
+++ examples/edge-functions/supabase/functions/opengraph/index.ts
@@ -1,6 +1,12 @@
-import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
 
-import {handler} from './handler.tsx'
+import { serve } from 'https://deno.land/std@0.140.0/http/server.ts'
+
+import { handler } from './handler.tsx'
 
 // @ts-ignore
-serve(handler);
+serve(handler)
+
+// To deploy: supabase functions deploy opengraph --no-verify-jwt
