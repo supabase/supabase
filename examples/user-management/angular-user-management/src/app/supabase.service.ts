2ee839010238007250daaadd8f8fd3e6d9e631ea
thorwebdev <thor@supabase.io>
2022-10-28 10:35:15 +0800

    fix: angular types

--- examples/user-management/angular-user-management/src/app/supabase.service.ts
+++ examples/user-management/angular-user-management/src/app/supabase.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
 import {
   AuthChangeEvent,
   AuthSession,
@@ -6,74 +6,61 @@ import {
   Session,
   SupabaseClient,
   User,
-} from '@supabase/supabase-js';
-import { environment } from 'src/environments/environment';
-import { Database } from 'src/schema';
+} from '@supabase/supabase-js'
+import { environment } from 'src/environments/environment'
+import { Database } from 'src/schema'
 
-export interface Profile {
-  id?: string;
-  username: string;
-  website: string;
-  avatar_url: string;
-}
+export type Profile = Database['public']['Tables']['profiles']['Row']
+export type ProfileUpsert = Database['public']['Tables']['profiles']['Insert']
 
 @Injectable({
   providedIn: 'root',
 })
 export class SupabaseService {
-  private supabase: SupabaseClient<Database>;
-  _session: AuthSession | null = null;
+  private supabase: SupabaseClient<Database>
+  _session: AuthSession | null = null
 
   constructor() {
-    this.supabase = createClient<Database>(
-      environment.supabaseUrl,
-      environment.supabaseKey
-    );
+    this.supabase = createClient<Database>(environment.supabaseUrl, environment.supabaseKey)
   }
 
   get session() {
     this.supabase.auth.getSession().then(({ data }) => {
-      this._session = data.session;
-    });
-    return this._session;
+      this._session = data.session
+    })
+    return this._session
   }
 
   profile(user: User) {
-    return this.supabase
-      .from('profiles')
-      .select(`username, website, avatar_url`)
-      .eq('id', user.id)
-      .single();
+    return this.supabase.from('profiles').select(`*`).eq('id', user.id).single()
   }
 
-  authChanges(
-    callback: (event: AuthChangeEvent, session: Session | null) => void
-  ) {
-    return this.supabase.auth.onAuthStateChange(callback);
+  authChanges(callback: (event: AuthChangeEvent, session: Session | null) => void) {
+    return this.supabase.auth.onAuthStateChange(callback)
   }
 
   signIn(email: string) {
-    return this.supabase.auth.signInWithOtp({ email });
+    return this.supabase.auth.signInWithOtp({ email })
   }
 
   signOut() {
-    return this.supabase.auth.signOut();
+    return this.supabase.auth.signOut()
   }
 
-  updateProfile(profile: Profile) {
+  updateProfile(profile: ProfileUpsert) {
     const update = {
       ...profile,
-      updated_at: new Date(),
-    };
+      updated_at: new Date().toISOString(),
+    }
 
-    return this.supabase.from('profiles').upsert(update);
+    return this.supabase.from('profiles').upsert(update)
   }
 
   downLoadImage(path: string) {
-    return this.supabase.storage.from('avatars').download(path);
+    return this.supabase.storage.from('avatars').download(path)
   }
 
   uploadAvatar(filePath: string, file: File) {
-    return this.supabase.storage.from('avatars').upload(filePath, file);
+    return this.supabase.storage.from('avatars').upload(filePath, file)
   }
 }
