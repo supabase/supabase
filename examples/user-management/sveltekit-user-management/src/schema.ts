41fd1f249539e186af91e3161e33b170db1d3c8c
thorwebdev <thor@supabase.io>
2022-10-28 09:00:38 +0800

    chore: update user-management schema.

--- examples/user-management/sveltekit-user-management/src/schema.ts
+++ examples/user-management/sveltekit-user-management/src/schema.ts
@@ -8,6 +8,7 @@ export interface Database {
 					id: string
 					updated_at: string | null
 					username: string | null
+					full_name: string | null
 					avatar_url: string | null
 					website: string | null
 				}
@@ -15,6 +16,7 @@ export interface Database {
 					id: string
 					updated_at?: string | null
 					username?: string | null
+					full_name?: string | null
 					avatar_url?: string | null
 					website?: string | null
 				}
@@ -22,11 +24,20 @@ export interface Database {
 					id?: string
 					updated_at?: string | null
 					username?: string | null
+					full_name?: string | null
 					avatar_url?: string | null
 					website?: string | null
 				}
 			}
 		}
-		Functions: {}
+		Views: {
+			[_ in never]: never
+		}
+		Functions: {
+			[_ in never]: never
+		}
+		Enums: {
+			[_ in never]: never
+		}
 	}
 }
