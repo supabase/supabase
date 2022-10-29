4f2b2214bdaa57791c1c50fef6b531dc75af57b1
Joshen Lim <joshenlimek@gmail.com>
2022-10-29 12:09:39 +0700

    Midway styling docs

--- apps/temp-docs/pages/_app.tsx
+++ apps/temp-docs/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from '../components/Providers'
 import { SearchProvider } from '~/components/Search'
 import '../styles/main.scss'
+import '../styles/algolia-search.scss'
 import '../styles/prism-okaidia.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
