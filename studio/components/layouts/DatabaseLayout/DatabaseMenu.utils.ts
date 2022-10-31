74dbb3e3621619e4f8084f1fd72d7f81c50340dc
Joshen Lim <joshenlimek@gmail.com>
2022-11-01 00:30:05 +0700

    Tiny fixes

--- studio/components/layouts/DatabaseLayout/DatabaseMenu.utils.ts
+++ studio/components/layouts/DatabaseLayout/DatabaseMenu.utils.ts
@@ -12,6 +12,7 @@ export const generateDatabaseMenu = (project?: Project): ProductMenuGroup[] => {
 
   return [
     {
+      title: 'Database',
       items: [
         { name: 'Tables', key: 'tables', url: `/project/${ref}/database/tables`, items: [] },
         {
