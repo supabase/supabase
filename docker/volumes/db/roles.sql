3a5f803a867b2b45abb62e2066d6a64f8043b485
Qiao Han <qiao@supabase.io>
2022-10-31 19:28:21 +0800

    chore: update default role passwords

---
+++ docker/volumes/db/roles.sql
@@ -0,0 +1,8 @@
+-- NOTE: change to your own passwords for production environments
+\set pgpass `echo "$PGPASSWORD"`
+
+ALTER USER supabase_admin WITH PASSWORD :'pgpass';
+ALTER USER authenticator WITH PASSWORD :'pgpass';
+ALTER USER pgbouncer WITH PASSWORD :'pgpass';
+ALTER USER supabase_auth_admin WITH PASSWORD :'pgpass';
+ALTER USER supabase_storage_admin WITH PASSWORD :'pgpass';
