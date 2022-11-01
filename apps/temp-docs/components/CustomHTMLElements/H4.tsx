98277bce3d56e982abaed34db56ffdd0a042e75f
Joshen Lim <joshenlimek@gmail.com>
2022-11-01 23:35:44 +0700

    Update tabs for with-nextjs.mdx

--- apps/temp-docs/components/CustomHTMLElements/H4.tsx
+++ apps/temp-docs/components/CustomHTMLElements/H4.tsx
@@ -1,17 +1,17 @@
-import { getAnchor, removeAnchor } from './CustomMDX.utils'
+import { getAnchor, removeAnchor } from './CustomHTMLElements.utils'
 
-const H3 = ({ children }) => {
+const H4 = ({ children }) => {
   const anchor = getAnchor(children)
   const link = `#${anchor}`
   return (
-    <h3 id={anchor} className="group flex gap-1 items-center">
+    <h4 id={anchor} className="group flex gap-1 items-center">
       {removeAnchor(children)}
       {anchor && (
         <a href={link} className="opacity-0 group-hover:opacity-100 transition">
           #
         </a>
       )}
-    </h3>
+    </h4>
   )
 }
-export default H3
+export default H4
