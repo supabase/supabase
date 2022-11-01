25fbb56967fd6e483f5cad02ccd8e87af314603f
Qiao Han <qiao@supabase.io>
2022-11-01 12:19:55 +0800

    chore: do not update su role

--- docker/volumes/db/roles.sql
+++ docker/volumes/db/roles.sql
@@ -1,7 +1,6 @@
 -- NOTE: change to your own passwords for production environments
 \set pgpass `echo "$PGPASSWORD"`
 
-ALTER USER supabase_admin WITH PASSWORD :'pgpass';
 ALTER USER authenticator WITH PASSWORD :'pgpass';
 ALTER USER pgbouncer WITH PASSWORD :'pgpass';
 ALTER USER supabase_auth_admin WITH PASSWORD :'pgpass';
