1993cae7170eb5a2d7e6ffc44c5d9dfcd66c2ce0
Terry Sutton <saltcod@gmail.com>
2022-11-01 16:37:37 -0230

    Add scrollspying to TOC headings

--- apps/temp-docs/components/CustomMDX/H3.tsx
+++ apps/temp-docs/components/CustomMDX/H3.tsx
@@ -1,17 +1,57 @@
 import { getAnchor, removeAnchor } from './CustomMDX.utils'
 
-const H2 = ({ children }) => {
+import { useInView } from 'react-intersection-observer'
+
+// move this to a utils file if looks good
+function highlightSelectedTocItem(id) {
+  const tocMenuItems = document.querySelectorAll('.toc-menu a')
+
+  // remove all active items first
+  const currentActiveItem = document.querySelector('.toc-menu .toc__menu-item--active')
+  currentActiveItem?.classList.remove('toc__menu-item--active')
+
+  // Add active class to the current item
+  tocMenuItems.forEach((item) => {
+    // @ts-ignore
+    if (item.href.split('#')[1] === id) {
+      item.classList.add('toc__menu-item--active')
+    }
+  })
+}
+
+const H3 = ({ children }) => {
+  const { ref, inView, entry } = useInView({
+    /* Optional options */
+    threshold: 0,
+    onChange: (inView, entry) => {
+      highlightSelectedTocItem(entry.target.id)
+    },
+  })
+
+  // useEffect(() => {
+  //   if (entry?.target) {
+  //     //highlightSelectedTocItem(entry.target.id)
+  //   }
+
+  //   //console.log(entry?.target?.id)
+  // }, [inView])
+
   const anchor = getAnchor(children)
   const link = `#${anchor}`
+
   return (
-    <h2 id={anchor} className="group flex gap-1 items-center scroll-mt-24">
+    <h3
+      id={anchor}
+      ref={ref}
+      className={`group flex gap-1 items-center scroll-mt-24 ${inView ? 'visible' : 'not-visible'}`}
+    >
       {removeAnchor(children)}
       {anchor && (
         <a href={link} className="opacity-0 group-hover:opacity-100 transition">
           #
         </a>
       )}
-    </h2>
+    </h3>
   )
 }
-export default H2
+export default H3
