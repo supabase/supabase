2e0ade652d361392b0e3a897bb93bba38fe8cc1e
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-11-04 04:36:22 +0800

    added scroll highlighting

--- apps/docs/pages/ref/[...slug].tsx
+++ apps/docs/pages/ref/[...slug].tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from 'react'
 // pages/index.js
 
 import fs from 'fs'
@@ -14,43 +15,158 @@ import jsSpec from '~/../../spec/supabase_js_v2_temp.yaml'
 import CodeBlock from '~/components/CodeBlock/CodeBlock'
 import { Tabs } from '~/../../packages/ui'
 
-export default function Post(props) {
+import { useRouter } from 'next/router'
+
+const marginTop = 300
+export default function Ref(props) {
+  const myRef = useRef(null)
+
+  const [offsetY, setOffsetY] = useState(0)
+  const [sections, setSections] = useState([])
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+    setOffsetY(0)
+  }, [])
+
+  useEffect(() => {
+    const els: HTMLElement[] = Array.from(document.querySelectorAll('div.ref-container'))
+
+    const allSections = els.map((el: HTMLElement, index: number) => {
+      const { top: boundingTop } = el.getBoundingClientRect()
+
+      return {
+        topic: el.getAttribute('id')!,
+        boundingTop,
+        isActive: index === 0,
+      }
+    })
+
+    console.log('allSections', allSections)
+    setSections(allSections)
+  }, [])
+
+  useEffect(() => {
+    if (sections.length <= 1) return
+
+    const onScroll = () => {
+      setOffsetY(window.pageYOffset)
+    }
+    window.addEventListener('scroll', onScroll)
+
+    return () => window.removeEventListener('scroll', onScroll)
+  }, [sections])
+
+  useEffect(() => {
+    if (sections.length === 0) return
+
+    if (sections.length === 1) {
+      sections[0].isActive = true
+      return
+    }
+
+    sections.forEach((section, index) => {
+      if (index === 0) {
+        section.isActive = sections[index + 1].boundingTop > offsetY + marginTop
+      } else {
+        if (sections[index + 1]) {
+          section.isActive =
+            sections[index + 1].boundingTop > offsetY + marginTop &&
+            sections[index].boundingTop <= offsetY + marginTop
+        } else {
+          section.isActive = sections[index].boundingTop <= offsetY + marginTop
+        }
+      }
+    })
+  }, [sections, offsetY])
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      // console.log('entries', entries)
+      const entry = entries[0]
+      // console.log('entry', entry)
+    })
+    // console.log('myRef', myRef.current)
+    observer.observe(myRef.current)
+  }, [])
+
+  const router = useRouter()
+
+  function updateUrl(key) {
+    // router.replace(
+    //   {
+    //     pathname: `/ref/js/${key}`,
+    //     // query: { sortBy: 'price' },
+    //   }
+    //   // undefined,
+    //   // { shallow: true }
+    // )
+    router.replace(
+      {
+        pathname: `/ref/js/${key}`,
+        // query: { sortBy: 'price' },
+      },
+      undefined,
+      { scroll: false }
+    )
+  }
+
   return (
-    <div className="my-32 mx-5 mx-auto">
-      <div className="flex flex-col gap-32 max-w-7xl mx-auto">
-        {props.docs.map((x) => {
-          return (
-            <div className="grid grid-cols-2 gap-32">
-              <div className="prose" key={x.id}>
-                <h1 className="text-3xl not-prose">{x.title ?? x.id}</h1>
-                {x.content && (
-                  <div className="prose">
-                    <MDXRemote {...x.content} components={components} />
-                  </div>
-                )}
-                {jsSpec.pages[x.id].notes && (
-                  <div>
-                    <ReactMarkdown>{jsSpec.pages[x.id].notes}</ReactMarkdown>
-                  </div>
-                )}
+    <div>
+      <div className="flex my-16">
+        <div className="px-10 fixed">
+          {sections.map((x, i) => {
+            return (
+              <div
+                className={[
+                  'text-sm',
+                  sections && sections[i].isActive ? 'text-brand-900 text-scale-900' : '',
+                ].join(' ')}
+                key={i}
+              >
+                {x.topic}
               </div>
-              <div className="w-full">
-                {jsSpec.pages[x.id].examples && (
-                  <Tabs
-                    defaultActiveId={jsSpec.pages[x.id].examples[0].name}
-                    size="small"
-                    type="underlined"
-                    scrollable
-                  >
-                    {jsSpec.pages[x.id].examples &&
-                      jsSpec.pages[x.id].examples.map((x, i) => {
-                        return (
-                          <Tabs.Panel id={x.name} label={x.name}>
-                            <CodeBlock lang="js" className="useless-code-block-class">
-                              {x.js && x.js.replace('```', '').replace('js', '')}
-                            </CodeBlock>
-                            <CodeBlock lang="js" className="useless-code-block-class">
-                              {`{ 
+            )
+          })}
+        </div>
+
+        <div className="ml-64 w-full">
+          <div className="flex flex-col gap-32 mx-auto max-w-5xl" ref={myRef}>
+            {props.docs.map((x) => {
+              return (
+                <div className="grid grid-cols-2 gap-32 ref-container" id={x.id}>
+                  <div className="prose" key={x.id}>
+                    <h1 className="text-3xl not-prose" onClick={() => updateUrl(x.id)}>
+                      {x.title ?? x.id}
+                    </h1>
+                    {x.content && (
+                      <div className="prose">
+                        <MDXRemote {...x.content} components={components} />
+                      </div>
+                    )}
+                    {jsSpec.pages[x.id].notes && (
+                      <div>
+                        <ReactMarkdown>{jsSpec.pages[x.id].notes}</ReactMarkdown>
+                      </div>
+                    )}
+                  </div>
+                  <div className="w-full">
+                    {jsSpec.pages[x.id].examples && (
+                      <Tabs
+                        defaultActiveId={jsSpec.pages[x.id].examples[0].name}
+                        size="small"
+                        type="underlined"
+                        scrollable
+                      >
+                        {jsSpec.pages[x.id].examples &&
+                          jsSpec.pages[x.id].examples.map((x, i) => {
+                            return (
+                              <Tabs.Panel id={x.name} label={x.name}>
+                                <CodeBlock lang="js" className="useless-code-block-class">
+                                  {x.js && x.js.replace('```', '').replace('js', '')}
+                                </CodeBlock>
+                                <CodeBlock lang="js" className="useless-code-block-class">
+                                  {`{ 
   data: [
     {
       id: 1,
@@ -69,16 +185,18 @@ export default function Post(props) {
   statusText: 'OK',
 }
                               `}
-                            </CodeBlock>
-                          </Tabs.Panel>
-                        )
-                      })}
-                  </Tabs>
-                )}
-              </div>
-            </div>
-          )
-        })}
+                                </CodeBlock>
+                              </Tabs.Panel>
+                            )
+                          })}
+                      </Tabs>
+                    )}
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
       </div>
     </div>
   )
