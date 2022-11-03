d085ff9a8c6c15481f6bb11f82d6bcacffb175f1
Terry Sutton <saltcod@gmail.com>
2022-11-03 09:15:06 -0230

    Add the actual files

---
+++ apps/docs/pages/_app.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import type { AppProps } from 'next/app'
+import { ThemeProvider } from '../components/Providers'
+import { SearchProvider } from '~/components/DocSearch'
+import '../styles/main.scss'
+import '../styles/docsearch.scss'
+import '../styles/algolia-search.scss'
+import '../styles/prism-okaidia.scss'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider>
+      <SearchProvider>
+        <Component {...pageProps} />
+      </SearchProvider>
+    </ThemeProvider>
+  )
+}
+
+export default MyApp
