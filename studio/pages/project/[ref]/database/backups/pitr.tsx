b1db797900f200f59a8bd8658625ea52dbf9e308
Joshen Lim <joshenlimek@gmail.com>
2022-11-09 17:07:28 +0800

    Update CTA for PITR on free plan

--- studio/pages/project/[ref]/database/backups/pitr.tsx
+++ studio/pages/project/[ref]/database/backups/pitr.tsx
@@ -1,7 +1,6 @@
-import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { observer } from 'mobx-react-lite'
-import { IconInfo, Tabs } from 'ui'
+import { Tabs } from 'ui'
 import { PermissionAction } from '@supabase/shared-types/out/constants'
 
 import { NextPageWithLayout } from 'types'
@@ -11,6 +10,7 @@ import Loading from 'components/ui/Loading'
 import NoPermission from 'components/ui/NoPermission'
 import { PITRNotice, PITRSelection } from 'components/interfaces/Database/Backups/PITR'
 import BackupsError from 'components/interfaces/Database/Backups/BackupsError'
+import UpgradeToPro from 'components/ui/UpgradeToPro'
 
 const DatabasePhysicalBackups: NextPageWithLayout = () => {
   const { ui } = useStore()
@@ -57,18 +57,11 @@ const PITR = () => {
   if (error) return <BackupsError />
   if (!isEnabled)
     return (
-      <div className="block w-full rounded border border-gray-400 border-opacity-50 bg-gray-300 p-3">
-        <div className="flex space-x-3">
-          <IconInfo size={20} strokeWidth={1.5} />
-          <p className="text-sm">
-            Point in time backups is an Enterprise feature. Reach out to us{' '}
-            <Link href={`/support/new?ref=${ref}&category=sales`}>
-              <a className="text-brand-900">here</a>
-            </Link>{' '}
-            if you're interested!
-          </p>
-        </div>
-      </div>
+      <UpgradeToPro
+        projectRef={ref}
+        primaryText="Free Plan does not include point in time recovery."
+        secondaryText="Please upgrade to the Pro plan to enable point in time recovery for your project."
+      />
     )
 
   return (
