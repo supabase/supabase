2d67782e7b18c6e71a513fce3c8493d14f412430
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-11-15 19:24:34 +0800

    updated nav bar

--- apps/docs/layouts/SiteLayout.tsx
+++ apps/docs/layouts/SiteLayout.tsx
@@ -10,9 +10,9 @@ const Layout = ({ children }) => {
       <main>
         <img
           src={`${router.basePath}/img/gradient-bg.png`}
-          className="absolute left-0 w-[520px] top-0"
+          className="absolute left-0 w-[520px] top-0 -z-10"
         />
-        <div className="max-w-[1400px] mx-auto">
+        <div className="mx-auto sticky top-0 z-10">
           <TopNavBar />
         </div>
         <div className="grid grid-cols-12 opacity-100 duration-100 max-w-[1400px] mx-auto py-16 gap-4 px-5">
