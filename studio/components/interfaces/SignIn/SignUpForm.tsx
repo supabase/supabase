905bded8d2926cb9d04392268902a443fe0b0179
Joshen Lim <joshenlimek@gmail.com>
2022-11-15 18:48:20 +0800

    Update sign in animation

--- studio/components/interfaces/SignIn/SignUpForm.tsx
+++ studio/components/interfaces/SignIn/SignUpForm.tsx
@@ -1,12 +1,12 @@
+import * as yup from 'yup'
+import { useRef, useState } from 'react'
 import HCaptcha from '@hcaptcha/react-hcaptcha'
+import { Alert, Button, Form, IconEye, IconEyeOff, Input } from 'ui'
+
 import { useStore } from 'hooks'
 import { post } from 'lib/common/fetch'
 import { API_URL } from 'lib/constants'
 import { passwordSchema } from 'lib/schemas'
-import { useRouter } from 'next/router'
-import { useRef, useState } from 'react'
-import { Button, Form, IconEye, IconEyeOff, Input } from 'ui'
-import * as yup from 'yup'
 import PasswordConditionsHelper from './PasswordConditionsHelper'
 
 const signUpSchema = passwordSchema.shape({
@@ -15,10 +15,10 @@ const signUpSchema = passwordSchema.shape({
 
 const SignUpForm = () => {
   const { ui } = useStore()
-  const router = useRouter()
-
-  const [captchaToken, setCaptchaToken] = useState<string | null>(null)
   const captchaRef = useRef<HCaptcha>(null)
+  const [isSubmitted, setIsSubmitted] = useState(false)
+  const [passwordHidden, setPasswordHidden] = useState(true)
+  const [captchaToken, setCaptchaToken] = useState<string | null>(null)
 
   const onSignUp = async ({ email, password }: { email: string; password: string }) => {
     const toastId = ui.setNotification({
@@ -45,13 +45,7 @@ const SignUpForm = () => {
     const error = response.error
 
     if (!error) {
-      ui.setNotification({
-        id: toastId,
-        category: 'success',
-        message: `Signed successfully! Please confirm your email before signing in`,
-      })
-
-      await router.push('/sign-up/success')
+      setIsSubmitted(true)
     } else {
       setCaptchaToken(null)
       captchaRef.current?.resetCaptcha()
@@ -64,76 +58,89 @@ const SignUpForm = () => {
     }
   }
 
-  const [passwordHidden, setPasswordHidden] = useState(true)
-
   return (
-    <Form
-      validateOnBlur
-      id="signUp-form"
-      initialValues={{ email: '', password: '' }}
-      validationSchema={signUpSchema}
-      onSubmit={onSignUp}
-    >
-      {({ isSubmitting, values }: { isSubmitting: boolean; values: any }) => {
-        return (
-          <div className="flex flex-col gap-4">
-            <Input
-              id="email"
-              name="email"
-              type="email"
-              label="Email"
-              placeholder="you@example.com"
-              disabled={isSubmitting}
-              autoComplete="email"
-            />
-
-            <Input
-              id="password"
-              name="password"
-              type={passwordHidden ? 'password' : 'text'}
-              label="Password"
-              placeholder="&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;"
-              disabled={isSubmitting}
-              autoComplete="new-password"
-              actions={
-                <Button
-                  icon={passwordHidden ? <IconEye /> : <IconEyeOff />}
-                  type="default"
-                  onClick={() => setPasswordHidden((prev) => !prev)}
-                />
-              }
-            />
-
-            <PasswordConditionsHelper password={values.password} />
-
-            <div className="self-center">
-              <HCaptcha
-                ref={captchaRef}
-                sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_SITE_KEY!}
-                size="invisible"
-                onVerify={(token) => {
-                  setCaptchaToken(token)
-                }}
-                onExpire={() => {
-                  setCaptchaToken(null)
-                }}
+    <div className="relative">
+      <div
+        className={`absolute top-0 duration-500 delay-300 w-full ${
+          isSubmitted ? 'opacity-100' : 'opacity-0'
+        }`}
+      >
+        <Alert className="w-full" withIcon variant="success" title="Successfully signed up">
+          Please check your email to confirm your account before signing in to the Supabase
+          dashboard
+        </Alert>
+      </div>
+      <Form
+        validateOnBlur
+        id="signUp-form"
+        className={`w-full py-1 transition-all overflow-y-hidden duration-500 ${
+          isSubmitted ? 'max-h-[100px] opacity-0 pointer-events-none' : 'max-h-[1000px] opacity-100'
+        }`}
+        initialValues={{ email: '', password: '' }}
+        validationSchema={signUpSchema}
+        onSubmit={onSignUp}
+      >
+        {({ isSubmitting, values }: { isSubmitting: boolean; values: any }) => {
+          return (
+            <div className="flex flex-col gap-4">
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                label="Email"
+                placeholder="you@example.com"
+                disabled={isSubmitting}
+                autoComplete="email"
               />
-            </div>
 
-            <Button
-              block
-              form="signUp-form"
-              htmlType="submit"
-              size="large"
-              disabled={isSubmitting}
-              loading={isSubmitting}
-            >
-              Sign Up
-            </Button>
-          </div>
-        )
-      }}
-    </Form>
+              <Input
+                id="password"
+                name="password"
+                type={passwordHidden ? 'password' : 'text'}
+                label="Password"
+                placeholder="&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;"
+                disabled={isSubmitting}
+                autoComplete="new-password"
+                actions={
+                  <Button
+                    icon={passwordHidden ? <IconEye /> : <IconEyeOff />}
+                    type="default"
+                    onClick={() => setPasswordHidden((prev) => !prev)}
+                  />
+                }
+              />
+
+              <PasswordConditionsHelper password={values.password} />
+
+              <div className="self-center">
+                <HCaptcha
+                  ref={captchaRef}
+                  sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_SITE_KEY!}
+                  size="invisible"
+                  onVerify={(token) => {
+                    setCaptchaToken(token)
+                  }}
+                  onExpire={() => {
+                    setCaptchaToken(null)
+                  }}
+                />
+              </div>
+
+              <Button
+                block
+                form="signUp-form"
+                htmlType="submit"
+                size="large"
+                disabled={isSubmitting}
+                loading={isSubmitting}
+              >
+                Sign Up
+              </Button>
+            </div>
+          )
+        }}
+      </Form>
+    </div>
   )
 }
 
