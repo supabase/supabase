420082ea8133aab655bfc4c2f31e3fc82e4e6f0d
Joshen Lim <joshenlimek@gmail.com>
2022-11-16 15:59:41 +0800

    Scaffold enc keys ui

--- studio/components/interfaces/Settings/Vault/index.ts
+++ studio/components/interfaces/Settings/Vault/index.ts
@@ -1,4 +1,6 @@
 import VaultToggle from './VaultToggle'
 import SecretsManagement from './SecretsManagement'
+import EncryptionKeySelector from './EncryptionKeySelector'
+import EncryptionKeysManagement from './EncryptionKeysManagement'
 
-export { VaultToggle, SecretsManagement }
+export { VaultToggle, SecretsManagement, EncryptionKeySelector, EncryptionKeysManagement }
