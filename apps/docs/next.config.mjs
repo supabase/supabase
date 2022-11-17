b7a5da62a5976503c59213f1e31f022d6d937a8c
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-11-17 18:50:35 +0800

    accept any path

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -28,7 +28,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: ':path*',
         headers: [
           {
             key: 'Strict-Transport-Security',
