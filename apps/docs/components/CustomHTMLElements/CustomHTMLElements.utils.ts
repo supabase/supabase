9afe0f34aa645c113a18e3991c91f555c55fc882
Terry Sutton <saltcod@gmail.com>
2022-11-21 16:50:35 -0330

    Fix custom heading links in mdx files

--- apps/docs/components/CustomHTMLElements/CustomHTMLElements.utils.ts
+++ apps/docs/components/CustomHTMLElements/CustomHTMLElements.utils.ts
@@ -1,11 +1,12 @@
 // Check if heading has custom anchor first, before forming the anchor based on the title
 export const getAnchor = (text: any): string | undefined => {
+  console.log({ text })
   if (typeof text === 'object') {
+    console.log('is object')
     if (Array.isArray(text)) {
-      const customAnchor = text.find(
-        (x) => typeof x === 'string' && x.includes('{#') && x.endsWith('}')
-      )
-      if (customAnchor !== undefined) return customAnchor.slice(3, customAnchor.indexOf('}'))
+      console.log('is array')
+      const customAnchor = text.find((x) => typeof x === 'string' && x.startsWith('#'))
+      if (customAnchor !== undefined) return customAnchor.slice(1)
 
       const formattedText = text
         .map((x) => {
@@ -33,8 +34,8 @@ export const getAnchor = (text: any): string | undefined => {
       return anchor
     }
   } else if (typeof text === 'string') {
-    if (text.includes('{#') && text.endsWith('}')) {
-      return text.slice(text.indexOf('{#') + 2, text.indexOf('}'))
+    if (text.startsWith('#')) {
+      return text.slice(1)
     } else {
       return text
         .toLowerCase()
@@ -48,9 +49,9 @@ export const getAnchor = (text: any): string | undefined => {
 
 export const removeAnchor = (text: any) => {
   if (typeof text === 'object' && Array.isArray(text)) {
-    return text.filter((x) => !(typeof x === 'string' && x.includes('{#') && x.endsWith('}')))
+    return text.filter((x) => !(typeof x === 'string' && x.startsWith('#')))
   } else if (typeof text === 'string') {
-    if (text.indexOf('{#') > 0) return text.slice(0, text.indexOf('{#'))
+    if (text.startsWith('#')) return text.slice(1)
     else return text
   }
   return text
