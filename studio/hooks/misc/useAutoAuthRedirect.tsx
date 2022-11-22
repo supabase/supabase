2454f57fec2fe0c0d596345109672ba784a01688
Alaister Young <a@alaisteryoung.com>
2022-11-22 08:05:21 +0000

    fixes vercel and database.new integrations
    by having both ?next=  (external) and ?redirectTo= (internal)

--- studio/hooks/misc/useAutoAuthRedirect.tsx
+++ studio/hooks/misc/useAutoAuthRedirect.tsx
@@ -1,17 +1,64 @@
-import { auth, getNextPath } from 'lib/gotrue'
+import { auth, getReturnToPath } from 'lib/gotrue'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import { useCallback, useEffect } from 'react'
 import { useSWRConfig } from 'swr'
 
-function useAutoAuthRedirect() {
+export function usePushNext() {
   const router = useRouter()
+
+  return useCallback(
+    async () => {
+      const searchParams = new URLSearchParams(location.search)
+
+      const hasNext = searchParams.has('next')
+
+      // preference the next param, as it's used for integration redirects
+      if (hasNext) {
+        const next = searchParams.get('next')!
+
+        // vercel integration
+        const isVercelIntegration = next.includes('https://vercel.com')
+        if (isVercelIntegration) {
+          searchParams.delete('returnTo')
+
+          return await router.push(`/vercel/integrate?${searchParams.toString()}`)
+        }
+
+        // database.new integration
+        if (next === 'new-project') {
+          searchParams.delete('next')
+          searchParams.delete('returnTo')
+          const remainingSearchParams = searchParams.toString()
+
+          return await router.push(
+            '/new/new-project' + (remainingSearchParams ? `?${remainingSearchParams}` : '')
+          )
+        }
+
+        return await router.push(
+          next + (searchParams.toString() ? `?${searchParams.toString()}` : '')
+        )
+      }
+
+      return await router.push(getReturnToPath())
+    },
+    // watch params so router.query will get updated
+    [router.query]
+  )
+}
+
+function useAutoAuthRedirect() {
+  const pushNext = usePushNext()
   const { cache } = useSWRConfig()
 
   useEffect(() => {
-    const returnTo = new URLSearchParams(location.search).get('next')
+    const searchParams = new URLSearchParams(location.search)
+    const hasReturnTo = searchParams.has('returnTo')
+    const hasNext = searchParams.has('next')
+    const shouldRedirect = hasReturnTo || hasNext
 
-    if (!returnTo) {
-      // If there's no returnTo, then we don't need to do anything
+    if (!shouldRedirect) {
+      // If there's no returnTo or next, then we don't need to do anything
       return
     }
 
@@ -32,7 +79,7 @@ function useAutoAuthRedirect() {
         // @ts-ignore
         cache.clear()
 
-        await router.push(getNextPath())
+        await pushNext()
       }
     })()
   }, [])
