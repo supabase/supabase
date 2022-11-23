6399663cc31144708d28514a21d4adb0bb7127f9
Stojan Dimitrovski <sdimitrovski@gmail.com>
2022-11-23 10:25:10 +0100

    feat: add enterprise login flow (#10269)
    * feat: add enterprise login flow
    
    * run prettier and clean up imports
    
    Co-authored-by: Alaister Young <a@alaisteryoung.com>

---
+++ studio/pages/sign-in-sso.tsx
@@ -0,0 +1,37 @@
+import SignInSSOForm from 'components/interfaces/SignIn/SignInSSOForm'
+import { SignInLayout } from 'components/layouts'
+import Link from 'next/link'
+import { NextPageWithLayout } from 'types'
+
+const SignInSSOPage: NextPageWithLayout = () => {
+  return (
+    <>
+      <div className="flex flex-col gap-5">
+        <SignInSSOForm />
+      </div>
+
+      <div className="my-8 self-center text-sm">
+        <div>
+          <span className="text-scale-1000">Don't have an enterprise account?</span>{' '}
+          <Link href="/sign-up">
+            <a className="underline text-scale-1200 hover:text-scale-1100 transition">
+              Sign Up Now
+            </a>
+          </Link>
+        </div>
+      </div>
+    </>
+  )
+}
+
+SignInSSOPage.getLayout = (page) => (
+  <SignInLayout
+    heading="Welcome back"
+    subheading="Sign in to your enterprise account"
+    logoLinkToMarketingSite={true}
+  >
+    {page}
+  </SignInLayout>
+)
+
+export default SignInSSOPage
