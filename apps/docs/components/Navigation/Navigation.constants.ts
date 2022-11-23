051dc11810d5ae1550ff98163c1e70f06ea13c51
awalias <antwilson@hotmail.co.uk>
2022-11-23 13:20:55 +0100

    fix: prettier fix

--- apps/docs/components/Navigation/Navigation.constants.ts
+++ apps/docs/components/Navigation/Navigation.constants.ts
@@ -294,7 +294,7 @@ export const menuItems: NavMenu = {
           name: 'Developer Tools',
           url: undefined,
           items: [
-            { name: 'Estuary', url: '/guides/integrations/estuary', items: [] },            
+            { name: 'Estuary', url: '/guides/integrations/estuary', items: [] },
             { name: 'pgMustard', url: '/guides/integrations/pgmustard', items: [] },
             { name: 'Prisma', url: '/guides/integrations/prisma', items: [] },
             { name: 'Sequin', url: '/guides/integrations/sequin', items: [] },
