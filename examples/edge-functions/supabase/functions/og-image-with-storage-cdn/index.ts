06a279479b2847db04836c56977ad8116b487d8e
thorwebdev <thor@supabase.io>
2022-11-23 16:41:23 +0800

    feat: add og image storage caching example.

--- examples/edge-functions/supabase/functions/og-image-with-storage-cdn/index.ts
+++ examples/edge-functions/supabase/functions/og-image-with-storage-cdn/index.ts
@@ -6,6 +6,6 @@ import { serve } from 'https://deno.land/std@0.140.0/http/server.ts'
 
 import { handler } from './handler.tsx'
 
-console.log(`Function "opengraph" up and running!`)
+console.log(`Function "og-image-with-storage-cdn" up and running!`)
 
 serve(handler)
