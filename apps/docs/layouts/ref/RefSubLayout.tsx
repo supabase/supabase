1584a831e06d118f968d7e631370feca7d2bc5ed
Terry Sutton <saltcod@gmail.com>
2022-11-24 14:06:05 -0330

    Add cli config parsing

--- apps/docs/layouts/ref/RefSubLayout.tsx
+++ apps/docs/layouts/ref/RefSubLayout.tsx
@@ -28,13 +28,13 @@ const Section: FC<ISectionContainer> = (props) => {
   return (
     <article key={props.id} id={props.id}>
       <header className="not-prose mb-12">
-        <h1
+        <h2
           className={['text-xl font-medium text-scale-1200', props.monoFont && 'font-mono'].join(
             ' '
           )}
         >
           {props.title}
-        </h1>
+        </h2>
       </header>
       <div className="grid grid-cols-2 ref-container gap-16">{props.children}</div>
     </article>
---
+++ apps/docs/pages/new/reference/cli/config.tsx
@@ -0,0 +1,89 @@
+// @ts-expect-error
+import cliConfig from '~/../../spec/cli_v1_config.yaml' assert { type: 'yml' }
+import CodeBlock from '~/components/CodeBlock/CodeBlock'
+import Options from '~/components/Options'
+import Param from '~/components/Params'
+import RefSubLayout from '~/layouts/ref/RefSubLayout'
+import ReactMarkdown from 'react-markdown'
+
+export type Link = {
+  name: string
+  link: string
+}
+
+export type Parameter = {
+  id: string
+  title: string
+  description: string
+  summary: string
+  tags?: string[]
+  links?: Link[]
+  subcommands?: []
+  usage?: string
+  required?: boolean
+  default?: boolean
+}
+
+// Parameters are grouped on the page by tag
+const TAGS = ['general', 'auth', 'api', 'database', 'dashboard', 'local']
+
+export default function CliUsage() {
+  return (
+    <div>
+      <div className="flex my-16">
+        <div className="w-full prose">
+          <h1 className="text-4xl mb-16">{cliConfig.info.title}</h1>
+          <ReactMarkdown>{cliConfig.info.description}</ReactMarkdown>
+          <div>
+            {TAGS.map((tag) =>
+              cliConfig.parameters
+                .filter((param: Parameter) => param.tags[0] === tag)
+                .map((parameter: Parameter, index) => (
+                  <div>
+                    {index === 0 && <h2 className="text-xl capitalize">{tag}</h2>}
+                    <div className="mt-8">
+                      <div>
+                        <h2 className="text-xl font-medium text-scale-1200 font-mono">
+                          <span className="mr-2">$</span>
+                          {parameter.title}
+                        </h2>
+                        <div className="grid" id={parameter.id}>
+                          <div className="border-b pb-8" key={parameter.id}>
+                            <div className=" mb-16">
+                              <p className="mb-4 scroll-mt-16 mt-0 text-scale-1100 text-base">
+                                <ReactMarkdown>{parameter.description}</ReactMarkdown>
+                              </p>
+                              <div className="grid gap-2">
+                                <div className="flex gap-2">
+                                  Required: <code>{parameter.required.toString()}</code>
+                                </div>
+                                <div className="flex gap-2">
+                                  Default:
+                                  <code>
+                                    {parameter.default ? parameter.default.toString() : 'None'}
+                                  </code>
+                                </div>
+                              </div>
+                            </div>
+                            {parameter.links &&
+                              parameter.links.map((link) => (
+                                <div>
+                                  <h3>See also:</h3>
+                                  <li>
+                                    <a href={link.link}>{link.name}</a>
+                                  </li>
+                                </div>
+                              ))}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
