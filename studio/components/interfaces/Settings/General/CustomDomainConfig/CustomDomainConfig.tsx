8e9914a1bebe38c345c7a0a0afdb6ae81d07f318
Joshen Lim <joshenlimek@gmail.com>
2022-11-25 15:23:39 +0800

    Update copy

--- studio/components/interfaces/Settings/General/CustomDomainConfig/CustomDomainConfig.tsx
+++ studio/components/interfaces/Settings/General/CustomDomainConfig/CustomDomainConfig.tsx
@@ -54,7 +54,7 @@ const CustomDomainConfig = () => {
                 projectRef={ref as string}
                 secondaryText={
                   tier === PRICING_TIER_PRODUCT_IDS.FREE
-                    ? 'To configure a custom domain for your project, please upgrade to the Pro plan and purchase the add-on'
+                    ? 'To configure a custom domain for your project, please upgrade to the Pro plan with the custom domains add-on selected'
                     : 'To configure a custom domain for your project, please purchase the add-on'
                 }
               />
