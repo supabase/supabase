4852df732d7ecd00357946bf017db96929928e06
Alaister Young <a@alaisteryoung.com>
2022-11-25 15:57:46 +0000

    move useProfile to react-query

---
+++ studio/data/profile/keys.ts
@@ -0,0 +1,3 @@
+export const profileKeys = {
+  profile: () => ['profile'] as const,
+}
---
+++ studio/data/profile/profile-query.ts
@@ -0,0 +1,52 @@
+import { useQuery, useQueryClient, UseQueryOptions } from '@tanstack/react-query'
+import { get } from 'lib/common/fetch'
+import { API_URL } from 'lib/constants'
+import { useCallback } from 'react'
+import { profileKeys } from './keys'
+
+export type Profile = {
+  id: number
+  auth0_id: string
+  primary_email: string
+  username: string
+  first_name: string
+  last_name: string
+  mobile: string | null
+  is_alpha_user: boolean
+  gotrue_id: string
+  free_project_limit: number
+}
+
+export type ProfileResponse = Profile
+
+export async function getProfile(signal?: AbortSignal) {
+  const response = await get(`${API_URL}/profile`, {
+    signal,
+  })
+  if (response.error) {
+    throw response.error
+  }
+
+  return response as ProfileResponse
+}
+
+export type ProfileData = Awaited<ReturnType<typeof getProfile>>
+export type ProfileError = unknown
+
+export const useProfileQuery = <TData = ProfileData>({
+  enabled = true,
+  ...options
+}: UseQueryOptions<ProfileData, ProfileError, TData> = {}) =>
+  useQuery<ProfileData, ProfileError, TData>(
+    profileKeys.profile(),
+    ({ signal }) => getProfile(signal),
+    options
+  )
+
+export const useProfilePrefetch = () => {
+  const client = useQueryClient()
+
+  return useCallback(() => {
+    client.prefetchQuery(profileKeys.profile(), ({ signal }) => getProfile(signal))
+  }, [])
+}
