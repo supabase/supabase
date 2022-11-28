393b12d8053b40b803d23cc10a40e9718306b9c8
winwangqi <winwangqi@live.cn>
2022-11-28 16:51:11 +0800

    fix: POSTGRES_PASSWORD key
    Fix POSTGRES_PASSWORD key, consistent with /docker/.env.example POSTGRES_PASSWORD

--- docker/volumes/db/roles.sql
+++ docker/volumes/db/roles.sql
@@ -1,5 +1,5 @@
 -- NOTE: change to your own passwords for production environments
-\set pgpass `echo "$PGPASSWORD"`
+\set pgpass `echo "$POSTGRES_PASSWORD"`
 
 ALTER USER authenticator WITH PASSWORD :'pgpass';
 ALTER USER pgbouncer WITH PASSWORD :'pgpass';
