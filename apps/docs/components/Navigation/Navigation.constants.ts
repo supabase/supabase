a50acf524ac856f14b95a4194151e86235b7c69e
dannykng <danny@supabase.io>
2022-11-28 16:30:45 -0800

    Move examples back to Functions overview

--- apps/docs/components/Navigation/Navigation.constants.ts
+++ apps/docs/components/Navigation/Navigation.constants.ts
@@ -229,7 +229,6 @@ export const menuItems: NavMenu = {
         { name: 'Quickstart', url: '/guides/functions/quickstart', items: [] },
         { name: 'Auth', url: '/guides/functions/auth', items: [] },
         { name: 'Best Practices', url: '/guides/functions/best-practices', items: [] },
-        { name: 'Examples', url: '/guides/functions/examples', items: [] },
         { name: 'CI/CD Workflow', url: '/guides/functions/cicd-workflow', items: [] },
       ],
     },
