3d4fb09696d87413e0f0026314ce720c56bce216
Alaister Young <a@alaisteryoung.com>
2022-12-01 12:30:42 +0000

    remove wrapper editor form

--- studio/components/interfaces/Database/Wrappers/WrapperEditor.tsx
+++ studio/components/interfaces/Database/Wrappers/WrapperEditor.tsx
@@ -92,37 +92,31 @@ const WrapperEditor = ({ visible, wrapper, onCancel }: WrapperEditorProps) => {
           }
         }}
       >
-        <Form initialValues={initialValues} validate={validate} onSubmit={onSubmit}>
-          {({ isSubmitting, values }: any) => {
-            return (
-              <div className="py-6">
-                <SidePanel.Content>
-                  <div className="space-y-10">
-                    {wrapper.server.options.map((option) => (
-                      <Input
-                        key={option.name}
-                        id={option.name}
-                        name={option.name}
-                        label={option.label}
-                        defaultValue={option.defaultValue ?? ''}
-                        required={option.required ?? false}
-                        layout="horizontal"
-                      />
-                    ))}
+        <div className="py-6">
+          <SidePanel.Content>
+            <div className="space-y-10">
+              {wrapper.server.options.map((option) => (
+                <Input
+                  key={option.name}
+                  id={option.name}
+                  name={option.name}
+                  label={option.label}
+                  defaultValue={option.defaultValue ?? ''}
+                  required={option.required ?? false}
+                  layout="horizontal"
+                />
+              ))}
 
-                    {newTables.map((table, i) => (
-                      <div key={i}>{table.table_name}</div>
-                    ))}
+              {newTables.map((table, i) => (
+                <div key={i}>{table.table_name}</div>
+              ))}
 
-                    <Button type="default" onClick={() => setIsAddTableOpen(true)}>
-                      Add table
-                    </Button>
-                  </div>
-                </SidePanel.Content>
-              </div>
-            )
-          }}
-        </Form>
+              <Button type="default" onClick={() => setIsAddTableOpen(true)}>
+                Add table
+              </Button>
+            </div>
+          </SidePanel.Content>
+        </div>
       </SidePanel>
 
       <WrapperTableEditor
