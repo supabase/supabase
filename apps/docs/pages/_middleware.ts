0a76a31759996660eea24009531b74e19c4b93a8
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-12-02 10:07:35 +0800

    make it run again

--- apps/docs/pages/_middleware.ts
+++ apps/docs/pages/_middleware.ts
@@ -1,15 +1,15 @@
-// import { NextResponse } from 'next/server'
-// import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
-// // This function can be marked `async` if using `await` inside
-// export function middleware(request: NextRequest) {
-//   const headers = new Headers(request.headers)
-//   console.log('middleware?')
-//   console.log({ headers })
-//   //return NextResponse.redirect(new URL('/about-2', request.url))
-// }
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const headers = new Headers(request.headers)
+  console.log('middleware?')
+  console.log({ headers })
+  //return NextResponse.redirect(new URL('/about-2', request.url))
+}
 
 // See "Matching Paths" below to learn more
-// export const config = {
-//   matcher: '/about/:path*',
-// }
+export const config = {
+  matcher: '/about/:path*',
+}
