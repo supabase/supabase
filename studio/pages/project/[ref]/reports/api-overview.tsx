70f7edb4f0e4fbf1c9c088041332a86b112244c3
TzeYiing <ty@tzeyiing.com>
2022-12-05 17:52:55 +0800

    feat: refactored hook generation logic

--- studio/pages/project/[ref]/reports/api-overview.tsx
+++ studio/pages/project/[ref]/reports/api-overview.tsx
@@ -18,41 +18,38 @@ export const ApiOverviewPage: NextPageWithLayout = () => {
   const { ref } = router.query
   const config = PRESET_CONFIG[Presets.API_OVERVIEW]
   const hooks = hooksFactory(ref as string, config)
-  const [statusCodes, statusCodesHandlers] = hooks.statusCodes()
-  const [errorRate, errorRateHandlers] = hooks.errorRate()
-  const [requestPaths, requestPathsHandlers] = hooks.requestPaths()
-  const { isLoading, Layout } = usePresetReport(
-    [statusCodes, errorRate, requestPaths],
-    [statusCodesHandlers, errorRateHandlers, requestPathsHandlers]
-  )
+  const statusCodes = hooks.statusCodes()
+  const errorRates = hooks.errorRates()
+  const requestPaths = hooks.requestPaths()
+  const { isLoading, Layout } = usePresetReport([statusCodes, errorRates, requestPaths])
   return (
     <Layout title={config.title}>
       <div className="grid  lg:grid-cols-4">
         <ReportWidget
           isLoading={isLoading}
-          params={statusCodes.params}
+          params={statusCodes[0].params}
           className="col-span-4 col-start-1"
           title="API Status Codes"
           description="Distribution of API responses by status codes."
-          data={statusCodes.logData}
+          data={statusCodes[0].logData}
           renderer={renderStatusCodesChart}
         />
         <ReportWidget
           isLoading={isLoading}
-          params={errorRate.params}
+          params={errorRates[0].params}
           className="col-span-4 col-start-1"
           title="Error Rate"
           description="Percentage of API 5XX and 4XX error responses."
-          data={errorRate.logData}
+          data={errorRates[0].logData}
           renderer={renderErrorRateChart}
         />
         <ReportWidget
           isLoading={isLoading}
-          params={requestPaths.params}
+          params={requestPaths[0].params}
           className="col-span-4 col-start-1"
           title="Slow API Requests"
           description="Most frequently used requests, sorted by total querying time"
-          data={requestPaths.logData}
+          data={requestPaths[0].logData}
           renderer={renderRequestsPathsTable}
         />
       </div>
