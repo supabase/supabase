cfc0c742667c0d3bcdf83384a922f53c2a269fd5
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-12-06 20:15:24 +0800

    add collapsible

--- apps/docs/components/FooterHelpCallout.tsx
+++ apps/docs/components/FooterHelpCallout.tsx
@@ -3,7 +3,7 @@ const FooterHelpCallout = () => {
     <div className="min-w-full px-8 py-6 prose bg-scale-300 dark:bg-whiteA-200 rounded">
       <h5 className="text-sm m-0 text-scale-1100">Need some help?</h5>
       <p className="m-0 text-sm text-scale-900">
-        Not to worry, we're here to help with our specialist engineers. You can submit a support
+        Not to worry, we are here to help with our specialist engineers. You can submit a support
         ticket via our <a href="https://app.supabase.com/support/new">dashboard</a>.
       </p>
     </div>
