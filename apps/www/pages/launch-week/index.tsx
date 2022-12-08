f99a53a3a440715910bbc90d33e2ceac0af50cd2
Francisco Mazzoni <francisco@supabase.io>
2022-12-08 10:22:40 -0300

    bg fix, responsive first pass

--- apps/www/pages/launch-week/index.tsx
+++ apps/www/pages/launch-week/index.tsx
@@ -223,9 +223,9 @@ export default function launchweek() {
                   className="h-[79px]"
                   id={day1.d.toString()}
                 >
-                  <div className="h-[400px] flex">
+                  <div className="h-[400px] flex flex-col lg:flex-row">
                     <div
-                      className={`flex flex-col flex-1 justify-between border rounded-xl h-full relative after:opacity-25 xl:after:opacity-90 after:absolute after:bg-no-repeat after:bg-[right_20%_top_50px] after:bg-contain after:bg-[url('/images/launchweek/docs-update-bg.png')] after:top-0 after:right-0 after:bottom-0 after:left-0  p-14 text-2xl`}
+                      className={`flex flex-col flex-1 justify-between border rounded-xl h-full relative after:opacity-10 xl:after:opacity-90 after:absolute after:bg-no-repeat after:bg-[center_top_50px] lg:after:bg-[right_20%_top_50px]  after:bg-contain after:bg-[url('/images/launchweek/docs-update-bg.png')] after:top-0 after:right-0 after:bottom-0 after:left-0  p-14 text-2xl`}
                     >
                       <div className="flex items-center relative z-10">
                         <span>{day1.description}</span>
@@ -251,9 +251,9 @@ export default function launchweek() {
                   className="h-[79px]"
                   id={day2.d.toString()}
                 >
-                  <div className="h-[400px] flex gap-5">
+                  <div className="h-[400px] flex flex-col gap-5 lg:flex-row">
                     <div
-                      className={`flex-1 flex flex-col justify-between basis-2/3 border rounded-xl h-full bg-no-repeat p-14 text-2xl bg-[url('/images/launchweek/image-processing-bg.png')] bg-[right_28px_top_35px]`}
+                      className={`flex-1 flex flex-col justify-between basis-2/3 border rounded-xl h-full p-14 text-2xl bg-no-repeat bg-[url('/images/launchweek/image-processing-bg.png')] bg-[right_28px_top_35px] bg-contain`}
                     >
                       <div className="flex items-center">
                         <div>{day2.steps[0].title}</div>
@@ -266,7 +266,7 @@ export default function launchweek() {
                     <div
                       className={`flex-1 flex flex-col items-center justify-between basis-1/3 border rounded-xl h-full bg-no-repeat p-14 text-2xl bg-[url('/images/launchweek/cdn-caching-bg.png')] bg-[center_bottom]`}
                     >
-                      <div className="flex flex-col items-center gap-2">
+                      <div className="flex flex-col items-center gap-2 min-w-[300px]">
                         <Badge className="!bg-transparent h-fit ml-4 text-sm !py-1 !px-4 text-transparent bg-clip-text bg-gradient-to-r from-[#99bbab] to-[#396f55] border-[#598973] dark:from-white dark:to-[#1a7a4ca1] dark:border-[#598973]">
                           New
                         </Badge>
@@ -291,7 +291,7 @@ export default function launchweek() {
                   className="h-[79px]"
                   id={day3.d.toString()}
                 >
-                  <div className="h-[400px] flex gap-5">
+                  <div className="h-[400px] flex flex-col gap-5 lg:flex-row">
                     <div
                       className={`flex flex-col justify-between flex-1 basis-2/3 border rounded-xl h-full bg-no-repeat p-14 text-2xl`}
                     >
@@ -307,7 +307,7 @@ export default function launchweek() {
                       className={`flex-1 flex flex-col items-center justify-between basis-1/3 border rounded-xl h-full bg-no-repeat p-14 text-2xl bg-[url('/images/launchweek/encryption-bg.png')] bg-[center_center] shadow-[inset_0px_130px_50px_-52p_rgb(10,31,30)]`}
                     >
                       {/* inset 0px 130px 50px -52px #121f1e; */}
-                      <div className="flex flex-col items-center gap-2">
+                      <div className="flex flex-col items-center gap-2 min-w-[300px]">
                         <Badge className="!bg-transparent h-fit ml-4 text-sm !py-1 !px-4 text-transparent bg-clip-text bg-gradient-to-r from-[#99bbab] to-[#396f55] border-[#598973] dark:from-white dark:to-[#1a7a4ca1] dark:border-[#598973]">
                           New
                         </Badge>
@@ -359,7 +359,7 @@ export default function launchweek() {
                   className="h-[79px]"
                   id={day5.d.toString()}
                 >
-                  <div className="h-[400px] flex gap-5">
+                  <div className="h-[400px] flex flex-col gap-5 lg:flex-row">
                     <div
                       className={`flex flex-col justify-between flex-1 basis-2/3 border rounded-xl h-full bg-no-repeat p-14 text-2xl bg-[url('/images/launchweek/wrappers-bg.svg')] bg-contain bg-[right]`}
                     >
@@ -374,7 +374,7 @@ export default function launchweek() {
                     <div
                       className={`flex-1 flex flex-col items-center justify-between basis-1/3 border rounded-xl h-full bg-no-repeat p-14 text-2xl`}
                     >
-                      <div className="flex flex-col items-center gap-2">
+                      <div className="flex flex-col items-center gap-2 min-w-[300px]">
                         <Badge className="!bg-transparent h-fit ml-4 text-sm !py-1 !px-4 text-transparent bg-clip-text bg-gradient-to-r from-[#99bbab] to-[#396f55] border-[#598973] dark:from-white dark:to-[#1a7a4ca1] dark:border-[#598973]">
                           New
                         </Badge>
