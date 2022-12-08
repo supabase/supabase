31b8273c991f12f674563e4f64cdc002f851e190
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-12-08 10:35:56 +0800

    remove console logs

--- apps/docs/hooks/useWindowLocation.ts
+++ apps/docs/hooks/useWindowLocation.ts
@@ -10,17 +10,13 @@ const useWindowLocation = (): Location | void => {
   }
 
   useEffect(() => {
-    //console.log('HOOK USEEFFECT FOR WINDOWWW')
     if (!isMounted) return
-    //console.log('I AM MOUNTED')
 
     if (!location) {
       setWindowLocation(window.location)
     }
 
     const handler = () => {
-      //console.log('handlinggg...')
-      //console.log(window.location)
       setWindowLocation(window.location)
     }
 
@@ -38,18 +34,4 @@ const useWindowLocation = (): Location | void => {
   return location
 }
 
-// const useWindowLocation = (): any => {
-//   const [location, setLocation] = useState((window && window.location) ?? undefined)
-//   const setWindowLocation = () => {
-//     console.log('Hello', window.location)
-//     setLocation(window.location)
-//   }
-//   useEffect(() => {
-//     window.addEventListener('popstate', setWindowLocation)
-//     return () => {
-//       window.removeEventListener('popstate', setWindowLocation)
-//     }
-//   }, [])
-//   return location
-// }
 export default useWindowLocation
