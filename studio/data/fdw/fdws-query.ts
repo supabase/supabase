092c578f7a9295177369aff2dbce65e326ea76c6
Alaister Young <a@alaisteryoung.com>
2022-12-09 20:30:41 +0000

    updates FDWs query to include tables and columns

--- studio/data/fdw/fdws-query.ts
+++ studio/data/fdw/fdws-query.ts
@@ -4,17 +4,49 @@ import { ExecuteSqlData, useExecuteSqlPrefetch, useExecuteSqlQuery } from '../sq
 export const getFDWsSql = () => {
   const sql = /* SQL */ `
     select
-      oid as id,
-      srvname as name
-    from pg_catalog.pg_foreign_server;
+      s.oid as "id",
+      s.srvname as "name",
+      (
+        select jsonb_agg(
+          jsonb_build_object(
+            'id', c.oid,
+            'name', c.relname,
+            'columns', (
+              select jsonb_agg(
+                jsonb_build_object(
+                  'name', a.attname,
+                  'type', pg_catalog.format_type(a.atttypid, a.atttypmod)
+                )
+              )
+              from pg_catalog.pg_attribute a
+              where a.attrelid = c.oid and a.attnum > 0 and not a.attisdropped
+            )
+          )
+        )
+        from pg_catalog.pg_class c
+        where c.oid = any (select t.ftrelid from pg_catalog.pg_foreign_table t where t.ftserver = s.oid)
+      ) as "tables"
+    from pg_catalog.pg_foreign_server s;
   `
 
   return sql
 }
 
+export type FDWColumn = {
+  name: string
+  type: string
+}
+
+export type FDWTable = {
+  id: string
+  name: string
+  columns: FDWColumn[]
+}
+
 export type FDW = {
   id: string
   name: string
+  tables: FDWTable[]
 }
 
 export type FDWsResponse = {
