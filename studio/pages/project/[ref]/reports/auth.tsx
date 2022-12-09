513317e6049dfc3d46bd7aa5cd49920477e2e8b3
TzeYiing <ty@tzeyiing.com>
2022-12-09 18:16:08 +0800

    feat: add cumulative users chart

--- studio/pages/project/[ref]/reports/auth.tsx
+++ studio/pages/project/[ref]/reports/auth.tsx
@@ -10,6 +10,7 @@ import ReportWidget from 'components/interfaces/Reports/ReportWidget'
 import {
   renderDailyActiveUsers,
   renderNewUsers,
+  renderCumulativeUsers,
 } from 'components/interfaces/Reports/renderers/AuthRenderers'
 
 export const ApiBotsPage: NextPageWithLayout = () => {
@@ -19,19 +20,28 @@ export const ApiBotsPage: NextPageWithLayout = () => {
   const hooks = hooksFactory(ref as string, config)
   const dailyActiveUsers = hooks.dailyActiveUsers()
   const newUsers = hooks.newUsers()
-  const { isLoading, Layout } = usePresetReport(
-    [dailyActiveUsers, newUsers],
-  )
+  const cumulativeUsers = hooks.cumulativeUsers()
+
+  const { isLoading, Layout } = usePresetReport([dailyActiveUsers, newUsers, cumulativeUsers])
   return (
     <Layout title={config.title}>
-      <div className="grid  lg:grid-cols-4">
+      <div className="grid  lg:grid-cols-4 gap-4">
+        <ReportWidget
+          isLoading={isLoading}
+          params={cumulativeUsers[0].params}
+          className="col-span-2 col-start-1"
+          title="Users Over Time"
+          description="Daily count of confirmed users"
+          data={cumulativeUsers[0].data!}
+          renderer={renderCumulativeUsers}
+        />
         <ReportWidget
           isLoading={isLoading}
           params={dailyActiveUsers[0].params}
-          className="col-span-4 col-start-1"
+          className="col-span-2 col-start-3"
           title="Daily Active Users"
           description="The daily count of active authenticated users"
-          data={dailyActiveUsers[0].logData}
+          data={dailyActiveUsers[0].logData!}
           renderer={renderDailyActiveUsers}
         />
         <ReportWidget
@@ -40,7 +50,7 @@ export const ApiBotsPage: NextPageWithLayout = () => {
           className="col-span-4 col-start-1"
           title="Daily New Users"
           description="The daily count of new user signups"
-          data={newUsers[0].logData}
+          data={newUsers[0].logData!}
           renderer={renderNewUsers}
         />
       </div>
