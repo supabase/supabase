712474097e50c0819e4dc047f4bfa6f998ecae8a
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-12-14 00:55:47 +0800

    testing

--- studio/pages/projects.tsx
+++ studio/pages/projects.tsx
@@ -5,10 +5,13 @@ import Connecting from 'components/ui/Loading/Loading'
 import { useStore } from 'hooks'
 import { IS_PLATFORM } from 'lib/constants'
 import { observer } from 'mobx-react-lite'
+import { useState } from 'react'
 import { NextPageWithLayout } from 'types'
+import { Button, Popover, SidePanel } from 'ui'
 
 const ProjectsPage: NextPageWithLayout = () => {
   const { app } = useStore()
+  const [open, setOpen] = useState(false)
 
   return (
     <>
@@ -18,6 +21,44 @@ const ProjectsPage: NextPageWithLayout = () => {
         </div>
       ) : (
         <div className="py-4 px-5">
+          <Button onClick={() => setOpen(!open)}>Open sidebar</Button>
+          <SidePanel header="something" visible={!open} onCancel={() => setOpen(!open)}>
+            Something in here
+            <Popover
+              overlay={
+                <div className="max-h-[110px] w-[64px] overflow-y-auto">
+                  <p>World 1</p>
+                  <p>World 2</p>
+                  <p>World 3</p>
+                  <p>World 4</p>
+                  <p>World 5</p>
+                  <p>World 6</p>
+                  <p>World 7</p>
+                  <p>World 8</p>
+                </div>
+              }
+            >
+              Hello
+            </Popover>
+          </SidePanel>
+
+          <Popover
+            overlay={
+              <div className="max-h-[110px] w-[64px] overflow-y-auto">
+                <p>World 1</p>
+                <p>World 2</p>
+                <p>World 3</p>
+                <p>World 4</p>
+                <p>World 5</p>
+                <p>World 6</p>
+                <p>World 7</p>
+                <p>World 8</p>
+              </div>
+            }
+          >
+            Hello
+          </Popover>
+
           {IS_PLATFORM && (
             <div className="my-2">
               <div className="flex">
