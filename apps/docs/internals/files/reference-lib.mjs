69d41c45b654bcb68613ce40de586ff1bef51965
Terry Sutton <saltcod@gmail.com>
2022-12-15 15:56:30 -0330

    Cleanup

--- apps/docs/internals/files/reference-lib.mjs
+++ apps/docs/internals/files/reference-lib.mjs
@@ -2,22 +2,7 @@ import fs from 'fs'
 
 import yaml from 'js-yaml'
 import commonLibSections from '../../../../spec/common-client-libs-sections.json' assert { type: 'json' }
-
-function flattenSections(sections) {
-  var a = []
-  for (var i = 0; i < sections.length; i++) {
-    if (sections[i].id) {
-      // only push a section that has an id
-      // these are reserved for sidebar subtitles
-      a.push(sections[i])
-    }
-    if (sections[i].items) {
-      // if there are subitems, loop through
-      a = a.concat(flattenSections(sections[i].items))
-    }
-  }
-  return a
-}
+import { flattenSections } from '../helpers.mjs'
 
 const flatCommonLibSections = flattenSections(commonLibSections)
 
