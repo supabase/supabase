895050b270b59154c45ddcfc5ad8562ba2685a94
btoo <8883465+btoo@users.noreply.github.com>
2022-12-29 01:47:35 -0500

    fix several issues with nextjs-slack-clone:
    - fix session restoration in nextjs-slack-clone (setUser needs to be called from not only onAuthStateChange but also getSession)
    
    - fix message listener unmounting (supabase.removeChannel expects a RealtimeChannel, not a string)
    
    - fix fetchMessages order (ascending option must be provided within an options object)

--- examples/slack-clone/nextjs-slack-clone/lib/Store.js
+++ examples/slack-clone/nextjs-slack-clone/lib/Store.js
@@ -62,9 +62,9 @@ export const useStore = (props) => {
       .subscribe()
     // Cleanup on unmount
     return () => {
-      supabase.removeChannel('public:messages')
-      supabase.removeChannel('public:users')
-      supabase.removeChannel('public:channels')
+      supabase.removeChannel(supabase.channel('public:messages'))
+      supabase.removeChannel(supabase.channel('public:users'))
+      supabase.removeChannel(supabase.channel('public:channels'))
     }
   }, [])
 
@@ -179,7 +179,7 @@ export const fetchMessages = async (channelId, setState) => {
       .from('messages')
       .select(`*, author:user_id(*)`)
       .eq('channel_id', channelId)
-      .order('inserted_at', true)
+      .order('inserted_at', { ascending: true })
     if (setState) setState(data)
     return data
   } catch (error) {
