5334adcd528e7c003fc6a084ab31c69c9d35ec06
Alaister Young <a@alaisteryoung.com>
2023-01-05 15:59:05 +0000

    fix build

--- studio/components/interfaces/Billing/Subscription/Subscription.types.ts
+++ studio/components/interfaces/Billing/Subscription/Subscription.types.ts
@@ -14,5 +14,5 @@ export interface StripeProduct {
   price_id?: string
   unit_amount: number
   supabase_prod_id: string
-  key: 'FREE' | 'PRO' | 'ENTERPRISE'
+  key: 'FREE' | 'PRO' | 'TEAM' | 'ENTERPRISE'
 }
