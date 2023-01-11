b6b534b04335f0bcaaa610bcb8b0334adb808ff6
Joshen Lim <joshenlimek@gmail.com>
2023-01-11 17:00:21 +0800

    Add ViewStore

--- studio/stores/pgmeta/SchemaStore.ts
+++ studio/stores/pgmeta/SchemaStore.ts
@@ -1,17 +1,8 @@
-import { observable, makeObservable } from 'mobx'
 import { PostgresSchema } from '@supabase/postgres-meta'
-
-import PostgresMetaInterface, { IPostgresMetaInterface } from '../common/PostgresMetaInterface'
+import PostgresMetaInterface from '../common/PostgresMetaInterface'
 import { IRootStore } from '../RootStore'
-import { SchemaView, ResponseError } from 'types'
 
-export interface ISchemaStore extends IPostgresMetaInterface<PostgresSchema> {
-  views: SchemaView[]
-  loadViews: (schema: string) => Promise<any | { error: ResponseError }>
-}
 export default class SchemaStore extends PostgresMetaInterface<PostgresSchema> {
-  views: SchemaView[] = []
-
   constructor(
     rootStore: IRootStore,
     dataUrl: string,
@@ -21,10 +12,6 @@ export default class SchemaStore extends PostgresMetaInterface<PostgresSchema> {
     options?: { identifier: string }
   ) {
     super(rootStore, dataUrl, headers, options)
-    makeObservable(this, {
-      views: observable,
-    })
-    this.views = []
   }
 
   // Dashboard to hide schemas with pg_ prefix
@@ -33,21 +20,5 @@ export default class SchemaStore extends PostgresMetaInterface<PostgresSchema> {
       !schema.name.startsWith('pg_') && (typeof filter === 'function' ? filter(schema) : true)
 
     return super.list(schemasFilter)
-  }
-
-  async loadViews(schema: string) {
-    const query = `
-      select schemaname as schema, viewname as name from pg_catalog.pg_views
-      where schemaname = '${schema}'
-      order by schemaname, viewname;
-    `
-    const views = await this.rootStore.meta.query(query)
-    this.views = views
-    return views
-  }
-
-  // loadBySchema is not supported in this store
-  async loadBySchema(schema: string) {
-    return []
   }
 }
