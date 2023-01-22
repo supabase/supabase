49132b448c1c8650aa417df690e387eb1acad2f3
Ruggero Tomaselli <ruggerotomaselli@gmail.com>
2023-01-22 20:36:48 +0000

    Add type to import to be more explicit

--- studio/components/interfaces/TableGridEditor/EmptyState.tsx
+++ studio/components/interfaces/TableGridEditor/EmptyState.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react'
 import { observer } from 'mobx-react-lite'
-import { PostgresTable } from '@supabase/postgres-meta'
+import type { PostgresTable } from '@supabase/postgres-meta'
 import { PermissionAction } from '@supabase/shared-types/out/constants'
 import { checkPermissions, useStore } from 'hooks'
 import ProductEmptyState from 'components/to-be-cleaned/ProductEmptyState'
