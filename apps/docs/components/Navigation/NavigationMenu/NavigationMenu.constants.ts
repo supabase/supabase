02bed732d11b6e78335832c53c849b8b872d06a8
Terry Sutton <saltcod@gmail.com>
2023-01-26 08:43:23 -0330

    Re-remove cli section

--- apps/docs/components/Navigation/NavigationMenu/NavigationMenu.constants.ts
+++ apps/docs/components/Navigation/NavigationMenu/NavigationMenu.constants.ts
@@ -612,26 +612,6 @@ export const resources = {
       ],
     },
     {
-      name: 'Supabase CLI',
-      items: [
-        {
-          name: 'Overview',
-          url: '/guides/resources/supabase-cli',
-          items: [],
-        },
-        {
-          name: 'Local Development',
-          url: '/guides/resources/supabase-cli/local-development',
-          items: [],
-        },
-        {
-          name: 'Managing Environments',
-          url: '/guides/resources/supabase-cli/managing-environments',
-          items: [],
-        },
-      ],
-    },
-    {
       name: 'Postgres resources',
       url: undefined,
       items: [
