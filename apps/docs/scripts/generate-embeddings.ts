72cc226db455f122ce17ca6fd86d31699a84f593
Greg Richardson <greg.nmr@gmail.com>
2023-01-27 13:10:28 -0700

    feat(docs-gpt): generate embeddings using openai

--- apps/docs/scripts/generate-embeddings.ts
+++ apps/docs/scripts/generate-embeddings.ts
@@ -1,18 +1,23 @@
 import { createClient } from '@supabase/supabase-js'
+import { createHash } from 'crypto'
 import dotenv from 'dotenv'
-import { join } from 'path'
-import { readdir, readFile, stat } from 'fs/promises'
 import { ObjectExpression } from 'estree'
+import { readdir, readFile, stat } from 'fs/promises'
 import { Content, Root } from 'mdast'
 import { fromMarkdown } from 'mdast-util-from-markdown'
 import { mdxFromMarkdown, MdxjsEsm } from 'mdast-util-mdx'
 import { toMarkdown } from 'mdast-util-to-markdown'
 import { mdxjs } from 'micromark-extension-mdxjs'
+import 'openai'
+import { Configuration, OpenAIApi } from 'openai'
+import { join } from 'path'
 import { u } from 'unist-builder'
 import { filter } from 'unist-util-filter'
-import { createHash } from 'crypto'
+import { inspect } from 'util'
 
 dotenv.config()
+
+const ignoredFiles = ['pages/404.mdx']
 
 /**
  * Extracts ES literals from an `estree` `ObjectExpression`
@@ -98,12 +103,20 @@ function splitTreeBy(tree: Root, predicate: (node: Content) => boolean) {
   }, [])
 }
 
+type Meta = ReturnType<typeof extractMetaExport>
+
+type ProcessedMdx = {
+  checksum: string
+  meta: Meta
+  sections: string[]
+}
+
 /**
  * Processes MDX content for search indexing.
  * It extracts metadata, strips it of all JSX,
  * and splits it into sub-sections based on criteria.
  */
-function processMdxForSearch(content: string) {
+function processMdxForSearch(content: string): ProcessedMdx {
   const checksum = createHash('sha256').update(content).digest('base64')
 
   const mdxTree = fromMarkdown(content, {
@@ -128,6 +141,7 @@ function processMdxForSearch(content: string) {
 
   if (!mdTree) {
     return {
+      checksum,
       meta,
       sections: [],
     }
@@ -170,9 +184,13 @@ async function walk(dir: string): Promise<string[]> {
 }
 
 async function generateEmbeddings() {
-  if (!process.env.DOCS_SEARCH_SUPABASE_URL || !process.env.DOCS_SEARCH_SUPABASE_SERVICE_KEY) {
+  if (
+    !process.env.DOCS_SEARCH_SUPABASE_URL ||
+    !process.env.DOCS_SEARCH_SUPABASE_SERVICE_KEY ||
+    !process.env.OPENAI_KEY
+  ) {
     return console.log(
-      'Environment variables DOCS_SEARCH_SUPABASE_URL and DOCS_SEARCH_SUPABASE_SERVICE_KEY are required: skipping saving of document embeddings'
+      'Environment variables DOCS_SEARCH_SUPABASE_URL, DOCS_SEARCH_SUPABASE_SERVICE_KEY, and OPENAI_KEY are required: skipping embeddings generation'
     )
   }
 
@@ -181,71 +199,133 @@ async function generateEmbeddings() {
     process.env.DOCS_SEARCH_SUPABASE_SERVICE_KEY
   )
 
-  const markdownFiles = (await walk('pages')).filter((fileName) => /\.mdx?$/.test(fileName))
+  const markdownFiles = (await walk('pages'))
+    .filter((fileName) => /\.mdx?$/.test(fileName))
+    .filter((fileName) => !ignoredFiles.includes(fileName))
 
-  // TODO: purge old data from DB
+  console.log(`Discovered ${markdownFiles.length} pages`)
+  console.log('Checking which pages are new or have changed')
+
   for (const markdownFile of markdownFiles) {
     const path = markdownFile.replace(/^pages/, '').replace(/\.mdx?$/, '')
+    try {
+      const contents = await readFile(markdownFile, 'utf8')
 
-    const contents = await readFile(markdownFile, 'utf8')
+      const { checksum, meta, sections } = processMdxForSearch(contents)
 
-    const { checksum, meta, sections } = processMdxForSearch(contents)
+      // Check for existing page in DB and compare checksums
+      const { error: fetchPageError, data: existingPage } = await supabaseClient
+        .from('page')
+        .select()
+        .filter('path', 'eq', path)
+        .limit(1)
+        .maybeSingle()
 
-    const { error: fetchPageError, data: existingPage } = await supabaseClient
-      .from('page')
-      .select()
-      .filter('path', 'eq', path)
-      .limit(1)
-      .maybeSingle()
-
-    if (fetchPageError) {
-      throw fetchPageError
-    }
-
-    if (existingPage?.checksum === checksum) {
-      continue
-    }
-
-    console.log({ checksum, path, meta })
-
-    if (existingPage) {
-      console.log(`Docs have changed for '${path}', removing old page sections/embeddings`)
-
-      const { error: deletePageSectionError } = await supabaseClient
-        .from('page_section')
-        .delete()
-        .filter('page_id', 'eq', existingPage.id)
-
-      if (deletePageSectionError) {
-        throw deletePageSectionError
+      if (fetchPageError) {
+        throw fetchPageError
       }
-    }
 
-    const { error: upsertPageError, data: page } = await supabaseClient
-      .from('page')
-      .upsert({ path, checksum }, { onConflict: 'path' })
-      .select()
-      .limit(1)
-      .single()
+      // We use checksum to determine if this page & its sections need to be regenerated
+      if (existingPage?.checksum === checksum) {
+        continue
+      }
 
-    if (upsertPageError) {
-      throw upsertPageError
-    }
+      console.log({ checksum, path, meta })
 
-    console.log(`Adding page sections/embeddings for '${path}'`)
-    for (const section of sections) {
-      const { error, data: pageSection } = await supabaseClient
-        .from('page_section')
-        .insert({ page_id: page.id, content: section })
+      if (existingPage) {
+        console.log(
+          `Docs have changed for '${path}', removing old page sections and their embeddings`
+        )
+
+        const { error: deletePageSectionError } = await supabaseClient
+          .from('page_section')
+          .delete()
+          .filter('page_id', 'eq', existingPage.id)
+
+        if (deletePageSectionError) {
+          throw deletePageSectionError
+        }
+      }
+
+      // Create/update page record. Intentionally clear checksum until we
+      // have successfully generated all page sections.
+      const { error: upsertPageError, data: page } = await supabaseClient
+        .from('page')
+        .upsert({ checksum: null, path, meta }, { onConflict: 'path' })
         .select()
         .limit(1)
         .single()
 
-      if (error) {
-        throw error
+      if (upsertPageError) {
+        throw upsertPageError
       }
+
+      console.log(`Adding ${sections.length} page sections (with embeddings) for '${path}'`)
+      for (const section of sections) {
+        // OpenAI recommends replacing newlines with spaces for best results (specific to embeddings)
+        const input = section.replace(/\n/g, ' ')
+
+        try {
+          const configuration = new Configuration({ apiKey: process.env.OPENAI_KEY })
+          const openai = new OpenAIApi(configuration)
+
+          const embeddingResponse = await openai.createEmbedding({
+            model: 'text-embedding-ada-002',
+            input,
+          })
+
+          if (embeddingResponse.status !== 200) {
+            throw new Error(inspect(embeddingResponse.data, false, 2))
+          }
+
+          const [responseData] = embeddingResponse.data.data
+
+          const { error: insertPageSectionError, data: pageSection } = await supabaseClient
+            .from('page_section')
+            .insert({
+              page_id: page.id,
+              content: section,
+              token_count: embeddingResponse.data.usage.total_tokens,
+              embedding: responseData.embedding,
+            })
+            .select()
+            .limit(1)
+            .single()
+
+          if (insertPageSectionError) {
+            throw insertPageSectionError
+          }
+        } catch (err) {
+          // TODO: decide how to better handle failed embeddings
+          console.error(
+            `Failed to generate embeddings for '${path}' page section starting with '${input.slice(
+              0,
+              40
+            )}...'`
+          )
+
+          throw err
+        }
+      }
+
+      // Set page checksum so that we know this page was stored successfully
+      const { error: updatePageError } = await supabaseClient
+        .from('page')
+        .update({ checksum })
+        .filter('id', 'eq', page.id)
+
+      if (updatePageError) {
+        throw updatePageError
+      }
+    } catch (err) {
+      console.error(
+        `Page '${path}' or one/multiple of its page sections failed to store properly. Page has been marked with null checksum to indicate that it needs to be re-generated.`
+      )
+      console.error(err)
     }
   }
+
+  console.log('Embedding generation complete')
 }
 
 async function main() {
