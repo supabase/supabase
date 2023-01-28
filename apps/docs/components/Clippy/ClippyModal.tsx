3a15681b5c5650ae88cdec022a8f74e5ab37db15
Greg Richardson <greg.nmr@gmail.com>
2023-01-27 22:15:14 -0700

    feat(docs-gpt): end-to-end gpt-3 completion using context injection

--- apps/docs/components/Clippy/ClippyModal.tsx
+++ apps/docs/components/Clippy/ClippyModal.tsx
@@ -1,4 +1,5 @@
-import { FC, useState } from 'react'
+import { useSupabaseClient } from '@supabase/auth-helpers-react'
+import { FC, useCallback, useState } from 'react'
 import { Input } from '~/../../packages/ui'
 
 type Props = {
@@ -7,6 +8,26 @@ type Props = {
 
 const ClippyModal: FC<Props> = ({ onClose }) => {
   const [query, setQuery] = useState('')
+  const [answer, setAnswer] = useState<string>()
+  const [isLoading, setIsLoading] = useState(false)
+  const supabaseClient = useSupabaseClient()
+
+  const handleConfirm = useCallback(async () => {
+    setAnswer(undefined)
+    setIsLoading(true)
+
+    const response = await supabaseClient.functions.invoke('clippy-search', { body: { query } })
+
+    setIsLoading(false)
+
+    // TODO: display an error on the UI
+    if (response.error) {
+      console.error('Clippy search failed', response.error)
+      return
+    }
+
+    setAnswer(response.data.answer)
+  }, [query, supabaseClient])
 
   return (
     <div
@@ -14,7 +35,8 @@ const ClippyModal: FC<Props> = ({ onClose }) => {
       onClick={onClose}
     >
       <div
-        className={`mx-auto flex rounded-lg p-6 w-full max-w-2xl shadow-2xl overflow-hidden border text-left border-scale-500 dark:bg-scale-300 cursor-auto`}
+        className={`mx-auto flex flex-col gap-6 rounded-lg p-6 w-full max-w-2xl shadow-2xl overflow-hidden border text-left border-scale-500 dark:bg-scale-300 cursor-auto`}
+        onClick={(e) => e.stopPropagation()}
       >
         <Input
           className="w-full"
@@ -23,7 +45,18 @@ const ClippyModal: FC<Props> = ({ onClose }) => {
           placeholder="What can I help you with?"
           value={query}
           onChange={(e) => setQuery(e.target.value)}
+          onKeyDown={(e) => {
+            switch (e.key) {
+              case 'Enter':
+                handleConfirm()
+                return
+              default:
+                return
+            }
+          }}
         />
+        {isLoading && 'Loading'}
+        {answer && <div>{answer}</div>}
       </div>
     </div>
   )
