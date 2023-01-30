f93e97cf6d6dd5b84584b89520ff02ec12f8abaf
Greg Richardson <greg.nmr@gmail.com>
2023-01-30 08:12:36 -0700

    feat(docs-gpt): clippy image

--- apps/docs/components/Clippy/ClippyBubble.tsx
+++ apps/docs/components/Clippy/ClippyBubble.tsx
@@ -9,7 +9,7 @@ interface Props {
 const ClippyBubble: FC<Props> = ({ className, onClick }) => {
   return (
     <div
-      className={`flex gap-2 bg-black rounded-xl p-6 max-w-xs shadow-2xl cursor-pointer ${className}`}
+      className={`flex gap-2 dark:bg-scale-600 rounded-xl p-6 max-w-xs shadow-xl mb-4 relative cursor-pointer ${className}`}
       onClick={onClick}
     >
       What can I help you with?
@@ -19,9 +19,11 @@ const ClippyBubble: FC<Props> = ({ className, onClick }) => {
           <span className="text-[12px]">J</span>
         </div>
       </div>
-      <svg className="absolute -bottom-4 right-3" viewBox="0 0 1 1" width={16} fill="black">
-        <path d="M0,0 L1,0 L1,1 Z" />
-      </svg>
+      <div className="absolute -bottom-4 right-[90px] text-scale-600">
+        <svg viewBox="0 0 1 1" width={16} fill="currentColor">
+          <path d="M0,0 L1,0 L1,1 Z" />
+        </svg>
+      </div>
     </div>
   )
 }
