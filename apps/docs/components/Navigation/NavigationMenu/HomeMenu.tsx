fe9830626bb0af97e26bab0afe0c8752f52a0120
dannykng <danny@supabase.io>
2023-01-31 13:46:17 -0800

    Add Community badge

--- apps/docs/components/Navigation/NavigationMenu/HomeMenu.tsx
+++ apps/docs/components/Navigation/NavigationMenu/HomeMenu.tsx
@@ -100,6 +100,7 @@ const home = [
       icon: '/img/icons/menu/reference-python',
       href: '/reference/python/introduction',
       level: 'reference_python',
+      community: true,
     },
     {
       label: 'C#',
