e49945ece3997dd582eeaa3756256f36619893bd
awalias <antwilson@hotmail.co.uk>
2023-02-01 20:52:30 +0800

    fix: prettier

--- apps/docs/components/FooterCTACallout.tsx
+++ apps/docs/components/FooterCTACallout.tsx
@@ -1,13 +1,14 @@
 const FooterCTACallout = () => {
-    return (
-      <div className="mt-32">
-        <div className="min-w-full px-8 py-6 prose bg-scale-300 dark:bg-whiteA-200 rounded">
-          <h5 className="text-sm m-0 text-scale-1100">Looking for Serverless Postgres?</h5>
-          <p className="m-0 text-sm text-scale-900">
-            Supabase is the fastest way to get started with Postgres in a serverless environment. <a href="https://supabase.com/database?utm=postgres-helpers">Learn more</a>.
-          </p>
-        </div>
+  return (
+    <div className="mt-32">
+      <div className="min-w-full px-8 py-6 prose bg-scale-300 dark:bg-whiteA-200 rounded">
+        <h5 className="text-sm m-0 text-scale-1100">Looking for Serverless Postgres?</h5>
+        <p className="m-0 text-sm text-scale-900">
+          Supabase is the fastest way to get started with Postgres in a serverless environment.{' '}
+          <a href="https://supabase.com/database?utm=postgres-helpers">Learn more</a>.
+        </p>
       </div>
+    </div>
   )
 }
 
