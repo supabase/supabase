4779ded07e1c594b26bacd44a12f145e240431ec
phamhieu <phamhieu1998@gmail.com>
2023-02-05 17:34:51 +0700

    feat: send GA4 props on telemetry call

--- studio/components/ui/PageTelemetry.tsx
+++ studio/components/ui/PageTelemetry.tsx
@@ -1,6 +1,7 @@
 import { useStore } from 'hooks'
 import { post } from 'lib/common/fetch'
 import { API_URL, IS_PLATFORM } from 'lib/constants'
+import { getScreenResolution } from 'lib/telemetry'
 import { observer } from 'mobx-react-lite'
 import { useRouter } from 'next/router'
 import { FC, useEffect } from 'react'
@@ -10,24 +11,11 @@ const PageTelemetry: FC = ({ children }) => {
   const router = useRouter()
   const { ui } = useStore()
   const { profile } = ui
-  let clientId: string
 
   useEffect(() => {
     function handleRouteChange() {
       handlePageTelemetry(profile)
     }
-
-    // @ts-ignore
-    window?.gtag(
-      'get',
-      `'${process.env.NEXT_PUBLIC_GA4_MEASUREMENT_ID}'`,
-      'client_id',
-      // @ts-ignore
-      (client_id) => {
-        console.log(client_id)
-        clientId = client_id
-      }
-    )
 
     // Listen for page changes after a navigation or when the query changes
     router.events.on('routeChangeComplete', handleRouteChange)
@@ -53,8 +41,6 @@ const PageTelemetry: FC = ({ children }) => {
        */
       let referrer: string | undefined = document.referrer
 
-      console.log('my clientId = ', clientId)
-
       /**
        * Send page telemetry
        *
@@ -64,6 +50,7 @@ const PageTelemetry: FC = ({ children }) => {
       post(`${API_URL}/telemetry/page`, {
         referrer: referrer,
         title: document.title,
+        ga: ui.googleAnalyticsProps,
       })
     }
   }
