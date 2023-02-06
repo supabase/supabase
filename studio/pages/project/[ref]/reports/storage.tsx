b7342f4fa8ed4d4f3158e0cd837028d3b78c07ea
TzeYiing <ty@tzeyiing.com>
2023-02-07 03:50:10 +0800

    feat: add storage renderers

--- studio/pages/project/[ref]/reports/storage.tsx
+++ studio/pages/project/[ref]/reports/storage.tsx
@@ -8,8 +8,14 @@ import { hooksFactory, usePresetReport } from 'components/interfaces/Reports/Rep
 import { PRESET_CONFIG } from 'components/interfaces/Reports/Reports.constants'
 import ReportWidget from 'components/interfaces/Reports/ReportWidget'
 import {
+  renderCacheHitRate,
   renderLargestObjectsPerBucket,
+  renderMostFiles,
+  renderStaleFiles,
+  renderTopDownloaded,
+  renderTopSizes,
 } from 'components/interfaces/Reports/renderers/StorageRenderers'
+import Divider from 'components/ui/Divider'
 
 export const StorageReport: NextPageWithLayout = () => {
   const router = useRouter()
@@ -22,13 +28,6 @@ export const StorageReport: NextPageWithLayout = () => {
   const staleFiles = hooks.staleFiles()
   const topSizes = hooks.topSizes()
   const cacheHitRate = hooks.cacheHitRate()
-  // const dailyActiveUsers = hooks.dailyActiveUsers()
-  // const newUsers = hooks.newUsers()
-  // const cumulativeUsers = hooks.cumulativeUsers()
-  // const failedMigrations = hooks.failedMigrations()
-  // const signUpProviders = hooks.signUpProviders()
-  // const unverifiedUsers = hooks.unverifiedUsers()
-  // const bannedUsers = hooks.bannedUsers()
 
   const { isLoading, Layout } = usePresetReport([
     largestObjectsPerBucket,
@@ -36,90 +35,72 @@ export const StorageReport: NextPageWithLayout = () => {
     mostFiles,
     staleFiles,
     topSizes,
-    cacheHitRate, 
-    
+    cacheHitRate,
   ])
   return (
     <Layout title={config.title}>
       <div className="flex flex-col grid  lg:grid-cols-6 gap-4">
-      <ReportWidget
+        <ReportWidget
+          isLoading={isLoading}
+          params={cacheHitRate[0].params}
+          className="col-span-3 col-start-1"
+          title="Cache Hit Rate"
+          description="Edge Network Cache Hit Rate"
+          data={cacheHitRate[0]?.logData || []}
+          renderer={renderCacheHitRate}
+        />
+
+        <ReportWidget
           isLoading={isLoading}
           params={largestObjectsPerBucket[0].params}
           className="col-span-3 col-start-1"
           title="Largest Objects"
           description="Large objects in each bucket"
-          data={largestObjectsPerBucket[0].data}
+          data={largestObjectsPerBucket[0]?.data || []}
           renderer={renderLargestObjectsPerBucket}
         />
-        {/* <ReportWidget
+        <ReportWidget
           isLoading={isLoading}
-          params={cumulativeUsers[0].params}
+          params={topDownloaded[0].params}
           className="col-span-3 col-start-1"
-          title="Users Over Time"
-          description="Cumulative count of confirmed users"
-          data={cumulativeUsers[0].data!}
-          renderer={renderCumulativeUsers}
+          title="Top Downloaded Objects"
+          description="Top download objectss"
+          data={topDownloaded[0]?.data || []}
+          renderer={renderTopDownloaded}
         />
+
+        <Divider light />
+
         <ReportWidget
           isLoading={isLoading}
-          params={newUsers[0].params}
-          className="col-span-3 col-start-4"
-          title="Daily New Users"
-          description="The daily count of new user signups"
-          data={newUsers[0].data!}
-          renderer={renderNewUsers}
-        />
-        <ReportWidget
-          isLoading={isLoading}
-          params={dailyActiveUsers[0].params}
-          className="col-span-6 col-start-1"
-          title="Daily Active Users"
-          description="The daily count of active authenticated users"
-          data={dailyActiveUsers[0].logData!}
-          renderer={renderDailyActiveUsers}
+          params={mostFiles[0].params}
+          className="col-span-3 col-start-1"
+          title="Users with Most Files"
+          description="Users with the most associated storage files"
+          data={mostFiles[0]?.data || []}
+          renderer={renderMostFiles}
         />
 
         <ReportWidget
           isLoading={isLoading}
-          params={signUpProviders[0].params}
-          className="row-span-4 col-span-4 col-start-3"
-          title="Sign Up Providers"
-          description="The breakdown of providers used for user creation"
-          data={signUpProviders[0].data!}
-          renderer={renderSignUpProviders}
-        />
-        <ReportWidget
-          isLoading={isLoading}
-          params={unverifiedUsers[0].params}
-          className="row-span-1 col-span-2 col-start-1"
-          title="Unverified Users"
-          tooltip="High number of unverified users may indicate spamming and malicious behavior."
-          data={unverifiedUsers[0].data! || []}
-          renderer={renderUnverifiedUsers}
+          params={staleFiles[0].params}
+          className="col-span-3 col-start-1"
+          title="Stale Objects"
+          description="Objects by months from last access"
+          data={staleFiles[0]?.data || []}
+          renderer={renderStaleFiles}
         />
 
         <ReportWidget
           isLoading={isLoading}
-          params={bannedUsers[0].params}
-          className="row-span-1 col-span-2 col-start-1"
-          title="Banned Users"
-          tooltip="High number of banned users may indicate malicious behavior."
-          data={bannedUsers[0].data! || []}
-          renderer={renderBannedUsers}
+          params={topSizes[0].params}
+          className="col-span-3 col-start-1"
+          title="Top Object Sizes"
+          description="Most frequently used object sizes"
+          data={topSizes[0]?.logData || []}
+          renderer={renderTopSizes}
         />
-
-        <h2 className="col-span-full">Sever Health</h2>
-        <ReportWidget
-          isLoading={isLoading}
-          params={failedMigrations[0].params}
-          className="col-span-2 col-start-1"
-          title="Failed Migrations"
-          tooltip="Failed GoTrue migrations may cause issues with user authentication."
-          data={failedMigrations[0].logData!}
-          renderer={renderFailedMigrations}
-        />
-  */}
-      </div> 
+      </div>
     </Layout>
   )
 }
