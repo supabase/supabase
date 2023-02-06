22dd4b37cc961ede9f9955e4ce0ea1312b8b4db9
Terry Sutton <saltcod@gmail.com>
2023-02-06 16:49:41 -0330

    Move flag

--- apps/docs/components/Clippy/Clippy.tsx
+++ apps/docs/components/Clippy/Clippy.tsx
@@ -7,19 +7,24 @@ import clippyImage from '../../public/img/clippy.png'
 import ClippyBubble from './ClippyBubble'
 import ClippyModal from './ClippyModal'
 import { useClippy } from './ClippyProvider'
-
+import { useFlag } from '~/hooks/useFlag'
 const Clippy: FC = () => {
   const { isDarkMode } = useTheme()
   const { isOpen, open, close } = useClippy()
+  const enableClippy = useFlag('enableClippy')
 
   return (
     <>
       {!isOpen && (
         <div className="hidden md:flex flex-col items-end gap-1 md:gap-4">
-          <ClippyBubble onClick={open} />
-          <div className="w-[80px] lg:w-[120px] p-8 md:p-0">
-            <Image src={isDarkMode ? clippyImageDark : clippyImage} alt="Clippy" />
-          </div>
+          {enableClippy && (
+            <>
+              <ClippyBubble onClick={open} />
+              <div className="w-[80px] lg:w-[120px] p-8 md:p-0">
+                <Image src={isDarkMode ? clippyImageDark : clippyImage} alt="Clippy" />
+              </div>
+            </>
+          )}
         </div>
       )}
       {isOpen && createPortal(<ClippyModal onClose={close} />, document.body)}
