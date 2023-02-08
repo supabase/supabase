5c2eb8394bc6a5835c22292998a49eef93fda5af
Rodrigo Mansueli Nunes <rodrigo@mansueli.com>
2023-02-08 18:48:11 -0300

    render-migration-guide

--- apps/docs/components/Navigation/NavigationMenu/NavigationMenu.constants.ts
+++ apps/docs/components/Navigation/NavigationMenu/NavigationMenu.constants.ts
@@ -657,6 +657,11 @@ export const resources = {
           url: '/guides/resources/migrating-to-supabase/heroku',
           items: [],
         },
+        {
+          name: 'Render',
+          url: '/guides/resources/migrating-to-supabase/render',
+          items: [],
+        },
       ],
     },
     {
@@ -719,6 +724,7 @@ export const migrate = {
     { name: 'Firestore Data', url: '/guides/migrations/firestore-data', items: [] },
     { name: 'Firebase Storage', url: '/guides/migrations/firebase-storage', items: [] },
     { name: 'Heroku', url: '/guides/migrations/heroku', items: [] },
+    { name: 'Render', url: '/guides/migrations/render', items: [] },
   ],
 }
 
