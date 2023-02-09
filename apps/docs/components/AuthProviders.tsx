8d1f9df40c04fcea5712bf5082fa29f622ca4173
Ollie Halliday <22655069+Hallidayo@users.noreply.github.com>
2023-02-09 18:30:20 +0000

    added icon container and icons

--- apps/docs/components/AuthProviders.tsx
+++ apps/docs/components/AuthProviders.tsx
@@ -1,7 +1,28 @@
+import Image from 'next/image'
 import providers from '../data/authProviders'
 import ButtonCard from './ButtonCard'
 
 export default function AuthProviders() {
+  const IconContainer: React.FC = (props) => {
+    return (
+      <div
+        className={[
+          'relative',
+          'flex items-center justify-center shrink-0',
+          'h-10 w-10 rounded-lg',
+          'group',
+          'cursor-pointer',
+          'overflow-hidden',
+          'border-scale-500 hover:border-scale-700 bg-white dark:bg-scale-300',
+          'border rounded-full',
+          'transition',
+        ].join(' ')}
+      >
+        {props.children}
+      </div>
+    )
+  }
+
   return (
     <div className="grid md:grid-cols-12 gap-4">
       {providers.map((x) => (
@@ -9,7 +30,12 @@ export default function AuthProviders() {
           <ButtonCard to={x.href} title={x.name}>
             <div className="px-6 py-4">
               <div className="flex justify-between">
-                <p className="mt-0">{x.name}</p>
+                <div className="flex gap-3">
+                  <IconContainer>
+                    <Image width={20} height={20} src={`${x.logo}.svg`} alt={x.name} />
+                  </IconContainer>
+                  <p className="mt-0">{x.name}</p>
+                </div>
                 <p className="mt-0">
                   {x.official ? (
                     <span className={`badge badge--official`}>Official</span>
