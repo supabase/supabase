b32483ce59c44b4c35e69f7267e0910105097b03
Ruggero Tomaselli <ruggerotomaselli@gmail.com>
2023-02-12 14:48:37 +0100

    Force type of protocol

--- studio/pages/api/constants.ts
+++ studio/pages/api/constants.ts
@@ -1,8 +1,10 @@
+import type { AutoApiService } from "data/config/project-api-query"
+
 const PUBLIC_URL = new URL(process.env.SUPABASE_PUBLIC_URL || 'http://localhost:8000')
 
 export const PROJECT_REST_URL = `${PUBLIC_URL.origin}/rest/v1/`
 export const PROJECT_ENDPOINT = PUBLIC_URL.host
-export const PROJECT_ENDPOINT_PROTOCOL = PUBLIC_URL.protocol.replace(':', '')
+export const PROJECT_ENDPOINT_PROTOCOL = PUBLIC_URL.protocol.replace(':', '') as AutoApiService["protocol"]
 
 export const DEFAULT_PROJECT = {
   id: 1,
