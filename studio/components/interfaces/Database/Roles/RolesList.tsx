c86a362cabae65f5a3515f76b176eb41ef824e00
Joshen Lim <joshenlimek@gmail.com>
2023-02-14 16:50:29 +0800

    Add search and filter for roles

--- studio/components/interfaces/Database/Roles/RolesList.tsx
+++ studio/components/interfaces/Database/Roles/RolesList.tsx
@@ -1,14 +1,14 @@
+import Link from 'next/link'
 import { FC, useState } from 'react'
 import { observer } from 'mobx-react-lite'
 import * as Tooltip from '@radix-ui/react-tooltip'
 import { PostgresRole } from '@supabase/postgres-meta'
-import { Button, IconPlus } from 'ui'
+import { Button, Input, IconPlus, IconSearch } from 'ui'
 
 import { useParams, useStore } from 'hooks'
 import SparkBar from 'components/ui/SparkBar'
 import { FormHeader } from 'components/ui/Forms'
 import RoleRow from './RoleRow'
-import Link from 'next/link'
 import DeleteRoleModal from './DeleteRoleModal'
 
 interface Props {
@@ -18,9 +18,17 @@ interface Props {
 const RolesList: FC<Props> = ({}) => {
   const { ref } = useParams()
   const { meta } = useStore()
-  const roles = meta.roles.list()
 
+  const [filterString, setFilterString] = useState('')
+  const [filterType, setFilterType] = useState<'all' | 'active'>('all')
   const [selectedRoleToDelete, setSelectedRoleToDelete] = useState<any>()
+
+  const roles = meta.roles.list()
+  const filteredRoles = (
+    filterType === 'active'
+      ? meta.roles.list((role: PostgresRole) => role.active_connections > 0)
+      : meta.roles.list()
+  ).filter((role: PostgresRole) => role.name.includes(filterString))
 
   const connectionLimit = Math.max(...roles.map((role: PostgresRole) => role.connection_limit))
   const totalActiveConnections = roles
@@ -38,7 +46,43 @@ const RolesList: FC<Props> = ({}) => {
             title="Database Roles"
             description="Manage access control to your database through users, groups, and permissions"
           />
-          <div className="flex items-center mb-6 space-x-6">
+        </div>
+
+        <div className="mb-4 -mt-2 flex items-center justify-between">
+          <div className="flex items-center space-x-4">
+            <Input
+              size="small"
+              placeholder="Search for a role"
+              icon={<IconSearch size="tiny" />}
+              onChange={(event: any) => setFilterString(event.target.value)}
+            />
+            <div className="flex items-center border border-scale-700 rounded-full w-min h-[34px]">
+              <button
+                className={[
+                  'text-sm w-[100px] h-full text-center rounded-l-full flex items-center justify-center transition',
+                  filterType === 'all'
+                    ? 'bg-scale-500 text-scale-1200'
+                    : 'hover:bg-scale-400 text-scale-1100',
+                ].join(' ')}
+                onClick={() => setFilterType('all')}
+              >
+                All roles
+              </button>
+              <div className="h-full w-[1px] border-r border-scale-700"></div>
+              <button
+                className={[
+                  'text-sm w-[100px] h-full text-center rounded-r-full flex items-center justify-center transition',
+                  filterType === 'active'
+                    ? 'bg-scale-500 text-scale-1200'
+                    : 'hover:bg-scale-400 text-scale-1100',
+                ].join(' ')}
+                onClick={() => setFilterType('active')}
+              >
+                Active roles
+              </button>
+            </div>
+          </div>
+          <div className="flex items-center space-x-6">
             <Tooltip.Root delayDuration={0}>
               <Tooltip.Trigger>
                 <div className="w-42">
@@ -86,9 +130,17 @@ const RolesList: FC<Props> = ({}) => {
         </div>
 
         <div>
-          {roles.map((role: PostgresRole, i: number) => (
+          {filteredRoles.map((role: PostgresRole, i: number) => (
             <RoleRow key={role.id} role={role} onSelectDelete={setSelectedRoleToDelete} />
           ))}
+          {filterString.length > 0 && filteredRoles.length === 0 && (
+            <div className="bg-scale-100 dark:bg-scale-300 border border-scale-300 dark:border-scale-500 px-6 py-4 rounded space-y-1">
+              <p className="text-sm text-scale-1100">No results found</p>
+              <p className="text-sm text-scale-1000">
+                Your search for "{filterString}" did not return any results
+              </p>
+            </div>
+          )}
         </div>
       </div>
 
