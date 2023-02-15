872ba091127662decb2e86fa5cf64883c17121c1
Terry Sutton <saltcod@gmail.com>
2023-02-15 12:00:03 -0330

    Add secret fetching

--- apps/docs/components/Clippy/Clippy.tsx
+++ apps/docs/components/Clippy/Clippy.tsx
@@ -13,21 +13,25 @@ const Clippy: FC = () => {
   const { isDarkMode } = useTheme()
   const { isOpen, open, close } = useClippy()
 
-  return (
-    <>
-      {!isOpen && (
-        <div className="hidden md:flex flex-col items-end gap-1 md:gap-4">
-          {/* <>
+  if (!IS_PLATFORM) {
+    return null
+  } else {
+    return (
+      <>
+        {!isOpen && (
+          <div className="hidden md:flex flex-col items-end gap-1 md:gap-4">
+            {/* <>
               <ClippyBubble onClick={open} />
               <div className="w-[80px] lg:w-[120px] p-8 md:p-0">
-                <Image src={isDarkMode ? clippyImageDark : clippyImage} alt="Clippy" />
+              <Image src={isDarkMode ? clippyImageDark : clippyImage} alt="Clippy" />
               </div>
             </> */}
-        </div>
-      )}
-      {isOpen && IS_PLATFORM && createPortal(<ClippyModal onClose={close} />, document.body)}
-    </>
-  )
+          </div>
+        )}
+        {isOpen && createPortal(<ClippyModal onClose={close} />, document.body)}
+      </>
+    )
+  }
 }
 
 export default Clippy
