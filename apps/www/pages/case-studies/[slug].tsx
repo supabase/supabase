51688d947f812aca8076fd00791a4f4f453e5ec2
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2023-02-16 14:06:39 +0800

    udpate layout a bit

--- apps/www/pages/case-studies/[slug].tsx
+++ apps/www/pages/case-studies/[slug].tsx
@@ -1,11 +1,12 @@
 import { Button, IconChevronRight, IconExternalLink } from '@supabase/ui'
 import matter from 'gray-matter'
-import authors from 'lib/authors.json'
+
 import { MDXRemote } from 'next-mdx-remote'
 import { NextSeo } from 'next-seo'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { IconChevronLeft, IconChevronsLeft } from '~/../../packages/ui'
 import CTABanner from '~/components/CTABanner'
 import DefaultLayout from '~/components/Layouts/Default'
 import mdxComponents from '~/lib/mdx/mdxComponents'
@@ -27,22 +28,18 @@ export async function getStaticProps({ params }: any) {
   const filePath = `${params.slug}`
   const postContent = await getPostdata(filePath, '_case-studies')
   const { data, content } = matter(postContent)
-
   const mdxSource: any = await mdxSerialize(content)
 
   const relatedPosts = getSortedPosts('_case-studies', 5, mdxSource.scope.tags)
 
   const allPosts = getSortedPosts('_case-studies')
-
   const currentIndex = allPosts
     .map(function (e) {
       return e.slug
     })
     .indexOf(filePath)
-
   const nextPost = allPosts[currentIndex + 1]
   const prevPost = allPosts[currentIndex - 1]
-
   const payload = {
     props: {
       prevPost: currentIndex === 0 ? null : prevPost ? prevPost : null,
@@ -57,13 +54,10 @@ export async function getStaticProps({ params }: any) {
       },
     },
   }
-
   return payload
 }
 
-function BlogPostPage(props: any) {
-  // @ts-ignore
-  // const author = props.blog.author ? authors[props.blog.author] : authors['supabase']
+function CaseStudyPage(props: any) {
   const content = props.blog.content
 
   const { basePath } = useRouter()
@@ -122,70 +116,90 @@ function BlogPostPage(props: any) {
       <DefaultLayout>
         <div
           className="
-          dark:bg-scale-200 bg-scale-200
             container mx-auto px-8 py-16 sm:px-16
             xl:px-20
           "
         >
-          <div className="grid grid-cols-12 gap-8 xl:gap-16">
-            <article className="col-span-12 max-w-6xl mx-auto flex flex-col gap-8">
-              <div className="col-span-12 flex flex-col gap-8 lg:col-span-10 xl:col-span-8 md:max-w-2xl xl:max-w-4xl">
-                <p className="text-brand-900 mb-2 mt-0">
-                  Case Studies / <a>{props.blog.name}</a>
-                </p>
-                <h1 className="text-scale-1200 text-4xl font-semibold xl:text-5xl">
-                  {props.blog.title}
-                </h1>
-                <h2 className="text-scale-1200 text-xl xl:text-2xl">{props.blog.description}</h2>
-              </div>
+          <div className="grid grid-cols-12 gap-4">
+            <div className="col-span-12 mb-2 lg:col-span-2">
+              {/* Back button */}
+              <p>
+                <a
+                  href={'/case-studies'}
+                  className="text-scale-900 hover:text-scale-1200 flex cursor-pointer items-center text-sm transition"
+                >
+                  <IconChevronLeft style={{ padding: 0 }} />
+                  Back
+                </a>
+              </p>
+            </div>
 
-              <div className="grid grid-cols-12 prose max-w-none gap-8 lg:gap-20">
-                <div className="col-span-12 lg:col-span-4 lg:block xl:col-span-4">
-                  <div className="space-y-8 lg:sticky lg:top-24 lg:mb-24">
-                    {/* Logo */}
-                    <div className={`relative h-16 w-32`}>
-                      <p className="flex flex-row ">
-                        <Image
-                          layout="fill"
-                          src={`${props.blog.logo}`}
-                          alt={`${props.blog.title} logo`}
-                          objectFit="scale-down"
-                          objectPosition="left"
-                          className="
+            <div
+              className="col-span-12 lg:col-span-8
+          
+          "
+            >
+              <div className="">
+                <article className="flex flex-col gap-8">
+                  <div className="flex flex-col gap-8 max-w-xxl">
+                    <Link passHref href="/case-studies">
+                      <a className="text-brand-900 hover:text-brand-1000 mb-2 mt-0">Case Studies</a>
+                    </Link>
+                    <h1 className="text-scale-1200 text-4xl font-semibold xl:text-5xl">
+                      {props.blog.title}
+                    </h1>
+                    <h2 className="text-scale-1200 text-xl xl:text-2xl">
+                      {props.blog.description}
+                    </h2>
+                  </div>
+
+                  <div className="grid grid-cols-12 prose max-w-none gap-8 lg:gap-20">
+                    <div className="col-span-12 lg:col-span-4 lg:block xl:col-span-4">
+                      <div className="space-y-8 lg:sticky lg:top-24 lg:mb-24">
+                        {/* Logo */}
+                        <div className={`relative h-16 w-32`}>
+                          <p className="flex flex-row ">
+                            <Image
+                              layout="fill"
+                              src={`${props.blog.logo}`}
+                              alt={`${props.blog.title} logo`}
+                              objectFit="scale-down"
+                              objectPosition="left"
+                              className="
                       bg-no-repeat
                       
                       dark:brightness-200 
                       dark:contrast-0
                       dark:filter
                     "
-                        />
-                      </p>
-                    </div>
-
-                    <div className="flex flex-col space-y-2">
-                      <span className="text-scale-900">About</span>
-                      <p>{props.blog.about}</p>
-                      <span className="not-prose ">
-                        <a
-                          href={props.blog.company_url}
-                          className=" flex cursor-pointer items-center space-x-1 opacity-50 transition-opacity hover:opacity-100"
-                        >
-                          <span>{props.blog.company_url}</span>
-                          <IconExternalLink size={14} />
-                        </a>
-                      </span>
-                    </div>
-
-                    {props.blog.misc.map((x: any) => {
-                      return (
-                        <div className="flex flex-col gap-0">
-                          <span className="text-scale-900">{x.label}</span>
-                          <span className="text-scale-1100">{x.text}</span>
+                            />
+                          </p>
                         </div>
-                      )
-                    })}
-                    <div className="flex flex-col md:flex-row lg:flex-col gap-8 justify-between">
-                      {props.blog.stats.map((x: any) => {
+
+                        <div className="flex flex-col space-y-2">
+                          <span className="text-scale-900">About</span>
+                          <p>{props.blog.about}</p>
+                          <span className="not-prose ">
+                            <a
+                              href={props.blog.company_url}
+                              className=" flex cursor-pointer items-center space-x-1 opacity-50 transition-opacity hover:opacity-100"
+                            >
+                              <span>{props.blog.company_url}</span>
+                              <IconExternalLink size={14} />
+                            </a>
+                          </span>
+                        </div>
+
+                        {props.blog.misc.map((x: any) => {
+                          return (
+                            <div className="flex flex-col gap-0">
+                              <span className="text-scale-900">{x.label}</span>
+                              <span className="text-scale-1100">{x.text}</span>
+                            </div>
+                          )
+                        })}
+                        <div className="flex flex-col md:flex-row lg:flex-col gap-8 justify-between">
+                          {/* {props.blog.stats.map((x: any) => {
                         return (
                           <div className="flex space-x-3">
                             <div className="bg-brand-900 mt-0.5 h-8 w-0.5"></div>
@@ -197,36 +211,38 @@ function BlogPostPage(props: any) {
                             </div>
                           </div>
                         )
-                      })}
-                    </div>
+                      })} */}
+                        </div>
 
-                    <div className="">
-                      <p>Ready to get started?</p>
-                      <div>
-                        <Button type="default" iconRight={<IconChevronRight />}>
-                          Contact sales
-                        </Button>
+                        <div className="">
+                          <p>Ready to get started?</p>
+                          <div>
+                            <Button type="default" iconRight={<IconChevronRight />}>
+                              Contact sales
+                            </Button>
+                          </div>
+                        </div>
                       </div>
                     </div>
+                    <div className="xm:col-span-7 col-span-12 lg:col-span-8 xl:col-span-8 ">
+                      <MDXRemote {...content} components={mdxComponents()} />
+                    </div>
+                    {/* Sidebar */}
                   </div>
-                </div>
-                <div className="xm:col-span-7 col-span-12 lg:col-span-8 xl:col-span-8 ">
-                  <MDXRemote {...content} components={mdxComponents()} />
-                </div>
-                {/* Sidebar */}
+                </article>
               </div>
-            </article>
+            </div>
           </div>
         </div>
 
-        <div className="mt-32">
+        {/* <div className="mt-32">
           <h3 className="text-2xl">Read more case studies</h3>
           <div className="grid grid-cols-3 gap-8">
-            {/* {props.relatedPosts.map((post) => {
+            {props.relatedPosts.map((post) => {
                 return <NextCard post={post} />
-              })} */}
+              })}
           </div>
-        </div>
+        </div> */}
 
         <CTABanner />
       </DefaultLayout>
@@ -234,8 +250,4 @@ function BlogPostPage(props: any) {
   )
 }
 
-// function BlogPostPage() {
-//   return <h1>blog post</h1>
-// }
-
-export default BlogPostPage
+export default CaseStudyPage
