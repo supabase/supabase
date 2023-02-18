2cd4c9ecff39a996eff94b49f754ac39dc3335b3
Greg Richardson <greg.nmr@gmail.com>
2023-02-18 13:22:24 -0700

    fix(docs-search): search result links

--- apps/docs/components/Clippy/SearchResult.tsx
+++ apps/docs/components/Clippy/SearchResult.tsx
@@ -1,5 +1,7 @@
+import Link from 'next/link'
 import { FC } from 'react'
 import { IconBookOpen, IconHash } from '~/../../packages/ui'
+import { useClippy } from './ClippyProvider'
 
 export enum SearchResultType {
   Document = 'document',
@@ -7,29 +9,34 @@ export enum SearchResultType {
 }
 
 type Props = {
+  href: string
   type: SearchResultType
   title: string
   chip?: string
 }
 
-const SearchResult: FC<Props> = ({ type, title, chip }) => {
+const SearchResult: FC<Props> = ({ href, type, title, chip }) => {
+  const { close } = useClippy()
+
   return (
-    <a
-      className="flex flex-row items-center bg-scale-500 hover:bg-scale-700 transition p-4 rounded-md border border-scale-600 text-sm cursor-pointer"
-      onClick={close}
-    >
-      <div className="w-6 h-6 p-1 flex items-center justify-center mr-4 text-brand-1100 rounded-md bg-scale-700">
-        {getIconByType(type)}
-      </div>
-      <div className="flex flex-col gap-2 items-start">
-        {chip && (
-          <div className="rounded-xl bg-scale-700 pl-3 pr-3 pt-0.5 pb-0.5 text-xs text-scale-1100">
-            {chip}
-          </div>
-        )}
-        <div>{title}</div>
-      </div>
-    </a>
+    <Link href={href}>
+      <a
+        className="flex flex-row items-center bg-scale-400 hover:bg-scale-600 transition p-4 rounded-md border border-scale-600 text-sm cursor-pointer"
+        onClick={close}
+      >
+        <div className="w-6 h-6 p-1 flex items-center justify-center mr-4 text-brand-1100 rounded-md bg-scale-700">
+          {getIconByType(type)}
+        </div>
+        <div className="flex flex-col gap-2 items-start">
+          {chip && (
+            <div className="rounded-xl bg-scale-700 pl-3 pr-3 pt-0.5 pb-0.5 text-xs text-scale-1100">
+              {chip}
+            </div>
+          )}
+          <div>{title}</div>
+        </div>
+      </a>
+    </Link>
   )
 }
 
