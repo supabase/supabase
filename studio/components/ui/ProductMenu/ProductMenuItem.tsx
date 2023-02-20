810be7ca1985b431e7025ba8122adf8c3f94cd20
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2023-02-21 01:53:03 +0800

    init insights page POC

--- studio/components/ui/ProductMenu/ProductMenuItem.tsx
+++ studio/components/ui/ProductMenu/ProductMenuItem.tsx
@@ -16,6 +16,32 @@ interface Props {
   label?: string
 }
 
+const Label = ({ label }: { label: string }) => {
+  const color =
+    label.toLowerCase() === 'new' ? 'text-brand-900 bg-brand-500' : 'text-amber-900 bg-amber-500'
+
+  return (
+    <span
+      className={[
+        `text-mono 
+        h-[18px],
+        flex
+        items-center
+        justify-center
+        px-1.5
+        rounded-full 
+        tracking-widest
+        text-[10px] 
+        font-medium
+        truncate`,
+        color,
+      ].join(' ')}
+    >
+      {label}
+    </span>
+  )
+}
+
 const ProductMenuItem: FC<Props> = ({
   name = '',
   isActive,
@@ -37,9 +63,7 @@ const ProductMenuItem: FC<Props> = ({
           className={`flex items-center gap-2 w-full ${textClassName}`}
         >
           <span className="truncate">{name} </span>
-          {label !== undefined && (
-            <span className="text-orange-800 text-xs font-normal truncate">{label}</span>
-          )}
+          {label !== undefined && <Label label={label} />}
         </div>
         {action}
       </div>
