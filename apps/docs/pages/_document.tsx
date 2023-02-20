6642d6446cc02796d36112ea3db7934cd97204a1
egor-romanov <egor.romanov@gmail.com>
2023-02-20 19:50:01 +0300

    fix prettier errors

--- apps/docs/pages/_document.tsx
+++ apps/docs/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en" className="dark">
       <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       </Head>
       <body>
         <Main />
