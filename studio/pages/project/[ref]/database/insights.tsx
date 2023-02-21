e50ec9b70520646b2265a52dc92f643d32cdffaa
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2023-02-21 16:37:13 +0800

    Update insights.tsx

--- studio/pages/project/[ref]/database/insights.tsx
+++ studio/pages/project/[ref]/database/insights.tsx
@@ -6,49 +6,69 @@ import { checkPermissions } from 'hooks'
 import { DatabaseLayout } from 'components/layouts'
 import { Extensions } from 'components/interfaces/Database'
 import NoPermission from 'components/ui/NoPermission'
-import { Button, IconGlobe, IconMonitor, Tabs } from 'ui'
+import { Button, IconGlobe, IconMonitor, IconThumbsUp, Tabs } from 'ui'
 import useDbQuery from 'hooks/analytics/useDbQuery'
 import { useEffect, useState } from 'react'
 import Table from 'components/to-be-cleaned/Table'
 
-const limit = 100
+const limit = 10
 
 const QueryMostFrequentlyInvoked = `
-SELECT
-auth.rolname,
-interval '1 millisecond' * statements.total_exec_time AS total_exec_time,
-to_char((statements.total_exec_time/sum(statements.total_exec_time) OVER()) * 100, 'FM90D0') || '%'  AS prop_exec_time,
-to_char(statements.calls, 'FM999G999G999G990') AS ncalls,
-statements.query AS query
-FROM pg_stat_statements as statements
-INNER JOIN pg_authid AS auth ON statements.userid = auth.oid
-ORDER BY total_exec_time DESC
-LIMIT ${limit};
-`
-
-const QueryMostTimeConsuming = `
-SELECT
-auth.rolname,
-interval '1 millisecond' * statements.total_exec_time AS total_exec_time,
-to_char((statements.total_exec_time/sum(statements.total_exec_time) OVER()) * 100, 'FM90D0') || '%'  AS prop_exec_time,
-to_char(statements.calls, 'FM999G999G999G990') AS ncalls,
-statements.query AS query
-FROM pg_stat_statements as statements
-INNER JOIN pg_authid AS auth ON statements.userid = auth.oid
-ORDER BY total_exec_time DESC
-LIMIT ${limit};
-`
-
-const QuerySlowestExecutionTime = `
+-- Most frequently called queries
+-- A limit of 100 has been added below
 select
     auth.rolname,
+    statements.query,
     statements.calls,
+    -- -- Postgres 13, 14, 15
     statements.total_exec_time + statements.total_plan_time as total_time,
     statements.min_exec_time + statements.min_plan_time as min_time,
     statements.max_exec_time + statements.max_plan_time as max_time,
     statements.mean_exec_time + statements.mean_plan_time as mean_time,
-    statements.rows / statements.calls as avg_rows,
-    statements.query
+    -- -- Postgres <= 12
+    -- total_time,
+    -- min_time,
+    -- max_time,
+    -- mean_time,
+    statements.rows / statements.calls as avg_rows
+  from pg_stat_statements as statements
+    inner join pg_authid as auth on statements.userid = auth.oid
+  order by
+    statements.calls desc
+  limit
+    ${limit};`
+
+const QueryMostTimeConsuming = `-- Most time consuming queries
+-- A limit of 100 has been added below
+select
+    auth.rolname,
+    statements.query,
+    statements.calls,
+    statements.total_exec_time + statements.total_plan_time as total_time,
+    to_char(((statements.total_exec_time + statements.total_plan_time)/sum(statements.total_exec_time + statements.total_plan_time) OVER()) * 100, 'FM90D0') || '%'  AS prop_total_time
+  from pg_stat_statements as statements
+    inner join pg_authid as auth on statements.userid = auth.oid
+  order by
+    total_time desc
+  limit ${limit};`
+
+const QuerySlowestExecutionTime = `-- Slowest queries by max execution time
+-- A limit of 100 has been added below
+select
+    auth.rolname,
+    statements.query,
+    statements.calls,
+    -- -- Postgres 13, 14, 15
+    statements.total_exec_time + statements.total_plan_time as total_time,
+    statements.min_exec_time + statements.min_plan_time as min_time,
+    statements.max_exec_time + statements.max_plan_time as max_time,
+    statements.mean_exec_time + statements.mean_plan_time as mean_time,
+    -- -- Postgres <= 12
+    -- total_time,
+    -- min_time,
+    -- max_time,
+    -- mean_time,
+    statements.rows / statements.calls as avg_rows
   from pg_stat_statements as statements
     inner join pg_authid as auth on statements.userid = auth.oid
   order by
@@ -57,26 +77,23 @@ select
     ${limit};
 `
 
+const QueryHitRate = `-- Cache and index hit rate
+select
+    'index hit rate' as name,
+    (sum(idx_blks_hit)) / nullif(sum(idx_blks_hit + idx_blks_read),0) as ratio
+  from pg_statio_user_indexes
+  union all
+  select
+    'table hit rate' as name,
+    sum(heap_blks_hit) / nullif(sum(heap_blks_hit) + sum(heap_blks_read),0) as ratio
+  from pg_statio_user_tables;
+`
+
 export const Content = () => {
-  // const [timeConsumingData, setTimeConsumingData] = useState<any[]>()
-
-  // async function fetchData() {
-  //   try {
-  //     const data = await useDbQuery(QueryMostFrequentlyInvoked)
-  //     setTimeConsumingData(data)
-  //     return data
-  //   } catch (error) {
-  //     throw error
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   fetchData()
-  // }, [])
-
   const QueryMostFrequentlyInvokedData = useDbQuery(QueryMostFrequentlyInvoked)
   const QueryMostTimeConsumingData = useDbQuery(QueryMostTimeConsuming)
   const QuerySlowestExecutionTimeData = useDbQuery(QuerySlowestExecutionTime)
+  const QueryHitRateData = useDbQuery(QueryHitRate)
 
   const isLoadedQueryMostFrequentlyInvokedData =
     QueryMostFrequentlyInvokedData &&
@@ -99,27 +116,92 @@ export const Content = () => {
     QuerySlowestExecutionTimeData[0].data
       ? true
       : false
+  const isLoadedQueryHitRateData =
+    QueryHitRateData &&
+    QueryHitRateData.length > 0 &&
+    QueryHitRateData[0] &&
+    QueryHitRateData[0].data
+      ? true
+      : false
 
-  // if (isLoadedQueryMostFrequentlyInvokedData) {
-  //   console.log('QueryMostFrequentlyInvokedData', QueryMostFrequentlyInvokedData)
-  // }
+  if (isLoadedQueryMostFrequentlyInvokedData) {
+    console.log('QueryMostFrequentlyInvokedData', QueryMostFrequentlyInvokedData)
+  }
   if (isLoadedQueryMostTimeConsumingData) {
     console.log('QueryMostTimeConsumingData', QueryMostTimeConsumingData)
   }
-  // if (isLoadedQuerySlowestExecutionTimeData) {
-  //   console.log('QuerySlowestExecutionTimeData', QuerySlowestExecutionTimeData)
-  // }
+  if (isLoadedQuerySlowestExecutionTimeData) {
+    console.log('QuerySlowestExecutionTimeData', QuerySlowestExecutionTimeData)
+  }
+  if (isLoadedQueryHitRateData) {
+    console.log('QueryHitRateData', QueryHitRateData)
+  }
 
   return (
     <div className="my-8">
       <div className="px-16 mx-auto">
         <h1 className="text-3xl my-8">Insights</h1>
       </div>
+
+      <div className="px-16 mx-auto my-12">
+        <h4 className="mb-4 text-2xl">Index hit rate</h4>
+        <p className="text-scale-1100 text-sm py-2 mb-6 max-w-2xl">
+          For normal operations of Postgres and performance, you'll want to have your Postgres cache
+          hit ratio about 99%. If you see your cache hit ratio below that, you probably need to look
+          at moving to an instance with larger memory.
+        </p>
+        <div className="flex flex-row gap-16">
+          <div>
+            {QueryHitRateData[0]?.data[0]?.name}
+            <div className="flex items-center gap-6">
+              <div className="flex items-baseline">
+                <h1 className="text-5xl">
+                  {(QueryHitRateData[0]?.data[0]?.ratio * 100).toFixed(2)}
+                </h1>
+                <span className="text-3xl">%</span>
+              </div>
+              <div className="w-8 h-8 bg-brand-400 rounded border border-brand-500 text-brand-900 flex items-center justify-center">
+                <IconThumbsUp strokeWidth={2} size={16} />
+              </div>
+            </div>
+          </div>
+          <div>
+            {QueryHitRateData[0]?.data[1]?.name}
+            <div className="flex items-center gap-6">
+              <div className="flex items-baseline">
+                <h1 className="text-5xl">
+                  {(QueryHitRateData[0]?.data[1]?.ratio * 100).toFixed(2)}
+                </h1>
+                <span className="text-3xl">%</span>
+              </div>
+              <div className="w-8 h-8 bg-brand-400 rounded border border-brand-500 text-brand-900 flex items-center justify-center">
+                <IconThumbsUp strokeWidth={2} size={16} />
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Hit rate */}
+        {/* <div className="w-full h-4 bg-scale-400 relative rounded-full overflow-hidden">
+          <div
+            style={{ width: QueryHitRateData[0]?.data[0]?.ratio * 100 + '%' }}
+            className={`bg-brand-900 h-4 absolute`}
+          ></div>
+        </div> */}
+      </div>
+
       <div className="mx-auto flex flex-col">
+        <div className="px-16">
+          <h4 className="mb-4 text-2xl">Query analysis</h4>
+          <p className="text-scale-1100 text-sm py-2 mb-6 max-w-2xl">
+            For normal operations of Postgres and performance, you'll want to have your Postgres
+            cache hit ratio about 99%. If you see your cache hit ratio below that, you probably need
+            to look at moving to an instance with larger memory.
+          </p>
+        </div>
         <Tabs
           scrollable
           type="underlined"
-          size="large"
+          size="medium"
           listClassNames="px-16"
           addOnAfter={
             <Button
@@ -146,26 +228,26 @@ export const Content = () => {
               head={
                 <>
                   <Table.th className="table-cell">rol name</Table.th>
-                  <Table.th className="table-cell">total_exec_time</Table.th>
-                  <Table.th className="table-cell">prop_exec_time</Table.th>
-                  <Table.th className="table-cell">ncalls</Table.th>
-                  <Table.th className="table-cell">query</Table.th>
+                  <Table.th className="table-cell">prop_total_time</Table.th>
+                  <Table.th className="table-cell">calls</Table.th>
+                  <Table.th className="table-cell">total_time</Table.th>
+                  <Table.th className="table-cell w-[200px]">query</Table.th>
                 </>
               }
               body={
-                isLoadedQueryMostFrequentlyInvokedData ? (
-                  QueryMostFrequentlyInvokedData[0].data.map((item, i) => {
+                isLoadedQueryMostTimeConsumingData ? (
+                  QueryMostTimeConsumingData[0].data.map((item, i) => {
                     return (
                       <Table.tr key={i} hoverable className="relative">
                         <Table.td className="table-cell whitespace-nowrap">{item.rolname}</Table.td>
                         <Table.td className="table-cell whitespace-nowrap">
-                          {item.total_exec_time.milliseconds} ms
+                          {item.prop_total_time}
                         </Table.td>
+                        <Table.td className="table-cell whitespace-nowrap">{item.calls}</Table.td>
                         <Table.td className="table-cell whitespace-nowrap">
-                          {item.prop_exec_time}
+                          {item.total_time}
                         </Table.td>
-                        <Table.td className="table-cell whitespace-nowrap">{item.ncalls}</Table.td>
-                        <Table.td className="table-cell whitespace-nowrap truncate">
+                        <Table.td className="w-[200px] table-cell whitespace-nowrap truncate">
                           <p className="truncate">{item.query}</p>
                         </Table.td>
                       </Table.tr>
@@ -192,38 +274,36 @@ export const Content = () => {
                 <>
                   {/* <Table.th className="table-cell">source</Table.th> */}
                   <Table.th className="table-cell">rolname</Table.th>
-                  <Table.th className="table-cell">ncalls</Table.th>
-                  <Table.th className="table-cell">prop_exec_time</Table.th>
-                  <Table.th className="table-cell">total_exec_time</Table.th>
+                  <Table.th className="table-cell">avg_rows</Table.th>
+                  <Table.th className="table-cell">calls</Table.th>
+                  <Table.th className="table-cell">max_time</Table.th>
+                  <Table.th className="table-cell">mean_time</Table.th>
+                  <Table.th className="table-cell">min_time</Table.th>
+                  <Table.th className="table-cell">total_time</Table.th>
                   <Table.th className="table-cell">query</Table.th>
                 </>
               }
               body={
-                isLoadedQueryMostTimeConsumingData ? (
-                  QueryMostTimeConsumingData[0].data.map((item, i) => {
+                isLoadedQueryMostFrequentlyInvokedData ? (
+                  QueryMostFrequentlyInvokedData[0].data.map((item, i) => {
                     return (
                       <Table.tr key={i} hoverable className="relative">
-                        {/* <Table.td className="whitespace-nowrap flex gap-3 items-center text-scale-1200">
-                          {item.query.includes('-- source: dashboard') ? (
-                            <>
-                              <IconMonitor size={12} /> Dashboard
-                            </>
-                          ) : (
-                            <>
-                              <IconGlobe size={12} /> API
-                            </>
-                          )}
-                        </Table.td> */}
+                        <Table.td className="table-cell whitespace-nowrap">{item.rolname}</Table.td>
                         <Table.td className="table-cell whitespace-nowrap">
-                          {item.rolname}{' '}
+                          {item.avg_rows}
                         </Table.td>
-                        <Table.td className="table-cell whitespace-nowrap">{item.ncalls}</Table.td>
+                        <Table.td className="table-cell whitespace-nowrap">{item.calls}</Table.td>
                         <Table.td className="table-cell whitespace-nowrap">
-                          {item.prop_exec_time}
+                          {item.max_time}
                         </Table.td>
-                        <Table.td className="table-cell whitespace-nowrap">
-                          {item.total_exec_time.seconds && item.total_exec_time.seconds + 's'}{' '}
-                          {item.total_exec_time.milliseconds}ms
+                        <Table.td className="table-cell whitespace-nowrap truncate">
+                          {item.mean_time}
+                        </Table.td>
+                        <Table.td className="table-cell whitespace-nowrap truncate">
+                          {item.min_time}
+                        </Table.td>
+                        <Table.td className="table-cell whitespace-nowrap truncate">
+                          {item.total_time}
                         </Table.td>
                         <Table.td className="table-cell whitespace-nowrap truncate">
                           {item.query}
@@ -250,13 +330,13 @@ export const Content = () => {
             <Table
               head={
                 <>
-                  <Table.th className="table-cell">rol name</Table.th>
+                  <Table.th className="table-cell">rolname</Table.th>
+                  <Table.th className="table-cell">avg_rows</Table.th>
                   <Table.th className="table-cell">calls</Table.th>
-                  <Table.th className="table-cell">total_time</Table.th>
-                  <Table.th className="table-cell">min_time</Table.th>
                   <Table.th className="table-cell">max_time</Table.th>
                   <Table.th className="table-cell">mean_time</Table.th>
-                  <Table.th className="table-cell">avg_rows</Table.th>
+                  <Table.th className="table-cell">min_time</Table.th>
+                  <Table.th className="table-cell">total_time</Table.th>
                   <Table.th className="table-cell">query</Table.th>
                 </>
               }
@@ -266,21 +346,21 @@ export const Content = () => {
                     return (
                       <Table.tr key={i} hoverable className="relative">
                         <Table.td className="table-cell whitespace-nowrap">{item.rolname}</Table.td>
+                        <Table.td className="table-cell whitespace-nowrap">
+                          {item.avg_rows}
+                        </Table.td>
                         <Table.td className="table-cell whitespace-nowrap">{item.calls}</Table.td>
                         <Table.td className="table-cell whitespace-nowrap">
-                          {item.total_time}
+                          {item.max_time}
                         </Table.td>
-                        <Table.td className="table-cell whitespace-nowrap">
+                        <Table.td className="table-cell whitespace-nowrap truncate">
+                          {item.mean_time}
+                        </Table.td>
+                        <Table.td className="table-cell whitespace-nowrap truncate">
                           {item.min_time}
                         </Table.td>
                         <Table.td className="table-cell whitespace-nowrap truncate">
-                          {item.max_time}
-                        </Table.td>
-                        <Table.td className="table-cell whitespace-nowrap">
-                          {item.mean_time}
-                        </Table.td>
-                        <Table.td className="table-cell whitespace-nowrap">
-                          {item.avg_rows}
+                          {item.total_time}
                         </Table.td>
                         <Table.td className="table-cell whitespace-nowrap truncate">
                           {item.query}
