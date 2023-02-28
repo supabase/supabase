b4eef32fa194ad5dc9b5b94f2df29458cc9f0f5b
Terry Sutton <saltcod@gmail.com>
2023-02-28 16:24:15 -0330

    Fix ts error

--- apps/www/components/LaunchWeek/Ticket/ticket-image.tsx
+++ apps/www/components/LaunchWeek/Ticket/ticket-image.tsx
@@ -16,7 +16,7 @@ export default function TicketImage() {
               rel="stylesheet"
             />
           </Head>
-          <TicketVisual
+          {/* <TicketVisual
             size={1700 / 650}
             username={query.username ? query.username.toString() : undefined}
             ticketNumber={parseInt(query.ticketNumber.toString(), 10)}
@@ -28,7 +28,7 @@ export default function TicketImage() {
                 : undefined
             }
             golden={query.golden ? true : false}
-          />
+          /> */}
         </div>
       </div>
     )
