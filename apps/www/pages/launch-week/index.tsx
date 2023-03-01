fcfc2fa9f85e226fe4baaf36c77b75548829811f
Terry Sutton <saltcod@gmail.com>
2023-03-01 17:09:02 -0330

    Fix build error

--- apps/www/pages/launch-week/index.tsx
+++ apps/www/pages/launch-week/index.tsx
@@ -95,7 +95,6 @@ export default function TicketHome() {
           </SectionContainer>
 
           <LaunchWeekPrizeSection />
-          <TicketBrickWall />
         </div>
       </DefaultLayout>
     </>
