9b1825df370b268d284a213161e574eab6618bb1
TzeYiing <ty@tzeyiing.com>
2023-03-01 18:45:25 +0800

    fix: minor styling tweaks to prevent chart overflow hiding

--- studio/components/ui/Charts/BarChart.tsx
+++ studio/components/ui/Charts/BarChart.tsx
@@ -121,7 +121,7 @@ const BarChart: React.FC<BarChartProps> = ({
         </RechartBarChart>
       </Container>
       {data && (
-        <div className="text-scale-900 -mt-5 flex items-center justify-between text-xs">
+        <div className="text-scale-900 -mt-9 flex items-center justify-between text-xs">
           <span>{dayjs(data[0][xAxisKey]).format(customDateFormat)}</span>
           <span>{dayjs(data[data?.length - 1]?.[xAxisKey]).format(customDateFormat)}</span>
         </div>
