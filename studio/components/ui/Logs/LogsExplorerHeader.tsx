420b33d18e8d245f7a3bbeea4bb171ce123bd92e
Joshen Lim <joshenlimek@gmail.com>
2023-03-02 15:00:13 +0800

    Small style fix

--- studio/components/ui/Logs/LogsExplorerHeader.tsx
+++ studio/components/ui/Logs/LogsExplorerHeader.tsx
@@ -1,16 +1,29 @@
-import Link from 'next/link'
-import { FC } from 'react'
-import { Button, IconBookOpen, IconExternalLink, IconList, IconX, SidePanel, Tabs } from 'ui'
+import * as Tooltip from '@radix-ui/react-tooltip'
 import { LOGS_EXPLORER_DOCS_URL } from 'components/interfaces/Settings/Logs'
-import { useState } from 'react'
-import { logConstants } from 'shared-data'
 import Table from 'components/to-be-cleaned/Table'
+import { useStore } from 'hooks'
+import { copyToClipboard } from 'lib/helpers'
+import Link from 'next/link'
+import { FC, useState } from 'react'
+import { logConstants } from 'shared-data'
+import {
+  Button,
+  IconBookOpen,
+  IconCheck,
+  IconClipboard,
+  IconExternalLink,
+  IconList,
+  IconX,
+  SidePanel,
+  Tabs,
+} from 'ui'
 
 interface Props {
   subtitle?: string
 }
 
 const LogsExplorerHeader: FC<Props> = ({ subtitle }) => {
+  const { ui } = useStore()
   const [showReference, setShowReference] = useState(false)
 
   return (
@@ -27,69 +40,133 @@ const LogsExplorerHeader: FC<Props> = ({ subtitle }) => {
         <h1 className="text-2xl text-scale-1200">Logs Explorer</h1>
         {subtitle && <span className="text-2xl text-scale-1000">{subtitle}</span>}
       </div>
-      <div className="flex flex-row gap-4">
+      <div className="flex flex-row gap-2">
+        <Link href={LOGS_EXPLORER_DOCS_URL}>
+          <a>
+            <Button type="default" icon={<IconExternalLink strokeWidth={1.5} />}>
+              Documentation
+            </Button>
+          </a>
+        </Link>
 
-      <Link href={LOGS_EXPLORER_DOCS_URL}>
-        <a>
-          <Button type="default" icon={<IconExternalLink strokeWidth={1.5} />}>
-            Documentation
-          </Button>
-        </a>
-      </Link>
-
-      <SidePanel
-        size="large"
-        header={
-          <div className="flex flex-row justify-between items-center">
-            <h3>Field Reference</h3>
+        <SidePanel
+          size="large"
+          header={
+            <div className="flex flex-row justify-between items-center">
+              <h3>Field Reference</h3>
+              <Button
+                type="text"
+                className="px-1"
+                onClick={() => setShowReference(false)}
+                icon={<IconX size={18} strokeWidth={1.5} />}
+              />
+            </div>
+          }
+          visible={showReference}
+          cancelText="Close"
+          onCancel={() => setShowReference(false)}
+          hideFooter
+          triggerElement={
             <Button
               type="default"
-              onClick={() => setShowReference(false)}
-              icon={<IconX strokeWidth={1.5} />}
+              onClick={() => setShowReference(true)}
+              icon={<IconBookOpen strokeWidth={1.5} />}
             >
-              Close
+              Field Reference
             </Button>
-          </div>
-        }
-        visible={showReference}
-        cancelText="Close"
-        onCancel={() => setShowReference(false)}
-        hideFooter
-        triggerElement={
-          <Button
-            type="default"
-            onClick={() => setShowReference(true)}
-            icon={<IconBookOpen strokeWidth={1.5} />}
+          }
+        >
+          <SidePanel.Content>
+            <div className="pt-4 pb-2">
+              <p className="text-sm">
+                The following table shows all the available paths that can be queried from each
+                respective source, by following the syntax:{' '}
+                <code className="text-xs">
+                  select {`{path}`} from {`{source}`};
+                </code>
+              </p>
+            </div>
+          </SidePanel.Content>
+          <SidePanel.Separator />
+          <Tabs
+            scrollable
+            size="small"
+            type="underlined"
+            defaultActiveId="edge_logs"
+            listClassNames="px-2"
           >
-            Field Reference
-          </Button>
-        }
-      >
-        <Tabs scrollable size="small" type="underlined" defaultActiveId="edge_logs" listClassNames="px-2">
-          {logConstants.schemas.map((schema) => (
-            <Tabs.Panel id={schema.reference} label={schema.name} className="px-4">
-              <Table
-                head={[
-                  <Table.th className="text-xs !p-2" key="path">
-                    Path
-                  </Table.th>,
-                  <Table.th key="type" className="text-xs !p-2">
-                    Type
-                  </Table.th>,
-                ]}
-                body={schema.fields
-                  .sort((a: any, b: any) => a.path - b.path)
-                  .map((field) => (
-                    <Table.tr key={field.path}>
-                      <Table.td className="font-mono text-xs !p-2">{field.path}</Table.td>
-                      <Table.td className="font-mono text-xs !p-2">{field.type}</Table.td>
-                    </Table.tr>
-                  ))}
-              />
-            </Tabs.Panel>
-          ))}
-        </Tabs>
-      </SidePanel>
+            {logConstants.schemas.map((schema) => (
+              <Tabs.Panel id={schema.reference} label={schema.name} className="px-4 pb-4">
+                <Table
+                  head={[
+                    <Table.th className="text-xs !p-2" key="path">
+                      Path
+                    </Table.th>,
+                    <Table.th key="type" className="text-xs !p-2">
+                      Type
+                    </Table.th>,
+                  ]}
+                  body={schema.fields
+                    .sort((a: any, b: any) => a.path - b.path)
+                    .map((field) => {
+                      const [isCopied, setIsCopied] = useState(false)
+                      return (
+                        <Table.tr key={field.path}>
+                          <Table.td
+                            className="font-mono text-xs !p-2 cursor-pointer hover:text-scale-1200 transition flex items-center space-x-2"
+                            onClick={() =>
+                              copyToClipboard(field.path, () => {
+                                setIsCopied(true)
+                                setTimeout(() => setIsCopied(false), 3000)
+                              })
+                            }
+                          >
+                            <span>{field.path}</span>
+                            {isCopied ? (
+                              <Tooltip.Root delayDuration={0}>
+                                <Tooltip.Trigger>
+                                  <IconCheck size={14} strokeWidth={3} className="text-brand-900" />
+                                </Tooltip.Trigger>
+                                <Tooltip.Content side="bottom">
+                                  <Tooltip.Arrow className="radix-tooltip-arrow" />
+                                  <div
+                                    className={[
+                                      'rounded bg-scale-100 py-1 px-2 leading-none shadow',
+                                      'border border-scale-200',
+                                    ].join(' ')}
+                                  >
+                                    <span className="text-xs text-scale-1200">Copied</span>
+                                  </div>
+                                </Tooltip.Content>
+                              </Tooltip.Root>
+                            ) : (
+                              <Tooltip.Root delayDuration={0}>
+                                <Tooltip.Trigger>
+                                  <IconClipboard size="tiny" strokeWidth={1.5} />
+                                </Tooltip.Trigger>
+                                <Tooltip.Content side="bottom">
+                                  <Tooltip.Arrow className="radix-tooltip-arrow" />
+                                  <div
+                                    className={[
+                                      'rounded bg-scale-100 py-1 px-2 leading-none shadow',
+                                      'border border-scale-200',
+                                    ].join(' ')}
+                                  >
+                                    <span className="text-xs text-scale-1200">Copy value</span>
+                                  </div>
+                                </Tooltip.Content>
+                              </Tooltip.Root>
+                            )}
+                          </Table.td>
+                          <Table.td className="font-mono text-xs !p-2">{field.type}</Table.td>
+                        </Table.tr>
+                      )
+                    })}
+                />
+              </Tabs.Panel>
+            ))}
+          </Tabs>
+        </SidePanel>
       </div>
     </div>
   )
