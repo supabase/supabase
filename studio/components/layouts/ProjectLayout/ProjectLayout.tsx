85a1664d16cb045c82f53c7a5124ee49aca8514e
Joshen Lim <joshenlimek@gmail.com>
2023-03-02 13:52:19 +0800

    fix

--- studio/components/layouts/ProjectLayout/ProjectLayout.tsx
+++ studio/components/layouts/ProjectLayout/ProjectLayout.tsx
@@ -14,6 +14,7 @@ import PausingState from './PausingState'
 import BuildingState from './BuildingState'
 import { ProjectContextProvider } from './ProjectContext'
 import RestoringState from './RestoringState'
+import UpgradingState from './UpgradingState'
 
 interface Props {
   title?: string
@@ -125,6 +126,8 @@ const ContentWrapper: FC<ContentWrapperProps> = observer(({ isLoading, children 
     <>
       {isLoading || ui.selectedProject === undefined ? (
         <Connecting />
+      ) : isProjectUpgrading ? (
+        <UpgradingState />
       ) : isProjectPausing ? (
         <PausingState project={ui.selectedProject} />
       ) : requiresPostgrestConnection && isProjectOffline ? (
