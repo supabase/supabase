3056e27c27e9148d147ec96641ee817a757cc889
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-03-04 15:44:11 +0100

    Finish prize section layout

--- apps/www/components/LaunchWeek/LabelBadge.tsx
+++ apps/www/components/LaunchWeek/LabelBadge.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function LabelBadge({ text, gradientText = true }: Props) {
   return (
-    <span className="bg-[#32313F] rounded-full px-4 py-2 text-xs">
+    <span className="bg-[#32313F] rounded px-1 py-[2px] text-xs">
       <span className={!gradientText ? '' : 'gradient-text-purple-500'}>{text}</span>
     </span>
   )
