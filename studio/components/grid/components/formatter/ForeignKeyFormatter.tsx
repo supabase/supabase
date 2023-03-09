9e966a6fd01e7f4d2a41af5433420669b93679bc
Joshen Lim <joshenlimek@gmail.com>
2023-03-09 16:04:09 +0800

    Scaffold new action for fk cell

--- studio/components/grid/components/formatter/ForeignKeyFormatter.tsx
+++ studio/components/grid/components/formatter/ForeignKeyFormatter.tsx
@@ -1,43 +1,52 @@
-import * as React from 'react'
+import Link from 'next/link'
+import { PropsWithChildren } from 'react'
 import { FormatterProps } from '@supabase/react-data-grid'
 import { SupaRow } from '../../types'
 import { NullValue } from '../common'
 import { ForeignTableModal } from '../common/ForeignTableModal'
 import { useDispatch, useTrackedState } from '../../store'
 import { deepClone } from '../../utils'
+import * as Tooltip from '@radix-ui/react-tooltip'
+import { Button, IconArrowRight } from 'ui'
+import { useParams } from 'hooks'
 
-export const ForeignKeyFormatter = (
-  p: React.PropsWithChildren<FormatterProps<SupaRow, unknown>>
-) => {
-  const state = useTrackedState()
-  const dispatch = useDispatch()
+export const ForeignKeyFormatter = (p: PropsWithChildren<FormatterProps<SupaRow, unknown>>) => {
+  const { ref, id } = useParams()
   const value = p.row[p.column.key]
-
-  function onRowChange(_value: any | null) {
-    const rowData = deepClone(p.row)
-    rowData[p.column.key] = _value
-
-    const { error } = state.rowService!.update(rowData, p.row, p.column.key)
-    if (error) {
-      if (state.onError) state.onError(error)
-    } else {
-      dispatch({
-        type: 'EDIT_ROW',
-        payload: { row: rowData, idx: p.row.idx },
-      })
-    }
-  }
-
-  function onChange(_value: any | null) {
-    onRowChange(_value)
-  }
 
   return (
     <div className="sb-grid-foreign-key-formatter flex justify-between">
       <span className="sb-grid-foreign-key-formatter__text">
         {value === null ? <NullValue /> : value}
       </span>
-      <ForeignTableModal columnName={p.column.key} defaultValue={value} onChange={onChange} />
+      <Tooltip.Root delayDuration={0}>
+        <Tooltip.Trigger>
+          <Link href={`/project/${ref}/editor/${id}?filter={primary_key}%3Aeq%3A${value}`}>
+            <a>
+              <Button
+                type="default"
+                size="tiny"
+                className="translate-y-[2px]"
+                onClick={() => {}}
+                icon={<IconArrowRight size="tiny" />}
+                style={{ padding: '3px' }}
+              />
+            </a>
+          </Link>
+        </Tooltip.Trigger>
+        <Tooltip.Content side="bottom">
+          <Tooltip.Arrow className="radix-tooltip-arrow" />
+          <div
+            className={[
+              'rounded bg-scale-100 py-1 px-2 leading-none shadow',
+              'border border-scale-200',
+            ].join(' ')}
+          >
+            <span className="text-xs text-scale-1200">View referencing record</span>
+          </div>
+        </Tooltip.Content>
+      </Tooltip.Root>
+      {/* <ForeignTableModal columnName={p.column.key} defaultValue={value} onChange={onChange} /> */}
     </div>
   )
 }
