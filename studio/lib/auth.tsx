070c52ebaefe64908073922fc93272056fb6d77f
Joshen Lim <joshenlimek@gmail.com>
2023-03-16 09:19:20 +0800

    Return default session if self hosted

--- studio/lib/auth.tsx
+++ studio/lib/auth.tsx
@@ -13,6 +13,29 @@ import {
 } from 'react'
 import { GOTRUE_ERRORS } from './constants'
 
+const DEFAULT_SESSION: any = {
+  access_token: undefined,
+  expires_at: 0,
+  expires_in: 0,
+  refresh_token: '',
+  token_type: '',
+  user: {
+    aud: '',
+    app_metadata: {},
+    confirmed_at: '',
+    created_at: '',
+    email: '',
+    email_confirmed_at: '',
+    id: '',
+    identities: [],
+    last_signed_in_at: '',
+    phone: '',
+    role: '',
+    updated_at: '',
+    user_metadata: {},
+  },
+}
+
 /* Auth Context */
 
 export type AuthContext = { refreshSession: () => Promise<Session | null> } & (
@@ -123,7 +146,7 @@ export const AuthProvider = ({ children }: PropsWithChildren<AuthProviderProps>)
       return { session, isLoading: false, refreshSession } as const
     }
 
-    return { session: null, isLoading: isLoading, refreshSession } as const
+    return { session: DEFAULT_SESSION, isLoading: isLoading, refreshSession } as const
   }, [session, isLoading, refreshSession])
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
