baf6da89ad8128bd0a9224d4b6d28b26486ff210
Alaister Young <a@alaisteryoung.com>
2023-03-16 12:24:05 +0800

    fix: add new self hosted endpoints

---
+++ studio/pages/api/storage/[ref]/buckets/[id]/objects/public-url.ts
@@ -0,0 +1,28 @@
+import { createClient } from '@supabase/supabase-js'
+import apiWrapper from 'lib/api/apiWrapper'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+const supabase = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_SERVICE_KEY!)
+
+export default (req: NextApiRequest, res: NextApiResponse) => apiWrapper(req, res, handler)
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { method } = req
+
+  switch (method) {
+    case 'POST':
+      return handlePost(req, res)
+    default:
+      res.setHeader('Allow', ['POST'])
+      res.status(405).json({ data: null, error: { message: `Method ${method} Not Allowed` } })
+  }
+}
+
+const handlePost = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { id } = req.query
+  const { path } = req.body
+
+  const { data } = supabase.storage.from(id as string).getPublicUrl(path)
+
+  return res.status(200).json(data)
+}
