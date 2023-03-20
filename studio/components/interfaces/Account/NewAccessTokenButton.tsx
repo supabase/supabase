f8e25b958e5dfa322a520bdacfc3523a6032706c
phamhieu <phamhieu1998@gmail.com>
2023-03-20 11:46:34 +0700

    feat: generate experimental api token

--- studio/components/interfaces/Account/NewAccessTokenButton.tsx
+++ studio/components/interfaces/Account/NewAccessTokenButton.tsx
@@ -1,5 +1,5 @@
 import { FC, useState } from 'react'
-import { Input, Button, Modal, Form, Alert } from 'ui'
+import { Input, Button, Modal, Form, Alert, IconMoreVertical, Dropdown } from 'ui'
 import { useStore } from 'hooks'
 import {
   NewAccessToken,
@@ -11,6 +11,7 @@ const NewAccessTokenButton = observer(() => {
   const { ui } = useStore()
   const [isOpen, setIsOpen] = useState(false)
   const [newToken, setNewToken] = useState<NewAccessToken | undefined>(undefined)
+  const [tokenScope, setTokenScope] = useState<'V0' | undefined>(undefined)
 
   const validate = (values: any) => {
     const errors: any = {}
@@ -26,7 +27,8 @@ const NewAccessTokenButton = observer(() => {
     setSubmitting(true)
 
     try {
-      const response = await createAccessToken({ name: values.tokenName })
+      console.log('submit tokenScope: ', tokenScope)
+      const response = await createAccessToken({ name: values.tokenName, scope: tokenScope })
       setNewToken(response)
 
       setSubmitting(false)
@@ -43,14 +45,39 @@ const NewAccessTokenButton = observer(() => {
 
   return (
     <>
-      <Button
-        onClick={() => {
-          setNewToken(undefined)
-          setIsOpen(!isOpen)
-        }}
-      >
-        Generate new token
-      </Button>
+      <div className="container max-w-7xl">
+        <div className="flex justify-between">
+          <Button
+            onClick={() => {
+              setNewToken(undefined)
+              setTokenScope(undefined)
+              setIsOpen(!isOpen)
+            }}
+          >
+            Generate new token
+          </Button>
+          <div className="flex items-center space-x-4">
+            <Dropdown
+              side="bottom"
+              align="start"
+              overlay={[
+                <Dropdown.Item
+                  key="toggle-private"
+                  onClick={() => {
+                    setNewToken(undefined)
+                    setTokenScope('V0')
+                    setIsOpen(!isOpen)
+                  }}
+                >
+                  Generate experimental api token
+                </Dropdown.Item>,
+              ]}
+            >
+              <IconMoreVertical size="medium" strokeWidth={2} />
+            </Dropdown>
+          </div>
+        </div>
+      </div>
       {newToken && <NewTokenItem data={newToken} />}
       <Modal
         closable
@@ -60,7 +87,9 @@ const NewAccessTokenButton = observer(() => {
         onCancel={() => setIsOpen(!isOpen)}
         header={
           <div className="flex items-baseline gap-2">
-            <h5 className="text-sm text-scale-1200">Generate New Token</h5>
+            <h5 className="text-sm text-scale-1200">
+              {tokenScope === 'V0' ? 'Generate Experimental API Token' : 'Generate New Token'}
+            </h5>
           </div>
         }
       >
@@ -80,6 +109,18 @@ const NewAccessTokenButton = observer(() => {
                   className="w-full"
                 />
               </Modal.Content>
+              {tokenScope === 'V0' && (
+                <Modal.Content>
+                  <Alert
+                    withIcon
+                    variant="warning"
+                    title="Experimental api provides endpoints that can delete your organizations and projects. These actions cannot be undone."
+                  >
+                    As such, it is reserved for advanced users only. If you do not know about this,
+                    do not use.
+                  </Alert>
+                </Modal.Content>
+              )}
               <Modal.Separator />
               <Modal.Content>
                 <Button htmlType="submit" loading={isSubmitting} size="small" block danger>
