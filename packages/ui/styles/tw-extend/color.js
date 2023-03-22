44c04f85360f2e00d60ae761b23c6a3bef3e9a1d
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2023-03-22 15:21:56 +0800

    update new coloras

---
+++ packages/ui/styles/tw-extend/color.js
@@ -0,0 +1,6 @@
+module.exports = {
+"backgroundColor-DEFAULT": "var(--background-color-default, #232323);",
+"backgroundColor-body": "var(--background-color-body, #1c1c1c);",
+"borderColor-DEFAULT": "var(--border-color-default, #282828);",
+"borderColor-body": "var(--border-color-body, #282828);"
+}
