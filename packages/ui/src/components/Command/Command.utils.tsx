d9fef00e47b7526ac59c59f249f3c60886f379de
Greg Richardson <greg.nmr@gmail.com>
2023-03-22 13:49:16 -0600

    chore(cmdk): fix more custom command types

--- packages/ui/src/components/Command/Command.utils.tsx
+++ packages/ui/src/components/Command/Command.utils.tsx
@@ -54,33 +54,34 @@ const CommandDialog = ({ children, onKeyDown, ...props }: CommandDialogProps) =>
 
 type CommandPrimitiveInputProps = React.ComponentPropsWithoutRef<typeof CommandPrimitive.Input>
 
-type CommandInput = CommandPrimitiveInputProps & {
+type CommandInputProps = CommandPrimitiveInputProps & {
   loading?: boolean
   _key?: string | number
 }
 
-const CommandInput = React.forwardRef<HTMLInputElement, CommandInput>(
-  ({ className, value, loading, onValueChange, _key, ...props }, ref) => (
-    <div className="flex flex-col items-center" cmdk-input-wrapper="">
-      {/* <IconSearch strokeWidth={2} className="text-scale-1200 mr-2 h-4 w-4 shrink-0 opacity-50" /> */}
-      <CommandPrimitive.Input
-        value={value}
-        key={_key}
-        autoFocus
-        onValueChange={onValueChange}
-        ref={ref}
-        className={cn(
-          'flex h-11 w-full rounded-md bg-transparent px-4 py-7 text-sm outline-none',
-          'focus:shadow-none focus:ring-transparent',
-          'placeholder:text-scale-800 disabled:cursor-not-allowed disabled:opacity-50 dark:text-scale-1200 border-0',
-          className
-        )}
-        {...props}
-      />
-      <LoadingLine loading={loading} />
-    </div>
-  )
-)
+const CommandInput = React.forwardRef<
+  React.ElementRef<typeof CommandPrimitive.Input>,
+  CommandInputProps
+>(({ className, value, loading, onValueChange, _key, ...props }, ref) => (
+  <div className="flex flex-col items-center" cmdk-input-wrapper="">
+    {/* <IconSearch strokeWidth={2} className="text-scale-1200 mr-2 h-4 w-4 shrink-0 opacity-50" /> */}
+    <CommandPrimitive.Input
+      value={value}
+      key={_key}
+      autoFocus
+      onValueChange={onValueChange}
+      ref={ref}
+      className={cn(
+        'flex h-11 w-full rounded-md bg-transparent px-4 py-7 text-sm outline-none',
+        'focus:shadow-none focus:ring-transparent',
+        'placeholder:text-scale-800 disabled:cursor-not-allowed disabled:opacity-50 dark:text-scale-1200 border-0',
+        className
+      )}
+      {...props}
+    />
+    <LoadingLine loading={loading} />
+  </div>
+))
 
 CommandInput.displayName = CommandPrimitive.Input.displayName
 
@@ -110,9 +111,15 @@ const CommandEmpty = React.forwardRef<
 
 CommandEmpty.displayName = CommandPrimitive.Empty.displayName
 
+type CommandPrimitiveGroupProps = React.ComponentPropsWithoutRef<typeof CommandPrimitive.Group>
+
+type CommandGroupProps = CommandPrimitiveGroupProps & {
+  _key?: string | number
+}
+
 const CommandGroup = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive.Group>,
-  React.ComponentPropsWithoutRef<typeof CommandPrimitive.Group>
+  CommandGroupProps
 >(({ className, _key, ...props }, ref) => (
   <CommandPrimitive.Group
     key={_key}
