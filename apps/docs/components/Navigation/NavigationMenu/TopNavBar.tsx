68fabc87c38acb78b1b144ef6f78ca1565d4e932
Greg Richardson <greg.nmr@gmail.com>
2023-03-23 13:27:15 -0600

    refactor(cmdk): remove old docs search

--- apps/docs/components/Navigation/NavigationMenu/TopNavBar.tsx
+++ apps/docs/components/Navigation/NavigationMenu/TopNavBar.tsx
@@ -1,13 +1,21 @@
+import { useTheme } from 'common/Providers'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { FC, useEffect, useState } from 'react'
-import { Button, IconCommand, IconMenu, IconMoon, IconSearch, IconSun, Input, Listbox } from 'ui'
+import {
+  Button,
+  IconCommand,
+  IconMenu,
+  IconMoon,
+  IconSearch,
+  IconSun,
+  Listbox,
+  SearchButton,
+} from 'ui'
 import { REFERENCES } from '~/components/Navigation/Navigation.constants'
-import { useTheme } from 'common/Providers'
 
 import { getPageType } from '~/lib/helpers'
-import SearchButton from '~/components/Search/SearchButton'
 
 const TopNavBar: FC = () => {
   const { isDarkMode, toggleTheme } = useTheme()
