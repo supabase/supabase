b931487c3e9b856b135e492ff12171e4955892da
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2023-03-24 14:48:01 +0800

    update radix and tailwind to correct versions. update all tooltips with Portal subcomponent

--- studio/components/interfaces/Organization/TeamSettings/TeamSettings.tsx
+++ studio/components/interfaces/Organization/TeamSettings/TeamSettings.tsx
@@ -97,19 +97,21 @@ const TeamSettings = () => {
                   </Button>
                 </Tooltip.Trigger>
                 {!canLeave && (
-                  <Tooltip.Content side="bottom">
-                    <Tooltip.Arrow className="radix-tooltip-arrow" />
-                    <div
-                      className={[
-                        'rounded bg-scale-100 py-1 px-2 leading-none shadow',
-                        'border border-scale-200',
-                      ].join(' ')}
-                    >
-                      <span className="text-xs text-scale-1200">
-                        An organization requires at least 1 owner
-                      </span>
-                    </div>
-                  </Tooltip.Content>
+                  <Tooltip.Portal>
+                    <Tooltip.Content side="bottom">
+                      <Tooltip.Arrow className="radix-tooltip-arrow" />
+                      <div
+                        className={[
+                          'rounded bg-scale-100 py-1 px-2 leading-none shadow',
+                          'border border-scale-200',
+                        ].join(' ')}
+                      >
+                        <span className="text-xs text-scale-1200">
+                          An organization requires at least 1 owner
+                        </span>
+                      </div>
+                    </Tooltip.Content>
+                  </Tooltip.Portal>
                 )}
               </Tooltip.Root>
             </div>
