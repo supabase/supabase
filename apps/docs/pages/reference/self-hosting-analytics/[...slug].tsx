e5c08bf9bb90690d342c53f03890dd6de8791258
Chase Granberry <chase@logflare.app>
2023-03-27 15:39:10 -0700

    Use APISpecs

--- apps/docs/pages/reference/self-hosting-analytics/[...slug].tsx
+++ apps/docs/pages/reference/self-hosting-analytics/[...slug].tsx
@@ -1,5 +1,5 @@
+import analyticsSpec from '~/../../spec/analytics_v0_openapi.json' assert { type: 'json' }
 import selfHostingAnalyticsCommonSections from '~/../../spec/common-self-hosting-analytics-sections.json'
-
 import RefSectionHandler from '~/components/reference/RefSectionHandler'
 import { flattenSections } from '~/lib/helpers'
 import handleRefGetStaticPaths from '~/lib/mdx/handleRefStaticPaths'
@@ -8,8 +8,11 @@ import { gen_v3 } from '~/lib/refGenerator/helpers'
 
 const sections = flattenSections(selfHostingAnalyticsCommonSections)
 
+// @ts-ignore
+const spec = gen_v3(analyticsSpec, 'wat', { apiUrl: 'apiv0' })
+
 export default function JSReference(props) {
-  return <RefSectionHandler sections={sections} pageProps={props} type="api" />
+  return <RefSectionHandler sections={sections} spec={spec} pageProps={props} type="api" />
 }
 
 export async function getStaticProps() {
