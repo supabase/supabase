6397c6124c1e71fc30954141b522ad9f958ccf83
TzeYiing <ty@tzeyiing.com>
2023-03-28 01:15:41 +0800

    feat: use LOGFLARE_URL to build analytics route for now

--- studio/pages/api/constants.ts
+++ studio/pages/api/constants.ts
@@ -1,5 +1,8 @@
 const PUBLIC_URL = new URL(process.env.SUPABASE_PUBLIC_URL || 'http://localhost:8000')
-export const PROJECT_ANALYTICS_URL = `${PUBLIC_URL.origin}/analytics/v1/`
+
+// Use LOGFLARE_URL until analytics/v1/ routing is supported
+export const PROJECT_ANALYTICS_URL = `${process.env.LOGFLARE_URL}/api/`
+
 export const PROJECT_REST_URL = `${PUBLIC_URL.origin}/rest/v1/`
 export const PROJECT_ENDPOINT = PUBLIC_URL.host
 export const PROJECT_ENDPOINT_PROTOCOL = PUBLIC_URL.protocol.replace(':', '')
@@ -14,3 +17,4 @@ export const DEFAULT_PROJECT = {
   region: 'local',
   inserted_at: '2021-08-02T06:40:40.646Z',
 }
+  
