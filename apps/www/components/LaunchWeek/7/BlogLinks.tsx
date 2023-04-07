4498a1bad1caf69129ef52ff93826d664fae44d0
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-04-07 14:53:10 +0200

    chore: prepare lw7 blog post links

--- apps/www/components/LaunchWeek/7/BlogLinks.tsx
+++ apps/www/components/LaunchWeek/7/BlogLinks.tsx
@@ -1,4 +1,6 @@
+import { motion } from 'framer-motion'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 import days from '~/components/LaunchWeek/lw7_days'
 
@@ -57,47 +59,65 @@ const BlogLinks = () => {
   return (
     <div className="flex flex-col gap-3 lg:gap-4 border-t border-scale-400 py-4 lg:py-8 mt-4 lg:mt-8">
       <h3 className="text-white text-xl mb-4">More Launch Week 7</h3>
-      {activeDays.map((day) => (
-        <div className={`h-auto w-full flex flex-col lg:flex-row relative overflow-hidden`}>
-          <div
-            className={`
+      {activeDays.map((day) =>
+        day.steps?.map((step) => (
+          <motion.div
+            className={`h-auto w-full flex flex-col lg:flex-row relative overflow-hidden`}
+            initial="default"
+            animate="default"
+            whileHover="hover"
+          >
+            <Link
+              href={
+                (step.blog || step.github || step.hackernews || step.blog || step.docs) as string
+              }
+            >
+              <a
+                className={`
                 flex flex-col flex-1 gap-3 items-start justify-center border rounded-xl h-full relative overflow-hidden
                 p-6 lg:p-10 text-2xl
                 before:absolute before:w-full before:h-full before:top-52 before:right-0 before:bottom-0 before:left-0
                 before:border-[#1f3536] before:-z-10
               `}
-          >
-            <div className="flex items-center text-lg flex-col-reverse lg:flex-row lg:justify-start gap-2 text-black dark:text-white">
-              <div className="text-transparent bg-clip-text bg-gradient-to-r from-[#F4FFFA] to-[#B7B2C9]">
-                {day.title}
-              </div>
-            </div>
-            <SectionButtons
-              // docs={day.steps[0].docs}
-              blog={day.steps[0].blog}
-              // youtube_id={day.steps[0].youtube_id}
-            />
-          </div>
-          {day.steps[0].thumb && (
-            <Image
-              src={day.steps[0].thumb}
-              className={`
-                  absolute opacity-90
-                  w-full h-full -z-10 transition-all duration-300
-                `}
-              layout="fill"
-              objectPosition="100% 50%"
-              objectFit="cover"
-            />
-          )}
-          <div
-            className={`absolute opacity-10 w-full h-full -z-10 transition-all duration-300`}
-            style={{
-              background: `radial-gradient(650px 150px at 50% 100%, #4635A730, transparent)`,
-            }}
-          />
-        </div>
-      ))}
+              >
+                <div className="flex items-center text-lg flex-col-reverse lg:flex-row lg:justify-start gap-2 text-black dark:text-white">
+                  <div className="text-transparent bg-clip-text bg-gradient-to-r from-[#F4FFFA] to-[#B7B2C9] drop-shadow-lg">
+                    {step.title}
+                  </div>
+                </div>
+                {/* <SectionButtons
+                  // docs={step.docs}
+                  blog={step.blog}
+                  // youtube_id={step.youtube_id}
+                /> */}
+                {step.thumb && (
+                  <motion.div
+                    className="absolute opacity-90 inset-0 w-full h-full -z-10"
+                    variants={hoverVariant}
+                  >
+                    <Image
+                      src={step.thumb}
+                      className={`
+                    absolute opacity-90
+                    w-full h-full -z-10 transition-all duration-300
+                  `}
+                      layout="fill"
+                      objectPosition="100% 50%"
+                      objectFit="cover"
+                    />
+                  </motion.div>
+                )}
+                <div
+                  className={`absolute opacity-10 w-full h-full -z-10 transition-all duration-300`}
+                  style={{
+                    background: `radial-gradient(650px 150px at 50% 100%, #4635A730, transparent)`,
+                  }}
+                />
+              </a>
+            </Link>
+          </motion.div>
+        ))
+      )}
     </div>
   )
 }
@@ -135,5 +155,18 @@ const PlaySvg = () => (
     />
   </svg>
 )
+
+const defaultEase = [0.25, 0.25, 0, 1]
+const hoverVariant = {
+  default: { scale: 1, opacity: 0.9, ease: defaultEase, duration: 0.2 },
+  hover: {
+    scale: 1.05,
+    opacity: 1,
+    transition: {
+      duration: 0.4,
+      ease: defaultEase,
+    },
+  },
+}
 
 export default BlogLinks
