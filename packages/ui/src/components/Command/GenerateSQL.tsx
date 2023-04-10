01dfe9e1b1ccc69f2469144d18381be0d4236024
Greg Richardson <greg.nmr@gmail.com>
2023-04-10 13:16:09 -0600

    feat(cmdk): sql ai to remember chat history context

--- packages/ui/src/components/Command/GenerateSQL.tsx
+++ packages/ui/src/components/Command/GenerateSQL.tsx
@@ -1,11 +1,8 @@
-import { SSE } from 'sse.js'
+import { useCallback, useEffect, useState } from 'react'
 import { format } from 'sql-formatter'
-import type { CreateCompletionResponse } from 'openai'
-import { useCallback, useEffect, useReducer, useRef, useState } from 'react'
 import {
   Button,
   CodeBlock,
-  IconAlertCircle,
   IconAlertTriangle,
   IconCheck,
   IconClipboard,
@@ -13,72 +10,18 @@ import {
   IconSave,
   IconUser,
   Input,
+  MessageRole,
+  MessageStatus,
+  useAiChat,
 } from 'ui'
 
+import CopyToClipboard from 'react-copy-to-clipboard'
 import { cn } from './../../utils/cn'
+import { SAMPLE_QUERIES } from './Command.constants'
 import { AiIcon, AiIconChat } from './Command.icons'
 import { CommandItem } from './Command.utils'
 import { useCommandMenu } from './CommandMenuProvider'
-import CopyToClipboard from 'react-copy-to-clipboard'
-import { SAMPLE_QUERIES } from './Command.constants'
-
-function getEdgeFunctionUrl() {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL?.replace(/\/$/, '')
-
-  if (!supabaseUrl) return undefined
-
-  // https://github.com/supabase/supabase-js/blob/10d3423506cbd56345f7f6ab2ec2093c8db629d4/src/SupabaseClient.ts#L96
-  const isPlatform = supabaseUrl.match(/(supabase\.co)|(supabase\.in)/)
-
-  if (isPlatform) {
-    const [schemeAndProjectId, domain, tld] = supabaseUrl.split('.')
-    return `${schemeAndProjectId}.functions.${domain}.${tld}`
-  } else {
-    return `${supabaseUrl}/functions/v1`
-  }
-}
-
-function promptDataReducer(
-  state: any[],
-  action: {
-    index?: number
-    answer?: string | undefined
-    status?: string
-    query?: string | undefined
-    type?: 'remove-last-item' | string
-  }
-) {
-  // set a standard state to use later
-  let current = [...state]
-
-  if (action.type) {
-    switch (action.type) {
-      case 'remove-last-item':
-        current.pop()
-        return [...current]
-      default:
-        break
-    }
-  }
-
-  // check that an index is present
-  if (action.index === undefined) return [...state]
-
-  if (!current[action.index]) {
-    current[action.index] = { query: '', answer: '', status: '' }
-  }
-
-  current[action.index].answer = action.answer
-
-  if (action.query) {
-    current[action.index].query = action.query
-  }
-  if (action.status) {
-    current[action.index].status = action.status
-  }
-
-  return [...current]
-}
+import { stripIndent } from 'common-tags'
 
 const SQLOutputActions = ({ answer }: { answer: string }) => {
   const [showCopied, setShowCopied] = useState(false)
@@ -150,104 +93,43 @@ const SQLOutputActions = ({ answer }: { answer: string }) => {
 }
 
 const GenerateSQL = () => {
-  const [promptIndex, setPromptIndex] = useState(0)
-  const [answer, setAnswer] = useState<string | undefined>('')
-  const [isResponding, setIsResponding] = useState(false)
-  const [hasClippyError, setHasClippyError] = useState(false)
   const [selectedCategory, setSelectedCategory] = useState<string>(SAMPLE_QUERIES[0].category)
 
-  const eventSourceRef = useRef<SSE>()
-  const [promptData, dispatchPromptData] = useReducer(promptDataReducer, [])
   const { isLoading, setIsLoading, search, setSearch } = useCommandMenu()
 
-  const cantHelp = answer?.trim() === "Sorry, I don't know how to help with that."
+  const { submit, reset, messages, isResponding, hasError } = useAiChat({
+    messageTemplate: (message) =>
+      stripIndent`
+        Generate a Postgres SQL query based on the following natural language prompt.
+        - Only output valid SQL - all explanations must be SQL comments
+        - SQL comments should be short
+        - Your very last output should be "\`\`\`"
+        - For primary keys, always use "integer primary key generated always as identity"
 
-  const handleConfirm = useCallback(
-    async (query: string) => {
-      const edgeFunctionUrl = getEdgeFunctionUrl()
+        Natural language prompt:
+        ${message}
 
-      if (!edgeFunctionUrl) {
-        return console.error('No edge function url')
-      }
+        Postgres SQL query (markdown SQL only):
+      `,
+    setIsLoading,
+  })
 
-      setAnswer(undefined)
+  const handleSubmit = useCallback(
+    (message: string) => {
       setSearch('')
-      dispatchPromptData({ index: promptIndex, answer: undefined, query })
-      setIsResponding(false)
-      setHasClippyError(false)
-      setIsLoading(true)
-
-      // [Joshen] Eventually we need to pass the table data in here as well
-      const queryToSend = `
-Generate a Postgres SQL query based on the following natural language prompt. For primary keys, always use "integer primary key generated always as identity":
-${query}
-
-Postgres SQL query:
-`.trim()
-
-      const eventSource = new SSE(`${edgeFunctionUrl}/clippy-search`, {
-        headers: {
-          apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '',
-          Authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`,
-          'Content-Type': 'application/json',
-        },
-        payload: JSON.stringify({ query: queryToSend, context: promptData }),
-      })
-
-      function handleError<T>(err: T) {
-        setIsLoading(false)
-        setIsResponding(false)
-        setHasClippyError(true)
-        console.error(err)
-      }
-
-      eventSource.addEventListener('error', handleError)
-      eventSource.addEventListener('message', (e: any) => {
-        try {
-          setIsLoading(false)
-          if (e.data === '[DONE]') {
-            setIsResponding(false)
-            setAnswer(undefined)
-            setPromptIndex((x) => {
-              return x + 1
-            })
-            return
-          }
-          setIsResponding(true)
-
-          const completionResponse: CreateCompletionResponse = JSON.parse(e.data)
-          const [{ text: content }] = completionResponse.choices
-          const text = content ?? ''
-
-          setAnswer((answer) => {
-            const currentAnswer = answer ?? ''
-            dispatchPromptData({ index: promptIndex, answer: currentAnswer + text })
-            return (answer ?? '') + text
-          })
-        } catch (err) {
-          handleError(err)
-        }
-      })
-
-      eventSource.stream()
-      eventSourceRef.current = eventSource
-      setIsLoading(true)
+      submit(message)
     },
-    [promptIndex, promptData]
+    [submit]
   )
 
-  function handleResetPrompt() {
-    eventSourceRef.current?.close()
-    eventSourceRef.current = undefined
+  const handleReset = useCallback(() => {
     setSearch('')
-    setAnswer(undefined)
-    setIsResponding(false)
-    setHasClippyError(false)
-  }
+    reset()
+  }, [reset])
 
   useEffect(() => {
     if (search) {
-      handleConfirm(search)
+      handleSubmit(search)
     }
   }, [])
 
@@ -265,52 +147,56 @@ Postgres SQL query:
   return (
     <div onClick={(e) => e.stopPropagation()}>
       <div className={cn('relative mb-[62px] py-4 max-h-[720px] overflow-auto')}>
-        {promptData.map((prompt, i) => {
-          if (!prompt.query) return <></>
-
-          const formattedPromptAnwswer = (prompt?.answer ?? '')
-            .replace(/`/g, '')
-            .replace(/sql\n/g, '')
-            .trim()
-          const promptAnswer = isResponding
-            ? formattedPromptAnwswer
-            : formatAnswer(formattedPromptAnwswer)
-
-          return (
-            <>
-              {prompt.query && (
+        {messages.map((message) => {
+          switch (message.role) {
+            case MessageRole.User:
+              return (
                 <div className="flex gap-6 mx-4 [overflow-anchor:none] mb-6">
                   <div
                     className="
                       w-7 h-7 bg-scale-200 rounded-full border border-scale-400 flex items-center justify-center text-scale-1000 first-letter:
                       ring-scale-200 ring-1 shadow-sm
-                  "
+                    "
                   >
                     <IconUser strokeWidth={1.5} size={16} />
                   </div>
-                  <div className="prose text-scale-1000">{prompt.query}</div>
+                  <div className="prose text-scale-1000">{message.content}</div>
                 </div>
-              )}
+              )
+            case MessageRole.Assistant:
+              const answer = formatAnswer(
+                message.content
+                  .replace(/```sql/g, '')
+                  .replace(/```.*/gs, '')
+                  .replace(/-- End of SQL query\.*/g, '')
+                  .trim()
+              )
 
-              <div className="px-4 [overflow-anchor:none] mb-6">
-                {cantHelp ? (
-                  <p className="flex flex-col gap-4 items-center p-4">
-                    <div className="grid md:flex items-center gap-2 mt-4 text-center justify-items-center">
-                      <IconAlertCircle />
-                      <p>Sorry, I don&apos;t know how to help with that.</p>
-                    </div>
-                    <Button size="tiny" type="secondary" onClick={handleResetPrompt}>
-                      Try again?
-                    </Button>
-                  </p>
-                ) : (
+              const cantHelp = answer === "Sorry, I don't know how to help with that."
+
+              return (
+                <div className="px-4 [overflow-anchor:none] mb-6">
                   <div className="flex gap-6 [overflow-anchor:none] mb-6">
                     <div>
                       <AiIconChat />
                     </div>
                     <>
-                      {isLoading && promptIndex === i ? (
+                      {message.status === MessageStatus.Pending ? (
                         <div className="bg-scale-700 h-[21px] w-[13px] mt-1 animate-bounce"></div>
+                      ) : cantHelp ? (
+                        <div className="p-6 flex flex-col flex-grow items-center gap-6 mt-4">
+                          <IconAlertTriangle
+                            className="text-amber-900"
+                            strokeWidth={1.5}
+                            size={21}
+                          />
+                          <p className="text-lg text-scale-1200 text-center">
+                            Sorry, I don't know how to help with that.
+                          </p>
+                          <Button size="tiny" type="secondary" onClick={handleReset}>
+                            Try again?
+                          </Button>
+                        </div>
                       ) : (
                         <div className="space-y-2 flex-grow max-w-[93%]">
                           <CodeBlock
@@ -318,19 +204,18 @@ Postgres SQL query:
                             language="sql"
                             className="relative prose dark:prose-dark bg-scale-300 max-w-none"
                           >
-                            {promptAnswer}
+                            {answer}
                           </CodeBlock>
-                          {!isResponding && <SQLOutputActions answer={prompt.answer} />}
+                          {!isResponding && <SQLOutputActions answer={answer} />}
                         </div>
                       )}
                     </>
                   </div>
-                )}
-              </div>
-            </>
-          )
+                </div>
+              )
+          }
         })}
-        {promptData.length === 0 && !hasClippyError && (
+        {messages.length === 0 && !hasError && (
           <div>
             <div className="px-10">
               <h3>Example queries</h3>
@@ -363,7 +248,7 @@ Postgres SQL query:
                         type="command"
                         onSelect={() => {
                           if (!search) {
-                            handleConfirm(query)
+                            handleSubmit(query)
                           }
                         }}
                         forceMount
@@ -384,14 +269,14 @@ Postgres SQL query:
           </div>
         )}
 
-        {hasClippyError && (
+        {hasError && (
           <div className="p-6 flex flex-col items-center gap-6 mt-4">
             <IconAlertTriangle className="text-amber-900" strokeWidth={1.5} size={21} />
             <p className="text-lg text-scale-1200 text-center">
               Sorry, looks like Clippy is having a hard time!
             </p>
             <p className="text-sm text-scale-900 text-center">Please try again in a bit.</p>
-            <Button size="tiny" type="secondary" onClick={handleResetPrompt}>
+            <Button size="tiny" type="secondary" onClick={handleReset}>
               Try again?
             </Button>
           </div>
@@ -439,7 +324,7 @@ Postgres SQL query:
                 if (isLoading || isResponding) {
                   return
                 }
-                handleConfirm(search)
+                handleSubmit(search)
                 return
               default:
                 return
