421151bf5a6fb0d4736b76ad1c32f39e8a5bcef7
Inian <inian1234@gmail.com>
2023-04-11 13:16:11 +0800

    remove unnecessary config reference for edge functions

--- apps/docs/pages/reference/self-hosting-functions/[...slug].tsx
+++ apps/docs/pages/reference/self-hosting-functions/[...slug].tsx
@@ -1,5 +1,5 @@
-import functionsSpec from '~/../../spec/transforms/functions_v0_openapi_deparsed.json' assert { type: 'json' }
 import selfHostingFunctionsCommonSections from '~/../../spec/common-self-hosting-functions-sections.json'
+
 import RefSectionHandler from '~/components/reference/RefSectionHandler'
 import { flattenSections } from '~/lib/helpers'
 import handleRefGetStaticPaths from '~/lib/mdx/handleRefStaticPaths'
@@ -8,11 +8,8 @@ import { gen_v3 } from '~/lib/refGenerator/helpers'
 
 const sections = flattenSections(selfHostingFunctionsCommonSections)
 
-// @ts-ignore
-const spec = gen_v3(functionsSpec, 'wat', { apiUrl: 'apiv0' })
-
 export default function JSReference(props) {
-  return <RefSectionHandler sections={sections} spec={spec} pageProps={props} type="api" />
+  return <RefSectionHandler sections={sections} pageProps={props} type="api" />
 }
 
 export async function getStaticProps() {
