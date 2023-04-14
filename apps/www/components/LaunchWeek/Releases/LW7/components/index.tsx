bc7b4c1387faff4f29f25763b8f981147084866f
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-04-14 15:05:00 +0200

    chore: fix undefined href

--- apps/www/components/LaunchWeek/Releases/LW7/components/index.tsx
+++ apps/www/components/LaunchWeek/Releases/LW7/components/index.tsx
@@ -197,22 +197,20 @@ export const ChipLink = ({
     >
       {children}
     </span>
-  ) : (
-    !!href && (
-      <Link href={href}>
-        <a
-          target={target ?? '_self'}
-          rel="noopener"
-          className={[
-            'flex justify-between w-full min-h-[43px] sm:w-auto items-center border border-[#232323] bg-gradient-to-r text-white from-[#46444490] to-[#19191980] hover:from-[#4e4e4e90] hover:to-[#19191980] backdrop-blur-xl rounded-full text-sm py-2 pl-3 pr-2',
-            className,
-          ].join(' ')}
-        >
-          {children}
-        </a>
-      </Link>
-    )
-  )
+  ) : !!href ? (
+    <Link href={href}>
+      <a
+        target={target ?? '_self'}
+        rel="noopener"
+        className={[
+          'flex justify-between w-full min-h-[43px] sm:w-auto items-center border border-[#232323] bg-gradient-to-r text-white from-[#46444490] to-[#19191980] hover:from-[#4e4e4e90] hover:to-[#19191980] backdrop-blur-xl rounded-full text-sm py-2 pl-3 pr-2',
+          className,
+        ].join(' ')}
+      >
+        {children}
+      </a>
+    </Link>
+  ) : null
 
 export const SectionButtons = ({
   blog,
