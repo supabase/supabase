f00e594cf3ea1452d0172ef5fc847bb53505d85f
Joshen Lim <joshenlimek@gmail.com>
2023-04-20 14:03:26 +0700

    Fix messaging in table editor

--- studio/components/interfaces/TableGridEditor/SidePanelEditor/TableEditor/TableEditor.tsx
+++ studio/components/interfaces/TableGridEditor/SidePanelEditor/TableEditor/TableEditor.tsx
@@ -273,7 +273,10 @@ const TableEditor: FC<Props> = ({
                 className="!px-4 !py-3 mt-3"
                 title="You are allowing anonymous access to your table"
               >
-                <p>The table foo will be publicly writable and readable</p>
+                <p>
+                  {tableFields.name ? `The table ${tableFields.name}` : 'Your table'} will be
+                  publicly writable and readable
+                </p>
                 <p className="mt-4">
                   <Link href="https://supabase.com/docs/guides/auth/row-level-security">
                     <a target="_blank">
