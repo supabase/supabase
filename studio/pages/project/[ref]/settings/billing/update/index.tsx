c78e7f56905dd77a36580c913d2badba46fb3823
Joshen Lim <joshenlimek@gmail.com>
2023-04-26 12:12:02 +0800

    Remove back button in teams subscription page, and refactor to disable UI instead of redirecting if project update is disabled

--- studio/pages/project/[ref]/settings/billing/update/index.tsx
+++ studio/pages/project/[ref]/settings/billing/update/index.tsx
@@ -31,8 +31,8 @@ const BillingUpdate: NextPageWithLayout = () => {
   const [subscription, setSubscription] = useState<StripeSubscription>()
   const [selectedPlan, setSelectedPlan] = useState<any>()
 
-  const isEnterprise =
-    subscription && subscription.tier.supabase_prod_id === PRICING_TIER_PRODUCT_IDS.ENTERPRISE
+  const isTeams = subscription?.tier?.supabase_prod_id === PRICING_TIER_PRODUCT_IDS.TEAM
+  const isEnterprise = subscription?.tier?.supabase_prod_id === PRICING_TIER_PRODUCT_IDS.ENTERPRISE
 
   useEffect(() => {
     if (projectRef) {
@@ -44,6 +44,8 @@ const BillingUpdate: NextPageWithLayout = () => {
   useEffect(() => {
     if (isEnterprise) {
       router.push(`/project/${projectRef}/settings/billing/update/enterprise`)
+    } else if (isTeams) {
+      router.push(`/project/${projectRef}/settings/billing/update/team`)
     }
   }, [subscription])
 
