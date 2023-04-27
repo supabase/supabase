f3a3751a9771ea8f0c19e1d8e78a155f61db2bc7
TzeYiing <ty@tzeyiing.com>
2023-04-27 19:08:15 +0900

    feat: LogEventChart timestamp filling, chart query bugfix for filters

--- studio/hooks/analytics/useLogsPreview.tsx
+++ studio/hooks/analytics/useLogsPreview.tsx
@@ -15,10 +15,12 @@ import {
   LogsTableName,
   PREVIEWER_DATEPICKER_HELPERS,
 } from 'components/interfaces/Settings/Logs'
-import { Dispatch, SetStateAction, useEffect, useRef, useState } from 'react'
+import { Dispatch, SetStateAction, useEffect, useMemo, useRef, useState } from 'react'
 import { API_URL } from 'lib/constants'
 import { get } from 'lib/common/fetch'
 import dayjs from 'dayjs'
+import useFillTimeseriesSorted from './useFillTimeseriesSorted'
+import useTimeseriesUnixToIso from './useTimeseriesUnixToIso'
 
 interface Data {
   logData: LogData[]
@@ -29,7 +31,7 @@ interface Data {
   filters: Filters
   params: LogsEndpointParams
   oldestTimestamp?: string
-  eventChartData: EventChartData[] | null
+  eventChartData: EventChartData[]
 }
 interface Handlers {
   loadOlder: () => void
@@ -176,6 +178,21 @@ function useLogsPreview(
       setFilters({ ...newFilters, ...filterOverride })
     }
   }
+
+  const normalizedEventChartData = useTimeseriesUnixToIso(
+    eventChartResponse?.result || [],
+    'timestamp'
+  )
+
+  const eventChartData = useFillTimeseriesSorted(
+    normalizedEventChartData,
+    'timestamp',
+    'count',
+    0,
+    params.iso_timestamp_start,
+    params.iso_timestamp_end
+  )
+
   return [
     {
       newCount,
@@ -186,7 +203,7 @@ function useLogsPreview(
       filters,
       params,
       oldestTimestamp: oldestTimestamp ? String(oldestTimestamp) : undefined,
-      eventChartData: eventChartResponse?.result || null,
+      eventChartData,
     },
     {
       setFilters: handleSetFilters,
