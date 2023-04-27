f99fcdecef689e0d8a3678179afe8391e22fbf56
TzeYiing <ty@tzeyiing.com>
2023-04-27 19:31:43 +0900

    chore: fix failing test

--- studio/hooks/analytics/useFillTimeseriesSorted.ts
+++ studio/hooks/analytics/useFillTimeseriesSorted.ts
@@ -6,6 +6,9 @@ import { useMemo } from 'react'
  */
 const useFillTimeseriesSorted = (...args: Parameters<typeof fillTimeseries>) => {
   return useMemo(() => {
+    const [data, timestampKey] = args
+    if (!data[0]?.[timestampKey]) return data;
+
     const filled = fillTimeseries(...args)
     return filled.sort((a, b) => {
       return (new Date(a[args[1]]) as any) - (new Date(b[args[1]]) as any)
