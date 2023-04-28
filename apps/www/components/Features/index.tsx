1e8e876e6560f7cdd378b4362574d8da50ba3f9c
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-04-28 19:43:24 +0200

    chore: add images to product cards

--- apps/www/components/Features/index.tsx
+++ apps/www/components/Features/index.tsx
@@ -1,10 +1,10 @@
-import { Badge } from 'ui'
 import Solutions from 'data/Solutions.json'
 import Telemetry from '~/lib/telemetry'
 import gaEvents from '~/lib/gaEvents'
 import SectionContainer from '../Layouts/SectionContainer'
 import { useTelemetryProps } from 'common/hooks/useTelemetryProps'
 import { useRouter } from 'next/router'
+import Image from 'next/image'
 
 const Card = ({
   classname,
@@ -21,20 +21,20 @@ const Card = ({
 }) => (
   <div
     className={[
-      'col-span-1 lg:col-span-4 h-[400px] flex flex-col gap-5 lg:flex-row',
+      'col-span-1 lg:col-span-4 h-[460px] flex flex-col gap-5 lg:flex-row',
       classname,
     ].join(' ')}
     onClick={onClick}
   >
     <div
-      className={`relative overflow-hidden group/2 flex-1 flex flex-col items-center gap-5 lg:items-start justify-between bg-slate-100
-                  w-full border border-[#232323] rounded-xl h-full px-2 sm:px-6 py-14 shadow-lg`}
+      className={`relative overflow-hidden group/2 flex-1 flex flex-col items-center gap-5 lg:items-start justify-between bg-scale-100
+                  w-full border border-[#232323] rounded-xl h-full px-2 sm:px-6 py-12 shadow-lg`}
     >
-      <div className="flex flex-col items-center mx-auto max-w-xs text-center gap-2 text-white">
-        <h3 className="z-0 relative xs:text-2xl text-xl">{title}</h3>
-        <p className="text-sm sm:text-base text-slate-1000">{subtitle}</p>
-        {image}
+      <div className="relative z-10 flex flex-col items-center mx-auto max-w-xs text-center gap-2 text-white">
+        <h3 className="xs:text-2xl text-xl">{title}</h3>
+        <p className="text-sm sm:text-base text-scale-1100">{subtitle}</p>
       </div>
+      {image}
     </div>
   </div>
 )
@@ -79,33 +79,73 @@ const Features = () => {
   }
 
   return (
-    <SectionContainer className="space-y-8 max-w-7xl pb-0 pt-0 md:!pt-0">
-      <dl className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:gap-6 lg:grid-cols-6">
+    <SectionContainer className="space-y-8 max-w-7xl pb-0 pt-16 md:!pt-0">
+      <dl className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-6">
         <Card
           title={'PostgreSQL Database'}
           subtitle="Every project is a full Postgres database, the world's most trusted relational database."
-          image={null}
           onClick={() => sendTelemetryEvent(name)}
+          image={
+            <div className="absolute inset-0 z-0">
+              <Image
+                src="/images/index/database-dark.jpg"
+                layout="fill"
+                objectPosition="50% 50%"
+                objectFit="cover"
+                quality={100}
+              />
+            </div>
+          }
         />
         <Card
           title={Solutions['authentication'].name}
           subtitle={Solutions['authentication'].description}
-          image={null}
+          image={
+            <div className="absolute inset-0 z-0">
+              <Image
+                src="/images/index/auth-dark.jpg"
+                layout="fill"
+                objectPosition="50% 50%"
+                objectFit="cover"
+                quality={100}
+              />
+            </div>
+          }
           classname="lg:!col-span-2"
           onClick={() => sendTelemetryEvent(name)}
         />
         <Card
           title={Solutions['storage'].name}
           subtitle={Solutions['storage'].description}
-          image={null}
+          image={
+            <div className="absolute inset-0 z-0">
+              <Image
+                src="/images/index/storage-dark.jpg"
+                layout="fill"
+                objectPosition="50% 50%"
+                objectFit="cover"
+                quality={100}
+              />
+            </div>
+          }
           classname="lg:!col-span-2"
           onClick={() => sendTelemetryEvent(name)}
         />
         <Card
           title={Solutions['edge-functions'].name}
           subtitle={Solutions['edge-functions'].description}
-          image={null}
           onClick={() => sendTelemetryEvent(name)}
+          image={
+            <div className="absolute inset-0 z-0">
+              <Image
+                src="/images/index/edge-dark.svg"
+                layout="fill"
+                objectPosition="50% 50%"
+                objectFit="cover"
+                quality={100}
+              />
+            </div>
+          }
         />
       </dl>
     </SectionContainer>
