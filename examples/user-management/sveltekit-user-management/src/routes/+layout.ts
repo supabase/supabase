0a6a5d06d4c875949925037d4878c5e454d7a0af
Andrew Smith <a.smith@silentworks.co.uk>
2023-04-29 15:23:52 +0000

    Update sveltekit example to use 0.9 auth helpers

--- examples/user-management/sveltekit-user-management/src/routes/+layout.ts
+++ examples/user-management/sveltekit-user-management/src/routes/+layout.ts
@@ -1,7 +1,21 @@
-import type { LayoutLoad } from './$types';
-import { getSupabase } from '@supabase/auth-helpers-sveltekit';
+// src/routes/+layout.ts
+import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public'
+import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit'
+import type { Database } from '../schema'
 
-export const load: LayoutLoad = async (event) => {
-	const { session } = await getSupabase(event);
-	return { session };
-};
+export const load = async ({ fetch, data, depends }) => {
+	depends('supabase:auth')
+
+	const supabase = createSupabaseLoadClient<Database>({
+		supabaseUrl: PUBLIC_SUPABASE_URL,
+		supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
+		event: { fetch },
+		serverSession: data.session
+	})
+
+	const {
+		data: { session }
+	} = await supabase.auth.getSession()
+
+	return { supabase, session }
+}
