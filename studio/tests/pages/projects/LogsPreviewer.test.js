009311190afbc7800a38f26dfc40fb601f39673e
TzeYiing <ty@tzeyiing.com>
2023-05-02 02:53:25 +0900

    fix: refactor auth logs renderers to match other log pages

--- studio/tests/pages/projects/LogsPreviewer.test.js
+++ studio/tests/pages/projects/LogsPreviewer.test.js
@@ -87,25 +87,27 @@ test.each([
     queryType: 'auth',
     tableName: undefined,
     tableLog: logDataFixture({
-      event_message: JSON.stringify({
-        msg: 'some message',
+      event_message: "some event_message",
+      metadata: {
+        msg: 'some metadata_msg',
         path: '/auth-path',
         level: 'info',
         status: 300,
-      }),
+      }
     }),
     selectionLog: logDataFixture({
-      event_message: JSON.stringify({
-        msg: 'some message',
+      event_message: "some event_message",
+      metadata: {
+        msg: 'some metadata_msg',
         path: '/auth-path',
         level: 'info',
         status: 300,
-      }),
+      }
     }),
-    tableTexts: [/auth\-path/, /some message/, /INFO/],
+    tableTexts: [/auth\-path/, /some event_message/, /INFO/],
     selectionTexts: [
       /auth\-path/,
-      /some message/,
+      /some metadata_msg/,
       /INFO/,
       /300/,
       /Timestamp/,
