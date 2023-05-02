ccd155dd0345fbcb971d6b876ad89fa6b93ae08e
TzeYiing <ty@tzeyiing.com>
2023-05-02 19:30:39 +0900

    chore: add tests for all , fixed some warnings

--- studio/tests/pages/projects/reports/api-report.test.js
+++ studio/tests/pages/projects/reports/api-report.test.js
@@ -7,7 +7,7 @@ import userEvent from '@testing-library/user-event'
 beforeEach(() => {
   // reset mocks between tests
   get.mockReset()
-  get.mockImplementation(async (_url) => [{ data: [] }])
+  get.mockImplementation(async (_url) => [{ result: [] }])
 })
 
 test(`static elements`, async () => {
@@ -27,15 +27,85 @@ test('refresh button', async () => {
   await waitFor(() => expect(get).toBeCalled())
 })
 
-test('expandable tables', async () => {
-  get.mockImplementation(async (_url) => [
-    { data: [{ timestamp: new Date().toISOString(), count: 123 }] },
-  ])
-  const expandables = await screen.findByText('View API requests')
-  const first = expandables[0]
-  get.mockImplementationOnce(async (_url) => [{ data: [{ path: '/my-path', count: 22 }] }])
-  userEvent.click(first)
-  await screen.findByText('Hide API Requests')
-  await screen.findByText(/\/my\-path/)
-  await screen.findByText(/22/)
+test('append - api request routes', async () => {
+  get.mockImplementation(async (url) => {
+    if (decodeURIComponent(url).includes("request.path")) {
+      return { result: [{ path: 'mypath', method: 'GET', status_code: 200, search: 'some-query', count: 22 }] }
+    }
+    return { result: [{ timestamp: new Date().toISOString(), count: 123 }] }
+  })
+  render(<ApiReport />)
+  await waitFor(() => expect(get).toBeCalled())
+  await screen.findAllByText(/mypath/)
+  await screen.findAllByText(/GET/)
+  await screen.findAllByText(/200/)
+  await screen.findAllByText(/some\-query/)
+  await screen.findAllByText(/22/)
 })
+
+
+test('append - error routes', async () => {
+  get.mockImplementation(async (url) => {
+    const uri = decodeURIComponent(url)
+    if (uri.includes("400")) {
+      return { result: [{ path: 'mypath', method: 'GET', status_code: 200, search: 'some-query', count: 22 }] }
+    }
+    return { result: [{ timestamp: new Date().toISOString(), count: 123 }] }
+  })
+  render(<ApiReport />)
+  await waitFor(() => expect(get).toBeCalled())
+  await screen.findAllByText(/mypath/)
+  await screen.findAllByText(/GET/)
+  await screen.findAllByText(/200/)
+  await screen.findAllByText(/some\-query/)
+  await screen.findAllByText(/22/)
+})
+
+
+test('append - error routes', async () => {
+  get.mockImplementation(async (url) => {
+    const uri = decodeURIComponent(url)
+    if (uri.includes("avg") && uri.includes("request.path")) {
+      return { result: [{ path: 'mypath', method: 'GET', status_code: 200, avg: 534, search: 'some-query', count: 22 }] }
+    }
+    return { result: [{ timestamp: new Date().toISOString(), count: 123 }] }
+  })
+  render(<ApiReport />)
+  await waitFor(() => expect(get).toBeCalled())
+  await screen.findAllByText(/mypath/)
+  await screen.findAllByText(/GET/)
+  await screen.findAllByText(/200/)
+  await screen.findAllByText(/some\-query/)
+  await screen.findAllByText(/22/)
+  await screen.findAllByText(/534\.00ms/)
+})
+
+
+
+// test('expandable error routes', async () => {
+//   get.mockImplementation(async (url) => {
+//     if (url.includes('path')) {
+//       return [{ data: [{ path: '/my-path', method: 'GET', search: '?=123', count: 22 }] }]
+//     }
+//     return [{ data: [{ timestamp: new Date().toISOString(), count: 123 }] }]
+//   })
+
+//   render(<ApiReport />)
+//   await screen.findByText(/\/my\-path/)
+//   await screen.findByText(/\?\=123/)
+//   await screen.findByText(/22/)
+// })
+
+// test('expandable high latency routes', async () => {
+//   get.mockImplementation(async (url) => {
+//     if (url.includes('path')) {
+//       return [{ data: [{ path: '/my-path', method: 'GET', search: '?=123', avg_ms: 55, count: 22 }] }]
+//     }
+//     return [{ data: [{ timestamp: new Date().toISOString(), avg_ms: 123  }] }]
+//   })
+//   render(<ApiReport />)
+//   await screen.findByText(/\/my\-path/)
+//   await screen.findByText(/\?\=123/)
+//   await screen.findByText(/22/)
+//   await screen.findByText(/55/)
+// })
