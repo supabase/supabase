048bf9b24b7717e9a22de79a4b980be10b81629f
Greg Richardson <greg.nmr@gmail.com>
2023-05-02 12:46:01 -0600

    chore(prettier): blog post print width

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
+  endOfLine: 'lf',
+  sqlKeywordCase: 'lower',
+}
---
+++ apps/www/_blog/.prettierrc.js
@@ -0,0 +1,6 @@
+const base = require('../../../.prettierrc.js')
+
+module.exports = {
+  ...base,
+  printWidth: 80,
+}
