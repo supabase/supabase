a10ed58999937b5414697fc41e0ff8bddc79794b
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-05-03 10:04:52 +0200

    chore: update backed by alignment

--- apps/www/components/BackedBy.tsx
+++ apps/www/components/BackedBy.tsx
@@ -13,7 +13,7 @@ const BackedBy = ({
   return (
     <div
       className={[
-        'flex text-center',
+        'flex text-center gap-4 md:gap-2',
         layout === 'horizontal'
           ? 'flex-col lg:flex-row items-center justify-center'
           : 'flex-col items-center',
@@ -21,25 +21,25 @@ const BackedBy = ({
       ].join(' ')}
     >
       <small className="small text-xs">backed by</small>
-      <div className="w-full sm:max-w-lg mt-4 md:mt-3 lg:ml-0">
-        <div className="flex flex-wrap items-center justify-center gap-y-8 sm:flex-nowrap">
+      <div className="w-full sm:max-w-lg lg:ml-0">
+        <div className="flex flex-wrap items-center justify-center gap-4 sm:gap-8 sm:flex-nowrap">
           <img
-            className="h-8 w-auto pr-5 sm:h-8 md:pr-10"
+            className="h-6 w-auto sm:h-8"
             src={`${basePath}/images/logos/yc--grey.png`}
             alt="Y Combinator"
           />
           <img
-            className="relative w-auto h-5 pr-5 sm:h-5 md:pr-10"
+            className="relative w-auto h-4 sm:h-5"
             src={`${basePath}/images/logos/mozilla--grey.png`}
             alt="Mozilla"
           />
           <img
-            className="relative w-auto h-5 pr-5 sm:h-5 md:pr-10"
+            className="relative w-auto h-4 sm:h-5"
             src={`${basePath}/images/logos/coatue.png`}
             alt="Coatue"
           />
           <img
-            className="relative w-auto h-6 sm:h-6"
+            className="relative w-auto h-5 sm:h-6"
             src={`${basePath}/images/logos/felicis.png`}
             alt="Felicis"
           />
