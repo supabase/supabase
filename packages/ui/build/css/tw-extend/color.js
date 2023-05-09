b88474e9b891391102f4b1e547ceca4f93912b94
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2023-05-09 23:35:58 +0800

    added new colours. moved themes into new directory

--- packages/ui/build/css/tw-extend/color.js
+++ packages/ui/build/css/tw-extend/color.js
@@ -19,9 +19,13 @@ module.exports = {
   cssVariable: "var(--theme-variables-warning)",
   value: "hsl(26.899999999999977, 93.8%, 56.1%)"
 },
+"theme-variables-danger": {
+  cssVariable: "var(--theme-variables-danger)",
+  value: "hsl(0, 100%, 64.7%)"
+},
 "theme-variables-success": {
   cssVariable: "var(--theme-variables-success)",
-  value: "hsl(140.3, 100%, 69.2%)"
+  value: "hsl(123.80000000000001, 100%, 66.3%)"
 },
 "theme-variables-text-body": {
   cssVariable: "var(--theme-variables-text-body)",
@@ -49,19 +53,19 @@ module.exports = {
 },
 "backgroundColor-success-100": {
   cssVariable: "var(--background-color-success-100)",
-  value: "hsl(139 38.9% 50.1%)"
+  value: "hsl(121 43% 48.6%)"
 },
 "backgroundColor-success-200": {
   cssVariable: "var(--background-color-success-200)",
-  value: "hsl(137 33.6% 37.7%)"
+  value: "hsl(119 37.3% 36.8%)"
 },
 "backgroundColor-success-300": {
   cssVariable: "var(--background-color-success-300)",
-  value: "hsl(133 17.8% 17%)"
+  value: "hsl(114 21.2% 16.6%)"
 },
 "backgroundColor-success-DEFAULT": {
   cssVariable: "var(--background-color-success-default)",
-  value: "hsl(140.3, 100%, 69.2%)"
+  value: "hsl(123.80000000000001, 100%, 66.3%)"
 },
 "backgroundColor-warning-100": {
   cssVariable: "var(--background-color-warning-100)",
@@ -79,6 +83,22 @@ module.exports = {
   cssVariable: "var(--background-color-warning-default)",
   value: "hsl(26.899999999999977, 93.8%, 56.1%)"
 },
+"backgroundColor-danger-100": {
+  cssVariable: "var(--background-color-danger-100)",
+  value: "hsl(2.07 50.9% 46.5%)"
+},
+"backgroundColor-danger-200": {
+  cssVariable: "var(--background-color-danger-200)",
+  value: "hsl(3.65 46.8% 35%)"
+},
+"backgroundColor-danger-300": {
+  cssVariable: "var(--background-color-danger-300)",
+  value: "hsl(6.9 29% 16.2%)"
+},
+"backgroundColor-danger-DEFAULT": {
+  cssVariable: "var(--background-color-danger-default)",
+  value: "hsl(0, 100%, 64.7%)"
+},
 "borderColor-DEFAULT": {
   cssVariable: "var(--border-color-default)",
   value: "hsl(91.8 0% 17.3%)"
@@ -86,6 +106,14 @@ module.exports = {
 "borderColor-body": {
   cssVariable: "var(--border-color-body)",
   value: "hsl(91.8 0% 14.6%)"
+},
+"borderColor-body-alt": {
+  cssVariable: "var(--border-color-body-alt)",
+  value: "hsl(91.8 0% 12.2%)"
+},
+"borderColor-selection": {
+  cssVariable: "var(--border-color-selection)",
+  value: "hsl(91.8 0% 53.5%)"
 },
 "borderColor-control": {
   cssVariable: "var(--border-color-control)",
@@ -106,6 +134,26 @@ module.exports = {
 "textColor-body-lightest": {
   cssVariable: "var(--text-color-body-lightest)",
   value: "hsl(91.8 0% 30.2%)"
+},
+"textColor-success-DEFAULT": {
+  cssVariable: "var(--text-color-success-default)",
+  value: "hsl(123.80000000000001, 100%, 66.3%)"
+},
+"textColor-link-DEFAULT": {
+  cssVariable: "var(--text-color-link-default)",
+  value: "hsl(153.10000000000002, 60.2%, 52.7%)"
+},
+"textColor-warning-DEFAULT": {
+  cssVariable: "var(--text-color-warning-default)",
+  value: "hsl(26.899999999999977, 93.8%, 56.1%)"
+},
+"textColor-danger-DEFAULT": {
+  cssVariable: "var(--text-color-danger-default)",
+  value: "hsl(0, 100%, 64.7%)"
+},
+"textColor-disabled-DEFAULT": {
+  cssVariable: "var(--text-color-disabled-default)",
+  value: "hsl(0 0% 46.5%)"
 },
 "utility-mix-positive": {
   cssVariable: "var(--utility-mix-positive)",
