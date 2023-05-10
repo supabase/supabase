e14b6fb427b9651ac1d62b86f1a8f0c7a9a72d78
Andrew Smith <a.smith@silentworks.co.uk>
2023-05-10 10:23:13 +0000

    Update filename and improved the opening sentence

--- apps/docs/components/Navigation/NavigationMenu/NavigationMenu.constants.ts
+++ apps/docs/components/Navigation/NavigationMenu/NavigationMenu.constants.ts
@@ -707,8 +707,8 @@ export const supabase_cli = {
     { name: 'Overview', url: '/guides/cli' },
     { name: 'Managing Environments', url: '/guides/cli/managing-environments' },
     {
-      name: 'Using environment variables in your config',
-      url: '/guides/cli/using-envitonment-variables-in-your-config',
+      name: 'Using environment variables in config.toml',
+      url: '/guides/cli/using-envitonment-variables-in-config',
     },
     {
       name: 'Reference',
