861a1087e6b57a31c0a6590830e30c6c97ad1d7d
Jan Tennert <jan.m.tennert@gmail.com>
2023-05-10 17:35:07 +0200

    Fix navigation, fix sign up examples having the same id

--- apps/docs/pages/reference/kotlin/crawlers/[...slug].tsx
+++ apps/docs/pages/reference/kotlin/crawlers/[...slug].tsx
@@ -1,6 +1,5 @@
 import clientLibsCommonSections from '~/../../spec/common-client-libs-sections.json'
 import typeSpec from '~/../../spec/enrichments/tsdoc_v2/combined.json'
-// @ts-expect-error
 import spec from '~/../../spec/supabase_kt_v0.yml' assert { type: 'yml' }
 import RefSectionHandler from '~/components/reference/RefSectionHandler'
 import { flattenSections } from '~/lib/helpers'
@@ -10,35 +9,36 @@ import { useRouter } from 'next/router'
 import RefSEO from '~/components/reference/RefSEO'
 
 const sections = flattenSections(clientLibsCommonSections)
+const libraryPath = '/kotlin'
 
-export default function JSReference(props) {
-  const router = useRouter()
-  const slug = router.query.slug[0]
-  const filteredSection = sections.filter((section) => section.id === slug)
+export default function KotlinReference(props) {
+    const router = useRouter()
+    const slug = router.query.slug[0]
+    const filteredSection = sections.filter((section) => section.id === slug)
 
-  const pageTitle = filteredSection[0]?.title
-    ? `${filteredSection[0]?.title} | Supabase`
-    : 'Supabase'
+    const pageTitle = filteredSection[0]?.title
+        ? `${filteredSection[0]?.title} | Supabase`
+        : 'Supabase'
 
-  return (
-    <>
-      <RefSEO title={pageTitle} />
+    return (
+        <>
+            <RefSEO title={pageTitle} />
 
-      <RefSectionHandler
-        sections={filteredSection}
-        spec={spec}
-        typeSpec={typeSpec}
-        pageProps={props}
-        type="client-lib"
-      />
-    </>
-  )
+            <RefSectionHandler
+                sections={filteredSection}
+                spec={spec}
+                typeSpec={typeSpec}
+                pageProps={props}
+                type="client-lib"
+            />
+        </>
+    )
 }
 
 export async function getStaticProps() {
-  return handleRefStaticProps(sections, '/kotlin')
+    return handleRefStaticProps(sections, libraryPath)
 }
 
-export function getStaticPaths() {
-  return handleRefGetStaticPaths()
+export async function getStaticPaths() {
+    return handleRefGetStaticPaths(sections, libraryPath)
 }
