c0d0c7452ff7a1afb8de7b9942f6aa2de85b870f
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-05-12 18:05:43 +0200

    chore: update import

--- apps/docs/layouts/guides/index.tsx
+++ apps/docs/layouts/guides/index.tsx
@@ -8,9 +8,9 @@ import { IconExternalLink } from 'ui'
 import components from '~/components'
 import { highlightSelectedTocItem } from '~/components/CustomHTMLElements/CustomHTMLElements.utils'
 import { FooterHelpCalloutType } from '~/components/FooterHelpCallout'
+import GuidesTableOfContents from '~/components/GuidesTableOfContents'
 import useHash from '~/hooks/useHash'
 import { LayoutMainContent } from '../DefaultLayout'
-import GuidesTableOfContents from '../../components/GuidesTableOfContents'
 import ExpandableVideo from 'ui/src/components/ExpandableVideo/ExpandableVideo'
 
 interface Props {
