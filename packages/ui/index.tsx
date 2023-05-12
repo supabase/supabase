fa0b972308333130137964b27831caa165ca8895
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-05-12 11:10:26 +0200

    Update packages/ui/index.tsx
    Co-authored-by: Greg Richardson <greg@supabase.io>

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -16,7 +16,6 @@ export * from './src/components/Icon/IconContext'
 export * from './src/components/GlassPanel'
 export * from './src/components/IconPanel'
 export * from './src/components/Icon/IconBackground'
-// export * from './src/components/Image'
 
 // DISPLAYS
 
