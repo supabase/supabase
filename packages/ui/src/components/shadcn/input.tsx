9b4047218f7f26a2c671b436704e61e79a137554
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2023-05-22 02:26:16 +0800

    adding calendar and form example

--- packages/ui/src/components/shadcn/input.tsx
+++ packages/ui/src/components/shadcn/input.tsx
@@ -10,13 +10,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          `flex h-10 w-full rounded-md 
-          border border-scale-700 bg-transparent px-3 py-2 
-          text-sm text-scale-1200 ring-offset-background 
-          file:border-0 file:bg-transparent file:text-sm file:font-medium 
-          placeholder:text-scale-700
-          focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring 
-          focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50`,
+          'flex h-10 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
           className
         )}
         ref={ref}
