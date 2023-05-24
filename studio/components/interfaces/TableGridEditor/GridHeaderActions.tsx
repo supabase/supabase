8eb0cc76e8b8a2b844d433eba373b08e98d3c20f
Arjun3492 <arjun.kohli.330@gmail.com>
2023-05-24 22:40:32 +0530

    fixed issue 14130 - Reduce RLS warning clutter https://github.com/supabase/supabase/issues/14130

--- studio/components/interfaces/TableGridEditor/GridHeaderActions.tsx
+++ studio/components/interfaces/TableGridEditor/GridHeaderActions.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react'
 import Link from 'next/link'
-import { Button, IconAlertCircle, IconCode, IconLock } from 'ui'
+import { Button, IconCode, IconLock } from 'ui'
 import * as Tooltip from '@radix-ui/react-tooltip'
 import type { PostgresPolicy, PostgresTable } from '@supabase/postgres-meta'
 
@@ -73,26 +73,17 @@ const GridHeaderActions: FC<Props> = ({
         </Tooltip.Root>
       )}
 
-      <Link href={`/project/${projectRef}/auth/policies?search=${table.id}`}>
+      {table.rls_enabled && <Link href={`/project/${projectRef}/auth/policies?search=${table.id}`}>
         <a>
           <Button
-            type={table.rls_enabled ? 'link' : 'warning'}
-            icon={
-              table.rls_enabled ? (
-                <IconLock strokeWidth={2} size={14} />
-              ) : (
-                <IconAlertCircle strokeWidth={2} size={14} />
-              )
-            }
+            type="link"
+            icon={<IconLock strokeWidth={2} size={14} />}
           >
-            {!table.rls_enabled
-              ? 'RLS is not enabled'
-              : `${policies.length == 0 ? 'No' : policies.length} active RLS polic${
-                  policies.length > 1 || policies.length == 0 ? 'ies' : 'y'
-                }`}
+            {`${policies.length == 0 ? 'No' : policies.length} active RLS polic${policies.length > 1 || policies.length == 0 ? 'ies' : 'y'
+              }`}
           </Button>
         </a>
-      </Link>
+      </Link>}
       <div className="mt-[1px]">
         <RenderAPIPreviewToggle />
       </div>
