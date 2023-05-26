e1ec420b4ce5d2cecafd5b6c19e0dbc256b25b7a
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-05-26 14:27:15 +0200

    add sections to vector page

--- apps/www/components/Sections/FeaturesSection.tsx
+++ apps/www/components/Sections/FeaturesSection.tsx
@@ -1,23 +1,29 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { ReactNode } from 'react'
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
 import { Button } from 'ui'
 import SectionContainer from '~/components/Layouts/SectionContainer'
 import { pageData } from '~/data/products/vector/page'
 
-const FeaturesSection = () => {
+interface Props {
+  title: string | ReactNode
+  paragraph: string
+  features: {
+    icon: string
+    title: string
+    text: string
+  }[]
+}
+
+const FeaturesSection = ({ title, paragraph, features }: Props) => {
   return (
     <SectionContainer>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 xl:gap-10 justify-between">
         <div className="col-span-full lg:col-span-4 gap-2 flex flex-col">
           <h2 className="text-2xl sm:text-3xl xl:text-4xl max-w-[280px] sm:max-w-xs xl:max-w-[360px] tracking-[-1px]">
-            <span className="heading-gradient">Powerful Features</span>
-            <br />
-            <span className="heading-gradient-brand">Seamless Development</span>
+            {title}
           </h2>
-          <p className="p">
-            We offer an all-in-one platform to empower your entire AI project from start to finish.
-          </p>
+          <p className="p">{paragraph}</p>
           <Link href="">
             <a className="">
               <Button type="default" size="small">
@@ -27,36 +33,34 @@ const FeaturesSection = () => {
           </Link>
         </div>
         <div className="col-span-full lg:col-start-6 lg:col-span-7 space-y-10 lg:space-y-0 flex flex-col lg:grid lg:grid-cols-2 lg:gap-16">
-          {pageData.features.map(
-            (feature: { icon: string; title: string; text: string }, i: number) => {
-              return (
-                <div className="h-full flex items-start space-x-3 w-full" key={i}>
-                  <div className="flex items-center">
-                    <div className="relative w-full h-6 flex items-center mx-auto">
-                      <svg
-                        width="17"
-                        height="17"
-                        viewBox="0 0 17 17"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d={feature.icon}
-                          stroke="white"
-                          strokeMiterlimit="10"
-                          strokeLinejoin="bevel"
-                        />
-                      </svg>
-                    </div>
-                  </div>
-                  <div className="text-sm lg:text-base">
-                    <h2 className="text-base">{feature.title}</h2>
-                    <ReactMarkdown className="prose pt-1 text-sm">{feature.text}</ReactMarkdown>
+          {features.map((feature: { icon: string; title: string; text: string }, i: number) => {
+            return (
+              <div className="h-full flex items-start space-x-3 w-full" key={i}>
+                <div className="flex items-center">
+                  <div className="relative w-full h-6 flex items-center mx-auto">
+                    <svg
+                      width="17"
+                      height="17"
+                      viewBox="0 0 17 17"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d={feature.icon}
+                        stroke="white"
+                        strokeMiterlimit="10"
+                        strokeLinejoin="bevel"
+                      />
+                    </svg>
                   </div>
                 </div>
-              )
-            }
-          )}
+                <div className="text-sm lg:text-base">
+                  <h2 className="text-base">{feature.title}</h2>
+                  <ReactMarkdown className="prose pt-1 text-sm">{feature.text}</ReactMarkdown>
+                </div>
+              </div>
+            )
+          })}
         </div>
       </div>
     </SectionContainer>
--- apps/www/data/products/vector/page.tsx
+++ apps/www/data/products/vector/page.tsx
@@ -1,3 +1,5 @@
+import IntegrationsSVG from '../../../components/products/vector/IntegrationsSVG'
+
 export const pageData = {
   metaTitle: '',
   metaDescription: '',
@@ -28,36 +30,92 @@ export const pageData = {
       image: 'asdfadsf',
     },
   ],
-  features: [
-    {
-      icon: 'M2.22628 8.76631C2.07453 6.92839 2.70175 5.03824 4.10791 3.63208C6.64632 1.09367 10.7619 1.09367 13.3003 3.63208C15.8387 6.17049 15.8387 10.2861 13.3003 12.8245C10.7619 15.3629 6.64632 15.3629 4.10791 12.8245C3.50852 12.2251 3.05066 11.5377 2.73434 10.804M0.516543 7.55176L2.23116 9.26638L3.94578 7.55176M5.95088 4.97787L11.3675 4.97787L11.3675 7.1379L5.95088 7.1379L5.95088 4.97787ZM6.2096 7.13777L11.0825 7.13777L11.0825 9.2978L6.2096 9.2978L6.2096 7.13777ZM5.95114 9.29726L11.3678 9.29726L11.3678 11.4573H5.95114V9.29726Z',
-      title: 'Fully managed or Self-Hosted',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
+  integrations: {
+    title: 'Leverage the tools you love',
+    image: <IntegrationsSVG />,
+  },
+  useCasesSection: {
+    title: 'What you can build with Supabase Vector',
+    paragraph: 'Manage your vector embeddings to power any AI project from start to finish.',
+    useCases: [
+      {
+        img: 'toast-messages.svg',
+        title: 'Semantic Search',
+        description: 'Search your own knowledge base by semantic similarity.',
+      },
+      {
+        img: 'live-avatars.svg',
+        title: 'Chatbots',
+        description: 'Enhance chatbot memory with content-based long-term retention.',
+      },
+      {
+        img: 'live-cursors.svg',
+        title: 'Hybrid search',
+        description: 'Combine semantic and full-text search with powerful SQL filtering.',
+      },
+      {
+        img: 'leaderboard.svg',
+        title: 'Image Similarity',
+        description:
+          'Transform images into image vector representations to detect similarity patterns.',
+      },
+      {
+        img: 'stock-market.svg',
+        title: 'Data Management',
+        description: 'Automatically tag, deduplicate or detect patterns in your vector store.',
+      },
+      {
+        img: 'in-app-chat.svg',
+        title: 'Recommendations',
+        description: 'Discover related content: articles, videos, restaurants, and more.',
+      },
+    ],
+  },
+  featuresSection: {
+    title: (
+      <>
+        <span className="heading-gradient">Powerful Features</span>
+        <br />
+        <span className="heading-gradient-brand">Seamless Development</span>
+      </>
+    ),
+    paragraph:
+      'Develop, integrate and deploy secure and scalable AI applications at unprecedented speed.',
+    cta: {
+      label: '',
+      url: '',
     },
-    {
-      icon: 'M2.22628 8.76631C2.07453 6.92839 2.70175 5.03824 4.10791 3.63208C6.64632 1.09367 10.7619 1.09367 13.3003 3.63208C15.8387 6.17049 15.8387 10.2861 13.3003 12.8245C10.7619 15.3629 6.64632 15.3629 4.10791 12.8245C3.50852 12.2251 3.05066 11.5377 2.73434 10.804M0.516543 7.55176L2.23116 9.26638L3.94578 7.55176M5.95088 4.97787L11.3675 4.97787L11.3675 7.1379L5.95088 7.1379L5.95088 4.97787ZM6.2096 7.13777L11.0825 7.13777L11.0825 9.2978L6.2096 9.2978L6.2096 7.13777ZM5.95114 9.29726L11.3678 9.29726L11.3678 11.4573H5.95114V9.29726Z',
-      title: 'Multi region',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
-    },
-    {
-      icon: 'M2.22628 8.76631C2.07453 6.92839 2.70175 5.03824 4.10791 3.63208C6.64632 1.09367 10.7619 1.09367 13.3003 3.63208C15.8387 6.17049 15.8387 10.2861 13.3003 12.8245C10.7619 15.3629 6.64632 15.3629 4.10791 12.8245C3.50852 12.2251 3.05066 11.5377 2.73434 10.804M0.516543 7.55176L2.23116 9.26638L3.94578 7.55176M5.95088 4.97787L11.3675 4.97787L11.3675 7.1379L5.95088 7.1379L5.95088 4.97787ZM6.2096 7.13777L11.0825 7.13777L11.0825 9.2978L6.2096 9.2978L6.2096 7.13777ZM5.95114 9.29726L11.3678 9.29726L11.3678 11.4573H5.95114V9.29726Z',
-      title: 'Health Benefits',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
-    },
-    {
-      icon: 'M2.22628 8.76631C2.07453 6.92839 2.70175 5.03824 4.10791 3.63208C6.64632 1.09367 10.7619 1.09367 13.3003 3.63208C15.8387 6.17049 15.8387 10.2861 13.3003 12.8245C10.7619 15.3629 6.64632 15.3629 4.10791 12.8245C3.50852 12.2251 3.05066 11.5377 2.73434 10.804M0.516543 7.55176L2.23116 9.26638L3.94578 7.55176M5.95088 4.97787L11.3675 4.97787L11.3675 7.1379L5.95088 7.1379L5.95088 4.97787ZM6.2096 7.13777L11.0825 7.13777L11.0825 9.2978L6.2096 9.2978L6.2096 7.13777ZM5.95114 9.29726L11.3678 9.29726L11.3678 11.4573H5.95114V9.29726Z',
-      title: 'No vendor lock-in',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
-    },
-    {
-      icon: 'M2.22628 8.76631C2.07453 6.92839 2.70175 5.03824 4.10791 3.63208C6.64632 1.09367 10.7619 1.09367 13.3003 3.63208C15.8387 6.17049 15.8387 10.2861 13.3003 12.8245C10.7619 15.3629 6.64632 15.3629 4.10791 12.8245C3.50852 12.2251 3.05066 11.5377 2.73434 10.804M0.516543 7.55176L2.23116 9.26638L3.94578 7.55176M5.95088 4.97787L11.3675 4.97787L11.3675 7.1379L5.95088 7.1379L5.95088 4.97787ZM6.2096 7.13777L11.0825 7.13777L11.0825 9.2978L6.2096 9.2978L6.2096 7.13777ZM5.95114 9.29726L11.3678 9.29726L11.3678 11.4573H5.95114V9.29726Z',
-      title: 'Automatic Backups',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
-    },
-    {
-      icon: 'M2.22628 8.76631C2.07453 6.92839 2.70175 5.03824 4.10791 3.63208C6.64632 1.09367 10.7619 1.09367 13.3003 3.63208C15.8387 6.17049 15.8387 10.2861 13.3003 12.8245C10.7619 15.3629 6.64632 15.3629 4.10791 12.8245C3.50852 12.2251 3.05066 11.5377 2.73434 10.804M0.516543 7.55176L2.23116 9.26638L3.94578 7.55176M5.95088 4.97787L11.3675 4.97787L11.3675 7.1379L5.95088 7.1379L5.95088 4.97787ZM6.2096 7.13777L11.0825 7.13777L11.0825 9.2978L6.2096 9.2978L6.2096 7.13777ZM5.95114 9.29726L11.3678 9.29726L11.3678 11.4573H5.95114V9.29726Z',
-      title: 'Highly Scalable',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
-    },
-  ],
+    features: [
+      {
+        icon: 'M2.22628 8.76631C2.07453 6.92839 2.70175 5.03824 4.10791 3.63208C6.64632 1.09367 10.7619 1.09367 13.3003 3.63208C15.8387 6.17049 15.8387 10.2861 13.3003 12.8245C10.7619 15.3629 6.64632 15.3629 4.10791 12.8245C3.50852 12.2251 3.05066 11.5377 2.73434 10.804M0.516543 7.55176L2.23116 9.26638L3.94578 7.55176M5.95088 4.97787L11.3675 4.97787L11.3675 7.1379L5.95088 7.1379L5.95088 4.97787ZM6.2096 7.13777L11.0825 7.13777L11.0825 9.2978L6.2096 9.2978L6.2096 7.13777ZM5.95114 9.29726L11.3678 9.29726L11.3678 11.4573H5.95114V9.29726Z',
+        title: 'Fully managed or Self-Hosted',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
+      },
+      {
+        icon: 'M2.22628 8.76631C2.07453 6.92839 2.70175 5.03824 4.10791 3.63208C6.64632 1.09367 10.7619 1.09367 13.3003 3.63208C15.8387 6.17049 15.8387 10.2861 13.3003 12.8245C10.7619 15.3629 6.64632 15.3629 4.10791 12.8245C3.50852 12.2251 3.05066 11.5377 2.73434 10.804M0.516543 7.55176L2.23116 9.26638L3.94578 7.55176M5.95088 4.97787L11.3675 4.97787L11.3675 7.1379L5.95088 7.1379L5.95088 4.97787ZM6.2096 7.13777L11.0825 7.13777L11.0825 9.2978L6.2096 9.2978L6.2096 7.13777ZM5.95114 9.29726L11.3678 9.29726L11.3678 11.4573H5.95114V9.29726Z',
+        title: 'Multi region',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
+      },
+      {
+        icon: 'M2.22628 8.76631C2.07453 6.92839 2.70175 5.03824 4.10791 3.63208C6.64632 1.09367 10.7619 1.09367 13.3003 3.63208C15.8387 6.17049 15.8387 10.2861 13.3003 12.8245C10.7619 15.3629 6.64632 15.3629 4.10791 12.8245C3.50852 12.2251 3.05066 11.5377 2.73434 10.804M0.516543 7.55176L2.23116 9.26638L3.94578 7.55176M5.95088 4.97787L11.3675 4.97787L11.3675 7.1379L5.95088 7.1379L5.95088 4.97787ZM6.2096 7.13777L11.0825 7.13777L11.0825 9.2978L6.2096 9.2978L6.2096 7.13777ZM5.95114 9.29726L11.3678 9.29726L11.3678 11.4573H5.95114V9.29726Z',
+        title: 'Health Benefits',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
+      },
+      {
+        icon: 'M2.22628 8.76631C2.07453 6.92839 2.70175 5.03824 4.10791 3.63208C6.64632 1.09367 10.7619 1.09367 13.3003 3.63208C15.8387 6.17049 15.8387 10.2861 13.3003 12.8245C10.7619 15.3629 6.64632 15.3629 4.10791 12.8245C3.50852 12.2251 3.05066 11.5377 2.73434 10.804M0.516543 7.55176L2.23116 9.26638L3.94578 7.55176M5.95088 4.97787L11.3675 4.97787L11.3675 7.1379L5.95088 7.1379L5.95088 4.97787ZM6.2096 7.13777L11.0825 7.13777L11.0825 9.2978L6.2096 9.2978L6.2096 7.13777ZM5.95114 9.29726L11.3678 9.29726L11.3678 11.4573H5.95114V9.29726Z',
+        title: 'No vendor lock-in',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
+      },
+      {
+        icon: 'M2.22628 8.76631C2.07453 6.92839 2.70175 5.03824 4.10791 3.63208C6.64632 1.09367 10.7619 1.09367 13.3003 3.63208C15.8387 6.17049 15.8387 10.2861 13.3003 12.8245C10.7619 15.3629 6.64632 15.3629 4.10791 12.8245C3.50852 12.2251 3.05066 11.5377 2.73434 10.804M0.516543 7.55176L2.23116 9.26638L3.94578 7.55176M5.95088 4.97787L11.3675 4.97787L11.3675 7.1379L5.95088 7.1379L5.95088 4.97787ZM6.2096 7.13777L11.0825 7.13777L11.0825 9.2978L6.2096 9.2978L6.2096 7.13777ZM5.95114 9.29726L11.3678 9.29726L11.3678 11.4573H5.95114V9.29726Z',
+        title: 'Automatic Backups',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
+      },
+      {
+        icon: 'M2.22628 8.76631C2.07453 6.92839 2.70175 5.03824 4.10791 3.63208C6.64632 1.09367 10.7619 1.09367 13.3003 3.63208C15.8387 6.17049 15.8387 10.2861 13.3003 12.8245C10.7619 15.3629 6.64632 15.3629 4.10791 12.8245C3.50852 12.2251 3.05066 11.5377 2.73434 10.804M0.516543 7.55176L2.23116 9.26638L3.94578 7.55176M5.95088 4.97787L11.3675 4.97787L11.3675 7.1379L5.95088 7.1379L5.95088 4.97787ZM6.2096 7.13777L11.0825 7.13777L11.0825 9.2978L6.2096 9.2978L6.2096 7.13777ZM5.95114 9.29726L11.3678 9.29726L11.3678 11.4573H5.95114V9.29726Z',
+        title: 'Highly Scalable',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
+      },
+    ],
+  },
 }
