0f36aeec3605ff50fc86ddd1369545094810d94f
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-05-29 12:43:53 +0200

    Add hero image and update content

--- apps/www/components/Sections/HighlightCards.tsx
+++ apps/www/components/Sections/HighlightCards.tsx
@@ -5,7 +5,7 @@ import InteractiveShimmerCard from '../InteractiveShimmerCard'
 interface Highlight {
   image: string
   title: string
-  paragraph: string
+  paragraph: string | React.ReactNode
 }
 
 const HighlightCards = ({ highlights }: { highlights: Highlight[] }) => {
@@ -13,10 +13,12 @@ const HighlightCards = ({ highlights }: { highlights: Highlight[] }) => {
     <SectionContainer>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
         {highlights.map((highlight) => (
-          <InteractiveShimmerCard innerClassName="flex flex-col p-8 !bg-scale-200">
-            <div className="w-full aspect-square mb-4">image</div>
-            <h3 className="text-lg text-scale-1200 font-medium mb-2">{highlight.title}</h3>
-            <p className="text-scale-900">{highlight.paragraph}</p>
+          <InteractiveShimmerCard innerClassName="flex flex-col !bg-scale-200">
+            <div className="w-full aspect-square mb-4"></div>
+            <div className="p-8">
+              <h3 className="text-lg text-scale-1200 font-medium mb-2">{highlight.title}</h3>
+              <p className="text-scale-900">{highlight.paragraph}</p>
+            </div>
           </InteractiveShimmerCard>
         ))}
       </div>
--- apps/www/components/Sections/ProductHeaderCentered.tsx
+++ apps/www/components/Sections/ProductHeaderCentered.tsx
@@ -1,56 +1,68 @@
 import { Button, IconBookOpen, IconPlayCircle } from 'ui'
 import Link from 'next/link'
 import ProductIcon from '../ProductIcon'
+import Image from 'next/image'
 
 interface Types {
   h1: string | React.ReactNode
-  subheader: string[]
+  subheader: string | React.ReactNode
   icon?: string
   title?: string
-  image?: React.ReactNode
+  image?: string | React.ReactNode
   footer?: React.ReactNode
-  documentation_url?: string
+  cta?: {
+    label?: string
+    link: string
+  }
+  video?: string
 }
 
 const ProductHeaderCentered = (props: Types) => (
-  <div className="container relative mx-auto px-6 pt-16 pb-0 sm:px-16 lg:pt-32 xl:px-20">
+  <div className="container relative w-full mx-auto px-6 pt-4 pb-0 sm:px-16 xl:px-20">
     <div className="flex flex-col text-center items-center">
-      <div className="flex flex-col items-center space-y-2 mx-auto max-w-2xl">
+      {props.image && typeof props.image === 'string' ? (
+        <div className="relative w-full max-w-[830px] mx-auto z-0 aspect-[2.3/1] -mb-8 md:-mb-12 lg:-mb-16">
+          <Image src={props.image} layout="fill" objectFit="contain" objectPosition="top" />
+        </div>
+      ) : (
+        <div className="col-span-12 mt-8 lg:col-span-7 lg:mt-0 xl:col-span-6 xl:col-start-7">
+          {props.image}
+        </div>
+      )}
+      <div className="relative z-10 flex flex-col items-center space-y-2 mx-auto max-w-2xl">
         <div>
           {props.icon || props.title ? (
             <div className="mb-4 flex justify-center items-center gap-3">
               {props.icon && <ProductIcon icon={props.icon} />}
               {props.title && (
-                <span className="text-scale-1200" key={`product-name-${props.title}`}>
+                <span
+                  className="text-brand-900 font-mono uppercase tracking-widest text-sm"
+                  key={`product-name-${props.title}`}
+                >
                   {props.title}
                 </span>
               )}
             </div>
           ) : null}
-          <h1 className="h1 tracking-[-1px]" key={`h1`}>
+          <h1 className="h1 text-3xl md:text-4xl tracking-[-1px]" key={`h1`}>
             {props.h1}
           </h1>
         </div>
         <div>
-          {props.subheader &&
-            props.subheader.map((subheader, i) => {
-              return (
-                <p className="p lg:text-lg" key={i}>
-                  {subheader}
-                </p>
-              )
-            })}
+          <p className="p !text-scale-1000">{props.subheader}</p>
         </div>
-        <div className="flex flex-row md:flex-row md:items-center">
-          <Link href="https://supabase.com/dashboard" as="https://supabase.com/dashboard">
-            <a>
-              <Button size="medium" className="text-white">
-                Start for free
-              </Button>
-            </a>
-          </Link>
-          {props.documentation_url && (
-            <Link href={props.documentation_url} as={props.documentation_url}>
+        <div className="flex flex-row md:flex-row pt-8 md:items-center">
+          {props.cta && (
+            <Link href={props.cta.link} as={props.cta.link}>
+              <a>
+                <Button size="medium" className="text-white">
+                  {props.cta.label ?? 'Start for free'}
+                </Button>
+              </a>
+            </Link>
+          )}
+          {props.video && (
+            <Link href={props.video} as={props.video}>
               <a className="ml-2">
                 <Button type="default" size="medium" icon={<IconPlayCircle />}>
                   Watch video
@@ -61,11 +73,6 @@ const ProductHeaderCentered = (props: Types) => (
         </div>
         {props.footer && <div className="mb-4">{props.footer}</div>}
       </div>
-      {/* {props.image && (
-        <div className="col-span-12 mt-8 lg:col-span-7 lg:mt-0 xl:col-span-6 xl:col-start-7">
-          {props.image}
-        </div>
-      )} */}
     </div>
   </div>
 )
