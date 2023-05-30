967674c866f3e0bef04967a8ba01deb527eb93d9
Alaister Young <a@alaisteryoung.com>
2023-05-30 15:49:53 +1000

    adds integrations page scaffold in settings

--- studio/components/layouts/OrganizationLayout.tsx
+++ studio/components/layouts/OrganizationLayout.tsx
@@ -1,21 +1,47 @@
+import { useParams } from 'common'
 import { useStore } from 'hooks'
 import { observer } from 'mobx-react-lite'
-import { FC, ReactNode } from 'react'
+import { useRouter } from 'next/router'
+import { PropsWithChildren } from 'react'
+import { Tabs } from 'ui'
 import { AccountLayout } from './'
 
-interface Props {
-  children: ReactNode
-}
-
-const OrganizationLayout: FC<Props> = ({ children }) => {
+const OrganizationLayout = ({ children }: PropsWithChildren<{}>) => {
   const { ui } = useStore()
+  const router = useRouter()
+  const { slug } = useParams()
+  const id = router.asPath.split('/').at(-1)
 
   return (
     <AccountLayout
       title={ui.selectedOrganization?.name ?? 'Supabase'}
       breadcrumbs={[{ key: `org-settings`, label: 'Settings' }]}
     >
-      {children}
+      <div className="p-4 pt-0">
+        <div className="space-y-3">
+          <section className="mt-4">
+            <h1 className="text-3xl">{ui.selectedOrganization?.name ?? 'Organization'} settings</h1>
+          </section>
+          <nav>
+            <Tabs
+              size="small"
+              type="underlined"
+              activeId={id}
+              onChange={(id: any) => {
+                router.push(`/org/${slug}/${id}`)
+              }}
+            >
+              <Tabs.Panel id="general" label="General" />
+              <Tabs.Panel id="team" label="Team" />
+              <Tabs.Panel id="integrations" label="Integrations" />
+              <Tabs.Panel id="billing" label="Billing" />
+              <Tabs.Panel id="invoices" label="Invoices" />
+            </Tabs>
+          </nav>
+        </div>
+
+        <div className="mb-8">{children}</div>
+      </div>
     </AccountLayout>
   )
 }
