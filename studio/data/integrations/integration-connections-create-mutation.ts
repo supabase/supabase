a3a50fa066e333dddc0598fc3fbc29407ae40e1c
Alaister Young <a@alaisteryoung.com>
2023-06-14 00:29:48 +1000

    adds working project linker

---
+++ studio/data/integrations/integration-connections-create-mutation.ts
@@ -0,0 +1,51 @@
+import { useMutation, UseMutationOptions } from '@tanstack/react-query'
+import { post } from 'lib/common/fetch'
+import { API_URL } from 'lib/constants'
+
+export type IntegrationConnectionsCreateVariables = {
+  organizationIntegrationId: string
+  connections: {
+    foreignProjectId: string
+    supabaseProjectId: string
+  }[]
+}
+
+export async function createIntegrationConnections({
+  organizationIntegrationId,
+  connections,
+}: IntegrationConnectionsCreateVariables) {
+  const response = await post(`${API_URL}/integrations/connections`, {
+    organizationIntegrationId,
+    connections,
+  })
+  if (response.error) {
+    throw response.error
+  }
+
+  return response
+}
+
+type IntegrationConnectionsCreateData = Awaited<ReturnType<typeof createIntegrationConnections>>
+
+export const useIntegrationConnectionsCreateMutation = ({
+  onSuccess,
+  ...options
+}: Omit<
+  UseMutationOptions<
+    IntegrationConnectionsCreateData,
+    unknown,
+    IntegrationConnectionsCreateVariables
+  >,
+  'mutationFn'
+> = {}) => {
+  return useMutation<
+    IntegrationConnectionsCreateData,
+    unknown,
+    IntegrationConnectionsCreateVariables
+  >((vars) => createIntegrationConnections(vars), {
+    async onSuccess(data, variables, context) {
+      await onSuccess?.(data, variables, context)
+    },
+    ...options,
+  })
+}
