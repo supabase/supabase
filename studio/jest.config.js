f144460cddf5ec8a3b4ab817fa0a2b57eeb485c9
Ryuya <ryuyxx@gmail.com>
2023-06-15 03:31:45 +0900

    Discard changes to studio/jest.config.js

--- studio/jest.config.js
+++ studio/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   moduleDirectories: ['<rootDir>', 'node_modules'],
   setupFiles: ['jest-canvas-mock', './tests/setup/radix'],
   testEnvironment: 'jsdom',
-  testTimeout: 20000,
+  testTimeout: 10000,
   testRegex: '(.*\\.test.(js|jsx|ts|tsx)$)',
   transform: { '^.+\\.(t|j)sx?$': 'ts-jest' },
   globals: {
