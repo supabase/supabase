9671b902fba8e9f60b3b5f8c7880ad4f3a1cd20c
Long Hoang <1732217+loong@users.noreply.github.com>
2023-06-14 22:29:23 +0800

    Add guide for the case of high memory usage
    Update apps/docs/pages/guides/platform/exhaust-ram.mdx
    
    Co-authored-by: Kevin Grüneberg <k.grueneberg1994@gmail.com>
    
    Update apps/docs/pages/guides/platform/exhaust-ram.mdx
    
    Co-authored-by: Kevin Grüneberg <k.grueneberg1994@gmail.com>
    
    Update apps/docs/pages/guides/platform/exhaust-ram.mdx
    
    Co-authored-by: Kevin Grüneberg <k.grueneberg1994@gmail.com>
    
    Fix underscore issue with prettier
    
    Add link to high ram guide on usage v2 page
    
    Update apps/docs/pages/guides/platform/exhaust-ram.mdx
    
    Co-authored-by: Kevin Grüneberg <k.grueneberg1994@gmail.com>
    
    Update apps/docs/pages/guides/platform/exhaust-ram.mdx
    
    Co-authored-by: Kevin Grüneberg <k.grueneberg1994@gmail.com>
    
    Update apps/docs/pages/guides/platform/exhaust-ram.mdx
    
    Co-authored-by: Greg Richardson <greg@supabase.io>
    
    Update apps/docs/pages/guides/platform/exhaust-ram.mdx
    
    Co-authored-by: Greg Richardson <greg@supabase.io>
    
    Update apps/docs/pages/guides/platform/exhaust-ram.mdx
    
    Co-authored-by: Greg Richardson <greg@supabase.io>
    
    Update apps/docs/pages/guides/platform/exhaust-ram.mdx
    
    Co-authored-by: Greg Richardson <greg@supabase.io>
    
    Update apps/docs/pages/guides/platform/exhaust-ram.mdx
    
    Co-authored-by: Greg Richardson <greg@supabase.io>
    
    Update apps/docs/pages/guides/platform/exhaust-ram.mdx
    
    Co-authored-by: Greg Richardson <greg@supabase.io>
    
    Update apps/docs/pages/guides/platform/exhaust-ram.mdx
    
    Co-authored-by: Greg Richardson <greg@supabase.io>
    
    Update apps/docs/pages/guides/platform/exhaust-ram.mdx
    
    Co-authored-by: Greg Richardson <greg@supabase.io>
    
    Move admonition note on high memory up

--- apps/docs/components/Navigation/NavigationMenu/NavigationMenu.constants.ts
+++ apps/docs/components/Navigation/NavigationMenu/NavigationMenu.constants.ts
@@ -1056,6 +1056,10 @@ export const platform: NavMenuConstant = {
           name: 'High CPU Usage',
           url: '/guides/platform/exhaust-cpu',
         },
+        {
+          name: 'High RAM Usage',
+          url: '/guides/platform/exhaust-ram',
+        },
       ],
     },
   ],
