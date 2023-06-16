f82a1784ad58f7bcdba92facd7e84f1ee4af3f1a
Long Hoang <1732217+loong@users.noreply.github.com>
2023-06-16 15:17:33 +0800

    Add option to show 7 days worth of report metrics

--- studio/lib/constants/metrics.tsx
+++ studio/lib/constants/metrics.tsx
@@ -416,16 +416,17 @@ export const TIME_PERIODS_INFRA = [
   {
     key: '1h',
     label: 'Last hour',
-    interval: '1m',
   },
   {
     key: '3h',
     label: 'Last 3 hours',
-    interval: '5m',
   },
   {
     key: '1d',
     label: 'Last 24 hours',
-    interval: '30m',
+  },
+  {
+    key: '7d',
+    label: 'Last 7 days',
   },
 ]
