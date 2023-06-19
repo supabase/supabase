81b6532611e15889a30eeaf5164dc64c9ba7af8f
Isaiah Hamilton <70828596+Isaiah-Hamilton@users.noreply.github.com>
2023-06-19 14:42:50 -0400

    chore: update community number

--- apps/www/data/Community.tsx
+++ apps/www/data/Community.tsx
@@ -42,7 +42,7 @@ const data: CommunityItem[] = [
   },
   {
     title: 'Twitter',
-    stat: '72,000+',
+    stat: '74,000+',
     statLabel: 'Followers',
     img: 'twitter.png',
     detail: () => (
