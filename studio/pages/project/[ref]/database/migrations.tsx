e094ebc5b826ab8b867da6d7127dc1ac4e2f944d
Alaister Young <a@alaisteryoung.com>
2023-06-20 23:22:38 +1000

    add boilerplate for migrations page

---
+++ studio/pages/project/[ref]/database/migrations.tsx
@@ -0,0 +1,37 @@
+import { DatabaseLayout } from 'components/layouts'
+import { useProjectContext } from 'components/layouts/ProjectLayout/ProjectContext'
+import { useMigrationsQuery } from 'data/database/migrations-query'
+import { NextPageWithLayout } from 'types'
+
+const MigrationsPage: NextPageWithLayout = () => {
+  const { project } = useProjectContext()
+  const { data, isLoading, isSuccess, isError } = useMigrationsQuery({
+    projectRef: project?.ref,
+    connectionString: project?.connectionString,
+  })
+
+  return (
+    <div>
+      {isLoading && <div>Loading state...</div>}
+      {isError && <div>Error state...</div>}
+      {isSuccess && (
+        <div>
+          {data.result.map((migration) => (
+            <div key={migration.version}>
+              <div>{migration.version}</div>
+              <div>
+                {migration.statements?.map((statement, i) => (
+                  <div key={i}>{statement}</div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  )
+}
+
+MigrationsPage.getLayout = (page) => <DatabaseLayout title="Migrations">{page}</DatabaseLayout>
+
+export default MigrationsPage
