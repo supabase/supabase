0c72f255fa2483120a7f8b75779261de68eb5b66
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2023-06-23 22:11:07 +0800

    chore: update to support new theme

--- packages/ui/internals/tokens/copy-tokens.js
+++ packages/ui/internals/tokens/copy-tokens.js
@@ -4,11 +4,12 @@ const fs = require('fs')
 /*
  * Branch to target in GitHub repo
  */
-const branch = 'feat/new-components'
+const branch = 'feat/figma-variables' // 'feat/new-components'
 // using Math.random() in a param as GitHub caches raw content
 // const url = `https://raw.githubusercontent.com/MildTomato/supabase-design-tokens/${branch}/tokens.json?e=${Math.random()}`
 const baseUrl = `https://raw.githubusercontent.com/MildTomato/supabase-design-tokens/${branch}/tokens/`
 
+console.log(baseUrl)
 /*
  * Files that need to be copied over from Tokens repo
  */
@@ -17,12 +18,13 @@ const TOKEN_FILES_METADATA = [
   // { fileName: '$metadata.json', type: 'config' },
   // { fileName: '$themes.json', type: 'config' },
   // source files (comment out for now)
-  // { fileName: 'global.json', type: 'source' },
+  { fileName: 'global.json', type: 'source', subdirectory: '' },
   // { fileName: 'global-two.json', type: 'source' },
   // semantic (comment out for now)
   // { fileName: 'typography.json', type: 'semantic' },
   // themes
-  { fileName: 'root.json', type: 'theme', subdirectory: 'new/' }, // root theme
+  // { fileName: 'root.json', type: 'theme', subdirectory: 'new/' }, // root theme
+  { fileName: 'dark.json', type: 'theme', subdirectory: 'new/' },
   { fileName: 'light.json', type: 'theme', subdirectory: 'new/' },
   // { fileName: 'darker-dark.json', type: 'theme', subdirectory: 'new/' },
 ]
@@ -37,15 +39,30 @@ const PATHS = {
 async function getTokensFile() {
   const promises = TOKEN_FILES_METADATA.map((tokenMetadata, i) => {
     return new Promise((resolve, reject) => {
+      console.log(
+        'URL being copied',
+        baseUrl +
+          tokenMetadata.subdirectory +
+          tokenMetadata.fileName +
+          `?e=${Math.floor(Math.random() * 1000000)}`
+      )
       https
         .get(
-          baseUrl + tokenMetadata.subdirectory + tokenMetadata.fileName + `?e=${Math.random()}`,
+          baseUrl +
+            tokenMetadata.subdirectory +
+            tokenMetadata.fileName +
+            `?e=${Math.floor(Math.random() * 1000000)}`,
           (res) => {
+            // console.log('res', res)
             let data = ''
             res.on('data', (chunk) => {
               data += chunk
             })
             res.on('end', () => {
+              console.log(
+                'should save to ',
+                `./tokens/${PATHS[tokenMetadata.type]}${tokenMetadata.fileName}`
+              )
               fs.writeFile(
                 `./tokens/${PATHS[tokenMetadata.type]}${tokenMetadata.fileName}`,
                 data,
