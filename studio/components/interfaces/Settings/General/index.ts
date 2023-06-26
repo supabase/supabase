9bf6ad8bff2476d3a8d2e6c3d23213794c8ba9dd
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2023-06-26 19:50:16 +0200

    feat: transfer project to another org

--- studio/components/interfaces/Settings/General/index.ts
+++ studio/components/interfaces/Settings/General/index.ts
@@ -3,5 +3,15 @@ import Infrastructure from './Infrastructure/Infrastructure'
 import CustomDomainConfig from './CustomDomainConfig/CustomDomainConfig'
 import DeleteProjectPanel from './DeleteProjectPanel/DeleteProjectPanel'
 import DeleteProjectButton from './DeleteProjectPanel/DeleteProjectButton'
+import TransferProjectButton from './TransferProjectPanel/TransferProjectButton'
+import TransferProjectPanel from './TransferProjectPanel/TransferProjectPanel'
 
-export { General, Infrastructure, CustomDomainConfig, DeleteProjectPanel, DeleteProjectButton }
+export {
+  General,
+  Infrastructure,
+  CustomDomainConfig,
+  DeleteProjectPanel,
+  DeleteProjectButton,
+  TransferProjectPanel,
+  TransferProjectButton,
+}
