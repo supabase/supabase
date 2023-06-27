eadf48234f6c3c618b011befe5ec63a987132305
TzeYiing <ty@tzeyiing.com>
2023-06-28 02:25:09 +0800

    chore: add failing test

---
+++ studio/tests/pages/projects/billing/Infrastructure.test.tsx
@@ -0,0 +1,26 @@
+import { get } from 'lib/common/fetch'
+import { render } from '../../../helpers'
+import { screen } from '@testing-library/react'
+import Infrastructure from 'components/interfaces/BillingV2/Usage/Infrastructure'
+beforeEach(() => {
+  // reset mocks between tests
+  ;(get as jest.Mock).mockReset()
+  ;(get as jest.Mock).mockImplementation(async (_url: string) => {
+    console.log('url', _url)
+
+    return [{ result: [] }]
+  })
+})
+
+test(`renders category attributes static elements`, async () => {
+  render(
+    <Infrastructure
+      projectRef={'someref'}
+      startDate={new Date().toISOString()}
+      endDate={new Date().toISOString()}
+      currentBillingCycleSelected={false}
+    />
+  )
+  // renders usage categories info
+  await screen.findByText('Max CPU usage of your server')
+})
