6683e48a64c2e668fc664f1d8e56b6d58662fd22
TzeYiing <ty@tzeyiing.com>
2023-06-27 19:58:36 +0800

    chore: add testing around datepicker selected date display

--- studio/tests/pages/projects/Logs.Datepickers.test.js
+++ studio/tests/pages/projects/Logs.Datepickers.test.js
@@ -4,7 +4,8 @@ import { PREVIEWER_DATEPICKER_HELPERS } from 'components/interfaces/Settings/Log
 import DatePickers from 'components/interfaces/Settings/Logs/Logs.DatePickers'
 import dayjs from 'dayjs'
 import { render } from '../../helpers'
-
+import timezone from 'dayjs/plugin/timezone'
+dayjs.extend(timezone)
 test('renders warning', async () => {
   const from = dayjs().subtract(60, 'days')
   const to = dayjs()
@@ -18,4 +19,20 @@ test('renders warning', async () => {
   userEvent.click(await screen.findByText(RegExp(from.format('DD MMM'))))
   await screen.findByText(/memory errors/)
   await screen.findByText(RegExp(from.format('MMMM YYYY')))
+})
+
+test('renders dates in local time', async () => {
+  const from = dayjs().subtract(1, 'days')
+  const to = dayjs()
+  const tz = 'Asia/Taipei'
+  render(
+    <DatePickers
+      helpers={PREVIEWER_DATEPICKER_HELPERS}
+      to={to.toISOString()}
+      from={from.toISOString()}
+    />
+  )
+  // renders time locally
+  userEvent.click(await screen.findByText(RegExp(from.tz(tz).format('DD MMM'))))
+  await screen.findByText(RegExp(from.tz(tz).format('MMMM YYYY')))
 })
