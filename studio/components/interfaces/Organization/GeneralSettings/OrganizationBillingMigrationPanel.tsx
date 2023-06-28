e0ec15addee54dde429cae84c3986f6ed4f7d309
Terry Sutton <saltcod@gmail.com>
2023-06-28 17:33:12 -0230

    Move org migration button to Billing tab

--- studio/components/interfaces/Organization/GeneralSettings/OrganizationBillingMigrationPanel.tsx
+++ studio/components/interfaces/Organization/GeneralSettings/OrganizationBillingMigrationPanel.tsx
@@ -1,5 +1,5 @@
 import { observer } from 'mobx-react-lite'
-import { Alert, Button, IconExternalLink } from 'ui'
+import { Alert, Badge, Button, IconExternalLink, IconTruck } from 'ui'
 
 import Panel from 'components/ui/Panel'
 import MigrateOrganizationBillingButton from './MigrateOrganizationBillingButton'
@@ -7,35 +7,42 @@ import Link from 'next/link'
 
 const OrganizationBillingMigrationPanel = observer(() => {
   return (
-    <Panel
-      title={
-        <p key="panel-title" className="uppercase">
-          Migrate to organization billing
-        </p>
-      }
-    >
-      <Panel.Content>
-        <Alert
-          withIcon
-          variant="neutral"
-          title={<span>Use the new organization-level billing</span>}
-        >
-          <p>Migrating to the new organization-level billing is irreversible.</p>
-
-          <div className="flex mt-3 items-center space-x-4">
-            <Link href="https://www.notion.so/supabase/Organization-Level-Billing-9c159d69375b4af095f0b67881276582?pvs=4">
-              <a target="_blank" rel="noreferrer">
-                <Button type="default" icon={<IconExternalLink strokeWidth={1.5}  />}>
-                  Documentation
-                </Button>
-              </a>
-            </Link>
-
-            <MigrateOrganizationBillingButton />
+    <div className="space-y-2">
+      <div className="flex gap-4 items-center">
+        <h4>Organization-level billing</h4>
+        <Badge color="green">
+          <span className="flex gap-2 items-center text-xs uppercase">
+            <IconTruck width={12} /> New
+          </span>
+        </Badge>
+      </div>
+      <p className="text-sm opacity-50">Migrate this project to organization-level billing</p>
+      <Panel>
+        <Panel.Content>
+          <div className="flex justify-between gap-12">
+            <div className="space-y-2">
+              <p className="text-sm text-scale-1100">
+                You will have a single subscription for all projects inside the organization, rather
+                than individual subscriptions per project.
+              </p>
+              <p className="text-sm text-scale-1100">
+                Migrating your organization billing is not reversible.
+              </p>
+            </div>
+            <div className="flex items-center gap-4 ml-12">
+              <Link href="https://www.notion.so/supabase/Organization-Level-Billing-9c159d69375b4af095f0b67881276582?pvs=4">
+                <a target="_blank" rel="noreferrer">
+                  <Button type="default" icon={<IconExternalLink strokeWidth={1.5} />}>
+                    Documentation
+                  </Button>
+                </a>
+              </Link>
+              <MigrateOrganizationBillingButton />
+            </div>
           </div>
-        </Alert>
-      </Panel.Content>
-    </Panel>
+        </Panel.Content>
+      </Panel>
+    </div>
   )
 })
 
