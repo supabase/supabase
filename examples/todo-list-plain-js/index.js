cc361b64e2e6601350b43ff489ba1c620563ca4c
Preet Suthar <preetsutharxd@gmail.com>
2023-07-02 10:50:43 +0530

    update codee

--- examples/todo-list-plain-js/index.js
+++ examples/todo-list-plain-js/index.js
@@ -1,33 +1,22 @@
-
-// Simple JavaScript example to take a name input and save it in a Supabase database.
-
 const { createClient } = require("@supabase/supabase-js");
 const readline = require("readline");
 
-const supabaseUrl = "replace-with-your-supabase-url";
+const supabaseUrl = "https://mbwscisakidilfvsknjb.supabase.co";
 const supabaseKey =
-  "replace-with-your-supabasekey";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1id3NjaXNha2lkaWxmdnNrbmpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc2NzkxODksImV4cCI6MjAwMzI1NTE4OX0.HTyKnzPEOkAEEkKa83qfCwjiw7eZtwCekOZ9e3IZaxc";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-async function fetchTableData(tableName) {
+async function fetchTodoData() {
   try {
-    const { data, error } = await supabase.from(tableName).select("*");
-    if (error) throw error;
-    return data;
+    const { data, error } = await supabase.from("todoreg").select("*");
+
+    if (error) {
+      throw error;
+    }
+
+    console.log(data);
   } catch (error) {
     console.error("Error fetching data:", error.message);
-    return null;
-  }
-}
-
-async function insertRow(tableName, row) {
-  try {
-    const { data, error } = await supabase.from(tableName).insert(row);
-    if (error) throw error;
-    return data;
-  } catch (error) {
-    console.error("Error inserting row:", error.message);
-    return null;
   }
 }
 
@@ -38,30 +27,50 @@ const rl = readline.createInterface({
 
 async function main() {
   try {
-    const name = await new Promise((resolve) => {
-      rl.question(" New To-Do ", (answer) => {
-        resolve(answer);
-      });
-    });
-
-    const { data, error } = await supabase
-      .from("To-Do-reg")
-      .insert({ todo: todo });
-
-    if (error) {
-      throw error;
-    }
-
-    console.log("Inserted To-do:", todo);
-    import("chalk").then((chalk) => {
-      console.log(
-        chalk.default.blue("To-Do Added!")
+    const input = await new Promise((resolve) => {
+      rl.question(
+        "Enter 1 to for new todo\nEnter 2 to fetch todo\n\n>> ",
+        (answer) => {
+          resolve(answer);
+        }
       );
     });
+    if (input == "2") {
+      fetchTodoData();
+    } else if (input == "1") {
+      try {
+        const todo = await new Promise((resolve) => {
+          rl.question(" New To-Do: ", (answer) => {
+            resolve(answer);
+          });
+        });
+
+        const { data, error } = await supabase
+          .from("todoreg")
+          .insert({ todo: todo });
+
+        if (error) {
+          throw error;
+        }
+
+        console.log("Inserted To-do:", todo);
+        import("chalk").then((chalk) => {
+          console.log(chalk.default.blue("To-Do Added!"));
+        });
+      } catch (error) {
+        import("chalk").then((chalk) => {
+          console.error(
+            chalk.default.red("Error adding To-Do:", error.message)
+          );
+        });
+      } finally {
+        rl.close();
+      }
+    } else {
+      console.log("please enter valid choice!");
+    }
   } catch (error) {
-    import("chalk").then((chalk) => {
-      console.error(chalk.default.red("Error adding To-Do:", error.message));
-    });
+    console.log(error);
   } finally {
     rl.close();
   }
