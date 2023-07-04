d58b1c55243c22d2a6d3ea926eda436352731cdc
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2023-07-05 01:14:13 +0800

    chore: update Alert

--- packages/ui/src/components/shadcn/stories/Alert.stories.tsx
+++ packages/ui/src/components/shadcn/stories/Alert.stories.tsx
@@ -1,5 +1,5 @@
 import { Alert, AlertDescription, AlertTitle } from '@ui/components/shadcn/ui/alert'
-import { AlertCircle, Terminal, AlertTriangle } from 'lucide-react'
+import { AlertCircle, Terminal, AlertTriangle, Info } from 'lucide-react'
 import { Meta } from '@storybook/react'
 
 const meta: Meta<typeof Alert> = {
@@ -8,27 +8,37 @@ const meta: Meta<typeof Alert> = {
 }
 
 export const Default = () => (
-  <Alert className="flex gap-6">
-    <div className="bg-foreground rounded h-5 w-5 flex items-center justify-center">
-      <AlertTriangle className="h-3 w-3 text-background" strokeWidth={2} />
-    </div>
-    <div>
+  <Alert>
+      <Info strokeWidth={2} />
       <AlertTitle>Heads up!</AlertTitle>
-      <AlertDescription>You can add components to your app using the cli.</AlertDescription>
-    </div>
+      <AlertDescription>You can add components to your app using the cli.</AlertDescription> 
   </Alert>
 )
 
 Default.storyName = 'Default'
 
 export const Destructive = () => (
-  <Alert variant="destructive">
-    <AlertCircle className="h-4 w-4" />
-    <AlertTitle>Error</AlertTitle>
-    <AlertDescription>Your session has expired. Please log in again.</AlertDescription>
-  </Alert>
+ <Alert variant="destructive">
+      <AlertCircle className="h-4 w-4" />
+      <AlertTitle>Error</AlertTitle>
+      <AlertDescription>
+        Your session has expired. Please log in again.
+      </AlertDescription>
+    </Alert>
 )
 
 Destructive.storyName = 'Destructive'
+
+export const Warning = () => (
+ <Alert variant="warning">
+    <AlertCircle className="h-4 w-4" />
+    <AlertTitle>Error</AlertTitle>
+    <AlertDescription>
+      Your session has expired. Please log in again.
+    </AlertDescription>
+  </Alert>
+)
+
+Warning.storyName = 'Warning'
 
 export default meta
