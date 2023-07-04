8c4a9429d311193a4ebff71d3233259df80477ee
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2023-07-05 01:14:13 +0800

    chore: update Alert

--- packages/ui/src/components/shadcn/ui/alert.tsx
+++ packages/ui/src/components/shadcn/ui/alert.tsx
@@ -3,14 +3,33 @@ import { cva, type VariantProps } from 'class-variance-authority'
 
 import { cn } from '@ui/lib/utils'
 
+
+
 const alertVariants = cva(
-  'relative w-full rounded-lg border p-5 [&>svg]:absolute [&>svg]:text-foreground [&>svg]:left-4 [&>svg]:top-4 [&>svg+div]:translate-y-[-3px] [&:has(svg)]:pl-8',
+  'relative w-full rounded-lg border p-4 [&:has(svg)]:pl-14 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground'
+  // custom
+  + ' [&>svg]:w-[23px] [&>svg]:h-[23px] [&>svg]:p-1 [&>svg]:flex [&>svg]:rounded',
   {
     variants: {
       variant: {
-        default: 'bg-alternative text-foreground',
+        default:  
+          'text-foreground' + 
+          // removed `bg-background`
+
+          // custom styles
+          ' bg-alternative [&>svg]:text-background [&>svg]:bg-foreground-strong',
         destructive:
-          'text-destructive border-destructive/50 [&>svg]:text-destructive text-destructive',
+          'text-destructive' + 
+          // removed 'border-destructive/50 dark:border-destructive [&>svg]:text-destructive'
+
+          // custom styles
+          ' border-destructive-300 bg-destructive-400 [&>svg]:text-destructive-400 [&>svg]:bg-destructive',
+        
+        /**
+         * custom variant
+         */
+        warning:
+          'text-warning border-warning-300 bg-warning-400 [&>svg]:text-warning-400 [&>svg]:bg-warning',
       },
     },
     defaultVariants: {
