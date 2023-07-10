3aa0ac4b21c57b6ed2c5d6ec95d59da3d7fca3b6
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-07-10 19:08:58 +0200

    fix import

--- apps/www/data/products/vector/pageData.tsx
+++ apps/www/data/products/vector/pageData.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link'
-import IntegrationsImage from '~/components/products/vector/IntegrationsImage'
-import OpenAIImage from '~/components/products/vector/OpenAIImage'
-import SecureAndScalableImg from '~/components/products/vector/SecureAndScalableImg'
-import PGvectorImg from '~/components/products/vector/PGvectorImg'
-import DeployGlobally from '~/components/products/vector/DeployGlobally'
+import IntegrationsImage from '~/components/Products/Vector/IntegrationsImage'
+import OpenAIImage from '~/components/Products/Vector/OpenAIImage'
+import SecureAndScalableImg from '~/components/Products/Vector/SecureAndScalableImg'
+import PGvectorImg from '~/components/Products/Vector/PGvectorImg'
+import DeployGlobally from '~/components/Products/Vector/DeployGlobally'
 
 export default {
   metaTitle: '',
