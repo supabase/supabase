871eac8ec17c7bbb4b8f597b1ff4851a308049a8
Greg Richardson <greg.nmr@gmail.com>
2023-07-14 10:42:30 -0600

    feat(sql-editor): sql ai api

---
+++ studio/data/ai/sql-generate-mutation.ts
@@ -0,0 +1,39 @@
+import { useMutation, UseMutationOptions, useQueryClient } from '@tanstack/react-query'
+import { post } from 'lib/common/fetch'
+import { aiKeys } from './keys'
+
+export type SqlGenerateResponse = {
+  sql: string
+  title: string
+}
+
+export type SqlGenerateVariables = {
+  prompt: string
+}
+
+export async function generateSql({ prompt }: SqlGenerateVariables) {
+  const response = await post<SqlGenerateResponse>('/api/ai/sql', { prompt })
+  if ('error' in response) {
+    throw response.error
+  }
+
+  return response
+}
+
+type SqlGenerateData = Awaited<ReturnType<typeof generateSql>>
+
+export const useSqlGenerateMutation = ({
+  onSuccess,
+  ...options
+}: Omit<UseMutationOptions<SqlGenerateData, unknown, SqlGenerateVariables>, 'mutationFn'> = {}) => {
+  const queryClient = useQueryClient()
+
+  return useMutation<SqlGenerateData, unknown, SqlGenerateVariables>((vars) => generateSql(vars), {
+    async onSuccess(data, variables, context) {
+      await queryClient.invalidateQueries(aiKeys.sql())
+
+      await onSuccess?.(data, variables, context)
+    },
+    ...options,
+  })
+}
