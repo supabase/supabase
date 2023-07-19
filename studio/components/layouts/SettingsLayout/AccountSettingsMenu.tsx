a10865e90aa1803be46fbf4cc8478ea07e55fe04
Joshen Lim <joshenlimek@gmail.com>
2023-07-19 11:16:05 +0800

    Update account settings pages to use new layout

--- studio/components/layouts/SettingsLayout/AccountSettingsMenu.tsx
+++ studio/components/layouts/SettingsLayout/AccountSettingsMenu.tsx
@@ -1,5 +1,28 @@
+import { useRouter } from 'next/router'
+import SettingsMenuItem from './SettingsMenuItem'
+
 const AccountSettingsMenu = () => {
-  return <div>Account settings</div>
+  const router = useRouter()
+  const accountSettings = [
+    { label: 'Preferences', pathname: `/account/me` },
+    { label: 'Access Tokens', pathname: `/account/tokens` },
+  ]
+
+  return (
+    <div className="space-y-10">
+      <div className="space-y-2">
+        <p className="text-sm font-medium">Account Settings</p>
+        {accountSettings.map((link) => (
+          <SettingsMenuItem
+            key={link.label}
+            label={link.label}
+            href={link.pathname}
+            isActive={link.pathname === router.pathname}
+          />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default AccountSettingsMenu
