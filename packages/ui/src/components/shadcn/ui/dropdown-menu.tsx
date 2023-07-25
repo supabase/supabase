d5675c1391b8a829ad8d9af518ea60f58f992890
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2023-07-25 14:23:59 +0800

    modal behavior disabled for new dropdowns/comboboxes

--- packages/ui/src/components/shadcn/ui/dropdown-menu.tsx
+++ packages/ui/src/components/shadcn/ui/dropdown-menu.tsx
@@ -153,7 +153,7 @@ const DropdownMenuSeparator = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DropdownMenuPrimitive.Separator
     ref={ref}
-    className={cn('-mx-1 my-1 h-px bg-muted', className)}
+    className={cn('-mx-1 my-1 h-px bg-border', className)}
     {...props}
   />
 ))
