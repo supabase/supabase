53ff8cee88d882c17785a1efc438312a7eccc99b
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-07-26 16:52:45 +0200

    lw8: switch homepage lw8 visual with static image

--- apps/www/pages/launch-week/tickets/index.tsx
+++ apps/www/pages/launch-week/tickets/index.tsx
@@ -45,6 +45,7 @@ export default function TicketsPage({ users }: Props) {
   const OG_IMAGE = `${SITE_ORIGIN}/images/launchweek/8/lw8-og.jpg`
 
   const { isDarkMode, toggleTheme } = useTheme()
+  const [initialDarkMode] = useState(isDarkMode)
   const [isLoading, setIsLoading] = useState(false)
   const [offset, setOffset] = useState(1)
   const [isLast, setIsLast] = useState(false)
@@ -93,11 +94,11 @@ export default function TicketsPage({ users }: Props) {
   }, [])
 
   useEffect(() => {
-    if (!isDarkMode) toggleTheme(true)
+    toggleTheme(true)
     document.body.className = 'dark bg-[#020405]'
     return () => {
       document.body.className = ''
-      isDarkMode ? toggleTheme(true) : toggleTheme(false)
+      toggleTheme(initialDarkMode)
     }
   }, [])
 
