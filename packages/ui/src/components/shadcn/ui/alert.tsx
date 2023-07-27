ab6c2650c97307c4ec8eb0ead64e4175bc612236
Ivan Vasilov <vasilov.ivan@gmail.com>
2023-07-27 13:09:32 +0200

    Switch the alert to the new Shadcn one.

--- packages/ui/src/components/shadcn/ui/alert.tsx
+++ packages/ui/src/components/shadcn/ui/alert.tsx
@@ -3,9 +3,10 @@ import { cva, type VariantProps } from 'class-variance-authority'
 
 import { cn } from '@ui/lib/utils'
 
+// Ivan: replaced [&:has(svg)]:pl-14 with [&>svg~*]:pl-10 cause of github.com/shadcn-ui/ui/issues/998
 const alertVariants = cva(
   cn(
-    'relative w-full rounded-lg border p-4 [&:has(svg)]:pl-14 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground',
+    'relative w-full rounded-lg border p-4 [&>svg~*]:pl-10 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground',
     '[&>svg]:w-[23px] [&>svg]:h-[23px] [&>svg]:p-1 [&>svg]:flex [&>svg]:rounded'
   ),
   {
