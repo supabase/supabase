f0f07fbeb83407ae116c302eb24eb28a764778ee
Alaister Young <alaister@users.noreply.github.com>
2023-08-01 13:17:13 +1000

    Update studio/components/interfaces/Organization/IntegrationSettings/Integration.tsx
    Co-authored-by: Joshen Lim <joshenlimek@gmail.com>

--- studio/components/interfaces/Organization/IntegrationSettings/Integration.tsx
+++ studio/components/interfaces/Organization/IntegrationSettings/Integration.tsx
@@ -170,7 +170,7 @@ const IntegrationConnectionItem = ({
         onSelectConfirm={onConfirm}
       >
         <Modal.Content>
-          <p className="py-4 text-sm text-scale-1100">
+          <p className="py-4 text-sm text-light">
             {`This action cannot be undone. Are you sure you want to delete this connection?`}
           </p>
         </Modal.Content>
