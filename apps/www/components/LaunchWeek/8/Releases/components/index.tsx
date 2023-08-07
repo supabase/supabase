95f021e42ca796b257760c9e3b16bce1e84adcee
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-08-07 15:46:05 +0200

    remove comments

--- apps/www/components/LaunchWeek/8/Releases/components/index.tsx
+++ apps/www/components/LaunchWeek/8/Releases/components/index.tsx
@@ -240,8 +240,8 @@ export const AccordionHeader = ({
 export const VideoPreviewTrigger = ({ title, thumbnail }: { title: string; thumbnail: string }) => (
   <div className="flex items-center h-full gap-3 text-xs group/vid text-scale-1100 hover:text-scale-1200 transition-colors">
     <span>{title}</span>
-    <div className="relative h-8 !aspect-video flex items-center justify-center rounded overflow-hidden border border-scale-800 opacity-80 group-hover/vid:opacity-100 transition-colors">
-      <div className="absolute z-10 w-2.5 h-2.5 text-scale-900 opacity-80 group-hover/vid:opacity-100 transition-opacity">
+    <div className="relative h-8 !aspect-video flex items-center justify-center rounded overflow-hidden border border-scale-1000 opacity-80 group-hover/vid:opacity-100 transition-colors">
+      <div className="absolute z-10 w-2.5 h-2.5 text-scale-1000 opacity-80 group-hover/vid:opacity-100">
         <svg viewBox="0 0 81 91" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M76.5621 37.998C82.3369 41.3321 82.3369 49.6673 76.5621 53.0014L13.2198 89.5721C7.44504 92.9062 0.226562 88.7386 0.226562 82.0704L0.226566 8.92901C0.226566 2.26085 7.44506 -1.90673 13.2199 1.42735L76.5621 37.998Z"
--- packages/ui/src/layout/banners/LW8CountdownBanner/VideoPreview.tsx
+++ packages/ui/src/layout/banners/LW8CountdownBanner/VideoPreview.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const VideoPreview = () => (
   <div className="relative h-8 !aspect-video flex items-center justify-center rounded overflow-hidden border transition-colors">
-    <div className="absolute z-10 w-2.5 h-2.5 text-scale-1200 opacity-100">
+    <div className="absolute z-10 w-2.5 h-2.5 text-white opacity-100">
       <svg viewBox="0 0 81 91" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path
           d="M76.5621 37.998C82.3369 41.3321 82.3369 49.6673 76.5621 53.0014L13.2198 89.5721C7.44504 92.9062 0.226562 88.7386 0.226562 82.0704L0.226566 8.92901C0.226566 2.26085 7.44506 -1.90673 13.2199 1.42735L76.5621 37.998Z"
@@ -15,7 +15,6 @@ const VideoPreview = () => (
       alt="Video thumbnail"
       layout="fill"
       objectFit="cover"
-      // className="absolute inset-0 object-cover"
     />
   </div>
 )
