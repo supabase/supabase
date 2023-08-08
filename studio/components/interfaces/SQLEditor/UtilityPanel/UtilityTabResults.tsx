12cf13087a66e6c09da38778f1927a569078e890
Terry Sutton <saltcod@gmail.com>
2023-08-08 16:26:38 -0230

    Feature flag supabase ai

--- studio/components/interfaces/SQLEditor/UtilityPanel/UtilityTabResults.tsx
+++ studio/components/interfaces/SQLEditor/UtilityPanel/UtilityTabResults.tsx
@@ -1,7 +1,13 @@
 import { useSqlDebugMutation } from 'data/ai/sql-debug-mutation'
 import { useEntityDefinitionsQuery } from 'data/database/entity-definitions-query'
 import { isError } from 'data/utils/error-check'
-import { useLocalStorageQuery, useSelectedOrganization, useSelectedProject, useStore } from 'hooks'
+import {
+  useLocalStorageQuery,
+  useSelectedOrganization,
+  useSelectedProject,
+  useStore,
+  useFlag,
+} from 'hooks'
 import { IS_PLATFORM } from 'lib/constants'
 import { format } from 'sql-formatter'
 import { useSqlEditorStateSnapshot } from 'state/sql-editor'
@@ -41,6 +47,7 @@ const UtilityTabResults = ({ id, isExecuting }: UtilityTabResultsProps) => {
   const snippet = snap.snippets[id]
   const result = snap.results[id]?.[0]
   const isUtilityPanelCollapsed = (snippet?.splitSizes?.[1] ?? 0) === 0
+  const supabaseAIEnabled = useFlag('sqlEditorSupabaseAI')
 
   if (isUtilityPanelCollapsed) return null
 
@@ -55,46 +62,48 @@ const UtilityTabResults = ({ id, isExecuting }: UtilityTabResultsProps) => {
       <div className="bg-table-header-light dark:bg-table-header-dark">
         <div className="flex flex-row justify-between items-center pr-8">
           <p className="m-0 border-0 px-6 py-4 font-mono">{result.error.message ?? result.error}</p>
-          <Button
-            icon={
-              <div className="scale-75">
-                <AiIconAnimation className="w-3 h-3" loading={isDebugSqlLoading} />
-              </div>
-            }
-            disabled={!!sqlDiff}
-            onClick={async () => {
-              try {
-                const { solution, sql } = await debugSql({
-                  sql: snippet.snippet.content.sql.replace(sqlAiDisclaimerComment, '').trim(),
-                  errorMessage: result.error.message,
-                  entityDefinitions,
-                })
-
-                const formattedSql =
-                  sqlAiDisclaimerComment +
-                  '\n\n' +
-                  format(sql, {
-                    language: 'postgresql',
-                    keywordCase: 'lower',
-                  })
-                setAiInput('')
-                setDebugSolution(solution)
-                setSqlDiff({
-                  original: snippet.snippet.content.sql,
-                  modified: formattedSql,
-                })
-              } catch (error: unknown) {
-                if (isError(error)) {
-                  ui.setNotification({
-                    category: 'error',
-                    message: error.message,
-                  })
-                }
+          {supabaseAIEnabled && (
+            <Button
+              icon={
+                <div className="scale-75">
+                  <AiIconAnimation className="w-3 h-3" loading={isDebugSqlLoading} />
+                </div>
               }
-            }}
-          >
-            Debug using AI
-          </Button>
+              disabled={!!sqlDiff}
+              onClick={async () => {
+                try {
+                  const { solution, sql } = await debugSql({
+                    sql: snippet.snippet.content.sql.replace(sqlAiDisclaimerComment, '').trim(),
+                    errorMessage: result.error.message,
+                    entityDefinitions,
+                  })
+
+                  const formattedSql =
+                    sqlAiDisclaimerComment +
+                    '\n\n' +
+                    format(sql, {
+                      language: 'postgresql',
+                      keywordCase: 'lower',
+                    })
+                  setAiInput('')
+                  setDebugSolution(solution)
+                  setSqlDiff({
+                    original: snippet.snippet.content.sql,
+                    modified: formattedSql,
+                  })
+                } catch (error: unknown) {
+                  if (isError(error)) {
+                    ui.setNotification({
+                      category: 'error',
+                      message: error.message,
+                    })
+                  }
+                }
+              }}
+            >
+              Debug with Supabase AI
+            </Button>
+          )}
         </div>
         {(result.error.message ?? result.error)?.includes(
           'canceling statement due to statement timeout'
