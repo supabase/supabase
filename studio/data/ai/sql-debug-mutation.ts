706399d4e28581bd15d816922ec44d115840c0b7
Joshen Lim <joshenlimek@gmail.com>
2023-08-09 13:13:49 +0800

    Use ResponseError for sql-debug-mutation

--- studio/data/ai/sql-debug-mutation.ts
+++ studio/data/ai/sql-debug-mutation.ts
@@ -1,6 +1,7 @@
 import { useMutation, UseMutationOptions } from '@tanstack/react-query'
 import { isResponseOk, post } from 'lib/common/fetch'
 import { BASE_PATH } from 'lib/constants'
+import { ResponseError } from 'types'
 
 export type SqlDebugResponse = {
   solution: string
@@ -32,8 +33,8 @@ type SqlDebugData = Awaited<ReturnType<typeof debugSql>>
 export const useSqlDebugMutation = ({
   onSuccess,
   ...options
-}: Omit<UseMutationOptions<SqlDebugData, unknown, SqlDebugVariables>, 'mutationFn'> = {}) => {
-  return useMutation<SqlDebugData, unknown, SqlDebugVariables>((vars) => debugSql(vars), {
+}: Omit<UseMutationOptions<SqlDebugData, ResponseError, SqlDebugVariables>, 'mutationFn'> = {}) => {
+  return useMutation<SqlDebugData, ResponseError, SqlDebugVariables>((vars) => debugSql(vars), {
     async onSuccess(data, variables, context) {
       await onSuccess?.(data, variables, context)
     },
