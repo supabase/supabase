6630c49d25ceacf5a4715768c44fc6561d858147
Joshen Lim <joshenlimek@gmail.com>
2023-08-10 23:18:13 +0800

    midway converting invoices to RQ

--- studio/data/invoices/keys.ts
+++ studio/data/invoices/keys.ts
@@ -1,5 +1,9 @@
 export const invoicesKeys = {
   overdueInvoices: () => ['invoices', 'overdue'] as const,
+  invoice: (id: string | undefined) => ['invoices', id] as const,
+  projectInvoices: (projectRef: string | undefined) => ['invoices', projectRef] as const,
+  projectInvoicesCount: (projectRef: string | undefined) =>
+    ['invoices', projectRef, 'count'] as const,
   upcomingPreview: (projectRef: string | undefined) =>
     ['invoices', projectRef, 'upcoming-preview'] as const,
   orgUpcomingPreview: (projectRef: string | undefined) =>
