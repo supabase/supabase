93b947e83f42c989ebb69727b0859c932512b8ad
Bobbie Soedirgo <bobbie@soedirgo.dev>
2023-08-17 19:37:12 +0800

    fix: more naming of lw keys

--- apps/www/pages/launch-week/tickets/index.tsx
+++ apps/www/pages/launch-week/tickets/index.tsx
@@ -23,7 +23,7 @@ interface Props {
 
 const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL ?? 'http://localhost:54321',
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
 
 const generateOgs = async (users: UserData[]) => {
