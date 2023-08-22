34f91d6e195e30bb7638d4e0e60b10f87d3c4591
Furqan Khan <furqankhan5403@gmail.com>
2023-08-22 18:16:38 +0530

    Added new twitter logo (X) at the home page

--- apps/www/components/Footer/index.tsx
+++ apps/www/components/Footer/index.tsx
@@ -52,7 +52,17 @@ const Footer = (props: Props) => {
                 className="text-scale-900 hover:text-scale-1200 transition"
               >
                 <span className="sr-only">Twitter</span>
-                <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 45 45" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100">
+                <svg
+                  className="h-6 w-6"
+                  fill="currentColor"
+                  viewBox="0 0 45 45"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  x="0px"
+                  y="0px"
+                  width="100"
+                  height="100"
+                >
                   <path d="M 11 4 C 7.134 4 4 7.134 4 11 L 4 39 C 4 42.866 7.134 46 11 46 L 39 46 C 42.866 46 46 42.866 46 39 L 46 11 C 46 7.134 42.866 4 39 4 L 11 4 z M 13.085938 13 L 21.023438 13 L 26.660156 21.009766 L 33.5 13 L 36 13 L 27.789062 22.613281 L 37.914062 37 L 29.978516 37 L 23.4375 27.707031 L 15.5 37 L 13 37 L 22.308594 26.103516 L 13.085938 13 z M 16.914062 15 L 31.021484 35 L 34.085938 35 L 19.978516 15 L 16.914062 15 z"></path>
                 </svg>
               </a>
@@ -149,7 +159,7 @@ const Footer = (props: Props) => {
         </div>
         <div className="border-scale-500 dark:border-scale-600 mt-32 flex justify-between border-t pt-8">
           <small className="small">&copy; Supabase Inc</small>
-          <DarkModeToggle disabled = {isLaunchWeekPage} />
+          <DarkModeToggle disabled={isLaunchWeekPage} />
         </div>
       </SectionContainer>
     </footer>
