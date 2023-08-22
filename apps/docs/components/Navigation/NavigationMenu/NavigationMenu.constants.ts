1a40b38610ed4937927d97d3bbbe1607fa0e1918
Andrew Smith <a.smith@silentworks.co.uk>
2023-08-22 03:01:25 +0000

    Add email based auth with PKCE flow

--- apps/docs/components/Navigation/NavigationMenu/NavigationMenu.constants.ts
+++ apps/docs/components/Navigation/NavigationMenu/NavigationMenu.constants.ts
@@ -481,6 +481,10 @@ export const auth = {
         },
         { name: 'Password Reset', url: '/guides/auth/auth-password-reset' },
         { name: 'Email Templates', url: '/guides/auth/auth-email-templates' },
+        {
+          name: 'Email Auth with PKCE flow for SSR',
+          url: '/guides/auth/email-based-auth-with-pkce-flow-for-ssr',
+        },
       ],
     },
     {
