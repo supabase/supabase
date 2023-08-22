794ec1e809e06786e9be988e8201e5c9a473fde1
Aakash Rajbhar <akash.kumarajbhar@gmail.com>
2023-08-22 22:05:13 +0530

    Changed the twitter icon to new X icon in the footer

--- apps/www/components/Footer/index.tsx
+++ apps/www/components/Footer/index.tsx
@@ -47,15 +47,23 @@ const Footer = (props: Props) => {
               </a>
             </Link>
             <div className="flex space-x-5">
-              <a
-                href="https://twitter.com/supabase"
-                className="text-scale-900 hover:text-scale-1200 transition"
-              >
-                <span className="sr-only">Twitter</span>
-                <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                  <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+                    <a
+                      href="https://twitter.com/supabase"
+                      className="text-scale-900 hover:text-scale-1200 transition"
+        >
+                      <span className="sr-only">Twitter</span>
+                      <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true" >
+                        <path
+                        
+                          d="M19.25 6.75L6.75 19.25M6.75 6.75L19.25 19.25"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
                 </svg>
               </a>
+
 
               <a
                 href="https://github.com/supabase"
