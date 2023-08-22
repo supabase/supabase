1cb8b9e9bb1d0984dc3f17ff217e30e382bb7adf
Bobbie Soedirgo <bobbie@soedirgo.dev>
2023-08-22 17:38:51 +0800

    docs: using custom schemas

--- apps/docs/components/Navigation/NavigationMenu/NavigationMenu.constants.ts
+++ apps/docs/components/Navigation/NavigationMenu/NavigationMenu.constants.ts
@@ -737,6 +737,7 @@ export const api: NavMenuConstant = {
           url: '/guides/api/joins-and-nesting',
           items: [],
         },
+        { name: 'Using custom schemas', url: '/guides/api/using-custom-schemas', items: [] },
       ],
     },
     {
