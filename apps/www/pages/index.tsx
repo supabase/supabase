5a0b8d08ad5679ee1a096ea5f01bd7d6e6f32552
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-08-24 16:38:33 +0300

    fix import

--- apps/www/pages/index.tsx
+++ apps/www/pages/index.tsx
@@ -11,7 +11,7 @@ import Hero from '~/components/Hero/Hero'
 // Import Swiper styles if swiper used on page
 import 'swiper/swiper.min.css'
 
-const Products = dynamic(() => import('~/components/Products/index'))
+const Products = dynamic(() => import('~/components/ProductsSection/index'))
 const BackedBy = dynamic(() => import('~/components/BackedBy'))
 const BuiltExamples = dynamic(() => import('components/BuiltWithSupabase/index'))
 const MadeForDevelopers = dynamic(() => import('components/MadeForDevelopers/index'))
