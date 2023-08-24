0427d836f3d6f41586209f132dcb469cb4b0081f
Joshen Lim <joshenlimek@gmail.com>
2023-08-24 11:44:48 +0800

    Fix indexes query

--- studio/data/database/indexes-query.ts
+++ studio/data/database/indexes-query.ts
@@ -17,10 +17,8 @@ export const getIndexesQuery = ({ schema }: { schema: string }) => {
 SELECT schemaname as "schema",
   tablename as "table",
   indexname as "name",
-  indexdef as "definition",
-  indisvalid as "enabled"
-FROM pg_indexes a
-  LEFT JOIN pg_index b ON b.indexrelid = a.indexname::regclass
+  indexdef as "definition"
+FROM pg_indexes
 WHERE schemaname = '${schema}';
 `.trim()
 
