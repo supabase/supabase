d5a50cb89ce27a3e042e0a4984527e662bc6ec3a
kishanhitk <kishansharma1231@gmail.com>
2023-08-25 22:27:30 +0530

    fix: `confirmAlert` not dismissing.

--- studio/components/interfaces/Auth/Users/UserDropdown.tsx
+++ studio/components/interfaces/Auth/Users/UserDropdown.tsx
@@ -82,8 +82,7 @@ const UserDropdown = ({ user, canRemoveUser, canRemoveMFAFactors }: UserDropdown
           ui.setNotification({ category: 'success', message: `Successfully deleted ${user.email}` })
           PageState.users = PageState.users.filter((x: any) => x.id != user.id)
           PageState.totalUsers -= 1
-        } finally {
-        }
+        } catch (error) {}
       },
     })
   }
