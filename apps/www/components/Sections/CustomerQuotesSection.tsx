82cdab7407150dc0984c45735bd72c01405c957d
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-08-25 13:24:53 +0300

    restore folder name

--- apps/www/components/Sections/CustomerQuotesSection.tsx
+++ apps/www/components/Sections/CustomerQuotesSection.tsx
@@ -7,7 +7,7 @@ import { IconArrowUpRight } from 'ui'
 import { LazyMotion, domAnimation, m, useInView } from 'framer-motion'
 
 import SectionContainer from '~/components/Layouts/SectionContainer'
-import CustomersVisual from '~/components/ProductsSection/VectorAI/CustomersVisual'
+import CustomersVisual from '~/components/Products/VectorAI/CustomersVisual'
 import { INITIAL_BOTTOM, getAnimation } from '~/lib/animations'
 
 interface Customer {
--- apps/www/data/products/vector/pageData.tsx
+++ apps/www/data/products/vector/pageData.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link'
-import IntegrationsImage from '~/components/ProductsSection/VectorAI/IntegrationsImage'
-import OpenAIImage from '~/components/ProductsSection/VectorAI/OpenAIImage'
-import SecureAndScalableImg from '~/components/ProductsSection/VectorAI/SecureAndScalableImg'
-import PGvectorImg from '~/components/ProductsSection/VectorAI/PGvectorImg'
-import DeployGlobally from '~/components/ProductsSection/VectorAI/DeployGlobally'
+import IntegrationsImage from '~/components/Products/VectorAI/IntegrationsImage'
+import OpenAIImage from '~/components/Products/VectorAI/OpenAIImage'
+import SecureAndScalableImg from '~/components/Products/VectorAI/SecureAndScalableImg'
+import PGvectorImg from '~/components/Products/VectorAI/PGvectorImg'
+import DeployGlobally from '~/components/Products/VectorAI/DeployGlobally'
 
 export default {
   metaTitle: '',
