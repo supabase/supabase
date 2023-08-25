f984bf2984495a8e6c0aafff20e440951c5b4339
Ivan Vasilov <vasilov.ivan@gmail.com>
2023-08-25 14:42:05 +0200

    Add a page for signing in via MFA.

--- studio/data/profile/keys.ts
+++ studio/data/profile/keys.ts
@@ -1,4 +1,5 @@
 export const profileKeys = {
   profile: () => ['profile'] as const,
   mfaFactors: () => ['mfa', 'factors'] as const,
+  aaLevel: () => ['mfa', 'aaLevel'] as const,
 }
---
+++ studio/pages/sign-in-mfa.tsx
@@ -0,0 +1,25 @@
+import SignInMfaForm from 'components/interfaces/SignIn/SignInMfaForm'
+import { SignInLayout } from 'components/layouts'
+import { NextPageWithLayout } from 'types'
+
+const SignInMfaPage: NextPageWithLayout = () => {
+  return (
+    <>
+      <div className="flex flex-col gap-5">
+        <SignInMfaForm />
+      </div>
+    </>
+  )
+}
+
+SignInMfaPage.getLayout = (page) => (
+  <SignInLayout
+    heading="Two-factor authentication"
+    subheading="Enter the authentication code from your  two-factor authentication (TOTP) app"
+    logoLinkToMarketingSite={true}
+  >
+    {page}
+  </SignInLayout>
+)
+
+export default SignInMfaPage
