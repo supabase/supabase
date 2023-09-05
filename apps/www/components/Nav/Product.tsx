e5dd9fc60d4e655a471f90b18aef143de93a09c7
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-09-05 20:08:24 +0200

    add customer stories in products nav dropdown

--- apps/www/components/Nav/Product.tsx
+++ apps/www/components/Nav/Product.tsx
@@ -3,12 +3,15 @@ import { ListItem } from '.'
 
 import SolutionsData from 'data/Solutions'
 import ComparisonsData from 'data/Comparisons'
+import CustomersData from 'data/CustomerStories'
 import { TextLink } from 'ui'
+
+const aside = [CustomersData, ComparisonsData]
 
 const Product = () => (
   <>
     {/* <ul className="grid gap-2 p-2 xl:grid-cols-2 w-[300px] xl:w-[600px] "> */}
-    <ul className="grid gap-2 p-2 2xl:grid-cols-2 w-[300px] 2xl:w-[600px] ">
+    <ul className="grid gap-2 p-2 w-[300px]">
       {Object.values(SolutionsData).map((component) => (
         <ListItem
           key={component.name}
@@ -19,18 +22,38 @@ const Product = () => (
         />
       ))}
     </ul>
-    <ul className="bg-alternative flex flex-col p-4 w-[250px]">
-      <label className="text-muted text-xs font-mono mb-1">{ComparisonsData.label}</label>
-      {ComparisonsData.comparisons.map((link) => (
-        <li key={link.text}>
-          <TextLink
-            url={link.url}
-            label={link.text}
-            className="focus-visible:ring-offset-4 focus-visible:ring-offset-background-overlay"
-          />
-        </li>
-      ))}
-    </ul>
+    <div className="bg-alternative flex flex-col gap-6 p-4 w-[250px]">
+      <div>
+        <p className="text-muted text-xs font-mono mb-1 block">Customer Stories</p>
+        <ul>
+          {CustomersData.slice(0, 3).map((customer) => (
+            <li key={customer.organization}>
+              <TextLink
+                hasChevron={false}
+                url={customer.url}
+                label={customer.organization}
+                className="focus-visible:ring-offset-4 focus-visible:ring-offset-background-overlay"
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <p className="text-muted text-xs font-mono mb-1">{ComparisonsData.label}</p>
+        <ul>
+          {ComparisonsData.comparisons.map((link) => (
+            <li key={link.text}>
+              <TextLink
+                hasChevron={false}
+                url={link.url}
+                label={link.text}
+                className="focus-visible:ring-offset-4 focus-visible:ring-offset-background-overlay"
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   </>
 )
 
