3d37d41612741174b7678fd1a61ade883f14139d
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-09-07 11:53:21 +0200

    improve spacing in product nav dropdown

--- apps/www/components/Nav/Product.tsx
+++ apps/www/components/Nav/Product.tsx
@@ -23,34 +23,39 @@ const Product = () => (
     </ul>
     <div className="bg-alternative flex flex-col gap-6 p-4 w-[250px]">
       <div>
-        <p className="text-muted text-xs uppercase tracking-widest font-mono mb-1 block">
+        <p className="text-muted text-xs uppercase tracking-widest font-mono mb-2 block">
           Customer Stories
         </p>
-        <ul>
+        <ul className="flex flex-col gap-2">
           {CustomersData.slice(0, 3).map((customer) => (
             <li key={customer.organization}>
               <TextLink
                 hasChevron={false}
                 url={customer.url}
                 label={customer.organization}
-                className="focus-visible:ring-offset-4 focus-visible:ring-offset-background-overlay"
+                className="mt-0 focus-visible:ring-offset-4 focus-visible:ring-offset-background-overlay"
               />
             </li>
           ))}
         </ul>
+        <TextLink
+          url="/customers"
+          label="View all"
+          className="text-lighter text-xs focus-visible:ring-offset-4 focus-visible:ring-offset-background-overlay"
+        />
       </div>
       <div>
-        <p className="text-muted text-xs uppercase tracking-widest font-mono mb-1">
+        <p className="text-muted text-xs uppercase tracking-widest font-mono mb-2">
           {ComparisonsData.label}
         </p>
-        <ul>
+        <ul className="flex flex-col gap-2">
           {ComparisonsData.comparisons.map((link) => (
             <li key={link.text}>
               <TextLink
                 hasChevron={false}
                 url={link.url}
                 label={link.text}
-                className="focus-visible:ring-offset-4 focus-visible:ring-offset-background-overlay"
+                className="mt-0 focus-visible:ring-offset-4 focus-visible:ring-offset-background-overlay"
               />
             </li>
           ))}
