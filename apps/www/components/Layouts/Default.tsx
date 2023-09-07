f9f7013b9b75375197dc90f88c93bd13d7b4e92e
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-09-07 16:56:25 +0200

    try nav dropdown with more spacing

--- apps/www/components/Layouts/Default.tsx
+++ apps/www/components/Layouts/Default.tsx
@@ -1,11 +1,13 @@
 import { useEffect } from 'react'
 import Nav from 'components/Nav/index'
 import Footer from 'components/Footer/index'
+import PostTypes from '../../types/post'
 
 type Props = {
   hideHeader?: boolean
   hideFooter?: boolean
   className?: string
+  blogPosts?: PostTypes[]
   footerClassName?: string
   children: React.ReactNode
 }
@@ -21,7 +23,7 @@ const DefaultLayout = (props: Props) => {
 
   return (
     <>
-      {!hideHeader && <Nav />}
+      {!hideHeader && <Nav blogPosts={props.blogPosts} />}
       <div className="min-h-screen">
         <main className={className}>{children}</main>
       </div>
--- apps/www/components/Nav/MenuItem.tsx
+++ apps/www/components/Nav/MenuItem.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
 import { cn } from 'ui'
-import ProductIcon from '../ProductIcon'
 
 const MenuItem = React.forwardRef<
   React.ElementRef<'a'>,
@@ -12,18 +11,43 @@ const MenuItem = React.forwardRef<
       <a
         ref={ref}
         className={cn(
-          'group flex flex-row select-none space-x-2 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-overlay-hover focus-visible:ring-2 focus-visible:ring-foreground-lighter focus-visible:text-foreground-strong',
+          'group flex flex-row items-center select-none gap-3 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-overlay-hover focus-visible:ring-2 focus-visible:ring-foreground-lighter focus-visible:text-foreground-strong',
+          description && 'items-start',
           className
         )}
         {...props}
       >
         {children ?? (
           <>
-            {icon && <ProductIcon icon={icon} color="alt" />}
+            {/* {icon && <ProductIcon icon={icon} color="alt" />} */}
+            {icon && (
+              <div
+                className={[
+                  // 'inline-flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-md bg-alternative text-bg',
+                  // color && color === 'alt' ? 'bg-surface-300 text-brand' : '',
+                ].join(' ')}
+              >
+                <svg
+                  className="h-5 w-5 text-brand"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="1.5"
+                    d={icon}
+                    stroke="currentColor"
+                  />
+                </svg>
+              </div>
+            )}
             <div className="flex flex-col gap-1">
-              <div className="text-sm font-medium leading-none">{title}</div>
+              <div className={cn('leading-none', description && 'mt-0.5')}>{title}</div>
               {description && (
-                <p className="line-clamp-2 text-sm leading-snug text-light">{description}</p>
+                <p className="line-clamp-1 text-sm leading-snug text-lighter">{description}</p>
               )}
             </div>
           </>
