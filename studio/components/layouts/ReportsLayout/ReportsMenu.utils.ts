a607adaa1a7faa2a11149cd042706bf86cb19350
TzeYiing <ty@tzeyiing.com>
2023-09-09 04:09:54 +0800

    feat: storage report cache misses

--- studio/components/layouts/ReportsLayout/ReportsMenu.utils.ts
+++ studio/components/layouts/ReportsLayout/ReportsMenu.utils.ts
@@ -1,9 +1,10 @@
 import { Project } from 'types'
 import { ProductMenuGroup } from 'components/ui/ProductMenu/ProductMenu.types'
+import { useFlag } from 'hooks'
 
 export const generateReportsMenu = (project?: Project): ProductMenuGroup[] => {
   const ref = project?.ref ?? 'default'
-
+  const storageReportFlag = useFlag('storageReport')
   return [
     {
       title: '',
@@ -25,6 +26,17 @@ export const generateReportsMenu = (project?: Project): ProductMenuGroup[] => {
           items: [],
           label: 'NEW',
         },
+        ...(storageReportFlag
+          ? [
+              {
+                name: 'Storage',
+                key: 'storage',
+                url: `/project/${ref}/reports/storage`,
+                items: [],
+                label: 'NEW',
+              },
+            ]
+          : []),
         {
           name: 'Database',
           key: 'database',
