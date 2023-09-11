41511a9a9408aea945a0cb4a3c3c0caf38e01180
Joshen Lim <joshenlimek@gmail.com>
2023-09-11 17:21:09 +0800

    Improve empty state by adding import CSV cta for empty table in table editor

--- studio/components/grid/SupabaseGrid.tsx
+++ studio/components/grid/SupabaseGrid.tsx
@@ -187,6 +187,7 @@ const SupabaseGridLayout = forwardRef<SupabaseGridRef, SupabaseGridProps>((props
             {...gridProps}
             rows={data?.rows ?? []}
             updateRow={updateTableRow}
+            onImportData={onImportData}
             onEditForeignKeyColumnValue={onEditForeignKeyColumnValue}
           />
           <Footer isLoading={isLoading || isRefetching} />
