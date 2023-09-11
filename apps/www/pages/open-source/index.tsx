2427191431a58f2de82324eafe54619bb9871c58
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-09-11 16:41:24 +0200

    improve oss component

--- apps/www/pages/open-source/index.tsx
+++ apps/www/pages/open-source/index.tsx
@@ -11,10 +11,11 @@ import ProductHeaderCentered from '../../components/Sections/ProductHeaderCenter
 import pageData from '~/data/open-source'
 import Repos from '../../components/OpenSource/Repos'
 
+// Import Swiper styles if swiper used on page
+import 'swiper/swiper.min.css'
+
 const OpenSource = () => {
   const octokit = new Octokit()
-
-  const [activePill, setActivePill] = useState('All')
   const [repos, setRepos] = useState([{}])
 
   const maintainerTags = maintainers
@@ -62,108 +63,11 @@ const OpenSource = () => {
       />
       <DefaultLayout>
         <SectionContainer className="overflow-hidden pt-8 pb-12 md:pt-12">
-          <ProductHeaderCentered {...pageData.heroSection} />
+          <ProductHeaderCentered className="-mb-14" {...pageData.heroSection} />
         </SectionContainer>
         <SectionContainer className="!pt-0">
           <Repos repos={repos} tabs={pageData.repo_tabs} title="Yooo" paragraph="asfd" />
         </SectionContainer>
-        {/* <div className="text-scale-1200 container mx-auto">
-          <SectionContainer>
-            <div className="container">
-              <div className="flex items-center mb-16">
-                <div className="col">
-                  <h1 className="mb-10 text-4xl font-medium">Open source</h1>
-                  <p className="max-w-xl">
-                    Supabase is an open source company, supporting existing open source tools and
-                    communities wherever possible.
-                  </p>
-                </div>
-              </div>
-              <h2 className="mb-6 text-2xl font-medium">Sponsors</h2>
-              <Sponsors />
-            </div>
-          </SectionContainer> */}
-
-        {/* <SectionContainer>
-            <div className="">
-              <h2 className="mb-6 text-2xl font-medium">Community Maintainers</h2>
-
-              <div className="overflow-auto md:max-w-none hidden sm:block">
-                <ul className="flex 2xl:gap-4 items-center p-0">
-                  {maintainerPills.map((x) => (
-                    <li
-                      key={x}
-                      className={`mx-4 rounded-t-lg inline-block p-2 cursor-pointer hover:text-brand-300 ${
-                        activePill == x ? 'bg-gray-200 dark:bg-gray-400 text-brand-300' : ''
-                      }`}
-                      onClick={() => setActivePill(x)}
-                    >
-                      {x}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <div className="grid lg:grid-cols-2 gap-4">
-                {maintainers
-                  .filter((x) => activePill == 'All' || x.tags?.includes(activePill))
-                  .sort((a, b) => a.handle.localeCompare(b.handle))
-                  .map((x, idx) => (
-                    <div className="" key={idx}>
-                      <a
-                        className="flex gap-4"
-                        href={`https://github.com/${x.handle}`}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        <div className="grow bg-gray-300 dark:bg-gray-400 p-4 rounded-lg">
-                          <div className="flex gap-4 shrink-0">
-                            <Image
-                              className="rounded-full my-0"
-                              width={50}
-                              height={50}
-                              layout="fixed"
-                              alt={x.handle}
-                              src={`https://github.com/${x.handle}.png`}
-                            />
-                            <div className="">
-                              <h4 className="text-lg my-0">@{x.handle}</h4>
-                              <span className="text-sm">{x.description}</span>
-                            </div>
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                  ))}
-              </div>
-            </div>
-          </SectionContainer>
-
-          <SectionContainer>
-            <div>
-              <h2 className="mb-6 text-2xl font-medium">Repositories</h2>
-              <div className="grid gap-4">
-                {repos.length < 1 && <div></div>}
-                {repos.length >= 1 &&
-                  repos.map((props: any, idx: any) => (
-                    <div className="bg-gray-300 dark:bg-gray-400 p-4 rounded-lg" key={idx}>
-                      <a className="h-full" href={props.html_url}>
-                        <div className="card__body">
-                          <h4 className="uppercase my-2 font-semibold">{props.name}</h4>
-                          <span className="text-sm">{props.description}</span>
-                        </div>
-                        <hr className="my-2" />
-                        <div className="flex justify-between py-2 text-xs">
-                          <div>@{props.full_name}</div>
-                          <div>{props.stargazers_count} ★</div>
-                        </div>
-                      </a>
-                    </div>
-                  ))}
-              </div>
-            </div>
-          </SectionContainer>
-        </div>
-      */}
       </DefaultLayout>
     </>
   )
