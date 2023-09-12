8903e9088044dde6ece5f3f6614d7b73b621baf6
Ivan Vasilov <vasilov.ivan@gmail.com>
2023-09-12 12:42:10 +0200

    Another bump.

--- studio/jest.config.js
+++ studio/jest.config.js
@@ -4,8 +4,8 @@ module.exports = {
   setupFiles: ['jest-canvas-mock', './tests/setup/radix'],
   testEnvironment: 'jsdom',
   testTimeout: 10000,
-  testRegex: '(.*\\.test.(js|jsx|ts|tsx)$)',
   transform: { '^.+\\.(t|j)sx?$': 'ts-jest' },
+  testRegex: '(.*\\.test.(js|jsx|ts|tsx)$)',
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.test.json',
