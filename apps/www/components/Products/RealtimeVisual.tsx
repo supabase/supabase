aeb0d853f1e1ac8a4315ed56c69fecbb168f9fb1
Filsommer <filsommer@hotmail.com>
2023-09-18 19:30:37 +0100

    adds dynamic cursor to Realtime product card

--- apps/www/components/Products/RealtimeVisual.tsx
+++ apps/www/components/Products/RealtimeVisual.tsx
@@ -1,7 +1,29 @@
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import classNames from 'classnames'
+
+function getRandomAxis(prevIteration: number) {
+  const possibleXValues = [0, 1, 4]
+  const possibleYValues = [0, 1]
+  return {
+    x: possibleXValues[Math.floor(Math.random() * possibleXValues.length)],
+    y: possibleYValues[Math.floor(Math.random() * possibleYValues.length)],
+    iteration: prevIteration + 1,
+  }
+}
+const defaultInteractionString = `group-hover:translate-x-0 group-hover:translate-y-0`
 
 const RealtimeVisual = () => {
+  const [axis, setAxis] = useState({ x: 8, y: 2, iteration: 0 })
+  const dynamicTransformString = `group-hover:translate-x-${axis.x} group-hover:translate-y-${axis.y}`
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setAxis((prev) => getRandomAxis(prev.iteration))
+    }, 1500)
+    return () => clearInterval(interval)
+  }, [])
+
   return (
     <figure
       className="absolute inset-0 z-0 overflow-hidden"
@@ -14,7 +36,11 @@ const RealtimeVisual = () => {
       <img
         src="/images/index/products/realtime-user-cursor.svg"
         alt="user cursor"
-        className="absolute w-6 h-6 z-10 left-14 bottom-[100px] transition translate-x-8 translate-y-2 group-hover:translate-x-0 group-hover:translate-y-0 !duration-300 will-change-transform"
+        className={classNames(
+          `absolute w-6 h-6 z-10 left-14 bottom-[100px] transition translate-x-7 translate-y-2 ${
+            axis.iteration > 1 ? dynamicTransformString : defaultInteractionString
+          } !duration-300 will-change-transform `
+        )}
       />
       <img
         src="/images/index/products/realtime-user-cursor.svg"
