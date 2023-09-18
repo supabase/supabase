39e8198aec8ff11ca6b6880f7d9a1fc0d9c392f6
Richie McIlroy <33632126+richiemcilroy@users.noreply.github.com>
2023-09-18 12:15:01 +0100

    Fix text color in editor

--- studio/components/grid/components/grid/Grid.tsx
+++ studio/components/grid/components/grid/Grid.tsx
@@ -181,7 +181,7 @@ export const Grid = memo(
                         style={{ height: `calc(100% - 35px)` }}
                         className="flex flex-col items-center justify-center"
                       >
-                        <p className="text-sm text">This table is empty</p>
+                        <p className="text-sm text-light">This table is empty</p>
                         {onAddRow !== undefined && onImportData !== undefined && (
                           <>
                             <p className="text-sm text-light mt-1">
