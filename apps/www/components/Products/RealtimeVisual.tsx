5ff345b34e59cb7246d8853a071e1406dc138c76
Filsommer <filsommer@hotmail.com>
2023-09-20 10:52:59 +0100

    makes realtime cursors more human-like

--- apps/www/components/Products/RealtimeVisual.tsx
+++ apps/www/components/Products/RealtimeVisual.tsx
@@ -1,32 +1,78 @@
 import Image from 'next/image'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import classNames from 'classnames'
 
-function getRandomAxis(prevIteration: number) {
-  const possibleXValues = [0, 1, 4]
-  const possibleYValues = [0, 1]
+const CURSOR1_INTERVAL_MS = 900
+const CURSOR2_INTERVAL_MS = 1350
+
+function getRandomAxis() {
+  const possibleXValues = [-1, 1, 2, 3]
+  const possibleYValues = [-2, -1, 0.5, 1]
   return {
     x: possibleXValues[Math.floor(Math.random() * possibleXValues.length)],
     y: possibleYValues[Math.floor(Math.random() * possibleYValues.length)],
-    iteration: prevIteration + 1,
   }
 }
-const defaultInteractionString = `group-hover:translate-x-0 group-hover:translate-y-0`
 
 const RealtimeVisual = () => {
-  const [axis, setAxis] = useState({ x: 8, y: 2, iteration: 0 })
-  const dynamicTransformString = `group-hover:translate-x-${axis.x} group-hover:translate-y-${axis.y}`
+  const containerRef = useRef(null)
+  const [isContainerHovered, setIsContainerHovered] = useState(false)
+  const [axis1, setAxis1] = useState({ x: 2, y: 1 })
+  const [axis2, setAxis2] = useState({ x: 1, y: 0 })
+
+  const handleCursors = (event: MouseEvent) => {
+    if (!containerRef.current) return null
+
+    const containerRefElement = containerRef.current as HTMLDivElement
+
+    const {
+      x: contX,
+      y: contY,
+      width: containerWidth,
+      height: containerHeight,
+    } = containerRefElement.getBoundingClientRect()
+    const xCont = event.clientX - contX
+    const yCont = event.clientY - contY
+
+    const isHovered =
+      xCont > -3 && xCont < containerWidth + 3 && yCont > -3 && yCont < containerHeight + 3
+    setIsContainerHovered(isHovered)
+
+    if (!isHovered) return
+  }
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setAxis((prev) => getRandomAxis(prev.iteration))
-    }, 1500)
-    return () => clearInterval(interval)
+    if (typeof window === 'undefined') return
+
+    window.addEventListener('mousemove', handleCursors)
+    return () => {
+      window.removeEventListener('mousemove', handleCursors)
+    }
   }, [])
+
+  useEffect(() => {
+    let intervalId1: NodeJS.Timer
+    let intervalId2: NodeJS.Timer
+    // Only update cursor positions (via setInterval) if container is hovered
+    if (isContainerHovered) {
+      intervalId1 = setInterval(() => {
+        setAxis1(getRandomAxis())
+      }, CURSOR1_INTERVAL_MS)
+      intervalId2 = setInterval(() => {
+        setAxis2(getRandomAxis())
+      }, CURSOR2_INTERVAL_MS)
+    }
+    return () => {
+      // Clear the intervals when the component unmounts or is updated
+      clearInterval(intervalId1)
+      clearInterval(intervalId2)
+    }
+  }, [isContainerHovered])
 
   return (
     <figure
       className="absolute inset-0 z-0 overflow-hidden"
+      ref={containerRef}
       style={{
         background: 'radial-gradient(320px 320px at 30% 100%, #85E0B740, transparent)',
       }}
@@ -36,15 +82,15 @@ const RealtimeVisual = () => {
       <img
         src="/images/index/products/realtime-user-cursor.svg"
         alt="user cursor"
+        style={isContainerHovered ? { transform: `translate(${axis1.x}rem, ${axis1.y}rem)` } : {}}
         className={classNames(
-          `absolute w-6 h-6 z-10 left-14 bottom-[100px] transition translate-x-7 translate-y-2 ${
-            axis.iteration > 1 ? dynamicTransformString : defaultInteractionString
-          } !duration-300 will-change-transform `
+          `absolute w-6 h-6 z-10 left-14 bottom-[100px] transition translate-x-7 translate-y-2 !duration-300 will-change-transform `
         )}
       />
       <img
         src="/images/index/products/realtime-user-cursor.svg"
         alt="user cursor"
+        style={isContainerHovered ? { transform: `translate(${axis2.x}rem, ${axis2.y}rem)` } : {}}
         className="absolute w-6 h-6 z-10 right-[40%] bottom-3 transition translate-x-80 group-hover:translate-x-0 !duration-700 will-change-transform"
       />
       <Image
