7b14e2308b8f621958d21c5dbf252f5256e5f055
Joshen Lim <joshenlimek@gmail.com>
2023-09-25 13:06:07 +0800

    Midway adding queries for service statues

---
+++ studio/data/service-status/keys.ts
@@ -0,0 +1,14 @@
+export const serviceStatusKeys = {
+  postgres: (projectRef: string | undefined) =>
+    ['projects', projectRef, 'service-status', 'postgres'] as const,
+  postgrest: (projectRef: string | undefined) =>
+    ['projects', projectRef, 'service-status', 'postgrest'] as const,
+  storage: (projectRef: string | undefined) =>
+    ['projects', projectRef, 'service-status', 'storage'] as const,
+  realtime: (projectRef: string | undefined) =>
+    ['projects', projectRef, 'service-status', 'realtime'] as const,
+  auth: (projectRef: string | undefined) =>
+    ['projects', projectRef, 'service-status', 'auth'] as const,
+  supavisor: (projectRef: string | undefined) =>
+    ['projects', projectRef, 'service-status', 'supavisor'] as const,
+}
---
+++ studio/data/service-status/postgrest-service-status-query.ts
@@ -0,0 +1,42 @@
+import { useQuery, UseQueryOptions } from '@tanstack/react-query'
+
+import { get } from 'data/fetchers'
+import { ResponseError } from 'types'
+import { serviceStatusKeys } from './keys'
+
+export type PostgrestServiceStatusVariables = {
+  projectRef?: string
+}
+
+export async function getPostgrestServiceStatus(
+  { projectRef }: PostgrestServiceStatusVariables,
+  signal?: AbortSignal
+) {
+  if (!projectRef) throw new Error('projectRef is required')
+
+  const { data, error } = await get(`/platform/projects/{ref}/live`, {
+    params: { path: { ref: projectRef } },
+    signal,
+  })
+  if (error) throw error
+  return data
+}
+
+export type PostgrestServiceStatusData = Awaited<ReturnType<typeof getPostgrestServiceStatus>>
+export type PostgrestServiceStatusError = ResponseError
+
+export const usePostgrestServiceStatusQuery = <TData = PostgrestServiceStatusData>(
+  { projectRef }: PostgrestServiceStatusVariables,
+  {
+    enabled = true,
+    ...options
+  }: UseQueryOptions<PostgrestServiceStatusData, PostgrestServiceStatusError, TData> = {}
+) =>
+  useQuery<PostgrestServiceStatusData, PostgrestServiceStatusError, TData>(
+    serviceStatusKeys.postgrest(projectRef),
+    ({ signal }) => getPostgrestServiceStatus({ projectRef }, signal),
+    {
+      enabled: enabled && typeof projectRef !== 'undefined',
+      ...options,
+    }
+  )
