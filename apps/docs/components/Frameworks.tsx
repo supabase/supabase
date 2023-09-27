d3526f9968c7ee6d2a8496dcc2329f8e53537e1c
Terry Sutton <saltcod@gmail.com>
2023-09-27 16:19:41 -0230

    Use resolved theme (#17753)
    * Use resolved theme
    
    * Update

--- apps/docs/components/Frameworks.tsx
+++ apps/docs/components/Frameworks.tsx
@@ -2,7 +2,7 @@ import ButtonCard from './ButtonCard'
 import { useTheme } from 'next-themes'
 
 const Frameworks = () => {
-  const { theme } = useTheme()
+  const { resolvedTheme } = useTheme()
 
   const frameworks = [
     {
@@ -110,8 +110,7 @@ const Frameworks = () => {
             layout="horizontal"
             to={x.href}
             title={x.name}
-            // [Joshen] Nice to have: theming
-            icon={theme === 'dark' ? x.logo.dark : x.logo.light}
+            icon={resolvedTheme === 'dark' ? x.logo.dark : x.logo.light}
           />
         </div>
       ))}
