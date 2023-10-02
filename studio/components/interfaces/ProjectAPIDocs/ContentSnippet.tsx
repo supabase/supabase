d5867d18628b37043ea630fad594505b224be11d
Joshen Lim <joshenlimek@gmail.com>
2023-10-02 14:47:04 +0800

    fix

--- studio/components/interfaces/ProjectAPIDocs/ContentSnippet.tsx
+++ studio/components/interfaces/ProjectAPIDocs/ContentSnippet.tsx
@@ -1,8 +1,8 @@
-import SimpleCodeBlock from 'components/to-be-cleaned/SimpleCodeBlock'
-import { Markdown } from '../Markdown'
-import { useProjectApiQuery } from 'data/config/project-api-query'
 import { useParams } from 'common'
-import { PropsWithChildren } from 'react'
+
+import SimpleCodeBlock from 'components/to-be-cleaned/SimpleCodeBlock'
+import { useProjectApiQuery } from 'data/config/project-api-query'
+import { Markdown } from '../Markdown'
 
 interface ContentSnippetProps {
   useServiceKey?: boolean
@@ -11,7 +11,7 @@ interface ContentSnippetProps {
     key: string
     category: string
     title: string
-    description?: string
+    description: string
     js?: (apikey?: string, endpoint?: string) => string
     bash?: (apikey?: string, endpoint?: string) => string
   }
@@ -21,8 +21,7 @@ const ContentSnippet = ({
   useServiceKey = false,
   selectedLanguage,
   snippet,
-  children,
-}: PropsWithChildren<ContentSnippetProps>) => {
+}: ContentSnippetProps) => {
   const { ref: projectRef } = useParams()
   const { data } = useProjectApiQuery({ projectRef })
 
@@ -35,25 +34,22 @@ const ContentSnippet = ({
     <div id={snippet.key} className="space-y-4 py-6 pb-2 last:pb-6">
       <div className="px-4 space-y-2">
         <h2 className="doc-heading">{snippet.title}</h2>
-        {snippet.description !== undefined && (
-          <div className="doc-section">
-            <article className="text text-sm text-light">
-              <Markdown
-                className="max-w-none"
-                content={snippet.description.replaceAll('[ref]', projectRef ?? '_')}
-              />
-            </article>
-          </div>
-        )}
+        <div className="doc-section">
+          <article className="text text-sm text-light">
+            <Markdown
+              className="max-w-none"
+              content={snippet.description.replaceAll('[ref]', projectRef ?? '_')}
+            />
+          </article>
+        </div>
       </div>
-      {children !== undefined && codeSnippet !== undefined && (
+      {codeSnippet !== undefined && (
         <div className="px-4 codeblock-container">
           <div className="bg rounded p-2">
             <SimpleCodeBlock className={selectedLanguage}>{codeSnippet}</SimpleCodeBlock>
           </div>
         </div>
       )}
-      {children}
     </div>
   )
 }
