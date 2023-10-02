6d8c736f3e0b9cac10a155a016123f261f7ad8db
Alaister Young <a@alaisteryoung.com>
2023-10-02 18:40:47 +1100

    move next-themes dependency

--- packages/common/Providers.tsx
+++ packages/common/Providers.tsx
@@ -2,5 +2,6 @@ import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { type ThemeProviderProps } from 'next-themes/dist/types'
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  // @ts-ignore next-themes is old :/
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
--- packages/common/index.tsx
+++ packages/common/index.tsx
@@ -1,11 +1,5 @@
-import SidebarLayout from './layouts/SidebarLayout'
-
 export * from './Providers'
 export * from './helpers'
 export * from './hooks'
 export * from './gotrue'
 export * from './auth'
-
-export const Layouts = {
-  SidebarLayout,
-}
