872f1967c7ecf34c8ac73875568ced95eac93de0
Terry Sutton <saltcod@gmail.com>
2023-10-02 21:42:01 -0230

    Fix theme switcher issues

--- apps/www/components/AuthWidget/AuthWidgetSection.tsx
+++ apps/www/components/AuthWidget/AuthWidgetSection.tsx
@@ -15,7 +15,7 @@ const supabase = createClient(
 )
 
 function AuthWidgetSection() {
-  const { theme } = useTheme()
+  const { resolvedTheme } = useTheme()
   const { basePath } = useRouter()
   const [radius, setRadius] = useState('4px')
   const [brandColor, setBrandColor] = useState({
@@ -81,10 +81,11 @@ function AuthWidgetSection() {
                 </div>
                 <Auth.UserContextProvider supabaseClient={supabase}>
                   <AuthContainer supabaseClient={supabase}>
+                    aaaaaaaaaaaaaaaaaaaaa
                     <Auth
                       // @ts-ignore
                       socialLayout={layout}
-                      theme={theme === 'dark' ? 'dark' : 'default'}
+                      theme={resolvedTheme === 'dark' ? 'dark' : 'default'}
                       providers={['google', 'facebook', 'twitter']}
                       supabaseClient={supabase}
                       localization={{
