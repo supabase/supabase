eeb7d6f080cb9b0a85abc963789c132255f39735
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-10-04 15:25:31 +0200

    remove fade in on image

--- apps/www/components/Products/VectorAI/IntegrationsImage.tsx
+++ apps/www/components/Products/VectorAI/IntegrationsImage.tsx
@@ -1,20 +1,11 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import Image from 'next/image'
 import { useTheme } from 'next-themes'
-import { LazyMotion, domAnimation, m, useInView } from 'framer-motion'
-import { INITIAL_BOTTOM, getAnimation } from '~/lib/animations'
 import { useBreakpoint } from 'common'
 
 const IntegrationsImage = () => {
   const { resolvedTheme } = useTheme()
   const isMobile = useBreakpoint(767)
-  const ref = useRef(null)
-  const isInView = useInView(ref, { margin: '-25%', once: true })
-
-  const initial = INITIAL_BOTTOM
-  const animate = getAnimation({})
-
-  console.log('ismobile', isMobile)
 
   const image =
     resolvedTheme === 'dark'
@@ -22,21 +13,14 @@ const IntegrationsImage = () => {
       : `/images/product/vector/vector-tools-light${isMobile ? '-mobile' : ''}.svg`
 
   return (
-    <LazyMotion features={domAnimation}>
-      <m.div
-        ref={ref}
-        initial={initial}
-        animate={isInView ? animate : initial}
-        className="relative w-full h-full"
-      >
-        <Image
-          src={image}
-          alt="Diagram of Machine Learning tools that integrate with Supabase Vector"
-          layout="fill"
-          objectFit="contain"
-        />
-      </m.div>
-    </LazyMotion>
+    <div className="relative w-full h-full">
+      <Image
+        src={image}
+        alt="Diagram of Machine Learning tools that integrate with Supabase Vector"
+        layout="fill"
+        objectFit="contain"
+      />
+    </div>
   )
 }
 
