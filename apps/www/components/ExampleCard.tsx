3679793c15950d19d75e6bb15a4d9173228b6b57
Isaiah Hamilton <70828596+Isaiah-Hamilton@users.noreply.github.com>
2023-10-04 22:04:55 -0400

    update example card

--- apps/www/components/ExampleCard.tsx
+++ apps/www/components/ExampleCard.tsx
@@ -4,87 +4,41 @@ import Image from 'next/image'
 
 function ExampleCard(props: any) {
   return (
-    <>
-      <div
-        className="dark:bg-scale-300
-          border-scale-400
-          flex
-          h-40
-          flex-col
-          justify-between rounded rounded-b-none
-          border
-          border-t border-r border-l
-          bg-white p-5
-          "
-      >
-        <div className="mb-4">
-          <h4 className="h6">{props.title}</h4>
-          <p className="p text-sm">{props.description}</p>
-          <div className="flex items-center">
-            <div className="relative border-scale-500 inline !w-6 !h-6 rounded-full overflow-hidden">
-              <Image
-                src={props.author_img}
-                alt={props.author + ' GitHub profile picture'}
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
-            <span className="text-scale-1200 ml-2 text-sm">{props.author}</span>
+    <div className="relative rounded-xl bg-scale-400 from-scale-800 to-scale-800 p-px transition-all shadow-md h-full">
+      <div className="relative h-full z-10 rounded-xl bg-scale-200 dark:bg-scale-300 overflow-hidden transition-all text-scale-1100 flex flex-col p-5">
+        <div className="mb-4 lg:mb-8 flex-1">
+          <h2 className="text text-lg font-medium">{props.title}</h2>
+          <div className="my-2 block">
+            <p className="text-light">{props.description}</p>
           </div>
         </div>
-      </div>
-      <Divider light />
-      <div>
-        <div
-          className="
-          bg-scale-100
-          dark:bg-scale-400
-          border-scale-400 flex
-          flex-col justify-between rounded rounded-t-none
-          border
-          border-b border-r border-l
-          border-t-0 p-5"
-        >
-          <Link href={props.repo_url} as={props.repo_url} passHref>
-            <a
-              className="text-scale-1100 hover:text-scale-1200 flex flex-row items-center text-sm"
-              target="_blank"
-            >
-              <span>{props.repo_name}</span>
-              <span className="ml-1 inline-block">
-                <IconGitHub size={14} />
-              </span>
+        <div className="flex items-start gap-2">
+          <Link href={props.repo_url}>
+            <a target="_blank" tabIndex={-1}>
+              <Button size="tiny" type="default" iconRight={<IconArrowUpRight />}>
+                View Code
+              </Button>
             </a>
           </Link>
-
-          <div className="mt-3 flex items-stretch gap-2 h-[26px]">
-            {props.vercel_deploy_url && (
-              <a target="_blank" href={props.vercel_deploy_url}>
+          {props.demo_url && (
+            <Link href={props.demo_url}>
+              <a target="_blank" tabIndex={-1}>
+                <Button size="tiny" type="default" iconRight={<IconArrowUpRight />}>
+                  Launch Demo
+                </Button>
+              </a>
+            </Link>
+          )}
+          {props.vercel_deploy_url && (
+            <Link href={props.vercel_deploy_url}>
+              <a target="_blank">
                 <Image src="https://vercel.com/button" alt="vercel button" width={75} height={26} />
               </a>
-            )}
-            {props.demo_url && (
-              <Link href={props.demo_url} as={props.demo_url}>
-                <a target="_blank" tabIndex={-1}>
-                  <Button size="tiny" type="default" iconRight={<IconArrowUpRight />}>
-                    Launch Demo
-                  </Button>
-                </a>
-              </Link>
-            )}
-            {!props.demo_url && (
-              <Link href={props.repo_url} as={props.repo_url}>
-                <a target="_blank" tabIndex={-1}>
-                  <Button size="tiny" type="default" iconRight={<IconArrowUpRight />}>
-                    View Code
-                  </Button>
-                </a>
-              </Link>
-            )}
-          </div>
+            </Link>
+          )}
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
