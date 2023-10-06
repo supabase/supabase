55ea3445c6adac983c87f50a43910cf757a4c4ab
shishiro <dheervathshishiro@gmail.com>
2023-10-06 23:43:48 +0530

    fixed the bug in the editor table

--- studio/components/grid/components/editor/DateTimeEditor.tsx
+++ studio/components/grid/components/editor/DateTimeEditor.tsx
@@ -1,20 +1,15 @@
-import { ChangeEvent } from 'react'
-import { EditorProps } from '@supabase/react-data-grid'
-import dayjs from 'dayjs'
-import customParseFormat from 'dayjs/plugin/customParseFormat'
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
+import { EditorProps } from '@supabase/react-data-grid';
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
 
-dayjs.extend(customParseFormat)
-
-function autoFocusAndSelect(input: HTMLInputElement | null) {
-  input?.focus()
-  input?.select()
-}
+dayjs.extend(customParseFormat);
 
 interface Props<TRow, TSummaryRow = unknown> extends EditorProps<TRow, TSummaryRow> {
-  format: string
+  format: string;
 }
 
-const INPUT_DATE_TIME_FORMAT = 'YYYY-MM-DDTHH:mm:ss'
+const INPUT_DATE_TIME_FORMAT = 'YYYY-MM-DDTHH:mm:ss';
 
 function BaseEditor<TRow, TSummaryRow = unknown>({
   row,
@@ -23,38 +18,48 @@ function BaseEditor<TRow, TSummaryRow = unknown>({
   onRowChange,
   onClose,
 }: Props<TRow, TSummaryRow>) {
-  const value = row[column.key as keyof TRow] as unknown as string
-  const timeValue = value ? dayjs(value, format).format(INPUT_DATE_TIME_FORMAT) : value
+  const [inputValue, setInputValue] = useState<string>(row[column.key as keyof TRow] || ''); 
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+      inputRef.current.select();
+    }
+  }, []); 
 
   function onChange(event: ChangeEvent<HTMLInputElement>) {
-    const _value = event.target.value
+    const _value = event.target.value;
+
     if (_value.length === 0) {
-      onRowChange({ ...row, [column.key]: null })
+      setInputValue('');
+      onRowChange({ ...row, [column.key]: null });
     } else {
-      const _timeValue = dayjs(_value).format(format)
-      onRowChange({ ...row, [column.key]: _timeValue })
+      setInputValue(_value);
+      const _timeValue = dayjs(_value).format(format);
+      onRowChange({ ...row, [column.key]: _timeValue });
     }
   }
 
   return (
     <input
       className="sb-grid-datetime-editor"
-      ref={autoFocusAndSelect}
-      value={timeValue ?? ''}
+      ref={(input) => (inputRef.current = input)}
+      value={inputValue}
       onChange={onChange}
       onBlur={() => onClose(true)}
       type="datetime-local"
       step="1"
     />
-  )
+  );
 }
 
 export function DateTimeEditor<TRow, TSummaryRow = unknown>(props: EditorProps<TRow, TSummaryRow>) {
-  return <BaseEditor {...props} format="YYYY-MM-DDTHH:mm:ss" />
+  return <BaseEditor {...props} format={INPUT_DATE_TIME_FORMAT} />;
 }
 
 export function DateTimeWithTimezoneEditor<TRow, TSummaryRow = unknown>(
   props: EditorProps<TRow, TSummaryRow>
 ) {
-  return <BaseEditor {...props} format="YYYY-MM-DDTHH:mm:ssZ" />
+  return <BaseEditor {...props} format="YYYY-MM-DDTHH:mm:ssZ" />;
 }
