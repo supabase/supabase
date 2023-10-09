da7c2b52cd655173cd8bfac60bcb307c8348851b
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-10-09 18:07:39 +0200

    Improve mobile (#18032)
    * Improve mobile announcement in vector page

--- apps/www/components/Announcement/Badge.tsx
+++ apps/www/components/Announcement/Badge.tsx
@@ -9,9 +9,17 @@ interface Props {
   badge?: string
   target?: '_self' | '_blank' | string
   className?: string
+  hasArrow?: boolean
 }
 
-const AnnouncementBadge = ({ url, announcement, badge, target = '_self', className }: Props) => (
+const AnnouncementBadge = ({
+  url,
+  announcement,
+  badge,
+  target = '_self',
+  className,
+  hasArrow = true,
+}: Props) => (
   <div
     className={cn(
       'relative w-full max-w-xl flex justify-center opacity-0 !animate-[fadeIn_0.5s_cubic-bezier(0.25,0.25,0,1)_0.5s_both]',
@@ -48,7 +56,9 @@ const AnnouncementBadge = ({ url, announcement, badge, target = '_self', classNa
           </Badge>
         )}
         <span className="text-foreground">{announcement}</span>
-        <ArrowNarrowRightIcon className="h-4 ml-2 -translate-x-1 transition-transform group-hover/announcement:translate-x-0" />
+        {hasArrow && (
+          <ArrowNarrowRightIcon className="h-4 ml-2 -translate-x-1 transition-transform group-hover/announcement:translate-x-0" />
+        )}
         <div
           className="absolute inset-0 -z-10 bg-gradient-to-br
             opacity-70
