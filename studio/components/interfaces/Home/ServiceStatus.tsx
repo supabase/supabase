5329a3d439b8d71e42ef150821827bcd45e0aabb
Joshen Lim <joshenlimek@gmail.com>
2023-10-11 16:55:15 +0800

    Use new endpoints to check auth storage realtime rest service status, and deprecate unused RQs

--- studio/components/interfaces/Home/ServiceStatus.tsx
+++ studio/components/interfaces/Home/ServiceStatus.tsx
@@ -1,6 +1,6 @@
 import { useParams } from 'common'
 import { AlertTriangle, CheckCircle2 } from 'lucide-react'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import {
   Button,
   IconLoader,
@@ -9,12 +9,8 @@ import {
   Popover_Shadcn_,
 } from 'ui'
 
-import { createClient } from '@supabase/supabase-js'
-import { useProjectApiQuery } from 'data/config/project-api-query'
-import { useAuthServiceStatusQuery } from 'data/service-status/auth-service-status-query'
 import { usePostgresServiceStatusQuery } from 'data/service-status/postgres-service-status-query'
-import { usePostgrestServiceStatusQuery } from 'data/service-status/postgrest-service-status-query'
-import { useStorageServiceStatusQuery } from 'data/service-status/storage-service-status-query'
+import { useProjectServiceStatusQuery } from 'data/service-status/service-status-query'
 import { useSelectedProject } from 'hooks'
 
 const ServiceStatus = () => {
@@ -23,100 +19,33 @@ const ServiceStatus = () => {
   const [open, setOpen] = useState(false)
 
   const isBranch = project?.parentRef !== project?.ref
-  const { data } = useProjectApiQuery({ projectRef: ref })
-  const { endpoint, defaultApiKey } = data?.autoApiService || {}
 
-  // [Joshen] Realtime status will be handled separately
-  const [realtimeStatus, setRealtimeStatus] = useState<'loading' | 'success' | 'error'>('loading')
-  const realtimeClient =
-    endpoint && defaultApiKey ? createClient(`https://${endpoint}`, defaultApiKey) : undefined
-  if (realtimeClient !== undefined) {
-    const channel = realtimeClient.channel('health', { config: { broadcast: { self: true } } })
-    channel.on('broadcast', { event: 'health-check' }, (payload) => {
-      if (payload.event === 'health-check') {
-        setRealtimeStatus('success')
-        realtimeClient.removeChannel(channel)
-      }
-    })
-    channel.subscribe((status) => {
-      if (status !== 'SUBSCRIBED') return
-      channel.send({ type: 'broadcast', event: 'health-check', payload: {} })
-    })
-  }
+  // Covers auth, storage, realtime, rest
+  const { data: status, isLoading } = useProjectServiceStatusQuery({ projectRef: ref })
 
-  useEffect(() => {
-    setTimeout(() => {
-      if (realtimeStatus === 'loading') setRealtimeStatus('error')
-    }, 5000)
-  }, [])
-
-  // [Joshen] Still need pooler service check eventually
-  const {
-    isLoading: isLoadingPostgres,
-    isError: isErrorPostgres,
-    isSuccess: isSuccessPostgres,
-  } = usePostgresServiceStatusQuery({
-    projectRef: ref,
-    connectionString: project?.connectionString,
-  })
-  const {
-    isLoading: isLoadingPostgrest,
-    isError: isErrorPostgrest,
-    isSuccess: isSuccessPostgrest,
-  } = usePostgrestServiceStatusQuery({
-    projectRef: ref,
-  })
-  const {
-    isLoading: isLoadingStorage,
-    isError: isErrorStorage,
-    isSuccess: isSuccessStorage,
-  } = useStorageServiceStatusQuery({
-    projectRef: ref,
-  })
-  const {
-    isLoading: isLoadingAuth,
-    isError: isErrorAuth,
-    isSuccess: isSuccessAuth,
-  } = useAuthServiceStatusQuery(
-    {
+  // [Joshen] Need pooler service check eventually
+  // [Joshen] Need edge functions check eventually
+  const { isLoading: isLoadingPostgres, isSuccess: isSuccessPostgres } =
+    usePostgresServiceStatusQuery({
       projectRef: ref,
-      endpoint: endpoint,
-      anonKey: defaultApiKey,
-    },
-    { enabled: defaultApiKey !== undefined }
-  )
+      connectionString: project?.connectionString,
+    })
+
+  const authStatus = status?.find((service) => service.name === 'auth')
+  const restStatus = status?.find((service) => service.name === 'rest')
+  const realtimeStatus = status?.find((service) => service.name === 'realtime')
+  const storageStatus = status?.find((service) => service.name === 'storage')
 
   const services = [
     {
-      name: 'Auth',
-      isLoading: isLoadingAuth,
-      isError: isErrorAuth,
-      isSuccess: isSuccessAuth,
-    },
-    {
       name: 'Database',
       isLoading: isLoadingPostgres,
-      isError: isErrorPostgres,
       isSuccess: isSuccessPostgres,
     },
-    {
-      name: 'PostgREST',
-      isLoading: isLoadingPostgrest,
-      isError: isErrorPostgrest,
-      isSuccess: isSuccessPostgrest,
-    },
-    {
-      name: 'Realtime',
-      isLoading: realtimeStatus === 'loading',
-      isError: realtimeStatus === 'error',
-      isSuccess: realtimeStatus === 'success',
-    },
-    {
-      name: 'Storage',
-      isLoading: isLoadingStorage,
-      isError: isErrorStorage,
-      isSuccess: isSuccessStorage,
-    },
+    { name: 'PostgREST', isLoading, isSuccess: restStatus?.healthy ?? false },
+    { name: 'Auth', isLoading, isSuccess: authStatus?.healthy ?? false },
+    { name: 'Realtime', isLoading, isSuccess: realtimeStatus?.healthy ?? false },
+    { name: 'Storage', isLoading, isSuccess: storageStatus?.healthy ?? false },
   ]
 
   const isLoadingChecks = services.some((service) => service.isLoading)
@@ -146,33 +75,26 @@ const ServiceStatus = () => {
         align="end"
         style={{ marginLeft: '-247px' }}
       >
-        {services.map((service) => {
-          if (['Auth', 'Realtime'].includes(service.name) && defaultApiKey === undefined) {
-            return null
-          }
-          return (
-            <div
-              key={service.name}
-              className="px-4 py-2 text-xs flex items-center justify-between border-b"
-            >
-              <div>
-                <p>{service.name}</p>
-                <p className="text-light">
-                  {service.isLoading && 'Checking status'}
-                  {service.isSuccess && 'No issues'}
-                  {service.isError && 'Unable to connect'}
-                </p>
-              </div>
-              {service.isLoading && <IconLoader className="animate-spin" size="tiny" />}
-              {service.isSuccess && (
-                <CheckCircle2 className="text-brand" size={18} strokeWidth={1.5} />
-              )}
-              {service.isError && (
-                <AlertTriangle className="text-amber-900" size={18} strokeWidth={1.5} />
-              )}
+        {services.map((service) => (
+          <div
+            key={service.name}
+            className="px-4 py-2 text-xs flex items-center justify-between border-b"
+          >
+            <div>
+              <p>{service.name}</p>
+              <p className="text-light">
+                {service.isLoading && 'Checking status'}
+                {service.isSuccess ? 'No issues' : 'Unable to connect'}
+              </p>
             </div>
-          )
-        })}
+            {service.isLoading && <IconLoader className="animate-spin" size="tiny" />}
+            {service.isSuccess ? (
+              <CheckCircle2 className="text-brand" size={18} strokeWidth={1.5} />
+            ) : (
+              <AlertTriangle className="text-amber-900" size={18} strokeWidth={1.5} />
+            )}
+          </div>
+        ))}
       </PopoverContent_Shadcn_>
     </Popover_Shadcn_>
   )
--- studio/data/service-status/keys.ts
+++ studio/data/service-status/keys.ts
@@ -1,14 +1,8 @@
 export const serviceStatusKeys = {
   postgres: (projectRef: string | undefined) =>
     ['projects', projectRef, 'service-status', 'postgres'] as const,
-  postgrest: (projectRef: string | undefined) =>
-    ['projects', projectRef, 'service-status', 'postgrest'] as const,
-  storage: (projectRef: string | undefined) =>
-    ['projects', projectRef, 'service-status', 'storage'] as const,
-  realtime: (projectRef: string | undefined) =>
-    ['projects', projectRef, 'service-status', 'realtime'] as const,
-  auth: (projectRef: string | undefined) =>
-    ['projects', projectRef, 'service-status', 'auth'] as const,
   pooler: (projectRef: string | undefined) =>
     ['projects', projectRef, 'service-status', 'pooler'] as const,
+  serviceStatus: (projectRef: string | undefined) =>
+    ['projects', projectRef, 'service-status'] as const,
 }
