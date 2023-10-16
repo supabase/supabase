3d70a1cdca364580b2672f8f96c7fa50d30750e6
Ivan Vasilov <vasilov.ivan@gmail.com>
2023-10-16 10:55:43 +0200

    Revert "Chore/support temp enable db writes (#18171)" (#18229)
    This reverts commit d200ed941439976d32e6eb6f8479f71a5a4715d7.

--- studio/data/oauth/oauth-app-update-mutation.ts
+++ studio/data/oauth/oauth-app-update-mutation.ts
@@ -1,4 +1,3 @@
-import { OAuthScope } from '@supabase/shared-types/out/constants'
 import { useMutation, UseMutationOptions, useQueryClient } from '@tanstack/react-query'
 import { toast } from 'react-hot-toast'
 
@@ -13,7 +12,6 @@ export type OAuthAppUpdateVariables = {
   name: string
   website: string
   icon?: string | null
-  scopes?: OAuthScope[]
   redirect_uris: string[]
 }
 
@@ -23,7 +21,6 @@ export async function updateOAuthApp({
   name,
   website,
   icon,
-  scopes,
   redirect_uris,
 }: OAuthAppUpdateVariables) {
   if (!id) throw new Error('OAuth app ID is required')
@@ -36,7 +33,6 @@ export async function updateOAuthApp({
     name,
     website,
     icon,
-    scopes,
     redirect_uris,
   })
   if (response.error) throw response.error
