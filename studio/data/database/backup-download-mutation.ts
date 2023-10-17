5dda59ba95b97bb2a8b3259eb17d7be8a4072d0f
Ivan Vasilov <vasilov.ivan@gmail.com>
2023-10-17 10:50:48 +0200

    chore: Run prettier on `studio/data/api` folder (#18261)
    * Remove studio/data to not be ignored when running prettier.
    
    * Run prettier format on the whole repo.
    
    * Run prettier format after each codegen.

--- studio/data/database/backup-download-mutation.ts
+++ studio/data/database/backup-download-mutation.ts
@@ -14,13 +14,13 @@ export async function downloadBackup({ ref, backup }: BackupDownloadVariables) {
   const { data, error } = await post('/platform/database/{ref}/backups/download', {
     params: { path: { ref } },
     body: {
-      id: backup.id, // this is the only one needed actually  
+      id: backup.id, // this is the only one needed actually
       inserted_at: backup.inserted_at,
-      project_id: backup.project_id, 
+      project_id: backup.project_id,
       data: {},
       s3_bucket: 'deprecated',
       s3_path: 'deprecated',
-      status: 'deprecated'
+      status: 'deprecated',
     },
   })
   if (error) throw error
