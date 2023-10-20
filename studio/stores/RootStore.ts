12a082ff6da19b86837ca6a1919d5159cd7433c3
Alaister Young <a@alaisteryoung.com>
2023-10-20 17:20:21 +1100

    fix graphiql

--- studio/stores/RootStore.ts
+++ studio/stores/RootStore.ts
@@ -1,16 +1,9 @@
-import { configure } from 'mobx'
 import { Project } from 'types'
+import UiStore, { IUiStore } from './UiStore'
 import AppStore, { IAppStore } from './app/AppStore'
 import MetaStore, { IMetaStore } from './pgmeta/MetaStore'
 import ProjectFunctionsStore, { IProjectFunctionsStore } from './project/ProjectFunctionsStore'
 import VaultStore, { IVaultStore } from './project/VaultStore'
-import UiStore, { IUiStore } from './UiStore'
-
-// Temporary disable mobx warnings
-// TODO: need to remove this after refactoring old stores.
-configure({
-  enforceActions: 'never',
-})
 
 export interface IRootStore {
   app: IAppStore
