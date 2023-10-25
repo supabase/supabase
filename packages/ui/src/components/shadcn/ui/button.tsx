f388042b5d98cde7aae0bc40342fe7152e922407
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-10-25 16:58:35 +0200

    update tokens on Button and other components

--- packages/ui/src/components/shadcn/ui/button.tsx
+++ packages/ui/src/components/shadcn/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+        default: 'bg-blue-300 text-primary-foreground hover:bg-primary/90',
         destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline: 'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
         secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
