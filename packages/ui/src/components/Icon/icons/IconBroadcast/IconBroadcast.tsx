759c2bf786226155b1ee1ae0687928f70c26aea8
Ivan Vasilov <vasilov.ivan@gmail.com>
2023-10-25 16:12:06 +0200

    Change the icon for broadcast.

--- packages/ui/src/components/Icon/icons/IconBroadcast/IconBroadcast.tsx
+++ packages/ui/src/components/Icon/icons/IconBroadcast/IconBroadcast.tsx
@@ -3,51 +3,34 @@ import IconBase from './../../IconBase'
 const SvgComponent = () => (
   <>
     <path
+      id="Ellipse 146"
+      d="M17.3154 6.70312C18.6968 8.06227 19.5532 9.95249 19.5532 12.0426C19.5532 14.1326 18.6968 16.0229 17.3154 17.382M6.79102 6.70312C5.40966 8.06227 4.55322 9.95249 4.55322 12.0426C4.55322 14.1326 5.40966 16.0229 6.79102 17.382"
       stroke="#4CC38A"
+      strokeMiterlimit="10"
       strokeLinejoin="bevel"
-      strokeMiterlimit={10}
-      d="M15.695 8.824a4.48 4.48 0 0 1 1.358 3.219 4.48 4.48 0 0 1-1.358 3.218M9.411 8.824a4.48 4.48 0 0 0-1.358 3.219 4.48 4.48 0 0 0 1.358 3.218"
-    />
-    <path
-      stroke="#85E0B7"
-      strokeLinejoin="bevel"
-      strokeMiterlimit={10}
-      d="M15.695 15.26a4.487 4.487 0 0 1-3.142 1.278 4.487 4.487 0 0 1-3.142-1.277M15.695 8.824a4.487 4.487 0 0 0-3.142-1.277 4.487 4.487 0 0 0-3.142 1.277"
-      opacity={0.33}
-    />
-    <path
-      stroke="#4CC38A"
-      strokeLinejoin="bevel"
-      strokeMiterlimit={10}
-      d="M17.815 6.703a7.467 7.467 0 0 1 2.238 5.34c0 2.09-.856 3.98-2.238 5.339M7.291 6.703a7.468 7.468 0 0 0-2.238 5.34c0 2.09.857 3.98 2.238 5.339"
-    />
-    <path
-      stroke="#85E0B7"
-      strokeLinejoin="bevel"
-      strokeMiterlimit={10}
-      d="M17.815 17.382a7.479 7.479 0 0 1-5.262 2.154 7.48 7.48 0 0 1-5.262-2.154M17.815 6.703a7.479 7.479 0 0 0-5.262-2.154A7.48 7.48 0 0 0 7.29 6.703"
-      opacity={0.33}
     />
     <ellipse
-      cx={12.553}
-      cy={12.043}
+      id="Ellipse 144"
+      cx="12.0532"
+      cy="12.0428"
+      rx="3.00928"
+      ry="3.00666"
       stroke="#4CC38A"
+      strokeMiterlimit="10"
       strokeLinejoin="bevel"
-      strokeMiterlimit={10}
-      rx={1.5}
-      ry={1.499}
     />
     <path
+      id="Vector 96"
+      d="M12.0747 15.0488L12.0747 23.9996"
       stroke="#4CC38A"
+      strokeMiterlimit="10"
       strokeLinejoin="bevel"
-      strokeMiterlimit={10}
-      d="M12.532 13.28 12.575 24"
     />
   </>
 )
 
 function IconBroadcast(props: any) {
-  return <IconBase src={<SvgComponent />} viewBox="0 0 25 25" {...props} />
+  return <IconBase src={<SvgComponent />} viewBox="0 0 24 24" {...props} />
 }
 
 export default IconBroadcast
