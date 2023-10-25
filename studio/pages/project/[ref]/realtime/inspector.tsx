94835e2e857c06980c76b8c3d3b10ea2b9fdd47f
Ivan Vasilov <vasilov.ivan@gmail.com>
2023-10-25 14:09:02 +0200

    Add checks for permissions on the realtime inspector.

--- studio/pages/project/[ref]/realtime/inspector.tsx
+++ studio/pages/project/[ref]/realtime/inspector.tsx
@@ -2,8 +2,18 @@ import { NextPageWithLayout } from 'types'
 
 import { RealtimeInspector } from 'components/interfaces/Realtime/Inspector'
 import RealtimeLayout from 'components/layouts/RealtimeLayout/RealtimeLayout'
+import { useCheckPermissions } from 'hooks'
+import { PermissionAction } from '@supabase/shared-types/out/constants'
+import ProjectLayout from 'components/layouts'
+import NoPermission from 'components/ui/NoPermission'
 
 export const InspectorPage: NextPageWithLayout = () => {
+  const canReadAPIKeys = useCheckPermissions(PermissionAction.READ, 'service_api_keys')
+
+  if (!canReadAPIKeys) {
+    return <NoPermission isFullPage resourceText="access your project's realtime functionalities" />
+  }
+
   return <RealtimeInspector />
 }
 
