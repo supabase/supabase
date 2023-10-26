373e689ae76c02774b71066922992a2079b1e11b
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-10-26 15:23:52 +0200

    update ExampleProject and TableEditorMenu tokens

--- studio/pages/sign-in.tsx
+++ studio/pages/sign-in.tsx
@@ -26,10 +26,10 @@ const SignInPage: NextPageWithLayout = () => {
 
         <div className="relative">
           <div className="absolute inset-0 flex items-center">
-            <div className="w-full border-t border-scale-700" />
+            <div className="w-full border-t border-strong" />
           </div>
           <div className="relative flex justify-center text-sm">
-            <span className="px-2 text-sm bg-scale-200 text-foreground">or</span>
+            <span className="px-2 text-sm bg-background text-foreground">or</span>
           </div>
         </div>
 
