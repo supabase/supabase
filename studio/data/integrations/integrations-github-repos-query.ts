a999b20da0ca4b177ed27386780f7493935dfd6d
Kamil Ogórek <kamil.ogorek@gmail.com>
2023-10-26 12:54:02 +0200

    ref: Fetch all GitHub repositories for GitHub integration config page (#18470)
    * ref: Fix all GitHub repositories for GitHub integration config page
    * Add API schema

--- studio/data/integrations/integrations-github-repos-query.ts
+++ studio/data/integrations/integrations-github-repos-query.ts
@@ -8,6 +8,9 @@ export type GitHubReposVariables = {
   integrationId: string | undefined
 }
 
+// We don't want to fetch more than 1000 repositories, as it's already more than enough.
+const MAX_PAGES = 10
+
 export async function getGitHubRepos(
   { integrationId }: GitHubReposVariables,
   signal?: AbortSignal
@@ -16,12 +19,42 @@ export async function getGitHubRepos(
     throw new Error('integrationId is required')
   }
 
+  const repos: Awaited<ReturnType<typeof getSingleGithubReposPage>> = []
+  const perPage = 100
+  let page = 1
+
+  // This is unfortunate, because we will do redundant API call in case there are exactly 100 results,
+  // but the API is returning an array instead of `{ repos: [], total_count: n }`.
+  while (true) {
+    const reposChunk = await getSingleGithubReposPage(integrationId, perPage, page, signal)
+    repos.push(...reposChunk)
+    page += 1
+
+    // Stop asking for more data if last request was exhaustive or we reached a limit, .
+    if (reposChunk.length < perPage || page > MAX_PAGES) {
+      break
+    }
+  }
+
+  return repos
+}
+
+async function getSingleGithubReposPage(
+  integrationId: string,
+  perPage: number,
+  page: number,
+  signal?: AbortSignal
+) {
   const { data, error } = await get(
     '/platform/integrations/github/repos/{organization_integration_id}',
     {
       params: {
         path: {
           organization_integration_id: integrationId,
+        },
+        query: {
+          per_page: perPage,
+          page,
         },
       },
       signal,
@@ -30,7 +63,6 @@ export async function getGitHubRepos(
   if (error) {
     throw error
   }
-
   return data
 }
 
