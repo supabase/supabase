2ac06891da898d80c0302665a3d075aa75df3037
Tyler <18113850+dshukertjr@users.noreply.github.com>
2023-11-04 14:26:06 +0800

    docs: update Flutter docs/ guides/ examples to reflect the new `Skip nonce checks` option for Google login (#18697)
    * update example google sign in app
    
    * update flutter google sign in example
    
    * update flutter auth blog post to add skip nonce check content
    
    * update google sign in guide to include skip nonce check content
    
    * minor update on the code sample for google auth guide
    
    * delete unused code from code sample
    
    * minor update of the blog writing
    
    * blog article refactor
    
    * minor format fix
    
    * Update apps/docs/pages/guides/auth/social-login/auth-google.mdx
    
    Co-authored-by: Charis <26616127+charislam@users.noreply.github.com>
    
    * Update apps/docs/pages/guides/auth/social-login/auth-google.mdx
    
    Co-authored-by: Charis <26616127+charislam@users.noreply.github.com>
    
    * Update apps/docs/pages/guides/auth/social-login/auth-google.mdx
    
    Co-authored-by: Charis <26616127+charislam@users.noreply.github.com>
    
    * Update apps/www/_blog/2023-07-18-flutter-authentication.mdx
    
    Co-authored-by: Charis <26616127+charislam@users.noreply.github.com>
    
    * Update apps/www/_blog/2023-07-18-flutter-authentication.mdx
    
    Co-authored-by: Charis <26616127+charislam@users.noreply.github.com>
    
    * Update apps/www/_blog/2023-07-18-flutter-authentication.mdx
    
    Co-authored-by: Charis <26616127+charislam@users.noreply.github.com>
    
    * Update apps/www/_blog/2023-07-18-flutter-authentication.mdx
    
    Co-authored-by: Charis <26616127+charislam@users.noreply.github.com>
    
    * fix typo
    
    ---------
    
    Co-authored-by: Charis <26616127+charislam@users.noreply.github.com>

--- examples/auth/flutter-native-google-auth/lib/screens/login_screen.dart
+++ examples/auth/flutter-native-google-auth/lib/screens/login_screen.dart
@@ -1,10 +1,4 @@
-import 'dart:convert';
-import 'dart:io';
-import 'dart:math';
-
-import 'package:crypto/crypto.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_appauth/flutter_appauth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:myauthapp/main.dart';
 import 'package:myauthapp/screens/profile_screen.dart';
@@ -37,103 +31,40 @@ class _LoginScreenState extends State<LoginScreen> {
     });
   }
 
-  /// Function to generate a random 16 character string.
-  String _generateRandomString() {
-    final random = Random.secure();
-    return base64Url.encode(List<int>.generate(16, (_) => random.nextInt(256)));
-  }
-
   Future<AuthResponse> _googleSignIn() async {
-    /// TODO: update the iOS and Web client ID with your own.
+    /// TODO: update the Web client ID with your own.
     ///
-    /// Client ID that you registered with Google Cloud.
-    /// Note that in order to perform Google sign in on Android, you need to
-    /// provide the web client ID, not the Android client ID.
-    final clientId = Platform.isIOS ? 'IOS_CLIENT_ID' : 'WEB_CLIENT_ID';
+    /// Web Client ID that you registered with Google Cloud.
+    const webClientId = 'my-web.apps.googleusercontent.com';
 
-    late final String? idToken;
-    late final String? accessToken;
-    String? rawNonce;
+    /// TODO: update the iOS client ID with your own.
+    ///
+    /// iOS Client ID that you registered with Google Cloud.
+    const iosClientId = 'my-ios.apps.googleusercontent.com';
 
-    // Use AppAuth to perform Google sign in on iOS
-    // and use GoogleSignIn package for Google sign in on Android
-    if (Platform.isIOS) {
-      const appAuth = FlutterAppAuth();
+    // Google sign in on Android will work without providing the Android
+    // Client ID registered on Google Cloud.
 
-      // Just a random string
-      rawNonce = _generateRandomString();
-      final hashedNonce = sha256.convert(utf8.encode(rawNonce)).toString();
+    final GoogleSignIn googleSignIn = GoogleSignIn(
+      clientId: iosClientId,
+      serverClientId: webClientId,
+    );
+    final googleUser = await googleSignIn.signIn();
+    final googleAuth = await googleUser!.authentication;
+    final accessToken = googleAuth.accessToken;
+    final idToken = googleAuth.idToken;
 
-      /// Set as reversed DNS form of Google Client ID + `:/` for Google login
-      final redirectUrl = '${clientId.split('.').reversed.join('.')}:/';
-
-      /// Fixed value for google login
-      const discoveryUrl =
-          'https://accounts.google.com/.well-known/openid-configuration';
-
-      // authorize the user by opening the concent page
-      final result = await appAuth.authorize(
-        AuthorizationRequest(
-          clientId,
-          redirectUrl,
-          discoveryUrl: discoveryUrl,
-          nonce: hashedNonce,
-          scopes: [
-            'openid',
-            'email',
-            'profile',
-          ],
-        ),
-      );
-
-      if (result == null) {
-        throw 'No result';
-      }
-
-      // Request the access and id token to google
-      final tokenResult = await appAuth.token(
-        TokenRequest(
-          clientId,
-          redirectUrl,
-          authorizationCode: result.authorizationCode,
-          discoveryUrl: discoveryUrl,
-          codeVerifier: result.codeVerifier,
-          nonce: result.nonce,
-          scopes: [
-            'openid',
-            'email',
-          ],
-        ),
-      );
-
-      accessToken = tokenResult?.accessToken;
-      idToken = tokenResult?.idToken;
-    } else {
-      final GoogleSignIn googleSignIn = GoogleSignIn(
-        serverClientId: clientId,
-        scopes: [
-          'openid',
-          'email',
-        ],
-      );
-      final googleUser = await googleSignIn.signIn();
-      final googleAuth = await googleUser!.authentication;
-      accessToken = googleAuth.accessToken;
-      idToken = googleAuth.idToken;
-    }
-
-    if (idToken == null) {
-      throw 'No ID Token';
-    }
     if (accessToken == null) {
-      throw 'No Access Token';
+      throw 'No Access Token found.';
+    }
+    if (idToken == null) {
+      throw 'No ID Token found.';
     }
 
     return supabase.auth.signInWithIdToken(
       provider: Provider.google,
       idToken: idToken,
       accessToken: accessToken,
-      nonce: rawNonce,
     );
   }
 
