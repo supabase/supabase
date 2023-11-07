2c2d11b7c77be999a18950f58188e64527070917
Joshen Lim <joshenlimek@gmail.com>
2023-11-07 21:06:00 +0800

    Shift map stuff and install deck.gl to experiment

--- studio/components/interfaces/Settings/Infrastructure/InfrastructureConfiguration/InstanceConfiguration.constants.ts
+++ studio/components/interfaces/Settings/Infrastructure/InfrastructureConfiguration/InstanceConfiguration.constants.ts
@@ -46,3 +46,28 @@ export const MOCK_DATABASES: DatabaseConfiguration[] = [
     inserted_at: '2023-11-01 06:47:46.837002',
   },
 ]
+
+// [Joshen] FYI coordinates are manually eye-balled, but I don't think they need to be absolutely accurate
+export const MOCK_AVAILABLE_REPLICA_REGIONS: {
+  coordinates: [number, number]
+  key: string
+  country: string
+}[] = [
+  { coordinates: [104, 1], key: 'SOUTHEAST_ASIA', country: 'Singapore' },
+  { coordinates: [139, 36], key: 'NORTHEAST_ASIA', country: 'Tokyo' },
+  { coordinates: [128, 36], key: 'NORTHEAST_ASIA_2', country: 'Seoul' },
+  { coordinates: [-120, 60], key: 'CENTRAL_CANADA', country: 'Canada' },
+  { coordinates: [-120, 35], key: 'WEST_US', country: 'West US' },
+  { coordinates: [-77, 35], key: 'EAST_US', country: 'East US' },
+]
+
+export const MOCK_CREATED_REGIONS: {
+  name: string
+  coordinates: [number, number]
+  key: string
+  country: string
+}[] = [
+  { name: 'Primary', coordinates: [104, 1], key: 'SOUTHEAST_ASIA', country: 'Singapore' },
+  { name: 'Replica #1', coordinates: [139, 36], key: 'NORTHEAST_ASIA', country: 'Tokyo' },
+  { name: 'Replica #2', coordinates: [-100, 47], key: 'CENTRAL_CANADA', country: 'Canada' },
+]
--- studio/components/interfaces/Settings/Infrastructure/InfrastructureConfiguration/MapView.tsx
+++ studio/components/interfaces/Settings/Infrastructure/InfrastructureConfiguration/MapView.tsx
@@ -1,5 +1,88 @@
+import { useState } from 'react'
+import { ComposableMap, Geographies, Geography, Marker } from 'react-simple-maps'
+
+import GeographyData from './MapData.json'
+import { BASE_PATH } from 'lib/constants'
+import {
+  MOCK_AVAILABLE_REPLICA_REGIONS,
+  MOCK_CREATED_REGIONS,
+} from './InstanceConfiguration.constants'
+
 const MapView = () => {
-  return <div>Map view</div>
+  const [tooltip, setTooltip] = useState<{ x: number; y: number; region: any }>()
+  const undeployedRegions = MOCK_AVAILABLE_REPLICA_REGIONS.filter(
+    (region) => !MOCK_CREATED_REGIONS.map((x) => x.key).includes(region.key)
+  )
+
+  return (
+    <div className="bg-background">
+      <ComposableMap projectionConfig={{ scale: 140, center: [14, 7] }} height={354}>
+        <Geographies geography={GeographyData}>
+          {({ geographies }) =>
+            geographies.map((geo) => (
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                strokeWidth={0.3}
+                pointerEvents="none"
+                className="fill-gray-300 stroke-gray-200"
+              />
+            ))
+          }
+        </Geographies>
+        {MOCK_CREATED_REGIONS.map((region) => (
+          <Marker
+            key={region.key}
+            coordinates={region.coordinates}
+            onMouseEnter={(event) => setTooltip({ x: event.clientX, y: event.clientY, region })}
+            onMouseLeave={() => setTooltip(undefined)}
+          >
+            <circle
+              r={4}
+              className={`cursor-pointer ${
+                region.name === 'Primary' ? 'fill-brand' : 'fill-brand-500'
+              }`}
+            />
+          </Marker>
+        ))}
+        {undeployedRegions.map((region) => (
+          <Marker
+            key={region.key}
+            coordinates={region.coordinates}
+            onMouseEnter={(event) => setTooltip({ x: event.clientX, y: event.clientY, region })}
+            onMouseLeave={() => setTooltip(undefined)}
+          >
+            <circle r={4} className="cursor-pointer fill-scale-900" />
+          </Marker>
+        ))}
+      </ComposableMap>
+
+      {tooltip !== undefined && (
+        <div
+          className="absolute w-[200px] bg-black bg-opacity-50 rounded border"
+          style={{ left: tooltip.x - 410, top: tooltip.y - 130 }}
+        >
+          <div className="px-3 py-2 flex items-center justify-between">
+            <p
+              className={`text-xs ${
+                tooltip.region.name === undefined ? 'text-foreground-light' : ''
+              }`}
+            >
+              {tooltip.region.name ?? 'Not deployed'}
+            </p>
+            <div className="flex items-center justify-between space-x-2">
+              <img
+                alt="region icon"
+                className="w-5 rounded-sm"
+                src={`${BASE_PATH}/img/regions/${tooltip.region.key}.svg`}
+              />
+              <p className="text-xs">{tooltip.region.country}</p>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default MapView
