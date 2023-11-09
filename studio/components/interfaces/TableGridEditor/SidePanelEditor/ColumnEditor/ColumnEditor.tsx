2798f434352b0790b3363117f6c8942c60a8bda6
Joshen Lim <joshenlimek@gmail.com>
2023-11-09 15:08:16 +0800

    Add comms around removal of TCE in table editor

--- studio/components/interfaces/TableGridEditor/SidePanelEditor/ColumnEditor/ColumnEditor.tsx
+++ studio/components/interfaces/TableGridEditor/SidePanelEditor/ColumnEditor/ColumnEditor.tsx
@@ -3,7 +3,19 @@ import { useParams } from 'common'
 import { isEmpty, noop } from 'lodash'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
-import { Button, Checkbox, IconExternalLink, IconPlus, Input, SidePanel, Toggle } from 'ui'
+import {
+  AlertDescription_Shadcn_,
+  AlertTitle_Shadcn_,
+  Alert_Shadcn_,
+  Button,
+  Checkbox,
+  IconAlertCircle,
+  IconExternalLink,
+  IconPlus,
+  Input,
+  SidePanel,
+  Toggle,
+} from 'ui'
 
 import { Dictionary } from 'components/grid'
 import { useProjectContext } from 'components/layouts/ProjectLayout/ProjectContext'
@@ -338,6 +350,43 @@ const ColumnEditor = ({
           />
         </FormSectionContent>
       </FormSection>
+
+      {isNewRecord && (
+        <>
+          <SidePanel.Separator />
+          <FormSection
+            header={<FormSectionLabel className="lg:!col-span-4">Security</FormSectionLabel>}
+          >
+            <FormSectionContent loading={false} className="lg:!col-span-8">
+              <Alert_Shadcn_>
+                <IconAlertCircle />
+                <AlertTitle_Shadcn_>
+                  Column Encryption has been removed from the GUI
+                </AlertTitle_Shadcn_>
+                <AlertDescription_Shadcn_>
+                  <p className="!leading-normal">
+                    You may still encrypt columns through the SQL editor using{' '}
+                    <Link
+                      href={`/project/${ref}/database/extensions?filter=pgsodium`}
+                      className="text-brand hover:underline"
+                    >
+                      pgsodium's
+                    </Link>{' '}
+                    Transparent Column Encryption (TCE).
+                  </p>
+                  {/* Pending decision on comms: https://supabase.slack.com/archives/C0543H0QL1Z/p1699001072364709 */}
+                  <Button type="default" icon={<IconExternalLink />} className="mt-2">
+                    <Link href="/" target="_blank" rel="noreferrer">
+                      Learn more
+                    </Link>
+                  </Button>
+                </AlertDescription_Shadcn_>
+              </Alert_Shadcn_>
+            </FormSectionContent>
+          </FormSection>
+        </>
+      )}
+
       <ForeignKeySelector
         column={columnFields}
         visible={isEditingRelation}
