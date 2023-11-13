20aacbdda3840335f8cd194e1330c3570998875a
Alaister Young <a@alaisteryoung.com>
2023-11-13 17:25:29 +1100

    run formatter

--- apps/www/components/Blog/ShareArticleActions.tsx
+++ apps/www/components/Blog/ShareArticleActions.tsx
@@ -4,7 +4,7 @@ import { IconLinkedinSolid, IconTwitterX, IconYCombinator } from 'ui'
 const ShareArticleActions = ({ title, slug }: { title: string; slug: string }) => (
   <div className="mt-4 flex items-center space-x-4">
     <Link
-        aria-label="Share on X "
+      aria-label="Share on X"
       href={`https://twitter.com/share?text=${title}&url=https://supabase.com/blog/${slug}`}
       target="_blank"
       className="text-foreground-lighter hover:text-foreground"
@@ -13,7 +13,7 @@ const ShareArticleActions = ({ title, slug }: { title: string; slug: string }) =
     </Link>
 
     <Link
-        aria-label="Share on Linkedin "
+      aria-label="Share on Linkedin"
       href={`https://www.linkedin.com/shareArticle?url=https://supabase.com/blog/${slug}&title=${title}`}
       target="_blank"
       className="text-foreground-lighter hover:text-foreground"
@@ -21,7 +21,7 @@ const ShareArticleActions = ({ title, slug }: { title: string; slug: string }) =
       <IconLinkedinSolid size={24} />
     </Link>
     <Link
-        aria-label="Share on Newycombinator "
+      aria-label="Share on Hacker News"
       href={`https://news.ycombinator.com/submitlink?u=https://supabase.com/blog/${slug}&t=${title}`}
       target="_blank"
       className="text-foreground-lighter hover:text-foreground"
