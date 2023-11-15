01dffafd9f081764326be8d4fb1a2f6b7670182b
Joshen Lim <joshenlimek@gmail.com>
2023-11-15 17:13:45 +0800

    Lock integration settings while on a preview branch

--- studio/components/interfaces/Settings/Integrations/IntegrationsSettings.tsx
+++ studio/components/interfaces/Settings/Integrations/IntegrationsSettings.tsx
@@ -1,6 +1,10 @@
+import Link from 'next/link'
+import { AlertDescription_Shadcn_, AlertTitle_Shadcn_, Alert_Shadcn_, IconAlertCircle } from 'ui'
+
 import SidePanelGitHubRepoLinker from 'components/interfaces/Organization/IntegrationSettings/SidePanelGitHubRepoLinker'
 import SidePanelVercelProjectLinker from 'components/interfaces/Organization/IntegrationSettings/SidePanelVercelProjectLinker'
-import { ScaffoldDivider } from 'components/layouts/Scaffold'
+import { ScaffoldContainer, ScaffoldDivider } from 'components/layouts/Scaffold'
+import { useProjectByRef, useSelectedProject } from 'hooks'
 import { BASE_PATH } from 'lib/constants'
 import GitHubSection from './GithubIntegration/GithubSection'
 import VercelSection from './VercelIntegration/VercelSection'
@@ -16,8 +20,29 @@ export const IntegrationImageHandler = ({ title }: { title: 'vercel' | 'github' 
 }
 
 const IntegrationSettings = () => {
+  const project = useSelectedProject()
+  const parentProject = useProjectByRef(project?.parent_project_ref)
+  const isBranch = project?.parent_project_ref !== undefined
+
   return (
     <>
+      {isBranch && (
+        <ScaffoldContainer>
+          <Alert_Shadcn_ variant="default" className="mt-6">
+            <IconAlertCircle strokeWidth={2} />
+            <AlertTitle_Shadcn_>
+              You are currently on a preview branch of your project
+            </AlertTitle_Shadcn_>
+            <AlertDescription_Shadcn_>
+              To adjust your project's integration settings, you may return to your{' '}
+              <Link href={`/project/${parentProject?.ref}/settings/general`} className="text-brand">
+                main branch
+              </Link>
+              .
+            </AlertDescription_Shadcn_>
+          </Alert_Shadcn_>
+        </ScaffoldContainer>
+      )}
       <GitHubSection />
       <ScaffoldDivider />
       <VercelSection isProjectScoped={true} />
