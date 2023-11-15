5f00aa6763f68b877251cbfb99c2380ac09a0ca5
Charis <26616127+charislam@users.noreply.github.com>
2023-11-15 16:09:58 -0500

    wrap tab lists to prevent overflow (#18859)
    Quick fix to add a wrapping option that makes tabs visible if there are
    many of them.
    
    Long-term fix would be to detect overflow and switch to a dropdown in
    that case. Or use dropdown for smaller screens only and let it wrap on
    larger screens (since it will be rarer for wrapping to trigger there).

--- packages/ui/src/components/Tabs/Tabs.tsx
+++ packages/ui/src/components/Tabs/Tabs.tsx
@@ -16,6 +16,7 @@ interface TabsProps {
   onChange?: any
   onClick?: any
   scrollable?: boolean
+  wrappable?: boolean
   addOnBefore?: React.ReactNode
   addOnAfter?: React.ReactNode
   listClassNames?: string
@@ -35,6 +36,7 @@ const Tabs: React.FC<PropsWithChildren<TabsProps>> & TabsSubComponents = ({
   onChange,
   onClick,
   scrollable,
+  wrappable,
   addOnBefore,
   addOnAfter,
   listClassNames,
@@ -89,6 +91,7 @@ const Tabs: React.FC<PropsWithChildren<TabsProps>> & TabsSubComponents = ({
 
   const listClasses = [__styles[type].list]
   if (scrollable) listClasses.push(__styles.scrollable)
+  if (wrappable) listClasses.push(__styles.wrappable)
   if (listClassNames) listClasses.push(listClassNames)
 
   return (
