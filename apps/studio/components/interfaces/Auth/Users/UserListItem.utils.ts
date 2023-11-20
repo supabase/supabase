93ee9eeebe882eb113776cc9ac1533a4c4866260
Alaister Young <a@alaisteryoung.com>
2023-11-20 23:26:02 +1100

    thanks Ivan for this one

--- apps/studio/components/interfaces/Auth/Users/UserListItem.utils.ts
+++ apps/studio/components/interfaces/Auth/Users/UserListItem.utils.ts
@@ -17,23 +17,15 @@ export function getDisplayName(user: User, fallback = '-') {
     first_name,
   } = user.raw_user_meta_data ?? {}
 
+  const last = familyName || family_name || surname || lastName || last_name
+  const first = givenName || given_name || firstName || first_name
+
   return (
     displayName ||
     display_name ||
     fullName ||
     full_name ||
-    (familyName && givenName && `${givenName} ${familyName}`) ||
-    (family_name && given_name && `${given_name} ${family_name}`) ||
-    (surname && firstName && `${firstName} ${surname}`) ||
-    (surname && first_name && `${first_name} ${surname}`) ||
-    (lastName && firstName && `${firstName} ${lastName}`) ||
-    (last_name && first_name && `${first_name} ${last_name}`) ||
-    (lastName && givenName && `${givenName} ${lastName}`) ||
-    (last_name && given_name && `${given_name} ${last_name}`) ||
-    (surname && givenName && `${givenName} ${surname}`) ||
-    (surname && given_name && `${given_name} ${surname}`) ||
-    (lastName && given_name && `${given_name} ${lastName}`) ||
-    (last_name && givenName && `${givenName} ${last_name}`) ||
+    (first && last && `${first} ${last}`) ||
     fallback
   )
 }
