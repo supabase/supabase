5bcb84b7100dcd6d969fc78cea3347f2df8324c5
Joshen Lim <joshenlimek@gmail.com>
2023-11-20 12:04:07 +0800

    Chore/refactor organization details query to organization members (#19012)
    * Refactor organization details query to organization members
    
    * Remove organzation detials key
    
    * Deprecate Member type from dashboard, use api spec as source of truth
    
    * Add mfa enabled support proactively
    
    * Show mfa enabled column proactively
    
    * Add icon

--- apps/studio/data/organizations/keys.ts
+++ apps/studio/data/organizations/keys.ts
@@ -1,6 +1,6 @@
 export const organizationKeys = {
   list: () => ['organizations'] as const,
-  detail: (slug: string | undefined) => ['organizations', slug, 'detail'] as const,
+  members: (slug?: string) => ['organizations', slug, 'members'] as const,
   paymentMethods: (slug: string | undefined) => ['organizations', slug, 'payment-methods'] as const,
   roles: (slug: string | undefined) => ['organizations', slug, 'roles'] as const,
   freeProjectLimitCheck: (slug: string | undefined) =>
