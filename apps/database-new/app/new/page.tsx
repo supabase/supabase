fbfe910c222caa63698e440fee2b164c5ba78f9c
Ivan Vasilov <vasilov.ivan@gmail.com>
2023-11-22 17:01:32 +0100

    Initial work on database.new visualizer (#19027)
    * Initial commit.
    
    * Remove bunch of unneeded deps.
    
    * Fix the providers.
    
    * Minor fixes to make the themes work.
    
    * Fix prettier errors.
    
    * Add default constraint.
    
    * Midway styling
    
    * General styling improvements
    
    * More styling stuff
    
    * Bit more styling
    
    * Bit more pizzazz
    
    * Scaffold all threads modal
    
    * Small style fix
    
    * Ensure loading state persist in new page until redirect
    
    * Fix
    
    * Test';
    
    * Add basic validation
    
    * Fix
    
    * Remove console logs
    
    * Hide show all threads button
    
    * chore: style the blocks
    
    * fix: stroke width
    
    * add some shadow
    
    * stroke width
    
    * Update TableNode.tsx
    
    * Swap hide chat for hide code
    
    * more sidebar styling
    
    * Resolve conflicts
    
    * Some style fixeds
    
    * Some style fixeds
    
    * mock profile
    
    * update user dorpdown
    
    * Update globals.css
    
    * Chore/db new layouts and url params (#19142)
    
    * chore: layouts
    
    * Fix + clean up
    
    ---------
    
    Co-authored-by: Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
    
    * Fix header
    
    * Scaffold profile and conversations
    
    * Some quick styling
    
    * Update apps/database-new/.env.local.example
    
    Co-authored-by: Greg Richardson <greg.nmr@gmail.com>
    
    ---------
    
    Co-authored-by: Joshen Lim <joshenlimek@gmail.com>
    Co-authored-by: Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
    Co-authored-by: Greg Richardson <greg.nmr@gmail.com>

---
+++ apps/database-new/app/new/page.tsx
@@ -0,0 +1,58 @@
+'use client'
+import { useMutation } from '@tanstack/react-query'
+import { Loader2 } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+
+import { Input } from 'ui'
+
+export default function NewThread() {
+  const router = useRouter()
+
+  const { mutate, isPending, isSuccess } = useMutation({
+    mutationFn: async (prompt: string) => {
+      const body = { prompt }
+      const response = await fetch('/api/ai/sql/threads/create', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+      })
+      const result = await response.json()
+      return result
+    },
+    onSuccess(data) {
+      const url = `/${data.threadId}/${data.runId}`
+      router.push(url)
+    },
+  })
+
+  return (
+    <div className="h-full flex items-center justify-center w-full flex-col gap-y-4">
+      <div className="flex items-center gap-x-1.5 font-mono text-xl">
+        <span>database</span>
+        <div className="w-1.5 h-1.5 rounded-full bg-purple-900"></div>
+        <span>new</span>
+      </div>
+      <Input
+        autoFocus
+        size="xlarge"
+        className="w-11/12 max-w-xl shadow"
+        inputClassName="rounded-full"
+        placeholder="e.g Create a Telegram-like chat application"
+        disabled={isPending || isSuccess}
+        onKeyDown={(e) => {
+          if (e.code === 'Enter') {
+            const value = (e.target as any).value
+            if (value.length > 0) mutate(value)
+          }
+        }}
+        actions={
+          isPending || isSuccess ? (
+            <div className="mr-3">
+              <Loader2 size={18} className="animate-spin" />
+            </div>
+          ) : null
+        }
+      />
+    </div>
+  )
+}
