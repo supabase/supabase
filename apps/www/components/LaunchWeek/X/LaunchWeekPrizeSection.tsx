a67b301e1c0fc615236b900bdfa91f258c7a1b3e
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-11-23 17:43:25 +0100

    update swag prize

--- apps/www/components/LaunchWeek/X/LaunchWeekPrizeSection.tsx
+++ apps/www/components/LaunchWeek/X/LaunchWeekPrizeSection.tsx
@@ -74,7 +74,7 @@ export default function LaunchWeekPrizeSection({ className }: { className?: stri
               content={
                 <div className="p-4 md:p-6 flex flex-col gap-2 text-sm items-start justify-between h-full">
                   <LabelBadge text="50 codes" />
-                  100% Supabase Store Discount
+                  100% Discount on Supabase Store
                 </div>
               }
             />
