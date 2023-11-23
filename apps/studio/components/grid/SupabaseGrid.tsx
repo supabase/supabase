293601f288a63faacf084990dd1dbac6656c2f89
Austin Tran <tranhoangvu.it1990@gmail.com>
2023-11-23 22:30:55 +0700

    chore: fixed issue when delete sorted column cause crash on table editor

--- apps/studio/components/grid/SupabaseGrid.tsx
+++ apps/studio/components/grid/SupabaseGrid.tsx
@@ -69,7 +69,14 @@ const SupabaseGridLayout = (props: SupabaseGridProps) => {
   const roleImpersonationState = useRoleImpersonationStateSnapshot()
 
   const { project } = useProjectContext()
-  const { data, error, isSuccess, isError, isLoading, isRefetching } = useTableRowsQuery(
+  const {
+    data,
+    error: rqError,
+    isSuccess,
+    isError,
+    isLoading,
+    isRefetching,
+  } = useTableRowsQuery(
     {
       queryKey: [props.table.schema, props.table.name],
       projectRef: project?.ref,
@@ -155,6 +162,20 @@ const SupabaseGridLayout = (props: SupabaseGridProps) => {
       initializeData()
     }
   }, [state.table, props.table, props.schema])
+
+  const error: null | { message: string } = rqError ? (rqError as any).error : null
+  if (error) {
+    if (error?.message.includes('does not exist')) {
+      setParams((prevParams) => {
+        return {
+          ...prevParams,
+          filter: [],
+          sort: [],
+        }
+      })
+      return null
+    }
+  }
 
   return (
     <div className="sb-grid">
