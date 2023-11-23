f8dee0502f14206cbaa35506f9f614b787bd874e
Ivan Vasilov <vasilov.ivan@gmail.com>
2023-11-23 13:24:29 +0100

    Keep all search params when redirecting to SSO login.

--- apps/studio/components/interfaces/SignIn/SignInWithSSO.tsx
+++ apps/studio/components/interfaces/SignIn/SignInWithSSO.tsx
@@ -1,10 +1,11 @@
+import { buildPathWithParams } from 'lib/gotrue'
 import Link from 'next/link'
 import { Button, IconLock } from 'ui'
 
 const SignInWithSSO = () => {
   return (
     <Button asChild block size="large" type="outline" icon={<IconLock width={18} height={18} />}>
-      <Link href="/sign-in-sso">Continue with SSO</Link>
+      <Link href={buildPathWithParams('/sign-in-sso')}>Continue with SSO</Link>
     </Button>
   )
 }
