67a18ef014884ae8e3ba012f6339d42111637bba
Kamil Ogórek <kamil.ogorek@gmail.com>
2023-11-23 16:48:09 +0100

    fix: Pass searchParams during SignInWithSSO rendering (#19171)

--- apps/studio/components/interfaces/SignIn/SignInWithSSO.tsx
+++ apps/studio/components/interfaces/SignIn/SignInWithSSO.tsx
@@ -1,10 +1,17 @@
 import Link from 'next/link'
 import { Button, IconLock } from 'ui'
 
-const SignInWithSSO = () => {
+const SignInWithSSO = ({ searchParams }: { searchParams?: string }) => {
   return (
     <Button asChild block size="large" type="outline" icon={<IconLock width={18} height={18} />}>
-      <Link href="/sign-in-sso">Continue with SSO</Link>
+      <Link
+        href={{
+          pathname: '/sign-in-sso',
+          query: searchParams,
+        }}
+      >
+        Continue with SSO
+      </Link>
     </Button>
   )
 }
