bc948ebbc9a5d668c38566b3ba9ca733fda1b543
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2023-11-24 19:55:43 +0800

    added server action in

---
+++ apps/database-new/app/actions.ts
@@ -0,0 +1,23 @@
+'use server'
+
+import { createClient } from '@/lib/supabase/server'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+import { cookies } from 'next/headers'
+
+export async function logout() {
+  //   await submitForm()
+  console.log('hello')
+  const cookieStore = cookies()
+  const supabase = createClient(cookieStore)
+
+  const { error } = await supabase.auth.signOut()
+
+  if (error) {
+    console.log('Error logging out:', error.message)
+    return
+  }
+
+  revalidatePath('/', 'layout')
+  redirect('/')
+}
