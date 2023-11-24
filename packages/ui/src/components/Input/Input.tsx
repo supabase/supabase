f830db640d47df00ad8866ec3b854c7ee1d5dfc2
Joshen Lim <joshenlimek@gmail.com>
2023-11-24 14:12:49 +0800

    The proper fix

--- packages/ui/src/components/Input/Input.tsx
+++ packages/ui/src/components/Input/Input.tsx
@@ -137,8 +137,8 @@ function Input({
 
   if (error) inputClasses.push(__styles.variants.error)
   if (!error) inputClasses.push(__styles.variants.standard)
-  if (icon) inputClasses.push(__styles.with_icon)
   if (size) inputClasses.push(__styles.size[size])
+  if (icon) inputClasses.push(__styles.with_icon)
   if (disabled) inputClasses.push(__styles.disabled)
   if (inputClassName) inputClasses.push(inputClassName)
 
@@ -171,7 +171,7 @@ function Input({
           ref={inputRef}
           type={type}
           value={reveal && hidden ? HIDDEN_PLACEHOLDER : value}
-          className={inputClasses.join(' ')}
+          className={cn(inputClasses)}
           {...props}
         />
         {icon && <InputIconContainer icon={icon} className={iconContainerClassName} />}
