156188590f80956cb3a05b930664a531eab3442a
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-11-28 00:34:52 +0100

    fix www announcement banner

--- packages/ui/src/layout/banners/LWXCountdownBanner/LWXCountdownBanner.tsx
+++ packages/ui/src/layout/banners/LWXCountdownBanner/LWXCountdownBanner.tsx
@@ -18,7 +18,7 @@ export function LWXCountdownBanner() {
   if (isLaunchWeekPage || isHomePage) return null
 
   return (
-    <div className="relative w-full h-14 p-2 flex items-center group justify-center text-white bg-[#020405] hover:bg-[#05080a] transition-colors overflow-hidden">
+    <div className="relative w-full h-14 p-2 flex items-center group justify-center text-foreground bg-alternative hover:bg-surface-200 dark:bg-[#020405] hover:dark:bg-[#05080a] transition-colors overflow-hidden">
       <div className="relative z-10 flex items-center justify-center">
         <div
           className={[
@@ -28,7 +28,13 @@ export function LWXCountdownBanner() {
         >
           <div className="flex gap-1.5 items-center">
             <p>Launch Week</p>
-            <Image src={LWXLogo} alt="Supabase Launch Week X Logo" width={14} height={14} />
+            <Image
+              src={LWXLogo}
+              alt="Supabase Launch Week X Logo"
+              width={14}
+              height={14}
+              className="filter contrast-0 dark:contrast-100"
+            />
           </div>
           <div className="hidden sm:block">
             <Countdown date={new Date(announcement.launchDate)} showCard={false} />
@@ -36,7 +42,7 @@ export function LWXCountdownBanner() {
           <Button
             onClick={() => null}
             size="tiny"
-            type="alternative"
+            type="secondary"
             className="px-2 py-1 !leading-none text-xs"
           >
             Claim your ticket
