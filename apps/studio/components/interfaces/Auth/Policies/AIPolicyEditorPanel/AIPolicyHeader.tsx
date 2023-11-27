e63adf33a4ec90a413c27b9f4d229ecdc387f29d
Joshen Lim <joshenlimek@gmail.com>
2023-11-27 15:47:44 +0800

    Hide assistant button is not platform

--- apps/studio/components/interfaces/Auth/Policies/AIPolicyEditorPanel/AIPolicyHeader.tsx
+++ apps/studio/components/interfaces/Auth/Policies/AIPolicyEditorPanel/AIPolicyHeader.tsx
@@ -1,3 +1,4 @@
+import { IS_PLATFORM } from 'lib/constants'
 import { AiIcon, Button } from 'ui'
 
 export const AIPolicyHeader = ({
@@ -11,15 +12,17 @@ export const AIPolicyHeader = ({
     <div className="space-y-1 py-4 px-4 bg-overlay sm:px-6 border-b border-overlay">
       <div className="flex flex-row justify-between items-center">
         <span className="text-sm">Create a new row level security policy</span>
-        <Button
-          size="tiny"
-          type="default"
-          className="pl-1.5 py-0.5"
-          icon={<AiIcon className="scale-75 [&>div>div]:border-white -mr-0.5" />}
-          onClick={() => setAssistantVisible(!assistantVisible)}
-        >
-          {assistantVisible ? <>Close Assistant</> : <>Open Assistant</>}
-        </Button>
+        {IS_PLATFORM && (
+          <Button
+            size="tiny"
+            type="default"
+            className="pl-1.5 py-0.5"
+            icon={<AiIcon className="scale-75 [&>div>div]:border-white -mr-0.5" />}
+            onClick={() => setAssistantVisible(!assistantVisible)}
+          >
+            {assistantVisible ? <>Close Assistant</> : <>Open Assistant</>}
+          </Button>
+        )}
       </div>
     </div>
   )
