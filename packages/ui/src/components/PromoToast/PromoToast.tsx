31c58bb194da9825f09aff0991d0bcb234528588
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-11-28 15:32:36 +0100

    fix typo

--- packages/ui/src/components/PromoToast/PromoToast.tsx
+++ packages/ui/src/components/PromoToast/PromoToast.tsx
@@ -25,7 +25,7 @@ const PromoToast = () => {
 
   return (
     <div className="grid gap-4 fixed z-50 bottom-8 right-8 w-[80vw] sm:w-[350px] bg-alternative-200 hover:bg-alternative transition-colors border border-default rounded p-6 shadow-lg overflow-hidden">
-      <div className="realtive z-10 text-foreground flex flex-col text-base uppercase tracking-[1px]">
+      <div className="relative z-10 text-foreground flex flex-col text-base uppercase tracking-[1px]">
         <div className="flex gap-1.5 items-center">
           <p>Launch Week</p>
           <Image src={LWXLogo} alt="Supabase Launch Week X Logo" width={14} height={14} />
