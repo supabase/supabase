46e176e145fb9f68e2090183453baeb96f995f6b
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2023-12-01 04:31:15 +0800

    Update EnableBranchingModal.tsx

--- apps/studio/components/layouts/AppLayout/EnableBranchingButton/EnableBranchingModal.tsx
+++ apps/studio/components/layouts/AppLayout/EnableBranchingButton/EnableBranchingModal.tsx
@@ -190,45 +190,42 @@ const EnableBranchingModal = () => {
             )}
 
             {!hasPitrEnabled && (
-              <div className="">
-                <Alert_Shadcn_ className="rounded-none border-r-0 border-l-0 px-7 py-6 [&>svg]:top-6 [&>svg]:left-6">
-                  {/* <AlertCircleIcon strokeWidth={2} /> */}
-                  <AlertTitle_Shadcn_ className="text-base">
-                    We strongly encourage enabling Point in time recovery (PITR)
-                  </AlertTitle_Shadcn_>
-                  <AlertDescription_Shadcn_>
-                    This is to ensure that you can always recover data if you make a "bad
-                    migration". For example, if you accidentally delete a column or some of your
-                    production data.
-                  </AlertDescription_Shadcn_>
-                  {isFreePlan && (
-                    <AlertDescription_Shadcn_ className="mt-2">
-                      To enable PITR, you may first upgrade your organization's plan to at least
-                      Pro, then purchase the PITR add on for your project via the{' '}
-                      <Link
-                        href={`/project/${ref}/settings/addons?panel=pitr`}
-                        className="text-brand"
-                        onClick={() => snap.setShowEnableBranchingModal(false)}
-                      >
-                        project settings
-                      </Link>
-                      .
-                    </AlertDescription_Shadcn_>
-                  )}
-                  <Button size="tiny" type="default" className="mt-4">
+              <Alert_Shadcn_ className="rounded-none px-7 py-6 [&>svg]:top-6 [&>svg]:left-6 !border-t-0 !border-l-0 !border-r-0">
+                {/* <AlertCircleIcon strokeWidth={2} /> */}
+                <AlertTitle_Shadcn_ className="text-base">
+                  We strongly encourage enabling Point in Time Recovery (PITR)
+                </AlertTitle_Shadcn_>
+                <AlertDescription_Shadcn_>
+                  This is to ensure that you can always recover data if you make a "bad migration".
+                  For example, if you accidentally delete a column or some of your production data.
+                </AlertDescription_Shadcn_>
+                {isFreePlan && (
+                  <AlertDescription_Shadcn_ className="mt-2">
+                    To enable PITR, you may first upgrade your organization's plan to at least Pro,
+                    then purchase the PITR add on for your project via the{' '}
                     <Link
-                      href={
-                        isFreePlan
-                          ? `/org/${selectedOrg?.slug}/billing?panel=subscriptionPlan`
-                          : `/project/${ref}/settings/addons?panel=pitr`
-                      }
+                      href={`/project/${ref}/settings/addons?panel=pitr`}
+                      className="text-brand"
                       onClick={() => snap.setShowEnableBranchingModal(false)}
                     >
-                      {isFreePlan ? 'Upgrade to Pro' : 'Enable PITR add-on'}
+                      project settings
                     </Link>
-                  </Button>
-                </Alert_Shadcn_>
-              </div>
+                    .
+                  </AlertDescription_Shadcn_>
+                )}
+                <Button size="tiny" type="default" className="mt-4">
+                  <Link
+                    href={
+                      isFreePlan
+                        ? `/org/${selectedOrg?.slug}/billing?panel=subscriptionPlan`
+                        : `/project/${ref}/settings/addons?panel=pitr`
+                    }
+                    onClick={() => snap.setShowEnableBranchingModal(false)}
+                  >
+                    {isFreePlan ? 'Upgrade to Pro' : 'Enable PITR add-on'}
+                  </Link>
+                </Button>
+              </Alert_Shadcn_>
             )}
 
             <Modal.Content className="px-7 py-6 flex flex-col gap-3">
