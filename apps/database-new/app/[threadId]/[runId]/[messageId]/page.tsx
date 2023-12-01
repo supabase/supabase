dcf426d834780fe0c6504e2cfd8895bca3b805d6
Terry Sutton <saltcod@gmail.com>
2023-12-01 10:03:28 -0330

    update loaders

--- apps/database-new/app/[threadId]/[runId]/[messageId]/page.tsx
+++ apps/database-new/app/[threadId]/[runId]/[messageId]/page.tsx
@@ -1,5 +1,6 @@
 import { CodeEditor } from '@/app/[threadId]/[runId]/[messageId]/CodeEditor'
 import { SchemaFlow } from '@/app/[threadId]/[runId]/[messageId]/SchemaFlow'
+import { Loader2 } from 'lucide-react'
 import { Suspense } from 'react'
 
 interface ThreadPageProps {
@@ -13,12 +14,20 @@ interface ThreadPageProps {
 export default async function ThreadPage({ params }: ThreadPageProps) {
   return (
     <div className="grow max-h-screen flex flex-row items-center justify-between bg-alternative h-full">
-      <Suspense fallback={<p>loading..</p>}>
+      <Suspense fallback={<Loader />}>
         <SchemaFlow params={params} />
       </Suspense>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Loader />}>
         <CodeEditor params={params} />
       </Suspense>
+    </div>
+  )
+}
+
+const Loader = () => {
+  return (
+    <div className="flex items-center justify-center w-full">
+      <Loader2 className="animate-spin opacity-30" />
     </div>
   )
 }
