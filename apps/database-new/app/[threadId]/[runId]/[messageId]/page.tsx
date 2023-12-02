aa3cd181c4f21dbadac1204a007a4d9cba5d0adc
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2023-12-02 19:38:58 +0800

    Update page.tsx

--- apps/database-new/app/[threadId]/[runId]/[messageId]/page.tsx
+++ apps/database-new/app/[threadId]/[runId]/[messageId]/page.tsx
@@ -14,12 +14,12 @@ interface ThreadPageProps {
 export default async function ThreadPage({ params }: ThreadPageProps) {
   return (
     <div className="grow max-h-screen flex flex-row items-center justify-between bg-alternative h-full">
-      <Suspense fallback={<Loader />}>
+      {/* <Suspense fallback={<Loader />}>
         <SchemaFlow params={params} />
       </Suspense>
       <Suspense fallback={<Loader />}>
         <CodeEditor params={params} />
-      </Suspense>
+      </Suspense> */}
     </div>
   )
 }
