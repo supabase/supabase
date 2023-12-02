6d65442ac11d188e34939b27ce5f1f4d973b4c2b
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2023-12-02 19:50:54 +0800

    Update SchemaFlow.tsx

--- apps/database-new/app/[threadId]/[runId]/[messageId]/SchemaFlow.tsx
+++ apps/database-new/app/[threadId]/[runId]/[messageId]/SchemaFlow.tsx
@@ -7,28 +7,29 @@ import OpenAI from 'openai'
 
 const openai = new OpenAI()
 
-async function waitForRunCompletion(params: {
-  threadId: string
-  runId: string
-  messageId: string
-}) {
-  let realRun = await openai.beta.threads.runs.retrieve(params.threadId, params.runId)
+// async function waitForRunCompletion(params: {
+//   threadId: string
+//   runId: string
+//   messageId: string
+// }) {
+//   let realRun = await openai.beta.threads.runs.retrieve(params.threadId, params.runId)
 
-  while (realRun.status === 'in_progress') {
-    console.log('while.. waiting for in_progress status to change for ', params.runId)
-    await new Promise((resolve) => setTimeout(resolve, 2000))
-    realRun = await openai.beta.threads.runs.retrieve(params.threadId, params.runId)
-    console.log('realRun', realRun.status)
-  }
+//   while (realRun.status === 'in_progress') {
+//     console.log('while.. waiting for in_progress status to change for ', params.runId)
+//     await new Promise((resolve) => setTimeout(resolve, 2000))
+//     realRun = await openai.beta.threads.runs.retrieve(params.threadId, params.runId)
+//     console.log('realRun', realRun.status)
+//   }
 
-  console.log('Run completed:', realRun.status)
-}
+//   console.log('Run completed:', realRun.status)
+//   return
+// }
 
 export async function SchemaFlow({ params }: { params: any }) {
   const { threadId, runId, messageId } = params
 
   // keep polling until the run is completed
-  await waitForRunCompletion(params)
+  // await waitForRunCompletion(params)
 
   const content = await getThread({ threadId, runId, messageId })
   const tables = await parseTables(content)
