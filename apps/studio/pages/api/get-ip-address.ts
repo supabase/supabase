6309c78e8474e1e84d6a72bf7298cddd7d6dfb56
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2023-12-04 19:13:23 +0100

    json

--- apps/studio/pages/api/get-ip-address.ts
+++ apps/studio/pages/api/get-ip-address.ts
@@ -1,14 +1,14 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from 'next'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  let ipAddress = req.headers["x-real-ip"] as string;
+  let ipAddress = req.headers['x-real-ip'] as string
 
-  const forwardedFor = req.headers["x-forwarded-for"] as string;
+  const forwardedFor = req.headers['x-forwarded-for'] as string
   if (!ipAddress && forwardedFor) {
-    ipAddress = forwardedFor?.split(",").at(0) ?? "Unknown";
+    ipAddress = forwardedFor?.split(',').at(0) ?? 'Unknown'
   }
 
-  res.status(200).json(ipAddress);
-};
+  return res.status(200).json({ ipAddress })
+}
 
-export default handler;
+export default handler
