1ae4a62c85ab9cfc4ac8cb5c00d4d0ace1139df5
Ivan Vasilov <vasilov.ivan@gmail.com>
2023-12-05 16:05:02 +0100

    feat: Add streaming responses to the new RLS Editor (#19304)
    * Add vercel/ai. Commit additional next types.
    
    * Add a API route in /app for streaming responses.
    
    * Make the components work with streaming response.
    
    * Add a dummy page to /app folder to fix a linking bug.
    
    * Fix the dummy page.
    
    * Fix the dummy page again.
    
    * Add an empty layout to the app folder.
    
    * Make the code snippets in the chat read-only.
    
    * Remove queries and mutations for suggest.
    
    * Reset the chat when closing the panel.
    
    * Make the AI instructions a bit better.
    
    * Don't render empty code blocks.
    
    * Try to use remark for rendering the code. Style fixes for CodeBlock in AI assistant panel.
    
    * Fix the styling of the definitions when sending them to OpenAI.
    
    * Fix the css styling of the messages and code blocks.
    
    * Move the suggest API route from app to pages folder.
    
    * Revert the change for app API routes.
    
    * Make the API route look like the rest of the API routes.
    
    * Use Pre instead of Code because the <code> tags aren't working if between spans.
    
    * Minor leftovers.
    
    * Revert to using app route handlers.
    
    * Change the wording on the diff header.
    
    * Add nextjs types.
    
    * Fix a missing import.
    
    * Move AssistantChatForm back to db-new.
    
    * Fix a build error.
    
    * Rename the suggest route to assistant.
    
    * Fix Joshen's comments.

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -182,7 +182,6 @@ export { Separator } from './src/components/shadcn/ui/separator'
 // patterns
 
 export * from './src/patterns/SchemaTableNode'
-export { AssistantChatForm } from './src/patterns/AssistantChatForm'
 
 // links
 
