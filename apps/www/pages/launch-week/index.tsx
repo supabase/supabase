6c139be1588b1d2b4eadb35e6ff803e483c4e14e
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-12-07 11:29:41 +0100

    more padding to meetups

--- apps/www/pages/launch-week/index.tsx
+++ apps/www/pages/launch-week/index.tsx
@@ -115,7 +115,7 @@ export default function LaunchWeekIndex({ meetups }: Props) {
           <MainStage />
           <BuildStage />
           {/* <TicketingFlow /> */}
-          <SectionContainer id="meetups" className="!pt-4 scroll-mt-[66px]">
+          <SectionContainer id="meetups" className="scroll-mt-[66px]">
             <LWXMeetups meetups={meetups} />
           </SectionContainer>
           <SectionContainer className="lg:pb-40">
