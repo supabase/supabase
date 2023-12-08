501ff19e59e227f3fc35214510f93d02c190427f
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2023-12-08 10:44:00 +0100

    chore: capitalize on pricing page (#19524)

--- packages/shared-data/pricing.ts
+++ packages/shared-data/pricing.ts
@@ -548,7 +548,7 @@ export const pricing: Pricing = {
     icon: 'M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z',
     features: [
       {
-        title: 'Community support',
+        title: 'Community Support',
         plans: {
           free: true,
           pro: true,
@@ -558,7 +558,7 @@ export const pricing: Pricing = {
         usage_based: false,
       },
       {
-        title: 'Email support',
+        title: 'Email Support',
         plans: {
           free: false,
           pro: true,
@@ -568,7 +568,7 @@ export const pricing: Pricing = {
         usage_based: false,
       },
       {
-        title: 'Email support SLA',
+        title: 'Email Support SLA',
         plans: {
           free: false,
           pro: false,
@@ -598,7 +598,7 @@ export const pricing: Pricing = {
         usage_based: false,
       },
       {
-        title: 'Designated customer success Team',
+        title: 'Designated Customer Success Team',
         plans: {
           free: false,
           pro: false,
