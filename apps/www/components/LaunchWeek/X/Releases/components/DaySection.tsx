b13a2c99b8cf205f732ba352a51123d19d27642d
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-12-11 15:24:39 +0100

    add hackathon

--- apps/www/components/LaunchWeek/X/Releases/components/DaySection.tsx
+++ apps/www/components/LaunchWeek/X/Releases/components/DaySection.tsx
@@ -30,7 +30,7 @@ const DaySection = ({ day }: { day: WeekDayProps }) => {
         >
           {day.dd}, {day.date}
         </div>
-        {day.shipped && (
+        {!!day.links && (
           <ul className="flex-1 h-full w-full justify-end grid grid-cols-2 md:flex flex-col gap-1">
             {day.links?.map((link) => (
               <li key={link.href}>
@@ -41,32 +41,9 @@ const DaySection = ({ day }: { day: WeekDayProps }) => {
         )}
       </div>
 
-      {/* Day Countdown */}
-      {!day.shipped && day.hasCountdown && (
-        <div className="flex items-center gap-2 h-5">
-          <svg
-            width="17"
-            height="17"
-            viewBox="0 0 17 17"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <g opacity="0.5">
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M4.32656 7.58047V5.98047C4.32656 3.77133 6.11742 1.98047 8.32656 1.98047C10.5357 1.98047 12.3266 3.77133 12.3266 5.98047V7.58047C13.2102 7.58047 13.9266 8.29681 13.9266 9.18047V13.1805C13.9266 14.0641 13.2102 14.7805 12.3266 14.7805H4.32656C3.44291 14.7805 2.72656 14.0641 2.72656 13.1805V9.18047C2.72656 8.29681 3.44291 7.58047 4.32656 7.58047ZM10.7266 5.98047V7.58047H5.92656V5.98047C5.92656 4.65499 7.00108 3.58047 8.32656 3.58047C9.65205 3.58047 10.7266 4.65499 10.7266 5.98047Z"
-                fill="#A0A0A0"
-              />
-            </g>
-          </svg>
-          <CountdownComponent date={day.published_at} showCard={false} />
-        </div>
-      )}
-
       {/* Day card */}
-      {day.shipped && day.steps.length > 0 && (
-        <div className="flex col-span-2">
+      <div className="flex col-span-2">
+        {day.shipped && day.steps.length > 0 ? (
           <Link
             href={day.blog!}
             className={cn(
@@ -109,8 +86,37 @@ const DaySection = ({ day }: { day: WeekDayProps }) => {
                   )
               )}
           </Link>
-        </div>
-      )}
+        ) : (
+          <div
+            className={cn(
+              `min-h-[210px] group aspect-[3.67/1] relative overflow-hidden flex-1 flex flex-col justify-end
+              bg-surface-100/10 border border-dashed border-[#14191B] text-[#8B9092]
+              rounded-xl p-4 sm:p-6 md:p-8 text-2xl bg-contain shadow-lg`,
+              cssGroup
+            )}
+          >
+            <div className="flex items-center gap-2 h-5">
+              <svg
+                width="17"
+                height="17"
+                viewBox="0 0 17 17"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g opacity="0.5">
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M4.32656 7.58047V5.98047C4.32656 3.77133 6.11742 1.98047 8.32656 1.98047C10.5357 1.98047 12.3266 3.77133 12.3266 5.98047V7.58047C13.2102 7.58047 13.9266 8.29681 13.9266 9.18047V13.1805C13.9266 14.0641 13.2102 14.7805 12.3266 14.7805H4.32656C3.44291 14.7805 2.72656 14.0641 2.72656 13.1805V9.18047C2.72656 8.29681 3.44291 7.58047 4.32656 7.58047ZM10.7266 5.98047V7.58047H5.92656V5.98047C5.92656 4.65499 7.00108 3.58047 8.32656 3.58047C9.65205 3.58047 10.7266 4.65499 10.7266 5.98047Z"
+                    fill="currentColor"
+                  />
+                </g>
+              </svg>
+              {day.hasCountdown && <CountdownComponent date={day.published_at} showCard={false} />}
+            </div>
+          </div>
+        )}
+      </div>
     </section>
   )
 }
