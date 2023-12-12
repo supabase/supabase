f13508f9b70eada108226790e2a4659b656fd489
Greg Richardson <greg.nmr@gmail.com>
2023-12-12 16:42:31 -0700

    refactor: jest env loading

--- packages/common/test/setup.ts
+++ packages/common/test/setup.ts
@@ -1,8 +1,8 @@
 import { config } from 'dotenv'
 import { statSync } from 'fs'
 
-// Use studio .env for now
+// Use studio .env.local for now
 const envPath = '../../apps/studio/.env.local'
 
 statSync(envPath)
-config({ path: '../../apps/studio/.env.local' })
+config({ path: envPath })
