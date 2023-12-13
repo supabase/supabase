1c39f604acedb5db0c89bc1fd09230428fdb9fb6
Joshen Lim <joshenlimek@gmail.com>
2023-12-13 14:42:41 +0700

    Replace all usage of meta.publications with react query

---
+++ apps/studio/data/database-publications/database-publications-query.ts
@@ -0,0 +1,67 @@
+import { UseQueryOptions, useQuery, useQueryClient } from '@tanstack/react-query'
+import { get } from 'data/fetchers'
+import { useCallback } from 'react'
+import { ResponseError } from 'types'
+import { databasePublicationsKeys } from './keys'
+
+export type DatabasePublicationsVariables = {
+  projectRef?: string
+  connectionString?: string
+}
+
+export async function getDatabasePublications(
+  { projectRef, connectionString }: DatabasePublicationsVariables,
+  signal?: AbortSignal
+) {
+  if (!projectRef) throw new Error('projectRef is required')
+
+  let headers = new Headers()
+  if (connectionString) headers.set('x-connection-encrypted', connectionString)
+
+  const { data, error } = await get('/platform/pg-meta/{ref}/publications', {
+    params: {
+      header: {
+        'x-connection-encrypted': connectionString!,
+      },
+      path: {
+        ref: projectRef,
+      },
+    },
+    headers,
+    signal,
+  })
+
+  if (error) throw error
+  return data
+}
+
+export type DatabasePublicationsData = Awaited<ReturnType<typeof getDatabasePublications>>
+export type DatabasePublicationsError = ResponseError
+
+export const useDatabasePublicationsQuery = <TData = DatabasePublicationsData>(
+  { projectRef, connectionString }: DatabasePublicationsVariables,
+  {
+    enabled = true,
+    ...options
+  }: UseQueryOptions<DatabasePublicationsData, DatabasePublicationsError, TData> = {}
+) =>
+  useQuery<DatabasePublicationsData, DatabasePublicationsError, TData>(
+    databasePublicationsKeys.list(projectRef),
+    ({ signal }) => getDatabasePublications({ projectRef, connectionString }, signal),
+    {
+      enabled: enabled && typeof projectRef !== 'undefined',
+      ...options,
+    }
+  )
+
+export const useDatabasePublicationsPrefetch = ({ projectRef }: DatabasePublicationsVariables) => {
+  const client = useQueryClient()
+
+  return useCallback(() => {
+    if (projectRef) {
+      client.prefetchQuery(databasePublicationsKeys.list(projectRef), ({ signal }) =>
+        getDatabasePublications({ projectRef }, signal)
+      )
+    }
+  }, [projectRef])
+}
--- apps/studio/pages/project/[ref]/database/replication.tsx
+++ apps/studio/pages/project/[ref]/database/replication.tsx
@@ -1,21 +1,28 @@
-import { useState } from 'react'
-import { observer } from 'mobx-react-lite'
 import { PermissionAction } from '@supabase/shared-types/out/constants'
+import { observer } from 'mobx-react-lite'
+import { useState } from 'react'
 
-import { useCheckPermissions, usePermissionsLoaded, useStore } from 'hooks'
-import { NextPageWithLayout } from 'types'
-import { DatabaseLayout } from 'components/layouts'
 import { PublicationsList, PublicationsTables } from 'components/interfaces/Database'
-import NoPermission from 'components/ui/NoPermission'
+import { DatabaseLayout } from 'components/layouts'
+import { useProjectContext } from 'components/layouts/ProjectLayout/ProjectContext'
 import { ScaffoldContainer, ScaffoldSection } from 'components/layouts/Scaffold'
+import NoPermission from 'components/ui/NoPermission'
+import { useDatabasePublicationsQuery } from 'data/database-publications/database-publications-query'
+import { useCheckPermissions, usePermissionsLoaded } from 'hooks'
+import { NextPageWithLayout } from 'types'
 
 // [Joshen] Technically, best that we have these as separate URLs
 // makes it easier to manage state, but foresee that this page might
 // be consolidated somewhere else eventually for better UX
 
 const DatabaseReplication: NextPageWithLayout = () => {
-  const { meta } = useStore()
-  const publications = meta.publications.list()
+  const { project } = useProjectContext()
+
+  const { data } = useDatabasePublicationsQuery({
+    projectRef: project?.ref,
+    connectionString: project?.connectionString,
+  })
+  const publications = data ?? []
 
   const canViewPublications = useCheckPermissions(
     PermissionAction.TENANT_SQL_ADMIN_READ,
@@ -37,7 +44,7 @@ const DatabaseReplication: NextPageWithLayout = () => {
           <div className="mb-4">
             <h3 className="mb-1 text-xl text-foreground">Database Replications</h3>
           </div>
-          {selectedPublicationId === undefined ? (
+          {selectedPublication === undefined ? (
             <PublicationsList onSelectPublication={setSelectedPublicationId} />
           ) : (
             <PublicationsTables
