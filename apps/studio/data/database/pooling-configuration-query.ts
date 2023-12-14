dfb802e9f78b4734249df9ce223932481da24e4b
Ivan Vasilov <vasilov.ivan@gmail.com>
2023-12-14 17:51:47 +0100

    Update the query and the mutation for the connection pooling to use data/fetchers methods.

--- apps/studio/data/database/pooling-configuration-query.ts
+++ apps/studio/data/database/pooling-configuration-query.ts
@@ -1,6 +1,5 @@
 import { useQuery, useQueryClient, UseQueryOptions } from '@tanstack/react-query'
-import { get } from 'lib/common/fetch'
-import { API_URL } from 'lib/constants'
+import { get } from 'data/fetchers'
 import { useCallback } from 'react'
 import { ResponseError } from 'types'
 import { databaseKeys } from './keys'
@@ -9,31 +8,16 @@ export type PoolingConfigurationVariables = {
   projectRef: string
 }
 
-export type PoolingConfiguration = {
-  db_dns_name: string
-  db_host: string
-  db_name: string
-  db_port: number
-  db_ssl: boolean
-  db_user: string
-  default_pool_size: number | null
-  ignore_startup_parameters: string
-  inserted_at: string
-  max_client_conn: number | null
-  pgbouncer_enabled: boolean
-  supavisor_enabled: boolean
-  pgbouncer_status: string
-  pool_mode: string
-  connectionString: string
-}
-
 export async function getPoolingConfiguration(
   { projectRef }: PoolingConfigurationVariables,
   signal?: AbortSignal
 ) {
-  const response = await get(`${API_URL}/projects/${projectRef}/config/pgbouncer`, { signal })
-  if (response.error) throw response.error
-  return response as PoolingConfiguration
+  const { data, error } = await get(`/platform/projects/{ref}/config/pgbouncer`, {
+    params: { path: { ref: projectRef } },
+    signal,
+  })
+  if (error) throw error
+  return data
 }
 
 export type PoolingConfigurationData = Awaited<ReturnType<typeof getPoolingConfiguration>>
