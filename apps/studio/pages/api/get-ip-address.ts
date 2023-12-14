e43b8a8421e0fee3ceb48e087f148127cafb1349
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2023-12-14 14:39:30 +0100

    check ips

--- apps/studio/pages/api/get-ip-address.ts
+++ apps/studio/pages/api/get-ip-address.ts
@@ -4,11 +4,12 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   let ipAddress = req.headers['x-real-ip'] as string
 
   const forwardedFor = req.headers['x-forwarded-for'] as string
+  const cfConnectingIp = req.headers['cf-connecting-ip'] as string
   if (!ipAddress && forwardedFor) {
     ipAddress = forwardedFor?.split(',').at(0) ?? 'Unknown'
   }
 
-  return res.status(200).json({ ipAddress })
+  return res.status(200).json({ ipAddress, forwardedFor, cfConnectingIp })
 }
 
 export default handler
