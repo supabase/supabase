91f8c69b43def9b31adcd699e7f5890b6ddc6e41
Charis Lam <26616127+charislam@users.noreply.github.com>
2023-12-18 15:44:24 -0500

    bug fix types

--- apps/docs/lib/storage.ts
+++ apps/docs/lib/storage.ts
@@ -2,17 +2,15 @@ export const LOCAL_STORAGE_KEYS = {
   SAVED_ORG_PROJECT_BRANCH: 'docs.ui.user.selected.org_project_branch',
 } as const
 
+type LocalStorageKey = (typeof LOCAL_STORAGE_KEYS)[keyof typeof LOCAL_STORAGE_KEYS]
+
 type StorageType = 'local' | 'session'
 
 function getStorage(storageType: StorageType) {
   return storageType === 'local' ? window.localStorage : window.sessionStorage
 }
 
-export function store(
-  storageType: StorageType,
-  key: keyof typeof LOCAL_STORAGE_KEYS,
-  value: string
-) {
+export function store(storageType: StorageType, key: LocalStorageKey, value: string) {
   if (typeof window === 'undefined') return
   const storage = getStorage(storageType)
 
@@ -23,13 +21,13 @@ export function store(
   }
 }
 
-export function retrieve(storageType: StorageType, key: keyof typeof LOCAL_STORAGE_KEYS) {
+export function retrieve(storageType: StorageType, key: LocalStorageKey) {
   if (typeof window === 'undefined') return
   const storage = getStorage(storageType)
   return storage.getItem(key)
 }
 
-export function remove(storageType: StorageType, key: keyof typeof LOCAL_STORAGE_KEYS) {
+export function remove(storageType: StorageType, key: LocalStorageKey) {
   if (typeof window === 'undefined') return
   const storage = getStorage(storageType)
   return storage.removeItem(key)
