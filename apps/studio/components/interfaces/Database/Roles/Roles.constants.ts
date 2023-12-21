6294dbaa52aa628f59dd7c2de859941fae009f21
Terry Sutton <saltcod@gmail.com>
2023-12-21 19:15:47 -0330

    Chore/rewrite roles form with shadcn (#19923)
    * Fix new role form
    
    * Rewrite form
    
    * Commit a sin against ts
    
    * Make the afterLabel a ReactNode.
    
    * Fix minor type issues.
    
    * Fix type
    
    * Types
    
    * Use lodash sortBy instead of the mutating array.sort.
    
    * Fix the order of the roles with active connections.
    
    * Sort the roles by lowercased names.
    
    ---------
    
    Co-authored-by: Ivan Vasilov <vasilov.ivan@gmail.com>

--- apps/studio/components/interfaces/Database/Roles/Roles.constants.ts
+++ apps/studio/components/interfaces/Database/Roles/Roles.constants.ts
@@ -15,7 +15,7 @@ export const SUPABASE_ROLES = [
   'pgsodium_keyiduser',
   'pgsodium_keymaker',
   'pgtle_admin',
-]
+] as const
 
 // [Joshen] This was originally in the Roles mobx store
 // Just keeping it for now in case we need to differ it from ^ SUPABASE_ROLES
@@ -30,32 +30,38 @@ export const SYSTEM_ROLES = [
   'pg_database_owner',
   'pg_read_all_data',
   'pg_write_all_data',
-]
+] as const
 
-export const ROLE_PERMISSIONS: any = {
+export const ROLE_PERMISSIONS = {
   can_login: {
     disabled: false,
     description: 'User can login',
+    grant_by_dashboard: true,
   },
   can_create_role: {
     disabled: false,
     description: 'User can create roles',
+    grant_by_dashboard: true,
   },
   can_create_db: {
     disabled: false,
     description: 'User can create databases',
+    grant_by_dashboard: true,
   },
   can_bypass_rls: {
     disabled: true,
     description: 'User bypasses every row level security policy',
+    grant_by_dashboard: false,
   },
   is_superuser: {
     disabled: true,
     description: 'User is a Superuser',
+    grant_by_dashboard: false,
   },
   is_replication_role: {
     disabled: true,
     description:
       'User can initiate streaming replication and put the system in and out of backup mode',
+    grant_by_dashboard: false,
   },
-}
+} as const
