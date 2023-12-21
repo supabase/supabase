cf0a4c6d4e9ac9900799001d223e15b13f77065c
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-12-21 14:19:08 +0100

    Update homepage customer stories (#19915)
    update homepage customer stories

--- apps/www/components/CustomerStories/index.tsx
+++ apps/www/components/CustomerStories/index.tsx
@@ -1,21 +1,27 @@
-import SectionHeader from 'components/UI/SectionHeader'
-import PostTypes from '~/types/post'
-import SectionContainer from '../Layouts/SectionContainer'
 import Link from 'next/link'
 import { GlassPanel } from 'ui'
-import TextLink from '../TextLink'
 
-const CustomerStories = ({ customerStories }: any) => {
-  const caseStudyThumbs = customerStories.map((blog: PostTypes, idx: number) => {
+import SectionContainer from '../Layouts/SectionContainer'
+import TextLink from '../TextLink'
+import SectionHeader from 'components/UI/SectionHeader'
+import customerStories from '~/data/CustomerStories'
+import Panel from '../Panel'
+
+const CustomerStories = () => {
+  // const selection = ['Pebblely', 'Chatbase', 'Mendable.ai']
+  const selection = ['Pebblely', 'Good Tape', 'Mendable.ai']
+  const customers = customerStories.filter((story: any) => selection.includes(story.organization))
+
+  const caseStudyThumbs = customers.map((customer: any, idx: number) => {
     return {
-      logo: blog.logo_inverse,
-      title: blog.title,
-      link: blog.url,
+      logo: customer.logo_inverse,
+      title: customer.title,
+      link: customer.url,
     }
   })
 
   return (
-    <SectionContainer>
+    <SectionContainer id="customers">
       <div className="mb-12">
         <SectionHeader
           title={'Infrastructure'}
@@ -24,24 +30,27 @@ const CustomerStories = ({ customerStories }: any) => {
           paragraph={
             'See how Supabase empowers companies of all sizes to accelerate their growth and streamline their work.'
           }
+          className="xl:w-1/2"
         />
       </div>
-      <div className="mx-auto mt-5 grid grid-cols-12 gap-8 lg:gap-12">
+      <div className="mx-auto mt-5 grid grid-cols-12 gap-5">
         {caseStudyThumbs.map((caseStudy: any, i: number) => (
           <Link
             href={`${caseStudy.link}`}
             key={caseStudy.title}
             className="col-span-12 md:col-span-4"
           >
-            <GlassPanel
-              {...caseStudy}
-              background={true}
-              showIconBg={true}
-              showLink={true}
-              hasLightIcon
-            >
-              {caseStudy.description}
-            </GlassPanel>
+            <Panel hasActiveOnHover outerClassName="h-full">
+              <GlassPanel
+                {...caseStudy}
+                background={false}
+                className="border-none"
+                showIconBg={true}
+                showLink={true}
+              >
+                {caseStudy.description}
+              </GlassPanel>
+            </Panel>
           </Link>
         ))}
       </div>
