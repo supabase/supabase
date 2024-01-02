e9c1966ab5b356e4ad0caed79b9e946790b3a967
Alaister Young <alaister@users.noreply.github.com>
2024-01-02 16:13:33 +1100

    chore: improve auth screens loading (#19940)
    * chore: improve auth screens loading
    
    * Update apps/studio/components/interfaces/Auth/Users/UsersList.tsx
    
    Co-authored-by: Joshen Lim <joshenlimek@gmail.com>
    
    * handle error
    
    * better pagination for users list
    
    * disgusting boolean logic
    
    * return user to previous page after deleting last user
    
    * only show reloading indicator when it's refetching
    
    * fix date formatting
    
    ---------
    
    Co-authored-by: Joshen Lim <joshenlimek@gmail.com>

--- apps/studio/components/interfaces/Auth/Users/UsersListItem.tsx
+++ apps/studio/components/interfaces/Auth/Users/UsersListItem.tsx
@@ -1,10 +1,10 @@
+import dayjs from 'dayjs'
 import { Badge } from 'ui'
 
 import SimpleCodeBlock from 'components/to-be-cleaned/SimpleCodeBlock'
 import Table from 'components/to-be-cleaned/Table'
 import { User } from 'data/auth/users-query'
 import UserDropdown from './UserDropdown'
-import { getDateFromIsoString } from './Users.utils'
 import { getDisplayName } from './UserListItem.utils'
 
 interface UserListItemProps {
@@ -15,8 +15,8 @@ interface UserListItemProps {
 
 const UserListItem = ({ user, canRemoveUser, canRemoveMFAFactors }: UserListItemProps) => {
   const isUserConfirmed = user.email_confirmed_at || user.phone_confirmed_at
-  const createdAt = getDateFromIsoString(user.created_at)
-  const lastSignedIn = getDateFromIsoString(user.last_sign_in_at)
+  const createdAt = dayjs(user.created_at)
+  const lastSignedIn = dayjs(user.last_sign_in_at)
 
   return (
     <Table.tr className="relative" key={user.id}>
