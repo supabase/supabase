a69170dc35be102dcf4225fd92102b89383a1181
Kamil Ogórek <kamil.ogorek@gmail.com>
2024-01-03 11:46:28 +0100

    ref: Enable non-error objects to be captured by Sentry

--- apps/studio/sentry.server.config.ts
+++ apps/studio/sentry.server.config.ts
@@ -6,8 +6,6 @@ Sentry.init({
   debug: false,
   ignoreErrors: [
     'ResizeObserver',
-    'Non-Error exception captured',
-    'Non-Error promise rejection',
     // [Joshen] We currently use stripe-js for customers to save their credit card data
     // I'm unable to reproduce this error on local, staging nor prod across chrome, safari or firefox
     // Based on https://github.com/stripe/stripe-js/issues/26, it seems like this error is safe to ignore,
