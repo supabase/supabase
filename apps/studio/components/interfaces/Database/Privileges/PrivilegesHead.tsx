d559b9f04b6eb68b0a8697b9c3543db086ab33e4
Alaister Young <a@alaisteryoung.com>
2024-01-04 00:31:50 +1100

    use shadcn select

--- apps/studio/components/interfaces/Database/Privileges/PrivilegesHead.tsx
+++ apps/studio/components/interfaces/Database/Privileges/PrivilegesHead.tsx
@@ -1,6 +1,16 @@
 import { PostgresSchema, PostgresTable } from '@supabase/postgres-meta'
 
-import { Button, Listbox } from 'ui'
+import {
+  Button,
+  Listbox,
+  SelectContent_Shadcn_,
+  SelectGroup_Shadcn_,
+  SelectItem_Shadcn_,
+  SelectLabel_Shadcn_,
+  SelectTrigger_Shadcn_,
+  SelectValue_Shadcn_,
+  Select_Shadcn_,
+} from 'ui'
 
 export interface PrivilegesHeadProps {
   selectedSchema: string
@@ -38,14 +48,14 @@ const PrivilegesHead = ({
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-4">
           <div className="w-[230px]">
-            <SchemasListbox
+            <SchemasSelect
               selectedSchema={selectedSchema}
               schemas={schemas}
               onChangeSchema={onChangeSchema}
             />
           </div>
           <div className="w-[230px]">
-            <TablesListbox
+            <TablesSelect
               selectedTable={selectedTable}
               tables={tables}
               onChangeTable={onChangeTable}
@@ -53,7 +63,7 @@ const PrivilegesHead = ({
           </div>
           <div className="h-[20px] w-px border-r border-scale-600"></div>
           <div className="w-[230px]">
-            <RolesListbox selectedRole={selectedRole} roles={roles} onChangeRole={onChangeRole} />
+            <RolesSelect selectedRole={selectedRole} roles={roles} onChangeRole={onChangeRole} />
           </div>
         </div>
 
@@ -81,7 +91,7 @@ const PrivilegesHead = ({
   )
 }
 
-const SchemasListbox = ({
+const SchemasSelect = ({
   selectedSchema,
   schemas,
   onChangeSchema,
@@ -91,25 +101,26 @@ const SchemasListbox = ({
   onChangeSchema: (schema: string) => void
 }) => {
   return (
-    <Listbox size="small" value={selectedSchema} onChange={onChangeSchema}>
-      <Listbox.Option disabled key="normal-schemas" value="normal-schemas" label="Schemas">
-        <p className="text-sm">Schemas</p>
-      </Listbox.Option>
-      {schemas.map((schema) => (
-        <Listbox.Option
-          key={schema.id}
-          value={schema.name}
-          label={schema.name}
-          addOnBefore={() => <span className="text-scale-900">schema</span>}
-        >
-          <span className="text-scale-1200 text-sm">{schema.name}</span>
-        </Listbox.Option>
-      ))}
-    </Listbox>
+    <Select_Shadcn_ value={selectedSchema} onValueChange={onChangeSchema}>
+      <SelectTrigger_Shadcn_>
+        <SelectValue_Shadcn_ placeholder="Select a schema" />
+      </SelectTrigger_Shadcn_>
+      <SelectContent_Shadcn_>
+        <SelectGroup_Shadcn_>
+          <SelectLabel_Shadcn_>Schemas</SelectLabel_Shadcn_>
+
+          {schemas.map((schema) => (
+            <SelectItem_Shadcn_ key={schema.name} value={schema.name}>
+              <span className="text-foreground-light">schema</span> {schema.name}
+            </SelectItem_Shadcn_>
+          ))}
+        </SelectGroup_Shadcn_>
+      </SelectContent_Shadcn_>
+    </Select_Shadcn_>
   )
 }
 
-const RolesListbox = ({
+const RolesSelect = ({
   selectedRole,
   roles,
   onChangeRole,
@@ -119,25 +130,26 @@ const RolesListbox = ({
   onChangeRole: (role: string) => void
 }) => {
   return (
-    <Listbox size="small" value={selectedRole} onChange={onChangeRole}>
-      <Listbox.Option disabled key="normal-roles" value="normal-roles" label="Roles">
-        <p className="text-sm">Roles</p>
-      </Listbox.Option>
-      {roles.map((role) => (
-        <Listbox.Option
-          key={role}
-          value={role}
-          label={role}
-          addOnBefore={() => <span className="text-scale-900">role</span>}
-        >
-          <span className="text-scale-1200 text-sm">{role}</span>
-        </Listbox.Option>
-      ))}
-    </Listbox>
+    <Select_Shadcn_ value={selectedRole} onValueChange={onChangeRole}>
+      <SelectTrigger_Shadcn_>
+        <SelectValue_Shadcn_ placeholder="Select a role" />
+      </SelectTrigger_Shadcn_>
+      <SelectContent_Shadcn_>
+        <SelectGroup_Shadcn_>
+          <SelectLabel_Shadcn_>Roles</SelectLabel_Shadcn_>
+
+          {roles.map((role) => (
+            <SelectItem_Shadcn_ key={role} value={role}>
+              <span className="text-foreground-light">role</span> {role}
+            </SelectItem_Shadcn_>
+          ))}
+        </SelectGroup_Shadcn_>
+      </SelectContent_Shadcn_>
+    </Select_Shadcn_>
   )
 }
 
-const TablesListbox = ({
+const TablesSelect = ({
   selectedTable,
   tables,
   onChangeTable,
@@ -147,21 +159,22 @@ const TablesListbox = ({
   onChangeTable: (table: string) => void
 }) => {
   return (
-    <Listbox size="small" value={selectedTable?.name} onChange={onChangeTable}>
-      <Listbox.Option disabled key="normal-tables" value="normal-tables" label="Tables">
-        <p className="text-sm">Tables</p>
-      </Listbox.Option>
-      {tables.map((table) => (
-        <Listbox.Option
-          key={table}
-          value={table}
-          label={table}
-          addOnBefore={() => <span className="text-scale-900">table</span>}
-        >
-          <span className="text-scale-1200 text-sm">{table}</span>
-        </Listbox.Option>
-      ))}
-    </Listbox>
+    <Select_Shadcn_ value={selectedTable?.name} onValueChange={onChangeTable}>
+      <SelectTrigger_Shadcn_>
+        <SelectValue_Shadcn_ placeholder="Select a table" />
+      </SelectTrigger_Shadcn_>
+      <SelectContent_Shadcn_>
+        <SelectGroup_Shadcn_>
+          <SelectLabel_Shadcn_>Tables</SelectLabel_Shadcn_>
+
+          {tables.map((table) => (
+            <SelectItem_Shadcn_ key={table} value={table}>
+              <span className="text-foreground-light">table</span> {table}
+            </SelectItem_Shadcn_>
+          ))}
+        </SelectGroup_Shadcn_>
+      </SelectContent_Shadcn_>
+    </Select_Shadcn_>
   )
 }
 
