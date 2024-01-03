07529fefec7dc69a7ebc3ac4984d80eff6cc1ba2
Alaister Young <a@alaisteryoung.com>
2024-01-03 16:15:59 +1100

    inline toggling

--- apps/studio/components/interfaces/Database/Privileges/Privileges.constants.ts
+++ apps/studio/components/interfaces/Database/Privileges/Privileges.constants.ts
@@ -1,2 +1,5 @@
 export const COLUMN_PRIVILEGE_TYPES = ['SELECT', 'INSERT', 'UPDATE'] as const
 export type ColumnPrivilegeType = (typeof COLUMN_PRIVILEGE_TYPES)[number]
+
+export const TABLE_PRIVILEGE_TYPES = ['DELETE'] as const
+export type TablePrivilegeType = (typeof TABLE_PRIVILEGE_TYPES)[number]
--- apps/studio/components/interfaces/Database/Privileges/PrivilegesTable.tsx
+++ apps/studio/components/interfaces/Database/Privileges/PrivilegesTable.tsx
@@ -6,10 +6,15 @@ import { useTablePrivilegesGrantMutation } from 'data/privileges/table-privilege
 import { useTablePrivilegesRevokeMutation } from 'data/privileges/table-privileges-revoke-mutation'
 import { useState } from 'react'
 import { IconLoader, Toggle } from 'ui'
-import { COLUMN_PRIVILEGE_TYPES, ColumnPrivilegeType } from './Privileges.constants'
+import {
+  COLUMN_PRIVILEGE_TYPES,
+  ColumnPrivilegeType,
+  TABLE_PRIVILEGE_TYPES,
+} from './Privileges.constants'
 import { PrivilegeColumnUI, TablePrivilegesUI } from './Privileges.types'
 import { getPrivilegesLoadingKey, isPrivilegesLoading } from './Privileges.utils'
 
+const ALL_PRIVILEGE_TYPES = [...COLUMN_PRIVILEGE_TYPES, ...TABLE_PRIVILEGE_TYPES]
 export interface PrivilegesTableProps {
   tableId: number
   tablePrivileges: TablePrivilegesUI[]
@@ -22,55 +27,83 @@ const PrivilegesTable = ({ tableId, tablePrivileges, columns, role }: Privileges
 
   const [loadingStates, setLoadingStates] = useState<Set<string>>(() => new Set())
 
-  // const { mutate: grantTable } = useTablePrivilegesGrantMutation({
-  //   onMutate(variables) {
-  //     setLoadingStates((prev) => {
-  //       const newSet = new Set(prev)
+  const { mutate: grantTable } = useTablePrivilegesGrantMutation({
+    onMutate(variables) {
+      setLoadingStates((prev) => {
+        const newSet = new Set(prev)
 
-  //       variables.grants.forEach((grant) => {
-  //         newSet.add(getPrivilegesLoadingKey('table', grant))
-  //       })
+        variables.grants.forEach((grant) => {
+          newSet.add(getPrivilegesLoadingKey('table', grant))
+        })
 
-  //       return newSet
-  //     })
-  //   },
-  //   onSettled(_data, _error, variables) {
-  //     setLoadingStates((prev) => {
-  //       const newSet = new Set(prev)
+        return newSet
+      })
+    },
+    onSettled(_data, _error, variables) {
+      setLoadingStates((prev) => {
+        const newSet = new Set(prev)
 
-  //       variables.grants.forEach((grant) => {
-  //         newSet.delete(getPrivilegesLoadingKey('table', grant))
-  //       })
+        variables.grants.forEach((grant) => {
+          newSet.delete(getPrivilegesLoadingKey('table', grant))
+        })
 
-  //       return newSet
-  //     })
-  //   },
-  // })
+        return newSet
+      })
+    },
+  })
 
-  // const { mutate: revokeTable } = useTablePrivilegesRevokeMutation({
-  //   onMutate(variables) {
-  //     setLoadingStates((prev) => {
-  //       const newSet = new Set(prev)
+  const { mutate: revokeTable } = useTablePrivilegesRevokeMutation({
+    onMutate(variables) {
+      setLoadingStates((prev) => {
+        const newSet = new Set(prev)
 
-  //       variables.revokes.forEach((revoke) => {
-  //         newSet.add(getPrivilegesLoadingKey('table', revoke))
-  //       })
+        variables.revokes.forEach((revoke) => {
+          newSet.add(getPrivilegesLoadingKey('table', revoke))
+        })
 
-  //       return newSet
-  //     })
-  //   },
-  //   onSettled(_data, _error, variables) {
-  //     setLoadingStates((prev) => {
-  //       const newSet = new Set(prev)
+        return newSet
+      })
+    },
+    onSettled(_data, _error, variables) {
+      setLoadingStates((prev) => {
+        const newSet = new Set(prev)
 
-  //       variables.revokes.forEach((revoke) => {
-  //         newSet.delete(getPrivilegesLoadingKey('table', revoke))
-  //       })
+        variables.revokes.forEach((revoke) => {
+          newSet.delete(getPrivilegesLoadingKey('table', revoke))
+        })
 
-  //       return newSet
-  //     })
-  //   },
-  // })
+        return newSet
+      })
+    },
+  })
+
+  const onToggleTable = (
+    privileges: readonly TablePrivilegesUI['privilege_type'][],
+    shouldGrant = true
+  ) => {
+    if (!project) return console.error('No project found')
+    if (shouldGrant) {
+      grantTable({
+        projectRef: project.ref,
+        connectionString: project.connectionString,
+        grants: privileges.map((privilege) => ({
+          relation_id: tableId,
+          grantee: role,
+          privilege_type: privilege,
+        })),
+      })
+    } else {
+      revokeTable({
+        projectRef: project.ref,
+        connectionString: project.connectionString,
+        revokes: privileges.map((privilege) => ({
+          relation_id: tableId,
+          grantee: role,
+          privilege_type: privilege,
+        })),
+      })
+    }
+  }
 
   const { mutate: grantColumn } = useColumnPrivilegesGrantMutation({
     onMutate(variables) {
@@ -170,8 +203,8 @@ const PrivilegesTable = ({ tableId, tablePrivileges, columns, role }: Privileges
       className="table-fixed mb-4"
       head={[
         <Table.th key="header-column"></Table.th>,
-        ...COLUMN_PRIVILEGE_TYPES.map((privilege) => {
-          // const checked = tablePrivileges.find((p) => p.privilege_type === privilege) !== undefined
+        ...ALL_PRIVILEGE_TYPES.map((privilege) => {
+          const checked = tablePrivileges.find((p) => p.privilege_type === privilege) !== undefined
           const isLoading = isPrivilegesLoading(loadingStates, 'table', {
             relation_id: tableId,
             grantee: role,
@@ -180,13 +213,23 @@ const PrivilegesTable = ({ tableId, tablePrivileges, columns, role }: Privileges
 
           return (
             <Table.th key={`header-${privilege}`}>
-              <div className="inline-flex items-baseline gap-2 relative">
-                {isLoading && (
-                  <div className="absolute inset-0 flex items-center justify-center w-full h-full bg-scale-300 z-10">
-                    <IconLoader className="animate-spin" size={16} />
-                  </div>
-                )}
-                {privilege.charAt(0) + privilege.slice(1).toLowerCase()}
+              <div className="inline-flex items-baseline gap-2">
+                <span>{privilege.charAt(0) + privilege.slice(1).toLowerCase()}</span>
+                <div className="relative">
+                  {isLoading && (
+                    <div className="absolute inset-0 flex items-center justify-center w-full h-full bg-surface-200 z-10">
+                      <IconLoader className="animate-spin" size={16} />
+                    </div>
+                  )}
+                  <Toggle
+                    checked={checked}
+                    onChange={() => {
+                      onToggleTable([privilege], !checked)
+                    }}
+                    disabled={isLoading}
+                    size="tiny"
+                  />
+                </div>
               </div>
             </Table.th>
           )
@@ -204,30 +247,33 @@ const PrivilegesTable = ({ tableId, tablePrivileges, columns, role }: Privileges
               grantee: role,
               privilege_type: privilege as ColumnPrivilegeType,
             })
-            const disabledDueToParent =
-              tablePrivileges.find((p) => p.privilege_type === privilege) !== undefined
-            //const disabled = isLoading || disabledDueToParent
-            const disabled = isLoading
 
             return (
               <Table.td key={privilege}>
                 {COLUMN_PRIVILEGE_TYPES.includes(privilege as any) && (
                   <div className="inline-flex relative">
                     {isLoading && (
-                      <div className="absolute inset-0 flex items-center justify-center w-full h-full bg-scale-200 z-10">
+                      <div className="absolute inset-0 flex items-center justify-center w-full h-full bg-surface-100 z-10">
                         <IconLoader className="animate-spin" size={14} />
                       </div>
                     )}
                     <Toggle
                       checked={checked}
-                      onChange={() =>
-                        onToggleColumn(column, [privilege as ColumnPrivilegeType], !checked)
-                      }
-                      disabled={disabled}
+                      onChange={() => {
+                        onToggleColumn(column, [privilege], !checked)
+                      }}
+                      disabled={isLoading}
                       size="tiny"
                     />
                   </div>
                 )}
+              </Table.td>
+            )
+          })}
+          {TABLE_PRIVILEGE_TYPES.map((privilege) => {
+            return (
+              <Table.td key={privilege}>
+                <span className="text-xs text-foreground-lighter">N/A</span>
               </Table.td>
             )
           })}
