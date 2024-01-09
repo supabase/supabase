dcfce8ec3397d7398325a8dd765ba6855c277557
Ivan Vasilov <vasilov.ivan@gmail.com>
2024-01-09 17:39:40 +0100

    fix: Don't fetch columns when fetching tables by default (#20157)
    * Only fetch the tables from the required schema in the MetaStore.
    
    * Don't include the columns in the getTables API call unless a flag is passed.
    
    * Use the new flag in the ForeignKeySelector.
    
    * Columns should be included for the schema visualizer.
    
    * Address comments.

--- apps/studio/stores/pgmeta/MetaStore.ts
+++ apps/studio/stores/pgmeta/MetaStore.ts
@@ -425,8 +425,9 @@ export default class MetaStore implements IMetaStore {
     const projectRef = project?.ref
     const connectionString = project?.connectionString
     const tables = await queryClient.fetchQuery({
-      queryKey: tableKeys.list(projectRef, 'public'),
-      queryFn: ({ signal }) => getTables({ projectRef, connectionString }, signal),
+      queryKey: tableKeys.list(projectRef, sourceTableSchema),
+      queryFn: ({ signal }) =>
+        getTables({ projectRef, connectionString, schema: sourceTableSchema }, signal),
     })
 
     const duplicatedTable = find(tables, { schema: sourceTableSchema, name: duplicatedTableName })
