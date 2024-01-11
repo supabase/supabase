6156133cfe40aa4766506f1a0f537fedd4819d92
Alaister Young <alaister@users.noreply.github.com>
2024-01-12 01:37:28 +1100

    fix: truncation while impersonating roles (#20349)
    fix: truncation while impersonating

--- apps/studio/components/grid/components/header/Header.tsx
+++ apps/studio/components/grid/components/header/Header.tsx
@@ -1,8 +1,18 @@
+import * as Tooltip from '@radix-ui/react-tooltip'
 import { PermissionAction } from '@supabase/shared-types/out/constants'
 import clsx from 'clsx'
 import saveAs from 'file-saver'
 import Papa from 'papaparse'
 import { ReactNode, useState } from 'react'
+
+import { useDispatch, useTrackedState } from 'components/grid/store'
+import { Filter, Sort, SupaTable } from 'components/grid/types'
+import { useProjectContext } from 'components/layouts/ProjectLayout/ProjectContext'
+import { useTableRowsCountQuery } from 'data/table-rows/table-rows-count-query'
+import { useTableRowsQuery } from 'data/table-rows/table-rows-query'
+import { useCheckPermissions, useStore, useUrlState } from 'hooks'
+import { useRoleImpersonationStateSnapshot } from 'state/role-impersonation-state'
+import { useTableEditorStateSnapshot } from 'state/table-editor'
 import {
   Button,
   DropdownMenu,
@@ -17,15 +27,6 @@ import {
   IconX,
   cn,
 } from 'ui'
-
-import { useDispatch, useTrackedState } from 'components/grid/store'
-import { Filter, Sort, SupaTable } from 'components/grid/types'
-import { useProjectContext } from 'components/layouts/ProjectLayout/ProjectContext'
-import { useTableRowsCountQuery } from 'data/table-rows/table-rows-count-query'
-import { useTableRowsQuery } from 'data/table-rows/table-rows-query'
-import { useCheckPermissions, useStore, useUrlState } from 'hooks'
-import { useRoleImpersonationStateSnapshot } from 'state/role-impersonation-state'
-import { useTableEditorStateSnapshot } from 'state/table-editor'
 import RLSBannerWarning from './RLSBannerWarning'
 import RefreshButton from './RefreshButton'
 import FilterDropdown from './filter'
@@ -240,6 +241,7 @@ const RowHeader = ({ table, sorts, filters }: RowHeaderProps) => {
   const snap = useTableEditorStateSnapshot()
 
   const roleImpersonationState = useRoleImpersonationStateSnapshot()
+  const isImpersonatingRole = roleImpersonationState.role !== undefined
 
   const [isExporting, setIsExporting] = useState(false)
 
@@ -351,9 +353,31 @@ const RowHeader = ({ table, sorts, filters }: RowHeaderProps) => {
             : `${selectedRows.size} row selected`}
         </span>
         {!allRowsSelected && totalRows > allRows.length && (
-          <Button type="link" onClick={() => onSelectAllRows()}>
-            Select all {totalRows} rows
-          </Button>
+          <Tooltip.Root delayDuration={0}>
+            <Tooltip.Trigger asChild>
+              <Button type="link" onClick={() => onSelectAllRows()} disabled={isImpersonatingRole}>
+                Select all {totalRows} rows
+              </Button>
+            </Tooltip.Trigger>
+
+            {isImpersonatingRole && (
+              <Tooltip.Portal>
+                <Tooltip.Content side="bottom">
+                  <Tooltip.Arrow className="radix-tooltip-arrow" />
+                  <div
+                    className={[
+                      'rounded bg-alternative py-1 px-2 leading-none shadow',
+                      'border border-background',
+                    ].join(' ')}
+                  >
+                    <span className="text-xs text-foreground">
+                      Table truncation is not supported when impersonating a role
+                    </span>
+                  </div>
+                </Tooltip.Content>
+              </Tooltip.Portal>
+            )}
+          </Tooltip.Root>
         )}
       </div>
       <div className="h-[20px] border-r border-gray-700" />
