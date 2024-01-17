20d52aeaeaa6167497c03ea0571379551ca1de49
Jordi Enric <jordi.err@gmail.com>
2024-01-17 12:18:28 +0100

    use shadcn tabs instead

--- apps/studio/components/interfaces/Settings/Vault/VaultNavTabs.tsx
+++ apps/studio/components/interfaces/Settings/Vault/VaultNavTabs.tsx
@@ -1,22 +1,27 @@
 import Link from 'next/link'
-import React from 'react'
-import { NavigationTabs, NavigationTabsItem } from 'ui'
+import React, { PropsWithChildren } from 'react'
+import { TabsContent_Shadcn_, TabsList_Shadcn_, TabsTrigger_Shadcn_, Tabs_Shadcn_ } from 'ui'
 
 type Props = {
   projRef: string
   activeTab: 'secrets' | 'keys'
 }
 
-const VaultNavTabs = (props: Props) => {
+const VaultNavTabs = (props: PropsWithChildren<Props>) => {
   return (
-    <NavigationTabs className="mb-4">
-      <NavigationTabsItem active={props.activeTab === 'secrets'}>
-        <Link href={`/project/${props.projRef}/settings/vault/secrets`}>Secrets Management</Link>
-      </NavigationTabsItem>
-      <NavigationTabsItem active={props.activeTab === 'keys'}>
-        <Link href={`/project/${props.projRef}/settings/vault/keys`}>Secrets Management</Link>
-      </NavigationTabsItem>
-    </NavigationTabs>
+    <Tabs_Shadcn_ value={props.activeTab}>
+      <TabsList_Shadcn_>
+        <TabsTrigger_Shadcn_ value="secrets" asChild>
+          <Link href={`/project/${props.projRef}/settings/vault/secrets`}>Secrets Management</Link>
+        </TabsTrigger_Shadcn_>
+        <TabsTrigger_Shadcn_ value="keys" asChild>
+          <Link href={`/project/${props.projRef}/settings/vault/keys`}>Encryption Keys</Link>
+        </TabsTrigger_Shadcn_>
+      </TabsList_Shadcn_>
+      <TabsContent_Shadcn_ className="mt-4" value={props.activeTab}>
+        {props.children}
+      </TabsContent_Shadcn_>
+    </Tabs_Shadcn_>
   )
 }
 
