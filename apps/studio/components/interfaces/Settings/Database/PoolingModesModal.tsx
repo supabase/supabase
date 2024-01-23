f2e5098a47e384fd467bf3434c45bc963267b11a
Joshen Lim <joshenlimek@gmail.com>
2024-01-23 17:17:02 +0700

    Update pooling modes modal copy

--- apps/studio/components/interfaces/Settings/Database/PoolingModesModal.tsx
+++ apps/studio/components/interfaces/Settings/Database/PoolingModesModal.tsx
@@ -1,5 +1,5 @@
-import { AlertTitle } from '@ui/components/shadcn/ui/alert'
 import { useParams } from 'common'
+import { Markdown } from 'components/interfaces/Markdown'
 import { usePoolingConfigurationQuery } from 'data/database/pooling-configuration-query'
 import { AlertTriangleIcon } from 'lucide-react'
 import { useDatabaseSettingsStateSnapshot } from 'state/database-settings'
@@ -26,6 +26,7 @@ export const PoolingModesModal = () => {
   return (
     <Modal
       hideFooter
+      size="xlarge"
       visible={snap.showPoolingModeHelper}
       header={
         <div className="w-full flex items-center justify-between">
@@ -44,70 +45,51 @@ export const PoolingModesModal = () => {
       onCancel={() => snap.setShowPoolingModeHelper(false)}
     >
       <Modal.Content className="py-4 text-sm flex flex-col gap-y-4">
-        <p className="text-foreground-light">
-          A "connection pool" is a system (external to Postgres) which manages Postgres connections
-          by allocating connections whenever clients make requests. Each pooling mode handles
-          connections differently.
-        </p>
-        <div className="flex flex-col gap-y-1">
-          <p>Transaction mode (Port: 6543)</p>
-          <p className="text-foreground-light">
-            Recommended if you are connecting from{' '}
-            <span className="text-foreground">serverless environments</span> since the same database
-            connection can be re-used across multiple clients connecting to the pooler.{' '}
-            <span className="text-amber-900">
-              Prepared statements don't work in transaction mode
-            </span>
-            .
-          </p>
-          {data?.pool_mode === 'session' && (
-            <Alert_Shadcn_ variant="warning">
-              <AlertTriangleIcon strokeWidth={2} />
-              <AlertTitle_Shadcn_>
-                Pooling mode is currently configured to use session mode
-              </AlertTitle_Shadcn_>
-              <AlertDescription_Shadcn_>
-                To use transaction mode on port 6543, change the pooling mode to transaction first
-                in the{' '}
-                <span
-                  tabIndex={0}
-                  className="text-foreground cursor-pointer underline underline-offset-2"
-                  onClick={() => {
-                    snap.setShowPoolingModeHelper(false)
-                    navigateToPoolerSettings()
-                  }}
-                >
-                  connection pooling settings
-                </span>
-                .
-              </AlertDescription_Shadcn_>
-            </Alert_Shadcn_>
-          )}
-        </div>
-        <div className="flex flex-col gap-y-1">
-          <p>Session mode (Port: 5432)</p>
-          <p className="text-foreground-light">
-            Similar to connecting to your database directly. There is{' '}
-            <span className="text-foreground">full support for prepared statements</span> but a new
-            database connection is created for each client and you{' '}
-            <span className="text-amber-900">might run into database connection limits</span>.
-          </p>
-        </div>
-        <p className="text-foreground-light">
-          If you want to use both session mode and transaction mode at the same time, change the
-          pooling mode to transaction first in the{' '}
-          <span
-            tabIndex={0}
-            className="text-foreground cursor-pointer underline underline-offset-2"
-            onClick={() => {
-              snap.setShowPoolingModeHelper(false)
-              navigateToPoolerSettings()
-            }}
-          >
-            connection pooling settings
-          </span>
-          . You can then connect to session mode on port 5432 and transaction mode on port 6543.
-        </p>
+        <Markdown
+          className="max-w-full [&>h3]:text-sm"
+          content={`
+A connection pooler is a system (external to Postgres) which manages Postgres connections
+by allocating connections whenever clients make requests. Each pooling mode handles
+connections differently.
+
+### Transaction mode
+Recommended if you are connecting from *serverless environments*. A connection is assigned to the client for the duration of a transaction. Two consecutive transactions from the same client could be executed over two different connections. Some session-based Postgres features such as prepared statements are *not available* with this option.
+`}
+        />
+        {data?.pool_mode === 'session' && (
+          <Alert_Shadcn_ variant="warning">
+            <AlertTriangleIcon strokeWidth={2} />
+            <AlertTitle_Shadcn_>
+              Pooling mode is currently configured to use session mode
+            </AlertTitle_Shadcn_>
+            <AlertDescription_Shadcn_>
+              To use transaction mode concurrently with session mode, change the pooling mode to
+              transaction first in the{' '}
+              <span
+                tabIndex={0}
+                className="text-foreground cursor-pointer underline underline-offset-2"
+                onClick={() => {
+                  snap.setShowPoolingModeHelper(false)
+                  navigateToPoolerSettings()
+                }}
+              >
+                connection pooling settings
+              </span>
+              . After this, you can use transaction mode on port 6543 and session mode on port 5432.
+            </AlertDescription_Shadcn_>
+          </Alert_Shadcn_>
+        )}
+
+        <Markdown
+          className="max-w-full [&>h3]:text-sm"
+          content={`
+### Session mode
+This mode is similar to connecting to your database directly. There is full support for prepared statements in this mode. When a new client connects, a connection is assigned to the client until it disconnects. You *might run into pooler connection limits* since the connection is held till the client disconnects.
+
+### Using session and transaction modes at the same time
+To get the best of both worlds, as a starting point, we recommend using session mode just when you need support for prepared statements and transaction mode in other cases.
+`}
+        />
       </Modal.Content>
       <Modal.Separator />
       <Modal.Content className="flex items-center justify-end pb-2">
