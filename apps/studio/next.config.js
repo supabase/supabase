b5b08bb29e3a8e35960b2714b991aefe3e4eb66a
Terry Sutton <saltcod@gmail.com>
2024-01-22 21:57:15 -0330

    Prettier

--- apps/studio/next.config.js
+++ apps/studio/next.config.js
@@ -47,7 +47,6 @@ const csp = [
         `style-src 'self' 'unsafe-inline' ${STYLE_SRC_URLS} ${VERCEL_LIVE_URL};`,
         `font-src 'self' ${FONT_SRC_URLS} ${VERCEL_LIVE_URL};`,
         `worker-src 'self' blob: data:;`
-
       ]
       : [
         `default-src 'self' ${DEFAULT_SRC_URLS};`,
