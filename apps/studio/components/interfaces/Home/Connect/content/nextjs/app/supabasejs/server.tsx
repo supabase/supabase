64d71e8e66abb9ab18358a4fc85064a0dd40f368
Terry Sutton <saltcod@gmail.com>
2024-01-23 16:16:48 -0330

    Add files

---
+++ apps/studio/components/interfaces/Home/Connect/content/nextjs/app/supabasejs/server.tsx
@@ -0,0 +1,50 @@
+import { ContentFileProps } from 'components/interfaces/Home/Connect/Connect.types'
+import SimpleCodeBlock from 'components/to-be-cleaned/SimpleCodeBlock'
+
+const ContentFile = ({ projectKeys }: ContentFileProps) => {
+  return (
+    <div>
+      <SimpleCodeBlock className="bash">
+        {`
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          try {
+            cookieStore.set({ name, value, ...options });
+          } catch (error) {
+            // The \`set\` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+        remove(name: string, options: CookieOptions) {
+          try {
+            cookieStore.set({ name, value: "", ...options });
+          } catch (error) {
+            // The \`delete\` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+      },
+    },
+  );
+};
+`}
+      </SimpleCodeBlock>
+    </div>
+  )
+}
+
+export default ContentFile
+
