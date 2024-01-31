202c93e823d798092a7da4f1c897220d7c7d3f2b
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2024-01-30 19:22:16 -0500

    add next-seo

--- apps/www/pages/careers/[id].tsx
+++ apps/www/pages/careers/[id].tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next"
+import { NextSeo } from 'next-seo'
 
 export const getServerSideProps = (context: any) => {
   const params = context.params
@@ -6,8 +7,27 @@ export const getServerSideProps = (context: any) => {
 }
 
 const Job: NextPage = ({ id }: any) => {
+  const meta_title = 'Careers | Supabase'
+  const meta_description = 'Help build software developers love'
+
   return (
+    <>
+    <NextSeo
+        title={meta_title}
+        description={meta_description}
+        openGraph={{
+          title: meta_title,
+          description: meta_description,
+          url: `https://supabase.com/careers`,
+          // images: [
+          //   {
+          //     url: `https://supabase.com${basePath}/images/career/careers_og.jpg`,
+          //   },
+          // ],
+        }}
+      />
     <div>{id}</div>
+    </>
   )
 }
 
