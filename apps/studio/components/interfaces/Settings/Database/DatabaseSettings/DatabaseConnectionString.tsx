2dc412c50a3388206dde7b71583fbe49949d3840
Joel Lee <lee.yi.jie.joel@gmail.com>
2024-01-31 15:48:03 +0800

    feat: add warning to connection settings about SQLAlchemy dialect (#20858)
    * Update DatabaseConnectionString.tsx
    
    * fix: add dropdown for SQLAlchemy connections
    
    * fix: remove redundant code
    
    * Convert dropdowns to collapsibles
    
    * ci: Autofix updates from GitHub workflow
    
    * Remove unused states
    
    * Lint
    
    ---------
    
    Co-authored-by: joel <joel@joels-MacBook-Pro.local>
    Co-authored-by: Terry Sutton <saltcod@gmail.com>
    Co-authored-by: github-tidy-bot <github-tidy-bot@supabase.com>
    Co-authored-by: Joshen Lim <joshenlimek@gmail.com>

--- apps/studio/components/interfaces/Settings/Database/DatabaseSettings/DatabaseConnectionString.tsx
+++ apps/studio/components/interfaces/Settings/Database/DatabaseSettings/DatabaseConnectionString.tsx
@@ -2,7 +2,17 @@ import * as Tooltip from '@radix-ui/react-tooltip'
 import { useParams, useTelemetryProps } from 'common'
 import { useRouter } from 'next/router'
 import { useEffect, useRef, useState } from 'react'
-import { Button, IconChevronDown, IconExternalLink, Input, Separator, Tabs } from 'ui'
+import {
+  Button,
+  CollapsibleContent_Shadcn_,
+  CollapsibleTrigger_Shadcn_,
+  Collapsible_Shadcn_,
+  IconChevronDown,
+  IconExternalLink,
+  Input,
+  Separator,
+  Tabs,
+} from 'ui'
 
 import { useProjectContext } from 'components/layouts/ProjectLayout/ProjectContext'
 import AlertError from 'components/ui/AlertError'
@@ -49,7 +59,6 @@ export const DatabaseConnectionString = () => {
   const [poolingMode, setPoolingMode] = useState<'transaction' | 'session' | 'statement'>(
     'transaction'
   )
-  const [showUriSyntax, setShowUriSyntax] = useState(false)
   const [selectedTab, setSelectedTab] = useState<
     'uri' | 'psql' | 'golang' | 'jdbc' | 'dotnet' | 'nodejs' | 'php' | 'python'
   >('uri')
@@ -213,59 +222,91 @@ export const DatabaseConnectionString = () => {
           <>
             <Separator />
             <Panel.Content className="!py-3 space-y-2">
-              <div
-                className="flex items-center gap-x-2 transition cursor-pointer opacity-75 hover:opacity-100"
-                onClick={() => setShowUriSyntax(!showUriSyntax)}
-              >
-                <p className="text-xs text-foreground">
-                  How to connect to a different database or switch to another user
-                </p>
-                <IconChevronDown
-                  strokeWidth={1.5}
-                  className={`transition ${showUriSyntax ? '-rotate-180' : ''}`}
-                />
-              </div>
-              {showUriSyntax && (
-                <div className="text-foreground-light">
-                  <p className="text-xs">
-                    You can use the following URI format to switch to a different database or user
-                    {usePoolerConnection ? ' when using connection pooling' : ''}.
-                  </p>
-                  <p className="text-sm font-mono tracking-tight text-foreground-lighter">
-                    {poolerConnStringSyntax.map((x, idx) => {
-                      if (x.tooltip) {
-                        return (
-                          <Tooltip.Root key={`syntax-${idx}`} delayDuration={0}>
-                            <Tooltip.Trigger asChild>
-                              <span className="text-foreground text-xs">{x.value}</span>
-                            </Tooltip.Trigger>
-                            <Tooltip.Portal>
+              <Collapsible_Shadcn_>
+                <CollapsibleTrigger_Shadcn_ className="group [&[data-state=open]>div>svg]:!-rotate-180">
+                  <div className="flex items-center gap-x-2 w-full">
+                    <p className="text-xs text-foreground-light group-hover:text-foreground transition">
+                      How to connect to a different database or switch to another user
+                    </p>
+                    <IconChevronDown
+                      className="transition-transform duration-200"
+                      strokeWidth={1.5}
+                      size={14}
+                    />
+                  </div>
+                </CollapsibleTrigger_Shadcn_>
+                <CollapsibleContent_Shadcn_ className="my-2">
+                  <div className="text-foreground-light">
+                    <p className="text-xs">
+                      You can use the following URI format to switch to a different database or user
+                      {usePoolerConnection ? ' when using connection pooling' : ''}.
+                    </p>
+                    <p className="text-sm font-mono tracking-tight text-foreground-lighter">
+                      {poolerConnStringSyntax.map((x, idx) => {
+                        if (x.tooltip) {
+                          return (
+                            <Tooltip.Root key={`syntax-${idx}`} delayDuration={0}>
+                              <Tooltip.Trigger asChild>
+                                <span className="text-foreground text-xs">{x.value}</span>
+                              </Tooltip.Trigger>
                               <Tooltip.Portal>
-                                <Tooltip.Content side="bottom">
-                                  <Tooltip.Arrow className="radix-tooltip-arrow" />
-                                  <div
-                                    className={[
-                                      'rounded bg-alternative py-1 px-2 leading-none shadow',
-                                      'border border-background',
-                                    ].join(' ')}
-                                  >
-                                    <span className="text-xs text-foreground">{x.tooltip}</span>
-                                  </div>
-                                </Tooltip.Content>
+                                <Tooltip.Portal>
+                                  <Tooltip.Content side="bottom">
+                                    <Tooltip.Arrow className="radix-tooltip-arrow" />
+                                    <div
+                                      className={[
+                                        'rounded bg-alternative py-1 px-2 leading-none shadow',
+                                        'border border-background',
+                                      ].join(' ')}
+                                    >
+                                      <span className="text-xs text-foreground">{x.tooltip}</span>
+                                    </div>
+                                  </Tooltip.Content>
+                                </Tooltip.Portal>
                               </Tooltip.Portal>
-                            </Tooltip.Portal>
-                          </Tooltip.Root>
-                        )
-                      } else {
-                        return (
-                          <span key={`syntax-${idx}`} className="text-xs">
-                            {x.value}
-                          </span>
-                        )
-                      }
-                    })}
-                  </p>
-                </div>
+                            </Tooltip.Root>
+                          )
+                        } else {
+                          return (
+                            <span key={`syntax-${idx}`} className="text-xs">
+                              {x.value}
+                            </span>
+                          )
+                        }
+                      })}
+                    </p>
+                  </div>
+                </CollapsibleContent_Shadcn_>
+              </Collapsible_Shadcn_>
+
+              {selectedTab === 'python' && (
+                <Collapsible_Shadcn_>
+                  <CollapsibleTrigger_Shadcn_ className="group [&[data-state=open]>div>svg]:!-rotate-180">
+                    <div className="flex items-center gap-x-2 w-full">
+                      <p className="text-xs text-foreground-light group-hover:text-foreground transition">
+                        Connecting to SQL Alchemy
+                      </p>
+                      <IconChevronDown
+                        className="transition-transform duration-200"
+                        strokeWidth={1.5}
+                        size={14}
+                      />
+                    </div>
+                  </CollapsibleTrigger_Shadcn_>
+                  <CollapsibleContent_Shadcn_ className="my-2">
+                    <div className="text-foreground-light text-xs grid gap-2">
+                      <p>
+                        Please use <code>postgresql://</code> instead of <code>postgres://</code> as
+                        your dialect when connecting via SQLAlchemy.
+                      </p>
+                      <p>
+                        Example:
+                        <code>create_engine("postgresql+psycopg2://...")</code>
+                      </p>
+                      <p className="text-sm font-mono tracking-tight text-foreground-lighter"></p>
+                    </div>
+                  </CollapsibleContent_Shadcn_>
+                </Collapsible_Shadcn_>
               )}
             </Panel.Content>
           </>
