6e72aed7dba8b2b751fa1681d9dfdd210030223e
Joshen Lim <joshenlimek@gmail.com>
2024-01-31 11:56:23 +0700

    Fix

--- apps/studio/components/interfaces/ProjectCreation/ProjectCreation.utils.ts
+++ apps/studio/components/interfaces/ProjectCreation/ProjectCreation.utils.ts
@@ -4,7 +4,8 @@ import { pluckObjectFields } from 'lib/helpers'
 export function getAvailableRegions(cloudProvider: CloudProvider): Region {
   if (cloudProvider === 'AWS') {
     return process.env.NEXT_PUBLIC_ENVIRONMENT === 'staging'
-      ? pluckObjectFields(AWS_REGIONS, ['SOUTHEAST_ASIA'])
+      ? // [Joshen] TEMP FIX, CHANGE BACK TO APSE1
+        pluckObjectFields(AWS_REGIONS, ['NORTHEAST_ASIA'])
       : AWS_REGIONS
   } else if (cloudProvider === 'FLY') {
     return FLY_REGIONS
--- apps/studio/lib/constants/infrastructure.ts
+++ apps/studio/lib/constants/infrastructure.ts
@@ -18,10 +18,7 @@ export const AWS_REGIONS = {
   CENTRAL_EU: 'Central EU (Frankfurt)',
   SOUTH_ASIA: 'South Asia (Mumbai)',
   // [Joshen] Temp block apse1 for prod
-  ...(process.env.NEXT_PUBLIC_ENVIRONMENT === 'staging' ||
-  process.env.NEXT_PUBLIC_ENVIRONMENT === 'local'
-    ? { SOUTHEAST_ASIA: 'Southeast Asia (Singapore)' }
-    : {}),
+  // SOUTHEAST_ASIA: 'Southeast Asia (Singapore)'
   NORTHEAST_ASIA: 'Northeast Asia (Tokyo)',
   NORTHEAST_ASIA_2: 'Northeast Asia (Seoul)',
   OCEANIA: 'Oceania (Sydney)',
@@ -35,8 +32,9 @@ export const FLY_REGIONS = {
   SOUTHEAST_ASIA: 'Singapore',
 } as const
 
+// [Joshen] TEMP FIX, CHANGE BACK TO APSE1
 export const AWS_REGIONS_DEFAULT =
-  process.env.NEXT_PUBLIC_ENVIRONMENT !== 'prod' ? AWS_REGIONS.SOUTHEAST_ASIA : AWS_REGIONS.WEST_US
+  process.env.NEXT_PUBLIC_ENVIRONMENT !== 'prod' ? AWS_REGIONS.NORTHEAST_ASIA : AWS_REGIONS.WEST_US
 
 // TO DO, change default to US region for prod
 const FLY_REGIONS_DEFAULT = FLY_REGIONS.SOUTHEAST_ASIA
