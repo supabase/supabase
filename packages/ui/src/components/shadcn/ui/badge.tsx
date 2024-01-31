a506587a7128f42d4edc1493ed1da0fbc391f9fc
Ivan Vasilov <vasilov.ivan@gmail.com>
2024-01-31 11:29:20 +0100

    fix: Change all imports in the `ui` package which import via the @ui shortcut. (#20745)
    * Change all imports in the ui package which import via the @ui shortcut.
    
    * Fix also all shadcn stories.

--- packages/ui/src/components/shadcn/ui/badge.tsx
+++ packages/ui/src/components/shadcn/ui/badge.tsx
@@ -1,7 +1,7 @@
-import * as React from 'react'
 import { cva, type VariantProps } from 'class-variance-authority'
+import * as React from 'react'
 
-import { cn } from '@ui/lib/utils'
+import { cn } from '../../../lib/utils/cn'
 
 const badgeVariants = cva(
   'inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-opacity-10',
