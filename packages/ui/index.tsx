18c790c846529df106cd52f6e6785415d1f416ec
Ivan Vasilov <vasilov.ivan@gmail.com>
2024-02-01 14:38:40 +0100

    Migrate SchemaTableNode.

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -194,10 +194,6 @@ export { ScrollArea, ScrollBar } from './src/components/shadcn/ui/scroll-area'
 
 export { Separator } from './src/components/shadcn/ui/separator'
 
-// patterns
-
-export * from './src/patterns/SchemaTableNode'
-
 // links
 
 export * from './src/components/TextLink'
