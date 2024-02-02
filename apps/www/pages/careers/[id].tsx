ce00b76e7b29dc580205c502bd010be67dcc424d
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2024-02-01 23:28:29 -0500

    init application ui

--- apps/www/pages/careers/[id].tsx
+++ apps/www/pages/careers/[id].tsx
@@ -1,19 +1,27 @@
-import { NextPage } from "next"
+import { NextPage } from 'next'
 import { NextSeo } from 'next-seo'
-import DefaultLayout from "~/components/Layouts/Default"
+import DefaultLayout from '~/components/Layouts/Default'
+import { Button, Input } from 'ui'
+import { TextArea } from '@ui/components/shadcn/ui/text-area'
 
-export const getServerSideProps = (context: any) => {
-  const params = context.params
-  return { props: { id: params!.id } }
+export const getServerSideProps = async (context: any) => {
+  const id = context.params!.id
+  const response = await fetch(
+    `https://boards-api.greenhouse.io/v1/boards/supabase/jobs/${id}?questions=true`
+  )
+  const data = await response.json()
+  return { props: { data } }
 }
 
-const Job: NextPage = ({ id }: any) => {
+const Job: NextPage = ({ data }: any) => {
   const meta_title = 'Careers | Supabase'
   const meta_description = 'Help build software developers love'
 
+  console.log(data)
+
   return (
     <>
-    <NextSeo
+      <NextSeo
         title={meta_title}
         description={meta_description}
         openGraph={{
@@ -28,7 +36,86 @@ const Job: NextPage = ({ id }: any) => {
         }}
       />
       <DefaultLayout>
-        <div>{id}</div>
+        <div className="container relative mx-auto px-6 py-10 lg:pt-12 lg:px-16 xl:px-20 text-center space-y-4">
+          <h1 className="text-sm text-brand md:text-base">{data.location.name}</h1>
+          <h2 className="text-3xl md:text-4xl xl:text-5xl lg:max-w-2xl xl:max-w-3xl lg:mx-auto tracking-[-1.5px]">
+            {data.title}
+          </h2>
+          <a href="#application">
+            <Button className="text-white xl:text-sm mt-4">Apply Now</Button>
+          </a>
+        </div>
+        <div className="container mx-auto px-6 py-10 lg:pt-12 lg:px-16 xl:px-20 space-y-10">
+          <iframe
+            title="Who we hire at Supabase"
+            className="w-full rounded-md aspect-video"
+            src="https://www.youtube-nocookie.com/embed/-BG9XptyCKI?si=28Yk9yYRrEhdQJ0k"
+            allow="autoplay; modestbranding; encrypted-media"
+            allowFullScreen
+          />
+          <div dangerouslySetInnerHTML={{ __html: data.content }} />
+        </div>
+        <div
+          id="application"
+          className="container mx-auto px-6 py-10 lg:pt-12 lg:px-16 xl:px-20 space-y-10"
+        >
+          <div className="flex items-center justify-between">
+            <h2 className="text-2xl">Apply for this Job</h2>
+            <div className="space-x-1 text-sm">
+              <span className="text-red-900">*</span>
+              <span className="text-foreground-light">Required</span>
+            </div>
+          </div>
+          <div className="space-y-4">
+            {data.questions.map((question: any) =>
+              question.fields.map((item: any, i: number) => {
+                if (item.type == 'input_text') {
+                  return (
+                    <Input
+                      key={i}
+                      label={
+                        <div>
+                          {question.label}
+                          {question.required ? <span className="ml-1 text-red-900">*</span> : null}
+                        </div>
+                      }
+                      required={question.required}
+                    />
+                  )
+                }
+                if (item.type == 'input_file') {
+                  return (
+                    <Input
+                      key={i}
+                      label={
+                        <div>
+                          {question.label}
+                          {question.required ? <span className="ml-1 text-red-900">*</span> : null}
+                        </div>
+                      }
+                      type="file"
+                      required={question.required}
+                    />
+                  )
+                }
+                if (item.type == 'textarea') {
+                  return (
+                    <div>
+                      <div className="block text-foreground-light text-sm mb-2">
+                        {question.label}
+                        {question.required ? <span className="ml-1 text-red-900">*</span> : null}
+                      </div>
+                      <TextArea key={i} />
+                    </div>
+                  )
+                }
+              })
+            )}
+            <div className="!mt-8">
+              <Button size="medium">Submit Application</Button>
+            </div>
+          </div>
+        </div>
       </DefaultLayout>
     </>
   )
