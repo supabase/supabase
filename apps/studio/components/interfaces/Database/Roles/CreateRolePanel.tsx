c5d2f49ff21d25c2e56785caeea8e61e488a9faf
Joshen Lim <joshenlimek@gmail.com>
2024-02-07 21:27:55 +0800

    Revert

--- apps/studio/components/interfaces/Database/Roles/CreateRolePanel.tsx
+++ apps/studio/components/interfaces/Database/Roles/CreateRolePanel.tsx
@@ -71,7 +71,7 @@ const CreateRolePanel = ({ visible, onClose }: CreateRolePanelProps) => {
 
   const handleClose = () => {
     onClose()
-    form.reset()
+    form.reset(initialValues)
   }
 
   return (
