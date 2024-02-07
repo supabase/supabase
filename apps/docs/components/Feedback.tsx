6507f6de5ec0640079b0420d2e7d5ff2e88dc3ed
Charis Lam <26616127+charislam@users.noreply.github.com>
2024-02-07 18:48:23 -0500

    fix: style feedback component

--- apps/docs/components/Feedback.tsx
+++ apps/docs/components/Feedback.tsx
@@ -1,49 +1,136 @@
-import { AnimatePresence, motion } from 'framer-motion'
-import { type MouseEvent, type PropsWithChildren, useRef, useState, } from 'react'
+import { type Variants, motion, MotionConfig, useReducedMotion } from 'framer-motion'
+import { useMemo, useRef, useState } from 'react'
 
-import { Button, IconCheck, IconX, cn } from 'ui'
+import { IconCheck, IconDiscussions, IconX, cn } from 'ui'
 
-type FeedbackButtonProps = {
-	onClick: (e: MouseEvent) => void
+type Response = 'yes' | 'no'
+
+const buttonClasses = cn(
+  'inline-flex items-center justify-center py-1 px-1',
+  'text-center font-regular text-foreground text-xs',
+  'ease-out duration-200 transition-all',
+  'rounded-md border border-strong hover:border-stronger bg-transparent',
+  'outline-none focus-visible:outline-4 focus-visible:outline-offset-1 focus-visible:outline-border-strong'
+)
+
+const yesVariants: Variants = {
+  yes: {
+    opacity: 1,
+  },
+  no: {
+    opacity: 0,
+    transitionEnd: {
+      visibility: 'hidden',
+    },
+  },
+  default: {
+    opacity: 1,
+  },
 }
 
-function FeedbackButton({ children }: PropsWithChildren) {
-	return (
-		<AnimatePresence>
-			<motion.button>{children}</motion.button>
-		</AnimatePresence>
-	)
+function createNoVariants(reducedMotion: boolean): Variants {
+  return {
+    yes: {
+      opacity: 0,
+      transitionEnd: {
+        visibility: 'hidden',
+      },
+    },
+    no: {
+      opacity: 1,
+      ...(reducedMotion
+        ? null
+        : {
+            translateX: 'calc(-1 * (100% + 0.5rem))',
+            transition: {
+              translateX: {
+                delay: 0.3,
+                duration: 0.5,
+                type: 'tween',
+              },
+            },
+          }),
+    },
+    default: {
+      opacity: 1,
+    },
+  }
+}
+
+const feedbackVariants: Variants = {
+  yes: {
+    opacity: 1,
+  },
+  no: {
+    opacity: 1,
+    // better coordination with sliding x button
+    transition: {
+      delay: 0.1,
+    },
+  },
+  default: {
+    opacity: 0,
+    visibility: 'hidden',
+  },
 }
 
 function Feedback() {
-	const [selected, setSelected] = useState<'yes' | 'no' | null>(null)
-	const feedbackMessageRef = useRef<HTMLParagraphElement>(null)
+  const [response, setResponse] = useState<Response | null>(null)
+  const feedbackButtonRef = useRef<HTMLButtonElement>(null)
+  const reducedMotion = useReducedMotion()
 
-	return (
-		<div className="flex gap-2 items-center mb-2">
-			{(!selected || selected === 'yes') && <Button
-				type="outline"
-				className={cn('px-1', selected === 'yes' && 'bg-green-500')}
-				onClick={() => setSelected('yes')}
-			>
-				<IconCheck />
-				<span className="sr-only">Yes</span>
-			</Button>}
-			{(!selected || selected === 'no') && <Button
-				type="outline"
-				className={cn('px-1', selected === 'no' && 'bg-red-500')}
-				onClick={() => setSelected('no')}
-			>
-				<IconX />
-				<span className="sr-only">No</span>
-			</Button>}
-			{selected && (
-				<p ref={feedbackMessageRef} className="text-[0.8rem] text-foreground-lighter">
-					{selected === 'no' ? <>How can we improve?</> : <>What went well?</>}
-				</p>
-			)}
-		</div>
-	)
+  const isYes = response === 'yes'
+  const isNo = response === 'no'
+  const animate = isYes ? 'yes' : isNo ? 'no' : 'default'
+
+  const noVariants = useMemo(() => createNoVariants(reducedMotion), [reducedMotion])
+
+  function handleResponse(response: Response) {
+    setResponse(response)
+    feedbackButtonRef.current?.focus()
+  }
+
+  return (
+    <>
+      <div className="relative flex gap-2 items-center mb-2">
+        <motion.button
+          className={cn(
+            buttonClasses,
+            'hover:text-brand-600',
+            isYes && 'text-brand-600 border-stronger'
+          )}
+          onClick={() => handleResponse('yes')}
+          variants={yesVariants}
+          animate={animate}
+        >
+          <IconCheck />
+          <span className="sr-only">Yes</span>
+        </motion.button>
+        <motion.button
+          className={cn(
+            buttonClasses,
+            'hover:text-warning-600',
+            isNo && 'text-warning-600 border-stronger'
+          )}
+          onClick={() => handleResponse('no')}
+          variants={noVariants}
+          animate={animate}
+        >
+          <IconX />
+          <span className="sr-only">No</span>
+        </motion.button>
+      </div>
+      <motion.button
+        ref={feedbackButtonRef}
+        className={cn('flex items-center gap-2', 'text-[0.8rem] text-foreground-lighter')}
+        variants={feedbackVariants}
+        animate={animate}
+      >
+        {isYes ? <>What went well?</> : <>How can we improve?</>}
+        <IconDiscussions size="tiny" />
+      </motion.button>
+    </>
+  )
 }
 
 export { Feedback }
--- apps/docs/layouts/guides/index.tsx
+++ apps/docs/layouts/guides/index.tsx
@@ -14,176 +14,179 @@ import { Feedback } from '~/components/Feedback'
 import { LayoutMainContent } from '../DefaultLayout'
 
 interface Props {
-	meta: {
-		title: string
-		description?: string // used in meta tags
-		hide_table_of_contents?: boolean
-		breadcrumb?: string
-		subtitle?: string // used on the page under the H1
-		footerHelpType?: FooterHelpCalloutType
-		video?: string
-		tocVideo?: string
-		canonical?: string
-	}
-	editLink?: string
-	children: any
-	toc?: any
-	currentPage?: string
-	hideToc?: boolean
+  meta: {
+    title: string
+    description?: string // used in meta tags
+    hide_table_of_contents?: boolean
+    breadcrumb?: string
+    subtitle?: string // used on the page under the H1
+    footerHelpType?: FooterHelpCalloutType
+    video?: string
+    tocVideo?: string
+    canonical?: string
+  }
+  editLink?: string
+  children: any
+  toc?: any
+  currentPage?: string
+  hideToc?: boolean
 }
 
 const Layout: FC<Props> = (props) => {
-	const [hash] = useHash()
+  const [hash] = useHash()
 
-	const articleRef = useRef()
-	const [tocList, setTocList] = useState([])
+  const articleRef = useRef()
+  const [tocList, setTocList] = useState([])
 
-	const { asPath } = useRouter()
-	const router = useRouter()
+  const { asPath } = useRouter()
+  const router = useRouter()
 
-	const EDIT_BUTTON_EXCLUDE_LIST = ['/404']
+  const EDIT_BUTTON_EXCLUDE_LIST = ['/404']
 
-	useEffect(() => {
-		if (hash && tocList.length > 0) {
-			highlightSelectedTocItem(hash as string)
-		}
-	}, [hash, JSON.stringify(tocList)])
+  useEffect(() => {
+    if (hash && tocList.length > 0) {
+      highlightSelectedTocItem(hash as string)
+    }
+  }, [hash, JSON.stringify(tocList)])
 
-	useEffect(() => {
-		const articleEl = articleRef.current as HTMLElement
+  useEffect(() => {
+    const articleEl = articleRef.current as HTMLElement
 
-		if (!articleRef.current) return
-		const headings = Array.from(articleEl.querySelectorAll('h2, h3'))
-		const newHeadings = headings
-			.filter((heading) => heading.id)
-			.map((heading) => {
-				const text = heading.textContent.replace('#', '')
-				const link = heading.querySelector('a').getAttribute('href')
-				const level = heading.tagName === 'H2' ? 2 : 3
-				return { text, link, level }
-			})
-		setTocList(newHeadings)
-	}, [])
+    if (!articleRef.current) return
+    const headings = Array.from(articleEl.querySelectorAll('h2, h3'))
+    const newHeadings = headings
+      .filter((heading) => heading.id)
+      .map((heading) => {
+        const text = heading.textContent.replace('#', '')
+        const link = heading.querySelector('a').getAttribute('href')
+        const level = heading.tagName === 'H2' ? 2 : 3
+        return { text, link, level }
+      })
+    setTocList(newHeadings)
+  }, [])
 
-	const hasTableOfContents = tocList.length > 0
-	const tocVideoPreview = `http://img.youtube.com/vi/${props.meta.tocVideo}/0.jpg`
+  const hasTableOfContents = tocList.length > 0
+  const tocVideoPreview = `http://img.youtube.com/vi/${props.meta.tocVideo}/0.jpg`
 
-	// page type, ie, Auth, Database, Storage etc
-	const ogPageType = asPath.split('/')[2]
-	// open graph image url constructor
-	const ogImageUrl = encodeURI(
-		`https://obuldanrptloktxcffvn.supabase.co/functions/v1/og-images?site=docs${ogPageType ? `&type=${ogPageType}` : ''
-		}&title=${props.meta?.title}&description=${props.meta?.description}`
-	)
+  // page type, ie, Auth, Database, Storage etc
+  const ogPageType = asPath.split('/')[2]
+  // open graph image url constructor
+  const ogImageUrl = encodeURI(
+    `https://obuldanrptloktxcffvn.supabase.co/functions/v1/og-images?site=docs${
+      ogPageType ? `&type=${ogPageType}` : ''
+    }&title=${props.meta?.title}&description=${props.meta?.description}`
+  )
 
-	return (
-		<>
-			<Head>
-				<title>{`${props.meta?.title} | Supabase Docs`}</title>
-				<meta name="description" content={props.meta?.description} />
-				<meta property="og:image" content={ogImageUrl} />
-				<meta name="twitter:image" content={ogImageUrl} />
-				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-			</Head>
-			<NextSeo
-				canonical={props.meta?.canonical ?? `https://supabase.com/docs${asPath}`}
-				openGraph={{
-					url: `https://supabase.com/docs${asPath}`,
-					type: 'article',
-					videos: props.meta?.video && [
-						{
-							// youtube based video meta
-							url: props.meta?.video,
-							width: 640,
-							height: 385,
-							type: 'application/x-shockwave-flash',
-						},
-					],
-					article: {
-						publishedTime: new Date().toISOString(),
-						modifiedTime: new Date().toISOString(),
-						authors: ['Supabase'],
-					},
-				}}
-			/>
-			<LayoutMainContent className="pb-0">
-				<div className={['grid grid-cols-12 relative gap-4'].join(' ')}>
-					<div
-						className={[
-							'relative',
-							!props.hideToc ? 'col-span-12 md:col-span-9' : 'col-span-12',
-							'transition-all ease-out',
-							'duration-100',
-						].join(' ')}
-					>
-						{props.meta.breadcrumb && (
-							<p className="text-brand tracking-wider mb-3">{props.meta.breadcrumb}</p>
-						)}
-						<article
-							ref={articleRef}
-							className={`${props.meta?.hide_table_of_contents || !hasTableOfContents ? '' : ''
-								} prose max-w-none`}
-						>
-							<h1 className="mb-0">{props.meta.title}</h1>
-							{props.meta.subtitle && (
-								<h2 className="mt-3 text-xl text-foreground-light">{props.meta.subtitle}</h2>
-							)}
-							<div className="w-full border-b my-8"></div>
-							<MDXProvider components={components}>{props.children}</MDXProvider>
+  return (
+    <>
+      <Head>
+        <title>{`${props.meta?.title} | Supabase Docs`}</title>
+        <meta name="description" content={props.meta?.description} />
+        <meta property="og:image" content={ogImageUrl} />
+        <meta name="twitter:image" content={ogImageUrl} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <NextSeo
+        canonical={props.meta?.canonical ?? `https://supabase.com/docs${asPath}`}
+        openGraph={{
+          url: `https://supabase.com/docs${asPath}`,
+          type: 'article',
+          videos: props.meta?.video && [
+            {
+              // youtube based video meta
+              url: props.meta?.video,
+              width: 640,
+              height: 385,
+              type: 'application/x-shockwave-flash',
+            },
+          ],
+          article: {
+            publishedTime: new Date().toISOString(),
+            modifiedTime: new Date().toISOString(),
+            authors: ['Supabase'],
+          },
+        }}
+      />
+      <LayoutMainContent className="pb-0">
+        <div className={['grid grid-cols-12 relative gap-4'].join(' ')}>
+          <div
+            className={[
+              'relative',
+              !props.hideToc ? 'col-span-12 md:col-span-9' : 'col-span-12',
+              'transition-all ease-out',
+              'duration-100',
+            ].join(' ')}
+          >
+            {props.meta.breadcrumb && (
+              <p className="text-brand tracking-wider mb-3">{props.meta.breadcrumb}</p>
+            )}
+            <article
+              ref={articleRef}
+              className={`${
+                props.meta?.hide_table_of_contents || !hasTableOfContents ? '' : ''
+              } prose max-w-none`}
+            >
+              <h1 className="mb-0">{props.meta.title}</h1>
+              {props.meta.subtitle && (
+                <h2 className="mt-3 text-xl text-foreground-light">{props.meta.subtitle}</h2>
+              )}
+              <div className="w-full border-b my-8"></div>
+              <MDXProvider components={components}>{props.children}</MDXProvider>
 
-							{EDIT_BUTTON_EXCLUDE_LIST.includes(router.route) ? (
-								<></>
-							) : (
-								<div className="mt-16 not-prose">
-									<div>
-										<a
-											href={`https://github.com/${props.editLink ||
-												`supabase/supabase/edit/master/apps/docs/pages${router.asPath}.mdx`
-												}
+              {EDIT_BUTTON_EXCLUDE_LIST.includes(router.route) ? (
+                <></>
+              ) : (
+                <div className="mt-16 not-prose">
+                  <div>
+                    <a
+                      href={`https://github.com/${
+                        props.editLink ||
+                        `supabase/supabase/edit/master/apps/docs/pages${router.asPath}.mdx`
+                      }
                     `}
-											className="text-sm transition flex items-center gap-1 text-scale-1000 hover:text-scale-1200 w-fit"
-										>
-											Edit this page on GitHub <IconExternalLink size={14} strokeWidth={1.5} />
-										</a>
-									</div>
-								</div>
-							)}
-						</article>
-					</div>
-					{!props.hideToc && hasTableOfContents && !props.meta?.hide_table_of_contents && (
-						<div
-							className={[
-								'col-span-3 self-start',
-								'border-overlay bg-background',
-								'thin-scrollbar overflow-y-auto hidden md:block md:col-span-3 px-2',
-								'sticky top-[calc(var(--mobile-header-height,40px)+2rem)] lg:top-[calc(var(--desktop-header-height,60px)+2rem)]',
-								'transition-all ease-out',
-								'duration-100',
-							].join(' ')}
-						>
-							<div className="border-l">
-								{props.meta?.tocVideo && !!tocVideoPreview && (
-									<div className="relative mb-6 pl-5">
-										<ExpandableVideo imgUrl={tocVideoPreview} videoId={props.meta.tocVideo} />
-									</div>
-								)}
-								<div className="px-5 mb-8">
-									<span className="block font-mono text-xs uppercase text-foreground mb-4">
-										Is this helpful?
-									</span>
-									<Feedback />
-								</div>
-								<span className="block font-mono text-xs uppercase text-foreground px-5 mb-6">
-									On this page
-								</span>
-								<GuidesTableOfContents list={tocList} />
-							</div>
-						</div>
-					)}
-				</div>
-			</LayoutMainContent>
-		</>
-	)
+                      className="text-sm transition flex items-center gap-1 text-scale-1000 hover:text-scale-1200 w-fit"
+                    >
+                      Edit this page on GitHub <IconExternalLink size={14} strokeWidth={1.5} />
+                    </a>
+                  </div>
+                </div>
+              )}
+            </article>
+          </div>
+          {!props.hideToc && hasTableOfContents && !props.meta?.hide_table_of_contents && (
+            <div
+              className={[
+                'col-span-3 self-start',
+                'border-overlay bg-background',
+                'thin-scrollbar overflow-y-auto hidden md:block md:col-span-3 px-2',
+                'sticky top-[calc(var(--mobile-header-height,40px)+2rem)] lg:top-[calc(var(--desktop-header-height,60px)+2rem)]',
+                'transition-all ease-out',
+                'duration-100',
+              ].join(' ')}
+            >
+              <div className="border-l">
+                {props.meta?.tocVideo && !!tocVideoPreview && (
+                  <div className="relative mb-6 pl-5">
+                    <ExpandableVideo imgUrl={tocVideoPreview} videoId={props.meta.tocVideo} />
+                  </div>
+                )}
+                <div className="px-5 mb-5">
+                  <span className="block font-mono text-xs uppercase text-foreground mb-4">
+                    Is this helpful?
+                  </span>
+                  <Feedback />
+                </div>
+                <span className="block font-mono text-xs uppercase text-foreground px-5 mb-6">
+                  On this page
+                </span>
+                <GuidesTableOfContents list={tocList} />
+              </div>
+            </div>
+          )}
+        </div>
+      </LayoutMainContent>
+    </>
+  )
 }
 
 export default Layout
