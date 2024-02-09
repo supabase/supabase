3fe67108d72c62e55406059e81b5b26f4bd48bf4
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2024-02-09 18:20:14 +0800

    separate FeaturedThumb component

--- apps/www/pages/blog.tsx
+++ apps/www/pages/blog.tsx
@@ -1,47 +1,15 @@
 import fs from 'fs'
 import { useState } from 'react'
-
 import { useRouter } from 'next/router'
-import Image from 'next/legacy/image'
-import Link from 'next/link'
-
 import { NextSeo } from 'next-seo'
 import { generateRss } from '~/lib/rss'
 import { getSortedPosts } from '~/lib/posts'
-import authors from 'lib/authors.json'
 
 import PostTypes from '~/types/post'
 import DefaultLayout from '~/components/Layouts/Default'
 import BlogListItem from '~/components/Blog/BlogListItem'
 import BlogFilters from '~/components/Blog/BlogFilters'
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPosts({ directory: '_blog', runner: '** BLOG PAGE **' })
-  const rss = generateRss(allPostsData)
-
-  // create a rss feed in public directory
-  // rss feed is added via <Head> component in render return
-  fs.writeFileSync('./public/rss.xml', rss)
-
-  // generate a series of rss feeds for each author (for PlanetPG)
-  const planetPgPosts = allPostsData.filter((post: any) => post.tags?.includes('planetpg'))
-  const planetPgAuthors = planetPgPosts.map((post: any) => post.author.split(','))
-  const uniquePlanetPgAuthors = new Set([].concat(...planetPgAuthors))
-
-  uniquePlanetPgAuthors.forEach((author) => {
-    const authorPosts = planetPgPosts.filter((post: any) => post.author.includes(author))
-    if (authorPosts.length > 0) {
-      const authorRss = generateRss(authorPosts, author)
-      fs.writeFileSync(`./public/planetpg-${author}-rss.xml`, authorRss)
-    }
-  })
-
-  return {
-    props: {
-      blogs: allPostsData,
-    },
-  }
-}
+import FeaturedThumb from '~/components/Blog/FeaturedThumb'
 
 function Blog(props: any) {
   const [blogs, setBlogs] = useState(props.blogs)
@@ -117,70 +85,32 @@ function Blog(props: any) {
   )
 }
 
-function FeaturedThumb(blog: PostTypes) {
-  // @ts-ignore
-  const authorArray = blog.author.split(',')
+export async function getStaticProps() {
+  const allPostsData = getSortedPosts({ directory: '_blog', runner: '** BLOG PAGE **' })
+  const rss = generateRss(allPostsData)
 
-  const author = []
-  for (let i = 0; i < authorArray.length; i++) {
-    // @ts-ignore
-    author.push(
-      authors.find((authors: any) => {
-        // @ts-ignore
-        return authors.author_id === authorArray[i]
-      })
-    )
+  // create a rss feed in public directory
+  // rss feed is added via <Head> component in render return
+  fs.writeFileSync('./public/rss.xml', rss)
+
+  // generate a series of rss feeds for each author (for PlanetPG)
+  const planetPgPosts = allPostsData.filter((post: any) => post.tags?.includes('planetpg'))
+  const planetPgAuthors = planetPgPosts.map((post: any) => post.author.split(','))
+  const uniquePlanetPgAuthors = new Set([].concat(...planetPgAuthors))
+
+  uniquePlanetPgAuthors.forEach((author) => {
+    const authorPosts = planetPgPosts.filter((post: any) => post.author.includes(author))
+    if (authorPosts.length > 0) {
+      const authorRss = generateRss(authorPosts, author)
+      fs.writeFileSync(`./public/planetpg-${author}-rss.xml`, authorRss)
+    }
+  })
+
+  return {
+    props: {
+      blogs: allPostsData,
+    },
   }
-
-  return (
-    <div key={blog.slug} className="w-full cursor-pointer">
-      <Link href={`${blog.path}`} className="grid gap-8 lg:grid-cols-2 lg:gap-16">
-        <div className="relative w-full aspect-[2/1] lg:aspect-[3/2] overflow-auto rounded-lg border">
-          <Image
-            src={`/images/blog/` + (blog.thumb ? blog.thumb : blog.image)}
-            layout="fill"
-            className="object-cover"
-            alt="blog thumbnail"
-          />
-        </div>
-        <div className="flex flex-col space-y-2">
-          <div className="text-light flex space-x-2 text-sm">
-            <span>{blog.date}</span>
-            <span>•</span>
-            <span>{blog.readingTime}</span>
-          </div>
-
-          <div>
-            <h2 className="h2">{blog.title}</h2>
-            <p className="p text-xl">{blog.description}</p>
-          </div>
-
-          <div className="flex flex-col w-max gap-2">
-            {author.map((author: any, i: number) => {
-              return (
-                <div className="flex items-center space-x-3" key={i}>
-                  {author.author_image_url && (
-                    <div className="relative h-10 w-10 overflow-auto">
-                      <Image
-                        src={author.author_image_url}
-                        alt={`${author.author} avatar`}
-                        className="rounded-full object-cover"
-                        layout="fill"
-                      />
-                    </div>
-                  )}
-                  <div className="flex flex-col">
-                    <span className="text-foreground m-0 text-sm">{author.author}</span>
-                    <span className="text-light m-0 text-xs">{author.position}</span>
-                  </div>
-                </div>
-              )
-            })}
-          </div>
-        </div>
-      </Link>
-    </div>
-  )
 }
 
 export default Blog
