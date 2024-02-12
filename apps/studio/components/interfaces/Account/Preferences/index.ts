fbbdf1d5b223dfd42984c93df799ee6cb5399375
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-02-12 07:36:00 +0000

    remove old code

--- apps/studio/components/interfaces/Account/Preferences/index.ts
+++ apps/studio/components/interfaces/Account/Preferences/index.ts
@@ -1,6 +1,5 @@
 import AccountInformation from './AccountInformation'
 import AnalyticsSettings from './AnalyticsSettings'
-import ThemeSettingsOld from './ThemeSettingsOld'
 import ThemeSettings from './ThemeSettings'
 
 export { AccountInformation, AnalyticsSettings, ThemeSettingsOld, ThemeSettings }
