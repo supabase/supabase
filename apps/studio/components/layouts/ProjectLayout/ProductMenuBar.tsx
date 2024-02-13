920938c8ea10fab8b216368acd81b2c8c0752b3b
Joshen Lim <joshenlimek@gmail.com>
2024-02-13 16:20:11 +0800

    Fix

--- apps/studio/components/layouts/ProjectLayout/ProductMenuBar.tsx
+++ apps/studio/components/layouts/ProjectLayout/ProductMenuBar.tsx
@@ -9,7 +9,7 @@ const ProductMenuBar = ({ title, children }: PropsWithChildren<ProductMenuBarPro
     <div
       className={[
         'hide-scrollbar flex w-64 flex-col border-r', // Layout
-        'bg-background border-default min-h-screen',
+        'bg-background border-default',
       ].join(' ')}
     >
       <div
