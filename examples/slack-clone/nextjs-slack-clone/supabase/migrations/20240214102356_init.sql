0e6f282e6402ab239b76c7d9117e0fd4bb31ee5d
thorwebdev <thor@supabase.io>
2024-02-15 22:25:17 +0800

    feat: get user role from jwt.

--- examples/slack-clone/nextjs-slack-clone/supabase/migrations/20240214102356_init.sql
+++ examples/slack-clone/nextjs-slack-clone/supabase/migrations/20240214102356_init.sql
@@ -64,9 +64,8 @@ declare
 begin
   select count(*)
   from public.role_permissions
-  inner join public.user_roles on role_permissions.role = user_roles.role
   where role_permissions.permission = authorize.requested_permission
-    and user_roles.user_id = authorize.user_id
+    and role_permissions.role = (auth.jwt() -> 'app_metadata' ->> 'user_role')::public.app_role
   into bind_permissions;
   
   return bind_permissions > 0;
