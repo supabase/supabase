871de3bceafe3989febe6ae4fc4d267570ec7999
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2024-02-16 14:45:10 +0100

    blog hero title

--- apps/www/components/Blog/FeaturedThumb.tsx
+++ apps/www/components/Blog/FeaturedThumb.tsx
@@ -40,7 +40,7 @@ function FeaturedThumb(blog: PostTypes) {
           </div>
 
           <div>
-            <h2 className="h2 !text-2xl !mb-2">{blog.title}</h2>
+            <h2 className="h2 lg:!text-2xl xl:!text-3xl !mb-2">{blog.title}</h2>
             <p className="p xl:text-lg">{blog.description}</p>
           </div>
 
