7abb4b03dc08d15f3849ae4f803437221735c8b5
Joshen Lim <joshenlimek@gmail.com>
2024-02-22 14:34:12 +0800

    Fix safari height issue

--- apps/studio/components/layouts/AccountLayout/AccountLayout.tsx
+++ apps/studio/components/layouts/AccountLayout/AccountLayout.tsx
@@ -150,7 +150,7 @@ const AccountLayout = ({ children, title, breadcrumbs }: PropsWithChildren<Accou
         <title>{title ? `${title} | Supabase` : 'Supabase'}</title>
         <meta name="description" content="Supabase Studio" />
       </Head>
-      <div className="h-full min-h-[0px] basis-0 flex-1">
+      <div className="h-screen min-h-[0px] basis-0 flex-1">
         <WithSidebar
           hideSidebar={navLayoutV2}
           title={title}
