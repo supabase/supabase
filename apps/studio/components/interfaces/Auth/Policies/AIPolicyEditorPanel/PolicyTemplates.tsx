279d864f48182283367273041f0a4e4b2c10359c
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-02-23 01:19:24 +0800

    templates side panel updated to use new tailwind classes and ScrollArea

--- apps/studio/components/interfaces/Auth/Policies/AIPolicyEditorPanel/PolicyTemplates.tsx
+++ apps/studio/components/interfaces/Auth/Policies/AIPolicyEditorPanel/PolicyTemplates.tsx
@@ -1,8 +1,12 @@
 import { useState } from 'react'
 import {
   Badge,
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
   IconSearch,
   Input,
+  ScrollArea,
   TooltipContent_Shadcn_,
   TooltipTrigger_Shadcn_,
   Tooltip_Shadcn_,
@@ -14,6 +18,7 @@ import { getGeneralPolicyTemplates } from '../PolicyEditorModal/PolicyEditorModa
 import NoSearchResults from 'components/ui/NoSearchResults'
 import { Markdown } from 'components/interfaces/Markdown'
 import CopyButton from 'components/ui/CopyButton'
+import { Search } from 'lucide-react'
 
 interface PolicyTemplatesProps {
   onSelectTemplate: (template: any) => void
@@ -32,46 +37,58 @@ export const PolicyTemplates = ({ onSelectTemplate }: PolicyTemplatesProps) => {
       : templates
 
   return (
-    <div className="flex flex-col gap-y-4 p-4">
+    <div className="h-full px-content py-content flex flex-col gap-3">
+      <label className="sr-only" htmlFor="template-search">
+        Search templates
+      </label>
       <Input
         size="small"
-        icon={<IconSearch />}
+        id="template-search"
+        icon={<Search size={16} className="text-foreground-muted" />}
         placeholder="Search templates"
         value={search}
         onChange={(event) => setSearch(event.target.value)}
       />
-      <div className="flex flex-col gap-y-2">
-        {search.length > 0 && filteredTemplates.length === 0 && (
-          <NoSearchResults searchString={search} />
-        )}
-        {filteredTemplates.map((template) => (
-          <Tooltip_Shadcn_ key={template.id} delayDuration={100}>
-            <TooltipTrigger_Shadcn_ asChild>
-              <div
-                className={cn(
-                  'grid grid-cols-12 rounded px-4 py-3 gap-x-4 cursor-pointer transition',
-                  'border bg-surface-100 hover:border-strong'
-                )}
-                onClick={() => onSelectTemplate(template.statement)}
+
+      {search.length > 0 && filteredTemplates.length === 0 && (
+        <NoSearchResults searchString={search} className="min-w-full" />
+      )}
+
+      <div className="flex flex-col gap-1.5">
+        {filteredTemplates.map((template) => {
+          return (
+            <HoverCard key={template.id} openDelay={100} closeDelay={0}>
+              <HoverCardTrigger asChild>
+                <button
+                  key={template.id}
+                  className={cn(
+                    'text-left',
+                    'flex flex-row',
+                    'rounded-panel px-4 py-3 gap-x-4 cursor-pointer transition',
+                    'border bg-surface-100 hover:border-strong'
+                  )}
+                  onClick={() => onSelectTemplate(template.statement)}
+                >
+                  <div className="min-w-16">
+                    <Badge className="!rounded font-mono" color="scale">
+                      {template.command}
+                    </Badge>
+                  </div>
+                  <div className="text-sm mt-[3px] flex flex-col gap-y-1">
+                    <p>{template.name}</p>
+                    <Markdown content={template.description} className="[&>p]:m-0 space-y-2" />
+                    {/* <p>{template.description}</p> */}
+                  </div>
+                </button>
+              </HoverCardTrigger>
+              <HoverCardContent
+                hideWhenDetached
+                side="left"
+                align="center"
+                className="w-96 flex flex-col gap-y-2"
               >
-                <div className="col-span-2">
-                  <Badge className="!rounded" color="scale">
-                    {template.command}
-                  </Badge>
-                </div>
-                <div className="col-span-10 text-sm mt-[3px] flex flex-col gap-y-1">
-                  <p>{template.name}</p>
-                  <Markdown content={template.description} className="[&>p]:m-0 space-y-2" />
-                </div>
-              </div>
-            </TooltipTrigger_Shadcn_>
-            <TooltipContent_Shadcn_
-              side="left"
-              align="start"
-              className="w-96 flex flex-col gap-y-2 py-3"
-            >
-              <p className="text-xs">Policy SQL preview:</p>
-              <div className="bg-surface-300 py-2 px-3 rounded relative">
+                {/* <p className="text-xs">Policy SQL preview:</p> */}
+                {/* <div className="bg-surface-300 py-2 px-3 rounded relative"> */}
                 <SimpleCodeBlock
                   showCopy={false}
                   className="sql"
@@ -85,10 +102,11 @@ export const PolicyTemplates = ({ onSelectTemplate }: PolicyTemplatesProps) => {
                   className="px-1 absolute top-1.5 right-1.5"
                   text={template.statement}
                 />
-              </div>
-            </TooltipContent_Shadcn_>
-          </Tooltip_Shadcn_>
-        ))}
+                {/* </div> */}
+              </HoverCardContent>
+            </HoverCard>
+          )
+        })}
       </div>
     </div>
   )
