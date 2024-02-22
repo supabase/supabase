1c1e3595063a40cebf7ea35ac7701f1cf83ca42e
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-02-23 01:20:03 +0800

    header updated to use simpler open/close button

--- apps/studio/components/interfaces/Auth/Policies/AIPolicyEditorPanel/AIPolicyHeader.tsx
+++ apps/studio/components/interfaces/Auth/Policies/AIPolicyEditorPanel/AIPolicyHeader.tsx
@@ -1,7 +1,7 @@
 import { PostgresPolicy } from '@supabase/postgres-meta'
 import styles from '@ui/layout/ai-icon-animation/ai-icon-animation-style.module.css'
 import clsx from 'clsx'
-import { X } from 'lucide-react'
+import { PanelLeftClose, PanelRightClose, X } from 'lucide-react'
 import { Button, SheetClose_Shadcn_, SheetHeader_Shadcn_, SheetTitle_Shadcn_, cn } from 'ui'
 
 export const AIPolicyHeader = ({
@@ -15,29 +15,47 @@ export const AIPolicyHeader = ({
 }) => {
   return (
     <SheetHeader_Shadcn_
-      className={`${
-        selectedPolicy !== undefined ? 'pt-3 pb-0' : 'py-3'
-      } flex flex-row justify-between items-center !pr-0`}
+      className={cn(
+        selectedPolicy !== undefined ? 'pt-3 pb-0' : 'py-3',
+        'flex flex-row justify-between items-center'
+      )}
     >
       <div className="flex flex-row gap-3 items-center max-w-[75%]">
         <SheetClose_Shadcn_
           className={clsx(
-            'text-light hover:text ring-offset-background transition-opacity hover:opacity-100',
+            'text-muted hover:text ring-offset-background transition-opacity hover:opacity-100',
             'focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
-            'disabled:pointer-events-none data-[state=open]:bg-secondary'
+            'disabled:pointer-events-none data-[state=open]:bg-secondary',
+            'transition'
           )}
         >
           <X className="h-3 w-3" />
           <span className="sr-only">Close</span>
         </SheetClose_Shadcn_>
-        <div className="h-[24px] w-[1px] bg-border" />
+        <div className="h-[24px] w-[1px] bg-border-overlay" />
         <SheetTitle_Shadcn_ className="truncate">
           {selectedPolicy !== undefined
             ? `Update policy: ${selectedPolicy.name}`
             : 'Create a new Row Level Security policy'}
         </SheetTitle_Shadcn_>
       </div>
-      <Button
+      <button
+        aria-expanded={assistantVisible}
+        aria-controls="ai-chat-assistant"
+        className={cn(
+          !assistantVisible ? 'text-foreground-lighter' : 'text-light',
+          'hover:text-foreground',
+          'transition'
+        )}
+        onClick={() => setAssistantVisible(!assistantVisible)}
+      >
+        {!assistantVisible ? (
+          <PanelLeftClose size={19} strokeWidth={1} />
+        ) : (
+          <PanelRightClose size={19} strokeWidth={1} />
+        )}
+      </button>
+      {/* <Button
         aria-expanded={assistantVisible}
         aria-controls="ai-chat-assistant"
         size="tiny"
@@ -67,7 +85,7 @@ export const AIPolicyHeader = ({
           </svg>
         }
         onClick={() => setAssistantVisible(!assistantVisible)}
-      />
+      /> */}
     </SheetHeader_Shadcn_>
   )
 }
