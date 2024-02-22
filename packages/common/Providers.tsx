28dd5338c3a71a309f0c56e8c4286390ae817617
Charis Lam <26616127+charislam@users.noreply.github.com>
2024-02-22 18:15:02 -0500

    chore: remove unnecessary changes

--- packages/common/Providers.tsx
+++ packages/common/Providers.tsx
@@ -2,11 +2,11 @@ import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { type ThemeProviderProps } from 'next-themes/dist/types'
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-	// @ts-ignore next-themes is old :/
+  // @ts-ignore next-themes is old :/
 
-	return (
-		<NextThemesProvider {...props} themes={['dark', 'light', 'deep-dark']}>
-			{children}
-		</NextThemesProvider>
-	)
+  return (
+    <NextThemesProvider {...props} themes={['dark', 'light', 'deep-dark']}>
+      {children}
+    </NextThemesProvider>
+  )
 }
