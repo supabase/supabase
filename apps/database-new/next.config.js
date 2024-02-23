2f31d21f7f778faf279683b97f0edb049aa6125c
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-02-23 16:17:52 +0800

    remove from db.design

--- apps/database-new/next.config.js
+++ apps/database-new/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['ui', 'ui-patterns', 'icons'],
+  transpilePackages: ['ui', 'ui-patterns'],
   webpack: (config, { dev, isServer, webpack, nextRuntime }) => {
     config.module.rules.push({
       test: /\.node$/,
