303e02e26088fd20737d2315ffb3d2c06d0e8b02
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-02-27 03:51:42 +0800

    Update NavigationBar.tsx

--- apps/studio/components/layouts/ProjectLayout/NavigationBar/NavigationBar.tsx
+++ apps/studio/components/layouts/ProjectLayout/NavigationBar/NavigationBar.tsx
@@ -91,133 +91,150 @@ const NavigationBar = () => {
   const settingsRoutes = generateSettingsRoutes(projectRef, project)
 
   return (
-    <nav
-      data-state={snap.navigationPanelOpen ? 'expanded' : 'collapsed'}
-      className={[
-        'transition-width duration-200',
-        'w-14 data-[state=expanded]:w-[17rem]',
-        'py-2',
-        'hide-scrollbar flex flex-col justify-between overflow-y-auto',
-        'border-r bg-studio border-default',
-        'group',
-      ].join(' ')}
-      onMouseEnter={() => {
-        snap.setNavigationPanelOpen(true)
-      }}
-      onMouseLeave={() => {
-        if (!userDropdownOpen) snap.setNavigationPanelOpen(false)
-      }}
-    >
-      <ul className="flex flex-col gap-1 justify-start px-2">
-        {(!navLayoutV2 || !IS_PLATFORM) && (
-          <Link
-            href={IS_PLATFORM ? '/projects' : `/project/${projectRef}`}
-            className="mx-2 flex items-center h-[40px]"
-          >
-            <img
-              src={`${router.basePath}/img/supabase-logo.svg`}
-              alt="Supabase"
-              className="absolute h-[40px] w-6 cursor-pointer rounded"
-            />
-            <SVG
-              src={`${BASE_PATH}/img/logo/supabase-wordmark.svg`}
-              width={96}
-              className={cn(
-                'absolute fill-foreground',
-                'left-8 group-data-[state=expanded]:left-[3rem]',
-                'opacity-0 group-data-[state=expanded]:opacity-100',
-                'transition-all',
-                'duration-200',
-                'delay-100',
-                'group-data-[state=expanded]:delay-0'
-              )}
-            />
-          </Link>
-        )}
-        <NavigationIconLink
-          isActive={isUndefined(activeRoute) && !isUndefined(router.query.ref)}
-          route={{
-            key: 'HOME',
-            label: 'Home',
-            icon: <Home size={ICON_SIZE} strokeWidth={ICON_STROKE_WIDTH} />,
-            link: `/project/${projectRef}`,
-          }}
-        />
-        <Separator className="my-1 bg-border-muted" />
-        {toolRoutes.map((route) => (
-          <NavigationIconLink key={route.key} route={route} isActive={activeRoute === route.key} />
-        ))}
-        <Separator className="my-1 bg-border-muted" />
-        {productRoutes.map((route) => (
-          <NavigationIconLink key={route.key} route={route} isActive={activeRoute === route.key} />
-        ))}
-        <Separator className="my-1 bg-border-muted" />
-        {otherRoutes.map((route) => {
-          if (route.key === 'api' && isNewAPIDocsEnabled) {
-            return (
-              <Tooltip.Root delayDuration={0} key={route.key}>
-                <Tooltip.Trigger asChild>
-                  <Button
-                    type="text"
-                    size="tiny"
-                    onClick={() => snap.setShowProjectApiDocs(true)}
-                    className="border-none group"
-                  >
-                    <div className="py-[7px]">
-                      <IconFileText
-                        size={18}
-                        strokeWidth={2}
-                        className="transition text-foreground-lighter group-hover:text-foreground"
-                      />
-                    </div>
-                  </Button>
-                </Tooltip.Trigger>
-                <Tooltip.Portal>
-                  <Tooltip.Content side="right">
-                    <Tooltip.Arrow className="radix-tooltip-arrow" />
-                    <div
-                      className={[
-                        'bg-alternative shadow-lg shadow-background-surface-100	py-1.5 px-3 rounded leading-none', // background
-                        'border border-default', //border
-                      ].join(' ')}
-                    >
-                      <span className="text-foreground text-xs">Project API Docs</span>
-                    </div>
-                  </Tooltip.Content>
-                </Tooltip.Portal>
-              </Tooltip.Root>
-            )
-          } else {
-            return (
-              <NavigationIconLink
-                key={route.key}
-                route={route}
-                isActive={activeRoute === route.key}
+    <div className="w-14 h-full flex flex-col">
+      <nav
+        data-state={snap.navigationPanelOpen ? 'expanded' : 'collapsed'}
+        className={[
+          'z-10',
+          'bg-studio',
+          'data-[state=expanded]:shadow-xl',
+          'h-full',
+          'transition-width duration-200',
+          'w-14 data-[state=expanded]:w-[17rem]',
+          'py-2',
+          'hide-scrollbar flex flex-col justify-between overflow-y-auto',
+          'border-r bg-studio border-default',
+          'group',
+        ].join(' ')}
+        onMouseEnter={() => {
+          snap.setNavigationPanelOpen(true)
+        }}
+        onMouseLeave={() => {
+          if (!userDropdownOpen) snap.setNavigationPanelOpen(false)
+        }}
+      >
+        <ul className="flex flex-col gap-1 justify-start px-2">
+          {(!navLayoutV2 || !IS_PLATFORM) && (
+            <Link
+              href={IS_PLATFORM ? '/projects' : `/project/${projectRef}`}
+              className="mx-2 flex items-center h-[40px]"
+            >
+              <img
+                src={`${router.basePath}/img/supabase-logo.svg`}
+                alt="Supabase"
+                className="absolute h-[40px] w-6 cursor-pointer rounded"
               />
-            )
-          }
-        })}
-      </ul>
-
-      <ul className="flex flex-col px-2">
-        {settingsRoutes.map((route) => (
-          <NavigationIconLink key={route.key} route={route} isActive={activeRoute === route.key} />
-        ))}
-
-        {IS_PLATFORM && (
-          <Button
-            type="text"
-            size="tiny"
-            onClick={() => setIsOpen(true)}
-            className="h-10 [&>span]:relative [&>span]:items-center [&>span]:gap-3 [&>span]:flex [&>span]:w-full [&>span]:h-full p-0"
-          >
-            <IconSearch
-              size={ICON_SIZE}
-              strokeWidth={2}
-              className="absolute left-2 text-foreground-lighter"
+              <SVG
+                src={`${BASE_PATH}/img/logo/supabase-wordmark.svg`}
+                width={96}
+                className={cn(
+                  'absolute fill-foreground',
+                  'left-8 group-data-[state=expanded]:left-[3rem]',
+                  'opacity-0 group-data-[state=expanded]:opacity-100',
+                  'transition-all',
+                  'duration-200',
+                  'delay-100',
+                  'group-data-[state=expanded]:delay-0'
+                )}
+              />
+            </Link>
+          )}
+          <NavigationIconLink
+            isActive={isUndefined(activeRoute) && !isUndefined(router.query.ref)}
+            route={{
+              key: 'HOME',
+              label: 'Home',
+              icon: <Home size={ICON_SIZE} strokeWidth={ICON_STROKE_WIDTH} />,
+              link: `/project/${projectRef}`,
+            }}
+          />
+          <Separator className="my-1 bg-border-muted" />
+          {toolRoutes.map((route) => (
+            <NavigationIconLink
+              key={route.key}
+              route={route}
+              isActive={activeRoute === route.key}
             />
-            <span
-              className=" 
+          ))}
+          <Separator className="my-1 bg-border-muted" />
+          {productRoutes.map((route) => (
+            <NavigationIconLink
+              key={route.key}
+              route={route}
+              isActive={activeRoute === route.key}
+            />
+          ))}
+          <Separator className="my-1 bg-border-muted" />
+          {otherRoutes.map((route) => {
+            if (route.key === 'api' && isNewAPIDocsEnabled) {
+              return (
+                <Tooltip.Root delayDuration={0} key={route.key}>
+                  <Tooltip.Trigger asChild>
+                    <Button
+                      type="text"
+                      size="tiny"
+                      onClick={() => snap.setShowProjectApiDocs(true)}
+                      className="border-none group"
+                    >
+                      <div className="py-[7px]">
+                        <IconFileText
+                          size={18}
+                          strokeWidth={2}
+                          className="transition text-foreground-lighter group-hover:text-foreground"
+                        />
+                      </div>
+                    </Button>
+                  </Tooltip.Trigger>
+                  <Tooltip.Portal>
+                    <Tooltip.Content side="right">
+                      <Tooltip.Arrow className="radix-tooltip-arrow" />
+                      <div
+                        className={[
+                          'bg-alternative shadow-lg shadow-background-surface-100	py-1.5 px-3 rounded leading-none', // background
+                          'border border-default', //border
+                        ].join(' ')}
+                      >
+                        <span className="text-foreground text-xs">Project API Docs</span>
+                      </div>
+                    </Tooltip.Content>
+                  </Tooltip.Portal>
+                </Tooltip.Root>
+              )
+            } else {
+              return (
+                <NavigationIconLink
+                  key={route.key}
+                  route={route}
+                  isActive={activeRoute === route.key}
+                />
+              )
+            }
+          })}
+        </ul>
+
+        <ul className="flex flex-col px-2">
+          {settingsRoutes.map((route) => (
+            <NavigationIconLink
+              key={route.key}
+              route={route}
+              isActive={activeRoute === route.key}
+            />
+          ))}
+
+          {IS_PLATFORM && (
+            <Button
+              type="text"
+              size="tiny"
+              onClick={() => setIsOpen(true)}
+              className="h-10 [&>span]:relative [&>span]:items-center [&>span]:gap-3 [&>span]:flex [&>span]:w-full [&>span]:h-full p-0"
+            >
+              <IconSearch
+                size={ICON_SIZE}
+                strokeWidth={2}
+                className="absolute left-2 text-foreground-lighter"
+              />
+              <span
+                className=" 
                     absolute
                     left-7
                     group-data-[state=expanded]:left-10
@@ -234,64 +251,64 @@ const NavigationBar = () => {
                     transition-all 
                     delay-100              
                   "
-            >
-              <span className="w-full text-left text-foreground-light truncate">Search</span>
-            </span>
+              >
+                <span className="w-full text-left text-foreground-light truncate">Search</span>
+              </span>
 
-            <div
-              className={[
-                'absolute',
-                'left-[10rem]',
-                'opacity-0',
-                'delay-100',
-                'group-data-[state=expanded]:opacity-100 ',
-                'text-foreground-lighter',
-                'bg-surface-100 shadow-xs shadow-background-surface-100	h-6 py-1.5 px-2 rounded-md leading-none', // background
-                'border border-default', // border
-                'flex items-center gap-1', // layout
-                'transition-all ',
-              ].join(' ')}
-            >
-              {os === 'macos' ? (
-                <IconCommand size={11.5} strokeWidth={1.5} />
-              ) : (
-                <p className="text-xs">CTRL</p>
-              )}
-              <p className="text-xs">K</p>
-            </div>
-          </Button>
-        )}
+              <div
+                className={[
+                  'absolute',
+                  'left-[10rem]',
+                  'opacity-0',
+                  'delay-100',
+                  'group-data-[state=expanded]:opacity-100 ',
+                  'text-foreground-lighter',
+                  'bg-surface-100 shadow-xs shadow-background-surface-100	h-6 py-1.5 px-2 rounded-md leading-none', // background
+                  'border border-default', // border
+                  'flex items-center gap-1', // layout
+                  'transition-all ',
+                ].join(' ')}
+              >
+                {os === 'macos' ? (
+                  <IconCommand size={11.5} strokeWidth={1.5} />
+                ) : (
+                  <p className="text-xs">CTRL</p>
+                )}
+                <p className="text-xs">K</p>
+              </div>
+            </Button>
+          )}
 
-        <DropdownMenu
-          open={userDropdownOpen}
-          onOpenChange={(open: boolean) => {
-            console.log('open', open)
-            setUserDropdownOpenState(open)
-            if (open === false) {
-              snap.setNavigationPanelOpen(false)
-            }
-          }}
-        >
-          <DropdownMenuTrigger asChild>
-            <Button
-              type="text"
-              size="tiny"
-              className={cn(
-                'mt-3',
-                'h-10 [&>span]:relative [&>span]:flex [&>span]:w-full [&>span]:h-full p-0'
-              )}
-              block
-            >
-              <div className="relative w-full h-full flex items-center justify-center">
-                <figure className="absolute left-1 min-h-7 min-w-7 bg-foreground rounded-full flex items-center justify-center">
-                  <User
-                    size={ICON_SIZE - 2}
-                    strokeWidth={ICON_STROKE_WIDTH}
-                    className="text-background"
-                  />
-                </figure>
-                <span
-                  className=" 
+          <DropdownMenu
+            open={userDropdownOpen}
+            onOpenChange={(open: boolean) => {
+              console.log('open', open)
+              setUserDropdownOpenState(open)
+              if (open === false) {
+                snap.setNavigationPanelOpen(false)
+              }
+            }}
+          >
+            <DropdownMenuTrigger asChild>
+              <Button
+                type="text"
+                size="tiny"
+                className={cn(
+                  'mt-3',
+                  'h-10 [&>span]:relative [&>span]:flex [&>span]:w-full [&>span]:h-full p-0'
+                )}
+                block
+              >
+                <div className="relative w-full h-full flex items-center justify-center">
+                  <figure className="absolute left-1 min-h-7 min-w-7 bg-foreground rounded-full flex items-center justify-center">
+                    <User
+                      size={ICON_SIZE - 2}
+                      strokeWidth={ICON_STROKE_WIDTH}
+                      className="text-background"
+                    />
+                  </figure>
+                  <span
+                    className=" 
                     left-10
                     w-[10rem]
                     absolute
@@ -310,60 +327,63 @@ const NavigationBar = () => {
 
                     truncate
                   "
-                >
-                  <span className="w-full text-left text-foreground truncate">
-                    {profile?.username}
+                  >
+                    <span className="w-full text-left text-foreground truncate">
+                      {profile?.username}
+                    </span>
+                    <span className="w-full text-left text-foreground-light text-xs truncate">
+                      {profile?.primary_email}
+                    </span>
                   </span>
-                  <span className="w-full text-left text-foreground-light text-xs truncate">
-                    {profile?.primary_email}
-                  </span>
-                </span>
-              </div>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent side="right" align="start">
-            {IS_PLATFORM && (
-              <>
-                <DropdownMenuSub>{}</DropdownMenuSub>
-                <DropdownMenuItem key="header" className="space-x-2" asChild>
-                  <Link href="/account/me">
-                    <IconSettings size={14} strokeWidth={1.5} />
-                    <p>Account preferences</p>
-                  </Link>
-                </DropdownMenuItem>
-                <DropdownMenuItem
-                  key="header"
-                  className="space-x-2"
-                  onClick={() => snap.setShowFeaturePreviewModal(true)}
-                  onSelect={() => snap.setShowFeaturePreviewModal(true)}
+                </div>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent side="top" align="start">
+              {IS_PLATFORM && (
+                <>
+                  <DropdownMenuSub>{}</DropdownMenuSub>
+                  <DropdownMenuItem key="header" className="space-x-2" asChild>
+                    <Link href="/account/me">
+                      <IconSettings size={14} strokeWidth={1.5} />
+                      <p>Account preferences</p>
+                    </Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    key="header"
+                    className="space-x-2"
+                    onClick={() => snap.setShowFeaturePreviewModal(true)}
+                    onSelect={() => snap.setShowFeaturePreviewModal(true)}
+                  >
+                    <FlaskConical size={14} strokeWidth={2} />
+                    <p>Feature previews</p>
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                </>
+              )}
+              <DropdownMenuLabel>Theme</DropdownMenuLabel>
+              <DropdownMenuGroup>
+                <DropdownMenuRadioGroup
+                  value={theme}
+                  onValueChange={(value) => {
+                    setTheme(value)
+                  }}
                 >
-                  <FlaskConical size={14} strokeWidth={2} />
-                  <p>Feature previews</p>
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-              </>
-            )}
-            <DropdownMenuLabel>Theme</DropdownMenuLabel>
-            <DropdownMenuGroup>
-              <DropdownMenuRadioGroup
-                value={theme}
-                onValueChange={(value) => {
-                  setTheme(value)
-                }}
-              >
-                {themes
-                  .filter((x) => x.value === 'light' || x.value === 'dark' || x.value === 'system')
-                  .map((theme: Theme) => (
-                    <DropdownMenuRadioItem key={theme.value} value={theme.value}>
-                      {theme.name}
-                    </DropdownMenuRadioItem>
-                  ))}
-              </DropdownMenuRadioGroup>
-            </DropdownMenuGroup>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </ul>
-    </nav>
+                  {themes
+                    .filter(
+                      (x) => x.value === 'light' || x.value === 'dark' || x.value === 'system'
+                    )
+                    .map((theme: Theme) => (
+                      <DropdownMenuRadioItem key={theme.value} value={theme.value}>
+                        {theme.name}
+                      </DropdownMenuRadioItem>
+                    ))}
+                </DropdownMenuRadioGroup>
+              </DropdownMenuGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
