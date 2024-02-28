6599a88925d3d3c23aa378cc6cb134be56418b19
Joshen Lim <joshenlimek@gmail.com>
2024-02-28 14:05:20 +0800

    fix

--- apps/studio/components/interfaces/Auth/Policies/AIPolicyEditorPanel/AIPolicyHeader.tsx
+++ apps/studio/components/interfaces/Auth/Policies/AIPolicyEditorPanel/AIPolicyHeader.tsx
@@ -68,40 +68,9 @@ export const AIPolicyHeader = ({
           </button>
         </TooltipTrigger_Shadcn_>
         <TooltipContent_Shadcn_ side="left">
-          {assistantVisible ? 'Hide' : 'Show'} AI chat assistant
+          {assistantVisible ? 'Hide' : 'Show'} tools
         </TooltipContent_Shadcn_>
       </Tooltip_Shadcn_>
-      {/* <Button
-        aria-expanded={assistantVisible}
-        aria-controls="ai-chat-assistant"
-        size="tiny"
-        type="outline"
-        className={cn(
-          'group rounded-r-none border-r-0',
-          styles['ai-icon__container--allow-hover-effect'],
-          'pl-2 pr-1.5 py-0.5'
-        )}
-        rounded
-        icon={
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width=""
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            className="rotate-180 scale-75 opacity-75"
-          >
-            <rect width="18" height="18" x="3" y="3" rx="2" />
-            <path d="M15 3v18" />
-            <path d="m10 15-3-3 3-3" />
-          </svg>
-        }
-        onClick={() => setAssistantVisible(!assistantVisible)}
-      /> */}
     </SheetHeader_Shadcn_>
   )
 }
