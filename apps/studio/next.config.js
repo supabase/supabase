69ba60c388db3df32d21ad6d33533b5ba7d64604
Joshen Lim <joshenlimek@gmail.com>
2024-02-28 17:14:57 +0800

    Update CSP

--- apps/studio/next.config.js
+++ apps/studio/next.config.js
@@ -60,7 +60,7 @@ const csp = [
   process.env.NEXT_PUBLIC_ENVIRONMENT === 'staging'
     ? [
         `default-src 'self' ${DEFAULT_SRC_URLS} ${SUPABASE_STAGING_PROJECTS_URL} ${SUPABASE_STAGING_PROJECTS_URL_WS} ${VERCEL_LIVE_URL} ${PUSHER_URL} ${PUSHER_URL_WS};`,
-        `script-src 'self' 'unsafe-eval' 'unsafe-inline' ${SCRIPT_SRC_URLS} ${VERCEL_LIVE_URL} ${VERCEL_SPEED_INSIGHTS_URL};`,
+        `script-src 'self' 'unsafe-eval' 'unsafe-inline' ${SCRIPT_SRC_URLS} ${VERCEL_LIVE_URL};`,
         `frame-src 'self' ${FRAME_SRC_URLS} ${VERCEL_LIVE_URL};`,
         `img-src 'self' blob: data: ${IMG_SRC_URLS} ${SUPABASE_STAGING_PROJECTS_URL} ${VERCEL_URL};`,
         `style-src 'self' 'unsafe-inline' ${STYLE_SRC_URLS} ${VERCEL_LIVE_URL};`,
