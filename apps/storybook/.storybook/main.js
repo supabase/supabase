9ee89439397289528e633455be2725d6cb0fb40a
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-03-01 14:28:30 +0800

    Update main.js

--- apps/storybook/.storybook/main.js
+++ apps/storybook/.storybook/main.js
@@ -17,13 +17,14 @@ const config = {
     // '../../apps/**/*.stories.@(js|jsx|mjs|ts|tsx)',
     // only expose a few stories as most older based ones are broken
     '../../../packages/ui/src/components/shadcn/**/*.stories.@(js|jsx|mjs|ts|tsx)',
-    '../../../packages/ui-patterns/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    // '../../../packages/ui-patterns/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
   addons: [
     getAbsolutePath('@storybook/addon-links'),
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@storybook/addon-onboarding'),
     getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath("@storybook/addon-mdx-gfm")
   ],
   framework: {
     name: getAbsolutePath('@storybook/nextjs'),
