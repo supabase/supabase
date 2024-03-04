6b987a48bd9bc9c081100b4b6c0386387f1a7286
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-03-04 23:27:15 +0800

    Update Dialog.stories.tsx

--- packages/ui/src/components/shadcn/stories/Dialog.stories.tsx
+++ packages/ui/src/components/shadcn/stories/Dialog.stories.tsx
@@ -1,13 +1,16 @@
-import { Meta } from '@storybook/react'
+import { useArgs } from '@storybook/preview-api'
+import { Meta, StoryObj } from '@storybook/react'
+import React from 'react'
 import { Button } from '../ui/button'
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogSection,
+  DialogTitle,
+  DialogTrigger,
 } from '../ui/dialog'
 import { Input } from '../ui/input'
 import { Label } from '../ui/label'
@@ -53,3 +56,56 @@ export function Default() {
 }
 
 export default meta
+
+type Story = StoryObj<typeof Dialog>
+
+export const withSize: Story = {
+  args: {
+    // open: true,
+    // size: 'xlarge',
+    // variant: 'destructive',
+    // title: 'Are you sure you want to delete?',
+  },
+  /**
+   * 👇 To avoid linting issues, it is recommended to use a function with a capitalized name.
+   * If you are not concerned with linting, you may use an arrow function.
+   */
+  render: function Render(args) {
+    return (
+      <>
+        <Dialog {...args}>
+          <DialogTrigger asChild>
+            <Button variant="outline">Edit Profile</Button>
+          </DialogTrigger>
+          <DialogContent size="xlarge">
+            <DialogHeader>
+              <DialogTitle>Edit profile</DialogTitle>
+              <DialogDescription>
+                Make changes to your profile here. Click save when you're done.
+              </DialogDescription>
+            </DialogHeader>
+            <DialogSection>
+              <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="name" className="text-right">
+                    Name
+                  </Label>
+                  <Input id="name" value="Pedro Duarte" className="col-span-3" />
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="username" className="text-right">
+                    Username
+                  </Label>
+                  <Input id="username" value="@peduarte" className="col-span-3" />
+                </div>
+              </div>
+            </DialogSection>
+            <DialogFooter>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      </>
+    )
+  },
+}
