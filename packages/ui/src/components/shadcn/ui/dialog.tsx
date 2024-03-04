0609962292ce6c872c1162024922dc62c62c7fc2
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-03-04 23:27:24 +0800

    add padding to more Dialog stuff

--- packages/ui/src/components/shadcn/ui/dialog.tsx
+++ packages/ui/src/components/shadcn/ui/dialog.tsx
@@ -55,7 +55,7 @@ DialogOverlay.displayName = DialogPrimitive.Overlay.displayName
 const DialogContentVariants = cva(
   cn(
     'my-8',
-    'relative z-50 grid w-full gap-4 border p-6 shadow-md dark:shadow-sm duration-200',
+    'relative z-50 grid w-full gap-4 border shadow-md dark:shadow-sm duration-200',
     'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
     'data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95',
     'data-[state=closed]:slide-out-to-left-[0%] data-[state=closed]:slide-out-to-top-[0%',
@@ -121,14 +121,23 @@ const DialogHeader = React.forwardRef<
 
 DialogHeader.displayName = 'DialogHeader'
 
-const DialogFooter = ({ className, children, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
+const DialogFooter = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof DialogPaddingVariants>
+>(({ className, children, padding, ...props }, ref) => (
   <div
-    className={cn('flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2', className)}
+    ref={ref}
+    className={cn(
+      'flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2',
+      'border-t',
+      DialogPaddingVariants({ padding }),
+      className
+    )}
     {...props}
   >
     {children}
   </div>
-)
+))
 DialogFooter.displayName = 'DialogFooter'
 
 const DialogTitle = React.forwardRef<
