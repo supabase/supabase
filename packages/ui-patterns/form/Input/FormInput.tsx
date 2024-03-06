fc1594c01c5c903f043238ea95c1d348eb330a91
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-03-06 15:11:24 +0800

    Select now has a SelectItem

--- packages/ui-patterns/form/Input/FormInput.tsx
+++ packages/ui-patterns/form/Input/FormInput.tsx
@@ -38,4 +38,4 @@ const FormFieldInput = forwardRef<
   )
 })
 
-export { FormFieldInput }
+export { FormFieldInput, FormItemInput }
--- packages/ui-patterns/form/Select/FormSelect.tsx
+++ packages/ui-patterns/form/Select/FormSelect.tsx
@@ -1,15 +1,27 @@
 import React, { ComponentProps, ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
-import {
-  FormControl_Shadcn_,
-  FormField_Shadcn_,
-  FormItem_Shadcn_,
-  SelectTrigger_Shadcn_,
-  Select_Shadcn_,
-} from 'ui'
-import { InputWithLayout } from '../withLayout/InputWithLayout'
+import { FormControl_Shadcn_, FormField_Shadcn_, SelectTrigger_Shadcn_, Select_Shadcn_ } from 'ui'
+import { FormItemProps, FormItemWrapper } from '../utils'
 import { SelectWithLayout } from '../withLayout/SelectWithLayout'
 
-export interface Props extends Omit<ComponentProps<typeof InputWithLayout>, 'ref'> {
+const FormItemSelect = forwardRef<
+  ElementRef<typeof SelectWithLayout>,
+  ComponentPropsWithoutRef<typeof SelectWithLayout> & FormItemProps<any, any>
+>(({ field, ...props }, ref) => (
+  <FormItemWrapper>
+    <SelectWithLayout
+      onValueChange={field.onChange}
+      defaultValue={field.value}
+      {...field}
+      {...props}
+      isReactForm={true}
+      ref={ref}
+    />
+  </FormItemWrapper>
+))
+
+FormItemSelect.displayName = 'FormItemSelect'
+
+export interface FormFieldSelectProps extends Omit<ComponentProps<typeof SelectWithLayout>, 'ref'> {
   name: React.ComponentProps<typeof FormField_Shadcn_>['name']
   control: any // { key: string }
   formField: Omit<React.ComponentProps<typeof FormField_Shadcn_>, 'ref'>
@@ -17,26 +29,14 @@ export interface Props extends Omit<ComponentProps<typeof InputWithLayout>, 'ref
 
 const FormSelect = forwardRef<
   ElementRef<typeof Select_Shadcn_>,
-  Omit<ComponentPropsWithoutRef<typeof Select_Shadcn_>, 'name'> & Props
+  Omit<ComponentPropsWithoutRef<typeof Select_Shadcn_>, 'name'> & FormFieldSelectProps
 >(({ formField, name, control, ...props }, ref) => {
   return (
     <FormField_Shadcn_
       {...formField}
       name={name}
       control={control}
-      render={({ field }) => (
-        <FormItem_Shadcn_>
-          <FormControl_Shadcn_>
-            <SelectWithLayout
-              onValueChange={field.onChange}
-              defaultValue={field.value}
-              {...props}
-              {...field}
-              isReactForm={true}
-            />
-          </FormControl_Shadcn_>
-        </FormItem_Shadcn_>
-      )}
+      render={({ field }) => <FormItemSelect ref={ref} {...props} field={field} />}
     />
   )
 })
