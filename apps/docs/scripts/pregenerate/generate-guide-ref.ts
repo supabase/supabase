7d588d2d10d9b6446f9d6027190f5451e1a3094b
Charis Lam <26616127+charislam@users.noreply.github.com>
2024-03-06 21:41:28 -0500

    refactor: clean up

--- apps/docs/scripts/pregenerate/generate-guide-ref.ts
+++ apps/docs/scripts/pregenerate/generate-guide-ref.ts
@@ -5,9 +5,11 @@ import { parse } from 'yaml'
 
 import {
   type Menu,
+  type MenuId,
   type ReferenceMenu,
   menus,
 } from '../../components/Navigation/NavigationMenu/menus'
+import type { ICommonSection } from '../../components/reference/Reference.types'
 import { flattenSections } from '../../lib/helpers'
 import commonSections from '../../spec/common-client-libs-sections.json'
 
@@ -16,50 +18,56 @@ const REF_IDS = ['Database', 'Auth', 'Edge Functions', 'Realtime', 'Storage']
 const isClientLibReferenceMenu = (menu: Menu): menu is ReferenceMenu =>
   'commonSectionsFile' in menu && menu.commonSectionsFile === 'common-client-libs-sections.json'
 
+const attachSpec = async (library: ReferenceMenu) => ({
+  ...library,
+  spec: parse(await readFile(join(__dirname, '../..', 'spec', library.specFile), 'utf-8')),
+})
+
+const getCategory = (category: string) =>
+  commonSections.find((section) => section.type === 'category' && section.title === category)
+    ?.items ?? []
+
+const saveJson = (refId: string, json: Array<unknown>) =>
+  writeFile(
+    join(__dirname, 'generated', `commonClientLibFlat-${refId}.json`),
+    JSON.stringify(json, null, 2),
+    { encoding: 'utf-8' }
+  )
+
+const annotateWithLibraries =
+  <Library extends { id: MenuId; spec: any }>(clientLibraries: Array<Library>) =>
+  async (section: ICommonSection) => {
+    const libraries = [] as Array<string>
+
+    await Promise.all(
+      clientLibraries.map(
+        (library) =>
+          library.spec.functions.some((fn) => fn.id === section.id) && libraries.push(library.id)
+      )
+    )
+
+    return {
+      ...section,
+      libraries,
+    }
+  }
+
 const main = async () => {
   mkdirSync(join(__dirname, 'generated'), {
     recursive: true,
   })
 
-  const clientLibraries = await Promise.all(menus.filter(isClientLibReferenceMenu).map(async (library) => ({
-    ...library,
-    spec: parse(await readFile(join(__dirname, '../..', 'spec', library.specFile), 'utf-8'))
-  })))
+  const clientLibraries = await Promise.all(menus.filter(isClientLibReferenceMenu).map(attachSpec))
+  const annotateSection = annotateWithLibraries(clientLibraries)
 
-  const promises = []
-
-  REF_IDS.forEach(async (refId) => {
-    const sections =
-      commonSections.find((section) => section.type === 'category' && section.title === refId)
-        ?.items ?? []
-
-    const flatSections = flattenSections(sections)
-
-    const annotatedSections = await Promise.all(flatSections.map(async (section) => {
-      const libraries = [] as Array<string>
-
-      await Promise.all(clientLibraries.map(async (library) => {
-        if (library.spec.functions.some((fn) => fn.id === section.id)) {
-          libraries.push(library.id)
-        }
-      }))
-
-      return {
-        ...section,
-        libraries
-      }
-    }))
-    
-    promises.push(
-      writeFile(
-        join(__dirname, 'generated', `commonClientLibFlat-${refId}.json`),
-        JSON.stringify(annotatedSections, null, 2),
-        { encoding: 'utf-8' }
-      )
-    )
-  })
-
-  await Promise.all(promises)
+  await Promise.all(
+    REF_IDS.map(async (refId) => {
+      const sections = getCategory(refId)
+      const flatSections = flattenSections(sections)
+      const annotatedSections = await Promise.all(flatSections.map(annotateSection))
+      return saveJson(refId, annotatedSections)
+    })
+  )
 }
 
 main()
