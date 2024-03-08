525332ff97b4ebfb0980f7e2d65bc3bc06f13d04
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-03-08 14:27:16 +0800

    Feat/form item layout (#21835)
    * init FormInput
    
    * fix imports
    
    * more clean up
    
    * Update FormLayout.tsx
    
    * Update FormLayout.tsx
    
    * adding size prop
    
    * more updates
    
    * moved things. added checkbox
    
    * check in
    
    * Update FormLayout.tsx
    
    * Select now has a SelectItem
    
    * layouts moved
    
    * fix type errors
    
    * add layout stories
    
    * update story
    
    * refactored layout to be easier to maintain. dropped the defaultTheme support
    
    * update Form
    
    * update Badge
    
    * remove old code
    
    * remove more
    
    * more
    
    * Update index.tsx
    
    * clean up
    
    * Update preview.css
    
    * fix type errors
    
    * Update index.tsx
    
    * Update InfoTooltip.tsx
    
    * fix issues
    
    * add seperator
    
    * Update FormLayout.tsx
    
    * match styles to old component

--- packages/ui/src/components/shadcn/ui/input.tsx
+++ packages/ui/src/components/shadcn/ui/input.tsx
@@ -1,24 +1,37 @@
+import { VariantProps, cva } from 'class-variance-authority'
 import * as React from 'react'
-
+import { SIZE_VARIANTS, SIZE_VARIANTS_DEFAULT } from '../../../lib/constants'
 import { cn } from '../../../lib/utils/cn'
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+export interface InputProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'size'>,
+    VariantProps<typeof InputVariants> {}
 
-const customClasses = ['bg-control']
+const InputVariants = cva('aria-[]', {
+  variants: {
+    size: {
+      ...SIZE_VARIANTS,
+    },
+  },
+  defaultVariants: {
+    size: SIZE_VARIANTS_DEFAULT,
+  },
+})
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, size = 'medium', ...props }, ref) => {
     return (
       <input
         type={type}
-        className={cn(
-          'flex h-10 w-full rounded-md border border-control bg-control px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-foreground-muted',
-          'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-background-control focus-visible:ring-offset-2 focus-visible:ring-offset-foreground-muted disabled:cursor-not-allowed disabled:opacity-50',
-          ...customClasses,
-          className
-        )}
         ref={ref}
         {...props}
+        className={cn(
+          'flex h-10 w-full rounded-md border border-control bg-foreground/[.026] px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-foreground-muted',
+          'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-background-control focus-visible:ring-offset-2 focus-visible:ring-offset-foreground-muted disabled:cursor-not-allowed disabled:opacity-50',
+          'aria-[invalid=true]:bg-destructive-200 aria-[invalid=true]:border-destructive-400 aria-[invalid=true]:focus:border-destructive aria-[invalid=true]:focus-visible:border-destructive',
+          InputVariants({ size }),
+          className
+        )}
       />
     )
   }
