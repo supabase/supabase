2a89e06428dcaf664458bab616bd4d65b05fe39d
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2024-03-08 12:36:04 +0100

    changelog fix

--- apps/www/pages/changelog.tsx
+++ apps/www/pages/changelog.tsx
@@ -13,7 +13,6 @@ import { GetServerSideProps } from 'next'
 import Link from 'next/link'
 import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/outline'
 import { deletedDiscussions } from '~/lib/changelog.utils'
-import { getNavLatestPosts, getStaticLatestPosts } from '~/lib/posts'
 import PostTypes from '~/types/post'
 
 export type Discussion = {
@@ -221,7 +220,6 @@ export const getServerSideProps: GetServerSideProps = async ({ res, query }) => 
       changelog: sortedCombinedEntries,
       pageInfo: pageInfo,
       restPage: Number(restPage),
-      latestPosts: getNavLatestPosts(),
     },
   }
 }
