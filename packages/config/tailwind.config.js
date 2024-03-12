9e872555862d1a21f62c24c0c2aa0f7ea5d538b9
Charis Lam <26616127+charislam@users.noreply.github.com>
2024-03-12 09:47:04 -0400

    docs: add usage examples

--- packages/config/tailwind.config.js
+++ packages/config/tailwind.config.js
@@ -420,10 +420,20 @@ const uiConfig = ui({
  *
  * When users prefer reduced motion, the duration of transform transitions is
  * reduced to something negiglible (1ms). The original `transition-*` properties
- * aren't overriden to provide flexibility, in situations where you want to
+ * aren't overridden to provide flexibility, in situations where you want to
  * handle the `prefers-reduced-motion` case some other way.
  *
  * See https://css-tricks.com/levels-of-fix/.
+ *
+ * Usage: <div className="transition-safe duration-safe-100">
+ *        - Transitioned properties will animate with duration 100, _except_
+ *          transform properties when prefers-reduced-motion is on, which
+ *          will animate instantaneously.
+ *
+ * Note:
+ *   - `duration-safe` must be used with `transition-safe`
+ *   - Non-safe `duration` must be used with non-safe `transition`
+ *   - (Cannot be mixed)
  */
 function motionSafeTransition({ addUtilities, matchUtilities, theme }) {
   addUtilities({
