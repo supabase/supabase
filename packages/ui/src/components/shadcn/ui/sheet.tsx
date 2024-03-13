204b4e04a3f43ef970be136c1c32986dc2bf2604
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-03-13 18:00:21 +0800

    fix: FormItemLayout width fix, `<Sheet/>` renamed with prop name reverted, started new form story (#21989)
    * init
    
    * Fix some TS errors. Comment out a unspecified form field.
    
    ---------
    
    Co-authored-by: Ivan Vasilov <vasilov.ivan@gmail.com>

--- packages/ui/src/components/shadcn/ui/sheet.tsx
+++ packages/ui/src/components/shadcn/ui/sheet.tsx
@@ -5,6 +5,7 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import * as React from 'react'
 
 import { cn } from '../../../lib/utils/cn'
+import { X } from 'lucide-react'
 
 const Sheet = SheetPrimitive.Root
 
@@ -14,23 +15,23 @@ const SheetClose = SheetPrimitive.Close
 
 const portalVariants = cva('fixed inset-0 z-40 flex', {
   variants: {
-    position: {
+    side: {
       top: 'items-start',
       bottom: 'items-end',
       left: 'justify-start',
       right: 'justify-end',
     },
   },
-  defaultVariants: { position: 'right' },
+  defaultVariants: { side: 'right' },
 })
 
 interface SheetPortalProps
   extends SheetPrimitive.DialogPortalProps,
     VariantProps<typeof portalVariants> {}
 
-const SheetPortal = ({ position, children, ...props }: SheetPortalProps) => (
+const SheetPortal = ({ side, children, ...props }: SheetPortalProps) => (
   <SheetPrimitive.Portal {...props}>
-    <div className={portalVariants({ position })}>{children}</div>
+    <div className={portalVariants({ side })}>{children}</div>
   </SheetPrimitive.Portal>
 )
 SheetPortal.displayName = SheetPrimitive.Portal.displayName
@@ -41,7 +42,7 @@ const SheetOverlay = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <SheetPrimitive.Overlay
     className={cn(
-      'fixed inset-0 z-40 bg-background/80 backdrop-blur-sm transition-all duration-100 data-[state=closed]:animate-out data-[state=closed]:fade-out data-[state=open]:fade-in',
+      'fixed inset-0 z-40 bg-alternative/90 backdrop-blur-sm transition-all duration-100 data-[state=closed]:animate-out data-[state=closed]:fade-out data-[state=open]:fade-in',
       className
     )}
     {...props}
@@ -50,99 +51,96 @@ const SheetOverlay = React.forwardRef<
 ))
 SheetOverlay.displayName = SheetPrimitive.Overlay.displayName
 
-const sheetVariants = cva(
-  'fixed z-40 scale-100 gap-4 bg-background p-6 opacity-100 shadow-lg border',
-  {
-    variants: {
-      position: {
-        top: 'animate-in slide-in-from-top w-full duration-300',
-        bottom: 'animate-in slide-in-from-bottom w-full duration-300',
-        left: 'animate-in slide-in-from-left h-full duration-300',
-        right: 'animate-in slide-in-from-right h-full duration-300',
-      },
-      size: {
-        content: '',
-        default: '',
-        sm: '',
-        lg: '',
-        xl: '',
-        xxl: '',
-        full: '',
-      },
+const sheetVariants = cva('fixed z-40 scale-100 gap-4 bg-studio opacity-100 shadow-lg', {
+  variants: {
+    side: {
+      top: 'animate-in slide-in-from-top w-full duration-300 border-b',
+      bottom: 'animate-in slide-in-from-bottom w-full duration-300 border-t',
+      left: 'animate-in slide-in-from-left h-full duration-300 border-r',
+      right: 'animate-in slide-in-from-right h-full duration-300 border-l',
     },
-    compoundVariants: [
-      {
-        position: ['top', 'bottom'],
-        size: 'content',
-        class: 'max-h-screen',
-      },
-      {
-        position: ['top', 'bottom'],
-        size: 'default',
-        class: 'h-1/3',
-      },
-      {
-        position: ['top', 'bottom'],
-        size: 'sm',
-        class: 'h-1/4',
-      },
-      {
-        position: ['top', 'bottom'],
-        size: 'lg',
-        class: 'h-1/2',
-      },
-      {
-        position: ['top', 'bottom'],
-        size: 'xl',
-        class: 'h-5/6',
-      },
-      {
-        position: ['top', 'bottom'],
-        size: 'full',
-        class: 'h-screen',
-      },
-      {
-        position: ['right', 'left'],
-        size: 'content',
-        class: 'max-w-screen',
-      },
-      {
-        position: ['right', 'left'],
-        size: 'default',
-        class: 'w-1/3',
-      },
-      {
-        position: ['right', 'left'],
-        size: 'sm',
-        class: 'w-1/4',
-      },
-      {
-        position: ['right', 'left'],
-        size: 'lg',
-        class: 'w-1/2',
-      },
-      {
-        position: ['right', 'left'],
-        size: 'xl',
-        class: 'w-4/6',
-      },
-      {
-        position: ['right', 'left'],
-        size: 'xxl',
-        class: 'w-5/6',
-      },
-      {
-        position: ['right', 'left'],
-        size: 'full',
-        class: 'w-screen',
-      },
-    ],
-    defaultVariants: {
-      position: 'right',
+    size: {
+      content: '',
+      default: '',
+      sm: '',
+      lg: '',
+      xl: '',
+      xxl: '',
+      full: '',
+    },
+  },
+  compoundVariants: [
+    {
+      side: ['top', 'bottom'],
+      size: 'content',
+      class: 'max-h-screen',
+    },
+    {
+      side: ['top', 'bottom'],
       size: 'default',
+      class: 'h-1/3',
     },
-  }
-)
+    {
+      side: ['top', 'bottom'],
+      size: 'sm',
+      class: 'h-1/4',
+    },
+    {
+      side: ['top', 'bottom'],
+      size: 'lg',
+      class: 'h-1/2',
+    },
+    {
+      side: ['top', 'bottom'],
+      size: 'xl',
+      class: 'h-5/6',
+    },
+    {
+      side: ['top', 'bottom'],
+      size: 'full',
+      class: 'h-screen',
+    },
+    {
+      side: ['right', 'left'],
+      size: 'content',
+      class: 'max-w-screen',
+    },
+    {
+      side: ['right', 'left'],
+      size: 'default',
+      class: 'w-1/3',
+    },
+    {
+      side: ['right', 'left'],
+      size: 'sm',
+      class: 'w-1/4',
+    },
+    {
+      side: ['right', 'left'],
+      size: 'lg',
+      class: 'w-1/2',
+    },
+    {
+      side: ['right', 'left'],
+      size: 'xl',
+      class: 'w-4/6',
+    },
+    {
+      side: ['right', 'left'],
+      size: 'xxl',
+      class: 'w-5/6',
+    },
+    {
+      side: ['right', 'left'],
+      size: 'full',
+      class: 'w-screen',
+    },
+  ],
+  defaultVariants: {
+    side: 'right',
+    size: 'default',
+  },
+})
 
 export interface DialogContentProps
   extends React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>,
@@ -151,32 +149,41 @@ export interface DialogContentProps
 const SheetContent = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Content>,
   DialogContentProps
->(({ position, size, className, children, ...props }, ref) => (
-  <SheetPortal position={position}>
+>(({ side, size, className, children, ...props }, ref) => (
+  <SheetPortal side={side}>
     <SheetOverlay />
     <SheetPrimitive.Content
       ref={ref}
-      className={cn(sheetVariants({ position, size }), className)}
+      className={cn(sheetVariants({ side, size }), className)}
       {...props}
     >
       {children}
-      {/* <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
+      <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
         <X className="h-4 w-4" />
         <span className="sr-only">Close</span>
-      </SheetPrimitive.Close> */}
+      </SheetPrimitive.Close>
     </SheetPrimitive.Content>
   </SheetPortal>
 ))
 SheetContent.displayName = SheetPrimitive.Content.displayName
 
 const SheetHeader = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
-  <div className={cn('px-5 py-4 text-center sm:text-left', className)} {...props} />
+  <div className={cn('px-5 py-4 text-center sm:text-left border-b', className)} {...props} />
 )
 SheetHeader.displayName = 'SheetHeader'
 
+const SheetSection = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
+  <div className={cn('px-content py-content', className)} {...props} />
+)
+SheetSection.displayName = 'SheetSection'
+
 const SheetFooter = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
-    className={cn('flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2', className)}
+    className={cn(
+      'px-5 py-3 border-t w-full',
+      'flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2',
+      className
+    )}
     {...props}
   />
 )
@@ -211,8 +218,9 @@ export {
   SheetClose,
   SheetContent,
   SheetDescription,
-  SheetFooter,
   SheetHeader,
+  SheetSection,
+  SheetFooter,
   SheetTitle,
   SheetTrigger,
 }
