0bb9547795fc71ee7e7e6362519c81974fa16835
Ivan Vasilov <vasilov.ivan@gmail.com>
2024-03-14 10:22:42 +0100

    Add sameWidthAsTrigger to the popover component.

--- packages/ui/src/components/shadcn/ui/popover.tsx
+++ packages/ui/src/components/shadcn/ui/popover.tsx
@@ -2,6 +2,7 @@ import * as PopoverPrimitive from '@radix-ui/react-popover'
 import * as React from 'react'
 
 import { cn } from '../../../lib/utils/cn'
+import styles from './popover.module.css'
 
 const Popover = PopoverPrimitive.Root
 
@@ -11,28 +12,42 @@ type PopoverContentProps = {
   portal?: boolean
   align?: 'center' | 'start' | 'end'
   sideOffset?: number
+  sameWidthAsTrigger?: boolean
 } & React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
 
 const PopoverContent = React.forwardRef<
   React.ElementRef<typeof PopoverPrimitive.Content>,
   PopoverContentProps
->(({ className, align = 'center', sideOffset = 4, portal = false, ...props }, ref) => {
-  const Portal = portal ? PopoverPrimitive.Portal : React.Fragment
-  return (
-    <Portal>
-      <PopoverPrimitive.Content
-        ref={ref}
-        align={align}
-        sideOffset={sideOffset}
-        className={cn(
-          'z-50 w-72 rounded-md border border-overlay bg-overlay p-4 text-popover-foreground shadow-md outline-none animate-in data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
-          className
-        )}
-        {...props}
-      />
-    </Portal>
-  )
-})
+>(
+  (
+    {
+      className,
+      align = 'center',
+      sideOffset = 4,
+      portal = false,
+      sameWidthAsTrigger = true,
+      ...props
+    },
+    ref
+  ) => {
+    const Portal = portal ? PopoverPrimitive.Portal : React.Fragment
+    return (
+      <Portal>
+        <PopoverPrimitive.Content
+          ref={ref}
+          align={align}
+          sideOffset={sideOffset}
+          className={cn(
+            sameWidthAsTrigger ? styles['popover-trigger-width'] : '',
+            'z-50 w-72 rounded-md border border-overlay bg-overlay p-4 text-popover-foreground shadow-md outline-none animate-in data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+            className
+          )}
+          {...props}
+        />
+      </Portal>
+    )
+  }
+)
 PopoverContent.displayName = 'PopoverContent'
 
 export { Popover, PopoverContent, PopoverTrigger }
