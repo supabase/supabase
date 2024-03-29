bf2367d7954544ae9dac589ac83adb77378a432f
Joshen Lim <joshenlimek@gmail.com>
2024-03-29 15:22:35 +0700

    Read replicas support enabling physical backups rather than forcing a PITR addon

--- apps/studio/components/interfaces/Settings/Infrastructure/InfrastructureConfiguration/DeployNewReplicaPanel.tsx
+++ apps/studio/components/interfaces/Settings/Infrastructure/InfrastructureConfiguration/DeployNewReplicaPanel.tsx
@@ -20,6 +20,7 @@ import { useSelectedOrganization, useSelectedProject } from 'hooks'
 import { AWS_REGIONS, AWS_REGIONS_DEFAULT, AWS_REGIONS_KEYS, BASE_PATH } from 'lib/constants'
 import { AVAILABLE_REPLICA_REGIONS, AWS_REGIONS_VALUES } from './InstanceConfiguration.constants'
 import { useBackupsQuery } from 'data/database/backups-query'
+import { useEnablePhysicalBackupsMutation } from 'data/database/enable-physical-backups-mutation'
 
 // [Joshen] FYI this is purely for AWS only, need to update to support Fly eventually
 
@@ -39,11 +40,32 @@ const DeployNewReplicaPanel = ({
   const { ref: projectRef } = useParams()
   const project = useSelectedProject()
   const org = useSelectedOrganization()
+  const [refetchInterval, setRefetchInterval] = useState<number | boolean>(false)
 
   const { data } = useReadReplicasQuery({ projectRef })
-  const { data: backups } = useBackupsQuery({ projectRef })
+  const { data: backups } = useBackupsQuery(
+    { projectRef },
+    {
+      refetchInterval: refetchInterval as any,
+      refetchOnWindowFocus: false,
+      onSuccess: (data) => {
+        if (data.walg_enabled) setRefetchInterval(false)
+      },
+    }
+  )
   const { data: addons, isSuccess } = useProjectAddonsQuery({ projectRef })
   const { data: subscription } = useOrgSubscriptionQuery({ orgSlug: org?.slug })
+
+  const { mutate: enablePhysicalBackups, isLoading: isEnabling } = useEnablePhysicalBackupsMutation(
+    {
+      onSuccess: () => {
+        toast.success(
+          'Physical backups are currently being enabled, please check back in a few minutes!'
+        )
+        setRefetchInterval(5000)
+      },
+    }
+  )
 
   const { mutate: setUpReplica, isLoading: isSettingUp } = useReadReplicaSetUpMutation({
     onSuccess: () => {
@@ -128,38 +150,33 @@ const DeployNewReplicaPanel = ({
           <Alert_Shadcn_>
             <WarningIcon />
             <AlertTitle_Shadcn_>
-              Point in time recovery is required to deploy replicas
+              {refetchInterval !== false
+                ? 'Physical backups are currently being enabled'
+                : 'Physical backups are required to deploy replicas'}
             </AlertTitle_Shadcn_>
-            {isFreePlan ? (
-              <AlertDescription_Shadcn_>
-                To enable PITR, you may first upgrade your organization's plan to at least Pro, then
-                purchase the PITR add on for your project via the{' '}
-                <Link
-                  href={`/project/${projectRef}/settings/addons?panel=pitr`}
-                  className="text-brand"
-                >
-                  project settings
-                </Link>
-                .
+            <AlertDescription_Shadcn_>
+              {refetchInterval !== false
+                ? 'This warning will go away once physical backups have been enabled - check back in a few minutes!'
+                : 'Enabling physical backups will take a few minutes, after which you will be able to deploy read replicas.'}
+            </AlertDescription_Shadcn_>
+            {refetchInterval !== false ? (
+              <AlertDescription_Shadcn_ className="mt-2">
+                You may start deploying read replicas thereafter once this is completed.
               </AlertDescription_Shadcn_>
             ) : (
-              <AlertDescription_Shadcn_>
-                Enable the add-on in your project's settings first before deploying read replicas.
+              <AlertDescription_Shadcn_ className="mt-2">
+                <Button
+                  type="default"
+                  loading={isEnabling}
+                  disabled={isEnabling}
+                  onClick={() => {
+                    if (projectRef) enablePhysicalBackups({ ref: projectRef })
+                  }}
+                >
+                  Enable physical backups
+                </Button>
               </AlertDescription_Shadcn_>
             )}
-            <AlertDescription_Shadcn_ className="mt-2">
-              <Button type="default">
-                <Link
-                  href={
-                    isFreePlan
-                      ? `/org/${org?.slug}/billing?panel=subscriptionPlan`
-                      : `/project/${projectRef}/settings/addons?panel=pitr`
-                  }
-                >
-                  {isFreePlan ? 'Upgrade to Pro' : 'Enable PITR add-on'}
-                </Link>
-              </Button>
-            </AlertDescription_Shadcn_>
           </Alert_Shadcn_>
         )}
 
@@ -253,9 +270,9 @@ const DeployNewReplicaPanel = ({
               href="https://supabase.com/docs/guides/platform/org-based-billing#usage-based-billing-for-compute"
               target="_blank"
               rel="noreferrer"
-              className="underline"
+              className="underline hover:text-foreground transition"
             >
-              Usage-based billing
+              usage-based billing
             </Link>{' '}
             for compute.
           </p>
