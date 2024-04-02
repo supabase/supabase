bbe8fa7f60d527cb04dec1c89d4a37c054815fa7
Joshen Lim <joshenlimek@gmail.com>
2024-04-02 14:22:36 +0700

    Add multiselect example

--- packages/ui/src/components/TreeView/TreeView.stories.tsx
+++ packages/ui/src/components/TreeView/TreeView.stories.tsx
@@ -295,3 +295,144 @@ export const withEditEntity: Story = {
     )
   },
 }
+export const withMultiSelect: Story = {
+  args: {
+    className: 'w-[420px] h-[520px]',
+    multiSelect: true,
+    data: {
+      name: 'basic tree',
+      children: [
+        {
+          id: 1,
+          name: 'Current batch',
+          children: [
+            { id: 2, name: 'index.js' },
+            { id: 3, name: 'styles.css' },
+          ],
+        },
+        {
+          id: 4,
+          name: 'Older queries',
+          children: [
+            {
+              id: 5,
+              name: 'all countries',
+            },
+            {
+              id: 6,
+              name: 'add new countries',
+            },
+            {
+              id: 7,
+              name: 'regions',
+            },
+            {
+              id: 8,
+              name: 'regions by customer',
+            },
+          ],
+        },
+        {
+          id: 9,
+          name: 'query all users',
+        },
+        {
+          id: 10,
+          name: 'users in last day',
+        },
+        {
+          id: 11,
+          name: 'new users over time',
+        },
+      ],
+    },
+  },
+  /**
+   * 👇 To avoid linting issues, it is recommended to use a function with a capitalized name.
+   * If you are not concerned with linting, you may use an arrow function.
+   */
+  render: function Render(args) {
+    const [treeData, setDataTreeState] = useState(args.data)
+
+    function handleRenameSelect(id) {
+      // find id in treeData and set it to edit mode
+      let updatedTreeData = { ...treeData }
+      const findNode = (node: any) => {
+        if (node.id === id) {
+          node.metadata = { isEditing: true }
+        }
+        if (node.children) {
+          node.children.forEach(findNode)
+        }
+      }
+      console.log(updatedTreeData)
+      updatedTreeData.children.forEach(findNode)
+      setDataTreeState(updatedTreeData)
+    }
+
+    return (
+      <TreeView
+        {...args}
+        data={flattenTree(args.data)}
+        aria-label="directory tree"
+        nodeRenderer={({ element, isBranch, isExpanded, getNodeProps, level, isSelected }) => {
+          return (
+            <ContextMenu modal={false}>
+              <ContextMenuTrigger asChild>
+                <TreeViewItem
+                  isExpanded={isExpanded}
+                  isBranch={isBranch}
+                  isSelected={isSelected}
+                  level={level}
+                  xPadding={16}
+                  name={element.name}
+                  isEditing={element.metadata?.isEditing === true}
+                  onEditSubmit={(value) => {
+                    console.log('value', value)
+                    let updatedTreeData = { ...treeData }
+                    const findNode = (node: any) => {
+                      if (node.id === element.id) {
+                        node.name = value
+                        node.metadata = { isEditing: false }
+                      }
+                      if (node.children) {
+                        node.children.forEach(findNode)
+                      }
+                    }
+                    updatedTreeData.children.forEach(findNode)
+                    setDataTreeState(updatedTreeData)
+                  }}
+                  {...getNodeProps()}
+                />
+              </ContextMenuTrigger>
+              <ContextMenuContent
+                onCloseAutoFocus={(e) => {
+                  // stop focus propagation
+                  // so input in TreeViewItem gets focus
+                  e.stopPropagation()
+                }}
+              >
+                <ContextMenuItem
+                  onSelect={(e) => {
+                    handleRenameSelect(element.id)
+                  }}
+                  onFocusCapture={(e) => {
+                    // stop focus propagation
+                    // so input in TreeViewItem gets focus
+                    e.stopPropagation()
+                  }}
+                >
+                  Rename
+                </ContextMenuItem>
+                <ContextMenuItem disabled>Open in new tab</ContextMenuItem>
+                <ContextMenuItem disabled>Share with team</ContextMenuItem>
+                <ContextMenuSeparator />
+                <ContextMenuItem disabled>Delete</ContextMenuItem>
+              </ContextMenuContent>
+            </ContextMenu>
+          )
+        }}
+      />
+    )
+  },
+}
