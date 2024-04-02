713eabdb53b32952f7270395e6a61fa15f0db22e
Einshine || Shine <theeinshine3301@gmail.com>
2024-04-02 10:20:41 +0530

    Ran Prettier : )

--- apps/docs/hooks/useHash.ts
+++ apps/docs/hooks/useHash.ts
@@ -1,33 +1,36 @@
 import { useState, useCallback, useEffect } from 'react';
 
 const useHash = () => {
- const [hash, setHash] = useState(() => {
+  const [hash, setHash] = useState(() => {
     if (typeof window !== 'undefined') {
       const url = new URL(window.location.href);
       return url.hash.substring(1); // Removes the leading '#'
     }
     return undefined;
- });
+  });
 
- const hashChangeHandler = useCallback(() => {
+  const hashChangeHandler = useCallback(() => {
     const url = new URL(window.location.href);
     setHash(url.hash.substring(1));
- }, []);
+  }, []);
 
- useEffect(() => {
+  useEffect(() => {
     window.addEventListener('hashchange', hashChangeHandler);
     return () => {
       window.removeEventListener('hashchange', hashChangeHandler);
     };
- }, [hashChangeHandler]); //included dependancy.
+  }, [hashChangeHandler]); //included dependancy.
 
- const updateHash = useCallback((newHash) => {
-    if (newHash !== hash) {
-      window.location.hash = newHash;
-    }
- }, [hash]); //included dependancy.
+  const updateHash = useCallback(
+    (newHash) => {
+      if (newHash !== hash) {
+        window.location.hash = newHash;
+      }
+    },
+    [hash]
+  ); //included dependancy.
 
- return [hash, updateHash];
+  return [hash, updateHash];
 };
 
 export default useHash;
