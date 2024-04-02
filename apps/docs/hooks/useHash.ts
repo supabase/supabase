cc23878ca0b4f2fe53c96d23ab5ef7e91c274aad
Charis Lam <26616127+charislam@users.noreply.github.com>
2024-04-02 15:48:34 -0400

    style: appease prettier

--- apps/docs/hooks/useHash.ts
+++ apps/docs/hooks/useHash.ts
@@ -1,36 +1,36 @@
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useCallback, useEffect } from 'react'
 
 const useHash = () => {
   const [hash, setHash] = useState(() => {
     if (typeof window !== 'undefined') {
-      const url = new URL(window.location.href);
-      return url.hash.substring(1); // Removes the leading '#'
+      const url = new URL(window.location.href)
+      return url.hash.substring(1) // Removes the leading '#'
     }
-    return undefined;
-  });
+    return undefined
+  })
 
   const hashChangeHandler = useCallback(() => {
-    const url = new URL(window.location.href);
-    setHash(url.hash.substring(1));
-  }, []);
+    const url = new URL(window.location.href)
+    setHash(url.hash.substring(1))
+  }, [])
 
   useEffect(() => {
-    window.addEventListener('hashchange', hashChangeHandler);
+    window.addEventListener('hashchange', hashChangeHandler)
     return () => {
-      window.removeEventListener('hashchange', hashChangeHandler);
-    };
-  }, [hashChangeHandler]); //included dependancy.
+      window.removeEventListener('hashchange', hashChangeHandler)
+    }
+  }, [hashChangeHandler])
 
   const updateHash = useCallback(
     (newHash) => {
       if (newHash !== hash) {
-        window.location.hash = newHash;
+        window.location.hash = newHash
       }
     },
     [hash]
-  ); //included dependancy.
+  )
 
-  return [hash, updateHash];
-};
+  return [hash, updateHash]
+}
 
-export default useHash;
+export default useHash
