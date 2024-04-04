b828d9aae09e17c4654c7b99a2575ac2d6da1c45
Ivan Vasilov <vasilov.ivan@gmail.com>
2024-04-04 13:15:48 +0200

    Add link from Realtime to realtime policies.

--- apps/studio/components/layouts/RealtimeLayout/RealtimeMenu.utils.tsx
+++ apps/studio/components/layouts/RealtimeLayout/RealtimeMenu.utils.tsx
@@ -1,5 +1,6 @@
 import type { ProductMenuGroup } from 'components/ui/ProductMenu/ProductMenu.types'
 import type { Project } from 'data/projects/project-detail-query'
+import { ArrowUpRight } from 'lucide-react'
 
 export const generateRealtimeMenu = (project?: Project): ProductMenuGroup[] => {
   const ref = project?.ref ?? 'default'
@@ -12,6 +13,13 @@ export const generateRealtimeMenu = (project?: Project): ProductMenuGroup[] => {
           name: 'Inspector',
           key: 'inspector',
           url: `/project/${ref}/realtime/inspector`,
+          items: [],
+        },
+        {
+          name: 'Policies',
+          key: 'policies',
+          url: `/project/${ref}/auth/policies?schema=realtime`,
+          rightIcon: <ArrowUpRight strokeWidth={1} />,
           items: [],
         },
       ],
