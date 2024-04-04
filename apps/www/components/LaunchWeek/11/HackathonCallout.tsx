dca27f58bdcb251cce3378be374ff43ae8f3acf0
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2024-04-04 14:48:05 +0200

    remove comment

--- apps/www/components/LaunchWeek/11/HackathonCallout.tsx
+++ apps/www/components/LaunchWeek/11/HackathonCallout.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { TextLink, cn } from 'ui'
 
 const HackathonCallout = ({ className }: { className?: string }) => {
-  // const isLive = false
-
   return (
     <div
       className={cn(
