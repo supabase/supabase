636a2d6f9b8dcb6a2793446143b8a49e56a5c5bc
Charis Lam <26616127+charislam@users.noreply.github.com>
2024-04-11 19:24:55 -0400

    feat: minor polish of www version

--- apps/www/components/CommandMenu/CommandMenu.tsx
+++ apps/www/components/CommandMenu/CommandMenu.tsx
@@ -1,12 +1,30 @@
-import { CommandInput, CommandList, CommandMenu } from 'ui-patterns/CommandMenu'
+import { LayoutDashboard } from 'lucide-react'
+import {
+  CommandInput,
+  CommandList,
+  CommandMenu,
+  useRegisterCommands,
+} from 'ui-patterns/CommandMenu'
 import { useChangelogCommand } from 'ui-patterns/CommandMenu/prepackaged/Changelog'
 import { useDocsAiCommands } from 'ui-patterns/CommandMenu/prepackaged/DocsAi'
 import { useDocsSearchCommands } from 'ui-patterns/CommandMenu/prepackaged/DocsSearch'
 import { useSupportCommands } from 'ui-patterns/CommandMenu/prepackaged/Support'
 import { useThemeSwitcherCommands } from 'ui-patterns/CommandMenu/prepackaged/ThemeSwitcher'
 
+const useOpenDashboardCommand = () => {
+  useRegisterCommands('Dashboard', [
+    {
+      id: 'dashboard',
+      name: 'Go to your Dashboard',
+      route: '/dashboard',
+      icon: () => <LayoutDashboard />,
+    },
+  ])
+}
+
 const WwwCommandMenu = () => {
   useDocsSearchCommands()
+  useOpenDashboardCommand()
   useDocsAiCommands()
   useSupportCommands()
   useChangelogCommand()
