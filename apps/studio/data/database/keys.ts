2310b5b46d40c4ee38ac3910e3e6c4f8f41b3882
Alaister Young <a@alaisteryoung.com>
2024-04-12 11:55:52 +0800

    fix schemas query invalidation

--- apps/studio/data/database/keys.ts
+++ apps/studio/data/database/keys.ts
@@ -1,5 +1,4 @@
 export const databaseKeys = {
-  schemaList: (projectRef: string | undefined) => ['projects', projectRef, 'schemas'] as const,
   backups: (projectRef: string | undefined) => [projectRef, 'database', 'backups'] as const,
   poolingConfiguration: (projectRef: string | undefined) =>
     [projectRef, 'database', 'pooling-configuration'] as const,
