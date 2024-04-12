b2b13ed2c365474f7ebf088a8f0e2d677ab96a82
Francesco Sansalvadore <francescosansalvadore@mac.wind3.hub>
2024-04-12 12:58:36 +0200

    update og image

--- apps/www/pages/ga-week/index.tsx
+++ apps/www/pages/ga-week/index.tsx
@@ -31,7 +31,7 @@ export default function GAWeekIndex({ meetups }: Props) {
 
   const TITLE = 'Supabase GA Week | 15-19 April 2024'
   const DESCRIPTION = 'Join us for a week of announcing new features, every day at 7 AM PT.'
-  const OG_IMAGE = `${SITE_ORIGIN}/images/launchweek/lw11/lw11-ga-og.png`
+  const OG_IMAGE = `${SITE_ORIGIN}/images/launchweek/11/lw11-og-ga.png`
 
   const ticketNumber = query.ticketNumber?.toString()
   const bgImageId = query.bgImageId?.toString()
