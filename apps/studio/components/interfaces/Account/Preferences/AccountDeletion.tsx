b837e00daf5cdae23f565c70d8350c19a1aad7b9
Monica <monicakhprivate@hotmail.com>
2024-04-12 16:06:26 +0300

    Second design iteration

--- apps/studio/components/interfaces/Account/Preferences/AccountDeletion.tsx
+++ apps/studio/components/interfaces/Account/Preferences/AccountDeletion.tsx
@@ -1,90 +1,40 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Panel from 'components/ui/Panel'
-import ConfirmationModal from 'ui-patterns/Dialogs/ConfirmationModal'
-import {
-  Button,
-  Modal,
-  Alert_Shadcn_,
-  AlertTitle_Shadcn_,
-  AlertDescription_Shadcn_,
-  IconAlertTriangle,
-} from 'ui'
-import { useSendSupportTicketMutation } from 'data/feedback/support-ticket-send'
-import toast from 'react-hot-toast'
+import { Alert } from 'ui'
+import DeleteAccountButton from './DeleteAccountButton'
+import { useProfile } from 'lib/profile'
 
 const AccountDeletion = () => {
-  const [showDelete, setShowDelete] = useState(false)
-  const [isSubmitting, setIsSubmitting] = useState(false)
-
-  const { mutate: submitSupportTicket, isLoading } = useSendSupportTicketMutation({
-    onSuccess: () => {
-      toast.success('Support request sent. Thank you!')
-      setShowDelete(false)
-      setIsSubmitting(false)
-    },
-    onError: (error) => {
-      toast.error(`Failed to submit support ticket: ${error.message}`)
-      setIsSubmitting(false)
-    },
-  })
-
-  const onConfirmHandleDelete = () => {
-    const payload = {
-      subject: 'Account Deletion Request',
-      message: 'I want to delete my account.',
-      category: 'account_management',
-      severity: 'high',
-      allowSupportAccess: false,
-    }
-
-    console.log(payload)
-
-    setIsSubmitting(true)
-    submitSupportTicket(payload)
-  }
-
-  const openConfirmationModal = () => {
-    setShowDelete(true)
-  }
+  const { profile } = useProfile()
 
   return (
     <>
-      <Panel title={<h5 key="panel-title">Delete account - are you sure?</h5>}>
+      <Panel
+        title={
+          <p key="panel-title" className="uppercase">
+            Danger Zone
+          </p>
+        }
+      >
         <Panel.Content>
-          <Button type="danger" onClick={openConfirmationModal} disabled={isLoading}>
-            Delete account
-          </Button>
+          <Alert
+            withIcon
+            variant="danger"
+            // @ts-ignore
+            title={
+              <span className="text-red-900">
+                Deleting your account is permanent and cannot be undone.
+              </span>
+            }
+          >
+            <p className="text-red-900">
+              Your data will be deleted within 30 days, except we may retain a limited set of data
+              for longer where required or permitted by law.
+            </p>
+            <DeleteAccountButton profile={profile} />
+          </Alert>
         </Panel.Content>
       </Panel>
-
-      <ConfirmationModal
-        danger
-        size="medium"
-        visible={showDelete}
-        header="Delete account"
-        buttonLabel="Confirm"
-        buttonLoadingLabel="Sending request..."
-        onSelectConfirm={onConfirmHandleDelete}
-        onSelectCancel={() => setShowDelete(false)}
-      >
-        <Modal.Content>
-          <div className="py-6">
-            <Alert_Shadcn_ variant="warning">
-              <IconAlertTriangle strokeWidth={2} />
-              <AlertTitle_Shadcn_>Are You Sure You Want to Delete Your Account?</AlertTitle_Shadcn_>
-              <AlertDescription_Shadcn_>
-                <ul className="list-disc pl-2 text-sm text-foreground-light space-y-1">
-                  <li>Deleting your account is permanent and cannot be undone.</li>
-                  <li>
-                    Your data will be deleted within 30 days, except we may retain a limited set of
-                    data for longer where required or permitted by law.
-                  </li>
-                </ul>
-              </AlertDescription_Shadcn_>
-            </Alert_Shadcn_>
-          </div>
-        </Modal.Content>
-      </ConfirmationModal>
     </>
   )
 }
---
+++ apps/studio/components/interfaces/Account/Preferences/DeleteAccountButton.tsx
@@ -0,0 +1,137 @@
+import { useState } from 'react'
+import { Button, Form, Input, Modal } from 'ui'
+
+import { useStore } from 'hooks'
+import { useSendSupportTicketMutation } from 'data/feedback/support-ticket-send'
+import type { Profile } from 'data/profile/types'
+
+const DeleteAccountButton = ({ profile }: { profile?: Profile }) => {
+  const { ui } = useStore()
+
+  const [isOpen, setIsOpen] = useState(false)
+  const [value, setValue] = useState('')
+  const [supportTicketSubmissionStatus, setSupportTicketSubmissionStatus] = useState<
+    boolean | null
+  >(null)
+
+  const account = profile?.primary_email
+
+  const onValidate = (values: any) => {
+    const errors: any = {}
+    if (!values.account) {
+      errors.account = 'Enter the account information.'
+    }
+    if (values.account !== account) {
+      errors.account = 'Value entered does not match the value above.'
+    }
+    return errors
+  }
+
+  const { mutate: submitSupportTicket, isLoading } = useSendSupportTicketMutation({
+    onSuccess: () => {
+      setSupportTicketSubmissionStatus(true)
+    },
+    onError: (error) => {
+      setSupportTicketSubmissionStatus(false)
+    },
+  })
+
+  const onConfirmDelete = async (values: any) => {
+    if (!account) return console.error('Account information is required')
+
+    const payload = {
+      subject: 'Account Deletion Request',
+      message: 'I want to delete my account.',
+      category: 'Account deletion',
+      severity: 'Medium',
+      allowSupportAccess: false,
+    }
+
+    try {
+      await submitSupportTicket(payload)
+      setIsOpen(false)
+    } catch (error) {
+      ui.setNotification({
+        category: 'error',
+        message: `Failed to submit support ticket: ${error}`,
+      })
+    } finally {
+      ui.setNotification({
+        category: 'success',
+        message: `Support request sent. Thank you!`,
+      })
+    }
+  }
+
+  return (
+    <>
+      <div className="mt-2">
+        <Button loading={!account} onClick={() => setIsOpen(true)} type="danger">
+          Delete account
+        </Button>
+      </div>
+      <Modal
+        closable
+        hideFooter
+        size="small"
+        visible={isOpen}
+        onCancel={() => setIsOpen(false)}
+        header={
+          <div className="flex items-baseline gap-2">
+            <h5 className="text-sm text-foreground">Delete account</h5>
+            <span className="text-xs text-foreground-lighter">Are you sure?</span>
+          </div>
+        }
+      >
+        <Form
+          validateOnBlur
+          initialValues={{ account: '' }}
+          onSubmit={onConfirmDelete}
+          validate={onValidate}
+        >
+          {() => (
+            <div className="space-y-4 py-3">
+              <Modal.Content>
+                <p className="text-sm text-foreground-lighter">
+                  This action <span className="text-foreground">cannot</span> be undone. This will
+                  permanently delete your account.
+                </p>
+              </Modal.Content>
+              <Modal.Separator />
+              <Modal.Content>
+                <Input
+                  id="account"
+                  label={
+                    <span>
+                      Please type <span className="font-bold">{profile?.primary_email ?? ''}</span>{' '}
+                      to confirm
+                    </span>
+                  }
+                  onChange={(e) => setValue(e.target.value)}
+                  value={value}
+                  placeholder="Enter the account above"
+                  className="w-full"
+                />
+              </Modal.Content>
+              <Modal.Separator />
+              <Modal.Content>
+                <Button
+                  block
+                  size="small"
+                  type="danger"
+                  htmlType="submit"
+                  loading={isLoading}
+                  disabled={isLoading}
+                >
+                  I understand, delete this account
+                </Button>
+              </Modal.Content>
+            </div>
+          )}
+        </Form>
+      </Modal>
+    </>
+  )
+}
+
+export default DeleteAccountButton
