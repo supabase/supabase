839c61cb4e5458c71c3c0ac3cf753d4074d8e19d
Andrew Smith <a.smith@silentworks.co.uk>
2024-04-15 19:38:36 +0000

    update sveltekit user management example (#22688)

--- examples/user-management/sveltekit-user-management/src/routes/auth/callback/+server.ts
+++ examples/user-management/sveltekit-user-management/src/routes/auth/callback/+server.ts
@@ -1,11 +1,12 @@
+// src/routes/auth/callback/+server.ts
 import { redirect } from '@sveltejs/kit'
 
 export const GET = async ({ url, locals: { supabase } }) => {
-	const code = url.searchParams.get('code')
+  const code = url.searchParams.get('code')
 
-	if (code) {
-		await supabase.auth.exchangeCodeForSession(code)
-	}
+  if (code) {
+    await supabase.auth.exchangeCodeForSession(code)
+  }
 
-	throw redirect(303, '/account')
+  throw redirect(303, '/account')
 }
