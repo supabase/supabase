31eba7562dac6a4d7f5c70e99f471ccb97a9c4da
hallidayo <22655069+Hallidayo@users.noreply.github.com>
2024-04-15 19:49:25 +0100

    connect flutter changes

--- apps/studio/components/interfaces/Home/Connect/content/flutter/supabaseflutter/content.tsx
+++ apps/studio/components/interfaces/Home/Connect/content/flutter/supabaseflutter/content.tsx
@@ -12,41 +12,9 @@ const ContentFile = ({ projectKeys }: ContentFileProps) => {
   return (
     <ConnectTabs>
       <ConnectTabTriggers>
-        <ConnectTabTrigger value="pubspec.yaml" />
         <ConnectTabTrigger value="lib/main.dart" />
+        <ConnectTabTrigger value="lib/main.dart (app)" />
       </ConnectTabTriggers>
-
-      <ConnectTabContent value="pubspec.yaml">
-        <SimpleCodeBlock className="bash" parentClassName="min-h-72">
-          {`
-name: supabase_todos
-description: Sample app on how to get started with Supabase on Flutter.
-
-publish_to: 'none'
-
-version: 1.0.0+1
-
-environment:
-  sdk: '>=2.17.0 <4.0.0'
-
-dependencies:
-  flutter:
-    sdk: flutter
-
-  supabase_flutter: ^2.0.0
-
-dev_dependencies:
-  flutter_test:
-    sdk: flutter
-  lint: ^1.5.3
-
-flutter:
-  uses-material-design: true
-  assets:
-    - .env
-        `}
-        </SimpleCodeBlock>
-      </ConnectTabContent>
 
       <ConnectTabContent value="lib/main.dart">
         <SimpleCodeBlock className="dart" parentClassName="min-h-72">
@@ -54,15 +22,19 @@ flutter:
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
   await Supabase.initialize(
     url: ${projectKeys.apiUrl ?? 'your-project-url'},
     anonKey: ${projectKeys.anonKey ?? 'your-anon-key'},
   );
   runApp(MyApp());
 }
+        `}
+        </SimpleCodeBlock>
+      </ConnectTabContent>
 
+      <ConnectTabContent value="lib/main.dart (app)">
+        <SimpleCodeBlock className="dart" parentClassName="min-h-72">
+          {`
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
