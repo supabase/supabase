3a7885fc174e13cedfc8643a53af4a24fc1dcc96
Jordi Enric <jordi.err@gmail.com>
2024-04-16 16:11:36 +0200

    clean warehouse query

--- apps/studio/data/analytics/warehouse-query.ts
+++ apps/studio/data/analytics/warehouse-query.ts
@@ -3,34 +3,25 @@ import { get } from 'data/fetchers'
 import { analyticsKeys } from './keys'
 
 export type WarehouseQueryVariables = {
-  projectRef: string
+  ref: string
   sql: string
 }
 
-export type WarehouseQueryResponse = any
-
 export async function getWarehouseQuery(
-  { projectRef, sql }: WarehouseQueryVariables,
+  { ref, sql }: WarehouseQueryVariables,
   signal?: AbortSignal
 ) {
-  if (!projectRef) {
-    throw new Error('projectRef is required')
+  if (!ref) {
+    throw new Error('ref is required')
   }
   if (!sql) {
     throw new Error('SQL must be provided')
   }
 
-  const response = await get<WarehouseQueryResponse>(
-    `/platform/projects/{ref}/analytics/warehouse/query`,
-    {
-      params: { path: { ref: projectRef }, query: { bq_sql: sql } },
-      signal,
-    }
-  )
-
-  if (response.error) {
-    throw response.error
-  }
+  const response = await get(`/platform/projects/{ref}/analytics/warehouse/query`, {
+    params: { path: { ref: ref }, query: { bq_sql: sql } },
+    signal,
+  })
 
   return response
 }
@@ -39,17 +30,17 @@ export type WarehouseQueryData = Awaited<ReturnType<typeof getWarehouseQuery>>
 export type WarehouseQueryError = unknown
 
 export const useWarehouseQueryQuery = <TData = WarehouseQueryData>(
-  { projectRef, sql }: WarehouseQueryVariables,
+  { ref, sql }: WarehouseQueryVariables,
   {
     enabled = true,
     ...options
   }: UseQueryOptions<WarehouseQueryData, WarehouseQueryError, TData> = {}
 ) =>
   useQuery<WarehouseQueryData, WarehouseQueryError, TData>(
-    analyticsKeys.warehouseQuery(projectRef, sql),
-    ({ signal }) => getWarehouseQuery({ projectRef, sql }, signal),
+    analyticsKeys.warehouseQuery(ref, sql),
+    ({ signal }) => getWarehouseQuery({ ref, sql }, signal),
     {
-      enabled: enabled && typeof projectRef !== 'undefined',
+      enabled: enabled && typeof ref !== 'undefined',
       staleTime: Infinity,
       // 15 mins cache time
       cacheTime: 15 * 60 * 1000,
