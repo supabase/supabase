e886b5b35b1e399a04fecbc5fe0d92c0f084232f
Einshine || Shine <theeinshine3301@gmail.com>
2024-04-17 00:09:31 +0530

    Enhanced useHash.ts hook ^^ (#22374)
    * Enhanced useHash.ts hook ^^
    
    added dependencies,
    auto URL parsing rather than manually splitting it,
    lastly cleanup in useEffect!
    
    * Ran Prettier : )
    
    * style: appease prettier
    
    ---------
    
    Co-authored-by: Charis Lam <26616127+charislam@users.noreply.github.com>

--- apps/docs/hooks/useHash.ts
+++ apps/docs/hooks/useHash.ts
@@ -1,12 +1,17 @@
 import { useState, useCallback, useEffect } from 'react'
 
 const useHash = () => {
-  const [hash, setHash] = useState(() =>
-    typeof window !== 'undefined' ? window.location.hash.split('#')[1] : undefined
-  )
+  const [hash, setHash] = useState(() => {
+    if (typeof window !== 'undefined') {
+      const url = new URL(window.location.href)
+      return url.hash.substring(1) // Removes the leading '#'
+    }
+    return undefined
+  })
 
   const hashChangeHandler = useCallback(() => {
-    setHash(window.location.hash.split('#')[1])
+    const url = new URL(window.location.href)
+    setHash(url.hash.substring(1))
   }, [])
 
   useEffect(() => {
@@ -18,7 +23,9 @@ const useHash = () => {
 
   const updateHash = useCallback(
     (newHash) => {
-      if (newHash !== hash) window.location.hash = newHash
+      if (newHash !== hash) {
+        window.location.hash = newHash
+      }
     },
     [hash]
   )
