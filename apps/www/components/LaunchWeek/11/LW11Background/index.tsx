88f26ef22f808c8f1ed3c3e68bd6c69061e846a5
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2024-04-16 16:35:19 +0200

    eagle 2 (#22789)
    * init
    
    * deep dark on dark mode
    
    * add twitter spaces
    
    * bg color
    
    * improve hp hero badge spacing
    
    * blog outline
    
    * updates title
    
    * some text updates
    
    * Adds more details
    
    * first images
    
    * update images
    
    * restore dark theme on unmount
    
    * update announcement banners
    
    * update
    
    * update illustrations
    
    * ga week summary blog card
    
    * update d2 blog link
    
    * update d2 blog link
    
    * hide vid atm
    
    * add link to guide
    
    * Adds links
    
    * add correct video id
    
    ---------
    
    Co-authored-by: Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
    Co-authored-by: Francesco Sansalvadore <francescosansalvadore@MacBook-Pro-2.local>
    Co-authored-by: Copple <10214025+kiwicopple@users.noreply.github.com>
    Co-authored-by: Inian <inian1234@gmail.com>

--- apps/www/components/LaunchWeek/11/LW11Background/index.tsx
+++ apps/www/components/LaunchWeek/11/LW11Background/index.tsx
@@ -155,16 +155,6 @@ const LW11Background = ({ className }: { className?: string }) => {
 
   !hasInitialized && init()
 
-  // useEffect(() => {
-  //   document.body.classList.add('bg-[#060809]')
-
-  //   return () => {
-  //     if (document.body.classList.contains('bg-[#060809]')) {
-  //       document.body.classList.remove('bg-[#060809]')
-  //     }
-  //   }
-  // }, [])
-
   return (
     <canvas
       ref={canvasRef}
