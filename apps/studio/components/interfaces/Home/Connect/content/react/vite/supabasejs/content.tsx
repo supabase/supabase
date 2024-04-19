4d9754a74f5a159ba0623119225eed8306ef2b2d
Terry Sutton <saltcod@gmail.com>
2024-04-19 10:39:30 -0230

    Rename again

--- apps/studio/components/interfaces/Home/Connect/content/react/vite/supabasejs/content.tsx
+++ apps/studio/components/interfaces/Home/Connect/content/react/vite/supabasejs/content.tsx
@@ -31,8 +31,8 @@ VITE_SUPABASE_ANON_KEY=${projectKeys.anonKey ?? 'your-anon-key'}
           {`
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_APP_URL;
-const supabaseKey = import.meta.env.VITE_APP_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase
