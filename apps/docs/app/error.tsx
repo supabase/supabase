cdf3f074ff0837f0a9f237b17472821734a429ed
Charis Lam <26616127+charislam@users.noreply.github.com>
2024-04-22 19:03:55 -0400

    fix(usability): prettify top-level error page

--- apps/docs/app/error.tsx
+++ apps/docs/app/error.tsx
@@ -1,5 +1,17 @@
 'use client'
 
-const ErrorPage = () => <>SOME ERROR!!!!</>
+import Link from 'next/link'
+import { Button } from 'ui'
+
+const ErrorPage = () => (
+  <div className="h-full w-full flex flex-col gap-8 p-8 items-center justify-center">
+    <span className="text-center text-5xl text-foreground-lighter">
+      Sorry, something went wrong
+    </span>
+    <Button asChild type="secondary" className="w-fit p-4 text-lg">
+      <Link href="/">Return to homepage</Link>
+    </Button>
+  </div>
+)
 
 export default ErrorPage
