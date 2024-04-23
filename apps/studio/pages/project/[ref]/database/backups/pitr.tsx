f9d9bacc0800355d115038b6608dd053fb665dcd
Terry Sutton <saltcod@gmail.com>
2024-04-23 10:18:40 -0230

    update

--- apps/studio/pages/project/[ref]/database/backups/pitr.tsx
+++ apps/studio/pages/project/[ref]/database/backups/pitr.tsx
@@ -83,7 +83,7 @@ const PITR = () => {
               primaryText="Point in Time Recovery is a Pro plan add-on."
               secondaryText={
                 plan === 'free'
-                  ? 'With PITR, you can roll back to a specific time (to the second!). PITR starts from $100/mo and is available for Pro plan customers. Note that the Pro Plan already includes daily backups for no extra charge—PITR is an optional upgrade.'
+                  ? 'With PITR, you can roll back to a specific time (to the second!). PITR starts from $100/mo and is available for Pro plan customers. Note that the Pro plan already includes daily backups for no extra charge—PITR is an optional upgrade.'
                   : 'Please enable the add-on to enable point in time recovery for your project.'
               }
               addon="pitr"
