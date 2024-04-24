eb74475bd5d78d4c423373c65de0a2c0c04a3152
Charis Lam <26616127+charislam@users.noreply.github.com>
2024-04-24 11:41:02 -0400

    fix: remove passwordless login redirect, page exists

--- apps/www/lib/redirects.js
+++ apps/www/lib/redirects.js
@@ -1563,11 +1563,6 @@ module.exports = [
   },
   {
     permanent: true,
-    source: '/docs/guides/auth/passwordless-login',
-    destination: '/docs/guides/auth/phone-login',
-  },
-  {
-    permanent: true,
     source: '/docs/guides/auth/passwordless-login/phone-sms-otp-messagebird',
     destination: '/docs/guides/auth/phone-login/messagebird',
   },
