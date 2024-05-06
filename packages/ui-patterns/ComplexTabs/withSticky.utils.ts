30fa3a63fd9941281a2476cb1087d41fa50bcd7e
Charis Lam <26616127+charislam@users.noreply.github.com>
2024-05-06 15:25:18 -0400

    refactor: simplify useSticky

--- packages/ui-patterns/ComplexTabs/withSticky.utils.ts
+++ packages/ui-patterns/ComplexTabs/withSticky.utils.ts
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, useRef, useState } from 'react'
+import { useCallback, useRef } from 'react'
 import { useInView } from 'react-intersection-observer'
 
 const useSticky = <Element extends HTMLElement>({
@@ -8,50 +8,40 @@ const useSticky = <Element extends HTMLElement>({
   enabled?: boolean
   style?: CSSStyleDeclaration
 } = {}) => {
-  const [inView, setInView] = useState(false)
   const stickyRef = useRef<Element>(null)
-  const { ref: observedRef } = useInView({
-    threshold: 0.1,
-    onChange: (inView) => (inView ? setInView(true) : setInView(false)),
-    skip: !enabled,
-  })
 
   const handleSticking = useCallback(
-    enabled
-      ? () => {
-          if (!stickyRef.current) return
+    (inView: boolean) => {
+      if (!stickyRef.current) return
 
-          if (inView) {
-            stickyRef.current.style.position = 'sticky'
-            stickyRef.current.style.top = '100px'
-            stickyRef.current.style.zIndex = '5'
+      if (inView) {
+        stickyRef.current.style.position = 'sticky'
+        stickyRef.current.style.top = '100px'
+        stickyRef.current.style.zIndex = '5'
 
-            for (const property in style) {
-              // @ts-ignore
-              stickyRef.current.style[property] = style[property]
-            }
-          } else {
-            stickyRef.current.style.position = ''
-            stickyRef.current.style.top = ''
-            stickyRef.current.style.zIndex = ''
-            for (const property in style) {
-              // @ts-ignore
-              stickyRef.current.style[property] = ''
-            }
-          }
+        for (const property in style) {
+          // @ts-ignore
+          stickyRef.current.style[property] = style[property]
         }
-      : () => {},
+      } else {
+        stickyRef.current.style.position = ''
+        stickyRef.current.style.top = ''
+        stickyRef.current.style.zIndex = ''
+        for (const property in style) {
+          // @ts-ignore
+          stickyRef.current.style[property] = ''
+        }
+      }
+    },
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [enabled, inView, JSON.stringify(style)]
+    [JSON.stringify(style)]
   )
 
-  /**
-   * Change the sticking behavior when the containing element scrolls in and out
-   * of sight.
-   */
-  useMemo(() => {
-    handleSticking()
-  }, [inView])
+  const { ref: observedRef, inView } = useInView({
+    threshold: 0.1,
+    onChange: handleSticking,
+    skip: !enabled,
+  })
 
   return {
     inView,
