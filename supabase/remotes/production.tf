7886c093a976a57b9c409a63c339cac293c3e1fe
Han Qiao <sweatybridge@gmail.com>
2024-05-08 15:57:11 +0800

    chore: update terraform database settings

--- supabase/remotes/production.tf
+++ supabase/remotes/production.tf
@@ -7,7 +7,9 @@ variable "linked_project" {
 resource "supabase_settings" "production" {
   project_ref = var.linked_project
 
-  database = jsonencode({})
+  database = jsonencode({
+    statement_timeout = "10s"
+  })
 
   api = jsonencode({
     db_schema            = "public, storage, graphql_public"
