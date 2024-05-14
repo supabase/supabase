99635fbb5e26ebebe9940d8352752077c41c65f6
Joshen Lim <joshenlimek@gmail.com>
2024-05-14 15:47:27 +0800

    Support restarting replicas (#26240)
    * Support restarting replicas
    
    * Support restarting replica through map view
    
    * Lint
    
    * Lint

--- apps/studio/components/interfaces/Settings/Infrastructure/InfrastructureConfiguration/InstanceConfiguration.tsx
+++ apps/studio/components/interfaces/Settings/Infrastructure/InfrastructureConfiguration/InstanceConfiguration.tsx
@@ -29,6 +29,7 @@ import { REPLICA_STATUS } from './InstanceConfiguration.constants'
 import { addRegionNodes, generateNodes, getDagreGraphLayout } from './InstanceConfiguration.utils'
 import { LoadBalancerNode, PrimaryNode, RegionNode, ReplicaNode } from './InstanceNode'
 import MapView from './MapView'
+import { RestartReplicaConfirmationModal } from './RestartReplicaConfirmationModal'
 
 // [Joshen] Just FYI, UI assumes single provider for primary + replicas
 // [Joshen] Idea to visualize grouping based on region: https://reactflow.dev/examples/layout/sub-flows
@@ -259,6 +260,7 @@ const InstanceConfigurationUI = () => {
                   setNewReplicaRegion(region)
                   setShowNewReplicaPanel(true)
                 }}
+                onSelectRestartReplica={setSelectedReplicaToRestart}
                 onSelectDropReplica={setSelectedReplicaToDrop}
               />
             )}
@@ -288,28 +290,13 @@ const InstanceConfigurationUI = () => {
         onCancel={() => setShowDeleteAllModal(false)}
       />
 
-      <ComputeInstanceSidePanel />
-
-      {/* <ConfirmationModal
-        size="medium"
-        visible={selectedReplicaToRestart !== undefined}
-        title="Confirm to restart selected replica?"
-        confirmLabel="Restart replica"
-        confirmLabelLoading="Restarting replica"
+      <RestartReplicaConfirmationModal
+        selectedReplica={selectedReplicaToRestart}
+        onSuccess={() => setRefetchInterval(5000)}
         onCancel={() => setSelectedReplicaToRestart(undefined)}
-        onConfirm={() => onConfirmRestartReplica()}
-      >
-        <p className="text-sm">Before restarting the replica, consider:</p>
-        <ul className="text-sm text-foreground-light py-1 list-disc mx-4 space-y-1">
-          <li>
-            Network traffic from this region may slow down while the replica is restarting,
-            especially if you have no other replicas in this region
-          </li>
-        </ul>
-        <p className="text-sm mt-2">
-          Are you sure you want to restart this replica (ID: {selectedReplicaToRestart?.id}) now?{' '}
-        </p>
-      </ConfirmationModal> */}
+      />
+
+      <ComputeInstanceSidePanel />
     </>
   )
 }
