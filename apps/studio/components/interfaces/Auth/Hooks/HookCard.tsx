e761c3ea1ea7c57c7fbccc62521d201324d9ecdf
Ivan Vasilov <vasilov.ivan@gmail.com>
2024-05-20 16:58:50 +0200

    Minor redesign of the hook card.

--- apps/studio/components/interfaces/Auth/Hooks/HookCard.tsx
+++ apps/studio/components/interfaces/Auth/Hooks/HookCard.tsx
@@ -12,12 +12,19 @@ interface HookCardProps {
 export const HookCard = ({ hook, canUpdateConfig, onToggle, onSelect }: HookCardProps) => {
   return (
     <div className="bg-surface-100 border-default overflow-hidden border shadow px-5 py-4 flex flex-row">
+      <div className="px-3 pt-1">
+        <Webhook size={24} strokeWidth="0.5" />
+      </div>
       <div className="flex flex-col flex-0 overflow-y-auto w-full">
         <span className="text-sm text-foreground">{hook.title}</span>
-        <span className="text-sm text-foreground-light">{hook.subtitle}</span>
+        <span className="text-sm text-foreground-lighter">{hook.subtitle}</span>
         <div className="text-sm flex flex-row space-x-5 py-4">
           {hook.method.type === 'postgres' ? (
             <div className="flex flex-col w-full space-y-2">
+              <div className="flex flex-row items-center">
+                <span className="text-foreground-light w-20">type</span>
+                <span className="text-foreground">Postgres function</span>
+              </div>
               <div className="flex flex-row items-center">
                 <span className="text-foreground-light w-20">schema</span>
                 <Input
@@ -45,6 +52,10 @@ export const HookCard = ({ hook, canUpdateConfig, onToggle, onSelect }: HookCard
             </div>
           ) : (
             <div className="flex flex-col w-full space-y-2">
+              <div className="flex flex-row items-center">
+                <span className="text-foreground-light w-20">type</span>
+                <span className="text-foreground">HTTPS endpoint</span>
+              </div>
               <div className="flex flex-row items-center">
                 <span className="text-foreground-light w-20">endpoint</span>
                 <Input
@@ -79,6 +90,18 @@ export const HookCard = ({ hook, canUpdateConfig, onToggle, onSelect }: HookCard
         </div>
       </div>
       <div className="p-4 flex-1">
+        {hook.enabled ? (
+          <Badge className="space-x-1" size="large" variant="brand">
+            <div className="h-3.5 w-3.5 bg-brand rounded-full flex justify-center items-center">
+              <Check className="h-2 w-2 text-background-overlay " strokeWidth={6} />
+            </div>
+            <span>Enabled</span>
+          </Badge>
+        ) : (
+          <Badge variant="warning" size="large">
+            <span>Disabled</span>
+          </Badge>
+        )}
       </div>
     </div>
   )
