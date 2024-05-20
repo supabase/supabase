91b64af0bda23cd2b933caedf8fe6d369063cdb3
Jordi Enric <jordi.err@gmail.com>
2024-05-20 19:26:00 +0200

    rm observer

--- apps/studio/pages/project/[ref]/logs/collections/[collectionToken].tsx
+++ apps/studio/pages/project/[ref]/logs/collections/[collectionToken].tsx
@@ -1,10 +1,9 @@
 import { WarehouseCollectionDetail } from 'components/interfaces/DataWarehouse/WarehouseCollectionDetail'
 import { LogsLayout } from 'components/layouts'
-import { observer } from 'mobx-react-lite'
 import { NextPageWithLayout } from 'types'
 
 const PageLayout: NextPageWithLayout = () => <WarehouseCollectionDetail />
 
 PageLayout.getLayout = (page) => <LogsLayout>{page}</LogsLayout>
 
-export default observer(PageLayout)
+export default PageLayout
