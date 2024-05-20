6aae6683ca66bb3ccc5635ab0d9ed1367d1c25ae
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-05-21 00:38:03 +0800

    Update aspect-ratio-demo.tsx

--- apps/design-system/registry/default/example/aspect-ratio-demo.tsx
+++ apps/design-system/registry/default/example/aspect-ratio-demo.tsx
@@ -6,7 +6,7 @@ export default function AspectRatioDemo() {
   return (
     <AspectRatio ratio={16 / 9} className="bg-muted">
       <Image
-        src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
+        src="https://images.unsplash.com/photo-1560937526-61078e7eb7d8?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="Photo by Drew Beamer"
         fill
         className="rounded-md object-cover"
