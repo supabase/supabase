c3771eb9800f41c6ef0f03c05067722b55dd5bea
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-05-21 11:22:41 +0800

    remove old ui registry

--- apps/design-system/registry/registry.ts
+++ apps/design-system/registry/registry.ts
@@ -1,12 +1,5 @@
 import { Registry } from '@/registry/schema'
-import { ui } from '@/registry/ui'
 import { examples } from '@/registry//examples'
 import { fragments } from '@/registry/fragments'
-// import { blocks } from '@/registry/blocks'
 
-export const registry: Registry = [
-  // ...ui,
-  ...fragments,
-  ...examples,
-  // ...blocks
-]
+export const registry: Registry = [...fragments, ...examples]
