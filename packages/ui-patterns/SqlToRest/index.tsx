b95771648b1bd8a65336fd95a75fac6c9e93b767
Greg Richardson <greg.nmr@gmail.com>
2024-05-21 11:23:21 -0600

    feat: update 'create a pr' link

--- packages/ui-patterns/SqlToRest/index.tsx
+++ packages/ui-patterns/SqlToRest/index.tsx
@@ -326,7 +326,7 @@ export default function SqlToRest({
             <div className="mt-2 text-white flex gap-1 leading-6">
               <GitPullRequest className="inline-block" width={16} />
               <a
-                href="https://github.com/supabase/supabase/issues/new/choose"
+                href="https://github.com/supabase-community/sql-to-rest/issues/new/choose"
                 target="_blank"
                 rel="noopener noreferrer"
               >
