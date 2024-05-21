c9534eabcd152cbdddcd1f2e4eefaeaf92639861
Charis Lam <26616127+charislam@users.noreply.github.com>
2024-05-21 01:46:27 -0400

    refactor: use collapsible for hidden content

--- apps/docs/components/ApiSchemaOption.tsx
+++ apps/docs/components/ApiSchemaOption.tsx
@@ -1,5 +1,11 @@
 import { FC, PropsWithChildren, useState } from 'react'
-import { IconXCircle } from '~/../../packages/ui'
+import {
+  CollapsibleContent_Shadcn_,
+  CollapsibleTrigger_Shadcn_,
+  Collapsible_Shadcn_,
+  IconXCircle,
+  cn,
+} from 'ui'
 import ApiSchema from '~/components/ApiSchema'
 
 interface IOptions {
@@ -15,34 +21,24 @@ type OptionsSubComponents = {
 const ApiSchemaOptions: FC<PropsWithChildren<IOptions>> & OptionsSubComponents = (props) => {
   const [open, setOpen] = useState(false)
   return (
-    <div className="mt-0">
-      <button
-        className={[
-          'px-5',
-          'border-t border-l border-r border-default',
-          'text-left text-sm text-foreground-light',
-          'hover:bg-surface-100 transition-all',
-
-          'flex items-center gap-2',
-          open ? 'w-full py-1.5 rounded-tl-lg rounded-tr-lg' : 'py-1 border-b rounded-full',
-        ].join(' ')}
-        onClick={() => setOpen(!open)}
-      >
-        <div>
-          <div className={[!open ? 'rotate-45' : 'rotate-0'].join(' ')}>
-            <IconXCircle size={14} />
-          </div>
-        </div>
-        {`${!open ? `Open` : `Close`} ${props.name ?? 'object schema'}`}
-      </button>
-      <div
-        className={['transition-all opacity-0', open ? 'opacity-100 h-auto' : 'invisible h-0'].join(
-          ' '
-        )}
-      >
-        {props.children}
-      </div>
-    </div>
+    <Collapsible_Shadcn_ open={open} onOpenChange={setOpen} className="mt-0">
+      <CollapsibleTrigger_Shadcn_ asChild>
+        <button
+          className={cn(
+            'px-5',
+            'border-t border-l border-r border-default',
+            'text-left text-sm text-foreground-light',
+            'hover:bg-surface-100 transition-all',
+            'flex items-center gap-2',
+            open ? 'w-full py-1.5 rounded-tl-lg rounded-tr-lg' : 'py-1 border-b rounded-full'
+          )}
+        >
+          <IconXCircle size={14} className={open ? '' : 'rotate-45'} />
+          {`${!open ? `Open` : `Close`} ${props.name ?? 'object schema'}`}
+        </button>
+      </CollapsibleTrigger_Shadcn_>
+      <CollapsibleContent_Shadcn_>{props.children}</CollapsibleContent_Shadcn_>
+    </Collapsible_Shadcn_>
   )
 }
 
