423a96bc17cb775dff0a74665ae7575f33b53027
Ivan Vasilov <vasilov.ivan@gmail.com>
2024-05-23 17:36:37 +0200

    Add a redirect from / to the base path.

--- apps/design-system/next.config.mjs
+++ apps/design-system/next.config.mjs
@@ -16,6 +16,20 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      ...(process.env.NEXT_PUBLIC_BASE_PATH?.length
+        ? [
+            {
+              source: '/',
+              destination: process.env.NEXT_PUBLIC_BASE_PATH,
+              basePath: false,
+              permanent: false,
+            },
+          ]
+        : []),
+    ]
+  },
 }
 
 export default withContentlayer(nextConfig)
