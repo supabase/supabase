9e32f4c69899d1bd9afb3e82aac4fab82ec5c2bc
Ivan Vasilov <vasilov.ivan@gmail.com>
2024-05-27 13:58:44 +0200

    Change the page to use scaffold containers.

--- apps/studio/pages/project/[ref]/auth/hooks.tsx
+++ apps/studio/pages/project/[ref]/auth/hooks.tsx
@@ -2,6 +2,7 @@ import { PermissionAction } from '@supabase/shared-types/out/constants'
 
 import { HooksListing } from 'components/interfaces/Auth/Hooks/HooksListing'
 import { AuthLayout } from 'components/layouts'
+import { ScaffoldContainer } from 'components/layouts/Scaffold'
 import NoPermission from 'components/ui/NoPermission'
 import { useCheckPermissions, usePermissionsLoaded } from 'hooks'
 import type { NextPageWithLayout } from 'types'
@@ -14,9 +15,9 @@ const Hooks: NextPageWithLayout = () => {
     return <NoPermission isFullPage resourceText="access your project's auth hooks" />
   } else {
     return (
-      <div className="mx-auto max-w-4xl px-5 pt-12 pb-20">
+      <ScaffoldContainer className="pt-12 pb-20">
         <HooksListing />
-      </div>
+      </ScaffoldContainer>
     )
   }
 }
