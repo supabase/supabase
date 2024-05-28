ff06fe265f1f5c30c6c2cab2311f581955e7d5f4
Terry Sutton <saltcod@gmail.com>
2024-05-28 12:13:36 -0230

    Fix value set for storage upload unit (#26803)
    * Fix value set for storage upload unit
    
    * Convert to RHF
    
    * Update select
    
    * Load config properly
    
    * Cleanup

--- packages/ui/src/components/Listbox/Listbox2.tsx
+++ packages/ui/src/components/Listbox/Listbox2.tsx
@@ -12,6 +12,7 @@ import { cn } from '../../lib/utils/cn'
 import { useFormContext } from '../Form/FormContext'
 import { IconCheck } from '../Icon/icons/IconCheck'
 import { SelectContext } from './SelectContext'
+import { Select_Shadcn_ } from 'ui'
 
 export interface Props extends Omit<React.InputHTMLAttributes<HTMLButtonElement>, 'size'> {
   className?: string
