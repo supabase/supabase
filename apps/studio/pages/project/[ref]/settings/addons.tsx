2109d22c466c0bcfd4b0017027bde6cf3c90b18d
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-05-28 13:28:23 +0800

    [Dashboard] init layouts in project settings (#26790)
    * init layouts in project settings
    
    * Update general.tsx
    
    * update gap
    
    * Update Scaffold.tsx
    
    * spacing issues
    
    * add bottom padding
    
    * Update api.tsx

--- apps/studio/pages/project/[ref]/settings/addons.tsx
+++ apps/studio/pages/project/[ref]/settings/addons.tsx
@@ -1,9 +1,25 @@
 import Addons from 'components/interfaces/Settings/Addons/Addons'
 import { SettingsLayout } from 'components/layouts'
+import {
+  ScaffoldContainer,
+  ScaffoldDescription,
+  ScaffoldHeader,
+  ScaffoldTitle,
+} from 'components/layouts/Scaffold'
 import type { NextPageWithLayout } from 'types'
 
 const ProjectAddons: NextPageWithLayout = () => {
-  return <Addons />
+  return (
+    <>
+      <ScaffoldContainer>
+        <ScaffoldHeader>
+          <ScaffoldTitle>Add ons</ScaffoldTitle>
+          <ScaffoldDescription>Level up your project with add-ons</ScaffoldDescription>
+        </ScaffoldHeader>
+      </ScaffoldContainer>
+      <Addons />
+    </>
+  )
 }
 
 ProjectAddons.getLayout = (page) => <SettingsLayout title="Add ons">{page}</SettingsLayout>
