8b9295b84ec387bd85026bac0dc9c2715319b791
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-05-29 16:17:58 +0800

    Update PostgrestConfig.tsx

--- apps/studio/components/interfaces/Settings/API/PostgrestConfig.tsx
+++ apps/studio/components/interfaces/Settings/API/PostgrestConfig.tsx
@@ -140,7 +140,7 @@ const PostgrestConfig = () => {
       <form id={formId} onSubmit={form.handleSubmit(onSubmit)}>
         <FormPanel
           disabled={true}
-          header={<p>API Settings</p>}
+          header={<p>Data API Settings</p>}
           footer={
             <div className="flex px-8 py-4">
               <FormActions
