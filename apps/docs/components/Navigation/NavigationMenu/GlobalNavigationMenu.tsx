52604116b181394b1593700d491d2d3f4051d060
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2024-05-30 12:52:01 +0200

    Menubar docs component exploration

--- apps/docs/components/Navigation/NavigationMenu/GlobalNavigationMenu.tsx
+++ apps/docs/components/Navigation/NavigationMenu/GlobalNavigationMenu.tsx
@@ -1,75 +1,70 @@
 import Link from 'next/link'
 import React from 'react'
+import { ChevronDown } from 'lucide-react'
 import {
   Badge,
   cn,
-  IconChevronRight,
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarTrigger,
 } from 'ui'
-import { GLOBAL_MENU_ITEMS } from './NavigationMenu.constants'
-import HomeMenuIconPicker from './HomeMenuIconPicker'
 
-const GlobalNavigationMenu = () => {
+import HomeMenuIconPicker from './HomeMenuIconPicker'
+import { GLOBAL_MENU_ITEMS } from './NavigationMenu.constants'
+
+const GlobalMenubar = () => {
   return (
-    <div className="h-[30px] px-10 flex relative gap-2 justify-start items-end w-full">
-      <NavigationMenu
-        delayDuration={0}
-        skipDelayDuration={0}
-        className="hidden sm:space-x-4 lg:flex justify-start"
-        hasViewport={false}
-        viewportClassName="rounded-xl bg-background justify-start z-50"
-        orientation="vertical"
-      >
-        <NavigationMenuList>
-          {GLOBAL_MENU_ITEMS.map((section, sectionIndex) =>
-            section[0].menuItems ? (
-              <NavigationMenuItem className="text-sm relative" key={section[0].label}>
-                <NavigationMenuTrigger className="bg-transparent font-normal text-foreground-lighter hover:text-brand-link data-[state=open]:!text-brand-link data-[radix-collection-item]:focus-visible:ring-2 data-[radix-collection-item]:focus-visible:ring-foreground-lighter data-[radix-collection-item]:focus-visible:text-foreground p-2 h-auto">
-                  {section[0].label}
-                </NavigationMenuTrigger>
-                <NavigationMenuContent className="!top-[calc(100%+4px)] z-50 min-w-[14rem] overflow-hidden rounded-md border border-overlay bg-overlay p-1 text-foreground-light shadow-md !duration-0 w-64">
-                  {section[0].menuItems?.map((menuItem) =>
-                    menuItem.map((item) =>
+    <div className="h-[30px] px-5 lg:px-10 flex relative top-0 gap-2 justify-start overflow-x-scroll no-scrollbar lg:items-end w-full">
+      <Menubar className="border-none">
+        {GLOBAL_MENU_ITEMS.map((section, sectionIndex) =>
+          section[0].menuItems ? (
+            <MenubarMenu>
+              <MenubarTrigger className="inline-flex items-center justify-center rounded-md bg-transparent font-normal text-foreground-lighter hover:text-brand-link data-[state=open]:!text-brand-link transition-colors focus:outline-none focus:bg-accent focus:text-accent-foreground disabled:opacity-50 disabled:pointer-events-none bg-background hover:bg-accent hover:text-accent-foreground data-[state=open]:bg-accent/50 data-[active]:bg-accent/50 h-10 group w-max group">
+                {section[0].label}
+                <ChevronDown
+                  className="relative top-[1px] ml-1 h-3 w-3 transition duration-200 group-data-[state=open]:rotate-180"
+                  aria-hidden="true"
+                />
+              </MenubarTrigger>
+              <MenubarContent className="!top-[calc(100%+4px)] z-50 min-w-[14rem] overflow-hidden rounded-md border border-overlay bg-overlay p-1 text-foreground-light shadow-md !duration-0 w-64">
+                {section[0].menuItems?.map((menuItem, menuItemIndex) => (
+                  <>
+                    {menuItemIndex !== 0 && <MenubarSeparator />}
+                    {menuItem.map((item) =>
                       !item.href ? (
                         <div className="font-mono tracking-wider flex items-center text-foreground-muted text-xs uppercase rounded-md p-2 leading-none">
-                          {item.label}
+                          {item.label}{' '}
                         </div>
                       ) : (
-                        <NavigationMenuLink asChild>
+                        <MenubarItem className="text-sm relative" key={section[0].label}>
                           <MenuItem
                             href={item.href}
                             title={item.label}
                             community={item.community}
                             icon={item.icon}
                           />
-                        </NavigationMenuLink>
+                        </MenubarItem>
                       )
-                    )
-                  )}
-                </NavigationMenuContent>
-              </NavigationMenuItem>
-            ) : (
-              <NavigationMenuItem className="text-sm " key={section[0].label}>
-                <NavigationMenuLink asChild>
-                  <MenuItem
-                    href={section[0].href}
-                    title={section[0].label}
-                    className={cn(
-                      'group-hover:bg-transparent text-foreground-lighter focus-visible:text-brand-link',
-                      sectionIndex === 0 && 'pl-0'
                     )}
-                  />
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-            )
-          )}
-        </NavigationMenuList>
-      </NavigationMenu>
+                  </>
+                ))}
+              </MenubarContent>
+            </MenubarMenu>
+          ) : (
+            <MenuItem
+              href={section[0].href}
+              title={section[0].label}
+              className={cn(
+                'group-hover:bg-transparent text-foreground-lighter focus-visible:text-brand-link',
+                sectionIndex === 0 && 'pl-0'
+              )}
+            />
+          )
+        )}
+      </Menubar>
     </div>
   )
 }
@@ -86,7 +81,7 @@ const MenuItem = React.forwardRef<
       href={href}
       ref={ref}
       className={cn(
-        'group/menu-item flex items-center gap-2',
+        'group/menu-item flex items-center gap-2 whitespace-nowrap text-nowrap flex-nowrap',
         'flex items-center text-foreground-light text-sm hover:text-foreground select-none rounded-md p-2 leading-none no-underline outline-none focus-visible:ring-2 focus-visible:ring-foreground-lighter focus-visible:text-foreground',
         className
       )}
@@ -103,4 +98,4 @@ const MenuItem = React.forwardRef<
   )
 })
 
-export default GlobalNavigationMenu
+export default GlobalMenubar
--- apps/docs/components/Navigation/NavigationMenu/GlobalNavigationMenuA.tsx
+++ apps/docs/components/Navigation/NavigationMenu/GlobalNavigationMenuA.tsx
@@ -1,8 +1,8 @@
 import Link from 'next/link'
 import React from 'react'
 import {
+  Badge,
   cn,
-  IconChevronRight,
   NavigationMenu,
   NavigationMenuContent,
   NavigationMenuItem,
@@ -11,45 +11,51 @@ import {
   NavigationMenuTrigger,
 } from 'ui'
 import { GLOBAL_MENU_ITEMS } from './NavigationMenu.constants'
+import HomeMenuIconPicker from './HomeMenuIconPicker'
 
 const GlobalNavigationMenu = () => {
   return (
-    <div className="h-[30px] px-10 flex relative gap-2 justify-start items-end w-full">
+    <div className="h-[30px] px-5 lg:px-10 flex relative top-0 gap-2 justify-start overflow-x-scroll no-scrollbar lg:items-end w-full">
       <NavigationMenu
         delayDuration={0}
-        className="hidden sm:space-x-4 lg:flex justify-start"
-        viewportClassName="rounded-xl bg-background justify-start"
+        skipDelayDuration={0}
+        className="gap-2 sm:gap-4 flex justify-start"
+        hasViewport={false}
+        orientation="vertical"
       >
         <NavigationMenuList>
           {GLOBAL_MENU_ITEMS.map((section, sectionIndex) =>
             section[0].menuItems ? (
-              <NavigationMenuItem className="text-sm" key={section[0].label}>
+              <NavigationMenuItem className="text-sm relative" key={section[0].label}>
                 <NavigationMenuTrigger className="bg-transparent font-normal text-foreground-lighter hover:text-brand-link data-[state=open]:!text-brand-link data-[radix-collection-item]:focus-visible:ring-2 data-[radix-collection-item]:focus-visible:ring-foreground-lighter data-[radix-collection-item]:focus-visible:text-foreground p-2 h-auto">
                   {section[0].label}
                 </NavigationMenuTrigger>
-                <NavigationMenuContent className="rounded-xl">
+                <NavigationMenuContent className="!top-[calc(100%+4px)] z-50 min-w-[14rem] overflow-hidden rounded-md border border-overlay bg-overlay p-1 text-foreground-light shadow-md !duration-0 w-64">
                   {section[0].menuItems?.map((menuItem) =>
-                    menuItem.map((item) => (
-                      <NavigationMenuLink asChild>
-                        {!item.href ? (
-                          <div className="font-mono text-foreground-muted text-xs">
-                            {item.label}
-                          </div>
-                        ) : (
+                    menuItem.map((item) =>
+                      !item.href ? (
+                        <div className="font-mono tracking-wider flex items-center text-foreground-muted text-xs uppercase rounded-md p-2 leading-none">
+                          {item.label}
+                        </div>
+                      ) : (
+                        <NavigationMenuLink asChild>
                           <MenuItem
                             href={item.href}
                             title={item.label}
-                            className="group-hover:bg-transparent text-foreground focus-visible:text-brand-link"
-                            hoverColor="brand"
+                            community={item.community}
+                            icon={item.icon}
                           />
-                        )}
-                      </NavigationMenuLink>
-                    ))
+                        </NavigationMenuLink>
+                      )
+                    )
                   )}
                 </NavigationMenuContent>
               </NavigationMenuItem>
             ) : (
-              <NavigationMenuItem className="text-sm " key={section[0].label}>
+              <NavigationMenuItem
+                className="text-sm whitespace-nowrap text-nowrap flex-nowrap"
+                key={section[0].label}
+              >
                 <NavigationMenuLink asChild>
                   <MenuItem
                     href={section[0].href}
@@ -58,7 +64,6 @@ const GlobalNavigationMenu = () => {
                       'group-hover:bg-transparent text-foreground-lighter focus-visible:text-brand-link',
                       sectionIndex === 0 && 'pl-0'
                     )}
-                    hoverColor="brand"
                   />
                 </NavigationMenuLink>
               </NavigationMenuItem>
@@ -73,86 +78,30 @@ const GlobalNavigationMenu = () => {
 const MenuItem = React.forwardRef<
   React.ElementRef<'a'>,
   React.ComponentPropsWithoutRef<'a'> & {
-    description?: string
     icon?: string
-    hasChevron?: boolean
-    hoverColor?: 'foreground' | 'brand'
+    community?: boolean
   }
->(
-  (
-    {
-      className,
-      title,
-      href = '',
-      description,
-      icon,
-      hasChevron,
-      children,
-      hoverColor = 'foreground',
-      ...props
-    },
-    ref
-  ) => {
-    return (
-      <Link
-        href={href}
-        ref={ref}
-        className={cn(
-          'group/menu-item flex items-center text-foreground-lighter text-sm hover:text-foreground select-none gap-3 rounded-md p-2 leading-none no-underline outline-none focus-visible:ring-2 focus-visible:ring-foreground-lighter focus-visible:text-foreground',
-          description && 'items-center',
-          className
-        )}
-        {...props}
-      >
-        {children ?? (
-          <>
-            {icon && (
-              <div className="shrink-0 bg-surface-200 min-w-10 w-10 h-10 flex items-center justify-center rounded-lg">
-                <svg
-                  className="h-5 w-5 group-hover/menu-item:text-foreground group-focus-visible/menu-item:text-foreground"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.5"
-                    d={icon}
-                    stroke="currentColor"
-                  />
-                </svg>
-              </div>
-            )}
-            <div className="flex flex-col justify-center">
-              <div className="flex items-center gap-1">
-                <p
-                  className={cn(
-                    'leading-snug',
-                    hoverColor === 'brand' && 'group-hover/menu-item:text-brand-link'
-                  )}
-                >
-                  {title}
-                </p>
-                {hasChevron && (
-                  <IconChevronRight
-                    strokeWidth={2}
-                    className="w-3 text-foreground transition-all will-change-transform -translate-x-1 opacity-0 group-hover/menu-item:translate-x-0 group-hover/menu-item:opacity-100"
-                  />
-                )}
-              </div>
-              {description && (
-                <p className="line-clamp-1 -mb-1 leading-relaxed text-foreground-lighter group-hover/menu-item:text-foreground-light group-focus-visible/menu-item:text-foreground-light">
-                  {description}
-                </p>
-              )}
-            </div>
-          </>
-        )}
-      </Link>
-    )
-  }
-)
+>(({ className, title, href = '', icon, community, children, ...props }, ref) => {
+  return (
+    <Link
+      href={href}
+      ref={ref}
+      className={cn(
+        'group/menu-item flex items-center gap-2 whitespace-nowrap text-nowrap flex-nowrap',
+        'flex items-center text-foreground-light text-sm hover:text-foreground select-none rounded-md p-2 leading-none no-underline outline-none focus-visible:ring-2 focus-visible:ring-foreground-lighter focus-visible:text-foreground',
+        className
+      )}
+      {...props}
+    >
+      {children ?? (
+        <>
+          {icon && <HomeMenuIconPicker icon={icon} />}
+          <span className="flex-1">{title}</span>
+          {community && <Badge size="small">Community</Badge>}
+        </>
+      )}
+    </Link>
+  )
+})
 
 export default GlobalNavigationMenu
