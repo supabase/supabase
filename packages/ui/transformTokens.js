52c9b7ef1e6cc351f47aebd30abed388ed2d7fad
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-06-04 13:17:51 +0800

    chore: move to deeper dark

--- packages/ui/transformTokens.js
+++ packages/ui/transformTokens.js
@@ -228,7 +228,8 @@ themeFiles.map(function (filePath, i) {
   let fileName = fileNameCleaner(filePath)
 
   // rename concept-one to dark-new
-  fileName = fileName === 'concept-one' ? 'deep-dark' : fileName
+  fileName = fileName === 'dark' ? 'classic-dark' : fileName
+  fileName = fileName === 'concept-one' ? 'dark' : fileName
 
   let configTailwindFilesByType = []
 
