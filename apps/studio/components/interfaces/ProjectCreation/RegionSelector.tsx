99af0e44833d6e5ce81cdc251640241cdf175d23
Alaister Young <alaister@users.noreply.github.com>
2024-06-04 21:09:43 +0800

    Revert "[Dashboard] New project form now using react-hook-form" (#26995)
    Revert "[Dashboard] New project form now using react-hook-form (#26808)"
    
    This reverts commit 2dc2178dcc48bf1dbe02905f31140621eb97f1e8.

--- apps/studio/components/interfaces/ProjectCreation/RegionSelector.tsx
+++ apps/studio/components/interfaces/ProjectCreation/RegionSelector.tsx
@@ -1,29 +1,24 @@
-import { useDefaultRegionQuery } from 'data/misc/get-default-region-query'
 import { CloudProvider, PROVIDERS } from 'lib/constants'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
-import { ControllerRenderProps, UseFormReturn } from 'react-hook-form'
-import {
-  SelectContent_Shadcn_,
-  SelectGroup_Shadcn_,
-  SelectItem_Shadcn_,
-  SelectTrigger_Shadcn_,
-  SelectValue_Shadcn_,
-  Select_Shadcn_,
-} from 'ui'
-import { FormItemLayout } from 'ui-patterns/form/FormItemLayout/FormItemLayout'
+import { Listbox } from 'ui'
+
+import { useDefaultRegionQuery } from 'data/misc/get-default-region-query'
 import { getAvailableRegions } from './ProjectCreation.utils'
 
 interface RegionSelectorProps {
   cloudProvider: CloudProvider
-  field: ControllerRenderProps<any, 'dbRegion'>
-  form: UseFormReturn<any>
+  selectedRegion: string
+  onSelectRegion: (value: string) => void
 }
 
-export const RegionSelector = ({ cloudProvider, field, form }: RegionSelectorProps) => {
+export const RegionSelector = ({
+  cloudProvider,
+  selectedRegion,
+  onSelectRegion,
+}: RegionSelectorProps) => {
   const router = useRouter()
   const availableRegions = getAvailableRegions(PROVIDERS[cloudProvider].id)
-
   const {
     data: region,
     isSuccess,
@@ -35,49 +30,45 @@ export const RegionSelector = ({ cloudProvider, field, form }: RegionSelectorPro
 
   useEffect(() => {
     // only pick a region if one hasn't already been selected
-    if (isError && !field.value) {
+    if (isSuccess && region && !selectedRegion) {
+      onSelectRegion(region)
+    } else if (isError && !selectedRegion) {
       // if an error happened, and the user haven't selected a region, just select the default one for him
-      form.setValue('dbRegion', PROVIDERS[cloudProvider].default_region)
+      onSelectRegion(PROVIDERS[cloudProvider].default_region)
     }
-  }, [cloudProvider, isError, isSuccess, field.value, form])
+  }, [cloudProvider, isError, isSuccess, region, selectedRegion])
 
   return (
-    <FormItemLayout
+    <Listbox
       layout="horizontal"
       label="Region"
-      description="Select the region closest to your users for the best performance."
+      type="select"
+      value={selectedRegion}
+      onChange={(value) => onSelectRegion(value)}
+      descriptionText="Select the region closest to your users for the best performance."
     >
-      <Select_Shadcn_
-        value={field.value}
-        onValueChange={(value) => {
-          if (isSuccess && region && !field.value) {
-            field.onChange(value)
-          }
-        }}
-      >
-        <SelectTrigger_Shadcn_>
-          <SelectValue_Shadcn_ placeholder="Select a region for your project.." />
-        </SelectTrigger_Shadcn_>
-        <SelectContent_Shadcn_>
-          <SelectGroup_Shadcn_>
-            {Object.keys(availableRegions).map((option: string, i) => {
-              const label = Object.values(availableRegions)[i] as string
-              return (
-                <SelectItem_Shadcn_ key={option} value={label}>
-                  <div className="flex items-center gap-3">
-                    <img
-                      alt="region icon"
-                      className="w-5 rounded-sm"
-                      src={`${router.basePath}/img/regions/${Object.keys(availableRegions)[i]}.svg`}
-                    />
-                    <span className="text-foreground">{label}</span>
-                  </div>
-                </SelectItem_Shadcn_>
-              )
-            })}
-          </SelectGroup_Shadcn_>
-        </SelectContent_Shadcn_>
-      </Select_Shadcn_>
-    </FormItemLayout>
+      <Listbox.Option disabled key="empty" label="---" value="">
+        <span className="text-foreground">Select a region for your project</span>
+      </Listbox.Option>
+      {Object.keys(availableRegions).map((option: string, i) => {
+        const label = Object.values(availableRegions)[i] as string
+        return (
+          <Listbox.Option
+            key={option}
+            label={label}
+            value={label}
+            addOnBefore={() => (
+              <img
+                alt="region icon"
+                className="w-5 rounded-sm"
+                src={`${router.basePath}/img/regions/${Object.keys(availableRegions)[i]}.svg`}
+              />
+            )}
+          >
+            <span className="text-foreground">{label}</span>
+          </Listbox.Option>
+        )
+      })}
+    </Listbox>
   )
 }
