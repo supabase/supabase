f7cece5b9a987b9c5cedec636e99d255d58651ce
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2024-06-06 15:49:02 +0200

    set current active docs nav section

--- apps/docs/components/Navigation/NavigationMenu/GlobalNavigationMenu.tsx
+++ apps/docs/components/Navigation/NavigationMenu/GlobalNavigationMenu.tsx
@@ -1,6 +1,6 @@
-import React, { Fragment } from 'react'
-import Link from 'next/link'
+import React, { FC, Fragment, PropsWithChildren, useEffect, useState } from 'react'
 import { useBreakpoint } from 'common'
+import Link from 'next/link'
 import {
   Badge,
   cn,
@@ -19,23 +19,42 @@ import {
 
 import HomeMenuIconPicker from './HomeMenuIconPicker'
 import { GLOBAL_MENU_ITEMS } from './NavigationMenu.constants'
+import { usePathname } from 'next/navigation'
 
-const GlobalNavigationMenu = () => {
-  const isTablet = useBreakpoint('md')
+/**
+ * Get current Top Nav active label based on current pathname
+ */
+const useActiveMenuParent = (pathname: string) => {
+  const [activeParent, setActiveParent] = useState('')
 
-  const ResponsiveScrollArea = ({ children }) =>
-    !isTablet ? (
-      <>{children}</>
-    ) : (
-      <>
-        <ScrollArea className="relative">
-          <div className="absolute h-full right-0 w-7 z-50 bg-gradient-to-r from-transparent to-background" />
-          {children}
-          <ScrollBar orientation="horizontal" className="hidden" />
-          <NavigationMenuViewport containerProps={{ className: 'bg-transparent' }} />
-        </ScrollArea>
-      </>
-    )
+  useEffect(() => {
+    GLOBAL_MENU_ITEMS.map((section) => {
+      // check if homepage
+      if (pathname === '/') {
+        return setActiveParent('Home')
+      }
+      // check if first level menu items match beginning of url
+      if (section[0].href?.startsWith(pathname)) {
+        return setActiveParent(section[0].label)
+      }
+      // check if second level menu items match beginning of url
+      if (section[0].menuItems) {
+        section[0].menuItems.map((menuItemGroup) =>
+          menuItemGroup.map(
+            (menuItem) => menuItem.href?.startsWith(pathname) && setActiveParent(section[0].label)
+          )
+        )
+      }
+    })
+  }, [pathname])
+
+  return activeParent
+}
+
+const GlobalNavigationMenu: FC = () => {
+  const pathname = usePathname()
+  const isLowerThanMd = useBreakpoint('md')
+  const activeParent = useActiveMenuParent(pathname)
 
   return (
     <div className="h-[30px] flex relative gap-2 justify-start items-end w-full">
@@ -43,10 +62,10 @@ const GlobalNavigationMenu = () => {
         delayDuration={0}
         skipDelayDuration={0}
         className="w-full space-x-4 flex justify-start"
-        renderViewport={isTablet}
+        renderViewport={isLowerThanMd}
         orientation="horizontal"
       >
-        <ResponsiveScrollArea>
+        <ResponsiveScrollArea isLowerThanMd={isLowerThanMd}>
           <NavigationMenuList className="pl-5 pr-3 lg:px-10">
             {GLOBAL_MENU_ITEMS.map((section, sectionIndex) =>
               section[0].menuItems ? (
@@ -57,7 +76,9 @@ const GlobalNavigationMenu = () => {
                   <NavigationMenuTrigger
                     className={cn(
                       navigationMenuTriggerStyle(),
-                      'bg-transparent font-normal text-foreground-lighter hover:text-foreground data-[state=open]:!text-foreground data-[radix-collection-item]:focus-visible:ring-2 data-[radix-collection-item]:focus-visible:ring-foreground-lighter data-[radix-collection-item]:focus-visible:text-foreground p-2 h-auto'
+                      'bg-transparent font-normal rounded-none text-foreground-lighter hover:text-foreground data-[state=open]:!text-foreground data-[radix-collection-item]:focus-visible:ring-2 data-[radix-collection-item]:focus-visible:ring-foreground-lighter data-[radix-collection-item]:focus-visible:text-foreground p-2 h-[37px]',
+                      activeParent === section[0].label &&
+                        'text-foreground border-b border-foreground'
                     )}
                   >
                     {section[0].label === 'Home' ? (
@@ -93,14 +114,15 @@ const GlobalNavigationMenu = () => {
               ) : (
                 <NavigationMenuItem
                   key={`desktop-docs-menu-section-${section[0].label}`}
-                  className="text-sm"
+                  className="text-sm relative"
                 >
                   <NavigationMenuLink asChild>
                     <Link
                       href={section[0].href}
                       className={cn(
-                        'flex-1 whitespace-nowrap flex items-center text-foreground-lighter text-sm hover:text-foreground select-none rounded-md p-2 leading-none no-underline outline-none focus-visible:ring-2 focus-visible:ring-foreground-lighter focus-visible:text-foreground',
-                        sectionIndex === 0 && '-ml-2'
+                        'relative flex-1 whitespace-nowrap border-b border-transparent flex items-center text-foreground-lighter text-sm hover:text-foreground select-none rounded-none p-2 leading-none no-underline outline-none focus-visible:ring-2 focus-visible:ring-foreground-lighter focus-visible:text-foreground h-[37px]',
+                        sectionIndex === 0 && '-ml-2',
+                        activeParent === section[0].label && 'text-foreground border-foreground'
                       )}
                     >
                       {section[0].label === 'Home' ? (
@@ -119,6 +141,23 @@ const GlobalNavigationMenu = () => {
     </div>
   )
 }
+
+const ResponsiveScrollArea: FC<PropsWithChildren<{ isLowerThanMd: boolean }>> = ({
+  isLowerThanMd,
+  children,
+}) =>
+  !isLowerThanMd ? (
+    <>{children}</>
+  ) : (
+    <>
+      <ScrollArea className="relative">
+        <div className="absolute h-full right-0 w-7 z-50 bg-gradient-to-r from-transparent to-background" />
+        {children}
+        <ScrollBar orientation="horizontal" className="hidden" />
+        <NavigationMenuViewport containerProps={{ className: 'bg-transparent' }} />
+      </ScrollArea>
+    </>
+  )
 
 export const MenuItem = React.forwardRef<
   React.ElementRef<'a'>,
