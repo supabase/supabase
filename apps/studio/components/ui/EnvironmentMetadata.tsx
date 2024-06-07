668a46b13f7880e32646abbda6df74caa4233718
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-06-08 00:26:27 +0800

    Update EnvironmentMetadata.tsx

--- apps/studio/components/ui/EnvironmentMetadata.tsx
+++ apps/studio/components/ui/EnvironmentMetadata.tsx
@@ -9,10 +9,12 @@ function EnvironmentMetadata() {
   const repoOwner = process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_OWNER
   const repoSlug = process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG
 
+  const isPlatform = process.env.NEXT_PUBLIC_IS_PLATFORM
+
   const repoUrl = `https://github.com/${repoOwner}/${repoSlug}`
 
   useEffect(() => {
-    if (environment === 'staging' || environment === 'local') {
+    if ((environment === 'staging' || environment === 'local') && isPlatform) {
       console.log(
         `\n⚡️ ⚡️ ⚡️\n\n%cSupabase Dashboard\n\n%cEnvironment: %c${environment || 'undefined'}\n%cCommit SHA: %c${commitSha ? `${repoUrl}/commit/${commitSha}` : 'undefined'}\n%cCommit Message: %c${commitMessage || 'undefined'}\n%cPull Request ID: %c${pullRequestId ? `${repoUrl}/pull/${pullRequestId}` : 'undefined'}\n%cBranch: %c${branch ? `${repoUrl}/tree/${branch}` : 'undefined'}\n%cRepository: %c${repoOwner || 'undefined'}/${repoSlug || 'undefined'}\n\n`,
         'color: green; font-size: 16px; font-weight: bold;',
@@ -32,7 +34,7 @@ function EnvironmentMetadata() {
     }
   }, [environment, commitSha, commitMessage, pullRequestId, branch, repoOwner, repoSlug])
 
-  if (environment === 'staging' || environment === 'local') {
+  if ((environment === 'staging' || environment === 'local') && isPlatform) {
     return (
       <div className="bg-foreground text-background px-4 py-1 font-mono text-xs flex items-center gap-4 w-full top-0 z-50">
         <span>
