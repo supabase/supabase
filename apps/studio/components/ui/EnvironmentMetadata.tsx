54dcb06c971a90de4a0a8373d3330d4c14803836
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-06-08 00:24:08 +0800

    Update EnvironmentMetadata.tsx

--- apps/studio/components/ui/EnvironmentMetadata.tsx
+++ apps/studio/components/ui/EnvironmentMetadata.tsx
@@ -2,16 +2,17 @@ import { useEffect } from 'react'
 
 function EnvironmentMetadata() {
   const environment = process.env.NEXT_PUBLIC_ENVIRONMENT
+  const commitSha = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA
+  const commitMessage = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_MESSAGE
+  const pullRequestId = process.env.NEXT_PUBLIC_VERCEL_GIT_PULL_REQUEST_ID
+  const branch = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF
+  const repoOwner = process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_OWNER
+  const repoSlug = process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG
 
-  if (environment === 'staging' || environment === 'local') {
-    const commitSha = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA
-    const commitMessage = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_MESSAGE
-    const pullRequestId = process.env.NEXT_PUBLIC_VERCEL_GIT_PULL_REQUEST_ID
-    const branch = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF
-    const repoOwner = process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_OWNER
-    const repoSlug = process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG
+  const repoUrl = `https://github.com/${repoOwner}/${repoSlug}`
 
-    useEffect(() => {
+  useEffect(() => {
+    if (environment === 'staging' || environment === 'local') {
       console.log(
         `\n⚡️ ⚡️ ⚡️\n\n%cSupabase Dashboard\n\n%cEnvironment: %c${environment || 'undefined'}\n%cCommit SHA: %c${commitSha ? `${repoUrl}/commit/${commitSha}` : 'undefined'}\n%cCommit Message: %c${commitMessage || 'undefined'}\n%cPull Request ID: %c${pullRequestId ? `${repoUrl}/pull/${pullRequestId}` : 'undefined'}\n%cBranch: %c${branch ? `${repoUrl}/tree/${branch}` : 'undefined'}\n%cRepository: %c${repoOwner || 'undefined'}/${repoSlug || 'undefined'}\n\n`,
         'color: green; font-size: 16px; font-weight: bold;',
@@ -28,10 +29,10 @@ function EnvironmentMetadata() {
         'color: inherit;',
         'color: green;'
       )
-    }, [environment, commitSha, commitMessage, pullRequestId, branch, repoOwner, repoSlug])
+    }
+  }, [environment, commitSha, commitMessage, pullRequestId, branch, repoOwner, repoSlug])
 
-    const repoUrl = `https://github.com/${repoOwner}/${repoSlug}`
-
+  if (environment === 'staging' || environment === 'local') {
     return (
       <div className="bg-foreground text-background px-4 py-1 font-mono text-xs flex items-center gap-4 w-full top-0 z-50">
         <span>
