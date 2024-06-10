4f062856be8890b459972882d3d804357588a3fa
Charis Lam <26616127+charislam@users.noreply.github.com>
2024-06-10 14:12:33 -0400

    fix: fix packages

--- packages/ui-patterns/Cmdk/DocsSearch.tsx
+++ packages/ui-patterns/Cmdk/DocsSearch.tsx
@@ -1,3 +1,4 @@
+import { useSupabaseClient } from '@supabase/auth-helpers-react'
 import {
   type DocsSearchResult as Page,
   type DocsSearchResultSection as PageSection,
@@ -29,12 +30,13 @@ const questions = [
 ]
 
 const DocsSearch = () => {
+  const supabaseClient = useSupabaseClient()
   const {
     searchState: state,
     handleDocsSearch: handleSearch,
     handleDocsSearchDebounced: debouncedSearch,
     resetSearch,
-  } = useDocsSearch()
+  } = useDocsSearch(supabaseClient)
   const { search, setSearch, inputRef, site, setIsOpen } = useCommandMenu()
   const initialLoad = useRef(true)
   const router = useRouter()
