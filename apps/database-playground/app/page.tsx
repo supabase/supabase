3dc54d46d0b37db634d913bf1ad304a886bc0004
Greg Richardson <greg.nmr@gmail.com>
2024-06-10 22:21:54 -0600

    feat(db-playground): animations and transitions

--- apps/database-playground/app/page.tsx
+++ apps/database-playground/app/page.tsx
@@ -5,6 +5,7 @@ import { Editor } from '@monaco-editor/react'
 import { useChat } from 'ai/react'
 import Chart from 'chart.js/auto'
 import { codeBlock } from 'common-tags'
+import { LazyMotion, m } from 'framer-motion'
 import { ArrowUp } from 'lucide-react'
 import { useCallback, useEffect, useRef, useState } from 'react'
 import { Chart as ChartWrapper } from 'react-chartjs-2'
@@ -17,6 +18,8 @@ import { format } from 'sql-formatter'
 import { AiIconAnimation, markdownComponents } from 'ui'
 import { Button } from 'ui/src/components/shadcn/ui/button'
 import { Report } from './api/chat/route'
+
+const loadFramerFeatures = () => import('./framer-features').then((res) => res.default)
 
 // React's double-rendering in dev mode causes pglite errors
 // Temp: storing single instance in module scope
@@ -81,7 +84,7 @@ export default function Page() {
   const [isEditorVisible, setIsEditorVisible] = useState(false)
   const { reports } = useReportSuggestions(db)
 
-  const { messages, input, handleInputChange, handleSubmit, append } = useChat({
+  const { messages, input, handleInputChange, handleSubmit, append, isLoading } = useChat({
     api: 'api/chat',
     maxToolRoundtrips: 5,
     async onToolCall({ toolCall }) {
@@ -121,7 +124,9 @@ export default function Page() {
           try {
             const chart = new Chart(canvas, config)
             chart.destroy()
-            return "Chart has been generated and displayed to the user. Acknowledge the user's request."
+            return codeBlock`
+              The chart has been generated and displayed to the user above. Acknowledge the user's request.
+            `
           } catch (err) {
             if (err instanceof Error) {
               return { error: err.message }
@@ -161,181 +166,242 @@ export default function Page() {
   }, [])
 
   return (
-    <div className="w-full h-full flex p-6">
-      {isEditorVisible && (
-        <div className="h-full w-[50rem] py-4 rounded-md bg-[#1e1e1e]">
-          <Editor
-            language="pgsql"
-            value={sql}
-            theme="vs-dark"
-            options={{
-              tabSize: 2,
-              minimap: {
-                enabled: false,
-              },
-              fontSize: 13,
-            }}
-            onMount={async (editor, monaco) => {
-              // Register pgsql formatter
-              monaco.languages.registerDocumentFormattingEditProvider('pgsql', {
-                async provideDocumentFormattingEdits(model) {
-                  const currentCode = editor.getValue()
-                  const formattedCode = format(currentCode, {
-                    language: 'postgresql',
-                    keywordCase: 'lower',
-                  })
-                  return [
-                    {
-                      range: model.getFullModelRange(),
-                      text: formattedCode,
-                    },
-                  ]
+    <LazyMotion features={loadFramerFeatures}>
+      <div className="w-full h-full flex p-6">
+        {isEditorVisible && (
+          <div className="h-full w-[50rem] py-4 rounded-md bg-[#1e1e1e]">
+            <Editor
+              language="pgsql"
+              value={sql}
+              theme="vs-dark"
+              options={{
+                tabSize: 2,
+                minimap: {
+                  enabled: false,
                 },
-              })
+                fontSize: 13,
+              }}
+              onMount={async (editor, monaco) => {
+                // Register pgsql formatter
+                monaco.languages.registerDocumentFormattingEditProvider('pgsql', {
+                  async provideDocumentFormattingEdits(model) {
+                    const currentCode = editor.getValue()
+                    const formattedCode = format(currentCode, {
+                      language: 'postgresql',
+                      keywordCase: 'lower',
+                    })
+                    return [
+                      {
+                        range: model.getFullModelRange(),
+                        text: formattedCode,
+                      },
+                    ]
+                  },
+                })
 
-              // Format on cmd+s
-              editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyS, async () => {
+                // Format on cmd+s
+                editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyS, async () => {
+                  await editor.getAction('editor.action.formatDocument').run()
+                })
+
+                // Run format on the initial value
                 await editor.getAction('editor.action.formatDocument').run()
-              })
+              }}
+            />
+          </div>
+        )}
+        <div className="flex-1 h-full flex flex-col items-stretch">
+          <div
+            className="flex-1 flex flex-col items-center overflow-y-auto"
+            ref={(element) => {
+              if (element) {
+                scrollRef.current = element
 
-              // Run format on the initial value
-              await editor.getAction('editor.action.formatDocument').run()
-            }}
-          />
-        </div>
-      )}
-      <div className="flex-1 h-full flex flex-col items-stretch">
-        <div
-          className="flex-1 flex flex-col items-center overflow-y-auto"
-          ref={(element) => {
-            if (element) {
-              scrollRef.current = element
+                const resizeObserver = new ResizeObserver(() => {
+                  if (element.scrollHeight !== prevScrollHeightRef.current) {
+                    prevScrollHeightRef.current = element.scrollHeight
+                    console.log('resize scroll')
+                    element.scrollTo({
+                      top: element.scrollHeight - element.clientHeight,
+                      behavior: 'smooth',
+                    })
+                  }
+                })
 
-              const resizeObserver = new ResizeObserver(() => {
-                if (element.scrollHeight !== prevScrollHeightRef.current) {
-                  prevScrollHeightRef.current = element.scrollHeight
-                  console.log('resize scroll')
-                  element.scrollTo({
-                    top: element.scrollHeight - element.clientHeight,
-                    behavior: 'smooth',
-                  })
+                for (const child of Array.from(element.children)) {
+                  resizeObserver.observe(child)
                 }
-              })
-
-              for (const child of Array.from(element.children)) {
-                resizeObserver.observe(child)
               }
-            }
-          }}
-        >
-          {messages.length > 0 ? (
-            <div className="flex flex-col gap-4 w-full max-w-4xl p-10">
-              {messages.map((message) => {
-                switch (message.role) {
-                  case 'user':
-                    return (
-                      <div
-                        key={message.id}
-                        className="self-end px-5 py-2.5 text-base rounded-full bg-neutral-100"
-                      >
-                        {message.content}
-                      </div>
-                    )
-                  case 'assistant':
-                    return (
-                      <div
-                        key={message.id}
-                        className="self-stretch flex flex-col items-stretch gap-6"
-                      >
-                        <ReactMarkdown
-                          remarkPlugins={[remarkGfm, [remarkMath, { singleDollarTextMath: false }]]}
-                          rehypePlugins={[[rehypeKatex, { output: 'html' }]]}
-                          components={markdownComponents}
-                          className="prose [&_.katex-display>.katex]:text-left"
-                        >
-                          {message.content}
-                        </ReactMarkdown>
-                        {message.toolInvocations?.map((toolInvocation) => {
-                          switch (toolInvocation.toolName) {
-                            case 'generateChart': {
-                              const { type, data, options } = toolInvocation.args.config
-                              return (
-                                <ErrorBoundary
-                                  key={toolInvocation.toolCallId}
-                                  fallbackRender={() => (
-                                    <div className="bg-warning-300">Error loading chart</div>
-                                  )}
-                                >
-                                  <ChartWrapper type={type} data={data} options={options} />
-                                </ErrorBoundary>
-                              )
-                            }
-                          }
-                        })}
-                      </div>
-                    )
-                }
-              })}
-            </div>
-          ) : (
-            <div className="flex-1 w-full max-w-4xl flex flex-col gap-10 justify-center items-center">
-              <h3 className="text-2xl font-light">What would you like to do?</h3>
-              <div className="flex flex-row gap-6 flex-wrap justify-center items-start">
-                {reports ? (
-                  reports.map((report) => (
-                    <div
-                      key={report.name}
-                      className="w-64 h-32 flex flex-col overflow-ellipsis shadow-lg shadow-neutral-50 rounded-md cursor-pointer"
-                      onMouseDown={() => append({ role: 'user', content: report.description })}
-                    >
-                      <div className="p-4 bg-neutral-200 text-sm rounded-t-md text-neutral-600 font-bold text-center">
-                        {report.name}
-                      </div>
-                      <div className="flex-1 p-4 flex flex-col justify-center border border-neutral-200 text-neutral-500 text-xs font-normal italic rounded-b-md text-center overflow-hidden">
-                        {report.description}
-                      </div>
-                    </div>
-                  ))
-                ) : (
-                  <div className="flex flex-col gap-6 justify-center items-center">
-                    <h3 className="text-lg italic font-light text-neutral-500">
-                      Brainstorming ideas
-                    </h3>
-                    <div className="scale-150">
-                      <AiIconAnimation loading />
-                    </div>
-                  </div>
-                )}
-              </div>
-            </div>
-          )}
-        </div>
-        <div className="flex flex-col items-center gap-2 pb-2">
-          <form
-            className="flex items-center py-2 px-3 rounded-full bg-neutral-100 w-full max-w-4xl"
-            onSubmit={(e) => {
-              handleSubmit(e)
-              scrollToBottom()
             }}
           >
-            <input
-              className="flex-grow border-none focus-visible:ring-0 text-base bg-inherit placeholder:text-neutral-400"
-              name="prompt"
-              value={input}
-              onChange={handleInputChange}
-              placeholder="Message Supabase AI"
-              autoFocus
-              id="input"
-            />
-            <Button className="rounded-full w-8 h-8 p-1.5 text-white bg-neutral-800" type="submit">
-              <ArrowUp />
-            </Button>
-          </form>
-          <div className="text-xs text-neutral-500">
-            AI can make mistakes. Check important information.
+            {messages.length > 0 ? (
+              <div className="flex flex-col gap-4 w-full max-w-4xl p-10">
+                {messages.map((message) => {
+                  switch (message.role) {
+                    case 'user':
+                      return (
+                        <m.div
+                          key={message.id}
+                          variants={{
+                            hidden: {
+                              opacity: 0,
+                              y: 10,
+                            },
+                            show: {
+                              opacity: 1,
+                              y: 0,
+                            },
+                          }}
+                          initial="hidden"
+                          animate="show"
+                          className="self-end px-5 py-2.5 text-base rounded-full bg-neutral-100"
+                        >
+                          {message.content}
+                        </m.div>
+                      )
+                    case 'assistant':
+                      return (
+                        <div
+                          key={message.id}
+                          className="self-stretch flex flex-col items-stretch gap-6"
+                        >
+                          {message.content && (
+                            <ReactMarkdown
+                              remarkPlugins={[
+                                remarkGfm,
+                                [remarkMath, { singleDollarTextMath: false }],
+                              ]}
+                              rehypePlugins={[[rehypeKatex, { output: 'html' }]]}
+                              components={{ ...markdownComponents, img: () => null }}
+                              className="prose [&_.katex-display>.katex]:text-left"
+                            >
+                              {message.content}
+                            </ReactMarkdown>
+                          )}
+                          {message.toolInvocations?.map((toolInvocation) => {
+                            switch (toolInvocation.toolName) {
+                              case 'generateChart': {
+                                const { type, data, options } = toolInvocation.args.config
+                                return (
+                                  <ErrorBoundary
+                                    key={toolInvocation.toolCallId}
+                                    fallbackRender={() => (
+                                      <div className="bg-warning-300">Error loading chart</div>
+                                    )}
+                                  >
+                                    <m.div
+                                      className="relative w-full"
+                                      variants={{
+                                        hidden: {
+                                          opacity: 0,
+                                        },
+                                        show: {
+                                          opacity: 1,
+                                        },
+                                      }}
+                                      initial="hidden"
+                                      animate="show"
+                                    >
+                                      <ChartWrapper type={type} data={data} options={options} />
+                                    </m.div>
+                                  </ErrorBoundary>
+                                )
+                              }
+                            }
+                          })}
+                        </div>
+                      )
+                  }
+                })}
+                {isLoading && (
+                  <m.div layout>
+                    <AiIconAnimation loading />
+                  </m.div>
+                )}
+              </div>
+            ) : (
+              <div className="flex-1 w-full max-w-4xl flex flex-col gap-10 justify-center items-center">
+                <m.h3 layout className="text-2xl font-light">
+                  What would you like to do?
+                </m.h3>
+                <div>
+                  {reports ? (
+                    <m.div
+                      className="flex flex-row gap-6 flex-wrap justify-center items-start"
+                      variants={{
+                        show: {
+                          transition: {
+                            staggerChildren: 0.05,
+                          },
+                        },
+                      }}
+                      initial="hidden"
+                      animate="show"
+                    >
+                      {reports.map((report) => (
+                        <m.div
+                          key={report.name}
+                          layoutId={`report-suggestion-${report.name}`}
+                          className="w-64 h-32 flex flex-col overflow-ellipsis shadow-lg shadow-neutral-50 rounded-md cursor-pointer"
+                          onMouseDown={() => append({ role: 'user', content: report.description })}
+                          variants={{
+                            hidden: { scale: 0 },
+                            show: { scale: 1 },
+                          }}
+                        >
+                          <div className="p-4 bg-neutral-200 text-sm rounded-t-md text-neutral-600 font-bold text-center">
+                            {report.name}
+                          </div>
+                          <div className="flex-1 p-4 flex flex-col justify-center border border-neutral-200 text-neutral-500 text-xs font-normal italic rounded-b-md text-center overflow-hidden">
+                            {report.description}
+                          </div>
+                        </m.div>
+                      ))}
+                    </m.div>
+                  ) : (
+                    <div className="flex flex-col gap-6 justify-center items-center">
+                      <h3 className="text-lg italic font-light text-neutral-500">
+                        Brainstorming some ideas
+                      </h3>
+                      <div className="scale-150">
+                        <AiIconAnimation loading />
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </div>
+            )}
+          </div>
+          <div className="flex flex-col items-center gap-2 pb-2">
+            <form
+              className="flex items-center py-2 px-3 rounded-full bg-neutral-100 w-full max-w-4xl"
+              onSubmit={(e) => {
+                handleSubmit(e)
+                scrollToBottom()
+              }}
+            >
+              <input
+                className="flex-grow border-none focus-visible:ring-0 text-base bg-inherit placeholder:text-neutral-400"
+                name="prompt"
+                value={input}
+                onChange={handleInputChange}
+                placeholder="Message Supabase AI"
+                autoFocus
+                id="input"
+              />
+              <Button
+                className="rounded-full w-8 h-8 p-1.5 text-white bg-neutral-800"
+                type="submit"
+              >
+                <ArrowUp />
+              </Button>
+            </form>
+            <div className="text-xs text-neutral-500">
+              AI can make mistakes. Check important information.
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </LazyMotion>
   )
 }
