227c4eb98a9898303da80e41a0da7e1e777163cb
Filipe Cabaço <filipecabaco@gmail.com>
2024-06-17 15:16:47 +0100

    fix: Change example to use the new Authz format
    Changes the example to use the new Authz format in the example

--- examples/realtime/nextjs-authorization-demo/app/protected/page.tsx
+++ examples/realtime/nextjs-authorization-demo/app/protected/page.tsx
@@ -1,34 +1,76 @@
 'use client'
 import CreateRoomModal from '@/components/create-room-modal'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { RealtimeChannel } from '@supabase/realtime-js'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, use } from 'react'
 
 export default function Chat() {
   const [userId, setUserId] = useState<string | null>(null)
   const [loading, setLoading] = useState<boolean>(true)
   const [rooms, setRooms] = useState<any>([])
+  const [users, setUsers] = useState<any>([])
   const [selectedRoom, setSelectedRoom] = useState<string | undefined>()
-  const [channel, setChannel] = useState<RealtimeChannel | null>(null)
+  const [channel, setChannel] = useState<any>(null)
   const [showModal, setShowModal] = useState<boolean>(false)
   const [error, setError] = useState<string | null>(null)
 
   const supabase = createClientComponentClient()
 
   const getChannels = async () => {
-    await supabase.auth.getUser()
-    const token = (await supabase.auth.getSession()).data.session?.access_token
-    if (!token) return
+    const channels = await supabase.from('rooms').select('topic')
+    setRooms(channels.data?.map(({ topic }) => topic) || [])
+  }
 
-    supabase.realtime.setAuth(token)
-    const channels = await supabase.realtime.listChannels()
-    setRooms(channels.map((channel) => channel.name))
-    setLoading(false)
+  const addUserToChannel = async (email: string) => {
+    const user = await supabase.from('profiles').select('user_id').eq('email', email)
+    console.log()
+    if (!user.data?.length) {
+      addMessage(true, true, `User ${email} not found`)
+    } else {
+      console.log(user.count)
+      const room = await supabase.from('rooms').select('id').eq('topic', selectedRoom)
+
+      await supabase
+        .from('rooms_users')
+        .upsert({ user_id: user.data?.[0].user_id, room_id: room.data?.[0].id })
+      addMessage(true, true, `Added ${email} to channel ${selectedRoom}`)
+      setUsers([...users, email])
+    }
+  }
+
+  const addMessage = async (mine: boolean, system: boolean, message: string) => {
+    const bubble = document.createElement('div')
+    const is_self_classes = mine ? ['bg-green-600', 'self-end'] : ['bg-blue-600', 'self-start']
+    const is_system_classes = system ? ['bg-stone-500', 'self-center', 'italic', 'text-center'] : []
+    const style = [
+      'flex',
+      'gap-2',
+      'items-center',
+      'rounded-xl',
+      'text-white',
+      'text-bold',
+      'w-2/3',
+      'p-2',
+    ]
+      .concat(is_self_classes)
+      .concat(is_system_classes)
+    bubble.classList.add(...style)
+    bubble.innerHTML = message
+    document.getElementById('chat')!.appendChild(bubble)
   }
 
   useEffect(() => {
-    getChannels()
-    supabase.auth.getUser().then((user) => setUserId(user.data.user?.id || null))
+    supabase.auth
+      .getUser()
+      .then((user) => setUserId(user.data.user?.id || null))
+      .then(async () => {
+        await supabase.auth.getUser()
+        const token = (await supabase.auth.getSession()).data.session?.access_token!
+        supabase.realtime.setAuth(token)
+        getChannels()
+      })
+      .then(() => {
+        setLoading(false)
+      })
   }, [supabase])
 
   useEffect(() => {
@@ -40,39 +82,39 @@ export default function Chat() {
       channel?.unsubscribe()
 
       let newChannel = supabase.realtime.channel(selectedRoom, {
-        config: { broadcast: { self: true } },
+        config: {
+          broadcast: { self: true },
+          presence: { key: userId! },
+          private: true, // This line will tell the server that you want to use a private channel for this connection
+        },
       })
 
       newChannel
-        .on('broadcast', { event: 'message' }, ({ payload: payload }) => {
-          const bubble = document.createElement('div')
-          const mine =
-            payload.user_id == userId ? ['bg-green-600', 'self-end'] : ['bg-blue-600', 'self-start']
-          const style = [
-            'flex',
-            'gap-2',
-            'items-center',
-            'rounded-xl',
-            'text-white',
-            'text-bold',
-            'w-2/3',
-            'p-2',
-          ].concat(mine)
-          bubble.classList.add(...style)
-          bubble.innerHTML = payload.message
-          document.getElementById('chat')!.appendChild(bubble)
+        .on('broadcast', { event: 'message' }, ({ payload: payload }) =>
+          addMessage(payload.user_id == userId, false, payload.message)
+        )
+        .on('presence', { event: 'sync' }, () => {
+          console.log(['sync', newChannel.presenceState()])
+        })
+        .on('presence', { event: 'join' }, ({ key, newPresences }) => {
+          console.log(['join', key, newPresences])
+        })
+        .on('presence', { event: 'leave' }, ({ key, leftPresences }) => {
+          console.log(['leave', key, leftPresences])
         })
         .subscribe((status, err) => {
           if (status == 'SUBSCRIBED') {
+            console.log(err)
             setChannel(newChannel)
             setLoading(false)
             setError(null)
           }
           if (status == 'CLOSED') {
+            console.log(err)
             setChannel(null)
           }
           if (status == 'CHANNEL_ERROR') {
-            console.log({ status, err })
+            console.log(err)
             setError(err?.message || null)
             setLoading(false)
           }
@@ -89,9 +131,9 @@ export default function Chat() {
       )}
       {showModal ? <CreateRoomModal /> : ''}
       <div className="flex w-full h-full gap-4">
-        <div className="grow-0 flex  flex-col gap-2">
+        <div className="grow-0 flex flex-col gap-2 w-[20rem] overflow-hidden">
           <div className="bg-white h-full rounded-sm text-slate-900">
-            <div className="flex flex-col sm:max-w-md gap-2 text-foreground">
+            <div className="flex flex-col">
               {rooms?.map((room: string) => {
                 return (
                   <button
@@ -102,13 +144,20 @@ export default function Chat() {
                     }}
                     className={
                       selectedRoom == room
-                        ? 'bg-green-600 rounded-sm pointer p-1 text-white'
-                        : 'rounded-sm cursor-pointer hover:bg-green-100 p-1 text-black'
+                        ? 'bg-green-600 rounded-sm pointer p-2 text-white text-left'
+                        : 'rounded-sm cursor-pointer hover:bg-green-100 p-2 text-black text-left'
                     }
                   >
-                    # {room}
+                    #{room}
                   </button>
                 )
+              })}
+            </div>
+          </div>
+          <div className="bg-white h-full rounded-sm text-slate-900">
+            <div className="flex flex-col gap-2 p-2">
+              {users?.map((email: string) => {
+                return <div key={email}>{email}</div>
               })}
             </div>
           </div>
@@ -138,11 +187,18 @@ export default function Chat() {
               const form = e.target as HTMLFormElement
               const target = form.elements[0] as HTMLInputElement
               const message = target.value
-              channel?.send({
-                type: 'broadcast',
-                event: 'message',
-                payload: { message, user_id: userId },
-              })
+
+              if (message.startsWith('/invite')) {
+                const email = message.replace('/invite ', '')
+                addUserToChannel(email)
+              } else {
+                channel?.send({
+                  type: 'broadcast',
+                  event: 'message',
+                  payload: { message, user_id: userId },
+                })
+              }
+
               target.value = ''
             }}
           >
