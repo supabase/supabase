661302dde1ba9003ea70fa687b04a9c9030e321c
Jan Tennert <jan.m.tennert@gmail.com>
2024-06-19 18:59:54 +0200

    Update content.tsx

--- apps/studio/components/interfaces/Home/Connect/content/androidkotlin/supabasekt/content.tsx
+++ apps/studio/components/interfaces/Home/Connect/content/androidkotlin/supabasekt/content.tsx
@@ -39,7 +39,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SupabaseTutorialTheme {
+            MaterialTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
