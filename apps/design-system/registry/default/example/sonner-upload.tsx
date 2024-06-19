c7f66eb8c78bf099fa80509c67c0d64076c176c4
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-06-19 19:22:10 +0800

    Update sonner-upload.tsx

--- apps/design-system/registry/default/example/sonner-upload.tsx
+++ apps/design-system/registry/default/example/sonner-upload.tsx
@@ -1,99 +1,83 @@
-'use client'
-
-import { Loader2 } from 'lucide-react'
-import { useEffect, useState } from 'react'
+import { Check, Loader2 } from 'lucide-react'
+import { useState } from 'react'
 import { toast } from 'sonner'
 import { Button, Progress } from 'ui'
 
 export default function SonnerUpload() {
-  const [progress, setProgress] = useState(0)
-  const [isUploading, setIsUploading] = useState(false)
+  const [isUploading, setIsUploadingState] = useState(false)
 
-  const uploadPromise = () => {
+  const uploadPromise = (files: number, toastId: number) => {
+    toast.loading(<UploadContent progress={0} files={files} />, {
+      id: toastId,
+    })
+
     return new Promise((resolve) => {
       let currentProgress = 0
-      setIsUploading(true)
-      setProgress(0)
 
-      const interval = setInterval(() => {
+      toast.loading(<UploadContent progress={currentProgress} files={files} />, {
+        id: toastId,
+      })
+
+      const intervalId = setInterval(() => {
         currentProgress += Math.floor(Math.random() * 10) + 1 // Increment progress by a random value
         if (currentProgress > 100) currentProgress = 100 // Ensure progress does not exceed 100%
 
-        setProgress((prevProgress) => {
-          if (currentProgress >= 100) {
-            clearInterval(interval)
-            resolve({ name: 'Sonner', length: 6 }) // Resolve the promise when progress reaches 100
-            setIsUploading(false)
-            return currentProgress
-          }
-          return currentProgress
+        if (currentProgress >= 100) {
+          clearInterval(intervalId) // Clear the interval once upload is complete
+          resolve({ progress: currentProgress, message: 'Upload complete', files: 3, toastId }) // Resolve the promise when progress reaches 100
+        }
+
+        toast.loading(<UploadContent progress={currentProgress} files={files} />, {
+          id: toastId,
         })
-      }, 750) // Adjust the interval time as needed
+      }, 200) // Adjust the interval time as needed
     })
   }
 
-  // useEffect(() => {
-  //   if (isUploading) {
-  //     if (progress < 100) {
-  //       toast(<UploadContent />, {
-  //         id: 'upload',
-  //       })
-  //     }
-  //   }
-  //   if (progress === 100) {
-  //     console.log('now at 100')
-  //     toast(<UploadContent />, {
-  //       id: 'upload',
-  //     })
-
-  //     // wait for 5 seconds
-  //     setTimeout(() => {
-  //       toast.dismiss('upload')
-  //     }, 5000)
-  //   }
-  // }, [progress])
-
-  const UploadContent = ({ progress }: { progress: number }) => (
+  const UploadContent = ({ progress, files }: { progress: number; files: number }) => (
     <div className="flex gap-3 w-full">
-      <Loader2 className="animate-spin text-foreground-muted mt-0.5" size={16} />
+      {progress >= 100 ? (
+        <Check className="text-foreground mt-0.5" size={16} strokeWidth={4} />
+      ) : (
+        <Loader2 className="animate-spin text-foreground-muted mt-0.5" size={16} />
+      )}
       <div className="flex flex-col gap-2 w-full">
-        <p className="text-foreground-light text-sm">Still uploading {progress}%</p>
+        <div className="flex w-full justify-between">
+          <p className="text-foreground text-sm">
+            {progress >= 100
+              ? `Upload complete`
+              : files
+                ? `Uploading ${files} files...`
+                : `Uploading...`}
+          </p>
+          <p className="text-foreground-light text-sm font-mono">{`${progress}%`}</p>
+        </div>
         <Progress value={progress} className="w-full" />
+        <small className="text-foreground-muted text-xs">
+          Please do not close the browser until completed
+        </small>
       </div>
     </div>
   )
 
   return (
     <div className="flex flex-col gap-3">
-      <Progress value={progress} className="w-full" />
       <Button
         type="default"
-        disabled={isUploading}
-        loading={isUploading}
         onClick={async () => {
-          await toast(<UploadContent progress={progress} />, {
-            id: 'upload',
-          })
-
-          // await toast(`Starting to upload file`, { id: 'upload' })
-
-          // toast.promise(uploadPromise, {
-          //   id: 'upload',
-          //   loading: `Uploading - ${progress}%`,
-          //   success: (data) => {
-          //     return `Upload complete`
-          //   },
-          //   error: 'Error',
-          // })
-
-          // toast.custom((t) => (
-          //   <div>
-          //     {progress}
-          //     This is a custom component <button onClick={() => toast.dismiss(t)}>close</button>
-          //   </div>
-          // ))
+          // random id
+          const toastId = Math.random()
+          const max = 20 // specify the maximum value
+          const files = Math.floor(Math.random() * max)
           try {
-            uploadPromise()
+            setIsUploadingState(true)
+            const promise = await uploadPromise(files, toastId)
+            // @ts-expect-error
+            toast.success(<UploadContent progress={promise.progress} files={promise.files} />, {
+              duration: 2000,
+              id: toastId,
+            })
+            setIsUploadingState(false)
           } catch (error) {}
         }}
       >
