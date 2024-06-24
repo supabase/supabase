fd9acdf28d1427647e246dceda805aa589b5bd27
Charis <26616127+charislam@users.noreply.github.com>
2024-06-24 18:47:53 -0400

    refactor(docs): migrate home page to app router (#27221)

--- apps/docs/layouts/HomeLayout.tsx
+++ apps/docs/layouts/HomeLayout.tsx
@@ -1,73 +1,22 @@
-import { MDXProvider } from '@mdx-js/react'
-import { NextSeo } from 'next-seo'
-import Head from 'next/head'
-import { useRouter } from 'next/router'
-import { FC } from 'react'
-import components from '~/components'
+import { type PropsWithChildren } from 'react'
+
 import HomePageCover from '~/components/HomePageCover'
 import { MenuId } from '~/components/Navigation/NavigationMenu/NavigationMenu'
 import { LayoutMainContent } from './DefaultLayout'
 import { MainSkeleton } from './MainSkeleton'
 
-interface Props {
-  meta: {
-    title: string
-    canonical?: string
-    description?: string
-    video?: string
-  }
-  children: any
-}
-
-const HomeLayout: FC<Props> = (props: Props) => {
-  const { asPath } = useRouter()
-
+const HomeLayout = ({ children }: PropsWithChildren) => {
   return (
-    <>
-      <Head>
-        <title>{asPath === '/' ? 'Supabase Docs' : `${props.meta?.title} | Supabase Docs`}</title>
-        <meta name="description" content={props.meta?.description} />
-        <meta property="og:image" content={`https://supabase.com/docs/img/supabase-og-image.png`} />
-        <meta
-          name="twitter:image"
-          content={`https://supabase.com/docs/img/supabase-og-image.png`}
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
-      <NextSeo
-        canonical={props.meta?.canonical ?? `https://supabase.com/docs${asPath}`}
-        openGraph={{
-          url: `https://supabase.com/docs${asPath}`,
-          type: 'article',
-          videos: props.meta?.video && [
-            {
-              // youtube based video meta
-              url: props.meta?.video,
-              width: 640,
-              height: 385,
-              type: 'application/x-shockwave-flash',
-            },
-          ],
-          article: {
-            publishedTime: new Date().toISOString(),
-            modifiedTime: new Date().toISOString(),
-            authors: ['Supabase'],
-          },
-        }}
-      />
-      <MainSkeleton menuId={MenuId.Home}>
-        <article>
-          <HomePageCover meta={props.meta} />
-          <LayoutMainContent>
-            <div className={['relative transition-all ease-out', 'duration-150 '].join(' ')}>
-              <div className="prose max-w-none">
-                <MDXProvider components={components}>{props.children}</MDXProvider>
-              </div>
-            </div>
-          </LayoutMainContent>
-        </article>
-      </MainSkeleton>
-    </>
+    <MainSkeleton menuId={MenuId.Home}>
+      <article>
+        <HomePageCover title="Supabase Documentation" />
+        <LayoutMainContent>
+          <div className={['relative transition-all ease-out', 'duration-150 '].join(' ')}>
+            <div className="prose max-w-none">{children}</div>
+          </div>
+        </LayoutMainContent>
+      </article>
+    </MainSkeleton>
   )
 }
 
