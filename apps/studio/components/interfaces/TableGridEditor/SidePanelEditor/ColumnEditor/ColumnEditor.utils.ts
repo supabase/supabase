42dd70befaee3348c08682e5118bbffe0512efaa
Joshen Lim <joshenlimek@gmail.com>
2024-06-26 18:18:41 +0800

    Address comment

--- apps/studio/components/interfaces/TableGridEditor/SidePanelEditor/ColumnEditor/ColumnEditor.utils.ts
+++ apps/studio/components/interfaces/TableGridEditor/SidePanelEditor/ColumnEditor/ColumnEditor.utils.ts
@@ -129,7 +129,9 @@ export const generateUpdateColumnPayload = (
   const check = field.check?.trim()
 
   const payload: Partial<UpdateColumnPayload> = {}
-  if (!isEqual(originalColumn.name, name)) {
+  // [Joshen] Trimming on the original name as well so we don't rename columns that already
+  // contain whitespaces (and accidentally bringing user apps down)
+  if (!isEqual(originalColumn.name.trim(), name)) {
     payload.name = name
   }
   if (!isEqual(originalColumn.comment, comment)) {
