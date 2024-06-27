16e2d62b0672dd5c1e56e463f64fd5f386767c5b
Joshen Lim <joshenlimek@gmail.com>
2024-06-27 16:51:08 +0800

    Add some sentry filters

--- apps/studio/sentry.client.config.ts
+++ apps/studio/sentry.client.config.ts
@@ -42,6 +42,7 @@ Sentry.init({
   ignoreErrors: [
     // Used exclusively in Monaco Editor.
     'ResizeObserver',
+    's.getModifierState is not a function',
     // [Joshen] We currently use stripe-js for customers to save their credit card data
     // I'm unable to reproduce this error on local, staging nor prod across chrome, safari or firefox
     // Based on https://github.com/stripe/stripe-js/issues/26, it seems like this error is safe to ignore,
