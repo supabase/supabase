43b0242f21e6375ca243224faa2841ef3cdc8e22
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-06-27 10:34:45 +0800

    add tree view

--- apps/studio-prototype/src/app/[org]/table-editor/table-editor-items-panel.tsx
+++ apps/studio-prototype/src/app/[org]/table-editor/table-editor-items-panel.tsx
@@ -14,6 +14,7 @@ import {
 import { useHotkeys } from 'react-hotkeys-hook'
 import { ChevronRight, Plus, Search } from 'lucide-react'
 import { Input } from 'ui-patterns/DataInputs/Input'
+import { TreeViewExample } from './tree-view-example'
 
 export default function TableEditorItemsPanel() {
   const { org } = useParams()
@@ -151,6 +152,7 @@ export default function TableEditorItemsPanel() {
               size="tiny"
             />
           </div>
+          <TreeViewExample />
         </CollapsibleContent_Shadcn_>
       </Collapsible_Shadcn_>
     </div>
