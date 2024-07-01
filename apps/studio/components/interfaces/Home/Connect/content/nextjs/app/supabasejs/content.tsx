68f59de900dc0242c9270edf65320698bbb98cd6
Steve Xu <stevexugc@gmail.com>
2024-07-01 22:18:56 +0800

    fix(docs): createServerClient warning

--- apps/studio/components/interfaces/Home/Connect/content/nextjs/app/supabasejs/content.tsx
+++ apps/studio/components/interfaces/Home/Connect/content/nextjs/app/supabasejs/content.tsx
@@ -55,40 +55,29 @@ export default async function Page() {
       <ConnectTabContent value="utils/supabase/server.ts">
         <SimpleCodeBlock className="ts" parentClassName="min-h-72">
           {`
-import { createServerClient, type CookieOptions } from "@supabase/ssr";
-import { cookies } from "next/headers";
+import { createServerClient } from '@supabase/ssr';
+import { cookies } from 'next/headers';
 
-export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
+export function createClient() {
+  const cookieStore = cookies();
+
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value;
+        getAll() {
+          return cookieStore.getAll();
         },
-        set(name: string, value: string, options: CookieOptions) {
-          try {
-            cookieStore.set({ name, value, ...options });
-          } catch (error) {
-            // The \`set\` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
-        },
-        remove(name: string, options: CookieOptions) {
-          try {
-            cookieStore.set({ name, value: "", ...options });
-          } catch (error) {
-            // The \`delete\` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) =>
+            cookieStore.set(name, value, options),
+          );
         },
       },
     },
   );
-};
+}
 `}
         </SimpleCodeBlock>
       </ConnectTabContent>
