5e8c3ab67187cee9e8dd735674443e6ce40046e4
Andrew Smith <a.smith@silentworks.co.uk>
2024-07-06 11:42:13 +0000

    Update to keep auth confirm path consistent

--- examples/user-management/sveltekit-user-management/src/routes/+page.server.ts
+++ examples/user-management/sveltekit-user-management/src/routes/+page.server.ts
@@ -22,7 +22,7 @@ export const actions: Actions = {
 		} = event
 		const formData = await request.formData()
 		const email = formData.get('email') as string
-    const validEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,8}$/.test(email)
+    const validEmail = /^[\w-\.+]+@([\w-]+\.)+[\w-]{2,8}$/.test(email)
     
 		if (!validEmail) {
 			return fail(400, { errors: { email: "Please enter a valid email address" }, email })
