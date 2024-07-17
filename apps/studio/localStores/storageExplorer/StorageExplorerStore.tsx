7d2da5e63ab32819f2148b336b2d94f19eca14af
Alaister Young <alaister@users.noreply.github.com>
2024-07-17 17:23:26 +0800

    Update apps/studio/localStores/storageExplorer/StorageExplorerStore.tsx

--- apps/studio/localStores/storageExplorer/StorageExplorerStore.tsx
+++ apps/studio/localStores/storageExplorer/StorageExplorerStore.tsx
@@ -683,9 +683,9 @@ class StorageExplorerStore {
             chunkSize,
             onShouldRetry(error) {
               const status = error.originalResponse ? error.originalResponse.getStatus() : 0
-              const doNotRetryStatuses = [400, 403, 404, 429, 409, 404]
+              const doNotRetryStatuses = [400, 403, 404, 409, 429]
 
-              return doNotRetryStatuses.includes(status)
+              return !(doNotRetryStatuses.includes(status))
             },
             onError(error) {
               numberOfFilesUploadedFail += 1
