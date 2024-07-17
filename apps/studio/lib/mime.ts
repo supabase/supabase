c2af30ba352c17726ce91a2336761e239dc6a393
Alaister Young <a@alaisteryoung.com>
2024-07-17 14:26:11 +1000

    lowercase extensions

--- apps/studio/lib/mime.ts
+++ apps/studio/lib/mime.ts
@@ -3,7 +3,9 @@ import db from 'mime-db'
 export const MIME_TYPES_BY_EXTENSION = Object.fromEntries(
   Object.entries(db)
     .filter(([, { extensions }]) => extensions !== undefined)
-    .flatMap(([mime, { extensions }]) => extensions!.map((extension) => [extension, mime]))
+    .flatMap(([mime, { extensions }]) =>
+      extensions!.map((extension) => [extension.toLowerCase(), mime])
+    )
 )
 
 export function lookupMime(extension?: string): string | undefined {
@@ -11,5 +13,5 @@ export function lookupMime(extension?: string): string | undefined {
     return undefined
   }
 
-  return MIME_TYPES_BY_EXTENSION[extension]
+  return MIME_TYPES_BY_EXTENSION[extension.toLowerCase()]
 }
