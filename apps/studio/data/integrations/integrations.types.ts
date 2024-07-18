0e259ed9393cbfbbc11da6705741e1693acd95b7
Alaister Young <a@alaisteryoung.com>
2024-07-18 15:18:04 +1000

    fix types

--- apps/studio/data/integrations/integrations.types.ts
+++ apps/studio/data/integrations/integrations.types.ts
@@ -156,7 +156,7 @@ export type IntegrationProjectConnection = {
   foreign_project_id: string
   organization_integration_id: string
   env_sync_targets?: string[]
-  public_env_var_prefix: string
+  public_env_var_prefix?: string
   metadata: Imetadata
 }
 
