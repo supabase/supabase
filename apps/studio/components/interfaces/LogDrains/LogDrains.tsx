f648333c1a8309236f1461d1732315bc2c4c8c0d
Jordi Enric <jordi.err@gmail.com>
2024-07-18 18:58:05 +0200

    add log drain table

--- apps/studio/components/interfaces/LogDrains/LogDrains.tsx
+++ apps/studio/components/interfaces/LogDrains/LogDrains.tsx
@@ -1,17 +1,23 @@
 import { useLogDrainsQuery } from 'data/log-drains/log-drains-query'
 import { useProjectAddonsQuery } from 'data/subscriptions/project-addons-query'
 import Link from 'next/link'
-import { LOG_DRAIN_SOURCES } from './LogDrains.constants'
+import { LOG_DRAIN_SOURCES, LogDrainSource } from './LogDrains.constants'
 import { useParams } from 'common'
 import CardButton from 'components/ui/CardButton'
 import Panel from 'components/ui/Panel'
+import { GenericSkeletonLoader } from 'ui-patterns'
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from 'ui'
 
-export function LogDrains() {
+export function LogDrains({ onNewDrainClick }: { onNewDrainClick: (src: LogDrainSource) => void }) {
   const { ref } = useParams()
   const { data: logDrains, isLoading } = useLogDrainsQuery({ ref })
 
   if (isLoading) {
-    return <div></div>
+    return (
+      <div>
+        <GenericSkeletonLoader />
+      </div>
+    )
   }
 
   if (!isLoading && logDrains?.length === 0) {
@@ -23,7 +29,9 @@ export function LogDrains() {
             title={src.name}
             description={src.description}
             icon={src.icon}
-            linkHref={`/project/${ref}/settings/log-drains?new=1&src=${src.value}`}
+            onClick={() => {
+              onNewDrainClick(src.value)
+            }}
           />
         ))}
       </div>
@@ -32,8 +40,29 @@ export function LogDrains() {
 
   return (
     <>
-      <Panel className="p-3">
-        {logDrains?.map((drain: any) => <pre>{JSON.stringify(drain, null, 2)}</pre>)}
+      <Panel className="">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Name</TableHead>
+              <TableHead>Source</TableHead>
+              <TableHead>Inserted At</TableHead>
+              <TableHead className="text-right">Actions</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {logDrains?.map((drain) => (
+              <TableRow key={drain.id}>
+                <TableCell className="font-medium">{drain.name}</TableCell>
+                <TableCell>{drain.source}</TableCell>
+                <TableCell>{new Date(drain.inserted_at).toLocaleString()}</TableCell>
+                <TableCell className="text-right">
+                  <Link href={`/project/${ref}/settings/log-drains/${drain.id}`}>View</Link>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </Panel>
     </>
   )
