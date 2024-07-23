7eeb6d5d095f6312e01ecd5cb7233ac266c46654
Charis Lam <26616127+charislam@users.noreply.github.com>
2024-07-23 15:20:52 -0400

    perf: shave bundle size

--- apps/docs/features/docs/Reference.mdx.client.tsx
+++ apps/docs/features/docs/Reference.mdx.client.tsx
@@ -3,11 +3,17 @@
 /**
  * The MDXProvider is necessary so that MDX partials will have access
  * to components.
+ *
+ * Since the reference pages are so heavy, keep the weight down by only
+ * including the bare minimum.
  */
 
 import { MDXProvider } from '@mdx-js/react'
 import { type PropsWithChildren } from 'react'
-import { components } from '~/features/docs/MdxBase.shared'
+
+import { Admonition } from 'ui'
+
+const components = { Admonition }
 
 export function MDXProviderReference({ children }: PropsWithChildren) {
   return <MDXProvider components={components}>{children}</MDXProvider>
