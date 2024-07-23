40a8bb49d2766fd04a43e9c1175ca020e58236df
Ivan Vasilov <vasilov.ivan@gmail.com>
2024-07-23 17:05:08 +0200

    Fix the Category select, logo upload and description in the side sheet.

--- apps/studio/components/interfaces/Settings/Integrations/SupabaseIntegration/CreateIntegrationSheet.tsx
+++ apps/studio/components/interfaces/Settings/Integrations/SupabaseIntegration/CreateIntegrationSheet.tsx
@@ -4,6 +4,7 @@ import { SubmitHandler, useForm } from 'react-hook-form'
 import toast from 'react-hot-toast'
 import z from 'zod'
 
+import { Markdown } from 'components/interfaces/Markdown'
 import { useIntegrationDirectoryEntryCreateMutation } from 'data/integrations-directory/integration-directory-entry-create-mutation'
 import { useIntegrationDirectoryEntryDeleteMutation } from 'data/integrations-directory/integration-directory-entry-delete-mutation'
 import { useIntegrationDirectoryEntryUpdateMutation } from 'data/integrations-directory/integration-directory-entry-update-mutation'
@@ -21,17 +22,28 @@ import {
   FormField_Shadcn_,
   Form_Shadcn_,
   Input_Shadcn_,
+  SelectContent_Shadcn_,
+  SelectItem_Shadcn_,
+  SelectTrigger_Shadcn_,
+  SelectValue_Shadcn_,
+  Select_Shadcn_,
   Separator,
   SheetClose,
   SheetFooter,
   SheetHeader,
   SheetSection,
   SheetTitle,
+  TabsContent_Shadcn_,
+  TabsList_Shadcn_,
+  TabsTrigger_Shadcn_,
+  Tabs_Shadcn_,
   cn,
 } from 'ui'
 import ConfirmationModal from 'ui-patterns/Dialogs/ConfirmationModal'
 import { FormItemLayout } from 'ui-patterns/form/FormItemLayout/FormItemLayout'
 import { WarningIcon } from 'ui-patterns/Icons/StatusIcons'
+import { DroppableTextArea } from './DroppableTextArea'
+import { LogoFileInput } from './LogoFileInput'
 
 const FORM_ID = 'create-integration-sidepanel'
 
@@ -39,6 +51,18 @@ interface CreateIntegrationSheetProps {
   integrationEntry?: IntegrationEntry
   setVisible: (value: boolean) => void
 }
+
+export const IntegrationCategory = {
+  api: 'API',
+  auth: 'Auth',
+  caching: 'Caching / Offline-First',
+  data: 'Data Platform',
+  devtools: 'DevTools',
+  fdw: 'Foreign Data Wrapper',
+  lowcode: 'Low-Code',
+  messaging: 'Messaging',
+  storage: 'Storage',
+} as const
 
 const FormSchema = z.object({
   call_to_action_link: z.string().optional(),
@@ -57,7 +81,7 @@ const FormSchema = z.object({
   developer: z.string(),
   docs: z.string(),
   images: z.array(z.string()),
-  logo: z.string(),
+  logo: z.string().nullable(),
   overview: z.string(),
   slug: z.string(),
   title: z.string(),
@@ -83,7 +107,7 @@ export const CreateIntegrationSheet = ({
       developer: integrationEntry?.developer ?? '',
       docs: integrationEntry?.docs ?? '',
       images: integrationEntry?.images ?? [],
-      logo: integrationEntry?.logo ?? '',
+      logo: integrationEntry?.logo ?? null,
       overview: integrationEntry?.overview ?? '',
       slug: integrationEntry?.slug ?? '',
       title: integrationEntry?.title ?? '',
@@ -126,7 +150,7 @@ export const CreateIntegrationSheet = ({
             title: data.title,
             video: data.video,
             website: data.website,
-          },
+          } as any,
         },
         {
           onSuccess: () => {
@@ -152,7 +176,7 @@ export const CreateIntegrationSheet = ({
             title: data.title,
             video: data.video,
             website: data.website,
-          },
+          } as any,
         },
         {
           onSuccess: () => {
@@ -239,36 +263,56 @@ export const CreateIntegrationSheet = ({
               </Alert_Shadcn_>
             )}
 
-            <FormField_Shadcn_
-              key="slug"
-              control={form.control}
-              name="slug"
-              render={({ field }) => (
-                <FormItemLayout
-                  label="Slug"
-                  description="The unique identifier for this integration (will be used in the URL)"
-                >
-                  <FormControl_Shadcn_>
-                    <Input_Shadcn_ {...field} autoFocus />
-                  </FormControl_Shadcn_>
-                </FormItemLayout>
-              )}
-            />
-            <FormField_Shadcn_
-              key="title"
-              control={form.control}
-              name="title"
-              render={({ field }) => (
-                <FormItemLayout
-                  label="Title"
-                  description="A title of this integration (will be used in the search)"
-                >
-                  <FormControl_Shadcn_>
-                    <Input_Shadcn_ {...field} />
-                  </FormControl_Shadcn_>
-                </FormItemLayout>
-              )}
-            />
+            <div className="flex flex-row space-x-6">
+              <div className="w-3/4 space-y-6">
+                <FormField_Shadcn_
+                  key="slug"
+                  control={form.control}
+                  name="slug"
+                  render={({ field }) => (
+                    <FormItemLayout
+                      label="Slug"
+                      description="The unique identifier for this integration (will be used in the URL)"
+                    >
+                      <FormControl_Shadcn_>
+                        <Input_Shadcn_ {...field} autoFocus />
+                      </FormControl_Shadcn_>
+                    </FormItemLayout>
+                  )}
+                />
+                <FormField_Shadcn_
+                  key="title"
+                  control={form.control}
+                  name="title"
+                  render={({ field }) => (
+                    <FormItemLayout
+                      label="Title"
+                      description="A title of this integration (will be used in the search)"
+                    >
+                      <FormControl_Shadcn_>
+                        <Input_Shadcn_ {...field} />
+                      </FormControl_Shadcn_>
+                    </FormItemLayout>
+                  )}
+                />
+              </div>
+              <div className="w-1/4 flex items-center justify-center">
+                <FormField_Shadcn_
+                  key="logo"
+                  control={form.control}
+                  name="logo"
+                  render={({ field }) => (
+                    <FormItemLayout>
+                      <FormControl_Shadcn_>
+                        <LogoFileInput logoUrl={field.value} onChange={field.onChange} />
+                        {/* <SupabaseFileInput value={field.value} onChange={field.onChange} /> */}
+                      </FormControl_Shadcn_>
+                    </FormItemLayout>
+                  )}
+                />
+              </div>
+            </div>
+
             <FormField_Shadcn_
               key="developer"
               control={form.control}
@@ -289,43 +333,25 @@ export const CreateIntegrationSheet = ({
               control={form.control}
               name="category"
               render={({ field }) => (
-                <FormItemLayout
-                  label="Category"
-                  description="CHANGE ME TO BE A DROPDOWN WITH ENUM VALUES"
-                >
+                <FormItemLayout label="Category">
                   <FormControl_Shadcn_>
-                    <Input_Shadcn_ {...field} />
+                    <Select_Shadcn_>
+                      <SelectTrigger_Shadcn_>
+                        <SelectValue_Shadcn_ placeholder="Pick a category" />
+                      </SelectTrigger_Shadcn_>
+                      <SelectContent_Shadcn_>
+                        {Object.entries(IntegrationCategory).map(([key, value]) => (
+                          <SelectItem_Shadcn_ key={key} value={key}>
+                            {value}
+                          </SelectItem_Shadcn_>
+                        ))}
+                      </SelectContent_Shadcn_>
+                    </Select_Shadcn_>
                   </FormControl_Shadcn_>
                 </FormItemLayout>
               )}
             />
-            <FormField_Shadcn_
-              key="logo"
-              control={form.control}
-              name="logo"
-              render={({ field }) => (
-                <FormItemLayout label="Logo" description="CHANGE ME TO BE AN UPLOAD FIELD">
-                  <FormControl_Shadcn_>
-                    <Input_Shadcn_ {...field} />
-                  </FormControl_Shadcn_>
-                </FormItemLayout>
-              )}
-            />
-            <FormField_Shadcn_
-              key="images"
-              control={form.control}
-              name="images"
-              render={({ field }) => (
-                <FormItemLayout
-                  label="Images"
-                  description="CHANGE ME TO BE A MULTI-FILE UPLOAD FIELD"
-                >
-                  <FormControl_Shadcn_>
-                    <Input_Shadcn_ {...field} />
-                  </FormControl_Shadcn_>
-                </FormItemLayout>
-              )}
-            />
+
             <FormField_Shadcn_
               key="website"
               control={form.control}
@@ -380,17 +406,30 @@ export const CreateIntegrationSheet = ({
                 </FormItemLayout>
               )}
             />
+
             <FormField_Shadcn_
               key="description"
               control={form.control}
               name="description"
               render={({ field }) => (
-                <FormItemLayout
-                  label="Description"
-                  description="Short description for your integration. Will be shown on the directory page."
-                >
+                <FormItemLayout label="Description">
                   <FormControl_Shadcn_>
-                    <ExpandingTextArea {...field} className="min-h-24" />
+                    <Tabs_Shadcn_ defaultValue="write" className="w-full">
+                      <TabsList_Shadcn_ className="grid w-full grid-cols-2">
+                        <TabsTrigger_Shadcn_ value="write">Write</TabsTrigger_Shadcn_>
+                        <TabsTrigger_Shadcn_ value="preview">Preview</TabsTrigger_Shadcn_>
+                      </TabsList_Shadcn_>
+                      <TabsContent_Shadcn_ value="write" className="">
+                        <DroppableTextArea
+                          value={field.value}
+                          onChange={field.onChange}
+                          className="min-h-[400px]"
+                        />
+                      </TabsContent_Shadcn_>
+                      <TabsContent_Shadcn_ value="preview">
+                        <Markdown content={field.value} />
+                      </TabsContent_Shadcn_>
+                    </Tabs_Shadcn_>
                   </FormControl_Shadcn_>
                 </FormItemLayout>
               )}
