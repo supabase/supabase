3cdd410c29e2cbd23a460c8a12b830fe1a638363
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2024-07-26 10:49:55 +0200

    fix

--- apps/www/pages/launch-week/tickets/[username].tsx
+++ apps/www/pages/launch-week/tickets/[username].tsx
@@ -15,7 +15,6 @@ import DefaultLayout from '~/components/Layouts/Default'
 import SectionContainer from '~/components/Layouts/SectionContainer'
 import { TicketState, ConfDataContext, UserData } from '~/components/LaunchWeek/hooks/use-conf-data'
 import LW12Background from '~/components/LaunchWeek/12/LW12Background'
-import { isBrowser } from 'common'
 
 const LW12TicketContainer = dynamic(
   () => import('~/components/LaunchWeek/12/Ticket/TicketContainer')
@@ -111,8 +110,8 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   )
 
   // fetch a specific user
-  if (username && !isBrowser) {
-    const { data } = await supabaseAdmin!
+  if (username && supabaseAdmin) {
+    const { data } = await supabaseAdmin
       .from('tickets_view')
       .select('name, username, ticket_number, metadata, platinum, secret, role, company, location')
       .eq('launch_week', 'lw12')
