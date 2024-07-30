d78bb6ae62c76945a1830c62387ce98609a3a725
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-07-30 14:37:55 +0800

    smol updates

--- apps/www/pages/index.tsx
+++ apps/www/pages/index.tsx
@@ -7,9 +7,9 @@ const Products = dynamic(() => import('~/components/Products/index'))
 const BuiltExamples = dynamic(() => import('components/BuiltWithSupabase/index'))
 const MadeForDevelopers = dynamic(() => import('components/MadeForDevelopers/index'))
 const AdminAccess = dynamic(() => import('components/AdminAccess/index'))
-const CTABanner = dynamic(() => import('components/CTABanner/index'))
 const CustomerStories = dynamic(() => import('components/CustomerStories'))
 const TwitterSocialSection = dynamic(() => import('~/components/TwitterSocialSection'))
+const CTABanner = dynamic(() => import('components/CTABanner/index'))
 const ReactTooltip = dynamic(() => import('react-tooltip'), { ssr: false })
 
 const Index = () => {
