e8da48c33fcafad0c1ee5370d6f64b26836a7aff
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-07-31 20:06:04 +0800

    ticket themes updated

--- apps/www/components/LaunchWeek/12/Ticket/ticketThemes.tsx
+++ apps/www/components/LaunchWeek/12/Ticket/ticketThemes.tsx
@@ -1,244 +1,105 @@
 export const themes = {
   regular: {
-    TICKET_BORDER: '#292929',
-    TICKET_FOREGROUND: '#F8F9FA',
+    TICKET_BORDER: 'hsl(var(--border-muted))',
+    TICKET_FOREGROUND: 'hsl(var(--foreground-default))',
     TICKET_FOREGROUND_LIGHT: '#888888',
-    TICKET_BACKGROUND: '#212121',
-    TICKET_BACKGROUND_CODE: '#171717',
-    CODE_HIGHLIGHT_BACKGROUND: '#292929',
-    CODE_HIGHLIGHT_BORDER: '#F8F9FA',
-    CODE_LINE_NUMBER: '#F8F9FA',
+    TICKET_BACKGROUND: 'hsl(var(--background-surface-100) / 1)',
+    TICKET_BACKGROUND_CODE: 'hsl(var(--background-surface-75) / 1)',
+    CODE_HIGHLIGHT_BACKGROUND: 'hsl(var(--background-surface-300) / 1)',
+    CODE_HIGHLIGHT_BORDER: '',
+    CODE_LINE_NUMBER: 'hsl(var(--foreground-lighter) / 0.5)',
     CODE_THEME: {
       hljs: {
-        display: 'block',
-        overflowX: 'auto',
-        color: '#ddd',
-      },
-      'hljs-tag': {
-        color: '#ddd',
+        color: '#fff', // corresponds to --ray-foreground
       },
       'hljs-keyword': {
-        color: '#ddd',
-        fontWeight: 'normal',
+        color: '#bda4ff', // corresponds to --ray-token-keyword
       },
-      'hljs-selector-tag': {
-        color: '#ddd',
-        fontWeight: 'normal',
-      },
-      'hljs-literal': {
-        color: '#ddd',
-        fontWeight: 'normal',
-      },
-      'hljs-strong': {
-        color: '#ddd',
-      },
-      'hljs-name': {
-        color: '#ddd',
-      },
-      'hljs-code': {
-        color: '#66d9ef',
-      },
-      'hljs-class .hljs-title': {
-        color: 'gray',
-      },
-      'hljs-attribute': {
-        color: '#bf79db',
-      },
-      'hljs-symbol': {
-        color: '#bf79db',
-      },
-      'hljs-regexp': {
-        color: '#bf79db',
-      },
-      'hljs-link': {
-        color: '#bf79db',
+      'hljs-number': {
+        color: '#ffffff', // corresponds to --ray-token-constant
       },
       'hljs-string': {
-        color: '#3ECF8E',
-      },
-      'hljs-bullet': {
-        color: '#3ECF8E',
-      },
-      'hljs-subst': {
-        color: '#3ECF8E',
-      },
-      'hljs-title': {
-        color: '#3ECF8E',
-        fontWeight: 'normal',
-      },
-      'hljs-section': {
-        color: '#3ECF8E',
-        fontWeight: 'normal',
-      },
-      'hljs-emphasis': {
-        color: '#3ECF8E',
-      },
-      'hljs-type': {
-        color: '#3ECF8E',
-        fontWeight: 'normal',
-      },
-      'hljs-built_in': {
-        color: '#3ECF8E',
-      },
-      'hljs-builtin-name': {
-        color: '#3ECF8E',
-      },
-      'hljs-selector-attr': {
-        color: '#3ECF8E',
-      },
-      'hljs-selector-pseudo': {
-        color: '#3ECF8E',
-      },
-      'hljs-addition': {
-        color: '#3ECF8E',
-      },
-      'hljs-variable': {
-        color: '#3ECF8E',
-      },
-      'hljs-template-tag': {
-        color: '#3ECF8E',
-      },
-      'hljs-template-variable': {
-        color: '#3ECF8E',
+        color: '#ffcda1', // corresponds to --ray-token-string
+        fontWeight: 'bold',
       },
       'hljs-comment': {
-        color: '#999',
+        color: 'purple', // corresponds to --ray-token-comment
       },
-      'hljs-quote': {
-        color: '#75715e',
+      'hljs-params': {
+        color: '#ffffff', // corresponds to --ray-token-parameter
       },
-      'hljs-deletion': {
-        color: '#75715e',
+      'hljs-function': {
+        color: '3ecf8e', // corresponds to --ray-token-function
       },
-      'hljs-meta': {
-        color: '#75715e',
+      'hljs-variable': {
+        color: '#ffcda1', // corresponds to --ray-token-string-expression
       },
-      'hljs-doctag': {
-        fontWeight: 'normal',
+      'hljs-punctuation': {
+        color: '#ffffff', // corresponds to --ray-token-punctuation
       },
-      'hljs-selector-id': {
-        fontWeight: 'normal',
+      'hljs-link': {
+        color: '#ffffff', // corresponds to --ray-token-link
+      },
+      'hljs-literal': {
+        color: '#ffffff', // corresponds to --ray-token-number
+      },
+      'hljs-attr': {
+        color: '#3ecf8e', // corresponds to --ray-token-property
       },
     },
   },
   platinum: {
-    TICKET_BORDER: '#B2B2B2',
-    TICKET_BACKGROUND: '#FFFFFF',
-    TICKET_BACKGROUND_CODE: '#F8F9FA',
-    TICKET_FOREGROUND: '#171717',
-    TICKET_FOREGROUND_LIGHT: '#707070',
-    CODE_HIGHLIGHT_BACKGROUND: '#E6E6E6',
-    CODE_HIGHLIGHT_BORDER: '#707070',
-    CODE_LINE_NUMBER: '#707070',
+    TICKET_BORDER: 'hsl(var(--border-muted))',
+    TICKET_FOREGROUND: 'hsl(var(--foreground-default))',
+    TICKET_FOREGROUND_LIGHT: '#888888',
+    TICKET_BACKGROUND: '#212427',
+    TICKET_BACKGROUND_CODE: '#24292D',
+    CODE_HIGHLIGHT_BACKGROUND: '#272B2E',
+    CODE_HIGHLIGHT_BORDER: '',
+    CODE_LINE_NUMBER: 'hsl(var(--foreground-lighter) / 0.5)',
     CODE_THEME: {
       hljs: {
-        display: 'block',
-        overflowX: 'auto',
-        color: '#171717',
-      },
-      'hljs-tag': {
-        color: '#707070',
+        color: '#abb2bf', // General text color
       },
       'hljs-keyword': {
-        color: '#707070',
-        fontWeight: 'normal',
-      },
-      'hljs-selector-tag': {
-        color: '#707070',
-        fontWeight: 'normal',
-      },
-      'hljs-literal': {
-        color: '#707070',
-        fontWeight: 'normal',
-      },
-      'hljs-strong': {
-        color: '#707070',
-      },
-      'hljs-name': {
-        color: '#707070',
-      },
-
-      'hljs-code': { color: '#66d9ef' },
-      'hljs-class .hljs-title': {
-        color: 'gray',
-      },
-      'hljs-attribute': {
-        color: '#bf79db',
-      },
-      'hljs-symbol': {
-        color: '#bf79db',
-      },
-      'hljs-regexp': {
-        color: '#bf79db',
-      },
-      'hljs-link': {
-        color: '#bf79db',
+        color: '#c678dd', // Keywords like "await"
       },
       'hljs-string': {
-        color: 'hsl(153.4, 100%, 36.7%)',
-      },
-      'hljs-bullet': {
-        color: 'hsl(153.4, 100%, 36.7%)',
-      },
-      'hljs-subst': {
-        color: 'hsl(153.4, 100%, 36.7%)',
+        color: '#98c379', // Strings
+        fontWeight: 'bold',
       },
       'hljs-title': {
-        color: 'hsl(153.4, 100%, 36.7%)',
-        fontWeight: 'normal',
-      },
-      'hljs-section': {
-        color: 'hsl(153.4, 100%, 36.7%)',
-        fontWeight: 'normal',
-      },
-      'hljs-emphasis': {
-        color: 'hsl(153.4, 100%, 36.7%)',
-      },
-      'hljs-type': {
-        color: 'hsl(153.4, 100%, 36.7%)',
-        fontWeight: 'normal',
-      },
-      'hljs-built_in': {
-        color: 'hsl(153.4, 100%, 36.7%)',
-      },
-      'hljs-builtin-name': {
-        color: 'hsl(153.4, 100%, 36.7%)',
-      },
-      'hljs-selector-attr': {
-        color: 'hsl(153.4, 100%, 36.7%)',
-      },
-      'hljs-selector-pseudo': {
-        color: 'hsl(153.4, 100%, 36.7%)',
-      },
-      'hljs-addition': {
-        color: 'hsl(153.4, 100%, 36.7%)',
+        color: '#e06c75', // Titles (like function names)
       },
       'hljs-variable': {
-        color: 'hsl(153.4, 100%, 36.7%)',
+        color: '#e06c75', // Variables
       },
-      'hljs-template-tag': {
-        color: 'hsl(153.4, 100%, 36.7%)',
+      'hljs-attribute': {
+        color: '#d19a66', // Attributes
       },
-      'hljs-template-variable': {
-        color: 'hsl(153.4, 100%, 36.7%)',
+      'hljs-number': {
+        color: '#d19a66', // Numbers
       },
       'hljs-comment': {
-        color: '#888',
+        color: '#5c6370', // Comments
       },
-      'hljs-quote': {
-        color: '#75715e',
+      'hljs-function': {
+        color: '#61afef', // Function names
       },
-      'hljs-deletion': {
-        color: '#75715e',
+      'hljs-params': {
+        color: '#abb2bf', // Parameters
+      },
+      'hljs-punctuation': {
+        color: '#abb2bf', // Punctuation
       },
       'hljs-meta': {
-        color: '#75715e',
+        color: '#abb2bf', // Meta
       },
-      'hljs-doctag': {
-        fontWeight: 'normal',
+      'hljs-literal': {
+        color: '#56b6c2', // Literal values
       },
-      'hljs-selector-id': {
-        fontWeight: 'normal',
+      'hljs-link': {
+        color: '#61afef', // Links
       },
     },
   },
@@ -253,115 +114,47 @@ export const themes = {
     CODE_LINE_NUMBER: '#5F7BF6',
     CODE_THEME: {
       hljs: {
-        display: 'block',
-        overflowX: 'auto',
-        color: '#ddd',
-      },
-      'hljs-tag': {
-        color: '#EDEDED',
+        color: '#fff', // General text color
       },
       'hljs-keyword': {
-        color: '#EDEDED',
-        fontWeight: 'normal',
-      },
-      'hljs-selector-tag': {
-        color: '#EDEDED',
-        fontWeight: 'normal',
-      },
-      'hljs-literal': {
-        color: '#EDEDED',
-        fontWeight: 'normal',
-      },
-      'hljs-strong': {
-        color: '#EDEDED',
-      },
-      'hljs-name': {
-        color: '#EDEDED',
-      },
-
-      'hljs-code': {
-        color: '#66d9ef',
-      },
-      'hljs-class .hljs-title': {
-        color: 'gray',
-      },
-      'hljs-attribute': {
-        color: '#bf79db',
-      },
-      'hljs-symbol': {
-        color: '#bf79db',
-      },
-      'hljs-regexp': {
-        color: '#bf79db',
-      },
-      'hljs-link': {
-        color: '#bf79db',
+        color: '#fff', // Keywords like "await"
       },
       'hljs-string': {
-        color: '#48FF1A',
-      },
-      'hljs-bullet': {
-        color: '#48FF1A',
-      },
-      'hljs-subst': {
-        color: '#48FF1A',
+        color: 'white', // Strings
+        fontWeight: 'bold',
       },
       'hljs-title': {
-        color: '#48FF1A',
-        fontWeight: 'normal',
-      },
-      'hljs-section': {
-        color: '#48FF1A',
-        fontWeight: 'normal',
-      },
-      'hljs-emphasis': {
-        color: '#48FF1A',
-      },
-      'hljs-type': {
-        color: '#48FF1A',
-        fontWeight: 'normal',
-      },
-      'hljs-built_in': {
-        color: '#48FF1A',
-      },
-      'hljs-builtin-name': {
-        color: '#48FF1A',
-      },
-      'hljs-selector-attr': {
-        color: '#48FF1A',
-      },
-      'hljs-selector-pseudo': {
-        color: '#48FF1A',
-      },
-      'hljs-addition': {
-        color: '#48FF1A',
+        color: '#e06c75', // Titles (like function names)
       },
       'hljs-variable': {
-        color: '#48FF1A',
+        color: '#e06c75', // Variables
       },
-      'hljs-template-tag': {
-        color: '#48FF1A',
+      'hljs-attribute': {
+        color: '#d19a66', // Attributes
       },
-      'hljs-template-variable': {
-        color: '#48FF1A',
+      'hljs-number': {
+        color: '#d19a66', // Numbers
       },
       'hljs-comment': {
-        color: '#5F7BF6',
+        color: '#5c6370', // Comments
       },
-      'hljs-quote': {
-        color: '#5F7BF6',
+      'hljs-function': {
+        color: '#61afef', // Function names
       },
-      'hljs-deletion': {
-        color: '#5F7BF6',
+      'hljs-params': {
+        color: '#abb2bf', // Parameters
+      },
+      'hljs-punctuation': {
+        color: '#abb2bf', // Punctuation
       },
       'hljs-meta': {
-        color: '#5F7BF6',
+        color: '#abb2bf', // Meta
       },
-      'hljs-doctag': {
-        fontWeight: 'normal',
+      'hljs-literal': {
+        color: '#56b6c2', // Literal values
       },
-      'hljs-selector-id': {
-        fontWeight: 'normal',
+      'hljs-link': {
+        color: '#61afef', // Links
       },
     },
   },
