84e7da7c385946724214b00c0cf1bfc4c6be595f
Ivan Vasilov <vasilov.ivan@gmail.com>
2024-08-01 11:29:36 +0200

    Make the text area look the same as input.

--- packages/ui/src/components/shadcn/ui/text-area.tsx
+++ packages/ui/src/components/shadcn/ui/text-area.tsx
@@ -4,16 +4,14 @@ import { cn } from '../../../lib/utils/cn'
 
 export interface TextAreaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
-const customClasses = ['bg-control']
-
 const TextArea = React.forwardRef<HTMLTextAreaElement, TextAreaProps>(
   ({ className, ...props }, ref) => {
     return (
       <textarea
         className={cn(
-          'flex min-h-10 w-full rounded-md border border-control bg-control px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-foreground-muted',
+          'flex min-h-10 w-full rounded-md border border-control bg-foreground/[.026] px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-foreground-muted',
           'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-background-control focus-visible:ring-offset-2 focus-visible:ring-offset-foreground-muted disabled:cursor-not-allowed disabled:opacity-50',
-          ...customClasses,
+          'aria-[invalid=true]:bg-destructive-200 aria-[invalid=true]:border-destructive-400 aria-[invalid=true]:focus:border-destructive aria-[invalid=true]:focus-visible:border-destructive',
           className
         )}
         ref={ref}
