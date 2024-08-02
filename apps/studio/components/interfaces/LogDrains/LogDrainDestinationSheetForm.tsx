459ff47a3a4e6ed0af84f0f2761370c4ffc37605
Jordi Enric <jordi.err@gmail.com>
2024-08-02 12:31:04 +0200

    fix log drain name exists on update issue

--- apps/studio/components/interfaces/LogDrains/LogDrainDestinationSheetForm.tsx
+++ apps/studio/components/interfaces/LogDrains/LogDrainDestinationSheetForm.tsx
@@ -211,7 +211,7 @@ export function LogDrainDestinationSheetForm({
                 // Temp check to make sure the name is unique
                 const logDrainName = form.getValues('name')
                 const logDrainExists = logDrains?.find((drain) => drain.name === logDrainName)
-                if (logDrainExists) {
+                if (logDrainExists && mode === 'create') {
                   toast.error('Log drain name already exists')
                   return
                 }
