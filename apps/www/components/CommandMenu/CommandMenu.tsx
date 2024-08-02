c47434f3bb584a1536050f5feb27844a9884a21a
Charis <26616127+charislam@users.noreply.github.com>
2024-08-02 15:52:24 -0400

    feat(www): new command menu (#28346)
    Replace www command menu with the new one.

---
+++ apps/www/components/CommandMenu/CommandMenu.tsx
@@ -0,0 +1,23 @@
+import { CommandHeader, CommandInput, CommandList, CommandMenu } from 'ui-patterns/CommandMenu'
+import { useChangelogCommand } from 'ui-patterns/CommandMenu/prepackaged/Changelog'
+import { useDocsAiCommands } from 'ui-patterns/CommandMenu/prepackaged/DocsAi'
+import { useDocsSearchCommands } from 'ui-patterns/CommandMenu/prepackaged/DocsSearch'
+import { useSupportCommands } from 'ui-patterns/CommandMenu/prepackaged/Support'
+import { useThemeSwitcherCommands } from 'ui-patterns/CommandMenu/prepackaged/ThemeSwitcher'
+
+export default function WwwCommandMenuEager() {
+  useDocsSearchCommands()
+  useDocsAiCommands()
+  useChangelogCommand()
+  useSupportCommands()
+  useThemeSwitcherCommands()
+
+  return (
+    <CommandMenu>
+      <CommandHeader>
+        <CommandInput />
+      </CommandHeader>
+      <CommandList />
+    </CommandMenu>
+  )
+}
