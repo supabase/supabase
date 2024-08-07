a3ecf36c655fed375ebf08b64bcab860d791caa6
Joshen Lim <joshenlimek@gmail.com>
2024-08-07 17:38:17 +0800

    Show correct number of private snippets for SQL folders (#28411)
    * Show correct number of private snippets for SQL folders
    
    * Ensure that private snippet count is updated when sharing / unsharing queries
    
    * Fix snippets caching issuie
    
    * Fix move query modal showing more than just the project's folders

--- apps/studio/data/content/keys.ts
+++ apps/studio/data/content/keys.ts
@@ -4,4 +4,6 @@ export const contentKeys = {
     ['projects', projectRef, 'content', 'folders', id].filter(Boolean),
   resource: (projectRef: string | undefined, id?: string) =>
     ['projects', projectRef, 'content', id] as const,
+  count: (projectRef: string | undefined, type?: string) =>
+    ['projects', projectRef, 'content', 'count', type].filter(Boolean),
 }
