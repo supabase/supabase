4c76fedc90e5a3e82e09786cd9fce3788193fd7f
dshukertjr <dshukertjr@gmail.com>
2024-08-09 10:24:56 +0900

    Revert "fix onAuthStateChange unsubscribe code"
    This reverts commit 8d438ae1456019147dbee3f8077c1838ad164c54.

--- examples/realtime/flutter-figma-clone/lib/main.dart
+++ examples/realtime/flutter-figma-clone/lib/main.dart
@@ -1,13 +1,12 @@
-import 'package:canvas/auth/login_page.dart';
-import 'package:canvas/project/projects_page.dart';
+import 'package:canvas/canvas/canvas_page.dart';
 import 'package:flutter/material.dart';
-import 'package:supabase_auth_ui/supabase_auth_ui.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   Supabase.initialize(
-    url: 'https://akcrpifcnvufkecfmxqz.supabase.co',
+    url: 'http://127.0.0.1:54321',
     anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrY3JwaWZjbnZ1ZmtlY2ZteHF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY2MTkxMjIsImV4cCI6MjAyMjE5NTEyMn0.nxkhanVHQ7OJ2bwGU1sgrBb7x3JZiGidlAxnFyiv6Jo',
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0',
   );
   runApp(const MyApp());
 }
@@ -19,12 +18,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Figma Clone',
+    return const MaterialApp(
+      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      home: supabase.auth.currentSession == null
-          ? const LoginPage()
-          : const ProjectsPage(),
+      home: CanvasPage(),
     );
   }
 }
