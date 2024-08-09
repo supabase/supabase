c4e2d1f4b9e579db429f2880bc1de8e04033640b
Stojan Dimitrovski <sdimitrovski@gmail.com>
2024-08-09 16:11:44 +0200

    feat: put firebase auth tpa behind feature flag

--- apps/studio/components/interfaces/Auth/ThirdPartyAuthForm/CreateFirebaseAuthDialog.tsx
+++ apps/studio/components/interfaces/Auth/ThirdPartyAuthForm/CreateFirebaseAuthDialog.tsx
@@ -25,6 +25,7 @@ import {
   Separator,
 } from 'ui'
 import { FormItemLayout } from 'ui-patterns/form/FormItemLayout/FormItemLayout'
+import { useFlag } from 'hooks/ui/useFlag'
 
 interface CreateFirebaseAuthIntegrationProps {
   visible: boolean
@@ -68,6 +69,13 @@ export const CreateFirebaseAuthIntegrationDialog = ({
     },
   })
 
+  const allowedProjectIDs = useFlag<string | null | undefined>('firebaseTPAAllowedProjectIDs')
+
+  const isAvailable =
+    typeof allowedProjectIDs === 'string'
+      ? allowedProjectIDs.split(',').includes(projectRef)
+      : !!allowedProjectIDs
+
   useEffect(() => {
     if (visible) {
       form.reset({
@@ -101,10 +109,25 @@ export const CreateFirebaseAuthIntegrationDialog = ({
 
         <Separator />
         <DialogSection>
-          <Form_Shadcn_ {...form}>
-            <form id={FORM_ID} onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-              {/* Enabled flag can't be changed for now because there's no update API call for integrations */}
-              {/* <FormField_Shadcn_
+          {!isAvailable && (
+            <p className="text-sm text-foreground-light pb-2">
+              Third-party Auth with Firebase is currently in a private alpha release.{' '}
+              <a
+                className="underline"
+                href="https://forms.supabase.com/third-party-auth-with-firebase"
+                target="_blank"
+              >
+                Please submit your interest for it
+              </a>{' '}
+              and someone from the Supabase team will reach out. You can also try it out via the
+              CLI.
+            </p>
+          )}
+          {isAvailable && (
+            <Form_Shadcn_ {...form}>
+              <form id={FORM_ID} onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                {/* Enabled flag can't be changed for now because there's no update API call for integrations */}
+                {/* <FormField_Shadcn_
               key="enabled"
               control={form.control}
               name="enabled"
@@ -126,52 +149,72 @@ export const CreateFirebaseAuthIntegrationDialog = ({
             />
             <Separator /> */}
 
-              <p className="text-sm text-foreground-light">
-                This will enable a JWT token from a specific Firebase project to access data from
-                this Supabase project.
-              </p>
-              <FormField_Shadcn_
-                key="firebaseProjectId"
-                control={form.control}
-                name="firebaseProjectId"
-                render={({ field }) => (
-                  <FormItemLayout label="Firebase Auth Project ID">
-                    <FormControl_Shadcn_>
-                      <Input_Shadcn_ {...field} />
-                    </FormControl_Shadcn_>
-                  </FormItemLayout>
-                )}
-              />
+                <p className="text-sm text-foreground-light">
+                  This will enable a JWT token from a specific Firebase project to access data from
+                  this Supabase project.
+                </p>
+                <FormField_Shadcn_
+                  key="firebaseProjectId"
+                  control={form.control}
+                  name="firebaseProjectId"
+                  render={({ field }) => (
+                    <FormItemLayout label="Firebase Auth Project ID">
+                      <FormControl_Shadcn_>
+                        <Input_Shadcn_ {...field} />
+                      </FormControl_Shadcn_>
+                    </FormItemLayout>
+                  )}
+                />
 
-              <Alert_Shadcn_ variant="warning">
-                <AlertTriangle strokeWidth={2} />
-                <AlertTitle_Shadcn_>
-                  This connection requires a Row Level Security (RLS) policy
-                </AlertTitle_Shadcn_>
-                <AlertDescription_Shadcn_>
-                  You will need to manually add a RLS policy after creating this Firebase
-                  connection. Otherwise, the Supabase project will be accessible to anyone with a
-                  valid Firebase JWT token.
-                </AlertDescription_Shadcn_>
-              </Alert_Shadcn_>
-            </form>
-          </Form_Shadcn_>
+                <Alert_Shadcn_ variant="warning">
+                  <AlertTriangle strokeWidth={2} />
+                  <AlertTitle_Shadcn_>
+                    This connection requires a Row Level Security (RLS) policy
+                  </AlertTitle_Shadcn_>
+                  <AlertDescription_Shadcn_>
+                    You will need to manually add a RLS policy after creating this Firebase
+                    connection. Otherwise, the Supabase project will be accessible to anyone with a
+                    valid Firebase JWT token.
+                  </AlertDescription_Shadcn_>
+                </Alert_Shadcn_>
+              </form>
+            </Form_Shadcn_>
+          )}
         </DialogSection>
         <DialogFooter>
-          {!isCreating && (
-            <div className="flex-1">
-              <Button type="danger" onClick={() => onDelete()} icon={<Trash />}>
-                Remove connection
+          {!isAvailable && (
+            <>
+              <Button disabled={isLoading} type="default" onClick={() => onClose()}>
+                Cancel
               </Button>
-            </div>
+              <Button
+                htmlType="submit"
+                onClick={() => {
+                  window.location.href = 'https://forms.supabase.com/third-party-auth-with-firebase'
+                }}
+              >
+                Register interest
+              </Button>
+            </>
           )}
+          {isAvailable && (
+            <>
+              {!isCreating && (
+                <div className="flex-1">
+                  <Button type="danger" onClick={() => onDelete()} icon={<Trash />}>
+                    Remove connection
+                  </Button>
+                </div>
+              )}
 
-          <Button disabled={isLoading} type="default" onClick={() => onClose()}>
-            Cancel
-          </Button>
-          <Button form={FORM_ID} htmlType="submit" disabled={isLoading} loading={isLoading}>
-            {isCreating ? 'Create connection' : 'Update connection'}
-          </Button>
+              <Button disabled={isLoading} type="default" onClick={() => onClose()}>
+                Cancel
+              </Button>
+              <Button form={FORM_ID} htmlType="submit" disabled={isLoading} loading={isLoading}>
+                {isCreating ? 'Create connection' : 'Update connection'}
+              </Button>
+            </>
+          )}
         </DialogFooter>
       </DialogContent>
     </Dialog>
