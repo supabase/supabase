c3397dc93ab181d1c304dea40667e9be8da54ba8
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2024-08-12 16:08:26 +0200

    whale 1 edit (#28556)
    fix build item

--- packages/ui/src/layout/banners/LW12CountdownBanner/LW12CountdownBanner.tsx
+++ packages/ui/src/layout/banners/LW12CountdownBanner/LW12CountdownBanner.tsx
@@ -5,7 +5,7 @@ import { usePathname } from 'next/navigation'
 import { Button } from '../../../components/Button/Button'
 import { cn } from '../../../lib/utils/cn'
 import announcement from '../data/Announcement.json'
-import Countdown from './Countdown'
+// import Countdown from './Countdown'
 
 export function LW12CountdownBanner() {
   const pathname = usePathname()
@@ -28,7 +28,7 @@ export function LW12CountdownBanner() {
         >
           <p className="flex gap-1.5 items-center">{announcement.text}</p>
           <Button size="tiny" type="default" className="px-2 !leading-none text-xs" asChild>
-            <Link href="https://supabase.com/launch-week">{announcement.cta}</Link>
+            <Link href="https://supabase.com/launch-week#day-1">{announcement.cta}</Link>
           </Button>
         </div>
       </div>
