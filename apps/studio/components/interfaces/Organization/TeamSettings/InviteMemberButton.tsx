5c784296ee655b279bfc5363132ddabbac935f43
Joshen Lim <joshenlimek@gmail.com>
2024-08-12 21:46:53 +0800

    Fix

--- apps/studio/components/interfaces/Organization/TeamSettings/InviteMemberButton.tsx
+++ apps/studio/components/interfaces/Organization/TeamSettings/InviteMemberButton.tsx
@@ -87,8 +87,8 @@ export const InviteMemberButton = () => {
     { enabled: canReadSubscriptions }
   )
   const currentPlan = subscription?.plan
-  const isOptedIntoProjectLevelPermissions = true
-  // useIsOptedIntoProjectLevelPermissions(slug as string) && currentPlan?.id === 'enterprise'
+  const isOptedIntoProjectLevelPermissions =
+    useIsOptedIntoProjectLevelPermissions(slug as string) && currentPlan?.id === 'enterprise'
 
   const userMemberData = members?.find((m) => m.gotrue_id === profile?.gotrue_id)
   const hasOrgRole =
