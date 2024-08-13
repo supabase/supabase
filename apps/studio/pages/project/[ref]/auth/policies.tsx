058698e6e89dd7d78e2b4c145f97bb866cc76f48
Joshen Lim <joshenlimek@gmail.com>
2024-08-13 14:04:01 +0800

    Chore/auth policies page fixes (#28579)
    * Use ButtonTooltip component for create policy CTAs
    
    * Use ButtonTooltip component in PolicyRow
    
    * Remove use of old tooltip component in PolicyRow
    
    * Update Auth policies to allow read only to access edit policy UI but prevent saving from there
    
    * Fix

--- apps/studio/pages/project/[ref]/auth/policies.tsx
+++ apps/studio/pages/project/[ref]/auth/policies.tsx
@@ -10,6 +10,7 @@ import Policies from 'components/interfaces/Auth/Policies/Policies'
 import AuthLayout from 'components/layouts/AuthLayout/AuthLayout'
 import { useProjectContext } from 'components/layouts/ProjectLayout/ProjectContext'
 import AlertError from 'components/ui/AlertError'
+import { ButtonTooltip } from 'components/ui/ButtonTooltip'
 import NoPermission from 'components/ui/NoPermission'
 import SchemaSelector from 'components/ui/SchemaSelector'
 import { GenericSkeletonLoader } from 'components/ui/ShimmeringLoader'
@@ -20,7 +21,7 @@ import { useCheckPermissions, usePermissionsLoaded } from 'hooks/misc/useCheckPe
 import { useUrlState } from 'hooks/ui/useUrlState'
 import { EXCLUDED_SCHEMAS } from 'lib/constants/schemas'
 import type { NextPageWithLayout } from 'types'
-import { Button, Input, TooltipContent_Shadcn_, TooltipTrigger_Shadcn_, Tooltip_Shadcn_ } from 'ui'
+import { Button, Input } from 'ui'
 
 /**
  * Filter tables by table name and policy name
@@ -146,26 +147,23 @@ const AuthPoliciesPage: NextPageWithLayout = () => {
             </Button>
 
             {isAiAssistantEnabled && (
-              <Tooltip_Shadcn_>
-                <TooltipTrigger_Shadcn_ asChild>
-                  <Button
-                    type="primary"
-                    disabled={!canCreatePolicies || schemaHasNoTables}
-                    onClick={() => setShowPolicyAiEditor(true)}
-                  >
-                    Create a new policy
-                  </Button>
-                </TooltipTrigger_Shadcn_>
-                {(!canCreatePolicies || schemaHasNoTables) && (
-                  <TooltipContent_Shadcn_ side="bottom">
-                    {!canCreatePolicies
+              <ButtonTooltip
+                type="primary"
+                disabled={!canCreatePolicies || schemaHasNoTables}
+                onClick={() => setShowPolicyAiEditor(true)}
+                tooltip={{
+                  content: {
+                    side: 'bottom',
+                    text: !canCreatePolicies
                       ? 'You need additional permissions to create RLS policies'
                       : schemaHasNoTables
                         ? `No table in schema ${schema} to create policies on`
-                        : null}
-                  </TooltipContent_Shadcn_>
-                )}
-              </Tooltip_Shadcn_>
+                        : undefined,
+                  },
+                }}
+              >
+                Create a new policy
+              </ButtonTooltip>
             )}
           </div>
         </div>
