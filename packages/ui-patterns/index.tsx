097e26813045a8f82e91e090102714eb6770e3d4
Charis <26616127+charislam@users.noreply.github.com>
2024-08-13 10:23:11 -0400

    clean: remove old command menu code (#28491)

--- packages/ui-patterns/index.tsx
+++ packages/ui-patterns/index.tsx
@@ -2,6 +2,7 @@
  * The components are listed here so that VsCode can find out about them and list them as import suggestions. Don't
  * import directly from here.
  */
+export * from './CommandMenu'
 export * from './ConsentToast'
 export * from './CountdownWidget'
 export * from './ExpandableVideo'
