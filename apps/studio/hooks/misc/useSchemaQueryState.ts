201f10018133f977940878ea9f3e3daff39b9218
Ivan Vasilov <vasilov.ivan@gmail.com>
2024-08-15 14:42:05 +0200

    Make the useQueryState work with SSR in playwright.

--- apps/studio/hooks/misc/useSchemaQueryState.ts
+++ apps/studio/hooks/misc/useSchemaQueryState.ts
@@ -5,15 +5,27 @@ import { LOCAL_STORAGE_KEYS } from 'lib/constants'
 import { useCallback } from 'react'
 import { useLocalStorage } from './useLocalStorage'
 
+/**
+ * This hook wraps useQueryState because useQueryState imports app router for some reason which breaks the SSR in
+ * the playwright tests. I've localized the issue to "NODE_ENV='test'" in the playwright tests.
+ */
+const useIsomorphicUseQueryState = (defaultSchema: string) => {
+  if (typeof window === 'undefined') {
+    return [defaultSchema, () => {}] as const
+  } else {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    return useQueryState('schema', parseAsString.withDefault(defaultSchema))
+  }
+}
+
 export const useQuerySchemaState = () => {
   const { ref } = useParams()
-  console.log(ref)
   const [defaultSchema, setDefaultSchema] = useLocalStorage(
     LOCAL_STORAGE_KEYS.LAST_SELECTED_SCHEMA(ref ?? ''),
     'public'
   )
 
-  const [schema, setSchema] = useQueryState('schema', parseAsString.withDefault(defaultSchema))
+  const [schema, setSchema] = useIsomorphicUseQueryState(defaultSchema)
 
   const setSelectedSchema = useCallback(
     (schema: string) => {
