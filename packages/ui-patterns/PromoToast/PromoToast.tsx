2b378b10c95b81d1c243cce8421a8565ae83c73c
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2024-08-16 15:30:47 +0200

    d5 follow up (#28696)
    promo toast

--- packages/ui-patterns/PromoToast/PromoToast.tsx
+++ packages/ui-patterns/PromoToast/PromoToast.tsx
@@ -45,10 +45,10 @@ const PromoToast = () => {
       )}
     >
       <p className="relative z-10 text-foreground flex flex-col text-xl w-full leading-7">
-        Launch Week 12: Day 4
+        Launch Week 12: Day 5
       </p>
       <div className="relative z-10 text-foreground-lighter flex flex-col text-sm w-full mb-2">
-        Introducing Log Drains
+        Postgres Foreign Data Wrappers with Wasm
         {/* <CountdownComponent date={new Date(announcement.launchDate)} showCard={false} /> */}
       </div>
 
