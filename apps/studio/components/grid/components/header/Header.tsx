bbaa131e4c89604b022953dd1e1e95491f0a6e74
Jordi Enric <jordi.err@gmail.com>
2024-08-28 12:03:31 +0200

    improve table editor headers

--- apps/studio/components/grid/components/header/Header.tsx
+++ apps/studio/components/grid/components/header/Header.tsx
@@ -64,7 +64,7 @@ const Header = ({
 
   return (
     <div>
-      <div className="flex h-10 items-center justify-between bg-dash-sidebar px-5 py-1.5">
+      <div className="flex h-10 items-center justify-between bg-dash-sidebar px-5 py-1.5 gap-2 overflow-x-auto">
         {customHeader ? (
           <>{customHeader}</>
         ) : (
