525141496f5e9e3fc729ee3daafce555fe77d6ef
Charis Lam <26616127+charislam@users.noreply.github.com>
2024-09-04 08:27:53 -0400

    fix,feat(sitemap): add all pages + priority
    Pages were missing from the sitemap after the reorganization. Added them
    back in.
    
    Also added an optional priority that can be specified by including
    `sitemapPriority` in content metadata. Used on the deprecated Auth
    Helpers pages to see if we can tweak the relative SEO ranking for them.

--- apps/docs/internals/helpers.ts
+++ apps/docs/internals/helpers.ts
@@ -1,6 +1,6 @@
 export function flattenSections(sections) {
-  var a = []
-  for (var i = 0; i < sections.length; i++) {
+  let a = []
+  for (let i = 0; i < sections.length; i++) {
     if (sections[i].id) {
       // only push a section that has an id
       // these are reserved for sidebar subtitles
