e441fb98da8a4bf08e50484db860244795960cc9
Charis Lam <26616127+charislam@users.noreply.github.com>
2024-09-05 17:10:48 -0400

    feat(docs): global troubleshooting page layout

--- apps/docs/app/guides/troubleshooting/page.tsx
+++ apps/docs/app/guides/troubleshooting/page.tsx
@@ -1,14 +1,15 @@
-import { TroubleshootingEntry } from '~/features/docs/Troubleshooting.ui'
+import { TroubleshootingPreview } from '~/features/docs/Troubleshooting.ui'
 import { getAllTroubleshootingEntries } from '~/features/docs/Troubleshooting.utils'
+import { LayoutMainContent } from '~/layouts/DefaultLayout'
 
 export default async function GlobalTroubleshootingPage() {
   const troubleshootingEntries = await getAllTroubleshootingEntries()
 
   return (
-    <>
+    <LayoutMainContent className="w-full max-w-full">
       {troubleshootingEntries.map((entry) => (
-        <TroubleshootingEntry key={entry.data.database_id} entry={entry} />
+        <TroubleshootingPreview key={entry.data.database_id} entry={entry} />
       ))}
-    </>
+    </LayoutMainContent>
   )
 }
--- apps/docs/features/docs/Troubleshooting.ui.tsx
+++ apps/docs/features/docs/Troubleshooting.ui.tsx
@@ -1,9 +1,26 @@
 import { getAllTroubleshootingEntries } from './Troubleshooting.utils'
+import { MDXRemoteBase } from './MdxBase'
+import { ErrorBoundary } from 'react-error-boundary'
 
-export function TroubleshootingEntry({
+export function TroubleshootingPreview({
   entry,
 }: {
   entry: Awaited<ReturnType<typeof getAllTroubleshootingEntries>>[number]
 }) {
-  return <article>{entry.data.title}</article>
+  return (
+    <article
+      className="prose border rounded-lg"
+      style={{ '--local-padding': '1rem' } as React.CSSProperties}
+    >
+      <h2 className="m-0 p-[var(--local-padding)]">{entry.data.title}</h2>
+      <hr className="m-0" />
+      <ErrorBoundary fallback={null}>
+        <div className="p-[var(--local-padding)]">
+          <div className="rounded-lg border px-4">
+            <MDXRemoteBase source={entry.content.slice(0, 150)} />
+          </div>
+        </div>
+      </ErrorBoundary>
+    </article>
+  )
 }
