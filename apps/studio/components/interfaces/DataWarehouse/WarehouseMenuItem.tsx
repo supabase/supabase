6e6910f82ba2b9d83084720cde39e869dfd26573
Jordi Enric <37541088+jordienr@users.noreply.github.com>
2024-09-09 17:16:12 +0200

    warehouse ui/ux fixes (#29057)
    * fix metadata not showing in Raw tab in log detail
    
    * fix rows not filling up horizontal space
    
    * show collection loader only when fetching collections, since the query can be disabled
    
    * simplify language in create collection modal
    
    * fix create collection modal copy in cta
    
    * rm typecast, add tooltip on collection creation
    
    * improve copy on empty collection
    
    * improve copy on colelction empty state
    
    * rm unused code
    
    * prompt users to refresh, add button to refresh, after sending first events to a collection
    
    * use generic mr skelington instead of supalogo
    
    * add template for search in event message
    
    * hide field reference when source is warehouse
    
    * rm font bold on selected log
    
    * update curl example
    
    * add focusable prop to codeblock
    
    * change focus border color in codeblock
    
    * make codeblock focusable by default
    
    * move refresh btn
    
    * rm unnecessary focusable i codeblcok
    
    * fix raw log rendering
    
    * add search
    
    * update plceholder
    
    * add timerange search
    
    * improve empty state copy in wh
    
    * add open in warehouse explorer button
    
    * update test conection dialog ui
    
    * Update apps/studio/components/interfaces/DataWarehouse/TestCollectionDialog.tsx
    
    Co-authored-by: Joshen Lim <joshenlimek@gmail.com>
    
    * Update apps/studio/components/interfaces/DataWarehouse/WarehouseCollectionDetail.tsx
    
    Co-authored-by: Joshen Lim <joshenlimek@gmail.com>
    
    * Update apps/studio/components/interfaces/DataWarehouse/WarehouseCollectionDetail.tsx
    
    Co-authored-by: Joshen Lim <joshenlimek@gmail.com>
    
    * rm clog
    
    * Update apps/studio/components/interfaces/Settings/Logs/LogsQueryPanel.tsx
    
    Co-authored-by: Joshen Lim <joshenlimek@gmail.com>
    
    * fix loading state in collections sidebar
    
    * fix test collection sheet layout
    
    * add some keyboard shortcuts to filters
    
    * use isLoading in wh log detail to prevent double loaders
    
    * make search input longr
    
    * truncate titles
    
    * truncat sidebar names
    
    * rename dayjs
    
    * update copy from warehouse to collections
    
    * update remove collection copy
    
    ---------
    
    Co-authored-by: Joshen Lim <joshenlimek@gmail.com>

--- apps/studio/components/interfaces/DataWarehouse/WarehouseMenuItem.tsx
+++ apps/studio/components/interfaces/DataWarehouse/WarehouseMenuItem.tsx
@@ -94,7 +94,7 @@ export const WarehouseMenuItem = ({ item }: Props) => {
         key={item.token + '-collection-item'}
         href={`/project/${projectRef}/logs/collections/${item.token}`}
       >
-        <div>{item.name}</div>
+        <div className="truncate">{item.name}</div>
 
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
@@ -221,8 +221,9 @@ export const WarehouseMenuItem = ({ item }: Props) => {
           >
             <div className="p-3 px-6 space-y-4">
               <p className="text-sm text-foreground-light">
-                Are you sure you want to delete the selected collection?
-                <br /> This action cannot be undone.
+                All data in this collection will be deleted.
+                <br />
+                <div className="mt-2">This action cannot be undone.</div>
               </p>
             </div>
 
