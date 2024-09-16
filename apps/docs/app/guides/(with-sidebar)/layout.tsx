7350044e622ea952adaba6bf1b4a2a675fcdddd1
Charis Lam <26616127+charislam@users.noreply.github.com>
2024-09-16 17:44:58 -0400

    wip

--- apps/docs/app/guides/(with-sidebar)/layout.tsx
+++ apps/docs/app/guides/(with-sidebar)/layout.tsx
@@ -1,16 +1,29 @@
-'use client'
-
-import { usePathname } from 'next/navigation'
 import { type PropsWithChildren } from 'react'
 
-import { getMenuId } from '~/components/Navigation/NavigationMenu/NavigationMenu.utils'
+import { supabaseMisc } from '~/lib/supabaseMisc'
 import Layout from '~/layouts/guides'
 
-const GuidesLayout = ({ children }: PropsWithChildren) => {
-  const pathname = usePathname()
-  const menuId = getMenuId(pathname)
+const GuidesLayout = async ({ children }: PropsWithChildren) => {
+  const partners = await getPartners()
+  const partnerNavItems = partners.map((partner) => ({
+    name: partner.title,
+    url: `https://supabase.com/partners/integrations/${partner.slug}` as `https://${string}`,
+  }))
 
-  return <Layout menuId={menuId}>{children}</Layout>
+  return <Layout additionalNavItems={partnerNavItems}>{children}</Layout>
+}
+
+async function getPartners() {
+  const { data, error } = await supabaseMisc()
+    .from('partners')
+    .select('slug, title')
+    .eq('type', 'technology')
+    .order('title')
+  if (error) {
+    console.error(new Error('Error fetching partners', { cause: error }))
+  }
+
+  return data ?? []
 }
 
 export default GuidesLayout
