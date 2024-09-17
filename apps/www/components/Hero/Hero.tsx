e76aa11508da0c2decb93e6a9bb4fa2b51393ee0
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2024-09-17 11:07:38 +0200

    homepage cleanup (#29295)
    * new homepage logos marquee
    
    * add products claim
    
    * logos spacing
    
    * dashboard table editor demo
    
    * dashboard table editor demo
    
    * dashboard recordings
    
    * rls policies
    
    * update hp frameworks
    
    * add dataAPIs to homepage bento
    
    * tweet section update
    
    * padding
    
    * hp products update
    
    * new customer stories hp section
    
    * height
    
    * finish homepage details
    
    * update logos
    
    * fix database visual
    
    * improve functions visual ux
    
    * animate data-apis visual
    
    * realtime visual update
    
    * reduce-motion and update chatbase logo
    
    * lighter heading paragraph
    
    * improve video tabs components
    
    * add priority to offsetted images
    
    * video plays inline
    
    * fix mobile tweets spacing
    
    * smoll
    
    * load video slightly before the component enters the viewport
    
    * fix product pointer-events
    
    * theme based and light weight dashboard video
    
    * testimonials margin
    
    * remove yellow tint from light mode recordings
    
    * fix vitest window-matchMedia error
    
    * fix vitest window-matchMedia error
    
    * fix url type
    
    * remove tweet

--- apps/www/components/Hero/Hero.tsx
+++ apps/www/components/Hero/Hero.tsx
@@ -23,12 +23,8 @@ const Hero = () => {
               <div className="relative z-10 lg:h-auto pt-[90px] lg:pt-[90px] lg:min-h-[300px] flex flex-col items-center justify-center sm:mx-auto md:w-3/4 lg:mx-0 lg:w-full gap-4 lg:gap-8">
                 <div className="flex flex-col items-center">
                   <h1 className="text-foreground text-4xl sm:text-5xl sm:leading-none lg:text-7xl">
-                    <span className="block text-[#F4FFFA00] bg-clip-text bg-gradient-to-b from-foreground to-foreground-light">
-                      Build in a weekend
-                    </span>
-                    <span className="text-transparent bg-clip-text bg-gradient-to-br from-[#3ECF8E] via-[#3ECF8E] to-[#3ecfb2] block md:ml-0">
-                      Scale to billions
-                    </span>
+                    <span className="block text-foreground">Build in a weekend</span>
+                    <span className="text-brand block md:ml-0">Scale to billions</span>
                   </h1>
                   <p className="pt-2 text-foreground my-3 text-sm sm:mt-5 lg:mb-0 sm:text-base lg:text-lg">
                     Supabase is an open source Firebase alternative.{' '}
@@ -38,7 +34,7 @@ const Hero = () => {
                   </p>
                 </div>
                 <div className="flex items-center gap-2">
-                  <Button asChild size="medium" className="text-white">
+                  <Button asChild size="medium">
                     <Link
                       href="https://supabase.com/dashboard"
                       as="https://supabase.com/dashboard"
--- apps/www/data/Solutions.tsx
+++ apps/www/data/Solutions.tsx
@@ -1,12 +1,27 @@
 import { products } from 'shared-data'
 import { PRODUCT_NAMES, PRODUCT_SHORTNAMES } from 'shared-data/products'
 
-const solutions = {
+export type SolutionsType = {
+  [key: string]: {
+    name: string
+    icon: string
+    description: string | JSX.Element
+    description_short: string
+    label: string
+    url: string
+  }
+}
+
+const solutions: SolutionsType = {
   [PRODUCT_SHORTNAMES.DATABASE]: {
     name: PRODUCT_NAMES.DATABASE,
     icon: products.database.icon[24],
-    description:
-      "Every project is a full Postgres database, the world's most trusted relational database.",
+    description: (
+      <>
+        Every project is <strong>a full Postgres database</strong>, the world's most trusted
+        relational database.
+      </>
+    ),
     description_short: 'Fully portable Postgres database',
     label: '',
     url: '/database',
@@ -14,7 +29,11 @@ const solutions = {
   [PRODUCT_SHORTNAMES.AUTHENTICATION]: {
     name: PRODUCT_NAMES.AUTHENTICATION,
     icon: products.authentication.icon[24],
-    description: 'Add user sign ups and logins, securing your data with Row Level Security.',
+    description: (
+      <>
+        <strong>Add user sign ups and logins</strong>, securing your data with Row Level Security.
+      </>
+    ),
     description_short: 'User Management out of the box',
     label: '',
     url: '/auth',
@@ -22,7 +41,11 @@ const solutions = {
   [PRODUCT_SHORTNAMES.STORAGE]: {
     name: PRODUCT_NAMES.STORAGE,
     icon: products.storage.icon[24],
-    description: 'Store, organize, and serve large files. Any media, including videos and images.',
+    description: (
+      <>
+        <strong>Store, organize, and serve</strong> large files, from videos to images.
+      </>
+    ),
     description_short: 'Serverless storage for any media',
     label: '',
     url: '/storage',
@@ -30,7 +53,11 @@ const solutions = {
   [PRODUCT_SHORTNAMES.FUNCTIONS]: {
     name: PRODUCT_NAMES.FUNCTIONS,
     icon: products.functions.icon[24],
-    description: 'Write custom code without deploying or scaling servers.',
+    description: (
+      <>
+        Easily write custom code <strong>without deploying or scaling servers.</strong>
+      </>
+    ),
     description_short: 'Deploy code globally on the edge',
     label: '',
     url: '/edge-functions',
@@ -38,8 +65,11 @@ const solutions = {
   [PRODUCT_SHORTNAMES.REALTIME]: {
     name: PRODUCT_NAMES.REALTIME,
     icon: products.realtime.icon[24],
-    description:
-      'Create multiplayer experiences by sharing, broadcasting, and listening to changes from other clients or the Database.',
+    description: (
+      <>
+        <strong>Build multiplayer experiences</strong> with real-time data synchronization.
+      </>
+    ),
     description_short: 'Synchronize and broadcast events',
     label: '',
     url: '/realtime',
@@ -47,7 +77,12 @@ const solutions = {
   [PRODUCT_SHORTNAMES.VECTOR]: {
     name: PRODUCT_NAMES.VECTOR,
     icon: products.vector.icon[24],
-    description: 'Integrate your favorite ML-models to store, index and search vector embeddings.',
+    description: (
+      <>
+        Integrate your favorite ML-models to{' '}
+        <strong>store, index and search vector embeddings</strong>.
+      </>
+    ),
     description_short: 'AI toolkit to manage embeddings',
     label: '',
     url: '/vector',
