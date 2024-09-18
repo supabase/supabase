e0793bc38898aeafaab850c9486978b8317c37ee
Joshen Lim <joshenlimek@gmail.com>
2024-09-18 14:27:39 +0800

    Attempt to fix tests

---
+++ apps/studio/tests/mocks/browser.ts
@@ -0,0 +1,13 @@
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+})
