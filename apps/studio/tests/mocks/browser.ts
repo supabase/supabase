58525c5309c3921409896fa6b622a85c219439a7
Joshen Lim <joshenlimek@gmail.com>
2024-09-18 14:52:58 +0800

    Attempt to fix tests

--- apps/studio/tests/mocks/browser.ts
+++ apps/studio/tests/mocks/browser.ts
@@ -1,13 +1,15 @@
+import { vi } from 'vitest'
+
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
-  value: jest.fn().mockImplementation((query) => ({
+  value: vi.fn().mockImplementation((query) => ({
     matches: false,
     media: query,
     onchange: null,
-    addListener: jest.fn(), // Deprecated
-    removeListener: jest.fn(), // Deprecated
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
+    addListener: vi.fn(),
+    removeListener: vi.fn(),
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
   })),
 })
