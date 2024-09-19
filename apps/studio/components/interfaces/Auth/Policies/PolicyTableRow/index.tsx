d4ea34bb8e7c9a53f5f661a508bf2a9b4f9a5ac4
Terry Sutton <saltcod@gmail.com>
2024-09-19 16:55:59 -0230

    POC for revised RLS page

--- apps/studio/components/interfaces/Auth/Policies/PolicyTableRow/index.tsx
+++ apps/studio/components/interfaces/Auth/Policies/PolicyTableRow/index.tsx
@@ -1,11 +1,16 @@
 import type { PostgresPolicy, PostgresTable } from '@supabase/postgres-meta'
 import { noop } from 'lodash'
-import { AlertDescription_Shadcn_, AlertTitle_Shadcn_, Alert_Shadcn_ } from 'ui'
 
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@ui/components/shadcn/ui/tooltip'
 import { useProjectContext } from 'components/layouts/ProjectLayout/ProjectContext'
-import { WarningIcon } from 'ui'
 import Panel from 'components/ui/Panel'
 import { useDatabasePoliciesQuery } from 'data/database-policies/database-policies-query'
+import { Info } from 'lucide-react'
 import PolicyRow from './PolicyRow'
 import PolicyTableRowHeader from './PolicyTableRowHeader'
 
@@ -47,35 +52,9 @@ const PolicyTableRow = ({
         />
       }
     >
-      {(!table.rls_enabled || policies.length === 0) && (
+      {policies.length === 0 && (
         <div className="px-6 py-4 flex flex-col gap-y-3">
-          {table.rls_enabled && policies.length === 0 && (
-            <Alert_Shadcn_>
-              <WarningIcon />
-              <AlertTitle_Shadcn_>
-                Row Level Security is enabled for this table, but no policies are set
-              </AlertTitle_Shadcn_>
-              <AlertDescription_Shadcn_>
-                Select queries may return 0 results.
-              </AlertDescription_Shadcn_>
-            </Alert_Shadcn_>
-          )}
-          {!table.rls_enabled && !isLocked && (
-            <Alert_Shadcn_ variant="warning">
-              <WarningIcon />
-              <AlertTitle_Shadcn_>
-                Warning: Row Level Security is disabled. Your table is publicly readable and
-                writable.
-              </AlertTitle_Shadcn_>
-              <AlertDescription_Shadcn_>
-                Anyone with the project's anonymous key can modify or delete your data. Enable RLS
-                and create access policies to keep your data secure.
-              </AlertDescription_Shadcn_>
-            </Alert_Shadcn_>
-          )}
-          {policies.length === 0 && (
-            <p className="text-foreground-light text-sm">No policies created yet</p>
-          )}
+          <p className="text-foreground-light text-sm">No policies created yet</p>
         </div>
       )}
 
@@ -87,6 +66,26 @@ const PolicyTableRow = ({
           onSelectDeletePolicy={onSelectDeletePolicy}
         />
       ))}
+      {!table.rls_enabled && !isLocked && (
+        <div className="bg-surface-200 px-6 py-2 text-xs flex items-center gap-2">
+          <div className="w-1.5 h-1.5 bg-warning-600 rounded-full" />
+          <span className="font-bold text-warning-600">Warning:</span>{' '}
+          <span className="text-foreground-light">
+            Row Level Security is disabled. Your table is publicly readable and writable.
+          </span>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <Info className="w-3 h-3 " />
+              </TooltipTrigger>
+              <TooltipContent className="w-[400px]">
+                Anyone with the project's anonymous key can modify or delete your data. Enable RLS
+                and create access policies to keep your data secure.
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
+      )}
     </Panel>
   )
 }
