39ed470f0066157b3abf2a3a2306a1c452e18885
Jordi Enric <jordi.err@gmail.com>
2024-09-26 17:26:14 +0200

    add use client

--- packages/ui-patterns/TimestampInfo/TimestampInfo.tsx
+++ packages/ui-patterns/TimestampInfo/TimestampInfo.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { useEffect, useRef, useState } from 'react'
 import { Tooltip, TooltipContent, TooltipTrigger } from 'ui/src/components/shadcn/ui/tooltip'
 import { cn } from 'ui/src/lib/utils'
