fe1ba5d134af6bfd5523290ef6bf4916443be8dc
Ivan Vasilov <vasilov.ivan@gmail.com>
2024-09-30 17:42:32 +0200

    fix: Sticky resource banner (#29562)
    * Make the resource banner sticky.
    
    * Remove main from all layouts that are using it.
    
    * Update apps/studio/components/ui/ResourceExhaustionWarningBanner/ResourceExhaustionWarningBanner.tsx
    
    Co-authored-by: Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
    
    * Fix the whitespace.
    
    * Fix some of the pages with broken layouts.
    
    * Fix a merge error.
    
    * Fix the layout of the SQL editor.
    
    ---------
    
    Co-authored-by: Jonathan Summers-Muir <MildTomato@users.noreply.github.com>

--- apps/studio/pages/project/[ref]/index.tsx
+++ apps/studio/pages/project/[ref]/index.tsx
@@ -7,8 +7,8 @@ import { CLIENT_LIBRARIES, EXAMPLE_PROJECTS } from 'components/interfaces/Home/H
 import ProjectUsageSection from 'components/interfaces/Home/ProjectUsageSection'
 import { SecurityStatus } from 'components/interfaces/Home/SecurityStatus'
 import ServiceStatus from 'components/interfaces/Home/ServiceStatus'
-import { ProjectLayoutWithAuth } from 'components/layouts/ProjectLayout/ProjectLayout'
 import { ProjectPausedState } from 'components/layouts/ProjectLayout/PausedState/ProjectPausedState'
+import { ProjectLayoutWithAuth } from 'components/layouts/ProjectLayout/ProjectLayout'
 import { ComputeBadgeWrapper } from 'components/ui/ComputeBadgeWrapper'
 import ProjectUpgradeFailedBanner from 'components/ui/ProjectUpgradeFailedBanner'
 import { useSelectedOrganization } from 'hooks/misc/useSelectedOrganization'
@@ -117,12 +117,6 @@ const Home: NextPageWithLayout = () => {
   )
 }
 
-Home.getLayout = (page) => (
-  <ProjectLayoutWithAuth>
-    <main style={{ maxHeight: '100vh' }} className="flex-1 overflow-y-auto">
-      {page}
-    </main>
-  </ProjectLayoutWithAuth>
-)
+Home.getLayout = (page) => <ProjectLayoutWithAuth>{page}</ProjectLayoutWithAuth>
 
 export default Home
