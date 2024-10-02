06acd4a89d785cbc4a8a005ec84157e70c9a8a9e
avallete <andrew.valleteau@supabase.io>
2024-10-02 17:04:39 +0200

    fix(studio): views filterings on local studio experience

--- apps/studio/pages/api/pg-meta/[ref]/foreign-tables.ts
+++ apps/studio/pages/api/pg-meta/[ref]/foreign-tables.ts
@@ -4,6 +4,7 @@ import apiWrapper from 'lib/api/apiWrapper'
 import { get } from 'lib/common/fetch'
 import { constructHeaders } from 'lib/api/apiHelpers'
 import { PG_META_URL } from 'lib/constants'
+import { getPgMetaRedirectUrl } from './tables'
 
 export default (req: NextApiRequest, res: NextApiResponse) =>
   apiWrapper(req, res, handler, { withAuth: true })
@@ -23,25 +24,9 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
 
 const handleGetAll = async (req: NextApiRequest, res: NextApiResponse) => {
   const headers = constructHeaders(req.headers)
-
-  const query = Object.entries(req.query).reduce((query, entry) => {
-    const [key, value] = entry
-    if (Array.isArray(value)) {
-      for (const v of value) {
-        query.append(key, v)
-      }
-    } else if (value) {
-      query.set(key, value)
-    }
-    return query
-  }, new URLSearchParams())
-
-  let url = `${PG_META_URL}/foreign-tables`
-  if (Object.keys(req.query).length > 0) {
-    url += `?${query}`
-  }
-
-  const response = await get(url, { headers })
+  let response = await get(getPgMetaRedirectUrl(req, 'foreign-tables'), {
+    headers,
+  })
   if (response.error) {
     return res.status(400).json({ error: response.error })
   }
