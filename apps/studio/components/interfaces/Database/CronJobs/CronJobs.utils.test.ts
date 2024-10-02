9687bc4f3391baaebcd8186824c5f967097afc30
Ivan Vasilov <vasilov.ivan@gmail.com>
2024-10-02 13:56:14 +0200

    Fix the tests.

--- apps/studio/components/interfaces/Database/CronJobs/CronJobs.utils.test.ts
+++ apps/studio/components/interfaces/Database/CronJobs/CronJobs.utils.test.ts
@@ -3,23 +3,20 @@ import { parseCronJobCommand } from './CronJobs.utils'
 
 describe('parseCronJobCommand', () => {
   it('should return a default object when the command is null', () => {
-    const command = 'some random text'
-    expect(parseCronJobCommand('')).toBe({ snippet: 'some random text', type: 'sql_snippet' })
+    expect(parseCronJobCommand('')).toStrictEqual({ snippet: '', type: 'sql_snippet' })
   })
 
   it('should return a default object when the command is random', () => {
     const command = 'some random text'
-    expect(parseCronJobCommand(command)).toBe({ snippet: 'some random text', type: 'sql_snippet' })
+    expect(parseCronJobCommand(command)).toStrictEqual({
+      snippet: 'some random text',
+      type: 'sql_snippet',
+    })
   })
 
-  it('should return a default object when the command is random', () => {
-    const command = 'some random text'
-    expect(parseCronJobCommand(command)).toBe({ snippet: 'some random text', type: 'sql_snippet' })
-  })
-
-  it('should return a default object when the command is random', () => {
+  it('should return a edge function config when the command posts to supabase.co', () => {
     const command = `select net.http_post( url:='https://_.supabase.co/functions/v1/_', headers:=jsonb_build_object(), body:=jsonb_build_object(), timeout_milliseconds:=5000 );`
-    expect(parseCronJobCommand(command)).toBe({
+    expect(parseCronJobCommand(command)).toStrictEqual({
       edgeFunctionName: 'https://_.supabase.co/functions/v1/_',
       method: 'POST',
       httpHeaders: [],
