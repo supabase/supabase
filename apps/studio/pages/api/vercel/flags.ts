44938d9710c4fd1d56236c02e2bc025d3a9f57cb
Ivan Vasilov <vasilov.ivan@gmail.com>
2024-10-05 15:07:45 +0200

    Fix the api route for flags.

--- apps/studio/pages/api/vercel/flags.ts
+++ apps/studio/pages/api/vercel/flags.ts
@@ -1,6 +1,9 @@
 import { type ApiData } from '@vercel/flags'
-import type { NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default async function handler(response: NextApiResponse<ApiData | null>) {
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse<ApiData | null>
+) {
   return response.status(200).json({ overrideEncryptionMode: 'plaintext' })
 }
