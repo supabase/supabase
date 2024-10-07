3a546ee482cf4e36289ff678c9c21dd093d78cb4
Joshen Lim <joshenlimek@gmail.com>
2024-10-07 14:57:03 +0800

    Update copy

--- apps/studio/components/ui/ResourceExhaustionWarningBanner/ResourceExhaustionWarningBanner.constants.ts
+++ apps/studio/components/ui/ResourceExhaustionWarningBanner/ResourceExhaustionWarningBanner.constants.ts
@@ -224,7 +224,7 @@ export const RESOURCE_WARNING_MESSAGES: Record<string, ResourceWarningMessage> =
       critical: {
         title: "Authentication emails are only sent to organization members' email addresses",
         description:
-          'Set up a custom SMTP provider to handle flows like password reset or user invitation, and other flows which require sending emails to any user',
+          'Set up a custom SMTP provider to handle flows like password reset, user invitation, and other flows which require sending emails to any user',
       },
     },
     cardContent: {
