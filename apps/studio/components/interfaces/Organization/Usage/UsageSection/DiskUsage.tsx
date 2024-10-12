a4f7237ad722e905f1822e976edc01046567531a
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2024-10-13 02:58:32 +0800

    Update DiskUsage.tsx

--- apps/studio/components/interfaces/Organization/Usage/UsageSection/DiskUsage.tsx
+++ apps/studio/components/interfaces/Organization/Usage/UsageSection/DiskUsage.tsx
@@ -111,7 +111,10 @@ const DiskUsage = ({
 
               <div className="flex items-center justify-between">
                 <p className="text-xs text-foreground-light">Overages in period</p>
-                <p className="text-xs">{gp3UsageInPeriod?.usage ?? 0} GB-Hrs</p>
+                <p className="text-xs">
+                  {gp3UsageInPeriod?.usage ?? 0} GP3 GB-Hrs
+                  {io2UsageInPeriod?.usage ? ` / ${io2UsageInPeriod.usage} IO2 GB-Hrs` : ``}
+                </p>
               </div>
             </div>
 
@@ -139,15 +142,8 @@ const DiskUsage = ({
 
                     const totalDiskUsage = primaryDiskUsage + replicaDiskUsage
 
-                    // No free disk for io2
-                    const freeDiskGb = project.databases[0]!.disk_type === 'gp3' ? 8 : 0
-
-                    const freeUsedPercentage =
-                      freeDiskGb === 0 ? 0 : (freeDiskGb / totalDiskUsage) * 100
-                    const overagesPercentage = 100 - freeUsedPercentage
-
                     return (
-                      <div>
+                      <div className="border-b pb-2">
                         <div className="flex justify-between">
                           <span className="text-foreground-light flex items-center gap-2">
                             {project.name}
@@ -190,87 +186,6 @@ const DiskUsage = ({
                                 </>
                               )}
                             </InfoTooltip>
-                          </div>
-                          <div className="relative">
-                            <div
-                              className={cn(
-                                'h-[12px] relative border rounded-sm w-full transition bg-surface-300'
-                              )}
-                            >
-                              <div className="h-full flex">
-                                <Tooltip.Root delayDuration={0}>
-                                  <Tooltip.Trigger asChild>
-                                    <div
-                                      className="bg-foreground relative overflow-hidden transition-all duration-500 ease-in-out"
-                                      style={{ width: `${freeUsedPercentage}%` }}
-                                    >
-                                      <div
-                                        className="absolute inset-0"
-                                        style={{
-                                          backgroundImage: `repeating-linear-gradient(
-                            -45deg,
-                            rgba(255,255,255,0.1),
-                            rgba(255,255,255,0.1) 1px,
-                            transparent 1px,
-                            transparent 4px
-                          )`,
-                                        }}
-                                      />
-                                    </div>
-                                  </Tooltip.Trigger>
-                                  <Tooltip.Portal>
-                                    <Tooltip.Content side="top">
-                                      <div
-                                        className={[
-                                          'rounded bg-alternative py-1 px-2 leading-none shadow',
-                                          'border border-background',
-                                        ].join(' ')}
-                                      >
-                                        <span className="text-xs text-foreground">
-                                          {freeDiskGb} GB included for free
-                                        </span>
-                                      </div>
-                                    </Tooltip.Content>
-                                  </Tooltip.Portal>
-                                </Tooltip.Root>
-
-                                <Tooltip.Root delayDuration={0}>
-                                  <Tooltip.Trigger asChild>
-                                    <div
-                                      className="bg-transparent border-r transition-all duration-500 ease-in-out"
-                                      style={{ width: `${overagesPercentage}%` }}
-                                    />
-                                  </Tooltip.Trigger>
-                                  <Tooltip.Portal>
-                                    <Tooltip.Content side="top">
-                                      <div
-                                        className={[
-                                          'rounded bg-alternative py-1 px-2 leading-none shadow',
-                                          'border border-background',
-                                        ].join(' ')}
-                                      >
-                                        <span className="text-xs text-foreground">
-                                          {totalDiskUsage - freeDiskGb} GB additional provisioned
-                                          disk
-                                        </span>
-                                      </div>
-                                    </Tooltip.Content>
-                                  </Tooltip.Portal>
-                                </Tooltip.Root>
-                              </div>
-                            </div>
-                          </div>
-                          <div className="flex items-center space-x-4 text-xs text-foreground-lighter">
-                            <div className="flex items-center">
-                              <div className="w-2 h-2 rounded-full bg-foreground mr-2" />
-                              <span>Free Disk</span>
-                            </div>
-                            {replicaDiskUsage > 0 && (
-                              <div className="flex items-center">
-                                <div className="w-2 h-2 rounded-full bg-border border border-strong mr-2" />
-                                <span>Additional Disk</span>
-                              </div>
-                            )}
                           </div>
                         </div>
                       </div>
