b15a1bcbd63a57141bfc05402555b6cebc107c55
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-10-14 15:27:10 +0800

    revert

--- apps/www/next.config.mjs
+++ apps/www/next.config.mjs
@@ -96,11 +96,6 @@ const nextConfig = {
     // We are already running linting via GH action, this will skip linting during production build on Vercel
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    outputFileTracingExcludes: {
-      '*': ['**/.next', '**/public', '**/.contentlayer'],
-    },
-  },
 }
 
 // next.config.js.
