2d4f896414f15b90bf44103262cec61b3ce5f26c
Raminder Singh <romi_ssk@yahoo.co.in>
2024-10-15 18:24:47 +0530

    publications table

--- apps/studio/components/interfaces/Replication/Publications.tsx
+++ apps/studio/components/interfaces/Replication/Publications.tsx
@@ -1,3 +1,48 @@
+import { ScaffoldContainer, ScaffoldSection } from 'components/layouts/Scaffold'
+import { FormHeader } from 'components/ui/Forms/FormHeader'
+import { ButtonTooltip } from 'components/ui/ButtonTooltip'
+import { Plus } from 'lucide-react'
+import Table from 'components/to-be-cleaned/Table'
+
 export const ReplicationPublications = () => {
-  return <div>Publications</div>
+  return (
+    <>
+      <ScaffoldContainer>
+        <ScaffoldSection>
+          <div className="col-span-12">
+            <FormHeader title="Publications"></FormHeader>
+            <div className="space-y-4">
+              <div className="flex items-center gap-2 flex-wrap">
+                <ButtonTooltip
+                  className="ml-auto"
+                  icon={<Plus />}
+                  tooltip={{
+                    content: {
+                      side: 'bottom',
+                      text: 'Create a publication',
+                    },
+                  }}
+                >
+                  New Publication
+                </ButtonTooltip>
+              </div>
+              <div className="my-4 w-full">
+                <Table
+                  head={[
+                    <Table.th key="icon" className="!px-0" />,
+                    <Table.th key="name">Name</Table.th>,
+                    <Table.th key="tables" className="hidden xl:table-cell">
+                      Num Tables
+                    </Table.th>,
+                    <Table.th key="buttons">Edit</Table.th>,
+                  ]}
+                  body={<div>No publications</div>}
+                ></Table>
+              </div>
+            </div>
+          </div>
+        </ScaffoldSection>
+      </ScaffoldContainer>
+    </>
+  )
 }
