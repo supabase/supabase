c6511639ad0393becfb201b6af7cfe9d4c5cce48
Joshen Lim <joshenlimek@gmail.com>
2024-10-15 15:18:22 +0800

    Fix

--- packages/common/hooks/useTelemetryProps.ts
+++ packages/common/hooks/useTelemetryProps.ts
@@ -9,6 +9,8 @@ export function useTelemetryProps() {
 
   return {
     screenResolution: isBrowser ? `${window.innerWidth}x${window.innerHeight}` : undefined,
+    page_url: window.location.href,
+    search: window.location.search,
     viewport_height: window.innerHeight,
     viewport_width: window.innerWidth,
     language: locale ?? 'en-US',
--- packages/common/telemetry.ts
+++ packages/common/telemetry.ts
@@ -1,19 +1,20 @@
 import { post } from './fetchWrappers'
 
 export function handlePageTelemetry(API_URL: string, route: string, telemetryProps: any) {
+  const { page_url, search, language, viewport_height, viewport_width } = telemetryProps
   return post(
     `${API_URL}/telemetry/page`,
     {
-      page_url: window.location.href,
+      page_url,
       page_title: document.title,
       pathname: route,
       ph: {
         referrer: document.referrer,
         user_agent: navigator.userAgent,
-        search: window.location.search,
-        language: telemetryProps?.language,
-        viewport_height: telemetryProps?.viewport_height,
-        viewport_width: telemetryProps?.viewport_width,
+        search,
+        language,
+        viewport_height,
+        viewport_width,
       },
     },
     {
