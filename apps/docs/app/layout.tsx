56986a7d1b768d50d4475ba79ef253ef75bd0d16
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-10-15 22:30:28 +0800

    added satoshi to docs site

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -14,11 +14,110 @@ import { BASE_PATH, IS_PRODUCTION } from '~/lib/constants'
 
 import { Inter, Roboto_Mono } from 'next/font/google'
 
+import localFont from 'next/font/local'
+
 // If loading a variable font, you don't need to specify the font weight
-const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-inter',
+// const inter = Inter({
+//   subsets: ['latin'],
+//   display: 'swap',
+//   variable: '--font-inter',
+// })
+
+// Import the Satoshi font from your local files
+const inter = localFont({
+  src: [
+    // {
+    //   path: '../public/fonts/satoshi/Satoshi-Thin.woff2',
+    //   weight: '100',
+    //   style: 'normal',
+    // },
+    // {
+    //   path: '../public/fonts/satoshi/Satoshi-ThinItalic.woff2',
+    //   weight: '100',
+    //   style: 'italic',
+    // },
+    // {
+    //   path: '../public/fonts/satoshi/Satoshi-ExtraLight.woff2',
+    //   weight: '200',
+    //   style: 'normal',
+    // },
+    // {
+    //   path: '../public/fonts/satoshi/Satoshi-ExtraLightItalic.woff2',
+    //   weight: '200',
+    //   style: 'italic',
+    // },
+    // {
+    //   path: '../public/fonts/satoshi/Satoshi-Light.woff2',
+    //   weight: '300',
+    //   style: 'normal',
+    // },
+    // {
+    //   path: '../public/fonts/satoshi/Satoshi-LightItalic.woff2',
+    //   weight: '300',
+    //   style: 'italic',
+    // },
+    // {
+    //   path: '../public/fonts/satoshi/Satoshi-Regular.woff2',
+    //   weight: '400',
+    //   style: 'normal',
+    // },
+    // {
+    //   path: '../public/fonts/satoshi/Satoshi-RegularItalic.woff2',
+    //   weight: '400',
+    //   style: 'italic',
+    // },
+    {
+      path: '../public/fonts/satoshi/Satoshi-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/satoshi/Satoshi-MediumItalic.woff2',
+      weight: '500',
+      style: 'italic',
+    },
+    // {
+    //   path: '../public/fonts/satoshi/Satoshi-SemiBold.woff2',
+    //   weight: '600',
+    //   style: 'normal',
+    // },
+    // {
+    //   path: '../public/fonts/satoshi/Satoshi-SemiBoldItalic.woff2',
+    //   weight: '600',
+    //   style: 'italic',
+    // },
+    {
+      path: '../public/fonts/satoshi/Satoshi-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/satoshi/Satoshi-BoldItalic.woff2',
+      weight: '700',
+      style: 'italic',
+    },
+    // {
+    //   path: '../public/fonts/satoshi/Satoshi-ExtraBold.woff2',
+    //   weight: '800',
+    //   style: 'normal',
+    // },
+    // {
+    //   path: '../public/fonts/satoshi/Satoshi-ExtraBoldItalic.woff2',
+    //   weight: '800',
+    //   style: 'italic',
+    // },
+    {
+      path: '../public/fonts/satoshi/Satoshi-Black.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/satoshi/Satoshi-BlackItalic.woff2',
+      weight: '900',
+      style: 'italic',
+    },
+  ],
+  variable: '--font-inter', // Optional if you want to use it as a CSS variable
 })
 
 const roboto_mono = Roboto_Mono({
