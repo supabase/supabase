f576e5bb91454032a06902dc0747c4fa9e73c341
Raminder Singh <romi_ssk@yahoo.co.in>
2024-10-16 16:17:18 +0530

    show publications in table

--- apps/studio/data/replication/keys.ts
+++ apps/studio/data/replication/keys.ts
@@ -1,3 +1,5 @@
 export const replicationKeys = {
   sources: (projectRef: string | undefined) => ['projects', projectRef, 'sources'] as const,
+  publications: (projectRef: string | undefined, source_id: number) =>
+    ['projects', projectRef, 'sources', source_id, 'publications'] as const,
 }
--- packages/api-types/types/api.d.ts
+++ packages/api-types/types/api.d.ts
@@ -3464,6 +3464,10 @@ export interface paths {
     /** Creates a replication source */
     post: operations['ReplicationSourcesController_createSource']
   }
+  '/platform/replication/{ref}/sources/{source_id}/publications': {
+    /** Gets source publications */
+    get: operations['ReplicationSourcesController_getPublications']
+  }
   '/platform/reset-password': {
     parameters: {
       query?: never
@@ -10945,6 +10949,10 @@ export interface components {
       id: string
       saml?: components['schemas']['SamlDescriptor']
       updated_at?: string
+    }
+    Publication: {
+      name: string
+      tables: components['schemas']['Table'][]
     }
     PublicUrlOptions: {
       download?: boolean
@@ -22449,6 +22457,28 @@ export interface operations {
         }
       }
       /** @description Failed to create replication source */
+      500: {
+        content: never
+      }
+    }
+  }
+  /** Gets source publications */
+  ReplicationSourcesController_getPublications: {
+    parameters: {
+      path: {
+        /** @description Project ref */
+        ref: string
+        /** @description Source id */
+        source_id: number
+      }
+    }
+    responses: {
+      200: {
+        content: {
+          'application/json': components['schemas']['Publication'][]
+        }
+      }
+      /** @description Failed to get source publications */
       500: {
         content: never
       }
