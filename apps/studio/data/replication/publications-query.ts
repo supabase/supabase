9c169ec862795295488faf9c9039101924a31869
Raminder Singh <romi_ssk@yahoo.co.in>
2024-10-16 16:17:18 +0530

    show publications in table

---
+++ apps/studio/data/replication/publications-query.ts
@@ -0,0 +1,42 @@
+import { UseQueryOptions, useQuery } from '@tanstack/react-query'
+
+import { get, handleError } from 'data/fetchers'
+import { ResponseError } from 'types'
+import { replicationKeys } from './keys'
+
+type ReplicationPublicationsParams = { projectRef?: string; sourceId: number }
+
+async function fetchReplicationPublications(
+  { projectRef, sourceId }: ReplicationPublicationsParams,
+  signal?: AbortSignal
+) {
+  if (!projectRef) throw new Error('projectRef is required')
+
+  const { data, error } = await get(
+    '/platform/replication/{ref}/sources/{source_id}/publications',
+    {
+      params: { path: { ref: projectRef, source_id: sourceId } },
+      signal,
+    }
+  )
+  if (error) {
+    handleError(error)
+  }
+
+  return data
+}
+
+export type ReplicationPublicationsData = Awaited<ReturnType<typeof fetchReplicationPublications>>
+
+export const useReplicationPublicationsQuery = <TData = ReplicationPublicationsData>(
+  { projectRef, sourceId }: ReplicationPublicationsParams,
+  {
+    enabled = true,
+    ...options
+  }: UseQueryOptions<ReplicationPublicationsData, ResponseError, TData> = {}
+) =>
+  useQuery<ReplicationPublicationsData, ResponseError, TData>(
+    replicationKeys.publications(projectRef, sourceId),
+    ({ signal }) => fetchReplicationPublications({ projectRef, sourceId }, signal),
+    { enabled: enabled && typeof projectRef !== 'undefined', ...options }
+  )
