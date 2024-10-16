8ffcf9916991ee0c735ea54bd7d62d0f2f6fccca
Raminder Singh <romi_ssk@yahoo.co.in>
2024-10-16 16:17:18 +0530

    show publications in table

--- packages/api-types/types/api.d.ts
+++ packages/api-types/types/api.d.ts
@@ -3325,6 +3325,10 @@ export interface paths {
     /** Creates a replication source */
     post: operations['ReplicationSourcesController_createSource']
   }
+  '/platform/replication/{ref}/sources/{source_id}/publications': {
+    /** Gets source publications */
+    get: operations['ReplicationSourcesController_getPublications']
+  }
   '/platform/reset-password': {
     parameters: {
       query?: never
@@ -10704,6 +10708,10 @@ export interface components {
       id: string
       saml?: components['schemas']['SamlDescriptor']
       updated_at?: string
+    }
+    Publication: {
+      name: string
+      tables: components['schemas']['Table'][]
     }
     PublicUrlOptions: {
       download?: boolean
@@ -21594,6 +21602,28 @@ export interface operations {
         }
       }
       /** @description Failed to create replication source */
+      500: {
+        content: never
+      }
+    }
+  }
+  /** Gets source publications */
+  ReplicationSourcesController_getPublications: {
+    parameters: {
+      path: {
+        /** @description Project ref */
+        ref: string
+        /** @description Source id */
+        source_id: number
+      }
+    }
+    responses: {
+      200: {
+        content: {
+          'application/json': components['schemas']['Publication'][]
+        }
+      }
+      /** @description Failed to get source publications */
       500: {
         content: never
       }
