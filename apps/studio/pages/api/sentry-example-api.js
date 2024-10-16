4336d201993727743ec8167e0cad9465d3014125
Ivan Vasilov <vasilov.ivan@gmail.com>
2024-10-16 14:20:17 +0200

    Remove extra files.

--- apps/studio/pages/api/sentry-example-api.js
+++
@@ -1,5 +0,0 @@
-// A faulty API route to test Sentry's error monitoring
-export default function handler(_req, res) {
-  throw new Error('Sentry Example API Route Error')
-  res.status(200).json({ name: 'John Doe' })
-}
