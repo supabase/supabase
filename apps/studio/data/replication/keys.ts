00e6b1e9ea0e921d1cd9139e04d744dc40947053
Raminder Singh <romi_ssk@yahoo.co.in>
2024-10-17 19:50:28 +0530

    create publication wip

--- apps/studio/data/replication/keys.ts
+++ apps/studio/data/replication/keys.ts
@@ -2,4 +2,6 @@ export const replicationKeys = {
   sources: (projectRef: string | undefined) => ['projects', projectRef, 'sources'] as const,
   publications: (projectRef: string | undefined, source_id: number) =>
     ['projects', projectRef, 'sources', source_id, 'publications'] as const,
+  tables: (projectRef: string | undefined, source_id: number) =>
+    ['projects', projectRef, 'sources', source_id, 'tables'] as const,
 }
