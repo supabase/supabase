9cd63fb35fce65beb59ad176e9474643d0bcdf56
Raminder Singh <romi_ssk@yahoo.co.in>
2024-10-21 18:07:45 +0530

    add ui to delete a sink

--- packages/api-types/types/api.d.ts
+++ packages/api-types/types/api.d.ts
@@ -3342,6 +3342,10 @@ export interface paths {
     /** Creates a sink */
     post: operations['ReplicationSinksController_createSink']
   }
+  '/platform/replication/{ref}/sinks/{sink_id}': {
+    /** Deletes a sink */
+    delete: operations['ReplicationSinksController_deleteSink']
+  }
   '/platform/replication/{ref}/sources': {
     /** Gets replication sources */
     get: operations['ReplicationSourcesController_getSources']
@@ -18769,6 +18773,26 @@ export interface operations {
         content: never
       }
       /** @description Failed to create sink */
+      500: {
+        content: never
+      }
+    }
+  }
+  /** Deletes a sink */
+  ReplicationSinksController_deleteSink: {
+    parameters: {
+      path: {
+        /** @description Project ref */
+        ref: string
+        /** @description Sink id */
+        sink_id: number
+      }
+    }
+    responses: {
+      200: {
+        content: never
+      }
+      /** @description Failed to delete sink */
       500: {
         content: never
       }
