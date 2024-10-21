6bdbb6ca9bc27ed2f019d2124347a6adef6e2715
Raminder Singh <romi_ssk@yahoo.co.in>
2024-10-21 17:32:21 +0530

    create and list sinks

--- packages/api-types/types/api.d.ts
+++ packages/api-types/types/api.d.ts
@@ -3339,6 +3339,8 @@ export interface paths {
   '/platform/replication/{ref}/sinks': {
     /** Gets replication sinks */
     get: operations['ReplicationSinksController_getSinks']
+    /** Creates a sink */
+    post: operations['ReplicationSinksController_createSink']
   }
   '/platform/replication/{ref}/sources': {
     /** Gets replication sources */
@@ -8406,6 +8408,14 @@ export interface components {
       name: string
       /** @description Publication tables */
       tables: components['schemas']['ReplicationTable'][]
+    }
+    CreateReplicationSinkBody: {
+      /** @description BigQuery dataset id */
+      dataset_id: string
+      /** @description BigQuery project id */
+      project_id: string
+      /** @description BigQuery service account key */
+      service_account_key: string
     }
     CreateRoleBody: {
       admins?: string[]
@@ -18736,6 +18746,29 @@ export interface operations {
         }
       }
       /** @description Failed to get replication sinks */
+      500: {
+        content: never
+      }
+    }
+  }
+  /** Creates a sink */
+  ReplicationSinksController_createSink: {
+    parameters: {
+      path: {
+        /** @description Project ref */
+        ref: string
+      }
+    }
+    requestBody: {
+      content: {
+        'application/json': components['schemas']['CreateReplicationSinkBody']
+      }
+    }
+    responses: {
+      201: {
+        content: never
+      }
+      /** @description Failed to create sink */
       500: {
         content: never
       }
