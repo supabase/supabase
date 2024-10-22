7cfb4ac01cf9bbff34c64c728135b6d0d5d5236c
Raminder Singh <romi_ssk@yahoo.co.in>
2024-10-22 16:55:51 +0530

    get pipeline status wip

---
+++ apps/studio/data/replication/pipeline-status-query.ts
@@ -0,0 +1,41 @@
+import { UseQueryOptions, useQuery } from '@tanstack/react-query'
+
+import { get, handleError } from 'data/fetchers'
+import { ResponseError } from 'types'
+import { replicationKeys } from './keys'
+
+type ReplicationPipelinesStatusParams = { projectRef?: string; pipelineId: number }
+
+async function fetchReplicationPipelineStatus(
+  { projectRef, pipelineId }: ReplicationPipelinesStatusParams,
+  signal?: AbortSignal
+) {
+  if (!projectRef) throw new Error('projectRef is required')
+
+  const { data, error } = await get('/platform/replication/{ref}/pipelines/{pipeline_id}/status', {
+    params: { path: { ref: projectRef, pipeline_id: pipelineId } },
+    signal,
+  })
+  if (error) {
+    handleError(error)
+  }
+
+  return data
+}
+
+export type ReplicationPipelineStatusData = Awaited<
+  ReturnType<typeof fetchReplicationPipelineStatus>
+>
+
+export const useReplicationPipelineStatusQuery = <TData = ReplicationPipelineStatusData>(
+  { projectRef, pipelineId }: ReplicationPipelinesStatusParams,
+  {
+    enabled = true,
+    ...options
+  }: UseQueryOptions<ReplicationPipelineStatusData, ResponseError, TData> = {}
+) =>
+  useQuery<ReplicationPipelineStatusData, ResponseError, TData>(
+    replicationKeys.pipelinesStatus(projectRef, pipelineId),
+    ({ signal }) => fetchReplicationPipelineStatus({ projectRef, pipelineId }, signal),
+    { enabled: enabled && typeof projectRef !== 'undefined', ...options }
+  )
