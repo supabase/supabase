6a99083d4f9e3fdff39044549e337eb11eb9b107
Raminder Singh <romi_ssk@yahoo.co.in>
2024-10-23 16:16:03 +0530

    show correct label on action buttons

--- packages/api-types/types/api.d.ts
+++ packages/api-types/types/api.d.ts
@@ -8936,6 +8936,10 @@ export interface components {
       source_id: number
       tenant_id: string
     }
+    ReplicationPipelinesStatusResponse: {
+      pipeline_id: number
+      status: string
+    }
     ReplicationPostgresConfig: {
       host: string
       name: string
@@ -18856,7 +18860,9 @@ export interface operations {
     }
     responses: {
       200: {
-        content: never
+        content: {
+          'application/json': components['schemas']['ReplicationPipelinesStatusResponse']
+        }
       }
       /** @description Failed to get pipeline status */
       500: {
