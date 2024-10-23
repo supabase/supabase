4b0ef89f5c4d3391338d697e0665751e9af65d7c
Raminder Singh <romi_ssk@yahoo.co.in>
2024-10-23 17:15:32 +0530

    start and stop pipelines

--- packages/api-types/types/api.d.ts
+++ packages/api-types/types/api.d.ts
@@ -3331,9 +3331,17 @@ export interface paths {
     /** Deletes a pipeline */
     delete: operations['ReplicationPipelinesController_deletePipeline']
   }
+  '/platform/replication/{ref}/pipelines/{pipeline_id}/start': {
+    /** Starts a pipeline */
+    post: operations['ReplicationPipelinesController_startPipeline']
+  }
   '/platform/replication/{ref}/pipelines/{pipeline_id}/status': {
     /** Gets status of a pipeline */
     get: operations['ReplicationPipelinesController_getPipelineStatus']
+  }
+  '/platform/replication/{ref}/pipelines/{pipeline_id}/stop': {
+    /** Stops a pipeline */
+    post: operations['ReplicationPipelinesController_stopPipeline']
   }
   '/platform/replication/{ref}/sinks': {
     /** Gets replication sinks */
@@ -21747,6 +21755,26 @@ export interface operations {
       }
     }
   }
+  /** Starts a pipeline */
+  ReplicationPipelinesController_startPipeline: {
+    parameters: {
+      path: {
+        /** @description Project ref */
+        ref: string
+        /** @description Pipeline id */
+        pipeline_id: number
+      }
+    }
+    responses: {
+      201: {
+        content: never
+      }
+      /** @description Failed to start pipeline */
+      500: {
+        content: never
+      }
+    }
+  }
   /** Gets status of a pipeline */
   ReplicationPipelinesController_getPipelineStatus: {
     parameters: {
@@ -21764,6 +21792,26 @@ export interface operations {
         }
       }
       /** @description Failed to get pipeline status */
+      500: {
+        content: never
+      }
+    }
+  }
+  /** Stops a pipeline */
+  ReplicationPipelinesController_stopPipeline: {
+    parameters: {
+      path: {
+        /** @description Project ref */
+        ref: string
+        /** @description Pipeline id */
+        pipeline_id: number
+      }
+    }
+    responses: {
+      201: {
+        content: never
+      }
+      /** @description Failed to stop pipeline */
       500: {
         content: never
       }
