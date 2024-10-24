ad90ecff1b41c64c1e2ad3ab9d3e25ae73a36d53
Raminder Singh <romi_ssk@yahoo.co.in>
2024-10-24 17:02:29 +0530

    show source/sink names on pipelines page

--- packages/api-types/types/api.d.ts
+++ packages/api-types/types/api.d.ts
@@ -8943,7 +8943,9 @@ export interface components {
       publication_name: string
       replicator_id: number
       sink_id: number
+      sink_name: string
       source_id: number
+      source_name: string
       tenant_id: string
     }
     ReplicationPipelinesStatusResponse: {
