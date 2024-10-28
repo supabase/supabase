49b4e58f12b58bb392047434f7c4e7a4a25bba1e
Terry Sutton <saltcod@gmail.com>
2024-10-28 12:36:10 -0230

    Allow excluding options in the url regex

--- apps/studio/tests/components/Auth/Auth.constants.test.ts
+++ apps/studio/tests/components/Auth/Auth.constants.test.ts
@@ -22,20 +22,20 @@ describe('Auth.constants: urlRegex', () => {
     ]
 
     validUrls.forEach((url) => {
-      expect(urlRegex.test(url)).toBe(true)
+      expect(urlRegex().test(url)).toBe(true)
     })
   })
 
   it('should not match invalid URLs', () => {
     const invalidUrls = ['supabase', 'mailto:test@gmail.com', 'hello world.com', 'email@domain.com']
 
-    const failingInvalidUrls = invalidUrls.filter((url) => urlRegex.test(url))
+    const failingInvalidUrls = invalidUrls.filter((url) => urlRegex().test(url))
     if (failingInvalidUrls.length > 0) {
       console.log('Failing invalid URLs:', failingInvalidUrls)
     }
 
     invalidUrls.forEach((url) => {
-      expect(urlRegex.test(url)).toBe(false)
+      expect(urlRegex().test(url)).toBe(false)
     })
   })
 })
