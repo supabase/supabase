0c041e63888fd71bf9d6f3ba2ee08d6c4a287714
Jordi Enric <37541088+jordienr@users.noreply.github.com>
2024-10-31 10:27:39 +0100

    Loki log drain update & ui fix (#30080)
    * fix
    
    * truncate name
    
    * improve UX for creating loki log drains
    
    * undo base64 encoding in fe
    
    * type pass in pass
    
    * Add title prop
    
    * Fix text size
    
    ---------
    
    Co-authored-by: Joshen Lim <joshenlimek@gmail.com>

--- apps/studio/components/interfaces/LogDrains/LogDrainDestinationSheetForm.tsx
+++ apps/studio/components/interfaces/LogDrains/LogDrainDestinationSheetForm.tsx
@@ -68,6 +68,8 @@ const formUnion = z.discriminatedUnion('type', [
     type: z.literal('loki'),
     url: z.string().min(1, { message: 'Loki URL is required' }),
     headers: z.record(z.string(), z.string()),
+    username: z.string().optional(),
+    password: z.string().optional(),
   }),
 ])
 
@@ -162,6 +164,8 @@ export function LogDrainDestinationSheetForm({
       url: defaultValues?.config?.url || '',
       api_key: defaultValues?.config?.api_key || '',
       region: defaultValues?.config?.region || '',
+      username: defaultValues?.config?.username || '',
+      password: defaultValues?.config?.password || '',
     },
   })
 
@@ -422,9 +426,23 @@ export function LogDrainDestinationSheetForm({
                     <LogDrainFormItem
                       type="url"
                       value="url"
+                      placeholder="https://my-logs-endpoint.grafana.net/loki/api/v1/push"
                       label="Loki URL"
                       formControl={form.control}
                       description="The Loki HTTP(S) endpoint to send events."
+                    />
+                    <LogDrainFormItem
+                      value="username"
+                      label="Username"
+                      placeholder="123456789"
+                      formControl={form.control}
+                    />
+                    <LogDrainFormItem
+                      type="password"
+                      value="password"
+                      label="Password"
+                      placeholder="glc_ABCD1234567890"
+                      formControl={form.control}
                     />
                   </div>
                 )}
