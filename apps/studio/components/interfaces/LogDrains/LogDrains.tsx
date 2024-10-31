2929967130e09b340627ad0e458ff297e0090ec6
Joshen Lim <joshenlimek@gmail.com>
2024-10-31 17:15:02 +0800

    Add title prop

--- apps/studio/components/interfaces/LogDrains/LogDrains.tsx
+++ apps/studio/components/interfaces/LogDrains/LogDrains.tsx
@@ -131,8 +131,13 @@ export function LogDrains({
           <TableBody>
             {logDrains?.map((drain) => (
               <TableRow key={drain.id}>
-                <TableCell className="font-medium truncate max-w-72">{drain.name}</TableCell>
-                <TableCell className="text-foreground-light truncate max-w-72">
+                <TableCell className="font-medium truncate max-w-72" title={drain.name}>
+                  {drain.name}
+                </TableCell>
+                <TableCell
+                  className="text-foreground-light truncate max-w-72"
+                  title={drain.description}
+                >
                   {drain.description}
                 </TableCell>
                 <TableCell className="text-right font-mono">{drain.type}</TableCell>
