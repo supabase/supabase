2776d01a4fa88edf6c733ac1e5917c77069a3b21
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-11-01 21:06:31 +0800

    Update vercel-toolbar.tsx

--- apps/studio/providers/vercel-toolbar.tsx
+++ apps/studio/providers/vercel-toolbar.tsx
@@ -2,13 +2,10 @@ import dynamic from 'next/dynamic'
 import { useUser } from 'common'
 import { PropsWithChildren } from 'react'
 
-// Dynamically import VercelToolbar to load it only when required
-const VercelToolbar = dynamic(
-  () => import('@vercel/toolbar/next').then((mod) => mod.VercelToolbar),
-  {
-    ssr: false, // Disable SSR for this component
-  }
-)
+// Dynamically load VercelToolbar
+const VercelToolbar = dynamic<any>(() => import('@vercel/toolbar'), {
+  ssr: false, // Disable SSR for the toolbar
+})
 
 export default function VercelToolbarProvider({ children }: PropsWithChildren<{}>) {
   const user = useUser()
