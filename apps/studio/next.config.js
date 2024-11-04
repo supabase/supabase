f02a965c47b967a5f410224cf0b21cdaca56a045
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-11-04 17:43:59 +0800

    Update next.config.js

--- apps/studio/next.config.js
+++ apps/studio/next.config.js
@@ -74,7 +74,7 @@ const csp = [
         `worker-src 'self' blob: data:;`,
       ]
     : [
-        `default-src 'self' ${DEFAULT_SRC_URLS};`,
+        `default-src 'self' ${DEFAULT_SRC_URLS} ${PUSHER_URL} ${PUSHER_URL_WS};`,
         `script-src 'self' 'unsafe-eval' 'unsafe-inline' ${SCRIPT_SRC_URLS} ${VERCEL_LIVE_URL};`,
         `frame-src 'self' ${FRAME_SRC_URLS} ${VERCEL_LIVE_URL};`,
         `img-src 'self' blob: data: ${IMG_SRC_URLS} ;`,
