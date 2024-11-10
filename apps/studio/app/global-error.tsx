a2a4a41ac5ae552eb7d285106727b3e78c61fa92
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-11-10 14:11:14 +0800

    add global error page

---
+++ apps/studio/app/global-error.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  return (
+    <html>
+      <body>
+        <h2>Something went wrong!</h2>
+        <button onClick={() => reset()}>Try again</button>
+      </body>
+    </html>
+  )
+}
