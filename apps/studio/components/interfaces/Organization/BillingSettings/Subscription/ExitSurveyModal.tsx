7bfc25467825b07dc8888c133ccf04e6e0a48983
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2024-11-14 16:44:37 +0800

    Update ExitSurveyModal.tsx

--- apps/studio/components/interfaces/Organization/BillingSettings/Subscription/ExitSurveyModal.tsx
+++ apps/studio/components/interfaces/Organization/BillingSettings/Subscription/ExitSurveyModal.tsx
@@ -205,7 +205,8 @@ const ExitSurveyModal = ({ visible, subscription, projects, onClose }: ExitSurve
 
         <div className="flex items-center justify-between border-t px-4 py-4">
           <p className="text-xs text-foreground-lighter">
-            The unused amount for the remaining time of your billing cycle will be refunded as credits
+            The unused amount for the remaining time of your billing cycle will be refunded as
+            credits
           </p>
 
           <div className="flex items-center space-x-2">
