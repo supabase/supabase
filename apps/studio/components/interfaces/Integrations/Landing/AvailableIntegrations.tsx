92e87d5fd6f9ffe656c6ace79e6cfbaa7e4382b5
Ivan Vasilov <vasilov.ivan@gmail.com>
2024-11-19 16:42:42 +0100

    Add useInstalledIntegrations hook.

--- apps/studio/components/interfaces/Integrations/Landing/AvailableIntegrations.tsx
+++ apps/studio/components/interfaces/Integrations/Landing/AvailableIntegrations.tsx
@@ -1,20 +1,22 @@
-import { useProjectContext } from 'components/layouts/ProjectLayout/ProjectContext'
-import { useFDWsQuery } from 'data/fdw/fdws-query'
 import { IntegrationCard } from './IntegrationCard'
 import { INTEGRATIONS } from './Integrations.constants'
+import { useInstalledIntegrations } from './useInstalledIntegrations'
 
 export const AvailableIntegrations = () => {
-  const { project } = useProjectContext()
-  const { data } = useFDWsQuery({
-    projectRef: project?.ref,
-    connectionString: project?.connectionString,
-  })
+  const { isLoading, installedIntegrations: ids } = useInstalledIntegrations()
+
+  // available integrations for install
+  const integrations = INTEGRATIONS.filter((i) => !ids.includes(i.id))
+
+  if (isLoading) {
+    return <div>Loading</div>
+  }
 
   return (
     <div className="p-9 flex flex-col gap-y-5">
       <h2>Available integrations</h2>
       <div className="flex flex-row flex-wrap gap-x-4 gap-y-3">
-        {INTEGRATIONS.map((i) => (
+        {integrations.map((i) => (
           <IntegrationCard {...i} />
         ))}
       </div>
--- apps/studio/components/interfaces/Integrations/Landing/InstalledIntegrations.tsx
+++ apps/studio/components/interfaces/Integrations/Landing/InstalledIntegrations.tsx
@@ -1,39 +1,15 @@
-import { wrapperMetaComparator } from 'components/interfaces/Database/Wrappers/Wrappers.utils'
-import { useProjectContext } from 'components/layouts/ProjectLayout/ProjectContext'
-import { useDatabaseExtensionsQuery } from 'data/database-extensions/database-extensions-query'
-import { useFDWsQuery } from 'data/fdw/fdws-query'
 import { IntegrationCard } from './IntegrationCard'
 import { INTEGRATIONS } from './Integrations.constants'
+import { useInstalledIntegrations } from './useInstalledIntegrations'
 
 export const InstalledIntegrations = () => {
-  const { project } = useProjectContext()
-  const { data, isLoading } = useFDWsQuery({
-    projectRef: project?.ref,
-    connectionString: project?.connectionString,
-  })
-  const { data: extensions, isLoading: isLoadingExtensions } = useDatabaseExtensionsQuery({
-    projectRef: project?.ref,
-    connectionString: project?.connectionString,
-  })
+  const { isLoading, installedIntegrations: ids } = useInstalledIntegrations()
 
-  const wrappers = data?.result || []
+  const installedIntegrations = INTEGRATIONS.filter((i) => ids.includes(i.id))
 
   if (isLoading) {
     return <div>Loading</div>
   }
-
-  const installedIntegrations = INTEGRATIONS.filter((i) => {
-    if (i.type === 'wrapper') {
-      return wrappers.find((w) => wrapperMetaComparator(i.meta, w))
-    }
-    if (i.type === 'postgres_extension') {
-      return i.requiredExtensions.every((extName) => {
-        const foundExtension = (extensions ?? []).find((ext) => ext.name === extName)
-        return !!foundExtension?.installed_version
-      })
-    }
-    return false
-  })
 
   if (installedIntegrations.length === 0) {
     return (
