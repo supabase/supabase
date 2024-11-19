b69fcfcfd6b3d3c9d0213d54c099575dd9d09190
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-11-19 15:33:37 +0800

    alt nav put in

--- apps/studio/components/layouts/Integrations/layout.tsx
+++ apps/studio/components/layouts/Integrations/layout.tsx
@@ -1,14 +1,36 @@
 import { Header } from 'components/layouts/Integrations/header'
 import ProjectLayout from 'components/layouts/ProjectLayout/ProjectLayout'
+import { ProductMenu } from 'components/ui/ProductMenu'
 import { useScroll } from 'framer-motion'
 import { withAuth } from 'hooks/misc/withAuth'
+import { useFlag } from 'hooks/ui/useFlag'
 import { PropsWithChildren, useEffect, useRef, useState } from 'react'
+import { generateAdvisorsMenu } from '../AdvisorsLayout/AdvisorsMenu.utils'
+import { useSelectedProject } from 'hooks/misc/useSelectedProject'
+import { useRouter } from 'next/router'
+import { INTEGRATIONS } from 'components/interfaces/Integrations/Landing/Integrations.constants'
+import { FDW, useFDWsQuery } from 'data/fdw/fdws-query'
+import { wrapperMetaComparator } from 'components/interfaces/Database/Wrappers/Wrappers.utils'
+import { Table2 } from 'lucide-react'
+import Image from 'next/image'
+import { useParams } from 'common'
 
 /**
  * Layout component for the Integrations section
  * Handles scroll-based sticky header behavior and authentication
  */
 const IntegrationsLayout = ({ ...props }: PropsWithChildren) => {
+  const layoutSidebar = useFlag('integrationLayoutSidebar')
+  if (layoutSidebar) {
+    return <IntegrationsLayoutSide {...props} />
+  }
+  return <IntegrationsLayoutTop {...props} />
+}
+
+/**
+ * Top level layout
+ */
+const IntegrationsLayoutTop = ({ ...props }: PropsWithChildren) => {
   // Refs for the main scrollable area and header
   const mainElementRef = useRef<HTMLDivElement>(null)
   const headerRef = useRef<HTMLDivElement>(null)
@@ -60,6 +82,91 @@ const IntegrationsLayout = ({ ...props }: PropsWithChildren) => {
       isBlocking={false}
     >
       <Header ref={headerRef} scroll={scroll} isSticky={isSticky} />
+      {props.children}
+    </ProjectLayout>
+  )
+}
+
+const IntegrationsLayoutSide = ({ ...props }: PropsWithChildren) => {
+  const { id } = useParams()
+  const project = useSelectedProject()
+
+  const router = useRouter()
+  const page = router.pathname.split('/')[4]
+
+  const { data: fdwData } = useFDWsQuery({
+    projectRef: project?.ref,
+    connectionString: project?.connectionString,
+  })
+
+  const wrappers = fdwData?.result || []
+
+  const generateIntegrationsMenu = () => {
+    const installedIntegrations = INTEGRATIONS.filter((integration) => {
+      if (integration.type === 'wrapper') {
+        return wrappers.find((wrapper) => wrapperMetaComparator(integration.meta, wrapper))
+      }
+      return false
+    })
+
+    return [
+      {
+        title: 'Installed Integrations',
+        items: [
+          {
+            name: 'All Integrations',
+            key: 'all-integrations',
+            url: `/project/${project?.ref}/integrations/landing`,
+            icon: <Table2 size={14} />,
+          },
+        ],
+      },
+      {
+        title: 'Installed Integrations',
+        items: installedIntegrations.map((integration) => ({
+          name: integration.name,
+          key: integration.id,
+          url: `/project/${project?.ref}/integrations/${integration.id}`,
+          icon: (
+            <div className="relative w-6 h-6 bg-surface-400 border rounded">
+              <Image fill src={integration.icon} alt={integration.name} className="p-0.5" />
+            </div>
+          ),
+        })),
+      },
+      id
+        ? {
+            title: 'Pages',
+            items: [
+              {
+                name: 'Overview',
+                key: 'settings',
+                url: `/project/${project?.ref}/integrations/${id}?tab=overview`,
+              },
+              {
+                name: 'Wrappers',
+                key: 'settings',
+                url: `/project/${project?.ref}/integrations/${id}?tab=wrappers`,
+              },
+              {
+                name: 'Logs',
+                key: 'settings',
+                url: `/project/${project?.ref}/integrations/${id}?tab=logs`,
+              },
+            ],
+          }
+        : undefined,
+    ].filter(Boolean)
+  }
+
+  // generateIntegrationsMenu()
+
+  return (
+    <ProjectLayout
+      isLoading={false}
+      product="Integrations"
+      productMenu={<ProductMenu page={page} menu={generateIntegrationsMenu()} />}
+    >
       {props.children}
     </ProjectLayout>
   )
