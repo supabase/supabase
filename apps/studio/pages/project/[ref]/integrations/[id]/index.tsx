940f470ff097907420d2c97bce52b670e8988b1b
Ivan Vasilov <vasilov.ivan@gmail.com>
2024-11-21 22:39:02 +0100

    Add logic to the integration pages to avoid weird loading bugs, deselecting tabs if the integration hasn't been installed etc.

--- apps/studio/pages/project/[ref]/integrations/[id]/index.tsx
+++ apps/studio/pages/project/[ref]/integrations/[id]/index.tsx
@@ -1,19 +1,42 @@
 import { useParams } from 'common'
+import { INTEGRATIONS } from 'components/interfaces/Integrations/Landing/Integrations.constants'
+import { useInstalledIntegrations } from 'components/interfaces/Integrations/Landing/useInstalledIntegrations'
 import IntegrationsLayout from 'components/layouts/Integrations/layout'
-import { IntegrationPageHandler } from 'pages/project/[ref]/integrations/[id]/IntegrationPageHandler'
+import { GenericSkeletonLoader } from 'components/ui/ShimmeringLoader'
+import { useRouter } from 'next/compat/router'
 import { NextPageWithLayout } from 'types'
 
 const IntegrationPage: NextPageWithLayout = () => {
-  const { id, pageId } = useParams()
+  const router = useRouter()
+  const { ref, id, pageId, childId } = useParams()
 
-  // Get the corresponding component dynamically
-  if (!id) {
-    return <div>Not found</div>
+  const { installedIntegrations: installedIntegrations, isLoading: isIntegrationsLoading } =
+    useInstalledIntegrations()
+
+  if (!router?.isReady || isIntegrationsLoading) {
+    return (
+      <div className="px-10 py-6">
+        <GenericSkeletonLoader />
+      </div>
+    )
   }
 
-  const Component = IntegrationPageHandler()
+  const integration = INTEGRATIONS.find((i) => i.id === id)
+  if (!id || !integration) {
+    return <div>Integration not found</div>
+  }
 
-  if (!Component) return <div>Not found</div>
+  const installation = installedIntegrations.find((inst) => inst.id === id)
+
+  // if the integration is not installed, redirect to the overview page
+  if (!installation && pageId !== 'overview') {
+    router.replace(`/project/${ref}/integrations/${id}/overview`)
+  }
+
+  // Get the corresponding component dynamically
+  const Component = integration.navigate(id, pageId, childId)
+
+  if (!Component) return <div>Component not found</div>
 
   return <Component />
 }
