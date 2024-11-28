bab1a1e7ff6ff0b71983ee87c103b8c5f0ccbf13
Joshen Lim <joshenlimek@gmail.com>
2024-11-28 18:16:42 +0800

    Clean up

--- apps/studio/components/interfaces/Auth/EmailTemplates/SpamValidation.tsx
+++ apps/studio/components/interfaces/Auth/EmailTemplates/SpamValidation.tsx
@@ -37,40 +37,36 @@ export const SpamValidation = ({ validationResult }: SpamValidationProps) => {
       description={
         hasSpamWarning ? (
           <>
-            {spamRules.length > 0 && (
-              <>
-                <div className="flex flex-col gap-y-3">
-                  <p>
-                    {hasSpamWarning
-                      ? ` Rectify the following issues to improve your email's deliverability in order of priority:`
-                      : ` Address the following issues to improve your email's deliverability:`}
-                  </p>
-                  <Table>
-                    <TableHeader className="font-mono uppercase text-xs [&_th]:h-auto [&_th]:py-2">
-                      <TableRow className="hover:bg-transparent">
-                        <TableHead className="pl-0">Warning</TableHead>
-                        <TableHead className="pr-0">Description</TableHead>
-                      </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                      {spamRules.map((rule) => (
-                        <TableRow key={rule.name} className="hover:bg-transparent">
-                          <TableCell className="pl-0 font-mono text-xs text-foreground">
-                            {rule.name}
-                          </TableCell>
-                          <TableCell className="pr-0">{rule.desc}</TableCell>
-                        </TableRow>
-                      ))}
-                    </TableBody>
-                  </Table>
-                </div>
-                <Separator />
-                <Markdown
-                  className="!max-w-none text-foreground-muted text-xs [&_a]:text-foreground-lighter mt-2"
-                  content="Spam validation is powered by [SpamAssassin](https://spamassassin.apache.org/doc.html). Full list of all available warnings can be found [here](https://gist.github.com/ychaouche/a2faff159c2a1fea16019156972c7f8b)."
-                />
-              </>
-            )}
+            <div className="flex flex-col gap-y-3">
+              <p>
+                {hasSpamWarning
+                  ? ` Rectify the following issues to improve your email's deliverability in order of priority:`
+                  : ` Address the following issues to improve your email's deliverability:`}
+              </p>
+              <Table>
+                <TableHeader className="font-mono uppercase text-xs [&_th]:h-auto [&_th]:py-2">
+                  <TableRow className="hover:bg-transparent">
+                    <TableHead className="pl-0">Warning</TableHead>
+                    <TableHead className="pr-0">Description</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {spamRules.map((rule) => (
+                    <TableRow key={rule.name} className="hover:bg-transparent">
+                      <TableCell className="pl-0 font-mono text-xs text-foreground">
+                        {rule.name}
+                      </TableCell>
+                      <TableCell className="pr-0">{rule.desc}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </div>
+            <Separator />
+            <Markdown
+              className="!max-w-none text-foreground-muted text-xs [&_a]:text-foreground-lighter mt-2"
+              content="Spam validation is powered by [SpamAssassin](https://spamassassin.apache.org/doc.html). Full list of all available warnings can be found [here](https://gist.github.com/ychaouche/a2faff159c2a1fea16019156972c7f8b)."
+            />
           </>
         ) : null
       }
