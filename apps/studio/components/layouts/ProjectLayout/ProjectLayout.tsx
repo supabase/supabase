4052aa8626375b2875f21d4c9758df72b1ab0bcd
Joshen Lim <joshenlimek@gmail.com>
2024-11-30 13:57:19 +0800

    Fix

--- apps/studio/components/layouts/ProjectLayout/ProjectLayout.tsx
+++ apps/studio/components/layouts/ProjectLayout/ProjectLayout.tsx
@@ -34,7 +34,6 @@ import RestartingState from './RestartingState'
 import RestoreFailedState from './RestoreFailedState'
 import RestoringState from './RestoringState'
 import { UpgradingState } from './UpgradingState'
-import { a } from 'vitest/dist/suite-IbNSsUWN'
 
 // [Joshen] This is temporary while we unblock users from managing their project
 // if their project is not responding well for any reason. Eventually needs a bit of an overhaul
