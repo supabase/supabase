634b62e9273ca33289e00ab1c1c9d4067eaef29f
Wen Bo Xie <5532241+w3b6x9@users.noreply.github.com>
2024-12-02 09:31:11 -0500

    fix: add project selection link to assistant page (#30799)

--- apps/www/pages/assistant.tsx
+++ apps/www/pages/assistant.tsx
@@ -53,7 +53,7 @@ const welcomeMessages = [
           </g>
         </svg>
         <p>
-          Welcome Product Hunter! 👋 Thanks for checking out the Supabase assistant. Let me show you
+          Welcome Product Hunter! 👋 Thanks for checking out the Supabase Assistant. Let me show you
           what I can do!
         </p>
       </div>
@@ -677,17 +677,19 @@ function Assistant() {
                 <h1 className="text-4xl sm:text-5xl sm:leading-none">Chat with Postgres</h1>
                 <p className="p text-lg !m-0">
                   Generate, run and debug queries, chart your data, create functions, policies and
-                  more. The assistant is here to help.
+                  more. The Assistant is here to help.
                 </p>
                 <div className="min-h-12">
                   {!isUserLoading &&
                     (isLoggedIn ? (
                       <Button type="primary" size="medium" asChild>
-                        <Link href="/dashboard/projects">Dashboard</Link>
+                        <Link href="/dashboard/project/_">Dashboard</Link>
                       </Button>
                     ) : (
                       <Button type="primary" size="medium" asChild>
-                        <Link href="https://supabase.com/dashboard">Start your project</Link>
+                        <Link href="https://supabase.com/dashboard/project/_">
+                          Start your project
+                        </Link>
                       </Button>
                     ))}
                 </div>
