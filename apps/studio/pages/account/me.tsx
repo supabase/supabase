0528457eaee2fc01072ec2d9b82a64b04f23ab59
Ivan Vasilov <vasilov.ivan@gmail.com>
2024-12-04 12:08:36 +0100

    Refactor all account pages to use ScaffoldContainer and have the same design.

--- apps/studio/pages/account/me.tsx
+++ apps/studio/pages/account/me.tsx
@@ -6,6 +6,12 @@ import {
 import { AccountDeletion } from 'components/interfaces/Account/Preferences/AccountDeletion'
 import { ProfileInformation } from 'components/interfaces/Account/Preferences/ProfileInformation'
 import AccountLayout from 'components/layouts/AccountLayout/AccountLayout'
+import {
+  ScaffoldContainer,
+  ScaffoldDescription,
+  ScaffoldHeader,
+  ScaffoldTitle,
+} from 'components/layouts/Scaffold'
 import AlertError from 'components/ui/AlertError'
 import Panel from 'components/ui/Panel'
 import { GenericSkeletonLoader } from 'components/ui/ShimmeringLoader'
@@ -15,9 +21,15 @@ import type { NextPageWithLayout } from 'types'
 
 const User: NextPageWithLayout = () => {
   return (
-    <div className="my-2">
+    <ScaffoldContainer>
+      <ScaffoldHeader>
+        <ScaffoldTitle>User Preferences</ScaffoldTitle>
+        <ScaffoldDescription>
+          Manage your profile, account settings, and preferences for your Supabase experience
+        </ScaffoldDescription>
+      </ScaffoldHeader>
       <ProfileCard />
-    </div>
+    </ScaffoldContainer>
   )
 }
 
@@ -42,7 +54,7 @@ const ProfileCard = () => {
   const { profile, error, isLoading, isError, isSuccess } = useProfile()
 
   return (
-    <article className="max-w-4xl p-4">
+    <article>
       {isLoading && (
         <Panel>
           <div className="p-4">
