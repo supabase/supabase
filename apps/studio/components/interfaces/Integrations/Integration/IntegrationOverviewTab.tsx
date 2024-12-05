cce5e6b78419c98153986044da143d6fc143c8f9
Ivan Vasilov <vasilov.ivan@gmail.com>
2024-12-05 15:28:04 +0100

    feat: Show alert to upgrade DB on queues integration (#30925)
    * List all integrations in the landing page.
    
    * Add a configuration in the integration definitions to show an alert if all their dependencies can't be installed.
    
    * Refactor the Integration Overview tab to show an integration specific alert if all its dependencies can't be met because of outdated image.
    
    * Fix smol misspell.
    
    * Update copy.
    
    * Fix copy.

--- apps/studio/components/interfaces/Integrations/Integration/IntegrationOverviewTab.tsx
+++ apps/studio/components/interfaces/Integrations/Integration/IntegrationOverviewTab.tsx
@@ -6,7 +6,7 @@ import { Markdown } from 'components/interfaces/Markdown'
 import { useDatabaseExtensionsQuery } from 'data/database-extensions/database-extensions-query'
 import { useSelectedProject } from 'hooks/misc/useSelectedProject'
 import { Badge, Separator } from 'ui'
-import { Admonition } from 'ui-patterns'
+import { Admonition } from 'ui-patterns/admonition'
 import { INTEGRATIONS } from '../Landing/Integrations.constants'
 import { BuiltBySection } from './BuildBySection'
 import { MarkdownContent } from './MarkdownContent'
@@ -26,22 +26,25 @@ export const IntegrationOverviewTab = ({
 
   const integration = INTEGRATIONS.find((i) => i.id === id)
 
-  const dependsOnExtension = (integration?.requiredExtensions ?? []).length > 0
-
   const { data: extensions } = useDatabaseExtensionsQuery({
     projectRef: project?.ref,
     connectionString: project?.connectionString,
   })
 
-  const neededExtensions = (extensions ?? []).filter((ext) =>
-    (integration?.requiredExtensions ?? []).includes(ext.name)
-  )
-
-  const hasMissingExtensions = neededExtensions.some((x) => !x.installed_version)
-
   if (!integration) {
     return <div>Unsupported integration type</div>
   }
+
+  const dependsOnExtension = (integration.requiredExtensions ?? []).length > 0
+
+  const installableExtensions = (extensions ?? []).filter((ext) =>
+    (integration.requiredExtensions ?? []).includes(ext.name)
+  )
+  const hasToInstallExtensions = installableExtensions.some((x) => !x.installed_version)
+
+  // The integration requires extensions that are not available to install on the current database image
+  const hasMissingExtensions =
+    installableExtensions.length !== integration.requiredExtensions.length
 
   return (
     <div className="flex flex-col gap-8 py-10">
@@ -65,19 +68,23 @@ export const IntegrationOverviewTab = ({
               className="max-w-full"
               content={`This integration uses the ${integration.requiredExtensions.map((x) => `\`${x}\``).join(', ')}
               extension${integration.requiredExtensions.length > 1 ? 's' : ''} directly in your Postgres database.
-              ${hasMissingExtensions ? `Install ${integration.requiredExtensions.length > 1 ? 'these' : 'this'} database extension${integration.requiredExtensions.length > 1 ? 's' : ''} to use ${integration.name} in your project.` : ''}
+              ${hasToInstallExtensions && !hasMissingExtensions ? `Install ${integration.requiredExtensions.length > 1 ? 'these' : 'this'} database extension${integration.requiredExtensions.length > 1 ? 's' : ''} to use ${integration.name} in your project.` : ''}
               `}
             />
 
-            <div className="flex flex-row gap-x-2">
-              {neededExtensions.map((extension) => (
-                <MissingExtensionAlert key={extension.name} extension={extension} />
-              ))}
-            </div>
+            {hasMissingExtensions ? (
+              integration.missingExtensionsAlert
+            ) : (
+              <div className="flex flex-row gap-x-2">
+                {installableExtensions.map((extension) => (
+                  <MissingExtensionAlert key={extension.name} extension={extension} />
+                ))}
+              </div>
+            )}
           </Admonition>
         </div>
       )}
-      {!!actions && !hasMissingExtensions && <div className="px-10 max-w-4xl">{actions}</div>}
+      {!!actions && !hasToInstallExtensions && <div className="px-10 max-w-4xl">{actions}</div>}
       <MarkdownContent key={integration.id} integrationId={integration.id} />
       <Separator />
       {children}
