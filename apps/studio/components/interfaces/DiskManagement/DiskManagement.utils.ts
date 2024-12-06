3ea50ddafe5103c3be9ffb26caa2d1d3fe62f89e
Joshen Lim <joshenlimek@gmail.com>
2024-12-06 18:04:12 +0800

    Fix

--- apps/studio/components/interfaces/DiskManagement/DiskManagement.utils.ts
+++ apps/studio/components/interfaces/DiskManagement/DiskManagement.utils.ts
@@ -104,9 +104,10 @@ export const calculateIOPSPrice = ({
       0,
       newProvisionedIOPS - DISK_LIMITS[DiskType.GP3].includedIops
     )
-    const oldPrice = oldChargeableIOPS * DISK_PRICING[oldStorageType].iops
-
-    const newPrice = newChargeableIOPS * DISK_PRICING[newStorageType].iops
+    // @ts-expect-error
+    const oldPrice = oldChargeableIOPS * DISK_PRICING[oldStorageType]?.iops ?? 0
+    // @ts-expect-error
+    const newPrice = newChargeableIOPS * DISK_PRICING[newStorageType]?.iops ?? 0
 
     return {
       oldPrice: (oldPrice * (1 + numReplicas)).toFixed(2),
@@ -117,8 +118,10 @@ export const calculateIOPSPrice = ({
       oldStorageType === 'gp3'
         ? (oldProvisionedIOPS - DISK_LIMITS[oldStorageType].includedIops) *
           DISK_PRICING[oldStorageType].iops
-        : oldProvisionedIOPS * DISK_PRICING[oldStorageType].iops
-    const newPrice = newProvisionedIOPS * DISK_PRICING[newStorageType].iops
+        : // @ts-expect-error
+          oldProvisionedIOPS * DISK_PRICING[oldStorageType]?.iops ?? 0
+    // @ts-expect-error
+    const newPrice = newProvisionedIOPS * DISK_PRICING[newStorageType]?.iops ?? 0
     return {
       oldPrice: (oldPrice * (1 + numReplicas)).toFixed(2),
       newPrice: (newPrice * (1 + numReplicas)).toFixed(2),
