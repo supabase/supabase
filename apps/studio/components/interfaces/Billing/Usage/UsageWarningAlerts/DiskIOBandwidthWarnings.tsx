dbdda469b55db814a30bbdda5513e615b4e09b22
hallidayo <22655069+Hallidayo@users.noreply.github.com>
2024-12-07 17:41:14 +0000

    changed to use admonition

--- apps/studio/components/interfaces/Billing/Usage/UsageWarningAlerts/DiskIOBandwidthWarnings.tsx
+++ apps/studio/components/interfaces/Billing/Usage/UsageWarningAlerts/DiskIOBandwidthWarnings.tsx
@@ -7,6 +7,7 @@ import {
   CriticalIcon,
   WarningIcon,
 } from 'ui'
+import { Admonition } from 'ui-patterns'
 
 // [Joshen] In the future, conditionals should be from resource exhaustion endpoint as single source of truth
 interface DiskIOBandwidthWarningsProps {
@@ -28,10 +29,12 @@ const DiskIOBandwidthWarnings = ({
 }: DiskIOBandwidthWarningsProps) => {
   if (hasLatest && latestIoBudgetConsumption >= 100) {
     return (
-        <Alert_Shadcn_ variant="destructive">
-          <WarningIcon />
-          <AlertTitle_Shadcn_>Your Disk IO Budget has been used up</AlertTitle_Shadcn_>
-          <AlertDescription_Shadcn_>
+      <Admonition
+        type="destructive"
+        title="Your Disk IO Budget has been used up"
+        description={
+          <>
+            {' '}
             <p className="mb-4">
               Your workload has used up all your Disk IO Budget and is now running at the baseline
               performance. If you need consistent disk performance, consider upgrading to a larger
@@ -42,74 +45,81 @@ const DiskIOBandwidthWarnings = ({
                 {isFreePlan ? 'Upgrade project' : 'Change compute add-on'}
               </Link>
             </Button>
-          </AlertDescription_Shadcn_>
-        </Alert_Shadcn_>
+          </>
+        }
+      />
     )
   }
 
   if (hasLatest && latestIoBudgetConsumption >= 80) {
     return (
-      <Alert_Shadcn_ variant="destructive">
-        <WarningIcon />
-        <AlertTitle_Shadcn_>You are close to running out of Disk IO Budget</AlertTitle_Shadcn_>
-        <AlertDescription_Shadcn_>
-          <p className="mb-4">
-            Your workload has consumed {latestIoBudgetConsumption}% of your Disk IO Budget. If you
-            use up all your Disk IO Budget, your instance will reverted to baseline performance. If
-            you need consistent disk performance, consider upgrading to a larger compute add-on.
-          </p>
-          <Button asChild type="danger">
-            <Link href={upgradeUrl}>
-              {isFreePlan ? 'Upgrade project' : 'Change compute add-on'}
-            </Link>
-          </Button>
-        </AlertDescription_Shadcn_>
-      </Alert_Shadcn_>
+      <Admonition
+        type="destructive"
+        title="You are close to running out of Disk IO Budget"
+        description={
+          <>
+            <p className="mb-4">
+              Your workload has consumed {latestIoBudgetConsumption}% of your Disk IO Budget. If you
+              use up all your Disk IO Budget, your instance will reverted to baseline performance.
+              If you need consistent disk performance, consider upgrading to a larger compute
+              add-on.
+            </p>
+            <Button asChild type="danger">
+              <Link href={upgradeUrl}>
+                {isFreePlan ? 'Upgrade project' : 'Change compute add-on'}
+              </Link>
+            </Button>
+          </>
+        }
+      />
     )
   }
 
   if (currentBillingCycleSelected && highestIoBudgetConsumption >= 100) {
     return (
-      <Alert_Shadcn_ variant="warning">
-        <CriticalIcon />
-        <AlertTitle_Shadcn_>You ran out of IO Budget at least once</AlertTitle_Shadcn_>
-        <AlertDescription_Shadcn_>
-          <p className="mb-4">
-            Your workload has used up all your Disk IO Budget and reverted to baseline performance
-            at least once during this billing cycle. If you need consistent disk performance,
-            consider upgrading to a larger compute add-on.
-          </p>
-          <Button asChild type="warning">
-            <Link href={upgradeUrl}>
-              {isFreePlan ? 'Upgrade project' : 'Change compute add-on'}
-            </Link>
-          </Button>
-        </AlertDescription_Shadcn_>
-      </Alert_Shadcn_>
+      <Admonition
+        type="warning"
+        title="You ran out of IO Budget at least once"
+        description={
+          <>
+            <p className="mb-4">
+              Your workload has used up all your Disk IO Budget and reverted to baseline performance
+              at least once during this billing cycle. If you need consistent disk performance,
+              consider upgrading to a larger compute add-on.
+            </p>
+            <Button asChild type="warning">
+              <Link href={upgradeUrl}>
+                {isFreePlan ? 'Upgrade project' : 'Change compute add-on'}
+              </Link>
+            </Button>
+          </>
+        }
+      />
     )
   }
 
   if (currentBillingCycleSelected && highestIoBudgetConsumption >= 80) {
     return (
-      <Alert_Shadcn_ variant="warning">
-        <CriticalIcon />
-        <AlertTitle_Shadcn_>
-          You were close to using all your IO Budget at least once
-        </AlertTitle_Shadcn_>
-        <AlertDescription_Shadcn_>
-          <p className="mb-4">
-            Your workload has consumed {highestIoBudgetConsumption}% of your Disk IO budget during
-            this billing cycle. If you use up all your Disk IO Budget, your instance will reverted
-            to baseline performance. If you need consistent disk performance, consider upgrading to
-            a larger compute add-on.
-          </p>
-          <Button asChild type="warning">
-            <Link href={upgradeUrl}>
-              {isFreePlan ? 'Upgrade project' : 'Change compute add-on'}
-            </Link>
-          </Button>
-        </AlertDescription_Shadcn_>
-      </Alert_Shadcn_>
+      <Admonition
+        type="warning"
+        title="You were close to using all your IO Budget at least once"
+        description={
+          <>
+            {' '}
+            <p className="mb-4">
+              Your workload has consumed {highestIoBudgetConsumption}% of your Disk IO budget during
+              this billing cycle. If you use up all your Disk IO Budget, your instance will reverted
+              to baseline performance. If you need consistent disk performance, consider upgrading
+              to a larger compute add-on.
+            </p>
+            <Button asChild type="warning">
+              <Link href={upgradeUrl}>
+                {isFreePlan ? 'Upgrade project' : 'Change compute add-on'}
+              </Link>
+            </Button>
+          </>
+        }
+      />
     )
   }
 
