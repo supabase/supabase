6785433a251e75f3ecce761c510b7b88da1d3d15
Bobbie Soedirgo <bobbie@soedirgo.dev>
2024-12-08 15:00:16 +0800

    chore: use @supabase/pg-meta for table privileges queries

--- packages/pg-meta/src/index.ts
+++ packages/pg-meta/src/index.ts
@@ -1,9 +1,11 @@
 import roles from './pg-meta-roles'
 import schemas from './pg-meta-schemas'
 import * as functions from './pg-meta-functions'
+import tablePrivileges from './pg-meta-table-privileges'
 
 export default {
   roles,
   schemas,
   functions,
+  tablePrivileges,
 }
