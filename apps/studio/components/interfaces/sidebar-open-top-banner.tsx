882ed67bbd31a303629b7165cd15203d3809e7e3
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-12-19 00:35:12 +0800

    reverse top bar

--- apps/studio/components/interfaces/sidebar-open-top-banner.tsx
+++ apps/studio/components/interfaces/sidebar-open-top-banner.tsx
@@ -29,97 +29,97 @@ export function SidebarOpenTopBanner() {
 
   return (
     <AnimatePresence>
-      {/* {state === 'collapsed' && ( */}
-      <motion.div
-        // initial={{ opacity: 0, x: -20, height: 0 }}
-        // animate={{
-        //   opacity: 1,
-        //   x: 0,
-        //   height: 'auto',
-        //   transition: { duration: 0.22, delay: 0.4 },
-        // }}
-        // exit={{
-        //   opacity: 0,
-        //   x: 20,
-        //   height: 0,
-        //   transition: { duration: 0.05 },
-        // }}
-        // style={{
-        //   paddingLeft: open ? '16px' : '8px',
-        //   paddingRight: open ? '16px' : '8px',
-        // }}
-        className="relative items-center flex w-full gap-5 pt-[6px] pb-[1px]"
-      >
-        {/* <SidebarOpenButton /> */}
-        <AnimatePresence>
-          {project && (
-            <motion.button
-              onClick={() => router.push(`/org/${org?.slug}`)}
-              className="group/org-back-button text-foreground-lighter flex items-center gap-1 hover:text-foreground text-xs cursor-pointer"
-              initial={{ opacity: 0, x: -20, width: 0 }}
-              animate={{ opacity: 1, x: 0, width: 'auto' }}
-              exit={{ opacity: 0, x: 20, width: 0 }}
-              transition={{ duration: 0.2 }}
-            >
-              <CornerLeftUp
-                size={14}
-                strokeWidth={1}
-                className="text-forefground-lighter group-hover/org-back-button:text-foreground group-hover/org-back-button:translate-y-[-2px] transition-transform"
-              />
-              Organization
-            </motion.button>
-          )}
-        </AnimatePresence>
-        <div className="flex items-center -space-x-px">
-          <OrganizationDropdown />
-          {project && <ProjectDropdown />}
+      {state === 'collapsed' && (
+        <motion.div
+          initial={{ opacity: 0, x: -20, height: 0 }}
+          animate={{
+            opacity: 1,
+            x: 0,
+            height: 'auto',
+            transition: { duration: 0.22, delay: 0.4 },
+          }}
+          exit={{
+            opacity: 0,
+            x: 20,
+            height: 0,
+            transition: { duration: 0.05 },
+          }}
+          style={{
+            paddingLeft: open ? '16px' : '8px',
+            paddingRight: open ? '16px' : '8px',
+          }}
+          className="relative items-center flex w-full gap-5 pt-[6px] pb-[1px]"
+        >
+          {/* <SidebarOpenButton /> */}
           <AnimatePresence>
-            {project?.ref && (
-              <motion.div
-                className="flex items-center gap-x-3"
-                initial={{ opacity: 0, x: -20 }}
-                animate={{ opacity: 1, x: 0 }}
-                exit={{ opacity: 0, x: -20 }}
-                transition={{
-                  duration: 0.15,
-                  ease: 'easeOut',
-                }}
+            {project && (
+              <motion.button
+                onClick={() => router.push(`/org/${org?.slug}`)}
+                className="group/org-back-button text-foreground-lighter flex items-center gap-1 hover:text-foreground text-xs cursor-pointer"
+                initial={{ opacity: 0, x: -20, width: 0 }}
+                animate={{ opacity: 1, x: 0, width: 'auto' }}
+                exit={{ opacity: 0, x: 20, width: 0 }}
+                transition={{ duration: 0.2 }}
               >
-                {connectDialogUpdate && <Connect />}
-                {!isBranchingEnabled && <EnableBranchingButton />}
-              </motion.div>
+                <CornerLeftUp
+                  size={14}
+                  strokeWidth={1}
+                  className="text-forefground-lighter group-hover/org-back-button:text-foreground group-hover/org-back-button:translate-y-[-2px] transition-transform"
+                />
+                Organization
+              </motion.button>
             )}
           </AnimatePresence>
-        </div>
-        <div className="flex items-center gap-x-2 w-full justify-end">
-          {IS_PLATFORM && (
-            <>
-              <FeedbackDropdown />
-              <NotificationsPopoverV2 />
-              <HelpPopover />
-            </>
+          <div className="flex items-center -space-x-px">
+            <OrganizationDropdown />
+            {project && <ProjectDropdown />}
+            <AnimatePresence>
+              {project?.ref && (
+                <motion.div
+                  className="flex items-center gap-x-3"
+                  initial={{ opacity: 0, x: -20 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  exit={{ opacity: 0, x: -20 }}
+                  transition={{
+                    duration: 0.15,
+                    ease: 'easeOut',
+                  }}
+                >
+                  {connectDialogUpdate && <Connect />}
+                  {!isBranchingEnabled && <EnableBranchingButton />}
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </div>
+          <div className="flex items-center gap-x-2 w-full justify-end">
+            {IS_PLATFORM && (
+              <>
+                <FeedbackDropdown />
+                <NotificationsPopoverV2 />
+                <HelpPopover />
+              </>
+            )}
+          </div>
+          {!!project?.ref && (
+            <motion.div
+              className="border-l flex-0 h-full"
+              initial={{ opacity: 0, x: -20 }}
+              animate={{
+                opacity: !aiAssistantPanel.open ? 1 : 0,
+                x: !aiAssistantPanel.open ? 0 : -20,
+                width: aiAssistantPanel.open ? 0 : 'auto',
+              }}
+              exit={{ opacity: 0, x: -20 }}
+              transition={{
+                duration: 0.15,
+                ease: 'easeOut',
+              }}
+            >
+              <AssistantButton />
+            </motion.div>
           )}
-        </div>
-        {!!project?.ref && (
-          <motion.div
-            className="border-l flex-0 h-full"
-            initial={{ opacity: 0, x: -20 }}
-            animate={{
-              opacity: !aiAssistantPanel.open ? 1 : 0,
-              x: !aiAssistantPanel.open ? 0 : -20,
-              width: aiAssistantPanel.open ? 0 : 'auto',
-            }}
-            exit={{ opacity: 0, x: -20 }}
-            transition={{
-              duration: 0.15,
-              ease: 'easeOut',
-            }}
-          >
-            <AssistantButton />
-          </motion.div>
-        )}
-      </motion.div>
-      {/* )} */}
+        </motion.div>
+      )}
     </AnimatePresence>
   )
 }
