e6efee12e78a771b927a36e089a14e60f63ca309
Charis <26616127+charislam@users.noreply.github.com>
2024-12-28 16:57:59 -0500

    hotfix: disable guide revalidation (#31948)

--- apps/docs/app/guides/troubleshooting/page.tsx
+++ apps/docs/app/guides/troubleshooting/page.tsx
@@ -17,7 +17,7 @@ import { SidebarSkeleton } from '~/layouts/MainSkeleton'
 import { PROD_URL } from '~/lib/constants'
 
 // 60 seconds/minute * 60 minutes/hour * 24 hours/day
-export const revalidate = 86_400
+// export const revalidate = 86_400
 
 export default async function GlobalTroubleshootingPage() {
   const troubleshootingEntries = await getAllTroubleshootingEntries()
