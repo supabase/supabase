255965c957f03478558e6c3ae2e8484cc7de3401
Alaister Young <a@alaisteryoung.com>
2025-01-02 14:47:32 +1100

    update logic to stringify initialValue before formatting

--- apps/studio/components/grid/components/editor/JsonEditor.tsx
+++ apps/studio/components/grid/components/editor/JsonEditor.tsx
@@ -1,4 +1,3 @@
-import { isNil } from 'lodash'
 import { Maximize } from 'lucide-react'
 import { useCallback, useState } from 'react'
 import type { RenderEditCellProps } from 'react-data-grid'
@@ -65,13 +64,15 @@ export const JsonEditor = <TRow, TSummaryRow = unknown>({
   })
 
   const gridColumn = state.gridColumns.find((x) => x.name == column.key)
-  const initialValue = row[column.key as keyof TRow]
-  const stringifiedValue = Array.isArray(initialValue)
-    ? JSON.stringify(initialValue)
-    : (initialValue as string)
-  const jsonString = prettifyJSON(
-    !isNil(stringifiedValue) ? tryFormatInitialValue(stringifiedValue) : ''
-  )
+
+  const rawInitialValue = row[column.key as keyof TRow] as unknown
+  const initialValue =
+    rawInitialValue === null || rawInitialValue === undefined || typeof rawInitialValue === 'string'
+      ? rawInitialValue
+      : JSON.stringify(rawInitialValue)
+
+  const jsonString = prettifyJSON(initialValue ? tryFormatInitialValue(initialValue) : '')
+
   const isTruncated =
     typeof initialValue === 'string' &&
     initialValue.endsWith('...') &&
