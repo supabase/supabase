8d00fd0aba1ba3cea49a4eae98749ba56e1ad86f
Kyle Rummens <58995471+kylerummens@users.noreply.github.com>
2025-01-01 21:52:25 -0700

    fix(dashboard): Improve scrollbar position on Table Editor list (#32505)
    * Improve padding on infinite table list when scrollbar is visible
    
    * Add back removed bottom padding
    
    ---------
    
    Co-authored-by: Kyle Rummens <kyle@prodatakey.com>

--- apps/studio/components/layouts/TableEditorLayout/TableEditorMenu.tsx
+++ apps/studio/components/layouts/TableEditorLayout/TableEditorMenu.tsx
@@ -269,7 +269,7 @@ const TableEditorMenu = () => {
                 />
               )}
               {(entityTypes?.length ?? 0) > 0 && (
-                <div className="flex flex-1" data-testid="tables-list">
+                <div className="flex flex-1 -mx-2" data-testid="tables-list">
                   <InfiniteList
                     items={entityTypes}
                     ItemComponent={EntityListItem}
