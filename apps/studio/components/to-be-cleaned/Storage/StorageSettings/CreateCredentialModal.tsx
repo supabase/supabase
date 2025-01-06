e00ba41fd1fe290480eda3fb3080c6ad4adf30d4
Joshen Lim <joshenlimek@gmail.com>
2025-01-06 11:52:46 +0800

    Chore/toggle for storage client connection s3 (#31209)
    * Scaffold UI components
    
    * Hook up API
    
    * Fix type

--- apps/studio/components/to-be-cleaned/Storage/StorageSettings/CreateCredentialModal.tsx
+++ apps/studio/components/to-be-cleaned/Storage/StorageSettings/CreateCredentialModal.tsx
@@ -27,6 +27,8 @@ import {
 } from 'ui'
 import { Input } from 'ui-patterns/DataInputs/Input'
 import { FormItemLayout } from 'ui-patterns/form/FormItemLayout/FormItemLayout'
+import { useProjectStorageConfigQuery } from 'data/config/project-storage-config-query'
+import { ButtonTooltip } from 'components/ui/ButtonTooltip'
 
 interface CreateCredentialModalProps {
   visible: boolean
@@ -39,6 +41,10 @@ export const CreateCredentialModal = ({ visible, onOpenChange }: CreateCredentia
   const [showSuccess, setShowSuccess] = useState(false)
 
   const canCreateCredentials = useCheckPermissions(PermissionAction.STORAGE_ADMIN_WRITE, '*')
+
+  const { data: config } = useProjectStorageConfigQuery({ projectRef })
+  const isS3ConnectionEnabled = config?.features.s3Protocol.enabled
+  const disableCreation = !isProjectActive || !canCreateCredentials || !isS3ConnectionEnabled
 
   const FormSchema = z.object({
     description: z.string().min(3, {
@@ -78,22 +84,20 @@ export const CreateCredentialModal = ({ visible, onOpenChange }: CreateCredentia
       <Tooltip_Shadcn_>
         <TooltipTrigger_Shadcn_ asChild>
           <DialogTrigger asChild>
-            <Button
-              type="default"
-              disabled={!isProjectActive || !canCreateCredentials}
-              className="pointer-events-auto"
-            >
+            <Button type="default" disabled={disableCreation} className="pointer-events-auto">
               New access key
             </Button>
           </DialogTrigger>
         </TooltipTrigger_Shadcn_>
-        {(!isProjectActive || !canCreateCredentials) && (
+        {disableCreation && (
           <TooltipContent_Shadcn_ side="bottom">
             {!isProjectActive
               ? 'Restore your project to create new access keys'
-              : !canCreateCredentials
-                ? 'You need additional permissions to create new access keys'
-                : ''}
+              : !isS3ConnectionEnabled
+                ? 'Connection via S3 protocol is currently disabled'
+                : !canCreateCredentials
+                  ? 'You need additional permissions to create new access keys'
+                  : ''}
           </TooltipContent_Shadcn_>
         )}
       </Tooltip_Shadcn_>
@@ -159,7 +163,7 @@ export const CreateCredentialModal = ({ visible, onOpenChange }: CreateCredentia
                 <DialogSection>
                   <FormField
                     name="description"
-                    render={({ field }) => (
+                    render={() => (
                       <FormItemLayout label="Description">
                         <Input
                           autoComplete="off"
