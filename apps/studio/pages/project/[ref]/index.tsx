f4635a2dff1232ef5afdc9729104020da4c8cf6f
Joshen Lim <joshenlimek@gmail.com>
2025-01-07 11:33:57 +0700

    Remove connectDialogUpdate flag and clean up old connection string components

--- apps/studio/pages/project/[ref]/index.tsx
+++ apps/studio/pages/project/[ref]/index.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useRef } from 'react'
 
 import { useParams } from 'common'
-import Connect from 'components/interfaces/Connect/Connect'
 import { ClientLibrary, ExampleProject } from 'components/interfaces/Home'
 import { CLIENT_LIBRARIES, EXAMPLE_PROJECTS } from 'components/interfaces/Home/Home.constants'
 import ProjectUsageSection from 'components/interfaces/Home/ProjectUsageSection'
@@ -14,7 +13,6 @@ import { InlineLink } from 'components/ui/InlineLink'
 import ProjectUpgradeFailedBanner from 'components/ui/ProjectUpgradeFailedBanner'
 import { useSelectedOrganization } from 'hooks/misc/useSelectedOrganization'
 import { useIsOrioleDb, useSelectedProject } from 'hooks/misc/useSelectedProject'
-import { useFlag } from 'hooks/ui/useFlag'
 import { IS_PLATFORM, PROJECT_STATUS } from 'lib/constants'
 import { useAppStateSnapshot } from 'state/app-state'
 import type { NextPageWithLayout } from 'types'
@@ -30,11 +28,8 @@ import {
 } from 'ui'
 
 const Home: NextPageWithLayout = () => {
-  const connectDialogUpdate = useFlag('connectDialogUpdate')
-
   const organization = useSelectedOrganization()
   const project = useSelectedProject()
-
   const isOrioleDb = useIsOrioleDb()
   const snap = useAppStateSnapshot()
   const { enableBranching } = useParams()
@@ -85,9 +80,6 @@ const Home: NextPageWithLayout = () => {
         <div className="flex items-center gap-x-3">
           {project?.status === PROJECT_STATUS.ACTIVE_HEALTHY && <SecurityStatus />}
           {IS_PLATFORM && project?.status === PROJECT_STATUS.ACTIVE_HEALTHY && <ServiceStatus />}
-          {IS_PLATFORM &&
-            project?.status === PROJECT_STATUS.ACTIVE_HEALTHY &&
-            !connectDialogUpdate && <Connect />}
         </div>
       </div>
 
