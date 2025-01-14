f2779455dc650ed524eaf2e1d41c38ca87b6ecc8
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2025-01-14 22:51:12 +0100

    remove component

--- packages/ui/src/components/TextLink/TextLink.tsx
+++ packages/ui/src/components/TextLink/TextLink.tsx
@@ -3,7 +3,6 @@
 import Link from 'next/link'
 import { cn } from '../../lib/utils/cn'
 import { ChevronRight } from 'lucide-react'
-import { Badge } from '../../..'
 
 interface Props {
   label: string
