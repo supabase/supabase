ce6fab19516e66f1e23ac9d217f84d3fae129c76
Joshen Lim <joshenlimek@gmail.com>
2025-01-14 10:46:19 +0800

    Smol fix

--- apps/studio/components/interfaces/Integrations/Queues/QueuesSettings.tsx
+++ apps/studio/components/interfaces/Integrations/Queues/QueuesSettings.tsx
@@ -206,7 +206,7 @@ export const QueuesSettings = () => {
                               <code className="text-xs">send_batch</code>,{' '}
                               <code className="text-xs">read</code>,{' '}
                               <code className="text-xs">pop</code>,
-                              <code className="text-xs">archive</code>, and
+                              <code className="text-xs">archive</code>, and{' '}
                               <code className="text-xs">delete</code>
                             </p>
                           </>
