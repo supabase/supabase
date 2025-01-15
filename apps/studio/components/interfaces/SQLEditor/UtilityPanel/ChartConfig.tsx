994cd1dcc7ec61fa789f60d76b81f60d826d12d3
Jordi Enric <jordi.err@gmail.com>
2025-01-15 15:35:08 +0100

    validate Y axis before flip to prevent crash

--- apps/studio/components/interfaces/SQLEditor/UtilityPanel/ChartConfig.tsx
+++ apps/studio/components/interfaces/SQLEditor/UtilityPanel/ChartConfig.tsx
@@ -17,6 +17,7 @@ import {
   SelectTrigger_Shadcn_,
   Select_Shadcn_,
 } from 'ui'
+import { toast } from 'sonner'
 
 type Results = { rows: readonly any[] }
 
@@ -147,112 +148,116 @@ export const ChartConfig = ({
     }
   }
 
+  const onFlip = () => {
+    const newY = config.xKey
+    const newX = config.yKey
+
+    const value = results.rows[0]?.[newY]
+    if (typeof value !== 'number' && isNaN(Number(value))) {
+      toast.error('Y-axis must be a number')
+      return
+    }
+    onConfigChange({ ...config, xKey: newY, yKey: newX })
+  }
+
   return (
     <ResizablePanelGroup direction="horizontal" className="flex-grow h-full">
       <ResizablePanel className="p-4 h-full" defaultSize={75}>
         <ChartPanel />
       </ResizablePanel>
-      {hasResults && (
-        <>
-          <ResizableHandle withHandle />
-          <ResizablePanel
-            defaultSize={25}
-            minSize={15}
-            className="px-3 py-3 space-y-4 !overflow-y-auto"
+      <ResizableHandle withHandle />
+      <ResizablePanel
+        defaultSize={25}
+        minSize={15}
+        className="px-3 py-3 space-y-4 !overflow-y-auto"
+      >
+        <div className="flex justify-between items-center h-5">
+          <h2 className="text-sm text-foreground-lighter">Chart options</h2>
+          {config.xKey && config.yKey && (
+            <Button
+              type="text"
+              onClick={onFlip}
+              title="Swap X and Y axis"
+              icon={<ArrowUpDown size="15" className="text-foreground-lighter" />}
+            >
+              Flip
+            </Button>
+          )}
+        </div>
+
+        <div>
+          <Label_Shadcn_ className="text-xs text-foreground-light">X Axis</Label_Shadcn_>
+          <Select_Shadcn_
+            value={config.xKey}
+            onValueChange={(value) => {
+              onConfigChange({ ...config, xKey: value })
+            }}
           >
-            <div className="flex justify-between items-center h-5">
-              <h2 className="text-sm text-foreground-lighter">Chart options</h2>
-              {config.xKey && config.yKey && (
-                <Button
-                  type="text"
-                  onClick={() => {
-                    const currentX = config.xKey
-                    const currentY = config.yKey
-                    onConfigChange({ ...config, xKey: currentY, yKey: currentX })
-                  }}
-                  title="Swap X and Y axis"
-                  icon={<ArrowUpDown size="15" className="text-foreground-lighter" />}
-                >
-                  Flip
-                </Button>
-              )}
-            </div>
+            <SelectTrigger_Shadcn_>{config.xKey || 'Select X Axis'}</SelectTrigger_Shadcn_>
+            <SelectContent_Shadcn_>
+              <SelectGroup_Shadcn_>
+                {resultKeys.map((key) => (
+                  <SelectItem_Shadcn_ value={key} key={key}>
+                    {key}
+                  </SelectItem_Shadcn_>
+                ))}
+              </SelectGroup_Shadcn_>
+            </SelectContent_Shadcn_>
+          </Select_Shadcn_>
+        </div>
 
-            <div>
-              <Label_Shadcn_ className="text-xs text-foreground-light">X Axis</Label_Shadcn_>
-              <Select_Shadcn_
-                value={config.xKey}
-                onValueChange={(value) => {
-                  onConfigChange({ ...config, xKey: value })
-                }}
-              >
-                <SelectTrigger_Shadcn_>{config.xKey || 'Select X Axis'}</SelectTrigger_Shadcn_>
-                <SelectContent_Shadcn_>
-                  <SelectGroup_Shadcn_>
-                    {resultKeys.map((key) => (
-                      <SelectItem_Shadcn_ value={key} key={key}>
-                        {key}
-                      </SelectItem_Shadcn_>
-                    ))}
-                  </SelectGroup_Shadcn_>
-                </SelectContent_Shadcn_>
-              </Select_Shadcn_>
-            </div>
+        <div>
+          <Label_Shadcn_ className="text-xs text-foreground-light">Y Axis</Label_Shadcn_>
+          <Select_Shadcn_
+            value={config.yKey}
+            onValueChange={(value) => {
+              onConfigChange({ ...config, yKey: value })
+            }}
+          >
+            <SelectTrigger_Shadcn_>{config.yKey || 'Select Y Axis'}</SelectTrigger_Shadcn_>
+            <SelectContent_Shadcn_>
+              <SelectGroup_Shadcn_>
+                {yAxisKeys.map((key) => (
+                  <SelectItem_Shadcn_ value={key} key={key}>
+                    {key}
+                  </SelectItem_Shadcn_>
+                ))}
+              </SelectGroup_Shadcn_>
+            </SelectContent_Shadcn_>
+          </Select_Shadcn_>
+        </div>
+        <div className="*:flex *:gap-2 *:items-center grid gap-2 *:text-foreground-light *:p-1.5 *:pl-0">
+          <Label_Shadcn_ className="" htmlFor="cumulative">
+            <Checkbox_Shadcn_
+              id="cumulative"
+              name="cumulative"
+              checked={config.cumulative}
+              onClick={() => onConfigChange({ ...config, cumulative: !config.cumulative })}
+            />
+            Cumulative
+          </Label_Shadcn_>
 
-            <div>
-              <Label_Shadcn_ className="text-xs text-foreground-light">Y Axis</Label_Shadcn_>
-              <Select_Shadcn_
-                value={config.yKey}
-                onValueChange={(value) => {
-                  onConfigChange({ ...config, yKey: value })
-                }}
-              >
-                <SelectTrigger_Shadcn_>{config.yKey || 'Select Y Axis'}</SelectTrigger_Shadcn_>
-                <SelectContent_Shadcn_>
-                  <SelectGroup_Shadcn_>
-                    {yAxisKeys.map((key) => (
-                      <SelectItem_Shadcn_ value={key} key={key}>
-                        {key}
-                      </SelectItem_Shadcn_>
-                    ))}
-                  </SelectGroup_Shadcn_>
-                </SelectContent_Shadcn_>
-              </Select_Shadcn_>
-            </div>
-            <div className="*:flex *:gap-2 *:items-center grid gap-2 *:text-foreground-light *:p-1.5 *:pl-0">
-              <Label_Shadcn_ className="" htmlFor="cumulative">
-                <Checkbox_Shadcn_
-                  id="cumulative"
-                  name="cumulative"
-                  checked={config.cumulative}
-                  onClick={() => onConfigChange({ ...config, cumulative: !config.cumulative })}
-                />
-                Cumulative
-              </Label_Shadcn_>
+          <Label_Shadcn_ htmlFor="showLabels">
+            <Checkbox_Shadcn_
+              id="showLabels"
+              name="showLabels"
+              checked={config.showLabels}
+              onClick={() => onConfigChange({ ...config, showLabels: !config.showLabels })}
+            />
+            Show labels
+          </Label_Shadcn_>
 
-              <Label_Shadcn_ htmlFor="showLabels">
-                <Checkbox_Shadcn_
-                  id="showLabels"
-                  name="showLabels"
-                  checked={config.showLabels}
-                  onClick={() => onConfigChange({ ...config, showLabels: !config.showLabels })}
-                />
-                Show labels
-              </Label_Shadcn_>
-
-              <Label_Shadcn_ htmlFor="showGrid">
-                <Checkbox_Shadcn_
-                  id="showGrid"
-                  name="showGrid"
-                  checked={config.showGrid}
-                  onClick={() => onConfigChange({ ...config, showGrid: !config.showGrid })}
-                />
-                Show grid
-              </Label_Shadcn_>
-            </div>
-          </ResizablePanel>
-        </>
-      )}
+          <Label_Shadcn_ htmlFor="showGrid">
+            <Checkbox_Shadcn_
+              id="showGrid"
+              name="showGrid"
+              checked={config.showGrid}
+              onClick={() => onConfigChange({ ...config, showGrid: !config.showGrid })}
+            />
+            Show grid
+          </Label_Shadcn_>
+        </div>
+      </ResizablePanel>
     </ResizablePanelGroup>
   )
 }
