0095f24625826d05d8e22c917673f9f8b1f2e016
Ivan Vasilov <vasilov.ivan@gmail.com>
2025-01-16 18:46:49 +0100

    Revert some of the unneeded changes.

--- tests/local-studio-tests/playwright.config.ts
+++ tests/local-studio-tests/playwright.config.ts
@@ -21,8 +21,6 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  /* Timeout for all tests. Extended from the default 30 seconds because of Github actions */
-  timeout: 60000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
