7943a71f7b74adc730d9760d30fdcdc5277f76ea
Jordi Enric <jordi.err@gmail.com>
2025-01-16 11:52:38 +0100

    rm unnecessary connect rule

--- apps/studio/next.config.js
+++ apps/studio/next.config.js
@@ -77,7 +77,6 @@ const csp = [
       ]
     : [
         `default-src 'self' ${DEFAULT_SRC_URLS} ${PUSHER_URL_WS} ${SENTRY_URL};`,
-        `connect-src 'self' ${DEFAULT_SRC_URLS};`,
         `script-src 'self' 'unsafe-eval' 'unsafe-inline' ${SCRIPT_SRC_URLS} ${VERCEL_LIVE_URL} ${PUSHER_URL};`,
         `frame-src 'self' ${FRAME_SRC_URLS} ${VERCEL_LIVE_URL};`,
         `img-src 'self' blob: data: ${IMG_SRC_URLS} ;`,
