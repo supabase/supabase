67cbbaacf27e8abda48ffc5ca3fbc63c6294ff84
Ivan Vasilov <vasilov.ivan@gmail.com>
2025-01-17 10:52:46 +0100

    fix: Unify the design for account pages (#30891)
    Refactor all account pages to use ScaffoldContainer and have the same design.

--- apps/studio/pages/account/audit.tsx
+++ apps/studio/pages/account/audit.tsx
@@ -1,20 +1,24 @@
 import { AuditLogs } from 'components/interfaces/Account'
 import AccountLayout from 'components/layouts/AccountLayout/AccountLayout'
-import { ScaffoldContainerLegacy } from 'components/layouts/Scaffold'
-import { FormHeader } from 'components/ui/Forms/FormHeader'
+import {
+  ScaffoldContainer,
+  ScaffoldDescription,
+  ScaffoldHeader,
+  ScaffoldTitle,
+} from 'components/layouts/Scaffold'
 import type { NextPageWithLayout } from 'types'
 
 const User: NextPageWithLayout = () => {
   return (
-    <ScaffoldContainerLegacy className="gap-0">
-      <div className="flex items-center justify-between">
-        <FormHeader
-          title="Account audit logs"
-          description="View the audit log trail of actions made from your account"
-        />
-      </div>
+    <ScaffoldContainer>
+      <ScaffoldHeader>
+        <ScaffoldTitle>Account Audit Logs</ScaffoldTitle>
+        <ScaffoldDescription>
+          View the audit log trail of actions made from your account
+        </ScaffoldDescription>
+      </ScaffoldHeader>
       <AuditLogs />
-    </ScaffoldContainerLegacy>
+    </ScaffoldContainer>
   )
 }
 
