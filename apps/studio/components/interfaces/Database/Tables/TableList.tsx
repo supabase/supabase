cf0a5f90aada66ea11137d40c9bdcc7a8d10435a
Ivan Vasilov <vasilov.ivan@gmail.com>
2025-01-17 10:54:02 +0100

    chore: Migrate tooltips in studio to use shadcn tooltips (#32679)
    * Remove radix-popover and radix-tooltip as deps from studio.
    
    * Migrate all uses of tooltip to shadcn tooltips.
    
    * Add tooltip portal to all shadcn tooltips.
    
    * Fix a warning about MenuItem not accepting refs in StorageMenu.
    
    * Remove unneeded styling for the tooltips.
    
    * Set a default delay duration for all tooltips in Studio.
    
    * Fix a type error.
    
    * Check and fix all tooltips.

--- apps/studio/components/interfaces/Database/Tables/TableList.tsx
+++ apps/studio/components/interfaces/Database/Tables/TableList.tsx
@@ -1,4 +1,3 @@
-import * as Tooltip from '@radix-ui/react-tooltip'
 import type { PostgresTable } from '@supabase/postgres-meta'
 import { PermissionAction } from '@supabase/shared-types/out/constants'
 import { noop } from 'lodash'
@@ -406,27 +405,16 @@ const TableList = ({
                       <Table.td>
                         {/* only show tooltips if required, to reduce noise */}
                         {x.name.length > 20 ? (
-                          <Tooltip.Root delayDuration={0} disableHoverableContent={true}>
-                            <Tooltip.Trigger
+                          <Tooltip_Shadcn_ disableHoverableContent={true}>
+                            <TooltipTrigger_Shadcn_
                               asChild
                               className="max-w-[95%] overflow-hidden text-ellipsis whitespace-nowrap"
                             >
                               <p>{x.name}</p>
-                            </Tooltip.Trigger>
-                            <Tooltip.Portal>
-                              <Tooltip.Content side="bottom">
-                                <Tooltip.Arrow className="radix-tooltip-arrow" />
-                                <div
-                                  className={[
-                                    'rounded bg-scale-100 py-1 px-2 leading-none shadow',
-                                    'border border-scale-200',
-                                  ].join(' ')}
-                                >
-                                  <span className="text-xs text-foreground">{x.name}</span>
-                                </div>
-                              </Tooltip.Content>
-                            </Tooltip.Portal>
-                          </Tooltip.Root>
+                            </TooltipTrigger_Shadcn_>
+
+                            <TooltipContent_Shadcn_ side="bottom">{x.name}</TooltipContent_Shadcn_>
+                          </Tooltip_Shadcn_>
                         ) : (
                           <p>{x.name}</p>
                         )}
