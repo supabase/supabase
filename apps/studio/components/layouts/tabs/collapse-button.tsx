c04ac90bd858a49261cd64d2c8d7cd5672f60568
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2025-01-28 19:08:44 +0800

    Update collapse-button.tsx

--- apps/studio/components/layouts/tabs/collapse-button.tsx
+++ apps/studio/components/layouts/tabs/collapse-button.tsx
@@ -2,46 +2,34 @@ import { PanelLeftClose, PanelLeftOpen } from 'lucide-react'
 import { useEffect } from 'react'
 import { cn } from 'ui'
 import { useSnapshot } from 'valtio'
-import { sidebarState } from '../tabs/sidebar-state'
 
 export function CollapseButton({ hideTabs }: { hideTabs: boolean }) {
-  const sidebar = useSnapshot(sidebarState)
-
-  useEffect(() => {
-    function handleKeyDown(event: KeyboardEvent) {
-      if (event.key.toLowerCase() === 'b' && (event.metaKey || event.ctrlKey)) {
-        event.preventDefault()
-        sidebarState.isOpen = !sidebarState.isOpen
-      }
-    }
-
-    window.addEventListener('keydown', handleKeyDown)
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown)
-    }
-  }, [])
-
   return (
     <button
       className={cn(
         'flex items-center justify-center w-10 h-10 hover:bg-surface-100 shrink-0',
         !hideTabs && 'border-b border-b-default'
       )}
-      onClick={() => (sidebarState.isOpen = !sidebar.isOpen)}
+      onClick={() => {
+        // handle sidebar collapse here
+      }}
     >
-      {sidebar.isOpen ? (
-        <PanelLeftClose
-          size={16}
-          strokeWidth={1.5}
-          className="text-foreground-lighter hover:text-foreground-light"
-        />
-      ) : (
-        <PanelLeftOpen
-          size={16}
-          strokeWidth={1.5}
-          className="text-foreground-lighter hover:text-foreground-light"
-        />
-      )}
+      {
+        // sidebar.isOpen
+        true ? (
+          <PanelLeftClose
+            size={16}
+            strokeWidth={1.5}
+            className="text-foreground-lighter hover:text-foreground-light"
+          />
+        ) : (
+          <PanelLeftOpen
+            size={16}
+            strokeWidth={1.5}
+            className="text-foreground-lighter hover:text-foreground-light"
+          />
+        )
+      }
     </button>
   )
 }
