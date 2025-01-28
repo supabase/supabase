6fa945226cde7d55f831c00dbde54129e7dbdf1e
Alaister Young <a@alaisteryoung.com>
2025-01-28 20:02:34 +0800

    fix home link

--- apps/studio/components/layouts/ProjectLayout/LayoutHeader/LayoutHeader.tsx
+++ apps/studio/components/layouts/ProjectLayout/LayoutHeader/LayoutHeader.tsx
@@ -46,6 +46,7 @@ interface LayoutHeaderProps {
   showProductMenu?: boolean
   customSidebarContent?: ReactNode
   handleMobileMenu: Function
+  showHomeLink?: boolean
 }
 
 const LayoutHeader = ({
@@ -54,6 +55,7 @@ const LayoutHeader = ({
   headerBorder = true,
   showProductMenu,
   handleMobileMenu,
+  showHomeLink = false,
 }: LayoutHeaderProps) => {
   const router = useRouter()
   const { ref: projectRef } = useParams()
@@ -103,16 +105,18 @@ const LayoutHeader = ({
       <div className="relative flex flex-1 overflow-hidden">
         <div className="flex w-full items-center justify-between py-2 pl-1 pr-3 md:px-3 flex-nowrap overflow-x-auto no-scrollbar">
           <div className="flex items-center text-sm">
-            <Link
-              href={IS_PLATFORM ? '/projects' : `/project/${projectRef}`}
-              className="mx-1 hidden md:flex items-center w-[40px] h-[40px]"
-            >
-              <img
-                alt="Supabase"
-                src={`${router.basePath}/img/supabase-logo.svg`}
-                className="absolute h-[40px] w-6 cursor-pointer rounded"
-              />
-            </Link>
+            {showHomeLink && (
+              <Link
+                href={IS_PLATFORM ? '/projects' : `/project/${projectRef}`}
+                className="mx-1 hidden md:flex items-center w-[40px] h-[40px]"
+              >
+                <img
+                  alt="Supabase"
+                  src={`${router.basePath}/img/supabase-logo.svg`}
+                  className="absolute h-[40px] w-6 cursor-pointer rounded"
+                />
+              </Link>
+            )}
 
             {projectRef && (
               <>
--- apps/studio/components/layouts/ProjectLayout/ProjectLayout.tsx
+++ apps/studio/components/layouts/ProjectLayout/ProjectLayout.tsx
@@ -186,6 +186,7 @@ const ProjectLayout = forwardRef<HTMLDivElement, PropsWithChildren<ProjectLayout
               <LayoutHeader
                 showProductMenu={!!(showProductMenu && productMenu)}
                 handleMobileMenu={handleMobileMenu}
+                showHomeLink={true}
               />
             )}
             {/* <div className="flex h-full flex-row grow overflow-y-auto"></div> */}
