32a4ddfd4a848a50686bf33ce199495f50812321
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2025-01-28 18:59:27 +0800

    revert

--- apps/studio/components/interfaces/Database/Schemas/SchemaGraphLegend.tsx
+++ apps/studio/components/interfaces/Database/Schemas/SchemaGraphLegend.tsx
@@ -1,49 +1,31 @@
 import { DiamondIcon, Fingerprint, Hash, Key } from 'lucide-react'
 
 export const SchemaGraphLegend = () => {
-  const SIZE = 12
-
   return (
-    <div className="absolute bottom-0 left-0 flex justify-center px-1 py-2 shadow-md bg-surface-100 w-full z-10 border-t">
-      <ul className="flex flex-wrap items-center justify-center gap-4">
-        <li className="flex items-center text-[10px] font-mono gap-1 text-foreground-light">
-          <Key
-            size={SIZE}
-            strokeWidth={1.5}
-            className="flex-shrink-0 text-foreground-lighter opacity-75"
-          />
+    <div className="absolute bottom-0 left-0 border-t flex justify-center px-1 py-2 shadow-md bg-surface-100 w-full z-10">
+      <ul className="flex flex-wrap  items-center justify-center gap-4">
+        <li className="flex items-center text-xs font-mono gap-1">
+          <Key size={15} strokeWidth={1.5} className="flex-shrink-0 text-light" />
           Primary key
         </li>
-        <li className="flex items-center text-[10px] font-mono gap-1 text-foreground-light">
-          <Hash
-            size={SIZE}
-            strokeWidth={1.5}
-            className="flex-shrink-0 text-foreground-lighter opacity-75"
-          />
+        <li className="flex items-center text-xs font-mono gap-1">
+          <Hash size={15} strokeWidth={1.5} className="flex-shrink-0 text-light" />
           Identity
         </li>
-        <li className="flex items-center text-[10px] font-mono gap-1 text-foreground-light">
-          <Fingerprint
-            size={SIZE}
-            strokeWidth={1.5}
-            className="flex-shrink-0 text-foreground-lighter opacity-75"
-          />
+        <li className="flex items-center text-xs font-mono gap-1">
+          <Fingerprint size={15} strokeWidth={1.5} className="flex-shrink-0 text-light" />
           Unique
         </li>
-        <li className="flex items-center text-[10px] font-mono gap-1 text-foreground-light">
-          <DiamondIcon
-            size={SIZE}
-            strokeWidth={1.5}
-            className="flex-shrink-0 text-foreground-lighter opacity-75"
-          />
+        <li className="flex items-center text-xs font-mono gap-1">
+          <DiamondIcon size={15} strokeWidth={1.5} className="flex-shrink-0 text-light" />
           Nullable
         </li>
-        <li className="flex items-center text-[10px] font-mono gap-1 text-foreground-light">
+        <li className="flex items-center text-xs font-mono gap-1">
           <DiamondIcon
-            size={SIZE}
+            size={15}
             strokeWidth={1.5}
             fill="currentColor"
-            className="flex-shrink-0 text-foreground-lighter opacity-75"
+            className="flex-shrink-0 text-light"
           />
           Non-Nullable
         </li>
