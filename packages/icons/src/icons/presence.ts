3fa7d7be900c745e8d0a124fa65e9b71d704364a
Ivan Vasilov <vasilov.ivan@gmail.com>
2025-01-29 14:23:28 +0100

    Recenter the presence icon.

--- packages/icons/src/icons/presence.ts
+++ packages/icons/src/icons/presence.ts
@@ -4,7 +4,7 @@ import createSupabaseIcon from '../createSupabaseIcon';
  * @component @name Presence
  * @description Supabase SVG icon component, renders SVG Element with children.
  *
- * @preview ![img](data:image/svg+xml;base64,PHN2ZyAgIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIKICB3aWR0aD0iMjQiCiAgaGVpZ2h0PSIyNCIKICB2aWV3Qm94PSIxMi41IDEyIDI0IDI0IgogIGZpbGw9Im5vbmUiCiAgc3Ryb2tlPSIjMDAwIiBzdHlsZT0iYmFja2dyb3VuZC1jb2xvcjogI2ZmZjsgYm9yZGVyLXJhZGl1czogMnB4IgogIHN0cm9rZS13aWR0aD0iMiIKICBzdHJva2UtbGluZWNhcD0icm91bmQiCiAgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIKPgogIDxwYXRoCiAgICBzdHJva2U9ImN1cnJlbnRDb2xvciIKICAgIHN0cm9rZS1saW5lam9pbj0icm91bmQiCiAgICBzdHJva2UtbWl0ZXJsaW1pdD0iMTAiCiAgICBkPSJtMjMuMTQ5IDIzLjQ5OSAzLjQyNCA3LjgzYS41LjUgMCAwIDAgLjk0Mi0uMDc0bC43NC0yLjg2OGEuNS41IDAgMCAxIC4zNjQtLjM2bDMuMDM5LS43NTZhLjUuNSAwIDAgMCAuMDgtLjk0M2wtNy45My0zLjQ4N2EuNS41IDAgMCAwLS42Ni42NThaIgogIC8+CiAgPHBhdGgKICAgIHN0cm9rZT0iY3VycmVudENvbG9yIgogICAgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIKICAgIHN0cm9rZS1taXRlcmxpbWl0PSIxMCIKICAgIGQ9Ik0yNC41NDQgMzIuNzQ2aC01LjYyM2EzIDMgMCAwIDEtMy0zVjE4LjVhMyAzIDAgMCAxIDMtM2gxMS4yNDdhMyAzIDAgMCAxIDMgM3Y1LjYyMyIKICAgIG9wYWNpdHk9IjAuNDUiCiAgLz4KPC9zdmc+Cg==)
+ * @preview ![img](data:image/svg+xml;base64,PHN2ZyAgIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIKICB3aWR0aD0iMjQiCiAgaGVpZ2h0PSIyNCIKICB2aWV3Qm94PSIwIDAgMjQgMjQiCiAgZmlsbD0ibm9uZSIKICBzdHJva2U9IiMwMDAiIHN0eWxlPSJiYWNrZ3JvdW5kLWNvbG9yOiAjZmZmOyBib3JkZXItcmFkaXVzOiAycHgiCiAgc3Ryb2tlLXdpZHRoPSIyIgogIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIKICBzdHJva2UtbGluZWpvaW49InJvdW5kIgo+CiAgPHBhdGgKICAgIHN0cm9rZT0iY3VycmVudENvbG9yIgogICAgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIKICAgIHN0cm9rZS1taXRlcmxpbWl0PSIxMCIKICAgIGQ9Im0xMC42NDkgMTEuNDk5IDMuNDI0IDcuODNhLjUuNSAwIDAgMCAuOTQyLS4wNzRsLjc0LTIuODY4YS41LjUgMCAwIDEgLjM2NC0uMzZsMy4wMzktLjc1NmEuNS41IDAgMCAwIC4wOC0uOTQzbC03LjkzLTMuNDg3YS41LjUgMCAwIDAtLjY2LjY1OFoiCiAgLz4KICA8cGF0aAogICAgc3Ryb2tlPSJjdXJyZW50Q29sb3IiCiAgICBzdHJva2UtbGluZWpvaW49InJvdW5kIgogICAgc3Ryb2tlLW1pdGVybGltaXQ9IjEwIgogICAgZD0iTTEyLjA0NCAyMC43NDZoLTUuNjIzYTMgMyAwIDAgMS0zLTNWNi41YTMgMyAwIDAgMSAzLTNoMTEuMjQ3YTMgMyAwIDAgMSAzIDN2NS42MjMiCiAgICBvcGFjaXR5PSIwLjQ1IgogIC8+Cjwvc3ZnPgo=)
  *
  * @param {Object} props - Supabase icons props and any valid SVG attribute
  * @returns {JSX.Element} JSX Element
@@ -17,8 +17,8 @@ const Presence = createSupabaseIcon('Presence', [
       stroke: 'currentColor',
       'stroke-linejoin': 'round',
       'stroke-miterlimit': '10',
-      d: 'm23.149 23.499 3.424 7.83a.5.5 0 0 0 .942-.074l.74-2.868a.5.5 0 0 1 .364-.36l3.039-.756a.5.5 0 0 0 .08-.943l-7.93-3.487a.5.5 0 0 0-.66.658Z',
-      key: '4ms7qc',
+      d: 'm10.649 11.499 3.424 7.83a.5.5 0 0 0 .942-.074l.74-2.868a.5.5 0 0 1 .364-.36l3.039-.756a.5.5 0 0 0 .08-.943l-7.93-3.487a.5.5 0 0 0-.66.658Z',
+      key: '3qqo02',
     },
   ],
   [
@@ -27,9 +27,9 @@ const Presence = createSupabaseIcon('Presence', [
       stroke: 'currentColor',
       'stroke-linejoin': 'round',
       'stroke-miterlimit': '10',
-      d: 'M24.544 32.746h-5.623a3 3 0 0 1-3-3V18.5a3 3 0 0 1 3-3h11.247a3 3 0 0 1 3 3v5.623',
+      d: 'M12.044 20.746h-5.623a3 3 0 0 1-3-3V6.5a3 3 0 0 1 3-3h11.247a3 3 0 0 1 3 3v5.623',
       opacity: '0.45',
-      key: '15kc61',
+      key: '1dmpg0',
     },
   ],
 ]);
