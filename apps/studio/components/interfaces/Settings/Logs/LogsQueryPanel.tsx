c89a11dbbee24c4d22a431b90e3c4ffe4fd4a877
Jordi Enric <jordi.err@gmail.com>
2025-01-29 11:46:48 +0100

    update logsquerypanel

--- apps/studio/components/interfaces/Settings/Logs/LogsQueryPanel.tsx
+++ apps/studio/components/interfaces/Settings/Logs/LogsQueryPanel.tsx
@@ -28,7 +28,7 @@ import {
   LOGS_SOURCE_DESCRIPTION,
   LogsTableName,
 } from './Logs.constants'
-import DatePickers from './Logs.DatePickers'
+import { LogsDatePicker, DatePickerValue } from './Logs.DatePickers'
 import { LogsWarning, LogTemplate, WarehouseCollection } from './Logs.types'
 import { WarehouseQueryTemplate } from './Warehouse.utils'
 
@@ -44,7 +44,7 @@ export interface LogsQueryPanelProps {
   onSelectTemplate: (template: LogTemplate) => void
   onSelectWarehouseTemplate: (template: WarehouseQueryTemplate) => void
   onSelectSource: (source: string) => void
-  onDateChange: React.ComponentProps<typeof DatePickers>['onChange']
+  onDateChange: (value: DatePickerValue) => void
   onDataSourceChange: (sourceType: SourceType) => void
 }
 
@@ -202,10 +202,16 @@ const LogsQueryPanel = ({
             )}
 
             {dataSource === 'logs' && (
-              <DatePickers
+              <LogsDatePicker
                 to={defaultTo}
                 from={defaultFrom}
-                onChange={onDateChange}
+                value={{
+                  from: defaultFrom,
+                  to: defaultTo,
+                  isHelper: false,
+                  text: '',
+                }}
+                onSubmit={onDateChange}
                 helpers={EXPLORER_DATEPICKER_HELPERS}
               />
             )}
