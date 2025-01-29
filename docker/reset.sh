86b5b12707c9f2b76598c6e5edfd720726791cfb
RampantDespair <rampantdespair@gmail.com>
2025-01-29 15:17:49 -0500

    Changed reset script to take into account VOLUME_BASE_PATH

--- docker/reset.sh
+++ docker/reset.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Ensure intended operation
 echo "WARNING: This will remove all containers and container data, and will reset the .env file. This action cannot be undone!"
 read -p "Are you sure you want to proceed? (y/N) " -n 1 -r
 echo    # Move to a new line
@@ -9,36 +10,54 @@ then
     exit 1
 fi
 
+# Initialize VOLUME_BASE_PATH as empty
+VOLUME_BASE_PATH=""
+
+# Try to find .env and extract VOLUME_BASE_PATH if it exists
+if [ -f ".env" ]; then
+    TEMP_PATH=$(grep -v '^#' .env | grep "^VOLUME_BASE_PATH=" | sed 's/^VOLUME_BASE_PATH=//')
+    if [ ! -z "$TEMP_PATH" ]; then
+        VOLUME_BASE_PATH=$(echo $TEMP_PATH | sed 's/^"\(.*\)"$/\1/')
+    fi
+fi
+
+# If VOLUME_BASE_PATH is empty, revert to default value "./volumes"
+if [ -z "$VOLUME_BASE_PATH" ]; then
+    VOLUME_BASE_PATH="./volumes"
+fi
+
+echo "Volumes will be reset at: $VOLUME_BASE_PATH"
+
+# Shut down and remove all containers
 echo "Stopping and removing all containers..."
 docker compose -f docker-compose.yml -f ./dev/docker-compose.dev.yml down -v --remove-orphans
 
-echo "Cleaning up bind-mounted directories..."
+# Retrieve bind-mounted directories
 BIND_MOUNTS=(
-  "./volumes/db/data"
+    "${VOLUME_BASE_PATH}/db-config"
+    "${VOLUME_BASE_PATH}/db-data"
+    "${VOLUME_BASE_PATH}/storage-data"
 )
 
+# Delete contents of bind-mounted directories
+echo "Cleaning up bind-mounted directories..."
 for DIR in "${BIND_MOUNTS[@]}"; do
-  if [ -d "$DIR" ]; then
-    echo "Deleting $DIR..."
-    rm -rf "$DIR"
-  else
-    echo "Directory $DIR does not exist. Skipping bind mount deletion step..."
-  fi
+    if [ -d "$DIR" ]; then
+        echo "Deleting contents of $DIR..."
+        rm -rf "$DIR"/*
+        rm -rf "$DIR"/.[!.]*  # This also removes hidden files
+    else
+        echo "Directory $DIR does not exist. Skipping cleanup step..."
+    fi
 done
 
-echo "Resetting .env file..."
-if [ -f ".env" ]; then
-  echo "Removing existing .env file..."
-  rm -f .env
-else
-  echo "No .env file found. Skipping .env removal step..."
-fi
-
+# Reset .env file
 if [ -f ".env.example" ]; then
-  echo "Copying .env.example to .env..."
-  cp .env.example .env
+    echo "Copying .env.example to .env..."
+    cp .env.example .env
 else
-  echo ".env.example file not found. Skipping .env reset step..."
+    echo ".env.example file not found. Skipping .env reset step..."
 fi
 
+# Cleanup complete
 echo "Cleanup complete!"
