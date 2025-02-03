11073208c46f538a5c92a2693f2863d69dddf8a3
Joshen Lim <joshenlimek@gmail.com>
2025-02-03 14:12:15 +0800

    Update copy

--- apps/studio/components/interfaces/ProjectCreation/SpecialSymbolsCallout.tsx
+++ apps/studio/components/interfaces/ProjectCreation/SpecialSymbolsCallout.tsx
@@ -3,11 +3,11 @@ import { InlineLink } from 'components/ui/InlineLink'
 export const SpecialSymbolsCallout = () => {
   return (
     <p className="mb-2">
-      Note: If using a connection string, you will need to{' '}
+      Note: If using the Postgres connection string, you will need to{' '}
       <InlineLink href="https://supabase.com/docs/guides/database/postgres/roles#special-symbols-in-passwords">
         percent-encode
       </InlineLink>{' '}
-      the special symbols in the password
+      your password
     </p>
   )
 }
