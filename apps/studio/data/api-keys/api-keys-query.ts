546fd22ed82be62d7b074fcecce8b2abc5b2ee6f
Joshen Lim <joshenlimek@gmail.com>
2025-02-03 12:15:43 +0800

    refactor api-keys-query

--- apps/studio/data/api-keys/api-keys-query.ts
+++ apps/studio/data/api-keys/api-keys-query.ts
@@ -2,8 +2,47 @@ import { useQuery, UseQueryOptions } from '@tanstack/react-query'
 import { get, handleError } from 'data/fetchers'
 import { ResponseError } from 'types'
 
-import { apiKeysKeys } from './keys'
 import { components } from 'api-types'
+import { apiKeysKeys } from './keys'
+
+type LegacyKeys = {
+  api_key: string
+  description?: string | null
+  hash?: string | null
+  id?: string | null
+  inserted_at?: string | null
+  name: string
+  prefix?: string | null
+  secret_jwt_template?: components['schemas']['ApiKeySecretJWTTemplate'] | null
+  type: 'legacy' | null
+  updated_at?: string | null
+}
+
+type SecretKeys = {
+  api_key: string
+  description?: string
+  hash: string
+  id: string
+  inserted_at: string
+  name: string
+  prefix: string
+  secret_jwt_template: components['schemas']['ApiKeySecretJWTTemplate']
+  type: 'secret'
+  updated_at?: string
+}
+
+type PublishableKeys = {
+  api_key: string
+  description?: string
+  hash?: string
+  id: string
+  inserted_at: string
+  name: string
+  prefix?: string
+  secret_jwt_template?: components['schemas']['ApiKeySecretJWTTemplate'] | null
+  type: 'publishable'
+  updated_at?: string
+}
 
 export interface APIKeysVariables {
   projectRef?: string
@@ -23,49 +62,7 @@ export async function getAPIKeys({ projectRef, reveal }: APIKeysVariables, signa
   }
 
   // [Jonny]: Overriding the types here since some stuff is not actually nullable or optional
-  return data as unknown as // // Legacy keys //
-  (| {
-        api_key: string
-        description?: string | null
-        hash?: string | null
-        id?: string | null
-        inserted_at?: string | null
-        name: string
-        prefix?: string | null
-        secret_jwt_template?: components['schemas']['ApiKeySecretJWTTemplate'] | null
-        type: 'legacy' | null
-        updated_at?: string | null
-      }
-    //
-    // Modern Keys
-    //
-    // Secret keys
-    | {
-        api_key: string
-        description?: string
-        hash: string
-        id: string
-        inserted_at: string
-        name: string
-        prefix: string
-        secret_jwt_template: components['schemas']['ApiKeySecretJWTTemplate']
-        type: 'secret'
-        updated_at?: string
-      }
-    // Publishable keys
-    | {
-        api_key: string
-        description?: string
-        hash?: string
-        id: string
-        inserted_at: string
-        name: string
-        prefix?: string
-        secret_jwt_template?: components['schemas']['ApiKeySecretJWTTemplate'] | null
-        type: 'publishable'
-        updated_at?: string
-      }
-  )[]
+  return data as unknown as (LegacyKeys | SecretKeys | PublishableKeys)[]
 }
 
 export type APIKeysData = Awaited<ReturnType<typeof getAPIKeys>>
