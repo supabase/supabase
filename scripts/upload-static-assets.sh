0b602f9eb0cb1e5f1df4a804795ea186f518d068
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2025-02-05 01:05:17 +0800

    Update upload-static-assets.sh

--- scripts/upload-static-assets.sh
+++ scripts/upload-static-assets.sh
@@ -23,7 +23,7 @@
 #######
 
 # If asset CDN is specifically disabled (i.e. studio self-hosted), we skip
-if [[ "$FORCE_ASSET_CDN" != "-1" ]]; then
+if [[ "$FORCE_ASSET_CDN" == "-1" ]]; then
     echo "Skipping asset upload. Set FORCE_ASSET_CDN=1 or VERCEL_ENV=production to execute."
     exit 0
 fi
