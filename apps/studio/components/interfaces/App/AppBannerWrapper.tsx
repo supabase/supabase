e0681a5b9074092cd425077bed02e2134d2486d3
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2025-02-06 02:42:51 +0800

    Update AppBannerWrapper.tsx

--- apps/studio/components/interfaces/App/AppBannerWrapper.tsx
+++ apps/studio/components/interfaces/App/AppBannerWrapper.tsx
@@ -9,15 +9,11 @@ import { RestrictionBanner } from 'components/layouts/AppLayout/RestrictionBanne
 import { getTheme } from 'components/ui/CodeEditor/CodeEditor.utils'
 import { useFlag } from 'hooks/ui/useFlag'
 import { useProfile } from 'lib/profile'
-import { OrganizationResourceBanner } from '../Organization/resource-banner'
-import { useSelectedProject } from 'hooks/misc/useSelectedProject'
-import { useParams } from 'common'
 
 const AppBannerWrapper = ({ children }: PropsWithChildren<{}>) => {
   const monaco = useMonaco()
   const { profile } = useProfile()
   const { resolvedTheme } = useTheme()
-  const { ref } = useParams()
 
   const ongoingIncident = useFlag('ongoingIncident')
   const showNoticeBanner = useFlag('showNoticeBanner')
