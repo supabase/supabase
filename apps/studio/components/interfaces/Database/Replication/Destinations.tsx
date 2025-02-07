3d98e902c020f6f807a4f1c89a6b7ebda810116b
Raminder Singh <romi_ssk@yahoo.co.in>
2025-02-07 17:37:33 +0530

    add destinations table

--- apps/studio/components/interfaces/Database/Replication/Destinations.tsx
+++ apps/studio/components/interfaces/Database/Replication/Destinations.tsx
@@ -1,24 +1,27 @@
 import { useParams } from 'common'
+import Table from 'components/to-be-cleaned/Table'
 import AlertError from 'components/ui/AlertError'
 import { ButtonTooltip } from 'components/ui/ButtonTooltip'
 import { useReplicationSinksQuery } from 'data/replication/sinks-query'
-import { Plus } from 'lucide-react'
-import { cn } from 'ui'
+import { Edit, MoreVertical, Pause, Play, Plus, Trash } from 'lucide-react'
+import {
+  Button,
+  cn,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from 'ui'
 import { GenericSkeletonLoader } from 'ui-patterns'
 
 const Destinations = () => {
   const { ref: projectRef } = useParams()
-  const {
-    data: sinks,
-    error,
-    isLoading,
-    isError,
-    isSuccess,
-  } = useReplicationSinksQuery({
+  const { data, error, isLoading, isError, isSuccess } = useReplicationSinksQuery({
     projectRef,
   })
 
-  const anyDestinations = isSuccess && sinks.sinks.length > 0
+  console.log(`data: ${JSON.stringify(data)}`)
+  const anyDestinations = isSuccess && data.sinks.length > 0
 
   return (
     <>
@@ -54,6 +57,63 @@ const Destinations = () => {
               Add destination
             </ButtonTooltip>
           </div>
+        )}
+
+        {anyDestinations && (
+          <Table
+            head={[
+              <Table.th key="name">Name</Table.th>,
+              <Table.th key="type">Type</Table.th>,
+              <Table.th key="status">Status</Table.th>,
+              <Table.th key="publication">Publication</Table.th>,
+              <Table.th key="actions"></Table.th>,
+            ]}
+            body={data.sinks.map((sink) => {
+              const pipelineEnabled = true
+              const status = pipelineEnabled ? 'Enabled' : 'Disabled'
+              return (
+                <Table.tr key={sink.id}>
+                  <Table.td>{sink.name}</Table.td>
+                  <Table.td>BigQuery</Table.td>
+                  <Table.td>{status}</Table.td>
+                  <Table.td>countries_pub</Table.td>
+                  <Table.td>
+                    {
+                      <div className="flex justify-end items-center space-x-2">
+                        <DropdownMenu>
+                          <DropdownMenuTrigger asChild>
+                            <Button type="default" className="px-1" icon={<MoreVertical />} />
+                          </DropdownMenuTrigger>
+                          <DropdownMenuContent side="bottom" align="end" className="w-32">
+                            {!pipelineEnabled && (
+                              <DropdownMenuItem className="space-x-2">
+                                <Play size={14} />
+                                <p>Enable</p>
+                              </DropdownMenuItem>
+                            )}
+                            {pipelineEnabled && (
+                              <DropdownMenuItem className="space-x-2">
+                                <Pause size={14} />
+                                <p>Disable</p>
+                              </DropdownMenuItem>
+                            )}
+                            <DropdownMenuItem className="space-x-2">
+                              <Edit size={14} />
+                              <p>Edit destination</p>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem className="space-x-2">
+                              <Trash stroke="red" size={14} />
+                              <p>Delete destination</p>
+                            </DropdownMenuItem>
+                          </DropdownMenuContent>
+                        </DropdownMenu>
+                      </div>
+                    }
+                  </Table.td>
+                </Table.tr>
+              )
+            })}
+          ></Table>
         )}
       </div>
     </>
