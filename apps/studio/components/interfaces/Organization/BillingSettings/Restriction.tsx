9c04723e5750258e9c825e1cd6c484870298e75c
Saxon Fletcher <saxonafletcher@gmail.com>
2025-02-07 16:49:30 +1000

    Update apps/studio/components/interfaces/Organization/BillingSettings/Restriction.tsx
    Co-authored-by: Long Hoang <1732217+loong@users.noreply.github.com>

--- apps/studio/components/interfaces/Organization/BillingSettings/Restriction.tsx
+++ apps/studio/components/interfaces/Organization/BillingSettings/Restriction.tsx
@@ -159,7 +159,7 @@ export const Restriction = () => {
             </p>
             <div className="flex items-center gap-x-2 mt-3">
               <Button key="upgrade-button" asChild type="default">
-                <Link href={`/org/${org?.slug}/billing?panel=subscriptionPlan&source=restricted`}>
+                <Link href={`/org/${org?.slug}/billing?panel=subscriptionPlan&source=fairUseRestricted`}>
                   Upgrade plan
                 </Link>
               </Button>
