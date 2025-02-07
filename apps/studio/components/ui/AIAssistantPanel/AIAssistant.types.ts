f58b594ee83fcc0812cee0335bd6a48d629ca5b8
Saxon Fletcher <saxonafletcher@gmail.com>
2025-02-07 14:11:23 +1000

    Deploy edge function from Assistant (#33293)
    * add edge function knowledge
    
    * deploy edge function mutaton
    
    * add edge function block with deploy
    
    * update function url
    
    * update tools
    
    * Update apps/studio/pages/api/ai/sql/tools.ts
    
    Co-authored-by: Greg Richardson <greg.nmr@gmail.com>
    
    * edge function tool fixes
    
    * copy
    
    * prompt changes and verify jwt
    
    * Very minor refactors
    
    ---------
    
    Co-authored-by: Joshen Lim <joshenlimek@gmail.com>
    Co-authored-by: Greg Richardson <greg.nmr@gmail.com>

--- apps/studio/components/ui/AIAssistantPanel/AIAssistant.types.ts
+++ apps/studio/components/ui/AIAssistantPanel/AIAssistant.types.ts
@@ -1,10 +1,11 @@
 export type SupportedAssistantEntities = 'rls-policies' | 'functions'
 export type SupportedAssistantQuickPromptTypes = 'suggest' | 'examples' | 'ask'
 
-export type AssistantSnippetProps = {
-  title: string
-  runQuery: 'true' | 'false'
-  isChart?: 'true' | 'false'
+export interface AssistantSnippetProps {
+  title?: string
+  isChart?: string
+  runQuery?: string
   xAxis?: string
   yAxis?: string
+  name?: string
 }
