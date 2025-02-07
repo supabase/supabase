1f09e41c49e2f36f3cb982f29e80f877f35e8a8a
Saxon Fletcher <saxonafletcher@gmail.com>
2025-02-07 14:36:11 +1000

    Move authentication settings (#33335)
    * all settings moved into the right places
    
    * clean a few things up
    
    * update ui for auth settings
    
    * more updating
    
    * rearrange settings
    
    * Update SmtpForm.tsx
    
    * updated styling
    
    * add old auth page to show links
    
    * add copy
    
    * udpate copy
    
    * smtp links
    
    * auth fixes
    
    * Smol fix
    
    * Another smol fix
    
    * Fix tab page menu selection
    
    * Add missing border
    
    * Gah one last one
    
    * Smol improvement for redirects from settings/auth to use id
    
    * Update apps/studio/components/layouts/AuthLayout/AuthLayout.utils.ts
    
    Co-authored-by: Kang Ming <kang.ming1996@gmail.com>
    
    * Update apps/studio/pages/project/[ref]/auth/mfa.tsx
    
    Co-authored-by: Kang Ming <kang.ming1996@gmail.com>
    
    * Update apps/studio/pages/project/[ref]/auth/mfa.tsx
    
    Co-authored-by: Kang Ming <kang.ming1996@gmail.com>
    
    * remove recommendation
    
    ---------
    
    Co-authored-by: Joshen Lim <joshenlimek@gmail.com>
    Co-authored-by: Kang Ming <kang.ming1996@gmail.com>

--- apps/studio/pages/project/[ref]/auth/rate-limits.tsx
+++ apps/studio/pages/project/[ref]/auth/rate-limits.tsx
@@ -2,7 +2,13 @@ import { PermissionAction } from '@supabase/shared-types/out/constants'
 
 import RateLimits from 'components/interfaces/Auth/RateLimits/RateLimits'
 import AuthLayout from 'components/layouts/AuthLayout/AuthLayout'
-import { FormsContainer } from 'components/ui/Forms/FormsContainer'
+import {
+  ScaffoldHeader,
+  ScaffoldContainer,
+  ScaffoldDescription,
+  ScaffoldTitle,
+} from 'components/layouts/Scaffold'
+import { DocsButton } from 'components/ui/DocsButton'
 import NoPermission from 'components/ui/NoPermission'
 import { useCheckPermissions, usePermissionsLoaded } from 'hooks/misc/useCheckPermissions'
 import type { NextPageWithLayout } from 'types'
@@ -15,9 +21,23 @@ const PageLayout: NextPageWithLayout = () => {
     return <NoPermission isFullPage resourceText="access your project's auth rate limit settings" />
   } else {
     return (
-      <FormsContainer>
-        <RateLimits />
-      </FormsContainer>
+      <div>
+        <ScaffoldHeader className="pb-0">
+          <ScaffoldContainer id="auth-page-top" className="flex items-center justify-between">
+            <div>
+              <ScaffoldTitle>Rate Limits</ScaffoldTitle>
+              <ScaffoldDescription>
+                Safeguard against bursts of incoming traffic to prevent abuse and maximize stability
+              </ScaffoldDescription>
+            </div>
+            <DocsButton href="https://supabase.com/docs/guides/platform/going-into-prod#rate-limiting-resource-allocation--abuse-prevention" />
+          </ScaffoldContainer>
+        </ScaffoldHeader>
+
+        <ScaffoldContainer className="my-8 space-y-8">
+          <RateLimits />
+        </ScaffoldContainer>
+      </div>
     )
   }
 }
