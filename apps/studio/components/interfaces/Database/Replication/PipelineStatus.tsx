5de85d18f5038f710d083fa02a29c9bd7e2d4fe8
Raminder Singh <romi_ssk@yahoo.co.in>
2025-02-10 17:31:21 +0530

    show status dot

--- apps/studio/components/interfaces/Database/Replication/PipelineStatus.tsx
+++ apps/studio/components/interfaces/Database/Replication/PipelineStatus.tsx
@@ -3,6 +3,7 @@ import { Pipeline } from './DestinationRow'
 import { useReplicationPipelineStatusQuery } from 'data/replication/pipeline-status-query'
 import AlertError from 'components/ui/AlertError'
 import ShimmeringLoader from 'ui-patterns/ShimmeringLoader'
+import { cn } from 'ui'
 
 const PipelineStatus = ({ pipeline }: { pipeline: Pipeline }) => {
   const { ref: projectRef } = useParams()
@@ -10,12 +11,23 @@ const PipelineStatus = ({ pipeline }: { pipeline: Pipeline }) => {
     projectRef,
     pipelineId: pipeline.id,
   })
+  const pipelineEnabled = data?.status === 'Stopped' ? false : true
   const status = data?.status === 'Stopped' ? 'Disabled' : 'Enabled'
   return (
     <>
       {isLoading && <ShimmeringLoader></ShimmeringLoader>}
       {isError && <AlertError error={error} subject="Failed to retrieve pipeline status" />}
-      {isSuccess && <>{status}</>}
+      {isSuccess && (
+        <div className="flex flex-row items-center">
+          <div
+            className={cn(
+              'w-2 h-2 rounded-full mr-1',
+              pipelineEnabled ? 'bg-brand' : 'bg-warning-600'
+            )}
+          ></div>
+          {status}
+        </div>
+      )}
     </>
   )
 }
