d6fc961ea51162b8cfa7d96964b790ee11755756
Raminder Singh <romi_ssk@yahoo.co.in>
2025-02-10 18:02:32 +0530

    move pipeline fetch query to parent component

--- apps/studio/data/replication/pipeline-status-query.ts
+++ apps/studio/data/replication/pipeline-status-query.ts
@@ -4,13 +4,14 @@ import { get, handleError } from 'data/fetchers'
 import { ResponseError } from 'types'
 import { replicationKeys } from './keys'
 
-type ReplicationPipelinesStatusParams = { projectRef?: string; pipelineId: number }
+type ReplicationPipelinesStatusParams = { projectRef?: string; pipelineId?: number }
 
 async function fetchReplicationPipelineStatus(
   { projectRef, pipelineId }: ReplicationPipelinesStatusParams,
   signal?: AbortSignal
 ) {
   if (!projectRef) throw new Error('projectRef is required')
+  if (!pipelineId) throw new Error('pipelineId is required')
 
   const { data, error } = await get('/platform/replication/{ref}/pipelines/{pipeline_id}/status', {
     params: { path: { ref: projectRef, pipeline_id: pipelineId } },
@@ -37,5 +38,8 @@ export const useReplicationPipelineStatusQuery = <TData = ReplicationPipelineSta
   useQuery<ReplicationPipelineStatusData, ResponseError, TData>(
     replicationKeys.pipelinesStatus(projectRef, pipelineId),
     ({ signal }) => fetchReplicationPipelineStatus({ projectRef, pipelineId }, signal),
-    { enabled: enabled && typeof projectRef !== 'undefined', ...options }
+    {
+      enabled: enabled && typeof projectRef !== 'undefined' && typeof pipelineId !== 'undefined',
+      ...options,
+    }
   )
