2320027528cd7ac2367578ef1f17333b86aeb97c
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2025-02-10 12:15:07 +0800

    revert sort/filter

--- apps/studio/components/grid/components/header/sort/SortPopover.tsx
+++ apps/studio/components/grid/components/header/sort/SortPopover.tsx
@@ -1,20 +1,19 @@
+import update from 'immutability-helper'
+import { isEqual } from 'lodash'
+import { ChevronDown, List } from 'lucide-react'
+import { useCallback, useMemo, useState } from 'react'
+
 import { formatSortURLParams } from 'components/grid/SupabaseGrid.utils'
 import { DropdownControl } from 'components/grid/components/common/DropdownControl'
 import type { Sort, SupaTable } from 'components/grid/types'
 import { useUrlState } from 'hooks/ui/useUrlState'
-import update from 'immutability-helper'
-import { isEqual } from 'lodash'
-import { ChevronDown, PlusCircle } from 'lucide-react'
-import { useCallback, useMemo, useState } from 'react'
 import {
   Button,
   PopoverContent_Shadcn_,
   PopoverSeparator_Shadcn_,
   PopoverTrigger_Shadcn_,
   Popover_Shadcn_,
-  cn,
 } from 'ui'
-import RuleSetButtonText from '../rules-set-button-text'
 import SortRow from './SortRow'
 
 export interface SortPopoverProps {
@@ -26,19 +25,10 @@ export interface SortPopoverProps {
 const SortPopover = ({ table, sorts, setParams }: SortPopoverProps) => {
   const [open, setOpen] = useState(false)
 
-  const hasSorts = (sorts || []).length > 0
-  const btnText = useMemo(() => {
-    return (
-      <RuleSetButtonText
-        rules={sorts}
-        type="sort"
-        renderRule={(sort) => {
-          const [column, direction] = sort.split(':')
-          return { column, value: direction }
-        }}
-      />
-    )
-  }, [sorts])
+  const btnText =
+    (sorts || []).length > 0
+      ? `Sorted by ${sorts.length} rule${sorts.length > 1 ? 's' : ''}`
+      : 'Sort'
 
   const onApplySorts = (appliedSorts: Sort[]) => {
     setParams((prevParams) => {
@@ -52,12 +42,7 @@ const SortPopover = ({ table, sorts, setParams }: SortPopoverProps) => {
   return (
     <Popover_Shadcn_ modal={false} open={open} onOpenChange={setOpen}>
       <PopoverTrigger_Shadcn_ asChild>
-        <Button
-          type={hasSorts ? 'default' : 'dashed'}
-          icon={!hasSorts && <PlusCircle strokeWidth={1.5} />}
-          className={cn('rounded-full', hasSorts && sorts.length <= 2 && 'pr-0.5')}
-          data-testid="table-editor-sort-button"
-        >
+        <Button type={(sorts || []).length > 0 ? 'link' : 'text'} icon={<List />}>
           {btnText}
         </Button>
       </PopoverTrigger_Shadcn_>
