98945c7755ddfcd102d1026228fe36171cef2b66
Jordi Enric <jordi.err@gmail.com>
2025-02-10 15:16:43 +0100

    add logic to remember last route

--- apps/studio/components/layouts/LogsLayout/LogsLayout.tsx
+++ apps/studio/components/layouts/LogsLayout/LogsLayout.tsx
@@ -1,11 +1,14 @@
 import { PermissionAction } from '@supabase/shared-types/out/constants'
-import { PropsWithChildren } from 'react'
+import { PropsWithChildren, useEffect } from 'react'
 
 import NoPermission from 'components/ui/NoPermission'
 import { useCheckPermissions } from 'hooks/misc/useCheckPermissions'
 import { withAuth } from 'hooks/misc/withAuth'
 import ProjectLayout from '../ProjectLayout/ProjectLayout'
 import { LogsSidebarMenuV2 } from './LogsSidebarMenuV2'
+import { useRouter } from 'next/router'
+import { useLocalStorageQuery } from 'hooks/misc/useLocalStorage'
+import { useParams } from 'common'
 
 interface LogsLayoutProps {
   title?: string
@@ -13,6 +16,29 @@ interface LogsLayoutProps {
 
 const LogsLayout = ({ title, children }: PropsWithChildren<LogsLayoutProps>) => {
   const canUseLogsExplorer = useCheckPermissions(PermissionAction.ANALYTICS_READ, 'logflare')
+  const { ref } = useParams()
+
+  const router = useRouter()
+  const [lastLogsPage, setLastLogsPage] = useLocalStorageQuery(
+    'supabase-last-logs-page',
+    router.pathname
+  )
+
+  useEffect(() => {
+    if (router.pathname.includes('/logs/')) {
+      setLastLogsPage(router.pathname)
+    }
+  }, [router, setLastLogsPage])
+
+  useEffect(() => {
+    const last5chars = router.pathname.slice(-5)
+    if (last5chars === '/logs' && lastLogsPage) {
+      router.push({
+        pathname: lastLogsPage,
+        query: { ref },
+      })
+    }
+  }, [router, lastLogsPage, ref])
 
   if (!canUseLogsExplorer) {
     return (
