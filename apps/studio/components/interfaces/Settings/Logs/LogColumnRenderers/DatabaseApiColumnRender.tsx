21cdfdb6b2bfa23abab3c943eb3a0110f6e7a16e
Jordi Enric <jordi.err@gmail.com>
2025-02-11 23:00:14 +0100

    fix type err

--- apps/studio/components/interfaces/Settings/Logs/LogColumnRenderers/DatabaseApiColumnRender.tsx
+++ apps/studio/components/interfaces/Settings/Logs/LogColumnRenderers/DatabaseApiColumnRender.tsx
@@ -15,17 +15,9 @@ const columns: Column<LogData>[] = [
       return (
         <RowLayout>
           <TimestampInfo utcTimestamp={props.row.timestamp!} />
-          <ResponseCodeFormatter value={String(props.row.status_code) || ''} />
-          <TextFormatter
-            className="w-20"
-            value={
-              typeof props.row.method === 'string' ? props.row.method : String(props.row.method)
-            }
-          />
-          <TextFormatter
-            className="w-full"
-            value={typeof props.row.path === 'string' ? props.row.path : String(props.row.path)}
-          />
+          <ResponseCodeFormatter value={String(props.row.status_code)} />
+          <TextFormatter value={String(props.row.method)} />
+          <TextFormatter value={String(props.row.path)} />
         </RowLayout>
       )
     },
