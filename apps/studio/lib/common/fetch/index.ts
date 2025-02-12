88c744613ccba683d66542215fdb73c0928f73ef
Joshen Lim <joshenlimek@gmail.com>
2025-02-12 17:29:49 +0800

    Revert "Remove unused files from lib/common/fetch"
    This reverts commit b314b05f065c3ed91ee586a1e8a8b4c3254d4f80.

--- apps/studio/lib/common/fetch/index.ts
+++ apps/studio/lib/common/fetch/index.ts
@@ -3,9 +3,14 @@
 export {
   constructHeaders,
   handleError,
+  handleHeadResponse,
   handleResponse,
   handleResponseError,
   isResponseOk,
 } from './base'
+export { delete_ } from './delete'
 export { get, getWithTimeout } from './get'
+export { head, headWithTimeout } from './head'
+export { patch } from './patch'
 export { post } from './post'
+export { put } from './put'
