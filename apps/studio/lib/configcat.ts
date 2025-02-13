ca5b44f703f89d6b71da1e6fe444b521b1d8e5cf
Joshen Lim <joshenlimek@gmail.com>
2025-02-13 14:11:29 +0800

    Lower config cat poll duration (#33576)

--- apps/studio/lib/configcat.ts
+++ apps/studio/lib/configcat.ts
@@ -10,7 +10,7 @@ function getClient() {
   client = configcat.getClient(
     process.env.NEXT_PUBLIC_CONFIGCAT_SDK_KEY ?? '',
     configcat.PollingMode.AutoPoll,
-    { pollIntervalSeconds: 600 }
+    { pollIntervalSeconds: 300 }
   )
 
   return client
