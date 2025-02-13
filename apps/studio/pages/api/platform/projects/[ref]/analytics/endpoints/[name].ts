dde7516c489aa69deb27512a04ef519ae4a50fa8
Joshen Lim <joshenlimek@gmail.com>
2025-02-13 14:04:26 +0800

    Revert changes for analytics/endpoint/[name]

--- apps/studio/pages/api/platform/projects/[ref]/analytics/endpoints/[name].ts
+++ apps/studio/pages/api/platform/projects/[ref]/analytics/endpoints/[name].ts
@@ -1,7 +1,7 @@
-import apiWrapper from 'lib/api/apiWrapper'
-import { get } from 'lib/common/fetch'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { PROJECT_ANALYTICS_URL } from 'pages/api/constants'
+import apiWrapper from 'lib/api/apiWrapper'
+import { get } from 'lib/common/fetch'
 
 export default (req: NextApiRequest, res: NextApiResponse) => apiWrapper(req, res, handler)
 
