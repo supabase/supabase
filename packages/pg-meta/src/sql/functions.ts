9da7ab2af2a3dc3568a470d3669c2918d2e63e60
avallete <andrew.valleteau@supabase.io>
2025-02-14 14:33:11 +0900

    chore: use pg and type parser

--- packages/pg-meta/src/sql/functions.ts
+++ packages/pg-meta/src/sql/functions.ts
@@ -89,6 +89,8 @@ from
           oid,
           unnest(arg_modes) as mode,
           unnest(arg_names) as name,
+          -- Coming from: coalesce(p.proallargtypes, p.proargtypes) postgres won't automatically assume
+          -- integer, we need to cast it to be properly parsed
           unnest(arg_types)::int8 as type_id,
           unnest(arg_has_defaults) as has_default
         from
