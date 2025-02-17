d83767167710d9e01b50073c3a1ddca8f1259ef9
avallete <andrew.valleteau@supabase.io>
2025-02-17 17:02:49 +0900

    chore(pg-meta): migrate types sql

--- packages/pg-meta/src/index.ts
+++ packages/pg-meta/src/index.ts
@@ -2,10 +2,12 @@ import roles from './pg-meta-roles'
 import schemas from './pg-meta-schemas'
 import * as functions from './pg-meta-functions'
 import tablePrivileges from './pg-meta-table-privileges'
+import types from './pg-meta-types'
 
 export default {
   roles,
   schemas,
   functions,
   tablePrivileges,
+  types,
 }
