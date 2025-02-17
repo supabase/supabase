54ce6637e274285d28cd58f10873caaa08435021
avallete <andrew.valleteau@supabase.io>
2025-02-17 15:00:08 +0900

    chore(pg-meta): migrate version

--- packages/pg-meta/src/index.ts
+++ packages/pg-meta/src/index.ts
@@ -2,10 +2,12 @@ import roles from './pg-meta-roles'
 import schemas from './pg-meta-schemas'
 import * as functions from './pg-meta-functions'
 import tablePrivileges from './pg-meta-table-privileges'
+import version from './pg-meta-version'
 
 export default {
   roles,
   schemas,
   functions,
   tablePrivileges,
+  version,
 }
