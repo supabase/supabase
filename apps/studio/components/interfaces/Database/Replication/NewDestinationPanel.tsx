48688b38fad20ec4ad66c46ecab7b6c490ddb7aa
Raminder Singh <romi_ssk@yahoo.co.in>
2025-02-19 15:00:34 +0530

    remove commented out code

--- apps/studio/components/interfaces/Database/Replication/NewDestinationPanel.tsx
+++ apps/studio/components/interfaces/Database/Replication/NewDestinationPanel.tsx
@@ -111,7 +111,6 @@ const NewDestinationPanel = ({ visible, sourceId, onClose }: NewDestinationPanel
     form.reset(defaultValues)
   }
 
-  // const submitRef = useRef<HTMLButtonElement>(null)
   const { enabled } = form.watch()
 
   return (
@@ -303,9 +302,6 @@ const NewDestinationPanel = ({ visible, sourceId, onClose }: NewDestinationPanel
                         )}
                       />
                     </div>
-                    {/* <Button ref={submitRef} htmlType="submit" type="default" className="hidden">
-                      Hidden form submit button
-                    </Button> */}
                   </div>
                 </form>
               </Form_Shadcn_>
@@ -322,161 +318,6 @@ const NewDestinationPanel = ({ visible, sourceId, onClose }: NewDestinationPanel
         </SheetContent>
       </Sheet>
     </>
-    // <SidePanel
-    //   visible={visible}
-    //   header="New Destination"
-    //   onCancel={onCancel}
-    //   onConfirm={() => {
-    //     if (submitRef.current) {
-    //       submitRef.current.click()
-    //       onConfirm()
-    //     }
-    //   }}
-    //   confirmText={'Create'}
-    // >
-    //   <SidePanel.Content className="flex flex-col py-4 gap-y-4">
-    //     <Form_Shadcn_ {...form}>
-    //       <form id={formId} onSubmit={form.handleSubmit(onSubmit)}>
-    //         <FormField_Shadcn_
-    //           name="type"
-    //           control={form.control}
-    //           render={({ field }) => (
-    //             <FormItem_Shadcn_>
-    //               <FormLabel_Shadcn_>Type</FormLabel_Shadcn_>
-    //               <FormControl_Shadcn_>
-    //                 <Select_Shadcn_ value={field.value}>
-    //                   <SelectTrigger_Shadcn_>{field.value}</SelectTrigger_Shadcn_>
-    //                   <SelectContent_Shadcn_>
-    //                     <SelectGroup_Shadcn_>
-    //                       <SelectItem_Shadcn_ value="BigQuery">BigQuery</SelectItem_Shadcn_>
-    //                     </SelectGroup_Shadcn_>
-    //                   </SelectContent_Shadcn_>
-    //                 </Select_Shadcn_>
-    //               </FormControl_Shadcn_>
-    //               <FormMessage_Shadcn_ />
-    //             </FormItem_Shadcn_>
-    //           )}
-    //         ></FormField_Shadcn_>
-    //         <FormField_Shadcn_
-    //           control={form.control}
-    //           name="name"
-    //           render={({ field }) => (
-    //             <FormItem_Shadcn_>
-    //               <FormLabel_Shadcn_>Name</FormLabel_Shadcn_>
-    //               <FormControl_Shadcn_>
-    //                 <Input_Shadcn_ {...field} placeholder="Name" />
-    //               </FormControl_Shadcn_>
-    //               <FormMessage_Shadcn_ />
-    //             </FormItem_Shadcn_>
-    //           )}
-    //         />
-    //         <FormField_Shadcn_
-    //           control={form.control}
-    //           name="projectId"
-    //           render={({ field }) => (
-    //             <FormItem_Shadcn_>
-    //               <FormLabel_Shadcn_>Project Id</FormLabel_Shadcn_>
-    //               <FormControl_Shadcn_>
-    //                 <Input_Shadcn_ {...field} placeholder="Project id" />
-    //               </FormControl_Shadcn_>
-    //               <FormMessage_Shadcn_ />
-    //             </FormItem_Shadcn_>
-    //           )}
-    //         />
-    //         <FormField_Shadcn_
-    //           control={form.control}
-    //           name="datasetId"
-    //           render={({ field }) => (
-    //             <FormItem_Shadcn_>
-    //               <FormLabel_Shadcn_>Dataset Id</FormLabel_Shadcn_>
-    //               <FormControl_Shadcn_>
-    //                 <Input_Shadcn_ {...field} placeholder="Dataset id" />
-    //               </FormControl_Shadcn_>
-    //               <FormMessage_Shadcn_ />
-    //             </FormItem_Shadcn_>
-    //           )}
-    //         />
-    //         <FormField_Shadcn_
-    //           control={form.control}
-    //           name="serviceAccountKey"
-    //           render={({ field }) => (
-    //             <FormItem_Shadcn_>
-    //               <FormLabel_Shadcn_>Service Account Key</FormLabel_Shadcn_>
-    //               <FormControl_Shadcn_>
-    //                 <TextArea_Shadcn_
-    //                   {...field}
-    //                   rows={4}
-    //                   maxLength={5000}
-    //                   placeholder="Service account key"
-    //                 />
-    //               </FormControl_Shadcn_>
-    //               <FormMessage_Shadcn_ />
-    //             </FormItem_Shadcn_>
-    //           )}
-    //         />
-    //         <FormField_Shadcn_
-    //           control={form.control}
-    //           name="publicationName"
-    //           render={({ field }) => (
-    //             <FormItem_Shadcn_>
-    //               <FormLabel_Shadcn_>Publication Name</FormLabel_Shadcn_>
-    //               <FormControl_Shadcn_>
-    //                 <Input_Shadcn_ {...field} placeholder="Publication name" />
-    //               </FormControl_Shadcn_>
-    //               <FormMessage_Shadcn_ />
-    //             </FormItem_Shadcn_>
-    //           )}
-    //         />
-    //         <FormField_Shadcn_
-    //           control={form.control}
-    //           name="maxSize"
-    //           render={({ field }) => (
-    //             <FormItem_Shadcn_>
-    //               <FormLabel_Shadcn_>Max Size</FormLabel_Shadcn_>
-    //               <FormControl_Shadcn_>
-    //                 <Input_Shadcn_ {...field} placeholder="Max size" />
-    //               </FormControl_Shadcn_>
-    //               <FormMessage_Shadcn_ />
-    //             </FormItem_Shadcn_>
-    //           )}
-    //         />
-    //         <FormField_Shadcn_
-    //           control={form.control}
-    //           name="maxFillSecs"
-    //           render={({ field }) => (
-    //             <FormItem_Shadcn_>
-    //               <FormLabel_Shadcn_>Max Fill Seconds</FormLabel_Shadcn_>
-    //               <FormControl_Shadcn_>
-    //                 <Input_Shadcn_ {...field} placeholder="Max fill seconds" />
-    //               </FormControl_Shadcn_>
-    //               <FormMessage_Shadcn_ />
-    //             </FormItem_Shadcn_>
-    //           )}
-    //         />
-    //         <FormField_Shadcn_
-    //           control={form.control}
-    //           name="enabled"
-    //           render={({ field }) => (
-    //             <FormItem_Shadcn_>
-    //               <FormLabel_Shadcn_>Enabled</FormLabel_Shadcn_>
-    //               <FormControl_Shadcn_>
-    //                 <Switch
-    //                   checked={field.value}
-    //                   onCheckedChange={field.onChange}
-    //                   disabled={field.disabled}
-    //                 />
-    //               </FormControl_Shadcn_>
-    //               <FormMessage_Shadcn_ />
-    //             </FormItem_Shadcn_>
-    //           )}
-    //         />
-    //         <Button ref={submitRef} htmlType="submit" type="default" className="hidden">
-    //           Hidden form submit button
-    //         </Button>
-    //       </form>
-    //     </Form_Shadcn_>
-    //   </SidePanel.Content>
-    // </SidePanel>
   )
 }
 
