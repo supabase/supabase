f31c4d2e00f5ddf081071eb8cc2e345bf37ccbf5
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2025-02-20 12:26:52 +0100

    Add mobile sheet for InfoTooltip

--- apps/docs/features/ui/InfoTooltip.tsx
+++ apps/docs/features/ui/InfoTooltip.tsx
@@ -1,6 +1,20 @@
-import { InfoIcon } from 'lucide-react'
-import React, { PropsWithChildren } from 'react'
-import { cn, Tooltip, TooltipContent, TooltipTrigger } from 'ui'
+'use client'
+
+import React, { PropsWithChildren, useState } from 'react'
+import { InfoIcon, XIcon } from 'lucide-react'
+import { ErrorBoundary } from 'react-error-boundary'
+import { useBreakpoint } from 'common'
+import {
+  Button,
+  cn,
+  CommandEmpty_Shadcn_,
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from 'ui'
 
 interface PopUpProps extends PropsWithChildren {
   tooltipContent: React.ReactNode
@@ -8,27 +22,62 @@ interface PopUpProps extends PropsWithChildren {
   contentContainerClassName?: string
 }
 
+const buttonClassName = cn(
+  'relative px-1 py-0 -my-px',
+  'rounded bg-surface-200 border border-dashed',
+  'transition-colors hover:border-strong group/inline-popup'
+)
+
 const InfoTooltip = ({
   children,
   className,
   tooltipContent,
   contentContainerClassName,
 }: PopUpProps) => {
+  const [open, setOpen] = useState(false)
+  const isMobile = useBreakpoint('md')
+
   return (
-    <Tooltip>
-      <TooltipTrigger
-        className={cn(
-          'relative px-1 py-0 -my-px',
-          'rounded bg-surface-200 border border-dashed',
-          'transition-colors hover:border-strong group/inline-popup',
-          className
-        )}
-      >
-        {children}
-        <InfoIcon className="absolute p-[1px] bg-background rounded-full -right-1 -top-1 w-3 h-3 text-foreground-lighter group-hover/inline-popup:text-foreground-light transition-colors" />
-      </TooltipTrigger>
-      <TooltipContent className={contentContainerClassName}>{tooltipContent}</TooltipContent>
-    </Tooltip>
+    <>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <span className={cn(buttonClassName, className)} onClick={() => setOpen(true)}>
+            {children}
+            <InfoIcon className="absolute p-[1px] bg-background rounded-full -left-1.5 -top-1.5 w-3 h-3 text-foreground-lighter group-hover/inline-popup:text-foreground-light transition-colors" />
+          </span>
+        </TooltipTrigger>
+        <TooltipContent className={contentContainerClassName}>{tooltipContent}</TooltipContent>
+      </Tooltip>
+      {isMobile && (
+        <Sheet open={open} onOpenChange={setOpen}>
+          <SheetContent
+            id="mobile-sheet-content"
+            showClose={false}
+            size="full"
+            side="bottom"
+            className={cn(
+              'md:hidden',
+              'text-foreground-lighter',
+              'rounded-t-lg overflow-hidden overflow-y-scroll',
+              'w-full h-fit min-h-[200px] py-2 px-4'
+            )}
+          >
+            <ErrorBoundary FallbackComponent={() => <CommandEmpty_Shadcn_ />}>
+              <SheetHeader className="flex items-center justify-between gap-2 px-0 py-2 mb-2 max-w-full">
+                <div className="flex items-center gap-2 max-w-[90%]">
+                  <InfoIcon className="p-[1px] min-w-4 min-h-4 text-foreground-lighter" />
+                  <p className="italic text-foreground-light truncate">{children}</p>
+                </div>
+                <Button type="text" onClick={() => setOpen(false)} className="px-1">
+                  <XIcon className="w-4 h-4 text-foreground-lighter" />
+                </Button>
+              </SheetHeader>
+              {tooltipContent}
+            </ErrorBoundary>
+          </SheetContent>
+        </Sheet>
+      )}
+    </>
   )
 }
 
