17bd2bd2a235a6a5bf53e705e9daf0afb196ca55
avallete <andrew.valleteau@supabase.io>
2025-02-20 18:35:05 +0900

    chore(pg-meta): Migrate tables (wip)

--- packages/pg-meta/src/index.ts
+++ packages/pg-meta/src/index.ts
@@ -1,5 +1,6 @@
 import roles from './pg-meta-roles'
 import schemas from './pg-meta-schemas'
+import * as tables from './pg-meta-tables'
 import * as functions from './pg-meta-functions'
 import tablePrivileges from './pg-meta-table-privileges'
 import materializedViews from './pg-meta-materialized-views'
@@ -8,6 +9,7 @@ import foreignTables from './pg-meta-foreign-tables'
 export default {
   roles,
   schemas,
+  tables,
   functions,
   tablePrivileges,
   materializedViews,
