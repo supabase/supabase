d7ada9f97f7a5fad9e517e25025f022096721af2
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2025-02-23 23:43:15 +0800

    Update DiskManagement.schema.ts

--- apps/studio/components/interfaces/DiskManagement/DiskManagement.schema.ts
+++ apps/studio/components/interfaces/DiskManagement/DiskManagement.schema.ts
@@ -51,7 +51,7 @@ export const CreateDiskStorageSchema = (defaultTotalSize: number) => {
     if (totalSize < defaultTotalSize) {
       ctx.addIssue({
         code: z.ZodIssueCode.custom,
-        message: `Disk size cannot be reduced in size. To reduce your disk size, head to the Infrastructure page and go through a Postgres version upgrade.`,
+        message: `Disk size cannot be reduced in size. Reduce your database size and then head to the Infrastructure settings and go through a Postgres version upgrade to right-size your disk.`,
         path: ['totalSize'],
       })
     }
