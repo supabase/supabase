fad3b630f0e285944f49ee457ea1e73c670b80d1
Terry Sutton <saltcod@gmail.com>
2025-02-25 10:47:59 -0330

    Edge functions secrets updates (#31329)
    * Rename secrets to env vars, add inline form
    
    * Add multiple
    
    * Fix adding multiple
    
    * Change name back
    
    * Sm update
    
    * Add docs for secrets management
    
    * Fix file paths
    
    * Prettier
    
    * Fix image paths
    
    * Images
    
    * clean up
    
    * Fix form submit
    
    * Minor fixes.
    
    * Remove console logs.
    
    ---------
    
    Co-authored-by: Ivan Vasilov <vasilov.ivan@gmail.com>

--- apps/studio/pages/project/[ref]/settings/functions.tsx
+++ apps/studio/pages/project/[ref]/settings/functions.tsx
@@ -8,16 +8,22 @@ import {
   ScaffoldTitle,
 } from 'components/layouts/Scaffold'
 import type { NextPageWithLayout } from 'types'
+import { DocsButton } from 'components/ui/DocsButton'
 
 const PageLayout: NextPageWithLayout = () => {
   return (
     <>
       <ScaffoldContainer>
-        <ScaffoldHeader>
-          <ScaffoldTitle>Edge Function Secrets Management</ScaffoldTitle>
-          <ScaffoldDescription>
-            Manage the secrets for your project's edge functions
-          </ScaffoldDescription>
+        <ScaffoldHeader className="flex flex-row justify-between">
+          <div>
+            <ScaffoldTitle>Edge Function Secrets</ScaffoldTitle>
+            <ScaffoldDescription>
+              Manage the secrets (environment variables) for your project's Edge Functions
+            </ScaffoldDescription>
+          </div>
+          <div className="flex items-center space-x-2">
+            <DocsButton href="https://supabase.com/docs/guides/functions/secrets" />
+          </div>
         </ScaffoldHeader>
       </ScaffoldContainer>
       <ScaffoldContainer bottomPadding>
