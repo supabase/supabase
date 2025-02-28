5504725f4b14c18aa24354cb31982b4bc1680562
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2025-02-28 17:50:40 +0800

    Update resource-banner.tsx

--- apps/studio/components/interfaces/Organization/resource-banner.tsx
+++ apps/studio/components/interfaces/Organization/resource-banner.tsx
@@ -1,8 +1,7 @@
-import { useParams } from 'common'
-import { MAX_WIDTH_CLASSES, PADDING_CLASSES } from 'components/layouts/Scaffold'
 import { AnimatePresence, motion } from 'framer-motion'
-import { useOrganizationRestrictions } from 'hooks/misc/useOrganizationRestrictions'
 import Link from 'next/link'
+
+import { useOrganizationRestrictions } from 'hooks/misc/useOrganizationRestrictions'
 import { cn, CriticalIcon, WarningIcon } from 'ui'
 
 const bannerMotionProps = {
@@ -10,14 +9,6 @@ const bannerMotionProps = {
   animate: { height: 'auto', opacity: 1 },
   exit: { height: 0, opacity: 0 },
   transition: { duration: 0.2, delay: 0.5 },
-} as const
-
-const containerMotionProps = {
-  animate: {
-    transition: {
-      staggerChildren: 0.1,
-    },
-  },
 } as const
 
 export function OrganizationResourceBanner({ headerBanner }: { headerBanner?: boolean }) {
@@ -126,43 +117,3 @@ export const HeaderBanner = ({
     </motion.div>
   )
 }
-
-// import dayjs from 'dayjs'
-// import Link from 'next/link'
-
-// import { useOrganizationsQuery } from 'data/organizations/organizations-query'
-// import { useSelectedProject } from 'hooks/misc/useSelectedProject'
-// import { AlertTitle_Shadcn_, Alert_Shadcn_, Button, CriticalIcon, WarningIcon } from 'ui'
-
-// /**
-//  * Shown on projects in organization which are above their qouta
-//  */
-// export const RestrictionBanner = () => {
-//   const project = useSelectedProject()
-//   const { data } = useOrganizationsQuery()
-//   const currentOrg = data?.find((org) => org.id === project?.organization_id)
-
-//   if (!currentOrg?.restriction_status) return null
-
-//   return (
-//     <Alert_Shadcn_
-//       variant={currentOrg.restriction_status === 'restricted' ? 'destructive' : 'warning'}
-//       className="rounded-none border-l-0 border-r-0 h-[44px] p-0 flex items-center justify-center"
-//     >
-//       <AlertTitle_Shadcn_ className="flex items-center gap-x-4">
-//         {currentOrg.restriction_status === 'restricted' ? <CriticalIcon /> : <WarningIcon />}
-//         <span>
-//           {currentOrg.restriction_status === 'grace_period' &&
-//             `Your organization has exceeded its quota. You are given a grace period until ${dayjs(currentOrg.restriction_data['grace_period_end']).format('DD MMM, YYYY')}`}
-//           {currentOrg.restriction_status === 'grace_period_over' &&
-//             `Your grace period is over and your project will not be able to serve requests when you used up your quota.`}
-//           {currentOrg.restriction_status === 'restricted' &&
-//             'Your project is unable to serve any requests as your organization has used up its quota.'}
-//         </span>
-//         <Button asChild type="default">
-//           <Link href={`/org/${currentOrg.slug}/billing`}>More information</Link>
-//         </Button>
-//       </AlertTitle_Shadcn_>
-//     </Alert_Shadcn_>
-//   )
-// }
