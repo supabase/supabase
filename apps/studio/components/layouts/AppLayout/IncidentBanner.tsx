501f977d99c052e5f0af0fbbc4d899bf06df859d
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2025-02-28 17:48:08 +0800

    clean

--- apps/studio/components/layouts/AppLayout/IncidentBanner.tsx
+++ apps/studio/components/layouts/AppLayout/IncidentBanner.tsx
@@ -1,18 +1,10 @@
-import { HeaderBanner } from 'components/interfaces/Organization/resource-banner'
-import { ExternalLink } from 'lucide-react'
 import Link from 'next/link'
-import { WarningIcon } from 'ui'
+
+import { HeaderBanner } from 'components/interfaces/Organization/resource-banner'
 
 const IncidentBanner = () => {
   return (
     <Link href="https://status.supabase.com" target="_blank" rel="noreferrer">
-      {/* <div className="px-2 mx-auto w-full xl:max-w-[700px] items-center flex flex-row gap-3">
-        <WarningIcon className="flex-shrink-0 bg-brand text-brand-200" />
-        <span className="text-sm">
-          We are currently investigating a technical issue, follow status.supabase.com for updates
-        </span>
-        <ExternalLink size={14} strokeWidth={1} />
-      </div> */}
       <HeaderBanner
         type="incident"
         title="We are currently investigating a technical issue"
