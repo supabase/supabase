24fc6d896070c0fdf1d715fae09a8e5e1b8fb4ce
Saxon Fletcher <saxonafletcher@gmail.com>
2025-03-03 15:47:32 +1000

    rate limits and sessions

--- apps/studio/pages/project/[ref]/auth/sessions.tsx
+++ apps/studio/pages/project/[ref]/auth/sessions.tsx
@@ -1,44 +1,56 @@
 import { PermissionAction } from '@supabase/shared-types/out/constants'
+import { ReactNode } from 'react'
 
-import { SessionsAuthSettingsForm, SmtpForm, ThirdPartyAuthForm } from 'components/interfaces/Auth'
+import { SessionsAuthSettingsForm } from 'components/interfaces/Auth'
 import AuthLayout from 'components/layouts/AuthLayout/AuthLayout'
 import DefaultLayout from 'components/layouts/DefaultLayout'
-import { ScaffoldContainer, ScaffoldHeader, ScaffoldTitle } from 'components/layouts/Scaffold'
+import { PageLayout } from 'components/layouts/PageLayout/PageLayout'
+import { ScaffoldContainer } from 'components/layouts/Scaffold'
 import NoPermission from 'components/ui/NoPermission'
 import { GenericSkeletonLoader } from 'components/ui/ShimmeringLoader'
 import { useCheckPermissions, usePermissionsLoaded } from 'hooks/misc/useCheckPermissions'
 import type { NextPageWithLayout } from 'types'
 
-const PageLayout: NextPageWithLayout = () => {
-  const isPermissionsLoaded = usePermissionsLoaded()
-  // TODO: check if these permissions cover third party auth as well
-  const canReadAuthSettings = useCheckPermissions(PermissionAction.READ, 'custom_config_gotrue')
-
-  return (
-    <>
-      <ScaffoldContainer>
-        <ScaffoldHeader>
-          <ScaffoldTitle>Sessions</ScaffoldTitle>
-        </ScaffoldHeader>
-      </ScaffoldContainer>
-      <ScaffoldContainer className="flex flex-col gap-10" bottomPadding>
-        {!isPermissionsLoaded ? (
-          <GenericSkeletonLoader />
-        ) : !canReadAuthSettings ? (
-          <NoPermission isFullPage resourceText="access your project's authentication settings" />
-        ) : (
-          <SessionsAuthSettingsForm />
-        )}
-      </ScaffoldContainer>
-    </>
-  )
+interface SessionsLayoutProps {
+  children: ReactNode
 }
 
-PageLayout.getLayout = (page) => {
+export const SessionsLayout = ({ children }: SessionsLayoutProps) => {
   return (
     <DefaultLayout>
-      <AuthLayout>{page}</AuthLayout>
+      <AuthLayout>
+        <PageLayout
+          title="User Sessions"
+          subtitle="Configure settings for user sessions and refresh tokens"
+        >
+          {children}
+        </PageLayout>
+      </AuthLayout>
     </DefaultLayout>
   )
 }
-export default PageLayout
+
+const SessionsPage: NextPageWithLayout = () => {
+  const isPermissionsLoaded = usePermissionsLoaded()
+  const canReadAuthSettings = useCheckPermissions(PermissionAction.READ, 'custom_config_gotrue')
+
+  if (isPermissionsLoaded && !canReadAuthSettings) {
+    return <NoPermission isFullPage resourceText="access your project's authentication settings" />
+  }
+
+  return (
+    <ScaffoldContainer>
+      {!isPermissionsLoaded ? (
+        <div className="mt-12">
+          <GenericSkeletonLoader />
+        </div>
+      ) : (
+        <SessionsAuthSettingsForm />
+      )}
+    </ScaffoldContainer>
+  )
+}
+
+SessionsPage.getLayout = (page) => <SessionsLayout>{page}</SessionsLayout>
+
+export default SessionsPage
