08f48c5bfdf2ae2c0c738db8fd6297a52e978931
Andrew Valleteau <avallete@users.noreply.github.com>
2025-03-04 12:11:55 +0100

    chore(pg-meta): migrate types sql (#33649)

--- packages/pg-meta/src/index.ts
+++ packages/pg-meta/src/index.ts
@@ -2,6 +2,7 @@ import roles from './pg-meta-roles'
 import schemas from './pg-meta-schemas'
 import * as functions from './pg-meta-functions'
 import tablePrivileges from './pg-meta-table-privileges'
+import types from './pg-meta-types'
 import version from './pg-meta-version'
 import indexes from './pg-meta-indexes'
 import columnPrivileges from './pg-meta-column-privileges'
@@ -11,6 +12,7 @@ export default {
   schemas,
   functions,
   tablePrivileges,
+  types,
   version,
   indexes,
   columnPrivileges,
