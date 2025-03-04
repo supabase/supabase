9817ac77e92f64397b795424c9b14bfba398d1ea
Etienne Stalmans <staaldraad@users.noreply.github.com>
2025-03-04 10:03:50 +0100

    feat: SOC 2 docs page (#33965)
    * feat: add SOC 2 page to docs
    
    * feat: more linking
    
    * fix: make linters happy
    
    * fix: prettier
    
    * chore: make security page available
    
    * typo: sp
    
    * publish security
    
    * chore: publish new security section
    
    * change mobile menu to show Security on Security section
    
    ---------
    
    Co-authored-by: Charis Lam <26616127+charislam@users.noreply.github.com>

--- apps/docs/components/Navigation/NavigationMenu/NavigationMenu.constants.ts
+++ apps/docs/components/Navigation/NavigationMenu/NavigationMenu.constants.ts
@@ -2030,6 +2030,20 @@ export const MIGRATION_PAGES: Partial<NavMenuSection & ComponentProps<typeof Ico
   },
 ]
 
+export const security: NavMenuConstant = {
+  icon: 'platform',
+  title: 'Security',
+  url: '/guides/security',
+  items: [
+    { name: 'Overview', url: '/guides/security' },
+    {
+      name: 'Compliance',
+      url: undefined,
+      items: [{ name: 'SOC 2', url: '/guides/security/soc-2-compliance' }],
+    },
+  ],
+}
+
 export const platform: NavMenuConstant = {
   icon: 'platform',
   title: 'Platform',
@@ -2377,6 +2391,7 @@ export const deployment: NavMenuConstant = {
         },
         { name: 'Maturity model', url: '/guides/deployment/maturity-model' },
         { name: 'Production checklist', url: '/guides/deployment/going-into-prod' },
+        { name: 'SOC 2 compliance', url: '/guides/security/soc-2-compliance' },
       ],
     },
     {
