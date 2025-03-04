6646ce14d5a515392157902d8147eccdd8db1912
Andrew Valleteau <avallete@users.noreply.github.com>
2025-03-04 11:40:59 +0100

    chore(pg-meta): migrate version (#33648)

--- packages/pg-meta/src/index.ts
+++ packages/pg-meta/src/index.ts
@@ -2,6 +2,7 @@ import roles from './pg-meta-roles'
 import schemas from './pg-meta-schemas'
 import * as functions from './pg-meta-functions'
 import tablePrivileges from './pg-meta-table-privileges'
+import version from './pg-meta-version'
 import indexes from './pg-meta-indexes'
 import columnPrivileges from './pg-meta-column-privileges'
 
@@ -10,6 +11,7 @@ export default {
   schemas,
   functions,
   tablePrivileges,
+  version,
   indexes,
   columnPrivileges,
 }
