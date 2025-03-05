0a9393486b3744dd99ebe8b612251065667862ea
Andrew Valleteau <avallete@users.noreply.github.com>
2025-03-05 18:28:16 +0100

    chore(pg-meta): migrate foreign-tables (#33703)
    * chore(pg-meta): migrate foreign-tables
    
    * chore: re-use columns zod definition
    
    * chore(pg-meta): migrate tables logic and tests (#33758)
    
    * chore(pg-meta): Migrate tables (wip)
    
    * wip list
    
    * chore(pg-meta): migrate tables test and code

--- packages/pg-meta/src/index.ts
+++ packages/pg-meta/src/index.ts
@@ -1,8 +1,10 @@
 import roles from './pg-meta-roles'
 import columns from './pg-meta-columns'
 import schemas from './pg-meta-schemas'
+import tables from './pg-meta-tables'
 import * as functions from './pg-meta-functions'
 import tablePrivileges from './pg-meta-table-privileges'
+import foreignTables from './pg-meta-foreign-tables'
 import policies from './pg-meta-policies'
 import triggers from './pg-meta-triggers'
 import types from './pg-meta-types'
@@ -14,8 +16,10 @@ export default {
   roles,
   columns,
   schemas,
+  tables,
   functions,
   tablePrivileges,
+  foreignTables,
   policies,
   triggers,
   types,
