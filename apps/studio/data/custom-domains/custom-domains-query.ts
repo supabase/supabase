3963cc83b4d4df3452b82184e703d06bc0554a13
Joshen Lim <joshenlimek@gmail.com>
2025-03-13 15:06:20 +0100

    Feed

--- apps/studio/data/custom-domains/custom-domains-query.ts
+++ apps/studio/data/custom-domains/custom-domains-query.ts
@@ -111,9 +111,8 @@ export const useCustomDomainsQuery = <TData = CustomDomainsData>(
     ({ signal }) => getCustomDomains({ projectRef }, signal),
     {
       enabled: enabled && IS_PLATFORM && typeof projectRef !== 'undefined',
-
       refetchInterval: (data: any) => {
-        if (data.status === '0_not_allowed') return false
+        if (data?.status === '0_not_allowed') return false
         return 1000 * 5 * 60 // 5 minutes
       },
       ...options,
