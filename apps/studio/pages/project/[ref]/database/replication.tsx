4de3fd15dec063d6ab5cc46cb3c8dbd36fe1163d
Raminder Singh <romi_ssk@yahoo.co.in>
2025-03-19 17:45:24 +0530

    hide replication page contents behind a feature flag

--- apps/studio/pages/project/[ref]/database/replication.tsx
+++ apps/studio/pages/project/[ref]/database/replication.tsx
@@ -4,25 +4,40 @@ import { FormHeader } from 'components/ui/Forms/FormHeader'
 import Destinations from 'components/interfaces/Database/Replication/Destinations'
 import { ScaffoldContainer, ScaffoldSection } from 'components/layouts/Scaffold'
 import DefaultLayout from 'components/layouts/DefaultLayout'
+import { useFlag } from 'hooks/ui/useFlag'
 
 const DatabaseReplicationPage: NextPageWithLayout = () => {
+  const enablePgReplicate = useFlag('enablePgReplicate')
   return (
     <>
-      <ScaffoldContainer>
-        <ScaffoldSection>
-          <div className="col-span-12">
-            <FormHeader
-              title="Database Replication"
-              description="Send data to other destinations"
-            />
-          </div>
-        </ScaffoldSection>
-        <ScaffoldSection>
-          <div className="col-span-12">
-            <Destinations />
-          </div>
-        </ScaffoldSection>
-      </ScaffoldContainer>
+      {enablePgReplicate ? (
+        <ScaffoldContainer>
+          <ScaffoldSection>
+            <div className="col-span-12">
+              <FormHeader
+                title="Database Replication"
+                description="Send data to other destinations"
+              />
+            </div>
+          </ScaffoldSection>
+          <ScaffoldSection>
+            <div className="col-span-12">
+              <Destinations />
+            </div>
+          </ScaffoldSection>
+        </ScaffoldContainer>
+      ) : (
+        <ScaffoldContainer>
+          <ScaffoldSection>
+            <div className="col-span-12">
+              <FormHeader
+                title="Feature Unavailable"
+                description="This feature is not available for you"
+              />
+            </div>
+          </ScaffoldSection>
+        </ScaffoldContainer>
+      )}
     </>
   )
 }
