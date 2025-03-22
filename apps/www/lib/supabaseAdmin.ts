3baa4c444be8d1973705aab572e6e4c7eed5e569
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2025-03-22 07:15:51 +0100

    feat: cleanup

--- apps/www/lib/supabaseAdmin.ts
+++ apps/www/lib/supabaseAdmin.ts
@@ -2,9 +2,8 @@ import { createClient } from '@supabase/supabase-js'
 import { Database } from './database.types'
 
 const supabase = createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL! || 'http://127.0.0.1:54321',
-  process.env.LIVE_SUPABASE_SERVICE_ROLE_KEY! ||
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MTk4MzgxMjk5Nn0.EGIM96RAZx35lJzdJsyH-qQwv8Hdp7fsn3W0YpN81IU',
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.LIVE_SUPABASE_SERVICE_ROLE_KEY!,
   {
     realtime: {
       params: {
