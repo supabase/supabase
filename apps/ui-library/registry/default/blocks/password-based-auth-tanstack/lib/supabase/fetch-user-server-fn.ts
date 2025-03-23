206aa5465a8f411b4ef8ebde5dd01857c4dad413
Ivan Vasilov <vasilov.ivan@gmail.com>
2025-03-23 21:00:21 +0100

    Fix the v0 button. Add some more docs.

--- apps/ui-library/registry/default/blocks/password-based-auth-tanstack/lib/supabase/fetch-user-server-fn.ts
+++ apps/ui-library/registry/default/blocks/password-based-auth-tanstack/lib/supabase/fetch-user-server-fn.ts
@@ -1,12 +1,13 @@
 import { createClient } from '@/registry/default/clients/tanstack/lib/supabase/server'
 import type { Factor, User } from '@supabase/supabase-js'
 import { createServerFn } from '@tanstack/react-start'
-
 type SSRSafeUser = User & {
   factors: (Factor & { factor_type: 'phone' | 'totp' })[]
 }
 
-export const fetchUser = createServerFn({ method: 'GET' }).handler(async () => {
+export const fetchUser: () => Promise<SSRSafeUser | null> = createServerFn({
+  method: 'GET',
+}).handler(async () => {
   const supabase = createClient()
   const { data, error } = await supabase.auth.getUser()
 
