37c5ad4be89f1e90b2d05045466e0b9c9ec3f401
Joshen Lim <joshenlimek@gmail.com>
2025-03-24 23:21:06 +0800

    Scaffold

---
+++ apps/studio/components/interfaces/Advisors/AdvisorRulesV2.tsx
@@ -0,0 +1,17 @@
+import { PageLayout } from 'components/layouts/PageLayout/PageLayout'
+import { ScaffoldContainer, ScaffoldSection } from 'components/layouts/Scaffold'
+
+export const AdvisorRulesV2 = () => {
+  return (
+    <PageLayout
+      title="Advisor Rules"
+      subtitle="Disable specific advisor categories or rules, or assign them to members for resolution"
+    >
+      <ScaffoldContainer>
+        <ScaffoldSection isFullWidth className="!pt-6">
+          Hello
+        </ScaffoldSection>
+      </ScaffoldContainer>
+    </PageLayout>
+  )
+}
--- apps/studio/pages/project/[ref]/advisors/rules.tsx
+++ apps/studio/pages/project/[ref]/advisors/rules.tsx
@@ -1,10 +1,16 @@
 import { AdvisorRules } from 'components/interfaces/Advisors/AdvisorRules'
+import { AdvisorRulesV2 } from 'components/interfaces/Advisors/AdvisorRulesV2'
 import AdvisorsLayout from 'components/layouts/AdvisorsLayout/AdvisorsLayout'
 import DefaultLayout from 'components/layouts/DefaultLayout'
 import { NextPageWithLayout } from 'types'
 
 const AdvisorRulesPage: NextPageWithLayout = () => {
-  return <AdvisorRules />
+  return (
+    <>
+      <AdvisorRulesV2 />
+      <AdvisorRules />
+    </>
+  )
 }
 
 AdvisorRulesPage.getLayout = (page) => (
