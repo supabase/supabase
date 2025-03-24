5e08f6185883f463baaba9395b3f975ce6866ad8
Joshen Lim <joshenlimek@gmail.com>
2025-03-24 16:59:43 +0800

    Update dropzone component for UI Library

--- apps/ui-library/registry/default/blocks/dropzone/components/dropzone.tsx
+++ apps/ui-library/registry/default/blocks/dropzone/components/dropzone.tsx
@@ -1,11 +1,24 @@
 'use client'
 
 import { cn } from '@/lib/utils'
-import { UseSupabaseUploadReturn } from '@/registry/default/blocks/dropzone/hooks/use-supabase-upload'
 import { Button } from '@/registry/default/components/ui/button'
-import { Tooltip, TooltipContent, TooltipTrigger } from '@/registry/default/components/ui/tooltip'
-import { CheckCircle, CircleAlert, File, Loader2, Trash, Upload } from 'lucide-react'
+import { CheckCircle, File, Loader2, Upload, X } from 'lucide-react'
 import { createContext, PropsWithChildren, useCallback, useContext } from 'react'
+import { UseSupabaseUploadReturn } from '../hooks/use-supabase-upload'
+
+export const formatBytes = (
+  bytes: number,
+  decimals = 2,
+  size?: 'bytes' | 'KB' | 'MB' | 'GB' | 'TB' | 'PB' | 'EB' | 'ZB' | 'YB'
+) => {
+  const k = 1000
+  const dm = decimals < 0 ? 0 : decimals
+  const sizes = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+
+  if (bytes === 0 || bytes === undefined) return size !== undefined ? `0 ${size}` : '0 bytes'
+  const i = size !== undefined ? sizes.indexOf(size) : Math.floor(Math.log(bytes) / Math.log(k))
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i]
+}
 
 type DropzoneContextType = Omit<UseSupabaseUploadReturn, 'getRootProps' | 'getInputProps'>
 
@@ -25,7 +38,7 @@ const Dropzone = ({
   const isActive = restProps.isDragActive
   const isInvalid =
     (restProps.isDragActive && restProps.isDragReject) ||
-    restProps.error ||
+    (restProps.errors.length > 0 && !restProps.isSuccess) ||
     restProps.files.some((file) => file.errors.length !== 0)
 
   return (
@@ -47,7 +60,19 @@ const Dropzone = ({
   )
 }
 const DropzoneContent = ({ className }: { className?: string }) => {
-  const { error, files, setFiles, onUpload, loading, success } = useDropzoneContext()
+  const {
+    files,
+    setFiles,
+    onUpload,
+    loading,
+    successes,
+    errors,
+    maxFileSize,
+    maxFiles,
+    isSuccess,
+  } = useDropzoneContext()
+
+  const exceedMaxFiles = files.length > maxFiles
 
   const handleRemoveFile = useCallback(
     (fileName: string) => {
@@ -56,59 +81,86 @@ const DropzoneContent = ({ className }: { className?: string }) => {
     [files, setFiles]
   )
 
-  if (success) {
+  if (isSuccess) {
     return (
-      <div className={cn('flex flex-row items-center gap-1 justify-center', className)}>
-        <CheckCircle className="text-primary text-lg font-medium" />
-        <p className="text-primary text-lg font-medium">Files uploaded successfully</p>
+      <div className={cn('flex flex-row items-center gap-x-2 justify-center', className)}>
+        <CheckCircle size={16} className="text-primary" />
+        <p className="text-primary text-sm">
+          Successfully uploaded {files.length} file{files.length > 1 ? 's' : ''}
+        </p>
       </div>
     )
   }
 
   return (
     <div className={cn('flex flex-col', className)}>
-      {files.map((file) => {
+      {files.map((file, idx) => {
+        const fileError = errors.find((e) => e.name === file.name)
+        const isSuccessfullyUploaded = !!successes.find((e) => e === file.name)
+
         return (
           <div
-            key={file.name}
-            className="flex items-center gap-2 border-b p-2 first:mt-4 last:mb-4 "
+            key={`${file.name}-${idx}`}
+            className="flex items-center gap-x-4 border-b py-2 first:mt-4 last:mb-4 "
           >
             {file.type.startsWith('image/') ? (
-              <div className="h-12 w-12 rounded border overflow-hidden flex-shrink-0 bg-muted">
+              <div className="h-10 w-10 rounded border overflow-hidden shrink-0 bg-muted flex items-center justify-center">
                 <img src={file.preview} alt={file.name} className="object-cover" />
               </div>
             ) : (
-              <div className="h-12 w-12 rounded border bg-muted flex items-center justify-center">
-                <File />
+              <div className="h-10 w-10 rounded border bg-muted flex items-center justify-center">
+                <File size={18} />
               </div>
             )}
-            <div className="grow flex items-center gap-2 justify-start">
-              <span className="text-sm">{file.name}</span>
-              {file.errors.length !== 0 && (
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <CircleAlert className="text-destructive" />
-                  </TooltipTrigger>
-                  <TooltipContent>{file.errors.map((e) => e.message).join(', ')}</TooltipContent>
-                </Tooltip>
+
+            <div className="shrink grow flex flex-col items-start truncate">
+              <p title={file.name} className="text-sm truncate max-w-full">
+                {file.name}
+              </p>
+              {file.errors.length > 0 ? (
+                <p className="text-xs text-destructive">
+                  {file.errors
+                    .map((e) =>
+                      e.message.startsWith('File is larger than')
+                        ? `File is larger than ${formatBytes(maxFileSize, 2)} (Size: ${formatBytes(file.size, 2)})`
+                        : e.message
+                    )
+                    .join(', ')}
+                </p>
+              ) : loading && !isSuccessfullyUploaded ? (
+                <p className="text-xs text-foreground-light">Uploading file...</p>
+              ) : !!fileError ? (
+                <p className="text-xs text-destructive">Failed to upload: {fileError.message}</p>
+              ) : isSuccessfullyUploaded ? (
+                <p className="text-xs text-primary">Successfully uploaded file</p>
+              ) : (
+                <p className="text-xs text-foreground-light">{formatBytes(file.size, 2)}</p>
               )}
             </div>
 
-            <Button
-              size="icon"
-              variant="destructive"
-              className="justify-self-end"
-              onClick={() => handleRemoveFile(file.name)}
-            >
-              <Trash />
-            </Button>
+            {!loading && !isSuccessfullyUploaded && (
+              <Button
+                size="icon"
+                variant="link"
+                className="shrink-0 justify-self-end"
+                onClick={() => handleRemoveFile(file.name)}
+              >
+                <X />
+              </Button>
+            )}
           </div>
         )
       })}
-      {files.length > 0 && (
+      {exceedMaxFiles && (
+        <p className="text-sm text-left mt-2 text-destructive">
+          You may upload only up to {maxFiles} files, please remove {files.length - maxFiles} file
+          {files.length - maxFiles > 1 ? 's' : ''}.
+        </p>
+      )}
+      {files.length > 0 && !exceedMaxFiles && (
         <div className="mt-2">
           <Button
-            variant="default"
+            variant="outline"
             onClick={onUpload}
             disabled={files.some((file) => file.errors.length !== 0) || loading}
           >
@@ -118,37 +170,46 @@ const DropzoneContent = ({ className }: { className?: string }) => {
                 Uploading...
               </>
             ) : (
-              <>Upload</>
+              <>Upload files</>
             )}
           </Button>
         </div>
       )}
-      {error && <p className="text-destructive text-lg font-medium">{error}</p>}
     </div>
   )
 }
 
 const DropzoneEmptyState = ({ className }: { className?: string }) => {
-  const { files, maxFiles, maxFileSize, inputRef, success } = useDropzoneContext()
+  const { maxFiles, maxFileSize, inputRef, isSuccess } = useDropzoneContext()
 
-  if ((maxFiles !== 0 && files.length >= maxFiles) || success) {
+  if (isSuccess) {
     return null
   }
 
   return (
-    <div className={cn('flex flex-col items-center', className)}>
-      <div className="rounded-full bg-primary text-primary-foreground p-2">
-        <Upload />
-      </div>
-      <p className="text-lg font-medium">
-        Add or{' '}
-        <a onClick={() => inputRef.current?.click()} className="underline cursor-pointer">
-          drop {maxFiles === 1 ? `file` : 'files'}
-        </a>
+    <div className={cn('flex flex-col items-center gap-y-2', className)}>
+      <Upload size={20} className="text-foreground-light" />
+      <p className="text-sm text-foreground">
+        Upload{!!maxFiles && maxFiles > 1 ? ` ${maxFiles}` : ''} file
+        {!maxFiles || maxFiles > 1 ? 's' : ''}
       </p>
-      {maxFileSize !== Number.POSITIVE_INFINITY && (
-        <p className="text-sm text-muted-foreground mt-2">Maximum file size: {maxFileSize} MB</p>
-      )}
+      <div className="flex flex-col items-center gap-y-1">
+        <p className="text-xs text-foreground-light">
+          Drag and drop or{' '}
+          <a
+            onClick={() => inputRef.current?.click()}
+            className="underline cursor-pointer transition hover:text-foreground"
+          >
+            select {maxFiles === 1 ? `file` : 'files'}
+          </a>{' '}
+          to upload
+        </p>
+        {maxFileSize !== Number.POSITIVE_INFINITY && (
+          <p className="text-xs text-foreground-light">
+            Maximum file size: {formatBytes(maxFileSize, 2)}
+          </p>
+        )}
+      </div>
     </div>
   )
 }
--- apps/ui-library/registry/default/components/ui/button.tsx
+++ apps/ui-library/registry/default/components/ui/button.tsx
@@ -1,15 +1,16 @@
-import * as React from 'react'
 import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
+import * as React from 'react'
 
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+  'inline-flex items-center cursor-pointer justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground shadow hover:bg-primary/90',
+        default:
+          'bg-brand-400 dark:bg-brand-500 text-foreground shadow hover:bg-brand/80 dark:hover:bg-brand/50',
         destructive: 'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
         outline:
           'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
