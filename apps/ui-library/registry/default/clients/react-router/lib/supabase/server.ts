644860f0901c0cdea92fd1e0895daa14f18fb637
Ivan Vasilov <vasilov.ivan@gmail.com>
2025-03-26 22:18:48 +0100

    Fix the react route client.

--- apps/ui-library/registry/default/clients/react-router/lib/supabase/server.ts
+++ apps/ui-library/registry/default/clients/react-router/lib/supabase/server.ts
@@ -3,21 +3,25 @@ import { createServerClient, parseCookieHeader, serializeCookieHeader } from '@s
 export function createClient(request: Request) {
   const headers = new Headers()
 
-  const supabase = createServerClient(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!, {
-    cookies: {
-      getAll() {
-        return parseCookieHeader(request.headers.get('Cookie') ?? '') as {
-          name: string
-          value: string
-        }[]
+  const supabase = createServerClient(
+    process.env.VITE_SUPABASE_URL!,
+    process.env.VITE_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return parseCookieHeader(request.headers.get('Cookie') ?? '') as {
+            name: string
+            value: string
+          }[]
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) =>
+            headers.append('Set-Cookie', serializeCookieHeader(name, value, options))
+          )
+        },
       },
-      setAll(cookiesToSet) {
-        cookiesToSet.forEach(({ name, value, options }) =>
-          headers.append('Set-Cookie', serializeCookieHeader(name, value, options))
-        )
-      },
-    },
-  })
+    }
+  )
 
   return { supabase, headers }
 }
