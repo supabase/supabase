43135f52da2f38e7dcb339fa0c52403e192cec18
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2025-03-27 12:00:33 +0800

    remove migrations

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -6,10 +6,3 @@ values
   ('Singapore', 'Singapore', 'lw12', now(), true);
 
 insert into public.launch_weeks (id) values ('lw14');
-
-insert into meetups
-  (title, country, launch_week, start_at, is_published)
-values
-  ('New York', 'USA', 'lw14', now(), true),
-  ('London', 'UK', 'lw14', now(), true),
-  ('Singapore', 'Singapore', 'lw14', now(), true);
