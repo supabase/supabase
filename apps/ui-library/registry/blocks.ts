a81d056d793a9dcd83eb6457b5e90ff507a6cea0
Ivan Vasilov <vasilov.ivan@gmail.com>
2025-03-27 18:41:31 +0200

    feat: Extra components for UI Library (#34363)
    * Fix the v0 button. Add some more docs. (+13 squashed commits)
    Squashed commits:
    [cc5954779e] Add success state to the forgot-password form.
    [258bfb1015] Simplify the tanstack auth block.
    [1ba5c223d9] Add missing pages to the nextjs auth.
    [b842e4acec] Fix the env vars in the React client.
    [2a2bcc5356] Fix the command URL.
    [a26a2d36c2] Add a tanstack block for password-based auth.
    [d68881f0d5] Fix the tanstack client.
    [1fd2e16d96] Add missing deps to satisfy TS build.
    [9797d745df] Various fixes.
    [3e9b676e99] Fix the registryBlock component.
    [540a5d600b] Set the supabase project for testing.
    [3eba892c92] Regenerate the llms.txt file.
    [bf526a0ecb] Regenerate the registry files.
    
    * Add current user avatar.
    
    * Add RealtimeAvatarStack.
    
    * Use the fields which are populated by Auth.
    
    * Regenerate the registry files.
    
    * Fix the imports.
    
    * Rebuild the registry files. Add a github login to the supabase config.
    
    * Minor fixes for the components.
    
    * Minor fix to the avatar stack.
    
    * Remove peekCode, show showCode prop to component preview.
    
    * Add examples for avatar stack and current user avatar.
    
    * Use the new generatenames function in the cursors.
    
    * Add documentation for avatar stack and avatar.
    
    * Switch the profile images.
    
    * Fix a type error.
    
    * More fixes.

--- apps/ui-library/registry/blocks.ts
+++ apps/ui-library/registry/blocks.ts
@@ -1,9 +1,11 @@
 import { type Registry, type RegistryItem } from 'shadcn/registry'
 import { clients } from './clients'
+import currentUserAvatar from './default/blocks/current-user-avatar/registry-item.json' assert { type: 'json' }
 import dropzone from './default/blocks/dropzone/registry-item.json' assert { type: 'json' }
 import passwordBasedAuthNextjs from './default/blocks/password-based-auth-nextjs/registry-item.json' assert { type: 'json' }
 import passwordBasedAuthReact from './default/blocks/password-based-auth-react/registry-item.json' assert { type: 'json' }
 import passwordBasedAuthTanstack from './default/blocks/password-based-auth-tanstack/registry-item.json' assert { type: 'json' }
+import realtimeAvatarStack from './default/blocks/realtime-avatar-stack/registry-item.json' assert { type: 'json' }
 
 import realtimeCursor from './default/blocks/realtime-cursor/registry-item.json' assert { type: 'json' }
 import { registryItemAppend } from './utils'
@@ -30,4 +32,6 @@ export const blocks = [
   registryItemAppend(passwordBasedAuthTanstack as RegistryItem, [tanstackClient!]),
   ...combine(dropzone as RegistryItem),
   ...combine(realtimeCursor as RegistryItem),
+  ...combine(currentUserAvatar as RegistryItem),
+  ...combine(realtimeAvatarStack as RegistryItem),
 ] as Registry['items']
