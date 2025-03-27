1df98099b010ce8bcf110482d768e3b7e4a9ae11
Terry Sutton <saltcod@gmail.com>
2025-03-27 14:50:38 -0230

    UI lib/docs (#34448)
    * Buncha docs
    
    * Add prompts instructions
    
    * Minor fixes.
    
    ---------
    
    Co-authored-by: Ivan Vasilov <vasilov.ivan@gmail.com>

--- apps/ui-library/components/mdx-components.tsx
+++ apps/ui-library/components/mdx-components.tsx
@@ -167,7 +167,7 @@ const components = {
       <StyleWrapper styleName={__style__}>
         <pre
           className={cn(
-            'mb-4 mt-6 max-h-[650px] overflow-x-auto rounded-lg border bg-surface-75/75 py-4',
+            'mb-4 mt-6 max-h-[650px] overflow-x-auto rounded-lg border bg-surface-75/75 py-4 text-foreground-light',
             className
           )}
           {...props}
