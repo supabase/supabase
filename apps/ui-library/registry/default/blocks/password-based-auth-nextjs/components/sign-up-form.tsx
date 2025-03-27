884a65a1172d46629010759704365ddd5d4b627f
Ivan Vasilov <vasilov.ivan@gmail.com>
2025-03-27 23:36:12 +0100

    Change the folder definition to /auth.

--- apps/ui-library/registry/default/blocks/password-based-auth-nextjs/components/sign-up-form.tsx
+++ apps/ui-library/registry/default/blocks/password-based-auth-nextjs/components/sign-up-form.tsx
@@ -45,7 +45,7 @@ export function SignUpForm({ className, ...props }: React.ComponentPropsWithoutR
         },
       })
       if (error) throw error
-      router.push('/example/password-based-auth/sign-up-success')
+      router.push('/example/password-based-auth/auth/sign-up-success')
     } catch (error: unknown) {
       setError(error instanceof Error ? error.message : 'An error occurred')
     } finally {
