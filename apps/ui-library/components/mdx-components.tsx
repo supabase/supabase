d1bfcf3dcdb96015253005f1e936790748253c6a
Ivan Vasilov <vasilov.ivan@gmail.com>
2025-03-28 16:34:48 +0100

    Fix the colors of the linkedcard.

--- apps/ui-library/components/mdx-components.tsx
+++ apps/ui-library/components/mdx-components.tsx
@@ -260,7 +260,7 @@ const components = {
   LinkedCard: ({ className, ...props }: React.ComponentProps<typeof Link>) => (
     <Link
       className={cn(
-        'flex w-full flex-col items-center rounded-xl border bg-card p-6 text-card-foreground shadow transition-colors hover:bg-muted/50 sm:p-10',
+        'flex w-full flex-col items-center rounded-xl border bg-surface-100 text-card-background p-6 shadow transition-colors hover:bg-muted/50 sm:p-10',
         className
       )}
       {...props}
