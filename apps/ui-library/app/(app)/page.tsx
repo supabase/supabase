f681cc08840e5c191694a4fcbedbbef180062652
Ivan Vasilov <vasilov.ivan@gmail.com>
2025-03-30 23:10:13 +0200

    Add realtime chat to the landing page.

--- apps/ui-library/app/(app)/page.tsx
+++ apps/ui-library/app/(app)/page.tsx
@@ -139,6 +139,7 @@ export default function Home() {
                 </div>
               </div>
               <HorizontalGridLine />
+
               {/* Realtime Chat */}
               <div
                 data-grid-item
