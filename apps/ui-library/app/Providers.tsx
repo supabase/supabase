bcf34878358ce194401b139624395f6ae279737b
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2025-03-31 20:45:43 +0800

    move telemetry wrapper down to child layout

--- apps/ui-library/app/Providers.tsx
+++ apps/ui-library/app/Providers.tsx
@@ -6,14 +6,11 @@ import { ThemeProviderProps } from 'next-themes/dist/types'
 
 import { FrameworkProvider } from '@/context/framework-context'
 import { MobileMenuProvider } from '@/context/mobile-menu-context'
-import { AuthProvider, IS_PLATFORM, PageTelemetry } from 'common'
+import { AuthProvider } from 'common'
 import { TooltipProvider } from 'ui'
 import { useConsent } from 'ui-patterns/ConsentToast'
-import { API_URL } from '@/lib/constants'
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  const { hasAcceptedConsent } = useConsent()
-
   return (
     <AuthProvider>
       <JotaiProvider>
@@ -24,11 +21,6 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
             </FrameworkProvider>
           </MobileMenuProvider>
         </NextThemesProvider>
-        <PageTelemetry
-          API_URL={API_URL}
-          hasAcceptedConsent={hasAcceptedConsent}
-          enabled={IS_PLATFORM}
-        />
       </JotaiProvider>
     </AuthProvider>
   )
