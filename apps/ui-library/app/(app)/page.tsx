5c42992d95e079880e0e1890e92e9cc5379f69a9
Terry Sutton <saltcod@gmail.com>
2025-03-31 11:20:37 -0230

    Mobile fixes

--- apps/ui-library/app/(app)/page.tsx
+++ apps/ui-library/app/(app)/page.tsx
@@ -75,8 +75,11 @@ export default function Home() {
                 </Link>
               </div>
               <HorizontalGridLine />
-              <div className="col-start-2 col-span-10 md:col-start-3 md:col-span-8 relative">
-                <div className="-mt-4 flex rounded-lg overflow-hidden">
+              <div
+                data-grid-item
+                className="col-start-2 col-span-10 md:col-start-3 md:col-span-8 relative"
+              >
+                <div className="-mt-4 grid md:flex rounded-lg overflow-hidden">
                   <BlockPreview name="realtime-cursor-demo" isPair />
                   <BlockPreview name="realtime-cursor-demo" isPair />
                 </div>
@@ -148,8 +151,11 @@ export default function Home() {
                 </Link>
               </div>
               <HorizontalGridLine />
-              <div className="col-start-2 col-span-10 md:col-start-3 md:col-span-8 relative">
-                <div className="-mt-4 flex rounded-lg overflow-hidden">
+              <div
+                data-grid-item
+                className="col-start-2 col-span-10 md:col-start-3 md:col-span-8 relative"
+              >
+                <div className="-mt-4 grid md:flex rounded-lg overflow-hidden">
                   <BlockPreview name="realtime-chat-demo" isPair />
                   <BlockPreview name="realtime-chat-demo" isPair />
                 </div>
