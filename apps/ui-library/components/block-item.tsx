757feb4986b0cc80d312f4b5aeff79531171593c
Ivan Vasilov <vasilov.ivan@gmail.com>
2025-03-31 10:01:43 +0200

    Add a comment for the dynamic import.

--- apps/ui-library/components/block-item.tsx
+++ apps/ui-library/components/block-item.tsx
@@ -2,6 +2,7 @@ import { OpenInV0Button } from '@/components/open-in-v0-button'
 
 import dynamic from 'next/dynamic'
 
+//The dynamic import is to prevent the command component from being rendered on the server and cause hydration errors
 const Command = dynamic(() => import('./command').then((mod) => mod.Command), { ssr: false })
 
 interface BlockItemProps {
