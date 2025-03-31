d0d3dd48bb8e62b016e76fb7ac23d260016e01ca
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2025-03-31 19:00:17 +0800

    chore: update telemetry

--- apps/ui-library/app/Providers.tsx
+++ apps/ui-library/app/Providers.tsx
@@ -1,15 +1,19 @@
 'use client'
 
-import { AuthProvider } from 'common'
 import { Provider as JotaiProvider } from 'jotai'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { ThemeProviderProps } from 'next-themes/dist/types'
 
 import { FrameworkProvider } from '@/context/framework-context'
 import { MobileMenuProvider } from '@/context/mobile-menu-context'
+import { AuthProvider, IS_PLATFORM, PageTelemetry } from 'common'
 import { TooltipProvider } from 'ui'
+import { useConsent } from 'ui-patterns/ConsentToast'
+import { API_URL } from '@/lib/constants'
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  const { hasAcceptedConsent } = useConsent()
+
   return (
     <AuthProvider>
       <JotaiProvider>
@@ -20,6 +24,11 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
             </FrameworkProvider>
           </MobileMenuProvider>
         </NextThemesProvider>
+        <PageTelemetry
+          API_URL={API_URL}
+          hasAcceptedConsent={hasAcceptedConsent}
+          enabled={IS_PLATFORM}
+        />
       </JotaiProvider>
     </AuthProvider>
   )
