afd764905c5c0802d72360020541cba2bb0eeba4
Joshen Lim <joshenlimek@gmail.com>
2025-04-02 16:18:28 +0800

    Fix account pages for when feature preview is off

--- apps/studio/pages/account/audit.tsx
+++ apps/studio/pages/account/audit.tsx
@@ -1,16 +1,39 @@
 import { AuditLogs } from 'components/interfaces/Account'
+import { useNewLayout } from 'components/interfaces/App/FeaturePreview/FeaturePreviewContext'
 import AccountLayout from 'components/layouts/AccountLayout/AccountLayout'
 import AppLayout from 'components/layouts/AppLayout/AppLayout'
 import DefaultLayout from 'components/layouts/DefaultLayout'
 import OrganizationLayout from 'components/layouts/OrganizationLayout'
-import { ScaffoldContainerLegacy } from 'components/layouts/Scaffold'
+import {
+  ScaffoldContainer,
+  ScaffoldContainerLegacy,
+  ScaffoldDescription,
+  ScaffoldHeader,
+  ScaffoldTitle,
+} from 'components/layouts/Scaffold'
 import type { NextPageWithLayout } from 'types'
 
 const Audit: NextPageWithLayout = () => {
+  const newLayoutPreview = useNewLayout()
+
+  if (newLayoutPreview) {
+    return (
+      <ScaffoldContainerLegacy className="gap-0">
+        <AuditLogs />
+      </ScaffoldContainerLegacy>
+    )
+  }
+
   return (
-    <ScaffoldContainerLegacy className="gap-0">
+    <ScaffoldContainer>
+      <ScaffoldHeader>
+        <ScaffoldTitle>Account Audit Logs</ScaffoldTitle>
+        <ScaffoldDescription>
+          View the audit log trail of actions made from your account
+        </ScaffoldDescription>
+      </ScaffoldHeader>
       <AuditLogs />
-    </ScaffoldContainerLegacy>
+    </ScaffoldContainer>
   )
 }
 
--- apps/studio/pages/account/security.tsx
+++ apps/studio/pages/account/security.tsx
@@ -1,20 +1,32 @@
 import { Smartphone } from 'lucide-react'
 
 import { TOTPFactors } from 'components/interfaces/Account'
+import { useNewLayout } from 'components/interfaces/App/FeaturePreview/FeaturePreviewContext'
 import AccountLayout from 'components/layouts/AccountLayout/AccountLayout'
 import AccountSettingsLayout from 'components/layouts/AccountLayout/AccountSettingsLayout'
 import AppLayout from 'components/layouts/AppLayout/AppLayout'
 import DefaultLayout from 'components/layouts/DefaultLayout'
 import OrganizationLayout from 'components/layouts/OrganizationLayout'
-import { FormHeader } from 'components/ui/Forms/FormHeader'
+import {
+  ScaffoldContainer,
+  ScaffoldDescription,
+  ScaffoldHeader,
+  ScaffoldTitle,
+} from 'components/layouts/Scaffold'
 import { useMfaListFactorsQuery } from 'data/profile/mfa-list-factors-query'
 import type { NextPageWithLayout } from 'types'
-import { Badge, cn, Collapsible } from 'ui'
+import {
+  Badge,
+  cn,
+  Collapsible_Shadcn_,
+  CollapsibleContent_Shadcn_,
+  CollapsibleTrigger_Shadcn_,
+} from 'ui'
 
 const collapsibleClasses = [
   'bg-surface-100',
-  'hover:bg-overlay-hover',
-  'data-open:bg-selection',
+  'hover:bg-surface-200',
+  'data-open:bg-surface-200',
   'border-default',
   'hover:border-strong data-open:border-strong',
   'data-open:pb-px col-span-12 rounded',
@@ -25,37 +37,49 @@ const collapsibleClasses = [
 ]
 
 const Security: NextPageWithLayout = () => {
+  const newLayoutPreview = useNewLayout()
   const { data } = useMfaListFactorsQuery()
 
-  return (
-    <>
-      <FormHeader
-        title="Multi-factor authentication"
-        description="Add an additional layer of security to your account by requiring more than just a password to sign in."
-      />
-      <Collapsible className={cn(collapsibleClasses, 'w-full flex flex-1 flex-grow')}>
-        <Collapsible.Trigger asChild>
-          <button
-            type="button"
-            className="group flex w-full items-center justify-between rounded py-3 px-4 md:px-6 text-foreground"
-          >
-            <div className="flex flex-row gap-4 items-center py-1">
-              <Smartphone strokeWidth={1.5} />
-              <span className="text-sm">Authenticator app</span>
-            </div>
+  const PageContent = () => (
+    <Collapsible_Shadcn_ className={cn(collapsibleClasses)}>
+      <CollapsibleTrigger_Shadcn_ asChild>
+        <button
+          type="button"
+          className="group flex w-full items-center justify-between rounded py-3 px-4 md:px-6 text-foreground"
+        >
+          <div className="flex flex-row gap-4 items-center py-1">
+            <Smartphone strokeWidth={1.5} />
+            <span className="text-sm">Authenticator app</span>
+          </div>
 
-            {data ? (
-              <Badge variant={data.totp.length === 0 ? 'default' : 'brand'}>
-                {data.totp.length} app{data.totp.length === 1 ? '' : 's'} configured
-              </Badge>
-            ) : null}
-          </button>
-        </Collapsible.Trigger>
-        <Collapsible.Content className="group border-t border-default bg-surface-100 py-6 px-4 md:px-6 text-foreground">
-          <TOTPFactors />
-        </Collapsible.Content>
-      </Collapsible>
-    </>
+          {data ? (
+            <Badge variant={data.totp.length === 0 ? 'default' : 'brand'}>
+              {data.totp.length} app{data.totp.length === 1 ? '' : 's'} configured
+            </Badge>
+          ) : null}
+        </button>
+      </CollapsibleTrigger_Shadcn_>
+      <CollapsibleContent_Shadcn_ className="group border-t border-default bg-surface-100 py-6 px-4 md:px-6 text-foreground">
+        <TOTPFactors />
+      </CollapsibleContent_Shadcn_>
+    </Collapsible_Shadcn_>
+  )
+
+  if (newLayoutPreview) {
+    return <PageContent />
+  }
+
+  return (
+    <ScaffoldContainer>
+      <ScaffoldHeader>
+        <ScaffoldTitle>Multi-Factor Authentication</ScaffoldTitle>
+        <ScaffoldDescription>
+          Add an additional layer of security to your account by requiring more than just a password
+          to sign in.
+        </ScaffoldDescription>
+      </ScaffoldHeader>
+      <PageContent />
+    </ScaffoldContainer>
   )
 }
 
