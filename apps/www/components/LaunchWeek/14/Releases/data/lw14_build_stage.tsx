fd7b2862f39a695e618c200451ebcf1fb050da1e
Wen Bo Xie <wenbo.xie3@gmail.com>
2025-04-04 15:50:55 +0200

    geo aware routing blog

--- apps/www/components/LaunchWeek/14/Releases/data/lw14_build_stage.tsx
+++ apps/www/components/LaunchWeek/14/Releases/data/lw14_build_stage.tsx
@@ -3,7 +3,7 @@
 import { ReactNode } from 'react'
 import { type ClassValue } from 'clsx'
 import { PRODUCT_MODULES } from 'shared-data/products'
-import { AppWindow, Database } from 'lucide-react'
+import { AppWindow, Database, ArrowUpDown } from 'lucide-react'
 
 export interface AdventDay {
   icon?: ReactNode // use svg jsx with 34x34px viewport
@@ -155,5 +155,20 @@ export const days: AdventDay[] = [
       },
     ],
     icon: <Database size={30} />,
+  },
+  {
+    title: 'Data API Routes to Nearest Read Replica',
+    // description: '',
+    id: 'data-api-routes',
+    is_shipped: true,
+    className: 'xl:col-span-2',
+    links: [
+      {
+        url: '/blog/data-api-nearest-replica',
+        label: 'Blog post',
+        target: '_blank',
+      },
+    ],
+    icon: <ArrowUpDown size={30} />,
   },
 ]
