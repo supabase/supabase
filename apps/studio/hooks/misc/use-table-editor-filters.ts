4c22019d227ee33e9c42106a7849caf081ec7b67
Jordi Enric <jordi.err@gmail.com>
2025-04-07 16:21:17 +0200

    rm consolelogs

--- apps/studio/hooks/misc/use-table-editor-filters.ts
+++ apps/studio/hooks/misc/use-table-editor-filters.ts
@@ -8,7 +8,6 @@ export const useTableEditorFilters = () => {
   const isNewSyntax = urlFilters.length > 0 && urlFilters[0].includes(',')
 
   function getFilters(): string[] {
-    console.log('urlParams', urlParams.getAll('filter'))
     if (isNewSyntax) {
       return urlParams.getAll('filter')[0].split(',')
     }
@@ -33,9 +32,6 @@ export const useTableEditorFilters = () => {
 
   const filters = getFilters()
   const sorts = urlParams.getAll('sort')
-
-  console.log('filters', filters)
-  console.log('sorts', sorts)
 
   return {
     filters,
