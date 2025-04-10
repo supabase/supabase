fb76ac5807d84b68ffe8db442eb2734f698140c2
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2025-04-10 12:22:31 +0800

    init
    fix FE-1535

--- apps/studio/components/layouts/ProjectLayout/LayoutHeader/FeedbackDropdown/FeedbackWidget.tsx
+++ apps/studio/components/layouts/ProjectLayout/LayoutHeader/FeedbackDropdown/FeedbackWidget.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/router'
 import { ChangeEvent, useEffect, useRef, useState } from 'react'
 import { toast } from 'sonner'
 
+import { PopoverSeparator } from '@ui/components/shadcn/ui/popover'
 import { useParams } from 'common'
 import { useSendFeedbackMutation } from 'data/feedback/feedback-send'
 import { useSendEventMutation } from 'data/telemetry/send-event-mutation'
@@ -15,8 +16,9 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
-  Input,
+  TextArea_Shadcn_,
 } from 'ui'
 import { convertB64toBlob, uploadAttachment } from './FeedbackDropdown.utils'
 
@@ -176,128 +178,119 @@ const FeedbackWidget = ({
   }
 
   return (
-    <div id="feedback-widget" className="text-area-text-sm">
-      <Input.TextArea
-        className="w-80 p-3"
-        size="small"
-        placeholder="Ideas on how to improve this page.&#10;Use the Support Form for technical issues."
-        rows={5}
-        value={feedback}
-        onChange={(e) => setFeedback(e.target.value)}
-        onPaste={handlePasteEvent}
-      />
-      <div className="w-full h-px bg-border" />
-      <div className="w-80 space-y-3 px-3 py-2 pb-4">
-        <div className="flex justify-between space-x-2">
-          <Button
-            type="default"
-            onClick={() => {
-              clearFeedback()
-              onClose()
-            }}
-          >
-            Cancel
-          </Button>
-          <div className="flex items-center space-x-2">
-            <Button type="default" onClick={clearFeedback}>
-              Clear
-            </Button>
-            {screenshot !== undefined ? (
-              <div
-                style={{ backgroundImage: `url("${screenshot}")` }}
-                onClick={() => {
-                  const blob = convertB64toBlob(screenshot)
-                  const blobUrl = URL.createObjectURL(blob)
-                  window.open(blobUrl, '_blank')
+    <>
+      <div className="px-3">
+        <TextArea_Shadcn_
+          placeholder="Ideas on how to improve this page.&#10;Use the Support Form for technical issues."
+          rows={5}
+          value={feedback}
+          onChange={(e) => setFeedback(e.target.value)}
+          onPaste={handlePasteEvent}
+          className="text-sm"
+        />
+      </div>
+      <PopoverSeparator />
+      <div className="px-3 flex flex-row justify-between items-start">
+        <div>
+          <p className="text-xs text-foreground">Have a technical issue?</p>
+          <p className="text-xs text-foreground-light">
+            Contact{' '}
+            <Link href="/support/new">
+              <span className="cursor-pointer text-brand transition-colors hover:text-brand-600">
+                support
+              </span>
+            </Link>{' '}
+            or{' '}
+            <a href="https://supabase.com/docs" target="_blank" rel="noreferrer">
+              <span className="cursor-pointer text-brand transition-colors hover:text-brand-600">
+                see docs
+              </span>
+            </a>
+            .
+          </p>
+        </div>
+        <div className="flex items-center gap-2 flex-row">
+          {screenshot !== undefined ? (
+            <div
+              style={{ backgroundImage: `url("${screenshot}")` }}
+              onClick={() => {
+                const blob = convertB64toBlob(screenshot)
+                const blobUrl = URL.createObjectURL(blob)
+                window.open(blobUrl, '_blank')
+              }}
+              className="cursor-pointer rounded h-[26px] w-[30px] border border-control relative bg-cover bg-center bg-no-repeat"
+            >
+              <button
+                className={[
+                  'cursor-pointer rounded-full bg-red-900 h-3 w-3',
+                  'flex items-center justify-center absolute -top-1 -right-1',
+                ].join(' ')}
+                onClick={(event) => {
+                  event.stopPropagation()
+                  setScreenshot(undefined)
                 }}
-                className="cursor-pointer rounded h-[26px] w-[30px] border border-control relative bg-cover bg-center bg-no-repeat"
               >
-                <button
-                  className={[
-                    'cursor-pointer rounded-full bg-red-900 h-3 w-3',
-                    'flex items-center justify-center absolute -top-1 -right-1',
-                  ].join(' ')}
-                  onClick={(event) => {
-                    event.stopPropagation()
-                    setScreenshot(undefined)
+                <X size={8} strokeWidth={3} />
+              </button>
+            </div>
+          ) : (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  type="default"
+                  disabled={isSavingScreenshot}
+                  loading={isSavingScreenshot}
+                  className="px-2"
+                >
+                  <ImageIcon size={14} />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent side="bottom" align="end" className="w-fit">
+                <DropdownMenuItem
+                  className="flex gap-2"
+                  key="upload-screenshot"
+                  onSelect={() => {
+                    if (uploadButtonRef.current) (uploadButtonRef.current as any).click()
                   }}
                 >
-                  <X size={8} strokeWidth={3} />
-                </button>
-              </div>
-            ) : (
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button
-                    type="default"
-                    disabled={isSavingScreenshot}
-                    loading={isSavingScreenshot}
-                    className="px-2 py-1.5"
-                  >
-                    <ImageIcon size={14} />
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent side="bottom" align="end">
-                  <DropdownMenuItem
-                    className="flex gap-2"
-                    key="upload-screenshot"
-                    onSelect={() => {
-                      if (uploadButtonRef.current) (uploadButtonRef.current as any).click()
-                    }}
-                  >
-                    <Upload size={14} />
-                    Upload screenshot
-                  </DropdownMenuItem>
-                  <DropdownMenuItem
-                    className="flex gap-2"
-                    key="capture-screenshot"
-                    onSelect={() => captureScreenshot()}
-                  >
-                    <Camera size={14} />
-                    Capture screenshot
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            )}
-            <input
-              type="file"
-              ref={uploadButtonRef}
-              className="hidden"
-              accept="image/png"
-              onChange={onFilesUpload}
-            />
-            <Button
-              disabled={feedback.length === 0 || isSending}
-              loading={isSending}
-              onClick={() => {
-                sendFeedback()
-                sendEvent({
-                  action: 'send_feedback_button_clicked',
-                  groups: { project: ref, organization: org?.slug },
-                })
-              }}
-            >
-              Send feedback
-            </Button>
-          </div>
+                  <Upload size={14} />
+                  Upload screenshot
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem
+                  className="flex gap-2"
+                  key="capture-screenshot"
+                  onSelect={() => captureScreenshot()}
+                >
+                  <Camera size={14} />
+                  Capture screenshot
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )}
+          <input
+            type="file"
+            ref={uploadButtonRef}
+            className="hidden"
+            accept="image/png"
+            onChange={onFilesUpload}
+          />
+          <Button
+            disabled={feedback.length === 0 || isSending}
+            loading={isSending}
+            onClick={() => {
+              sendFeedback()
+              sendEvent({
+                action: 'send_feedback_button_clicked',
+                groups: { project: ref, organization: org?.slug },
+              })
+            }}
+          >
+            Send
+          </Button>
         </div>
-        <p className="text-xs text-foreground-light">
-          Have a technical issue? Contact{' '}
-          <Link href="/support/new">
-            <span className="cursor-pointer text-brand transition-colors hover:text-brand-600">
-              Supabase support
-            </span>
-          </Link>{' '}
-          or{' '}
-          <a href="https://supabase.com/docs" target="_blank" rel="noreferrer">
-            <span className="cursor-pointer text-brand transition-colors hover:text-brand-600">
-              browse our docs
-            </span>
-          </a>
-          .
-        </p>
       </div>
-    </div>
+    </>
   )
 }
 
