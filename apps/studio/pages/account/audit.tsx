88ee6d0f6162a8e2ec61c92e7bb0598bdbe7b627
Joshen Lim <joshenlimek@gmail.com>
2025-04-14 18:05:20 +0800

    Fix re-rendering of Account AuditLogs (#34994)
    * Temp
    
    * Fix
    
    * Remove conosle log
    
    * Fix
    
    * Fix

--- apps/studio/pages/account/audit.tsx
+++ apps/studio/pages/account/audit.tsx
@@ -6,32 +6,26 @@ import DefaultLayout from 'components/layouts/DefaultLayout'
 import OrganizationLayout from 'components/layouts/OrganizationLayout'
 import {
   ScaffoldContainer,
-  ScaffoldContainerLegacy,
   ScaffoldDescription,
   ScaffoldHeader,
   ScaffoldTitle,
 } from 'components/layouts/Scaffold'
 import type { NextPageWithLayout } from 'types'
+import { cn } from 'ui'
 
 const Audit: NextPageWithLayout = () => {
   const newLayoutPreview = useNewLayout()
 
-  if (newLayoutPreview) {
-    return (
-      <ScaffoldContainerLegacy className="gap-0">
-        <AuditLogs />
-      </ScaffoldContainerLegacy>
-    )
-  }
-
   return (
-    <ScaffoldContainer>
-      <ScaffoldHeader>
-        <ScaffoldTitle>Account Audit Logs</ScaffoldTitle>
-        <ScaffoldDescription>
-          View the audit log trail of actions made from your account
-        </ScaffoldDescription>
-      </ScaffoldHeader>
+    <ScaffoldContainer className={cn(newLayoutPreview ? '[&>div]:mt-8' : '')}>
+      {!newLayoutPreview && (
+        <ScaffoldHeader>
+          <ScaffoldTitle>Account Audit Logs</ScaffoldTitle>
+          <ScaffoldDescription>
+            View the audit log trail of actions made from your account
+          </ScaffoldDescription>
+        </ScaffoldHeader>
+      )}
       <AuditLogs />
     </ScaffoldContainer>
   )
