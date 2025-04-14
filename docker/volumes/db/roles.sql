e8e1411ec71aa024cae15e7b7b578fc00dbecad5
arrorLabArts <120601830+arrorLabArts@users.noreply.github.com>
2025-04-15 03:49:56 +0530

    Update roles.sql
    roles.sql initialization script was missing the command to update the supabase_admin user's password.

--- docker/volumes/db/roles.sql
+++ docker/volumes/db/roles.sql
@@ -6,3 +6,4 @@ ALTER USER pgbouncer WITH PASSWORD :'pgpass';
 ALTER USER supabase_auth_admin WITH PASSWORD :'pgpass';
 ALTER USER supabase_functions_admin WITH PASSWORD :'pgpass';
 ALTER USER supabase_storage_admin WITH PASSWORD :'pgpass';
+ALTER USER supabase_admin WITH PASSWORD :'pgpass';
