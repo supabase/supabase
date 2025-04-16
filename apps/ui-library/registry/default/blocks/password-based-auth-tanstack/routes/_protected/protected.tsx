72fd93b2ea7a687848644e04ed256d32e8bf5b60
Ivan Vasilov <vasilov.ivan@gmail.com>
2025-04-16 11:14:48 +0200

    Update all auth blocks to use /protected.

--- apps/ui-library/registry/default/blocks/password-based-auth-tanstack/routes/_protected/protected.tsx
+++ apps/ui-library/registry/default/blocks/password-based-auth-tanstack/routes/_protected/protected.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createFileRoute('/_protected/info')({
+export const Route = createFileRoute('/_protected/protected')({
   component: Info,
   loader: async ({ context }) => {
     return {
