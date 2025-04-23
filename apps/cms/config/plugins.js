42c14ca0b7f7e06aa170fc5e205cd9fa5ceb61c2
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2025-04-23 10:12:58 +0200

    storage plugin

---
+++ apps/cms/config/plugins.js
@@ -0,0 +1,19 @@
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'strapi-provider-upload-supabase',
+      providerOptions: {
+        apiUrl: env('SUPABASE_API_URL'),
+        apiKey: env('SUPABASE_API_KEY'),
+        bucket: env('SUPABASE_BUCKET'),
+        directory: env('SUPABASE_DIRECTORY'),
+        options: {},
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+})
