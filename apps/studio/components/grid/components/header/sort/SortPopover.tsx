11cfa01c75cb0ff7e7c7e09a6c15f60bad581e8e
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2025-04-23 19:01:25 +0800

    fix sort popover issues

--- apps/studio/components/grid/components/header/sort/SortPopover.tsx
+++ apps/studio/components/grid/components/header/sort/SortPopover.tsx
@@ -2,6 +2,7 @@ import { useMemo } from 'react'
 
 import { formatSortURLParams } from 'components/grid/SupabaseGrid.utils'
 import { useTableSort } from 'components/grid/hooks/useTableSort'
+import { useTableEditorTableStateSnapshot } from 'state/table-editor-table'
 import SortPopoverPrimitive from './SortPopoverPrimitive'
 
 export interface SortPopoverProps {
@@ -10,13 +11,13 @@ export interface SortPopoverProps {
 
 const SortPopover = ({ portal = true }: SortPopoverProps) => {
   const { urlSorts, onApplySorts } = useTableSort()
+  const tableState = useTableEditorTableStateSnapshot()
+  const tableName = tableState?.table?.name || ''
 
   // Convert string[] to Sort[]
   const sorts = useMemo(() => {
-    const { tableState } = window as any
-    const tableName = tableState?.table?.name || ''
-    return tableName ? formatSortURLParams(tableName, urlSorts ?? []) : []
-  }, [urlSorts])
+    return tableName && urlSorts ? formatSortURLParams(tableName, urlSorts) : []
+  }, [tableName, urlSorts])
 
   return <SortPopoverPrimitive portal={portal} sorts={sorts} onApplySorts={onApplySorts} />
 }
