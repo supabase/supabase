ff4ee53951376814390af700e4ed180722244b82
Jordi Enric <jordi.err@gmail.com>
2025-04-29 10:56:03 +0200

    fix naming

--- apps/studio/tests/pages/projects/LogsPreviewer.test.tsx
+++ apps/studio/tests/pages/projects/LogsPreviewer.test.tsx
@@ -1,18 +1,13 @@
-import { findByText, screen, waitFor } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import { screen, waitFor } from '@testing-library/react'
 import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc'
-import { useRouter } from 'next/router'
 import { expect, test, vi } from 'vitest'
-
-dayjs.extend(utc)
-
 import { LogsTableName } from 'components/interfaces/Settings/Logs/Logs.constants'
 import LogsPreviewer from 'components/interfaces/Settings/Logs/LogsPreviewer'
-import { render } from '../../helpers'
+import { customRender } from 'tests/lib/custom-render'
+import userEvent from '@testing-library/user-event'
 
-// [Joshen] There's gotta be a much better way to mock these things so that it applies for ALL tests
-// Since these are easily commonly used things across all pages/components that we might be testing for
+dayjs.extend(utc)
 vi.mock('common', async (importOriginal) => {
   const actual = await importOriginal()
   return {
@@ -20,59 +15,68 @@ vi.mock('common', async (importOriginal) => {
     useIsLoggedIn: vi.fn(),
     isBrowser: false,
     LOCAL_STORAGE_KEYS: (actual as any).LOCAL_STORAGE_KEYS,
+    ...(actual as any),
   }
 })
+
 vi.mock('lib/gotrue', () => ({
   auth: { onAuthStateChange: vi.fn() },
 }))
 
-test.skip('Search will trigger a log refresh', async () => {
-  render(<LogsPreviewer projectRef="123" queryType="auth" />)
-
-  await userEvent.type(screen.getByPlaceholderText(/Search events/), 'something{enter}')
-
-  await waitFor(
-    () => {
-      // updates router query params
-      const router = useRouter()
-      expect(router.push).toHaveBeenCalledWith(
-        expect.objectContaining({
-          pathname: expect.any(String),
-          query: expect.objectContaining({
-            s: expect.stringContaining('something'),
-          }),
-        })
-      )
-    },
-    { timeout: 1500 }
+test('search loads with whatever is on the URL', async () => {
+  customRender(
+    <LogsPreviewer queryType="api" projectRef="default" tableName={LogsTableName.EDGE} />
   )
-  const table = await screen.findByRole('table')
-  await findByText(table, /some-message/, { selector: '*' }, { timeout: 1500 })
 })
 
-test.skip('poll count for new messages', async () => {
-  render(<LogsPreviewer queryType="api" projectRef="123" tableName={LogsTableName.EDGE} />)
-  await waitFor(() => screen.queryByText(/200/) === null)
-  // should display new logs count
-  await waitFor(() => screen.getByText(/999/))
+// sanity check to make sure msw is working
+test('logs tests load with msw data', async () => {
+  customRender(<LogsPreviewer queryType="api" projectRef="123" tableName={LogsTableName.EDGE} />)
 
-  // Refresh button only exists with the queryType param, which no longer shows the id column
-  await userEvent.click(screen.getByTitle('refresh'))
-  await waitFor(() => screen.queryByText(/999/) === null)
-  await screen.findByText(/200/)
+  // wait 3 seconds
+  await new Promise((resolve) => setTimeout(resolve, 3000))
+
+  expect(await screen.findByText('event message test')).toBeInTheDocument()
 })
 
-test.skip('stop polling for new count on error', async () => {
-  render(<LogsPreviewer queryType="api" projectRef="123" tableName={LogsTableName.EDGE} />)
-  await waitFor(() => screen.queryByText(/some-uuid123/) === null)
-  // should display error
-  await screen.findByText(/some logflare error/)
-  // should not load refresh counts if no data from main query
-  await expect(screen.findByText(/999/)).rejects.toThrowError()
+test.skip('can toggle log event chart', async () => {
+  customRender(<LogsPreviewer queryType="api" projectRef="123" tableName={LogsTableName.EDGE} />)
+
+  // wait for the logs to load first
+  await waitFor(() => {
+    expect(screen.getByTestId('logs-bar-chart')).toBeInTheDocument()
+  })
+
+  const toggle = screen.getByRole('button', { name: /Chart/i })
+  await userEvent.click(toggle)
+
+  await waitFor(() => {
+    expect(screen.getByTestId('logs-bar-chart')).not.toBeInTheDocument()
+  })
+
+  await userEvent.click(toggle)
+
+  await waitFor(() => {
+    expect(screen.getByTestId('logs-bar-chart')).toBeInTheDocument()
+  })
 })
 
-test.skip('log event chart', async () => {
-  render(<LogsPreviewer queryType="api" projectRef="123" tableName={LogsTableName.EDGE} />)
+test.skip('can write on the search bar', async () => {
+  customRender(<LogsPreviewer queryType="api" projectRef="123" tableName={LogsTableName.EDGE} />)
 
-  await waitFor(() => screen.queryByText(/some-uuid123/) === null)
+  const searchBar = screen.getByPlaceholderText(/Search/i)
+
+  await userEvent.type(searchBar, 'test')
+
+  expect(searchBar).toHaveValue('test')
+})
+
+test.skip('can click load more', async () => {
+  customRender(<LogsPreviewer queryType="api" projectRef="123" tableName={LogsTableName.EDGE} />)
+
+  const loadMore = screen.getByRole('button', { name: /Load more/i })
+
+  await userEvent.click(loadMore)
+
+  expect(loadMore).toHaveBeenCalled()
 })
