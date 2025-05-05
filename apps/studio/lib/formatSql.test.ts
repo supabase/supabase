01452c1e19fa0102a69b7b189c1c25aa29184ade
Jordi Enric <jordi.err@gmail.com>
2025-05-05 17:03:14 +0200

    add formatSql tests

---
+++ apps/studio/lib/formatSql.test.ts
@@ -0,0 +1,18 @@
+import { formatSql } from './formatSql'
+
+describe('formatSql', () => {
+  it('should format SQL', () => {
+    const result = formatSql('SELECT * FROM users')
+
+    expect(result).toBe(`select
+  *
+from
+  users`)
+  })
+
+  it('should return the original argument if it is not valid, not throw', () => {
+    const result = formatSql(123)
+
+    expect(result).toBe(123)
+  })
+})
