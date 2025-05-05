c3d3b842977dbbf960ec126b0b6253b9b5cedca6
Alaister Young <alaister@users.noreply.github.com>
2025-05-05 13:18:48 +0800

    chore: add usercentrics for consent management (#35384)
    * chore: add usercentrics for consent management
    
    * client component to make next.js happy
    
    * address feedback
    
    * move consent state to common
    
    * fix import
    
    * ensure page events are sent correctly
    
    * add feature flag provider to ui library site
    
    * fix ui lib 500 error
    
    * skip in test env
    
    ---------
    
    Co-authored-by: Joshen Lim <joshenlimek@gmail.com>
    Co-authored-by: Jordi Enric <jordi.err@gmail.com>

--- packages/ui-patterns/ConsentToast/index.tsx
+++ packages/ui-patterns/ConsentToast/index.tsx
@@ -17,7 +17,7 @@ export const ConsentToast = ({ onAccept = noop, onOptOut = noop }: ConsentToastP
     <div className="py-1 flex flex-col gap-y-3 w-full">
       <div>
         <p className="text-sm text-foreground">
-          We use first-party cookies to improve our services.{' '}
+          We use cookies to collect data and improve our services.{' '}
           <a
             target="_blank"
             rel="noreferrer noopener"
@@ -67,5 +67,3 @@ export const ConsentToast = ({ onAccept = noop, onOptOut = noop }: ConsentToastP
     </div>
   )
 }
-
-export { useConsent, useConsentValue } from '../shared/consent'
