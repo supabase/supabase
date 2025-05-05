a41966dfdd74be8d9233201db235bb8d07b05e88
Charis Lam <26616127+charislam@users.noreply.github.com>
2025-05-05 13:24:41 -0400

    chore(docs): add sentry monitoring

---
+++ apps/docs/sentry.server.config.ts
@@ -0,0 +1,12 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs'
+
+Sentry.init({
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+})
