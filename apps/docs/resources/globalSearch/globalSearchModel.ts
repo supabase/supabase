badcf17f70d1b4e156739bb03df4db0669678d57
Charis <26616127+charislam@users.noreply.github.com>
2025-05-06 13:11:29 -0400

    feat(content api): add client library api reference search results (#35484)
    * feat(content api): add client library api reference search results
    
    Allow searchDocs results to also return function references from the
    client library APIs
    
    * fix(content api): refine language enum handling

--- apps/docs/resources/globalSearch/globalSearchModel.ts
+++ apps/docs/resources/globalSearch/globalSearchModel.ts
@@ -2,8 +2,10 @@ import { type RootQueryTypeSearchDocsArgs } from '~/__generated__/graphql'
 import { convertPostgrestToApiError, type ApiErrorGeneric } from '~/app/api/utils'
 import { Result } from '~/features/helpers.fn'
 import { openAI } from '~/lib/openAi'
-import { supabase } from '~/lib/supabase'
+import { supabase, type DatabaseCorrected } from '~/lib/supabase'
 import { GuideModel } from '../guide/guideModel'
+import { ReferenceSDKFunctionModel, SDKLanguageValues } from '../reference/referenceSDKModel'
+import { SearchResultInterface } from './globalSearchInterface'
 
 export abstract class SearchResultModel {
   static async search(
@@ -22,26 +24,42 @@ export abstract class SearchResultModel {
           max_result: args.limit,
         })
       )
-        .map((matches) =>
-          matches
-            .map(({ type, page_title, href, content, subsections }) => {
-              switch (type) {
-                case 'markdown':
-                  return new GuideModel({
-                    title: page_title,
-                    href,
-                    content,
-                    subsections,
-                  })
-                default:
-                  return null
-              }
-            })
-            .filter(Boolean)
-        )
+        .map((matches) => matches.map(createModelFromMatch).filter(Boolean))
         .mapError(convertPostgrestToApiError)
 
       return matchResult
     })
+  }
+}
+
+function createModelFromMatch({
+  type,
+  page_title,
+  href,
+  content,
+  metadata,
+  subsections,
+}: DatabaseCorrected['public']['Functions']['search_content']['Returns'][number]): SearchResultInterface | null {
+  switch (type) {
+    case 'markdown':
+      return new GuideModel({
+        title: page_title,
+        href,
+        content,
+        subsections,
+      })
+    case 'reference':
+      const { language } = metadata
+      if (SDKLanguageValues.includes(language)) {
+        return new ReferenceSDKFunctionModel({
+          title: page_title,
+          href,
+          content,
+          language,
+          methodName: metadata.methodName,
+        })
+      }
+    default:
+      return null
   }
 }
