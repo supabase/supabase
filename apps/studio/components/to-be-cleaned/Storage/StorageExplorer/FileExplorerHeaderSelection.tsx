acd86da9e52446669104e7b2664ba7c2ab7a0f4c
Joshen Lim <joshenlimek@gmail.com>
2025-05-07 12:25:45 +0800

    Chore/refactor storage explorer store part 2 (#35382)
    * Shift every logic in storage mobx store to storage valtio store
    
    * Deprecate localStores folder
    
    * REMOVE MOBX ENTIRELYYYYYYY 😄
    
    * Update
    
    * Clean up
    
    * Small improvement for rename folder

--- apps/studio/components/to-be-cleaned/Storage/StorageExplorer/FileExplorerHeaderSelection.tsx
+++ apps/studio/components/to-be-cleaned/Storage/StorageExplorer/FileExplorerHeaderSelection.tsx
@@ -1,24 +1,24 @@
+import { PermissionAction } from '@supabase/shared-types/out/constants'
 import { Download, Move, Trash2, X } from 'lucide-react'
 
-import { PermissionAction } from '@supabase/shared-types/out/constants'
 import { useParams } from 'common'
 import { ButtonTooltip } from 'components/ui/ButtonTooltip'
 import { useCheckPermissions } from 'hooks/misc/useCheckPermissions'
-import { useStorageStore } from 'localStores/storageExplorer/StorageExplorerStore'
+import { useStorageExplorerStateSnapshot } from 'state/storage-explorer'
 import { Button } from 'ui'
 import { downloadFile } from './StorageExplorer.utils'
 
 const FileExplorerHeaderSelection = () => {
   const { ref: projectRef, bucketId } = useParams()
   const canUpdateFiles = useCheckPermissions(PermissionAction.STORAGE_WRITE, '*')
-  const storageExplorerStore = useStorageStore()
+
   const {
     selectedItems,
     downloadSelectedFiles,
     clearSelectedItems,
     setSelectedItemsToDelete,
     setSelectedItemsToMove,
-  } = storageExplorerStore
+  } = useStorageExplorerStateSnapshot()
 
   return (
     <div className="z-10 flex h-[40px] items-center rounded-t-md bg-brand-400 px-2 py-1 shadow [[data-theme*=dark]_&]:bg-brand-500">
