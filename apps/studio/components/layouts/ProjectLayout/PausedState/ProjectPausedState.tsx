9d36654fbeba9b13762715b5ed1d6b4e9a2b790a
Saxon Fletcher <saxonafletcher@gmail.com>
2025-05-13 23:14:07 +1000

    add advisor and stats to home (#35580)
    * add advisor and stats to home
    
    * public tables
    
    * table count
    
    * small fixes
    
    * loading state
    
    * Clean up + Add links to each advisor
    
    * Last few fixes
    
    * Tinnnnnnyy fix
    
    * Nits to make the layout shift less
    
    * Update LoadingState to minimize layout shift
    
    ---------
    
    Co-authored-by: Joshen Lim <joshenlimek@gmail.com>

--- apps/studio/components/layouts/ProjectLayout/PausedState/ProjectPausedState.tsx
+++ apps/studio/components/layouts/ProjectLayout/PausedState/ProjectPausedState.tsx
@@ -140,7 +140,7 @@ export const ProjectPausedState = ({ product }: ProjectPausedStateProps) => {
     <>
       <div className="space-y-4">
         <div className="w-full mx-auto mb-8 md:mb-16 max-w-7xl">
-          <div className="mx-6 flex md:h-[500px] items-center justify-center rounded border border-overlay bg-surface-100 p-4 md:p-8">
+          <div className="flex md:h-[500px] items-center justify-center rounded border border-overlay bg-surface-100 p-4 md:p-8">
             <div className="grid w-[550px] gap-4">
               <div className="mx-auto flex max-w-[300px] items-center justify-center space-x-4 lg:space-x-8">
                 <PauseCircle className="text-foreground-light" size={50} strokeWidth={1.5} />
