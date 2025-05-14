d4e87565f869a29917f61bb2c827010697bbeede
Jordi Enric <jordi.err@gmail.com>
2025-05-14 18:58:35 +0200

    fix sql-editor test

--- e2e/studio/features/sql-editor.spec.ts
+++ e2e/studio/features/sql-editor.spec.ts
@@ -5,15 +5,31 @@ import { getProjectRef } from '../env.config'
 
 test.describe('SQL Editor', () => {
   test('should check if SQL editor can run simple commands', async ({ page }) => {
-    await page.goto(toUrl(`/project/${getProjectRef()}/sql/1`))
+    await page.goto(toUrl(`/project/${getProjectRef()}/sql`))
 
-    // fill up sql
-    await page.getByText('select * from (select version').click()
-    await page.getByLabel('Editor content;Press Alt+F1').fill('select now();')
+    //
+    const editor = page.getByRole('code').nth(0)
+
+    // write some sql in the editor
+    await editor.click()
+    await page.keyboard.press('ControlOrMeta+KeyA')
+    await page.keyboard.type(`select 'hello world';`)
+
+    // run the query
     await page.getByRole('button', { name: 'Run', exact: true }).click()
 
-    // verify
-    await expect(page.getByRole('code')).toContainText('select now();')
-    await expect(page.getByRole('columnheader')).toContainText('now')
+    // Should say "Running..."
+    await expect(page.getByText('Running...')).toBeVisible()
+
+    // Wait until Running... is not visible
+    await expect(page.getByText('Running...')).not.toBeVisible()
+
+    // remove stuff from the editor
+    await page.keyboard.press('ControlOrMeta+KeyA')
+    await page.keyboard.press('Backspace')
+
+    // verify the result
+    const result = page.getByRole('gridcell', { name: 'hello world' })
+    await expect(result).toBeVisible()
   })
 })
