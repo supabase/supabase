9d9fa302648d4d6c41434143c96a6700bf988700
Jordi Enric <jordi.err@gmail.com>
2025-05-14 10:57:31 +0200

    fix urls in home.spec

--- e2e/studio/features/home.spec.ts
+++ e2e/studio/features/home.spec.ts
@@ -1,11 +1,11 @@
 import { expect } from '@playwright/test'
 import { test } from '../utils/test'
 import { ENV } from '../env.config'
-
+import { toUrl } from '../utils/to-url'
 test.describe('Project', async () => {
   test('Can navigate to project home page', async ({ page, ref }) => {
     console.log(page.url())
-    await page.goto(`/project/${ref}`)
+    await page.goto(toUrl(`/project/${ref}`))
 
     if (ENV.includes('selfhosted')) {
       await expect(page.getByRole('heading', { name: 'Welcome to your project' })).toBeVisible()
