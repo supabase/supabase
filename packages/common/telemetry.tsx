09e6e304b6f9ef143c7cae638482b2bb6b2817f4
Ivan Vasilov <vasilov.ivan@gmail.com>
2025-05-19 22:27:08 +0200

    Lets try to make studio work again.

--- packages/common/telemetry.tsx
+++ packages/common/telemetry.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { GoogleTagManager } from '@next/third-parties/google'
 import { components } from 'api-types'
 import { useRouter } from 'next/compat/router'
 import { usePathname } from 'next/navigation'
@@ -23,6 +24,8 @@ const { TELEMETRY_DATA } = LOCAL_STORAGE_KEYS
 // Reexports GoogleTagManager with the right API key set
 export const TelemetryTagManager = () => {
   const isGTMEnabled = Boolean(IS_PLATFORM && process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID)
+
+  console.log('isGTMEnabled', isGTMEnabled)
 
   if (!isGTMEnabled) {
     return
@@ -50,29 +53,7 @@ export const TelemetryTagManager = () => {
             `,
         }}
       />
-      <Script
-        id="google-tag-manager"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            // Google Tag Manager
-            (function(w, d, s, l, i) {
-              w[l] = w[l] || [];
-              w[l].push({
-                'gtm.start': new Date().getTime(),
-                event: 'gtm.js'
-              });
-              var f = d.getElementsByTagName(s)[0],
-                  j = d.createElement(s),
-                  dl = l != 'dataLayer' ? '&l=' + l : '';
-              j.async = true;
-              j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-              f.parentNode.insertBefore(j, f);
-            })(window, document, 'script', 'dataLayer', '${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}'); D
-            `,
-        }}
-      />
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID!} />
     </>
   )
 }
