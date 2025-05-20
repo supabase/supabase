6652b5ea6f15eeafd4f6b0e969c44fcb25308455
Michael Ong <minghao_3728@hotmail.com>
2025-05-20 17:51:08 +0800

    chore: minor fixes

--- e2e/studio/features/sql-editor.spec.ts
+++ e2e/studio/features/sql-editor.spec.ts
@@ -7,10 +7,10 @@ test.describe('SQL Editor', () => {
   test('should check if SQL editor can run simple commands', async ({ page }) => {
     await page.goto(toUrl(`/project/${getProjectRef()}/sql`))
 
-    //
     const editor = page.getByRole('code').nth(0)
 
     // write some sql in the editor
+    // This has to be done since the editor is not editable (input, textarea, etc.)
     await editor.click()
     await page.keyboard.press('ControlOrMeta+KeyA')
     await page.keyboard.type(`select 'hello world';`)
@@ -24,7 +24,7 @@ test.describe('SQL Editor', () => {
     // Wait until Running... is not visible
     await expect(page.getByText('Running...')).not.toBeVisible()
 
-    // remove stuff from the editor
+    // clear the editor
     await editor.click()
     await page.keyboard.press('ControlOrMeta+KeyA')
     await page.keyboard.press('Backspace')
