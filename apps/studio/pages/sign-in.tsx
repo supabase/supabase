3d33adedf1118ad616879ec7064fdb291a4af1bf
Alaister Young <a@alaisteryoung.com>
2025-05-20 18:35:40 +0800

    simplify

--- apps/studio/pages/sign-in.tsx
+++ apps/studio/pages/sign-in.tsx
@@ -13,7 +13,6 @@ import { Button } from 'ui'
 
 const SignInPage: NextPageWithLayout = () => {
   const router = useRouter()
-  const searchParams = new URLSearchParams(router.query as Record<string, string>).toString()
 
   useEffect(() => {
     if (!IS_PLATFORM) {
@@ -31,7 +30,7 @@ const SignInPage: NextPageWithLayout = () => {
             <Link
               href={{
                 pathname: '/sign-in-sso',
-                query: searchParams,
+                query: router.query,
               }}
             >
               Continue with SSO
@@ -57,7 +56,7 @@ const SignInPage: NextPageWithLayout = () => {
           <Link
             href={{
               pathname: '/sign-up',
-              query: searchParams,
+              query: router.query,
             }}
             className="underline transition text-foreground hover:text-foreground-light"
           >
