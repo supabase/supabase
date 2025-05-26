0629559cd2094a7f2841e378921859a3cb8f9457
Qiao Han <qiao@supabase.io>
2025-05-26 17:01:10 +0800

    chore: move transaction to outside pgmeta

--- apps/studio/data/sql/utils/migrations.ts
+++ apps/studio/data/sql/utils/migrations.ts
@@ -1,6 +1,7 @@
 import { makeRandomString } from 'lib/helpers'
+import { wrapWithTransaction } from './transaction'
 
-const initialiseHistoryTable = `begin;
+export const initialiseHistoryTable = `begin;
 
 create schema if not exists supabase_migrations;
 create table if not exists supabase_migrations.schema_migrations (version text not null primary key);
@@ -15,27 +16,32 @@ export const applyAndTrackMigrations = (query: string, name?: string, append?: b
   // Escapes literals using postgres dollar quoted string
   const dollar = `$${makeRandomString(20)}$`
   const quote = (s?: string) => (s ? dollar + s + dollar : `''`)
-  return `begin;
-
+  return wrapWithTransaction(`
 -- apply sql from post body
 ${query};
 
 -- track statements in history table
 ${trackMigrations(quote(query), quote(name), append)}
-
-commit;`
+`)
 }
 
 // All params are treated as valid sql expressions
 export const trackMigrations = (query: string, name?: string, append?: boolean) => {
   return `
-insert into supabase_migrations.schema_migrations as old (version, name, statements)
-select case
-  when ${append ?? 'count(version) = 1'} then max(version)
-  else to_char(current_timestamp, 'YYYYMMDDHHMISS')
-end, ${name}, array[${query}] from supabase_migrations.schema_migrations
-on conflict (version) do update set
-  statements = old.statements || EXCLUDED.statements,
-  name = coalesce(EXCLUDED.name, old.name);
-`
+do $$
+begin
+  if exists (
+    select from pg_tables where schemaname = 'supabase_migrations' and tablename = 'schema_migrations'
+  ) then
+    insert into supabase_migrations.schema_migrations as old (version, name, statements)
+    select case
+      when ${append ?? 'count(version) = 1'} then max(version)
+      else to_char(current_timestamp, 'YYYYMMDDHHMISS')
+    end, ${name}, array[${query}] from supabase_migrations.schema_migrations
+    on conflict (version) do update set
+      statements = old.statements || EXCLUDED.statements,
+      name = coalesce(EXCLUDED.name, old.name);
+  end if;
+end
+$$;`
 }
