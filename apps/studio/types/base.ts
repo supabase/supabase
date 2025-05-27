a7405f01e6f69d75a6ab6183caa018ff82ce8266
Alaister Young <a@alaisteryoung.com>
2025-05-27 16:32:48 +0800

    chore: respect retry-after header for rate-limiting

--- apps/studio/types/base.ts
+++ apps/studio/types/base.ts
@@ -99,11 +99,13 @@ export type SupaResponse<T> = T | ResponseFailure
 export class ResponseError extends Error {
   code?: number
   requestId?: string
+  retryAfter?: number
 
-  constructor(message: string | undefined, code?: number, requestId?: string) {
+  constructor(message: string | undefined, code?: number, requestId?: string, retryAfter?: number) {
     super(message || 'API error happened while trying to communicate with the server.')
     this.code = code
     this.requestId = requestId
+    this.retryAfter = retryAfter
   }
 }
 
