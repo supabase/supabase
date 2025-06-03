6189a037c0ded3dab90c20a6addc020008328ade
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2025-06-03 18:16:53 +0200

    update plugins import

--- apps/cms/src/payload.config.ts
+++ apps/cms/src/payload.config.ts
@@ -3,7 +3,7 @@ import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
-import { plugins } from './plugins'
+import { plugins } from './plugins/index'
 import { defaultLexical } from '@/fields/defaultLexical'
 import { getServerSideURL } from './utilities/getURL'
 
