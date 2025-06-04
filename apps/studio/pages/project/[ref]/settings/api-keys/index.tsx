9b4d3877de2ca2738f245236c4cee8026a3be834
Joshen Lim <joshenlimek@gmail.com>
2025-06-04 12:51:57 +0800

    Chore/api keys cleanup (#36097)
    * Remove api-keys.tsx, duplicated with api-keys/index.ts
    
    * Remove use of newApiKeys feature flag in SettingsMenu.utils.tsx
    
    * Refactor use of newApiKeys flag to basicApiKeys
    
    * Update logic for shouldDisableUI in useApiKeysVisibility hook
    
    * Smol fix

--- apps/studio/pages/project/[ref]/settings/api-keys/index.tsx
+++ apps/studio/pages/project/[ref]/settings/api-keys/index.tsx
@@ -1,11 +1,11 @@
-import { LegacyAPIKeys } from 'components/interfaces/APIKeys/LegacyAPIKeys'
 import ApiKeysLayout from 'components/layouts/APIKeys/APIKeysLayout'
 import DefaultLayout from 'components/layouts/DefaultLayout'
 import SettingsLayout from 'components/layouts/ProjectSettingsLayout/SettingsLayout'
+import { DisplayApiSettings } from 'components/ui/ProjectSettings'
 import type { NextPageWithLayout } from 'types'
 
 const ApiKeysLegacyPage: NextPageWithLayout = () => {
-  return <LegacyAPIKeys />
+  return <DisplayApiSettings legacy showNotice={false} />
 }
 
 ApiKeysLegacyPage.getLayout = (page) => (
