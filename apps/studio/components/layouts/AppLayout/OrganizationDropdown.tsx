1adcb11341bfc6b084d1c58895ec32dbb2578d7f
Terry Sutton <saltcod@gmail.com>
2025-06-13 13:40:26 -0230

    Link to all orgs if slug is undefined

--- apps/studio/components/layouts/AppLayout/OrganizationDropdown.tsx
+++ apps/studio/components/layouts/AppLayout/OrganizationDropdown.tsx
@@ -45,7 +45,10 @@ export const OrganizationDropdown = () => {
 
   return (
     <>
-      <Link href={`/org/${slug}`} className="flex items-center gap-2 flex-shrink-0 text-sm">
+      <Link
+        href={slug ? `/org/${slug}` : '/organizations'}
+        className="flex items-center gap-2 flex-shrink-0 text-sm"
+      >
         <Boxes size={14} strokeWidth={1.5} className="text-foreground-lighter" />
         <span
           className={cn(
