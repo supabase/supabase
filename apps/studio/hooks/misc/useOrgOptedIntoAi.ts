d6aa546f3edf0f8465171b1c675b1924ea3f811f
Joshen Lim <joshenlimek@gmail.com>
2025-06-16 21:55:54 +0800

    Fix incorrect logic for isHipaaProjectDisallowed (#36436)
    * Fix incorrect logic for isHipaaProjectDisallowed
    
    * Remove console log

--- apps/studio/hooks/misc/useOrgOptedIntoAi.ts
+++ apps/studio/hooks/misc/useOrgOptedIntoAi.ts
@@ -1,6 +1,9 @@
+import { subscriptionHasHipaaAddon } from 'components/interfaces/Billing/Subscription/Subscription.utils'
+import { useProjectSettingsV2Query } from 'data/config/project-settings-v2-query'
+import { useOrgSubscriptionQuery } from 'data/subscriptions/org-subscription-query'
 import { useDisallowHipaa } from 'hooks/misc/useDisallowHipaa'
-import { useSelectedProject } from 'hooks/misc/useSelectedProject'
 import { useSelectedOrganization } from 'hooks/misc/useSelectedOrganization'
+import { useSelectedProject } from 'hooks/misc/useSelectedProject'
 import { OPT_IN_TAGS } from 'lib/constants'
 
 /**
@@ -26,10 +29,18 @@ export function useOrgOptedIntoAi() {
 }
 
 export function useOrgOptedIntoAiAndHippaProject() {
+  const selectedProject = useSelectedProject()
   const selectedOrganization = useSelectedOrganization()
   const optInTags = selectedOrganization?.opt_in_tags
   const isOptedIntoAI = optInTags?.includes(OPT_IN_TAGS.AI_SQL) ?? false
 
-  const disallowHipaa = useDisallowHipaa()
-  return { isOptedInToAI: isOptedIntoAI, isHipaaProjectDisallowed: disallowHipaa(isOptedIntoAI) }
+  const { data: subscription } = useOrgSubscriptionQuery({ orgSlug: selectedOrganization?.slug })
+  const hasHipaaAddon = subscriptionHasHipaaAddon(subscription)
+
+  const { data: projectSettings } = useProjectSettingsV2Query({ projectRef: selectedProject?.ref })
+  const isProjectSensitive = !!projectSettings?.is_sensitive
+
+  const preventProjectFromUsingAI = hasHipaaAddon && isProjectSensitive
+
+  return { isOptedInToAI: isOptedIntoAI, isHipaaProjectDisallowed: preventProjectFromUsingAI }
 }
