46ff905ec0eba996af651911a40f25a429a66c6e
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2025-06-19 18:06:44 +0200

    add storage cms bucket migration

--- apps/cms/src/migrations/index.ts
+++ apps/cms/src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250619_155345 from './20250619_155345';
+import * as migration_20250619_160549 from './20250619_160549';
 
 export const migrations = [
   {
     up: migration_20250619_155345.up,
     down: migration_20250619_155345.down,
-    name: '20250619_155345'
+    name: '20250619_155345',
+  },
+  {
+    up: migration_20250619_160549.up,
+    down: migration_20250619_160549.down,
+    name: '20250619_160549'
   },
 ];
