2638f8da70095609481c68fd21d722e6edb8cf7a
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2025-06-19 14:56:31 +0800

    add flag check and redirect if flag is false

--- apps/studio/pages/project/[ref]/unified-logs.tsx
+++ apps/studio/pages/project/[ref]/unified-logs.tsx
@@ -1,9 +1,24 @@
+import { useRouter } from 'next/router'
+
 import { UnifiedLogs } from 'components/interfaces/UnifiedLogs/UnifiedLogs'
 import DefaultLayout from 'components/layouts/DefaultLayout'
 import UnifiedLogsLayout from 'components/layouts/UnifiedLogsLayout/UnifiedLogsLayout'
+import { useProjectByRef } from 'hooks/misc/useSelectedProject'
+import { useFlag } from 'hooks/ui/useFlag'
 import type { NextPageWithLayout } from 'types'
 
-export const LogPage: NextPageWithLayout = () => <UnifiedLogs />
+export const LogPage: NextPageWithLayout = () => {
+  const router = useRouter()
+  const unifiedLogsEnabled = useFlag('unifiedLogs')
+  const { ref } = router.query
+
+  // Redirect if flag is disabled
+  if (unifiedLogsEnabled === false) {
+    router.push(`/project/${ref}/logs`)
+  }
+
+  return <UnifiedLogs />
+}
 
 LogPage.getLayout = (page) => (
   <DefaultLayout>
