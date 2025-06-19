be4dbfacc63c5469e914a08bd579e0cf68040001
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2025-06-19 14:14:30 +0200

    try public schema

--- apps/cms/src/payload.config.ts
+++ apps/cms/src/payload.config.ts
@@ -59,7 +59,7 @@ export default buildConfig({
     pool: {
       connectionString: process.env.DATABASE_URI || '',
     },
-    schemaName: 'cms-payload',
+    // schemaName: 'cms-payload',
   }),
   sharp,
   plugins,
