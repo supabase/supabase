856d4cc5691fff956abec3bee14d70790bd641a0
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2025-06-19 17:54:26 +0200

    upgrade migrations fr

--- apps/cms/src/migrations/index.ts
+++ apps/cms/src/migrations/index.ts
@@ -1,15 +1,9 @@
-import * as migration_20250613_100000 from './20250613_100000';
-import * as migration_20250613_134829 from './20250613_134829';
+import * as migration_20250619_155345 from './20250619_155345';
 
 export const migrations = [
   {
-    up: migration_20250613_100000.up,
-    down: migration_20250613_100000.down,
-    name: '20250613_100000'
-  },
-  {
-    up: migration_20250613_134829.up,
-    down: migration_20250613_134829.down,
-    name: '20250613_134829',
+    up: migration_20250619_155345.up,
+    down: migration_20250619_155345.down,
+    name: '20250619_155345'
   },
 ];
