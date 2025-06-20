f223b455d68fe913f4075fe1c6026fd5205f971c
Joshen Lim <joshenlimek@gmail.com>
2025-06-20 12:46:17 +0800

    Unified Logs (Part 1) (#36298)
    * init new unified page
    
    * moar logs
    
    * init
    
    * add infinite and live logs example
    
    * Update useLogsPreview.tsx
    
    * add more sources
    
    * wrapped auth logs with edge logs
    
    * add role and user id
    
    * move unified logs
    
    * init
    
    * move demo pages. create a new directory to work in
    
    * extracted beta unified logs into own components
    
    * add example base page and components
    
    * add new files to use actual logging query
    
    * more organization
    
    * change import
    
    * adds new logs page. adds new query
    
    * add data table to UI pacakges
    
    * revert
    
    * table styles
    
    * text size
    
    * add timestamp, table, icons for log types, status code styling
    
    * add host
    
    * add log count to edge functions
    
    * starts to add dynamic filtering
    
    * spiking trace UI
    
    * Update status-code.ts
    
    * add new linik
    
    * now using POST
    
    * fix chart data for default 1 hour view
    
    * update API to accept POST requests
    
    * new filters
    
    * Update level.ts
    
    * fixed up chart to work on level filter. split up the logic into new files
    
    * prep for log type
    
    * prepped query for WHERE
    
    * fix: issue with white space in url param column parsing
    
    * level param now being removed correctly.
    
    * fix issue with chart showing wrong buckets for different time ranges
    
    * remove old query
    
    * refactor the queries into function for each source
    
    * total count fixed
    
    * lots of layout
    
    * start fixing log counts
    
    * comment out min and max for a while
    
    * added trace logging prototype in
    
    * random trace logs added for demo
    
    * added logs and ui to view logs if any
    
    * add Auth user
    
    * fix the live logs issue
    
    * some left over code
    
    * Midway
    
    * First pass refactor + clean up + reorganize files
    
    * Fix TS issues
    
    * Remove unused files
    
    * Clean up
    
    * Final clean up
    
    * more clean up
    
    * More clean up
    
    * Remove unused packages
    
    * Fix
    
    * Lint
    
    * Add feature flag for unified logs
    
    * Refactor
    
    * Remove trace UI
    
    * Snake case log types
    
    * more clean up
    
    * More clean up
    
    * Fix ts
    
    * more clean up
    
    * fixes
    
    * add flag check and redirect if flag is false
    
    * Update middleware.ts
    
    * Nit lint
    
    * Fix
    
    * Last refactors
    
    ---------
    
    Co-authored-by: Jonathan Summers-Muir <MildTomato@users.noreply.github.com>

--- apps/studio/components/interfaces/Settings/Logs/Logs.utils.ts
+++ apps/studio/components/interfaces/Settings/Logs/Logs.utils.ts
@@ -4,12 +4,12 @@ import { get, isEqual } from 'lodash'
 import uniqBy from 'lodash/uniqBy'
 import { useEffect } from 'react'
 
+import { IS_PLATFORM } from 'common'
 import BackwardIterator from 'components/ui/CodeEditor/Providers/BackwardIterator'
 import type { PlanId } from 'data/subscriptions/types'
 import logConstants from 'shared-data/logConstants'
 import { LogsTableName, SQL_FILTER_TEMPLATES } from './Logs.constants'
 import type { Filters, LogData, LogsEndpointParams } from './Logs.types'
-import { IS_PLATFORM } from 'common'
 
 /**
  * Convert a micro timestamp from number/string to iso timestamp
