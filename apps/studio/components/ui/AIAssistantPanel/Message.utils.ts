b5455ec571429696f9290b4ebf8ec530354d5adc
Joshen Lim <joshenlimek@gmail.com>
2025-06-23 13:52:50 +0800

    Add custom link component to assistant message block (#36527)
    * Add custom link component to assistant message block
    
    * Update based on feedback
    
    * Render plain text if URL is deemed unsafe

--- apps/studio/components/ui/AIAssistantPanel/Message.utils.ts
+++ apps/studio/components/ui/AIAssistantPanel/Message.utils.ts
@@ -34,3 +34,27 @@ export const findResultForManualId = (
   }
   return undefined
 }
+
+// [Joshen] From https://github.com/remarkjs/react-markdown/blob/fda7fa560bec901a6103e195f9b1979dab543b17/lib/index.js#L425
+export function defaultUrlTransform(value: string) {
+  const safeProtocol = /^(https?|ircs?|mailto|xmpp)$/i
+  const colon = value.indexOf(':')
+  const questionMark = value.indexOf('?')
+  const numberSign = value.indexOf('#')
+  const slash = value.indexOf('/')
+
+  if (
+    // If there is no protocol, it’s relative.
+    colon === -1 ||
+    // If the first colon is after a `?`, `#`, or `/`, it’s not a protocol.
+    (slash !== -1 && colon > slash) ||
+    (questionMark !== -1 && colon > questionMark) ||
+    (numberSign !== -1 && colon > numberSign) ||
+    // It is a protocol, it should be allowed.
+    safeProtocol.test(value.slice(0, colon))
+  ) {
+    return value
+  }
+
+  return ''
+}
