0c4010bb238c8e8c838c86cd1b533fffc07dac45
angelico <Angelico.delosreyes@gmail.com>
2025-06-26 12:14:26 +0800

    chore: Dashboard guards to limit what can and cannot be done by an AWS_K8S project (#36630)
    * chore: transition from AWS_NEW to AWS_K8S
    
    * chore: transition from useIsAwsNew to useIsAwsK8s
    
    * chore: new useIsAws method
    
    * chore: disk management protections
    
    * chore: IPV4 addon protections
    
    * chore: AWS_K8S restart protection
    
    * chore: AWS_K8S read replica protections
    
    * chore: restore to new project protections
    
    * Update apps/studio/components/interfaces/Settings/Infrastructure/InfrastructureConfiguration/DeployNewReplicaPanel.tsx
    
    Co-authored-by: Tom Gallacher <tgallacher@users.noreply.github.com>
    
    * chore: changes based on feedback
    
    * fix: prettier
    
    * minor fixes
    
    * Use disabled state for ipv4 address in ipv4sidepanel isntead of hiding the UI entirely
    
    * Revert hardcode in useSelectedProject
    
    ---------
    
    Co-authored-by: Tom Gallacher <tgallacher@users.noreply.github.com>
    Co-authored-by: Joshen Lim <joshenlimek@gmail.com>

--- apps/studio/components/interfaces/Settings/Addons/Addons.tsx
+++ apps/studio/components/interfaces/Settings/Addons/Addons.tsx
@@ -32,7 +32,7 @@ import { useOrgSubscriptionQuery } from 'data/subscriptions/org-subscription-que
 import { useProjectAddonsQuery } from 'data/subscriptions/project-addons-query'
 import type { ProjectAddonVariantMeta } from 'data/subscriptions/types'
 import { useSelectedOrganization } from 'hooks/misc/useSelectedOrganization'
-import { useIsOrioleDb, useIsOrioleDbInAws, useProjectByRef } from 'hooks/misc/useSelectedProject'
+import { useIsOrioleDbInAws, useProjectByRef } from 'hooks/misc/useSelectedProject'
 import { useFlag } from 'hooks/ui/useFlag'
 import { getCloudProviderArchitecture } from 'lib/cloudprovider-utils'
 import { BASE_PATH, INSTANCE_MICRO_SPECS, INSTANCE_NANO_SPECS } from 'lib/constants'
@@ -53,7 +53,6 @@ const Addons = () => {
   const parentProject = useProjectByRef(selectedProject?.parent_project_ref)
   const isBranch = parentProject !== undefined
   const isProjectActive = useIsProjectActive()
-  const isOrioleDb = useIsOrioleDb()
   const isOrioleDbInAws = useIsOrioleDbInAws()
 
   const { data: settings } = useProjectSettingsV2Query({ projectRef })
