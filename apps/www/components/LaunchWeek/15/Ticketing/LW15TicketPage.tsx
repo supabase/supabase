ceabe1b79ab4fa1a56a4b281b4cd367e241a1891
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2025-07-02 19:36:31 +0200

    lw15 ticket page init

---
+++ apps/www/components/LaunchWeek/15/Ticketing/LW15TicketPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SectionContainer from 'components/Layouts/SectionContainer'
+
+const LW15TicketPage = () => {
+  return (
+    <SectionContainer className="flex flex-col justify-between gap-12 !py-10 h-full">
+      Ticket Page TBD
+    </SectionContainer>
+  )
+}
+
+export default LW15TicketPage
---
+++ apps/www/pages/launch-week/ticket.tsx
@@ -0,0 +1,50 @@
+import { NextSeo } from 'next-seo'
+import { LW15_DATE, LW15_TITLE, LW15_URL, SITE_ORIGIN } from 'lib/constants'
+import { useRouter } from 'next/router'
+
+import { Lw15ConfDataProvider } from 'components/LaunchWeek/15/hooks/use-conf-data'
+import DefaultLayout from 'components/Layouts/Default'
+import LW15TicketPage from 'components/LaunchWeek/15/Ticketing/LW15TicketPage'
+
+const Lw15Page = () => {
+  const TITLE = `${LW15_TITLE} | ${LW15_DATE}`
+  const DESCRIPTION = 'Join us for a week of announcing new features, every day at 7 AM PT.'
+  const OG_IMAGE = `${SITE_ORIGIN}/images/launchweek/15/lw15-og.png?lw=15`
+
+  const { query } = useRouter()
+  const ticketNumber = query.ticketNumber?.toString()
+  const defaultUserData = {
+    id: query.id?.toString(),
+    ticket_number: ticketNumber ? parseInt(ticketNumber, 10) : undefined,
+    name: query.name?.toString(),
+    username: query.username?.toString(),
+    platinum: !!query.platinum,
+  }
+
+  return (
+    <>
+      <NextSeo
+        title={TITLE}
+        description={DESCRIPTION}
+        openGraph={{
+          title: TITLE,
+          description: DESCRIPTION,
+          url: LW15_URL,
+          images: [
+            {
+              url: OG_IMAGE,
+            },
+          ],
+        }}
+      />
+
+      <Lw15ConfDataProvider initState={{ userTicketData: defaultUserData, partymodeStatus: 'on' }}>
+        <DefaultLayout className="!min-h-[calc(100dvh-66px)] h-[calc(100dvh-66px)]">
+          <LW15TicketPage />
+        </DefaultLayout>
+      </Lw15ConfDataProvider>
+    </>
+  )
+}
+
+export default Lw15Page
