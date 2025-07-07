ab19e3e6169bde204bf4f28b1abe6dea0d07046f
Michael Ong <minghao_3728@hotmail.com>
2025-07-07 16:06:50 +0800

    chore: add final e2e tests for table-editor and sql-editor

--- e2e/studio/features/sql-editor.spec.ts
+++ e2e/studio/features/sql-editor.spec.ts
@@ -16,7 +16,7 @@ test.describe('SQL Editor', () => {
     await page.keyboard.press('ControlOrMeta+KeyA')
     await page.keyboard.type(`select 'hello world';`)
 
-    await page.getByRole('button', { name: /^Run( CTRL)?$/, exact: false }).click()
+    await page.getByTestId('sql-run-button').click()
 
     // Should say "Running..."
     await expect(page.getByText('Running...')).toBeVisible()
@@ -32,70 +32,102 @@ test.describe('SQL Editor', () => {
     // verify the result
     const result = page.getByRole('gridcell', { name: 'hello world' })
     await expect(result).toBeVisible()
+
+    // SQL written in the editor should not be the previous query.
+    await page.waitForTimeout(100)
+    await editor.click()
+    await page.keyboard.press('ControlOrMeta+KeyA')
+    await page.keyboard.type(`select length('hello');`)
+    await page.getByTestId('sql-run-button').click()
+
+    await expect(page.getByText('Running...')).toBeVisible()
+
+    await expect(page.getByText('Running...')).not.toBeVisible()
+
+    // verify the result is updated.
+    await expect(result).not.toBeVisible()
+    const result2 = page.getByRole('gridcell', { name: '5' })
+    await expect(result2).toBeVisible()
   })
 })
 
-test.describe('SQL Snippets', () => {
-  test('should create and load a new snippet', async ({ page }) => {
-    await page.goto(toUrl(`/project/${env.PROJECT_REF}/sql`))
+test('destructive query would tripper a warning modal', async ({ page }) => {
+  await page.goto(toUrl(`/project/${env.PROJECT_REF}/sql/new?skip=true`))
 
-    const addButton = page.getByTestId('sql-editor-new-query-button')
-    const runButton = page.getByTestId('sql-run-button')
-    await page.getByRole('button', { name: 'Favorites' }).click()
-    await page.getByRole('button', { name: 'Shared' }).click()
-    await expect(page.getByText('No shared queries')).toBeVisible()
-    await expect(page.getByText('No favorite queries')).toBeVisible()
+  const editor = page.getByRole('code').nth(0)
 
-    // write some sql in the editor
-    await addButton.click()
-    await page.getByRole('menuitem', { name: 'Create a new snippet' }).click()
+  // write some sql in the editor
+  // This has to be done since the editor is not editable (input, textarea, etc.)
+  await editor.click()
+  await editor.click()
+  await page.keyboard.press('ControlOrMeta+KeyA')
+  await page.keyboard.type(`delete table 'test';`)
+  await page.getByTestId('sql-run-button').click()
 
-    const editor = page.getByRole('code').nth(0)
-    await page.waitForTimeout(1000)
-    await editor.click()
-    await page.keyboard.type(`select 'hello world';`)
-    await expect(page.getByText("select 'hello world';")).toBeVisible()
-    await runButton.click()
+  // verify warning modal is visible
+  expect(page.getByRole('heading', { name: 'Potential issue detected with' })).toBeVisible()
+  expect(page.getByText('Query has destructive')).toBeVisible()
+})
 
-    // snippet exists
-    const privateSnippet = page.getByLabel('private-snippets')
-    await expect(privateSnippet).toContainText('Untitled query')
+test('should create and load a new snippet', async ({ page }) => {
+  await page.goto(toUrl(`/project/${env.PROJECT_REF}/sql`))
 
-    // favourite snippets
-    await page.getByTestId('sql-editor-utility-actions').click()
-    await page.getByRole('menuitem', { name: 'Add to favorites', exact: true }).click()
-    const favouriteSnippetsSection = page.getByLabel('favorite-snippets')
-    await expect(favouriteSnippetsSection).toContainText('Untitled query')
+  const addButton = page.getByTestId('sql-editor-new-query-button')
+  const runButton = page.getByTestId('sql-run-button')
+  await page.getByRole('button', { name: 'Favorites' }).click()
+  await page.getByRole('button', { name: 'Shared' }).click()
+  await expect(page.getByText('No shared queries')).toBeVisible()
+  await expect(page.getByText('No favorite queries')).toBeVisible()
 
-    // unfavorite snippets
-    await page.waitForTimeout(500)
-    await page.getByTestId('sql-editor-utility-actions').click()
-    await page.getByRole('menuitem', { name: 'Remove from favorites' }).click()
-    await expect(favouriteSnippetsSection).not.toBeVisible()
+  // write some sql in the editor
+  await addButton.click()
+  await page.getByRole('menuitem', { name: 'Create a new snippet' }).click()
 
-    // rename snippet
-    await privateSnippet.getByText('Untitled query').click({ button: 'right' })
-    await page.getByRole('menuitem', { name: 'Rename query', exact: true }).click()
-    await expect(page.getByRole('heading', { name: 'Rename' })).toBeVisible()
-    await page.getByRole('textbox', { name: 'Name' }).fill('test snippet')
-    await page.getByRole('button', { name: 'Rename query', exact: true }).click()
+  const editor = page.getByRole('code').nth(0)
+  await page.waitForTimeout(1000)
+  await editor.click()
+  await page.keyboard.type(`select 'hello world';`)
+  await expect(page.getByText("select 'hello world';")).toBeVisible()
+  await runButton.click()
 
-    const privateSnippet2 = privateSnippet.getByText('test snippet', { exact: true })
-    await expect(privateSnippet2).toBeVisible()
+  // snippet exists
+  const privateSnippet = page.getByLabel('private-snippets')
+  await expect(privateSnippet).toContainText('Untitled query')
 
-    // share with a team
-    await privateSnippet2.click({ button: 'right' })
-    await page.getByRole('menuitem', { name: 'Share query with team' }).click()
-    await expect(page.getByRole('heading', { name: 'Confirm to share query: test' })).toBeVisible()
-    await page.getByRole('button', { name: 'Share query', exact: true }).click()
-    const sharedSnippet = await page.getByLabel('project-level-snippets')
-    await expect(sharedSnippet).toContainText('test snippet')
+  // favourite snippets
+  await page.getByTestId('sql-editor-utility-actions').click()
+  await page.getByRole('menuitem', { name: 'Add to favorites', exact: true }).click()
+  const favouriteSnippetsSection = page.getByLabel('favorite-snippets')
+  await expect(favouriteSnippetsSection).toContainText('Untitled query')
 
-    // unshare a snippet
-    await sharedSnippet.getByText('test snippet').click({ button: 'right' })
-    await page.getByRole('menuitem', { name: 'Unshare query with team' }).click()
-    await expect(page.getByRole('heading', { name: 'Confirm to unshare query:' })).toBeVisible()
-    await page.getByRole('button', { name: 'Unshare query', exact: true }).click()
-    await expect(sharedSnippet).not.toBeVisible()
-  })
+  // unfavorite snippets
+  await page.waitForTimeout(500)
+  await page.getByTestId('sql-editor-utility-actions').click()
+  await page.getByRole('menuitem', { name: 'Remove from favorites' }).click()
+  await expect(favouriteSnippetsSection).not.toBeVisible()
+
+  // rename snippet
+  await privateSnippet.getByText('Untitled query').click({ button: 'right' })
+  await page.getByRole('menuitem', { name: 'Rename query', exact: true }).click()
+  await expect(page.getByRole('heading', { name: 'Rename' })).toBeVisible()
+  await page.getByRole('textbox', { name: 'Name' }).fill('test snippet')
+  await page.getByRole('button', { name: 'Rename query', exact: true }).click()
+
+  const privateSnippet2 = privateSnippet.getByText('test snippet', { exact: true })
+  await expect(privateSnippet2).toBeVisible()
+
+  // share with a team
+  await privateSnippet2.click({ button: 'right' })
+  await page.getByRole('menuitem', { name: 'Share query with team' }).click()
+  await expect(page.getByRole('heading', { name: 'Confirm to share query: test' })).toBeVisible()
+  await page.getByRole('button', { name: 'Share query', exact: true }).click()
+  const sharedSnippet = await page.getByLabel('project-level-snippets')
+  await expect(sharedSnippet).toContainText('test snippet')
+
+  // unshare a snippet
+  await sharedSnippet.getByText('test snippet').click({ button: 'right' })
+  await page.getByRole('menuitem', { name: 'Unshare query with team' }).click()
+  await expect(page.getByRole('heading', { name: 'Confirm to unshare query:' })).toBeVisible()
+  await page.getByRole('button', { name: 'Unshare query', exact: true }).click()
+  await expect(sharedSnippet).not.toBeVisible()
 })
