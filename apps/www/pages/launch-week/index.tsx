2afa8ba4f6452ed58037dfac5785ac7f2d87c61c
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2025-07-08 17:43:04 +0200

    lw15 ticketing (#36916)
    lw15 ticketing site

--- apps/www/pages/launch-week/index.tsx
+++ apps/www/pages/launch-week/index.tsx
@@ -1,17 +1,15 @@
 import { NextSeo } from 'next-seo'
-import { LW14_DATE, LW14_TITLE, LW14_URL, SITE_ORIGIN } from '~/lib/constants'
+import { LW15_DATE, LW15_TITLE, LW15_URL, SITE_ORIGIN } from 'lib/constants'
 import { useRouter } from 'next/router'
-import { Lw14ConfDataProvider } from '~/components/LaunchWeek/14/hooks/use-conf-data'
-import DefaultLayout from '~/components/Layouts/Default'
-import LWStickyNav from '~/components/LaunchWeek/14/Releases/LWStickyNav'
-import LWHeader from '~/components/LaunchWeek/14/Releases/LWHeader'
-import MainStage from '~/components/LaunchWeek/14/Releases/MainStage'
-import BuildStage from '~/components/LaunchWeek/14/Releases/BuildStage'
 
-const Lw14Page = () => {
-  const TITLE = `${LW14_TITLE} | ${LW14_DATE}`
-  const DESCRIPTION = 'Join us for a week of announcing new features, every day at 7 AM PT.'
-  const OG_IMAGE = `${SITE_ORIGIN}/images/launchweek/14/lw14-og.png?lw=14`
+import { Lw15ConfDataProvider } from 'components/LaunchWeek/15/hooks/use-conf-data'
+import DefaultLayout from 'components/Layouts/Default'
+import LW15LandingPage from 'components/LaunchWeek/15/Ticketing/LW15LandingPage'
+
+const Lw15Page = () => {
+  const TITLE = `${LW15_TITLE} | ${LW15_DATE}`
+  const DESCRIPTION = 'Join us for a week of announcing new features, every day at 8AM PT.'
+  const OG_IMAGE = `${SITE_ORIGIN}/images/launchweek/15/lw15-og.png?lw=15`
 
   const { query } = useRouter()
   const ticketNumber = query.ticketNumber?.toString()
@@ -31,7 +29,7 @@ const Lw14Page = () => {
         openGraph={{
           title: TITLE,
           description: DESCRIPTION,
-          url: LW14_URL,
+          url: LW15_URL,
           images: [
             {
               url: OG_IMAGE,
@@ -40,22 +38,21 @@ const Lw14Page = () => {
         }}
       />
 
-      <Lw14ConfDataProvider initState={{ userTicketData: defaultUserData, partymodeStatus: 'on' }}>
-        <DefaultLayout>
+      <Lw15ConfDataProvider initState={{ userTicketData: defaultUserData }}>
+        <DefaultLayout className="!min-h-[calc(100dvh-66px)] h-[calc(100dvh-66px)] dark:bg-black">
           <div
             style={{
-              fontFamily: 'Departure Mono, Source Code Pro, Office Code Pro, Menlo, monospace',
+              fontFamily:
+                "SuisseIntl-Book, custom-font, -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
             }}
+            className="h-full"
           >
-            <LWStickyNav />
-            <LWHeader />
-            <MainStage className="relative z-10" />
-            <BuildStage />
+            <LW15LandingPage />
           </div>
         </DefaultLayout>
-      </Lw14ConfDataProvider>
+      </Lw15ConfDataProvider>
     </>
   )
 }
 
-export default Lw14Page
+export default Lw15Page
