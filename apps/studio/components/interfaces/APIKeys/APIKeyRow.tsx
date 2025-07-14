59224cbdacdcb3fb715896acc120dc0cd755dad0
Joshen Lim <joshenlimek@gmail.com>
2025-07-14 16:34:10 +0800

    Remove unused file

--- apps/studio/components/interfaces/APIKeys/APIKeyRow.tsx
+++ apps/studio/components/interfaces/APIKeys/APIKeyRow.tsx
@@ -1,5 +1,6 @@
-import { MoreVertical } from 'lucide-react'
+import { APIKeysData } from 'data/api-keys/api-keys-query'
 import { motion } from 'framer-motion'
+import { MoreVertical } from 'lucide-react'
 import {
   Button,
   DropdownMenu,
@@ -10,7 +11,6 @@ import {
 } from 'ui'
 import { APIKeyDeleteDialog } from './APIKeyDeleteDialog'
 import { ApiKeyPill } from './ApiKeyPill'
-import { APIKeysData } from 'data/api-keys/api-keys-query'
 
 export const APIKeyRow = ({
   apiKey,
