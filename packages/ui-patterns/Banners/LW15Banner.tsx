cccd1b91f830691e6ab56f5cb16c7c395d9483dd
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2025-07-14 10:59:35 +0200

    add playsInline to banner video

--- packages/ui-patterns/Banners/LW15Banner.tsx
+++ packages/ui-patterns/Banners/LW15Banner.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useRef, useEffect } from 'react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { Button } from 'ui/src/components/Button'
@@ -8,21 +9,45 @@ export function LW15Banner() {
   const pathname = usePathname()
   const isHomePage = pathname === '/'
   const isLWPage = pathname?.includes('/launch-week')
+  const videoRef = useRef<HTMLVideoElement>(null)
+
+  // Force play video
+  useEffect(() => {
+    if (!videoRef || !videoRef.current) return
+
+    //open bug since 2017 that you cannot set muted in video element https://github.com/facebook/react/issues/10389
+    videoRef.current.defaultMuted = true
+    videoRef.current.muted = true
+
+    if (!!videoRef && !!videoRef.current) {
+      const promise = videoRef.current.play()
+      videoRef.current.play()
+      if (promise !== undefined) {
+        promise.then(() => {
+          // Auto-play started
+          videoRef.current?.play()
+        })
+      }
+    }
+  }, [videoRef])
 
   if (isHomePage || isLWPage) return null
 
   return (
     <div className="relative w-full p-2 flex items-center group justify-center text-foreground bg-alternative border-b border-muted transition-colors overflow-hidden">
       <video
+        ref={videoRef}
         src={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/images/launch-week/lw15/assets/lw15-galaxy.mp4`}
+        poster={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/images/launch-week/lw15/assets/lw15-galaxy.png`}
         autoPlay
         loop
         muted
+        playsInline
+        controls={false}
         className="absolute w-full h-full inset-0 object-cover z-0"
         style={{
           opacity: 0.05,
         }}
-        poster={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/images/launch-week/lw15/assets/lw15-galaxy.png`}
       />
       <div className="relative z-10 flex items-center justify-center">
         <div className="w-full flex gap-5 md:gap-10 items-center md:justify-center text-sm">
