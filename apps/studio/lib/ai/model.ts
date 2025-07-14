fb65d12c6257bd4f74f1bfcc12f7e10667cedf7d
Saxon Fletcher <saxonafletcher@gmail.com>
2025-07-14 15:57:23 +1000

    dynamic models

--- apps/studio/lib/ai/model.ts
+++ apps/studio/lib/ai/model.ts
@@ -14,8 +14,9 @@ export const regionMap = {
   eu: 'eu',
 }
 
-const PRO_MODEL = 'anthropic.claude-3-7-sonnet-20250219-v1:0'
-const NORMAL_MODEL = 'meta.llama4-maverick-17b-instruct-v1:0'
+const IS_THROTTLED = process.env.IS_THROTTLED !== 'false'
+const PRO_MODEL = process.env.AI_PRO_MODEL ?? 'anthropic.claude-3-7-sonnet-20250219-v1:0'
+const NORMAL_MODEL = process.env.AI_NORMAL_MODEL ?? 'meta.llama4-maverick-17b-instruct-v1:0'
 const OPENAI_MODEL = 'gpt-4.1-2025-04-14'
 
 export type ModelSuccess = {
@@ -47,7 +48,7 @@ export async function getModel(routingKey?: string, isLimited?: boolean): Promis
     // Select the Bedrock region based on the routing key
     const bedrockRegion: BedrockRegion = routingKey ? await selectBedrockRegion(routingKey) : 'us1'
     const bedrock = bedrockForRegion(bedrockRegion)
-    const model = NORMAL_MODEL
+    const model = IS_THROTTLED || isLimited ? NORMAL_MODEL : PRO_MODEL
     const modelName = `${regionMap[bedrockRegion]}.${model}`
 
     return {
