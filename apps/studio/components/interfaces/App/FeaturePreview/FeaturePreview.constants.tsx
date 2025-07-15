17b71018c67f7e504810bbadae4b35f86b792d01
Saxon Fletcher <saxonafletcher@gmail.com>
2025-07-15 16:05:27 +1000

    add behind feature flag

--- apps/studio/components/interfaces/App/FeaturePreview/FeaturePreview.constants.tsx
+++ apps/studio/components/interfaces/App/FeaturePreview/FeaturePreview.constants.tsx
@@ -2,6 +2,13 @@ import { LOCAL_STORAGE_KEYS } from 'common'
 
 export const FEATURE_PREVIEWS = [
   {
+    key: LOCAL_STORAGE_KEYS.UI_PREVIEW_BRANCHING_2_0,
+    name: 'Branching 2.0',
+    discussionsUrl: 'https://github.com/orgs/supabase/discussions/branching-2-0',
+    isNew: true,
+    isPlatformOnly: true,
+  },
+  {
     key: LOCAL_STORAGE_KEYS.UI_PREVIEW_REALTIME_SETTINGS,
     name: 'Realtime settings',
     discussionsUrl: undefined,
@@ -28,12 +35,5 @@ export const FEATURE_PREVIEWS = [
     discussionsUrl: 'https://github.com/orgs/supabase/discussions/20295',
     isNew: false,
     isPlatformOnly: false,
-  },
-  {
-    key: LOCAL_STORAGE_KEYS.UI_PREVIEW_BRANCHING_2_0,
-    name: 'Branching 2.0',
-    discussionsUrl: 'https://github.com/orgs/supabase/discussions/branching-2-0',
-    isNew: true,
-    isPlatformOnly: true,
   },
 ] as const
