14a290ebe0281eeaaa3cfdcbda686d19915fbc6e
Saxon Fletcher <saxonafletcher@gmail.com>
2025-07-16 14:41:47 +1000

    branch price copy

--- apps/studio/components/interfaces/BranchManagement/CreateBranchModal.tsx
+++ apps/studio/components/interfaces/BranchManagement/CreateBranchModal.tsx
@@ -361,9 +361,7 @@ export const CreateBranchModal = () => {
                   </figure>
                 </div>
                 <div className="flex flex-col gap-y-1">
-                  <p className="text-sm text-foreground">
-                    Preview branches are billed $0.01344 per hour
-                  </p>
+                  <p className="text-sm text-foreground">Branches are billed $0.01344 per hour</p>
                   <p className="text-sm text-foreground-light">
                     This cost will continue for as long as the branch has not been removed.
                   </p>
