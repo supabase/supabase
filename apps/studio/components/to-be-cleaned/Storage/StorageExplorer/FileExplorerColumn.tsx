7156092806873da8194a2c46a976bd0dbb0fe532
Joshen Lim <joshenlimek@gmail.com>
2025-07-22 22:43:00 +0800

    Add guard in storage explorer to not render store if api key is undefined (#37379)
    * add guard
    
    * Fix Storage Explorer empty view if no perms to read API keys

--- apps/studio/components/to-be-cleaned/Storage/StorageExplorer/FileExplorerColumn.tsx
+++ apps/studio/components/to-be-cleaned/Storage/StorageExplorer/FileExplorerColumn.tsx
@@ -1,11 +1,14 @@
 import { Transition } from '@headlessui/react'
+import { PermissionAction } from '@supabase/shared-types/out/constants'
 import { get, noop, sum } from 'lodash'
 import { Upload } from 'lucide-react'
 import { useEffect, useRef, useState } from 'react'
 import { useContextMenu } from 'react-contexify'
+import { toast } from 'sonner'
 
 import InfiniteList from 'components/ui/InfiniteList'
 import ShimmeringLoader from 'components/ui/ShimmeringLoader'
+import { useCheckPermissions } from 'hooks/misc/useCheckPermissions'
 import { BASE_PATH } from 'lib/constants'
 import { formatBytes } from 'lib/helpers'
 import { useStorageExplorerStateSnapshot } from 'state/storage-explorer'
@@ -82,6 +85,7 @@ const FileExplorerColumn = ({
   const fileExplorerColumnRef = useRef<any>(null)
 
   const snap = useStorageExplorerStateSnapshot()
+  const canUpdateStorage = useCheckPermissions(PermissionAction.STORAGE_WRITE, '*')
 
   useEffect(() => {
     if (fileExplorerColumnRef) {
@@ -126,7 +130,12 @@ const FileExplorerColumn = ({
 
   const onDrop = (event: any) => {
     onDragOver(event)
-    onFilesUpload(event, index)
+
+    if (!canUpdateStorage) {
+      toast('You need additional permissions to upload files to this project')
+    } else {
+      onFilesUpload(event, index)
+    }
   }
 
   const SelectAllCheckbox = () => (
