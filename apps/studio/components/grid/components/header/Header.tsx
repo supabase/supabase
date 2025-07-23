b6363ac66d41cc519a42e3f00d937156bd95d7f4
Joshen Lim <joshenlimek@gmail.com>
2025-07-23 17:40:46 +0800

    Add align

--- apps/studio/components/grid/components/header/Header.tsx
+++ apps/studio/components/grid/components/header/Header.tsx
@@ -484,7 +484,7 @@ const RowHeader = () => {
               Export
             </Button>
           </DropdownMenuTrigger>
-          <DropdownMenuContent className={showExportViaCLIOption ? 'w-52' : 'w-40'}>
+          <DropdownMenuContent align="start" className={showExportViaCLIOption ? 'w-52' : 'w-40'}>
             <DropdownMenuItem onClick={onRowsExportCSV}>Export as CSV</DropdownMenuItem>
             <DropdownMenuItem onClick={onRowsExportSQL}>Export as SQL</DropdownMenuItem>
             {/* [Joshen] Should make this available for all cases, but that'll involve updating
