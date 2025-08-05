ea784f478aca3227968294d95d76dbf30242a3e8
Saxon Fletcher <saxonafletcher@gmail.com>
2025-08-05 16:05:37 +1000

    poll size default

--- apps/studio/components/interfaces/Settings/Database/ConnectionPooling/ConnectionPooling.tsx
+++ apps/studio/components/interfaces/Settings/Database/ConnectionPooling/ConnectionPooling.tsx
@@ -80,15 +80,6 @@ export const ConnectionPooling = () => {
   const { mutate: updatePoolerConfig, isLoading: isUpdatingPoolerConfig } =
     usePgbouncerConfigurationUpdateMutation()
 
-  const form = useForm<z.infer<typeof PoolingConfigurationFormSchema>>({
-    resolver: zodResolver(PoolingConfigurationFormSchema),
-    defaultValues: {
-      default_pool_size: undefined,
-      max_client_conn: null,
-    },
-  })
-  const { default_pool_size } = form.watch()
-
   const hasIpv4Addon = !!addons?.selected_addons.find((addon) => addon.type === 'ipv4')
   const computeInstance = addons?.selected_addons.find((addon) => addon.type === 'compute_instance')
   const computeSize =
@@ -100,6 +91,15 @@ export const ConnectionPooling = () => {
     ]
   const defaultPoolSize = poolingOptimizations.poolSize ?? 15
   const defaultMaxClientConn = poolingOptimizations.maxClientConn ?? 200
+
+  const form = useForm<z.infer<typeof PoolingConfigurationFormSchema>>({
+    resolver: zodResolver(PoolingConfigurationFormSchema),
+    defaultValues: {
+      default_pool_size: undefined,
+      max_client_conn: null,
+    },
+  })
+  const { default_pool_size } = form.watch()
   const connectionPoolingUnavailable = pgbouncerConfig?.pool_mode === null
   const ignoreStartupParameters = pgbouncerConfig?.ignore_startup_parameters
 
@@ -128,7 +128,10 @@ export const ConnectionPooling = () => {
   }
 
   const resetForm = () => {
-    form.reset({ default_pool_size: pgbouncerConfig?.default_pool_size })
+    form.reset({
+      default_pool_size: pgbouncerConfig?.default_pool_size ?? defaultPoolSize,
+      max_client_conn: pgbouncerConfig?.max_client_conn ?? defaultMaxClientConn,
+    })
   }
 
   useEffect(() => {
@@ -236,8 +239,8 @@ export const ConnectionPooling = () => {
                           {...field}
                           type="number"
                           className="w-full"
-                          value={field.value || undefined}
-                          placeholder={!field.value ? `${defaultPoolSize}` : ''}
+                          value={field.value || ''}
+                          placeholder={defaultPoolSize.toString()}
                           {...form.register('default_pool_size', {
                             setValueAs: setValueAsNullableNumber,
                           })}
@@ -291,7 +294,7 @@ export const ConnectionPooling = () => {
                           className="w-full"
                           value={pgbouncerConfig?.max_client_conn || ''}
                           disabled={true}
-                          placeholder={!field.value ? `${defaultMaxClientConn}` : ''}
+                          placeholder={defaultMaxClientConn.toString()}
                           {...form.register('max_client_conn', {
                             setValueAs: setValueAsNullableNumber,
                           })}
