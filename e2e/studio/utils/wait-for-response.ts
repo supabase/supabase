25386031fdc693f9e738333504d97ae2278d3729
Michael Ong <minghao_3728@hotmail.com>
2025-08-06 13:29:07 +0800

    chore: minor fix to reduce flakiness of database playwright tests

--- e2e/studio/utils/wait-for-response.ts
+++ e2e/studio/utils/wait-for-response.ts
@@ -1,15 +1,36 @@
 import { Page } from '@playwright/test'
 
+type HttpMethod = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE'
+
 /**
  * Waits for a API response for a specific endpoint before continuing the playwright test.
  * @param page - Playwright page object
+ * @param basePath - Base path of API endpoint to wait for (e.g. 'pg-meta', 'platform/projects', etc.)
  * @param ref - Project reference
- * @param endpoint - The endpoint to wait for (e.g., 'types', 'triggers')
+ * @param action - Action path of API endpoint to wait for (e.g. 'types', 'triggers', 'content', etc.)
+ * @param options - Optional object which checks more scenarios
  */
-export async function waitForApiResponse(page: Page, ref: string, endpoint: string): Promise<void> {
-  await page.waitForResponse(
-    (response) =>
-      response.url().includes(`pg-meta/${ref}/${endpoint}`) ||
-      response.url().includes(`pg-meta/default/${endpoint}`)
-  )
+export async function waitForApiResponse(
+  page: Page,
+  basePath: string,
+  ref: string,
+  action: string,
+  options?: Options
+): Promise<void> {
+  // regex trims "/" both start and end.
+  const trimmedBasePath = basePath.replace(/^\/+|\/+$/g, '')
+  const httpMethod = options?.method
+
+  await page.waitForResponse((response) => {
+    const urlMatches =
+      response.url().includes(`${trimmedBasePath}/${ref}/${action}`) ||
+      response.url().includes(`${trimmedBasePath}/default/${action}`)
+
+    // checks HTTP method if exists
+    return httpMethod ? urlMatches && response.request().method() === httpMethod : urlMatches
+  })
+}
+
+type Options = {
+  method?: HttpMethod
 }
