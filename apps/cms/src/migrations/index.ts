0488f565b3a1d7879ad27bcf5e5cc47e39deb8cd
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2025-08-08 17:09:40 +0200

    add migration to update payload new migrations

--- apps/cms/src/migrations/index.ts
+++ apps/cms/src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250619_155345 from './20250619_155345.ts';
 import * as migration_20250619_160549 from './20250619_160549.ts';
+import * as migration_20250808_145722_update_payload_migrations from './20250808_145722_update_payload_migrations.ts';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250619_160549.up,
     down: migration_20250619_160549.down,
-    name: '20250619_160549'
+    name: '20250619_160549',
+  },
+  {
+    up: migration_20250808_145722_update_payload_migrations.up,
+    down: migration_20250808_145722_update_payload_migrations.down,
+    name: '20250808_145722_update_payload_migrations'
   },
 ];
