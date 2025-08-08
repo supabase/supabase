1ca9bef6ad4e920e837c7c198664b82bc84f1b03
taishikato <taishi.k0903@gmail.com>
2025-08-08 16:59:52 -0400

    delete `index.ts`

--- apps/studio/lib/ai-assistant-invalidation/index.ts
+++
@@ -1,8 +0,0 @@
-export { QueryCacheInvalidator as InvalidationRouter } from './query-cache-invalidator'
-export type {
-  InvalidationEvent,
-  InvalidationConfig,
-  EntityType,
-  ActionType,
-} from './query-cache-invalidator'
-export { useEnhancedExecuteSqlMutation } from './use-enhanced-execute-sql-mutation'
