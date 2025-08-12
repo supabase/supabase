0a6875afc01d35cdab90adc9b65a347ad67d5221
Danny White <3104761+dnywh@users.noreply.github.com>
2025-08-12 12:13:17 +1000

    wip: styles

--- apps/www/components/SurveyResults/SurveyChapter.tsx
+++ apps/www/components/SurveyResults/SurveyChapter.tsx
@@ -25,12 +25,36 @@ export function SurveyChapter({
   return (
     <section
       id={`chapter-${number}`}
-      className="flex flex-col bg-alternative border-t last-of-type:border-b pt-40"
+      // border-t last-of-type:border-b
+      className="flex flex-col bg-alternative"
     >
       <div className="flex flex-col container max-w-[60rem] mx-auto px-6 lg:px-0">
-        <header className="mx-auto flex flex-col gap-4 text-center text-balance">
-          <h2 className="text-3xl md:text-4xl xl:text-6xl text-balance leading-tight">{title}</h2>
-          <p className="text-lg xl:text-xl text-foreground-light text-balance">{description}</p>
+        <header
+          className="flex flex-col gap-24"
+          style={{
+            background: `radial-gradient(circle at center 230%, hsl(var(--brand-500)), transparent 70%)`,
+          }}
+        >
+          {/* Decorative progress bar */}
+          <div aria-hidden="true" className="flex flex-col">
+            {[0, 1, 2, 3].map((item, index) => (
+              <div
+                key={index}
+                className={`h-${(index + 1) * 4} w-full ${index === 0 ? 'bg-brand' : index === 1 ? 'bg-brand-500' : 'bg-brand-300'}`}
+                style={{
+                  maskImage: 'url("/survey/pattern-front.svg")',
+                  maskSize: '14.5px 15px',
+                  maskRepeat: 'repeat',
+                  maskPosition: 'top left',
+                }}
+              />
+            ))}
+          </div>
+          {/* Text content */}
+          <div className="flex flex-col gap-4  text-balance pb-12">
+            <h2 className="text-3xl md:text-4xl xl:text-6xl text-balance leading-tight">{title}</h2>
+            <p className="text-lg xl:text-xl text-foreground-light text-balance">{description}</p>
+          </div>
         </header>
         <div className="flex flex-col pb-32">{children}</div>
       </div>
