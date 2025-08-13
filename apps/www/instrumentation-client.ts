0f855879524eaed7549cd1ab053ed48b28496f0e
Joshen Lim <joshenlimek@gmail.com>
2025-08-13 11:52:51 +0700

    Set up Sentry for www

---
+++ apps/www/instrumentation-client.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs'
+
+Sentry.init({
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+})
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart
