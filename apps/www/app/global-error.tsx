957cd6f4796fa3e5701557a27a4d6e7ac129f667
Joshen Lim <joshenlimek@gmail.com>
2025-08-13 12:08:41 +0700

    Prettier

--- apps/www/app/global-error.tsx
+++ apps/www/app/global-error.tsx
@@ -1,13 +1,13 @@
-"use client";
+'use client'
 
-import * as Sentry from "@sentry/nextjs";
-import NextError from "next/error";
-import { useEffect } from "react";
+import * as Sentry from '@sentry/nextjs'
+import NextError from 'next/error'
+import { useEffect } from 'react'
 
 export default function GlobalError({ error }: { error: Error & { digest?: string } }) {
   useEffect(() => {
-    Sentry.captureException(error);
-  }, [error]);
+    Sentry.captureException(error)
+  }, [error])
 
   return (
     <html>
@@ -19,5 +19,5 @@ export default function GlobalError({ error }: { error: Error & { digest?: strin
         <NextError statusCode={0} />
       </body>
     </html>
-  );
+  )
 }
--- apps/www/instrumentation.ts
+++ apps/www/instrumentation.ts
@@ -1,13 +1,13 @@
-import * as Sentry from '@sentry/nextjs';
+import * as Sentry from '@sentry/nextjs'
 
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await import('./sentry.server.config');
+    await import('./sentry.server.config')
   }
 
   if (process.env.NEXT_RUNTIME === 'edge') {
-    await import('./sentry.edge.config');
+    await import('./sentry.edge.config')
   }
 }
 
-export const onRequestError = Sentry.captureRequestError;
+export const onRequestError = Sentry.captureRequestError
