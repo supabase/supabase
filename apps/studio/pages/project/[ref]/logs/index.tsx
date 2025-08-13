eec1d40b6fd3773ee4572cf62f9aea153469b89e
Jordi Enric <jordi.err@gmail.com>
2025-08-13 14:11:07 +0200

    simplify, only redirect to explorer, keep unifiedlog check

--- apps/studio/pages/project/[ref]/logs/index.tsx
+++ apps/studio/pages/project/[ref]/logs/index.tsx
@@ -1,35 +1,25 @@
 import { useRouter } from 'next/router'
-import { useContext, useEffect } from 'react'
+import { useEffect } from 'react'
 
-import { FeatureFlagContext, LOCAL_STORAGE_KEYS, useParams } from 'common'
+import { useParams } from 'common'
 import { useUnifiedLogsPreview } from 'components/interfaces/App/FeaturePreview/FeaturePreviewContext'
 import { UnifiedLogs } from 'components/interfaces/UnifiedLogs/UnifiedLogs'
 import DefaultLayout from 'components/layouts/DefaultLayout'
-import LogsLayout from 'components/layouts/LogsLayout/LogsLayout'
 import ProjectLayout from 'components/layouts/ProjectLayout/ProjectLayout'
-import { useLocalStorageQuery } from 'hooks/misc/useLocalStorage'
-import { useSelectedOrganizationQuery } from 'hooks/misc/useSelectedOrganization'
-import { IS_PLATFORM } from 'lib/constants'
-import type { NextPageWithLayout } from 'types'
+import { NextPageWithLayout } from 'types'
 
 export const LogPage: NextPageWithLayout = () => {
   const router = useRouter()
   const { ref } = useParams()
-  const { hasLoaded } = useContext(FeatureFlagContext)
 
-  const { data: org } = useSelectedOrganizationQuery()
   const { isEnabled: isUnifiedLogsEnabled } = useUnifiedLogsPreview()
 
-  const [lastVisitedLogsPage] = useLocalStorageQuery(
-    LOCAL_STORAGE_KEYS.LAST_VISITED_LOGS_PAGE,
-    'explorer'
-  )
-
   useEffect(() => {
-    if (hasLoaded && !!org && !isUnifiedLogsEnabled) {
-      router.replace(`/project/${ref}/logs/${lastVisitedLogsPage}`)
+    if (!isUnifiedLogsEnabled) {
+      router.replace(`/project/${ref}/logs/explorer`)
     }
-  }, [router, hasLoaded, org, lastVisitedLogsPage, ref, isUnifiedLogsEnabled])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isUnifiedLogsEnabled])
 
   if (isUnifiedLogsEnabled) {
     return (
@@ -41,14 +31,7 @@ export const LogPage: NextPageWithLayout = () => {
     )
   }
 
-  return (
-    <DefaultLayout>
-      <LogsLayout>
-        {/* Empty placeholder - the useEffect will handle redirect */}
-        <div></div>
-      </LogsLayout>
-    </DefaultLayout>
-  )
+  return null
 }
 
 // Don't use getLayout since we're handling layouts conditionally within the component
