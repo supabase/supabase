3d2cf12bf3973dde6e27e62aedd0012cdda0ef3f
Joshen Lim <joshenlimek@gmail.com>
2025-08-13 17:50:05 +0700

    Use new page layout

--- apps/studio/pages/project/[ref]/database/publications/index.tsx
+++ apps/studio/pages/project/[ref]/database/publications/index.tsx
@@ -3,8 +3,8 @@ import { PermissionAction } from '@supabase/shared-types/out/constants'
 import { PublicationsList } from 'components/interfaces/Database/Publications/PublicationsList'
 import DatabaseLayout from 'components/layouts/DatabaseLayout/DatabaseLayout'
 import DefaultLayout from 'components/layouts/DefaultLayout'
+import { PageLayout } from 'components/layouts/PageLayout/PageLayout'
 import { ScaffoldContainer, ScaffoldSection } from 'components/layouts/Scaffold'
-import { FormHeader } from 'components/ui/Forms/FormHeader'
 import NoPermission from 'components/ui/NoPermission'
 import { useAsyncCheckProjectPermissions } from 'hooks/misc/useCheckPermissions'
 import type { NextPageWithLayout } from 'types'
@@ -19,11 +19,8 @@ const DatabasePublications: NextPageWithLayout = () => {
 
   return (
     <ScaffoldContainer>
-      <ScaffoldSection>
-        <div className="col-span-12">
-          <FormHeader title="Database Publications" />
-          <PublicationsList />
-        </div>
+      <ScaffoldSection isFullWidth>
+        <PublicationsList />
       </ScaffoldSection>
     </ScaffoldContainer>
   )
@@ -31,7 +28,9 @@ const DatabasePublications: NextPageWithLayout = () => {
 
 DatabasePublications.getLayout = (page) => (
   <DefaultLayout>
-    <DatabaseLayout title="Database">{page}</DatabaseLayout>
+    <DatabaseLayout title="Database">
+      <PageLayout title="Database Publications">{page}</PageLayout>
+    </DatabaseLayout>
   </DefaultLayout>
 )
 
