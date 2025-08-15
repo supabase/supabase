23d1728d216c6a790e8dbd7efc756195386a1eaa
Joshen Lim <joshenlimek@gmail.com>
2025-08-15 16:53:17 +0700

    Add links to ETL logs

--- apps/studio/components/interfaces/Database/Replication/ReplicationPipelineStatus.tsx
+++ apps/studio/components/interfaces/Database/Replication/ReplicationPipelineStatus.tsx
@@ -174,6 +174,9 @@ export const ReplicationPipelineStatus = () => {
               }
             />
           </div>
+          <Button asChild type="default">
+            <Link href={`/project/${ref}/logs/etl-replication-logs`}>View logs</Link>
+          </Button>
           <Button
             type={statusName === 'stopped' ? 'primary' : 'default'}
             onClick={() => onTogglePipeline()}
