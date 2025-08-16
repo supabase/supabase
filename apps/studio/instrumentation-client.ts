89b66bb0df6d9618d1882796b35f4a060c4f0f52
Jordi Enric <jordi.err@gmail.com>
2025-08-16 11:54:36 +0200

    add missing onRouterTransitionStart

--- apps/studio/instrumentation-client.ts
+++ apps/studio/instrumentation-client.ts
@@ -152,3 +152,7 @@ function standardiseRouterUrl(url: string) {
 
   return finalUrl
 }
+
+// This export will instrument router navigations, and is only relevant if you enable tracing.
+// `captureRouterTransitionStart` is available from SDK version 9.12.0 onwards
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart
