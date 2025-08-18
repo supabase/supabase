2608f5e9d795a43a65e6d244d88153bfd464cb96
Joshen Lim <joshenlimek@gmail.com>
2025-08-18 12:31:37 +0800

    Add test case

--- apps/studio/components/interfaces/TableGridEditor/SidePanelEditor/SidePanelEditor.utils.test.ts
+++ apps/studio/components/interfaces/TableGridEditor/SidePanelEditor/SidePanelEditor.utils.test.ts
@@ -73,4 +73,16 @@ describe('SidePanelEditor.utils.test.ts', () => {
     const formattedRows = formatRowsForInsert({ rows, headers, columns: columns as any })
     expect(formattedRows).toEqual([{ id: 1, value: ['item1', 'item2', 'item3'] }])
   })
+
+  test('formatRowsForInsert should handle Postgres array values properly if provided JSON string', () => {
+    const rows = [{ id: 1, value: '["item1", "item2", "item3"]' }]
+    const headers = ['id', 'value']
+    const columns = [
+      { id: '1', name: 'id', data_type: 'bigint', format: 'int8', is_nullable: false },
+      { id: '2', name: 'value', data_type: 'ARRAY', format: '_text', is_nullable: true },
+    ]
+
+    const formattedRows = formatRowsForInsert({ rows, headers, columns: columns as any })
+    expect(formattedRows).toEqual([{ id: 1, value: ['item1', 'item2', 'item3'] }])
+  })
 })
