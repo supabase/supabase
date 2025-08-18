d25e5f49123ae42d2a37b1596ba05baf03df4949
Joshen Lim <joshenlimek@gmail.com>
2025-08-19 00:00:18 +0800

    Update warning dialog in NewAccessTokenDialog

--- apps/studio/components/interfaces/Account/AccessTokens/NewAccessTokenDialog.tsx
+++ apps/studio/components/interfaces/Account/AccessTokens/NewAccessTokenDialog.tsx
@@ -29,15 +29,15 @@ const TokenSchema = z.object({
 
 export interface NewAccessTokenDialogProps {
   open: boolean
-  onOpenChange: (open: boolean) => void
   tokenScope: 'V0' | undefined
+  onOpenChange: (open: boolean) => void
   onCreateToken: (token: any) => void
 }
 
 export const NewAccessTokenDialog = ({
   open,
-  onOpenChange,
   tokenScope,
+  onOpenChange,
   onCreateToken,
 }: NewAccessTokenDialogProps) => {
   const form = useForm<z.infer<typeof TokenSchema>>({
@@ -49,10 +49,7 @@ export const NewAccessTokenDialog = ({
 
   const onSubmit: SubmitHandler<z.infer<typeof TokenSchema>> = async (values) => {
     createAccessToken(
-      {
-        name: values.tokenName,
-        scope: tokenScope,
-      },
+      { name: values.tokenName, scope: tokenScope },
       {
         onSuccess: (data) => {
           toast.success('Access token created successfully')
@@ -83,28 +80,36 @@ export const NewAccessTokenDialog = ({
           </DialogTitle>
         </DialogHeader>
         <DialogSectionSeparator />
+        {tokenScope === 'V0' ? (
+          <Admonition
+            type="warning"
+            className="rounded-none border-t-0 border-x-0 mb-0"
+            title="The experimental API provides additional endpoints which allows you to manage your organizations and projects."
+            description={
+              <>
+                <p>
+                  These include deleting organizations and projects which cannot be undone. As such,
+                  be very careful when using this API.
+                </p>
+                <div className="mt-4">
+                  <Button asChild type="default" icon={<ExternalLink />}>
+                    <a href="https://api.supabase.com/api/v0" target="_blank" rel="noreferrer">
+                      Experimental API documentation
+                    </a>
+                  </Button>
+                </div>
+              </>
+            }
+          />
+        ) : (
+          <Admonition
+            type="warning"
+            className="rounded-none border-t-0 border-x-0 mb-0"
+            title="Access tokens can be used to control your whole account"
+            description="Be careful when sharing your tokens"
+          />
+        )}
         <DialogSection className="flex flex-col gap-4">
-          {tokenScope === 'V0' && (
-            <Admonition
-              type="warning"
-              title="The experimental API provides additional endpoints which allows you to manage your organizations and projects."
-              description={
-                <>
-                  <p>
-                    These include deleting organizations and projects which cannot be undone. As
-                    such, be very careful when using this API.
-                  </p>
-                  <div className="mt-4">
-                    <Button asChild type="default" icon={<ExternalLink />}>
-                      <a href="https://api.supabase.com/api/v0" target="_blank" rel="noreferrer">
-                        Experimental API documentation
-                      </a>
-                    </Button>
-                  </div>
-                </>
-              }
-            />
-          )}
           <Form_Shadcn_ {...form}>
             <form
               id={formId}
