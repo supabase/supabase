96351f2c597d2182bcb66c5c43d778286ceba4df
Chris Chinchilla <chris.ward@supabase.io>
2025-08-19 17:57:40 +0200

    Starte SvelteKit

---
+++ examples/auth/sveltekit/src/routes/+layout.server.ts
@@ -0,0 +1,9 @@
+import type { LayoutServerLoad } from './$types'
+
+export const load: LayoutServerLoad = async ({ locals: { safeGetSession }, cookies }) => {
+  const { session } = await safeGetSession()
+  return {
+    session,
+    cookies: cookies.getAll(),
+  }
+}
