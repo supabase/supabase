adf11788b2248983602787bf87e70d69f887f048
Danny White <3104761+dnywh@users.noreply.github.com>
2025-08-20 16:52:09 +1000

    feat: dedicated functions approach

--- apps/www/components/SurveyResults/charts/AcceleratorParticipationChart.tsx
+++ apps/www/components/SurveyResults/charts/AcceleratorParticipationChart.tsx
@@ -1,26 +1,5 @@
 import { SurveyChart, buildWhereClause } from '../SurveyChart'
 
-// Helper function to convert filters to the format expected by the database function
-function buildFunctionParams(activeFilters: Record<string, string>) {
-  const params: Record<string, any> = {}
-
-  // Convert single values to arrays for the function parameters
-  if (activeFilters.person_age && activeFilters.person_age !== 'unset') {
-    params.person_age_filter = [activeFilters.person_age]
-  }
-  if (activeFilters.location && activeFilters.location !== 'unset') {
-    params.location_filter = [activeFilters.location]
-  }
-  if (activeFilters.money_raised && activeFilters.money_raised !== 'unset') {
-    params.money_raised_filter = [activeFilters.money_raised]
-  }
-  // if (activeFilters.team_size && activeFilters.team_size !== 'unset') {
-  //   params.team_size_filter = [activeFilters.team_size]
-  // }
-
-  return params
-}
-
 function generateAcceleratorParticipationSQL(activeFilters: Record<string, string>) {
   const whereClause = buildWhereClause(activeFilters, ['accelerator_participation IS NOT NULL'])
 
@@ -51,7 +30,6 @@ export function AcceleratorParticipationChart() {
       targetColumn="accelerator_participation"
       filterColumns={['person_age', 'location', 'money_raised']}
       functionName="get_accelerator_stats"
-      functionParams={buildFunctionParams}
       generateSQLQuery={generateAcceleratorParticipationSQL}
     />
   )
--- apps/www/components/SurveyResults/charts/SalesToolsChart.tsx
+++ apps/www/components/SurveyResults/charts/SalesToolsChart.tsx
@@ -1,23 +1,5 @@
 import { SurveyChart, buildWhereClause } from '../SurveyChart'
 
-// Helper function to convert filters to the format expected by the database function
-function buildFunctionParams(activeFilters: Record<string, string>) {
-  const params: Record<string, any> = {}
-
-  // Convert single values to arrays for the function parameters
-  if (activeFilters.person_age && activeFilters.person_age !== 'unset') {
-    params.person_age_filter = [activeFilters.person_age]
-  }
-  if (activeFilters.location && activeFilters.location !== 'unset') {
-    params.location_filter = [activeFilters.location]
-  }
-  if (activeFilters.money_raised && activeFilters.money_raised !== 'unset') {
-    params.money_raised_filter = [activeFilters.money_raised]
-  }
-
-  return params
-}
-
 function generateSalesToolsSQL(activeFilters: Record<string, string>) {
   const whereClause = buildWhereClause(activeFilters)
 
@@ -57,7 +39,6 @@ export function SalesToolsChart() {
       targetColumn="sales_tools"
       filterColumns={['person_age', 'location', 'team_size']}
       functionName="get_sales_tools_stats"
-      functionParams={buildFunctionParams}
       generateSQLQuery={generateSalesToolsSQL}
     />
   )
