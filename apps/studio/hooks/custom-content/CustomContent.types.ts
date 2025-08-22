f9027a29cdab21eb22f82337065faaf99ea420b6
Joshen Lim <joshenlimek@gmail.com>
2025-08-22 17:57:40 +0800

    Chore/custom content connect UI frameworks (#38090)
    * Init custom content hook
    
    * Implement useCustomContent hook similarly to useIsFeatureEnabled, and implement extension of organization documents
    
    * Attempt to type things nicely
    
    * Add support for custom content example projects
    
    * Add support for custom content logs explorer default query
    
    * nit
    
    * Add support for custom content connect UI frameworks
    
    * Reset custom-content.json
    
    * Smol nit
    
    * set custom-content to null
    
    ---------
    
    Co-authored-by: Jordi Enric <jordi.err@gmail.com>

--- apps/studio/hooks/custom-content/CustomContent.types.ts
+++ apps/studio/hooks/custom-content/CustomContent.types.ts
@@ -1,3 +1,5 @@
+import { ConnectionType } from 'components/interfaces/Connect/Connect.constants'
+
 export type CustomContentTypes = {
   organizationLegalDocuments: {
     id: string
@@ -5,6 +7,7 @@ export type CustomContentTypes = {
     description: string
     action: { text: string; url: string }
   }[]
+
   projectHomepageExampleProjects: {
     title: string
     description: string
@@ -13,4 +16,18 @@ export type CustomContentTypes = {
   }[]
 
   logsDefaultQuery: string
+
+  /**
+   * When declaring files for each framework, there are 3 properties that can be dynamically rendered into the file content using handlebar notation:
+   * - {{apiUrl}}: The API URL of the project
+   * - {{anonKey}}: The anonymous key of the project (if still using legacy API keys)
+   * - {{publishableKey}}: The publishable API key of the project (if using new API keys)
+   *
+   * These could be helpful in rendering, for e.g an environment file like `.env`
+   */
+  connectFrameworks: {
+    key: string
+    label: string
+    obj: ConnectionType[]
+  }
 }
