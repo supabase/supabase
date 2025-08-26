3c2487a11ffde28cdb33c1b9f1e33c890d470ac4
Chris Chinchilla <chris@chrischinchilla.com>
2025-08-26 15:28:36 +0200

    Changes

--- examples/auth/sveltekit/src/routes/+layout.server.ts
+++ examples/auth/sveltekit/src/routes/+layout.server.ts
@@ -1,9 +1,10 @@
 import type { LayoutServerLoad } from './$types'
 
 export const load: LayoutServerLoad = async ({ locals: { safeGetSession }, cookies }) => {
-  const { session } = await safeGetSession()
+  const { session, user } = await safeGetSession()
   return {
     session,
+    user,
     cookies: cookies.getAll(),
   }
 }
