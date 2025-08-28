532ef3a5cc637bcdc8123775d9851eb6f3f91b7b
Jordi Enric <jordi.err@gmail.com>
2025-08-28 16:55:08 +0200

    rm unnecessary div

--- apps/studio/components/interfaces/Reports/v2/ReportChartUpsell.tsx
+++ apps/studio/components/interfaces/Reports/v2/ReportChartUpsell.tsx
@@ -66,7 +66,6 @@ export function ReportChartUpsell({
           </Link>
         </Button>
       </div>
-      <div className="absolute top-0 left-0 w-full h-full z-0"></div>
       <div className="absolute top-0 left-0 w-full h-full z-0">
         <LogChartHandler
           attributes={[
