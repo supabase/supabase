61ae7958b24e5c266832c39c34efdcedf8ce8f7f
taishikato <taishi.k0903@gmail.com>
2025-08-29 10:47:14 -0400

    imitate `dashboard/project/ref/functions/functionSlug` in `project/default/functions/versions`since i can't use `project/default/functions/functionSlug/` isn't available on local dev

--- apps/studio/pages/project/[ref]/functions/versions.tsx
+++ apps/studio/pages/project/[ref]/functions/versions.tsx
@@ -3,7 +3,17 @@ import DefaultLayout from 'components/layouts/DefaultLayout'
 import EdgeFunctionsLayout from 'components/layouts/EdgeFunctionsLayout/EdgeFunctionsLayout'
 import { PageLayout } from 'components/layouts/PageLayout/PageLayout'
 import { ScaffoldContainer, ScaffoldSection } from 'components/layouts/Scaffold'
+import { DocsButton } from 'components/ui/DocsButton'
+import { Download, FileArchive, Send } from 'lucide-react'
 import type { NextPageWithLayout } from 'types'
+import {
+  Button,
+  Popover_Shadcn_,
+  Separator,
+  PopoverTrigger_Shadcn_,
+  PopoverContent_Shadcn_,
+} from 'ui'
+import { Input } from 'ui-patterns/DataInputs/Input'
 
 const VersionsPage: NextPageWithLayout = () => {
   return (
@@ -16,13 +26,90 @@ const VersionsPage: NextPageWithLayout = () => {
 }
 
 VersionsPage.getLayout = (page) => {
+  const breadcrumbItems = [
+    {
+      label: 'Edge Functions',
+      href: `/project/default/functions`,
+    },
+  ]
+
+  const functionSlug = 'demo-functions'
+  const ref = 'default'
+
+  const navigationItems = functionSlug
+    ? [
+        {
+          label: 'Overview',
+          href: `/project/${ref}/functions/${functionSlug}`,
+        },
+        {
+          label: 'Invocations',
+          href: `/project/${ref}/functions/${functionSlug}/invocations`,
+        },
+        {
+          label: 'Logs',
+          href: `/project/${ref}/functions/${functionSlug}/logs`,
+        },
+        {
+          label: 'Code',
+          href: `/project/${ref}/functions/${functionSlug}/code`,
+        },
+        {
+          label: 'Details',
+          href: `/project/${ref}/functions/${functionSlug}/details`,
+        },
+      ]
+    : []
+
   return (
     <DefaultLayout>
       <EdgeFunctionsLayout>
         <PageLayout
-          size="large"
-          title="Version Restoration"
-          subtitle="Restore a previous version of your Edge Functions"
+          title={functionSlug}
+          breadcrumbs={breadcrumbItems}
+          navigationItems={navigationItems}
+          isCompact
+          size="full"
+          primaryActions={
+            <div className="flex items-center space-x-2">
+              <DocsButton href="https://supabase.com/docs/guides/functions" />
+              <Popover_Shadcn_>
+                <PopoverTrigger_Shadcn_ asChild>
+                  <Button type="default" icon={<Download />}>
+                    Download
+                  </Button>
+                </PopoverTrigger_Shadcn_>
+                <PopoverContent_Shadcn_ align="end" className="p-0">
+                  <div className="p-3 flex flex-col gap-y-2">
+                    <p className="text-xs text-foreground-light">Download via CLI</p>
+                    <Input
+                      copy
+                      showCopyOnHover
+                      readOnly
+                      containerClassName=""
+                      className="text-xs font-mono tracking-tighter"
+                      value={`supabase functions download ${functionSlug}`}
+                    />
+                  </div>
+                  <Separator className="!bg-border-overlay" />
+                  <div className="py-2 px-1">
+                    <Button
+                      type="text"
+                      className="w-min hover:bg-transparent"
+                      icon={<FileArchive />}
+                    >
+                      Download as ZIP
+                    </Button>
+                  </div>
+                </PopoverContent_Shadcn_>
+              </Popover_Shadcn_>
+              {!!functionSlug && (
+                <Button type="default" icon={<Send />}>
+                  Test
+                </Button>
+              )}
+            </div>
+          }
         >
           {page}
         </PageLayout>
