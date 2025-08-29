c19429afdfdcc9062a8e50594077413775e78efc
Danny White <3104761+dnywh@users.noreply.github.com>
2025-08-29 16:52:57 +1000

    fix: grammar

--- apps/studio/components/interfaces/ProjectCreation/RegionSelector.tsx
+++ apps/studio/components/interfaces/ProjectCreation/RegionSelector.tsx
@@ -88,7 +88,7 @@ export const RegionSelector = ({
           <p>Select the region closest to your users for the best performance.</p>
           {showNonProdFields && (
             <div className="mt-2 text-warning-600">
-              <p>Only the following regions are supported for local/staging projects:</p>
+              <p>Only these regions are supported for local/staging projects:</p>
               <ul className="list-disc list-inside mt-1">
                 <li>East US (North Virginia)</li>
                 <li>Central EU (Frankfurt)</li>
