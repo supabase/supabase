c3b41a89c38a6d79a8c1a9ac60e5378c6715741d
Jordi Enric <jordi.err@gmail.com>
2025-09-01 23:21:45 +0200

    update edge fn ui

--- apps/studio/pages/project/[ref]/reports/edge-functions.tsx
+++ apps/studio/pages/project/[ref]/reports/edge-functions.tsx
@@ -1,11 +1,18 @@
 import { useQueryClient } from '@tanstack/react-query'
 import { useParams } from 'common'
 import dayjs from 'dayjs'
-import { ArrowRight, ChevronDown, RefreshCw } from 'lucide-react'
-import { useEffect, useMemo, useState } from 'react'
+import { ArrowRight, RefreshCw } from 'lucide-react'
+import { useState, useMemo } from 'react'
 
-import { Label } from '@ui/components/shadcn/ui/label'
 import { ReportChartV2 } from 'components/interfaces/Reports/v2/ReportChartV2'
+import {
+  ReportsSelectFilter,
+  type SelectFilters,
+} from 'components/interfaces/Reports/v2/ReportsSelectFilter'
+import {
+  ReportsNumericFilter,
+  type NumericFilter,
+} from 'components/interfaces/Reports/v2/ReportsNumericFilter'
 import ReportHeader from 'components/interfaces/Reports/ReportHeader'
 import ReportPadding from 'components/interfaces/Reports/ReportPadding'
 import ReportStickyNav from 'components/interfaces/Reports/ReportStickyNav'
@@ -13,7 +20,6 @@ import { LogsDatePicker } from 'components/interfaces/Settings/Logs/Logs.DatePic
 import DefaultLayout from 'components/layouts/DefaultLayout'
 import ReportsLayout from 'components/layouts/ReportsLayout/ReportsLayout'
 import { ButtonTooltip } from 'components/ui/ButtonTooltip'
-import { Button, Checkbox, DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from 'ui'
 import { useChartHoverState } from 'components/ui/Charts/useChartHoverState'
 
 import { useEdgeFunctionsQuery } from 'data/edge-functions/edge-functions-query'
@@ -25,6 +31,7 @@ import { useReportDateRange } from 'hooks/misc/useReportDateRange'
 
 import type { NextPageWithLayout } from 'types'
 import { ReportSettings } from 'components/ui/Charts/ReportSettings'
+import { EDGE_FUNCTION_REGIONS } from 'components/interfaces/Reports/Reports.constants'
 
 const EdgeFunctionsReportV2: NextPageWithLayout = () => {
   return (
@@ -44,22 +51,28 @@ export default EdgeFunctionsReportV2
 
 const EdgeFunctionsUsage = () => {
   const { ref } = useParams()
-  const { data: functions, isLoading: isLoadingFunctions } = useEdgeFunctionsQuery({
+  const { data: functions } = useEdgeFunctionsQuery({
     projectRef: ref,
   })
 
   const chartSyncId = `edge-functions-${ref}`
   useChartHoverState(chartSyncId)
 
-  const [isOpen, setIsOpen] = useState(false)
-  const [functionIds, setFunctionIds] = useState<string[]>([])
-  const [tempFunctionIds, setTempFunctionIds] = useState<string[]>(functionIds)
+  // Filters
+  const [statusCodeFilter, setStatusCodeFilter] = useState<NumericFilter | undefined>()
+  const [regionFilter, setRegionFilter] = useState<SelectFilters>({})
+  const [executionTimeFilter, setExecutionTimeFilter] = useState<NumericFilter | undefined>()
+  const [functionFilter, setFunctionFilter] = useState<SelectFilters>({})
 
-  useEffect(() => {
-    if (isOpen) {
-      setTempFunctionIds(functionIds)
-    }
-  }, [isOpen, functionIds])
+  const filters = useMemo(
+    () => ({
+      status_code: statusCodeFilter,
+      region: regionFilter,
+      execution_time: executionTimeFilter,
+      functions: functionFilter,
+    }),
+    [statusCodeFilter, regionFilter, executionTimeFilter, functionFilter]
+  )
 
   const {
     selectedDateRange,
@@ -81,17 +94,15 @@ const EdgeFunctionsUsage = () => {
       startDate: selectedDateRange?.period_start?.date ?? '',
       endDate: selectedDateRange?.period_end?.date ?? '',
       interval: selectedDateRange?.interval ?? 'minute',
-      filters: {
-        functionIds,
-      },
+      filters,
     })
-  }, [ref, functions, selectedDateRange, functionIds])
+  }, [ref, functions, selectedDateRange, filters])
 
   const onRefreshReport = async () => {
     if (!selectedDateRange) return
 
     setIsRefreshing(true)
-    queryClient.invalidateQueries(['report-v2'])
+    queryClient.invalidateQueries({ queryKey: ['report-v2'] })
     setTimeout(() => setIsRefreshing(false), 1000)
   }
 
@@ -138,88 +149,47 @@ const EdgeFunctionsUsage = () => {
                 </div>
               )}
             </div>
-            <div>
-              <DropdownMenu open={isOpen} onOpenChange={setIsOpen}>
-                <DropdownMenuTrigger asChild>
-                  <Button type="default" iconRight={<ChevronDown />}>
-                    {functionIds.length === 0
-                      ? 'All Functions'
-                      : `${functionIds.length} function${
-                          functionIds.length > 1 ? 's' : ''
-                        } selected`}
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent side="bottom" align="start" className="w-72 p-0">
-                  <div className="space-y-px max-h-60 overflow-y-auto p-1">
-                    <Label
-                      key="all-functions"
-                      htmlFor="all-functions"
-                      className="flex items-center hover:bg-overlay-hover overflow-hidden p-2 rounded-sm"
-                    >
-                      <Checkbox
-                        id="all-functions"
-                        checked={tempFunctionIds.length === 0}
-                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                          if (event.target.checked) {
-                            setTempFunctionIds([])
-                          }
-                        }}
-                      />
-                      <div className="flex flex-col">
-                        <span>All Functions</span>
-                      </div>
-                    </Label>
-                    {functions
-                      ?.filter((fn: { slug: string }) => typeof fn.slug === 'string')
-                      .map((fn) => (
-                        <Label
-                          key={fn.id}
-                          htmlFor={fn.id}
-                          className="flex items-center hover:bg-overlay-hover overflow-hidden p-2 rounded-sm"
-                        >
-                          <div>
-                            <Checkbox
-                              id={fn.id}
-                              checked={tempFunctionIds.includes(fn.id)}
-                              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                                if (event.target.checked) {
-                                  setTempFunctionIds([...tempFunctionIds, fn.id])
-                                } else {
-                                  setTempFunctionIds(tempFunctionIds.filter((id) => id !== fn.id))
-                                }
-                              }}
-                            />
-                          </div>
-                          <div className="flex flex-col">
-                            <span>{fn.slug}</span>
-                          </div>
-                        </Label>
-                      ))}
-                  </div>
-                  <div className="flex items-center justify-end gap-2 border-t border-default p-2">
-                    <Button
-                      size="tiny"
-                      type="default"
-                      onClick={() => setIsOpen(false)}
-                      htmlType="button"
-                    >
-                      Cancel
-                    </Button>
-                    <Button
-                      size="tiny"
-                      type="primary"
-                      htmlType="button"
-                      onClick={() => {
-                        setFunctionIds(tempFunctionIds)
-                        setIsOpen(false)
-                      }}
-                    >
-                      Apply
-                    </Button>
-                  </div>
-                </DropdownMenuContent>
-              </DropdownMenu>
+            <div className="w-full flex items-center gap-2 flex-wrap">
+              <ReportsSelectFilter
+                label="Function"
+                options={
+                  functions?.map((fn: { name: string; id: string }) => ({
+                    label: fn.name,
+                    key: fn.id,
+                  })) ?? []
+                }
+                value={functionFilter}
+                onChange={setFunctionFilter}
+              />
+
+              <ReportsNumericFilter
+                label="Status Code"
+                value={statusCodeFilter}
+                onChange={setStatusCodeFilter}
+                defaultOperator="="
+              />
+
+              <ReportsNumericFilter
+                label="Execution Time"
+                value={executionTimeFilter}
+                onChange={setExecutionTimeFilter}
+                placeholder={`Enter time in ms`}
+                min={0}
+                max={99999}
+                defaultOperator=">="
+              />
+
+              <ReportsSelectFilter
+                label="Region"
+                options={EDGE_FUNCTION_REGIONS.map((region) => ({
+                  key: region.key,
+                  label: `${region.key} - ${region.label}`,
+                }))}
+                value={regionFilter}
+                onChange={setRegionFilter}
+              />
             </div>
+            {/* <pre>{JSON.stringify(filters, null, 2)}</pre> */}
           </div>
         }
       >
@@ -236,8 +206,8 @@ const EdgeFunctionsUsage = () => {
                   startDate={selectedDateRange?.period_start?.date}
                   endDate={selectedDateRange?.period_end?.date}
                   updateDateRange={updateDateRange}
-                  functionIds={functionIds}
                   syncId={chartSyncId}
+                  filters={filters}
                 />
               ))}
         </div>
