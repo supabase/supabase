49fdaedcacb0b5033e2e3a1aeb0a83baefbada94
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2025-09-02 10:58:28 -0700

    use ph flag instead of config cat for new homepage rollout

--- apps/studio/pages/project/[ref]/index.tsx
+++ apps/studio/pages/project/[ref]/index.tsx
@@ -1,4 +1,5 @@
 import { useFlag } from 'common'
+import { usePHFlag } from 'hooks/ui/useFlag'
 import DefaultLayout from 'components/layouts/DefaultLayout'
 import { ProjectLayoutWithAuth } from 'components/layouts/ProjectLayout/ProjectLayout'
 import type { NextPageWithLayout } from 'types'
@@ -6,7 +7,7 @@ import HomeNew from 'components/interfaces/HomeNew/Home'
 import Home from 'components/interfaces/Home/Home'
 
 const HomePage: NextPageWithLayout = () => {
-  const isHomeNew = useFlag('homeNew')
+  const isHomeNew = usePHFlag('homeNew')
   if (isHomeNew) {
     return <HomeNew />
   }
