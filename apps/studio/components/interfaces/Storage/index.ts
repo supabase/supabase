718a7c19a7bf3febc9a38a9c0482c2c2cf8a32d2
Danny White <3104761+dnywh@users.noreply.github.com>
2025-09-02 12:22:03 +1000

    fix: restore imports

--- apps/studio/components/interfaces/Storage/index.ts
+++ apps/studio/components/interfaces/Storage/index.ts
@@ -1,3 +1,11 @@
+// Main Storage components
+export { default as StorageExplorer } from './StorageExplorer/StorageExplorer'
+export { default as StoragePolicies } from './StoragePolicies/StoragePolicies'
+export { default as StorageSettings } from './StorageSettings/StorageSettings'
+
+// Bucket modals
+export { DeleteBucketModal } from './DeleteBucketModal'
+
 // Unified bucket modal components
 export { BucketModal, CreateBucketModal, EditBucketModal } from './BucketModal'
 
