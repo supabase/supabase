5d6ca54a3db8f860acb2529f41f78ae6926667ec
Saxon Fletcher <saxonafletcher@gmail.com>
2025-09-03 14:45:39 +1000

    style fix

--- apps/studio/components/interfaces/Settings/Addons/Addons.tsx
+++ apps/studio/components/interfaces/Settings/Addons/Addons.tsx
@@ -1,27 +1,21 @@
-import dayjs from 'dayjs'
 import { AlertCircle, Info } from 'lucide-react'
 import { useTheme } from 'next-themes'
 import Link from 'next/link'
-import { useMemo } from 'react'
 
 import { useFlag, useParams } from 'common'
 import {
   getAddons,
   subscriptionHasHipaaAddon,
 } from 'components/interfaces/Billing/Subscription/Subscription.utils'
-import ProjectUpdateDisabledTooltip from 'components/interfaces/Organization/BillingSettings/ProjectUpdateDisabledTooltip'
 import { useIsProjectActive } from 'components/layouts/ProjectLayout/ProjectContext'
-import { ScaffoldContainer, ScaffoldDivider, ScaffoldSection } from 'components/layouts/Scaffold'
+import { ScaffoldContainer, ScaffoldSection } from 'components/layouts/Scaffold'
 import AlertError from 'components/ui/AlertError'
-import { ButtonTooltip } from 'components/ui/ButtonTooltip'
 import { HorizontalShimmerWithIcon } from 'components/ui/Shimmers/Shimmers'
 import { ResourceItem } from 'components/ui/Resource/ResourceItem'
 import { ResourceList } from 'components/ui/Resource/ResourceList'
-import { useInfraMonitoringQuery } from 'data/analytics/infra-monitoring-query'
 import { useProjectSettingsV2Query } from 'data/config/project-settings-v2-query'
 import { useOrgSubscriptionQuery } from 'data/subscriptions/org-subscription-query'
 import { useProjectAddonsQuery } from 'data/subscriptions/project-addons-query'
-import type { ProjectAddonVariantMeta } from 'data/subscriptions/types'
 import { useIsFeatureEnabled } from 'hooks/misc/useIsFeatureEnabled'
 import { useSelectedOrganizationQuery } from 'hooks/misc/useSelectedOrganization'
 import {
@@ -29,8 +23,7 @@ import {
   useProjectByRefQuery,
   useSelectedProjectQuery,
 } from 'hooks/misc/useSelectedProject'
-import { getCloudProviderArchitecture } from 'lib/cloudprovider-utils'
-import { BASE_PATH, INSTANCE_MICRO_SPECS, INSTANCE_NANO_SPECS } from 'lib/constants'
+import { BASE_PATH } from 'lib/constants'
 import { getDatabaseMajorVersion, getSemanticVersion } from 'lib/helpers'
 import { useAddonsPagePanel } from 'state/addons-page'
 import {
@@ -70,26 +63,12 @@ export const Addons = () => {
 
   const hasHipaaAddon = subscriptionHasHipaaAddon(subscription) && settings?.is_sensitive
 
-  const cpuArchitecture = getCloudProviderArchitecture(selectedProject?.cloud_provider)
   // Only projects of version greater than supabase-postgrest-14.1.0.44 can use PITR
   const sufficientPgVersion =
     // introduced as generatedSemantic version could be < 141044 even if actual version is indeed past it
     // eg. 15.1.1.2 leads to 15112
     getDatabaseMajorVersion(selectedProject?.dbVersion ?? '') > 14 ||
     getSemanticVersion(selectedProject?.dbVersion ?? '') >= 141044
-
-  // [Joshen] We could possibly look into reducing the interval to be more "realtime"
-  // I tried setting the interval to 1m but no data was returned, may need to experiment
-  const startDate = useMemo(() => dayjs().subtract(15, 'minutes').millisecond(0).toISOString(), [])
-  const endDate = useMemo(() => dayjs().millisecond(0).toISOString(), [])
-  const { data: ioBudgetData } = useInfraMonitoringQuery({
-    projectRef,
-    attribute: 'disk_io_budget',
-    interval: '5m',
-    startDate,
-    endDate,
-  })
-  const [mostRecentRemainingIOBudget] = (ioBudgetData?.data ?? []).slice(-1)
 
   const {
     data: addons,
@@ -99,19 +78,7 @@ export const Addons = () => {
     isSuccess,
   } = useProjectAddonsQuery({ projectRef })
   const selectedAddons = addons?.selected_addons ?? []
-  const { computeInstance, pitr, customDomain, ipv4 } = getAddons(selectedAddons)
-
-  const meta = useMemo(() => {
-    const computeMeta = computeInstance?.variant?.meta as ProjectAddonVariantMeta | undefined
-
-    if (!computeMeta && selectedProject?.infra_compute_size === 'nano') {
-      return INSTANCE_NANO_SPECS
-    } else if (selectedProject?.infra_compute_size === 'micro') {
-      return INSTANCE_MICRO_SPECS
-    }
-
-    return computeMeta
-  }, [selectedProject, computeInstance])
+  const { pitr, customDomain, ipv4 } = getAddons(selectedAddons)
 
   const canUpdateIPv4 = settings?.db_ip_addr_config === 'ipv6'
 
@@ -166,17 +133,6 @@ export const Addons = () => {
 
         {isSuccess && (
           <>
-            {selectedProject?.infra_compute_size === 'nano' && subscription?.plan.id !== 'free' && (
-              <Alert_Shadcn_ variant="default">
-                <Info strokeWidth={2} />
-                <AlertTitle_Shadcn_>Free compute upgrade to Micro</AlertTitle_Shadcn_>
-                <AlertDescription_Shadcn_>
-                  Paid Plans include a free upgrade to Micro compute. Your project is ready to
-                  upgrade for no additional charges.
-                </AlertDescription_Shadcn_>
-              </Alert_Shadcn_>
-            )}
-
             <ResourceList>
               {projectAddonsDedicatedIpv4Address && (
                 <ResourceItem
@@ -265,50 +221,43 @@ export const Addons = () => {
                   </div>
                 </div>
               </ResourceItem>
-
               {hasHipaaAddon ? (
-                <div className="px-6">
-                  <Alert_Shadcn_>
-                    <AlertTitle_Shadcn_>PITR cannot be changed with HIPAA</AlertTitle_Shadcn_>
-                    <AlertDescription_Shadcn_>
-                      All projects should have PITR enabled by default and cannot be changed with
-                      HIPAA enabled. Contact support for further assistance.
-                    </AlertDescription_Shadcn_>
-                    <div className="mt-4">
-                      <Button type="default" asChild>
-                        <Link href="/support/new">Contact support</Link>
-                      </Button>
-                    </div>
-                  </Alert_Shadcn_>
-                </div>
+                <Alert_Shadcn_ className="rounded-none border-0 border-b px-6">
+                  <AlertTitle_Shadcn_>PITR cannot be changed with HIPAA</AlertTitle_Shadcn_>
+                  <AlertDescription_Shadcn_>
+                    All projects should have PITR enabled by default and cannot be changed with
+                    HIPAA enabled. Contact support for further assistance.
+                  </AlertDescription_Shadcn_>
+                  <div className="mt-4">
+                    <Button type="default" asChild>
+                      <Link href="/support/new">Contact support</Link>
+                    </Button>
+                  </div>
+                </Alert_Shadcn_>
               ) : !sufficientPgVersion ? (
-                <div className="px-6">
-                  <Alert_Shadcn_ className="mt-2">
-                    <AlertTitle_Shadcn_>Your project is too old to enable PITR</AlertTitle_Shadcn_>
-                    <AlertDescription_Shadcn_>
-                      <p className="text-sm leading-normal mb-2">
-                        Reach out to us via support if you're interested
-                      </p>
-                      <Button asChild type="default">
-                        <Link
-                          className="text-link"
-                          href={`/support/new?projectRef=${projectRef}&category=sales&subject=Project%20too%20old%20old%20for%20PITR`}
-                        >
-                          <a>Contact support</a>
-                        </Link>
-                      </Button>
-                    </AlertDescription_Shadcn_>
-                  </Alert_Shadcn_>
-                </div>
+                <Alert_Shadcn_ className="rounded-none border-0 border-b px-6">
+                  <AlertTitle_Shadcn_>Your project is too old to enable PITR</AlertTitle_Shadcn_>
+                  <AlertDescription_Shadcn_>
+                    <p className="text-sm leading-normal mb-2">
+                      Reach out to us via support if you're interested
+                    </p>
+                    <Button asChild type="default">
+                      <Link
+                        className="text-link"
+                        href={`/support/new?projectRef=${projectRef}&category=sales&subject=Project%20too%20old%20old%20for%20PITR`}
+                      >
+                        <a>Contact support</a>
+                      </Link>
+                    </Button>
+                  </AlertDescription_Shadcn_>
+                </Alert_Shadcn_>
               ) : isOrioleDbInAws ? (
-                <div className="px-6">
-                  <Alert_Shadcn_ className="mt-2">
-                    <AlertTitle_Shadcn_>PITR not supported</AlertTitle_Shadcn_>
-                    <AlertDescription_Shadcn_>
-                      Point in time recovery is not supported with OrioleDB
-                    </AlertDescription_Shadcn_>
-                  </Alert_Shadcn_>
-                </div>
+                <Alert_Shadcn_ className="rounded-none border-0 border-b px-6">
+                  <AlertTitle_Shadcn_>PITR not supported</AlertTitle_Shadcn_>
+                  <AlertDescription_Shadcn_>
+                    Point in time recovery is not supported with OrioleDB
+                  </AlertDescription_Shadcn_>
+                </Alert_Shadcn_>
               ) : null}
 
               {projectSettingsCustomDomains && (
--- apps/studio/components/ui/Resource/ResourceItem.tsx
+++ apps/studio/components/ui/Resource/ResourceItem.tsx
@@ -24,7 +24,9 @@ export const ResourceItem = forwardRef<HTMLDivElement, ResourceItemProps>(
         {...props}
       >
         {media && (
-          <div className="text-foreground-light flex items-center justify-center">{media}</div>
+          <div className="hidden @lg:block text-foreground-light flex items-center justify-center">
+            {media}
+          </div>
         )}
         <div className="flex-1">{children}</div>
         {meta && <div>{meta}</div>}
