640c5c9e6fd03481a396ea666e1244ba1a09312f
taishikato <taishi.k0903@gmail.com>
2025-09-04 18:41:17 -0400

    Update HomePage to include ExplainPlanFlow component

--- apps/studio/pages/project/[ref]/index.tsx
+++ apps/studio/pages/project/[ref]/index.tsx
@@ -1,16 +1,36 @@
 import { useFlag } from 'common'
-import { Home } from 'components/interfaces/Home/Home'
+// import { Home } from 'components/interfaces/Home/Home'
 import { HomeV2 } from 'components/interfaces/HomeNew/Home'
 import DefaultLayout from 'components/layouts/DefaultLayout'
 import { ProjectLayoutWithAuth } from 'components/layouts/ProjectLayout/ProjectLayout'
 import type { NextPageWithLayout } from 'types'
+import { ExplainPlanFlow } from 'components/interfaces/Database'
+
+const sample = `[
+  {
+    "Plan": {
+      "Node Type": "Hash Join",
+      "Plans": [
+        { "Node Type": "Seq Scan" },
+        { "Node Type": "Bitmap Heap Scan", "Plans": [{ "Node Type": "Bitmap Index Scan" }] }
+      ]
+    }
+  }
+]`
 
 const HomePage: NextPageWithLayout = () => {
   const isHomeNew = useFlag('homeNew')
   if (isHomeNew) {
     return <HomeV2 />
   }
-  return <Home />
+  return (
+    <>
+      <div className="w-full h-[500px]">
+        <ExplainPlanFlow json={sample} />
+      </div>
+      {/* <Home /> */}
+    </>
+  )
 }
 
 HomePage.getLayout = (page) => (
