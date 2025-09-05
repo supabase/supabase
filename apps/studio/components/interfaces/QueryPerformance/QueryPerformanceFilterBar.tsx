e5063f8a48d229289b1450b4273c3ea42df79239
kemal <hello@kemal.earth>
2025-09-05 13:56:16 +0100

    feat: remove redundant sort by
    We had conflicting sort by dropdown which clashed with the individual column sorting itself. Either we do something similar to our table editor or leave it out for now. Opting to leave it out for now.

--- apps/studio/components/interfaces/QueryPerformance/QueryPerformanceFilterBar.tsx
+++ apps/studio/components/interfaces/QueryPerformance/QueryPerformanceFilterBar.tsx
@@ -1,4 +1,4 @@
-import { ArrowDown, ArrowUp, RefreshCw } from 'lucide-react'
+import { RefreshCw } from 'lucide-react'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
 
@@ -9,15 +9,7 @@ import { useDatabaseRolesQuery } from 'data/database-roles/database-roles-query'
 import { DbQueryHook } from 'hooks/analytics/useDbQuery'
 import { useLocalStorageQuery } from 'hooks/misc/useLocalStorage'
 import { useSelectedProjectQuery } from 'hooks/misc/useSelectedProject'
-import {
-  Button,
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuRadioGroup,
-  DropdownMenuRadioItem,
-  DropdownMenuTrigger,
-} from 'ui'
-import { QueryPerformanceSort } from '../Reports/Reports.queries'
+import { Button } from 'ui'
 import { TextSearchPopover } from './TextSearchPopover'
 
 export const QueryPerformanceFilterBar = ({
@@ -37,19 +29,12 @@ export const QueryPerformanceFilterBar = ({
 
   const defaultSearchQueryValue = router.query.search ? String(router.query.search) : ''
   const defaultFilterRoles = router.query.roles ? (router.query.roles as string[]) : []
-  const defaultSortByValue = router.query.sort
-    ? ({ column: router.query.sort, order: router.query.order } as QueryPerformanceSort)
-    : undefined
 
   const [searchInputVal, setSearchInputVal] = useState(defaultSearchQueryValue)
   const [filters, setFilters] = useState<{ roles: string[]; query: string }>({
     roles: typeof defaultFilterRoles === 'string' ? [defaultFilterRoles] : defaultFilterRoles,
     query: '',
   })
-  // [Joshen] This is for the old UI, can deprecated after
-  const [sortByValue, setSortByValue] = useState<QueryPerformanceSort>(
-    defaultSortByValue ?? { column: 'total_time', order: 'desc' }
-  )
 
   const { isLoading, isRefetching } = queryPerformanceQuery
   const { data, isLoading: isLoadingRoles } = useDatabaseRolesQuery({
@@ -75,19 +60,6 @@ export const QueryPerformanceFilterBar = ({
     router.push({ ...router, query: { ...router.query, roles } })
   }
 
-  function getSortButtonLabel() {
-    if (sortByValue?.order === 'desc') {
-      return 'Sorted by latency - high to low'
-    } else {
-      return 'Sorted by latency - low to high'
-    }
-  }
-
-  const onSortChange = (order: 'asc' | 'desc') => {
-    setSortByValue({ column: 'total_time', order })
-    router.push({ ...router, query: { ...router.query, sort: 'total_time', order } })
-  }
-
   return (
     <div className="px-6 py-2 bg-surface-200 border-t -mt-px flex justify-between items-center">
       <div className="flex items-center gap-x-4">
@@ -103,29 +75,6 @@ export const QueryPerformanceFilterBar = ({
           />
 
           <TextSearchPopover name="Query" value={searchInputVal} onSaveText={onSearchQueryChange} />
-
-          <div className="border-r border-strong h-6" />
-
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button icon={sortByValue?.order === 'desc' ? <ArrowDown /> : <ArrowUp />}>
-                {getSortButtonLabel()}
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-56">
-              <DropdownMenuRadioGroup
-                value={sortByValue?.order}
-                onValueChange={(value: any) => onSortChange(value)}
-              >
-                <DropdownMenuRadioItem value="desc" defaultChecked={sortByValue?.order === 'desc'}>
-                  Sort by latency - high to low
-                </DropdownMenuRadioItem>
-                <DropdownMenuRadioItem value="asc" defaultChecked={sortByValue?.order === 'asc'}>
-                  Sort by latency - low to high
-                </DropdownMenuRadioItem>
-              </DropdownMenuRadioGroup>
-            </DropdownMenuContent>
-          </DropdownMenu>
         </div>
       </div>
 
