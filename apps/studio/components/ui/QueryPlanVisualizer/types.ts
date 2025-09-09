4611ba7a300e92b0d30584e56225d4aa4f910496
taishikato <taishi.k0903@gmail.com>
2025-09-09 17:51:25 -0400

    Refactor plan types into separate file

---
+++ apps/studio/components/ui/QueryPlanVisualizer/types.ts
@@ -0,0 +1,153 @@
+export type RawPlan = {
+  // Basic Plan Node Properties
+  ['Node Type']?: string
+  ['Parallel Aware']?: boolean
+  ['Async Capable']?: boolean
+  ['Relation Name']?: string
+  Alias?: string
+  ['Join Type']?: string
+  // Cost Estimates
+  ['Startup Cost']?: number
+  ['Total Cost']?: number
+  ['Plan Rows']?: number
+  ['Plan Width']?: number
+  // Conditions and Filters
+  Filter?: string
+  ['Hash Cond']?: string
+  ['Index Cond']?: string
+  ['Recheck Cond']?: string
+  ['Merge Cond']?: string
+  ['Join Filter']?: string
+  // Plan Structure / Hierarchy
+  ['Parent Relationship']?: string
+  ['Scan Direction']?: string
+  ['Index Name']?: string
+  ['Order By']?: string
+  Plans?: RawPlan[]
+  // ANALYZE (actuals)
+  ['Actual Startup Time']?: number
+  ['Actual Total Time']?: number
+  ['Actual Rows']?: number
+  ['Actual Loops']?: number
+  ['Rows Removed by Filter']?: number
+  ['Rows Removed by Index Recheck']?: number
+  ['Heap Fetches']?: number
+  Output?: string[]
+  // BUFFERS
+  ['Shared Hit Blocks']?: number
+  ['Shared Read Blocks']?: number
+  ['Shared Dirtied Blocks']?: number
+  ['Shared Written Blocks']?: number
+  ['Local Hit Blocks']?: number
+  ['Local Read Blocks']?: number
+  ['Local Dirtied Blocks']?: number
+  ['Local Written Blocks']?: number
+  ['Temp Read Blocks']?: number
+  ['Temp Written Blocks']?: number
+  ['I/O Read Time']?: number
+  ['I/O Write Time']?: number
+  // Misc node-specific extras (optional but useful)
+  ['Sort Method']?: string
+  ['Sort Space Used']?: number
+  ['Sort Space Type']?: string
+  // Subplan/CTE names
+  ['Subplan Name']?: string
+  ['CTE Name']?: string
+}
+
+export type PlanRoot = { Plan: RawPlan }
+
+export type PlanMeta = {
+  planningTime?: number
+  executionTime?: number
+  jitTotalTime?: number
+  subplanRoots?: { name: string; id: string }[]
+  errorMessage?: string
+  errorDetail?: string
+}
+
+export type PlanNodeData = {
+  label: string
+  joinType?: string
+  startupCost?: number
+  totalCost?: number
+  planRows?: number
+  planWidth?: number
+  relationName?: string
+  alias?: string
+  filter?: string
+  hashCond?: string
+  indexCond?: string
+  recheckCond?: string
+  mergeCond?: string
+  joinFilter?: string
+  parallelAware?: boolean
+  asyncCapable?: boolean
+  parentRelationship?: string
+  scanDirection?: string
+  indexName?: string
+  orderBy?: string
+  // ANALYZE actuals
+  actualStartupTime?: number
+  actualTotalTime?: number
+  actualRows?: number
+  actualLoops?: number
+  // Estimation
+  estFactor?: number
+  estActualTotalRows?: number
+  rowsRemovedByFilter?: number
+  rowsRemovedByIndexRecheck?: number
+  heapFetches?: number
+  outputCols?: string[]
+  // BUFFERS
+  sharedHit?: number
+  sharedRead?: number
+  sharedDirtied?: number
+  sharedWritten?: number
+  localHit?: number
+  localRead?: number
+  localDirtied?: number
+  localWritten?: number
+  tempRead?: number
+  tempWritten?: number
+  ioReadTime?: number
+  ioWriteTime?: number
+  // Exclusive (derived)
+  exclusiveTimeMs?: number
+  exclusiveCost?: number
+  exSharedHit?: number
+  exSharedRead?: number
+  exSharedDirtied?: number
+  exSharedWritten?: number
+  exLocalHit?: number
+  exLocalRead?: number
+  exLocalDirtied?: number
+  exLocalWritten?: number
+  exTempRead?: number
+  exTempWritten?: number
+  // Misc
+  sortMethod?: string
+  sortSpaceUsed?: number
+  sortSpaceType?: string
+  // Raw JSON for detail panel
+  raw?: RawPlan
+  // Subplan/CTE context
+  subplanName?: string
+  cteName?: string
+  subplanOf?: string
+}
+
+export type Agg = {
+  timeIncl: number
+  costIncl: number
+  sharedHit: number
+  sharedRead: number
+  sharedDirtied: number
+  sharedWritten: number
+  localHit: number
+  localRead: number
+  localDirtied: number
+  localWritten: number
+  tempRead: number
+  tempWritten: number
+}
