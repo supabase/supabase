5bf9ff317ce6df7e17424c65be5648a5c505a24c
Alan Daniel <stylesshjs@gmail.com>
2025-09-09 08:51:54 -0400

    rm dot

--- apps/www/components/Hero/Hero.tsx
+++ apps/www/components/Hero/Hero.tsx
@@ -17,7 +17,7 @@ const Hero = () => {
               <div className="relative z-10 lg:h-auto pt-[90px] lg:pt-[90px] lg:min-h-[300px] flex flex-col items-center justify-center sm:mx-auto md:w-3/4 lg:mx-0 lg:w-full gap-4 lg:gap-8">
                 <AnnouncementBadge
                   url="/events/migrating-from-firebase-mobbin"
-                  announcement="Looking to migrate to Supabase? Sign up for our webinar and learn how."
+                  announcement="Looking to migrate to Supabase? Sign up for our webinar and learn how"
                 />
 
                 <div className="flex flex-col items-center">
