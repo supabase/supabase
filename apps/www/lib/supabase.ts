678acff40762653cc0e6946839cc17be4c8fb931
Joshen Lim <joshenlimek@gmail.com>
2025-09-10 11:28:22 +0800

    Fix TS

--- apps/www/lib/supabase.ts
+++ apps/www/lib/supabase.ts
@@ -1,20 +1,17 @@
 import { createClient } from '@supabase/supabase-js'
 import { Database } from './database.types'
 
-const supabase =
-  process.env.NEXT_PUBLIC_SUPABASE_URL && process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-    ? createClient<Database>(
-        process.env.NEXT_PUBLIC_SUPABASE_URL,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-        {
-          realtime: {
-            params: {
-              eventsPerSecond: 1000,
-            },
-          },
-        }
-      )
-    : undefined
+const supabase = createClient<Database>(
+  process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '',
+  {
+    realtime: {
+      params: {
+        eventsPerSecond: 1000,
+      },
+    },
+  }
+)
 
 export type SupabaseClient = typeof supabase
 
