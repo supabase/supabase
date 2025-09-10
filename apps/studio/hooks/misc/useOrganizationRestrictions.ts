a32dce392c8024b8da5bf455a8c835e1e9533156
kemal <hello@kemal.earth>
2025-09-10 12:51:10 +0100

    fix: rendering anchor inside button

--- apps/studio/hooks/misc/useOrganizationRestrictions.ts
+++ apps/studio/hooks/misc/useOrganizationRestrictions.ts
@@ -30,13 +30,7 @@ export function useOrganizationRestrictions() {
     (invoice) => invoice.organization_id === org?.id
   )
 
-  if (
-    org &&
-    org.plan.id !== 'free' &&
-    billingCustomer &&
-    !billingCustomer.address?.line1 &&
-    !org.billing_partner
-  ) {
+  if (true) {
     warnings.push({
       type: 'warning',
       title: RESTRICTION_MESSAGES.MISSING_BILLING_INFO.title,
