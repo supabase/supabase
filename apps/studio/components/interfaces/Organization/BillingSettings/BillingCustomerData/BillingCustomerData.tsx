7650926b77621fc619ace609e5e63fe122e74e3c
Joshen Lim <joshenlimek@gmail.com>
2025-09-10 18:09:18 +0800

    Chore/swap use check permissions with use async check project permissions part 7 (#38581)
    * Wrap up remaining swapping of useCheckPermissions
    
    * remove OrganizationSettingsLayout on billing, team, integrations and usage org pages
    
    * Fix missing loading state in usage page
    
    * Add comments for next step

--- apps/studio/components/interfaces/Organization/BillingSettings/BillingCustomerData/BillingCustomerData.tsx
+++ apps/studio/components/interfaces/Organization/BillingSettings/BillingCustomerData/BillingCustomerData.tsx
@@ -16,10 +16,7 @@ import { useOrganizationCustomerProfileQuery } from 'data/organizations/organiza
 import { useOrganizationCustomerProfileUpdateMutation } from 'data/organizations/organization-customer-profile-update-mutation'
 import { useOrganizationTaxIdQuery } from 'data/organizations/organization-tax-id-query'
 import { useOrganizationTaxIdUpdateMutation } from 'data/organizations/organization-tax-id-update-mutation'
-import {
-  useAsyncCheckProjectPermissions,
-  useCheckPermissions,
-} from 'hooks/misc/useCheckPermissions'
+import { useAsyncCheckProjectPermissions } from 'hooks/misc/useCheckPermissions'
 import { useSelectedOrganizationQuery } from 'hooks/misc/useSelectedOrganization'
 import { Button, Card, CardFooter, Form_Shadcn_ as Form } from 'ui'
 import {
@@ -33,9 +30,9 @@ export const BillingCustomerData = () => {
   const { slug } = useParams()
   const { data: selectedOrganization } = useSelectedOrganizationQuery()
 
-  const { isSuccess: isPermissionsLoaded, can: canReadBillingCustomerData } =
+  const { can: canReadBillingCustomerData, isSuccess: isPermissionsLoaded } =
     useAsyncCheckProjectPermissions(PermissionAction.BILLING_READ, 'stripe.customer')
-  const canUpdateBillingCustomerData = useCheckPermissions(
+  const { can: canUpdateBillingCustomerData } = useAsyncCheckProjectPermissions(
     PermissionAction.BILLING_WRITE,
     'stripe.customer'
   )
