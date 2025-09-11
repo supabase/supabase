0d1a56c806182cd0064c11f684709cbfd9b0fa50
Jordi Enric <jordi.err@gmail.com>
2025-09-11 14:39:41 +0200

    fix test

--- apps/studio/data/reports/v2/auth-report.test.tsx
+++ apps/studio/data/reports/v2/auth-report.test.tsx
@@ -22,22 +22,22 @@ describe('defaultAuthReportFormatter', () => {
   it('should format the data correctly with multiple attributes', () => {
     const data = {
       result: [
-        { timestamp: String(timestamp), count: 1 },
-        { timestamp: String(timestamp + 1), count: 2 },
+        { timestamp: String(timestamp), active_users: 1 },
+        { timestamp: String(timestamp + 1), sign_in_attempts: 2 },
       ],
     }
 
     const attributes = [
-      { attribute: 'ActiveUsers', provider: 'logs', label: 'Active Users', enabled: true },
-      { attribute: 'SignInAttempts', provider: 'logs', label: 'Sign In Attempts', enabled: true },
+      { attribute: 'active_users', label: 'Active Users' },
+      { attribute: 'sign_in_attempts', label: 'Sign In Attempts' },
     ]
 
     const result = defaultAuthReportFormatter(data, attributes)
 
     expect(result).toEqual({
       data: [
-        { timestamp: String(timestamp), ActiveUsers: 1, SignInAttempts: 1 },
-        { timestamp: String(timestamp + 1), ActiveUsers: 2, SignInAttempts: 2 },
+        { timestamp: String(timestamp), active_users: 1, sign_in_attempts: 0 },
+        { timestamp: String(timestamp + 1), active_users: 0, sign_in_attempts: 2 },
       ],
       chartAttributes: attributes,
     })
