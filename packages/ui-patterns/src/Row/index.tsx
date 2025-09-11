364c7f903aa9088d973e1d14e07a7ac59e7926c8
Joshen Lim <joshenlimek@gmail.com>
2025-09-11 16:55:31 +0800

    Nit

--- packages/ui-patterns/src/Row/index.tsx
+++ packages/ui-patterns/src/Row/index.tsx
@@ -1,13 +1,13 @@
 'use client'
 
+import { ChevronLeft, ChevronRight } from 'lucide-react'
 import type React from 'react'
+import type { ReactNode } from 'react'
 import { forwardRef, useEffect, useMemo, useRef, useState } from 'react'
 import { Button, cn } from 'ui'
-import type { ReactNode } from 'react'
-import { ChevronLeft, ChevronRight } from 'lucide-react'
 
 interface RowProps extends React.HTMLAttributes<HTMLDivElement> {
-  // columns can be a fixed number or an array [lg, md, sm]
+  /** columns can be a fixed number or an array [lg, md, sm] */
   columns: number | [number, number, number]
   children: ReactNode
   className?: string
