84fb6da41b332496dd92589d83d1894d23c3362a
Katerina Skroumpelou <sk.katherine@gmail.com>
2025-09-16 14:38:41 +0300

    chore: trigger the tests

--- apps/docs/app/error.tsx
+++ apps/docs/app/error.tsx
@@ -5,6 +5,8 @@ import Link from 'next/link'
 import { useEffect } from 'react'
 import { Button } from 'ui'
 
+console.log('trigger those tests')
+
 const ErrorPage = ({ error }) => {
   useEffect(() => {
     Sentry.captureException(error)
