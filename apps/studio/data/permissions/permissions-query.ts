7204ebfc2508e339883a2e3d6d8400d3188ea5e8
Jordi Enric <37541088+jordienr@users.noreply.github.com>
2025-09-16 11:17:01 +0200

    Update apps/studio/data/permissions/permissions-query.ts
    Co-authored-by: Charis <26616127+charislam@users.noreply.github.com>

--- apps/studio/data/permissions/permissions-query.ts
+++ apps/studio/data/permissions/permissions-query.ts
@@ -11,7 +11,7 @@ export type PermissionsResponse = Permission[]
 export async function getPermissions(signal?: AbortSignal) {
   const { data, error } = await get('/platform/profile/permissions', { signal })
   if (error) {
-    const statusCode = (error as any)?.code || 'unknown'
+    const statusCode = (!!error && typeof error === 'object' && (error as any).code) || 'unknown'
 
     // This is to avoid sending 4XX errors
     // But we still want to capture errors without a status code or 5XXs
