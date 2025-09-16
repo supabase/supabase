2aee62a9941911afce1f673c0681cdb2a18d7020
Joshen Lim <joshenlimek@gmail.com>
2025-09-16 23:37:39 +0800

    Add self-remediation for orphan prefixes and refactor delete objects to be a singular request

---
+++ apps/studio/data/storage/bucket-prefix-delete-mutation.ts
@@ -0,0 +1,35 @@
+import { executeSql } from 'data/sql/execute-sql-query'
+
+/**
+ * [Joshen] JFYI this is solely being used in storage-explorer.tsx and hence doesn't have a useMutation hook
+ * It's solely supposed to aid users to self-remediate a known issue with orphan prefixes so the condition to
+ * clean the prefix from the storage.prefixes is intentionally strict here (e.g only level 1, since we've only
+ * seen this bug happening at the top level folder)
+ */
+
+type DeleteBucketPrefixParams = {
+  projectRef?: string
+  connectionString?: string
+  bucketId?: string
+  prefix?: string
+}
+export const deleteBucketPrefix = async (
+  { projectRef, connectionString, bucketId, prefix }: DeleteBucketPrefixParams,
+  signal?: AbortSignal
+) => {
+  if (!projectRef) throw new Error('projectRef is required')
+  if (!connectionString) throw new Error('connectionString is required')
+  if (!bucketId) throw new Error('bucketId is required')
+  if (!prefix) throw new Error('prefix is required')
+
+  const sql = /* SQL */ `
+delete from storage.prefixes where bucket_id = '${bucketId}' and name = '${prefix}' and level = 1;
+`.trim()
+
+  const { result } = await executeSql(
+    { projectRef, connectionString, sql, queryKey: ['delete-bucket-prefix'] },
+    signal
+  )
+
+  return result
+}
