b4922199cd22f91da3da8d561d3c84484137f3ec
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2025-09-16 13:03:13 +0200

    unblock cms draft mode (#38409)
    * remove port from cms start script
    
    * address cors
    
    * 30s revalidation on blog index
    
    * fix types
    
    * remove duplicate cache strategy
    
    * disable graphql
    
    * fix cms build
    
    * fix ProductDropdown crash
    
    * fix env var turbo

--- apps/cms/src/app/(payload)/api/graphql-playground/route.ts
+++
@@ -1,7 +0,0 @@
-/* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
-/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
-import config from '@payload-config'
-import '@payloadcms/next/css'
-import { GRAPHQL_PLAYGROUND_GET } from '@payloadcms/next/routes'
-
-export const GET = GRAPHQL_PLAYGROUND_GET(config)
