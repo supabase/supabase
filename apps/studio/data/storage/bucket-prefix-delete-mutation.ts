1cff81e0c4e1589c0ee724a16c5aa2eb8843ac2f
Joshen Lim <joshenlimek@gmail.com>
2025-09-16 23:39:00 +0800

    Update comment

--- apps/studio/data/storage/bucket-prefix-delete-mutation.ts
+++ apps/studio/data/storage/bucket-prefix-delete-mutation.ts
@@ -3,8 +3,9 @@ import { executeSql } from 'data/sql/execute-sql-query'
 /**
  * [Joshen] JFYI this is solely being used in storage-explorer.tsx and hence doesn't have a useMutation hook
  * It's solely supposed to aid users to self-remediate a known issue with orphan prefixes so the condition to
- * clean the prefix from the storage.prefixes is intentionally strict here (e.g only level 1, since we've only
- * seen this bug happening at the top level folder)
+ * clean the prefix from the storage.prefixes is intentionally strict here (e.g only level 1)
+ *
+ * We can make this a bit more loose if needed, but ideal case is that this issue is addressed at the storage level
  */
 
 type DeleteBucketPrefixParams = {
