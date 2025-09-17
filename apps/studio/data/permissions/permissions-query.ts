506b20734fe84b2449cfa87da6e8b736041e7f66
Jordi Enric <37541088+jordienr@users.noreply.github.com>
2025-09-17 16:39:35 +0200

    Add sample rate to fetcher (#38785)
    * add sample rate to fetcher
    
    * 20
    
    * default to always capture and base sample rate of 20%
    
    * rm unnecessray type

--- apps/studio/data/permissions/permissions-query.ts
+++ apps/studio/data/permissions/permissions-query.ts
@@ -18,7 +18,6 @@ export async function getPermissions(signal?: AbortSignal) {
     // since those may require investigation if they spike
     const sendError = statusCode >= 500 || statusCode === 'unknown'
     handleError(error, {
-      alwaysCapture: sendError,
       sentryContext: {
         tags: {
           permissionsQuery: true,
