79bbc6c8d1adfbc2732eca1e42fdbbf73d9835e9
Danny White <3104761+dnywh@users.noreply.github.com>
2025-09-18 11:35:18 +1000

    polish

--- packages/ui-patterns/src/Banners/SelectBanner.tsx
+++ packages/ui-patterns/src/Banners/SelectBanner.tsx
@@ -2,30 +2,33 @@ import Link from 'next/link'
 
 export function SelectBanner() {
   const selectSiteUrl = 'https://select.supabase.com/'
-  const desc = ['Our first user conference', '3rd Oct 2025']
-  const descExtended = 'Guillermo Rauch, Dylan Field, and more'
+  const desc = [
+    'Our first user conference',
+    '3rd Oct 2025',
+    'Guillermo Rauch, Dylan Field, and more',
+  ]
   const cta = 'Save your seat'
 
   const baseStyles = 'flex flex-col justify-center border-l border-muted py-8 '
   const textBlockStyles =
     baseStyles +
-    'pr-8 text-xs font-mono uppercase leading-none tracking-wide text-white/50 [&_p]:mt-[3px]'
+    'pr-8 text-xs font-mono uppercase leading-none tracking-wide text-white/50 [&_p]:mt-[2px]'
 
   return (
     <div
       className="relative w-full flex items-center group justify-center text-foreground-contrast dark:text-white bg-black border-b border-muted transition-colors overflow-hidden"
       style={{
-        '--grid-color': 'hsl(var(--border-muted))',
+        '--line-color': 'hsl(var(--border-muted))',
         '--line-width': '1px',
         '--offset-from-top': '64px',
         '--line-spacing': '12px', // Match -3 used elsewhere
         backgroundImage: `
           /* Top horizontal line: offset from middle line by line spacing */
-          linear-gradient(to bottom, transparent 0, transparent calc(var(--offset-from-top) - var(--line-spacing)), var(--grid-color) calc(var(--offset-from-top) - var(--line-spacing)), var(--grid-color) calc(var(--offset-from-top) - var(--line-spacing) + var(--line-width)), transparent calc(var(--offset-from-top) - var(--line-spacing) + var(--line-width))),
+          linear-gradient(to bottom, transparent 0, transparent calc(var(--offset-from-top) - var(--line-spacing)), var(--line-color) calc(var(--offset-from-top) - var(--line-spacing)), var(--line-color) calc(var(--offset-from-top) - var(--line-spacing) + var(--line-width)), transparent calc(var(--offset-from-top) - var(--line-spacing) + var(--line-width))),
           /* Middle horizontal line */
-          linear-gradient(to bottom, transparent 0, transparent var(--offset-from-top), var(--grid-color) var(--offset-from-top), var(--grid-color) calc(var(--offset-from-top) + var(--line-width)), transparent calc(var(--offset-from-top) + var(--line-width))),
+          linear-gradient(to bottom, transparent 0, transparent var(--offset-from-top), var(--line-color) var(--offset-from-top), var(--line-color) calc(var(--offset-from-top) + var(--line-width)), transparent calc(var(--offset-from-top) + var(--line-width))),
           /* Bottom horizontal line: offset from middle line by line spacing */
-          linear-gradient(to bottom, transparent 0, transparent calc(var(--offset-from-top) + var(--line-spacing)), var(--grid-color) calc(var(--offset-from-top) + var(--line-spacing)), var(--grid-color) calc(var(--offset-from-top) + var(--line-spacing) + var(--line-width)), transparent calc(var(--offset-from-top) + var(--line-spacing) + var(--line-width)))
+          linear-gradient(to bottom, transparent 0, transparent calc(var(--offset-from-top) + var(--line-spacing)), var(--line-color) calc(var(--offset-from-top) + var(--line-spacing)), var(--line-color) calc(var(--offset-from-top) + var(--line-spacing) + var(--line-width)), transparent calc(var(--offset-from-top) + var(--line-spacing) + var(--line-width)))
         `,
         backgroundSize: '100% 100%',
         backgroundRepeat: 'no-repeat',
@@ -47,17 +50,64 @@ export function SelectBanner() {
         <div className={`${textBlockStyles} hidden sm:flex`}>
           <p>{desc[1]}</p>
         </div>
-        <div className={`${textBlockStyles} hidden lg:flex`}>
-          <p>{descExtended}</p>
+        <div className={`${textBlockStyles} hidden xl:flex`}>
+          <p>{desc[2]}</p>
         </div>
 
         <div className="flex flex-col justify-center -px-4 border-x border-muted border-dashed relative after:absolute after:top-0 after:left-full after:w-screen after:h-full after:bg-black after:-z-10">
           <Link
             target="_blank"
             href={selectSiteUrl}
-            className="relative before:absolute before:inset-0 before:bg-black before:-z-10 px-4 py-1 h-10 flex items-center justify-center bg-brand/20 hover:bg-brand/50 outline-1 outline-dashed outline-brand/40 hover:outline-brand text-sm text-white font-medium transition-all duration-300"
+            className="relative before:absolute before:inset-0 before:bg-black before:-z-10 px-4 py-1 h-10 flex items-center justify-center bg-brand-600/20 hover:bg-brand-600/50 outline-1 outline-dashed outline-brand-600/40 hover:outline-brand-600 text-sm text-white font-medium transition-all duration-200"
           >
             {cta}
+            {/* Crosshairs */}
+            <div className="absolute pointer-events-none inset-0 z-10">
+              {/* Top-left crosshair */}
+              <div className="absolute top-0 left-0">
+                <div
+                  className="absolute -left-px -top-[3px] w-px h-[5px]"
+                  style={{ backgroundColor: 'hsl(var(--brand-600))' }}
+                />
+                <div
+                  className="absolute -left-[3px] -top-px w-[5px] h-px"
+                  style={{ backgroundColor: 'hsl(var(--brand-600))' }}
+                />
+              </div>
+              {/* Top-right crosshair */}
+              <div className="absolute top-0 right-0">
+                <div
+                  className="absolute -right-px -top-[3px] w-px h-[5px]"
+                  style={{ backgroundColor: 'hsl(var(--brand-600))' }}
+                />
+                <div
+                  className="absolute -right-[3px] -top-px w-[5px] h-px"
+                  style={{ backgroundColor: 'hsl(var(--brand-600))' }}
+                />
+              </div>
+              {/* Bottom-left crosshair */}
+              <div className="absolute bottom-0 left-0">
+                <div
+                  className="absolute -left-px -bottom-[3px] w-px h-[5px]"
+                  style={{ backgroundColor: 'hsl(var(--brand-600))' }}
+                />
+                <div
+                  className="absolute -left-[3px] -bottom-px w-[5px] h-px"
+                  style={{ backgroundColor: 'hsl(var(--brand-600))' }}
+                />
+              </div>
+              {/* Bottom-right crosshair */}
+              <div className="absolute bottom-0 right-0">
+                <div
+                  className="absolute -right-px -bottom-[3px] w-px h-[5px]"
+                  style={{ backgroundColor: 'hsl(var(--brand-600))' }}
+                />
+                <div
+                  className="absolute -right-[3px] -bottom-px w-[5px] h-px"
+                  style={{ backgroundColor: 'hsl(var(--brand-600))' }}
+                />
+              </div>
+            </div>
           </Link>
         </div>
       </div>
