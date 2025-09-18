a6463131b497f788433967e381fc9eb57d8d7927
kemal <hello@kemal.earth>
2025-09-18 13:27:45 +0100

    style: expand size of sheet

--- apps/studio/components/interfaces/QueryPerformance/QueryPerformanceGrid.tsx
+++ apps/studio/components/interfaces/QueryPerformance/QueryPerformanceGrid.tsx
@@ -421,7 +421,7 @@ export const QueryPerformanceGrid = ({ queryPerformanceQuery }: QueryPerformance
       />
 
       <Sheet open={selectedRow !== undefined} onOpenChange={() => setSelectedRow(undefined)}>
-        <SheetContent side="right" className="flex flex-col h-full bg-studio border-l !w-[500px]">
+        <SheetContent side="right" className="flex flex-col h-full bg-studio border-l !w-[580px]">
           <Tabs_Shadcn_
             value={view}
             className="flex flex-col h-full"
