d27b1cee94067112b3ce4937f55bdf874720eb1a
Charis Lam <26616127+charislam@users.noreply.github.com>
2025-09-18 16:47:41 -0400

    refactor: minor changes to safeHistoryReplaceState

--- apps/docs/lib/historyUtils.ts
+++ apps/docs/lib/historyUtils.ts
@@ -1,21 +1,13 @@
 import { debounce } from 'lodash-es'
 
-let lastUrl = ''
-
-const debouncedHistoryReplaceState = debounce((url: string) => {
+export const safeHistoryReplaceState = debounce((url: string) => {
   if (typeof window === 'undefined') return
 
-  if (url === lastUrl) return
+  if (url === window.location.href) return
 
   try {
     window.history.replaceState(null, '', url)
-    lastUrl = url
   } catch (error) {
     console.warn('Failed to call history.replaceState:', error)
   }
 }, 120)
-
-export const safeHistoryReplaceState = (url: string): void => {
-  const normalizedUrl = url.toString()
-  debouncedHistoryReplaceState(normalizedUrl)
-}
