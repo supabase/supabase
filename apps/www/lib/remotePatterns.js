da16e670a0d30542cca5240c0f58a6cdf283c07c
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2025-09-19 12:33:43 +0200

    use correct cms site origin (#38859)

--- apps/www/lib/remotePatterns.js
+++ apps/www/lib/remotePatterns.js
@@ -1,6 +1,6 @@
 const CMS_SITE_ORIGIN =
   process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
-    ? 'https://supabase.com'
+    ? 'https://cms.supabase.com'
     : process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL &&
         typeof process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL === 'string'
       ? `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL?.replace('zone-www-dot-com-git-', 'cms-git-')}`
