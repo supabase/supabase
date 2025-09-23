dc5ab444df7b661c2f2c20ab2f1dba4c725fd642
Ivan Vasilov <vasilov.ivan@gmail.com>
2025-09-23 12:34:56 +0300

    feat: Add typechecking as a required check for the merge queue (#38939)
    * Disable typesafe builds on prod for studio.
    
    * Add typecheck github action to the merge queue checks.
    
    * Add typescript dependency to packages to stabilize their versions.
    
    * Add tailwindcss to the catalog.

--- apps/studio/next.config.js
+++ apps/studio/next.config.js
@@ -531,10 +531,8 @@ const nextConfig = {
     pagesBufferLength: 100,
   },
   typescript: {
-    // On previews, typechecking is run via GitHub Action only for efficiency
-    // On production, we turn it on to prevent errors from conflicting PRs getting into
-    // prod
-    ignoreBuildErrors: process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? false : true,
+    // Typechecking is run via GitHub Action only for efficiency.
+    ignoreBuildErrors: true,
   },
   eslint: {
     // We are already running linting via GH action, this will skip linting during production build on Vercel
