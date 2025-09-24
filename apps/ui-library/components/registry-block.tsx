d1a1170a3097584e03359fcb1d73c610d6b92834
kallebysantos <kalleby_santos@hotmail.com>
2025-09-24 15:01:33 +0100

    to-refactor: changing imports to be relative
    TODO: remove hardcoded path string

--- apps/ui-library/components/registry-block.tsx
+++ apps/ui-library/components/registry-block.tsx
@@ -1,11 +1,12 @@
 'use server'
 
-import { generateRegistryTree } from '@/lib/process-registry'
+import { generateRegistryTree } from '../lib/process-registry';//'@/lib/process-registry'
 import path from 'path'
 import { BlockItemCode } from './block-item-code'
 
 export async function RegistryBlock({ itemName }: { itemName: string }) {
-  const registryPath = path.join(process.cwd(), 'public', 'r', `${itemName}.json`)
+  //const registryPath = path.join(process.cwd(), 'public', 'r', `${itemName}.json`)
+  const registryPath = path.join('/Users/kallebysantos/projects/community/supabase/supabase/apps/ui-library', 'public', 'r', `${itemName}.json`)
   const tree = generateRegistryTree(registryPath)
 
   return <BlockItemCode files={tree} />
