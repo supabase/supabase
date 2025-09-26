25fb60cfc89b27858bcd9c068dfca2a4b7701f8c
Devanshu Sharma <devanshusharma658@gmail.com>
2025-09-26 16:48:25 +0530

    file delete

--- apps/docs/components/Navigation/NavigationMenu/navigationDataTransform.ts
+++ apps/docs/components/Navigation/NavigationMenu/navigationDataTransform.ts
@@ -1,5 +1,1 @@
-import type { NavMenuSection, NavMenuGroup } from '../Navigation.types'
 
-export function transformNavMenuToNavSections(navGroups: NavMenuGroup[]): NavMenuSection[] {
-  return navGroups.flatMap((group) => group.items.filter((item) => item.enabled !== false))
-}
