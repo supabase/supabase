09cf0401572a7b0a44854237d13d05f021e36b89
Matt Rossman <22670878+mattrossman@users.noreply.github.com>
2025-09-26 11:50:47 -0400

    feat: add `crypto-js`, `encryptString` with sample key

--- apps/studio/lib/api/self-hosted/util.ts
+++ apps/studio/lib/api/self-hosted/util.ts
@@ -1,4 +1,7 @@
 import { IS_PLATFORM } from 'lib/constants'
+import crypto from 'crypto-js'
+
+const ENCRYPTION_KEY = 'SAMPLE_KEY' // See postgres-meta `CRYPTO_KEY` fallback
 
 /**
  * Asserts that the current environment is self-hosted.
@@ -7,4 +10,8 @@ export function assertSelfHosted() {
   if (IS_PLATFORM) {
     throw new Error('This function can only be called in self-hosted environments')
   }
+}
+
+export function encryptString(stringToEncrypt: string): string {
+  return crypto.AES.encrypt(stringToEncrypt, ENCRYPTION_KEY).toString()
 }
