775a31f26e836cfbf39e5810c076292b1756aa23
Eduardo Gurgel Pinho <eduardo.gurgel@supabase.io>
2025-10-01 21:10:22 +1300

    feat(ui library): improve realtime cursors joining, leaving and disconnecting
    Presence is used now to react to cursors joining and leaving.
    
    * When a cursor joins all the other cursors broadcast their last
    position (if available). This avoids an empty page when joining if cursors are not
    moving
    * When a cursor leaves the room their respective cursor is removed
    
    And finally the cursor position is broadcasted only the channel is
    successfully subscribed. Otherwise it might try to send POST requests to
    broadcast outside the websocket

--- apps/ui-library/registry/default/blocks/realtime-cursor/hooks/use-realtime-cursors.ts
+++ apps/ui-library/registry/default/blocks/realtime-cursor/hooks/use-realtime-cursors.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@/registry/default/clients/nextjs/lib/supabase/client'
-import { RealtimeChannel } from '@supabase/supabase-js'
+import { RealtimeChannel, REALTIME_SUBSCRIBE_STATES } from '@supabase/supabase-js'
 import { useCallback, useEffect, useRef, useState } from 'react'
 
 /**
@@ -70,6 +70,7 @@ export const useRealtimeCursors = ({
   const [color] = useState(generateRandomColor())
   const [userId] = useState(generateRandomNumber())
   const [cursors, setCursors] = useState<Record<string, CursorEventPayload>>({})
+  const cursorPayload = useRef<CursorEventPayload | null>(null)
 
   const channelRef = useRef<RealtimeChannel | null>(null)
 
@@ -90,6 +91,8 @@ export const useRealtimeCursors = ({
         timestamp: new Date().getTime(),
       }
 
+      cursorPayload.current = payload
+
       channelRef.current?.send({
         type: 'broadcast',
         event: EVENT_NAME,
@@ -106,6 +109,28 @@ export const useRealtimeCursors = ({
     channelRef.current = channel
 
     channel
+      .on('presence', { event: 'leave' }, ({ leftPresences }) => {
+        leftPresences.forEach(function(element) {
+          // Remove cursor when user leaves
+          setCursors((prev) => {
+            if (prev[element.key]) {
+              delete prev[element.key]
+            }
+
+            return {...prev}
+          })
+        })
+      })
+      .on('presence', { event: 'join' }, () => {
+        if (!cursorPayload.current) return
+
+        // All cursors broadcast their position when a new cursor joins
+        channelRef.current?.send({
+          type: 'broadcast',
+          event: EVENT_NAME,
+          payload: cursorPayload.current,
+        })
+      })
       .on('broadcast', { event: EVENT_NAME }, (data: { payload: CursorEventPayload }) => {
         const { user } = data.payload
         // Don't render your own cursor
@@ -122,22 +147,21 @@ export const useRealtimeCursors = ({
           }
         })
       })
-      .subscribe()
+      .subscribe(async (status) => {
+        if (status === REALTIME_SUBSCRIBE_STATES.SUBSCRIBED) {
+          await channel.track({ key: userId })
+          window.addEventListener('mousemove', handleMouseMove)
+        } else {
+          setCursors({})
+          window.removeEventListener('mousemove', handleMouseMove)
+        }
+      })
 
     return () => {
+      window.removeEventListener('mousemove', handleMouseMove)
       channel.unsubscribe()
     }
   }, [])
-
-  useEffect(() => {
-    // Add event listener for mousemove
-    window.addEventListener('mousemove', handleMouseMove)
-
-    // Cleanup on unmount
-    return () => {
-      window.removeEventListener('mousemove', handleMouseMove)
-    }
-  }, [handleMouseMove])
 
   return { cursors }
 }
