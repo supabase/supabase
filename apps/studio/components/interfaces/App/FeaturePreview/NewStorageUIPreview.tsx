7dd6330d7bb0d5b5b23ddbc848bba90a55012b18
Danny White <3104761+dnywh@users.noreply.github.com>
2025-10-01 11:15:53 +1000

    chore: improve feature preview description

--- apps/studio/components/interfaces/App/FeaturePreview/NewStorageUIPreview.tsx
+++ apps/studio/components/interfaces/App/FeaturePreview/NewStorageUIPreview.tsx
@@ -1,13 +1,31 @@
+import { BASE_PATH } from 'lib/constants'
+import Image from 'next/image'
+
 export const NewStorageUIPreview = () => {
   return (
-    <div>
-      <p className="text-sm text-foreground-light mb-4">Add some nice description here</p>
-      <p>Some nice image here</p>
+    <div className="text-sm text-foreground-light">
+      <p className=" mb-4">
+        Experience our enhanced Storage interface with support for analytics and vector bucket
+        types.
+      </p>
+      <Image
+        alt="new-storage-preview"
+        src={`${BASE_PATH}/img/previews/new-storage-preview.png`}
+        width={1160}
+        height={658}
+        className="rounded border mb-4"
+      />
       <div className="space-y-2 !mt-4">
-        <p className="text-sm">Enabling this preview will:</p>
-        <ul className="list-disc pl-6 text-sm text-foreground-light space-y-1">
-          <li>Specify what will happen</li>
+        <p>Enabling this preview will:</p>
+        <ul className="list-disc pl-6  space-y-1">
+          <li>Move Storage buckets from the sidebar into the main content area</li>
+          <li>Change the role of the sidebar to a bucket type selector</li>
+          <li>Nest settings and policies under their respective bucket types</li>
         </ul>
+        <p>
+          These changes are necessary to support incoming analytics and vector bucket types. File
+          storage will remain the default, and be shown by default when entering Storage.
+        </p>
       </div>
     </div>
   )
