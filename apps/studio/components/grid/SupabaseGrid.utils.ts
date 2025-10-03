e0240c59d64c1aec9e1c6a0b772f23ffa3da76d4
matlin <matlin@users.noreply.github.com>
2025-10-02 19:10:06 -0500

    fix: upgrade nuqs to properly support url param arrays for sorts and filters (#39214)
    * upgrade nuqs to get proper native url array params
    
    * use next useSearchParams because nuqs is stale

--- apps/studio/components/grid/SupabaseGrid.utils.ts
+++ apps/studio/components/grid/SupabaseGrid.utils.ts
@@ -1,17 +1,17 @@
 import AwesomeDebouncePromise from 'awesome-debounce-promise'
-import { compact, filter } from 'lodash'
-import { useEffect, useState } from 'react'
+import { compact } from 'lodash'
+import { useEffect, useMemo } from 'react'
 import { CalculatedColumn, CellKeyboardEvent } from 'react-data-grid'
 
 import type { Filter, SavedState } from 'components/grid/types'
 import { Entity, isTableLike } from 'data/table-editor/table-editor-types'
-import { useUrlState } from 'hooks/ui/useUrlState'
 import { copyToClipboard } from 'ui'
 import { FilterOperatorOptions } from './components/header/filter/Filter.constants'
 import { STORAGE_KEY_PREFIX } from './constants'
 import type { Sort, SupaColumn, SupaTable } from './types'
 import { formatClipboardValue } from './utils/common'
-import { parseAsArrayOf, parseAsBoolean, parseAsString, useQueryStates } from 'nuqs'
+import { parseAsNativeArrayOf, parseAsBoolean, parseAsString, useQueryStates } from 'nuqs'
+import { useSearchParams } from 'next/navigation'
 
 export const LOAD_TAB_FROM_CACHE_PARAM = 'loadFromCache'
 
@@ -197,16 +197,25 @@ export function useSyncTableEditorStateFromLocalStorageWithUrl({
   projectRef: string | undefined
   table: Entity | undefined
 }) {
-  const [urlParams, updateUrlParams] = useQueryStates(
+  // Warning: nuxt url state often fails to update to changes to URL
+  const [, updateUrlParams] = useQueryStates(
     {
-      sort: parseAsArrayOf(parseAsString).withDefault([]),
-      filter: parseAsArrayOf(parseAsString).withDefault([]),
+      sort: parseAsNativeArrayOf(parseAsString),
+      filter: parseAsNativeArrayOf(parseAsString),
       [LOAD_TAB_FROM_CACHE_PARAM]: parseAsBoolean.withDefault(false),
     },
     {
       history: 'replace',
     }
   )
+  // Use nextjs useSearchParams to get the latest URL params
+  const searchParams = useSearchParams()
+  const urlParams = useMemo(() => {
+    const sort = searchParams.getAll('sort')
+    const filter = searchParams.getAll('filter')
+    const loadFromCache = !!searchParams.get(LOAD_TAB_FROM_CACHE_PARAM)
+    return { sort, filter, loadFromCache }
+  }, [searchParams])
 
   useEffect(() => {
     if (!projectRef || !table) {
