a08d7de026a912e7bb43016e000e543cce9832d0
Danny White <3104761+dnywh@users.noreply.github.com>
2025-10-03 12:42:17 +1000

    fix: prevent loading of table for zero buckets

--- apps/studio/pages/project/[ref]/storage/files/index.tsx
+++ apps/studio/pages/project/[ref]/storage/files/index.tsx
@@ -3,10 +3,12 @@ import { useEffect } from 'react'
 
 import { useParams } from 'common'
 import { useIsNewStorageUIEnabled } from 'components/interfaces/App/FeaturePreview/FeaturePreviewContext'
+import { EmptyBucketState } from 'components/interfaces/Storage/EmptyBucketState'
 import { FilesBuckets } from 'components/interfaces/Storage/FilesBuckets'
 import DefaultLayout from 'components/layouts/DefaultLayout'
 import { StorageFilesLayout } from 'components/layouts/StorageLayout/StorageFilesLayout'
 import StorageLayout from 'components/layouts/StorageLayout/StorageLayout'
+import { useBucketsQuery } from 'data/storage/buckets-query'
 import type { NextPageWithLayout } from 'types'
 
 const StorageFilesPage: NextPageWithLayout = () => {
@@ -14,9 +16,21 @@ const StorageFilesPage: NextPageWithLayout = () => {
   const router = useRouter()
   const isStorageV2 = useIsNewStorageUIEnabled()
 
+  const { data: buckets = [], isLoading } = useBucketsQuery({ projectRef: ref })
+  const filesBuckets = buckets.filter((bucket) => !('type' in bucket) || bucket.type === 'STANDARD')
+
   useEffect(() => {
     if (!isStorageV2) router.replace(`/project/${ref}/storage`)
   }, [isStorageV2, ref, router])
+
+  if (!isLoading && filesBuckets.length === 0) {
+    return <EmptyBucketState bucketType="files" />
+  }
+
+  if (isLoading) {
+    // Prevent 1+ bucket layout from loading in case there are no buckets to load
+    return null
+  }
 
   return (
     <StorageFilesLayout>
