f5c6f86063dce03c246f647c26cb84cdc48a2fda
kemal <hello@kemal.earth>
2025-10-06 16:27:00 +0100

    feat: first draft of learn more section auth overview

--- apps/studio/components/interfaces/Auth/Overview/OverviewLearnMore.tsx
+++ apps/studio/components/interfaces/Auth/Overview/OverviewLearnMore.tsx
@@ -1,14 +1,105 @@
 import { ScaffoldSection, ScaffoldSectionTitle } from 'components/layouts/Scaffold'
-import { Card } from 'ui'
+import { Card, CardContent, CardHeader, CardTitle, Button } from 'ui'
+import Link from 'next/link'
+import { BookOpen } from 'lucide-react'
+import { Logs } from 'icons'
+import { AiIconAnimation } from 'ui'
 
 export const OverviewLearnMore = () => {
+  const LearnMoreCards = [
+    {
+      label: 'Docs',
+      title: 'Authentication documentation',
+      description: 'Read more on authentication and benefits of using Supabase policies.',
+      actions: [
+        {
+          label: 'Docs',
+          href: 'https://docs.supabase.com',
+          icon: <BookOpen />,
+        },
+      ],
+    },
+    {
+      label: 'Assistant',
+      title: 'Need help with authentication errors?',
+      description: 'Our Assistant can help you debug and fix authentication errors.',
+      actions: [
+        {
+          label: 'Ask Assistant',
+          onClick: () => {
+            console.log('Ask Assistant')
+          },
+          icon: <AiIconAnimation size={14} />,
+        },
+      ],
+    },
+    {
+      label: 'Logs',
+      title: 'Dive into our authentication logs',
+      description:
+        'Our authentication logs provide a deeper view into your auth requests and errors.',
+      actions: [
+        {
+          label: 'Go to logs',
+          href: '#',
+          icon: <Logs />,
+        },
+      ],
+    },
+  ]
+
   return (
     <ScaffoldSection isFullWidth>
       <ScaffoldSectionTitle className="mb-4">Learn more</ScaffoldSectionTitle>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 opacity-30 pointer-events-none">
-        <Card className="aspect-square" />
-        <Card className="aspect-square" />
-        <Card className="aspect-square" />
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
+        {LearnMoreCards.map((card) => (
+          <Card key={card.label} className="relative">
+            <CardHeader className="absolute top-0 left-0 right-0 border-b-0">
+              <CardTitle className="text-foreground-light">{card.label}</CardTitle>
+            </CardHeader>
+            <CardContent className="p-0">
+              <div className="bg-black/20 aspect-video flex w-full"></div>
+              <div className="p-4">
+                <div className="flex flex-col gap-1 mb-4">
+                  <h4>{card.title}</h4>
+                  <p className="text-sm text-foreground-lighter">{card.description}</p>
+                </div>
+                <div className="flex flex-col gap-2 items-start">
+                  {card.actions.map((action) => {
+                    if ('href' in action) {
+                      return (
+                        <Button
+                          key={action.label}
+                          className="inline-flex"
+                          type="default"
+                          icon={action.icon}
+                          asChild
+                        >
+                          <Link href={action.href} className="inline-flex">
+                            {action.label}
+                          </Link>
+                        </Button>
+                      )
+                    } else {
+                      return (
+                        <Button
+                          key={action.label}
+                          onClick={action.onClick}
+                          type="default"
+                          className="inline-flex"
+                          icon={action.icon}
+                        >
+                          {action.label}
+                        </Button>
+                      )
+                    }
+                    return null
+                  })}
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        ))}
       </div>
     </ScaffoldSection>
   )
