0f5938caf5728541ec895a7f99f3ea2b0fb00546
Ivan Vasilov <vasilov.ivan@gmail.com>
2025-10-07 15:34:07 +0200

    Fix the shadcn types.

--- apps/ui-library/registry/index.ts
+++ apps/ui-library/registry/index.ts
@@ -1,8 +1,7 @@
-import { type Registry } from 'shadcn/registry'
+import { type Registry, type RegistryItem } from 'shadcn/schema'
 
 import { examples } from '@/registry/examples'
 import { blocks as vueBlocks } from '@supabase/vue-blocks'
-import type { RegistryItem } from 'shadcn/registry'
 import { blocks } from './blocks'
 import { clients } from './clients'
 import aiEditorRules from './default/ai-editor-rules/registry-item.json' with { type: 'json' }
--- blocks/vue/registry/clients.ts
+++ blocks/vue/registry/clients.ts
@@ -1,4 +1,5 @@
-import type { RegistryItem } from 'shadcn/registry'
+import { type RegistryItem } from 'shadcn/schema'
+
 import nuxtjs from './default/clients/nuxtjs/registry-item.json' with { type: 'json' }
 import vue from './default/clients/vue/registry-item.json' with { type: 'json' }
 
