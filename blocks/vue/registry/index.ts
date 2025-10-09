12fa452a5388240b55367b4a99c4c99004e18f23
Baroshem <jakub.andrzejewski.dev@gmail.com>
2025-10-09 14:12:03 +0200

    docs: ui library password auth nuxt

--- blocks/vue/registry/index.ts
+++ blocks/vue/registry/index.ts
@@ -1,9 +1,10 @@
 import { type Registry } from 'shadcn/registry'
 
 import { clients } from './clients'
+import { passwordBasedAuth } from './password-based-auth'
 
 export const registry = {
   name: 'Supabase UI Library',
   homepage: 'https://supabase.com/ui',
-  items: [...clients],
+  items: [...clients, ...passwordBasedAuth],
 } satisfies Registry
