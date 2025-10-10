3cfe58103866370484c7e35593c5d8e46ac01e46
Saxon Fletcher <saxonafletcher@gmail.com>
2025-10-10 18:10:18 +1000

    fix

--- apps/studio/components/interfaces/HomeNew/GettingStarted/GettingStarted.utils.tsx
+++ apps/studio/components/interfaces/HomeNew/GettingStarted/GettingStarted.utils.tsx
@@ -236,7 +236,6 @@ export const getNoCodeWorkflowSteps = ({
     hasTables,
     hasSampleData,
     hasRlsPolicies,
-    hasConfiguredAuth,
     hasAppConnected,
     hasFirstUser,
     hasStorageObjects,
