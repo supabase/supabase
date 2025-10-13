11c2c9d101a95ba7274d68aecd038589784a5929
Ivan Vasilov <vasilov.ivan@gmail.com>
2025-10-13 13:14:58 +0200

    Convert the rules in eslint-config-supabase to be flat-config compatible.

--- packages/eslint-config-supabase/next.js
+++ packages/eslint-config-supabase/next.js
@@ -1,16 +1,39 @@
-module.exports = {
-  extends: ['prettier', 'next/core-web-vitals', 'eslint-config-turbo'],
-  rules: {
-    '@next/next/no-html-link-for-pages': 'off',
-    'react/jsx-key': 'off',
-    'no-restricted-exports': ['warn', { restrictDefaultExports: { direct: true } }],
-  },
-  overrides: [
-    {
-      files: ['pages/**', 'app/**'],
-      rules: {
-        'no-restricted-exports': 'off',
-      },
+const { defineConfig } = require('eslint/config')
+const js = require('@eslint/js')
+const { FlatCompat } = require('@eslint/eslintrc')
+const prettierConfig = require('eslint-config-prettier/flat')
+const { default: turboConfig } = require('eslint-config-turbo/flat')
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
+
+module.exports = defineConfig([
+  // Global files include for all rules
+  { files: ['pages/**.tsx', 'app/**.tsx'] },
+  // Global ignore for the .next folder
+  { ignores: ['.next', 'public'] },
+  turboConfig,
+  prettierConfig,
+  {
+    extends: compat.extends('next/core-web-vitals'),
+    linterOptions: {
+      reportUnusedDisableDirectives: 'error',
     },
-  ],
-}
+    rules: {
+      '@next/next/no-html-link-for-pages': 'off',
+      'react/jsx-key': 'off',
+      'no-restricted-exports': [
+        'warn',
+        {
+          restrictDefaultExports: {
+            direct: true,
+          },
+        },
+      ],
+      'no-restricted-exports': 'off',
+    },
+  },
+])
