9a15f44707273b9ae86562a341643e197aa5d39e
kallebysantos <kalleby_santos@hotmail.com>
2025-10-13 11:42:12 +0100

    revert: ui-library examples pages

--- apps/docs/app/example/ui-library/password-based-auth/auth/sign-up/page.tsx
+++
@@ -1,7 +0,0 @@
-import { SignUpForm } from 'ui-library/registry/default/blocks/password-based-auth-nextjs/components/sign-up-form'
-
-const PasswordBasedAuthDemo = () => {
-  return <SignUpForm />
-}
-
-export default PasswordBasedAuthDemo
--- apps/docs/app/example/ui-library/realtime-avatar-stack-demo/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import Demo from '@/registry/default/examples/realtime-avatar-stack-demo'
-
-const RealtimeAvatarStackDemoPage = () => {
-  return <Demo />
-}
-
-export default RealtimeAvatarStackDemoPage
