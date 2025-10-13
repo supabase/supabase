2c4fe4b09fa26d70ce1cfb7e1ae8aa4d57e755a1
kallebysantos <kalleby_santos@hotmail.com>
2025-10-13 11:45:54 +0100

    feat: add 'social-auth' guide page

--- apps/docs/components/Navigation/NavigationMenu/NavigationMenu.constants.ts
+++ apps/docs/components/Navigation/NavigationMenu/NavigationMenu.constants.ts
@@ -3228,6 +3228,7 @@ export const ui: NavMenuConstant = {
       items: [
         { name: 'Client', url: '/guides/ui/blocks/client' },
         { name: 'Password-based Authentication', url: '/guides/ui/blocks/password-based-auth' },
+        { name: 'Social Auth', url: '/guides/ui/blocks/social-auth' },
         { name: 'Dropzone', url: '/guides/ui/blocks/dropzone' },
       ]
     },
