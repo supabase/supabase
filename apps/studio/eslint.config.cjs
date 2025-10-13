a5cf74d4d47c5ff86a87e2b5297729c2453a12aa
Ivan Vasilov <vasilov.ivan@gmail.com>
2025-10-13 13:15:45 +0200

    Migrate all apps to use the new eslint config rules.

---
+++ apps/studio/eslint.config.cjs
@@ -0,0 +1,18 @@
+const { defineConfig } = require('eslint/config')
+const barrelFiles = require('eslint-plugin-barrel-files')
+const supabaseConfig = require('eslint-config-supabase/next')
+
+module.exports = defineConfig([
+  supabaseConfig,
+  {
+    plugins: {
+      'barrel-files': barrelFiles,
+    },
+    rules: {
+      '@next/next/no-img-element': 'off',
+      'react/no-unescaped-entities': 'off',
+      'react/display-name': 'warn',
+      'barrel-files/avoid-re-export-all': 'error',
+    },
+  },
+])
