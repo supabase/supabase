408b24bc908c720bffad9ad8f074a3806cb067b9
kallebysantos <kalleby_santos@hotmail.com>
2025-10-13 11:42:12 +0100

    revert: ui-library examples pages

--- apps/docs/app/example/ui-library/password-based-auth/auth/login/page.tsx
+++
@@ -1,7 +0,0 @@
-import { LoginForm } from 'ui-library/registry/default/blocks/password-based-auth-nextjs/components/login-form'
-
-const PasswordBasedAuthDemo = () => {
-  return <LoginForm />
-}
-
-export default PasswordBasedAuthDemo
