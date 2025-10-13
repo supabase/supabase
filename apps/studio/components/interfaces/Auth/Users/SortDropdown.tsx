11e2b910bd7722c6fc3d543918645a53cdde0546
Joshen Lim <joshenlimek@gmail.com>
2025-10-13 16:27:28 +0800

    Simplify disabled sort in performance mode

--- apps/studio/components/interfaces/Auth/Users/SortDropdown.tsx
+++ apps/studio/components/interfaces/Auth/Users/SortDropdown.tsx
@@ -1,3 +1,4 @@
+import { ButtonTooltip } from 'components/ui/ButtonTooltip'
 import { ArrowDownNarrowWide, ArrowDownWideNarrow } from 'lucide-react'
 
 import {
@@ -33,34 +34,31 @@ export const SortDropdown = ({
   showSortByPhone,
   setSortByValue,
 }: SortDropdownProps) => {
+  if (mode === 'performance') {
+    return (
+      <ButtonTooltip
+        disabled
+        type="default"
+        icon={<ArrowDownNarrowWide />}
+        tooltip={{
+          content: { side: 'bottom', text: 'Sort cannot be changed in optimized search mode' },
+        }}
+      >
+        Sorted by User ID
+      </ButtonTooltip>
+    )
+  }
+
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild disabled={mode === 'performance'}>
-        <Button
-          icon={
-            mode === 'performance' ? (
-              <ArrowDownNarrowWide />
-            ) : sortOrder === 'desc' ? (
-              <ArrowDownWideNarrow />
-            ) : (
-              <ArrowDownNarrowWide />
-            )
-          }
-        >
-          Sorted by{' '}
-          {mode === 'performance'
-            ? specificFilterColumn === 'id'
-              ? 'user ID'
-              : specificFilterColumn
-            : sortColumn === 'id'
-              ? 'user ID'
-              : sortColumn.replaceAll('_', ' ')}
+      <DropdownMenuTrigger asChild>
+        <Button icon={sortOrder === 'desc' ? <ArrowDownWideNarrow /> : <ArrowDownNarrowWide />}>
+          Sorted by {sortColumn.replaceAll('_', ' ')}
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-44" align="start">
         <DropdownMenuRadioGroup value={sortByValue} onValueChange={setSortByValue}>
           <DropdownMenuSub>
-            <DropdownMenuRadioItem value="id:asc">User ID</DropdownMenuRadioItem>
             <DropdownMenuSubTrigger>Sort by created at</DropdownMenuSubTrigger>
             <DropdownMenuSubContent>
               <DropdownMenuRadioItem value="created_at:asc">Ascending</DropdownMenuRadioItem>
