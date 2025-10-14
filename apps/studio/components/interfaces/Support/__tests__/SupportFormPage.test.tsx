bea2020f0370f5ed4cb00a894159c0c4ab9574ca
Charis Lam <26616127+charislam@users.noreply.github.com>
2025-10-14 17:20:12 -0400

    add dummy support form test to prevent vitest erroring

--- apps/studio/components/interfaces/Support/__tests__/SupportFormPage.test.tsx
+++ apps/studio/components/interfaces/Support/__tests__/SupportFormPage.test.tsx
@@ -1,3 +1,6 @@
+import { test } from 'vitest'
+
+test('placeholder', () => {})
 // import { screen, waitFor } from '@testing-library/react'
 // import userEvent from '@testing-library/user-event'
 // import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest'
