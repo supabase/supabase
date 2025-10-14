0a3f4184a74f12b126dd2265754e4bbea268674e
Stojan Dimitrovski <sdimitrovski@gmail.com>
2025-10-14 09:02:00 +0200

    feat: optimized users page (#39349)
    * feat: optimized users page
    
    * Update UI
    
    * Reinstate footer with count if mode is freeform
    
    * Simplify disabled sort in performance mode
    
    * Clean
    
    * Small fix
    
    * Final fixes
    
    * Shift users SQL query to packages/pg-meta
    
    * Nit unrelated: Clear query params from useLogsUrlState when going to logs tab of a selected user
    
    * Shift user count SQL and get count estimate SQL into packages/pg-meta
    
    * Fix
    
    * Nit
    
    * Nit
    
    * Minor nits
    
    * Refactor UX for searching
    
    ---------
    
    Co-authored-by: Joshen Lim <joshenlimek@gmail.com>

--- apps/studio/data/auth/users-count-query.ts
+++ apps/studio/data/auth/users-count-query.ts
@@ -1,7 +1,7 @@
+import { getUsersCountSQL } from '@supabase/pg-meta/src/sql/studio/get-users-count'
 import { useQuery, type UseQueryOptions } from '@tanstack/react-query'
 
 import { executeSql, type ExecuteSqlError } from 'data/sql/execute-sql-query'
-import { COUNT_ESTIMATE_SQL, THRESHOLD_COUNT } from 'data/table-rows/table-rows-count-query'
 import { authKeys } from './keys'
 import { type Filter } from './users-infinite-query'
 
@@ -12,86 +12,6 @@ type UsersCountVariables = {
   filter?: Filter
   providers?: string[]
   forceExactCount?: boolean
-}
-
-const getUsersCountSql = ({
-  filter,
-  keywords,
-  providers,
-  forceExactCount = false,
-}: {
-  filter?: Filter
-  keywords?: string
-  providers?: string[]
-  forceExactCount?: boolean
-}) => {
-  const hasValidKeywords = keywords && keywords !== ''
-
-  const conditions: string[] = []
-  const baseQueryCount = `select count(*) from auth.users`
-  const baseQuerySelect = `select * from auth.users`
-
-  if (hasValidKeywords) {
-    // [Joshen] Escape single quotes properly
-    const formattedKeywords = keywords.replaceAll("'", "''")
-    conditions.push(
-      `id::text ilike '%${formattedKeywords}%' or email ilike '%${formattedKeywords}%' or phone ilike '%${formattedKeywords}%'`
-    )
-  }
-
-  if (filter === 'verified') {
-    conditions.push(`email_confirmed_at IS NOT NULL or phone_confirmed_at IS NOT NULL`)
-  } else if (filter === 'anonymous') {
-    conditions.push(`is_anonymous is true`)
-  } else if (filter === 'unverified') {
-    conditions.push(`email_confirmed_at IS NULL AND phone_confirmed_at IS NULL`)
-  }
-
-  if (providers && providers.length > 0) {
-    // [Joshen] This is arguarbly not fully optimized, but at the same time not commonly used
-    // JFYI in case we do eventually run into performance issues here when filtering for SAML provider
-    if (providers.includes('saml 2.0')) {
-      conditions.push(
-        `(select jsonb_agg(case when value ~ '^sso' then 'sso' else value end) from jsonb_array_elements_text((raw_app_meta_data ->> 'providers')::jsonb)) ?| array[${providers.map((p) => (p === 'saml 2.0' ? `'sso'` : `'${p}'`)).join(', ')}]`.trim()
-      )
-    } else {
-      conditions.push(
-        `(raw_app_meta_data->>'providers')::jsonb ?| array[${providers.map((p) => `'${p}'`).join(', ')}]`
-      )
-    }
-  }
-
-  const combinedConditions = conditions.map((x) => `(${x})`).join(' and ')
-  const whereClause = conditions.length > 0 ? ` where ${combinedConditions}` : ''
-
-  if (forceExactCount) {
-    return `select (${baseQueryCount}${whereClause}), false as is_estimate;`
-  } else {
-    const selectBaseSql = `${baseQuerySelect}${whereClause}`
-    const countBaseSql = `${baseQueryCount}${whereClause}`
-
-    const escapedSelectSql = selectBaseSql.replaceAll("'", "''")
-
-    const sql = `
-${COUNT_ESTIMATE_SQL}
-
-with approximation as (
-    select reltuples as estimate
-    from pg_class
-    where oid = 'auth.users'::regclass
-)
-select 
-  case 
-    when estimate = -1 then (select pg_temp.count_estimate('${escapedSelectSql}'))::int
-    when estimate > ${THRESHOLD_COUNT} then ${conditions.length > 0 ? `(select pg_temp.count_estimate('${escapedSelectSql}'))::int` : 'estimate::int'}
-    else (${countBaseSql})
-  end as count,
-  estimate = -1 or estimate > ${THRESHOLD_COUNT} as is_estimate
-from approximation;
-`.trim()
-
-    return sql
-  }
 }
 
 export async function getUsersCount(
@@ -105,7 +25,7 @@ export async function getUsersCount(
   }: UsersCountVariables,
   signal?: AbortSignal
 ) {
-  const sql = getUsersCountSql({ filter, keywords, providers, forceExactCount })
+  const sql = getUsersCountSQL({ filter, keywords, providers, forceExactCount })
 
   const { result } = await executeSql(
     {
@@ -133,6 +53,7 @@ export async function getUsersCount(
 export type UsersCountData = Awaited<ReturnType<typeof getUsersCount>>
 export type UsersCountError = ExecuteSqlError
 
+/** [Joshen] Be wary of using this as it could potentially cause a huge load on the user's DB */
 export const useUsersCountQuery = <TData = UsersCountData>(
   {
     projectRef,
