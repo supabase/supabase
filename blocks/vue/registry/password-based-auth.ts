c4be1df08a5f4c341db85442355b1b126bf19881
Terry Sutton <saltcod@gmail.com>
2025-10-14 15:04:42 -0230

    Adjust paths

--- blocks/vue/registry/password-based-auth.ts
+++ blocks/vue/registry/password-based-auth.ts
@@ -1,4 +1,4 @@
-import { type Registry } from 'shadcn/registry'
+import { type Registry } from 'shadcn/schema'
 import nuxtjs from './default/password-based-auth/nuxtjs/registry-item.json' with { type: 'json' }
 import vue from './default/password-based-auth/vue/registry-item.json' with { type: 'json' }
 
