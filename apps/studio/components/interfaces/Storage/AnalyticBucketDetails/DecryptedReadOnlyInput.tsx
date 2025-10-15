53f909085052e1c9819208c76e522196c023aff5
Danny White <3104761+dnywh@users.noreply.github.com>
2025-10-15 16:00:39 +1000

    simplify

--- apps/studio/components/interfaces/Storage/AnalyticBucketDetails/DecryptedReadOnlyInput.tsx
+++ apps/studio/components/interfaces/Storage/AnalyticBucketDetails/DecryptedReadOnlyInput.tsx
@@ -2,7 +2,6 @@ import { useParams } from 'common'
 import { useVaultSecretDecryptedValueQuery } from 'data/vault/vault-secret-decrypted-value-query'
 import { useSelectedProjectQuery } from 'hooks/misc/useSelectedProject'
 import { ExternalLink, Eye, EyeOff, Loader } from 'lucide-react'
-import Link from 'next/link'
 import { useState } from 'react'
 import { Button, CardContent, Tooltip, TooltipContent, TooltipTrigger } from 'ui'
 import { Input } from 'ui-patterns/DataInputs/Input'
@@ -43,55 +42,57 @@ export const DecryptedReadOnlyInput = ({
     : value
 
   return (
-    <CardContent className="flex flex-row gap-2 py-6 border-b border-panel-border-interior-light">
-      <div className="flex-1">
-        <FormItemLayout
-          layout="horizontal"
-          label={label}
-          isReactForm={false}
-          description={descriptionText}
-        >
-          <Input
-            readOnly
-            // If the value is secure, allow copying to clipboard if the value is revealed. Otherwise, always allow copying
-            copy={!secureEntry || (!isDecryptedValueLoading && showHidden)}
-            value={renderedValue}
-            type={secureEntry ? (isLoading ? 'text' : showHidden ? 'text' : 'password') : 'text'}
-            actions={
-              secureEntry ? (
-                isLoading ? (
-                  <div className="flex items-center justify-center mr-1">
-                    <Button disabled type="default" icon={<Loader className="animate-spin" />} />
-                  </div>
-                ) : (
-                  <div className="flex items-center justify-center mr-1">
-                    <Button
-                      type="default"
-                      icon={showHidden ? <Eye /> : <EyeOff />}
-                      onClick={() => setShowHidden(!showHidden)}
-                    />
-                  </div>
-                )
-              ) : null
-            }
-          />
-        </FormItemLayout>
-      </div>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Button
-            asChild
-            type="outline"
-            size="tiny"
-            className="mt-1 px-1.5"
-            icon={<ExternalLink strokeWidth={2} className="text-foreground-lighter" />}
-            aria-label="Open in Vault"
-          >
-            <Link href={`/project/${ref}/integrations/vault/secrets?search=${value}`} />
-          </Button>
-        </TooltipTrigger>
-        <TooltipContent side="bottom">Open in Vault</TooltipContent>
-      </Tooltip>
+    <CardContent className="py-6 border-b border-panel-border-interior-light">
+      <FormItemLayout
+        layout="horizontal"
+        label={
+          <div className="flex items-center gap-x-2">
+            <span>{label}</span>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <a
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  href={`/project/${ref}/integrations/vault/secrets?search=${value}`}
+                >
+                  <ExternalLink
+                    size={14}
+                    className="text-foreground-muted hover:text-foreground-lighter transition"
+                  />
+                </a>
+              </TooltipTrigger>
+              <TooltipContent side="bottom">Open in Vault</TooltipContent>
+            </Tooltip>
+          </div>
+        }
+        description={descriptionText}
+        isReactForm={false}
+      >
+        <Input
+          readOnly
+          // If the value is secure, allow copying to clipboard if the value is revealed. Otherwise, always allow copying
+          copy={!secureEntry || (!isDecryptedValueLoading && showHidden)}
+          value={renderedValue}
+          type={secureEntry ? (isLoading ? 'text' : showHidden ? 'text' : 'password') : 'text'}
+          actions={
+            secureEntry ? (
+              isLoading ? (
+                <div className="flex items-center justify-center">
+                  <Button disabled type="default" icon={<Loader className="animate-spin" />} />
+                </div>
+              ) : (
+                <div className="flex items-center justify-center">
+                  <Button
+                    type="default"
+                    icon={showHidden ? <Eye /> : <EyeOff />}
+                    onClick={() => setShowHidden(!showHidden)}
+                  />
+                </div>
+              )
+            ) : null
+          }
+        />
+      </FormItemLayout>
     </CardContent>
   )
 }
