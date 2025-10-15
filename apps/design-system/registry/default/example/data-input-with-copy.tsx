1f6d2224c1d35f31ac0687eaf148dfdbd7b85ce9
Danny White <3104761+dnywh@users.noreply.github.com>
2025-10-15 12:29:22 +1000

    documentation

---
+++ apps/design-system/registry/default/example/data-input-with-copy.tsx
@@ -0,0 +1,5 @@
+import { Input } from 'ui-patterns/DataInputs/Input'
+
+export default function DataInputWithCopy() {
+  return <Input readOnly copy value="1234567890" />
+}
---
+++ apps/design-system/registry/default/example/data-input-with-reveal-copy.tsx
@@ -0,0 +1,5 @@
+import { Input } from 'ui-patterns/DataInputs/Input'
+
+export default function DataInputWithRevealCopy() {
+  return <Input readOnly reveal copy value="1234567890" />
+}
