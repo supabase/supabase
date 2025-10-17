3eb7da776666caa252580a8c5ced7a1afacdbef3
Charis <26616127+charislam@users.noreply.github.com>
2025-10-17 08:09:38 -0400

    feat(preferences): allow disable hotkeys (#39573)
    * feat(preferences): allow disable hotkeys
    
    Add a section in /account/me for disabling hotkeys. Only added one
    hotkey for now (Cmd + E for toggling editor side panel) but we can add
    more with the same pattern.
    
    * refactor: remove default export on ProjectLayout
    
    * feat(hotkeys): allow toggling of command menu and ai assistant hotkeys
    
    * Nit
    
    * PRettier lint
    
    ---------
    
    Co-authored-by: Joshen Lim <joshenlimek@gmail.com>

--- apps/studio/pages/_app.tsx
+++ apps/studio/pages/_app.tsx
@@ -30,12 +30,13 @@ import timezone from 'dayjs/plugin/timezone'
 import utc from 'dayjs/plugin/utc'
 import Head from 'next/head'
 import { NuqsAdapter } from 'nuqs/adapters/next/pages'
-import { ErrorInfo, useCallback } from 'react'
+import { ErrorInfo, PropsWithChildren, useCallback } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
 
 import {
   FeatureFlagProvider,
   getFlags,
+  LOCAL_STORAGE_KEYS,
   TelemetryTagManager,
   ThemeProvider,
   useThemeSandbox,
@@ -51,6 +52,7 @@ import { GlobalErrorBoundaryState } from 'components/ui/GlobalErrorBoundaryState
 import { useRootQueryClient } from 'data/query-client'
 import { customFont, sourceCodePro } from 'fonts'
 import { useCustomContent } from 'hooks/custom-content/useCustomContent'
+import { useLocalStorageQuery } from 'hooks/misc/useLocalStorage'
 import { AuthProvider } from 'lib/auth'
 import { API_URL, BASE_PATH, IS_PLATFORM, useDefaultProvider } from 'lib/constants'
 import { ProfileProvider } from 'lib/profile'
@@ -77,6 +79,15 @@ loader.config({
       : `${BASE_PATH}/monaco-editor`,
   },
 })
+
+const CommandProviderWithPreferences = ({ children }: PropsWithChildren) => {
+  const [commandMenuHotkeyEnabled] = useLocalStorageQuery<boolean>(
+    LOCAL_STORAGE_KEYS.HOTKEY_COMMAND_MENU,
+    true
+  )
+
+  return <CommandProvider openKey={commandMenuHotkeyEnabled ? 'k' : ''}>{children}</CommandProvider>
+}
 
 // [Joshen TODO] Once we settle on the new nav layout - we'll need a lot of clean up in terms of our layout components
 // a lot of them are unnecessary and introduce way too many cluttered CSS especially with the height styles that make
@@ -149,7 +160,7 @@ function CustomApp({ Component, pageProps }: AppPropsWithLayout) {
                         disableTransitionOnChange
                       >
                         <AppBannerContextProvider>
-                          <CommandProvider>
+                          <CommandProviderWithPreferences>
                             <FeaturePreviewContextProvider>
                               {getLayout(<Component {...pageProps} />)}
                               <StudioCommandMenu />
@@ -157,7 +168,7 @@ function CustomApp({ Component, pageProps }: AppPropsWithLayout) {
                             </FeaturePreviewContextProvider>
                             <SonnerToaster position="top-right" />
                             <MonacoThemeProvider />
-                          </CommandProvider>
+                          </CommandProviderWithPreferences>
                         </AppBannerContextProvider>
                       </ThemeProvider>
                     </RouteValidationWrapper>
