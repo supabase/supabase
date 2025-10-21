842464cc0908be1ba4c72fecfc424865d30c8477
Charis Lam <26616127+charislam@users.noreply.github.com>
2025-10-21 11:47:17 -0400

    refactor(policies page): more efficient sql query for getting anon/auth access tables

--- packages/pg-meta/src/sql/studio/check-tables-anon-authenticated-access.ts
+++ packages/pg-meta/src/sql/studio/check-tables-anon-authenticated-access.ts
@@ -4,9 +4,17 @@
  */
 export const getTablesWithAnonAuthenticatedAccessSQL = ({ schema }: { schema: string }) =>
   /* SQL */ `
-SELECT DISTINCT table_name
-FROM information_schema.role_table_grants
-WHERE table_schema = '${schema}'
-AND grantee IN ('anon','authenticated')
+SELECT c.relname AS table_name
+FROM pg_catalog.pg_class AS c
+JOIN pg_catalog.pg_namespace AS n ON n.oid = c.relnamespace
+WHERE n.nspname = '${schema}'
+  AND c.relkind IN ('r','p')  -- table, partitioned table
+  AND EXISTS (
+    SELECT 1
+    FROM pg_catalog.aclexplode(COALESCE(c.relacl, '{}'::aclitem[])) AS a
+    JOIN pg_catalog.pg_roles r ON r.oid = a.grantee
+    WHERE r.rolname IN ('anon','authenticated')
+  )
+;
 `.trim()
 
