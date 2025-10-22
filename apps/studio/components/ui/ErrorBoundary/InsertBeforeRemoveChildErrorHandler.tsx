23f9c923f4aa3bf963ea84e7dfa6584aa0e03ca0
Joshen Lim <joshenlimek@gmail.com>
2025-10-22 22:38:32 +0800

    nit

--- apps/studio/components/ui/ErrorBoundary/InsertBeforeRemoveChildErrorHandler.tsx
+++ apps/studio/components/ui/ErrorBoundary/InsertBeforeRemoveChildErrorHandler.tsx
@@ -1,8 +1,9 @@
 import { SupportCategories } from '@supabase/shared-types/out/constants'
-import { SupportLink } from 'components/interfaces/Support/SupportLink'
-import { detectBrowser } from 'lib/helpers'
 import { Blocks, ExternalLink } from 'lucide-react'
 import { useRouter } from 'next/router'
+
+import { SupportLink } from 'components/interfaces/Support/SupportLink'
+import { detectBrowser } from 'lib/helpers'
 import { Button } from 'ui'
 
 export const InsertBeforeRemoveChildErrorHandler = ({
