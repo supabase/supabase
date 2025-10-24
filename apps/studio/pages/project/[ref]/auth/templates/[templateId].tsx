5e5d944cd53f6957995bc6f62c07ad5da5f2fda7
Danny White <3104761+dnywh@users.noreply.github.com>
2025-10-24 11:26:07 +1000

    basic subpages

--- apps/studio/pages/project/[ref]/auth/templates/[templateId].tsx
+++ apps/studio/pages/project/[ref]/auth/templates/[templateId].tsx
@@ -1,15 +1,23 @@
 import { PermissionAction } from '@supabase/shared-types/out/constants'
 
-import { EmailTemplates } from 'components/interfaces/Auth/EmailTemplates/EmailTemplates'
 import { AuthEmailsLayout } from 'components/layouts/AuthLayout/AuthEmailsLayout'
 import DefaultLayout from 'components/layouts/DefaultLayout'
-import { ScaffoldContainer, ScaffoldSection } from 'components/layouts/Scaffold'
+import {
+  ScaffoldContainer,
+  ScaffoldSection,
+  ScaffoldSectionDescription,
+  ScaffoldSectionTitle,
+} from 'components/layouts/Scaffold'
 import NoPermission from 'components/ui/NoPermission'
 import { useAsyncCheckPermissions } from 'hooks/misc/useCheckPermissions'
+import { useRouter } from 'next/router'
 import type { NextPageWithLayout } from 'types'
 import { GenericSkeletonLoader } from 'ui-patterns'
 
-const TemplatesPage: NextPageWithLayout = () => {
+const TemplatePage: NextPageWithLayout = () => {
+  const router = useRouter()
+  const { templateId } = router.query
+
   const { can: canReadAuthSettings, isSuccess: isPermissionsLoaded } = useAsyncCheckPermissions(
     PermissionAction.READ,
     'custom_config_gotrue'
@@ -20,22 +28,33 @@ const TemplatesPage: NextPageWithLayout = () => {
   }
 
   return (
-    <ScaffoldContainer>
+    <ScaffoldContainer bottomPadding>
       {!isPermissionsLoaded ? (
         <ScaffoldSection isFullWidth>
           <GenericSkeletonLoader />
         </ScaffoldSection>
       ) : (
-        <EmailTemplates />
+        <ScaffoldSection isFullWidth>
+          <div>
+            <ScaffoldSectionTitle>Email Template: {templateId}</ScaffoldSectionTitle>
+            <ScaffoldSectionDescription>
+              Configure and customize your email template settings.
+            </ScaffoldSectionDescription>
+          </div>
+          {/* Template content will go here */}
+          <div className="mt-6">
+            <p className="text-foreground-light">Template editor will be implemented here.</p>
+          </div>
+        </ScaffoldSection>
       )}
     </ScaffoldContainer>
   )
 }
 
-TemplatesPage.getLayout = (page) => (
+TemplatePage.getLayout = (page) => (
   <DefaultLayout>
     <AuthEmailsLayout>{page}</AuthEmailsLayout>
   </DefaultLayout>
 )
 
-export default TemplatesPage
+export default TemplatePage
