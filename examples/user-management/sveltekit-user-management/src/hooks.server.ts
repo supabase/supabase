927945e8ae6cd30cc5d3649d6dadaffdd0833242
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2025-10-29 15:51:04 -0400

    docs: Fix SvelteKit user management example server hooks code (#39987)

--- examples/user-management/sveltekit-user-management/src/hooks.server.ts
+++ examples/user-management/sveltekit-user-management/src/hooks.server.ts
@@ -22,22 +22,27 @@ export const handle: Handle = async ({ event, resolve }) => {
   })
 
   /**
-   * Unlike `supabase.auth.getSession`, which is unsafe on the server because it
-   * doesn't validate the JWT, this function validates the JWT by first calling
-   * `getUser` and aborts early if the JWT signature is invalid.
+   * Unlike `supabase.auth.getSession()`, which returns the session _without_
+   * validating the JWT, this function also calls `getUser()` to validate the
+   * JWT before returning the session.
    */
   event.locals.safeGetSession = async () => {
+    const {
+      data: { session },
+    } = await event.locals.supabase.auth.getSession()
+    if (!session) {
+      return { session: null, user: null }
+    }
+
     const {
       data: { user },
       error,
     } = await event.locals.supabase.auth.getUser()
     if (error) {
+      // JWT validation has failed
       return { session: null, user: null }
     }
 
-    const {
-      data: { session },
-    } = await event.locals.supabase.auth.getSession()
     return { session, user }
   }
 
