8261ac23d7bf9f907ad4adb8c77497941dbfb438
Charis Lam <26616127+charislam@users.noreply.github.com>
2025-10-30 17:11:36 -0400

    chore: update react-day-picker

--- packages/ui/src/components/shadcn/ui/calendar.tsx
+++ packages/ui/src/components/shadcn/ui/calendar.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { ChevronLeft, ChevronRight } from 'lucide-react'
-import * as React from 'react'
 import { DayPicker } from 'react-day-picker'
 
 import { cn } from '../../../lib/utils/cn'
@@ -10,9 +9,6 @@ import { buttonVariants } from './button'
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
 
 function Calendar({ className, classNames, showOutsideDays = true, ...props }: CalendarProps) {
-  // [Joshen] Wondering if this is a bug with react-day-picker v8 whereby a single selected date
-  // has styles applied for both day_range_start and day_range_end. This might be fixed with the latest
-  // version of react-day-picker, but in case we can opt for a pure tailwind solution instead of JS
   const fullDateRangeSelected =
     props.mode === 'range' && !!props.selected?.from && !!props.selected?.to
 
@@ -21,42 +17,53 @@ function Calendar({ className, classNames, showOutsideDays = true, ...props }: C
       showOutsideDays={showOutsideDays}
       className={cn('p-3', className)}
       classNames={{
-        months: 'flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0',
+        months: 'flex flex-col sm:flex-row space-y-4 sm:[&>*:not(nav)+*]:ml-4 sm:space-y-0',
         month: 'space-y-4',
-        caption: 'flex justify-center pt-1 relative items-center',
+        month_caption: 'flex justify-center pt-1 relative items-center',
         caption_label: 'text-sm font-medium',
         nav: 'space-x-1 flex items-center',
-        nav_button: cn(
+        button_previous: cn(
           buttonVariants({ variant: 'outline' }),
-          'h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100'
+          'h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100',
+          'absolute left-5 top-4'
         ),
-        nav_button_previous: 'absolute left-1',
-        nav_button_next: 'absolute right-1',
-        table: 'w-full border-collapse space-y-1',
-        head_row: 'flex',
-        head_cell: 'text-foreground-muted rounded-md w-9 font-normal text-[0.8rem]',
-        row: 'flex w-full mt-2',
-        cell: cn(
+        button_next: cn(
+          buttonVariants({ variant: 'outline' }),
+          'h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100',
+          'absolute right-5 top-4'
+        ),
+        month_grid: 'w-full border-collapse space-y-1',
+        weekdays: 'flex',
+        weekday: 'text-foreground-muted rounded-md w-9 font-normal text-[0.8rem]',
+        week: 'flex w-full mt-2',
+        day: cn(
           'text-center text-sm p-0 relative [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md',
           'last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20'
         ),
-        day: cn(
+        day_button: cn(
           buttonVariants({ variant: 'ghost' }),
           'h-9 w-9 p-0 font-normal aria-selected:opacity-100'
         ),
-        day_selected: 'bg-brand-500 text-foreground text-foreground',
-        day_today: 'bg-accent text-accent-foreground',
-        day_outside: 'text-foreground-muted opacity-50',
-        day_disabled: 'text-foreground-muted opacity-50',
-        day_range_start: cn(fullDateRangeSelected && 'bg-brand-500 rounded-r-none'),
-        day_range_middle: 'aria-selected:bg-brand-400 rounded-none',
-        day_range_end: cn(fullDateRangeSelected && 'bg-brand-500 rounded-l-none'),
-        day_hidden: 'invisible',
+        selected: 'bg-brand-500 text-foreground text-foreground',
+        today: 'bg-accent text-accent-foreground',
+        outside: 'text-foreground-muted opacity-50',
+        disabled: 'text-foreground-muted opacity-50',
+        range_start: cn(fullDateRangeSelected && 'bg-brand-500 rounded-r-none'),
+        range_middle: 'aria-selected:bg-brand-400 rounded-none',
+        range_end: cn(fullDateRangeSelected && 'bg-brand-500 rounded-l-none'),
+        hidden: 'invisible',
         ...classNames,
       }}
       components={{
-        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
-        IconRight: () => <ChevronRight className="h-4 w-4" />,
+        Chevron: (props) => {
+          const { className, ...rest } = props
+
+          if (props.orientation === 'left') {
+            return <ChevronLeft className={cn('h-4 w-4', className)} {...rest} />
+          } else {
+            return <ChevronRight className={cn('h-4 w-4', className)} {...rest} />
+          }
+        },
       }}
       {...props}
     />
