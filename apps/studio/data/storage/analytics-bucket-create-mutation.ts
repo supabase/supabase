e4768995cb6a325389bcc37e3c89548245bbf958
Joshen Lim <joshenlimek@gmail.com>
2025-11-03 15:33:01 +0800

    Smol fix

--- apps/studio/data/storage/analytics-bucket-create-mutation.ts
+++ apps/studio/data/storage/analytics-bucket-create-mutation.ts
@@ -2,6 +2,7 @@ import { useMutation, UseMutationOptions, useQueryClient } from '@tanstack/react
 import { toast } from 'sonner'
 
 import { components } from 'api-types'
+import { useIsNewStorageUIEnabled } from 'components/interfaces/App/FeaturePreview/FeaturePreviewContext'
 import { handleError, post } from 'data/fetchers'
 import type { ResponseError } from 'types'
 import { storageKeys } from './keys'
@@ -36,12 +37,17 @@ export const useAnalyticsBucketCreateMutation = ({
   'mutationFn'
 > = {}) => {
   const queryClient = useQueryClient()
+  const isStorageV2 = useIsNewStorageUIEnabled()
 
   return useMutation<AnalyticsBucketCreateData, ResponseError, AnalyticsBucketCreateVariables>({
     mutationFn: (vars) => createAnalyticsBucket(vars),
     async onSuccess(data, variables, context) {
       const { projectRef } = variables
-      await queryClient.invalidateQueries(storageKeys.analyticsBuckets(projectRef))
+      if (isStorageV2) {
+        await queryClient.invalidateQueries(storageKeys.analyticsBuckets(projectRef))
+      } else {
+        await queryClient.invalidateQueries(storageKeys.buckets(projectRef))
+      }
       await onSuccess?.(data, variables, context)
     },
     async onError(data, variables, context) {
