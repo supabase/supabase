d8a5eb004bd4f6600c7e025ffd969811e91af0ff
Chris Chinchilla <chris.ward@supabase.io>
2025-11-05 16:26:24 +0100

    Prettier

--- apps/docs/lib/custom-content/getCustomContent.ts
+++ apps/docs/lib/custom-content/getCustomContent.ts
@@ -23,9 +23,7 @@ function contentToCamelCase(feature: CustomContent) {
 type CustomContentResult<T extends CustomContent[]> = {
   [key in CustomContentToCamelCase<T[number]> extends keyof CustomContentTypes
     ? CustomContentToCamelCase<T[number]>
-    : never]:
-    | CustomContentTypes[key]
-    | null
+    : never]: CustomContentTypes[key] | null
 }
 
 export const getCustomContent = <T extends CustomContent[]>(
