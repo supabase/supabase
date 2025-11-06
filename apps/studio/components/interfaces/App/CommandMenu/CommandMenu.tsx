28587ede40b1fee71fb331ec114a7ec70855325d
kallebysantos <kalleby_santos@hotmail.com>
2025-11-06 17:54:27 +0000

    stamp: hide organization commands for non-platform environment

--- apps/studio/components/interfaces/App/CommandMenu/CommandMenu.tsx
+++ apps/studio/components/interfaces/App/CommandMenu/CommandMenu.tsx
@@ -22,7 +22,9 @@ export default function StudioCommandMenu() {
   useApiUrlCommand()
   useProjectLevelTableEditorCommands()
   useProjectSwitchCommand()
-  useConfigureOrganizationCommand()
+
+  IS_PLATFORM && useConfigureOrganizationCommand()
+
   useQueryTableCommands()
   useBranchCommands()
   useSnippetCommands()
