cc13de150fd44191a24c84a737deace182f30901
kallebysantos <kalleby_santos@hotmail.com>
2025-11-06 15:27:23 +0000

    stamp: changing empty local state fallback content to match specific environment

--- apps/studio/components/interfaces/Settings/Database/SettingsDatabaseEmptyStateLocal.tsx
+++ apps/studio/components/interfaces/Settings/Database/SettingsDatabaseEmptyStateLocal.tsx
@@ -1,27 +1,30 @@
 import { Card, CardContent, CardHeader, CardTitle } from 'ui'
-import { DOCS_URL } from 'lib/constants'
+import { DOCS_URL, IS_LOCAL_CLI } from 'lib/constants'
 import { DocsButton } from 'components/ui/DocsButton'
 
 export function SettingsDatabaseEmptyStateLocal() {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Managing database settings locally</CardTitle>
+        <CardTitle>
+          Managing database settings {IS_LOCAL_CLI ? 'locally' : 'for self-hosted'}
+        </CardTitle>
       </CardHeader>
       <CardContent className="p-0 grid grid-cols-[repeat(auto-fit,minmax(240px,1fr))] divide-y md:divide-y-0 md:divide-x divide-default items-stretch">
         <div className="p-8">
           <div className="flex items-center gap-2">
             <h4 className="text-base text-foreground">Managing settings</h4>
           </div>
+
           <div className="text-sm text-foreground-light mt-1 mb-4 max-w-3xl">
-            <p>Local database config can be loaded in either of the following two ways</p>
-            <ul className="list-disc pl-6">
-              <li className="prose [&>code]:text-xs text-sm max-w-full">
-                Through <code>config.toml</code> file placed at <code>supabase/config.toml</code>,
-                which is automatically loaded on <code>supabase start</code>
-              </li>
-              <li className="prose [&>code]:text-xs space-x-1 text-sm max-w-full">
-                <span>Through</span>
+            <p className="prose [&>code]:text-xs space-x-1 text-sm max-w-full">
+              Local database config can be loaded through{' '}
+              {IS_LOCAL_CLI ? (
+                <span>
+                  <code>config.toml</code> file placed at <code>supabase/config.toml</code>, which
+                  is automatically loaded on <code>supabase start</code>
+                </span>
+              ) : (
                 <a
                   target="_blank"
                   rel="noopener noreferrer"
@@ -29,12 +32,15 @@ export function SettingsDatabaseEmptyStateLocal() {
                 >
                   .env file
                 </a>
-                <span>option when self-hosting</span>
-              </li>
-            </ul>
+              )}
+            </p>
           </div>
 
-          <DocsButton href={`${DOCS_URL}/guides/local-development/cli/config#database-config`} />
+          {IS_LOCAL_CLI ? (
+            <DocsButton href={`${DOCS_URL}/guides/local-development/cli/config#database-config`} />
+          ) : (
+            <DocsButton href={`${DOCS_URL}/guides/self-hosting`} />
+          )}
         </div>
       </CardContent>
     </Card>
