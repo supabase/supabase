6050ae523b56fc599eff5d17a999022941664c86
kallebysantos <kalleby_santos@hotmail.com>
2025-11-06 18:54:18 +0000

    stamp: hidding platform specific settings commands

--- apps/studio/components/layouts/ProjectSettingsLayout/ProjectSettings.Commands.tsx
+++ apps/studio/components/layouts/ProjectSettingsLayout/ProjectSettings.Commands.tsx
@@ -1,4 +1,5 @@
 import { useParams } from 'common'
+import { IS_PLATFORM } from 'lib/constants'
 import { COMMAND_MENU_SECTIONS } from 'components/interfaces/App/CommandMenu/CommandMenu.utils'
 import { useIsFeatureEnabled } from 'hooks/misc/useIsFeatureEnabled'
 import type { CommandOptions } from 'ui-patterns/CommandMenu'
@@ -20,38 +21,102 @@ export function useProjectSettingsGotoCommands(options?: CommandOptions) {
     COMMAND_MENU_SECTIONS.NAVIGATE,
     [
       {
-        id: 'nav-project-settings-general',
-        name: 'General Settings',
-        route: `/project/${ref}/settings/general`,
-        defaultHidden: true,
-      },
-      {
         id: 'nav-project-settings-database',
         name: 'Database Settings',
         route: `/project/${ref}/database/settings`,
         defaultHidden: true,
       },
-      {
-        id: 'nav-project-settings-auth',
-        name: 'Auth Settings',
-        route: authenticationSignInProviders
-          ? `/project/${ref}/auth/providers`
-          : `/project/${ref}/auth/policies`,
-        defaultHidden: true,
-      },
-      {
-        id: 'nav-project-settings-api',
-        name: 'API Settings',
-        route: `/project/${ref}/settings/api`,
-        defaultHidden: true,
-      },
-      {
-        id: 'nav-project-settings-storage',
-        name: 'Storage Settings',
-        route: `/project/${ref}/storage/settings`,
-        defaultHidden: true,
-      },
-      ...(projectSettingsCustomDomains
+      ...(IS_PLATFORM
+        ? ([
+            {
+              id: 'nav-project-settings-general',
+              name: 'General Settings',
+              route: `/project/${ref}/settings/general`,
+              defaultHidden: true,
+            },
+            {
+              id: 'nav-project-settings-auth',
+              name: 'Auth Settings',
+              route: authenticationSignInProviders
+                ? `/project/${ref}/auth/providers`
+                : `/project/${ref}/auth/policies`,
+              defaultHidden: true,
+            },
+            {
+              id: 'nav-project-settings-api',
+              name: 'API Settings',
+              route: `/project/${ref}/settings/api`,
+              defaultHidden: true,
+            },
+            {
+              id: 'nav-project-settings-storage',
+              name: 'Storage Settings',
+              route: `/project/${ref}/storage/settings`,
+              defaultHidden: true,
+            },
+            {
+              id: 'nav-project-settings-restart-project',
+              name: 'Restart project',
+              route: `/project/${ref}/settings/general#restart-project`,
+              defaultHidden: true,
+            },
+            {
+              id: 'nav-project-settings-pause-project',
+              name: 'Pause project',
+              route: `/project/${ref}/settings/general#pause-project`,
+              defaultHidden: true,
+            },
+            {
+              id: 'nav-project-settings-transfer-project',
+              name: 'Transfer project',
+              route: `/project/${ref}/settings/general#transfer-project`,
+              defaultHidden: true,
+            },
+            {
+              id: 'nav-project-settings-delete-project',
+              name: 'Delete project',
+              route: `/project/${ref}/settings/general#delete-project`,
+              defaultHidden: true,
+            },
+            {
+              id: 'nav-project-settings-database-password',
+              name: 'Database password',
+              route: `/project/${ref}/settings/general#database-password`,
+              defaultHidden: true,
+            },
+            {
+              id: 'nav-project-settings-connection-string',
+              name: 'Connection string',
+              route: `/project/${ref}/settings/general#connection-string`,
+              defaultHidden: true,
+            },
+            {
+              id: 'nav-project-settings-connection-pooling',
+              name: 'Connection pooling',
+              route: `/project/${ref}/settings/general#connection-pooling`,
+              defaultHidden: true,
+            },
+            {
+              id: 'nav-project-settings-ssl-configuration',
+              name: 'SSL configuration',
+              route: `/project/${ref}/settings/general#ssl-configuration`,
+              defaultHidden: true,
+            },
+            {
+              id: 'nav-project-settings-network-restrictions',
+              name: 'Network restrictions',
+              route: `/project/${ref}/database/settings#network-restrictions`,
+              defaultHidden: true,
+            },
+            {
+              id: 'nav-project-settings-banned-ips',
+              name: 'Banned IPs',
+              route: `/project/${ref}/database/settings#banned-ips`,
+              defaultHidden: true,
+            },
+          ] as IRouteCommand[])
+        : []),
+      ...(IS_PLATFORM && projectSettingsCustomDomains
         ? [
             {
               id: 'nav-project-settings-custom-domains',
@@ -61,66 +126,6 @@ export function useProjectSettingsGotoCommands(options?: CommandOptions) {
             } as IRouteCommand,
           ]
         : []),
-      {
-        id: 'nav-project-settings-restart-project',
-        name: 'Restart project',
-        route: `/project/${ref}/settings/general#restart-project`,
-        defaultHidden: true,
-      },
-      {
-        id: 'nav-project-settings-pause-project',
-        name: 'Pause project',
-        route: `/project/${ref}/settings/general#pause-project`,
-        defaultHidden: true,
-      },
-      {
-        id: 'nav-project-settings-transfer-project',
-        name: 'Transfer project',
-        route: `/project/${ref}/settings/general#transfer-project`,
-        defaultHidden: true,
-      },
-      {
-        id: 'nav-project-settings-delete-project',
-        name: 'Delete project',
-        route: `/project/${ref}/settings/general#delete-project`,
-        defaultHidden: true,
-      },
-      {
-        id: 'nav-project-settings-database-password',
-        name: 'Database password',
-        route: `/project/${ref}/settings/general#database-password`,
-        defaultHidden: true,
-      },
-      {
-        id: 'nav-project-settings-connection-string',
-        name: 'Connection string',
-        route: `/project/${ref}/settings/general#connection-string`,
-        defaultHidden: true,
-      },
-      {
-        id: 'nav-project-settings-connection-pooling',
-        name: 'Connection pooling',
-        route: `/project/${ref}/settings/general#connection-pooling`,
-        defaultHidden: true,
-      },
-      {
-        id: 'nav-project-settings-ssl-configuration',
-        name: 'SSL configuration',
-        route: `/project/${ref}/settings/general#ssl-configuration`,
-        defaultHidden: true,
-      },
-      {
-        id: 'nav-project-settings-network-restrictions',
-        name: 'Network restrictions',
-        route: `/project/${ref}/database/settings#network-restrictions`,
-        defaultHidden: true,
-      },
-      {
-        id: 'nav-project-settings-banned-ips',
-        name: 'Banned IPs',
-        route: `/project/${ref}/database/settings#banned-ips`,
-        defaultHidden: true,
-      },
       ...(projectSettingsLogDrains
         ? [
             {
