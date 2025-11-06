d3c94ffebe46a481214d6535c0335fbe379b1315
Terry Sutton <saltcod@gmail.com>
2025-11-06 12:26:50 -0330

    Fix import path

--- blocks/vue/registry/default/password-based-auth/nuxtjs/server/routes/auth/confirm.get.ts
+++ blocks/vue/registry/default/password-based-auth/nuxtjs/server/routes/auth/confirm.get.ts
@@ -1,13 +1,13 @@
-import { createSupabaseServerClient } from '../../supabase/client'
-import { getQuery, defineEventHandler, sendRedirect } from 'h3';
-import type { EmailOtpType } from "@supabase/supabase-js"
+import { getQuery, defineEventHandler, sendRedirect } from 'h3'
+import type { EmailOtpType } from '@supabase/supabase-js'
+import { createSupabaseServerClient } from '@/registry/default/clients/nuxtjs/server/supabase/client'
 
 export default defineEventHandler(async (event) => {
   const query = getQuery(event)
   const token_hash = query.token_hash as string | null
   const type = query.type as EmailOtpType | null
   const _next = query.next as string | undefined
-  const next = _next?.startsWith("/") ? _next : "/"
+  const next = _next?.startsWith('/') ? _next : '/'
 
   if (token_hash && type) {
     const supabase = createSupabaseServerClient(event)
@@ -20,15 +20,9 @@ export default defineEventHandler(async (event) => {
     if (!error) {
       return sendRedirect(event, next)
     } else {
-      return sendRedirect(
-        event,
-        `/auth/error?error=${encodeURIComponent(error.message)}`
-      )
+      return sendRedirect(event, `/auth/error?error=${encodeURIComponent(error.message)}`)
     }
   }
 
-  return sendRedirect(
-    event,
-    `/auth/error?error=${encodeURIComponent("No token hash or type")}`
-  )
+  return sendRedirect(event, `/auth/error?error=${encodeURIComponent('No token hash or type')}`)
 })
