a93d7898a88baf8f4208d682bfdb13ab4e53157f
Saxon Fletcher <saxonafletcher@gmail.com>
2025-11-07 11:47:41 +1000

    functions apply page

--- apps/studio/pages/project/[ref]/functions/[functionSlug]/details.tsx
+++ apps/studio/pages/project/[ref]/functions/[functionSlug]/details.tsx
@@ -1,13 +1,11 @@
 import { EdgeFunctionDetails } from 'components/interfaces/Functions/EdgeFunctionDetails/EdgeFunctionDetails'
 import DefaultLayout from 'components/layouts/DefaultLayout'
 import EdgeFunctionDetailsLayout from 'components/layouts/EdgeFunctionsLayout/EdgeFunctionDetailsLayout'
+import { PageContainer } from 'ui-patterns/PageContainer'
+import { PageSection } from 'ui-patterns/PageSection'
 import type { NextPageWithLayout } from 'types'
 
-const PageLayout: NextPageWithLayout = () => (
-  <div className="px-6">
-    <EdgeFunctionDetails />
-  </div>
-)
+const PageLayout: NextPageWithLayout = () => <EdgeFunctionDetails />
 
 PageLayout.getLayout = (page) => (
   <DefaultLayout>
