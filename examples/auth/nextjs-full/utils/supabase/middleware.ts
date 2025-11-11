534c300a7adb537e58eb961fd1ccf986b62a3498
Chris Chinchilla <chris.ward@supabase.io>
2025-11-11 12:37:11 +0000

    docs: SSR overhaul combined client page PoC (#38405)
    * Draft begins
    
    * Starte SvelteKit
    
    * Changes
    
    * Consolidate creating a client
    
    * Final draft
    
    * Update apps/docs/content/guides/auth/server-side/creating-a-client.mdx
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update apps/docs/content/guides/auth/server-side/creating-a-client.mdx
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Rework and simplify
    
    * Update apps/docs/content/guides/auth/server-side/creating-a-client.mdx
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update apps/docs/content/guides/auth/server-side/creating-a-client.mdx
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update apps/docs/content/guides/auth/server-side/creating-a-client.mdx
    
    Co-authored-by: Charis <26616127+charislam@users.noreply.github.com>
    
    * improve spacing
    
    * last
    
    * Simplify example
    
    * Prettier
    
    * Fix hono, remove old pages, and add redirects
    
    * Remove from menu
    
    * Update apps/docs/content/guides/auth/server-side/creating-a-client.mdx
    
    Co-authored-by: Charis <26616127+charislam@users.noreply.github.com>
    
    * Changes
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Charis <26616127+charislam@users.noreply.github.com>
    Co-authored-by: Alan Daniel <stylesshjs@gmail.com>

--- examples/auth/nextjs-full/utils/supabase/middleware.ts
+++ examples/auth/nextjs-full/utils/supabase/middleware.ts
@@ -1,5 +1,5 @@
-import { createServerClient } from '@supabase/ssr'
-import { NextResponse, type NextRequest } from 'next/server'
+import { createServerClient } from "@supabase/ssr"
+import { NextResponse, type NextRequest } from "next/server"
 
 export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
@@ -15,33 +15,34 @@ export async function updateSession(request: NextRequest) {
           return request.cookies.getAll()
         },
         setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value))
+          cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value))
           supabaseResponse = NextResponse.next({
             request,
           })
-          cookiesToSet.forEach(({ name, value }) => supabaseResponse.cookies.set(name, value))
+          cookiesToSet.forEach(({ name, value, options }) =>
+            supabaseResponse.cookies.set(name, value, options)
+          )
         },
       },
     }
   )
 
   // IMPORTANT: Avoid writing any logic between createServerClient and
-  // supabase.auth.getClaims(). A simple mistake could make it very hard to debug
+  // supabase.auth.getUser(). A simple mistake could make it very hard to debug
   // issues with users being randomly logged out.
 
-  // IMPORTANT: Don't remove getClaims()
-  const { data } = await supabase.auth.getClaims()
-
-  const user = data?.claims
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
 
   if (
     !user &&
-    !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth')
+    !request.nextUrl.pathname.startsWith("/login") &&
+    !request.nextUrl.pathname.startsWith("/auth")
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
-    url.pathname = '/login'
+    url.pathname = "/login"
     return NextResponse.redirect(url)
   }
 
