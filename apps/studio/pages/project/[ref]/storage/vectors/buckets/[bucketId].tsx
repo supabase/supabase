f2ae460a40e461f6dad7229d6cfd7ad5768843aa
Danny White <3104761+dnywh@users.noreply.github.com>
2025-11-12 14:25:56 +1100

    clarify breadcrumbs

--- apps/studio/pages/project/[ref]/storage/vectors/buckets/[bucketId].tsx
+++ apps/studio/pages/project/[ref]/storage/vectors/buckets/[bucketId].tsx
@@ -5,6 +5,7 @@ import DefaultLayout from 'components/layouts/DefaultLayout'
 import { PageLayout } from 'components/layouts/PageLayout/PageLayout'
 import StorageLayout from 'components/layouts/StorageLayout/StorageLayout'
 import { DocsButton } from 'components/ui/DocsButton'
+import { Bucket as BucketIcon } from 'icons'
 import { useStorageExplorerStateSnapshot } from 'state/storage-explorer'
 import type { NextPageWithLayout } from 'types'
 
@@ -16,7 +17,17 @@ const VectorsBucketPage: NextPageWithLayout = () => {
   return (
     <PageLayout
       title={bucketId}
-      breadcrumbs={[{ label: 'Vectors', href: `/project/${projectRef}/storage/vectors` }]}
+      icon={
+        <div className="shrink-0 w-10 h-10 relative bg-surface-100 border rounded-md flex items-center justify-center">
+          <BucketIcon size={20} className="text-foreground-light" />
+        </div>
+      }
+      breadcrumbs={[
+        { label: 'Vectors', href: `/project/${projectRef}/storage/vectors` },
+        {
+          label: 'Buckets',
+        },
+      ]}
       secondaryActions={config?.docsUrl ? [<DocsButton key="docs" href={config.docsUrl} />] : []}
     >
       <div className="storage-container flex flex-grow">
