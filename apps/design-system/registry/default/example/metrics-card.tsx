f02d924af0117271e5bc6fcb955e1bc66ff8a0df
kemal <hello@kemal.earth>
2025-11-13 12:00:16 +0000

    fix: hydration issues

--- apps/design-system/registry/default/example/metrics-card.tsx
+++ apps/design-system/registry/default/example/metrics-card.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useEffect, useState } from 'react'
 import {
   MetricsCard,
   MetricsCardHeader,
@@ -11,76 +12,41 @@ import {
 } from 'ui-patterns/MetricsCard'
 
 export default function MetricsCardDemo() {
-  const data = Array.from({ length: 12 }, (_, i) => ({
-    value: Math.floor(4000 + i * 100 + (Math.random() * 2000 - 800)),
-    timestamp: new Date().toISOString(),
-  }))
+  const [data, setData] = useState<Array<{ value: number; timestamp: string }>>([])
+
+  useEffect(() => {
+    setData(
+      Array.from({ length: 12 }, (_, i) => ({
+        value: Math.floor(4000 + i * 100 + (Math.random() * 2000 - 800)),
+        timestamp: new Date().toISOString(),
+      }))
+    )
+  }, [])
 
   const averageValue = data.reduce((acc, curr) => acc + curr.value, 0) / data.length
 
-  const diff = data[data.length - 1].value - data[0].value
+  const diff = data[data.length - 1]?.value - data[0]?.value
   const diffPercentage = (diff / averageValue) * 100
 
   return (
-    <div className="w-full grid grid-cols-2 gap-5">
-      <div className="w-full">
-        <MetricsCard>
-          <MetricsCardHeader hasLink href="https://www.supabase.io">
-            <MetricsCardLabel hasTooltip tooltip="This is a tooltip">
-              Metrics Card
-            </MetricsCardLabel>
-          </MetricsCardHeader>
-          <MetricsCardContent>
-            <MetricsCardValue>{averageValue.toFixed(2).toLocaleString()}</MetricsCardValue>
-            <MetricsCardDifferential variant="positive">
-              {diffPercentage.toFixed(1)}%
-            </MetricsCardDifferential>
-          </MetricsCardContent>
-          <MetricsCardSparkline data={data} dataKey="value" />
-        </MetricsCard>
-      </div>
-      <div className="w-full">
-        <MetricsCard>
-          <MetricsCardHeader hasLink={false}>
-            <MetricsCardLabel hasTooltip={false}>Metrics Card</MetricsCardLabel>
-          </MetricsCardHeader>
-          <MetricsCardContent orientation="horizontal">
-            <MetricsCardValue>{averageValue.toFixed(2).toLocaleString()}</MetricsCardValue>
-            <MetricsCardDifferential variant="positive">
-              {diffPercentage.toFixed(1)}%
-            </MetricsCardDifferential>
-          </MetricsCardContent>
-          <MetricsCardSparkline data={data} dataKey="value" />
-        </MetricsCard>
-      </div>
-      <div className="w-full">
-        <MetricsCard>
-          <MetricsCardHeader hasLink href="https://www.supabase.io">
-            <MetricsCardLabel hasTooltip tooltip="Something here">
-              Metrics Card
-            </MetricsCardLabel>
-          </MetricsCardHeader>
-          <MetricsCardContent>
-            <MetricsCardValue>{averageValue.toFixed(2).toLocaleString()}</MetricsCardValue>
-            <MetricsCardDifferential variant="positive">
-              {diffPercentage.toFixed(1)}%
-            </MetricsCardDifferential>
-          </MetricsCardContent>
-        </MetricsCard>
-      </div>
-      <div className="w-full">
-        <MetricsCard>
-          <MetricsCardHeader hasLink={false}>
-            <MetricsCardLabel hasTooltip={false}>Metrics Card</MetricsCardLabel>
-          </MetricsCardHeader>
-          <MetricsCardContent orientation="horizontal">
-            <MetricsCardValue>{averageValue.toFixed(2).toLocaleString()}</MetricsCardValue>
-            <MetricsCardDifferential variant="positive">
-              {diffPercentage.toFixed(1)}%
-            </MetricsCardDifferential>
-          </MetricsCardContent>
-        </MetricsCard>
-      </div>
+    <div className="w-1/2">
+      <MetricsCard isLoading={!data.length}>
+        <MetricsCardHeader hasLink href="https://www.supabase.io">
+          <MetricsCardLabel hasTooltip tooltip="The number of Active Users over the last 24 hours">
+            Active Users
+          </MetricsCardLabel>
+        </MetricsCardHeader>
+        <MetricsCardContent>
+          <MetricsCardValue>
+            {averageValue.toLocaleString(undefined, { maximumFractionDigits: 0 })}
+          </MetricsCardValue>
+          <MetricsCardDifferential variant={diffPercentage > 0 ? 'positive' : 'negative'}>
+            {diffPercentage > 0 ? '+' : '-'}
+            {diffPercentage.toFixed(1)}%
+          </MetricsCardDifferential>
+        </MetricsCardContent>
+        <MetricsCardSparkline data={data} dataKey="value" />
+      </MetricsCard>
     </div>
   )
 }
