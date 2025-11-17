b1efc0c57a3d5121d82fa4f40d0b803eabcaa347
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2025-11-17 17:07:34 +0100

    add useQueryStateWithSelect to Webhooks

--- apps/studio/components/interfaces/Database/Hooks/DeleteHookModal.tsx
+++ apps/studio/components/interfaces/Database/Hooks/DeleteHookModal.tsx
@@ -9,9 +9,15 @@ interface DeleteHookModalProps {
   visible: boolean
   selectedHook?: PostgresTrigger
   onClose: () => void
+  onDeleteStart?: (hookId: string) => void
 }
 
-const DeleteHookModal = ({ selectedHook, visible, onClose }: DeleteHookModalProps) => {
+const DeleteHookModal = ({
+  selectedHook,
+  visible,
+  onClose,
+  onDeleteStart,
+}: DeleteHookModalProps) => {
   const { name, schema } = selectedHook ?? {}
 
   const { data: project } = useSelectedProjectQuery()
@@ -32,6 +38,7 @@ const DeleteHookModal = ({ selectedHook, visible, onClose }: DeleteHookModalProp
       return toast.error('Unable find selected hook')
     }
 
+    onDeleteStart?.(selectedHook.id.toString())
     deleteDatabaseTrigger({
       trigger: selectedHook,
       projectRef: project.ref,
