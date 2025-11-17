89fcf8b723d0b92c7f21a66aa0143302b5974baf
kallebysantos <kalleby_santos@hotmail.com>
2025-11-17 20:23:53 +0000

    stamp: general settings page local empty state

---
+++ apps/studio/components/interfaces/Settings/General/GeneralEmptyLocalState.tsx
@@ -0,0 +1,79 @@
+import { Card, CardContent, CardHeader, CardTitle } from 'ui'
+import { DOCS_URL, IS_LOCAL_CLI } from 'lib/constants'
+import { DocsButton } from 'components/ui/DocsButton'
+
+export function GeneralEmptyStateLocal() {
+  return (
+    <div className="flex flex-col gap-6">
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            Managing project settings {IS_LOCAL_CLI ? 'locally' : 'for self-hosted'}
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="p-0 grid grid-cols-[repeat(auto-fit,minmax(240px,1fr))] divide-y md:divide-y-0 md:divide-x divide-default items-stretch">
+          <div className="p-8">
+            <div className="flex items-center gap-2">
+              <h4 className="text-base text-foreground">Managing settings</h4>
+            </div>
+
+            <div className="text-sm text-foreground-light mt-1 mb-4 max-w-3xl">
+              <p className="prose [&>code]:text-xs space-x-1 text-sm max-w-full">
+                Local project config can be loaded through{' '}
+                {IS_LOCAL_CLI ? (
+                  <span>
+                    <code>config.toml</code> file placed at <code>supabase/config.toml</code>, which
+                    is automatically loaded on <code>supabase start</code>
+                  </span>
+                ) : (
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://github.com/supabase/supabase/blob/master/docker/.env.example"
+                  >
+                    .env file
+                  </a>
+                )}
+              </p>
+            </div>
+
+            {IS_LOCAL_CLI ? (
+              <DocsButton href={`${DOCS_URL}/guides/local-development/cli/config#general-config`} />
+            ) : (
+              <DocsButton href={`${DOCS_URL}/guides/self-hosting`} />
+            )}
+          </div>
+        </CardContent>
+      </Card>
+
+      <Card>
+        <CardContent className="p-0 grid grid-cols-[repeat(auto-fit,minmax(240px,1fr))] divide-y md:divide-y-0 md:divide-x divide-default items-stretch">
+          <div className="p-8">
+            <div className="flex items-center gap-2">
+              <h4 className="text-base text-foreground">Managing project execution</h4>
+            </div>
+
+            <div className="text-sm text-foreground-light mt-1 mb-4 max-w-3xl">
+              <p className="prose [&>code]:text-xs space-x-1 text-sm max-w-full">
+                Local project can be restarted/stopped through{' '}
+                {IS_LOCAL_CLI ? (
+                  <span>
+                    <code>supabase</code> cli
+                  </span>
+                ) : (
+                  <span>Docker</span>
+                )}
+              </p>
+            </div>
+
+            {IS_LOCAL_CLI ? (
+              <DocsButton href={`${DOCS_URL}/guides/local-development/cli/config#general-config`} />
+            ) : (
+              <DocsButton href={`${DOCS_URL}/guides/self-hosting/docker#restarting-all-services`} />
+            )}
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
