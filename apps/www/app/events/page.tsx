954a48fcd4a165d85c466efe6dfe4b8a854e63cd
Alan Daniel <stylesshjs@gmail.com>
2025-11-18 15:13:03 -0400

    metadata

--- apps/www/app/events/page.tsx
+++ apps/www/app/events/page.tsx
@@ -1,10 +1,12 @@
-import { cn } from 'ui'
-import DefaultLayout from '~/components/Layouts/Default'
-import SectionContainer from '~/components/Layouts/SectionContainer'
+import type { Metadata } from 'next'
 import { getStaticEvents } from '~/lib/events'
-import { EventsProvider } from './context'
-import { EventBanner } from '~/components/Events/new/EventBanner'
 import { EventClientRenderer } from '~/components/Events/new/EventClientRenderer'
+
+export const metadata: Metadata = {
+  title: 'View all Supabase events and meetups.',
+  description:
+    'Find all the upcoming events, webinars and meetups hosted by supabase and its community.',
+}
 
 export default async function EventsPage() {
   // This needs to be server-side as we use FS api.
