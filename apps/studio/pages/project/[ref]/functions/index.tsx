557fd1aeaa4d27356f679275f2d9b5433a82cb45
kallebysantos <kalleby_santos@hotmail.com>
2025-11-21 16:57:37 +0000

    stamp: displaying local functions fallback message

--- apps/studio/pages/project/[ref]/functions/index.tsx
+++ apps/studio/pages/project/[ref]/functions/index.tsx
@@ -37,6 +37,7 @@ import {
 } from 'ui-patterns/PageHeader'
 import { PageSection, PageSectionContent } from 'ui-patterns/PageSection'
 import { GenericSkeletonLoader } from 'ui-patterns/ShimmeringLoader'
+import { Admonition } from 'ui-patterns'
 
 const EdgeFunctionsPage: NextPageWithLayout = () => {
   const { ref } = useParams()
@@ -86,7 +87,16 @@ const EdgeFunctionsPage: NextPageWithLayout = () => {
         <PageSectionContent>
           <div className="flex flex-col gap-6">
             {isLoading && <GenericSkeletonLoader />}
-            {isError && <AlertError error={error} subject="Failed to retrieve edge functions" />}
+            {isError &&
+              (IS_PLATFORM ? (
+                <AlertError error={error} subject="Failed to retrieve edge functions" />
+              ) : (
+                <Admonition type="warning" title="Failed to retrieve edge functions">
+                  <p className="prose [&>code]:text-xs text-sm">
+                    Local functions can be found at <code>supabase/functions</code> folder.
+                  </p>
+                </Admonition>
+              ))}
             {isSuccess && (
               <>
                 {hasFunctions ? (
