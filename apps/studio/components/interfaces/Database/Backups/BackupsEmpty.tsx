68ee0a4bd0b5652a83de8302814f88cd82a6fab4
Danny White <3104761+dnywh@users.noreply.github.com>
2025-11-26 14:42:28 +1100

    fix

--- apps/studio/components/interfaces/Database/Backups/BackupsEmpty.tsx
+++ apps/studio/components/interfaces/Database/Backups/BackupsEmpty.tsx
@@ -2,7 +2,7 @@ import { DatabaseBackup } from 'lucide-react'
 
 export const BackupsEmpty = () => {
   return (
-    <aside className=" border border-dashed w-full bg-surface-100 rounded-lg px-4 py-10 flex flex-col gap-y-3 items-center text-center text-balance">
+    <aside className="border border-dashed w-full bg-surface-100 rounded-lg px-4 py-10 flex flex-col gap-y-3 items-center text-center text-balance">
       <DatabaseBackup size={24} strokeWidth={1.5} className="text-foreground-muted" />
 
       <div className="flex flex-col items-center text-center">
