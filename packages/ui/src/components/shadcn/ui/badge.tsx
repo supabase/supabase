6fae7ada83d10f6997048476abb1b9e8d4d008fe
Danny White <3104761+dnywh@users.noreply.github.com>
2025-11-26 17:00:56 +1100

    sensible defaults

--- packages/ui/src/components/shadcn/ui/badge.tsx
+++ packages/ui/src/components/shadcn/ui/badge.tsx
@@ -3,33 +3,39 @@ import * as React from 'react'
 
 import { cn } from '../../../lib/utils/cn'
 
-const badgeVariants = cva('inline-flex items-center rounded-full font-normal whitespace-nowrap', {
-  variants: {
-    variant: {
-      default: 'bg-surface-75 text-foreground-light border border-strong',
-      warning: 'bg-warning bg-opacity-10 text-warning border border-warning-500',
-      success: 'bg-brand bg-opacity-10 text-brand-600 border border-brand-500',
-      destructive:
-        'bg-destructive bg-opacity-10 text-destructive-600 border border-destructive-500',
-      brand: 'bg-brand bg-opacity-10 text-brand-600 border border-brand-500',
-      secondary:
-        'bg-secondary bg-opacity-10 hover:bg-secondary/80 border-transparent text-secondary-foreground',
-      outline: 'bg-transparent text border border-foreground-muted',
+const badgeVariants = cva(
+  'inline-flex items-center justify-center rounded-full font-normal whitespace-nowrap tracking-[0.07em] uppercase font-medium text-[9.5px] leading-[1.1] px-[5.5px] py-[3px]',
+  {
+    variants: {
+      variant: {
+        default: 'bg-surface-75 text-foreground-light border border-strong',
+        warning: 'bg-warning bg-opacity-10 text-warning border border-warning-500',
+        success: 'bg-brand bg-opacity-10 text-brand-600 border border-brand-500',
+        destructive:
+          'bg-destructive bg-opacity-10 text-destructive-600 border border-destructive-500',
+        brand: 'bg-brand bg-opacity-10 text-brand-600 border border-brand-500',
+        secondary:
+          'bg-secondary bg-opacity-10 hover:bg-secondary/80 border-transparent text-secondary-foreground',
+        outline: 'bg-transparent text border border-foreground-muted',
+      },
+      size: {
+        // tiny: 'px-1.5 py-[3px]'T,
+        // small: 'px-2 py-0.5',
+        // large: 'px-3 py-0.5 text-xs',
+        tiny: '',
+        small: '',
+        large: '',
+      },
+      dot: {
+        true: '-ml-0.5 mr-1.5 h-2 w-2',
+      },
     },
-    size: {
-      tiny: 'px-1.5 py-[3px] text-[10px] leading-none tracking-wide uppercase',
-      small: 'px-2 py-0.5 text-xs',
-      large: 'px-3 py-0.5 text-sm',
+    defaultVariants: {
+      variant: 'default',
+      size: 'small',
     },
-    dot: {
-      true: '-ml-0.5 mr-1.5 h-2 w-2',
-    },
-  },
-  defaultVariants: {
-    variant: 'default',
-    size: 'small',
-  },
-})
+  }
+)
 
 export interface BadgeProps
   extends React.HTMLAttributes<HTMLDivElement>,
@@ -37,7 +43,6 @@ export interface BadgeProps
 
 function Badge({
   className,
-  type = 'default',
   variant = 'default',
   size = 'small',
   dot = false,
@@ -45,10 +50,7 @@ function Badge({
   ...props
 }: BadgeProps) {
   return (
-    <div
-      className={cn(badgeVariants({ variant, size, type }), 'leading-none', className)}
-      {...props}
-    >
+    <div className={cn(badgeVariants({ variant, size }), className)} {...props}>
       {dot && (
         <svg className={badgeVariants({ dot })} fill="currentColor" viewBox="0 0 8 8">
           <circle cx="4" cy="4" r="3" />
