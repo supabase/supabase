adf760ebf41facae9054d733222a6a872157ea44
Saxon Fletcher <saxonafletcher@gmail.com>
2025-11-28 15:22:02 +1000

    Auth interface components new page components (#40670)
    * pages first
    
    * auth interface components new page components
    
    * fixes
    
    * update components
    
    * fix

--- apps/studio/components/interfaces/Auth/ThirdPartyAuthForm/index.tsx
+++ apps/studio/components/interfaces/Auth/ThirdPartyAuthForm/index.tsx
@@ -4,11 +4,6 @@ import { useState } from 'react'
 import { toast } from 'sonner'
 
 import { useParams } from 'common'
-import {
-  ScaffoldSection,
-  ScaffoldSectionDescription,
-  ScaffoldSectionTitle,
-} from 'components/layouts/Scaffold'
 import AlertError from 'components/ui/AlertError'
 import { DocsButton } from 'components/ui/DocsButton'
 import { InlineLink } from 'components/ui/InlineLink'
@@ -21,6 +16,15 @@ import { useAsyncCheckPermissions } from 'hooks/misc/useCheckPermissions'
 import { DOCS_URL } from 'lib/constants'
 import { cn } from 'ui'
 import ConfirmationModal from 'ui-patterns/Dialogs/ConfirmationModal'
+import {
+  PageSection,
+  PageSectionAside,
+  PageSectionContent,
+  PageSectionDescription,
+  PageSectionMeta,
+  PageSectionSummary,
+  PageSectionTitle,
+} from 'ui-patterns/PageSection'
 import { AddIntegrationDropdown } from './AddIntegrationDropdown'
 import { CreateAuth0IntegrationDialog } from './CreateAuth0Dialog'
 import { CreateAwsCognitoAuthIntegrationDialog } from './CreateAwsCognitoAuthDialog'
@@ -65,11 +69,11 @@ export const ThirdPartyAuthForm = () => {
   }
 
   return (
-    <ScaffoldSection isFullWidth>
-      <div className="flex justify-between gap-4">
-        <div>
-          <ScaffoldSectionTitle>Third Party Auth</ScaffoldSectionTitle>
-          <ScaffoldSectionDescription className="mb-6">
+    <PageSection>
+      <PageSectionMeta>
+        <PageSectionSummary>
+          <PageSectionTitle>Third Party Auth</PageSectionTitle>
+          <PageSectionDescription>
             Use third-party authentication (TPA) systems based on JWTs to access your project.
             <br />
             Billing is based on the number of monthly active users (MAUs) requesting your API
@@ -80,120 +84,124 @@ export const ThirdPartyAuthForm = () => {
               billing docs
             </InlineLink>{' '}
             for more information.
-          </ScaffoldSectionDescription>
-        </div>
-        <div className="flex items-center gap-2 ">
-          <DocsButton href={`${DOCS_URL}/guides/auth/third-party/overview`} />
-          {integrations.length !== 0 && (
-            <AddIntegrationDropdown onSelectIntegrationType={setSelectedIntegration} />
-          )}
-        </div>
-      </div>
-
-      {isLoading && (
-        <div
-          className={cn(
-            'border rounded border-default px-20 py-16 flex flex-col items-center justify-center space-y-4'
-          )}
-        >
-          <Loader2 size={24} className="animate-spin" />
-        </div>
-      )}
-
-      {isSuccess ? (
-        integrations.length === 0 ? (
+          </PageSectionDescription>
+        </PageSectionSummary>
+        <PageSectionAside>
+          <div className="flex items-center gap-2">
+            <DocsButton href={`${DOCS_URL}/guides/auth/third-party/overview`} />
+            {integrations.length !== 0 && (
+              <AddIntegrationDropdown onSelectIntegrationType={setSelectedIntegration} />
+            )}
+          </div>
+        </PageSectionAside>
+      </PageSectionMeta>
+      <PageSectionContent>
+        {isLoading && (
           <div
             className={cn(
               'border rounded border-default px-20 py-16 flex flex-col items-center justify-center space-y-4'
             )}
           >
-            <p className="text-sm text-foreground-light">No providers configured yet</p>
-            <AddIntegrationDropdown
-              align="center"
-              buttonText="Add a new integration"
-              onSelectIntegrationType={setSelectedIntegration}
-            />
+            <Loader2 size={24} className="animate-spin" />
           </div>
-        ) : (
-          <div className="-space-y-px">
-            {integrations.map((integration) => {
-              return (
-                <IntegrationCard
-                  key={integration.id}
-                  integration={integration}
-                  canUpdateConfig={canUpdateConfig}
-                  onDelete={() => {
-                    setSelectedIntegrationForDeletion(integration)
-                  }}
-                />
-              )
-            })}
-          </div>
-        )
-      ) : null}
+        )}
 
-      <CreateFirebaseAuthIntegrationDialog
-        visible={selectedIntegration === 'firebase'}
-        onDelete={() => {}}
-        onClose={() => setSelectedIntegration(undefined)}
-      />
+        {isSuccess ? (
+          integrations.length === 0 ? (
+            <div
+              className={cn(
+                'border rounded border-default px-20 py-16 flex flex-col items-center justify-center space-y-4'
+              )}
+            >
+              <p className="text-sm text-foreground-light">No providers configured yet</p>
+              <AddIntegrationDropdown
+                align="center"
+                buttonText="Add a new integration"
+                onSelectIntegrationType={setSelectedIntegration}
+              />
+            </div>
+          ) : (
+            <div className="-space-y-px">
+              {integrations.map((integration) => {
+                return (
+                  <IntegrationCard
+                    key={integration.id}
+                    integration={integration}
+                    canUpdateConfig={canUpdateConfig}
+                    onDelete={() => {
+                      setSelectedIntegrationForDeletion(integration)
+                    }}
+                  />
+                )
+              })}
+            </div>
+          )
+        ) : null}
 
-      <CreateAwsCognitoAuthIntegrationDialog
-        visible={selectedIntegration === 'awsCognito'}
-        onDelete={() => {}}
-        onClose={() => setSelectedIntegration(undefined)}
-      />
+        <CreateFirebaseAuthIntegrationDialog
+          visible={selectedIntegration === 'firebase'}
+          onDelete={() => {}}
+          onClose={() => setSelectedIntegration(undefined)}
+        />
 
-      <CreateAuth0IntegrationDialog
-        visible={selectedIntegration === 'auth0'}
-        onDelete={() => {}}
-        onClose={() => setSelectedIntegration(undefined)}
-      />
+        <CreateAwsCognitoAuthIntegrationDialog
+          visible={selectedIntegration === 'awsCognito'}
+          onDelete={() => {}}
+          onClose={() => setSelectedIntegration(undefined)}
+        />
 
-      <CreateClerkAuthIntegrationDialog
-        visible={selectedIntegration === 'clerk'}
-        onDelete={() => {}}
-        onClose={() => setSelectedIntegration(undefined)}
-      />
+        <CreateAuth0IntegrationDialog
+          visible={selectedIntegration === 'auth0'}
+          onDelete={() => {}}
+          onClose={() => setSelectedIntegration(undefined)}
+        />
 
-      <CreateWorkOSIntegrationDialog
-        visible={selectedIntegration === 'workos'}
-        onDelete={() => {}}
-        onClose={() => setSelectedIntegration(undefined)}
-      />
+        <CreateClerkAuthIntegrationDialog
+          visible={selectedIntegration === 'clerk'}
+          onDelete={() => {}}
+          onClose={() => setSelectedIntegration(undefined)}
+        />
 
-      <ConfirmationModal
-        size="medium"
-        visible={!!selectedIntegrationForDeletion}
-        variant="destructive"
-        title="Confirm to delete integration"
-        confirmLabel="Delete"
-        confirmLabelLoading="Deleting"
-        onCancel={() => setSelectedIntegrationForDeletion(undefined)}
-        onConfirm={async () => {
-          if (!selectedIntegrationForDeletion) {
-            return
-          }
-          const type = getIntegrationType(selectedIntegrationForDeletion)
-          try {
-            await deleteIntegration({
-              projectRef: projectRef!,
-              tpaId: selectedIntegrationForDeletion.id,
-            })
-            toast.success(`Successfully deleted ${getIntegrationTypeLabel(type)}.`)
-            setSelectedIntegrationForDeletion(undefined)
-            setSelectedIntegration(undefined)
-          } catch (error) {
-            toast.error(`Failed to delete ${getIntegrationTypeLabel(type)}.`)
-            console.error(error)
-          }
-        }}
-      >
-        <p className="text-sm text-foreground-light">
-          Are you sure you want to delete the{' '}
-          {getIntegrationTypeLabel(getIntegrationType(selectedIntegrationForDeletion))} integration?
-        </p>
-      </ConfirmationModal>
-    </ScaffoldSection>
+        <CreateWorkOSIntegrationDialog
+          visible={selectedIntegration === 'workos'}
+          onDelete={() => {}}
+          onClose={() => setSelectedIntegration(undefined)}
+        />
+
+        <ConfirmationModal
+          size="medium"
+          visible={!!selectedIntegrationForDeletion}
+          variant="destructive"
+          title="Confirm to delete integration"
+          confirmLabel="Delete"
+          confirmLabelLoading="Deleting"
+          onCancel={() => setSelectedIntegrationForDeletion(undefined)}
+          onConfirm={async () => {
+            if (!selectedIntegrationForDeletion) {
+              return
+            }
+            const type = getIntegrationType(selectedIntegrationForDeletion)
+            try {
+              await deleteIntegration({
+                projectRef: projectRef!,
+                tpaId: selectedIntegrationForDeletion.id,
+              })
+              toast.success(`Successfully deleted ${getIntegrationTypeLabel(type)}.`)
+              setSelectedIntegrationForDeletion(undefined)
+              setSelectedIntegration(undefined)
+            } catch (error) {
+              toast.error(`Failed to delete ${getIntegrationTypeLabel(type)}.`)
+              console.error(error)
+            }
+          }}
+        >
+          <p className="text-sm text-foreground-light">
+            Are you sure you want to delete the{' '}
+            {getIntegrationTypeLabel(getIntegrationType(selectedIntegrationForDeletion))}{' '}
+            integration?
+          </p>
+        </ConfirmationModal>
+      </PageSectionContent>
+    </PageSection>
   )
 }
