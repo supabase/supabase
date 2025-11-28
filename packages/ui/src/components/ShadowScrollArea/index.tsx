691844e8093da64b96fc12b353470fc349c4da9a
kemal.earth <606977+kemaldotearth@users.noreply.github.com>
2025-11-28 09:32:52 +0000

    fix(studio): z-index of layout sidebar (#40832)
    * fix: z index of layout sidebar
    
    * fix: actually fix it on the table instead

--- packages/ui/src/components/ShadowScrollArea/index.tsx
+++ packages/ui/src/components/ShadowScrollArea/index.tsx
@@ -25,19 +25,19 @@ const ShadowScrollArea = React.forwardRef<HTMLDivElement, ShadowScrollAreaProps>
       '[&_td:last-child]:before:bottom-0 [&_td:last-child]:before:w-6 [&_td:last-child]:before:bg-gradient-to-l',
       '[&_td:last-child]:before:from-black/5 dark:[&_td:last-child]:before:from-black/20 [&_td:last-child]:before:to-transparent',
       '[&_td:last-child]:before:opacity-0 [&_td:last-child]:before:transition-all [&_td:last-child]:before:duration-400',
-      '[&_td:last-child]:before:easing-[0.24, 0.25, 0.05, 1] [&_td:last-child]:before:z-[60]',
+      '[&_td:last-child]:before:easing-[0.24, 0.25, 0.05, 1] [&_td:last-child]:before:z-[39]',
       '[&_th:last-child]:before:absolute [&_th:last-child]:before:top-0 [&_th:last-child]:before:-left-6',
       '[&_th:last-child]:before:bottom-0 [&_th:last-child]:before:w-6 [&_th:last-child]:before:bg-gradient-to-l',
       '[&_th:last-child]:before:from-black/5 dark:[&_th:last-child]:before:from-black/20 [&_th:last-child]:before:to-transparent',
       '[&_th:last-child]:before:opacity-0 [&_th:last-child]:before:transition-all [&_th:last-child]:before:duration-400',
-      '[&_th:last-child]:before:easing-[0.24, 0.25, 0.05, 1] [&_th:last-child]:before:z-[60]'
+      '[&_th:last-child]:before:easing-[0.24, 0.25, 0.05, 1] [&_th:last-child]:before:z-[39]'
     )
 
     return (
       <div className={cn(containerClassName, 'relative')}>
         <div
           className={cn(
-            'absolute inset-0 pointer-events-none z-50',
+            'absolute inset-0 pointer-events-none z-[38]',
             'before:absolute before:top-0 before:right-0 before:bottom-0 before:w-6 before:bg-gradient-to-l before:from-black/5 dark:before:from-black/20 before:to-transparent before:opacity-0 before:transition-all before:duration-400 before:easing-[0.24, 0.25, 0.05, 1]',
             'after:absolute after:top-0 after:left-0 after:bottom-0 after:w-6 after:bg-gradient-to-r after:from-black/5 dark:after:from-black/20 after:to-transparent after:opacity-0 after:transition-all after:duration-400 after:easing-[0.24, 0.25, 0.05, 1]',
             hasHorizontalScroll && 'hover:before:opacity-100 hover:after:opacity-100',
@@ -50,7 +50,7 @@ const ShadowScrollArea = React.forwardRef<HTMLDivElement, ShadowScrollAreaProps>
           className={cn(
             'w-full overflow-auto',
             stickyLastColumn && [
-              '[&_tr>*:last-child]:sticky [&_tr>*:last-child]:z-50 [&_tr>*:last-child]:right-0',
+              '[&_tr>*:last-child]:sticky [&_tr>*:last-child]:z-[38] [&_tr>*:last-child]:right-0',
               '[&_tr:hover>*:last-child]:bg-transparent',
               '[&_th>*:last-child]:bg-surface-100',
               stickyColumnShadow,
