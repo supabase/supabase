a0a7e03b6f4112d0fa15c51249cce9d5108cf144
Stojan Dimitrovski <sdimitrovski@gmail.com>
2025-12-03 11:54:20 +0100

    fix compilation

--- apps/studio/components/interfaces/Auth/PerformanceSettingsForm.tsx
+++ apps/studio/components/interfaces/Auth/PerformanceSettingsForm.tsx
@@ -100,7 +100,7 @@ export const PerformanceSettingsForm = () => {
       })
     ),
     defaultValues: {
-      DB_MAX_POOL_SIZE: undefined,
+      DB_MAX_POOL_SIZE: 10,
       DB_MAX_POOL_SIZE_UNIT: 'connections',
     },
   })
@@ -118,7 +118,7 @@ export const PerformanceSettingsForm = () => {
       if (!isUpdatingDatabaseForm) {
         databaseForm.reset({
           DB_MAX_POOL_SIZE:
-            authConfig?.DB_MAX_POOL_SIZE !== null ? authConfig?.DB_MAX_POOL_SIZE : 10,
+            authConfig?.DB_MAX_POOL_SIZE !== null ? (authConfig?.DB_MAX_POOL_SIZE ?? 10) : 10,
           DB_MAX_POOL_SIZE_UNIT:
             authConfig?.DB_MAX_POOL_SIZE_UNIT !== null
               ? authConfig?.DB_MAX_POOL_SIZE_UNIT
