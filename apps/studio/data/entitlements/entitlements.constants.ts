6843321b00365ae17b3517c99d9e326ed6c8ecba
Ignacio Dobronich <ignacio@dobronich.com>
2025-12-05 15:30:12 -0300

    Done

--- apps/studio/data/entitlements/entitlements.constants.ts
+++
@@ -1,5 +0,0 @@
-import type { components } from 'api-types'
-
-export type FeatureKey =
-  components['schemas']['ListEntitlementsResponse']['entitlements'][number]['feature']['key']
-export type Feature = (typeof Feature)[keyof typeof Feature]
