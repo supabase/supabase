734c7f1989b3369b211810d85466de9c323dc929
Saxon Fletcher <saxonafletcher@gmail.com>
2025-12-09 11:59:53 +1000

    update account me page

--- apps/studio/components/interfaces/Account/Preferences/AccountIdentities.tsx
+++ apps/studio/components/interfaces/Account/Preferences/AccountIdentities.tsx
@@ -7,13 +7,14 @@ import { useEffect, useState } from 'react'
 import { toast } from 'sonner'
 
 import { ButtonTooltip } from 'components/ui/ButtonTooltip'
-import Panel from 'components/ui/Panel'
 import { useProfileIdentitiesQuery } from 'data/profile/profile-identities-query'
 import { useUnlinkIdentityMutation } from 'data/profile/profile-unlink-identity-mutation'
 import { BASE_PATH } from 'lib/constants'
 import {
   Badge,
   Button,
+  Card,
+  CardContent,
   cn,
   Dialog,
   DialogContent,
@@ -30,6 +31,14 @@ import {
   GitHubChangeEmailAddress,
   SSOChangeEmailAddress,
 } from './ChangeEmailAddress'
+import {
+  PageSection,
+  PageSectionContent,
+  PageSectionDescription,
+  PageSectionMeta,
+  PageSectionSummary,
+  PageSectionTitle,
+} from 'ui-patterns/PageSection'
 
 const getProviderName = (provider: string) =>
   provider === 'github'
@@ -71,126 +80,134 @@ export const AccountIdentities = () => {
   }, [message])
 
   return (
-    <>
-      <Panel className="mb-4 md:mb-8" title={<h5>Account Identities</h5>}>
-        {isLoading && (
-          <Panel.Content>
-            <ShimmeringLoader />
-          </Panel.Content>
-        )}
-        {isSuccess &&
-          identities.map((identity) => {
-            const { identity_id, provider } = identity
-            const username = identity.identity_data?.user_name
-            const providerName = getProviderName(provider)
-            const iconKey =
-              provider === 'github'
-                ? 'github-icon'
-                : provider === 'email'
-                  ? 'email-icon2'
-                  : 'saml-icon'
-
-            return (
-              <Panel.Content
-                key={identity_id}
-                className={cn('flex justify-between', identities.length > 1 ? 'last:border-t' : '')}
-              >
-                <div className="flex gap-x-4">
-                  <Image
-                    className={cn(iconKey === 'github-icon' ? 'dark:invert' : '')}
-                    src={`${BASE_PATH}/img/icons/${iconKey}.svg`}
-                    width={30}
-                    height={30}
-                    alt={`${identity.provider} icon`}
-                  />
-                  <div>
-                    <div className="flex items-center gap-x-2">
-                      <p className="text-sm capitalize">{providerName}</p>
-                      {provider === 'email' && data.new_email && !isChangeExpired && (
-                        <Tooltip>
-                          <TooltipTrigger className="flex items-center">
-                            <Badge variant="default">Pending change</Badge>
-                          </TooltipTrigger>
-                          <TooltipContent>Changing to {data.new_email}</TooltipContent>
-                        </Tooltip>
-                      )}
-                      {/* [Joshen] Below is not supported yet, but ideal UX */}
-                      {/* {false && <Badge>Logged in as</Badge>} */}
-                    </div>
-                    <p className="text-sm text-foreground-lighter">
-                      {!!username ? <span>{username} • </span> : null}
-                      {identity.email}
-                    </p>
-                  </div>
-                </div>
-                <div className="flex items-center gap-x-1">
-                  {provider === 'email' && (
-                    <Button asChild type="default">
-                      <Link href="/reset-password">Reset password</Link>
-                    </Button>
-                  )}
-                  <ButtonTooltip
-                    type="text"
-                    icon={<Edit />}
-                    className="w-7"
-                    onClick={() => setSelectedProviderUpdateEmail(provider)}
-                    tooltip={{ content: { side: 'bottom', text: 'Update email address' } }}
-                  />
-                  {identities.length > 1 && (
-                    <ButtonTooltip
-                      type="text"
-                      icon={<Unlink />}
-                      className="w-7"
-                      onClick={() => setSelectedProviderUnlink(provider)}
-                      tooltip={{ content: { side: 'bottom', text: 'Unlink identity' } }}
-                    />
-                  )}
-                </div>
-              </Panel.Content>
-            )
-          })}
-      </Panel>
-
-      <Dialog
-        open={!!selectedProviderUpdateEmail}
-        onOpenChange={(open: boolean) => {
-          if (!open) setSelectedProviderUpdateEmail(undefined)
-        }}
-      >
-        <DialogContent>
-          <DialogHeader className="border-b">
-            <DialogTitle>
-              {selectedProviderUpdateEmail !== 'email'
-                ? `Updating email address for ${getProviderName(selectedProviderUpdateEmail ?? '')} identity`
-                : 'Update email address'}
-            </DialogTitle>
-          </DialogHeader>
-          {selectedProviderUpdateEmail === 'github' ? (
-            <GitHubChangeEmailAddress />
-          ) : selectedProviderUpdateEmail?.startsWith('sso') ? (
-            <SSOChangeEmailAddress />
-          ) : (
-            <ChangeEmailAddressForm onClose={() => setSelectedProviderUpdateEmail(undefined)} />
+    <PageSection>
+      <PageSectionMeta>
+        <PageSectionSummary>
+          <PageSectionTitle>Account identities</PageSectionTitle>
+          <PageSectionDescription>
+            Manage the providers linked to your Supabase account and update their details.
+          </PageSectionDescription>
+        </PageSectionSummary>
+      </PageSectionMeta>
+      <PageSectionContent>
+        <Card>
+          {isLoading && (
+            <CardContent>
+              <ShimmeringLoader />
+            </CardContent>
           )}
-        </DialogContent>
-      </Dialog>
+          {isSuccess && (
+            <div className="divide-y">
+              {identities.map((identity) => {
+                const { identity_id, provider } = identity
+                const username = identity.identity_data?.user_name
+                const providerName = getProviderName(provider)
+                const iconKey =
+                  provider === 'github'
+                    ? 'github-icon'
+                    : provider === 'email'
+                      ? 'email-icon2'
+                      : 'saml-icon'
 
-      <ConfirmationModal
-        variant="warning"
-        size="small"
-        loading={isUnlinking}
-        visible={!!selectedProviderUnlink}
-        title={`Unlink ${getProviderName(selectedProviderUnlink ?? '')} identity`}
-        onCancel={() => setSelectedProviderUnlink(undefined)}
-        onConfirm={onConfirmUnlinkIdentity}
-        confirmLabel="Unlink identity"
-        confirmLabelLoading="Unlinking identity"
-        alert={{
-          base: { variant: 'warning' },
-          title: `Confirm to disconnect your ${getProviderName(selectedProviderUnlink ?? '')} identity`,
-          description: `After disconnecting, you will only be able to sign in via ${selectedProviderUnlink === 'github' ? 'email and password' : 'your GitHub identity'}`,
-        }}
-      />
-    </>
+                return (
+                  <CardContent key={identity_id} className="flex justify-between items-center py-4">
+                    <div className="flex gap-x-4">
+                      <Image
+                        className={cn(iconKey === 'github-icon' ? 'dark:invert' : '')}
+                        src={`${BASE_PATH}/img/icons/${iconKey}.svg`}
+                        width={30}
+                        height={30}
+                        alt={`${identity.provider} icon`}
+                      />
+                      <div>
+                        <div className="flex items-center gap-x-2">
+                          <p className="text-sm capitalize">{providerName}</p>
+                          {provider === 'email' && data.new_email && !isChangeExpired && (
+                            <Tooltip>
+                              <TooltipTrigger className="flex items-center">
+                                <Badge variant="default">Pending change</Badge>
+                              </TooltipTrigger>
+                              <TooltipContent>Changing to {data.new_email}</TooltipContent>
+                            </Tooltip>
+                          )}
+                        </div>
+                        <p className="text-sm text-foreground-lighter">
+                          {!!username ? <span>{username} • </span> : null}
+                          {identity.email}
+                        </p>
+                      </div>
+                    </div>
+                    <div className="flex items-center gap-x-1">
+                      {provider === 'email' && (
+                        <Button asChild type="default">
+                          <Link href="/reset-password">Reset password</Link>
+                        </Button>
+                      )}
+                      <ButtonTooltip
+                        type="text"
+                        icon={<Edit />}
+                        className="w-7"
+                        onClick={() => setSelectedProviderUpdateEmail(provider)}
+                        tooltip={{ content: { side: 'bottom', text: 'Update email address' } }}
+                      />
+                      {identities.length > 1 && (
+                        <ButtonTooltip
+                          type="text"
+                          icon={<Unlink />}
+                          className="w-7"
+                          onClick={() => setSelectedProviderUnlink(provider)}
+                          tooltip={{ content: { side: 'bottom', text: 'Unlink identity' } }}
+                        />
+                      )}
+                    </div>
+                  </CardContent>
+                )
+              })}
+            </div>
+          )}
+        </Card>
+
+        <Dialog
+          open={!!selectedProviderUpdateEmail}
+          onOpenChange={(open: boolean) => {
+            if (!open) setSelectedProviderUpdateEmail(undefined)
+          }}
+        >
+          <DialogContent>
+            <DialogHeader className="border-b">
+              <DialogTitle>
+                {selectedProviderUpdateEmail !== 'email'
+                  ? `Updating email address for ${getProviderName(selectedProviderUpdateEmail ?? '')} identity`
+                  : 'Update email address'}
+              </DialogTitle>
+            </DialogHeader>
+            {selectedProviderUpdateEmail === 'github' ? (
+              <GitHubChangeEmailAddress />
+            ) : selectedProviderUpdateEmail?.startsWith('sso') ? (
+              <SSOChangeEmailAddress />
+            ) : (
+              <ChangeEmailAddressForm onClose={() => setSelectedProviderUpdateEmail(undefined)} />
+            )}
+          </DialogContent>
+        </Dialog>
+
+        <ConfirmationModal
+          variant="warning"
+          size="small"
+          loading={isUnlinking}
+          visible={!!selectedProviderUnlink}
+          title={`Unlink ${getProviderName(selectedProviderUnlink ?? '')} identity`}
+          onCancel={() => setSelectedProviderUnlink(undefined)}
+          onConfirm={onConfirmUnlinkIdentity}
+          confirmLabel="Unlink identity"
+          confirmLabelLoading="Unlinking identity"
+          alert={{
+            base: { variant: 'warning' },
+            title: `Confirm to disconnect your ${getProviderName(selectedProviderUnlink ?? '')} identity`,
+            description: `After disconnecting, you will only be able to sign in via ${selectedProviderUnlink === 'github' ? 'email and password' : 'your GitHub identity'}`,
+          }}
+        />
+      </PageSectionContent>
+    </PageSection>
   )
 }
