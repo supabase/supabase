fece34a18e2544035dc104da8b7bac03df29a991
Ali Waseem <waseema393@gmail.com>
2025-12-09 13:32:16 -0700

    fix flag lib

--- packages/common/feature-flags.tsx
+++ packages/common/feature-flags.tsx
@@ -162,14 +162,19 @@ const isObjectEmpty = (obj: Object) => {
   return Object.keys(obj).length === 0
 }
 
-export function useFlag<T = boolean>(name: string) {
+export function useFlag<T = boolean>(name: string, defaultValue = false) {
   const flagStore = useFeatureFlags()
-
   const store = flagStore.configcat
 
-  if (!isObjectEmpty(store) && store[name] === undefined) {
-    console.error(`Flag key "${name}" does not exist in ConfigCat flag store`)
-    return false
+  // Flag store is empty means config cat is not loaded yet, return default value
+  if (isObjectEmpty(store)) {
+    return defaultValue
   }
+
+  if (store[name] === undefined) {
+    console.error(`Flag key "${name}" does not exist in ConfigCat flag store`)
+    return defaultValue
+  }
+
   return store[name] as T
 }
