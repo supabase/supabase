8c20934067ef1ee1a71c6f7b3bac9f81596c7bb4
Joshen Lim <joshenlimek@gmail.com>
2025-12-09 12:07:28 +0800

    Remove unused old env var

--- apps/studio/lib/api/apiHelpers.ts
+++ apps/studio/lib/api/apiHelpers.ts
@@ -25,9 +25,7 @@ export function constructHeaders(headers: { [prop: string]: any }) {
     )
     return {
       ...cleansedHeaders,
-      ...(IS_PLATFORM
-        ? { apiKey: `${process.env.READ_ONLY_API_KEY}` }
-        : { apiKey: `${process.env.SUPABASE_SERVICE_KEY}` }),
+      ...(IS_PLATFORM ? {} : { apiKey: `${process.env.SUPABASE_SERVICE_KEY}` }),
     }
   } else {
     return {
