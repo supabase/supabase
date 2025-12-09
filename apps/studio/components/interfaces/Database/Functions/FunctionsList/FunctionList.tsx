6e7a3940c715214d10223e13ca40d8674511a9cc
marsou001 <61951643+marsou001@users.noreply.github.com>
2025-12-09 17:52:48 +0100

    modify disabled db function button style

--- apps/studio/components/interfaces/Database/Functions/FunctionsList/FunctionList.tsx
+++ apps/studio/components/interfaces/Database/Functions/FunctionsList/FunctionList.tsx
@@ -107,7 +107,7 @@ const FunctionList = ({
             <TableCell className="truncate">
               <Button
                 type="text"
-                className="text-link-table-cell text-sm p-0 hover:bg-transparent title"
+                className="text-link-table-cell text-sm disabled:opacity-100 disabled:no-underline p-0 hover:bg-transparent title"
                 disabled={isLocked}
                 onClick={() => editFunction(x)}
                 title={x.name}
