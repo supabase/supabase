fa087a15645faedf28fa058918aae9fb7d7f1b2a
kemal <hello@kemal.earth>
2025-12-11 17:01:01 +0000

    feat: update colours icon to francescos

--- apps/design-system/app/(app)/page.tsx
+++ apps/design-system/app/(app)/page.tsx
@@ -53,11 +53,7 @@ export default function Home() {
           <Link href="/docs/color-usage" className="h-full flex">
             <div className="p-6 gap-4 flex flex-col justify-between h-full w-full bg-surface-75 hover:bg-overlay/50 hover:border-foreground-muted cursor-pointer transition-all border rounded-md">
               <div className="flex flex-col gap-2 min-h-[24px]">
-                <div className="flex items-center flex-row flex-wrap gap-2">
-                  <div className="w-5 h-5 border border-brand flex-shrink-0" />
-                  <div className="w-5 h-5 border border-warning flex-shrink-0" />
-                  <div className="w-5 h-5 border border-destructive flex-shrink-0" />
-                </div>
+                <HomepageSvgHandler name="colours" className="w-6 h-6" />
               </div>
               <div>
                 <h3 className="font-medium text-foreground">Colors</h3>
