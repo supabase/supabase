c9e5dd622b34ad9a7a8f6bf9ce92492072e761bd
kemal <hello@kemal.earth>
2025-12-11 16:10:41 +0000

    fix: sidebar complaining about dialog title

--- apps/design-system/app/(app)/layout.tsx
+++ apps/design-system/app/(app)/layout.tsx
@@ -1,6 +1,7 @@
 import { SideNavigation } from '@/components/side-navigation'
 import { SiteFooter } from '@/components/site-footer'
 import { TopNavigation } from '@/components/top-navigation'
+import { MobileSidebarSheet } from '@/components/mobile-sidebar-sheet'
 import { ScrollArea } from 'ui'
 
 interface AppLayoutProps {
@@ -11,6 +12,7 @@ export default async function AppLayout({ children }: AppLayoutProps) {
   return (
     <>
       <TopNavigation />
+      <MobileSidebarSheet />
       <main className="flex-1 max-w-site mx-auto w-full border-l border-r border-b">
         <div className="flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] lg:grid-cols-[240px_minmax(0,1fr)]">
           <aside className="fixed top-10 z-30 hidden h-[calc(100vh-3rem)] w-full shrink-0 md:sticky md:block border-r">
