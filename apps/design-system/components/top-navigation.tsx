1069d3dc81c54ed642b7218c442d4cea33142d58
kemal <hello@kemal.earth>
2025-12-11 13:28:33 +0000

    fix: header classes and unused elements

--- apps/design-system/components/top-navigation.tsx
+++ apps/design-system/components/top-navigation.tsx
@@ -6,10 +6,9 @@ import { ThemeSwitcherDropdown } from './theme-switcher-dropdown'
 
 function TopNavigation() {
   return (
-    <header className="sticky top-0 z-50 w-full border-t bg-studio/95 backdrop-blur supports-[backdrop-filter]:bg-studio/60">
-      <div className="absolute border-b border-dashed w-full top-[3.4rem] -z-10"></div>
-      <nav className="h-14 w-full flex">
-        <div className="max-w-site border-b w-full flex flex-row items-center gap-6 mx-auto px-6 border-r border-l justify-between">
+    <header className="sticky top-0 z-50 w-full bg-studio/95 backdrop-blur supports-[backdrop-filter]:bg-studio/60 border-b border-l border-r">
+      <nav className="py-3 w-full flex">
+        <div className="max-w-site w-full flex flex-row items-center gap-6 mx-auto px-6 justify-between">
           <div className="flex items-center">
             <Link href="/" className="flex items-center lg:gap-6 gap-4">
               <h1 className="hidden md:flex line-clamp-1 lg:text-2xl text-base">
