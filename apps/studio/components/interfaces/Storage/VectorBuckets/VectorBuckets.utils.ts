fcd863b86e1a343e346173ed523ccf9ebfbb6b8d
Joshen Lim <joshenlimek@gmail.com>
2025-12-11 17:28:10 +0800

    Clean up

--- apps/studio/components/interfaces/Storage/VectorBuckets/VectorBuckets.utils.ts
+++ apps/studio/components/interfaces/Storage/VectorBuckets/VectorBuckets.utils.ts
@@ -4,10 +4,6 @@ export const getVectorBucketS3KeyName = (bucketId: string) => {
   return `${snakeCase(bucketId)}_keys`
 }
 
-export const getVectorBucketFDWSchemaName = (bucketId: string) => {
-  return `fdw_vector_${snakeCase(bucketId)}`
-}
-
 export const getVectorBucketFDWServerName = (bucketId: string) => {
   return `${getVectorBucketFDWName(bucketId)}_server`
 }
