42e09c06896ec7b995df794448059dc75b663dc5
Chris Chinchilla <chris@chrischinchilla.com>
2025-12-16 10:00:10 +0100

    Revert "docs: Remove Auth Helpers (#40986)"
    This reverts commit 9274b81bd76a9fa74e51d1c8577185e4db43fb29.

--- apps/www/lib/redirects.js
+++ apps/www/lib/redirects.js
@@ -1300,8 +1300,8 @@ module.exports = [
   },
   {
     permanent: true,
-    source: '/docs/guides/auth/auth-helpers/:path*',
-    destination: '/docs/guides/auth/server-side/migrating-to-ssr-from-auth-helpers',
+    source: '/docs/guides/auth/auth-helpers/auth-ui-overview',
+    destination: '/docs/guides/auth/auth-helpers/auth-ui',
   },
   {
     permanent: false,
@@ -2157,7 +2157,11 @@ module.exports = [
     source: '/customers/mendableai',
     destination: '/customers/firecrawl',
   },
-
+  {
+    permanent: true,
+    source: '/docs/guides/auth/auth-helpers/nextjs-server-components',
+    destination: '/docs/guides/auth/auth-helpers/nextjs',
+  },
   {
     permanent: true,
     source: '/docs/guides/getting-started/openai/vector-search',
