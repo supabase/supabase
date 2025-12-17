539d5a31fb1457ffe64ab79ccf3fb7f68ea17fdc
Ivan Vasilov <vasilov.ivan@gmail.com>
2025-12-17 10:06:44 +0200

    fix: Sort the folders in the SQL Editor (#41386)
    * Sort the folders according to the selected sort approach.
    
    * Minor fixes.
    
    * Minor fixes and clarifications.
    
    * Remove a useEffect dep.

--- apps/studio/state/sql-editor-v2.ts
+++ apps/studio/state/sql-editor-v2.ts
@@ -262,7 +262,9 @@ export const useSnippetFolders = (projectRef: string) => {
     () =>
       Object.values(snapshot.folders)
         .filter((x) => x.projectRef === projectRef)
-        .map((x) => x.folder),
+        .map((x) => x.folder)
+        // folders don't have created_at or inserted_at, so we always sort by name
+        .sort((a, b) => a.name.localeCompare(b.name)),
     [projectRef, snapshot.folders]
   )
 }
