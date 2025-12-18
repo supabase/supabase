6ae6d2aaeea85b73c5a7d9f24edb207a4a280cc0
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2025-12-18 22:24:02 +0100

    chore(studio): conditionally show product commands based on permisions

--- apps/studio/components/interfaces/App/CommandMenu/CreateCommands.tsx
+++ apps/studio/components/interfaces/App/CommandMenu/CreateCommands.tsx
@@ -80,6 +80,18 @@ export function useCreateCommands(options?: CommandOptions) {
     'authentication:performance',
   ])
 
+  const {
+    projectAuthAll: authEnabled,
+    projectEdgeFunctionAll: edgeFunctionsEnabled,
+    projectStorageAll: storageEnabled,
+    realtimeAll: realtimeEnabled,
+  } = useIsFeatureEnabled([
+    'project_auth:all',
+    'project_edge_function:all',
+    'project_storage:all',
+    'realtime:all',
+  ])
+
   const databaseCommands = useMemo(
     () =>
       [
@@ -174,80 +186,83 @@ export function useCreateCommands(options?: CommandOptions) {
 
   const authCommands = useMemo(
     () =>
-      [
-        {
-          id: 'create-auth-user',
-          name: 'Create Auth User',
-          route: `/project/${ref}/auth/users?new=true`,
-          icon: () => <UserPlus />,
-        },
-        {
-          id: 'create-rls-policy',
-          name: 'Create RLS Policy',
-          route: `/project/${ref}/auth/policies?new=true`,
-          icon: () => <ShieldPlus />,
-        },
-        ...(IS_PLATFORM
-          ? [
-              {
-                id: 'create-auth-hook-sms',
-                name: 'Create Auth Hook (SMS)',
-                route: `/project/${ref}/auth/hooks?hook=${sendSmsHook?.id}`,
-                icon: () => <MessageCircle />,
-              },
-              {
-                id: 'create-auth-hook-email',
-                name: 'Create Auth Hook (Email)',
-                route: `/project/${ref}/auth/hooks?hook=${sendEmailHook?.id}`,
-                icon: () => <Mail />,
-              },
-              {
-                id: 'create-auth-hook-custom-access-token',
-                name: 'Create Auth Hook (Custom Access Token)',
-                route: `/project/${ref}/auth/hooks?hook=${customAccessTokenHook?.id}`,
-                icon: () => <KeyRound />,
-              },
-              {
-                id: 'create-auth-hook-mfa-verification',
-                name: 'Create Auth Hook (MFA Verification Attempt)',
-                route: `/project/${ref}/auth/hooks?hook=${mfaVerificationHook?.id}`,
-                icon: () => <ShieldPlus />,
-                badge: () => (mfaVerificationHookEnabled ? <Badge>Team</Badge> : null),
-                className: mfaVerificationHookEnabled
-                  ? 'opacity-50 cursor-not-allowed pointer-events-none'
-                  : '',
-              },
-              {
-                id: 'create-auth-hook-password-verification',
-                name: 'Create Auth Hook (Password Verification Attempt)',
-                route: `/project/${ref}/auth/hooks?hook=${passwordVerificationHook?.id}`,
-                icon: () => <Lock />,
-                badge: () => (passwordVerificationHookEnabled ? <Badge>Team</Badge> : null),
-                className: passwordVerificationHookEnabled
-                  ? 'opacity-50 cursor-not-allowed pointer-events-none'
-                  : '',
-              },
-              {
-                id: 'create-auth-hook-before-user-created',
-                name: 'Create Auth Hook (Before User Created)',
-                route: `/project/${ref}/auth/hooks?hook=${beforeUserCreatedHook?.id}`,
-                icon: () => <KeyRound />,
-              },
-            ]
-          : []),
-        ...(IS_PLATFORM && authenticationOauth21
-          ? [
-              {
-                id: 'create-oauth-app',
-                name: 'Create OAuth App',
-                route: `/project/${ref}/auth/oauth-apps?new=true`,
-                icon: () => <KeyRound />,
-              },
-            ]
-          : []),
-      ].filter(Boolean) as ICommand[],
+      authEnabled
+        ? ([
+            {
+              id: 'create-auth-user',
+              name: 'Create Auth User',
+              route: `/project/${ref}/auth/users?new=true`,
+              icon: () => <UserPlus />,
+            },
+            {
+              id: 'create-rls-policy',
+              name: 'Create RLS Policy',
+              route: `/project/${ref}/auth/policies?new=true`,
+              icon: () => <ShieldPlus />,
+            },
+            ...(IS_PLATFORM
+              ? [
+                  {
+                    id: 'create-auth-hook-sms',
+                    name: 'Create Auth Hook (SMS)',
+                    route: `/project/${ref}/auth/hooks?hook=${sendSmsHook?.id}`,
+                    icon: () => <MessageCircle />,
+                  },
+                  {
+                    id: 'create-auth-hook-email',
+                    name: 'Create Auth Hook (Email)',
+                    route: `/project/${ref}/auth/hooks?hook=${sendEmailHook?.id}`,
+                    icon: () => <Mail />,
+                  },
+                  {
+                    id: 'create-auth-hook-custom-access-token',
+                    name: 'Create Auth Hook (Custom Access Token)',
+                    route: `/project/${ref}/auth/hooks?hook=${customAccessTokenHook?.id}`,
+                    icon: () => <KeyRound />,
+                  },
+                  {
+                    id: 'create-auth-hook-mfa-verification',
+                    name: 'Create Auth Hook (MFA Verification Attempt)',
+                    route: `/project/${ref}/auth/hooks?hook=${mfaVerificationHook?.id}`,
+                    icon: () => <ShieldPlus />,
+                    badge: () => (mfaVerificationHookEnabled ? <Badge>Team</Badge> : null),
+                    className: mfaVerificationHookEnabled
+                      ? 'opacity-50 cursor-not-allowed pointer-events-none'
+                      : '',
+                  },
+                  {
+                    id: 'create-auth-hook-password-verification',
+                    name: 'Create Auth Hook (Password Verification Attempt)',
+                    route: `/project/${ref}/auth/hooks?hook=${passwordVerificationHook?.id}`,
+                    icon: () => <Lock />,
+                    badge: () => (passwordVerificationHookEnabled ? <Badge>Team</Badge> : null),
+                    className: passwordVerificationHookEnabled
+                      ? 'opacity-50 cursor-not-allowed pointer-events-none'
+                      : '',
+                  },
+                  {
+                    id: 'create-auth-hook-before-user-created',
+                    name: 'Create Auth Hook (Before User Created)',
+                    route: `/project/${ref}/auth/hooks?hook=${beforeUserCreatedHook?.id}`,
+                    icon: () => <KeyRound />,
+                  },
+                ]
+              : []),
+            ...(IS_PLATFORM && authenticationOauth21
+              ? [
+                  {
+                    id: 'create-oauth-app',
+                    name: 'Create OAuth App',
+                    route: `/project/${ref}/auth/oauth-apps?new=true`,
+                    icon: () => <KeyRound />,
+                  },
+                ]
+              : []),
+          ].filter(Boolean) as ICommand[])
+        : [],
     [
       ref,
+      authEnabled,
       sendSmsHook?.id,
       sendEmailHook?.id,
       customAccessTokenHook?.id,
@@ -262,61 +277,68 @@ export function useCreateCommands(options?: CommandOptions) {
 
   const edgeFunctionsCommands = useMemo(
     () =>
-      [
-        {
-          id: 'create-edge-function-editor',
-          name: 'Create Edge Function via Editor',
-          route: `/project/${ref}/functions/new`,
-          icon: () => <EdgeFunctions />,
-        },
-        {
-          id: 'create-edge-function-cli',
-          name: 'Create Edge Function via CLI',
-          route: `/project/${ref}/functions?create=cli`,
-          icon: () => <EdgeFunctions />,
-        },
-        {
-          id: 'create-edge-function-ai',
-          name: 'Create Edge Function via AI',
-          action: () => {
-            openSidebar(SIDEBAR_KEYS.AI_ASSISTANT)
-            snap.newChat({
-              name: 'Create new edge function',
-              initialInput: `Create a new edge function that ...`,
-              suggestions: {
-                title:
-                  'I can help you create a new edge function. Here are a few example prompts to get you started:',
-                prompts: [
-                  {
-                    label: 'Stripe Payments',
-                    description: 'Create a new edge function that processes payments with Stripe',
+      edgeFunctionsEnabled
+        ? ([
+            {
+              id: 'create-edge-function-editor',
+              name: 'Create Edge Function via Editor',
+              route: `/project/${ref}/functions/new`,
+              icon: () => <EdgeFunctions />,
+            },
+            {
+              id: 'create-edge-function-cli',
+              name: 'Create Edge Function via CLI',
+              route: `/project/${ref}/functions?create=cli`,
+              icon: () => <EdgeFunctions />,
+            },
+            {
+              id: 'create-edge-function-ai',
+              name: 'Create Edge Function via AI',
+              action: () => {
+                openSidebar(SIDEBAR_KEYS.AI_ASSISTANT)
+                snap.newChat({
+                  name: 'Create new edge function',
+                  initialInput: `Create a new edge function that ...`,
+                  suggestions: {
+                    title:
+                      'I can help you create a new edge function. Here are a few example prompts to get you started:',
+                    prompts: [
+                      {
+                        label: 'Stripe Payments',
+                        description:
+                          'Create a new edge function that processes payments with Stripe',
+                      },
+                      {
+                        label: 'Email with Resend',
+                        description: 'Create a new edge function that sends emails with Resend',
+                      },
+                      {
+                        label: 'PDF Generator',
+                        description:
+                          'Create a new edge function that generates PDFs from HTML templates',
+                      },
+                    ],
                   },
-                  {
-                    label: 'Email with Resend',
-                    description: 'Create a new edge function that sends emails with Resend',
-                  },
-                  {
-                    label: 'PDF Generator',
-                    description:
-                      'Create a new edge function that generates PDFs from HTML templates',
-                  },
-                ],
+                })
+                setIsOpen(false)
               },
-            })
-            setIsOpen(false)
-          },
-          icon: () => (
-            <AiIconAnimation allowHoverEffect={false} size={20} className="text-foreground-light" />
-          ),
-        },
-        {
-          id: 'create-edge-function-secret',
-          name: 'Create Edge Function Secret',
-          route: `/project/${ref}/functions/secrets`,
-          icon: () => <LockKeyhole />,
-        },
-      ].filter(Boolean) as ICommand[],
-    [ref, openSidebar, snap, setIsOpen]
+              icon: () => (
+                <AiIconAnimation
+                  allowHoverEffect={false}
+                  size={20}
+                  className="text-foreground-light"
+                />
+              ),
+            },
+            {
+              id: 'create-edge-function-secret',
+              name: 'Create Edge Function Secret',
+              route: `/project/${ref}/functions/secrets`,
+              icon: () => <LockKeyhole />,
+            },
+          ].filter(Boolean) as ICommand[])
+        : [],
+    [ref, edgeFunctionsEnabled, openSidebar, snap, setIsOpen]
   )
 
   const isVectorBucketsEnabled = useIsVectorBucketsEnabled({ projectRef: ref })
@@ -324,29 +346,31 @@ export function useCreateCommands(options?: CommandOptions) {
 
   const storageCommands = useMemo(
     () =>
-      [
-        {
-          id: 'create-storage-bucket-files',
-          name: 'Create Storage Bucket (Files)',
-          route: `/project/${ref}/storage/files?new=true`,
-          icon: () => <FolderPlus />,
-        },
-        {
-          id: 'create-storage-bucket-analytics',
-          name: 'Create Storage Bucket (Analytics)',
-          route: `/project/${ref}/storage/analytics?new=true`,
-          icon: () => <BarChart3 />,
-          badge: () => (isAnalyticsBucketsEnabled ? <Badge>Pro</Badge> : null),
-        },
-        {
-          id: 'create-storage-bucket-vectors',
-          name: 'Create Storage Bucket (Vectors)',
-          route: `/project/${ref}/storage/vectors?new=true`,
-          icon: () => <Binary />,
-          badge: () => (isVectorBucketsEnabled ? <Badge>Pro</Badge> : null),
-        },
-      ].filter(Boolean) as ICommand[],
-    [ref]
+      storageEnabled
+        ? ([
+            {
+              id: 'create-storage-bucket-files',
+              name: 'Create Storage Bucket (Files)',
+              route: `/project/${ref}/storage/files?new=true`,
+              icon: () => <FolderPlus />,
+            },
+            {
+              id: 'create-storage-bucket-analytics',
+              name: 'Create Storage Bucket (Analytics)',
+              route: `/project/${ref}/storage/analytics?new=true`,
+              icon: () => <BarChart3 />,
+              badge: () => (isAnalyticsBucketsEnabled ? <Badge>Pro</Badge> : null),
+            },
+            {
+              id: 'create-storage-bucket-vectors',
+              name: 'Create Storage Bucket (Vectors)',
+              route: `/project/${ref}/storage/vectors?new=true`,
+              icon: () => <Binary />,
+              badge: () => (isVectorBucketsEnabled ? <Badge>Pro</Badge> : null),
+            },
+          ].filter(Boolean) as ICommand[])
+        : [],
+    [ref, storageEnabled, isAnalyticsBucketsEnabled, isVectorBucketsEnabled]
   )
 
   const { installedIntegrations } = useInstalledIntegrations()
@@ -395,46 +419,61 @@ export function useCreateCommands(options?: CommandOptions) {
     [ref, installedIntegrationIds]
   )
 
+  const sections = useMemo(
+    () => [
+      {
+        id: 'create-database',
+        name: 'Database',
+        commands: databaseCommands,
+      },
+      ...(authCommands.length > 0
+        ? [
+            {
+              id: 'create-auth',
+              name: 'Auth',
+              commands: authCommands,
+            },
+          ]
+        : []),
+      ...(edgeFunctionsCommands.length > 0
+        ? [
+            {
+              id: 'create-edge-functions',
+              name: 'Edge Functions',
+              commands: edgeFunctionsCommands,
+            },
+          ]
+        : []),
+      ...(storageCommands.length > 0
+        ? [
+            {
+              id: 'create-storage',
+              name: 'Storage',
+              commands: storageCommands,
+            },
+          ]
+        : []),
+      ...(integrationsCommands.length > 0
+        ? [
+            {
+              id: 'create-integrations',
+              name: 'Integrations',
+              commands: integrationsCommands,
+            },
+          ]
+        : []),
+    ],
+    [databaseCommands, authCommands, edgeFunctionsCommands, storageCommands, integrationsCommands]
+  )
+
   useRegisterPage(
     CREATE_STUDIO_ENTITY,
     {
       type: PageType.Commands,
-      sections: [
-        {
-          id: 'create-database',
-          name: 'Database',
-          commands: databaseCommands,
-        },
-        {
-          id: 'create-auth',
-          name: 'Auth',
-          commands: authCommands,
-        },
-        {
-          id: 'create-edge-functions',
-          name: 'Edge Functions',
-          commands: edgeFunctionsCommands,
-        },
-        {
-          id: 'create-storage',
-          name: 'Storage',
-          commands: storageCommands,
-        },
-        {
-          id: 'create-integrations',
-          name: 'Integrations',
-          commands: integrationsCommands,
-        },
-      ],
+      sections,
     },
     {
-      deps: [
-        databaseCommands,
-        authCommands,
-        edgeFunctionsCommands,
-        storageCommands,
-        integrationsCommands,
-      ],
+      deps: [sections],
     }
   )
 
