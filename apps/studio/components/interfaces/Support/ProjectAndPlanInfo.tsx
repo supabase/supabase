2fbb6cb9a7d6fb0a10f74b0a421af33038e557c7
Danny White <3104761+dnywh@users.noreply.github.com>
2025-12-18 10:16:06 +1000

    chore(studio): clean up support form (#41424)
    * admonition
    
    * clean up
    
    * clean up
    
    * plan info improvements
    
    * fix wonky text area
    
    * improve form
    
    * submit button
    
    * success state
    
    * hide callouts on success
    
    * fixes
    
    * fixes
    
    * nit
    
    * rabbit
    
    * deprecate InformationBox in favour of Admonition
    
    * fix tests
    
    * fix
    
    * rabbit
    
    * rabbit
    
    * rabbit
    
    * lint
    
    * US English

--- apps/studio/components/interfaces/Support/ProjectAndPlanInfo.tsx
+++ apps/studio/components/interfaces/Support/ProjectAndPlanInfo.tsx
@@ -1,5 +1,5 @@
 import { AnimatePresence, motion } from 'framer-motion'
-import { AlertCircle, Check, ChevronsUpDown, ExternalLink } from 'lucide-react'
+import { Check, ChevronsUpDown, ExternalLink } from 'lucide-react'
 import Link from 'next/link'
 import type { UseFormReturn } from 'react-hook-form'
 import { toast } from 'sonner'
@@ -7,12 +7,11 @@ import { toast } from 'sonner'
 
 import { useParams } from 'common'
 import CopyButton from 'components/ui/CopyButton'
-import InformationBox from 'components/ui/InformationBox'
 import { OrganizationProjectSelector } from 'components/ui/OrganizationProjectSelector'
 import { useIsFeatureEnabled } from 'hooks/misc/useIsFeatureEnabled'
 import {
-  Button,
   cn,
+  Button,
   CommandGroup_Shadcn_,
   CommandItem_Shadcn_,
   FormControl_Shadcn_,
@@ -20,8 +19,7 @@ import {
 } from 'ui'
 import { Admonition } from 'ui-patterns'
 import { FormItemLayout } from 'ui-patterns/form/FormItemLayout/FormItemLayout'
-import { ShimmeringLoader } from 'ui-patterns/ShimmeringLoader'
-import { useHighlightProjectRefContext } from './HighlightContext'
+import ShimmeringLoader from 'ui-patterns/ShimmeringLoader'
 import type { ExtendedSupportCategories } from './Support.constants'
 import type { SupportFormValues } from './SupportForm.schema'
 import { NO_ORG_MARKER, NO_PROJECT_MARKER } from './SupportForm.utils'
@@ -43,17 +41,14 @@ export function ProjectAndPlanInfo({
   subscriptionPlanId,
   showPlanExpectationInfo = true,
 }: ProjectAndPlanProps) {
-  const { ref } = useHighlightProjectRefContext()
   const hasProjectSelected = projectRef && projectRef !== NO_PROJECT_MARKER
 
   return (
-    <div ref={ref} className={'flex flex-col gap-y-2'}>
+    <div className="flex flex-col gap-y-2">
       <ProjectSelector form={form} orgSlug={orgSlug} projectRef={projectRef} />
       <ProjectRefHighlighted projectRef={projectRef} />
 
-      {!hasProjectSelected && (
-        <Admonition type="default" title="Please note that no project has been selected" />
-      )}
+      {!hasProjectSelected && <Admonition type="default" title="No project has been selected" />}
 
       {showPlanExpectationInfo &&
         orgSlug &&
@@ -125,7 +120,7 @@ function ProjectSelector({ form, orgSlug, projectRef }: ProjectSelectorProps) {
                     }}
                   >
                     {field.value === NO_PROJECT_MARKER && <Check size={16} />}
-                    <p className={field.value !== NO_PROJECT_MARKER ? 'ml-6' : ''}>
+                    <p className={cn(field.value !== NO_PROJECT_MARKER && 'ml-6')}>
                       No specific project
                     </p>
                   </CommandItem_Shadcn_>
@@ -146,9 +141,6 @@ interface ProjectRefHighlightedProps {
 function ProjectRefHighlighted({ projectRef }: ProjectRefHighlightedProps) {
   const isVisible = !!projectRef && projectRef !== NO_PROJECT_MARKER
 
-  const { shouldHighlightRef, setShouldHighlightRef: setHighlight } =
-    useHighlightProjectRefContext()
-
   return (
     <AnimatePresence>
       {isVisible && (
@@ -159,32 +151,15 @@ function ProjectRefHighlighted({ projectRef }: ProjectRefHighlightedProps) {
           transition={{ duration: 0.3 }}
           className="flex items-center gap-x-1"
         >
-          <p
-            className={cn(
-              'text-sm prose transition',
-              shouldHighlightRef ? 'text-foreground' : 'text-foreground-lighter'
-            )}
-          >
+          <p className="text-sm transition text-foreground-lighter">
             Project ID:{' '}
-            <code
-              className={cn(
-                'transition',
-                shouldHighlightRef
-                  ? 'text-brand font-medium border-brand-500 animate-pulse'
-                  : 'text-foreground-light'
-              )}
-            >
-              {projectRef}
-            </code>
+            <code className="text-code-inline !text-foreground-light">{projectRef}</code>
           </p>
           <CopyButton
             iconOnly
             type="text"
             text={projectRef}
-            onClick={() => {
-              toast.success('Copied to clipboard')
-              setHighlight(false)
-            }}
+            onClick={() => toast.success('Copied project ID to clipboard')}
           />
         </motion.div>
       )}
@@ -199,63 +174,58 @@ interface PlanExpectationInfoBoxProps {
 
 const PlanExpectationInfoBox = ({ orgSlug, planId }: PlanExpectationInfoBoxProps) => {
   const { billingAll } = useIsFeatureEnabled(['billing:all'])
+  const shouldShowUpgradeActions = billingAll && planId !== 'enterprise'
 
   return (
-    <InformationBox
-      icon={<AlertCircle size={18} strokeWidth={2} />}
-      defaultVisibility={true}
-      hideCollapse={true}
-      title="Expected response times are based on your organization's plan"
+    <Admonition
+      type="default"
+      title="Expected response times are based on your organization’s plan"
       description={
-        <div className="flex flex-col gap-y-4 mb-1">
+        <>
           {planId === 'free' && (
             <p>
-              Free Plan support is available within the community and officially by the team on a
-              best efforts basis. For a guaranteed response we recommend upgrading to the Pro Plan.
-              Enhanced SLAs for support are available on our Enterprise Plan.
+              Support on the Free plan is provided through the community and by the team on a
+              best-effort basis. For a guaranteed response time, we recommend upgrading to the Pro
+              plan. Enhanced support SLAs are available on the Enterprise plan.
             </p>
           )}
 
           {planId === 'pro' && (
             <p>
-              Pro Plan includes email-based support. You can expect an answer within 1 business day
-              in most situations for all severities. We recommend upgrading to the Team Plan for
-              prioritized ticketing on all issues and prioritized escalation to product engineering
-              teams. Enhanced SLAs for support are available on our Enterprise Plan.
+              The Pro plan includes email support. In most cases, you can expect a response within 1
+              business day for all severities. For prioritized ticketing on all issues and
+              prioritized escalation to product engineering, we recommend upgrading to the Team
+              plan. Enhanced support SLAs are available on the Enterprise plan.
             </p>
           )}
 
           {planId === 'team' && (
             <p>
-              Team Plan includes email-based support. You get prioritized ticketing on all issues
-              and prioritized escalation to product engineering teams. Low, Normal, and High
-              severity tickets will generally be handled within 1 business day, while Urgent issues,
-              we respond within 1 day, 365 days a year. Enhanced SLAs for support are available on
-              our Enterprise Plan.
+              The Team plan includes email support with prioritized ticketing and escalation to
+              product engineering. Low, normal, and high-severity tickets are typically handled
+              within 1 business day. Urgent issues are handled within 1 day, 365 days a year.
+              Enhanced support SLAs are available on the Enterprise plan.
             </p>
           )}
-
-          {billingAll && planId !== 'enterprise' && (
-            <div className="flex flex-col sm:flex-row items-start sm:items-center gap-y-2 sm:gap-x-2">
-              <Button asChild>
-                <Link
-                  href={`/org/${orgSlug}/billing?panel=subscriptionPlan&source=planSupportExpectationInfoBox`}
-                >
-                  Upgrade project
-                </Link>
-              </Button>
-              <Button asChild type="default" icon={<ExternalLink />}>
-                <Link
-                  href="https://supabase.com/contact/enterprise"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Enquire about Enterprise
-                </Link>
-              </Button>
-            </div>
-          )}
-        </div>
+        </>
+      }
+      actions={
+        shouldShowUpgradeActions && (
+          <>
+            <Button asChild>
+              <Link
+                href={`/org/${orgSlug}/billing?panel=subscriptionPlan&source=planSupportExpectationInfoBox`}
+              >
+                Upgrade plan
+              </Link>
+            </Button>
+            <Button asChild type="default" icon={<ExternalLink />}>
+              <Link href="https://supabase.com/contact/enterprise" target="_blank" rel="noreferrer">
+                Enquire about Enterprise
+              </Link>
+            </Button>
+          </>
+        )
       }
     />
   )
