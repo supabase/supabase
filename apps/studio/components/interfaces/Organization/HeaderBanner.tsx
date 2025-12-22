af7992b9369d361b8a5536d700a1d075579b9cec
Danny White <3104761+dnywh@users.noreply.github.com>
2025-12-22 10:18:28 +1000

    design polish

--- apps/studio/components/interfaces/Organization/HeaderBanner.tsx
+++ apps/studio/components/interfaces/Organization/HeaderBanner.tsx
@@ -28,87 +28,67 @@ export const HeaderBanner = ({
   title,
   message,
   link,
+  linkText = 'Details',
 }: {
-  type: 'danger' | 'warning' | 'note' | 'incident'
+  type: 'danger' | 'warning' | 'note'
   title: string
   message: string
   link?: string
+  linkText?: string
 }) => {
   const bannerStyles =
     type === 'danger'
-      ? 'bg-destructive-300 dark:bg-destructive-200'
-      : type === 'incident'
-        ? 'bg-brand-400'
-        : 'bg-warning-300 dark:bg-warning-200'
+      ? 'bg-destructive-200 border-destructive-400'
+      : type === 'warning'
+        ? 'bg-warning-200 border-warning-400'
+        : 'bg-surface-200/25 border-default'
   const Icon = type === 'danger' ? CriticalIcon : WarningIcon
+  const iconStyles =
+    type === 'danger'
+      ? 'text-destructive-200 bg-destructive-600'
+      : type === 'warning'
+        ? 'text-warning-200 bg-warning-600'
+        : 'text-background bg-foreground'
 
   return (
     <motion.div
       {...bannerMotionProps}
       className={cn(
-        `relative ${bannerStyles} border-b border-muted py-1 flex items-center justify-center flex-shrink-0 px-0`,
-        type === 'incident' && 'hover:bg-brand-300',
-        'flex-shrink-0'
+        'relative border-b py-3 flex items-center justify-center flex-shrink-0 px-0',
+        bannerStyles
       )}
       layout="position"
     >
-      <div className={cn('items-center flex flex-row gap-3')}>
-        <div className="absolute inset-y-0 left-0 right-0 overflow-hidden z-0">
-          <div
-            className="absolute inset-0 opacity-[0.8%]"
-            style={{
-              background: `repeating-linear-gradient(
-                    45deg,
-                    currentColor,
-                    currentColor 10px,
-                    transparent 10px,
-                    transparent 20px
-                  )`,
-              maskImage: 'linear-gradient(to top, black, transparent)',
-              WebkitMaskImage: 'linear-gradient(to top, black, transparent)',
-            }}
-          />
+      <div
+        className="absolute inset-0 opacity-[1.6%] dark:opacity-[0.8%]"
+        style={{
+          background: `repeating-linear-gradient(
+            45deg,
+            currentColor,
+            currentColor 10px,
+            transparent 10px,
+            transparent 20px
+          )`,
+          maskImage: 'linear-gradient(to bottom, transparent 0%, black 90%)',
+          WebkitMaskImage: 'linear-gradient(to bottom, transparent 0%, black 90%)',
+        }}
+      />
+      {/* Text content and link */}
+      <div className="relative z-[1] items-center flex flex-row gap-2">
+        <Icon className={cn('flex-shrink-0', iconStyles)} />
+        {/* Required text */}
+        <div className="flex flex-col md:flex-row gap-0 md:gap-2.5">
+          <p className="text-sm text-foreground font-medium">{title}</p>
+          <p className="text-sm text-foreground-light">{message}</p>
         </div>
-        <Icon
-          className={cn('z-[1] flex-shrink-0', type === 'incident' && 'bg-brand text-brand-200')}
-        />
-        <div className="flex flex-col md:flex-row gap-0 md:gap-3">
-          <span
-            className={cn(
-              'text-xs sm:text-sm z-[1]',
-              type === 'danger'
-                ? 'text-destructive'
-                : type === 'incident'
-                  ? 'text-foreground'
-                  : 'text-warning'
-            )}
-          >
-            {title}
-          </span>
-          <span
-            className={cn(
-              'text-xs sm:text-sm z-[1] opacity-75',
-              type === 'danger'
-                ? 'text-destructive'
-                : type === 'incident'
-                  ? 'text-foreground'
-                  : 'text-warning'
-            )}
-          >
-            {message}
-          </span>
-        </div>
+        {/* Optional link */}
         {link && (
-          <Link
-            href={link}
-            className={cn(
-              'lg:block hidden',
-              'text-foreground-lighter text-sm z-[1] m-0',
-              type === 'danger' ? 'text-destructive' : 'text-warning'
-            )}
-          >
-            View Details
-          </Link>
+          <div className="hidden lg:flex text-sm flex-row items-center gap-2">
+            <span className="text-foreground-muted">·</span>
+            <p className="text-foreground-light underline underline-offset-2 decoration-foreground-muted/80 hover:decoration-foreground transition-all">
+              <Link href={link}>{linkText}</Link>
+            </p>
+          </div>
         )}
       </div>
     </motion.div>
