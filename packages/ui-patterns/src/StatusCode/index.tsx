45239efe6adf2dbf998716fb0052380643a1da20
kemal <hello@kemal.earth>
2026-01-22 12:56:15 +0000

    feat: create statuscode component

---
+++ packages/ui-patterns/src/StatusCode/index.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export interface StatusCodeProps {
+  type: 'success' | 'warning' | 'error' | 'info' | 'default'
+}
+
+export const StatusCode = () => {
+  return <div>Status code</div>
+}
