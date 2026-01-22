ff9071ded79b10147fefd0bdbfdf362fd6b53473
Danny White <3104761+dnywh@users.noreply.github.com>
2026-01-22 17:06:01 +1000

    rabbit

--- apps/studio/components/ui/UpgradePlanButton.tsx
+++ apps/studio/components/ui/UpgradePlanButton.tsx
@@ -95,7 +95,7 @@ export const UpgradePlanButton = ({
     return (
       <ButtonTooltip
         disabled
-        type="primary"
+        type={variant}
         className={className}
         tooltip={{
           content: {
