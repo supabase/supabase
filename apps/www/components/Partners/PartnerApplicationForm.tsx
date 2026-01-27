d0cb12f10e3c911c8bd286df22a246eaa756af52
Alan Daniel <stylesshjs@gmail.com>
2026-01-27 10:54:20 -0400

    nits

--- apps/www/components/Partners/PartnerApplicationForm.tsx
+++ apps/www/components/Partners/PartnerApplicationForm.tsx
@@ -512,8 +512,7 @@ const PartnerApplicationForm: FC<Props> = ({ className }) => {
                         {isCheckingSlug && (
                           <div className="absolute right-3 top-1/2 -translate-y-1/2">
                             <Loader2
-                              className="h-4 w-4 text-foreground-lighter"
-                              style={{ animation: 'spin 1s linear infinite' }}
+                              className="h-4 w-4 text-foreground-lighter animate-spinner"
                             />
                           </div>
                         )}
