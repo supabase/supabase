dc31696c8577476c8ff6be9c06fc200f3dd84b14
Joshen Lim <joshenlimek@gmail.com>
2026-01-28 15:40:19 +0800

    Remove alpha callout from replication page (#42203)
    * Remove alpha callout from replication page
    
    * PRETTY

--- apps/studio/components/interfaces/Database/Replication/DestinationPanel/DestinationTypeSelection.tsx
+++ apps/studio/components/interfaces/Database/Replication/DestinationPanel/DestinationTypeSelection.tsx
@@ -1,7 +1,9 @@
 import { useFlag } from 'common'
 import { AnalyticsBucket, BigQuery, Database } from 'icons'
-import { Badge, cn, RadioGroupStacked, RadioGroupStackedItem } from 'ui'
+import { Badge, RadioGroupStacked, RadioGroupStackedItem, cn } from 'ui'
+
 import { DestinationType } from './DestinationPanel.types'
+import { InlineLink } from '@/components/ui/InlineLink'
 
 type DestinationTypeSelectionProps = {
   editMode: boolean
@@ -101,6 +103,15 @@ export const DestinationTypeSelection = ({
           </RadioGroupStackedItem>
         )}
       </RadioGroupStacked>
+
+      {selectedType !== 'Read Replica' && (
+        <p className="mt-3 text-sm text-foreground-light">
+          Replication is in alpha. Expect rapid changes and possible breaking updates.{' '}
+          <InlineLink href="https://github.com/orgs/supabase/discussions/39416">
+            Leave feedback
+          </InlineLink>
+        </p>
+      )}
     </div>
   )
 }
