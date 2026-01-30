1e8851fa3e918b2229010e4c0170fbccb2fb6843
Jordi Enric <jordi.err@gmail.com>
2026-01-30 14:04:31 +0100

    rm barrel

--- apps/studio/components/ui/Logs/index.ts
+++
@@ -1,3 +0,0 @@
-export { ChartIntervalDropdown } from './ChartIntervalDropdown'
-export { LOG_RETENTION, CHART_INTERVALS } from './logs.utils'
-export { default as LogsExplorerHeader } from './LogsExplorerHeader'
