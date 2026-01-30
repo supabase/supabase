507cd96d99a23cb5e9a556ec38e99e114bb95b53
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2026-01-30 10:30:51 -0800

    feat(telemetry): add dev telemetry toolbar

---
+++ apps/studio/components/ui/DevTelemetryToolbar/index.ts
@@ -0,0 +1,7 @@
+export { DevTelemetryToolbar } from './DevTelemetryToolbar'
+export { DevTelemetryToolbarTrigger } from './DevTelemetryToolbarTrigger'
+export {
+  DevTelemetryToolbarProvider,
+  useDevTelemetryToolbar,
+  type DevTelemetryEvent,
+} from './DevTelemetryToolbarContext'
