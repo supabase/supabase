d50fa2c8f61130d7fcac0c98841147cb89ccab90
kallebysantos <kalleby_santos@hotmail.com>
2026-02-01 03:05:05 +0000

    fix(test): updating valid edge-function url test

--- apps/studio/lib/api/edgeFunctions.test.ts
+++ apps/studio/lib/api/edgeFunctions.test.ts
@@ -1,3 +1,4 @@
+import { IS_PLATFORM } from 'common'
 import { expect, describe, it } from 'vitest'
 import { isValidEdgeFunctionURL } from './edgeFunctions'
 
@@ -9,9 +10,16 @@ describe('isValidEdgeFunctionURL', () => {
     'https://projectref.supabase.red/functions/v3/hello-world',
   ]
 
-  const invalidEdgeFunctionUrls = [
-    'https://notsupabase.com/functions/v1/test',
+  const validLocalEdgeFunctionsUrls = [
     'https://projectref.notsupabase.com/functions/v1/test',
+    'https://notsupabase.com/functions/v1/test',
+    'http://localhost:54321/functions/v1/test-2',
+    'http://kong:8000/functions/v1/hello-world',
+    'https://127.0.0.1:54321/functions/v1/test-3',
+    'https://127.0.0.1:54321/functions/v1/test-5',
+  ]
+
+  const invalidEdgeFunctionUrls = [
     'https://localhost?https://aaaa.supabase.co/functions/v1/xxx',
     'https://localhost:3000/?https://aaaa.supabase.co/functions/v1/xxx',
     'http://localhost:3000/?https://aaaa.supabase.co/functions/v1/xxx',
@@ -20,6 +28,12 @@ describe('isValidEdgeFunctionURL', () => {
   it('should match valid edge function URLs', () => {
     for (const url of validEdgeFunctionUrls) {
       expect(isValidEdgeFunctionURL(url), `Expected ${url} to be valid`).toBe(true)
+    }
+  })
+
+  it('should match valid local edge function URLs', () => {
+    for (const url of validLocalEdgeFunctionsUrls) {
+      expect(isValidEdgeFunctionURL(url), `Expected ${url} to be valid`).toBe(!IS_PLATFORM)
     }
   })
 
