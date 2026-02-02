4a07b574f09343b93fb857cfe332ca692197cfb0
Pamela Chia <pamelachiamayyee@gmail.com>
2026-02-02 23:50:07 +0800

    refactor(home): use hook for home_new exposure

--- apps/studio/pages/project/[ref]/index.tsx
+++ apps/studio/pages/project/[ref]/index.tsx
@@ -1,29 +1,17 @@
-import { useEffect, useRef } from 'react'
-
 import { IS_PLATFORM } from 'common'
 import { Home } from 'components/interfaces/Home/Home'
 import { HomeV2 } from 'components/interfaces/HomeNew/Home'
 import DefaultLayout from 'components/layouts/DefaultLayout'
 import { ProjectLayoutWithAuth } from 'components/layouts/ProjectLayout'
+import { useTrackExperimentExposure } from 'hooks/misc/useTrackExperimentExposure'
 import { usePHFlag } from 'hooks/ui/useFlag'
-import { useTrack } from 'lib/telemetry/track'
 import type { NextPageWithLayout } from 'types'
 
 const HomePage: NextPageWithLayout = () => {
   const homeNewVariant = usePHFlag<string>('homeNew')
   const isHomeNew = homeNewVariant === 'new-home'
-  const track = useTrack()
-  const hasTrackedExposure = useRef(false)
 
-  useEffect(() => {
-    if (!IS_PLATFORM) return
-    if (hasTrackedExposure.current) return
-    if (!homeNewVariant) return
-
-    hasTrackedExposure.current = true
-
-    track('home_new_experiment_exposed', { variant: homeNewVariant })
-  }, [homeNewVariant, track])
+  useTrackExperimentExposure('home_new', IS_PLATFORM ? homeNewVariant : undefined)
 
   if (isHomeNew) {
     return <HomeV2 />
