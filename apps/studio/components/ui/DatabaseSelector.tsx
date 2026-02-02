238a9566ae2f052e358f56737d3a8a5a37097e78
Joshen Lim <joshenlimek@gmail.com>
2026-02-02 16:23:27 +0800

    Clean up

--- apps/studio/components/ui/DatabaseSelector.tsx
+++ apps/studio/components/ui/DatabaseSelector.tsx
@@ -234,7 +234,7 @@ export const DatabaseSelector = ({
                       // the replica panel from a "portal" based component (e.g dialog, sheet, dropdown, etc)
                       // Although I'd prefer if there's a better way to resolve this
                       await timeout(50)
-                      setShowNewReplicaPanel(true)
+                      if (!unifiedReplication) setShowNewReplicaPanel(true)
                     }}
                     className="w-full flex items-center gap-2"
                   >
