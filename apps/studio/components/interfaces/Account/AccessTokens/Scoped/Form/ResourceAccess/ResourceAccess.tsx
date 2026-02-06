52871098374c40ef0d04953605414adb9b4d45be
kemal <hello@kemal.earth>
2026-02-06 16:01:09 +0000

    fix: my dads turkish speaking english

--- apps/studio/components/interfaces/Account/AccessTokens/Scoped/Form/ResourceAccess/ResourceAccess.tsx
+++ apps/studio/components/interfaces/Account/AccessTokens/Scoped/Form/ResourceAccess/ResourceAccess.tsx
@@ -51,7 +51,7 @@ export const ResourceAccess = ({ control, resourceAccess, setValue }: ResourceAc
             <FormControl_Shadcn_>
               <div className="space-y-3">
                 <fieldset className="flex gap-3">
-                  <legend className="sr-only">Resource access lavel</legend>
+                  <legend className="sr-only">Resource access level</legend>
                   {RESOURCE_OPTIONS.map((option) => (
                     <ResourceOption
                       key={option.value}
