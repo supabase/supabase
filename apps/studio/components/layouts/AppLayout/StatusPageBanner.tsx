ca8978f3681269c1e951553c1a57d2c07e9cb1fd
Charis Lam <26616127+charislam@users.noreply.github.com>
2026-02-10 15:53:11 -0500

    fix(studio): remove incident banner automation

--- apps/studio/components/layouts/AppLayout/StatusPageBanner.tsx
+++ apps/studio/components/layouts/AppLayout/StatusPageBanner.tsx
@@ -1,9 +1,7 @@
-import { LOCAL_STORAGE_KEYS, useFlag } from 'common'
-import { HeaderBanner } from 'components/interfaces/Organization/HeaderBanner'
-import { InlineLink } from 'components/ui/InlineLink'
+import { useFlag } from 'common'
 
-import { useIncidentStatusQuery } from '@/data/platform/incident-status-query'
-import { useLocalStorageQuery } from '@/hooks/misc/useLocalStorage'
+import { HeaderBanner } from '@/components/interfaces/Organization/HeaderBanner'
+import { InlineLink } from '@/components/ui/InlineLink'
 
 const BANNER_DESCRIPTION = (
   <>
@@ -12,54 +10,18 @@ const BANNER_DESCRIPTION = (
 )
 
 /**
- * Used to display ongoing incidents or maintenances
+ * Used to display ongoing incidents
  */
 export const StatusPageBanner = () => {
-  const { data: allStatusPageEvents } = useIncidentStatusQuery()
-  const { incidents = [], maintenanceEvents = [] } = allStatusPageEvents ?? {}
-
-  // Only show incident banner for incidents with real impact (not "none")
-  const highImpactIncident = incidents.find((incident) => incident.impact !== 'none')
-  const incidentEventId = highImpactIncident?.id ?? ''
-
-  const showIncidentBannerOverride =
+  const showIncidentBanner =
     useFlag('ongoingIncident') || process.env.NEXT_PUBLIC_ONGOING_INCIDENT === 'true'
 
-  const ongoingMaintenance = maintenanceEvents.length > 0
-  const maintenanceEventId = maintenanceEvents[0]?.id ?? ''
-
-  const [dismissedIncident, setDismissedIncident] = useLocalStorageQuery(
-    LOCAL_STORAGE_KEYS.INCIDENT_BANNER_DISMISSED(incidentEventId),
-    false
-  )
-
-  const [dismissedMaintenance, setDismissedMaintenance] = useLocalStorageQuery(
-    LOCAL_STORAGE_KEYS.MAINTENANCE_BANNER_DISMISSED(maintenanceEventId),
-    false
-  )
-
-  if (showIncidentBannerOverride || (highImpactIncident && !dismissedIncident)) {
+  if (showIncidentBanner) {
     return (
       <HeaderBanner
         variant="warning"
         title="We are investigating a technical issue"
         description={BANNER_DESCRIPTION}
-        onDismiss={
-          showIncidentBannerOverride || !highImpactIncident
-            ? undefined
-            : () => setDismissedIncident(true)
-        }
-      />
-    )
-  }
-
-  if (ongoingMaintenance && !dismissedMaintenance) {
-    return (
-      <HeaderBanner
-        variant="note"
-        title="Scheduled maintenance is in progress"
-        description={BANNER_DESCRIPTION}
-        onDismiss={() => setDismissedMaintenance(true)}
       />
     )
   }
