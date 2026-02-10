3dddd9aaa99fb7777b2afac9f518bd6bf4ebb7e6
Ivan Vasilov <vasilov.ivan@gmail.com>
2026-02-10 16:18:52 +0100

    Remove CMS code from the `www` app.

--- apps/www/next.config.mjs
+++ apps/www/next.config.mjs
@@ -1,16 +1,14 @@
+import { remarkCodeHike } from '@code-hike/mdx'
 import bundleAnalyzer from '@next/bundle-analyzer'
 import nextMdx from '@next/mdx'
 import { withSentryConfig } from '@sentry/nextjs'
-
+import codeHikeTheme from 'config/code-hike.theme.json' with { type: 'json' }
 import rehypeSlug from 'rehype-slug'
 import remarkGfm from 'remark-gfm'
 
 import redirects from './lib/redirects.js'
 import remotePatterns from './lib/remotePatterns.js'
 import rewrites from './lib/rewrites.js'
-
-import { remarkCodeHike } from '@code-hike/mdx'
-import codeHikeTheme from 'config/code-hike.theme.json' with { type: 'json' }
 
 const withMDX = nextMdx({
   extension: /\.mdx?$/,
@@ -86,16 +84,6 @@ const nextConfig = {
           {
             key: 'X-Robots-Tag',
             value: 'all',
-          },
-          // No X-Frame-Options header to allow iframe embedding
-        ],
-      },
-      {
-        source: '/api-v2/cms/preview',
-        headers: [
-          {
-            key: 'content-type',
-            value: 'text/html',
           },
           // No X-Frame-Options header to allow iframe embedding
         ],
