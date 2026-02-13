73f96688ddc0c59cca64724faabbcfebd503f567
Jeevan Mahesha <csejeevanm@gmail.com>
2026-02-13 15:52:45 +0530

    refactor: Remove zone.js dependency and implement provideZoneChangeDetection in app configuration

--- examples/user-management/angular-user-management/src/app/app.config.ts
+++ examples/user-management/angular-user-management/src/app/app.config.ts
@@ -1,5 +1,5 @@
-import { ApplicationConfig } from "@angular/core";
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core'
 
 export const appConfig: ApplicationConfig = {
-  providers: [],
+  providers: [provideZoneChangeDetection()],
 }
