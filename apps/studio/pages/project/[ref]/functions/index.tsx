d4c8dd67527f20c6da824a58b359e60e51dd4524
Luiz Felipe Machado <luizfelmach@gmail.com>
2026-02-16 18:16:33 -0300

    fix(studio): always render DeployEdgeFunctionButton on Edge Functions page
    Remove IS_PLATFORM guard so DeployEdgeFunctionButton is rendered in all environments.
    Also reorder imports for consistency.

--- apps/studio/pages/project/[ref]/functions/index.tsx
+++ apps/studio/pages/project/[ref]/functions/index.tsx
@@ -199,7 +199,7 @@ EdgeFunctionsPage.getLayout = (page: React.ReactElement) => {
                     Examples
                   </a>
                 </Button>
-                {IS_PLATFORM && <DeployEdgeFunctionButton />}
+                <DeployEdgeFunctionButton />
               </PageHeaderAside>
             </PageHeaderMeta>
           </PageHeader>
