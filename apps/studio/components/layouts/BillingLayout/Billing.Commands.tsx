8d96c9f71459c3d496e8c13cb9cf9a17df85c04b
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2026-02-17 18:19:48 +0100

    chore(studio): add Billing nav command

---
+++ apps/studio/components/layouts/BillingLayout/Billing.Commands.tsx
@@ -0,0 +1,26 @@
+import { COMMAND_MENU_SECTIONS } from 'components/interfaces/App/CommandMenu/CommandMenu.utils'
+import { useIsFeatureEnabled } from 'hooks/misc/useIsFeatureEnabled'
+import { useSelectedOrganizationQuery } from 'hooks/misc/useSelectedOrganization'
+import type { CommandOptions } from 'ui-patterns/CommandMenu'
+import { useRegisterCommands } from 'ui-patterns/CommandMenu'
+
+export function useBillingGotoCommands(options?: CommandOptions) {
+  const { data: organization } = useSelectedOrganizationQuery()
+  const billingEnabled = useIsFeatureEnabled('billing:all')
+  const slug = organization?.slug ?? '_'
+
+  useRegisterCommands(
+    COMMAND_MENU_SECTIONS.NAVIGATE,
+    billingEnabled
+      ? [
+          {
+            id: 'nav-billing',
+            name: 'Billing',
+            route: `/org/${slug}/billing`,
+            defaultHidden: true,
+          },
+        ]
+      : [],
+    { ...options, deps: [slug] }
+  )
+}
