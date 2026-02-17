4d935f618e6fb4ffdf22eb0bd23e57afe04501f5
Chris Chinchilla <chris.ward@supabase.io>
2026-02-17 10:07:21 +0100

    Draft

--- examples/auth/expo-social-auth/app/_layout.tsx
+++ examples/auth/expo-social-auth/app/_layout.tsx
@@ -1,5 +1,4 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
-import { useFonts } from 'expo-font'
 import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import 'react-native-reanimated'
@@ -29,15 +28,6 @@ function RootNavigator() {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme()
-
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  })
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null
-  }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
