6a9264e6b8f295d315eb73f940d790a85bfee7b4
M <202932523+channel101@users.noreply.github.com>
2026-02-27 21:43:07 +0900

    Remove external_kakao_secret from API types
    Removed 'external_kakao_secret' from both required and optional fields.

--- packages/api-types/types/api.d.ts
+++ packages/api-types/types/api.d.ts
@@ -2142,7 +2142,6 @@ export interface components {
       external_kakao_client_id: string | null
       external_kakao_email_optional: boolean | null
       external_kakao_enabled: boolean | null
-      external_kakao_secret: string | null
       external_keycloak_client_id: string | null
       external_keycloak_email_optional: boolean | null
       external_keycloak_enabled: boolean | null
@@ -3887,7 +3886,6 @@ export interface components {
       external_kakao_client_id?: string | null
       external_kakao_email_optional?: boolean | null
       external_kakao_enabled?: boolean | null
-      external_kakao_secret?: string | null
       external_keycloak_client_id?: string | null
       external_keycloak_email_optional?: boolean | null
       external_keycloak_enabled?: boolean | null
