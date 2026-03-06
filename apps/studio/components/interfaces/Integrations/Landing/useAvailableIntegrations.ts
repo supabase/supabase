7ca47465a2f62de0088ccab5d77b7996b1d6a224
Joshen Lim <joshenlimek@gmail.com>
2026-03-06 15:38:50 +0800

    Mid

--- apps/studio/components/interfaces/Integrations/Landing/useAvailableIntegrations.ts
+++ apps/studio/components/interfaces/Integrations/Landing/useAvailableIntegrations.ts
@@ -1,5 +1,6 @@
 import { useFlag } from 'common'
 
+import { IntegrationDefinition } from './Integrations.constants'
 import { useMarketplaceIntegrationsQuery } from '@/data/marketplace/integrations-query'
 
 export const useAvailableIntegrations = () => {
@@ -7,6 +8,24 @@ export const useAvailableIntegrations = () => {
 
   const { data, error } = useMarketplaceIntegrationsQuery({ enabled: isMarketplaceEnabled })
   const isPending = !data && !error
+
+  // [Joshen] Format marketplace integrations into existing ones for now
+  // Likely that we might need to change, but can look into separately
+
+  const formattedIntegrations: IntegrationDefinition[] = data?.map((integration) => {
+    const { id, title: name, summary: description, documentation_url: docsUrl } = integration
+
+    const status = undefined
+    const icon = undefined
+
+    return {
+      id,
+      name,
+      status,
+      description,
+      docsUrl,
+    }
+  })
 
   return { data, error, isPending, isError: !!error, isSuccess: !!data }
 }
--- apps/studio/data/marketplace/integrations-query.ts
+++ apps/studio/data/marketplace/integrations-query.ts
@@ -1,15 +1,19 @@
-import { createClient, type SupabaseClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'
 import { useQuery } from '@tanstack/react-query'
 import { handleError } from 'data/fetchers'
 import type { ResponseError, UseCustomQueryOptions } from 'types'
 
 import { marketplaceIntegrationsKeys } from './keys'
+import type { Database, Tables } from './marketplace.types'
 
-const createMarketplaceClient = (): SupabaseClient => {
+// [Joshen] There was a new way of writing queries IIRC - check with Alaister which file to reference
+// https://github.com/supabase/supabase/pull/43280/changes#diff-868487dea75aac9e001894eb61232c204181d67823bedd6d2e605e49957ec5e4
+
+const createMarketplaceClient = () => {
   const API_URL = process.env.NEXT_PUBLIC_MARKETPLACE_API_URL || ''
   const PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_MARKETPLACE_PUBLISHABLE_KEY || ''
 
-  return createClient(API_URL, PUBLISHABLE_KEY, {
+  return createClient<Database>(API_URL, PUBLISHABLE_KEY, {
     auth: {
       persistSession: false,
       autoRefreshToken: false,
@@ -25,12 +29,16 @@ const createMarketplaceClient = (): SupabaseClient => {
   })
 }
 
+// [Joshen] Something's wrong with this version of JS lib + generated types
+// Ping team-dev-workflows RE this
+type MarketplaceIntegration = Tables<'items'>[] & {}
+
 async function getMarketplaceIntegrations() {
   const client = createMarketplaceClient()
-  const { data, error } = await client.from('items').select('*')
+  const { data, error } = await client.from('items').select('*').returns<MarketplaceIntegration>()
 
   if (error) handleError(error)
-  return data
+  return data ?? []
 }
 
 type MarketplaceIntegrationsData = Awaited<ReturnType<typeof getMarketplaceIntegrations>>
