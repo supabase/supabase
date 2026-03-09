90b47ed24cfff1be5439a36418627852a94a5a5c
Ignacio Dobronich <ignacio@dobronich.com>
2026-03-09 20:35:11 -0300

    feat(billing): tax id dismissible banner

---
+++ apps/studio/components/layouts/AppLayout/TaxIdBanner.tsx
@@ -0,0 +1,51 @@
+import { LOCAL_STORAGE_KEYS } from 'common'
+import { useRouter } from 'next/router'
+
+import { HeaderBanner } from '@/components/interfaces/Organization/HeaderBanner'
+import { InlineLink } from '@/components/ui/InlineLink'
+import { useLocalStorageQuery } from '@/hooks/misc/useLocalStorage'
+import { useSelectedOrganizationQuery } from '@/hooks/misc/useSelectedOrganization'
+import { useOrganizationTaxIdQuery } from 'data/organizations/organization-tax-id-query'
+
+export const TaxIdBanner = () => {
+  const router = useRouter()
+  const { data: org } = useSelectedOrganizationQuery()
+  const slug = org?.slug
+
+  const { data: taxId, isSuccess: isTaxIdLoaded } = useOrganizationTaxIdQuery(
+    { slug },
+    { enabled: !!slug && org?.plan?.id !== 'free', staleTime: 1000 * 60 * 30 }
+  )
+
+  const [isDismissed, setIsDismissed, { isSuccess: isDismissLoaded }] = useLocalStorageQuery(
+    LOCAL_STORAGE_KEYS.TAX_ID_BANNER_DISMISSED(slug ?? ''),
+    false
+  )
+
+  if (
+    router.pathname.includes('sign-in') ||
+    !org ||
+    org.plan?.id === 'free' ||
+    !isTaxIdLoaded ||
+    !isDismissLoaded ||
+    taxId ||
+    isDismissed
+  ) {
+    return null
+  }
+
+  return (
+    <HeaderBanner
+      variant="note"
+      title="Add a Tax ID to your organization"
+      description={
+        <>
+          If you are a registered business, please{' '}
+          <InlineLink href={`/org/${slug}/billing#address`}>add a Tax ID</InlineLink> to your billing
+          settings. Not applicable for individual users.
+        </>
+      }
+      onDismiss={() => setIsDismissed(true)}
+    />
+  )
+}
--- packages/common/constants/local-storage.ts
+++ packages/common/constants/local-storage.ts
@@ -103,6 +103,10 @@ export const LOCAL_STORAGE_KEYS = {
 
   // Observability banner dismissed
   OBSERVABILITY_BANNER_DISMISSED: (ref: string) => `observability-banner-dismissed-${ref}`,
+
+  // Tax ID banner dismissed
+  TAX_ID_BANNER_DISMISSED: (slug: string) => `tax-id-banner-dismissed-${slug}`,
+
   TABLE_EDITOR_NEW_FILTER_BANNER_DISMISSED: (ref: string) =>
     `table-editor-new-filter-banner-dismissed-${ref}`,
 
