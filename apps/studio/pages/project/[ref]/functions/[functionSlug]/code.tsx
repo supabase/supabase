c536e0443c22e8ba22a11d9228c844d7c8450a68
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2026-03-09 20:22:31 +0100

    fix: Prevent users from loosing changes when navigating away

--- apps/studio/pages/project/[ref]/functions/[functionSlug]/code.tsx
+++ apps/studio/pages/project/[ref]/functions/[functionSlug]/code.tsx
@@ -1,12 +1,4 @@
 import { PermissionAction } from '@supabase/shared-types/out/constants'
-import { isEqual } from 'lodash'
-import { AlertCircle, CornerDownLeft, Loader2 } from 'lucide-react'
-import { useEffect, useMemo, useState } from 'react'
-import { toast } from 'sonner'
-
-import { formatFunctionBodyToFiles } from '@/components/interfaces/EdgeFunctions/EdgeFunctions.utils'
-import { FileData } from '@/components/ui/FileExplorerAndEditor/FileExplorerAndEditor.types'
-import { useLatest } from '@/hooks/misc/useLatest'
 import { IS_PLATFORM, useParams } from 'common'
 import { DeployEdgeFunctionWarningModal } from 'components/interfaces/EdgeFunctions/DeployEdgeFunctionWarningModal'
 import { DefaultLayout } from 'components/layouts/DefaultLayout'
@@ -21,7 +13,16 @@ import { useAsyncCheckPermissions } from 'hooks/misc/useCheckPermissions'
 import { useSelectedOrganizationQuery } from 'hooks/misc/useSelectedOrganization'
 import { useSelectedProjectQuery } from 'hooks/misc/useSelectedProject'
 import { BASE_PATH } from 'lib/constants'
+import { isEqual } from 'lodash'
+import { AlertCircle, CornerDownLeft, Loader2 } from 'lucide-react'
+import { useEffect, useMemo, useState } from 'react'
+import { toast } from 'sonner'
 import { LogoLoader } from 'ui'
+
+import { formatFunctionBodyToFiles } from '@/components/interfaces/EdgeFunctions/EdgeFunctions.utils'
+import { FileData } from '@/components/ui/FileExplorerAndEditor/FileExplorerAndEditor.types'
+import { useLatest } from '@/hooks/misc/useLatest'
+import { PreventNavigationOnUnsavedChanges } from '@/components/ui-patterns/Dialogs/PreventNavigationOnUnsavedChanges'
 
 const CodePage = () => {
   const { ref, functionSlug } = useParams()
@@ -146,27 +147,15 @@ const CodePage = () => {
     setFiles(initialFiles)
   }, [initialFiles])
 
-  // [Joshen] Probably a candidate for useStaticEffectEvent
-  const filesRef = useLatest(files)
-  const initialFilesRef = useLatest(initialFiles)
-  useEffect(() => {
-    const handleBeforeUnload = (e: BeforeUnloadEvent) => {
-      const normalizeFiles = (list: FileData[]) =>
-        list.map(({ id, name, content }) => ({ id, name, content }))
-      const hasUnsavedChanges = !isEqual(
-        normalizeFiles(initialFilesRef.current),
-        normalizeFiles(filesRef.current)
-      )
-
-      if (hasUnsavedChanges) {
-        e.preventDefault()
-        e.returnValue = '' // deprecated, but older browsers still require this
-      }
-    }
-    window.addEventListener('beforeunload', handleBeforeUnload)
-    return () => window.removeEventListener('beforeunload', handleBeforeUnload)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  const hasUnsavedChanges = useMemo(() => {
+    const normalizeFiles = (list: FileData[]) =>
+      list.map(({ id, name, content }) => ({ id, name, content }))
+    const hasUnsavedChanges = !isEqual(
+      normalizeFiles(initialFiles),
+      normalizeFiles(files)
+    )
+    return hasUnsavedChanges
+  }, [initialFiles, files])
 
   return (
     <div className="flex flex-col h-full">
@@ -252,6 +241,7 @@ const CodePage = () => {
         onConfirm={handleDeployConfirm}
         isDeploying={isDeploying}
       />
+      <PreventNavigationOnUnsavedChanges hasChanges={hasUnsavedChanges} />
     </div>
   )
 }
