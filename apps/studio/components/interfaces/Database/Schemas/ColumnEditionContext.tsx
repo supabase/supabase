9849d019da0d7e0b5b06733e29ec51590b855f0b
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2026-03-10 14:55:41 +0100

    Add missing file

---
+++ apps/studio/components/interfaces/Database/Schemas/ColumnEditionContext.tsx
@@ -0,0 +1,23 @@
+import { createContext, useContext } from 'react'
+import { ReactNode } from 'react-markdown/lib/react-markdown'
+
+export type ColumnEditionContextType = {
+  onEditColumn: (tableId: number, columnId: string) => void
+}
+
+export const ColumnEditionContext = createContext<ColumnEditionContextType | null>(null)
+
+export const ColumnEditionContextProvider = ({
+  children,
+  value,
+}: {
+  children: ReactNode
+  value: ColumnEditionContextType
+}) => <ColumnEditionContext.Provider value={value}>{children}</ColumnEditionContext.Provider>
+
+export const useColumnEditionContext = () => {
+  const context = useContext(ColumnEditionContext)
+  if (!context)
+    throw new Error('useColumnEditionContext must be used inside a <ColumnEditionContextProvider>')
+  return context
+}
