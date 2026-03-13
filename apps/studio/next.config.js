a11625183ff19c92f854d8543e9f1fa47126d892
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2026-03-13 11:08:51 +0100

    remove experimental nextjs build options

--- apps/studio/next.config.js
+++ apps/studio/next.config.js
@@ -605,11 +605,7 @@ const nextConfig = {
     // Typechecking is run via GitHub Action only for efficiency
     // For production, we run typechecks separate from the build command (pnpm typecheck && pnpm build)
     ignoreBuildErrors: true,
-  },
-  experimental: {
-    workerThreads: process.env.CI !== 'true',
-    cpus: 2,
-  },
+  }
 }
 
 // Make sure adding Sentry options is the last code to run before exporting, to
