39a2a37a46919eb3026abd237a6ca0953af68ac0
Danny White <3104761+dnywh@users.noreply.github.com>
2026-03-18 16:06:39 +1100

    language nit

--- apps/studio/components/interfaces/Integrations/CronJobs/CreateCronJobSheet/CreateCronJobSheet.constants.ts
+++ apps/studio/components/interfaces/Integrations/CronJobs/CreateCronJobSheet/CreateCronJobSheet.constants.ts
@@ -43,7 +43,7 @@ const httpRequestSchema = z.object({
   endpoint: httpEndpointUrlSchema({
     requiredMessage: 'Please provide a URL',
     invalidMessage: 'Please provide a valid URL',
-    prefixMessage: 'Please include HTTP/HTTPs to your URL',
+    prefixMessage: 'Please include HTTP or HTTPs in your URL',
   }),
   timeoutMs: z.coerce.number().int().gte(1000).lte(5000).default(1000),
   httpHeaders: z.array(z.object({ name: z.string(), value: z.string() })),
