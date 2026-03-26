d48317d0439fa25d2fe05015a28e8083bacabe4e
Ivan Vasilov <vasilov.ivan@gmail.com>
2026-03-26 16:30:38 +0100

    Add file router.

--- apps/lite-studio/app/routes.ts
+++ apps/lite-studio/app/routes.ts
@@ -1,3 +1,4 @@
-import { index, type RouteConfig } from '@react-router/dev/routes'
+import { type RouteConfig } from '@react-router/dev/routes'
+import { flatRoutes } from '@react-router/fs-routes'
 
-export default [index('routes/home.tsx')] satisfies RouteConfig
+export default flatRoutes() satisfies RouteConfig
--- apps/lite-studio/react-router.config.ts
+++ apps/lite-studio/react-router.config.ts
@@ -1,7 +1,5 @@
 import type { Config } from '@react-router/dev/config'
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  ssr: false,
 } satisfies Config
