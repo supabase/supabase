c7927b3826758b500408dc645607a171bc376206
Chris Chinchilla <chris.ward@supabase.io>
2026-03-26 10:11:51 +0100

    review

--- examples/user-management/ionic-react-user-management/src/supabaseClient.ts
+++ examples/user-management/ionic-react-user-management/src/supabaseClient.ts
@@ -1,11 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
-const supabaseKey = process.env.REACT_APP_SUPABASE_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error(
-    'Missing Supabase environment variables: REACT_APP_SUPABASE_URL and REACT_APP_SUPABASE_KEY must be set.'
+    'Missing Supabase environment variables: VITE_SUPABASE_URL and VITE_SUPABASE_KEY must be set.'
   )
 }
 
