8724447f53c6024db7453937f3b814ba3191145c
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2026-03-27 17:06:57 +0100

    use supabase button in input group

--- packages/ui/src/components/shadcn/ui/input-group.tsx
+++ packages/ui/src/components/shadcn/ui/input-group.tsx
@@ -2,7 +2,7 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import * as React from 'react'
 
 import { cn } from '../../../lib/utils/cn'
-import { Button } from './button'
+import { Button } from '../../Button'
 import { Input, type InputProps } from './input'
 import { Textarea, type TextareaProps } from './textarea'
 
@@ -79,33 +79,29 @@ function InputGroupAddon({
   )
 }
 
-const inputGroupButtonVariants = cva('flex items-center gap-2 text-sm shadow-none', {
+const inputGroupButtonVariants = cva('', {
   variants: {
     size: {
-      xs: "h-6 gap-1 rounded-[calc(var(--radius)-5px)] px-2 has-[>svg]:px-2 [&>svg:not([class*='size-'])]:size-3.5",
-      sm: 'h-8 gap-1.5 rounded-md px-2.5 has-[>svg]:px-2.5',
-      'icon-xs': 'size-6 rounded-[calc(var(--radius)-5px)] p-0 has-[>svg]:p-0',
-      'icon-sm': 'size-8 p-0 has-[>svg]:p-0',
+      tiny: "h-6 gap-1 rounded-md px-2 has-[>svg]:px-2 [&>svg:not([class*='size-'])]:size-3.5",
+      small: 'h-8 gap-1.5 rounded-md px-2.5 has-[>svg]:px-2.5',
     },
   },
   defaultVariants: {
-    size: 'xs',
+    size: 'tiny',
   },
 })
 
 function InputGroupButton({
   className,
-  type = 'button',
-  variant = 'ghost',
-  size = 'xs',
+  type = 'text',
+  size = 'tiny',
   ...props
 }: Omit<React.ComponentProps<typeof Button>, 'size'> &
   VariantProps<typeof inputGroupButtonVariants>) {
   return (
     <Button
       type={type}
-      data-size={size}
-      variant={variant}
+      size={size}
       className={cn(inputGroupButtonVariants({ size }), className)}
       {...props}
     />
