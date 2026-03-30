446262afc00dd408350b270a7c07b654634550f2
Kanishk Dudeja <kanishk@kanishkdudeja.com>
2026-03-30 17:37:24 +0530

    align focus input color

--- apps/studio/components/interfaces/Billing/Payment/Payment.utils.ts
+++ apps/studio/components/interfaces/Billing/Payment/Payment.utils.ts
@@ -59,21 +59,22 @@ export const getStripeElementsAppearanceOptions = (
 export const getAddressElementAppearanceOptions = (
   resolvedTheme: string | undefined
 ): Appearance => {
+  const isDark = resolvedTheme?.includes('dark')
+
   return {
     labels: 'above' as const,
-    theme: (resolvedTheme?.includes('dark') ? 'night' : 'flat') as 'night' | 'flat',
+    theme: (isDark ? 'night' : 'flat') as 'night' | 'flat',
     variables: {
       fontSizeBase: '14px',
       spacingUnit: '7px',
-      colorBackground: resolvedTheme?.includes('dark')
-        ? 'hsl(0deg 0% 14.1%)'
-        : 'hsl(0deg 0% 95.3%)',
+      colorPrimary: isDark ? 'hsl(0deg 0% 32%)' : 'hsl(0deg 0% 55%)',
+      colorBackground: isDark ? 'hsl(0deg 0% 14.1%)' : 'hsl(0deg 0% 95.3%)',
       borderRadius: '4px',
       fontFamily: 'CustomFont, Helvetica Neue, Helvetica, Arial, sans-serif',
     },
     rules: {
       '.Input': {
-        boxShadow: resolvedTheme?.includes('dark') ? 'none' : 'inset 0 0 0 1px hsl(0deg 0% 80%)',
+        boxShadow: isDark ? 'none' : 'inset 0 0 0 1px hsl(0deg 0% 80%)',
         height: '34px',
         lineHeight: '16px',
         padding: '8px 12px',
