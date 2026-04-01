c821a37e2e6ddb31726ca899dcb13d33511f4fe4
Danny White <3104761+dnywh@users.noreply.github.com>
2026-04-02 10:59:24 +1100

    format

--- apps/studio/components/interfaces/LogDrains/LogDrains.utils.test.ts
+++ apps/studio/components/interfaces/LogDrains/LogDrains.utils.test.ts
@@ -1,9 +1,9 @@
 import { describe, expect, it } from 'vitest'
 
 import {
-  HEADER_VALIDATION_ERRORS,
   getDefaultHeadersByType,
   getHeadersSectionDescription,
+  HEADER_VALIDATION_ERRORS,
   headerRecordToRows,
   headerRowsToRecord,
   logDrainHeaderEntriesSchema,
