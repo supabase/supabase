30fbe9e75e8bcecabbfc5c6de216e4f1ac5cea0b
Ivan Vasilov <vasilov.ivan@gmail.com>
2026-04-04 00:08:39 +0200

    Fix for a bug with context menu in react-data-grid.

--- apps/studio/components/grid/components/grid/RowRenderer.tsx
+++ apps/studio/components/grid/components/grid/RowRenderer.tsx
@@ -1,17 +1,41 @@
 import type { Key } from 'react'
+import { useRef, useState } from 'react'
 import { RenderRowProps, Row } from 'react-data-grid'
 import { ContextMenu_Shadcn_, ContextMenuTrigger_Shadcn_ } from 'ui'
 
 import { RowContextMenuContent } from '../menu/RowContextMenu'
 import { SupaRow } from '@/components/grid/types'
 
-export function RowRenderer(key: Key, props: RenderRowProps<SupaRow>) {
+function RowWithContextMenu({ row, ...props }: RenderRowProps<SupaRow>) {
+  const triggerRef = useRef<HTMLSpanElement>(null)
+  const [menuKey, setMenuKey] = useState(0)
+
+  // Hack to make ContextMenu work with react-data-grid, which doesn't support portals in rows. The original bug
+  // was that the ContextMenu didn't move when right-clicking two cells in the same row
+  function handleContextMenu(e: React.MouseEvent) {
+    e.preventDefault()
+    const trigger = triggerRef.current
+    if (!trigger) return
+    setMenuKey((k) => k + 1)
+    trigger.dispatchEvent(
+      new MouseEvent('contextmenu', {
+        bubbles: true,
+        cancelable: true,
+        clientX: e.clientX,
+        clientY: e.clientY,
+      })
+    )
+  }
+
   return (
-    <ContextMenu_Shadcn_ modal={false} key={key}>
-      <ContextMenuTrigger_Shadcn_ asChild>
-        <Row {...props} />
-      </ContextMenuTrigger_Shadcn_>
-      <RowContextMenuContent row={props.row} />
+    <ContextMenu_Shadcn_ modal={false}>
+      <ContextMenuTrigger_Shadcn_ ref={triggerRef} />
+      <RowContextMenuContent key={menuKey} row={row} />
+      <Row row={row} {...props} onContextMenu={handleContextMenu} />
     </ContextMenu_Shadcn_>
   )
+}
+
+export function RowRenderer(key: Key, props: RenderRowProps<SupaRow>) {
+  return <RowWithContextMenu key={key} {...props} />
 }
