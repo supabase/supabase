81b527fe97848539fac1a0a9a532090e0d6d1166
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2026-04-03 17:39:56 +0200

    chore: use dndkit for enumerated types sorting

--- apps/studio/components/interfaces/Database/EnumeratedTypes/EditEnumeratedTypeSidePanel.tsx
+++ apps/studio/components/interfaces/Database/EnumeratedTypes/EditEnumeratedTypeSidePanel.tsx
@@ -1,8 +1,21 @@
+import {
+  closestCenter,
+  DndContext,
+  DragEndEvent,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core'
+import {
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { AlertCircle, ExternalLink, Plus } from 'lucide-react'
 import Link from 'next/link'
-import { useEffect, useRef } from 'react'
-import { DragDropContext, Droppable, DroppableProvided } from 'react-beautiful-dnd'
+import { useEffect, useMemo, useRef } from 'react'
 import { useFieldArray, useForm } from 'react-hook-form'
 import { toast } from 'sonner'
 import {
@@ -69,23 +82,32 @@ const EditEnumeratedTypeSidePanel = ({
       values: [{ isNew: true, originalValue: '', updatedValue: '' }],
     },
   })
+  const { reset } = form
 
-  const { fields, append, remove, move } = useFieldArray({
+  const { fields, append, remove, swap } = useFieldArray({
     name: 'values',
     control: form.control,
   })
 
-  const updateOrder = (result: any) => {
-    // Dropped outside of the list
-    if (!result.destination) return
-    move(result.source.index, result.destination.index)
+  const handleDragEnd = (event: DragEndEvent) => {
+    if (event.over == null) return
+    const overIndex = fields.findIndex((item) => item.id === event.over?.id)
+    if (overIndex < 0) return
+    const activeIndex = fields.findIndex((item) => item.id === event.active.id)
+    if (activeIndex < 0) return
+
+    swap(activeIndex, overIndex)
   }
 
-  const originalEnumeratedTypes = (selectedEnumeratedType?.enums ?? []).map((x) => ({
-    isNew: false,
-    originalValue: x,
-    updatedValue: x,
-  }))
+  const originalEnumeratedTypes = useMemo(
+    () =>
+      (selectedEnumeratedType?.enums ?? []).map((x) => ({
+        isNew: false,
+        originalValue: x,
+        updatedValue: x,
+      })),
+    [selectedEnumeratedType]
+  )
 
   const onSubmit = (data: z.infer<typeof FormSchema>) => {
     if (project?.ref === undefined) return console.error('Project ref required')
@@ -123,7 +145,7 @@ const EditEnumeratedTypeSidePanel = ({
 
   useEffect(() => {
     if (selectedEnumeratedType !== undefined) {
-      form.reset({
+      reset({
         name: selectedEnumeratedType.name,
         description: selectedEnumeratedType.comment ?? '',
         values: originalEnumeratedTypes,
@@ -131,12 +153,18 @@ const EditEnumeratedTypeSidePanel = ({
     }
 
     if (selectedEnumeratedType == undefined) {
-      form.reset({
+      reset({
         values: originalEnumeratedTypes,
       })
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [selectedEnumeratedType])
+  }, [reset, originalEnumeratedTypes, selectedEnumeratedType])
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  )
 
   return (
     <SidePanel
@@ -179,67 +207,60 @@ const EditEnumeratedTypeSidePanel = ({
               )}
             />
 
-            <DragDropContext onDragEnd={(result: any) => updateOrder(result)}>
-              <Droppable droppableId="enum_type_values_droppable">
-                {(droppableProvided: DroppableProvided) => (
-                  <div ref={droppableProvided.innerRef}>
-                    {fields.map((field, index) => (
-                      <FormField_Shadcn_
-                        control={form.control}
-                        key={field.id}
-                        name={`values.${index}.updatedValue`}
-                        render={({ field: inputField }) => (
-                          <FormItem_Shadcn_>
-                            <FormLabel_Shadcn_ className={cn(index !== 0 && 'sr-only')}>
-                              Values
-                            </FormLabel_Shadcn_>
-                            {index === 0 && (
-                              <Alert_Shadcn_>
-                                <AlertCircle strokeWidth={1.5} />
-                                <AlertTitle_Shadcn_>
-                                  Existing values cannot be deleted or sorted
-                                </AlertTitle_Shadcn_>
-                                <AlertDescription_Shadcn_>
-                                  <p className="!leading-normal track">
-                                    You will need to delete and recreate the enumerated type with
-                                    the updated values instead.
-                                  </p>
-                                  <Button
-                                    asChild
-                                    type="default"
-                                    icon={<ExternalLink strokeWidth={1.5} />}
-                                    className="mt-2"
-                                  >
-                                    <Link
-                                      href="https://www.postgresql.org/message-id/21012.1459434338%40sss.pgh.pa.us"
-                                      target="_blank"
-                                      rel="noreferrer"
-                                    >
-                                      Learn more
-                                    </Link>
-                                  </Button>
-                                </AlertDescription_Shadcn_>
-                              </Alert_Shadcn_>
-                            )}
-                            <FormControl_Shadcn_>
-                              <EnumeratedTypeValueRow
-                                index={index}
-                                id={field.id}
-                                field={inputField}
-                                isDisabled={!field.isNew}
-                                onRemoveValue={() => remove(index)}
-                              />
-                            </FormControl_Shadcn_>
-                            <FormMessage_Shadcn_ className="ml-6" />
-                          </FormItem_Shadcn_>
-                        )}
-                      />
-                    ))}
-                    {droppableProvided.placeholder}
-                  </div>
+            <div>
+              <span
+                className={cn(
+                  'text-foreground-light text-sm',
+                  'transition-colors',
+                  'leading-normal'
                 )}
-              </Droppable>
-            </DragDropContext>
+              >
+                Values
+              </span>
+              <Alert_Shadcn_>
+                <AlertCircle strokeWidth={1.5} />
+                <AlertTitle_Shadcn_>Existing values cannot be deleted or sorted</AlertTitle_Shadcn_>
+                <AlertDescription_Shadcn_>
+                  <p className="!leading-normal track">
+                    You will need to delete and recreate the enumerated type with the updated values
+                    instead.
+                  </p>
+                  <Button
+                    asChild
+                    type="default"
+                    icon={<ExternalLink strokeWidth={1.5} />}
+                    className="mt-2"
+                  >
+                    <Link
+                      href="https://www.postgresql.org/message-id/21012.1459434338%40sss.pgh.pa.us"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Learn more
+                    </Link>
+                  </Button>
+                </AlertDescription_Shadcn_>
+              </Alert_Shadcn_>
+              <DndContext
+                sensors={sensors}
+                collisionDetection={closestCenter}
+                onDragEnd={handleDragEnd}
+              >
+                <SortableContext items={fields} strategy={verticalListSortingStrategy}>
+                  {fields.map((field, index) => (
+                    <EnumeratedTypeValueRow
+                      key={field.id}
+                      id={field.id}
+                      name={`values.${index}.updatedValue`}
+                      index={index}
+                      control={form.control}
+                      isDisabled={!field.isNew}
+                      onRemoveValue={() => remove(index)}
+                    />
+                  ))}
+                </SortableContext>
+              </DndContext>
+            </div>
 
             <Button
               type="default"
