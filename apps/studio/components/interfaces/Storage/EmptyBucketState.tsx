7cb837d3f6ff9c033d13d179b11b43fa997a2e1f
Ivan Vasilov <vasilov.ivan@gmail.com>
2026-04-17 18:11:51 +0200

    Minor fix.

--- apps/studio/components/interfaces/Storage/EmptyBucketState.tsx
+++ apps/studio/components/interfaces/Storage/EmptyBucketState.tsx
@@ -24,7 +24,7 @@ export const EmptyBucketState = ({
       description={config.valueProp}
       className={className}
     >
-      <CreateBucketButton onClick={onCreateBucket} />
+      {onCreateBucket ? <CreateBucketButton onClick={onCreateBucket} /> : null}
     </EmptyStatePresentational>
   )
 }
