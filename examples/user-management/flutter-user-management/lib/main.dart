7ffd4aac779a469bb35d107f02991b71d0cd730e
Chris Chinchilla <chris@chrischinchilla.com>
2026-04-29 13:23:43 +0200

    Flutter fix

--- examples/user-management/flutter-user-management/lib/main.dart
+++ examples/user-management/flutter-user-management/lib/main.dart
@@ -9,7 +9,7 @@ Future<void> main() async {
 
   await Supabase.initialize(
     url: dotenv.env['SUPABASE_URL']!,
-    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+    publishableKey: dotenv.env['SUPABASE_PUBLISHABLE_KEY']!,
   );
   runApp(const MyApp());
 }
