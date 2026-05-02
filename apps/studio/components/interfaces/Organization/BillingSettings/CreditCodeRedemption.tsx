a0584490ffff0298d6af148da45c8f45e6499673
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2026-05-02 16:41:28 +0800

    Update CreditCodeRedemption.tsx

--- apps/studio/components/interfaces/Organization/BillingSettings/CreditCodeRedemption.tsx
+++ apps/studio/components/interfaces/Organization/BillingSettings/CreditCodeRedemption.tsx
@@ -57,7 +57,10 @@ export const CreditCodeRedemption = ({
   )
 
   const { data: org, isLoading: isOrgLoading } = useOrganizationQuery({ slug })
-  const { data: orgBalance, isLoading: isOrgBalanceLoading } = useOrgBalanceQuery({ orgSlug: slug })
+  const { data: orgBalance, isLoading: isOrgBalanceLoading } = useOrgBalanceQuery(
+    { orgSlug: slug },
+    { enabled: modalVisible }
+  )
   const combinedCreditBalanceCents = orgBalance?.total_balance_cents
 
   const { can: canRedeemCode, isSuccess: isPermissionsLoaded } = useAsyncCheckPermissions(
