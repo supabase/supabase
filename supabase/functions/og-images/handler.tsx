2331fcd7fbf6c4abac6853e70945def8e7328b36
pranjal499 <pranjalprofsocial72@gmail.com>
2026-05-05 23:33:28 +0530

    fix(cli): localize deno dependencies to allow offline start

--- supabase/functions/og-images/handler.tsx
+++ supabase/functions/og-images/handler.tsx
@@ -1,4 +1,6 @@
+// @ts-ignore: Deno remote imports cannot be resolved by standard TypeScript
 import React from 'https://esm.sh/react@18.2.0?deno-std=0.140.0'
+// @ts-ignore: Deno remote imports cannot be resolved by standard TypeScript
 import { ImageResponse } from 'https://deno.land/x/og_edge@0.0.4/mod.ts'
 import CustomerStories from './component/CustomerStories.tsx'
 import Docs from './component/Docs.tsx'
@@ -26,7 +28,7 @@ const CIRCULAR_FONT_DATA = await FONT_CIRCULAR
 const MONO_FONT_DATA = await FONT_MONO
 
 const getParamValue = (
-  url: string,
+  url: URL,
   value: string,
   options?: {
     lowercase?: boolean
--- supabase/functions/og-images/index.ts
+++ supabase/functions/og-images/index.ts
@@ -1,6 +1,3 @@
-import { serve } from 'https://deno.land/std@0.170.0/http/server.ts'
-import { handler } from './handler.tsx'
+import { handler } from './handler.tsx';
 
-serve(handler)
-
-console.log('Serving og-images function')
+Deno.serve(handler)
