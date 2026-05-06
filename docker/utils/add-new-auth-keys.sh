18f7c3ea176c807c8e3797be9c85e3727cd0877e
Kalleby Santos <kalleby_santos@hotmail.com>
2026-05-06 12:26:35 +0100

    feat: exposing 'SUPABASE_JWKS' env to e functions

--- docker/utils/add-new-auth-keys.sh
+++ docker/utils/add-new-auth-keys.sh
@@ -152,6 +152,7 @@ echo ""
 echo "  Auth:     GOTRUE_JWT_KEYS: \${JWT_KEYS:-[]}"
 echo "  Realtime: API_JWT_JWKS: \${JWT_JWKS:-{\"keys\":[]}}"
 echo "  Storage:  JWT_JWKS: \${JWT_JWKS:-{\"keys\":[]}}"
+echo "  Functions:  SUPABASE_JWKS: \${JWT_JWKS:-{\"keys\":[]}}"
 echo ""
 
 if [ "$1" = "--update-env" ]; then
@@ -196,11 +197,13 @@ sed -i.old \
     -e '/^[ ]*#GOTRUE_JWT_KEYS:/ s/#//' \
     -e '/^[ ]*#API_JWT_JWKS:/ s/#//' \
     -e '/^[ ]*#JWT_JWKS:/ s/#//' \
+    -e '/^[ ]*#SUPABASE_JWKS:/ s/#//' \
     docker-compose.yml || true
 
 if grep -q '^[ ]*GOTRUE_JWT_KEYS:' docker-compose.yml && \
    grep -q '^[ ]*API_JWT_JWKS:' docker-compose.yml && \
-   grep -q '^[ ]*JWT_JWKS:' docker-compose.yml; then
+   grep -q '^[ ]*JWT_JWKS:' docker-compose.yml && \
+   grep -q '^[ ]*SUPABASE_JWKS:' docker-compose.yml; then
     echo "Done."
 else
     echo "Warning: could not edit docker-compose.yml. Uncomment auth configuration manually."
