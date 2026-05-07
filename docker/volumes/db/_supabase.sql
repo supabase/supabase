2af7d50af054195f9799cd088231a75138ca993a
Leandra <88313150+leandrawisnu@users.noreply.github.com>
2026-05-07 15:46:52 +0700

    Update _supabase.sql

--- docker/volumes/db/_supabase.sql
+++ docker/volumes/db/_supabase.sql
@@ -1,3 +1,8 @@
 \set pguser `echo "$POSTGRES_USER"`
 
+-- Create postgres database if it doesn't exist
+SELECT 'CREATE DATABASE postgres WITH OWNER ' || :'pguser'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'postgres')\gexec
+
+-- Create _supabase database
 CREATE DATABASE _supabase WITH OWNER :pguser;
