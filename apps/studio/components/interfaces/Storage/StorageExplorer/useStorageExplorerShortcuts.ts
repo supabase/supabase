f3ff3e7c75500863ee08d4dd8449a12016424037
Ali Waseem <waseema393@gmail.com>
2026-05-12 10:42:42 -0600

    remove refresh when items are selected

--- apps/studio/components/interfaces/Storage/StorageExplorer/useStorageExplorerShortcuts.ts
+++ apps/studio/components/interfaces/Storage/StorageExplorer/useStorageExplorerShortcuts.ts
@@ -32,9 +32,13 @@ export function useStorageExplorerShortcuts({ onClearSearch }: UseStorageExplore
 
   const { can: canUpdateFiles } = useAsyncCheckPermissions(PermissionAction.STORAGE_WRITE, '*')
 
-  useShortcut(SHORTCUT_IDS.STORAGE_EXPLORER_REFRESH, () => {
-    refreshAll()
-  })
+  useShortcut(
+    SHORTCUT_IDS.STORAGE_EXPLORER_REFRESH,
+    () => {
+      refreshAll()
+    },
+    { enabled: selectedItems.length === 0 }
+  )
 
   useShortcut(
     SHORTCUT_IDS.STORAGE_EXPLORER_DOWNLOAD_SELECTED,
