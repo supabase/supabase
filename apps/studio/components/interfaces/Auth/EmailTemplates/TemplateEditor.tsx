9701572d7e503e66a0508094fd72b13d06242b19
Danny White <3104761+dnywh@users.noreply.github.com>
2026-05-12 13:42:19 +1000

    fix

--- apps/studio/components/interfaces/Auth/EmailTemplates/TemplateEditor.tsx
+++ apps/studio/components/interfaces/Auth/EmailTemplates/TemplateEditor.tsx
@@ -329,8 +329,7 @@ export const TemplateEditor = ({ template }: TemplateEditorProps) => {
                             {variable.name === 'Token' &&
                               template.variables.some((x) => x.name === 'ConfirmationURL') && (
                                 <>
-                                  {' '}
-                                  Can be used instead of{' '}
+                                  , which can be used instead of{' '}
                                   <code className="text-code-inline">ConfirmationURL</code>
                                 </>
                               )}
