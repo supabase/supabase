08efaa5a8daf8d1dffcfc729ec149af41e5db04f
Rodrigo Mansueli <rodrigo@mansueli.com>
2026-05-14 10:55:53 -0300

    Update index.ts

--- examples/edge-functions/supabase/functions/drizzle/index.ts
+++ examples/edge-functions/supabase/functions/drizzle/index.ts
@@ -3,11 +3,11 @@ import postgres from 'postgres'
 import { countries } from '../_shared/schema.ts'
 
 const connectionString = Deno.env.get('SUPABASE_DB_URL')!
+// Disable prefetch as it is not supported for "Transaction" pool mode
+const client = postgres(connectionString, { prepare: false })
+const db = drizzle(client)
 
 Deno.serve(async (_req) => {
-  // Disable prefetch as it is not supported for "Transaction" pool mode
-  const client = postgres(connectionString, { prepare: false })
-  const db = drizzle(client)
   const allCountries = await db.select().from(countries)
 
   return Response.json(allCountries)
