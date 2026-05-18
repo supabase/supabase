e26d082e198c88fe5f3317c4c9627bc95ae3cae2
Danny White <3104761+dnywh@users.noreply.github.com>
2026-05-18 14:46:02 +1000

    chore: carry over Admonition copy polish

--- apps/studio/components/interfaces/Settings/Addons/IPv4SidePanel.tsx
+++ apps/studio/components/interfaces/Settings/Addons/IPv4SidePanel.tsx
@@ -161,7 +161,7 @@ const IPv4SidePanel = () => {
           {!isAws && (
             <Admonition
               type="default"
-              description="Dedicated IPv4 address is only available for AWS projects"
+              description="Dedicated IPv4 address is only available for AWS projects."
             />
           )}
 
--- apps/studio/components/interfaces/Support/ProjectAndPlanInfo.tsx
+++ apps/studio/components/interfaces/Support/ProjectAndPlanInfo.tsx
@@ -41,7 +41,7 @@ export function ProjectAndPlanInfo({
       <ProjectRefHighlighted projectRef={projectRef} />
 
       {!hasProjectSelected && (
-        <Admonition type="default" description="No project has been selected" />
+        <Admonition type="default" description="No project has been selected." />
       )}
     </div>
   )
