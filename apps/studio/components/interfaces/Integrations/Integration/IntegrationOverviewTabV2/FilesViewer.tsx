5109684a85f30c0cf319d9b86a2ce18c72381e38
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2026-06-01 19:18:41 +0200

    zoommable images in integration overview page

--- apps/studio/components/interfaces/Integrations/Integration/IntegrationOverviewTabV2/FilesViewer.tsx
+++ apps/studio/components/interfaces/Integrations/Integration/IntegrationOverviewTabV2/FilesViewer.tsx
@@ -1,43 +1,19 @@
-import { useState } from 'react'
-import { cn, Dialog, DialogContent } from 'ui'
+import { Image } from 'ui-patterns/Image'
 
 export const FilesViewer = ({ files }: { files: string[] }) => {
-  const [selected, setSelected] = useState(files[0])
-  const [showDialog, setShowDialog] = useState(false)
-
   return (
-    <>
-      <div className="flex flex-col gap-y-4">
-        <button onClick={() => setShowDialog(true)}>
-          <img
-            alt={selected}
-            src={selected}
-            className="rounded-md border object-cover aspect-video"
-          />
-        </button>
-
-        {files.length > 1 && (
-          <div className="grid grid-cols-10 gap-x-2">
-            {files.map((x) => (
-              <button key={x} onClick={() => setSelected(x)}>
-                <img
-                  alt={x}
-                  src={x}
-                  className={cn(
-                    'col-span-1 bg-surface-100 rounded-md object-cover aspect-square border transition',
-                    selected === x ? 'border-button-hover' : 'border-secondary'
-                  )}
-                />
-              </button>
-            ))}
-          </div>
-        )}
-      </div>
-      <Dialog open={showDialog} onOpenChange={setShowDialog}>
-        <DialogContent size="xxlarge">
-          <img alt={selected} src={selected} className="rounded-md border" />
-        </DialogContent>
-      </Dialog>
-    </>
+    <div className="grid grid-cols-2 gap-3 sm:grid-cols-3">
+      {files.map((file) => (
+        <Image
+          key={file}
+          src={file}
+          alt=""
+          zoomable
+          width={400}
+          height={225}
+          className="rounded-md border object-cover w-full"
+        />
+      ))}
+    </div>
   )
 }
