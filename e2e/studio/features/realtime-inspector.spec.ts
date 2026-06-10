29135b8f387572913d27f94aed723801b91f60c3
Ivan Vasilov <vasilov.ivan@gmail.com>
2026-06-10 12:47:30 +0200

    Try running the test without the waiting for project request.

--- e2e/studio/features/realtime-inspector.spec.ts
+++ e2e/studio/features/realtime-inspector.spec.ts
@@ -15,7 +15,7 @@ import { test } from '../utils/test.js'
 test.describe('Realtime Inspector', () => {
   test.beforeEach(async ({ page, ref }) => {
     await navigateToRealtimeInspector(page, ref)
-    await page.waitForResponse(new RegExp(`/platform/projects/${ref}`))
+    // await page.waitForResponse(new RegExp(`/platform/projects/${ref}`))
   })
 
   test.describe('Basic Inspector UI', () => {
