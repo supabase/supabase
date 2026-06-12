0dc57774e915bfafd570b9bdc4d2b8573a061105
Aleksi Immonen <aleksi@supabase.io>
2026-06-12 13:08:42 +0300

    fix(www): fix canonical tag indentation in enterprise

--- apps/www/pages/solutions/enterprise.tsx
+++ apps/www/pages/solutions/enterprise.tsx
@@ -20,7 +20,7 @@ const CTAForm = dynamic(() => import('components/Enterprise/CTAForm'))
 const Enterprise: NextPage = () => (
   <>
     <NextSeo
-            canonical="https://supabase.com/enterprise"
+      canonical="https://supabase.com/enterprise"
       title={content.metadata.metaTitle}
       description={content.metadata.metaDescription}
       openGraph={{
