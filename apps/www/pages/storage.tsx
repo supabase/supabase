c6a1eaff434500bd08463c069b5cafa1ebc915d4
Aleksi Immonen <aleksi@supabase.io>
2026-06-12 13:07:32 +0300

    fix(www): add canonical tag to storage page

--- apps/www/pages/storage.tsx
+++ apps/www/pages/storage.tsx
@@ -43,6 +43,7 @@ function StoragePage() {
   return (
     <>
       <NextSeo
+              canonical="https://supabase.com/storage"
         title={meta_title}
         description={meta_description}
         openGraph={{
