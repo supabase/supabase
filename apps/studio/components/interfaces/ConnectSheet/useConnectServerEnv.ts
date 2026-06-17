69b583d8f99903b84387080c11e3ef144fcec1a3
Ali Waseem <waseema393@gmail.com>
2026-06-17 13:01:30 -0600

    updated connect server formatting

--- apps/studio/components/interfaces/ConnectSheet/useConnectServerEnv.ts
+++ apps/studio/components/interfaces/ConnectSheet/useConnectServerEnv.ts
@@ -43,7 +43,6 @@ export interface UseConnectServerEnvResult {
   buildEnv: () => Promise<string>
 }
 
-
 export function useConnectServerEnv(): UseConnectServerEnvResult {
   const { ref: projectRef } = useParams()
 
