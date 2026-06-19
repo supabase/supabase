0722d7badccf9276245240f8d2c9e241e59e5ade
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2026-06-19 17:31:08 +0200

    adjust home page hero spacing

--- apps/www/app/(home)/_components/Hero.tsx
+++ apps/www/app/(home)/_components/Hero.tsx
@@ -8,14 +8,14 @@ export function Hero() {
   const sendTelemetryEvent = useSendTelemetryEvent()
 
   return (
-    <div className="mx-auto max-w-[var(--container-max-w,75rem)] px-6 pt-32 pb-24 md:pt-40 md:pb-32">
+    <div className="mx-auto max-w-(--container-max-w,75rem) px-6 pt-12 pb-24 md:pt-40 md:pb-32">
       <div className="flex flex-col gap-6 lg:gap-8">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-16 items-end">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 items-end">
           <h1 className="text-foreground text-4xl sm:text-5xl sm:leading-none">
             <span className="block">Build in a weekend</span>
             <span className="text-brand block">Scale to millions</span>
           </h1>
-          <p className="text-foreground-lighter text-sm text-balance">
+          <p className="text-foreground-lighter text-sm lg:text-base text-balance">
             Start your project with a Postgres database. Add Authentication, Data APIs, Edge
             Functions, Realtime Data, Storage, and Vector embeddings.
           </p>
