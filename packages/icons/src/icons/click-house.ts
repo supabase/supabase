1cce3b707f739a53c60edce484688d54728d5271
Jordan McQueen <j@jm.dev>
2026-06-23 11:47:09 +0900

    Fix ClickHouse icon to match the official logo

--- packages/icons/src/icons/click-house.ts
+++ packages/icons/src/icons/click-house.ts
@@ -4,7 +4,7 @@ import createSupabaseIcon from '../createSupabaseIcon';
  * @component @name ClickHouse
  * @description Supabase SVG icon component, renders SVG Element with children.
  *
- * @preview ![img](data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cGF0aCBkPSJNNCAzVjIxTTkuMzMgM1YyMU0xNC42NyAzVjIxTTIwIDNWMjFNMTQuNjcgMTVIMjAiIHN0cm9rZT0iIzAwMCIgc3R5bGU9ImJhY2tncm91bmQtY29sb3I6ICNmZmY7IGJvcmRlci1yYWRpdXM6IDJweCIgc3Ryb2tlLXdpZHRoPSIxLjUiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIvPgo8L3N2Zz4K)
+ * @preview ![img](data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cGF0aCBkPSJNMyAzVjIxTTcuNSAzVjIxTTEyIDNWMjFNMTYuNSAzVjIxTTIxIDEwVjE0IiBzdHJva2U9IiMwMDAiIHN0eWxlPSJiYWNrZ3JvdW5kLWNvbG9yOiAjZmZmOyBib3JkZXItcmFkaXVzOiAycHgiIHN0cm9rZS13aWR0aD0iMS41IiBzdHJva2UtbGluZWNhcD0icm91bmQiLz4KPC9zdmc+Cg==)
  *
  * @param {Object} props - Supabase icons props and any valid SVG attribute
  * @returns {JSX.Element} JSX Element
@@ -16,11 +16,11 @@ const ClickHouse = createSupabaseIcon(
     [
       'path',
       {
-        d: 'M4 3V21M9.33 3V21M14.67 3V21M20 3V21M14.67 15H20',
+        d: 'M3 3V21M7.5 3V21M12 3V21M16.5 3V21M21 10V14',
         stroke: 'currentColor',
         'stroke-width': '1.5',
         'stroke-linecap': 'round',
-        key: 'j9858c',
+        key: '1ho44o',
       },
     ],
   ],
