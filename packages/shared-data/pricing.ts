d2b2ecdc217d823c0d46eb5b963cbbc1d5029cf8
Riccardo Busetti <riccardo.busetti@supabase.io>
2026-06-24 15:20:17 +0200

    Fix

--- packages/shared-data/pricing.ts
+++ packages/shared-data/pricing.ts
@@ -197,7 +197,7 @@ export const pricing: Pricing = {
       },
       {
         key: 'database.replication',
-        title: 'Supabase Pipelines',
+        title: 'Pipelines',
         plans: {
           free: false,
           pro: [
