7541615923b0d3f783e39b55cf03043a5322ef34
7ttp <117663341+7ttp@users.noreply.github.com>
2026-06-25 18:33:58 +0530

    prettier

--- examples/edge-functions/supabase/functions/tweet-to-image/index.ts
+++ examples/edge-functions/supabase/functions/tweet-to-image/index.ts
@@ -36,20 +36,22 @@ async function handler(req: Request, ctx) {
     const tweet = tweets[0]
     console.log('formattedTweets', JSON.stringify(tweets, null, 2))
     const generatedImage = new ImageResponse(
-      <div
-        style={{
-          height: '100%',
-          width: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          backgroundColor: '#1c1c1c',
-          color: '#EDEDED',
-        }}
-      >
-        <Tweet {...tweet} />
-      </div>,
+      React.createElement(
+        'div',
+        {
+          style: {
+            height: '100%',
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: '#1c1c1c',
+            color: '#EDEDED',
+          },
+        },
+        React.createElement(Tweet, { ...tweet })
+      ),
       {
         width: 1200,
         height: 630,
