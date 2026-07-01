1583c68a3c21d78c1c99af2fb6d0bbd30e7e8246
Miranda Limonczenko <miranda.limonczenko@supabase.io>
2026-07-01 16:29:12 -0700

    Use standard button conventions

--- apps/docs/components/SkipToContent.tsx
+++ apps/docs/components/SkipToContent.tsx
@@ -1,10 +1,21 @@
 import { DOCS_CONTENT_CONTAINER_ID } from '~/features/ui/helpers.constants'
+import Link from 'next/link'
+import { Button, cn } from 'ui'
 
 const SkipToContent = () => {
   return (
-    <a href={`#${DOCS_CONTENT_CONTAINER_ID}`} className="skip-link">
-      Skip to content
-    </a>
+    <Button
+      size="medium"
+      asChild
+      className={cn(
+        'fixed top-0 left-4 z-[100] w-auto',
+        '-translate-y-full focus-visible:translate-y-4',
+        'transition-transform duration-200 ease-out',
+        'shadow-lg'
+      )}
+    >
+      <Link href={`#${DOCS_CONTENT_CONTAINER_ID}`}>Skip to content</Link>
+    </Button>
   )
 }
 
