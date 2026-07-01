69cbff7eb4fcc775233ec0200a2de2099027a31e
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2026-07-01 10:14:49 +0200

    refine docs font-sizes

--- apps/docs/features/docs/GuidesMdx.template.tsx
+++ apps/docs/features/docs/GuidesMdx.template.tsx
@@ -101,7 +101,7 @@ const GuideTemplate = ({
               <ReactMarkdown>{meta?.title || 'Supabase Docs'}</ReactMarkdown>
             </h1>
             {meta?.subtitle && (
-              <div className="mt-3 text-xl text-foreground-light [&>p]:m-0">
+              <div className="mt-6 not-prose [&_p]:text-xl [&_p]:leading-7 text-foreground-light [&>p]:m-0">
                 <ReactMarkdown>{meta.subtitle}</ReactMarkdown>
               </div>
             )}
