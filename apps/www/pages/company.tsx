8f8f316952ca04936f44c5908171a1aebe3531c2
Shane <o@shaneermitano.com>
2026-07-07 11:40:19 -0500

    content(www): lowercase billion in funding copy

--- apps/www/pages/company.tsx
+++ apps/www/pages/company.tsx
@@ -163,7 +163,7 @@ const Investors = () => {
           title="Our investors"
           paragraph={
             <>
-              We've raised $1 Billion in funding, backed by some of the world's leading investors.
+              We've raised $1 billion in funding, backed by some of the world's leading investors.
             </>
           }
         />
