73a8f32645734264433e2fb2466aa79740065aa5
Ivan Vasilov <vasilov.ivan@gmail.com>
2026-07-23 11:21:50 +0200

    Transform to null if the string is empty.

--- apps/studio/components/interfaces/UnifiedLogs/UnifiedLogs.queries.ts
+++ apps/studio/components/interfaces/UnifiedLogs/UnifiedLogs.queries.ts
@@ -105,11 +105,11 @@ const LEVEL_EXPR: SafeLogSqlFragment = safeSql`CASE
     END`
 
 // The `auth_user` column is derived from the source-specific fields that can be attributed to a user.
-const AUTH_USER_EXPR: SafeLogSqlFragment = safeSql`CASE
+const AUTH_USER_EXPR: SafeLogSqlFragment = safeSql`nullIf(CASE
       WHEN source = 'auth_logs' THEN log_attributes['auth_event.actor_id']
       WHEN source = 'edge_logs' THEN log_attributes['request.sb.jwt.authorization.payload.subject']
-      ELSE auth_user
-    END`
+      ELSE null
+    END, '')`
 
 const logTypeWhereCondition = (logTypes: string[]): SafeLogSqlFragment => {
   const effective = logTypes.filter((t) => t in LOG_TYPE_CONDITION)
