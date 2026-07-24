0d494eae322d67fab6104c5532bbd100174660ad
7ttp <117663341+7ttp@users.noreply.github.com>
2026-07-25 01:48:57 +0530

    conflict

--- apps/studio/components/interfaces/Database/Hooks/FormContents.tsx
+++ apps/studio/components/interfaces/Database/Hooks/FormContents.tsx
@@ -112,7 +112,7 @@ export const FormContents = ({ form, selectedHook }: FormContentsProps) => {
     httpHeaders,
     httpUrl,
     isSuccessEdgeFunctions,
-    legacyServiceRole,
+    edgeFunctionApiKey,
     ref,
     restUrl,
   ])
