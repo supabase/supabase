3d70b14f2ed894de39701034d4b33f807fded2d7
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2026-07-27 17:08:22 +0200

    Improve error message

--- apps/studio/app/api/scoped-access-token-permissions/route.ts
+++ apps/studio/app/api/scoped-access-token-permissions/route.ts
@@ -59,7 +59,7 @@ export async function GET() {
     }
 
     return NextResponse.json(
-      { error: 'Unable to scoped token permission scope map at this time' },
+      { error: 'Unable to fetch scoped token permission scope map at this time' },
       { status: errorCode, headers }
     )
   }
