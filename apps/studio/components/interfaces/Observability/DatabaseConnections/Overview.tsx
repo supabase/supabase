598b99d46826889bbc98080b9b1c929ac598170e
Joshen Lim <joshenlimek@gmail.com>
2026-07-27 18:14:01 +0800

    Use table for roles tooltip instead

--- apps/studio/components/interfaces/Observability/DatabaseConnections/Overview.tsx
+++ apps/studio/components/interfaces/Observability/DatabaseConnections/Overview.tsx
@@ -135,12 +135,15 @@ export const Overview = ({ live }: OverviewProps) => {
               <MetricCardLabel
                 tooltip={
                   <div>
-                    <p className="text-foreground-light pr-2">Connections by roles:</p>
-                    {rolesWithActiveConnections.map((role) => (
-                      <div key={role.id} className="flex items-center">
-                        <p className="min-w-32">{role.name}:</p> {role.activeConnections}
-                      </div>
-                    ))}
+                    <p className="text-foreground-light">Connections by roles:</p>
+                    <table>
+                      {rolesWithActiveConnections.map((role) => (
+                        <tr key={role.id}>
+                          <td>{role.name}:</td>
+                          <td className="pl-2">{role.activeConnections}</td>
+                        </tr>
+                      ))}
+                    </table>
                   </div>
                 }
               >
