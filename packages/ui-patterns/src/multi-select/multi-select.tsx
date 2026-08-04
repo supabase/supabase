0b04cbe5f37fa29e45eb58d0dc3cb47f46c6541a
kemal <hello@kemal.earth>
2026-08-04 13:15:12 +0100

    fix: height and padding

--- packages/ui-patterns/src/multi-select/multi-select.tsx
+++ packages/ui-patterns/src/multi-select/multi-select.tsx
@@ -293,8 +293,8 @@ const MultiSelectorTrigger = React.forwardRef<HTMLButtonElement, MultiSelectorTr
           type="button"
           role="combobox"
           className={cn(
-            'flex w-full min-w-[200px] min-h-[40px] items-center justify-between rounded-md border',
-            'border-alternative bg-control px-3 py-2 text-sm',
+            'flex w-full min-w-[200px] min-h-[34px] items-center justify-between rounded-md border',
+            'border-alternative bg-control px-3 py-0 text-sm',
             'placeholder:text-muted-foreground',
             'focus-ring',
             'disabled:cursor-not-allowed disabled:opacity-50',
