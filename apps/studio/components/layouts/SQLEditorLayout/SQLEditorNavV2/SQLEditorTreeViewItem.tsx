f8592fb64f67e04b9dbd4cccaf9c4c90ecadf0bf
Charis <26616127+charislam@users.noreply.github.com>
2026-08-05 11:53:26 -0400

    Revert "refactor(studio): static tab kind registry and tab id codec (PR 1/9) …"
    This reverts commit ffd6d6636ddfcb5956d0c1e4799777cbfaf48980.

--- apps/studio/components/layouts/SQLEditorLayout/SQLEditorNavV2/SQLEditorTreeViewItem.tsx
+++ apps/studio/components/layouts/SQLEditorLayout/SQLEditorNavV2/SQLEditorTreeViewItem.tsx
@@ -30,6 +30,7 @@ import {
 
 import { getSnippetSource } from '@/components/interfaces/SQLEditor/querySource'
 import { createSqlSnippetSkeletonV2 } from '@/components/interfaces/SQLEditor/SQLEditor.utils'
+import { LogsSnippetIcon } from '@/components/ui/EntityTypeIcon'
 import { getContentById, getSqlSnippetById } from '@/data/content/content-id-query'
 import { useSQLSnippetFolderContentsQuery } from '@/data/content/sql-folder-contents-query'
 import { Snippet } from '@/data/content/sql-folders-query'
@@ -43,7 +44,6 @@ import {
   type FolderStatus,
 } from '@/state/sql-editor/sql-editor-lifecycle'
 import { useSqlEditorV2StateSnapshot } from '@/state/sql-editor/sql-editor-state'
-import { LogsSnippetIcon } from '@/state/tabs/kinds.icons'
 
 interface SQLEditorTreeViewItemProps extends Omit<
   ComponentProps<typeof TreeViewItem>,
