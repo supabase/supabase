ae89621ef80aa15741fc35f7308c044b03bb50cd
himanshu-2l <himanshu-2l@users.noreply.github.com>
2026-08-06 23:39:36 +0530

    fix(studio): replace __dirname with dirname in vitest.config.ts

--- apps/studio/vitest.config.ts
+++ apps/studio/vitest.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@ui': resolve(__dirname, './../../packages/ui/src'),
+      '@ui': resolve(dirname, './../../packages/ui/src'),
     },
   },
   test: {
