81520feb457d8d8a0523b78f40f05e6f7870c27c
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2026-08-27 15:33:26 +0200

    move elastic.svg

--- apps/docs/components/MetricsStackCards.tsx
+++ apps/docs/components/MetricsStackCards.tsx
@@ -1,14 +1,15 @@
-import { Datadog, Elastic, Grafana } from 'icons'
+import { Datadog, Grafana } from 'icons'
 import { Flame } from 'lucide-react'
 import Link from 'next/link'
 import type { ReactNode } from 'react'
 
+import { ElasticLogo } from './icons/ElasticLogo'
 import { metricsStackOptions, type MetricsStackOption } from './MetricsStackCards.data'
 
 const ICONS: Record<MetricsStackOption['iconKind'], ReactNode> = {
   grafana: <Grafana className="h-5 w-5" />,
   datadog: <Datadog className="h-5 w-5" />,
-  elastic: <Elastic className="h-5 w-5" />,
+  elastic: <ElasticLogo className="h-5 w-5" />,
   flame: <Flame className="h-5 w-5" strokeWidth={1.5} />,
 }
 
