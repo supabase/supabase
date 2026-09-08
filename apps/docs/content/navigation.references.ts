af5705eca31b92c597cb7e1f65c34ba47f55abe6
tr00d <guillaume.faas@live.fr>
2026-09-08 08:48:37 +0200

    docs(csharp): add new C# Reference for v8.0.0

--- apps/docs/content/navigation.references.ts
+++ apps/docs/content/navigation.references.ts
@@ -84,9 +84,13 @@ export const REFERENCES = {
     name: 'C#',
     library: 'supabase-csharp',
     libPath: 'csharp',
-    versions: ['v1', 'v0'],
+    versions: ['v8', 'v1', 'v0'],
     icon: 'reference-csharp',
     meta: {
+      v8: {
+        libId: 'reference_csharp_v8',
+        specFile: 'supabase_csharp_v8',
+      },
       v1: {
         libId: 'reference_csharp_v1',
         specFile: 'supabase_csharp_v1',
