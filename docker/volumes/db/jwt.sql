27553ecd8fee866b8b0cb5190626058de07aaae6
Mandar Joshi <mandarjoshi1045@gmail.com>
2026-09-17 00:39:00 +0530

    fix: selfhost custom db in jwt.sql

--- docker/volumes/db/jwt.sql
+++ docker/volumes/db/jwt.sql
@@ -1,3 +1,4 @@
 \set jwt_exp `echo "$JWT_EXP"`
+\set pgdb `echo "$POSTGRES_DB"`
 
-ALTER DATABASE postgres SET "app.settings.jwt_exp" TO :'jwt_exp';
+ALTER DATABASE :"pgdb" SET "app.settings.jwt_exp" TO :'jwt_exp';
