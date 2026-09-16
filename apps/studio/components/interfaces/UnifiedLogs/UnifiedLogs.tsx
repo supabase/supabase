68496a1dba62305a4aa8f34ad2784dcf584d17e2
Joshen Lim <joshenlimek@gmail.com>
2026-09-16 19:20:44 +0800

    Update unified logs queries to fetch status, method and pathname properly for storage logs

--- apps/studio/components/interfaces/UnifiedLogs/UnifiedLogs.tsx
+++ apps/studio/components/interfaces/UnifiedLogs/UnifiedLogs.tsx
@@ -226,6 +226,8 @@ export const UnifiedLogs = () => {
   }, [rawFlatData])
   const liveMode = useLiveMode(flatData)
 
+  console.log(flatData)
+
   const totalDBRowCount = counts?.totalRowCount
   const filterDBRowCount = flatData.length
 
