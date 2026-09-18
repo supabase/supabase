4a7b92a9216a97bec7e7933a558e0a568f2872e0
Jordi Enric <jordi.err@gmail.com>
2026-09-18 16:14:46 +0200

    rm output: 'standalone'

--- apps/studio/next.config.ts
+++ apps/studio/next.config.ts
@@ -53,7 +53,6 @@ const marketplaceApiProtocol: 'http' | 'https' | null =
 const nextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: getAssetPrefix(),
-  output: 'standalone',
   experimental: {
     clientRouterFilter: false,
   },
