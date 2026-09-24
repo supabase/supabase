0aaf850784edce0c5dbd7ec5135c04891b5e4108
Danny White <3104761+dnywh@users.noreply.github.com>
2026-09-24 12:28:19 +1000

    standardise table editor dropdown triggers

--- apps/studio/components/interfaces/TableGridEditor/SidePanelEditor/ColumnEditor/InputWithSuggestions.tsx
+++ apps/studio/components/interfaces/TableGridEditor/SidePanelEditor/ColumnEditor/InputWithSuggestions.tsx
@@ -6,7 +6,7 @@
 // the component over to the UI library
 
 import { noop } from 'lodash'
-import { List } from 'lucide-react'
+import { ChevronDown } from 'lucide-react'
 import { useEffect, useRef, useState } from 'react'
 import {
   DropdownMenu,
@@ -112,7 +112,7 @@ const InputWithSuggestions = ({
                       content: { text: 'Choose a suggested value for this column', side: 'bottom' },
                     }}
                   >
-                    <List strokeWidth={1.5} size={14} />
+                    <ChevronDown className="text-foreground-lighter" strokeWidth={1.5} size={16} />
                   </ButtonTooltip>
                 </DropdownMenuTrigger>
 
